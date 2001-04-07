92e4c93d9582bdbfa9514fa0f10686c0386f751a
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2001-04-07 21:12:54 +0000

    Additional parameters for monitoring. Changed names for some of parameters.
    git-svn-id: svn://svn.zabbix.com/trunk@30 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_agent/sysinfo.h
+++ src/zabbix_agent/sysinfo.h
@@ -6,11 +6,17 @@ float	FILESIZE(const char * filename);
 float	DF(const char * mountPoint);
 float	getPROC(char *file,int lineno,int fieldno);
 float	FREEMEM(void);
+float	TOTALMEM(void);
+float	SHAREDMEM(void);
+float	BUFFERSMEM(void);
+float	CACHEDMEM(void);
 float	PING(void);
 float	PROCLOAD(void);
 float	PROCLOAD5(void);
 float	PROCLOAD15(void);
 float	SWAPFREE(void);
+float	SWAPTOTAL(void);
+float	UPTIME(void);
 float	EXECUTE(char *command);
 
 #endif
