1dec1ac9b26abb87fb6644cf31456adeb59fe882
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2001-06-19 14:15:52 +0000

    Added columns items.snmp_oid and items.snmp_community.
    git-svn-id: svn://svn.zabbix.com/trunk@86 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ upgrades/dbpatches/1.0alpha4_to_1.0alpha5/postgresql/patch.sql
@@ -0,0 +1,3 @@
+alter table items add type int4 DEFAULT '0' NOT NULL;
+alter table items add snmp_community varchar(64) DEFAULT '' NOT NULL;
+alter table items add snmp_oid varchar(255) DEFAULT '' NOT NULL;
