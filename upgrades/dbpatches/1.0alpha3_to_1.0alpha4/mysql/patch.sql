42cfc24064dd98d1d3ff5af191d3d8c82b1617fc
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2001-06-19 06:39:16 +0000

    Added column type for table items.
    git-svn-id: svn://svn.zabbix.com/trunk@84 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.0alpha3_to_1.0alpha4/mysql/patch.sql
+++ upgrades/dbpatches/1.0alpha3_to_1.0alpha4/mysql/patch.sql
@@ -1,10 +1,1 @@
-alter table items_template drop platformid;
-alter table hosts drop platformid;
-
-drop table platforms;
-
-alter table media add key (userid);
-alter table actions add key (triggerid);
-alter table triggers add key (istrue);
-alter table items add key (nextcheck);
-alter table items add key (status);
+alter table items add type int(4) DEFAULT '0' NOT NULL;
