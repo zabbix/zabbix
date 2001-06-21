42622bc33f95340e9ad0d6249aef09ada8569781
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2001-06-21 14:31:14 +0000

    Removed duplicate items.description
    git-svn-id: svn://svn.zabbix.com/trunk@94 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- create/mysql/schema.sql
+++ create/mysql/schema.sql
@@ -142,7 +142,6 @@ CREATE TABLE items (
 	type		int(4) DEFAULT '0' NOT NULL,
 	snmp_community	varchar(64) DEFAULT '' NOT NULL,
 	snmp_oid	varchar(255) DEFAULT '' NOT NULL,
-	description	varchar(255) DEFAULT '' NOT NULL,
 	hostid		int(4) NOT NULL,
 	description	varchar(255) DEFAULT '' NOT NULL,
 	key_		varchar(64) DEFAULT '' NOT NULL,
