ce63e31b8fe11d0d883f0052499f619aa9749eee
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2001-12-19 17:13:27 +0000

    - number of forked zabbix_agentd decreased to 5 (Alexei)  - support for parameter UserParameter in zabbix_agentd.conf (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@267 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- include/common.h
+++ include/common.h
@@ -13,12 +13,12 @@
 #define ITEM_TYPE_ZABBIX 0
 #define ITEM_TYPE_SNMP   1
 
-#define	AGENTD_FORKS	10
+#define	AGENTD_FORKS	5
 
 #define	TRAPPERD_FORKS	10
 
 #define	SUCKER_FORKS	11
-#define	SUCKER_DELAY	30
+#define	SUCKER_DELAY	60
 
 /* Housekeeping frequency */
 #define	SUCKER_HK	3600
