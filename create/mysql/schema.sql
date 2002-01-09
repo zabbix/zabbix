dc07909ad9445b2f5985ae5dd2ce4edf39f989a7
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2002-01-09 07:16:10 +0000

    - even more support for IT Services (Alexei)  - added column status to table services (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@276 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- create/mysql/schema.sql
+++ create/mysql/schema.sql
@@ -5,8 +5,10 @@
 CREATE TABLE services (
   serviceid		int(4)		NOT NULL auto_increment,
   name			varchar(128)	DEFAULT '' NOT NULL,
+  status		int(1)		DEFAULT '0' NOT NULL,
   triggerid		int(4),
-  PRIMARY KEY (serviceid)
+  PRIMARY KEY (serviceid),
+  KEY (triggerid)
 );
 
 #
@@ -18,7 +20,8 @@ CREATE TABLE services_links (
   servicedownid		int(4)		DEFAULT '0' NOT NULL,
   soft			int(1)		DEFAULT '0' NOT NULL,
   KEY (serviceupid),
-  KEY (servicedownid)
+  KEY (servicedownid),
+  UNIQUE (serviceupid,servicedownid)
 );
 
 #
