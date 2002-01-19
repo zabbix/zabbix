7cb348ad1e63605c7766d07abb1a886a65b6ee7b
Alexei Vladishev <alexei.vladishev@zabbix.com>
2002-01-19 19:25:23 +0000

    - support for configuration file for zabbix_trapper(d) (Alexei)

--- src/zabbix_trapper/zabbix_trapper.c
+++ src/zabbix_trapper/zabbix_trapper.c
@@ -27,6 +27,10 @@
 #include "db.h"
 #include "functions.h"
 
+char	*CONFIG_DBNAME		= NULL;
+char	*CONFIG_DBUSER		= NULL;
+char	*CONFIG_DBPASSWORD	= NULL;
+
 void	signal_handler( int sig )
 {
 	if( SIGALRM == sig )
@@ -41,6 +45,100 @@ void	signal_handler( int sig )
 //		fprintf(stderr,"\nGot QUIT or INT or TERM signal. Exiting..." );
 	}
 	exit( FAIL );
+}
+
+
+void	process_config_file(void)
+{
+	FILE	*file;
+	char	line[1024];
+	char	parameter[1024];
+	char	*value;
+	int	lineno;
+
+
+	file=fopen("/etc/zabbix/zabbix_trapperd.conf","r");
+	if(NULL == file)
+	{
+		syslog( LOG_CRIT, "Cannot open /etc/zabbix/zabbix_trapperd.conf");
+		exit(1);
+	}
+
+	lineno=1;
+	while(fgets(line,1024,file) != NULL)
+	{
+		if(line[0]=='#')	continue;
+		if(strlen(line)==1)	continue;
+
+		strcpy(parameter,line);
+
+		value=strstr(line,"=");
+
+		if(NULL == value)
+		{
+			syslog( LOG_CRIT, "Error in line [%s] Line %d", line, lineno);
+			fclose(file);
+			exit(1);
+		}
+		value++;
+		value[strlen(value)-1]=0;
+
+		parameter[value-line-1]=0;
+
+		syslog( LOG_DEBUG, "Parameter [%s] Value [%s]", parameter, value);
+
+		if(strcmp(parameter,"DebugLevel")==0)
+		{
+			if(strcmp(value,"1") == 0)
+			{
+				setlogmask(LOG_UPTO(LOG_CRIT));
+			}
+			else if(strcmp(value,"2") == 0)
+			{
+				setlogmask(LOG_UPTO(LOG_WARNING));
+			}
+			else if(strcmp(value,"3") == 0)
+			{
+				setlogmask(LOG_UPTO(LOG_DEBUG));
+			}
+			else
+			{
+				syslog( LOG_CRIT, "Wrong DebugLevel in line %d", lineno);
+				fclose(file);
+				exit(1);
+			}
+		}
+		else if(strcmp(parameter,"DBName")==0)
+		{
+			CONFIG_DBNAME=(char *)malloc(strlen(value));
+			strcpy(CONFIG_DBNAME,value);
+		}
+		else if(strcmp(parameter,"DBUser")==0)
+		{
+			CONFIG_DBUSER=(char *)malloc(strlen(value));
+			strcpy(CONFIG_DBUSER,value);
+		}
+		else if(strcmp(parameter,"DBPassword")==0)
+		{
+			CONFIG_DBPASSWORD=(char *)malloc(strlen(value));
+			strcpy(CONFIG_DBPASSWORD,value);
+		}
+		else
+		{
+			syslog( LOG_CRIT, "Unsupported parameter [%s] Line %d", parameter, lineno);
+			fclose(file);
+			exit(1);
+		}
+
+		lineno++;
+	}
+	fclose(file);
+	
+	if(CONFIG_DBNAME == NULL)
+	{
+		syslog( LOG_CRIT, "DBName not in config file");
+		exit(1);
+	}
 }
 
 int	main()
@@ -63,6 +161,8 @@ int	main()
 	openlog("zabbix_trapper",LOG_PID,LOG_USER);
 	//	ret=setlogmask(LOG_UPTO(LOG_DEBUG));
 	ret=setlogmask(LOG_UPTO(LOG_WARNING));
+
+	process_config_file();
 	
 	fgets(s,1024,stdin);
 	for( p=s+strlen(s)-1; p>s && ( *p=='\r' || *p =='\n' || *p == ' ' ); --p );
@@ -88,7 +188,7 @@ int	main()
 	value=atof(value_string);
 
 
-	DBconnect();
+	DBconnect(CONFIG_DBNAME, CONFIG_DBUSER, CONFIG_DBPASSWORD);
 
 	ret=process_data(server,key,value);
 
