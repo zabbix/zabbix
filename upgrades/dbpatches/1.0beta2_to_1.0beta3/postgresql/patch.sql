c760349138df71f67cf55a87596326d6ba6c8a04
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2002-05-13 17:13:28 +0000

    Minor changes
    git-svn-id: svn://svn.zabbix.com/trunk@363 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/1.0beta2_to_1.0beta3/postgresql/patch.sql
+++ upgrades/dbpatches/1.0beta2_to_1.0beta3/postgresql/patch.sql
@@ -25,6 +25,19 @@ CREATE TABLE services_links (
   PRIMARY KEY (linkid)
 );
 
+CREATE TABLE rights (
+  rightid               serial,
+  userid                int4          DEFAULT '' NOT NULL,
+  name                  char(255)       DEFAULT '' NOT NULL,
+  permission            char(1)         DEFAULT '' NOT NULL,
+  id                    int4,
+  PRIMARY KEY (rightid)
+);
+
+
+insert into rights (rightid,userid,name,permission,id) values (1,1,"Default permission","U",0);
+insert into rights (rightid,userid,name,permission,id) values (2,1,"Default permission","A",0);
+
 alter table users drop groupid;
 alter table config drop password_required;
 
@@ -36,6 +49,5 @@ insert into items_template (itemtemplateid,description,key_,delay,value_type)
         values (67,'Version of zabbix_agent(d) running','version[zabbix_agent]',3600, 1);
 insert into items_template (itemtemplateid,description,key_,delay,value_type)
         values (68,'WEB (HTTP) server is running','check_service[http]', 60, 0);
-insert into triggers_template (triggertemplateid,itemtemplateid,description,expr
-ession)
+insert into triggers_template (triggertemplateid,itemtemplateid,description,expression)
         values (68,68,'WEB (HTTP) server is down on %s','{:.last(0)}<1');
