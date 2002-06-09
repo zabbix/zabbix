1648772e2b5a676e7d5e47923519cb4d1d7eafbc
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2002-06-09 19:52:04 +0000

    - added table 'sessions' (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@408 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/index.php
+++ frontends/php/index.php
@@ -4,15 +4,39 @@
 
 	include "include/config.inc";
 
+	if(isset($register)&&($register=="Enter"))
+	{
+		$password=md5($password);
+		$sql="select u.userid,u.alias,u.name,u.surname from users u where u.alias='$name' and u.passwd='$password'";
+		$result=DBselect($sql);
+		if(DBnum_rows($result)==1)
+		{
+			$USER_DETAILS["userid"]=DBget_field($result,0,0);
+			$USER_DETAILS["alias"]=DBget_field($result,0,1);
+			$USER_DETAILS["name"]=DBget_field($result,0,2);
+			$USER_DETAILS["surname"]=DBget_field($result,0,3);
+			$sessionid=md5(time().$password.$name.rand(0,10000000));
+			setcookie("sessionid",$sessionid,time()+3600);
+			$sql="insert into sessions (sessionid,userid,lastaccess) values ('$sessionid',".$USER_DETAILS["userid"].",".time().")";
+			DBexecute($sql);
+		}
+	}
+
 	show_header($page["title"],0,0);
+
 ?>
 
 <?
-	echo "<center>";
-	echo "<font face=\"arial,helvetica\" size=2>";
-	echo "Connected as ".$USER_DETAILS["alias"]."</b>";
-	echo "</font>";
-	echo "</center>";
+	echo "Session ID:",$sessionid,"<br>";
+//	if(!isset($sessionid))
+	{
+		insert_login_form();
+	}
+//	echo "<center>";
+//	echo "<font face=\"arial,helvetica\" size=2>";
+//	echo "Connected as ".$USER_DETAILS["alias"]."</b>";
+//	echo "</font>";
+//	echo "</center>";
 ?>
 
 <?
