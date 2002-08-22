15f955a9e8abcd424c85344473d669319b42519c
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2002-08-22 17:58:55 +0000

    Minor changes
    git-svn-id: svn://svn.zabbix.com/trunk@461 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/index.php
+++ frontends/php/index.php
@@ -4,16 +4,44 @@
 
 	include "include/config.inc.php";
 
+	$password=$HTTP_POST_VARS["password"];
+	$name=$HTTP_POST_VARS["name"];
+	if(isset($HTTP_POST_VARS["register"]))
+	{
+		$register=$HTTP_POST_VARS["register"];
+	}
+	else
+	{
+		unset($register);
+	}
 	if(isset($HTTP_GET_VARS["reconnect"]))
 	{
-		setcookie("sessionid",$HTTP_COOKIE_VARS["sessionid"],time()-3600);
-		unset($HTTP_COOKIE_VARS["sessionid"]);
+		$reconnect=$HTTP_GET_VARS["reconnect"];
+	}
+	else
+	{
+		unset($reconnect);
+	}
+	if(isset($HTTP_COOKIE_VARS["sessionid"]))
+	{
+		$sessionid=$HTTP_COOKIE_VARS["sessionid"];
+	}
+	else
+	{
+		unset($sessionid);
 	}
 
-	if(isset($HTTP_POST_VARS["register"])&&($HTTP_POST_VARS["register"]=="Enter"))
+
+	if(isset($reconnect))
 	{
-		$password=md5($HTTP_POST_VARS["password"]);
-		$sql="select u.userid,u.alias,u.name,u.surname from users u where u.alias='".$HTTP_POST_VARS["name"]."' and u.passwd='$password'";
+		setcookie("sessionid",$sessionid,time()-3600);
+		unset($sessionid);
+	}
+
+	if(isset($register)&&($register=="Enter"))
+	{
+		$password=md5($password);
+		$sql="select u.userid,u.alias,u.name,u.surname from users u where u.alias='$name' and u.passwd='$password'";
 		$result=DBselect($sql);
 		if(DBnum_rows($result)==1)
 		{
@@ -21,20 +49,20 @@
 			$USER_DETAILS["alias"]=DBget_field($result,0,1);
 			$USER_DETAILS["name"]=DBget_field($result,0,2);
 			$USER_DETAILS["surname"]=DBget_field($result,0,3);
-			$sessionid=md5(time().$password.$HTTP_POST_VARS["name"].rand(0,10000000));
-			setcookie("sessionid",$HTTP_COOKIE_VARS["sessionid"],time()+3600);
-			$sql="insert into sessions (sessionid,userid,lastaccess) values ('".$HTTP_COOKIE_VARS["sessionid"]."',".$USER_DETAILS["userid"].",".time().")";
+			$sessionid=md5(time().$password.$name.rand(0,10000000));
+			setcookie("sessionid",$sessionid,time()+3600);
+			$sql="insert into sessions (sessionid,userid,lastaccess) values ('$sessionid',".$USER_DETAILS["userid"].",".time().")";
 			DBexecute($sql);
 		}
 	}
 
 	show_header($page["title"],0,0);
-
 ?>
 
 <?
 	if(!isset($sessionid))
 	{
+//		echo "-",$HTTP_COOKIE_VARS["sessionid"],"-<br>";
 		insert_login_form();
 	}
 	else
