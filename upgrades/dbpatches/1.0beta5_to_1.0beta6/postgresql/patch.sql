dbadd39ffc2be31094c98769e162573c83ae9db3
Alexei Vladishev <alexei.vladishev@zabbix.com>
2002-09-08 10:00:33 +0000

    Postpone IT Helpdes.

--- upgrades/dbpatches/1.0beta5_to_1.0beta6/postgresql/patch.sql
+++ upgrades/dbpatches/1.0beta5_to_1.0beta6/postgresql/patch.sql
@@ -1,47 +1,18 @@
 alter table functions alter lastvalue varchar(255);
-
+alter table functions alter parameter varchar(255) default '0' not null;
 
 --
--- Table structure for table 'problems'
+-- Table structure for table 'services_alarms'
 --
 
-CREATE TABLE problems (
-  problemid		int4		NOT NULL auto_increment,
-  userid		int4		DEFAULT '0' NOT NULL,
-  triggerid		int4,
-  lastupdate		int4		DEFAULT '0' NOT NULL,
+CREATE TABLE service_alarms (
+  servicealarmid	serial,
+  serviceid		int4		DEFAULT '0' NOT NULL,
   clock			int4		DEFAULT '0' NOT NULL,
-  status		int1		DEFAULT '0' NOT NULL,
-  descripion		varchar(255)	DEFAULT '' NOT NULL,
-  categoryid		int4,
-  priority		int1		DEFAULT '0' NOT NULL,
-  PRIMARY KEY (problemid),
-  KEY (status),
-  KEY (categoryid),
-  KEY (priority)
+  value			int4		DEFAULT '0' NOT NULL,
+  PRIMARY KEY (servicealarmid)
 );
 
---
--- Table structure for table 'categories'
---
+CREATE INDEX services_alarms_serviceid_clock on service_alarms (serviceid,clock);
+CREATE INDEX services_alarms_clock on service_alarms (clock);
 
-CREATE TABLE categories (
-  categoryid		int(4)		NOT NULL auto_increment,
-  descripion		varchar(64)	DEFAULT '' NOT NULL,
-  PRIMARY KEY (categoryid)
-);
-
---
--- Table structure for table 'problems_comments'
---
-
-CREATE TABLE problems_comments (
-  commentid		int(4)		NOT NULL auto_increment,
-  problemid		int(4)		DEFAULT '0' NOT NULL,
-  clock			int(4),
-  status_before		int(1)		DEFAULT '0' NOT NULL,
-  status_after		int(1)		DEFAULT '0' NOT NULL,
-  comment		blob,
-  PRIMARY KEY (commentid),
-  KEY (problemid,clock)
-);
