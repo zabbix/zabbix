21c47cb064b5458ef116413885b9216d60061697
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2002-09-16 11:53:00 +0000

    Fixes for network loads.
    git-svn-id: svn://svn.zabbix.com/trunk@498 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_agent/stats.h
+++ src/zabbix_agent/stats.h
@@ -6,15 +6,15 @@ INTERFACE
 {
 	char    *interface;
 	int	clock[60*15];
-	int	sent[60*15];
-	int	received[60*15];
-	int	sent_load1;
+	float	sent[60*15];
+	float	received[60*15];
+/*	int	sent_load1;
 	int	sent_load5;
 	int	sent_load15;
 	int	received_total;
 	int	received_load1;
 	int	received_load5;
-	int	received_load15;
+	int	received_load15;*/
 };
 
 #endif
