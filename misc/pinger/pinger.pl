8b048d40ae8524cf57e16e0870df5777f8981014
Alexei Vladishev <alexei.vladishev@zabbix.com>
2002-12-07 10:07:36 +0000

    - pinger.pl will use mass-send of collected data. No unnecessary forks    anymore. (Alexei)

--- misc/pinger/pinger.pl
+++ misc/pinger/pinger.pl
@@ -1,18 +1,33 @@
 #!/usr/bin/perl
 
-$hosts = `cat /home/zabbix/pinger/hosts | fping`;
+# CONFIGURATION
+
+$ZABBIX_SERVER="zabbix";
+$ZABBIX_PORT="10001";
+$HOST_FILE="hosts";
+$TMP_FILE="/tmp/zabbix.pinger.tmp";
+
+# END OF CONFIGURATION
+
+$hosts = `cat $HOST_FILE | fping`;
+
+system("rm -f $TMP_FILE");
 
 foreach $host (split(/\n/,$hosts))
 {
 	if($host=~/^((.)*) is alive$/)
 	{
-		$cmd="/home/zabbix/bin/zabbix_sender arsenal 10001 $1:alive 1"; 
+		$cmd="echo $ZABBIX_SERVER $ZABBIX_PORT $1:alive 1 >>$TMP_FILE"; 
 	}
 	else
 	{
 		$host=~/^((.)*) is((.)*)$/;
-		$cmd="/home/zabbix/bin/zabbix_sender arsenal 10001 $1:alive 0"; 
+		$cmd="echo $ZABBIX_SERVER $ZABBIX_PORT $1:alive 0 >>$TMP_FILE"; 
 	}
-	print $cmd,"\n";
+#	print $cmd,"\n";
 	system( $cmd );	
 }
+
+$cmd="zabbix_sender <$TMP_FILE";
+
+system($cmd);
