fd5f249d76f72509710ea8e70472c689057f0d4d
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2002-12-07 11:02:22 +0000

    - added copyright notice to source files (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@582 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- include/expression.c
+++ include/expression.c
@@ -1,3 +1,23 @@
+/* 
+** Zabbix
+** Copyright (C) 2000,2001,2002,2003 Alexei Vladishev
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
