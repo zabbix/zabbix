36f50581903cdddacc3624eb5a971f065a39416d
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2003-01-11 20:46:44 +0000

    - fixed Last100 and Prev100 for both Alarms and Alerts (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@636 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/alerts.php
+++ frontends/php/alerts.php
@@ -13,15 +13,15 @@
 ?>
 
 <?php
-        if(isset($start)&&($start<=0))
+        if(isset($HTTP_GET_VARS["start"])&&($HTTP_GET_VARS["start"]<=0))
         {
-                unset($start);
+                unset($HTTP_GET_VARS["start"]);
         }
-        if(isset($start))
+        if(isset($HTTP_GET_VARS["start"]))
         {
-                echo "[<A HREF=\"alerts.php?start=".($start-100)."\">";
+                echo "[<A HREF=\"alerts.php?start=".($HTTP_GET_VARS["start"]-100)."\">";
                 echo "Show previous 100</A>] ";
-                echo "[<A HREF=\"alerts.php?start=".($start+100)."\">";
+                echo "[<A HREF=\"alerts.php?start=".($HTTP_GET_VARS["start"]+100)."\">";
                 echo "Show next 100</A>]";
         }
         else
@@ -39,13 +39,13 @@
 
 <FONT COLOR="#000000">
 <?php
-	if(!isset($start))
+	if(!isset($HTTP_GET_VARS["start"]))
 	{
-		$sql="select a.alertid,a.clock,a.type,a.sendto,a.subject,a.message,ac.triggerid,a.status,a.retries from alerts a,actions ac where a.actionid=ac.actionid order by a.clock desc limit 1000";
+		$sql="select a.alertid,a.clock,a.type,a.sendto,a.subject,a.message,ac.triggerid,a.status,a.retries from alerts a,actions ac where a.actionid=ac.actionid order by a.clock desc limit 500";
 	}
 	else
 	{
-		$sql="select a.alertid,a.clock,a.type,a.sendto,a.subject,a.message,ac.triggerid,a.status,a.retries from alerts a,actions ac where a.actionid=ac.actionid order by a.clock desc limit ".($start+1000);
+		$sql="select a.alertid,a.clock,a.type,a.sendto,a.subject,a.message,ac.triggerid,a.status,a.retries from alerts a,actions ac where a.actionid=ac.actionid order by a.clock desc limit ".($HTTP_GET_VARS["start"]+500);
 	}
 	$result=DBselect($sql);
 
@@ -63,7 +63,7 @@
 	while($row=DBfetch($result))
 	{
 		$zzz++;	
-		if(isset($start)&&($zzz<$start))
+		if(isset($HTTP_GET_VARS["start"])&&($zzz<$HTTP_GET_VARS["start"]))
 		{
 			continue;
 		}
