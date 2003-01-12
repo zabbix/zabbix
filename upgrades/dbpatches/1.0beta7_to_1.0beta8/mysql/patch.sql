9bb0fbb8a50bcf395bafcf2d2edfba6a96cf0794
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2003-01-12 14:09:54 +0000

    Updated PostgreSQL schema and added patches.
    git-svn-id: svn://svn.zabbix.com/trunk@640 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/1.0beta7_to_1.0beta8/mysql/patch.sql
+++ upgrades/dbpatches/1.0beta7_to_1.0beta8/mysql/patch.sql
@@ -1,1 +1,15 @@
 create unique index hosts_host on hosts (host);
+
+--
+-- Table structure for table 'profiles'
+--
+
+CREATE TABLE profiles (
+  profileid		int(4)		NOT NULL auto_increment,
+  userid		int(4)		DEFAULT '0' NOT NULL,
+  idx			varchar(64)	DEFAULT '' NOT NULL,
+  value			varchar(64)	DEFAULT '' NOT NULL,
+  PRIMARY KEY (profileid),
+  KEY (userid),
+  UNIQUE (userid,idx)
+) type=InnoDB;
