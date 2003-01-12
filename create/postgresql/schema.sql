01f6a122d456422ef05f565be8a9c38a614badfb
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2003-01-12 14:09:54 +0000

    Updated PostgreSQL schema and added patches.
    git-svn-id: svn://svn.zabbix.com/trunk@640 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- create/postgresql/schema.sql
+++ create/postgresql/schema.sql
@@ -433,4 +433,19 @@ CREATE TABLE service_alarms (
 CREATE INDEX services_alarms_serviceid_clock on service_alarms (serviceid,clock);
 CREATE INDEX services_alarms_clock on service_alarms (clock);
 
+--
+-- Table structure for table 'profiles'
+--
+
+CREATE TABLE profiles (
+  profileid		serial,
+  userid		int4		DEFAULT '0' NOT NULL,
+  idx			varchar(64)	DEFAULT '' NOT NULL,
+  value			varchar(64)	DEFAULT '' NOT NULL,
+  PRIMARY KEY (profileid)
+);
+
+CREATE INDEX profiles_userid on profiles (userid);
+CREATE UNIQUE INDEX profiles_userid_idx on profiles (userid,idx);
+
 VACUUM ANALYZE;
--- upgrades/dbpatches/1.0beta7_to_1.0beta8/postgresql/patch.sql
+++ upgrades/dbpatches/1.0beta7_to_1.0beta8/postgresql/patch.sql
@@ -1,1 +1,16 @@
 CREATE UNIQUE INDEX hosts_host on hosts (host);
+
+--
+-- Table structure for table 'profiles'
+--
+
+CREATE TABLE profiles (
+  profileid		serial,
+  userid		int4		DEFAULT '0' NOT NULL,
+  idx			varchar(64)	DEFAULT '' NOT NULL,
+  value			varchar(64)	DEFAULT '' NOT NULL,
+  PRIMARY KEY (profileid)
+);
+
+CREATE INDEX profiles_userid on profiles (userid);
+CREATE UNIQUE INDEX profiles_userid_idx on profiles (userid,idx);
