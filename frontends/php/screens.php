daa5481f0be02ef2aafba98cb9b8cd80a3ec5387
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2003-03-23 09:21:41 +0000

    Support for screen builder. Thanks to Arturs Aboltins. (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@714 97f52cf1-0a1b-0410-bd0e-c28be96e8082

---
+++ frontends/php/screens.php
@@ -0,0 +1,114 @@
+<?php
+	include "include/config.inc.php";
+	$page["title"] = "User defined screens";
+	$page["file"] = "screens.php";
+
+	$nomenu=0;
+	if(isset($HTTP_GET_VARS["fullscreen"]))
+	{
+		$nomenu=1;
+	}
+	if(isset($HTTP_GET_VARS["scid"]))
+	{
+		show_header($page["title"],60,$nomenu);
+	}
+	else
+	{
+		show_header($page["title"],0,$nomenu);
+	}
+?>
+
+<?php
+	if(!isset($HTTP_GET_VARS["fullscreen"]))
+	{
+		show_table_header_begin();
+		echo "SCREENS";
+
+		show_table_v_delimiter();
+
+		echo "<font size=2>";
+
+		$result=DBselect("select scid,name,cols,rows from screens order by name");
+		while($row=DBfetch($result))
+		{
+			if(!check_right("Screen","R",$row["scid"]))
+			{
+				continue;
+			}
+			if( isset($HTTP_GET_VARS["scid"]) && ($HTTP_GET_VARS["scid"] == $row["scid"]) )
+			{
+				echo "<b>[";
+			}
+			echo "<a href='screens.php?scid=".$row["scid"]."'>".$row["name"]."</a>";
+			if(isset($HTTP_GET_VARS["scid"]) && ($HTTP_GET_VARS["scid"] == $row["scid"]) )
+			{
+				echo "]</b>";
+			}
+			echo " ";
+		}
+
+		if(DBnum_rows($result) == 0)
+		{
+			echo "No screens to display";
+		}
+
+		echo "</font>";
+		show_table_header_end();
+		echo "<br>";
+	}
+
+?>
+
+<?php
+	if(isset($HTTP_GET_VARS["scid"]))
+	{
+          $scid=$HTTP_GET_VARS["scid"];
+          $result=DBselect("select name,cols,rows from screens where scid=$scid");
+          $row=DBfetch($result);
+          echo "<TABLE BORDER=1 COLS=".$row["cols"]." align=center WIDTH=100% BGCOLOR=\"#FFFFFF\"";
+          for($r=0;$r<$row["rows"];$r++)
+          {
+          echo "<TR>";
+          for($c=0;$c<$row["cols"];$c++)
+          {
+                echo "<TD align=\"center\">\n";
+
+                echo "<a name=\"form\"></a>";
+                echo "<form method=\"get\" action=\"screenedit.php\">";
+                $iresult=DBSelect("select * from screens_items where scid=$scid and x=$c and y=$r");
+                if($iresult)
+                {
+                        $irow=DBfetch($iresult);
+                        $scitemid=$irow["scitemid"];
+                        $graphid=$irow["graphid"];
+                        $width=$irow["width"];
+                        $height=$irow["height"];
+                }
+                else
+                {
+                        $scitemid=0;
+                        $graphid=0;
+                        $width=100;
+                        $height=50;
+                }
+
+                if($graphid!=0)
+                {
+                        echo "<a href=charts.php?graphid=$graphid><img src='chart2.php?graphid=$graphid&width=$width&height=$height&period=3600' border=0></a>";
+                }
+                echo "</form>\n";
+               echo "</TD>";
+          }
+          echo "</TR>\n";
+          }
+          echo "</TABLE>";
+	}
+	else
+	{
+		show_table_header("Please select screen to display");		
+	}
+?>
+
+<?php
+	show_footer();
+?>
--- upgrades/dbpatches/1.0beta8_to_1.0beta9/mysql/patch.sql
+++ upgrades/dbpatches/1.0beta8_to_1.0beta9/mysql/patch.sql
@@ -1,1 +1,29 @@
 alter table services modify goodsla double(3,2) default '99.9' not null;
+
+#
+# Table structure for table 'screens'
+#
+
+CREATE TABLE screens (
+  scid int(10) unsigned NOT NULL auto_increment,
+  name varchar(255) NOT NULL default 'screen',
+  cols int(10) unsigned NOT NULL default '1',
+  rows int(10) unsigned NOT NULL default '1',
+  PRIMARY KEY  (scid)
+) TYPE=InnoDB;
+
+#
+# Table structure for table 'screens_items'
+#
+
+CREATE TABLE screens_items (
+  scitemid int(4) unsigned NOT NULL auto_increment,
+  scid int(4) unsigned NOT NULL default '0',
+  graphid int(4) unsigned NOT NULL default '0',
+  width int(4) unsigned NOT NULL default '100',
+  height int(4) unsigned NOT NULL default '50',
+  x int(4) unsigned NOT NULL default '0',
+  y int(4) unsigned NOT NULL default '0',
+  PRIMARY KEY  (scitemid)
+) TYPE=InnoDB;
+
