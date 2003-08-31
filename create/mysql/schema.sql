7d6504f33c74c5334b505550cf899814b03ba26e
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2003-08-31 07:58:36 +0000

    - added table users_groups, usrgrp (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@958 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- create/mysql/schema.sql
+++ create/mysql/schema.sql
@@ -514,3 +514,24 @@ CREATE TABLE stats (
   value_avg		double(16,4)	DEFAULT '0.0000' NOT NULL,
   PRIMARY KEY (itemid,year,month,day,hour)
 ) type=InnoDB;
+
+--
+-- Table structure for table 'usrgrp'
+--
+
+CREATE TABLE usrgrp (
+  usrgrpid		int(4)		NOT NULL auto_increment,
+  name			varchar(64)	DEFAULT '' NOT NULL,
+  PRIMARY KEY (usrgrpid),
+  UNIQUE (name)
+) type=InnoDB;
+
+--
+-- Table structure for table 'users_groups'
+--
+
+CREATE TABLE users_groups (
+  usrgrpid		int(4)		DEFAULT '0' NOT NULL,
+  userid		int(4)		DEFAULT '0' NOT NULL,
+  PRIMARY KEY (usrgrpid,userid)
+) type=InnoDB;
