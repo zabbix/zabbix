5876de7de8f176286860894296ef5e400ee362da
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2003-10-18 09:24:08 +0000

    More changes for delta support (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@999 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/1.0beta10_to_1.0beta11/postgresql/patch.sql
+++ upgrades/dbpatches/1.0beta10_to_1.0beta11/postgresql/patch.sql
@@ -31,4 +31,4 @@ insert into usrgrp (usrgrpid, name) values (NULL, 'Head of IT department');
 insert into usrgrp (usrgrpid, name) values (NULL, 'Zabbix administrators');  
 
 alter table items add delta int1  DEFAULT '0' NOT NULL;
-alter table items add prevorgvalue float8  DEFAULT '0.0';
+alter table items add prevorgvalue float8  DEFAULT NULL;
