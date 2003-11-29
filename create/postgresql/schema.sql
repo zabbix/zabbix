50c88cb294c61ab4ccabc5df09b37d9636f19b73
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2003-11-29 20:06:45 +0000

    - added table 'trends' (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@1070 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- create/postgresql/schema.sql
+++ create/postgresql/schema.sql
@@ -553,4 +553,18 @@ CREATE TABLE users_groups (
   FOREIGN KEY (userid) REFERENCES users
 );
 
+--
+-- Table structure for table 'trends'
+--
+
+CREATE TABLE trends (
+  itemid		int4		DEFAULT '0' NOT NULL,
+  clock			int4		DEFAULT '0' NOT NULL,
+  value_min		float8		DEFAULT '0.0000' NOT NULL,
+  value_avg		float8		DEFAULT '0.0000' NOT NULL,
+  value_max		float8		DEFAULT '0.0000' NOT NULL,
+  PRIMARY KEY (itemid,clock),
+  FOREIGN KEY (itemid) REFERENCES items
+);
+
 VACUUM ANALYZE;
