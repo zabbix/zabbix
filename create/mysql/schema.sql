5630e3801a29d6b42c2561a1e6423327a86ed21d
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2004-01-30 12:00:03 +0000

    - removed primary keys for both 'history' and 'history_str' (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@1223 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- create/mysql/schema.sql
+++ create/mysql/schema.sql
@@ -236,7 +236,8 @@ CREATE TABLE history (
   itemid		int(4)		DEFAULT '0' NOT NULL,
   clock			int(4)		DEFAULT '0' NOT NULL,
   value			double(16,4)	DEFAULT '0.0000' NOT NULL,
-  PRIMARY KEY (itemid,clock)
+--  PRIMARY KEY (itemid,clock)
+  KEY itemidclock (itemid, clock)
 ) type=InnoDB;
 
 --
@@ -247,7 +248,8 @@ CREATE TABLE history_str (
   itemid		int(4)		DEFAULT '0' NOT NULL,
   clock			int(4)		DEFAULT '0' NOT NULL,
   value			varchar(255)	DEFAULT '' NOT NULL,
-  PRIMARY KEY (itemid,clock)
+--  PRIMARY KEY (itemid,clock)
+  KEY itemidclock (itemid, clock)
 ) type=InnoDB;
 
 --
