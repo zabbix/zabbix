9678b598a3dcd101798e9f9a3ba42ef5b3863bbf
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2004-11-14 19:41:40 +0000

    Minor changes.
    git-svn-id: svn://svn.zabbix.com/trunk@1481 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ create/data/images/images.sql
@@ -0,0 +1,46 @@
+-- 
+-- Zabbix
+-- Copyright (C) 2000,2001,2002,2003,2004 Alexei Vladishev
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+--
+
+--
+-- Dumping data for table `images`
+--
+
+insert into images values(1,1,"Hub",load_file("Hub.png"));
+insert into images values(2,1,"Hub (small)",load_file("Hub_small.png"));
+insert into images values(3,1,"Network",load_file("Network.png"));
+insert into images values(4,1,"Network (small)",load_file("Network_small.png"));
+insert into images values(5,1,"Notebook",load_file("Notebook.png"));
+insert into images values(6,1,"Notebook (small)",load_file("Notebook_small.png"));
+insert into images values(7,1,"Phone",load_file("Phone.png"));
+insert into images values(8,1,"Phone (small)",load_file("Phone_small.png"));
+insert into images values(9,1,"Printer",load_file("Printer.png"));
+insert into images values(10,1,"Printer (small)",load_file("Printer_small.png"));
+insert into images values(11,1,"Router",load_file("Router.png"));
+insert into images values(12,1,"Router (small)",load_file("Router_small.png"));
+insert into images values(13,1,"Satellite",load_file("Satellite.png"));
+insert into images values(14,1,"Satellite (small)",load_file("Satellite_small.png"));
+insert into images values(15,1,"Server",load_file("Server.png"));
+insert into images values(16,1,"Server (small)",load_file("Server_small.png"));
+insert into images values(17,1,"UPS",load_file("UPS.png"));
+insert into images values(18,1,"UPS (small)",load_file("UPS_small.png"));
+insert into images values(19,1,"Workstation",load_file("Workstation.png"));
+insert into images values(20,1,"Workstation (small)",load_file("Workstation_small.png"));
+
+insert into images values(21,2,"Europe",load_file("europe.png"));
+insert into images values(22,2,"Europe B&W",load_file("europe2.jpg"));
