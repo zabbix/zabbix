9130fbff19435ece0bf12bb7f3a8bddfc2c6f842
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2004-11-21 10:06:16 +0000

    Basdic support of CPU stat under Linux (idle, system, nice, user times) (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@1501 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ src/zabbix_agent/cpustat.h
@@ -0,0 +1,38 @@
+/* 
+** Zabbix
+** Copyright (C) 2000,2001,2002,2003,2004 Alexei Vladishev
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+
+#ifndef ZABBIX_CPUSTAT_H
+#define ZABBIX_CPUSTAT_H
+
+#define CPUSTAT struct cpustat_type
+CPUSTAT
+{
+	char    *device;
+	int	major;
+	int	diskno;
+	int	clock[60*15];
+	float	user[60*15];
+	float	system[60*15];
+	float	nice[60*15];
+	float	idle[60*15];
+};
+
+void	collect_stats_cpustat(FILE *outfile);
+
+#endif
