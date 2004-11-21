15e6595ee5942a361cdc0152845c2678cbe61de1
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2004-11-21 17:17:11 +0000

    Minor changes.
    git-svn-id: svn://svn.zabbix.com/trunk@1503 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/zabbix_agent/cpustat.h
+++ src/zabbix_agent/cpustat.h
@@ -27,10 +27,10 @@ CPUSTAT
 	int	major;
 	int	diskno;
 	int	clock[60*15];
-	float	user[60*15];
-	float	system[60*15];
-	float	nice[60*15];
-	float	idle[60*15];
+	float	cpu_user[60*15];
+	float	cpu_system[60*15];
+	float	cpu_nice[60*15];
+	float	cpu_idle[60*15];
 };
 
 void	collect_stats_cpustat(FILE *outfile);
