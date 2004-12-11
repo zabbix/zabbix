3ba898cb3f8352ad3e8caab23f64026de280be06
Alexei Vladishev <alexei.vladishev@zabbix.com>
2004-12-11 10:19:07 +0000

    - one common process for sucker and trapper: zabbix_server (Alexei)

---
+++ src/zabbix_server/calculator.h
@@ -0,0 +1,35 @@
+/* 
+** Zabbix
+** Copyright (C) 2000,2001,2002,2003,2004 Alexei Vladishev
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+
+#ifndef ZABBIX_CALCULATOR_H
+#define ZABBIX_CALCULATOR_H
+
+extern	int	CONFIG_SENDER_FREQUENCY;
+extern	char	*CONFIG_DBHOST;
+extern	char	*CONFIG_DBNAME;
+extern	char	*CONFIG_DBUSER;
+extern	char	*CONFIG_DBPASSWORD;
+extern	char	*CONFIG_DBSOCKET;
+extern	char	*CONFIG_ALERT_SCRIPTS_PATH;
+
+extern	void	signal_handler( int sig );
+
+int calculator_loop();
+
+#endif
