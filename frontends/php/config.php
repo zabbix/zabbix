487834d930b410440aea5d0f0010fbf3791e614d
Alexei Vladishev <alexei.vladishev@zabbix.com>
2004-12-26 20:58:04 +0000

    - selection of configuration areas in config.php (Alexei)

--- frontends/php/config.php
+++ frontends/php/config.php
@@ -83,174 +83,195 @@
 ?>
 
 <?php
-	show_table_header(S_CONFIGURATION_OF_ZABBIX_BIG);
+	if(!isset($_GET["config"]))
+	{
+		$_GET["config"]=0;
+	}
+
+	$h1=S_CONFIGURATION_OF_ZABBIX_BIG;
+
+#	$h2=S_GROUP."&nbsp;";
+	$h2="";
+	$h2=$h2."<select class=\"biginput\" name=\"config\" onChange=\"submit()\">";
+	$h2=$h2."<option value=\"0\" ".iif(isset($_GET["config"])&&$_GET["config"]==0,"selected","").">".S_HOUSEKEEPER;
+	$h2=$h2."<option value=\"1\" ".iif(isset($_GET["config"])&&$_GET["config"]==1,"selected","").">".S_MEDIA_TYPES;
+	$h2=$h2."</select>";
+
+	show_header2($h1, $h2, "<form name=\"form2\" method=\"get\" action=\"config.php\">", "</form>");
+
+
+#	show_table_header(S_CONFIGURATION_OF_ZABBIX_BIG);
 ?>
 
 <?php
 	$config=select_config();
 ?>
 
-
 <?php
-
-	$col=0;
-	show_form_begin("config");
-	echo S_CONFIGURATION;
-
-	show_table2_v_delimiter($col++);
-	echo "<form method=\"get\" action=\"config.php\">";
-	echo nbsp(S_DO_NOT_KEEP_ALERTS_OLDER_THAN);
-	show_table2_h_delimiter();
-	echo "<input class=\"biginput\" name=\"alert_history\" value=\"".$config["alert_history"]."\" size=8>";
-
-	show_table2_v_delimiter($col++);
-	echo nbsp(S_DO_NOT_KEEP_ALARMS_OLDER_THAN);
-	show_table2_h_delimiter();
-	echo "<input class=\"biginput\" name=\"alarm_history\" value=\"".$config["alarm_history"]."\" size=8>";
-
-	show_table2_v_delimiter2();
-	echo "<input class=\"button\" type=\"submit\" name=\"register\" value=\"update\">";
-
-	show_table2_header_end();
-?>
-
-<?php
-	show_table_header(S_AVAILABLE_MEDIA_TYPES);
-?>
-
-<?php
-	table_begin();
-	table_header(array(S_ID,S_TYPE,S_DESCRIPTION_SMALL,S_ACTIONS));
-
-	$result=DBselect("select mt.mediatypeid,mt.type,mt.description,mt.smtp_server,mt.smtp_helo,mt.smtp_email,mt.exec_path from media_type mt order by mt.type");
-	$col=0;
-	while($row=DBfetch($result))
+	if($_GET["config"]==0)
 	{
-		if($row["type"]==0)
+		$col=0;
+		show_form_begin("config");
+		echo S_HOUSEKEEPER;
+
+		show_table2_v_delimiter($col++);
+		echo "<form method=\"get\" action=\"config.php\">";
+		echo nbsp(S_DO_NOT_KEEP_ALERTS_OLDER_THAN);
+		show_table2_h_delimiter();
+		echo "<input class=\"biginput\" name=\"alert_history\" value=\"".$config["alert_history"]."\" size=8>";
+
+		show_table2_v_delimiter($col++);
+		echo nbsp(S_DO_NOT_KEEP_ALARMS_OLDER_THAN);
+		show_table2_h_delimiter();
+		echo "<input class=\"biginput\" name=\"alarm_history\" value=\"".$config["alarm_history"]."\" size=8>";
+
+		show_table2_v_delimiter2();
+		echo "<input class=\"button\" type=\"submit\" name=\"register\" value=\"update\">";
+
+		show_table2_header_end();
+	}
+?>
+
+<?php
+	if($_GET["config"]==1)
+	{
+		echo "<br>";
+		show_table_header(S_AVAILABLE_MEDIA_TYPES);
+
+		table_begin();
+		table_header(array(S_ID,S_TYPE,S_DESCRIPTION_SMALL,S_ACTIONS));
+
+		$result=DBselect("select mt.mediatypeid,mt.type,mt.description,mt.smtp_server,mt.smtp_helo,mt.smtp_email,mt.exec_path from media_type mt order by mt.type");
+		$col=0;
+		while($row=DBfetch($result))
 		{
-			$type=S_EMAIL;
+			if($row["type"]==0)
+			{
+				$type=S_EMAIL;
+			}
+			else if($row["type"]==1)
+			{
+				$type=S_SCRIPT;
+			}
+			else
+			{
+				$type=S_UNKNOWN;
+			}
+			$actions="<a href=\"config.php?config=1&register=change&mediatypeid=".$row["mediatypeid"]."\">".S_CHANGE."</a>";
+			table_row(array(
+				$row["mediatypeid"],
+				$type,
+				$row["description"],
+				$actions),$col++);
 		}
-		else if($row["type"]==1)
+		if(DBnum_rows($result)==0)
 		{
-			$type=S_SCRIPT;
+				echo "<TR BGCOLOR=#EEEEEE>";
+				echo "<TD COLSPAN=4 ALIGN=CENTER>".S_NO_MEDIA_TYPES_DEFINED."</TD>";
+				echo "<TR>";
+		}
+		table_end();
+?>
+
+<?php
+		$type=@iif(isset($_GET["type"]),$_GET["type"],0);
+		$description=@iif(isset($_GET["description"]),$_GET["description"],"");
+		$smtp_server=@iif(isset($_GET["smtp_server"]),$_GET["smtp_server"],"localhost");
+		$smtp_helo=@iif(isset($_GET["smtp_helo"]),$_GET["smtp_helo"],"localhost");
+		$smtp_email=@iif(isset($_GET["smtp_email"]),$_GET["smtp_email"],"zabbix@localhost");
+		$exec_path=@iif(isset($_GET["exec_path"]),$_GET["exec_path"],"");
+
+		if(isset($_GET["register"]) && ($_GET["register"] == "change"))
+		{
+			$result=DBselect("select mediatypeid,type,description,smtp_server,smtp_helo,smtp_email,exec_path from media_type where mediatypeid=".$_GET["mediatypeid"]);
+			$mediatypeid=DBget_field($result,0,0);
+			$type=@iif(isset($_GET["type"]),$_GET["type"],DBget_field($result,0,1));
+			$description=DBget_field($result,0,2);
+			$smtp_server=DBget_field($result,0,3);
+			$smtp_helo=DBget_field($result,0,4);
+			$smtp_email=DBget_field($result,0,5);
+			$exec_path=DBget_field($result,0,6);
+		}
+
+?>
+
+<?php
+		show_form_begin("config");
+		echo S_MEDIA;
+
+		$col=0;
+
+		show_table2_v_delimiter($col++);
+		echo "<form name=\"selForm\" method=\"get\" action=\"config.php\">";
+		if(isset($_GET["mediatypeid"]))
+		{
+			echo "<input class=\"biginput\" name=\"mediatypeid\" type=\"hidden\" value=\"".$_GET["mediatypeid"]."\" size=8>";
+		}
+		echo "<input class=\"biginput\" name=\"config\" type=\"hidden\" value=\"1\" size=8>";
+
+		echo S_DESCRIPTION;
+		show_table2_h_delimiter();
+		echo "<input class=\"biginput\" name=\"description\" value=\"".$description."\" size=30>";
+
+		show_table2_v_delimiter($col++);
+		echo S_TYPE;
+		show_table2_h_delimiter();
+//	echo "<select class=\"biginput\" name=\"type\" size=\"1\" onChange=\"doSelect(this,'sel_dmk')\">";
+		echo "<select class=\"biginput\" name=\"type\" size=\"1\" onChange=\"submit()\">";
+		if($type==0)
+		{
+			echo "<option value=\"0\" selected>".S_EMAIL;
+			echo "<option value=\"1\">".S_SCRIPT;
 		}
 		else
 		{
-			$type=S_UNKNOWN;
+			echo "<option value=\"0\">".S_EMAIL;
+			echo "<option value=\"1\" selected>".S_SCRIPT;
 		}
-		$actions="<a href=\"config.php?register=change&mediatypeid=".$row["mediatypeid"]."\">".S_CHANGE."</a>";
-		table_row(array(
-			$row["mediatypeid"],
-			$type,
-			$row["description"],
-			$actions),$col++);
+		echo "</select>";
+
+		if($type==0)
+		{
+			echo "<input class=\"biginput\" name=\"exec_path\" type=\"hidden\" value=\"$exec_path\">";
+
+			show_table2_v_delimiter($col++);
+			echo nbsp(S_SMTP_SERVER);
+			show_table2_h_delimiter();
+			echo "<input class=\"biginput\" name=\"smtp_server\" value=\"".$smtp_server."\" size=30>";
+
+			show_table2_v_delimiter($col++);
+			echo nbsp(S_SMTP_HELO);
+			show_table2_h_delimiter();
+			echo "<input class=\"biginput\" name=\"smtp_helo\" value=\"".$smtp_helo."\" size=30>";
+
+			show_table2_v_delimiter($col++);
+			echo nbsp(S_SMTP_EMAIL);
+			show_table2_h_delimiter();
+			echo "<input class=\"biginput\" name=\"smtp_email\" value=\"".$smtp_email."\" size=30>";
+		}
+		if($type==1)
+		{
+			echo "<input class=\"biginput\" name=\"smtp_server\" type=\"hidden\" value=\"$smtp_server\">";
+			echo "<input class=\"biginput\" name=\"smtp_helo\" type=\"hidden\" value=\"$smtp_helo\">";
+			echo "<input class=\"biginput\" name=\"smtp_email\" type=\"hidden\" value=\"$smtp_email\">";
+
+			show_table2_v_delimiter($col++);
+			echo S_SCRIPT_NAME;
+			show_table2_h_delimiter();
+			echo "<input class=\"biginput\" name=\"exec_path\" value=\"".$exec_path."\" size=50>";
+		}
+
+		show_table2_v_delimiter2();
+		echo "<input class=\"button\" type=\"submit\" name=\"register\" value=\"add\">";
+
+		if(isset($_GET["mediatypeid"]))
+		{
+			echo "<input class=\"button\" type=\"submit\" name=\"register\" value=\"update media\">";
+			echo "<input class=\"button\" type=\"submit\" name=\"register\" value=\"delete\" onClick=\"return Confirm('".S_DELETE_SELECTED_MEDIA."');\">";
+		}
+
+		show_table2_header_end();
 	}
-	if(DBnum_rows($result)==0)
-	{
-			echo "<TR BGCOLOR=#EEEEEE>";
-			echo "<TD COLSPAN=4 ALIGN=CENTER>".S_NO_MEDIA_TYPES_DEFINED."</TD>";
-			echo "<TR>";
-	}
-	table_end();
-?>
-
-<?php
-	$type=@iif(isset($_GET["type"]),$_GET["type"],0);
-	$description=@iif(isset($_GET["description"]),$_GET["description"],"");
-	$smtp_server=@iif(isset($_GET["smtp_server"]),$_GET["smtp_server"],"localhost");
-	$smtp_helo=@iif(isset($_GET["smtp_helo"]),$_GET["smtp_helo"],"localhost");
-	$smtp_email=@iif(isset($_GET["smtp_email"]),$_GET["smtp_email"],"zabbix@localhost");
-	$exec_path=@iif(isset($_GET["exec_path"]),$_GET["exec_path"],"");
-
-	if(isset($_GET["register"]) && ($_GET["register"] == "change"))
-	{
-		$result=DBselect("select mediatypeid,type,description,smtp_server,smtp_helo,smtp_email,exec_path from media_type where mediatypeid=".$_GET["mediatypeid"]);
-		$mediatypeid=DBget_field($result,0,0);
-		$type=@iif(isset($_GET["type"]),$_GET["type"],DBget_field($result,0,1));
-		$description=DBget_field($result,0,2);
-		$smtp_server=DBget_field($result,0,3);
-		$smtp_helo=DBget_field($result,0,4);
-		$smtp_email=DBget_field($result,0,5);
-		$exec_path=DBget_field($result,0,6);
-	}
-
-?>
-
-<?php
-	show_form_begin("config");
-	echo S_MEDIA;
-
-	$col=0;
-
-	show_table2_v_delimiter($col++);
-	echo "<form name=\"selForm\" method=\"get\" action=\"config.php\">";
-	if(isset($_GET["mediatypeid"]))
-	{
-		echo "<input class=\"biginput\" name=\"mediatypeid\" type=\"hidden\" value=\"".$_GET["mediatypeid"]."\" size=8>";
-	}
-
-	echo S_DESCRIPTION;
-	show_table2_h_delimiter();
-	echo "<input class=\"biginput\" name=\"description\" value=\"".$description."\" size=30>";
-
-	show_table2_v_delimiter($col++);
-	echo S_TYPE;
-	show_table2_h_delimiter();
-//	echo "<select class=\"biginput\" name=\"type\" size=\"1\" onChange=\"doSelect(this,'sel_dmk')\">";
-	echo "<select class=\"biginput\" name=\"type\" size=\"1\" onChange=\"submit()\">";
-	if($type==0)
-	{
-		echo "<option value=\"0\" selected>".S_EMAIL;
-		echo "<option value=\"1\">".S_SCRIPT;
-	}
-	else
-	{
-		echo "<option value=\"0\">".S_EMAIL;
-		echo "<option value=\"1\" selected>".S_SCRIPT;
-	}
-	echo "</select>";
-
-	if($type==0)
-	{
-		echo "<input class=\"biginput\" name=\"exec_path\" type=\"hidden\" value=\"$exec_path\">";
-
-		show_table2_v_delimiter($col++);
-		echo nbsp(S_SMTP_SERVER);
-		show_table2_h_delimiter();
-		echo "<input class=\"biginput\" name=\"smtp_server\" value=\"".$smtp_server."\" size=30>";
-
-		show_table2_v_delimiter($col++);
-		echo nbsp(S_SMTP_HELO);
-		show_table2_h_delimiter();
-		echo "<input class=\"biginput\" name=\"smtp_helo\" value=\"".$smtp_helo."\" size=30>";
-
-		show_table2_v_delimiter($col++);
-		echo nbsp(S_SMTP_EMAIL);
-		show_table2_h_delimiter();
-		echo "<input class=\"biginput\" name=\"smtp_email\" value=\"".$smtp_email."\" size=30>";
-	}
-	if($type==1)
-	{
-		echo "<input class=\"biginput\" name=\"smtp_server\" type=\"hidden\" value=\"$smtp_server\">";
-		echo "<input class=\"biginput\" name=\"smtp_helo\" type=\"hidden\" value=\"$smtp_helo\">";
-		echo "<input class=\"biginput\" name=\"smtp_email\" type=\"hidden\" value=\"$smtp_email\">";
-
-		show_table2_v_delimiter($col++);
-		echo S_SCRIPT_NAME;
-		show_table2_h_delimiter();
-		echo "<input class=\"biginput\" name=\"exec_path\" value=\"".$exec_path."\" size=50>";
-	}
-
-	show_table2_v_delimiter2();
-	echo "<input class=\"button\" type=\"submit\" name=\"register\" value=\"add\">";
-
-	if(isset($_GET["mediatypeid"]))
-	{
-		echo "<input class=\"button\" type=\"submit\" name=\"register\" value=\"update media\">";
-		echo "<input class=\"button\" type=\"submit\" name=\"register\" value=\"delete\" onClick=\"return Confirm('".S_DELETE_SELECTED_MEDIA."');\">";
-	}
-
-	show_table2_header_end();
 ?>
 
 <?php
