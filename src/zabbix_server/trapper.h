6af93cbb5ee49a901197dc5f88ef077ffa4f5dac
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2005-04-26 12:19:23 +0000

    Modularisation for zabbix_server.
    git-svn-id: svn://svn.zabbix.com/trunk@1726 97f52cf1-0a1b-0410-bd0e-c28be96e8082

---
+++ src/zabbix_server/trapper.h
@@ -0,0 +1,29 @@
+/* 
+** Zabbix
+** Copyright (C) 2000,2001,2002,2003,2004 Alexei Vladishev
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+
+#ifndef ZABBIX_TRAPPER_H
+#define ZABBIX_TRAPPER_H
+
+extern	int	CONFIG_TIMEOUT;
+
+extern	void	signal_handler( int sig );
+
+pid_t	child_trapper_make(int i,int listenfd, int addrlen);
+
+#endif
