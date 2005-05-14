e2c5c273f9e76b84ded6aa82e5cea52db7eba67e
Alexei Vladishev <alexei.vladishev@zabbix.com>
2005-05-14 09:43:39 +0000

    - changed definition of services.goodsla to double(5.2) (Alexei)

--- create/mysql/schema.sql
+++ create/mysql/schema.sql
@@ -29,7 +29,7 @@ CREATE TABLE services (
   algorithm		int(1)		DEFAULT '0' NOT NULL,
   triggerid		int(4),
   showsla		int(1)		DEFAULT '0' NOT NULL,
-  goodsla		double(3,2)	DEFAULT '99.9' NOT NULL,
+  goodsla		double(5,2)	DEFAULT '99.9' NOT NULL,
   sortorder		int(4)		DEFAULT '0' NOT NULL,
   PRIMARY KEY (serviceid)
 ) type=InnoDB;
---
+++ upgrades/dbpatches/1.1alpha8_to_1.1alpha9/mysql/patch.sql
@@ -0,0 +1,1 @@
+alter table services modify goodsla double(5,2) DEFAULT '99.9' NOT NULL;
