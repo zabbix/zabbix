41e424f9d94caa68876a3a6a48565c54eef201bb
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2005-05-16 18:28:23 +0000

    Changed version to 1.1alpha9.
    git-svn-id: svn://svn.zabbix.com/trunk@1776 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_agent/sysinfo.c
+++ src/zabbix_agent/sysinfo.c
@@ -2541,7 +2541,7 @@ int	DISK_WBLK(const char *cmd, const char *parameter,double  *value)
 
 int	VERSION(const char *cmd, const char *parameter,char  **value)
 {
-	static	char	version[]="1.1alpha8";
+	static	char	version[]="1.1alpha9";
 
 	*value=strdup(version);
 	return	SYSINFO_RET_OK;
