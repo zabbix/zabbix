77d06861d6670c974dd55d0acfd55e560f2cc700
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2005-05-16 18:28:23 +0000

    Changed version to 1.1alpha9.
    git-svn-id: svn://svn.zabbix.com/trunk@1776 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/local_en.inc.php
+++ frontends/php/include/local_en.inc.php
@@ -25,7 +25,7 @@
 
 //	about.php
 	define("S_ABOUT_ZABBIX",			"About ZABBIX");
-	define("S_INFORMATION_ABOUT_ZABBIX",		"Information about ZABBIX (v1.1alpha8)");
+	define("S_INFORMATION_ABOUT_ZABBIX",		"Information about ZABBIX (v1.1alpha9)");
 	define("S_HOMEPAGE_OF_ZABBIX",			"Homepage of ZABBIX");
 	define("S_HOMEPAGE_OF_ZABBIX_DETAILS",		"This is home page of ZABBIX.");
 	define("S_LATEST_ZABBIX_MANUAL",		"Latest ZABBIX Manual");
@@ -192,7 +192,7 @@
 	define("S_COMPARE",				"Compare");
 
 //	Footer
-	define("S_ZABBIX_VER",				"ZABBIX 1.1alpha8");
+	define("S_ZABBIX_VER",				"ZABBIX 1.1alpha9");
 	define("S_COPYRIGHT_BY",			"Copyright 2001-2005 by ");
 	define("S_CONNECTED_AS",			"Connected as");
 	define("S_SIA_ZABBIX",				"SIA Zabbix");
