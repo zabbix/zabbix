29abd838c56f0e8940c362a27808939eb80ab25f
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2005-05-23 17:21:33 +0000

    - added table 'history_log' (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@1781 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- create/postgresql/schema.sql
+++ create/postgresql/schema.sql
@@ -644,4 +644,18 @@ CREATE TABLE hosts_templates (
 
 CREATE UNIQUE INDEX hosts_templates_hostid_templateid on hosts_templates (hostid, templateid);
 
+--
+-- Table structure for table 'history_log'
+--
+
+CREATE TABLE history_log (
+  itemid                int4            DEFAULT '0' NOT NULL,
+  clock                 int4            DEFAULT '0' NOT NULL,
+  value                 varchar(255)    DEFAULT '' NOT NULL,
+--  PRIMARY KEY (itemid,clock),
+  FOREIGN KEY (itemid) REFERENCES items
+);
+
+CREATE INDEX history_log_i_c on history_str (itemid, clock);
+
 VACUUM ANALYZE;
