71f1903372e0e7ec1ce8145b642f1eeac5a7058d
Alexei Vladishev <alexei.vladishev@zabbix.com>
2005-05-23 17:44:59 +0000

    - new item type - 'log' (Alexei)

--- include/db.c
+++ include/db.c
@@ -1730,6 +1730,21 @@ int	DBadd_history_str(int itemid, char *value, int clock)
 	return SUCCEED;
 }
 
+int	DBadd_history_log(int itemid, char *value, int clock)
+{
+	char	sql[MAX_STRING_LEN];
+	char	value_esc[MAX_STRING_LEN];
+
+	zabbix_log(LOG_LEVEL_DEBUG,"In add_history_log()");
+
+	DBescape_string(value,value_esc,MAX_STRING_LEN);
+	snprintf(sql,sizeof(sql)-1,"insert into history_log (clock,itemid,value) values (%d,%d,'%s')",clock,itemid,value_esc);
+	DBexecute(sql);
+
+	return SUCCEED;
+}
+
+
 int	DBget_items_count(void)
 {
 	int	res;
