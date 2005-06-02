771a227b9f266a19a8e8275f9114a0b275a9192d
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2005-06-02 08:25:06 +0000

    - added table escalation_rules (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@1805 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.1alpha10_to_1.1alpha11/mysql/patch.sql
+++ upgrades/dbpatches/1.1alpha10_to_1.1alpha11/mysql/patch.sql
@@ -1,1 +1,14 @@
 alter table screens_items add	rowspan		int(4)	DEFAULT '0' NOT NULL;
+
+--
+-- Table structure for table 'escalation_rules'
+--
+
+CREATE TABLE escalation_rules (
+  ruleid		int(4)		NOT NULL auto_increment,
+  escalationid		int(4)		DEFAULT '0' NOT NULL,
+  level			int(4)		DEFAULT '0' NOT NULL,
+  actiontype		int(4)		DEFAULT '0' NOT NULL,
+  PRIMARY KEY (ruleid),
+  KEY (escalationid)
+) type=InnoDB;
