c684228d1c81746ef658b2a646483c885ec12088
Alexei Vladishev <alexei.vladishev@zabbix.com>
2005-06-02 08:25:06 +0000

    - added table escalation_rules (Alexei)

--- upgrades/dbpatches/1.1alpha10_to_1.1alpha11/postgresql/patch.sql
+++ upgrades/dbpatches/1.1alpha10_to_1.1alpha11/postgresql/patch.sql
@@ -1,1 +1,14 @@
 alter table screens_items add	rowspan		int4	DEFAULT '0' NOT NULL;
+
+--
+-- Table structure for table 'escalation_rules'
+--
+
+CREATE TABLE escalation_rules (
+  ruleid		serial,
+  escalationid		int4		DEFAULT '0' NOT NULL,
+  level			int4		DEFAULT '0' NOT NULL,
+  actiontype		int4		DEFAULT '0' NOT NULL,
+  PRIMARY KEY (ruleid),
+  FOREIGN KEY (escalationid) REFERENCES escalations
+);
