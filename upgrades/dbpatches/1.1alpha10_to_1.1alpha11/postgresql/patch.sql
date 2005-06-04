503a72d79da019968e5b540a6424e8f3ae44706a
Alexei Vladishev <alexei.vladishev@zabbix.com>
2005-06-04 09:38:37 +0000

    New structure for escalation and escalation rules tables (Alexei)

--- upgrades/dbpatches/1.1alpha10_to_1.1alpha11/postgresql/patch.sql
+++ upgrades/dbpatches/1.1alpha10_to_1.1alpha11/postgresql/patch.sql
@@ -1,5 +1,21 @@
 alter table screens_items add	rowspan		int4	DEFAULT '0' NOT NULL;
 
+drop table escalation_rules;
+drop table escalations;
+
+--
+-- Table structure for table 'escalations'
+--
+
+CREATE TABLE escalations (
+  escalationid		serial,
+  name			varchar(64)	DEFAULT '0' NOT NULL,
+  dflt			int2		DEFAULT '0' NOT NULL,
+  PRIMARY KEY (escalationid)
+);
+
+CREATE UNIQUE INDEX escalations_name on escalations (name);
+
 --
 -- Table structure for table 'escalation_rules'
 --
@@ -7,6 +23,7 @@ alter table screens_items add	rowspan		int4	DEFAULT '0' NOT NULL;
 CREATE TABLE escalation_rules (
   ruleid		serial,
   escalationid		int4		DEFAULT '0' NOT NULL,
+  period		varchar(100)	DEFAULT '1-7,00:00-23:59' NOT NULL,
   level			int4		DEFAULT '0' NOT NULL,
   actiontype		int4		DEFAULT '0' NOT NULL,
   PRIMARY KEY (ruleid),
