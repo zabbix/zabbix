dee7748ce3a458856713e608569c47c6d5ea65d4
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2005-06-04 09:38:37 +0000

    New structure for escalation and escalation rules tables (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@1812 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/1.1alpha10_to_1.1alpha11/mysql/patch.sql
+++ upgrades/dbpatches/1.1alpha10_to_1.1alpha11/mysql/patch.sql
@@ -1,5 +1,20 @@
 alter table screens_items add	rowspan		int(4)	DEFAULT '0' NOT NULL;
 
+drop table escalation_rules;
+drop table escalations;
+
+--
+-- Table structure for table 'escalations'
+--
+
+CREATE TABLE escalations (
+  escalationid		int(4)		NOT NULL auto_increment,
+  name			varchar(64)	DEFAULT '0' NOT NULL,
+  dflt			int(2)		DEFAULT '0' NOT NULL,
+  PRIMARY KEY (escalationid),
+  UNIQUE (name)
+) type=InnoDB;
+
 --
 -- Table structure for table 'escalation_rules'
 --
@@ -7,6 +22,7 @@ alter table screens_items add	rowspan		int(4)	DEFAULT '0' NOT NULL;
 CREATE TABLE escalation_rules (
   ruleid		int(4)		NOT NULL auto_increment,
   escalationid		int(4)		DEFAULT '0' NOT NULL,
+  period		varchar(100)	DEFAULT '1-7,00:00-23:59' NOT NULL,
   level			int(4)		DEFAULT '0' NOT NULL,
   actiontype		int(4)		DEFAULT '0' NOT NULL,
   PRIMARY KEY (ruleid),
