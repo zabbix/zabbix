56ea69ddff826c1a7b0cd4ce1d8ed257d9c8ff5f
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2005-07-22 20:50:03 +0000

    Minor changes.
    git-svn-id: svn://svn.zabbix.com/trunk@1904 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- create/mysql/schema.sql
+++ create/mysql/schema.sql
@@ -691,3 +691,23 @@ CREATE TABLE history_log (
   PRIMARY KEY (id),
   KEY itemidclock (itemid, clock)
 ) type=InnoDB;
+
+--
+-- Table structure for table 'hosts_profiles'
+--
+
+CREATE TABLE hosts_profiles (
+  hostid		int(4)		DEFAULT '0' NOT NULL,
+  devicetype		varchar(64)	DEFAULT '' NOT NULL,
+  name			varchar(64)	DEFAULT '' NOT NULL,
+  os			varchar(64)	DEFAULT '' NOT NULL,
+  serialno		varchar(64)	DEFAULT '' NOT NULL,
+  tag			varchar(64)	DEFAULT '' NOT NULL,
+  macaddress		varchar(64)	DEFAULT '' NOT NULL,
+  hardware		blob		DEFAULT '' NOT NULL,
+  software		blob		DEFAULT '' NOT NULL,
+  contact		blob		DEFAULT '' NOT NULL,
+  location		blob		DEFAULT '' NOT NULL,
+  notes			lob		DEFAULT '' NOT NULL,
+  PRIMARY KEY (hostid)
+) type=InnoDB;
