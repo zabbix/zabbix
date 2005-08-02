64e6b251332cec4bd406b55e1d3c9bf485158474
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2005-08-02 13:28:16 +0000

    Minor changes.
    git-svn-id: svn://svn.zabbix.com/trunk@1920 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_agent_win32/base64.cpp
+++ src/zabbix_agent_win32/base64.cpp
@@ -1,11 +1,15 @@
+#include "zabbixw32.h"
+
+#define MAX_B64_SIZE 16*1024
+
 static char base64_set [] =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 void str_base64_encode(char *p_str, char *p_b64str, int in_size);
 void str_base64_decode(char *p_b64str, char *p_str, int *p_out_size);
-char char_base64_encode(unsigned char uc);
-unsigned char char_base64_decode(char c);
-int is_base64 (char c);
+static char char_base64_encode(unsigned char uc);
+static unsigned char char_base64_decode(char c);
+static int is_base64 (char c);
 
 /*------------------------------------------------------------------------
  *
@@ -20,7 +24,7 @@ int is_base64 (char c);
  * Comments	:
  *
  *----------------------------------------------------------------------*/
-private int is_base64 (char c)
+static int is_base64 (char c)
 {
 	if ( (c >= '0' && c <= '9')
 	  || (c >= 'a' && c <= 'z')
@@ -48,7 +52,7 @@ private int is_base64 (char c)
  * Comments	:
  *
  *----------------------------------------------------------------------*/
-private char char_base64_encode(unsigned char uc)
+static char char_base64_encode(unsigned char uc)
 {
 	return base64_set[uc];
 }
@@ -66,7 +70,7 @@ private char char_base64_encode(unsigned char uc)
  * Comments	:
  *
  *----------------------------------------------------------------------*/
-private unsigned char char_base64_decode(char c)
+static unsigned char char_base64_decode(char c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
@@ -104,7 +108,7 @@ private unsigned char char_base64_decode(char c)
  * Comments	:
  *
  *----------------------------------------------------------------------*/
-public void str_base64_encode(char *p_str, char *p_b64str, int in_size)
+void str_base64_encode(char *p_str, char *p_b64str, int in_size)
 {
 	int 	i;
 	unsigned char from1=0,from2=0,from3=0;
@@ -155,11 +159,11 @@ public void str_base64_encode(char *p_str, char *p_b64str, int in_size)
 			*(p_b64str++) = '=';	/* Padding */
 		};
 
-		if ( i % (76/4*3) == 0)
+/*		if ( i % (76/4*3) == 0)
 		{
 			*(p_b64str++) = '\r';
 			*(p_b64str++) = '\n';
-		}
+		}*/
 	};
 	
 	return;
@@ -179,7 +183,7 @@ public void str_base64_encode(char *p_str, char *p_b64str, int in_size)
  * Comments	:
  *
  *----------------------------------------------------------------------*/
-public void str_base64_decode(char *p_b64str, char *p_str, int *p_out_size)
+void str_base64_decode(char *p_b64str, char *p_str, int *p_out_size)
 {
 	int i;
 	int j = 0;
---
+++ src/zabbix_agent_win32/xml_comms.cpp
@@ -0,0 +1,64 @@
+#include "zabbixw32.h"
+
+int	comms_create_request(char *host, char *key, char *data, char *lastlogsize, char *request,int maxlen)
+{
+	int ret = SUCCEED;
+	char host_b64[MAX_STRING_LEN];
+	char key_b64[MAX_STRING_LEN];
+	char data_b64[MAX_STRING_LEN];
+	char lastlogsize_b64[MAX_STRING_LEN];
+
+	memset(host_b64,0,sizeof(host_b64));
+	memset(key_b64,0,sizeof(key_b64));
+	memset(data_b64,0,sizeof(data_b64));
+	memset(lastlogsize_b64,0,sizeof(lastlogsize_b64));
+
+	str_base64_encode(host, host_b64, strlen(host));
+	str_base64_encode(key, key_b64, strlen(key));
+	str_base64_encode(data, data_b64, strlen(data));
+	str_base64_encode(lastlogsize, lastlogsize_b64, strlen(lastlogsize));
+
+	if(lastlogsize[0]==0)
+	{
+		sprintf(request,"<req><host>%s</host><key>%s</key><data>%s</data></req>",host_b64,key_b64,data_b64);
+	}
+	else
+	{
+		sprintf(request,"<req><host>%s</host><key>%s</key><data>%s</data><lastlogsize>%s</lastlogsize></req>",host_b64,key_b64,data_b64,lastlogsize_b64);
+	}
+
+	return ret;
+}
+
+int	comms_parse_response(char *xml,char *host,char *key, char *data, char *lastlogsize, int maxlen)
+{
+	int ret = SUCCEED;
+	int i;
+
+	char host_b64[MAX_STRING_LEN];
+	char key_b64[MAX_STRING_LEN];
+	char data_b64[MAX_STRING_LEN];
+	char lastlogsize_b64[MAX_STRING_LEN];
+
+	memset(host_b64,0,sizeof(host_b64));
+	memset(key_b64,0,sizeof(key_b64));
+	memset(data_b64,0,sizeof(data_b64));
+	memset(lastlogsize_b64,0,sizeof(lastlogsize_b64));
+
+	xml_get_data(xml, "host", host_b64, sizeof(host_b64)-1);
+	xml_get_data(xml, "key", key_b64, sizeof(key_b64)-1);
+	xml_get_data(xml, "data", data_b64, sizeof(data_b64)-1);
+	xml_get_data(xml, "lastlogsize", lastlogsize_b64, sizeof(lastlogsize_b64)-1);
+
+	memset(key,0,maxlen);
+	memset(host,0,maxlen);
+	memset(data,0,maxlen);
+	memset(lastlogsize,0,maxlen);
+
+	str_base64_decode(host_b64, host, &i);
+	str_base64_decode(key_b64, key, &i);
+	str_base64_decode(data_b64, data, &i);
+	str_base64_decode(lastlogsize_b64, lastlogsize, &i);
+
+	return ret;
+}
