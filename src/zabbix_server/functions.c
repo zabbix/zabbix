74389b3125801632e658079e268c87464d2d3658
Alexei Vladishev <alexei.vladishev@zabbix.com>
2005-08-10 12:41:55 +0000

    Minor changes.

--- src/zabbix_server/functions.c
+++ src/zabbix_server/functions.c
@@ -493,7 +493,7 @@ int	process_data(int sockfd,char *server,char *key,char *value,char *lastlogsize
 		item.timestamp=atoi(timestamp);
 		item.eventlog_severity=atoi(severity);
 		item.eventlog_source=source;
-		zabbix_log(LOG_LEVEL_WARNING, "Value [%s] Lastlogsize [%s] Timestamp [%s]", value, lastlogsize, timestamp);
+		zabbix_log(LOG_LEVEL_DEBUG, "Value [%s] Lastlogsize [%s] Timestamp [%s]", value, lastlogsize, timestamp);
 	}
 
 	process_new_value(&item,s,&update_tr);
