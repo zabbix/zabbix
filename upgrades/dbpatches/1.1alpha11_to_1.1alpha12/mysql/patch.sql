831ae6761f98382b21ae96177bfa542dbcc20ca4
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2005-08-10 08:19:37 +0000

    - added column history_log.severity (Alexei)  - added column history_log.source (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@1954 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/1.1alpha11_to_1.1alpha12/mysql/patch.sql
+++ upgrades/dbpatches/1.1alpha11_to_1.1alpha12/mysql/patch.sql
@@ -1,1 +1,3 @@
 alter table history_log add timestamp int(4) DEFAULT '0' NOT NULL;
+alter table history_log add source varchar(64) DEFAULT '' NOT NULL;
+alter table history_log add severity int(4) DEFAULT '0' NOT NULL;
