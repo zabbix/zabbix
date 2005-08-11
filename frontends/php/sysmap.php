e7e5bae215e0727dbdb4ece30d248fa7e24528b9
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2005-08-11 14:38:29 +0000

    Minor changes.
    git-svn-id: svn://svn.zabbix.com/trunk@1974 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/sysmap.php
+++ frontends/php/sysmap.php
@@ -20,7 +20,7 @@
 ?>
 <?php
 	include "include/config.inc.php";
-	$page["title"] = "S_CONFIGURATION_OF_NETWORK_MAP";
+	$page["title"] = "S_CONFIGURATION_OF_NETWORK_MAPS";
 	$page["file"] = "sysmap.php";
 	show_header($page["title"],0,0);
 ?>
