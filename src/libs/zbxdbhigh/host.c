c2a6a57b5b52db670e1bd1ea7a1885855b67bc09
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2005-10-04 13:42:05 +0000

    Minor changes.
    git-svn-id: svn://svn.zabbix.com/trunk@2101 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxdbhigh/host.c
+++ src/libs/zbxdbhigh/host.c
@@ -34,7 +34,7 @@ int	DBadd_new_host(char *server, int port, int status, int useip, char *ip, int 
 	char	sql[MAX_STRING_LEN];
 	int	hostid;
 
-	snprintf(sql, sizeof(sql)-1,"insert into hosts (host,port,status,useip,ip,disable_until,available) values ('%s',%d,%d,useip,'%d',%d,%d)", server, port, status, useip, ip, disable_until, available);
+	snprintf(sql, sizeof(sql)-1,"insert into hosts (host,port,status,useip,ip,disable_until,available) values ('%s',%d,%d,%d,'%d',%d,%d)", server, port, status, useip, ip, disable_until, available);
 	if(FAIL == DBexecute(sql))
 	{
 		return FAIL;
