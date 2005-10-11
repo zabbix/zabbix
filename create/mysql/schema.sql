e0352d5bfe871bb9676f75935935fbce2fa1d6cd
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2005-10-11 12:03:39 +0000

    Minor changes.
    git-svn-id: svn://svn.zabbix.com/trunk@2169 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- create/mysql/schema.sql
+++ create/mysql/schema.sql
@@ -209,6 +209,8 @@ CREATE TABLE actions (
   message		blob		DEFAULT '' NOT NULL,
   nextcheck		int(4)		DEFAULT '0' NOT NULL,
   recipient		int(1)		DEFAULT '0' NOT NULL,
+  maxrepeats		int(4)		DEFAULT '0' NOT NULL,
+  repeatdelay		int(4)		DEFAULT '600' NOT NULL,
   PRIMARY KEY (actionid),
   KEY (triggerid)
 ) type=InnoDB;
