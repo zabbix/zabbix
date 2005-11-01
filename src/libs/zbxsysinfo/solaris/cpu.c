de267adc7399911d3d3339c8aa91bcab381ad50a
osmiy <osmiy@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2005-11-01 13:13:03 +0000

    - optimized sysinfo functions for solaris system (Eugene) - added support of new sysinfo function interface for solaris (Eugene) - fixed some errors in sysinfo functions for linux system (Eugene)
    git-svn-id: svn://svn.zabbix.com/trunk@2235 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/libs/zbxsysinfo/solaris/cpu.c
+++ src/libs/zbxsysinfo/solaris/cpu.c
@@ -19,182 +19,71 @@
 
 #include "config.h"
 
-#include <errno.h>
-
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-
-/* Definitions of uint32_t under OS/X */
-#ifdef HAVE_STDINT_H
-	#include <stdint.h>
-#endif
-#ifdef HAVE_STRINGS_H
-	#include <strings.h>
-#endif
-#ifdef HAVE_FCNTL_H
-	#include <fcntl.h>
-#endif
-#ifdef HAVE_DIRENT_H
-	#include <dirent.h>
-#endif
-/* Linux */
-#ifdef HAVE_SYS_VFS_H
-	#include <sys/vfs.h>
-#endif
-#ifdef HAVE_SYS_SYSINFO_H
-	#include <sys/sysinfo.h>
-#endif
-/* Solaris */
-#ifdef HAVE_SYS_STATVFS_H
-	#include <sys/statvfs.h>
-#endif
-/* Solaris */
-#ifdef HAVE_SYS_PROCFS_H
-/* This is needed to access the correct procfs.h definitions */
-	#define _STRUCTURED_PROC 1
-	#include <sys/procfs.h>
-#endif
-#ifdef HAVE_SYS_LOADAVG_H
-	#include <sys/loadavg.h>
-#endif
-#ifdef HAVE_SYS_SOCKET_H
-	#include <sys/socket.h>
-#endif
-#ifdef HAVE_NETINET_IN_H
-	#include <netinet/in.h>
-#endif
-#ifdef HAVE_ARPA_INET_H
-	#include <arpa/inet.h>
-#endif
-/* OpenBSD/Solaris */
-#ifdef HAVE_SYS_PARAM_H
-	#include <sys/param.h>
-#endif
-
-#ifdef HAVE_SYS_MOUNT_H
-	#include <sys/mount.h>
-#endif
-/* HP-UX */
-#ifdef HAVE_SYS_PSTAT_H
-	#include <sys/pstat.h>
-#endif
-
-#ifdef HAVE_NETDB_H
-	#include <netdb.h>
-#endif
-
-/* Solaris */
-#ifdef HAVE_SYS_SWAP_H
-	#include <sys/swap.h>
-#endif
-
-/* FreeBSD */
-#ifdef HAVE_SYS_SYSCTL_H
-	#include <sys/sysctl.h>
-#endif
-
-/* Solaris */
-#ifdef HAVE_SYS_SYSCALL_H
-	#include <sys/syscall.h>
-#endif
-
-/* FreeBSD */
-#ifdef HAVE_VM_VM_PARAM_H
-	#include <vm/vm_param.h>
-#endif
-/* FreeBSD */
-#ifdef HAVE_SYS_VMMETER_H
-	#include <sys/vmmeter.h>
-#endif
-/* FreeBSD */
-#ifdef HAVE_SYS_TIME_H
-	#include <sys/time.h>
-#endif
-
-#ifdef HAVE_MACH_HOST_INFO_H
-	#include <mach/host_info.h>
-#endif
-#ifdef HAVE_MACH_MACH_HOST_H
-	#include <mach/mach_host.h>
-#endif
-
-/* AIX CPU */
-#ifdef HAVE_KNLIST_H
-	#include <knlist.h>
-#endif
-
-#ifdef HAVE_KSTAT_H
-	#include <kstat.h>
-#endif
-
-#ifdef HAVE_LDAP
-	#include <ldap.h>
-#endif
-
 #include "common.h"
 #include "sysinfo.h"
 
-int	SYSTEM_CPU_IDLE1(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
+int     OLD_CPU(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	return	get_stat("cpu[idle1]",value,msg,mlen_max);
+	return	get_stat(cmd, flags, result);
 }
 
-int	SYSTEM_CPU_IDLE5(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_IDLE1(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	return	get_stat("cpu[idle5]",value,msg,mlen_max);
+	return	get_stat("cpu[idle1]",flags, result);
 }
 
-int	SYSTEM_CPU_IDLE15(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_IDLE5(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	return	get_stat("cpu[idle15]",value,msg,mlen_max);
+	return	get_stat("cpu[idle5]",flags, result);
 }
 
-int	SYSTEM_CPU_NICE1(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_IDLE15(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	return	get_stat("cpu[nice1]",value,msg,mlen_max);
+	return	get_stat("cpu[idle15]",flags, result);
 }
 
-int	SYSTEM_CPU_NICE5(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_NICE1(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	return	get_stat("cpu[nice5]",value,msg,mlen_max);
-}
-int	SYSTEM_CPU_NICE15(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
-{
-	return	get_stat("cpu[nice15]",value,msg,mlen_max);
+	return	get_stat("cpu[nice1]",flags, result);
 }
 
-int	SYSTEM_CPU_USER1(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_NICE5(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	return	get_stat("cpu[user1]",value,msg,mlen_max);
+	return	get_stat("cpu[nice5]",flags, result);
+}
+int	SYSTEM_CPU_NICE15(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
+{
+	return	get_stat("cpu[nice15]",flags, result);
 }
 
-int	SYSTEM_CPU_USER5(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_USER1(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	return	get_stat("cpu[user5]",value,msg,mlen_max);
+	return	get_stat("cpu[user1]",flags, result);
 }
 
-int	SYSTEM_CPU_USER15(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_USER5(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	return	get_stat("cpu[user15]",value,msg,mlen_max);
+	return	get_stat("cpu[user5]",flags, result);
 }
 
-int	SYSTEM_CPU_SYS1(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_USER15(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	return	get_stat("cpu[system1]",value,msg,mlen_max);
+	return	get_stat("cpu[user15]",flags, result);
 }
 
-int	SYSTEM_CPU_SYS5(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_SYS1(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	return	get_stat("cpu[system5]",value,msg,mlen_max);
+	return	get_stat("cpu[system1]",flags, result);
 }
 
-int	SYSTEM_CPU_SYS15(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_SYS5(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	return	get_stat("cpu[system15]",value,msg,mlen_max);
+	return	get_stat("cpu[system5]",flags, result);
+}
+
+int	SYSTEM_CPU_SYS15(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
+{
+	return	get_stat("cpu[system15]",flags, result);
 }
 
 static int get_cpu_data(unsigned long long *idle,
@@ -244,44 +133,61 @@ static int get_cpu_data(unsigned long long *idle,
 #define CPU_K 2
 #define CPU_W 3
 
-int	SYSTEM_CPU_UTILIZATION(const char *cmd, const char *param,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_UTIL(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
     unsigned long long cpu_val[4];
     unsigned long long interval_size;
 
-    char cpu_info[MAX_STRING_LEN];
+    char cpuname[MAX_STRING_LEN];
+    char mode[MAX_STRING_LEN];
     
     int info_id = 0;
     
-    int result = SYSINFO_RET_FAIL;
+    int ret = SYSINFO_RET_FAIL;
 
-    if(num_param(param) > 1)
+    if(num_param(param) > 2)
+    {
+        return SYSINFO_RET_FAIL;
+    }
+
+    if(get_param(param, 1, cpuname, MAX_STRING_LEN) != 0)
+    {
+	cpuname[0] = '\0';
+    }
+    if(cpuname[0] == '\0')
+    {
+	/* default parameter */
+        sprintf(cpuname, "all");
+    }
+    if(strncmp(cpuname, "all", MAX_STRING_LEN))
     {
 	return SYSINFO_RET_FAIL;
     }
-
-    if(get_param(param, 1, cpu_info, MAX_STRING_LEN) != 0)
+	
+    if(get_param(param, 2, mode, MAX_STRING_LEN) != 0)
     {
-	return SYSINFO_RET_FAIL;
-    }
-    else
-    {
-        strscpy(cpu_info, "idle");
+	mode[0] = '\0';
     }
     
-    if(strcmp(cpu_info,"idle") == 0)
+    if(mode[0] == '\0')
+    {
+	/* default parameter */
+        strscpy(mode, "idle");
+    }
+    
+    if(strcmp(mode,"idle") == 0)
     {
         info_id = CPU_I;
     }
-    else if(strcmp(cpu_info,"user") == 0)
+    else if(strcmp(mode,"user") == 0)
     {
         info_id = CPU_U;
     }
-    else if(strcmp(cpu_info,"kernel") == 0)
+    else if(strcmp(mode,"kernel") == 0)
     {
         info_id = CPU_K;
     }
-    else if(strcmp(cpu_info,"wait") == 0)
+    else if(strcmp(mode,"wait") == 0)
     {
 	info_id = CPU_W;
     }
@@ -296,22 +202,93 @@ int	SYSTEM_CPU_UTILIZATION(const char *cmd, const char *param,double  *value, co
         
 	if (interval_size > 0)
 	{
-	    *value = (cpu_val[info_id] * 100.0)/interval_size;
+	    result->type |= AR_DOUBLE;
+	    result->dbl = (cpu_val[info_id] * 100.0)/interval_size;
 
-            result = SYSINFO_RET_OK;
+            ret = SYSINFO_RET_OK;
         }
     }
-    return result;
+    return ret;
 }
 
-int	SYSTEM_CPU_LOAD1(const char *cmd, const char *parameter,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_LOAD(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
+{
+
+#define CPU_FNCLIST struct cpu_fnclist_s
+CPU_FNCLIST
+{
+	char *mode;
+	int (*function)();
+};
+
+	CPU_FNCLIST fl[] = 
+	{
+		{"avg1" ,	SYSTEM_CPU_LOAD1},
+		{"avg5" ,	SYSTEM_CPU_LOAD5},
+		{"avg15",	SYSTEM_CPU_LOAD15},
+		{0,		0}
+	};
+
+	char cpuname[MAX_STRING_LEN];
+	char mode[MAX_STRING_LEN];
+	int i;
+	
+        assert(result);
+
+        clean_result(result);
+	
+        if(num_param(param) > 2)
+        {
+                return SYSINFO_RET_FAIL;
+        }
+
+        if(get_param(param, 1, cpuname, MAX_STRING_LEN) != 0)
+        {
+                return SYSINFO_RET_FAIL;
+        }
+	if(cpuname[0] == '\0')
+	{
+		/* default parameter */
+		sprintf(cpuname, "all");
+	}
+	if(strncmp(cpuname, "all", MAX_STRING_LEN))
+	{
+		return SYSINFO_RET_FAIL;
+	}
+	
+	if(get_param(param, 2, mode, MAX_STRING_LEN) != 0)
+        {
+                mode[0] = '\0';
+        }
+        if(mode[0] == '\0')
+	{
+		/* default parameter */
+		sprintf(mode, "avg1");
+	}
+	for(i=0; fl[i].mode!=0; i++)
+	{
+		if(strncmp(mode, fl[i].mode, MAX_STRING_LEN)==0)
+		{
+			return (fl[i].function)(cmd, param, flags, result);
+		}
+	}
+	
+	return SYSINFO_RET_FAIL;
+}
+
+int	SYSTEM_CPU_LOAD1(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
 #ifdef HAVE_GETLOADAVG
 	double	load[3];
 
+	assert(result);
+
+        clean_result(result);
+		
 	if(getloadavg(load, 3))
 	{
-		*value=load[0];
+		result->type |= AR_DOUBLE;
+		result->dbl = load[0];
 		return SYSINFO_RET_OK;
 	}
 	else
@@ -323,14 +300,19 @@ int	SYSTEM_CPU_LOAD1(const char *cmd, const char *parameter,double  *value, cons
 #endif
 }
 
-int	SYSTEM_CPU_LOAD5(const char *cmd, const char *parameter,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_LOAD5(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
 #ifdef HAVE_GETLOADAVG
 	double	load[3];
 
+	assert(result);
+
+        clean_result(result);
+		
 	if(getloadavg(load, 3))
 	{
-		*value=load[1];
+		result->type = AR_DOUBLE;
+		result->dbl = load[1];
 		return SYSINFO_RET_OK;
 	}
 	else
@@ -342,14 +324,19 @@ int	SYSTEM_CPU_LOAD5(const char *cmd, const char *parameter,double  *value, cons
 #endif
 }
 	       
-int	SYSTEM_CPU_LOAD15(const char *cmd, const char *parameter,double  *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_LOAD15(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
 #ifdef HAVE_GETLOADAVG
 	double	load[3];
 
+	assert(result);
+
+        clean_result(result);
+		
 	if(getloadavg(load, 3))
 	{
-		*value=load[2];	
+		result->type = AR_DOUBLE;
+		result->dbl =load[2];	
 		return SYSINFO_RET_OK;
 	}
 	else
@@ -361,7 +348,7 @@ int	SYSTEM_CPU_LOAD15(const char *cmd, const char *parameter,double  *value, con
 #endif
 }
 
-int	SYSTEM_CPU_SWITCHES(const char *cmd, const char *parameter, double *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_SWITCHES(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
     kstat_ctl_t	    *kc;
     kstat_t	    *k;
@@ -370,6 +357,10 @@ int	SYSTEM_CPU_SWITCHES(const char *cmd, const char *parameter, double *value, c
     int	    cpu_count = 0;
     double  swt_count = 0.0;
     
+    assert(result);
+
+    clean_result(result);
+		
     kc = kstat_open();
 
     if(kc != NULL)
@@ -389,17 +380,18 @@ int	SYSTEM_CPU_SWITCHES(const char *cmd, const char *parameter, double *value, c
 	kstat_close(kc);
     }
 
-    *value = swt_count;
-    
     if(cpu_count == 0)
     {
 	return SYSINFO_RET_FAIL;
     }
 
+    result->type = AR_DOUBLE;
+    result->dbl = swt_count;
+    
     return SYSINFO_RET_OK;
 }
 
-int	SYSTEM_CPU_INTR(const char *cmd, const char *parameter, double *value, const char *msg, int mlen_max)
+int	SYSTEM_CPU_INTR(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
     kstat_ctl_t	    *kc;
     kstat_t	    *k;
@@ -408,6 +400,10 @@ int	SYSTEM_CPU_INTR(const char *cmd, const char *parameter, double *value, const
     int	    cpu_count = 0;
     double  intr_count = 0.0;
     
+    assert(result);
+
+    clean_result(result);
+	
     kc = kstat_open();
 
     if(kc != NULL)
@@ -427,12 +423,13 @@ int	SYSTEM_CPU_INTR(const char *cmd, const char *parameter, double *value, const
 	kstat_close(kc);
     }
 
-    *value = intr_count;
-    
     if(cpu_count == 0)
     {
 	return SYSINFO_RET_FAIL;
     }
+    
+    result->type = AR_DOUBLE;
+    result->dbl = intr_count;
     
     return SYSINFO_RET_OK;
 }
