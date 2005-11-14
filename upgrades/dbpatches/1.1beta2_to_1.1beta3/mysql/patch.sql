33e11aca92b26bf511ef9aef1aa479e94e5f9d9d
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2005-11-14 13:30:35 +0000

    - added table history_uint (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@2313 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.1beta2_to_1.1beta3/mysql/patch.sql
+++ upgrades/dbpatches/1.1beta2_to_1.1beta3/mysql/patch.sql
@@ -21,3 +21,14 @@ CREATE TABLE servers (
 ) type=InnoDB;
 insert into servers values(1,'DEFAULT','127.0.0.1',10051);
 
+--
+-- Table structure for table 'history_uint'
+--
+
+CREATE TABLE history_uint (
+  itemid		int(4)		DEFAULT '0' NOT NULL,
+  clock			int(4)		DEFAULT '0' NOT NULL,
+  value			bigint unsigned	DEFAULT '0' NOT NULL,
+--  PRIMARY KEY (itemid,clock)
+  KEY itemidclock (itemid, clock)
+) type=InnoDB;
