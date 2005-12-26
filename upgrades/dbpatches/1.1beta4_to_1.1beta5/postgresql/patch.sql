4d72d5435ff9877b58ee6cbf9432f248ce0026c4
Alexei Vladishev <alexei.vladishev@zabbix.com>
2005-12-26 20:32:02 +0000

    Not finished:  - new concept of defenition of actions, one screen (Alexei)  - new fields for table 'actions' (Alexei)

--- upgrades/dbpatches/1.1beta4_to_1.1beta5/postgresql/patch.sql
+++ upgrades/dbpatches/1.1beta4_to_1.1beta5/postgresql/patch.sql
@@ -1,1 +1,11 @@
 alter table screens_items  add elements		int4		DEFAULT '25' NOT NULL;
+alter table actions add  source			int2		DEFAULT '0' NOT NULL;
+alter table actions add  actiontype		int2		DEFAULT '0' NOT NULL;
+alter table actions add  filter_triggerid	int4		DEFAULT '0' NOT NULL;
+alter table actions add  filter_hostid		int4		DEFAULT '0' NOT NULL;
+alter table actions add  filter_groupid	int4		DEFAULT '0' NOT NULL;
+alter table actions add  filter_trigger_name	varchar(255)	DEFAULT '' NOT NULL;
+update actions set filter_triggerid=triggerid where scope=0;
+update actions set filter_hostid=triggerid where scope=1;
+alter table actions drop triggerid;
+alter table actions drop scope;
