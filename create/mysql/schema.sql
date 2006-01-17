4ae1caa9fbb2da1ba7952d2e232e205b8fc7a1ee
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2006-01-17 07:10:38 +0000

    - minor fix for schema of table 'rights' (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@2532 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- create/mysql/schema.sql
+++ create/mysql/schema.sql
@@ -479,7 +479,7 @@ CREATE TABLE sessions (
 
 CREATE TABLE rights (
   rightid		int(4)		NOT NULL auto_increment,
-  userid		int(4)		DEFAULT '' NOT NULL,
+  userid		int(4)		DEFAULT '0' NOT NULL,
   name			char(255)	DEFAULT '' NOT NULL,
   permission		char(1)		DEFAULT '' NOT NULL,
   id			int(4),
