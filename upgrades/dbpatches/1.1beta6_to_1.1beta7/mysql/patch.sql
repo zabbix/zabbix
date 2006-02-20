dd1e1557c1280b9517c5ca6ab9c024d00b87f669
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2006-02-20 14:15:16 +0000

    - added tables valuemaps and mappings (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@2663 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/1.1beta6_to_1.1beta7/mysql/patch.sql
+++ upgrades/dbpatches/1.1beta6_to_1.1beta7/mysql/patch.sql
@@ -2,3 +2,26 @@ alter table hosts add	templateid	int(4) DEFAULT '0' NOT NULL;
 alter table items add	templateid	int(4) DEFAULT '0' NOT NULL;
 alter table triggers add	templateid	int(4) DEFAULT '0' NOT NULL;
 alter table graphs add	templateid	int(4) DEFAULT '0' NOT NULL;
+
+--
+-- Table structure for table 'valuemaps'
+--
+
+CREATE TABLE valuemaps (
+  valuemapid		int(4)		NOT NULL auto_increment,
+  name			varchar(64)	DEFAULT '' NOT NULL,
+  PRIMARY KEY (valuemapid)
+) type=InnoDB;
+
+--
+-- Table structure for table 'mapping'
+--
+
+CREATE TABLE mappings (
+  mappingid		int(4)		NOT NULL auto_increment,
+  valuemapid		int(4)		DEFAULT '0' NOT NULL,
+  value			varchar(64)	DEFAULT '' NOT NULL,
+  newvalue		varchar(64)	DEFAULT '' NOT NULL,
+  PRIMARY KEY (mappingid),
+  KEY valuemapid (valuemapid)
+) type=InnoDB;
