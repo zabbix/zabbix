f47e4ea3ee8f7967ca36c878277aa9f19fa63da9
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2006-02-23 19:53:27 +0000

    Conversion to beta7.
    git-svn-id: svn://svn.zabbix.com/trunk@2685 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/locales/fr_fr.inc.php
+++ frontends/php/include/locales/fr_fr.inc.php
@@ -200,7 +200,6 @@
 	"S_COMPARE"=>						"Compare",
 
 //	Footer
-	"S_ZABBIX_VER"=>					"ZABBIX 1.1beta6",
 	"S_COPYRIGHT_BY"=>					"Copyright 2001-2006 par ",
 	"S_CONNECTED_AS"=>					"Connecté au nom de",
 	"S_SIA_ZABBIX"=>					"SIA Zabbix",
--- frontends/php/include/locales/it_it.inc.php
+++ frontends/php/include/locales/it_it.inc.php
@@ -202,7 +202,6 @@
 	"S_COMPARE"=>				"Compara",
 
 //	Footer
-	"S_ZABBIX_VER"=>			"ZABBIX 1.1beta6",
 	"S_COPYRIGHT_BY"=>			"Copyright 2001-2006 by ",
 	"S_CONNECTED_AS"=>			"Connesso come utente: ",
 	"S_SIA_ZABBIX"=>			"SIA Zabbix",
