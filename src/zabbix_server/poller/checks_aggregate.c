1b77fd5b73aed1ee22cf92a623189b302b9965f9
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2006-04-21 13:29:51 +0000

    Minor changes.
    git-svn-id: svn://svn.zabbix.com/trunk@2762 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_server/poller/checks_aggregate.c
+++ src/zabbix_server/poller/checks_aggregate.c
@@ -20,6 +20,38 @@
 #include "common.h"
 #include "checks_aggregate.h"
 
+/*
+ * grpfunc: grpmax, grpmin, grpsum, grpavg
+ * itemfunc: last, min, max, avg, sum
+ */
+static int	evaluate_aggregate(AGENT_RESULT *res,char *grpfunc, char *hostgroup, char *itemkey, char *itemfunc, char *param)
+{
+	char		sql[MAX_STRING_LEN];
+ 
+	DB_RESULT	*result;
+
+	int		i;
+
+	zabbix_log( LOG_LEVEL_WARNING, "In evaluate_aggregate('%s','%s','%s','%s','%s')",grpfunc,hostgroup,itemkey,itemfunc,param);
+
+	if(strcmp(itemfunc,"last") == 0)
+	{
+		snprintf(sql,sizeof(sql)-1,"select items.lastvalue,items.value_type from items,hosts_groups,hosts,groups where groups.groupid=1 and hosts_groups.groupid=groups.groupid and items.hostid=hosts.hostid and hosts_groups.hostid=hosts.hostid and items.lastvalue!=NULL and groups.name='%s'",hostgroup);
+	}
+	zabbix_log( LOG_LEVEL_WARNING, "SQL [%s]",sql);
+
+	result = DBselect(sql);
+	
+	for(i=0;i<DBnum_rows(result);i++)
+	{
+		zabbix_log( LOG_LEVEL_WARNING, "Item value([%s])",DBget_field(result,i,0));
+	}
+
+	DBfree_result(result);
+
+	return SUCCEED;
+}
+
 /******************************************************************************
  *                                                                            *
  * Function: get_value_aggregate                                              *
@@ -41,18 +73,21 @@ int	get_value_aggregate(DB_ITEM *item, AGENT_RESULT *result)
 {
 	char	function_grp[MAX_STRING_LEN];
 	char	key[MAX_STRING_LEN];
-	char	*p;
+	char	group[MAX_STRING_LEN];
+	char	itemkey[MAX_STRING_LEN];
+	char	function_item[MAX_STRING_LEN];
+	char	parameter[MAX_STRING_LEN];
+	char	*p,*p2;
 
 	int 	ret = SUCCEED;
 
 
-	zabbix_log( LOG_LEVEL_WARNING, "In get_value_affgregate([%s])",item->key);
+	zabbix_log( LOG_LEVEL_WARNING, "In get_value_aggregate([%s])",item->key);
 
 	init_result(result);
 
 	strscpy(key, item->key);
-	p=strstr(key,"(");
-	if(p!=NULL)
+	if((p=strstr(key,"(")) != NULL)
 	{
 		*p=0;
 		strscpy(function_grp,key);
@@ -61,17 +96,90 @@ int	get_value_aggregate(DB_ITEM *item, AGENT_RESULT *result)
 	}
 	else	ret = NOTSUPPORTED;
 
+	if(ret == SUCCEED)
+	{
+		if((p2=strstr(p,"'")) != NULL)
+		{
+			p2++;
+		}
+		else	ret = NOTSUPPORTED;
+
+		if((ret == SUCCEED) && (p=strstr(p2,"'")) != NULL)
+		{
+			*p=0;
+			strscpy(group,p2);
+			*p='\'';
+			p++;
+		}
+		else	ret = NOTSUPPORTED;
+	}
+
+	if(*p != ',')	ret = NOTSUPPORTED;
+
+	if(ret == SUCCEED)
+	{
+		if((p2=strstr(p,"'")) != NULL)
+		{
+			p2++;
+		}
+		else	ret = NOTSUPPORTED;
+
+		if((ret == SUCCEED) && (p=strstr(p2,"'")) != NULL)
+		{
+			*p=0;
+			strscpy(itemkey,p2);
+			*p='\'';
+			p++;
+		}
+		else	ret = NOTSUPPORTED;
+	}
+
+	if(*p != ',')	ret = NOTSUPPORTED;
+
+	if(ret == SUCCEED)
+	{
+		if((p2=strstr(p,"'")) != NULL)
+		{
+			p2++;
+		}
+		else	ret = NOTSUPPORTED;
+
+		if((ret == SUCCEED) && (p=strstr(p2,"'")) != NULL)
+		{
+			*p=0;
+			strscpy(function_item,p2);
+			*p='\'';
+			p++;
+		}
+		else	ret = NOTSUPPORTED;
+	}
+
+	if(*p != ',')	ret = NOTSUPPORTED;
+
+	if(ret == SUCCEED)
+	{
+		if((p2=strstr(p,"'")) != NULL)
+		{
+			p2++;
+			zabbix_log( LOG_LEVEL_WARNING, "p2[%s]",p2);
+		}
+		else	ret = NOTSUPPORTED;
+
+		if((ret == SUCCEED) && (p=strstr(p2,"'")) != NULL)
+		{
+			*p=0;
+			strscpy(parameter,p2);
+			*p='\'';
+			p++;
+		}
+		else	ret = NOTSUPPORTED;
+	}
+
 	SET_UI64_RESULT(result, 0);
 
-	zabbix_log( LOG_LEVEL_WARNING, "Evaluating aggregate [%s] grpfunc [%s]",item->key, function_grp);
+	zabbix_log( LOG_LEVEL_WARNING, "Evaluating aggregate[%s] grpfunc[%s] group[%s] itemkey[%s] itemfunc [%s] parameter [%s]",item->key, function_grp, group, itemkey, function_item, parameter);
 
-/*	else
-	{
-		snprintf(error,MAX_STRING_LEN-1,"Internal check [%s] is not supported", item->key);
-		zabbix_log( LOG_LEVEL_WARNING, error);
-		SET_STR_RESULT(result, strdup(error));
-		return NOTSUPPORTED;
-	}*/
+	evaluate_aggregate(result,function_grp, group, itemkey, function_item, parameter);
 
 	return ret;
 }
