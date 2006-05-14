004a8574d46caa26bd0292e530a75647868cddbe
hugetoad <hugetoad@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2006-05-14 10:34:33 +0000

    Minor changes.
    git-svn-id: svn://svn.zabbix.com/trunk@2833 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.1beta9_to_1.1beta10/postgresql/patch.sql
+++ upgrades/dbpatches/1.1beta9_to_1.1beta10/postgresql/patch.sql
@@ -32,3 +32,15 @@ CREATE TABLE items_applications (
 );
 
 alter table audit rename auditlog;
+
+alter table auditlog add resourcetype          int4          DEFAULT '0' NOT NULL;
+update auditlog set resourcetype=resource;
+alter table auditlog drop resource;
+
+alter table screens add hsize   int4  DEFAULT '1' NOT NULL;
+alter table screens add vsize   int4  DEFAULT '1' NOT NULL;
+update screens set hsize=cols;
+update screens set vsize=rows;
+alter table screens drop cols;
+alter table screens drop rows;
+
