1952a36587271dd1ed074e07e9552c2afef214ad
Alexei Vladishev <alexei.vladishev@zabbix.com>
2006-05-30 08:03:18 +0000

    - support of web.getpage[] (Alexei)

--- src/libs/zbxsysinfo/common/http.c
+++ src/libs/zbxsysinfo/common/http.c
@@ -26,7 +26,7 @@
 int	get_http_page(char *hostname, char *param, int port, char *buffer, int max_buf_len)
 {
 	char	*haddr;
-	char	c[1024];
+	char	c[MAX_STRING_LEN];
 	
 	int	s;
 	struct	sockaddr_in addr;
@@ -53,23 +53,23 @@ int	get_http_page(char *hostname, char *param, int port, char *buffer, int max_b
 	if (s == -1)
 	{
 		close(s);
-		return SYSINFO_RET_OK;
+		return SYSINFO_RET_FAIL;
 	}
 
 	if (connect(s, (struct sockaddr *) &addr, addrlen) == -1)
 	{
 		close(s);
-		return SYSINFO_RET_OK;
+		return SYSINFO_RET_FAIL;
 	}
 
-	snprintf(c,1024-1,"GET /%s HTTP/1.1\nHost: %s\nConnection: close\n\n", param, hostname);
+	snprintf(c,MAX_STRING_LEN-1,"GET /%s HTTP/1.1\nHost: %s\nConnection: close\n\n", param, hostname);
 
 	write(s,c,strlen(c));
 
 	memset(buffer, 0, max_buf_len);
 
 	total=0;
-	while((n=read(s, buffer+total, max_buf_len-1))>0)
+	while((n=read(s, buffer+total, max_buf_len-1-total))>0)
 	{
 		total+=n;
 		printf("Read %d bytes\n", total);
@@ -78,7 +78,58 @@ int	get_http_page(char *hostname, char *param, int port, char *buffer, int max_b
 	close(s);
 	return SYSINFO_RET_OK;
 }
-#define ZABBIX_TEST
+
+int	WEB_GETPAGE(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
+{
+#define	ZABBIX_MAX_WEBPAGE_SIZE	100*1024
+	char	hostname[MAX_STRING_LEN];
+	char	path[MAX_STRING_LEN];
+	char	port_str[MAX_STRING_LEN];
+
+	char	*buffer;
+
+	int	ret;
+
+        assert(result);
+
+        init_result(result);
+	
+        if(num_param(param) > 3)
+        {
+                return SYSINFO_RET_FAIL;
+        }
+        
+	if(get_param(param, 1, hostname, MAX_STRING_LEN) != 0)
+	{
+                return SYSINFO_RET_FAIL;
+	}
+
+	if(get_param(param, 2, path, MAX_STRING_LEN) != 0)
+	{
+		path[0]='\0';
+	}
+
+	if(get_param(param, 3, port_str, MAX_STRING_LEN) != 0)
+	{
+		strscpy(port_str, "80");
+	}
+
+	buffer=malloc(100*1024);
+	if(get_http_page(hostname, path, atoi(port_str), buffer, ZABBIX_MAX_WEBPAGE_SIZE) == SYSINFO_RET_OK)
+	{
+		SET_TEXT_RESULT(result, buffer);
+		ret = SYSINFO_RET_OK;
+	}
+	else
+	{
+		ret = SYSINFO_RET_FAIL;
+	}
+	free(buffer);
+
+	return ret;
+}
+
+/*#define ZABBIX_TEST*/
 
 #ifdef ZABBIX_TEST
 int main()
