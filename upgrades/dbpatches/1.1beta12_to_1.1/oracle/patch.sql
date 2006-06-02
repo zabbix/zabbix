90d525ad0b7d9f29284b076e61f993dc1ea5d726
osmiy <osmiy@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2006-06-02 11:13:07 +0000

    - minor changes
    git-svn-id: svn://svn.zabbix.com/trunk@2945 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.1beta12_to_1.1/oracle/patch.sql
+++ upgrades/dbpatches/1.1beta12_to_1.1/oracle/patch.sql
@@ -1,6 +1,6 @@
-alter table graphs_items add calc_fnc number(3) DEFAULT '2' NOT NULL;
-alter table graphs_items add type number(3) DEFAULT '0' NOT NULL;
-alter table graphs_items add periods_cnt number(10) DEFAULT '5' NOT NULL;
+ALTER TABLE graphs_items ADD calc_fnc number(3) DEFAULT '2' NOT NULL;
+ALTER TABLE graphs_items ADD TYPE number(3) DEFAULT '0' NOT NULL;
+ALTER TABLE graphs_items ADD periods_cnt number(10) DEFAULT '5' NOT NULL;
 
 DROP INDEX history_text_itemidclock;
 DROP TABLE history_text;
