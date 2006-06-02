9bd43d7aea638b8c5ce7e2745ef052056bb36245
osmiy <osmiy@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2006-06-02 06:42:49 +0000

    - added "set_image_header" function for global image controling (Eugene)  - Added database patch for oracle (Eugene)
    git-svn-id: svn://svn.zabbix.com/trunk@2938 97f52cf1-0a1b-0410-bd0e-c28be96e8082

---
+++ upgrades/dbpatches/1.1beta12_to_1.1/oracle/patch.sql
@@ -0,0 +1,4 @@
+alter table graphs_items add calc_fnc number(3) DEFAULT '2' NOT NULL;
+alter table graphs_items add type number(3) DEFAULT '0' NOT NULL;
+alter table graphs_items add periods_cnt number(10) DEFAULT '5' NOT NULL;
+
