305fbb4a83625bb5e63356c86c626c401e9df2b2
hugetoad <hugetoad@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2006-06-24 12:11:43 +0000

    - ZABBIX server to bind even if TIME_WAIT sockets exist (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@2972 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/zabbix_server/server.c
+++ src/zabbix_server/server.c
@@ -408,12 +408,22 @@ int	tcp_listen(const char *host, int port, socklen_t *addrlenp)
 {
 	int	sockfd;
 	struct	sockaddr_in      serv_addr;
+	int	on;
 /*	struct linger ling;*/
 
 	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{
 		zabbix_log( LOG_LEVEL_CRIT, "Cannot create socket");
 		exit(1);
+	}
+
+	/* Enable address reuse */
+	/* This is to immediately use the address even if it is in TIME_WAIT state */
+	/* http://www-128.ibm.com/developerworks/linux/library/l-sockpit/index.html */
+	on = 1;
+	if( -1 == setsockopt( sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on) ))
+	{
+		zabbix_log(LOG_LEVEL_WARNING, "Cannot setsockopt SO_REUSEADDR [%s]", strerror(errno));
 	}
 
 /*	if(CONFIG_NOTIMEWAIT == 1)
