519d62f67614549f03200830a666d2ccb02cddd7
Alexei Vladishev <alexei.vladishev@zabbix.com>
2006-08-01 09:34:58 +0000

    - better pattern of distribution of item next checks to improve overall  performance (Alexei)

--- src/zabbix_server/poller/poller.c
+++ src/zabbix_server/poller/poller.c
@@ -278,7 +278,9 @@ int get_values(void)
 		{
 			if(item.status == ITEM_STATUS_NOTSUPPORTED)
 			{
-				DBexecute("update items set nextcheck=%d, lastclock=%d where itemid=%d",calculate_item_nextcheck(CONFIG_REFRESH_UNSUPPORTED,now), now, item.itemid);
+				/* It is not correct */
+/*				snprintf(sql,sizeof(sql)-1,"update items set nextcheck=%d, lastclock=%d where itemid=%d",calculate_item_nextcheck(item.itemid, CONFIG_REFRESH_UNSUPPORTED,now), now, item.itemid);*/
+				DBexecute("update items set nextcheck=%d, lastclock=%d where itemid=%d",CONFIG_REFRESH_UNSUPPORTED+now, now, item.itemid);
 			}
 			else
 			{
