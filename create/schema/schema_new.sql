33f7031621b6b46a9e045821d6efd04205920781
Alexei Vladishev <alexei.vladishev@zabbix.com>
2006-11-06 11:54:33 +0000

    Minor changes.

--- create/schema/schema_new.sql
+++ create/schema/schema_new.sql
@@ -31,6 +31,7 @@ FIELD		|slave_history	|t_integer	|'0'	|NOT NULL	|
 FIELD		|slave_trends	|t_integer	|'0'	|NOT NULL	|
 FIELD		|event_lastid	|t_id		|'0'	|NOT NULL	|
 FIELD		|history_lastid	|t_bigint	|'0'	|NOT NULL	|
+FIELD		|history_str_lastid|t_bigint	|'0'	|NOT NULL	|
 FIELD		|history_uint_lastid|t_bigint	|'0'	|NOT NULL	|
 FIELD		|nodetype	|t_integer	|'0'	|NOT NULL	|
 FIELD		|masterid	|t_id		|'0'	|NOT NULL	|
