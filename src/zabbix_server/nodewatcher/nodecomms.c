95f878ecdfdc9655bdcc3f220179a49d05f5e6be
Alexei Vladishev <alexei.vladishev@zabbix.com>
2006-11-07 15:15:10 +0000

    Minor changes.

--- src/zabbix_server/nodewatcher/nodecomms.c
+++ src/zabbix_server/nodewatcher/nodecomms.c
@@ -89,7 +89,7 @@ int send_to_node(int dest_nodeid, int nodeid, char *data)
 	DB_ROW		row;
 
 //	zabbix_log( LOG_LEVEL_WARNING, "In send_to_node (%d,%d)", nodeid, strlen(xml));
-	zabbix_log( LOG_LEVEL_WARNING, "NODE %d: Sending data of node %d to node %d", CONFIG_NODEID, nodeid, dest_nodeid);
+	zabbix_log( LOG_LEVEL_WARNING, "NODE %d: Sending data of node %d to node %d datalen %d", CONFIG_NODEID, nodeid, dest_nodeid, strlen(data));
 //	zabbix_log( LOG_LEVEL_WARNING, "Data [%s]", data);
 
 	result = DBselect("select ip, port from nodes where nodeid=%d", dest_nodeid);
@@ -184,7 +184,12 @@ int send_to_node(int dest_nodeid, int nodeid, char *data)
 
 	if(strcmp(answer,"OK") == 0)
 	{
+		zabbix_log( LOG_LEVEL_DEBUG, "OK");
 		ret = SUCCEED;
+	}
+	else
+	{
+		zabbix_log( LOG_LEVEL_DEBUG, "NOT OK");
 	}
  
 	if( close(s)!=0 )
