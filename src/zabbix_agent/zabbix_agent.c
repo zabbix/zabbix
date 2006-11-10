e880c581a544c7402dad4366f1297d59f32e9d8e
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2006-11-10 11:53:42 +0000

    - fixed default config name for zabbix_agent (Alexei)
    git-svn-id: svn://svn.zabbix.com/branches/1.1.4@3447 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_agent/zabbix_agent.c
+++ src/zabbix_agent/zabbix_agent.c
@@ -108,7 +108,7 @@ void    init_config(void)
 
 	if(CONFIG_FILE == NULL)
 	{
-		CONFIG_FILE = strdup("/etc/zabbix/zabbix_agentd.conf");
+		CONFIG_FILE = strdup("/etc/zabbix/zabbix_agent.conf");
 	}
 	
 	parse_cfg_file(CONFIG_FILE,cfg);
