cd00554d9a4f112e06e0c986c243da38a7232277
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2006-11-14 18:09:56 +0000

    Minor changes.
    git-svn-id: svn://svn.zabbix.com/branches/1.1.4@3474 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/config.inc.php
+++ frontends/php/include/config.inc.php
@@ -890,7 +890,7 @@ COpt::profiling_start("page");
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo S_HTML_CHARSET; ?>">
-<meta name="Author" content="ZABBIX SIA (Alexei Vladishev, Eugene Grigorjev)">
+<meta name="Author" content="ZABBIX SIA">
 <link rel="stylesheet" href="css.css">
 <?php
 //	if($USER_DETAILS['alias']=='guest')
--- frontends/php/include/locales/en_gb.inc.php
+++ frontends/php/include/locales/en_gb.inc.php
@@ -280,7 +280,7 @@
 	"S_COMPARE"=>				"Compare",
 
 //	Footer
-	"S_ZABBIX_VER"=>			"ZABBIX 1.1.3",
+	"S_ZABBIX_VER"=>			"ZABBIX 1.1.4",
 	"S_COPYRIGHT_BY"=>			"Copyright 2001-2006 by ",
 	"S_CONNECTED_AS"=>			"Connected as",
 	"S_SIA_ZABBIX"=>			"SIA Zabbix",
