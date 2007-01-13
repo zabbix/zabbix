e00aa14a9cf414f03801673e26ee466ea14b1e38
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-01-13 13:06:37 +0000

    - misc HP-UX fixes (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@3705 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxsysinfo/hpux/diskio.c
+++ src/libs/zbxsysinfo/hpux/diskio.c
@@ -181,7 +181,7 @@ DEV_FNCLIST
 		zbx_snprintf(type, sizeof(type), "bps");
 	}
 	
-	if(get_param(param, 3, mode, sieof(mode)) != 0)
+	if(get_param(param, 3, mode, sizof(mode)) != 0)
         {
                 mode[0] = '\0';
         }
