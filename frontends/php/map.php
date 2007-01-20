1b726f98f9e48f16fe094aba6c7a2fb708cebe07
Alexei Vladishev <alexei.vladishev@zabbix.com>
2007-01-20 20:32:25 +0000

    - do not show connector as ON if trigger disabled (Alexei)

--- frontends/php/map.php
+++ frontends/php/map.php
@@ -141,7 +141,8 @@ include_once "include/page_header.php";
 		if(!is_null($link["triggerid"]))
 		{
 			$trigger=get_trigger_by_triggerid($link["triggerid"]);
-			if($trigger["value"] == TRIGGER_VALUE_TRUE)
+//			if($trigger["value"] == TRIGGER_VALUE_TRUE)
+			if($trigger["status"] == TRIGGER_STATUS_ENABLED && $trigger["value"] == TRIGGER_VALUE_TRUE)
 			{
 				$drawtype = $link["drawtype_on"];
 				$color = $colors[$link["color_on"]];
