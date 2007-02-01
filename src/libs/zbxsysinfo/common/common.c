df875f76e152252333ccc23bf9ae5b7a228aa480
guliver <guliver@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-02-01 15:06:08 +0000

    * added new metric: "system.boottime" to zabbix_agent  * added getPROC2() function (retrieve value from /proc file system by name)
    git-svn-id: svn://svn.zabbix.com/trunk@3785 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxsysinfo/common/common.c
+++ src/libs/zbxsysinfo/common/common.c
@@ -925,6 +925,68 @@ int	TCP_LISTEN(const char *cmd, const char *param, unsigned flags, AGENT_RESULT 
 }
 
 #ifdef	HAVE_PROC
+int getPROC2(char *file, char *param, int fieldno, unsigned flags, int type, AGENT_RESULT *result)
+{
+
+	FILE	*f;
+	char	*res;
+	char	buf[MAX_STRING_LEN];
+	int	i;
+	int	found;
+	unsigned long uValue;
+	double fValue;
+        
+	if (NULL == (f = fopen(file,"r")))
+	{
+		return SYSINFO_RET_FAIL;
+	}
+
+	// find line
+	found = 0;
+	while ( fgets(buf, MAX_STRING_LEN, f) != NULL ) 
+	{
+		if (strncmp(buf, "btime", 5) == 0)
+		{
+			found = 1;
+			break;
+		}
+	}
+
+	if (!found) return SYSINFO_RET_FAIL;
+
+	// find field
+	res = (char *)strtok(buf, " "); // btime field1 field2
+	for(i=1; i<=fieldno; i++)
+	{
+		res = (char *)strtok(NULL," ");
+	}
+
+	zbx_fclose(f);
+
+	if ( res == NULL )
+	{
+		return SYSINFO_RET_FAIL;
+	}
+
+	// convert field to right type
+	switch (type)
+	{
+	case AR_UINT64:
+		sscanf(res, "%lu", &uValue);
+		SET_UI64_RESULT(result, uValue);
+		break;
+	case AR_DOUBLE:
+		sscanf(res, "%lf", &fValue);
+		SET_DBL_RESULT(result, fValue);
+		break;
+	case AR_STRING: default:
+		SET_STR_RESULT(result, buf);
+		break;
+	}
+
+	return SYSINFO_RET_OK;
+}
+
 int	getPROC(char *file, int lineno, int fieldno, unsigned flags, AGENT_RESULT *result)
 {
 	FILE	*f;
