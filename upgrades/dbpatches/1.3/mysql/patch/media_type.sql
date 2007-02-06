49ca7e749192ea5ce6076b6e3dbe4d52a479a55f
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-02-06 10:17:12 +0000

    - added media_type.username, media_type.passwd (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@3794 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.3/mysql/patch/media_type.sql
+++ upgrades/dbpatches/1.3/mysql/patch/media_type.sql
@@ -7,9 +7,11 @@ CREATE TABLE media_type_tmp (
 	smtp_email		varchar(255)		DEFAULT ''	NOT NULL,
 	exec_path		varchar(255)		DEFAULT ''	NOT NULL,
 	gsm_modem		varchar(255)		DEFAULT ''	NOT NULL,
+	username		varchar(255)		DEFAULT ''	NOT NULL,
+	passwd			varchar(255)		DEFAULT ''	NOT NULL,
 	PRIMARY KEY (mediatypeid)
 ) ENGINE=InnoDB;
 
-insert into media_type_tmp select * from media_type;
+insert into media_type_tmp select mediatypeid,type,description,smtp_server,smtp_helo,smtp_email,exec_path,gsm_modem,'','' from media_type;
 drop table media_type;
 alter table media_type_tmp rename media_type;
