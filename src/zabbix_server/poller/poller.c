2f6b4a23edbb0e8556e1e8deee20c99eb2d830fd
alex <alex@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2007-03-24 19:18:47 +0000

    - added server-side support of external checks. Thanks to Mike Nestor. (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@3918 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/zabbix_server/poller/poller.c
+++ src/zabbix_server/poller/poller.c
@@ -27,6 +27,7 @@
 
 #include "checks_agent.h"
 #include "checks_aggregate.h"
+#include "checks_external.h"
 #include "checks_internal.h"
 #include "checks_simple.h"
 #include "checks_snmp.h"
@@ -80,6 +81,10 @@ int	get_value(DB_ITEM *item, AGENT_RESULT *result)
 	else if(item->type == ITEM_TYPE_AGGREGATE)
 	{
 		res=get_value_aggregate(item, result);
+	}
+	else if(item->type == ITEM_TYPE_EXTERNAL)
+	{
+		res=get_value_external(item, result);
 	}
 	else
 	{
