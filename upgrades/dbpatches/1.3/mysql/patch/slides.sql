616906768d2e6f3b86e96822dcb8e37e3b6cad2f
alex <alex@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2007-03-25 07:36:23 +0000

    - added new table slides and slideshows (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@3919 97f52cf1-0a1b-0410-bd0e-c28be96e8082

---
+++ upgrades/dbpatches/1.3/mysql/patch/slides.sql
@@ -0,0 +1,8 @@
+CREATE TABLE slides (
+        slideid         bigint unsigned         DEFAULT '0'     NOT NULL,
+        slideshowid             bigint unsigned         DEFAULT '0'     NOT NULL,
+        delay           integer         DEFAULT '0'     NOT NULL,
+        PRIMARY KEY (slideid)
+)  ENGINE=InnoDB;
+CREATE INDEX slides_slides_1 on slides (slideshowid);
+
