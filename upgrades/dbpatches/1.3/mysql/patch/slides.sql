af53d212ec003a5878fa86e4389f67a6b17f448c
Alexei Vladishev <alexei.vladishev@zabbix.com>
2007-03-26 07:49:26 +0000

    Added slides.step (Alexei)

--- upgrades/dbpatches/1.3/mysql/patch/slides.sql
+++ upgrades/dbpatches/1.3/mysql/patch/slides.sql
@@ -1,6 +1,7 @@
 CREATE TABLE slides (
         slideid         bigint unsigned         DEFAULT '0'     NOT NULL,
         slideshowid             bigint unsigned         DEFAULT '0'     NOT NULL,
+        step            integer         DEFAULT '0'     NOT NULL,
         delay           integer         DEFAULT '0'     NOT NULL,
         PRIMARY KEY (slideid)
 )  ENGINE=InnoDB;
