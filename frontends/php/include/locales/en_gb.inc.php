40e4e8ded9fbe88ee4d62f44dfe067e49484b66e
Alexei Vladishev <alexei.vladishev@zabbix.com>
2007-03-26 17:32:58 +0000

    Change of release version.

--- frontends/php/include/locales/en_gb.inc.php
+++ frontends/php/include/locales/en_gb.inc.php
@@ -457,7 +457,7 @@
 	'S_COMPARE'=>				'Compare',
 
 //	Footer
-	'S_ZABBIX_VER'=>			'ZABBIX 1.3.3',
+	'S_ZABBIX_VER'=>			'ZABBIX 1.3.4',
 	'S_COPYRIGHT_BY'=>			'Copyright 2001-2007 by ',
 	'S_CONNECTED_AS'=>			'Connected as',
 	'S_SIA_ZABBIX'=>			'SIA Zabbix',
