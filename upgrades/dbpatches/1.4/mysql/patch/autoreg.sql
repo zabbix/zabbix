41903d5790988c9795cedc98643b674f50585b02
Alexei Vladishev <alexei.vladishev@zabbix.com>
2007-04-12 12:45:57 +0000

    Removed table autoreg.

--- upgrades/dbpatches/1.4/mysql/patch/autoreg.sql
+++ upgrades/dbpatches/1.4/mysql/patch/autoreg.sql
@@ -1,11 +1,1 @@
-CREATE TABLE autoreg_tmp (
-	id		bigint unsigned		DEFAULT '0'	NOT NULL,
-	priority		integer		DEFAULT '0'	NOT NULL,
-	pattern		varchar(255)		DEFAULT ''	NOT NULL,
-	hostid		bigint unsigned		DEFAULT '0'	NOT NULL,
-	PRIMARY KEY (id)
-) ENGINE=InnoDB;
-
-insert into autoreg_tmp select * from autoreg;
 drop table autoreg;
-alter table autoreg_tmp rename autoreg;
--- upgrades/dbpatches/1.4/postgresql/patch/autoreg.sql
+++ upgrades/dbpatches/1.4/postgresql/patch/autoreg.sql
@@ -1,11 +1,1 @@
-CREATE TABLE autoreg_tmp (
-	id		bigint	DEFAULT '0'	NOT NULL,
-	priority	integer		DEFAULT '0'	NOT NULL,
-	pattern		varchar(255)		DEFAULT ''	NOT NULL,
-	hostid		bigint	DEFAULT '0'	NOT NULL,
-	PRIMARY KEY (id)
-);
-
-insert into autoreg_tmp select * from autoreg;
 drop table autoreg;
-alter table autoreg_tmp rename to autoreg;
