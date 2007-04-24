6489490b50144519f6b1175d7610fe6dd3ba9031
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-04-24 10:11:49 +0000

    Change of version to 1.3.6.
    git-svn-id: svn://svn.zabbix.com/trunk@4056 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -74,7 +74,7 @@
 
 #define	APPLICATION_NAME	"ZABBIX Agent"
 #define	ZABBIX_REVDATE		"16 April 2007"
-#define	ZABBIX_VERSION		"1.3.5"
+#define	ZABBIX_VERSION		"1.3.6"
 
 #if defined(_WINDOWS)
 /*#	pragma warning (disable: 4100)*/
