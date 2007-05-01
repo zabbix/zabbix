4972af517668ea206db500f53aa383e733b82ef9
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-05-01 12:06:28 +0000

    - new templates (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@4077 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_server/discoverer/discoverer.c
+++ src/zabbix_server/discoverer/discoverer.c
@@ -513,7 +513,6 @@ static int discover_service(DB_DCHECK *check, char *ip, int port)
 			case SVC_AGENT:
 			case SVC_SNMPv1:
 			case SVC_SNMPv2c:
-#ifdef HAVE_SNMP
 				memset(&item,0,sizeof(DB_ITEM));
 				strscpy(item.key,check->key_);
 				item.host_name	= ip;
@@ -545,6 +544,7 @@ static int discover_service(DB_DCHECK *check, char *ip, int port)
 					}
 				}
 				else
+#ifdef HAVE_SNMP
 				{
 					if(SUCCEED == get_value_snmp(&item, &value))
 					{
@@ -560,7 +560,9 @@ static int discover_service(DB_DCHECK *check, char *ip, int port)
 					}
 				}
 #else
-				ret = FAIL;
+				{
+					ret = FAIL;
+				}
 #endif
 				break;
 			/* Simple checks */
