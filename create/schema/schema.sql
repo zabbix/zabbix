90f1044ad520c79987196bdeb408877bc571bbd4
alex <alex@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2007-05-14 11:35:18 +0000

    - new column httpstep.status_codes (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@4129 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- create/schema/schema.sql
+++ create/schema/schema.sql
@@ -101,6 +101,7 @@ FIELD		|url		|t_varchar(128)	|''	|NOT NULL	|ZBX_SYNC
 FIELD		|timeout	|t_integer	|'30'	|NOT NULL	|ZBX_SYNC
 FIELD		|posts		|t_blob		|''	|NOT NULL	|ZBX_SYNC
 FIELD		|required	|t_varchar(255)	|''	|NOT NULL	|ZBX_SYNC
+FIELD		|status_codes	|t_varchar(255)	|''	|NOT NULL	|ZBX_SYNC
 INDEX		|httpstep_1	|httptestid
 
 TABLE|httpstepitem|httpstepitemid|ZBX_SYNC
