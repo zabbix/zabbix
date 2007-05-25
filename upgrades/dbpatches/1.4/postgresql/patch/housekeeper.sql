6219ef4d2bc3f02e6a6f5fc01a6dcc0a416f2e81
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-05-25 11:43:01 +0000

    Changed housekeeper value type in patches.
    git-svn-id: svn://svn.zabbix.com/trunk@4179 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.4/postgresql/patch/housekeeper.sql
+++ upgrades/dbpatches/1.4/postgresql/patch/housekeeper.sql
@@ -2,7 +2,7 @@ CREATE TABLE housekeeper_tmp (
 	housekeeperid	bigint DEFAULT '0'	NOT NULL,
 	tablename	varchar(64)		DEFAULT ''	NOT NULL,
 	field		varchar(64)		DEFAULT ''	NOT NULL,
-	value		integer		DEFAULT '0'	NOT NULL,
+	value		bigint		DEFAULT '0'	NOT NULL,
 	PRIMARY KEY (housekeeperid)
 );
 
