e88f467a46f2fa7b79fc81c4337d53eb466ca8f5
alex <alex@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2007-05-25 11:43:01 +0000

    Changed housekeeper value type in patches.
    git-svn-id: svn://svn.zabbix.com/trunk@4179 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/1.4/mysql/patch/housekeeper.sql
+++ upgrades/dbpatches/1.4/mysql/patch/housekeeper.sql
@@ -2,7 +2,7 @@ CREATE TABLE housekeeper_tmp (
 	housekeeperid		bigint unsigned		DEFAULT '0'	NOT NULL,
 	tablename		varchar(64)		DEFAULT ''	NOT NULL,
 	field		varchar(64)		DEFAULT ''	NOT NULL,
-	value		integer		DEFAULT '0'	NOT NULL,
+	value		bigint unsigned		DEFAULT '0'	NOT NULL,
 	PRIMARY KEY (housekeeperid)
 ) ENGINE=InnoDB;
 
