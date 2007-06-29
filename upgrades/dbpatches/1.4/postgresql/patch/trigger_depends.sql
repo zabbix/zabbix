32d7babb5c8c7ca2874f4de82551933c68449f08
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-06-29 11:34:28 +0000

    - fixed of PostgreSQL database upgrade schema (Alexei)
    git-svn-id: svn://svn.zabbix.com/branches/1.4.1@4390 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.4/postgresql/patch/trigger_depends.sql
+++ upgrades/dbpatches/1.4/postgresql/patch/trigger_depends.sql
@@ -4,8 +4,8 @@ CREATE TABLE trigger_depends_tmp (
 	triggerid_up		bigint DEFAULT '0'	NOT NULL,
 	PRIMARY KEY (triggerdepid)
 ) with OIDS;
-CREATE INDEX trigger_depends_1 on trigger_depends_tmp (triggerid_down,triggerid_up);
-CREATE INDEX trigger_depends_2 on trigger_depends_tmp (triggerid_up);
+CREATE INDEX trigger_depends_tmp_1 on trigger_depends_tmp (triggerid_down,triggerid_up);
+CREATE INDEX trigger_depends_tmp_2 on trigger_depends_tmp (triggerid_up);
 
 insert into trigger_depends_tmp select NULL,triggerid_down,triggerid_up from trigger_depends;
 drop table trigger_depends;
