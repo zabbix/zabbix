4c9f8fe18ce1efb1cb3369bd86e1bcfb006018c7
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-06-29 11:35:14 +0000

    - fixed of PostgreSQL database upgrade schema (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@4391 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.4/postgresql/patch/items_applications.sql
+++ upgrades/dbpatches/1.4/postgresql/patch/items_applications.sql
@@ -4,7 +4,7 @@ CREATE TABLE items_applications_tmp (
 	itemid		bigint DEFAULT '0'	NOT NULL,
 	PRIMARY KEY (itemappid)
 ) with OIDS;
-CREATE INDEX items_applications_1 on items_applications_tmp (applicationid,itemid);
+CREATE INDEX items_applications_tmp_1 on items_applications_tmp (applicationid,itemid);
 
 insert into items_applications_tmp select NULL,applicationid,itemid from items_applications;
 drop table items_applications;
