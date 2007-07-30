027dc5cffe0ee02491f3acd20e7ac7494f021341
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-07-30 10:49:37 +0000

    - fixed status dependency in IT Services, now trigger removed from service if it's has child nodes (Artem)
    git-svn-id: svn://svn.zabbix.com/branches/1.4.2@4478 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/services_form.php
+++ frontends/php/services_form.php
@@ -121,17 +121,11 @@ if(isset($_REQUEST['saction'])){
 		$service_times = get_request('service_times',array());
 		$childs = get_request('childs',array());
 
-		if(isset($_REQUEST["linktrigger"])){
-			$triggerid = $_REQUEST["triggerid"];
-			$status = get_trigger_priority($triggerid);
-		}
-		else {
-			$triggerid = null;
-			$status = 0;
-		}
+		$triggerid = (isset($_REQUEST["linktrigger"]))?($_REQUEST["triggerid"]):(null);
+
 		if(isset($service["serviceid"])){
 			$result = update_service($service["serviceid"],
-				$_REQUEST["name"],$triggerid,$status,$_REQUEST["algorithm"],
+				$_REQUEST["name"],$triggerid,$_REQUEST["algorithm"],
 				$_REQUEST["showsla"],$_REQUEST["goodsla"],$_REQUEST["sortorder"],
 				$service_times,$_REQUEST['parentid'],$childs);
 				
@@ -141,7 +135,7 @@ if(isset($_REQUEST['saction'])){
 			
 		} else {
 			$result = add_service(
-				$_REQUEST["name"],$triggerid,$status,$_REQUEST["algorithm"],
+				$_REQUEST["name"],$triggerid,$_REQUEST["algorithm"],
 				$_REQUEST["showsla"],$_REQUEST["goodsla"],$_REQUEST["sortorder"],
 				$service_times,$_REQUEST['parentid'],$childs);
 			show_messages($result, S_SERVICE_ADDED, S_CANNOT_ADD_SERVICE);
