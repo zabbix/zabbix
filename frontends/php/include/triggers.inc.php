4e0defb5ab41fdb3f417870540b5585c1daa1407
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-08-15 10:07:44 +0000

    - added missing { and } when displaying trigger expressions (Alexei)
    git-svn-id: svn://svn.zabbix.com/branches/1.4.2@4574 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/triggers.inc.php
+++ frontends/php/include/triggers.inc.php
@@ -920,7 +920,7 @@
 							'history.php?action='.( $function_data["value_type"] ==0 ? 'showvalues' : 'showgraph').
 							'&itemid='.$function_data['itemid']);
 					
-						$exp .= $link->ToString().'.'.bold($function_data["function"].'(').$function_data["parameter"].bold(')');
+						$exp .= '{'.$link->ToString().'.'.bold($function_data["function"].'(').$function_data["parameter"].bold(')').'}';
 					}
 				}
 				else
