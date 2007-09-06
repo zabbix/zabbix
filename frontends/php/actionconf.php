4b81a2abafea17b346c2b938b457d6691ff7829a
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-09-06 12:59:33 +0000

    - merged rev. 4729:4730 of branches/1.4.j (Artem) [[NTT-14] fixed "Configuration->Action Can't do anything with actions" issue]
    git-svn-id: svn://svn.zabbix.com/branches/1.4@4731 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/actionconf.php
+++ frontends/php/actionconf.php
@@ -230,13 +230,14 @@ include_once "include/page_header.php";
 	{
 		global $USER_DETAILS;
 
-		if(count($nodes = get_accessible_nodes_by_user($USER_DETAILS,PERM_READ_WRITE,PERM_MODE_LT,PERM_RES_IDS_ARRAY,get_current_nodeid())))
+		if(!count($nodes = get_accessible_nodes_by_user($USER_DETAILS,PERM_READ_WRITE,null,PERM_RES_IDS_ARRAY,get_current_nodeid())))
 			access_deny();
-
-		$result=DBselect('select distinct actionid from actions'.
+		
+		$query = 'select distinct actionid from actions'.
 				' where '.DBin_node('actionid',$nodes).
-				' and actionid in ('.implode(',',$_REQUEST['g_actionid']).') '
-				);
+				' and actionid in ('.implode(',',$_REQUEST['g_actionid']).') ';
+				
+		$result=DBselect($query);
 		
 		$actionids = array();
 		while($row=DBfetch($result))
@@ -255,13 +256,15 @@ include_once "include/page_header.php";
 	{
 		global $USER_DETAILS;
 
-		if(count($nodes = get_accessible_nodes_by_user($USER_DETAILS,PERM_READ_WRITE,PERM_MODE_LT,PERM_RES_IDS_ARRAY,get_current_nodeid())))
+		if(!count($nodes = get_accessible_nodes_by_user($USER_DETAILS,PERM_READ_WRITE,null,PERM_RES_IDS_ARRAY,get_current_nodeid())))
 			access_deny();
 
-		$result=DBselect('select distinct actionid from actions'.
+		$query = 'select distinct actionid from actions'.
 				' where '.DBin_node('actionid',$nodes).
-				' and actionid in ('.implode(',',$_REQUEST['g_actionid']).') '
-				);
+				' and actionid in ('.implode(',',$_REQUEST['g_actionid']).') ';
+
+		$result=DBselect($query);
+
 		$actionids = array();
 		while($row=DBfetch($result))
 		{
@@ -279,7 +282,7 @@ include_once "include/page_header.php";
 	{
 		global $USER_DETAILS;
 
-		if(count($nodes = get_accessible_nodes_by_user($USER_DETAILS,PERM_READ_WRITE,PERM_MODE_LT,PERM_RES_IDS_ARRAY,get_current_nodeid())))
+		if(!count($nodes = get_accessible_nodes_by_user($USER_DETAILS,PERM_READ_WRITE,null,PERM_RES_IDS_ARRAY,get_current_nodeid())))
 			access_deny();
 
 		$result=DBselect('select distinct actionid from actions'.
