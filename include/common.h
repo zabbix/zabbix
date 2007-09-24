79108eb70fcb2a0e89e45b76e6eda9585e44afab
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-09-24 06:54:53 +0000

    Fixed compilation warnings in   src/zabbix_server/discoverer/discoverer.c   src/libs/zbxcomms/comms.c   src/libs/zbxcommon/misc.c [svn merge svn://svn.zabbix.com/branches/1.4 -r4751:4753]
    git-svn-id: svn://svn.zabbix.com/trunk@4779 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -628,7 +628,9 @@ int	get_nodeid_by_id(zbx_uint64_t id);
 int	int_in_list(char *list, int value);
 int	uint64_in_list(char *list, zbx_uint64_t value);
 int	ip_in_list(char *list, char *ip);
-
+#ifdef HAVE_IPV6
+int	expand_ipv6(const char *ip, char *str, size_t str_len );
+#endif /* HAVE_IPV6 */
 /* Time related functions */
 double	time_diff(struct timeval *from, struct timeval *to);
 
