559fa40b0e8794372c4d6275b88f522082f9ead6
sasha <sasha@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2007-10-01 14:49:27 +0000

    - [ZBX-89] Unused fields:history_lastid, ..str_lastid & ..uint_lastid (Sasha)    [svn merge svn://svn.zabbix.com/branches/1.4 -r4816:4819]
    git-svn-id: svn://svn.zabbix.com/trunk@4820 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/nodes.inc.php
+++ frontends/php/include/nodes.inc.php
@@ -88,9 +88,9 @@
 		}
 
 		$result = DBexecute('insert into nodes (nodeid,name,timezone,ip,port,slave_history,slave_trends,'.
-				'event_lastid,history_lastid,nodetype,masterid) values ('.
+				'event_lastid,nodetype,masterid) values ('.
 				$new_nodeid.','.zbx_dbstr($name).','.$timezone.','.zbx_dbstr($ip).','.$port.','.$slave_history.','.$slave_trends.','.
-				'0,0,'.$nodetype.','.$masterid.')');
+				'0,'.$nodetype.','.$masterid.')');
 
 		if($result && $node_type == ZBX_NODE_MASTER)
 		{
