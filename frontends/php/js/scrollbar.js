f66a127799e435c95c35b01750e358caf960db0e
artem <artem@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2007-10-17 15:04:53 +0000

    - [DEV-1] added new navbar to httpdetails.php (Artem)
    git-svn-id: svn://svn.zabbix.com/trunk@4871 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/js/scrollbar.js
+++ frontends/php/js/scrollbar.js
@@ -549,7 +549,7 @@ getsTimeInUnix: function(){
 
 getsTime: function(){
 	var date = this.formatdate(this.barX);
-return date[2]+date[1]+date[0]+date[3]+date[4];
+return ''+date[2]+date[1]+date[0]+date[3]+date[4];
 },
 
 
