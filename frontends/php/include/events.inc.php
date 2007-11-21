7b205e2c61a61d1ab193d37c8a95bc66ebd1a261
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-11-21 14:50:35 +0000

    - [NTT-57] fixes for hosts with no triggers available in "Events" screen (Artem)
    git-svn-id: svn://svn.zabbix.com/branches/1.4.j@5085 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/events.inc.php
+++ frontends/php/include/events.inc.php
@@ -56,7 +56,9 @@
 		}
 
 //---
+		$triggers = array();
 		$trigger_list = '';
+		
 		$sql = 'SELECT DISTINCT t.triggerid,t.priority,t.description,t.expression,h.host,t.type '.
 			' FROM triggers t, functions f, items i, hosts h '.$sql_from.
 			' WHERE '.DBin_node('t.triggerid').
@@ -69,18 +71,20 @@
 			$trigger_list.=$rowz['triggerid'].',';
 		}
 
-		$trigger_list = '('.trim($trigger_list,',').')';
-		$sql_cond=($show_unknown == 0)?(' AND e.value<>'.TRIGGER_VALUE_UNKNOWN.' '):('');
-		
-		$sql = 'SELECT e.eventid, e.objectid as triggerid,e.clock,e.value '.
-				' FROM events e '.
-				' WHERE '.zbx_sql_mod('e.object',1000).'='.EVENT_OBJECT_TRIGGER.
-				  ' AND e.objectid IN '.$trigger_list.
-				  $sql_cond.
-				' ORDER BY e.eventid DESC';
-
-		$result = DBselect($sql,10*($start+$num));
-     
+		if(!empty($triggers)){
+			$trigger_list = '('.trim($trigger_list,',').')';
+			$sql_cond=($show_unknown == 0)?(' AND e.value<>'.TRIGGER_VALUE_UNKNOWN.' '):('');
+	
+			$sql = 'SELECT e.eventid, e.objectid as triggerid,e.clock,e.value '.
+					' FROM events e '.
+					' WHERE '.zbx_sql_mod('e.object',1000).'='.EVENT_OBJECT_TRIGGER.
+					  ' AND e.objectid IN '.$trigger_list.
+					  $sql_cond.
+					' ORDER BY e.eventid DESC';
+	
+			$result = DBselect($sql,10*($start+$num));
+		}
+		     
 		$table = new CTableInfo(S_NO_EVENTS_FOUND); 
 		$table->SetHeader(array(
 				S_TIME,
@@ -96,7 +100,7 @@
 		$col=0;
 		$skip = $start;
 
-		while(($row=DBfetch($result)) && ($col<$num)){
+		while(!empty($triggers) && ($col<$num) && ($row=DBfetch($result))){
 			
 			if($skip > 0){
 				if(($show_unknown == 0) && ($row['value'] == TRIGGER_VALUE_UNKNOWN)) continue;
