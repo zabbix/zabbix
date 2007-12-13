46c9a4a3462cdb2c65f2ace5e274fc7ccd2a048c
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-12-13 08:58:38 +0000

    - [ZBX-219] Zabbix server crashes after enabling discovery    [svn merge -r5168:5172 svn://svn.zabbix.com/branches/1.4]
    git-svn-id: svn://svn.zabbix.com/trunk@5173 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_server/discoverer/discoverer.c
+++ src/zabbix_server/discoverer/discoverer.c
@@ -723,7 +723,7 @@ static void process_rule(DB_DRULE *rule)
 
 	assert(rule);
 
-	zabbix_log(LOG_LEVEL_DEBUG, "In process_rule(name:%s,range:%s)",
+	zabbix_log(LOG_LEVEL_DEBUG, "In process_rule() [name:%s] [range:%s]",
 		rule->name,
 		rule->iprange);
 
@@ -805,7 +805,7 @@ static void process_rule(DB_DRULE *rule)
 
 		if( first < 0 || last < 0 )
 		{
-			zabbix_log(LOG_LEVEL_WARNING, "Wrong format of IP range [%s]",
+			zabbix_log(LOG_LEVEL_WARNING, "Discovery: Wrong format of IP range [%s]",
 				rule->iprange);
 			continue;
 		}
@@ -818,7 +818,7 @@ static void process_rule(DB_DRULE *rule)
 				case 1 : zbx_snprintf(ip, sizeof(ip), "%s%x", prefix, i); break;
 			}
 
-			zabbix_log(LOG_LEVEL_DEBUG, "Discovery: process_rule() [first %d] [last %d] [IP %s]", prefix, first, last, ip);
+			zabbix_log(LOG_LEVEL_DEBUG, "Discovery: process_rule() [IP:%s]", ip);
 
 			result = DBselect("select dcheckid,druleid,type,key_,snmp_community,ports from dchecks where druleid=" ZBX_FS_UI64,
 				rule->druleid);
