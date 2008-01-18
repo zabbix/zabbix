83c43012c9fd777e6a8d095c58f534d7de2963ad
sasha <sasha@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2008-01-18 10:52:08 +0000

    - [DEV-99] New application: zabbix_proxy
    git-svn-id: svn://svn.zabbix.com/trunk@5265 97f52cf1-0a1b-0410-bd0e-c28be96e8082

---
+++ src/zabbix_proxy/dbsyncer/dbsyncer.c
@@ -0,0 +1,75 @@
+/* 
+** ZABBIX
+** Copyright (C) 2000-2005 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+
+#include "common.h"
+
+#include "db.h"
+#include "log.h"
+#include "zlog.h"
+
+#include "dbcache.h"
+#include "dbsyncer.h"
+
+/******************************************************************************
+ *                                                                            *
+ * Function: main_dbsyncer_loop                                               *
+ *                                                                            *
+ * Purpose: periodically syncronises data in memory cache with database       *
+ *                                                                            *
+ * Parameters:                                                                *
+ *                                                                            *
+ * Return value:                                                              * 
+ *                                                                            *
+ * Author: Alexei Vladishev                                                   *
+ *                                                                            *
+ * Comments: never returns                                                    *
+ *                                                                            *
+ ******************************************************************************/
+int main_dbsyncer_loop()
+{
+	int	now;
+	struct timeval from;
+	struct timeval to;
+
+	zbx_setproctitle("connecting to the database");
+
+	DBconnect(ZBX_DB_CONNECT_NORMAL);
+
+	for(;;)
+	{
+		now  = time(NULL);
+
+		zabbix_log( LOG_LEVEL_WARNING, "Syncing ...");
+
+
+		gettimeofday(&from, NULL);
+
+		DCsync();
+
+		gettimeofday(&to, NULL);
+		zabbix_log( LOG_LEVEL_WARNING, "Spent " ZBX_FS_DBL " sec",
+			time_diff(&from,&to));
+
+		zbx_setproctitle("sender [sleeping for %d seconds]",
+			CONFIG_DBSYNCER_FREQUENCY);
+
+		sleep(CONFIG_DBSYNCER_FREQUENCY);
+	}
+	DBclose();
+}
