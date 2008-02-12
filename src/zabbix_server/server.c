9e3932a4c71e0975b706f5d39eccb93055a9757e
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2008-02-12 11:28:51 +0000

    - [DEV-109] Server changes - Pinger
    git-svn-id: svn://svn.zabbix.com/trunk@5350 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_server/server.c
+++ src/zabbix_server/server.c
@@ -1121,7 +1121,10 @@ int MAIN_ZABBIX_ENTRY(void)
 	}
 	else if(server_num <= CONFIG_POLLER_FORKS + CONFIG_TRAPPERD_FORKS + CONFIG_PINGER_FORKS)
 	{
-		main_pinger_loop(server_num, server_num - (CONFIG_POLLER_FORKS + CONFIG_TRAPPERD_FORKS));
+		zabbix_log(LOG_LEVEL_WARNING, "server #%d started [ICMP pinger]",
+				server_num);
+
+		main_pinger_loop(ZBX_PROCESS_SERVER, server_num - (CONFIG_POLLER_FORKS + CONFIG_TRAPPERD_FORKS));
 	}
 	else if(server_num <= CONFIG_POLLER_FORKS + CONFIG_TRAPPERD_FORKS + CONFIG_PINGER_FORKS + CONFIG_ALERTER_FORKS)
 	{
