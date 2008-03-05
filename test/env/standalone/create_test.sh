83919eda72fbd263d03751c3b5dfd59bbc7b85fe
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2008-03-05 21:31:58 +0000

    - [DEV-128] improvement of test data and scripts (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@5446 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- test/env/standalone/create_test.sh
+++ test/env/standalone/create_test.sh
@@ -1,51 +1,78 @@
 #echo "create database trunk"|mysql -uroot
 #cat data.sql|mysql -uroot trunk
 
-HOME=/home/zabbix/ttt
+DEST=/home/zabbix/test
+TMP_DIR=\\/home\\/zabbix\\/test\\/tmp
 PROXY_PORT=11000
 AGENT_PORT=12000
 SERVER_PORT=13000
-TMP_DIR=\\/tmp
 PROXY_NUM=2
+DEBUG=4
 
 echo Killing all processes
-killall zabbix_agentd
-killall zabbix_proxy
-killall zabbix_server
+killall zabbix_agentd 2>/dev/null
+killall zabbix_proxy 2>/dev/null
+killall zabbix_server 2>/dev/null
+sleep 3
+killall -9 zabbix_agentd 2>/dev/null
+killall -9 zabbix_proxy 2>/dev/null
+killall -9 zabbix_server 2>/dev/null
 
 echo Removing all temp files
-rm -f $TMP_DIR/*.log
-rm -f $TMP_DIR/*.log.old
-rm -f $TMP_DIR/*.pid
+mkdir $DEST 2>/dev/null
+rm -f $DEST/*.log
+mkdir $DEST/conf 2>/dev/null
+mkdir $DEST/sbin 2>/dev/null
+mkdir $DEST/tmp 2>/dev/null
+
+echo Making binaries
+cd ../../..
+#./go
+cd -
+cp ../../../sbin/* $DEST/sbin
 
 echo Recreating database
-echo "drop database trunk"|mysql -uroot
-echo "create database trunk"|mysql -uroot
-cat data.sql|mysql -uroot trunk
+#echo "drop database trunk"|mysql -uroot
+#echo "create database trunk"|mysql -uroot
+#cat data.sql|mysql -uroot trunk
 
 for ((i=1; i <= $PROXY_NUM ; i++)); do
 	echo Processing proxy$i
 	echo "drop database proxy$i"|mysql -uroot
 	echo "create database proxy$i"|mysql -uroot
-	$HOME/create/schema/gen.pl mysql|mysql -uroot proxy$i
+	../../../create/schema/gen.pl mysql|mysql -uroot proxy$i
 
 	PORT=`echo "$PROXY_PORT+$i"|bc`
-	cat conf/template_proxy.conf|sed "s/{SERVER_PORT}/$SERVER_PORT/g"|sed "s/{NUM}/$i/g"|sed "s/{PORT}/$PORT/g"|sed "s/{TMP_DIR}/$TMP_DIR/g" >conf/proxy$i.conf
+	cat conf/template_proxy.conf|\
+		sed "s/{SERVER_PORT}/$SERVER_PORT/g"|\
+		sed "s/{NUM}/$i/g"|sed "s/{PORT}/$PORT/g"|\
+		sed "s/{TMP_DIR}/$TMP_DIR/g"|\
+		sed "s/{DebugLevel}/$DEBUG/g"\
+		>$DEST/conf/proxy$i.conf
 
-	$HOME/sbin/zabbix_proxy -c $HOME/test/env/standalone/conf/proxy$i.conf
+	$DEST/sbin/zabbix_proxy -c $DEST/conf/proxy$i.conf
 done
 
 for ((i=1; i <= 20 ; i++)); do
 	echo Processing agent$i
 	PORT=`echo "$AGENT_PORT+$i"|bc`
-	cat conf/template_agent.conf|sed "s/{SERVER_PORT}/$SERVER_PORT/g"|sed "s/{NUM}/$i/g"|sed "s/{PORT}/$PORT/g"|sed "s/{TMP_DIR}/$TMP_DIR/g" >conf/agent$i.conf
+	cat conf/template_agent.conf|\
+		sed "s/{SERVER_PORT}/$SERVER_PORT/g"|\
+		sed "s/{NUM}/$i/g"|sed "s/{PORT}/$PORT/g"|\
+		sed "s/{TMP_DIR}/$TMP_DIR/g"|\
+		sed "s/{DebugLevel}/$DEBUG/g"\
+		>$DEST/conf/agent$i.conf
 
 	echo "update hosts set port=$PORT where host='u$i'"|mysql -uroot trunk
 
-	$HOME/sbin/zabbix_agentd -c $HOME/test/env/standalone/conf/agent$i.conf
+	$DEST/sbin/zabbix_agentd -c $DEST/conf/agent$i.conf
 done
 
 echo Processing server
-cat conf/template_server.conf|sed "s/{PORT}/$SERVER_PORT/g"|sed "s/{TMP_DIR}/$TMP_DIR/g" >conf/server.conf
+cat conf/template_server.conf|\
+	sed "s/{PORT}/$SERVER_PORT/g"|\
+	sed "s/{TMP_DIR}/$TMP_DIR/g"|\
+	sed "s/{DebugLevel}/$DEBUG/g"\
+	>$DEST/conf/server.conf
 
-$HOME/sbin/zabbix_server -c $HOME/test/env/standalone/conf/server.conf
+$DEST/sbin/zabbix_server -c $DEST/conf/server.conf
