062ea49cafa55403148e9062b7a5426055cb9df1
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2008-04-29 13:17:02 +0000

    - [DEV-137] update of MySQL patches, disabled database cache (Alexei)
    git-svn-id: svn://svn.zabbix.com/trunk@5668 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.6/mysql/patch/profiles.sql
+++ upgrades/dbpatches/1.6/mysql/patch/profiles.sql
@@ -1,3 +1,7 @@
 delete from profiles;
 
 alter table profiles drop index profiles_1;
+
+alter table profiles add value2          varchar(255)            DEFAULT ''      NOT NULL;
+alter table profiles add resource        varchar(255)            DEFAULT ''      NOT NULL;
+CREATE INDEX profiles_1 on profiles (idx);
--- upgrades/dbpatches/1.6/mysql/patch/sysmaps_link_triggers.sql
+++ upgrades/dbpatches/1.6/mysql/patch/sysmaps_link_triggers.sql
@@ -1,10 +1,11 @@
 -- TODO Populate data from sysmaps_links
 
 CREATE TABLE sysmaps_link_triggers (
+        linktriggerid           bigint unsigned         DEFAULT '0'     NOT NULL,
         linkid          bigint unsigned         DEFAULT '0'     NOT NULL,
-        triggerid               bigint unsigned                 ,
+        triggerid               bigint unsigned         DEFAULT '0'     NOT NULL,
         drawtype                integer         DEFAULT '0'     NOT NULL,
         color           varchar(6)              DEFAULT '000000'        NOT NULL,
-        PRIMARY KEY (linkid,triggerid)
+        PRIMARY KEY (linktriggerid)
 ) type=InnoDB;
-CREATE INDEX sysmaps_link_triggers_1 on sysmaps_link_triggers (linkid);
+CREATE UNIQUE INDEX sysmaps_link_triggers_1 on sysmaps_link_triggers (linkid,triggerid);
