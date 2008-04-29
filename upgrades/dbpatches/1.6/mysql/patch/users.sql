c26d47e2f060f3d1f292723af0973fb80c6a3c9c
Alexei Vladishev <alexei.vladishev@zabbix.com>
2008-04-29 13:17:02 +0000

    - [DEV-137] update of MySQL patches, disabled database cache (Alexei)

--- upgrades/dbpatches/1.6/mysql/patch/users.sql
+++ upgrades/dbpatches/1.6/mysql/patch/users.sql
@@ -1,2 +1,5 @@
 alter table users add autologin integer DEFAULT '0' NOT NULL;
 alter table users add theme varchar(128) DEFAULT 'default.css' NOT NULL;
+alter table users add attempt_failed          integer         DEFAULT 0       NOT NULL;
+alter table users add attempt_ip              varchar(39)             DEFAULT ''      NOT NULL;
+alter table users add attempt_clock           integer         DEFAULT 0       NOT NULL;
