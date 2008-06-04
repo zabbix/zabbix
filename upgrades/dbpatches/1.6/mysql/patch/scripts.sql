a2be008cc849f1939a01c6b268ba4933ee02cee1
Alexei Vladishev <alexei.vladishev@zabbix.com>
2008-06-04 18:57:41 +0000

    - [DEV-137] updated version to 1.5.3 (Alexei)

--- upgrades/dbpatches/1.6/mysql/patch/scripts.sql
+++ upgrades/dbpatches/1.6/mysql/patch/scripts.sql
@@ -3,5 +3,8 @@ CREATE TABLE scripts (
         name            varchar(255)            DEFAULT ''      NOT NULL,
         command         varchar(255)            DEFAULT ''      NOT NULL,
         host_access             integer         DEFAULT '0'     NOT NULL,
+	usrgrpid                bigint unsigned         DEFAULT '0'     NOT NULL,
+	groupid         bigint unsigned         DEFAULT '0'     NOT NULL,
+
         PRIMARY KEY (scriptid)
 ) type=InnoDB;
