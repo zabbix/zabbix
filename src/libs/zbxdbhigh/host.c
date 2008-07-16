609c596f7215f665b66709fa64894804f833144f
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2008-07-16 08:38:01 +0000

    - [DEV-137] misc bugs and improvements    [bug fix in function DBupdate_graph()]
    git-svn-id: svn://svn.zabbix.com/trunk@5828 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxdbhigh/host.c
+++ src/libs/zbxdbhigh/host.c
@@ -1214,7 +1214,7 @@ static int	DBupdate_graph(
 		"yaxistype=%i,yaxismin=%i,yaxismax=%i,templateid=" ZBX_FS_UI64 ","
 		"show_work_period=%i,show_triggers=%i,graphtype=%i,"
 		"show_legend=%i,show_3d=%i where graphid=" ZBX_FS_UI64,
-		name,width,height,yaxistype,yaxismin,yaxismax,templateid,show_work_period,show_triggers,graphtype,
+		name_esc,width,height,yaxistype,yaxismin,yaxismax,templateid,show_work_period,show_triggers,graphtype,
 		show_legend,show_3d,graphid);
 
 	zbx_free(name_esc);
