cb1011cad4285f5785bbf853a80f981e48dd526c
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2008-08-04 12:42:29 +0000

    - [ZBX-412] fixed malfunction of triggers based on log[] and eventlog[]
    git-svn-id: svn://svn.zabbix.com/branches/1.4@5868 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxdbhigh/db.c
+++ src/libs/zbxdbhigh/db.c
@@ -309,7 +309,7 @@ void	get_latest_event_status(zbx_uint64_t triggerid, int *prev_status, int *late
 	zabbix_log(LOG_LEVEL_DEBUG,"In get_latest_event_status(triggerid:" ZBX_FS_UI64,
 		triggerid);
 
-	zbx_snprintf(sql,sizeof(sql),"select eventid,value,clock from events where source=%d and object=%d and objectid=" ZBX_FS_UI64 " order by clock desc",
+	zbx_snprintf(sql,sizeof(sql),"select eventid,value,clock from events where source=%d and object=%d and objectid=" ZBX_FS_UI64 " order by eventid desc",
 		EVENT_SOURCE_TRIGGERS,
 		EVENT_OBJECT_TRIGGER,
 		triggerid);
