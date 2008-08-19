49996b7d098b0fd82ac749e5a1ded67381a0d501
Alexei Vladishev <alexei.vladishev@zabbix.com>
2008-08-19 09:47:07 +0000

    - updated patches for MySQL (Alexei)

--- upgrades/dbpatches/1.6/mysql/patch/hosts.sql
+++ upgrades/dbpatches/1.6/mysql/patch/hosts.sql
@@ -1,5 +1,5 @@
-alter table hosts add proxy_hostid bigint unsigned DEFAULT '0' NOT NULL;
-alter table hosts add lastaccess integer unsigned DEFAULT '0' NOT NULL;
+alter table hosts add proxy_hostid bigint unsigned DEFAULT '0' NOT NULL after hostid;
+alter table hosts add lastaccess integer DEFAULT '0' NOT NULL;
 alter table hosts add inbytes bigint unsigned DEFAULT '0' NOT NULL;
 alter table hosts add outbytes bigint unsigned DEFAULT '0' NOT NULL;
 alter table hosts modify ip varchar(39) DEFAULT '127.0.0.1' NOT NULL;
--- upgrades/dbpatches/1.6/mysql/patch/proxy_history.sql
+++ upgrades/dbpatches/1.6/mysql/patch/proxy_history.sql
@@ -1,12 +1,11 @@
 CREATE TABLE proxy_history (
         id              bigint unsigned                 NOT NULL        auto_increment unique,
-        host            varchar(64)             DEFAULT ''      NOT NULL,
-        key_            varchar(255)            DEFAULT ''      NOT NULL,
+        itemid          bigint unsigned         DEFAULT '0'     NOT NULL,
         clock           integer         DEFAULT '0'     NOT NULL,
         timestamp               integer         DEFAULT '0'     NOT NULL,
         source          varchar(64)             DEFAULT ''      NOT NULL,
         severity                integer         DEFAULT '0'     NOT NULL,
-        value           text            DEFAULT ''      NOT NULL,
+        value           text                    NOT NULL,
         PRIMARY KEY (id)
 ) type=InnoDB;
 CREATE INDEX proxy_history_1 on proxy_history (clock);
