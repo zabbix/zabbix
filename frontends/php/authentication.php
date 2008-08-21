76d960242c38748a26079d507d5414c7369c0697
artem <artem@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2008-08-21 14:27:01 +0000

    - [DEV-144] minor fix to LDAP form (Artem)
    git-svn-id: svn://svn.zabbix.com/trunk@5934 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/authentication.php
+++ frontends/php/authentication.php
@@ -47,7 +47,7 @@ include_once('include/page_header.php');
 		
 		'authentication_type'=>	array(T_ZBX_INT, O_OPT,	NULL,	IN('0,1,2'),			NULL),
 		
-		'user_password'=>		array(T_ZBX_STR, O_OPT,	NULL,	NOT_EMPTY,		'isset({config})&&({config}==1)&&(isset({authentication_type})||isset({test}))'),
+		'user_password'=>		array(T_ZBX_STR, O_OPT,	NULL,	NOT_EMPTY,		'isset({config})&&({config}==1)&&isset({form_refresh_ldap})&&(isset({authentication_type}) || isset({test}))'),
 
 /* actions */
 		'save'=>			array(T_ZBX_STR, O_OPT, P_SYS|P_ACT,	NULL,	NULL),
