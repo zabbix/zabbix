323406ee662a64657cdd384a7645c7aee5a2ef68
artem <artem@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2008-09-12 15:07:15 +0000

    - [DEV-214] changes in inventory (Artem)
    git-svn-id: svn://svn.zabbix.com/trunk@5992 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/hostprofiles.php
+++ frontends/php/hostprofiles.php
@@ -33,24 +33,25 @@ include_once "include/page_header.php";
 <?php
 //		VAR			TYPE	OPTIONAL FLAGS	VALIDATION	EXCEPTION
 	$fields=array(
-		"groupid"=>		array(T_ZBX_INT, O_OPT,	P_SYS,	DB_ID,	NULL),
-		"hostid"=>		array(T_ZBX_INT, O_OPT,	P_SYS,	DB_ID,	NULL)
+		'groupid'=>		array(T_ZBX_INT, O_OPT,	P_SYS,	DB_ID,	NULL),
+		'hostid'=>		array(T_ZBX_INT, O_OPT,	P_SYS,	DB_ID,	NULL),
+		'prof_type'=>	array(T_ZBX_INT, O_OPT,	P_SYS,	null,	NULL),
 	);
 
 	check_fields($fields);
 	validate_sort_and_sortorder('h.host',ZBX_SORT_UP);
-	
 	validate_group(PERM_READ_ONLY, array("allow_all_hosts","always_select_first_host","monitored_hosts","with_items"));
+	
+	$prof_type = get_request('prof_type',0);
 ?>
 <?php
+	$available_hosts = get_accessible_hosts_by_user($USER_DETAILS,PERM_READ_LIST,PERM_RES_IDS_ARRAY);
+	
 	$r_form = new CForm();
 	$r_form->SetMethod('get');
 
 	$cmbGroup = new CComboBox("groupid",$_REQUEST["groupid"],"submit()");
-
 	$cmbGroup->AddItem(0,S_ALL_SMALL);
-	
-	$available_hosts = get_accessible_hosts_by_user($USER_DETAILS,PERM_READ_LIST,PERM_RES_IDS_ARRAY);
 
 	$result=DBselect('SELECT DISTINCT g.groupid,g.name '.
 		' FROM groups g, hosts_groups hg, hosts h, items i '.
@@ -66,7 +67,14 @@ include_once "include/page_header.php";
 				get_node_name_by_elid($row['groupid']).$row['name']
 				);
 	}
+	
 	$r_form->AddItem(array(S_GROUP.SPACE,$cmbGroup));
+	
+	$cmbProf = new CComboBox('prof_type',$prof_type,'javascript: submit();');
+	$cmbProf->Additem(0, S_NORMAL);
+	$cmbProf->Additem(1, S_EXTENDED);
+	
+	$r_form->AddItem(array(SPACE.S_HOST_PROFILES.SPACE,$cmbProf));
 	
 	show_table_header(S_HOST_PROFILES_BIG, $r_form);
 ?>
@@ -74,91 +82,100 @@ include_once "include/page_header.php";
 <?php
 	if(isset($_REQUEST["hostid"])){
 		echo SBR;
-//BEGIN: HOSTS PROFILE ALTERNATE Section		
-		// insert_host_profile_form();
-		insert_host_profile_ext_form();
-//END: HOSTS PROFILE ALTERNATE Section
+
+		if($prof_type){
+			insert_host_profile_ext_form();
+		}
+		else{
+			insert_host_profile_form();
+		}
 	}
 	else{
 		$table = new CTableInfo();
-		$table->setHeader(array(
-			is_show_subnodes() ? make_sorting_link(S_NODE,'h.hostid') : null,
-//BEGIN: HOSTS PROFILE ALTERNATE Section		
-//DISABLE legacy INVENTORY DISPLAY and SORTING FIELDS
-			//make_sorting_link(S_HOST,'h.host'),
-			//make_sorting_link(S_NAME,'p.name'),
-			//make_sorting_link(S_OS,'p.os'),
-			//make_sorting_link(S_SERIALNO,'p.serialno'),
-			//make_sorting_link(S_TAG,'p.tag'),
-			//make_sorting_link(S_MACADDRESS,'p.macaddress'))
-//ADD new INVENTORY DISPLAY and SORTING FIELDS
-                        make_sorting_link(S_HOST,'h.host'),
-                       ($_REQUEST["groupid"] > 0)?null:make_sorting_link(S_GROUP,'g.name'),
-                        make_sorting_link(S_DEVICE_OS_SHORT,'hpe.device_os_short'),
-                        make_sorting_link(S_DEVICE_HW_ARCH,'hpe.device_hw_arch'),
-                        make_sorting_link(S_DEVICE_TYPE,'hpe.device_type'),
-			make_sorting_link(S_DEVICE_STATUS,'hpe.device_status'))
-//END: HOSTS PROFILE ALTERNATE Section
-		);
+		if($prof_type){
+			$table->setHeader(array(
+				is_show_subnodes() ? make_sorting_link(S_NODE,'h.hostid') : null,
+				make_sorting_link(S_HOST,'h.host'),
+			   ($_REQUEST["groupid"] > 0)?null:make_sorting_link(S_GROUP,'g.name'),
+				make_sorting_link(S_DEVICE_OS_SHORT,'hpe.device_os_short'),
+				make_sorting_link(S_DEVICE_HW_ARCH,'hpe.device_hw_arch'),
+				make_sorting_link(S_DEVICE_TYPE,'hpe.device_type'),
+				make_sorting_link(S_DEVICE_STATUS,'hpe.device_status'))
+			);
+			
+			if($_REQUEST["groupid"] > 0){
+				$sql='SELECT DISTINCT h.hostid,h.host,hpe.device_os_short,hpe.device_hw_arch,hpe.device_type,hpe.device_status'.
+					' FROM hosts h,hosts_profiles_ext hpe,hosts_groups hg,groups g '.
+					' WHERE h.hostid=hpe.hostid '.
+						' AND h.hostid=hg.hostid '.
+						' AND hg.groupid='.$_REQUEST['groupid'].
+						' AND '.DBcondition('h.hostid',$available_hosts).
+					order_by('h.host,h.hostid,g.name,hpe.device_os_short,hpe.device_hw_arch,hpe.device_type,hpe.device_status');
+			}
+			else{
+				$sql='SELECT h.hostid,h.host,g.name,hpe.device_os_short,hpe.device_hw_arch,hpe.device_type,hpe.device_status'.
+					' FROM hosts h,hosts_profiles_ext hpe,hosts_groups hg,groups g '.
+					' WHERE h.hostid=hpe.hostid'.
+						' AND h.hostid=hg.hostid '.
+						' AND hg.groupid=g.groupid'.
+						' AND '.DBcondition('h.hostid',$available_hosts).
+					order_by('h.host,h.hostid,g.name,hpe.device_os_short,hpe.device_hw_arch,hpe.device_type,hpe.device_status');
+			}
+			$result=DBselect($sql);
+			while($row=DBfetch($result)){
+				$table->AddRow(array(
+					get_node_name_by_elid($row['hostid']),
+					new CLink($row["host"],"?hostid=".$row["hostid"].url_param("groupid").'&prof_type='.$prof_type,"action"),
+					($_REQUEST["groupid"] > 0)?null:$row["name"],
+					$row["device_os_short"],
+					$row["device_hw_arch"],
+					$row["device_type"],
+					$row["device_status"]
+				));
+			}
 
-		if($_REQUEST["groupid"] > 0){
-//BEGIN: HOSTS PROFILE ALTERNATE Section		
-//DISABLE legacy SQL QUERY
-			//$sql='SELECT h.hostid,h.host,p.name,p.os,p.serialno,p.tag,p.macaddress'.
-			//	' FROM hosts h,hosts_profiles p,hosts_groups hg '.
-			//	' WHERE h.hostid=p.hostid'.
-			//		' and h.hostid=hg.hostid '.
-			//		' and hg.groupid='.$_REQUEST['groupid'].
-			//		' and '.DBcondition('h.hostid',$available_hosts).
-			//	order_by('h.host,h.hostid,p.name,p.os,p.serialno,p.tag,p.macaddress');
-//ADD new SQL QUERY
-                        $sql='SELECT DISTINCT h.hostid,h.host,hpe.device_os_short,hpe.device_hw_arch,hpe.device_type,hpe.device_status'.
-                                ' FROM hosts h,hosts_profiles_ext hpe,hosts_groups hg,groups g '.
-                                ' WHERE h.hostid=hpe.hostid '.
-                                        ' AND h.hostid=hg.hostid '.
-                                        ' AND hg.groupid='.$_REQUEST['groupid'].
-                                        ' AND '.DBcondition('h.hostid',$available_hosts).
-                                order_by('h.host,h.hostid,g.name,hpe.device_os_short,hpe.device_hw_arch,hpe.device_type,hpe.device_status');
-//END: HOSTS PROFILE ALTERNATE Section
 		}
 		else{
-//BEGIN: HOSTS PROFILE ALTERNATE Section		
-//DISABLE legacy SQL QUERY
-			//$sql='SELECT h.hostid,h.host,p.name,p.os,p.serialno,p.tag,p.macaddress'.
-			//	' FROM hosts h,hosts_profiles p '.
-			//	' WHERE h.hostid=p.hostid'.
-			//		' AND '.DBcondition('h.hostid',$available_hosts).
-			//	order_by('h.host,h.hostid,p.name,p.os,p.serialno,p.tag,p.macaddress');
-//ADD new SQL QUERY
-                        $sql='SELECT h.hostid,h.host,g.name,hpe.device_os_short,hpe.device_hw_arch,hpe.device_type,hpe.device_status'.
-                                ' FROM hosts h,hosts_profiles_ext hpe,hosts_groups hg,groups g '.
-                                ' WHERE h.hostid=hpe.hostid'.
-                                        ' AND h.hostid=hg.hostid '.
-                                        ' AND hg.groupid=g.groupid'.
-                                        ' AND '.DBcondition('h.hostid',$available_hosts).
-                                order_by('h.host,h.hostid,g.name,hpe.device_os_short,hpe.device_hw_arch,hpe.device_type,hpe.device_status');
-//END: HOSTS PROFILE ALTERNATE Section
+			$table->setHeader(array(
+				is_show_subnodes() ? make_sorting_link(S_NODE,'h.hostid') : null,
+				make_sorting_link(S_HOST,'h.host'),
+				make_sorting_link(S_NAME,'p.name'),
+				make_sorting_link(S_OS,'p.os'),
+				make_sorting_link(S_SERIALNO,'p.serialno'),
+				make_sorting_link(S_TAG,'p.tag'),
+				make_sorting_link(S_MACADDRESS,'p.macaddress'))
+			);
+			if($_REQUEST["groupid"] > 0){
+				$sql='SELECT h.hostid,h.host,p.name,p.os,p.serialno,p.tag,p.macaddress'.
+					' FROM hosts h,hosts_profiles p,hosts_groups hg '.
+					' WHERE h.hostid=p.hostid'.
+						' and h.hostid=hg.hostid '.
+						' and hg.groupid='.$_REQUEST['groupid'].
+						' and '.DBcondition('h.hostid',$available_hosts).
+					order_by('h.host,h.hostid,p.name,p.os,p.serialno,p.tag,p.macaddress');
+			}
+			else{
+				$sql='SELECT h.hostid,h.host,p.name,p.os,p.serialno,p.tag,p.macaddress'.
+					' FROM hosts h,hosts_profiles p '.
+					' WHERE h.hostid=p.hostid'.
+						' AND '.DBcondition('h.hostid',$available_hosts).
+					order_by('h.host,h.hostid,p.name,p.os,p.serialno,p.tag,p.macaddress');
+			}
+			$result=DBselect($sql);
+			while($row=DBfetch($result)){
+				$table->AddRow(array(
+					get_node_name_by_elid($row['hostid']),
+					new CLink($row["host"],'?hostid='.$row['hostid'].url_param('groupid').'&prof_type='.$prof_type,"action"),
+					$row["name"],
+					$row["os"],
+					$row["serialno"],
+					$row["tag"],
+					$row["macaddress"]
+				));
+			}
+
 		}
 
-		$result=DBselect($sql);
-		while($row=DBfetch($result)){
-			$table->AddRow(array(
-				get_node_name_by_elid($row['hostid']),
-				new CLink($row["host"],"?hostid=".$row["hostid"].url_param("groupid"),"action"),
-//BEGIN: HOSTS PROFILE ALTERNATE Section		
-				//$row["name"],
-				//$row["os"],
-				//$row["serialno"],
-				//$row["tag"],
-				//$row["macaddress"]
-				($_REQUEST["groupid"] > 0)?null:$row["name"],
-				$row["device_os_short"],
-				$row["device_hw_arch"],
-				$row["device_type"],
-				$row["device_status"]
-//END: HOSTS PROFILE ALTERNATE Section
-				));
-		}
 		$table->show();
 	}
 ?>
