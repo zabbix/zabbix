8af8d31843920a2cea4429a5dd804edbd5b17dbf
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2008-09-18 09:44:20 +0000

    - [DEV-137] Misc bugs and improvements    [postgresql db patches]
    git-svn-id: svn://svn.zabbix.com/trunk@6038 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ upgrades/dbpatches/1.6/mysql/patch/graphs_items.sql
@@ -0,0 +1,1 @@
+alter table graphs_items change color color varchar(32) DEFAULT '009600' NOT NULL;
--- upgrades/dbpatches/1.6/postgresql/patch/alerts.sql
+++ upgrades/dbpatches/1.6/postgresql/patch/alerts.sql
@@ -1,14 +1,27 @@
-drop index alerts_1;
-drop index alerts_2;
-drop index alerts_3;
-drop index alerts_4;
-drop index alerts_5;
-drop index alerts_6;
+CREATE TABLE alerts_tmp (
+        alertid         bigint          DEFAULT '0'     NOT NULL,
+        actionid                bigint          DEFAULT '0'     NOT NULL,
+        eventid         bigint          DEFAULT '0'     NOT NULL,
+        userid          bigint          DEFAULT '0'     NOT NULL,
+        clock           integer         DEFAULT '0'     NOT NULL,
+        mediatypeid             bigint          DEFAULT '0'     NOT NULL,
+        sendto          varchar(100)            DEFAULT ''      NOT NULL,
+        subject         varchar(255)            DEFAULT ''      NOT NULL,
+        message         text            DEFAULT ''      NOT NULL,
+        status          integer         DEFAULT '0'     NOT NULL,
+        retries         integer         DEFAULT '0'     NOT NULL,
+        error           varchar(128)            DEFAULT ''      NOT NULL,
+        nextcheck               integer         DEFAULT '0'     NOT NULL,
+        esc_step                integer         DEFAULT '0'     NOT NULL,
+        alerttype               integer         DEFAULT '0'     NOT NULL,
+        PRIMARY KEY (alertid)
+) with OIDS;
 
-alter table alerts drop triggerid;
-alter table alerts add eventid         bigint          DEFAULT '0'     NOT NULL;
-alter table alerts add esc_step integer DEFAULT '0'     NOT NULL;
-alter table alerts add alerttype integer DEFAULT '0'     NOT NULL;
+insert into alerts_tmp (alertid,actionid,userid,clock,mediatypeid,sendto,subject,message,status,retries,error,nextcheck) select alertid,actionid,userid,clock,mediatypeid,sendto,subject,message,status,retries,error,nextcheck from alerts;
+update alerts_tmp set status=3 where retries>=2;
+
+drop table alerts;
+alter table alerts_tmp rename to alerts;
 
 CREATE INDEX alerts_1 on alerts (actionid);
 CREATE INDEX alerts_2 on alerts (clock);
@@ -16,5 +29,3 @@ CREATE INDEX alerts_3 on alerts (eventid);
 CREATE INDEX alerts_4 on alerts (status,retries);
 CREATE INDEX alerts_5 on alerts (mediatypeid);
 CREATE INDEX alerts_6 on alerts (userid);
-
-update alerts set status=3 where retries>=2;
