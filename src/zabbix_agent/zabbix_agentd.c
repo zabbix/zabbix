fa4f31fcc33205b0ac19eebbc900955209f2cf04
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2008-11-04 14:24:35 +0000

    - [DEV-137] misc bugs

--- src/zabbix_agent/zabbix_agentd.c
+++ src/zabbix_agent/zabbix_agentd.c
@@ -254,6 +254,8 @@ int MAIN_ZABBIX_ENTRY(void)
 
 	init_collector_data();
 
+	load_user_parameters();
+
 	/* --- START THREADS ---*/
 
 	if(1 == CONFIG_DISABLE_PASSIVE)
@@ -363,8 +365,6 @@ int	main(int argc, char **argv)
 
 	if (ZBX_TASK_START == t.task || ZBX_TASK_INSTALL_SERVICE == t.task || ZBX_TASK_UNINSTALL_SERVICE == t.task || ZBX_TASK_START_SERVICE == t.task || ZBX_TASK_STOP_SERVICE == t.task)
 		load_config();
-
-	load_user_parameters();
 
 #if defined (_WINDOWS)
 	if (t.flags & ZBX_TASK_FLAG_MULTIPLE_AGENTS) {
