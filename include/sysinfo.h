7f06f52e13588031eb47905545bfbfdf7b0ed083
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2008-11-06 13:51:51 +0000

    - [DEV-250] more supported items
    git-svn-id: svn://svn.zabbix.com/trunk@6257 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/sysinfo.h
+++ include/sysinfo.h
@@ -170,6 +170,11 @@ typedef struct zbx_metric_type
 #define ZBX_AVG15	2
 #define ZBX_AVGMAX	3
 
+#define ZBX_PROC_STAT_ALL	0
+#define ZBX_PROC_STAT_RUN	1
+#define ZBX_PROC_STAT_SLEEP	2
+#define ZBX_PROC_STAT_ZOMB	3
+
 int	get_diskstat(const char *devname, time_t *now,
 		zbx_uint64_t *r_oper, zbx_uint64_t *r_sect, zbx_uint64_t *w_oper, zbx_uint64_t *w_sect);
 
@@ -188,7 +193,7 @@ int	process(const char *in_command, unsigned flags, AGENT_RESULT *result);
 
 int	add_user_parameter(char *key, char *command);
 void	test_parameters(void);
-void	test_parameter(char* key);
+void	test_parameter(const char* key, unsigned flags);
 
 void   	init_result(AGENT_RESULT *result);
 int    	copy_result(AGENT_RESULT *src, AGENT_RESULT *dist);
