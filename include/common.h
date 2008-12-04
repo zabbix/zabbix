3a5ea61ade64857dfa8874eeb8122f41d7ccc087
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2008-12-04 10:49:15 +0000

    - [NTD-37] vesrion changed to 1.0.6
    git-svn-id: svn://svn.zabbix.com/branches/1.4.j@6394 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -121,8 +121,8 @@
 #define OFF	0
 
 #define	APPLICATION_NAME	"ZABBIX Agent"
-#define	ZABBIX_REVDATE		"27 November 2008"
-#define	ZABBIX_VERSION		"1.0.5"
+#define	ZABBIX_REVDATE		"4 December 2008"
+#define	ZABBIX_VERSION		"1.0.6"
 
 #if defined(_WINDOWS)
 extern char ZABBIX_SERVICE_NAME[64];
