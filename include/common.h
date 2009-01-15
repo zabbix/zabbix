2e5f298ae5a458574371e122ce6551a7ad46db77
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2009-01-15 08:51:13 +0000

    - [NTD-50] version changed to 1.0.0
    git-svn-id: svn://svn.zabbix.com/branches/1.4.j@6566 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -121,8 +121,8 @@
 #define OFF	0
 
 #define	APPLICATION_NAME	"ZABBIX Agent"
-#define	ZABBIX_REVDATE		"13 January 2009"
-#define	ZABBIX_VERSION		"1.0.14"
+#define	ZABBIX_REVDATE		"15 January 2009"
+#define	ZABBIX_VERSION		"1.0.0"
 
 #if defined(_WINDOWS)
 extern char ZABBIX_SERVICE_NAME[64];
