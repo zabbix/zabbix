a5037ed7916c4f2a547bc23fbad2920f8fcc4819
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2009-01-20 10:29:06 +0000

    - [ZBX-657] improved performance of the graph related screen due to missing index
    git-svn-id: svn://svn.zabbix.com/branches/1.6@6610 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.6/mysql/patch/graphs_items.sql
+++ upgrades/dbpatches/1.6/mysql/patch/graphs_items.sql
@@ -1,3 +1,4 @@
 alter table graphs_items change color color varchar(32) DEFAULT '009600' NOT NULL;
 
 CREATE INDEX graphs_items_1 on graphs_items (itemid);
+CREATE INDEX graphs_items_2 on graphs_items (graphid);
