87dddc6e1f11554577ab56aaa9eb46933c4318dd
sasha <sasha@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2009-01-20 10:30:12 +0000

    - [ZBX-657] improved performance of the graph related screen due to missing index    [svn merge svn://svn.zabbix.com/branches/1.6 -r6608:6610]
    git-svn-id: svn://svn.zabbix.com/trunk@6611 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/1.6/oracle/patch/graphs_items.sql
+++ upgrades/dbpatches/1.6/oracle/patch/graphs_items.sql
@@ -1,3 +1,4 @@
 alter table graphs_items modify color varchar2(32) DEFAULT '009600';
 
 CREATE INDEX graphs_items_1 on graphs_items (itemid);
+CREATE INDEX graphs_items_2 on graphs_items (graphid);
--- upgrades/dbpatches/1.6/postgresql/patch/graphs_items.sql
+++ upgrades/dbpatches/1.6/postgresql/patch/graphs_items.sql
@@ -1,1 +1,2 @@
 CREATE INDEX graphs_items_1 on graphs_items (itemid);
+CREATE INDEX graphs_items_2 on graphs_items (graphid);
