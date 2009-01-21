2b0b3312ca6d183af93894c7e908be1ba45430ad
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2009-01-21 10:05:30 +0000

    - [NTD-62] version changed to 1.0.1
    git-svn-id: svn://svn.zabbix.com/branches/1.4.j@6617 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -121,8 +121,8 @@
 #define OFF	0
 
 #define	APPLICATION_NAME	"ZABBIX Agent"
-#define	ZABBIX_REVDATE		"15 January 2009"
-#define	ZABBIX_VERSION		"1.0.0"
+#define	ZABBIX_REVDATE		"21 January 2009"
+#define	ZABBIX_VERSION		"1.0.1"
 
 #if defined(_WINDOWS)
 extern char ZABBIX_SERVICE_NAME[64];
