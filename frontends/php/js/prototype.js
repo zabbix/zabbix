9539f2853009e251da54b277f15860972035eb3d
Artem Suharev <git-no-reply@zabbix.com>
2009-01-22 16:52:59 +0000

    - [DEV-231] improvements in js code (Artem)

--- frontends/php/js/prototype.js
+++ frontends/php/js/prototype.js
@@ -3037,7 +3037,7 @@ Object.extend(Selector, {
     pseudo:
 /^:((first|last|nth|nth-last|only)(-child|-of-type)|empty|checked|(en|dis)abled|not)(\((.*?)\))?(\b|$|(?=\s|[:+~>]))/,
     attrPresence: /^\[((?:[\w]+:)?[\w]+)\]/,
-    attr:         /\[((?:[\w-]*:)?[\w-]+)\s*(?:([!^$*~|]?=)\s*((['"])([^\4]*?)\4|([^'"][^\]]*?)))?\]/
+    attr:         /\[((?:[\w-]*:)?[\w-]+)\s*(?:([!^$*~|]?=)\s*((['"])([^\4]*?)\4|([^'"][^\]]*?)))?\]/			//"
   },
 
   // for Selector.match and Element#match
