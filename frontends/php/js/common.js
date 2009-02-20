caac45e53407212d547fa4542e95ac10142f735d
artem <artem@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2009-02-20 12:10:34 +0000

    - [ZBX-555] merged rev. 6771-6772 from /branches/1.6 (Artem)
    git-svn-id: svn://svn.zabbix.com/trunk@6773 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/js/common.js
+++ frontends/php/js/common.js
@@ -513,6 +513,16 @@ function ScaleChartToParenElement(obj_name){
 	}
 }
 
+function insert_in_element(element_name, text){
+	var elems = document.getElementsByName(element_name);
+	
+	for(var key=0; key < elems.length; key++){
+		if((typeof(elems[key]) != 'undefined') && !is_null(elems[key])){
+			$(elems[key]).update(text);
+		}
+	}
+}
+
 function insert_sizeable_graph(graph_id,url){
 	if((typeof(ZBX_G_WIDTH) != 'undefined')) url += "&amp;width="+ZBX_G_WIDTH;
 
@@ -522,6 +532,7 @@ function insert_sizeable_graph(graph_id,url){
 /************************************************************************************/
 /*									MAIN MENU stuff									*/
 /************************************************************************************/
+// Author: Aly
 
 var MMenu = {
 menus:			{'empty': 0, 'view': 0, 'cm': 0, 'reports': 0, 'config': 0, 'admin': 0},
@@ -558,4 +569,114 @@ showSubMenu: function(show_label){
 		}
 	}
 }
+}
+
+
+/************************************************************************************/
+/*						Automatic checkbox range selection 							*/
+/************************************************************************************/
+// Author: Aly
+
+var chkbx_range_ext = {
+startbox: 			null,			// start checkbox obj
+startbox_name: 		null,			// start checkbox name
+chkboxes:			new Array(),	// ckbx list
+
+init: function(){
+	var chk_bx = document.getElementsByTagName('input');
+	for(var i=0; i < chk_bx.length; i++){
+		if((typeof(chk_bx[i]) != 'undefined') && (chk_bx[i].type.toLowerCase() == 'checkbox')){
+			this.implement(chk_bx[i]);
+		}
+	}
+},
+
+implement: function(obj){
+	var obj_name = obj.name.split('[')[0];
+
+	if(typeof(this.chkboxes[obj_name]) == 'undefined') this.chkboxes[obj_name] = new Array();
+	this.chkboxes[obj_name].push(obj);
+
+	addListener(obj, 'click', this.check.bindAsEventListener(this), false);
+},
+
+check: function(e){
+	var e = e || window.event;
+	if(!e.ctrlKey) return true;
+	var obj = eventTarget(e);
+
+	if((typeof(obj) != 'undefined') && (obj.type.toLowerCase() == 'checkbox')){
+		var obj_name = obj.name.split('[')[0];
+
+		if(!is_null(this.startbox) && (this.startbox_name == obj_name) && (obj.name != this.startbox.name)){
+			var chkbx_list = this.chkboxes[obj_name];
+			var flag = false;
+			
+			for(var i=0; i < chkbx_list.length; i++){
+				if(typeof(chkbx_list[i]) !='undefined'){
+//alert(obj.name+' == '+chkbx_list[i].name);
+					if(flag){
+						chkbx_list[i].checked = this.startbox.checked;
+					}
+					
+					if(obj.name == chkbx_list[i].name) break;
+					if(this.startbox.name == chkbx_list[i].name) flag = true;
+				}
+			}
+			if(flag){
+				this.startbox = null;
+				this.startbox_name = null;
+				return true;
+			}
+			else{
+				for(var i=chkbx_list.length-1; i >= 0; i--){
+					if(typeof(chkbx_list[i]) !='undefined'){
+//alert(obj.name+' == '+chkbx_list[i].name);			
+						if(flag){
+							chkbx_list[i].checked = this.startbox.checked;
+						}
+						
+						if(obj.name == chkbx_list[i].name){
+							this.startbox = null;
+							this.startbox_name = null;
+							return true;
+						}
+						if(this.startbox.name == chkbx_list[i].name) flag = true;
+					}
+				}	
+			}
+
+		}
+		else{
+			if(!is_null(this.startbox)) this.startbox.checked = !this.startbox.checked;
+			
+			this.startbox = obj;
+			this.startbox_name = obj_name;
+		}
+	}
+}
+}
+/************************************************************************************/
+/*						Replace Standart Blink functionality						*/
+/************************************************************************************/
+// Author: Aly
+var blink = {
+	blinkobjs: new Array(),
+	
+	init: function(){
+		this.blinkobjs = document.getElementsByName("blink");
+		if(this.blinkobjs.length > 0) this.view();
+	},
+	hide: function(){
+		for(var id=0; id<this.blinkobjs.length; id++){
+			this.blinkobjs[id].style.visibility = 'hidden';
+		}
+		setTimeout('blink.view()',500);
+	},
+	view: function(){
+		for(var id=0; id<this.blinkobjs.length; id++){
+			this.blinkobjs[id].style.visibility = 'visible'
+		}
+		setTimeout('blink.hide()',1000);
+	}
 }
