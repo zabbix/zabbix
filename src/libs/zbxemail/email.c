a928f7470bdd8094b765d61e9b346fbc9841607b
sasha <sasha@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2009-04-08 13:22:16 +0000

    - [ZBX-856] fixed email sending if tcp packets from smtp server are segmented    [svn merge svn://svn.zabbix.com/branches/1.6 -r7161:7163]
    git-svn-id: svn://svn.zabbix.com/trunk@7164 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/libs/zbxemail/email.c
+++ src/libs/zbxemail/email.c
@@ -86,26 +86,22 @@ int	send_email(char *smtp_server,char *smtp_helo,char *smtp_email,char *mailto,c
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In send_email[smtp_server:%s]", smtp_server);
 
+	*error = '\0';
+
 	if(FAIL == zbx_tcp_connect(&s, CONFIG_SOURCE_IP, smtp_server, 25, 0))
 	{
 		zbx_snprintf(error,max_error_len,"Cannot connect to SMTP server [%s] [%s]", smtp_server, zbx_tcp_strerror());
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s",error);
 		goto out;
 	}
 
 	if (-1 == smtp_readln(s.socket, c, sizeof(c)))
 	{
 		zbx_snprintf(error,max_error_len,"Error receiving initial string from SMTP server [%s]", strerror(errno));
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 	if (0 != strncmp(OK_220, c, strlen(OK_220)))
 	{
 		zbx_snprintf(error,max_error_len,"No welcome message 220* from SMTP server [%s]", c);
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 
@@ -117,23 +113,17 @@ int	send_email(char *smtp_server,char *smtp_helo,char *smtp_email,char *mailto,c
 		if(e == -1)
 		{
 			zbx_snprintf(error,max_error_len,"Error sending HELO to mailserver [%s]", strerror(errno));
-			zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-			zabbix_syslog("%s", error);
 			goto out;
 		}
 				
 		if (-1 == smtp_readln(s.socket, c, sizeof(c)))
 		{
 			zbx_snprintf(error,max_error_len,"Error receiving answer on HELO request [%s]", strerror(errno));
-			zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-			zabbix_syslog("%s", error);
 			goto out;
 		}
 		if (0 != strncmp(OK_250, c, strlen(OK_250)))
 		{
 			zbx_snprintf(error,max_error_len,"Wrong answer on HELO [%s]", c);
-			zabbix_log(LOG_LEVEL_DEBUG, "%s",error);
-			zabbix_syslog("%s",error);
 			goto out;
 		}
 	}
@@ -146,23 +136,17 @@ int	send_email(char *smtp_server,char *smtp_helo,char *smtp_email,char *mailto,c
 	if(e == -1)
 	{
 		zbx_snprintf(error,max_error_len,"Error sending MAIL FROM to mailserver [%s]", strerror(errno));
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 
 	if (-1 == smtp_readln(s.socket, c, sizeof(c)))
 	{
 		zbx_snprintf(error,max_error_len,"Error receiving answer on MAIL FROM request [%s]", strerror(errno));
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 	if (0 != strncmp(OK_250, c, strlen(OK_250)))
 	{
 		zbx_snprintf(error,max_error_len,"Wrong answer on MAIL FROM [%s]", c);
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 			
@@ -172,23 +156,17 @@ int	send_email(char *smtp_server,char *smtp_helo,char *smtp_email,char *mailto,c
 	if(e == -1)
 	{
 		zbx_snprintf(error,max_error_len,"Error sending RCPT TO to mailserver [%s]", strerror(errno));
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 	if (-1 == smtp_readln(s.socket, c, sizeof(c)))
 	{
 		zbx_snprintf(error,max_error_len,"Error receiving answer on RCPT TO request [%s]", strerror(errno));
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 	/* May return 251 as well: User not local; will forward to <forward-path>. See RFC825 */
 	if( strncmp(OK_250,c,strlen(OK_250)) != 0 && strncmp(OK_251,c,strlen(OK_251)) != 0)
 	{
 		zbx_snprintf(error,max_error_len,"Wrong answer on RCPT TO [%s]", c);
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 	
@@ -198,22 +176,16 @@ int	send_email(char *smtp_server,char *smtp_helo,char *smtp_email,char *mailto,c
 	if(e == -1)
 	{
 		zbx_snprintf(error,max_error_len,"Error sending DATA to mailserver [%s]", strerror(errno));
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 	if (-1 == smtp_readln(s.socket, c, sizeof(c)))
 	{
 		zbx_snprintf(error,max_error_len,"Error receivng answer on DATA request [%s]", strerror(errno));
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 	if(strncmp(OK_354,c,strlen(OK_354)) != 0)
 	{
 		zbx_snprintf(error,max_error_len,"Wrong answer on DATA [%s]", c);
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 
@@ -239,8 +211,6 @@ int	send_email(char *smtp_server,char *smtp_helo,char *smtp_email,char *mailto,c
 	if(e == -1)
 	{
 		zbx_snprintf(error,max_error_len,"Error sending mail subject and body to mailserver [%s]", strerror(errno));
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 
@@ -250,22 +220,16 @@ int	send_email(char *smtp_server,char *smtp_helo,char *smtp_email,char *mailto,c
 	if(e == -1)
 	{
 		zbx_snprintf(error,max_error_len,"Error sending . to mailserver [%s]", strerror(errno));
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s",error);
 		goto out;
 	}
 	if (-1 == smtp_readln(s.socket, c, sizeof(c)))
 	{
 		zbx_snprintf(error,max_error_len,"Error receivng answer on . request [%s]", strerror(errno));
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s", error);
 		goto out;
 	}
 	if(strncmp(OK_250,c,strlen(OK_250)) != 0)
 	{
 		zbx_snprintf(error,max_error_len,"Wrong answer on end of data [%s]", c);
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s",error);
 		goto out;
 	}
 	
@@ -275,12 +239,17 @@ int	send_email(char *smtp_server,char *smtp_helo,char *smtp_email,char *mailto,c
 	if(e == -1)
 	{
 		zbx_snprintf(error,max_error_len,"Error sending QUIT to mailserver [%s]", strerror(errno));
-		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
-		zabbix_syslog("%s",error);
 		goto out;
 	}
 	ret = SUCCEED;
 out:	
 	zbx_tcp_close(&s);
+
+	if ('\0' != *error)
+	{
+		zabbix_log(LOG_LEVEL_DEBUG, "%s", error);
+		zabbix_syslog("%s", error);
+	}
+
 	return ret;
 }
