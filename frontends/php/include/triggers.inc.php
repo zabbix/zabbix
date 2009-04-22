e9d58e6b75a2fc3fb615b0bfdccdb5e822805db1
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2009-04-22 14:28:47 +0000

    - [ZBX-882] merged rev. 7208-7209 from /branches/1.6 (Artem)
    git-svn-id: svn://svn.zabbix.com/trunk@7210 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/triggers.inc.php
+++ frontends/php/include/triggers.inc.php
@@ -1206,19 +1206,21 @@
 	}
 	
 	function expand_trigger_description_simple($triggerid){
-		$sql = 'SELECT DISTINCT t.description,h.host,t.expression,t.triggerid '.
-				' FROM triggers t '.
-					' LEFT JOIN functions f on t.triggerid=f.triggerid '.
-					' LEFT JOIN items i on f.itemid=i.itemid '.
-					' LEFT JOIN hosts h on i.hostid=h.hostid '.
+		$sql = 'SELECT DISTINCT h.host,t.description,t.expression,t.triggerid '.
+				' FROM triggers t, functions f, items i, hosts h '.
+				' WHERE f.triggerid=t.triggerid '.
+					' AND i.itemid=f.itemid '.
+					' AND h.hostid=i.hostid '.
 				' WHERE t.triggerid='.$triggerid;
-		return expand_trigger_description_by_data(DBfetch(DBselect($sql)));
+		$trigger = DBfetch(DBselect($sql));
+		
+	return expand_trigger_description_by_data($trigger);
 	}
 
 	function expand_trigger_description($triggerid){
 		$description=expand_trigger_description_simple($triggerid);
 		$description=htmlspecialchars($description);
-		return $description;
+	return $description;
 	}
 
 	function update_trigger_value_to_unknown_by_hostid($hostids){
