30a136684d6cc2294da586b0aaaa53f4dae5c361
artem <artem@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2009-06-03 11:03:23 +0000

    - [ZBX-864] merged rev. 7345-7346 from /branches/1.6 (Artem)
    git-svn-id: svn://svn.zabbix.com/trunk@7348 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/config.inc.php
+++ frontends/php/include/config.inc.php
@@ -56,6 +56,7 @@ function __autoload($class_name){
 	require_once('include/profiles.inc.php');
 	require_once('conf/maintenance.inc.php');
 
+	require_once('include/nodes.inc.php');
 	require_once('include/hosts.inc.php');
 	
 // GLOBALS
@@ -203,175 +204,6 @@ function __autoload($class_name){
 
 	/********** END INITIALIZATION ************/
 
-	function init_nodes(){
-		/* Init CURRENT NODE ID */
-		if(defined('ZBX_NODES_INITIALIZED')) return;
-		
-		global $USER_DETAILS;
-		global $ZBX_LOCALNODEID, $ZBX_LOCMASTERID,
-			$ZBX_CURRENT_NODEID, $ZBX_CURMASTERID,  
-			$ZBX_NODES, $ZBX_NODES_IDS,
-			$ZBX_AVAILABLE_NODES, $ZBX_VIEWED_NODES,
-			$ZBX_WITH_ALL_NODES;
-
-		$ZBX_AVAILABLE_NODES = array();
-		$ZBX_NODES_IDS = array();
-		$ZBX_NODES = array();
-		$ZBX_CURRENT_NODEID = $ZBX_LOCALNODEID;
-		
-		$ZBX_WITH_ALL_NODES = !defined('ZBX_NOT_ALLOW_ALL_NODES');
-		
-		if(!defined('ZBX_PAGE_NO_AUTHERIZATION') && ZBX_DISTRIBUTED) {
-
-			if($USER_DETAILS['type'] == USER_TYPE_SUPER_ADMIN) {
-				$sql = 'SELECT DISTINCT n.nodeid,n.name,n.masterid FROM nodes n ';
-			}
-			else { 
-				$sql = 'SELECT DISTINCT n.nodeid,n.name,n.masterid '.
-					' FROM nodes n, groups hg,rights r, users_groups g '.
-					' WHERE r.id=hg.groupid '.
-					 	' AND r.groupid=g.usrgrpid '.
-					 	' AND g.userid='.$USER_DETAILS['userid'].
-					 	' AND n.nodeid='.DBid2nodeid('hg.groupid');
-			}
-			$db_nodes = DBselect($sql);
-			while($node = DBfetch($db_nodes)) {
-				$ZBX_NODES[$node['nodeid']] = $node;
-				$ZBX_NODES_IDS[$node['nodeid']] = $node['nodeid'];
-			}
-			
-			$ZBX_AVAILABLE_NODES = get_accessible_nodes_by_user($USER_DETAILS, PERM_READ_LIST, PERM_RES_IDS_ARRAY, $ZBX_NODES_IDS);
-			
-			$ZBX_VIEWED_NODES = get_viewed_nodes();
-			$ZBX_CURRENT_NODEID = $ZBX_VIEWED_NODES['selected'];
-
-			if($node_data = DBfetch(DBselect('SELECT masterid FROM nodes WHERE nodeid='.$ZBX_CURRENT_NODEID))){
-				$ZBX_CURMASTERID = $node_data['masterid'];
-			}
-			
-			if(!isset($ZBX_NODES[$ZBX_CURRENT_NODEID])) {
-				$ZBX_CURRENT_NODEID = $ZBX_LOCALNODEID;
-				$ZBX_CURMASTERID = $ZBX_LOCMASTERID;
-			}
-			
-			if(isset($_REQUEST['select_nodes']))
-				update_profile('web.nodes.selected', $ZBX_VIEWED_NODES['nodeids'], PROFILE_TYPE_ARRAY_ID);
-			if(isset($_REQUEST['switch_node']))
-				update_profile('web.nodes.switch_node', $ZBX_VIEWED_NODES['selected'], PROFILE_TYPE_ID);
-		}
-		else {
-			$ZBX_CURRENT_NODEID = $ZBX_LOCALNODEID;
-			$ZBX_CURMASTERID = $ZBX_LOCMASTERID;
-		}
-		
-		// zbx_set_post_cookie('zbx_current_nodeid', $ZBX_CURRENT_NODEID);
-		define('ZBX_NODES_INITIALIZED', 1);
-	}
-
-	function get_current_nodeid($forse_all_nodes = null, $perm = null){
-		global $USER_DETAILS, $ZBX_CURRENT_NODEID, $ZBX_AVAILABLE_NODES, $ZBX_VIEWED_NODES;
-		if(!isset($ZBX_CURRENT_NODEID)) {
-			init_nodes();
-		}	
-		
-		if(!is_null($perm)){
-			return get_accessible_nodes_by_user($USER_DETAILS, $perm, PERM_RES_IDS_ARRAY, $ZBX_AVAILABLE_NODES);
-		}
-		else if(is_null($forse_all_nodes)){
-			if($ZBX_VIEWED_NODES['selected'] == 0) {
-				$result = $ZBX_VIEWED_NODES['nodeids'];
-			}
-			else {
-				$result = $ZBX_VIEWED_NODES['selected'];
-			} 
-			if(empty($result)){
-				$result = $USER_DETAILS['node']['nodeid'];
-			}
-		}
-		else if($forse_all_nodes) {
-			$result = $ZBX_AVAILABLE_NODES;
-		}
-		else {
-			$result = $ZBX_CURRENT_NODEID;
-		}
-		
-	return $result;
-	}
-	
-	function get_viewed_nodes($options=array()) {
-		global $USER_DETAILS;
-		global $ZBX_LOCALNODEID, $ZBX_AVAILABLE_NODES;
-		
-		$config = select_config();
-		
-		$def_options = array(
-			'allow_all' => 0
-		);	
-		$options = array_merge($def_options, $options);
-		
-		$result = array('selected' => 0, 'nodes' => array(), 'nodeids' => array());
-				
-		if(!defined('ZBX_NOT_ALLOW_ALL_NODES')){
-			$result['nodes'][0] = array('nodeid' => 0, 'name' => S_ALL_S);
-		}
-		
-		$available_nodes = get_accessible_nodes_by_user($USER_DETAILS, PERM_READ_LIST, PERM_RES_DATA_ARRAY);
-		$available_nodes = get_tree_by_parentid($ZBX_LOCALNODEID, $available_nodes, 'masterid'); //remove parent nodes
-		
-		$selected_nodeids = get_request('selected_nodes', get_profile('web.nodes.selected', array($USER_DETAILS['node']['nodeid'])));
-		
-// +++ Fill $result['NODEIDS'], $result['NODES'] +++	
-		$nodes = array();
-		$nodeids = array();
-		foreach($selected_nodeids as $num => $nodeid) {
-			if(isset($available_nodes[$nodeid])) {
-				$result['nodes'][$nodeid] = array(
-					'nodeid' => $available_nodes[$nodeid]['nodeid'], 
-					'name' => $available_nodes[$nodeid]['name'], 
-					'masterid' => $available_nodes[$nodeid]['masterid']);
-				$nodeids[$nodeid] = $nodeid;
-			}
-		}
-// --- ---
-
-		$switch_node = get_request('switch_node', get_profile('web.nodes.switch_node', -1));
-
-		if(!isset($available_nodes[$switch_node]) || !uint_in_array($switch_node, $selected_nodeids)) { //check switch_node
-			$switch_node = 0;
-		}
-		
-		$result['nodeids'] = $nodeids;
-		if(!defined('ZBX_NOT_ALLOW_ALL_NODES')) {		
-			$result['selected'] = $switch_node;
-		}
-		else if(!empty($nodeids)){
-			$result['selected'] = ($switch_node > 0) ? $switch_node : array_shift($nodeids);
-		}
-
-	return $result;
-	}
-
-	function get_node_name_by_elid($id_val, $forse_with_all_nodes = null){
-		global $ZBX_NODES, $ZBX_VIEWED_NODES;
-
-	if($forse_with_all_nodes === false || (is_null($forse_with_all_nodes) && ($ZBX_VIEWED_NODES['selected'] != 0))) {
-			return null;
-		} 
-
-	$nodeid = id2nodeid($id_val);
-//SDI($nodeid.' - '.$ZBX_NODES[$nodeid]['name']);
-		if ( !isset($ZBX_NODES[$nodeid]) )
-			return null;
-
-		return '['.$ZBX_NODES[$nodeid]['name'].'] ';
-	}
-
-	function is_show_all_nodes(){
-		global	$ZBX_VIEWED_NODES;
-
-	return (ZBX_DISTRIBUTED && ($ZBX_VIEWED_NODES['selected'] == 0));
-	}
-
 	function access_deny(){
 
 		include_once('include/page_header.php');
@@ -399,7 +231,7 @@ function __autoload($class_name){
 	function show_messages($bool=TRUE,$okmsg=NULL,$errmsg=NULL){
 		global	$page, $ZBX_MESSAGES;
 
-		if (! defined('PAGE_HEADER_LOADED')) return;
+		if (!defined('PAGE_HEADER_LOADED')) return;
 		if (defined('ZBX_API_REQUEST')) return;
 
 		if (!isset($page["type"])) $page["type"] = PAGE_TYPE_HTML;
