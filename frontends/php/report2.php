36feee57420af6502d41a1279479253ddbcee58d
alex <alex@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2009-06-27 22:50:40 +0000

    - [ZBX-975] TRUE/FALSE was replaced to Problems/Ok in Availability Report    [svn merge -c7498 svn://svn.zabbix.com/branches/1.6]
    git-svn-id: svn://svn.zabbix.com/trunk@7499 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/report2.php
+++ frontends/php/report2.php
@@ -225,8 +225,8 @@ include_once 'include/page_header.php';
 				array(is_show_all_nodes()?S_NODE : null,
 				(($_REQUEST['hostid'] == 0) || (1 == $config))?S_HOST:NULL,
 				S_NAME,
-				S_TRUE,
-				S_FALSE,
+				S_PROBLEMS,
+				S_OK,
 				S_UNKNOWN,
 				S_GRAPH));
 
