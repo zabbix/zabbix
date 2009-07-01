0a304f7badced986b104a193f70e651982d126a5
Artem Suharev <git-no-reply@zabbix.com>
2009-07-01 11:18:44 +0000

    - [ZBX-965] fixed color in graph legend (Artem)  - [ZBX-908] merged rev. 7530-7531 from /branches/1.6 (Artem)

--- frontends/php/include/items.inc.php
+++ frontends/php/include/items.inc.php
@@ -596,7 +596,8 @@
 				'delay_flex='.zbx_dbstr($item['delay_flex']).','.
 				'params='.zbx_dbstr($item['params']).','.
 				'ipmi_sensor='.zbx_dbstr($item['ipmi_sensor']).','.
-				'templateid='.$item['templateid'].
+				'templateid='.$item['templateid'].','.
+				'nextcheck=0 '.
 			' WHERE itemid='.$itemid);
 
 		if ($result){
