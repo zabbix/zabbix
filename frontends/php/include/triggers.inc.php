e539b5d9530853d1c902e97989555ef3971faa46
artem <artem@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2009-07-16 13:05:07 +0000

    - [DEV-964] merged rev. 7671-7672 from /branches/1.6 (Artem)
    git-svn-id: svn://svn.zabbix.com/trunk@7673 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/triggers.inc.php
+++ frontends/php/include/triggers.inc.php
@@ -691,7 +691,8 @@ return $result;
 
 		/* Replace all {server:key.function(param)} and {MACRO} with '$ZBX_TR_EXPR_REPLACE_TO' */
 		while(ereg(ZBX_EREG_EXPRESSION_TOKEN_FORMAT, $expr, $arr)){
-			if ( $arr[ZBX_EXPRESSION_MACRO_ID] && !isset($ZBX_TR_EXPR_ALLOWED_MACROS[$arr[ZBX_EXPRESSION_MACRO_ID]]) ){
+
+			if($arr[ZBX_EXPRESSION_MACRO_ID] && !isset($ZBX_TR_EXPR_ALLOWED_MACROS[$arr[ZBX_EXPRESSION_MACRO_ID]]) ){
 				error('Unknown macro ['.$arr[ZBX_EXPRESSION_MACRO_ID].']');
 				return false;
 			}
@@ -728,20 +729,20 @@ return $result;
 							' AND h.hostid=i.hostid '.
 							' AND '.DBin_node('h.hostid', get_current_nodeid(false));
 
-				if (!$item = DBfetch(DBselect($sql)) ){
+				if(!$item = DBfetch(DBselect($sql)) ){
 					error('No such monitored parameter ('.$key.') for host ('.$host.')');
 					return false;
 				}
 
 				/* Check function */
-				if( !isset($ZBX_TR_EXPR_ALLOWED_FUNCTIONS[$function]) ){
+				if(!isset($ZBX_TR_EXPR_ALLOWED_FUNCTIONS[$function]) ){
 					error('Unknown function ['.$function.']');
 					return false;
 				}
 
 				$fnc_valid = &$ZBX_TR_EXPR_ALLOWED_FUNCTIONS[$function];
 
-				if ( is_array($fnc_valid['item_types']) &&
+				if(is_array($fnc_valid['item_types']) &&
 					!uint_in_array($item['value_type'], $fnc_valid['item_types'])){
 					$allowed_types = array();
 					foreach($fnc_valid['item_types'] as $type)
@@ -751,7 +752,7 @@ return $result;
 					return false;
 				}
 
-				if( !is_null($fnc_valid['args']) ){
+				if(!is_null($fnc_valid['args']) ){
 					$parameter = zbx_get_params($parameter);
 
 					if( !is_array($fnc_valid['args']) )
@@ -780,6 +781,10 @@ return $result;
 						}
 					}
 				}
+			}
+			else{
+				error('An item key must be used in trigger expression');
+				return false;
 			}
 
 			$expr = $arr[ZBX_EXPRESSION_LEFT_ID].$ZBX_TR_EXPR_REPLACE_TO.$arr[ZBX_EXPRESSION_RIGHT_ID];
@@ -842,6 +847,7 @@ return $result;
 				if(!$result2=add_trigger_dependency($triggerid, $triggerid_up)){
 					error(S_INCORRECT_DEPENDENCY.' ['.expand_trigger_description($triggerid_up).']');
 				}
+
 				$result &= $result2;
 			}
 		}
