741b2ac06974595d699b2f10edd288162ee3fed8
Alexey Fukalov <git-no-reply@zabbix.com>
2009-07-21 12:47:55 +0000

    - [DEV-405] api for users and usergroups screesns (Vedmak)

--- frontends/php/api/classes/class.cuser.php
+++ frontends/php/api/classes/class.cuser.php
@@ -11,6 +11,227 @@ class CUser {
 	public static $error;
 
 	/**
+	 * Get Users
+	 *
+	 * {@source}
+	 * @access public
+	 * @static
+	 * @since 1.8
+	 * @version 1
+	 *
+	 * @param _array $options
+	 * @param array $options['nodeids'] Node IDs
+	 * @param array $options['usrgrpids'] UserGroup IDs
+	 * @param array $options['userids'] User IDs
+	 * @param boolean $options['type']
+	 * @param boolean $options['status']
+	 * @param boolean $options['with_gui_access']
+	 * @param boolean $options['with_api_access']
+	 * @param boolean $options['select_usrgrps']
+	 * @param boolean $options['get_access'] 
+	 * @param int $options['extendoutput'] 
+	 * @param int $options['count'] 
+	 * @param string $options['pattern'] 
+	 * @param int $options['limit'] limit selection
+	 * @param string $options['order']
+	 * @return array
+	 */
+	public static function get($options=array()){
+		global $USER_DETAILS;
+
+		$result = array();
+		
+		$sort_columns = array('userid', 'alias'); // allowed columns for sorting
+	
+	
+		$sql_parts = array(
+			'select' => array('users' => 'u.userid'),
+			'from' => array('users u'),
+			'where' => array(),
+			'order' => array(),
+			'limit' => null);
+		
+		$def_options = array(
+			'nodeids'					=> 0,
+			'usrgrpids'					=> 0,
+			'userids'					=> 0,
+			'type'						=> null,
+			'status'					=> null,
+			'with_gui_access'			=> 0,
+			'with_api_access'			=> 0,
+// OutPut
+			'extendoutput'				=> 0,
+			'select_usrgrps'			=> 0,
+			'get_access'				=> 0,
+			'count'						=> 0,
+			'pattern'					=> '',
+			'order' 					=> '',
+			'limit'						=> 0
+		);
+
+		$options = array_merge($def_options, $options);
+
+// nodeids
+		$nodeids = $options['nodeids'] ? $options['nodeids'] : get_current_nodeid(false);
+
+// usrgrpids
+		if($options['usrgrpids'] != 0){
+			zbx_value2array($options['usrgrpids']);
+			if($options['extendoutput'] != 0){
+				$sql_parts['select']['usrgrpid'] = 'ug.usrgrpid';
+			}
+			$sql_parts['from']['ug'] = 'users_groups ug';
+			$sql_parts['where'][] = DBcondition('ug.usrgrpid', $options['usrgrpids']);
+			$sql_parts['where']['uug'] = 'u.userid=ug.userid';
+			
+		}
+
+// userids
+		if($options['userids'] != 0){
+			zbx_value2array($options['userids']);
+			$sql_parts['where'][] = DBcondition('u.userid', $options['userids']);
+		}
+
+// type
+		if(!is_null($options['status'])){
+			$sql_parts['where'][] = 'g.users_status='.$options['status'];
+		}
+// status
+		if(!is_null($options['status'])){
+			$sql_parts['where'][] = 'g.users_status='.$options['status'];
+		}
+
+// with_gui_access
+		if($options['with_gui_access'] != 0){
+			$sql_parts['where'][] = 'g.gui_access='.GROUP_GUI_ACCESS_ENABLED;
+		}
+// with_api_access
+		if($options['with_api_access'] != 0){
+			$sql_parts['where'][] = 'g.api_access='.GROUP_API_ACCESS_ENABLED;
+		}
+
+// extendoutput
+		if($options['extendoutput'] != 0){
+			$sql_parts['select']['usrgrp'] = 'u.*';
+		}
+		
+// count
+		if($options['count'] != 0){
+			$options['select_usrgrps'] = 0;
+			$sql_parts['select'] = array('count(u.userid) as rowscount');
+		}
+
+// pattern
+		if(!zbx_empty($options['pattern'])){
+			$sql_parts['where'][] = ' UPPER(u.alias) LIKE '.zbx_dbstr('%'.strtoupper($options['pattern']).'%');
+		}
+
+// order
+		// restrict not allowed columns for sorting
+		$options['order'] = str_in_array($options['order'], $sort_columns) ? $options['order'] : '';
+		if(!zbx_empty($options['order'])){
+			$sql_parts['order'][] = 'u.'.$options['order'];
+			if(!str_in_array('u.'.$options['order'], $sql_parts['select']) && $options['extendoutput'] == 0){
+				$sql_parts['select'][] = 'u.'.$options['order'];
+			}
+		}
+
+// limit
+		if(zbx_ctype_digit($options['limit']) && $options['limit']){
+			$sql_parts['limit'] = $options['limit'];
+		}
+//-------
+		$userids = array();
+		
+		$sql_parts['select'] = array_unique($sql_parts['select']);
+		$sql_parts['from'] = array_unique($sql_parts['from']);
+		$sql_parts['where'] = array_unique($sql_parts['where']);
+		$sql_parts['order'] = array_unique($sql_parts['order']);
+	
+		$sql_select = '';
+		$sql_from = '';
+		$sql_where = '';
+		$sql_order = '';
+		if(!empty($sql_parts['select']))	$sql_select.= implode(',',$sql_parts['select']);
+		if(!empty($sql_parts['from']))		$sql_from.= implode(',',$sql_parts['from']);
+		if(!empty($sql_parts['where']))		$sql_where.= ' AND '.implode(' AND ',$sql_parts['where']);
+		if(!empty($sql_parts['order']))		$sql_order.= ' ORDER BY '.implode(',',$sql_parts['order']);	
+		$sql_limit = $sql_parts['limit'];
+
+		$sql = 'SELECT '.$sql_select.'
+				FROM '.$sql_from.'
+				WHERE '.DBin_node('u.userid', $nodeids).
+				$sql_where.
+				$sql_order;
+		$res = DBselect($sql, $sql_limit);
+		while($user = DBfetch($res)){
+			if($options['count'])
+				$result = $user;
+			else{
+				$userids[$user['userid']] = $user['userid'];
+
+				if($options['extendoutput'] == 0){
+					$result[$user['userid']] = $user['userid'];
+				}
+				else{
+					if(!isset($result[$user['userid']])) $result[$user['userid']]= array();
+					
+					if($options['select_usrgrps'] && !isset($result[$user['userid']]['usrgrpids'])){
+						$result[$user['userid']]['usrgrpids'] = array();
+						$result[$user['userid']]['usrgrps'] = array();
+					}
+					
+					// usrgrpids
+					if(isset($user['usrgrpid'])){
+						if(!isset($result[$user['userid']]['usrgrpids'])) 
+							$result[$user['userid']]['usrgrpids'] = array();
+							
+						$result[$user['userid']]['usrgrpids'][$user['usrgrpid']] = $user['usrgrpid'];
+						unset($user['usrgrpid']);
+					}
+					
+					$result[$user['userid']] += $user;
+				}
+			}
+		}
+	
+	if($options['get_access'] != 0){
+	
+		foreach($result as $userid => $user){
+			$result[$userid] += array('api_access' => 0, 'gui_access' => 0, 'debug_mode' => 0, 'users_status' => 0);
+		}
+		
+		$sql = 'SELECT ug.userid, MAX(g.api_access) as api_access,  MAX(g.gui_access) as gui_access, 
+					MAX(g.debug_mode) as debug_mode, MAX(g.users_status) as users_status'.
+				' FROM usrgrp g, users_groups ug '.
+				' WHERE '.DBcondition('ug.userid', $userids).
+					' AND g.usrgrpid=ug.usrgrpid '.
+				' GROUP BY ug.userid';
+		$access = DBselect($sql);
+		
+		while($useracc = DBfetch($access)){
+			$result[$useracc['userid']] = array_merge($result[$useracc['userid']], $useracc);
+		}
+	}
+	
+// Adding Objects
+
+// Adding usegroups
+		if($options['select_usrgrps']){
+			$obj_params = array('extendoutput' => 1, 'userids' => $userids);
+			$usrgrps = CUserGroup::get($obj_params);
+			foreach($usrgrps as $usrgrpid => $usrgrp){
+				foreach($usrgrp['userids'] as $num => $userid){
+					$result[$userid]['usrgrpids'][$usrgrpid] = $usrgrpid;
+					$result[$userid]['usrgrps'][$usrgrpid] = $usrgrp;
+				}
+			}
+		}
+
+	return $result;
+	}
+	
+	/**
 	 * Authenticate user
 	 *
 	 * @static
@@ -62,7 +283,6 @@ class CUser {
 		$access = DBfetch(DBselect($sql));
 		return $access['access'] ? true : false;
 	}
-	
 
 	/**
 	 * Gets all User data from DB by User ID
@@ -162,7 +382,12 @@ class CUser {
 	/**
 	 * Update Users
 	 *
+	 * {@source}
+	 * @access public
 	 * @static
+	 * @since 1.8
+	 * @version 1
+	 *
 	 * @param array $users multidimensional array with Users data
 	 * @return boolean
 	 */
@@ -188,6 +413,12 @@ class CUser {
 	/**
 	 * Add Medias for User
 	 *
+	 * {@source}
+	 * @access public
+	 * @static
+	 * @since 1.8
+	 * @version 1
+	 *
 	 * <code>
 	 * $media_data = array(
 	 * 	*string 'userid => 'User ID',
@@ -201,7 +432,6 @@ class CUser {
 	 * );
 	 * </code>
 	 *
-	 * @static
 	 * @param array $media_data 
 	 * @return boolean
 	 */
@@ -226,6 +456,12 @@ class CUser {
 	/**
 	 * Delete User Medias
 	 *
+	 * {@source}
+	 * @access public
+	 * @static
+	 * @since 1.8
+	 * @version 1
+	 *
 	 * <code>
 	 * $media_data = array(
 	 * 	*string 'userid => 'User ID',
@@ -233,7 +469,6 @@ class CUser {
 	 * );
 	 * </code>
 	 *
-	 * @static
 	 * @param array $media_data 
 	 * @return boolean
 	 */
@@ -253,6 +488,12 @@ class CUser {
 	/**
 	 * Add Medias for User
 	 *
+	 * {@source}
+	 * @access public
+	 * @static
+	 * @since 1.8
+	 * @version 1
+	 *
 	 * <code>
 	 * $media_data = array(
 	 * 	*string 'userid => 'User ID',
@@ -267,7 +508,6 @@ class CUser {
 	 * );
 	 * </code>
 	 *
-	 * @static
 	 * @param array $media_data 
 	 * @return boolean
 	 */
@@ -293,7 +533,12 @@ class CUser {
 	/**
 	 * Delete Users
 	 *
+	 * {@source}
+	 * @access public
 	 * @static
+	 * @since 1.8
+	 * @version 1
+	 *
 	 * @param array $userids
 	 * @return boolean
 	 */
