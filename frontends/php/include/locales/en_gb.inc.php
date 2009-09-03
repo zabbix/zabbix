e692704dead43fbd2776d7bab7c8b26a51972e82
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2009-09-03 10:31:58 +0000

    - [ZBX-886] remove outdated string
    git-svn-id: svn://svn.zabbix.com/trunk@7858 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/locales/en_gb.inc.php
+++ frontends/php/include/locales/en_gb.inc.php
@@ -1482,7 +1482,6 @@
 	'S_SCREEN_REFRESH'=>				'Refresh (in seconds)',
 	'S_CREATE_USER'=>				'Create User',
 	'S_CREATE_GROUP'=>				'Create Group',
-	'S_DELETE_SELECTED_USERS_Q'=>			'Delete selected users?',
 	'S_NO_ACCESSIBLE_RESOURCES'=>			'No accessible resources',
 	'S_ADD_TO'=>					'Add to',
 	'S_REMOVE_FROM'=>				'Remove from',
