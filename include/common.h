ab966df4aaab39eec41f1427785ee4b71cc8b52b
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2009-09-08 15:13:03 +0000

    - [DEV-137] update version string
    git-svn-id: svn://svn.zabbix.com/branches/1.6@7876 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -122,7 +122,7 @@
 
 #define	APPLICATION_NAME	"ZABBIX Agent"
 #define	ZABBIX_REVDATE		"26 August 2009"
-#define	ZABBIX_VERSION		"1.6.6"
+#define	ZABBIX_VERSION		"1.6.7"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
 #if defined(_WINDOWS)
