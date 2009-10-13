f027b75b985607efbe9e842d2f659c77cb1c7422
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2009-10-13 23:22:19 +0000

    [DEV-137] more updates on MySQL patches
    git-svn-id: svn://svn.zabbix.com/trunk@8077 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.8/mysql/patch/sysmaps_elements.sql
+++ upgrades/dbpatches/1.8/mysql/patch/sysmaps_elements.sql
@@ -1,1 +1,1 @@
-alter table sysmap_elements  modify label           varchar(255)            DEFAULT ''      NOT NULL;
+alter table sysmaps_elements  modify label           varchar(255)            DEFAULT ''      NOT NULL;
