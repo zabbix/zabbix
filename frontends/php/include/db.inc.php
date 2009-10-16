d254012bea3686e7a87f0a04cfb91f59227d6a8a
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2009-10-16 14:01:02 +0000

    - [ZBX-641] fixed oracle OCI-Lob parse (Aly)
    git-svn-id: svn://svn.zabbix.com/branches/1.6@8102 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/db.inc.php
+++ frontends/php/include/db.inc.php
@@ -527,7 +527,14 @@ if(!isset($DB)){
 				if(ocifetchinto($cursor, $row, (OCI_ASSOC+OCI_RETURN_NULLS))){
 					$result = array();
 					foreach($row as $key => $value){
-						$result[strtolower($key)] = (str_in_array(strtolower(ocicolumntype($cursor,$key)),array('varchar','varchar2','blob','clob')) && is_null($value))?'':$value;
+						$field_type = strtolower(oci_field_type($cursor,$key));
+						$value = (str_in_array($field_type,array('varchar','varchar2','blob','clob')) && is_null($value))?'':$value;
+						
+						if(is_object($value) && (stristr($field_type, 'lob') !== false)){
+							$value = $value->load();
+						}
+						
+						$result[strtolower($key)] = $value;
 					}
 				}
 				break;
