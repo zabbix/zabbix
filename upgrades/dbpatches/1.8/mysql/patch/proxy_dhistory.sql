c139ed22c264dd7430bd6f05cc949c4d58edadbe
sasha <sasha@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2009-11-11 13:40:53 +0000

    [DEV-137] added patches for PostgreSQL, 1.6 -> 1.8 migration
    git-svn-id: svn://svn.zabbix.com/trunk@8245 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/1.8/mysql/patch/proxy_dhistory.sql
+++ upgrades/dbpatches/1.8/mysql/patch/proxy_dhistory.sql
@@ -1,4 +1,4 @@
-alter table proxy_dhistory modify key_            varchar(255)            DEFAULT ''      NOT NULL;
-alter table proxy_dhistory modify value           varchar(255)            DEFAULT ''      NOT NULL;
+alter table proxy_dhistory alter key_            varchar(255)            DEFAULT ''      NOT NULL;
+alter table proxy_dhistory alter value           varchar(255)            DEFAULT ''      NOT NULL;
 
 alter table proxy_dhistory add         dcheckid                bigint unsigned         DEFAULT '0'     NOT NULL;
