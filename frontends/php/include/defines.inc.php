40eaaa352f522197431aeb8e4a765937d660c82b
Alexei Vladishev <alexei.vladishev@zabbix.com>
2009-12-04 08:40:52 +0000

    - updated current version to 1.7.4

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,8 +19,8 @@
 **/
 ?>
 <?php
-	define('ZABBIX_VERSION','1.7.3');
-	define('ZABBIX_API_VERSION','1.7.3');
+	define('ZABBIX_VERSION','1.7.4');
+	define('ZABBIX_API_VERSION','1.0');
 /* USER DEFINES */
 
 	define('ZBX_LOGIN_ATTEMPTS',			5);
