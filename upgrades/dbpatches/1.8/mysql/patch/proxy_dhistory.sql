38169f32303abf273baf0f8051e75ed368c4fec9
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2009-12-04 19:25:57 +0000

    - [ZBX-1419] fix MySQL upgrade patch
    git-svn-id: svn://svn.zabbix.com/trunk@8527 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/1.8/mysql/patch/proxy_dhistory.sql
+++ upgrades/dbpatches/1.8/mysql/patch/proxy_dhistory.sql
@@ -1,4 +1,4 @@
-alter table proxy_dhistory alter key_            varchar(255)            DEFAULT ''      NOT NULL;
-alter table proxy_dhistory alter value           varchar(255)            DEFAULT ''      NOT NULL;
+alter table proxy_dhistory change key_ key_ varchar(255) DEFAULT '' NOT NULL;
+alter table proxy_dhistory change value value varchar(255) DEFAULT '' NOT NULL;
 
-alter table proxy_dhistory add         dcheckid                bigint unsigned         DEFAULT '0'     NOT NULL;
+alter table proxy_dhistory add dcheckid bigint unsigned DEFAULT '0' NOT NULL;
