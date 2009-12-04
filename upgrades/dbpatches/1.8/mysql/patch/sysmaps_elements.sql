149220821bf44695297e9828b3890cec5b6142ce
rich <rich@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2009-12-04 19:25:57 +0000

    - [ZBX-1419] fix MySQL upgrade patch
    git-svn-id: svn://svn.zabbix.com/trunk@8527 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/1.8/mysql/patch/sysmaps_elements.sql
+++ upgrades/dbpatches/1.8/mysql/patch/sysmaps_elements.sql
@@ -1,2 +1,3 @@
-alter table sysmaps_elements  alter label           varchar(255)            DEFAULT ''      NOT NULL;
+alter table sysmaps_elements change label label varchar(255) DEFAULT '' NOT NULL;
+
 ALTER TABLE sysmaps_elements ADD iconid_maintenance BIGINT unsigned DEFAULT '0' NOT NULL;
