9ff03064351d6274162a14ed0f3d9509f9cb227c
rich <rich@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2009-12-04 14:00:49 +0000

    - [ZBX-1400] fixed working time display for upgrades (Richlv)
    git-svn-id: svn://svn.zabbix.com/trunk@8510 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/1.8/mysql/patch/graph_theme.sql
+++ upgrades/dbpatches/1.8/mysql/patch/graph_theme.sql
@@ -20,5 +20,5 @@ CREATE TABLE graph_theme (
 CREATE INDEX graph_theme_1 on graph_theme (description);
 CREATE INDEX graph_theme_2 on graph_theme (theme);
 
-INSERT INTO graph_theme VALUES (1,'Original Blue','css_ob.css','F0F0F0','FFFFFF','333333','CCCCCC','AAAAAA','000000','222222','AA4444','11CC11','CC1111','FFFFFF',1,1);
+INSERT INTO graph_theme VALUES (1,'Original Blue','css_ob.css','F0F0F0','FFFFFF','333333','CCCCCC','AAAAAA','000000','222222','AA4444','11CC11','CC1111','E0E0E0',1,1);
 INSERT INTO graph_theme VALUES (2,'Black & Blue','css_bb.css','333333','0A0A0A','888888','222222','4F4F4F','EFEFEF','0088FF','CC4444','1111FF','FF1111','1F1F1F',1,1);
