fcbe6b3eaa6823c562a9ba141cad33cb7f5f6139
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2009-12-16 13:34:11 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@8724 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- trunk_map/upgrades/dbpatches/1.8/mysql/patch/sysmaps_elements.sql
+++
@@ -1,2 +0,0 @@
-alter table sysmaps_elements  alter label           varchar(255)            DEFAULT ''      NOT NULL;
-ALTER TABLE sysmaps_elements ADD iconid_maintenance BIGINT unsigned DEFAULT '0' NOT NULL;
