5908a25b890e2fd012179436c99dac900634da77
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-01-04 12:54:00 +0000

    ZBX-1563
    git-svn-id: svn://svn.zabbix.com/branches/dev@9009 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-1563-history-from/frontends/php/include/classes/class.cbutton.php
@@ -0,0 +1,54 @@
+<?php
+/*
+** ZABBIX
+** Copyright (C) 2000-2009 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+?>
+<?php
+class CButton extends CTag{
+	public function __construct($name='button', $caption='', $action=NULL, $accesskey=NULL){
+		parent::__construct('input','no');
+		$this->tag_body_start = '';
+		$this->attributes['type'] = 'submit';
+		$this->setAttribute('value', $caption);
+		$this->attributes['class'] = 'button';
+		$this->setName($name);
+		$this->setAction($action);
+		$this->setAccessKey($accesskey);
+	}
+
+	public function setAction($value=null){
+		$this->addAction('onclick', $value);
+	}
+
+	public function setTitle($value='button title'){
+		$this->setAttribute('title', $value);
+	}
+
+	public function setAccessKey($value='B'){
+		if(isset($value))
+			if(!isset($this->attributes['title']))
+				$this->setTitle($this->attributes['value'].' [Alt+'.$value.']');
+
+		return $this->setAttribute('accessKey', $value);
+	}
+
+	public function setType($type='button'){
+		$this->setAttribute('type',$type);
+	}
+}
+?>
