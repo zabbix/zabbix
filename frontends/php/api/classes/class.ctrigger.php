ebc9204a99f978f6ce43e4168691de18fb487991
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-01-20 09:31:38 +0000

    - [ZBX-1758] improved node filtering in sqls (Aly)
    git-svn-id: svn://svn.zabbix.com/branches/1.8@9456 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/api/classes/class.ctrigger.php
+++ frontends/php/api/classes/class.ctrigger.php
@@ -419,7 +419,7 @@ class CTrigger extends CZBXAPI{
 		if(!empty($sql_parts['order']))		$sql_order.= ' ORDER BY '.implode(',',$sql_parts['order']);
 		$sql_limit = $sql_parts['limit'];
 
-		$sql = 'SELECT '.$sql_select.
+		$sql = 'SELECT DISTINCT '.$sql_select.
 				' FROM '.$sql_from.
 				' WHERE '.DBin_node('t.triggerid', $nodeids).
 					$sql_where.
