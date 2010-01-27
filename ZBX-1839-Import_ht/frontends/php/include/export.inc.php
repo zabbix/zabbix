b364deba9a2db73bb9954fd4ebc654e5b18ddca8
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-01-27 08:23:40 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@9630 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-1839-Import_ht/frontends/php/include/export.inc.php
+++ ZBX-1839-Import_ht/frontends/php/include/export.inc.php
@@ -598,19 +598,7 @@ class zbxXML{
 						$host_templates[] = $current_template;
 					}
 					
-					// if($host_db['status'] == HOST_STATUS_TEMPLATE){
-						$host_db['templates'] = $host_templates;
-					// }
-					// else{
-						// $host_db['templates'] = $host_templates;
-					// }
-
-					// $r = CTemplate::massAdd(array('templates' => $current_host, 'templates_link' => $host_templates));
-					// if($r === false){
-						// error(CTemplate::resetErrors());
-						// $result = false;
-						// break;
-					// }
+					$host_db['templates'] = $host_templates;
 				}
 // }}} TEMPLATES
 
