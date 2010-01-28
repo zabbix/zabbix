424ae78f6edecfe2108c6d6ee3f385d9cbc59098
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-01-28 10:32:25 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@9707 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-1528-maps/upgrades/dbpatches/1.8/mysql/patch/items.sql
+++
@@ -1,8 +0,0 @@
-alter table items drop nextcheck;
-alter table items add data_type  integer     DEFAULT '0' NOT NULL;
-alter table items add authtype   integer     DEFAULT '0' NOT NULL;
-alter table items add username   varchar(64) DEFAULT ''  NOT NULL;
-alter table items add password   varchar(64) DEFAULT ''  NOT NULL;
-alter table items add publickey  varchar(64) DEFAULT ''  NOT NULL;
-alter table items add privatekey varchar(64) DEFAULT ''  NOT NULL;
-alter table items add mtime      integer     DEFAULT '0' NOT NULL;
