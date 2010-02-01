85451728a4638f579338e11e449a38056463e2f7
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-02-01 11:08:04 +0000

    ZBX-1837
    git-svn-id: svn://svn.zabbix.com/branches/dev@9778 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-1837-translations/upgrades/dbpatches/1.6/oracle/patch/operations.sql
@@ -0,0 +1,5 @@
+alter table operations add esc_period		number(10)	DEFAULT '0'	NOT NULL;
+alter table operations add esc_step_from	number(10)	DEFAULT '0'	NOT NULL;
+alter table operations add esc_step_to		number(10)	DEFAULT '0'	NOT NULL;
+alter table operations add default_msg		number(10)	DEFAULT '0'	NOT NULL;
+alter table operations add evaltype		number(10)	DEFAULT '0'	NOT NULL;
