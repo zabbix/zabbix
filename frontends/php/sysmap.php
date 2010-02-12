074193ad14c9bc1875a006abc06129a5f3956d2b
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-02-12 07:52:37 +0000

    - [ZBX-1940] additional fix if PHP output buffer not set (Aly)
    git-svn-id: svn://svn.zabbix.com/branches/1.8@10108 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/sysmap.php
+++ frontends/php/sysmap.php
@@ -85,7 +85,7 @@ include_once('include/page_header.php');
 <?php
 // ACTION /////////////////////////////////////////////////////////////////////////////
 	if(isset($_REQUEST['favobj'])){
-		@ob_flush();
+
 		$json = new CJSON();
 		if('sysmap' == $_REQUEST['favobj']){
 			$sysmapid = get_request('sysmapid',0);
