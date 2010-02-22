af2678b3b30a319df0f5b274dc3ca0de3b6e5cd4
dmitry <dmitry@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-02-22 15:30:52 +0000

    The second implementation commit. Dealt with "zbx_wsnprintf". (Dmitry)
    git-svn-id: svn://svn.zabbix.com/branches/dev@10334 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-2016-memory_corruption/src/zabbix_agent/cpustat.c
+++ ZBX-2016-memory_corruption/src/zabbix_agent/cpustat.c
@@ -71,7 +71,7 @@ int	init_cpu_collector(ZBX_CPUS_STAT_DATA *pcpus)
 
 	for(i = 0 /* 0 : _Total; >0 : cpu */; i <= pcpus->count; i++) {
 		if (i == 0)
-			zbx_wsnprintf(cpu, sizeof(cpu), TEXT("_Total"));
+			zbx_wsnprintf(cpu, sizeof(cpu)/sizeof(TCHAR), TEXT("_Total"));
 		else
 			_itow_s(i - 1, cpu, sizeof(cpu)/sizeof(TCHAR), 10);
 
