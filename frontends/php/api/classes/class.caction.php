77c214bd08345d3df5819c45ae879146b511a4c6
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-03-15 08:47:03 +0000

    - [ZBX-2162] fixed error when create action (Vedmak)  - merged: branches/1.8 10793
    git-svn-id: svn://svn.zabbix.com/trunk@10794 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/api/classes/class.caction.php
+++ frontends/php/api/classes/class.caction.php
@@ -850,7 +850,7 @@ COpt::memoryPick();
 				$opmediatypeid = get_dbid('opmediatypes', 'opmediatypeid');
 
 				$result &= (bool) DBexecute('INSERT INTO opmediatypes (opmediatypeid,operationid,mediatypeid)'.
-					' VALUES ('.$opmediatypeid.','.$operationid.','.$mediatypeid.')');
+					' VALUES ('.$opmediatypeid.','.$operationid.','.$operation['mediatypeid'].')');
 			}
 
 			if(!$result) break;
