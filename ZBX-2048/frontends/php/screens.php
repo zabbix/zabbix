f295455e82808c99b7e06cd4df70e3fb5bbfc890
maxim <maxim@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-03-24 14:47:09 +0000

    Fixed bug with screens combobox sorting at Monitoring->Screens->Screens combobox on the right
    git-svn-id: svn://svn.zabbix.com/branches/dev@11087 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-2048/frontends/php/screens.php
+++ ZBX-2048/frontends/php/screens.php
@@ -72,9 +72,6 @@
 			CProfile::update('web.screens.hats.'.$_REQUEST['favid'].'.state',$_REQUEST['state'], PROFILE_TYPE_INT);
 		}
 
-		if('filter' == $_REQUEST['favobj']){
-			CProfile::update('web.screens.filter.state',$_REQUEST['state'], PROFILE_TYPE_INT);
-		}
 		if('timeline' == $_REQUEST['favobj']){
 			if(isset($_REQUEST['elementid']) && isset($_REQUEST['period'])){
 				navigation_bar_calc('web.screens', true);
@@ -121,10 +118,7 @@
 <?php
 
 	$screens_wdgt = new CWidget();
-	
-	$scroll_div = new CDiv();
-	$scroll_div->setAttribute('id','scrollbar_cntr');
-	$screens_wdgt->addFlicker($scroll_div, CProfile::get('web.screens.filter.state',1));
+
 	
 	$formHeader = new CForm();
 	$cmbConfig = new CComboBox('config', 'screens.php', 'javascript: redirect(this.options[this.selectedIndex].value);');
@@ -135,7 +129,8 @@
 	
 	$screens = CScreen::get(array(
 		'nodeids' => get_current_nodeid(),
-		'extendoutput' => 1
+		'extendoutput' => 1,
+      'sortfield' => 'name'
 	));
 	$screens = zbx_toHash($screens, 'screenid');
 	order_result($screens, 'name');
@@ -254,6 +249,11 @@
 
 		$screens_wdgt->addItem($element);
 		$screens_wdgt->show();
+
+		$scroll_div = new CDiv();
+		$scroll_div->setAttribute('id','scrollbar_cntr');
+		$scroll_div->setAttribute('style','border: 0px #CC0000 solid; height: 25px; width: 800px;');
+		$scroll_div->show();
 
 		$jsmenu = new CPUMenu(null,170);
 		$jsmenu->InsertJavaScript();
