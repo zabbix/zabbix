977416aaebf8fc5b1907dd6e39b02b38c8cfcc3a
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-03-26 14:44:43 +0000

    - new branch created for issue NTP-162 (The condition of the action is abnormal. )
    git-svn-id: svn://svn.zabbix.com/branches/dev@11142 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ntp-162/include/ipc.h
@@ -0,0 +1,30 @@
+/*
+** ZABBIX
+** Copyright (C) 2000-2009 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+
+#ifndef ZABBIX_IPC_H
+#define ZABBIX_IPC_H
+
+#if defined(_WINDOWS)
+#	error "This module allowed only for Unix OS"
+#endif /* _WINDOWS */
+
+key_t zbx_ftok(char *path, int id);
+int zbx_shmget(key_t key, size_t size);
+
+#endif
