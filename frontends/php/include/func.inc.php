b08be2071895dc6cbccf705aac4d971cf86c1249
maxim <maxim@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-03-30 12:46:00 +0000

    - [ZBX-2232] fixed bug with last event duration time in event details (AM)
    git-svn-id: svn://svn.zabbix.com/branches/1.8@11232 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/func.inc.php
+++ frontends/php/include/func.inc.php
@@ -372,8 +372,8 @@ function zbx_date2age($start_date,$end_date=0,$utime = false){
 			(($hours && !$years && !$months)?$hours.S_HOUR_SHORT.' ':'').
 			(($minutes && !$years && !$months && !$weeks)?$minutes.S_MINUTE_SHORT.' ':'').
 			((!$years && !$months && !$weeks && !$days && ($ms || $seconds))?$seconds.S_SECOND_SHORT.' ':'').
-			(($ms && !$years && !$months && !$weeks && !$days && !$hours)?$ms.S_MILLISECOND_SHORT:'').
-         (!$ms && $original_time < 0.001 ? '< 1'.S_MILLISECOND_SHORT:'');
+			(($ms && !$years && !$months && !$weeks && !$days && !$hours) || $original_time == 0 ?$ms.S_MILLISECOND_SHORT:'').
+			(!$ms && $original_time > 0 && $original_time < 0.001 ? '< 1'.S_MILLISECOND_SHORT:'');
 
 return trim($str,' ');
 }
