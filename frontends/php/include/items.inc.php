4b99ac4a3cb07d4ecee1e08989702b96bb50420f
maxim <maxim@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2010-03-31 07:26:57 +0000

    - [ZBX-2246] added trigger wizard for logrt item (AM)
    git-svn-id: svn://svn.zabbix.com/trunk@11254 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/items.inc.php
+++ frontends/php/include/items.inc.php
@@ -329,7 +329,7 @@
 			return FALSE;
 		}
 
-		if(preg_match('/^log\[|eventlog\[/', $item['key_']) && ($item['value_type'] != ITEM_VALUE_TYPE_LOG)){
+		if(preg_match('/^(log|logrt|eventlog)\[/', $item['key_']) && ($item['value_type'] != ITEM_VALUE_TYPE_LOG)){
 			error(S_TYPE_INFORMATION_BUST_LOG_FOR_LOG_KEY);
 			return FALSE;
 		}
@@ -561,7 +561,7 @@
 			$item['delta']=0;
 		}
 
-		if(preg_match('/^log\[|eventlog\[/', $item['key_']) && ($item['value_type'] != ITEM_VALUE_TYPE_LOG)){
+		if(preg_match('/^(log|logrt|eventlog)\[/', $item['key_']) && ($item['value_type'] != ITEM_VALUE_TYPE_LOG)){
 			error(S_TYPE_INFORMATION_BUST_LOG_FOR_LOG_KEY);
 			return FALSE;
 		}
