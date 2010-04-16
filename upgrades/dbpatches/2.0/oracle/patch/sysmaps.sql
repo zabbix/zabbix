bb1d799bd560dec24ed8be8cc9aae7bd85ad42d0
artem <artem@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2010-04-16 11:07:33 +0000

    - [DEV-458] fixes in DB patches related to new map attribute (Aly)
    git-svn-id: svn://svn.zabbix.com/trunk@11567 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/2.0/oracle/patch/sysmaps.sql
+++ upgrades/dbpatches/2.0/oracle/patch/sysmaps.sql
@@ -2,6 +2,6 @@ ALTER TABLE sysmaps ADD expandproblem number(10) DEFAULT '1' NOT NULL;
 ALTER TABLE sysmaps ADD markelements number(10) DEFAULT '0' NOT NULL;
 
 UPDATE sysmaps SET expandproblem=0 WHERE highlight>1 AND highlight<4;
-UPDATE sysmaps SET markelements=1 WHERE highlight>4;
-UPDATE sysmaps SET highlight=(highlight-4) WHERE highlight>4;
+UPDATE sysmaps SET markelements=1 WHERE highlight>3;
+UPDATE sysmaps SET highlight=(highlight-4) WHERE highlight>3;
 UPDATE sysmaps SET highlight=(highlight-2) WHERE highlight>1;
