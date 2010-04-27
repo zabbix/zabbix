37cd03fc0c8cd5c74c653b1a2e20e17c7d7e2276
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-04-27 13:45:02 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@11725 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-1505-item_macro/frontends/php/api/classes/class.cusermacro.php
+++ ZBX-1505-item_macro/frontends/php/api/classes/class.cusermacro.php
@@ -1188,7 +1188,7 @@ class CUserMacro extends CZBXAPI{
 			if($res = preg_match_all('/'.ZBX_PREG_EXPRESSION_USER_MACROS.'/', $item['key_'], $arr)){
 				$macros = self::getMacros($arr[1], array('itemid' => $item['itemid']));
 
-				$search = zbx_objectValues($macros, 'macroid');
+				$search = array_keys($macros);
 				$values = array_values($macros);
 				$items[$num]['key_'] = str_replace($search, $values, $item['key_']);
 			}
