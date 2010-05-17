e754eba90b246941712808b6333a0035b3bc6023
artem <artem@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2010-05-17 12:45:25 +0000

    - [ZBX-1591] merged rev. 12028-12029 of /branches/1.8 (Aly)
    git-svn-id: svn://svn.zabbix.com/trunk@12030 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/config.inc.php
+++ frontends/php/include/config.inc.php
@@ -17,7 +17,7 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 **/
-	require_once('include/debug.inc.php');
+require_once('include/debug.inc.php');
 	
 function __autoload($class_name){
 	$class_name = zbx_strtolower($class_name);
