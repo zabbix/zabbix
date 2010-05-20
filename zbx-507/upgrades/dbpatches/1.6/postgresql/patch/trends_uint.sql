9d3d1d80e5f8ca038122bccb4f6e55e4df116aa0
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-05-20 12:16:49 +0000

    Deleted branch dev/zbx-507.
    git-svn-id: svn://svn.zabbix.com/branches/dev@12102 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- zbx-507/upgrades/dbpatches/1.6/postgresql/patch/trends_uint.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE trends_uint (
-        itemid          bigint          DEFAULT '0'     NOT NULL,
-        clock           integer         DEFAULT '0'     NOT NULL,
-        num             integer         DEFAULT '0'     NOT NULL,
-        value_min               bigint          DEFAULT '0'     NOT NULL,
-        value_avg               bigint          DEFAULT '0'     NOT NULL,
-        value_max               bigint          DEFAULT '0'     NOT NULL,
-        PRIMARY KEY (itemid,clock)
-) with OIDS;
--- zbx-507/upgrades/dbpatches/1.8/mysql/patch/hosts.sql
+++
@@ -1,9 +0,0 @@
-alter table hosts add maintenanceid bigint unsigned DEFAULT '0' NOT NULL;
-alter table hosts add maintenance_status integer DEFAULT '0' NOT NULL;
-alter table hosts add maintenance_type integer DEFAULT '0' NOT NULL;
-alter table hosts add maintenance_from integer DEFAULT '0' NOT NULL;
-alter table hosts add ipmi_ip varchar(64) DEFAULT '127.0.0.1' NOT NULL;
-alter table hosts add ipmi_errors_from integer DEFAULT '0' NOT NULL;
-alter table hosts add snmp_errors_from integer DEFAULT '0' NOT NULL;
-alter table hosts add ipmi_error varchar(128) DEFAULT '' NOT NULL;
-alter table hosts add snmp_error varchar(128) DEFAULT '' NOT NULL;
