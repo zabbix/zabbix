8ce8537fec43cae7852af15224bda4c7f33d526d
vedmak <vedmak@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2010-05-31 06:33:20 +0000

    - [ZBX-1529] fixed macros expand to nothing in templates (Vedmak)  - merged: branches/1.8 12367
    git-svn-id: svn://svn.zabbix.com/trunk@12368 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/triggers.php
+++ frontends/php/triggers.php
@@ -555,6 +555,7 @@ include_once('include/page_header.php');
 				'select_items' => API_OUTPUT_EXTEND,
 				'select_functions' => API_OUTPUT_EXTEND,
 				'select_dependencies' => API_OUTPUT_EXTEND,
+				'expandDescription' => 1,
 			);
 			$triggers = CTrigger::get($options);
 			order_result($triggers, $sortfield, $sortorder);
@@ -582,7 +583,7 @@ include_once('include/page_header.php');
 					}
 				}
 
-				$description[] = new CLink(expandTriggerDescription($trigger), 'triggers.php?form=update&triggerid='.$triggerid);
+			$description[] = new CLink($trigger['description'], 'triggers.php?form=update&triggerid='.$triggerid);
 
 		//add dependencies {
 				$deps = $trigger['dependencies'];
