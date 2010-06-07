a38d9a0cdd3d318cc9bd50e32170d2e58882e27e
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-06-07 08:17:37 +0000

    - [ZBX-2074] fixed wed scenarios showing incorrect speed (Vedmak)  - merged: branches/1.8 12583
    git-svn-id: svn://svn.zabbix.com/trunk@12585 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/2.0/mysql/patch/items.sql
+++ upgrades/dbpatches/2.0/mysql/patch/items.sql
@@ -1,1 +1,3 @@
 ALTER TABLE items CHANGE units units VARCHAR(255) DEFAULT '' NOT NULL;
+
+UPDATE items SET units='Bps' WHERE type=9 AND units='bps';
--- upgrades/dbpatches/2.0/oracle/patch/items.sql
+++ upgrades/dbpatches/2.0/oracle/patch/items.sql
@@ -1,1 +1,3 @@
 alter table items modify units           nvarchar2(255);
+
+UPDATE items SET units='Bps' WHERE type=9 AND units='bps';
