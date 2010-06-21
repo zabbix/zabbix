7e1b4234a7fbb81d0f34e183e8cd634b2fbbc4a3
maxim <maxim@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-06-21 12:51:05 +0000

    - [ZBX-2595] fixed logformat field showing (Maxim Andruhovich / AM)
    git-svn-id: svn://svn.zabbix.com/branches/dev@13001 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-2595/frontends/php/include/items.inc.php
+++ ZBX-2595/frontends/php/include/items.inc.php
@@ -317,7 +317,7 @@
 			error(S_INCORRECT_KEY_FORMAT.SPACE."'key_name[param1,param2,...]'");
 			return false;
 		}
-		
+
 		if(($item['type'] == ITEM_TYPE_DB_MONITOR && $item['key_'] == 'db.odbc.select[<unique short description>]') ||
 		   ($item['type'] == ITEM_TYPE_SSH && $item['key_'] == 'ssh.run[<unique short description>,<ip>,<port>,<encoding>]') ||
 		   ($item['type'] == ITEM_TYPE_TELNET && $item['key_'] == 'telnet.run[<unique short description>,<ip>,<port>,<encoding>]')) {
@@ -382,12 +382,12 @@
 			}
 		}
 
-		$sql = 'SELECT itemid,hostid '.
+		$sql = 'SELECT itemid, hostid, templateid '.
 				' FROM items '.
 				' WHERE hostid='.$item['hostid'].
 					' AND key_='.zbx_dbstr($item['key_']);
 		$db_item = DBfetch(DBselect($sql));
-		if($db_item && $item['templateid'] == 0){
+		if($db_item && (($item['templateid'] == 0) || ($item['templateid'] != $db_item['templateid']))){
 			error(S_AN_ITEM_WITH_THE_KEY.SPACE.'['.$item['key_'].']'.SPACE.S_ALREADY_EXISTS_FOR_HOST_SMALL.SPACE.'['.$host['host'].'].'.SPACE.S_THE_KEY_MUST_BE_UNIQUE);
 			return FALSE;
 		}
@@ -530,7 +530,7 @@
 			error(S_INCORRECT_KEY_FORMAT.SPACE."'key_name[param1,param2,...]'");
 			return false;
 		}
-		
+
 		if(($item['type'] == ITEM_TYPE_DB_MONITOR && $item['key_'] == 'db.odbc.select[<unique short description>]') ||
 		   ($item['type'] == ITEM_TYPE_SSH && $item['key_'] == 'ssh.run[<unique short description>,<ip>,<port>,<encoding>]') ||
 		   ($item['type'] == ITEM_TYPE_TELNET && $item['key_'] == 'telnet.run[<unique short description>,<ip>,<port>,<encoding>]')) {
