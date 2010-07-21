f3affd32423625c42496393bdf4df825798bd996
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-07-21 08:49:33 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@13428 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-2022-picker_IE7/upgrades/dbpatches/1.8/oracle/patch/proxy_autoreg_host.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE proxy_autoreg_host (
-        id              number(20)                      NOT NULL,
-        clock           number(10)              DEFAULT '0'     NOT NULL,
-        host            nvarchar2(64)           DEFAULT ''      ,
-        PRIMARY KEY (id)
-);
-CREATE INDEX proxy_autoreg_host_1 on proxy_autoreg_host (clock);
-
