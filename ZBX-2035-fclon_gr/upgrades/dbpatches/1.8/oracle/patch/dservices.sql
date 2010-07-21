7fc1b48b75a2c31a5e72b3be1ad4252566fd42b5
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-07-21 08:51:14 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@13432 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-2035-fclon_gr/upgrades/dbpatches/1.8/oracle/patch/dservices.sql
+++
@@ -1,9 +0,0 @@
--- See also dhosts.sql
-
-CREATE UNIQUE INDEX dservices_1 on dservices (dcheckid,type,key_,ip,port);
-CREATE INDEX dservices_2 on dservices (dhostid);
-
-alter table dservices modify key_            nvarchar2(255)          DEFAULT '0';
-alter table dservices modify value           nvarchar2(255)          DEFAULT '0';
-alter table dservices modify ip              nvarchar2(39)           DEFAULT '';
-
