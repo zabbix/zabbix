29ea7ea7e7db662c1296e1e577a9e111d4b9c7bd
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-07-21 08:33:41 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@13405 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-1780-latest_to_history/upgrades/dbpatches/1.8/oracle/patch/proxy_dhistory.sql
+++
@@ -1,6 +0,0 @@
-alter table proxy_dhistory modify key_            nvarchar2(255)            DEFAULT ''      NOT NULL;
-alter table proxy_dhistory modify value           nvarchar2(255)            DEFAULT ''      NOT NULL;
-
-alter table proxy_dhistory add         dcheckid                number(20)         DEFAULT '0'     NOT NULL;
-
-alter table proxy_dhistory modify ip            nvarchar2(39)            DEFAULT ''      NOT NULL;
