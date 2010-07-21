061428cc62056000877e18671902b13e07d0f9ff
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-07-21 08:56:29 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@13443 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-2117-trig_qoute_select/upgrades/dbpatches/1.6/postgresql/patch/scripts.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE scripts (
-        scriptid                bigint          DEFAULT '0'     NOT NULL,
-        name            varchar(255)            DEFAULT ''      NOT NULL,
-        command         varchar(255)            DEFAULT ''      NOT NULL,
-        host_access             integer         DEFAULT '2'     NOT NULL,
-        usrgrpid                bigint          DEFAULT '0'     NOT NULL,
-        groupid         bigint          DEFAULT '0'     NOT NULL,
-        PRIMARY KEY (scriptid)
-) with OIDS;
