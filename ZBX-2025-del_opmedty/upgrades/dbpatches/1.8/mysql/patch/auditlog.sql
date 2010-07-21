4df6871faea0c2719135ce5757b4fca836a4cd3c
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-07-21 08:49:54 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@13429 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-2025-del_opmedty/upgrades/dbpatches/1.8/mysql/patch/auditlog.sql
+++
@@ -1,3 +0,0 @@
-alter table auditlog add ip              varchar(39)             DEFAULT ''      NOT NULL;
-alter table auditlog add resourceid              bigint unsigned         DEFAULT '0'     NOT NULL;
-alter table auditlog add resourcename            varchar(255)            DEFAULT ''      NOT NULL;
