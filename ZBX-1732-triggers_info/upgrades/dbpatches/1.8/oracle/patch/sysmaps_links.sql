b1e711fc19d2ea24742da43e16897b8d007ac001
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-07-21 08:17:41 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@13404 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-1732-triggers_info/upgrades/dbpatches/1.8/oracle/patch/sysmaps_links.sql
+++
@@ -1,3 +0,0 @@
-alter table sysmaps_links  add label           nvarchar2(255)            DEFAULT ''      NOT NULL;
-
-alter table sysmaps_links modify color           nvarchar2(6)            DEFAULT '000000';
