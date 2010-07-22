1b55d263365ba25ed6810133a9c0150600b15bc1
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-07-22 12:11:53 +0000

    - [ZBX-2295] added rounding for latest values (Aly)
    git-svn-id: svn://svn.zabbix.com/branches/dev@13523 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- dev-465/frontends/php/include/func.inc.php
+++ dev-465/frontends/php/include/func.inc.php
@@ -460,7 +460,9 @@ function convert_units($value, $units, $convert=ITEM_CONVERT_WITH_UNITS){
 	$blackList = array('%','ms','rpm');
 
 	if(in_array(strtolower($units), $blackList) || (zbx_empty($units) && (($convert == ITEM_CONVERT_WITH_UNITS) || ($value < 1)))){
-		$value = sprintf('%.6f', $value);
+		$value = round($value, 2);
+		$value = sprintf('%.4f', $value);
+		
 
 		$value = preg_replace('/^([\-0-9]+)(\.)([0-9]*)[0]+$/U','$1$2$3', $value);
 		$value = rtrim($value, '.');
