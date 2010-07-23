2f39d121e451a798ebf8e02c981183098ff6bb9b
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-07-23 12:59:11 +0000

    - changed db schema
    git-svn-id: svn://svn.zabbix.com/branches/dev@13567 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- zbxnext-457/upgrades/dbpatches/2.0/mysql/patch/items.sql
+++ zbxnext-457/upgrades/dbpatches/2.0/mysql/patch/items.sql
@@ -1,3 +1,4 @@
-ALTER TABLE items CHANGE units units VARCHAR(255) DEFAULT '' NOT NULL;
+ALTER TABLE items MODIFY units VARCHAR(255) DEFAULT '' NOT NULL;
+ALTER TABLE items ADD lastns integer NULL;
 
 UPDATE items SET units='Bps' WHERE type=9 AND units='bps';
---
+++ zbxnext-457/upgrades/dbpatches/2.0/mysql/patch/triggers.sql
@@ -0,0 +1,1 @@
+ALTER TABLE triggers ADD ns integer DEFAULT '0' NOT NULL;
