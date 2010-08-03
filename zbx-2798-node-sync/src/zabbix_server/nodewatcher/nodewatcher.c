e1c410120b5b5eae0e5beef21298b20717d233bd
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-08-03 07:49:46 +0000

    We are now more strict with regard to where the DM data comes from.
    git-svn-id: svn://svn.zabbix.com/branches/dev@13787 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- zbx-2798-node-sync/src/zabbix_server/nodewatcher/nodewatcher.c
+++ zbx-2798-node-sync/src/zabbix_server/nodewatcher/nodewatcher.c
@@ -35,33 +35,113 @@
  *                                                                            *
  * Parameters:                                                                *
  *                                                                            *
- * Return value:  SUCCEED - nodeid is master node                             *
- *                FAIL - nodeid is slave node                                 *
+ * Return value:  SUCCEED - master_nodeid is a master node of current_nodeid  *
+ *                FAIL - otherwise                                            *
  *                                                                            *
  * Author: Aleksander Vladishev                                               *
  *                                                                            *
  * Comments:                                                                  *
  *                                                                            *
  ******************************************************************************/
-int	is_master_node(int current_nodeid, int nodeid)
+int	is_master_node(int current_nodeid, int master_nodeid)
 {
-	DB_RESULT	dbresult;
-	DB_ROW		dbrow;
+	DB_RESULT	result;
+	DB_ROW		row;
 	int		res = FAIL;
 
-	dbresult = DBselect("select masterid from nodes where nodeid=%d",
+	result = DBselect("select masterid from nodes where nodeid=%d",
 		current_nodeid);
 
-	if (NULL != (dbrow = DBfetch(dbresult))) {
-		current_nodeid = atoi(dbrow[0]);
-		if (current_nodeid == nodeid)
+	if (NULL != (row = DBfetch(result)))
+	{
+		current_nodeid = atoi(row[0]);
+		if (current_nodeid == master_nodeid)
 			res = SUCCEED;
 		else if (0 != current_nodeid)
-			res = is_master_node(current_nodeid, nodeid);
+			res = is_master_node(current_nodeid, master_nodeid);
 	}
-	DBfree_result(dbresult);
+	DBfree_result(result);
 
 	return res;
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Function: is_slave_node                                                    *
+ *                                                                            *
+ * Purpose:                                                                   *
+ *                                                                            *
+ * Parameters:                                                                *
+ *                                                                            *
+ * Return value:  SUCCEED - slave_nodeid is a slave node of current_nodeid    *
+ *                FAIL - otherwise                                            *
+ *                                                                            *
+ * Author: Aleksandrs Saveljevs                                               *
+ *                                                                            *
+ * Comments:                                                                  *
+ *                                                                            *
+ ******************************************************************************/
+int	is_slave_node(int current_nodeid, int slave_nodeid)
+{
+	DB_RESULT	result;
+	DB_ROW		row;
+	int		nodeid;
+	int		ret = FAIL;
+
+	result = DBselect(
+			"select nodeid"
+			" from nodes"
+			" where masterid=%d",
+			current_nodeid);
+
+	while (FAIL == ret && NULL != (row = DBfetch(result)))
+	{
+		nodeid = atoi(row[0]);
+		if (nodeid == slave_nodeid)
+			ret = SUCCEED;
+		else
+			ret = is_slave_node(nodeid, slave_nodeid);
+	}
+	DBfree_result(result);
+
+	return ret;
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Function: is_direct_slave_node                                             *
+ *                                                                            *
+ * Purpose:                                                                   *
+ *                                                                            *
+ * Parameters:                                                                *
+ *                                                                            *
+ * Return value:  SUCCEED - slave_nodeid is our direct slave node             *
+ *                FAIL - otherwise                                            *
+ *                                                                            *
+ * Author: Aleksander Vladishev                                               *
+ *                                                                            *
+ * Comments:                                                                  *
+ *                                                                            *
+ ******************************************************************************/
+int	is_direct_slave_node(int slave_nodeid)
+{
+	DB_RESULT	result;
+	DB_ROW		row;
+	int		ret = FAIL;
+
+	result = DBselect(
+			"select nodeid"
+			" from nodes"
+			" where nodeid=%d"
+				" and masterid=%d",
+			slave_nodeid,
+			CONFIG_NODEID);
+
+	if (NULL != (row = DBfetch(result)))
+		ret = SUCCEED;
+	DBfree_result(result);
+
+	return ret;
 }
 
 /******************************************************************************
@@ -81,10 +161,10 @@ int	is_master_node(int current_nodeid, int nodeid)
  ******************************************************************************/
 int main_nodewatcher_loop()
 {
-	int start, end;
+	int	start, end;
 	int	lastrun = 0;
 
-	zabbix_log(LOG_LEVEL_DEBUG, "In main_nodeupdater_loop()");
+	zabbix_log(LOG_LEVEL_DEBUG, "In main_nodewatcher_loop()");
 
 	zbx_setproctitle("connecting to the database");
 
