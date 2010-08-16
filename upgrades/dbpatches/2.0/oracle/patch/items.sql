9b8e870863281546ebae80897adab2fb5fca38f7
Rihards Olups <git-no-reply@zabbix.com>
2010-08-16 08:59:33 +0000

    - [ZBXNEXT-457] fix DOS style newlines

--- upgrades/dbpatches/2.0/oracle/patch/items.sql
+++ upgrades/dbpatches/2.0/oracle/patch/items.sql
@@ -1,4 +1,4 @@
-alter table items modify units           nvarchar2(255);
+alter table items modify units nvarchar2(255);
 ALTER TABLE items ADD lastns number(10) NULL;
 
 UPDATE items SET units='Bps' WHERE type=9 AND units='bps';
