3c01164a4aec53e78f33343e9fa9e8f7cf5e2373
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2010-08-18 14:09:14 +0000

    - database schema

--- upgrades/dbpatches/2.0/mysql/patch/escalations.sql
+++ upgrades/dbpatches/2.0/mysql/patch/escalations.sql
@@ -2,3 +2,5 @@ ALTER TABLE escalations MODIFY actionid bigint unsigned NOT NULL;
 ALTER TABLE escalations MODIFY triggerid bigint unsigned NULL;
 ALTER TABLE escalations MODIFY eventid bigint unsigned NOT NULL;
 ALTER TABLE escalations MODIFY r_eventid bigint unsigned NULL;
+UPDATE escalations SET triggerid=NULL WHERE triggerid=0;
+UPDATE escalations SET r_eventid=NULL WHERE r_eventid=0;
--- upgrades/dbpatches/2.0/postgresql/patch/escalations.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/escalations.sql
@@ -4,3 +4,5 @@ ALTER TABLE ONLY escalations ALTER triggerid DROP NOT NULL;
 ALTER TABLE ONLY escalations ALTER eventid DROP DEFAULT;
 ALTER TABLE ONLY escalations ALTER r_eventid DROP DEFAULT;
 ALTER TABLE ONLY escalations ALTER r_eventid DROP NOT NULL;
+UPDATE escalations SET triggerid=NULL WHERE triggerid=0;
+UPDATE escalations SET r_eventid=NULL WHERE r_eventid=0;
