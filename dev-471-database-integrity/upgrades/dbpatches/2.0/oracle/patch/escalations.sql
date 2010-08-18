b236c851ad6618dfaec02ea330004c01b373ad56
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-08-18 14:09:14 +0000

    - database schema
    git-svn-id: svn://svn.zabbix.com/branches/dev@13961 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- dev-471-database-integrity/upgrades/dbpatches/2.0/oracle/patch/escalations.sql
+++ dev-471-database-integrity/upgrades/dbpatches/2.0/oracle/patch/escalations.sql
@@ -4,3 +4,5 @@ ALTER TABLE escalations MODIFY triggerid NULL;
 ALTER TABLE escalations MODIFY eventid DEFAULT NULL;
 ALTER TABLE escalations MODIFY r_eventid DEFAULT NULL;
 ALTER TABLE escalations MODIFY r_eventid NULL;
+UPDATE escalations SET triggerid=NULL WHERE triggerid=0;
+UPDATE escalations SET r_eventid=NULL WHERE r_eventid=0;
