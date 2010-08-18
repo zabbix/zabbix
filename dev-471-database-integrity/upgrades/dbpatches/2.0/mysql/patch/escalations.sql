1720b84dfeec40d121d2fb51f993076f26101e44
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-08-18 14:03:35 +0000

    - database schema
    git-svn-id: svn://svn.zabbix.com/branches/dev@13960 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- dev-471-database-integrity/upgrades/dbpatches/2.0/mysql/patch/escalations.sql
+++ dev-471-database-integrity/upgrades/dbpatches/2.0/mysql/patch/escalations.sql
@@ -1,4 +1,4 @@
 ALTER TABLE escalations MODIFY actionid bigint unsigned NOT NULL;
-ALTER TABLE escalations MODIFY triggerid bigint unsigned NOT NULL;
+ALTER TABLE escalations MODIFY triggerid bigint unsigned NULL;
 ALTER TABLE escalations MODIFY eventid bigint unsigned NOT NULL;
 ALTER TABLE escalations MODIFY r_eventid bigint unsigned NULL;
--- dev-471-database-integrity/upgrades/dbpatches/2.0/oracle/patch/escalations.sql
+++ dev-471-database-integrity/upgrades/dbpatches/2.0/oracle/patch/escalations.sql
@@ -1,5 +1,6 @@
 ALTER TABLE escalations MODIFY actionid DEFAULT NULL;
 ALTER TABLE escalations MODIFY triggerid DEFAULT NULL;
+ALTER TABLE escalations MODIFY triggerid NULL;
 ALTER TABLE escalations MODIFY eventid DEFAULT NULL;
 ALTER TABLE escalations MODIFY r_eventid DEFAULT NULL;
 ALTER TABLE escalations MODIFY r_eventid NULL;
