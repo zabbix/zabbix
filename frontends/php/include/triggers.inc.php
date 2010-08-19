0c03e60d6798fa9c17b344d9a9ca9f968dc80951
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-08-19 07:43:41 +0000

    - [ZBX-2890] fixed parsing of negative values in trigger expressions (Vedmak)  - merged: branches/1.8 13971
    git-svn-id: svn://svn.zabbix.com/trunk@13972 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/triggers.inc.php
+++ frontends/php/include/triggers.inc.php
@@ -4138,7 +4138,8 @@ return $caption;
 $triggerExpressionRules['independent'] = Array(
 	'allowedSymbols' => "[0-9KMGTsmhdw. \/*+<>#=&|\-]+",
 	'notAllowedSymbols' => Array(
-					"[.\/*+<>#=&|\-]{2,}",
+					"[.\/*+<>#&|\-]{2,}",
+					"={2,}",
 					"[ .KMGTsmhdw]{2,}",
 					"(^\.|\.$)",
 					"\.\d+\.",
