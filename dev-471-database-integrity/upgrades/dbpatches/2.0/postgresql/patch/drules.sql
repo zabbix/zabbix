279c53d2c3ee8dc31d2b2c0160c2c9ae835cb0bc
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-08-20 12:22:07 +0000

    - database schema (`drules`.`unique_dcheckid` field is replaced by `druleuniq` table)
    git-svn-id: svn://svn.zabbix.com/branches/dev@13992 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- dev-471-database-integrity/upgrades/dbpatches/2.0/postgresql/patch/drules.sql
+++ dev-471-database-integrity/upgrades/dbpatches/2.0/postgresql/patch/drules.sql
@@ -1,9 +1,19 @@
+CREATE TABLE druleuniq (
+	druleuniqid              bigint                                    	NOT NULL,
+	druleid                  bigint                                    	NOT NULL,
+	dcheckid                 bigint                                    	NOT NULL,
+	PRIMARY KEY (druleuniqid)
+) with OIDS;
+CREATE UNIQUE INDEX druleuniq_1 on druleuniq (druleid);
+ALTER TABLE ONLY druleuniq ADD CONSTRAINT c_druleuniq_1 FOREIGN KEY (druleid) REFERENCES drules (druleid) ON DELETE CASCADE;
+ALTER TABLE ONLY druleuniq ADD CONSTRAINT c_druleuniq_2 FOREIGN KEY (dcheckid) REFERENCES dchecks (dcheckid) ON DELETE CASCADE;
+
+INSERT INTO druleuniq (druleuniqid,druleid,dcheckid)
+	SELECT druleid,druleid,unique_dcheckid FROM drules WHERE unique_dcheckid IN (SELECT dcheckid FROM dchecks);
+
 ALTER TABLE ONLY drules ALTER druleid DROP DEFAULT,
 			ALTER proxy_hostid DROP DEFAULT,
 			ALTER proxy_hostid DROP NOT NULL,
-			ALTER unique_dcheckid DROP DEFAULT,
-			ALTER unique_dcheckid DROP NOT NULL;
+			DROP unique_dcheckid;
 UPDATE drules SET proxy_hostid=NULL WHERE NOT proxy_hostid IN (SELECT hostid FROM hosts);
-UPDATE drules SET unique_dcheckid=NULL WHERE NOT unique_dcheckid IN (SELECT dcheckid FROM dchecks);
 ALTER TABLE ONLY drules ADD CONSTRAINT c_drules_1 FOREIGN KEY (proxy_hostid) REFERENCES hosts (hostid);
-ALTER TABLE ONLY drules ADD CONSTRAINT c_drules_2 FOREIGN KEY (unique_dcheckid) REFERENCES dchecks (dcheckid);
