6569039bf31ec6258f3bc968ffe0ecc268f9c446
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-09-06 18:22:08 +0000

    - [DEV-481] new branch created (low-level discovery: Solaris agent)
    git-svn-id: svn://svn.zabbix.com/branches/dev@14328 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ dev-481-low-level-discovery-solaris-agent/upgrades/dbpatches/2.0/mysql/patch/graph_theme.sql
@@ -0,0 +1,2 @@
+ALTER TABLE graph_theme MODIFY graphthemeid bigint unsigned NOT NULL,
+			MODIFY noneworktimecolor varchar(6) DEFAULT 'CCCCCC' NOT NULL;
---
+++ dev-481-low-level-discovery-solaris-agent/upgrades/dbpatches/2.0/postgresql/patch/graph_theme.sql
@@ -0,0 +1,2 @@
+ALTER TABLE ONLY graph_theme ALTER graphthemeid DROP DEFAULT,
+			     ALTER noneworktimecolor SET DEFAULT 'CCCCCC';
