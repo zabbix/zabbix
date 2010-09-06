0e1e994d12d110400bee5409e342a6f8a92dcb90
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-09-06 11:11:56 +0000

    - [DEV-471] branch deletd
    git-svn-id: svn://svn.zabbix.com/branches/dev@14314 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- dev-471-dm-crach/upgrades/dbpatches/2.0/mysql/patch/opconditions.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE opconditions MODIFY opconditionid bigint unsigned NOT NULL,
-			 MODIFY operationid bigint unsigned NOT NULL;
-DELETE FROM opconditions WHERE NOT operationid IN (SELECT operationid FROM operations);
-ALTER TABLE opconditions ADD CONSTRAINT c_opconditions_1 FOREIGN KEY (operationid) REFERENCES operations (operationid) ON DELETE CASCADE;
