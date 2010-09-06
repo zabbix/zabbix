6e20697dc79605fc020d78b26bdd8004f4051ae1
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-09-06 11:38:07 +0000

    - [DEV-471] fixed 2.0 upgrade patches for already patched databases from 1.6.x to 1.8.x
    git-svn-id: svn://svn.zabbix.com/trunk@14316 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/2.0/mysql/patch/graph_theme.sql
+++ upgrades/dbpatches/2.0/mysql/patch/graph_theme.sql
@@ -1,1 +1,2 @@
-ALTER TABLE graph_theme MODIFY graphthemeid bigint unsigned NOT NULL;
+ALTER TABLE graph_theme MODIFY graphthemeid bigint unsigned NOT NULL,
+			MODIFY noneworktimecolor varchar(6) DEFAULT 'CCCCCC' NOT NULL;
--- upgrades/dbpatches/2.0/postgresql/patch/graph_theme.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/graph_theme.sql
@@ -1,1 +1,2 @@
-ALTER TABLE ONLY graph_theme ALTER graphthemeid DROP DEFAULT;
+ALTER TABLE ONLY graph_theme ALTER graphthemeid DROP DEFAULT,
+			     ALTER noneworktimecolor SET DEFAULT 'CCCCCC';
