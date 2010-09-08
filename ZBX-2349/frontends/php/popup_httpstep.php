4e6a8d2b286a7733554c07f2e7c9bd3a2f450e6a
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-09-08 10:02:27 +0000

    Changed KEY_PARAM() check to allow users to create steps with unicode names. Also, the logic that extracts parameters from name is changed to support unicode.
    git-svn-id: svn://svn.zabbix.com/branches/dev@14364 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-2349/frontends/php/popup_httpstep.php
+++ ZBX-2349/frontends/php/popup_httpstep.php
@@ -19,43 +19,43 @@
 **/
 ?>
 <?php
-	require_once('include/config.inc.php');
-	require_once('include/triggers.inc.php');
-	require_once('include/forms.inc.php');
+    require_once('include/config.inc.php');
+    require_once('include/triggers.inc.php');
+    require_once('include/forms.inc.php');
 
-	$dstfrm		= get_request('dstfrm',		0);	// destination form
+    $dstfrm     = get_request('dstfrm',     0); // destination form
 
-	$page['title'] = "S_STEP_OF_SCENARIO";
-	$page['file'] = 'popup_httpstep.php';
+    $page['title'] = "S_STEP_OF_SCENARIO";
+    $page['file'] = 'popup_httpstep.php';
 
-	define('ZBX_PAGE_NO_MENU', 1);
+    define('ZBX_PAGE_NO_MENU', 1);
 
 include_once('include/page_header.php');
 
 ?>
 <?php
-//		VAR			TYPE	OPTIONAL FLAGS	VALIDATION	EXCEPTION
-	$fields=array(
-		'dstfrm'=>	array(T_ZBX_STR, O_MAND,P_SYS,	NOT_EMPTY,		null),
+//      VAR         TYPE    OPTIONAL FLAGS  VALIDATION  EXCEPTION
+    $fields=array(
+        'dstfrm'=>  array(T_ZBX_STR, O_MAND,P_SYS,  NOT_EMPTY,      null),
 
-		'stepid'=>		array(T_ZBX_INT, O_OPT,  P_SYS,	BETWEEN(0,65535),	null),
-		'list_name'=>	array(T_ZBX_STR, O_OPT,  P_SYS,	NOT_EMPTY,		'isset({save})&&isset({stepid})'),
+        'stepid'=>      array(T_ZBX_INT, O_OPT,  P_SYS, BETWEEN(0,65535),   null),
+        'list_name'=>   array(T_ZBX_STR, O_OPT,  P_SYS, NOT_EMPTY,      'isset({save})&&isset({stepid})'),
 
-		'name'=>	array(T_ZBX_STR, O_OPT,  null,	NOT_EMPTY.KEY_PARAM(),'isset({save})'),
-		'url'=>		array(T_ZBX_STR, O_OPT,  null,	NOT_EMPTY,		'isset({save})'),
-		'posts'=>	array(T_ZBX_STR, O_OPT,  null,	null,			'isset({save})'),
-		'timeout'=>	array(T_ZBX_INT, O_OPT,  null,	BETWEEN(0,65535),	'isset({save})'),
-		'required'=>	array(T_ZBX_STR, O_OPT,  null,	null,			'isset({save})'),
-		'status_codes'=>array(T_ZBX_INT_RANGE, O_OPT,  null,	null,		'isset({save})'),
+        'name'=>    array(T_ZBX_STR, O_OPT,  null,  NOT_EMPTY,'isset({save})'),
+        'url'=>     array(T_ZBX_STR, O_OPT,  null,  NOT_EMPTY,      'isset({save})'),
+        'posts'=>   array(T_ZBX_STR, O_OPT,  null,  null,           'isset({save})'),
+        'timeout'=> array(T_ZBX_INT, O_OPT,  null,  BETWEEN(0,65535),   'isset({save})'),
+        'required'=>    array(T_ZBX_STR, O_OPT,  null,  null,           'isset({save})'),
+        'status_codes'=>array(T_ZBX_INT_RANGE, O_OPT,  null,    null,       'isset({save})'),
 
-		'add'=>		array(T_ZBX_STR, O_OPT, P_SYS|P_ACT,	null,	null),
-		'save'=>	array(T_ZBX_STR, O_OPT, P_SYS|P_ACT,	null,	null),
+        'add'=>     array(T_ZBX_STR, O_OPT, P_SYS|P_ACT,    null,   null),
+        'save'=>    array(T_ZBX_STR, O_OPT, P_SYS|P_ACT,    null,   null),
 
-		'form'=>	array(T_ZBX_STR, O_OPT, P_SYS,	null,	null),
-		'form_refresh'=>array(T_ZBX_STR, O_OPT, null,	null,	null)
-	);
+        'form'=>    array(T_ZBX_STR, O_OPT, P_SYS,  null,   null),
+        'form_refresh'=>array(T_ZBX_STR, O_OPT, null,   null,   null)
+    );
 
-	check_fields($fields);
+    check_fields($fields);
 ?>
 <script language="JavaScript" type="text/javascript">
 <!--
@@ -72,8 +72,8 @@ function add_var_to_opener_obj(obj,name,value){
 -->
 </script>
 <?php
-	if(isset($_REQUEST['save']) && !isset($_REQUEST['stepid']))
-	{
+    if(isset($_REQUEST['save']) && !isset($_REQUEST['stepid']))
+    {
 ?>
 <script language="JavaScript" type="text/javascript">
 <!--
@@ -82,88 +82,88 @@ function add_httpstep(formname,name,timeout,url,posts,required,status_codes){
         var form = window.opener.document.forms[formname];
 
         if(!form){
-			close_window();
-			return false;
+            close_window();
+            return false;
         }
 
-	add_var_to_opener_obj(form,'new_httpstep[name]',name);
-	add_var_to_opener_obj(form,'new_httpstep[timeout]',timeout);
-	add_var_to_opener_obj(form,'new_httpstep[url]',url);
-	add_var_to_opener_obj(form,'new_httpstep[posts]',posts);
-	add_var_to_opener_obj(form,'new_httpstep[required]',required);
-	add_var_to_opener_obj(form,'new_httpstep[status_codes]',status_codes);
+    add_var_to_opener_obj(form,'new_httpstep[name]',name);
+    add_var_to_opener_obj(form,'new_httpstep[timeout]',timeout);
+    add_var_to_opener_obj(form,'new_httpstep[url]',url);
+    add_var_to_opener_obj(form,'new_httpstep[posts]',posts);
+    add_var_to_opener_obj(form,'new_httpstep[required]',required);
+    add_var_to_opener_obj(form,'new_httpstep[status_codes]',status_codes);
 
-	form.submit();
-	close_window();
-	return true;
+    form.submit();
+    close_window();
+    return true;
 }
 
 <?php
-		echo 'add_httpstep('.
-			zbx_jsvalue($_REQUEST['dstfrm']).','.
-			zbx_jsvalue($_REQUEST['name']).','.
-			zbx_jsvalue($_REQUEST['timeout']).','.
-			zbx_jsvalue($_REQUEST['url']).','.
-			zbx_jsvalue($_REQUEST['posts']).','.
-			zbx_jsvalue($_REQUEST['required']).','.
-			zbx_jsvalue($_REQUEST['status_codes']).");\n";
+        echo 'add_httpstep('.
+            zbx_jsvalue($_REQUEST['dstfrm']).','.
+            zbx_jsvalue($_REQUEST['name']).','.
+            zbx_jsvalue($_REQUEST['timeout']).','.
+            zbx_jsvalue($_REQUEST['url']).','.
+            zbx_jsvalue($_REQUEST['posts']).','.
+            zbx_jsvalue($_REQUEST['required']).','.
+            zbx_jsvalue($_REQUEST['status_codes']).");\n";
 ?>
 -->
 </script>
 <?php
-	}
-	if(isset($_REQUEST['save']) && isset($_REQUEST['stepid']))
-	{
+    }
+    if(isset($_REQUEST['save']) && isset($_REQUEST['stepid']))
+    {
 ?>
 <script language="JavaScript" type="text/javascript">
 <!--
 
 function update_httpstep(formname,list_name,stepid,name,timeout,url,posts,required,status_codes){
-	var form = window.opener.document.forms[formname];
+    var form = window.opener.document.forms[formname];
 
-	if(!form){
-		close_window();
-		return false;
-	}
+    if(!form){
+        close_window();
+        return false;
+    }
 
-	add_var_to_opener_obj(form,list_name + '[' + stepid + '][name]',name);
-	add_var_to_opener_obj(form,list_name + '[' + stepid + '][timeout]',timeout);
-	add_var_to_opener_obj(form,list_name + '[' + stepid + '][url]',url);
-	add_var_to_opener_obj(form,list_name + '[' + stepid + '][posts]',posts);
-	add_var_to_opener_obj(form,list_name + '[' + stepid + '][required]',required);
-	add_var_to_opener_obj(form,list_name + '[' + stepid + '][status_codes]',status_codes);
+    add_var_to_opener_obj(form,list_name + '[' + stepid + '][name]',name);
+    add_var_to_opener_obj(form,list_name + '[' + stepid + '][timeout]',timeout);
+    add_var_to_opener_obj(form,list_name + '[' + stepid + '][url]',url);
+    add_var_to_opener_obj(form,list_name + '[' + stepid + '][posts]',posts);
+    add_var_to_opener_obj(form,list_name + '[' + stepid + '][required]',required);
+    add_var_to_opener_obj(form,list_name + '[' + stepid + '][status_codes]',status_codes);
 
 
-	form.submit();
-	close_window();
+    form.submit();
+    close_window();
 return true;
 }
 
 <?php
-		echo 'update_httpstep('.
-			zbx_jsvalue($_REQUEST['dstfrm']).','.
-			zbx_jsvalue($_REQUEST['list_name']).','.
-			zbx_jsvalue($_REQUEST['stepid']).','.
-			zbx_jsvalue($_REQUEST['name']).','.
-			zbx_jsvalue($_REQUEST['timeout']).','.
-			zbx_jsvalue($_REQUEST['url']).','.
-			zbx_jsvalue($_REQUEST['posts']).','.
-			zbx_jsvalue($_REQUEST['required']).','.
-			zbx_jsvalue($_REQUEST['status_codes']).");\n";
+        echo 'update_httpstep('.
+            zbx_jsvalue($_REQUEST['dstfrm']).','.
+            zbx_jsvalue($_REQUEST['list_name']).','.
+            zbx_jsvalue($_REQUEST['stepid']).','.
+            zbx_jsvalue($_REQUEST['name']).','.
+            zbx_jsvalue($_REQUEST['timeout']).','.
+            zbx_jsvalue($_REQUEST['url']).','.
+            zbx_jsvalue($_REQUEST['posts']).','.
+            zbx_jsvalue($_REQUEST['required']).','.
+            zbx_jsvalue($_REQUEST['status_codes']).");\n";
 ?>
 -->
 </script>
 <?php
-	}
-	else
-	{
+    }
+    else
+    {
 ?>
 <?php
-	echo SBR;
+    echo SBR;
 
-	insert_httpstep_form();
+    insert_httpstep_form();
 
-	}
+    }
 ?>
 <?php
 
