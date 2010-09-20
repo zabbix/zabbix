671136df8b5719897b976afe98b28263dd069531
Aleksandrs Saveljevs <git-no-reply@zabbix.com>
2010-09-20 09:31:00 +0000

    [ZBXNEXT-512] Added columns "value_flags" to "triggers" and "value_changed" to "events".

--- upgrades/dbpatches/2.0/postgresql/patch/events.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/events.sql
@@ -1,4 +1,5 @@
 DROP INDEX events_2;
 CREATE INDEX events_2 on events (clock);
 ALTER TABLE ONLY events ALTER eventid DROP DEFAULT,
-			ADD ns integer DEFAULT '0' NOT NULL;
+			ADD ns integer DEFAULT '0' NOT NULL,
+			ADD value_changed integer DEFAULT '0' NOT NULL;
