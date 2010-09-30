06de607c1acfd34ac57046030edf87261608df20
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-09-30 11:12:19 +0000

    [ZBX-3019] Merged from 1.8.
    git-svn-id: svn://svn.zabbix.com/branches/dev@14637 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- zbx-3019-discovery-graph/src/libs/zbxdbhigh/db.c
+++ zbx-3019-discovery-graph/src/libs/zbxdbhigh/db.c
@@ -555,7 +555,7 @@ int	DBupdate_trigger_value(zbx_uint64_t triggerid, int trigger_type, int trigger
 			event.value = new_value;
 
 			/* Processing event */
-			if (FAIL == (ret = process_event(&event)))
+			if (FAIL == (ret = process_event(&event, 0)))
 			{
 				zabbix_log(LOG_LEVEL_DEBUG, "Event not added for triggerid [" ZBX_FS_UI64 "]",
 						triggerid);
@@ -2011,7 +2011,7 @@ void	DBregister_host(zbx_uint64_t proxy_hostid, const char *host, int now)
 	event.value	= TRIGGER_VALUE_TRUE;
 
 	/* Processing event */
-	process_event(&event);
+	process_event(&event, 0);
 }
 
 /******************************************************************************
