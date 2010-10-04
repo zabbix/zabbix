7f2b8a33fe0674a91eb810c232ddaa34bc82818b
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-10-04 08:24:46 +0000

    [ZBX-453] Merged from 1.8.
    git-svn-id: svn://svn.zabbix.com/branches/dev@14671 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- zbx-453-disable-static/src/libs/zbxcommon/str.c
+++ zbx-453-disable-static/src/libs/zbxcommon/str.c
@@ -2238,9 +2238,6 @@ static int	zbx_strncasecmp(const char *s1, const char *s2, size_t n)
 
 char	*zbx_strcasestr(const char *haystack, const char *needle)
 {
-/*#ifdef HAVE_STRCASESTR
-	return strcasestr(haystack, needle);
-#else*/
 	size_t		sz_h, sz_n;
 	const char	*p;
 
@@ -2262,7 +2259,6 @@ char	*zbx_strcasestr(const char *haystack, const char *needle)
 	}
 
 	return NULL;
-/*#endif*/
 }
 
 int	starts_with(const char *str, const char *prefix)
@@ -2283,7 +2279,7 @@ int	cmp_key_id(const char *key_1, const char *key_2)
 	return ((*p == '\0' || *p == '[' || *p == ',') && (*q == '\0' || *q == '[' || *q == ',') ? SUCCEED : FAIL);
 }
 
-const char *zbx_permission_string(int perm)
+const char	*zbx_permission_string(int perm)
 {
 	switch (perm) {
 	case PERM_DENY:
@@ -2299,7 +2295,7 @@ const char *zbx_permission_string(int perm)
 	}
 }
 
-char	*zbx_item_value_type_string(zbx_item_value_type_t value_type)
+const char	*zbx_item_value_type_string(zbx_item_value_type_t value_type)
 {
 	switch (value_type) {
 	case ITEM_VALUE_TYPE_FLOAT: return "Numeric (float)";
@@ -2311,7 +2307,7 @@ char	*zbx_item_value_type_string(zbx_item_value_type_t value_type)
 	}
 }
 
-char	*zbx_result_string(int result)
+const char	*zbx_result_string(int result)
 {
 	switch (result) {
 	case SUCCEED: return "SUCCEED";
@@ -2324,7 +2320,7 @@ char	*zbx_result_string(int result)
 	}
 }
 
-char	*zbx_trigger_severity_string(zbx_trigger_severity_t severity)
+const char	*zbx_trigger_severity_string(zbx_trigger_severity_t severity)
 {
 	switch (severity) {
 	case TRIGGER_SEVERITY_NOT_CLASSIFIED: return "Not classified";
@@ -2337,7 +2333,7 @@ char	*zbx_trigger_severity_string(zbx_trigger_severity_t severity)
 	}
 }
 
-char	*zbx_item_logtype_string(zbx_item_logtype_t logtype)
+const char	*zbx_item_logtype_string(zbx_item_logtype_t logtype)
 {
 	switch (logtype) {
 	case ITEM_LOGTYPE_INFORMATION: return "Information";
@@ -2349,7 +2345,7 @@ char	*zbx_item_logtype_string(zbx_item_logtype_t logtype)
 	}
 }
 
-char	*zbx_dservice_type_string(zbx_dservice_type_t service)
+const char	*zbx_dservice_type_string(zbx_dservice_type_t service)
 {
 	switch (service) {
 	case SVC_SSH: return "SSH";
