d196032cf74f85efb86f1e3c47b9ddb760eb8d3a
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2010-10-08 13:15:06 +0000

    - [DEV-485] fixed constraints and syntax in upgrade patches

--- upgrades/dbpatches/2.0/postgresql/patch/sysmap_url.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/sysmap_url.sql
@@ -1,11 +1,10 @@
 CREATE TABLE sysmap_url (
-  sysmapurlid bigint NOT NULL,
-  sysmapid bigint  NOT NULL,
-  name varchar(255) NOT NULL DEFAULT '',
-  url varchar(255) NOT NULL DEFAULT '',
-  elementtype int NOT NULL DEFAULT '0',
-  PRIMARY KEY (sysmapurlid)
+	sysmapurlid              bigint                                    NOT NULL,
+	sysmapid                 bigint                                    NOT NULL,
+	name                     varchar(255)                              NOT NULL,
+	url                      varchar(255)    DEFAULT ''                NOT NULL,
+	elementtype              integer         DEFAULT '0'               NOT NULL,
+	PRIMARY KEY (sysmapurlid)
 ) with OIDS;
 CREATE UNIQUE INDEX sysmap_url_1 on sysmap_url (sysmapid,name);
-
-ALTER TABLE ONLY sysmap_url ADD CONSTRAINT c_sysmapid_1 FOREIGN KEY (sysmapid) REFERENCES sysmaps (sysmapid) ON DELETE CASCADE;
+ALTER TABLE ONLY sysmap_url ADD CONSTRAINT c_sysmap_url_1 FOREIGN KEY (sysmapid) REFERENCES sysmaps (sysmapid) ON DELETE CASCADE;
