06be79860b1f8c34988eb4e48e418948cc8a8d50
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-10-13 11:27:40 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@14849 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3050/upgrades/dbpatches/1.8/mysql/patch/proxy_dhistory.sql
+++
@@ -1,4 +0,0 @@
-alter table proxy_dhistory change key_ key_ varchar(255) DEFAULT '' NOT NULL;
-alter table proxy_dhistory change value value varchar(255) DEFAULT '' NOT NULL;
-
-alter table proxy_dhistory add dcheckid bigint unsigned DEFAULT '0' NOT NULL;
