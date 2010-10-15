ef9b2383eb450d85b8c2130089343b0d43f4ed4a
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-10-15 16:04:09 +0000

    - [ZBXNEXT-512] fixed triggers.sql patch for mysql
    git-svn-id: svn://svn.zabbix.com/branches/dev@14924 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- zbxnext-512-event-design/upgrades/dbpatches/2.0/mysql/patch/triggers.sql
+++ zbxnext-512-event-design/upgrades/dbpatches/2.0/mysql/patch/triggers.sql
@@ -10,20 +10,15 @@ ALTER TABLE events MODIFY eventid bigint unsigned NOT NULL,
 
 -- Begin event redesign patch
 
-CREATE TEMPORARY TABLE tmp_events_eventid (eventid bigint unsigned PRIMARY KEY);
+CREATE TEMPORARY TABLE tmp_events_eventid (eventid bigint unsigned PRIMARY KEY,prev_value integer);
 CREATE INDEX tmp_events_index on events (source, object, objectid, clock, eventid, value);
 
 -- Which OK events should have value_changed flag set?
 -- Those that have a PROBLEM event (or no event) before them.
 
-INSERT INTO tmp_events_eventid (eventid)
+INSERT INTO tmp_events_eventid (eventid,prev_value)
 (
-	SELECT e1.eventid
-		FROM events e1
-		WHERE e1.source=0							-- EVENT_SOURCE_TRIGGERS
-			AND e1.object=0 						-- EVENT_OBJECT_TRIGGER
-			AND e1.value=0							-- TRIGGER_VALUE_FALSE
-			AND (SELECT e2.value
+	SELECT e1.eventid,(SELECT e2.value
 				FROM events e2
 				WHERE e2.source=e1.source
 					AND e2.object=e1.object
@@ -36,41 +31,52 @@ INSERT INTO tmp_events_eventid (eventid)
 						e2.clock DESC,
 						e2.eventid DESC,
 						e2.value DESC
-				LIMIT 1)<>e1.value 					-- (NULL) or TRIGGER_VALUE_TRUE
+				LIMIT 1) AS prev_value
+		FROM events e1
+		WHERE e1.source=0							-- EVENT_SOURCE_TRIGGERS
+			AND e1.object=0 						-- EVENT_OBJECT_TRIGGER
+			AND e1.value=0							-- TRIGGER_VALUE_FALSE (OK)
+		HAVING prev_value IS NULL OR prev_value = 1				-- (NULL) or TRIGGER_VALUE_TRUE (PROBLEM)
 );
 
 -- Which PROBLEM events should have value_changed flag set?
 -- (1) Those that have an OK event (or no event) before them.
 -- (2) Those that came from a "MULTIPLE PROBLEM" trigger.
 
-INSERT INTO tmp_events_eventid (eventid)
+INSERT INTO tmp_events_eventid (eventid,prev_value)
 (
-	SELECT e1.eventid
-		FROM events e1, triggers t
+	SELECT e1.eventid,(SELECT e2.value
+				FROM events e2
+				WHERE e2.source=e1.source
+					AND e2.object=e1.object
+					AND e2.objectid=e1.objectid
+					AND (e2.clock<e1.clock OR (e2.clock=e1.clock AND e2.eventid<e1.eventid))
+					AND e2.value<2					-- TRIGGER_VALUE_UNKNOWN
+				ORDER BY e2.source DESC,
+						e2.object DESC,
+						e2.objectid DESC,
+						e2.clock DESC,
+						e2.eventid DESC,
+						e2.value DESC
+				LIMIT 1) AS prev_value
+		FROM events e1,triggers t
 		WHERE e1.source=0							-- EVENT_SOURCE_TRIGGERS
 			AND e1.object=0 						-- EVENT_OBJECT_TRIGGER
 			AND e1.objectid=t.triggerid
 			AND e1.value=1							-- TRIGGER_VALUE_TRUE
-			AND
-			(
-				(t.type=0						-- TRIGGER_TYPE_NORMAL
-					AND (SELECT e2.value
-						FROM events e2
-						WHERE e2.source=e1.source
-							AND e2.object=e1.object
-							AND e2.objectid=e1.objectid
-							AND (e2.clock<e1.clock OR (e2.clock=e1.clock AND e2.eventid<e1.eventid))
-							AND e2.value<2			-- TRIGGER_VALUE_UNKNOWN
-						ORDER BY e2.source DESC,
-								e2.object DESC,
-								e2.objectid DESC,
-								e2.clock DESC,
-								e2.eventid DESC,
-								e2.value DESC
-						LIMIT 1)<>e1.value) 			-- (NULL) or TRIGGER_VALUE_FALSE
-				OR
-				(t.type=1)						-- TRIGGER_TYPE_MULTIPLE_TRUE
-			)
+			AND t.type=0							-- TRIGGER_TYPE_NORMAL
+		HAVING prev_value IS NULL OR prev_value = 0				-- (NULL) or TRIGGER_VALUE_TRUE (PROBLEM)
+);
+
+INSERT INTO tmp_events_eventid (eventid)
+(
+	SELECT e1.eventid
+		FROM events e1,triggers t
+		WHERE e1.source=0							-- EVENT_SOURCE_TRIGGERS
+			AND e1.object=0 						-- EVENT_OBJECT_TRIGGER
+			AND e1.objectid=t.triggerid
+			AND e1.value=1							-- TRIGGER_VALUE_TRUE (PROBLEM)
+			AND t.type=1							-- TRIGGER_TYPE_MULTIPLE_TRUE
 );
 
 -- Update the value_changed flag.
