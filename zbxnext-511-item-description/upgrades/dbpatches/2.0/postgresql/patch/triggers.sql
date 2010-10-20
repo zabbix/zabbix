72cb239f68750dee0d23ef4c7e376fa6fa7eb301
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-10-20 07:18:05 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@14949 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- zbxnext-511-item-description/upgrades/dbpatches/2.0/postgresql/patch/triggers.sql
+++ zbxnext-511-item-description/upgrades/dbpatches/2.0/postgresql/patch/triggers.sql
@@ -4,8 +4,7 @@ ALTER TABLE ONLY triggers RENAME COLUMN comments TO description;
 
 ALTER TABLE ONLY triggers ALTER triggerid DROP DEFAULT,
 			  ALTER templateid DROP DEFAULT,
-			  ALTER templateid DROP NOT NULL,
-			  ALTER description;
+			  ALTER templateid DROP NOT NULL;
 UPDATE triggers SET templateid=NULL WHERE templateid=0;
 UPDATE triggers SET templateid=NULL WHERE NOT templateid IS NULL AND NOT templateid IN (SELECT triggerid FROM triggers);
 ALTER TABLE ONLY triggers ADD CONSTRAINT c_triggers_1 FOREIGN KEY (templateid) REFERENCES triggers (triggerid) ON DELETE CASCADE;
