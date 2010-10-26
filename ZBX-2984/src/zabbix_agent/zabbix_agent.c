9cc87eed48fa93aea09db5d414da41011bf673ab
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-10-26 10:16:39 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@15072 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-2984/src/zabbix_agent/zabbix_agent.c
@@ -0,0 +1,226 @@
+/*
+** ZABBIX
+** Copyright (C) 2000-2005 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+
+#include "common.h"
+#include "zabbix_agent.h"
+
+#include "comms.h"
+#include "cfg.h"
+#include "log.h"
+#include "sysinfo.h"
+#include "zbxconf.h"
+#include "zbxgetopt.h"
+#include "alias.h"
+
+const char	*progname = NULL;
+const char	title_message[] = "Zabbix Agent";
+const char	usage_message[] = "[-Vhp] [-c <file>] [-t <item>]";
+#ifndef HAVE_GETOPT_LONG
+const char	*help_message[] = {
+	"Options:",
+	"  -c <file>     Specify configuration file. Use absolute path",
+	"  -h            give this help",
+	"  -V            display version number",
+	"  -p            print supported items and exit",
+	"  -t <item>     test specified item and exit",
+	0 /* end of text */
+};
+#else
+const char	*help_message[] = {
+	"Options:",
+	"  -c --config <file>  Specify configuration file. Use absolute path",
+	"  -h --help           give this help",
+	"  -V --version        display version number",
+	"  -p --print          print supported items and exit",
+	"  -t --test <item>    test specified item and exit",
+	0 /* end of text */
+};
+#endif
+
+struct zbx_option longopts[] =
+{
+	{"config",	1,	0,	'c'},
+	{"help",	0,	0,	'h'},
+	{"version",	0,	0,	'V'},
+	{"print",	0,	0,	'p'},
+	{"test",	1,	0,	't'},
+	{0,0,0,0}
+};
+
+void	child_signal_handler( int sig )
+{
+	if( SIGALRM == sig )
+	{
+		signal( SIGALRM, child_signal_handler );
+	}
+
+	if( SIGQUIT == sig || SIGINT == sig || SIGTERM == sig )
+	{
+	}
+	exit( FAIL );
+}
+
+static char	DEFAULT_CONFIG_FILE[] = "/etc/zabbix/zabbix_agent.conf";
+
+void    init_config(void)
+{
+	struct cfg_line cfg[] =
+	{
+/*               PARAMETER      ,VAR    ,FUNC,  TYPE(0i,1s),MANDATORY,MIN,MAX
+*/
+		{"Server",&CONFIG_HOSTS_ALLOWED,0,TYPE_STRING,PARM_MAND,0,0},
+		{"Timeout",&CONFIG_TIMEOUT,0,TYPE_INT,PARM_OPT,1,30},
+		{0}
+	};
+
+	parse_cfg_file(CONFIG_FILE, cfg);
+}
+
+int	main(int argc, char **argv)
+{
+	char		ch;
+	int		task = ZBX_TASK_START;
+	char		*TEST_METRIC = NULL;
+	zbx_sock_t	s_in;
+	zbx_sock_t	s_out;
+
+	int		ret;
+	char		**value, *command;
+
+	AGENT_RESULT	result;
+
+	memset(&result, 0, sizeof(AGENT_RESULT));
+
+	progname = get_program_name(argv[0]);
+
+/* Parse the command-line. */
+	while ((ch = (char)zbx_getopt_long(argc, argv, "c:hVpt:", longopts, NULL)) != (char)EOF)
+		switch (ch)
+		{
+			case 'c':
+				CONFIG_FILE = strdup(zbx_optarg);
+				break;
+			case 'h':
+				help();
+				exit(-1);
+				break;
+			case 'V':
+				version();
+				exit(-1);
+				break;
+			case 'p':
+				if (task == ZBX_TASK_START)
+					task = ZBX_TASK_PRINT_SUPPORTED;
+				break;
+			case 't':
+				if (task == ZBX_TASK_START)
+				{
+					task = ZBX_TASK_TEST_METRIC;
+					TEST_METRIC = strdup(zbx_optarg);
+				}
+				break;
+			default:
+				task = ZBX_TASK_SHOW_USAGE;
+				break;
+		}
+
+	if (CONFIG_FILE == NULL)
+		CONFIG_FILE = DEFAULT_CONFIG_FILE;
+
+	init_metrics(); /* Must be before init_config() */
+
+	if (ZBX_TASK_START == task)
+	{
+		load_config();
+		load_user_parameters(0);
+	}
+
+	/* Do not create debug files */
+	zabbix_open_log(LOG_TYPE_SYSLOG, LOG_LEVEL_EMPTY, NULL);
+
+	switch (task)
+	{
+		case ZBX_TASK_PRINT_SUPPORTED:
+			load_user_parameters(1);
+			test_parameters();
+			exit(-1);
+			break;
+		case ZBX_TASK_TEST_METRIC:
+			load_user_parameters(1);
+			test_parameter(TEST_METRIC, PROCESS_TEST);
+			exit(-1);
+			break;
+		case ZBX_TASK_SHOW_USAGE:
+			usage();
+			exit(-1);
+			break;
+	}
+
+	signal(SIGINT,  child_signal_handler);
+	signal(SIGTERM, child_signal_handler);
+	signal(SIGQUIT, child_signal_handler);
+	signal(SIGALRM, child_signal_handler);
+
+	alarm(CONFIG_TIMEOUT);
+
+	zbx_tcp_init(&s_in, (ZBX_SOCKET)fileno(stdin));
+	zbx_tcp_init(&s_out, (ZBX_SOCKET)fileno(stdout));
+
+	if( SUCCEED == (ret = zbx_tcp_check_security(&s_in, CONFIG_HOSTS_ALLOWED, 0)) )
+	{
+		if( SUCCEED == (ret = zbx_tcp_recv(&s_in, &command)) )
+		{
+			zbx_rtrim(command, "\r\n");
+
+			zabbix_log(LOG_LEVEL_DEBUG, "Requested [%s]", command);
+
+			init_result(&result);
+
+			process(command, 0, &result);
+
+			if( NULL == (value = GET_TEXT_RESULT(&result)) )
+				value = GET_MSG_RESULT(&result);
+
+			if(value)
+			{
+				zabbix_log(LOG_LEVEL_DEBUG, "Sending back [%s]", *value);
+
+				ret = zbx_tcp_send(&s_out, *value);
+			}
+
+			free_result(&result);
+		}
+
+		if( FAIL == ret )
+		{
+			zabbix_log(LOG_LEVEL_DEBUG, "Processing error: %s", zbx_tcp_strerror());
+		}
+	}
+
+	fflush(stdout);
+
+	free_metrics();
+	alias_list_free();
+
+	alarm(0);
+
+	zabbix_close_log();
+
+	return SUCCEED;
+}
---
+++ ZBX-2984/src/zabbix_server/watchdog/watchdog.c
@@ -0,0 +1,198 @@
+/*
+** ZABBIX
+** Copyright (C) 2000-2005 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+
+#include "common.h"
+
+#include "cfg.h"
+#include "db.h"
+#include "zbxdb.h"
+#include "log.h"
+#include "zlog.h"
+
+#include "../alerter/alerter.h"
+
+#include "zlog.h"
+#include "watchdog.h"
+
+#define ZBX_RECIPIENT struct zbx_recipient_t
+
+ZBX_RECIPIENT
+{
+	DB_ALERT	alert;
+	DB_MEDIATYPE	mediatype;
+};
+
+#define	ZBX_MAX_RECIPIENTS	32
+
+ZBX_RECIPIENT	recipients[ZBX_MAX_RECIPIENTS];
+
+static	int num = 0;
+static	int lastsent = 0;
+
+/******************************************************************************
+ *                                                                            *
+ * Function: send_alerts                                                      *
+ *                                                                            *
+ * Purpose: send warning message to all interested                            *
+ *                                                                            *
+ * Parameters:                                                                *
+ *                                                                            *
+ * Return value:                                                              *
+ *                                                                            *
+ * Author: Alexei Vladishev                                                   *
+ *                                                                            *
+ * Comments:                                                                  *
+ *                                                                            *
+ ******************************************************************************/
+static void send_alerts()
+{
+	int	i,now;
+	char	error[MAX_STRING_LEN];
+
+	now = time(NULL);
+
+	if(now>lastsent+900)
+	{
+		for(i=0;i<num;i++)
+		{
+			execute_action(&recipients[i].alert,&recipients[i].mediatype, error, sizeof(error));
+		}
+		lastsent = now;
+	}
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Function: init_config                                                      *
+ *                                                                            *
+ * Purpose: init list of medias to send notifications in case if DB is down   *
+ *                                                                            *
+ * Parameters:                                                                *
+ *                                                                            *
+ * Return value:                                                              *
+ *                                                                            *
+ * Author: Alexei Vladishev                                                   *
+ *                                                                            *
+ * Comments:                                                                  *
+ *                                                                            *
+ ******************************************************************************/
+static void init_config()
+{
+	DB_RESULT	result;
+	DB_ROW		row;
+
+	zabbix_log(LOG_LEVEL_DEBUG, "In init_config()");
+
+	result = DBselect("select mt.mediatypeid, mt.type, mt.description, mt.smtp_server, mt.smtp_helo, mt.smtp_email, mt.exec_path, mt.gsm_modem, mt.username, mt.passwd, m.mediaid,m.userid,m.mediatypeid,m.sendto,m.severity,m.period from media m, users_groups u, config c,media_type mt where m.userid=u.userid and u.usrgrpid=c.alert_usrgrpid and m.mediatypeid=mt.mediatypeid and m.active=%d",
+		MEDIA_STATUS_ACTIVE);
+	while((row=DBfetch(result)))
+	{
+		if(num>=ZBX_MAX_RECIPIENTS)	break;
+
+		memset(&recipients[num].mediatype, 0, sizeof(DB_MEDIATYPE));
+		memset(&recipients[num].alert, 0, sizeof(DB_ALERT));
+
+		ZBX_STR2UINT64(recipients[num].mediatype.mediatypeid,row[0]);
+		recipients[num].mediatype.type=atoi(row[1]);
+		recipients[num].mediatype.description=strdup(row[2]);
+		recipients[num].mediatype.smtp_server=strdup(row[3]);
+		recipients[num].mediatype.smtp_helo=strdup(row[4]);
+		recipients[num].mediatype.smtp_email=strdup(row[5]);
+		recipients[num].mediatype.exec_path=strdup(row[6]);
+		recipients[num].mediatype.gsm_modem=strdup(row[7]);
+		recipients[num].mediatype.username=strdup(row[8]);
+		recipients[num].mediatype.passwd=strdup(row[9]);
+
+		recipients[num].alert.sendto=strdup(row[13]);
+		recipients[num].alert.subject=strdup("Zabbix database is down.");
+		recipients[num].alert.message=strdup("Zabbix database is down.");
+
+		num++;
+	}
+
+	DBfree_result(result);
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Function: ping_database                                                    *
+ *                                                                            *
+ * Purpose: check availability of database                                    *
+ *                                                                            *
+ * Parameters:                                                                *
+ *                                                                            *
+ * Return value:                                                              *
+ *                                                                            *
+ * Author: Alexei Vladishev                                                   *
+ *                                                                            *
+ * Comments:                                                                  *
+ *                                                                            *
+ ******************************************************************************/
+static void ping_database()
+{
+	zabbix_log(LOG_LEVEL_DEBUG, "In ping_database()");
+	/* This is test SQL query, it does nothing */
+	if(DBping() == FAIL)
+	{
+		zabbix_log(LOG_LEVEL_WARNING, "Watchdog: Database is down");
+		send_alerts();
+	}
+	else
+	{
+		zabbix_log(LOG_LEVEL_DEBUG, "Watchdog: Database is up");
+	}
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Function: main_watchdog_loop                                               *
+ *                                                                            *
+ * Purpose: periodically checks availability of database and alerts admin if  *
+ *          down                                                              *
+ *                                                                            *
+ * Parameters:                                                                *
+ *                                                                            *
+ * Return value:                                                              *
+ *                                                                            *
+ * Author: Alexei Vladishev                                                   *
+ *                                                                            *
+ * Comments: check database availability every 60 seconds (hardcoded)         *
+ *                                                                            *
+ ******************************************************************************/
+void main_watchdog_loop()
+{
+	zabbix_log(LOG_LEVEL_DEBUG, "In main_watchdog_loop()");
+
+	/* Disable writing to database in zabbix_syslog() */
+	CONFIG_ENABLE_LOG = 0;
+
+	DBconnect(ZBX_DB_CONNECT_NORMAL);
+
+	init_config();
+
+	DBclose();
+
+	for (;;) {
+		ping_database();
+
+		sleep(60);
+	}
+
+	/* We will never reach this point */
+}
