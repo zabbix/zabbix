e9b1ec14aa686492a2d3fba40c4d5833cce2c99d
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-10-28 11:26:03 +0000

    git-svn-id: svn://svn.zabbix.com/branches/dev@15134 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- lld_bugs/upgrades/dbpatches/2.0/postgresql/patch/profiles.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ONLY profiles ALTER profileid DROP DEFAULT,
-			  ALTER userid DROP DEFAULT;
-DELETE FROM profiles WHERE NOT userid IN (SELECT userid FROM users);
-ALTER TABLE ONLY profiles ADD CONSTRAINT c_profiles_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
