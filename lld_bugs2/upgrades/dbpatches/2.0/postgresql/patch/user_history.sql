fb97d8876bb9619ea86e0e67a5dbfa9e1f436505
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-10-29 13:11:37 +0000

    rem
    git-svn-id: svn://svn.zabbix.com/branches/dev@15166 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- lld_bugs2/upgrades/dbpatches/2.0/postgresql/patch/user_history.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ONLY user_history ALTER userhistoryid DROP DEFAULT,
-			      ALTER userid DROP DEFAULT;
-DELETE FROM user_history WHERE NOT userid IN (SELECT userid FROM users);
-ALTER TABLE ONLY user_history ADD CONSTRAINT c_user_history_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
