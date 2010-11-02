df9d1869832370542f3410d94345fac6c7c75e84
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-11-02 06:15:45 +0000

    - [ZBXNEXT-538] fixed optional patches for IBM DB2
    git-svn-id: svn://svn.zabbix.com/branches/dev@15202 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- zbxnext-538-trunk/upgrades/dbpatches/2.0/ibmdb2/optional/history_uint.sql
+++ zbxnext-538-trunk/upgrades/dbpatches/2.0/ibmdb2/optional/history_uint.sql
@@ -1,2 +1,4 @@
 ALTER TABLE history_uint ALTER COLUMN itemid SET WITH DEFAULT NULL;
-ALTER TABLE history_uint ADD ns integer SET WITH DEFAULT '0' NOT NULL;
+REORG TABLE history_uint;
+ALTER TABLE history_uint ADD ns integer WITH DEFAULT '0' NOT NULL;
+REORG TABLE history_uint;
