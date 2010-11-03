88e653450d3ccab73b023c3e2e4e9b3fbd054677
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2010-11-03 12:27:55 +0000

    - [ZBXNEXT-512] upgrade patch for Oracle

--- upgrades/dbpatches/2.0/oracle/patch/events.sql
+++ upgrades/dbpatches/2.0/oracle/patch/events.sql
@@ -1,5 +1,1 @@
-DROP INDEX events_2;
-CREATE INDEX events_2 on events (clock);
-ALTER TABLE events MODIFY eventid DEFAULT NULL;
-ALTER TABLE events ADD ns number(10) DEFAULT '0' NOT NULL;
-ALTER TABLE events ADD value_changed number(10) DEFAULT '0' NOT NULL;
+-- See triggers.sql
