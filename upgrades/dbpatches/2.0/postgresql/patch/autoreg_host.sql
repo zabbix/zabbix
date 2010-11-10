04ab69a4aeadbd4b75d4fc6b28884e8ffcb6e148
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-11-10 13:20:58 +0000

    [DEV-471] improved efficiency of PostgreSQL upgrade patch for Zabbix 2.0
    git-svn-id: svn://svn.zabbix.com/trunk@15384 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/2.0/postgresql/patch/autoreg_host.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/autoreg_host.sql
@@ -2,5 +2,5 @@ ALTER TABLE ONLY autoreg_host ALTER autoreg_hostid DROP DEFAULT,
 			      ALTER proxy_hostid DROP DEFAULT,
 			      ALTER proxy_hostid DROP NOT NULL;
 UPDATE autoreg_host SET proxy_hostid=NULL WHERE proxy_hostid=0;
-DELETE FROM autoreg_host WHERE NOT proxy_hostid IS NULL AND NOT proxy_hostid IN (SELECT hostid FROM hosts);
+DELETE FROM autoreg_host WHERE proxy_hostid IS NOT NULL AND NOT EXISTS (SELECT 1 FROM hosts WHERE hosts.hostid=autoreg_host.proxy_hostid);
 ALTER TABLE ONLY autoreg_host ADD CONSTRAINT c_autoreg_host_1 FOREIGN KEY (proxy_hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
