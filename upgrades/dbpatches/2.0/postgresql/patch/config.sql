5065a2d4a71ab65a36336ae62f8e435628a13a94
alex <alex@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2010-11-10 13:20:58 +0000

    [DEV-471] improved efficiency of PostgreSQL upgrade patch for Zabbix 2.0
    git-svn-id: svn://svn.zabbix.com/trunk@15384 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/2.0/postgresql/patch/config.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/config.sql
@@ -3,7 +3,7 @@ ALTER TABLE ONLY config ALTER configid DROP DEFAULT,
 			ALTER alert_usrgrpid DROP NOT NULL,
 			ALTER discovery_groupid DROP DEFAULT,
 			ADD ns_support integer DEFAULT '0' NOT NULL;
-UPDATE config SET alert_usrgrpid=NULL WHERE NOT alert_usrgrpid IN (SELECT usrgrpid FROM usrgrp);
-UPDATE config SET discovery_groupid=NULL WHERE NOT discovery_groupid IN (SELECT groupid FROM groups);
+UPDATE config SET alert_usrgrpid=NULL WHERE NOT EXISTS (SELECT 1 FROM usrgrp WHERE usrgrp.usrgrpid=config.alert_usrgrpid);
+UPDATE config SET discovery_groupid=NULL WHERE NOT EXISTS (SELECT 1 FROM groups WHERE groups.groupid=config.discovery_groupid);
 ALTER TABLE ONLY config ADD CONSTRAINT c_config_1 FOREIGN KEY (alert_usrgrpid) REFERENCES usrgrp (usrgrpid);
 ALTER TABLE ONLY config ADD CONSTRAINT c_config_2 FOREIGN KEY (discovery_groupid) REFERENCES groups (groupid);
--- upgrades/dbpatches/2.0/postgresql/patch/operations.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/operations.sql
@@ -1,7 +1,7 @@
 ALTER TABLE ONLY operations ALTER operationid DROP DEFAULT,
 			    ALTER actionid DROP DEFAULT,
 			    ADD mediatypeid bigint NULL;
-DELETE FROM operations WHERE NOT actionid IN (SELECT actionid FROM actions);
+DELETE FROM operations WHERE NOT EXISTS (SELECT 1 FROM actions WHERE actions.actionid=operations.actionid);
 ALTER TABLE ONLY operations ADD CONSTRAINT c_operations_1 FOREIGN KEY (actionid) REFERENCES actions (actionid) ON DELETE CASCADE;
 ALTER TABLE ONLY operations ADD CONSTRAINT c_operations_2 FOREIGN KEY (mediatypeid) REFERENCES media_type (mediatypeid) ON DELETE CASCADE;
 UPDATE operations SET mediatypeid=(SELECT mediatypeid FROM opmediatypes WHERE operationid = operations.operationid);
