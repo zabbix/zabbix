df682f980261386dae83a04512f9ff19ecf06809
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-11-10 08:28:22 +0000

    [DEV-471] new branch created
    git-svn-id: svn://svn.zabbix.com/branches/dev@15364 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ DEV-471/upgrades/dbpatches/2.0/ibm_db2/patch/hosts_profiles.sql
@@ -0,0 +1,5 @@
+ALTER TABLE hosts_profiles ALTER COLUMN hostid SET WITH DEFAULT NULL;
+REORG TABLE hosts_profiles;
+DELETE FROM hosts_profiles WHERE NOT hostid IN (SELECT hostid FROM hosts);
+ALTER TABLE hosts_profiles ADD CONSTRAINT c_hosts_profiles_1 FOREIGN KEY (hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
+REORG TABLE hosts_profiles;
