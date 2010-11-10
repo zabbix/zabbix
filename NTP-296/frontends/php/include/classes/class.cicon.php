cbde0dadfca1e2fb15bb670fca1de2da86f04b6c
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-11-10 12:09:52 +0000

    New
    git-svn-id: svn://svn.zabbix.com/branches/dev@15377 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ NTP-296/frontends/php/include/classes/class.cicon.php
@@ -0,0 +1,33 @@
+<?php
+/*
+** ZABBIX
+** Copyright (C) 2000-2009 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+?>
+<?php
+class CIcon extends CDiv{
+	public function __construct($title, $class, $action=''){
+		parent::__construct(SPACE, $class.' menu_icon');
+
+		$this->setAttribute('title', $title);
+
+		if(!empty($action)){
+			$this->setAttribute('onclick', 'javascript: '.$action);
+		}
+	}
+}
+?>
---
+++ NTP-296/upgrades/dbpatches/2.0/postgresql/patch/auditlog.sql
@@ -0,0 +1,4 @@
+ALTER TABLE ONLY auditlog ALTER auditid DROP DEFAULT,
+			  ALTER userid DROP DEFAULT;
+DELETE FROM auditlog WHERE NOT userid IN (SELECT userid FROM users);
+ALTER TABLE ONLY auditlog ADD CONSTRAINT c_auditlog_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
