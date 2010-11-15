1d861ac42ab23f4054ab7c4a6e5e2b51df30e49e
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-11-15 08:51:55 +0000

    [ZBX-3214] Removed development branch.
    git-svn-id: svn://svn.zabbix.com/branches/dev@15470 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3214/upgrades/dbpatches/1.8/postgresql/patch/proxy_autoreg_host.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE proxy_autoreg_host (
-        id              serial                  NOT NULL,
-        clock           integer         DEFAULT '0'     NOT NULL,
-        host            varchar(64)             DEFAULT ''      NOT NULL,
-        PRIMARY KEY (id)
-) with OIDS;
-CREATE INDEX proxy_autoreg_host_1 on proxy_autoreg_host (clock);
