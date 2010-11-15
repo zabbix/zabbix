46bdef0955ca5e1205422cb1cdc4ff2c30a8ac46
Vedmak <Vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-11-15 14:37:18 +0000

    - [ZBX-3215] create
    git-svn-id: svn://svn.zabbix.com/branches/dev@15495 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-3215/upgrades/dbpatches/1.8/mysql/patch/dchecks.sql
@@ -0,0 +1,6 @@
+alter table dchecks add snmpv3_securityname varchar(64) DEFAULT '' NOT NULL;
+alter table dchecks add snmpv3_securitylevel integer DEFAULT '0' NOT NULL;
+alter table dchecks add snmpv3_authpassphrase varchar(64) DEFAULT '' NOT NULL;
+alter table dchecks add snmpv3_privpassphrase varchar(64) DEFAULT '' NOT NULL;
+
+CREATE INDEX dchecks_1 on dchecks (druleid);
