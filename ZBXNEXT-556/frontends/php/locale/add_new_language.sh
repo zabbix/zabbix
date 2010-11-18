52df039dde689d537a7660f6a8f30df4a797ca5c
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-11-18 15:22:30 +0000

    - [ZBXNEXT-556] fail on more error conditions; deal with svn stuff when adding a new locale
    git-svn-id: svn://svn.zabbix.com/branches/dev@15592 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-556/frontends/php/locale/add_new_language.sh
+++ ZBXNEXT-556/frontends/php/locale/add_new_language.sh
@@ -10,7 +10,10 @@ messagetemplate=frontend.pot
 [ -f $messagetemplate ] && {
 	mkdir -p $1/LC_MESSAGES
 	msginit --no-translator --no-wrap --locale=$1 --input=frontend.pot \
-	-o $1/LC_MESSAGES/frontend.po
+	-o $1/LC_MESSAGES/frontend.po || exit 1
+	svn add $1/LC_MESSAGES
+	svn ps svn:ignore "frontend.mo" $1/LC_MESSAGES
 } || {
 	echo "po template $messagetemplate missing"
+	exit 1
 }
