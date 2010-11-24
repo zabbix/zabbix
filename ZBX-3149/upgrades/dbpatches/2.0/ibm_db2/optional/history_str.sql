49998db71f5e486995e6ec75e297cd2488272cb7
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-11-24 19:27:54 +0000

    - [ZBX-3149] create dev branch to deal with some untranslatable strings
    git-svn-id: svn://svn.zabbix.com/branches/dev@15669 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-3149/upgrades/dbpatches/2.0/ibm_db2/optional/history_str.sql
@@ -0,0 +1,4 @@
+ALTER TABLE history_str ALTER COLUMN itemid SET WITH DEFAULT NULL;
+REORG TABLE history_str;
+ALTER TABLE history_str ADD ns integer WITH DEFAULT '0' NOT NULL;
+REORG TABLE history_str;
