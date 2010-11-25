b4f1d8947d5a38dda930ada16923bc89fc10d03b
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-11-25 14:37:30 +0000

    - [ZBXNEXT-556] merged, deleting branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@15683 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-556-dbpatch/upgrades/dbpatches/2.0/postgresql/patch/maintenances_windows.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE ONLY maintenances_windows ALTER maintenance_timeperiodid DROP DEFAULT,
-				      ALTER maintenanceid DROP DEFAULT,
-				      ALTER timeperiodid DROP DEFAULT;
-DROP INDEX maintenances_windows_1;
-DELETE FROM maintenances_windows WHERE NOT EXISTS (SELECT 1 FROM maintenances WHERE maintenances.maintenanceid=maintenances_windows.maintenanceid);
-DELETE FROM maintenances_windows WHERE NOT EXISTS (SELECT 1 FROM timeperiods WHERE timeperiods.timeperiodid=maintenances_windows.timeperiodid);
-CREATE UNIQUE INDEX maintenances_windows_1 ON maintenances_windows (maintenanceid,timeperiodid);
-ALTER TABLE ONLY maintenances_windows ADD CONSTRAINT c_maintenances_windows_1 FOREIGN KEY (maintenanceid) REFERENCES maintenances (maintenanceid) ON DELETE CASCADE;
-ALTER TABLE ONLY maintenances_windows ADD CONSTRAINT c_maintenances_windows_2 FOREIGN KEY (timeperiodid) REFERENCES timeperiods (timeperiodid) ON DELETE CASCADE;
