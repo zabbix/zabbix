25b5594d53b32e1bef25f8752abf36267d255de0
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-12-03 07:38:25 +0000

    - [ZBX-3259] creating branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@15814 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-3259/src/zabbix_agent/logfiles.h
@@ -0,0 +1,26 @@
+/*
+** ZABBIX
+** Copyright (C) 2000-2005 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+
+#ifndef ZABBIX_LOGFILES_H
+#define ZABBIX_LOGFILES_H
+
+int	process_log(char *filename, long *lastlogsize, char **value, const char *encoding);
+int	process_logrt(char *fileformat, long *lastlogsize, int *mtime, char **value, const char *encoding);
+
+#endif
