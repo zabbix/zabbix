c152940d6ac4e1a6cc090a4ccfabcc5ceb46dee4
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-12-03 14:07:24 +0000

    - [ZBX-3259] removed debug
    git-svn-id: svn://svn.zabbix.com/branches/dev@15835 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3259/frontends/php/js/prototype.js
+++ ZBX-3259/frontends/php/js/prototype.js
@@ -1490,9 +1490,6 @@ Ajax.Request = Class.create(Ajax.Base, {
   },
 
   respondToReadyState: function(readyState) {
-	alert("readystate = " + readyState);
-	alert("typeof(Ajax) = " + typeof(Ajax));
-	alert("Ajax = " + Ajax);
 	var state = Ajax.Request.Events[readyState], response = new Ajax.Response(this);
 
     if (state == 'Complete') {
