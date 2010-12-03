e05828b44be0459cba6db1d13a2a901b4cf29977
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-12-03 10:14:56 +0000

    - [ZBX-954] Url -> URL
    git-svn-id: svn://svn.zabbix.com/trunk@15818 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/locales/en_gb.inc.php
+++ frontends/php/include/locales/en_gb.inc.php
@@ -1341,7 +1341,7 @@
 	'S_EXPORT_HAVE_NO_MAPS'=>		_('Export does not contain Zabbix maps'),
 	'S_EXPORT_HAVE_NO_SCREENS'=>	_('Export does not contain Zabbix screens'),
 
-	'S_INCORRECT_ELEMENT_MAP_LINK' => _('All links should have "Name" and "Url" specified'),
+	'S_INCORRECT_ELEMENT_MAP_LINK' => _('All links should have "Name" and "URL" specified'),
 	'S_EACH_URL_SHOULD_HAVE_UNIQUE' => _('Each URL should have a unique name. Please make sure there is only one URL named'),
 
 //	map.php
