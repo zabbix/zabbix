7e1d60b26d9e2480d9628e8be18615238f01c377
Alexei Vladishev <alexei.vladishev@zabbix.com>
2010-12-08 11:46:19 +0000

    - [DEV-492] added initial Hudson and PHP Unit related files

---
+++ frontends/php/tests/all.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+ 
+class ArrayTest extends PHPUnit_Framework_TestCase
+{
+    public function testNewArrayIsEmpty()
+    {
+        // Create the Array fixture.
+        $fixture = array();
+ 
+        // Assert that the size of the Array fixture is 0.
+        $this->assertEquals(0, sizeof($fixture));
+    }
+ 
+    public function testArrayContainsAnElement()
+    {
+        // Create the Array fixture.
+        $fixture = array();
+ 
+        // Add an element to the Array fixture.
+        $fixture[] = 'Element';
+ 
+        // Assert that the size of the Array fixture is 1.
+        $this->assertEquals(1, sizeof($fixture));
+    }
+}
+?>
