2d3b8afc5ad6949bc763269843faf5e9952677b1
Alexei Vladishev <alexei.vladishev@zabbix.com>
2010-12-08 15:42:01 +0000

    - [DEV-492] reverting back all test file

--- frontends/php/tests/all.php
+++ frontends/php/tests/all.php
@@ -1,60 +1,30 @@
 <?php
 require_once 'PHPUnit/Framework.php';
-
-require('../include/func.inc.php');
-require('../include/items.inc.php');
-require('../include/defines.inc.php');
-require('../include/locales.inc.php');
  
 class All extends PHPUnit_Framework_TestCase
 {
-	public static function provider()
+	public function testNewArrayIsEmpty()
 	{
-		return array(
-			array('key[a]',true),
-			array('key["a"]',true),
-			array('key[a, b, c]',true),
-			array('key["a", "b", "c"]',true),
-			array('key[a, b, "c"]',true),
-			array('key["a", "b", c]',true),
-			array('key["a[][][]]],\"!@$#$^%*&*)"]',true),
-			array('key[["a"],b]',true),
-			array('complex.key[a, b, c]',true),
-			array('complex.key[[a, b], c]',true),
-			array('complex.key[abc"efg"h]',true),
-			array('complex.key[a][b]',true),
-			array('complex.key["a"]["b"]',true),
-			array('complex.key["a"][b]',true),
-			array('complex.key[a, b][c, d]',true),
-			array('complex.key["a", "b"]["c", "d"]',true),
-			array('more.complex.key[1, 2, [A, B, [a, b], C], 3]',true),
-			array('more.complex.key["1", "2", ["A", "B", ["a", "b"], "C"], "3"]',true),
-			array('more.complex.key[["1"]]',true),
-			array('key[,,]',true),
-			array('key[a]]',false),
-			array('key["a"]]',false),
-			array('key["a]',false),
-			array('key[a,]',true),
-			array('key["a",]',true),
-			array('key[["a",]',false),
-			array('key[a]654',false),
-			array('key["a"]654',false),
-			array('key[a][[b]',false),
-			array('key["a"][["b"]',false),
-			// Incorrect test case for testing
-			array('key(a)',true)
-		);
-	}
+	// Create the Array fixture.
+		$fixture = array();
 
-	/**
-	* @dataProvider provider
-	*/
-	public function testItemKeyValidation($a, $b)
+// Assert that the size of the Array fixture is 0.
+// OK
+		$this->assertEquals(0, sizeof($fixture));
+// FAIL
+		$this->assertEquals(1, sizeof($fixture));
+	}
+ 
+	public function testArrayContainsAnElement()
 	{
-		$result=check_item_key($a);
-		$this->assertEquals($result[0],$b,$result[1]);
+		// Create the Array fixture.
+		$fixture = array();
+	
+		// Add an element to the Array fixture.
+		$fixture[] = 'Element';
+ 
+		// Assert that the size of the Array fixture is 1.
+		$this->assertEquals(1, sizeof($fixture));
 	}
-
 }
 ?>
-
