e599050a31dc372f71e20e1507d30114d1711582
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-12-08 14:22:56 +0000

    - [DEV-492] added failed phpunit test for testing purposes
    git-svn-id: svn://svn.zabbix.com/branches/dev@15907 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- DEV-492/frontends/php/tests/all.php
+++ DEV-492/frontends/php/tests/all.php
@@ -3,25 +3,28 @@ require_once 'PHPUnit/Framework.php';
  
 class All extends PHPUnit_Framework_TestCase
 {
-    public function testNewArrayIsEmpty()
-    {
-        // Create the Array fixture.
-        $fixture = array();
+	public function testNewArrayIsEmpty()
+	{
+	// Create the Array fixture.
+		$fixture = array();
+
+// Assert that the size of the Array fixture is 0.
+// OK
+		$this->assertEquals(0, sizeof($fixture));
+// FAIL
+		$this->assertEquals(1, sizeof($fixture));
+	}
  
-        // Assert that the size of the Array fixture is 0.
-        $this->assertEquals(0, sizeof($fixture));
-    }
+	public function testArrayContainsAnElement()
+	{
+		// Create the Array fixture.
+		$fixture = array();
+	
+		// Add an element to the Array fixture.
+		$fixture[] = 'Element';
  
-    public function testArrayContainsAnElement()
-    {
-        // Create the Array fixture.
-        $fixture = array();
- 
-        // Add an element to the Array fixture.
-        $fixture[] = 'Element';
- 
-        // Assert that the size of the Array fixture is 1.
-        $this->assertEquals(1, sizeof($fixture));
-    }
+		// Assert that the size of the Array fixture is 1.
+		$this->assertEquals(1, sizeof($fixture));
+	}
 }
 ?>
