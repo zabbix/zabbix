32ce6b408d32caa648fca92b97c1c287623e03bd
Alexei Vladishev <alexei.vladishev@zabbix.com>
2010-12-10 09:36:35 +0000

    - [DEV-492] minor changes insome unit tests

--- frontends/php/tests/general/GeneralTests.php
+++ frontends/php/tests/general/GeneralTests.php
@@ -1,10 +1,7 @@
 <?php
-//require_once 'Framework/ClassTest.php';
 require_once 'itemKeyValidation.php';
 require_once 'otherTest.php';
-// ...
  
-//class Package_GeneralTests
 class GeneralTests
 {
 	public static function suite()
