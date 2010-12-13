b341d717afd02e161fbba05558f7799469611b55
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-12-13 12:58:48 +0000

    - [ZBXNEXT-3273] merge from branches/1.8
    git-svn-id: svn://svn.zabbix.com/branches/dev@16022 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3273/frontends/php/include/locales/en_gb.inc.php
+++ ZBX-3273/frontends/php/include/locales/en_gb.inc.php
@@ -1146,6 +1146,7 @@
 	'S_NUMERIC_FLOAT'=>					'Numeric (float)',
 	'S_CHARACTER'=>						'Character',
 	'S_WIZARD'=>						'Wizard',
+	'S_LOG' => 						'Log',
 	'S_TEXT'=>						'Text',
 	'S_AS_IS'=>						'As is',
 	'S_DELTA_SPEED_PER_SECOND'=>				'Delta (speed per second)',
@@ -2499,7 +2500,7 @@
 
 //	functions.js
 	'S_CREATE_LOG_TRIGGER'=>			'Create trigger',
-	'DO_YOU_REPLACE_CONDITIONAL_EXPRESSION_Q'=>	'Do you replace the conditional expression?',
+	'DO_YOU_REPLACE_CONDITIONAL_EXPRESSION_Q'=>	'Do you wish to replace the conditional expression?',
 	'S_ADD_SERVICE'=>				'Add Service',
 	'S_EDIT_SERVICE'=>				'Edit Service',
 	'S_DELETE_SERVICE'=>				'Delete Service',
