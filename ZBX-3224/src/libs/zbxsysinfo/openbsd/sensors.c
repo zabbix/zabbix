da97da02ce46a6ddecaf092474bc37cbdcb6be25
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-12-15 10:04:55 +0000

    - [ZBX-3224] Removed unnecessary code, fixed size of mib array.
    git-svn-id: svn://svn.zabbix.com/branches/dev@16119 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3224/src/libs/zbxsysinfo/openbsd/sensors.c
+++ ZBX-3224/src/libs/zbxsysinfo/openbsd/sensors.c
@@ -18,26 +18,21 @@
 **/
 
 #include "common.h"
-
 #include "sysinfo.h"
-
-#include "md5.h"
 
 #include <sys/sensors.h>
 
 #define CELSIUS(x) ((x - 273150000) / 1000000.0)
 
-int	sensor_value(int[], struct sensor *, char *);
-
-int	sensor_value(int mib[], struct sensor *sensor, char *key2)
+static int	sensor_value(int *mib, struct sensor *sensor, const char *key2)
 {
-	size_t		slen;
+	size_t	slen;
 
 	mib[3] = SENSOR_TEMP;
 	mib[4] = key2 ? atoi(key2) : 0;
 
 	slen = sizeof(*sensor);
-	if (sysctl(mib, 5, sensor, &slen, NULL, 0) == -1)
+	if (-1 == sysctl(mib, 5, sensor, &slen, NULL, 0))
 		return SYSINFO_RET_FAIL;
 
 	return SYSINFO_RET_OK;
@@ -45,32 +40,22 @@ int	sensor_value(int mib[], struct sensor *sensor, char *key2)
 
 int	GET_SENSOR(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	enum sensor_type	 type;
-	struct sensordev 	 sensordev;
-	struct sensor		 sensor;
-	size_t			 sdlen = sizeof(sensordev);
-	int			 mib[3], dev, numt, cnt = 0, ret = SYSINFO_RET_FAIL;
-	uint64_t	 	 aggr = 0;
-	char			 key[MAX_STRING_LEN], key2[MAX_STRING_LEN];
+	enum sensor_type	type;
+	struct sensordev	sensordev;
+	struct sensor		sensor;
+	size_t			sdlen = sizeof(sensordev);
+	int			mib[5], dev, numt, cnt = 0, ret = SYSINFO_RET_FAIL;
+	uint64_t		aggr = 0;
+	char			key[MAX_STRING_LEN], key2[MAX_STRING_LEN];
 
-	assert(result);
-
-	init_result(result);
-
-	if(num_param(param) > 2)
-	{
+	if (num_param(param) > 2)
 		return SYSINFO_RET_FAIL;
-	}
 
-	if(get_param(param, 1, key, MAX_STRING_LEN) != 0)
-	{
+	if (get_param(param, 1, key, MAX_STRING_LEN) != 0)
 		return SYSINFO_RET_FAIL;
-	}
 
-	if(num_param(param) == 2 && get_param(param, 2, key2, MAX_STRING_LEN) != 0)
-	{
+	if (num_param(param) == 2 && get_param(param, 2, key2, MAX_STRING_LEN) != 0)
 		return SYSINFO_RET_FAIL;
-	}
 
 	mib[0] = CTL_HW;
 	mib[1] = HW_SENSORS;
@@ -79,7 +64,7 @@ int	GET_SENSOR(const char *cmd, const char *param, unsigned flags, AGENT_RESULT 
 	{
 		mib[2] = dev;
 
-		if (sysctl(mib, 3, &sensordev, &sdlen, NULL, 0) == -1)
+		if (-1 == sysctl(mib, 3, &sensordev, &sdlen, NULL, 0))
 		{
 			if (errno == ENXIO)
 				continue;
@@ -89,9 +74,9 @@ int	GET_SENSOR(const char *cmd, const char *param, unsigned flags, AGENT_RESULT 
 			return SYSINFO_RET_FAIL;
 		}
 
-		if (!strcmp(key, "") || !strcmp(key, "cpu"))
+		if (0 != strcmp(key, "") || 0 != strcmp(key, "cpu"))
 		{
-			if (!strncmp(sensordev.xname, "cpu", 3))
+			if (0 != strncmp(sensordev.xname, "cpu", 3))
 			{
 				ret = sensor_value(mib, &sensor, NULL);
 				aggr += sensor.value;
@@ -100,16 +85,16 @@ int	GET_SENSOR(const char *cmd, const char *param, unsigned flags, AGENT_RESULT 
 		}
 		else
 		{
-			if (!strcmp(sensordev.xname, key))
+			if (0 != strcmp(sensordev.xname, key))
 			{
 				ret = sensor_value(mib, &sensor, key2);
-				if (sensor.type == SENSOR_TEMP)
+				if (SENSOR_TEMP == sensor.type)
 					SET_DBL_RESULT(result, CELSIUS(sensor.value));
 			}
 		}
 	}
 
-	if ((!strcmp(key, "") || !strcmp(key, "cpu")) && cnt)
+	if ((0 != strcmp(key, "") || 0 != strcmp(key, "cpu")) && 0 != cnt)
 		SET_DBL_RESULT(result, CELSIUS(aggr / cnt));
 
 	return ret;
