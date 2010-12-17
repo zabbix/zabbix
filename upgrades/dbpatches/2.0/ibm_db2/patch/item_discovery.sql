19ab21a7368e8d2e80e28f57a035a4d07fad5cce
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2010-12-17 14:14:12 +0000

    - [ZBXNEXT-575] fixes in upgrade patches after merge from trunk

--- upgrades/dbpatches/2.0/ibm_db2/patch/item_discovery.sql
+++ upgrades/dbpatches/2.0/ibm_db2/patch/item_discovery.sql
@@ -4,11 +4,19 @@ CREATE TABLE item_discovery (
 	parent_itemid            bigint                                    NOT NULL,
 	key_                     varchar(255)    WITH DEFAULT ''           NOT NULL,
 	PRIMARY KEY (itemdiscoveryid)
-);
-REORG TABLE item_discovery;
-CREATE UNIQUE INDEX item_discovery_1 on item_discovery (itemid,parent_itemid);
-REORG TABLE item_discovery;
-ALTER TABLE item_discovery ADD CONSTRAINT c_item_discovery_1 FOREIGN KEY (itemid) REFERENCES items (itemid) ON DELETE CASCADE;
-REORG TABLE item_discovery;
-ALTER TABLE item_discovery ADD CONSTRAINT c_item_discovery_2 FOREIGN KEY (parent_itemid) REFERENCES items (itemid) ON DELETE CASCADE;
-REORG TABLE item_discovery;
+)
+/
+REORG TABLE item_discovery
+/
+CREATE UNIQUE INDEX item_discovery_1 on item_discovery (itemid,parent_itemid)
+/
+REORG TABLE item_discovery
+/
+ALTER TABLE item_discovery ADD CONSTRAINT c_item_discovery_1 FOREIGN KEY (itemid) REFERENCES items (itemid) ON DELETE CASCADE
+/
+REORG TABLE item_discovery
+/
+ALTER TABLE item_discovery ADD CONSTRAINT c_item_discovery_2 FOREIGN KEY (parent_itemid) REFERENCES items (itemid) ON DELETE CASCADE
+/
+REORG TABLE item_discovery
+/
