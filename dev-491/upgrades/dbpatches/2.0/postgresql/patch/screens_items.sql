2e08af19d4cdf6e64760520bed0ea6cb644edb9b
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-12-17 10:34:12 +0000

    - [DEV-491] implemented to trunk as ZBXNEXT-587
    git-svn-id: svn://svn.zabbix.com/branches/dev@16198 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- dev-491/upgrades/dbpatches/2.0/postgresql/patch/screens_items.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ONLY screens_items ALTER screenitemid DROP DEFAULT,
-			       ALTER screenid DROP DEFAULT;
-DELETE FROM screens_items WHERE NOT EXISTS (SELECT 1 FROM screens WHERE screens.screenid=screens_items.screenid);
-ALTER TABLE ONLY screens_items ADD CONSTRAINT c_screens_items_1 FOREIGN KEY (screenid) REFERENCES screens (screenid) ON DELETE CASCADE;
