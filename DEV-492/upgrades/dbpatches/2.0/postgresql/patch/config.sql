d71f5a5cb3e7409565f569770c71b9043850f38e
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-12-17 17:18:22 +0000

    - [DEV-492] create dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@16230 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ DEV-492/upgrades/dbpatches/2.0/postgresql/patch/config.sql
@@ -0,0 +1,10 @@
+ALTER TABLE ONLY config ALTER configid DROP DEFAULT,
+			ALTER alert_usrgrpid DROP DEFAULT,
+			ALTER alert_usrgrpid DROP NOT NULL,
+			ALTER discovery_groupid DROP DEFAULT,
+			ADD ns_support integer DEFAULT '0' NOT NULL;
+UPDATE config SET alert_usrgrpid=NULL WHERE NOT EXISTS (SELECT 1 FROM usrgrp WHERE usrgrp.usrgrpid=config.alert_usrgrpid);
+UPDATE config SET discovery_groupid=NULL WHERE NOT EXISTS (SELECT 1 FROM groups WHERE groups.groupid=config.discovery_groupid);
+UPDATE config SET default_theme='css_ob.css' WHERE default_theme='default.css';
+ALTER TABLE ONLY config ADD CONSTRAINT c_config_1 FOREIGN KEY (alert_usrgrpid) REFERENCES usrgrp (usrgrpid);
+ALTER TABLE ONLY config ADD CONSTRAINT c_config_2 FOREIGN KEY (discovery_groupid) REFERENCES groups (groupid);
