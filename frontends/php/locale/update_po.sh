d5d4b86c93dc4ca73d840833d845233d8202f066
Rihards Olups <git-no-reply@zabbix.com>
2010-12-21 09:30:01 +0000

    - [ZBX-2647] include all php files in translatable string search

--- frontends/php/locale/update_po.sh
+++ frontends/php/locale/update_po.sh
@@ -2,7 +2,10 @@
 
 echo "Generating translation template..."
 
-xgettext --files-from=POTFILES.in --from-code=UTF-8 --directory=.. \
+# xgettext will be used on all php files
+find .. -type f -name '*.php' > POTFILES.in
+
+xgettext --files-from=POTFILES.in --from-code=UTF-8 \
 --output=frontend.pot --copyright-holder="SIA Zabbix" --no-wrap \
 --add-comments="GETTEXT:" --keyword=_n:1,2
 
