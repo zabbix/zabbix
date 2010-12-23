ed37a983822ce85736fef233b2762cfcc2e7092c
vladimir <vladimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-12-23 14:07:47 +0000

    - [ZBX-3314] remove
    git-svn-id: svn://svn.zabbix.com/branches/dev@16406 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3314/upgrades/dbpatches/2.0/ibm_db2/patch/opconditions.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE opconditions ALTER COLUMN opconditionid SET WITH DEFAULT NULL;
-REORG TABLE opconditions;
-ALTER TABLE opconditions ALTER COLUMN operationid SET WITH DEFAULT NULL;
-REORG TABLE opconditions;
-DELETE FROM opconditions WHERE NOT operationid IN (SELECT operationid FROM operations);
-ALTER TABLE opconditions ADD CONSTRAINT c_opconditions_1 FOREIGN KEY (operationid) REFERENCES operations (operationid) ON DELETE CASCADE;
-REORG TABLE opconditions;
