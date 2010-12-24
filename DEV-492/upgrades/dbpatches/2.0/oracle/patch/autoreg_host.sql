c5744716cd9108e1d710291feb2be29c18067613
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-12-24 23:19:17 +0000

    - [DEV-492] removed dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@16423 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- DEV-492/upgrades/dbpatches/2.0/oracle/patch/autoreg_host.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE autoreg_host MODIFY autoreg_hostid DEFAULT NULL;
-ALTER TABLE autoreg_host MODIFY proxy_hostid DEFAULT NULL;
-ALTER TABLE autoreg_host MODIFY proxy_hostid NULL;
-ALTER TABLE autoreg_host ADD listen_ip nvarchar2(39) DEFAULT '';
-ALTER TABLE autoreg_host ADD listen_port number(10) DEFAULT '0' NOT NULL;
-UPDATE autoreg_host SET proxy_hostid=NULL WHERE proxy_hostid=0;
-DELETE FROM autoreg_host WHERE NOT proxy_hostid IS NULL AND NOT proxy_hostid IN (SELECT hostid FROM hosts);
-ALTER TABLE autoreg_host ADD CONSTRAINT c_autoreg_host_1 FOREIGN KEY (proxy_hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
--- DEV-492/upgrades/dbpatches/2.0/postgresql/patch/acknowledges.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE ONLY acknowledges ALTER acknowledgeid DROP DEFAULT,
-			      ALTER userid DROP DEFAULT,
-			      ALTER eventid DROP DEFAULT;
-DELETE FROM acknowledges WHERE NOT EXISTS (SELECT 1 FROM users WHERE users.userid=acknowledges.userid);
-DELETE FROM acknowledges WHERE NOT EXISTS (SELECT 1 FROM events WHERE events.eventid=acknowledges.eventid);
-ALTER TABLE ONLY acknowledges ADD CONSTRAINT c_acknowledges_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
-ALTER TABLE ONLY acknowledges ADD CONSTRAINT c_acknowledges_2 FOREIGN KEY (eventid) REFERENCES events (eventid) ON DELETE CASCADE;
