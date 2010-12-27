b6a593ff34a98695ded4ae84ad2631aaefa5b5ed
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-12-27 07:57:09 +0000

    - [ZBX-3260] Removing development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@16435 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3260/upgrades/dbpatches/2.0/postgresql/patch/config.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE ONLY config ALTER configid DROP DEFAULT,
-			ALTER alert_usrgrpid DROP DEFAULT,
-			ALTER alert_usrgrpid DROP NOT NULL,
-			ALTER discovery_groupid DROP DEFAULT,
-			ADD ns_support integer DEFAULT '0' NOT NULL;
-UPDATE config SET alert_usrgrpid=NULL WHERE NOT EXISTS (SELECT 1 FROM usrgrp WHERE usrgrp.usrgrpid=config.alert_usrgrpid);
-UPDATE config SET discovery_groupid=NULL WHERE NOT EXISTS (SELECT 1 FROM groups WHERE groups.groupid=config.discovery_groupid);
-ALTER TABLE ONLY config ADD CONSTRAINT c_config_1 FOREIGN KEY (alert_usrgrpid) REFERENCES usrgrp (usrgrpid);
-ALTER TABLE ONLY config ADD CONSTRAINT c_config_2 FOREIGN KEY (discovery_groupid) REFERENCES groups (groupid);
