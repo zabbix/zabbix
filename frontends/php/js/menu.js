8b344a3cd8bfb5dd003076ba1f15651ce6617cb2
vladimir <vladimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2010-12-28 15:15:15 +0000

    - [ZBX-3241] fixed incorrectly shortened popup texts
    git-svn-id: svn://svn.zabbix.com/trunk@16499 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/js/menu.js
+++ frontends/php/js/menu.js
@@ -422,23 +422,41 @@ function menu_item (o_parent, n_order) {
 	eldiv.className = this.getstyle(1, 0);
 
 	//truncating long strings - they don't fit in the popup menu'
-	if(typeof(this.a_config[0])=='string' && this.a_config[0].length > 20){
-		eldiv.innerHTML = this.a_config[0].substring(0, 20) + '...';
+	if(typeof(this.a_config[0])=='string' && this.a_config[0].length > 35)
 		eldiv.setAttribute('title', this.a_config[0]);
-	}
-	else{
-		eldiv.innerHTML = this.a_config[0];
-	}
+
+	eldiv.innerHTML = this.a_config[0];
 
 	el.appendChild(eldiv);
 
 //	console.log(el,el.innerHTML);
 	document.body.appendChild(el);
 
+
 	this.e_ielement = document.getElementById('e' + o_root.n_id + '_' + this.n_id + 'i');
 	this.e_oelement = document.getElementById('e' + o_root.n_id + '_' + this.n_id + 'o');
 
 	this.b_visible = !this.n_depth;
+
+	var newResult = 0;
+	var nText = '';
+
+	newResult = (this.e_ielement.scrollWidth - this.getprop('width'));
+	if(newResult > 0){
+		// anti down
+		var x=500;
+		while(x){
+			newResult = (this.e_ielement.scrollWidth - this.getprop('width'));
+			nText = this.e_ielement.innerHTML;
+			this.e_ielement.innerHTML = nText.substring(0, nText.length-10);
+			x--;
+			if(newResult < 1) {
+				this.e_ielement.innerHTML = this.e_ielement.innerHTML + '...';
+				x=0;
+				break;
+			}
+		}
+	}
 
 	// no more initialization if leaf
 	if (this.a_config.length < item_offset)
