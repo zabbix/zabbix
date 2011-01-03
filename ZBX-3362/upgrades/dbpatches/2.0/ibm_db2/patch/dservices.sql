86f82f976809476955d661e0c32ce79911ddd9e7
vladimir <vladimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-03 15:42:08 +0000

    - [ZBX-3362] create
    git-svn-id: svn://svn.zabbix.com/branches/dev@16598 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-3362/upgrades/dbpatches/2.0/ibm_db2/patch/dservices.sql
@@ -0,0 +1,12 @@
+ALTER TABLE dservices ALTER COLUMN dserviceid SET WITH DEFAULT NULL;
+REORG TABLE dservices;
+ALTER TABLE dservices ALTER COLUMN dhostid SET WITH DEFAULT NULL;
+REORG TABLE dservices;
+ALTER TABLE dservices ALTER COLUMN dcheckid SET WITH DEFAULT NULL;
+REORG TABLE dservices;
+DELETE FROM dservices WHERE NOT dhostid IN (SELECT dhostid FROM dhosts);
+DELETE FROM dservices WHERE NOT dcheckid IN (SELECT dcheckid FROM dchecks);
+ALTER TABLE dservices ADD CONSTRAINT c_dservices_1 FOREIGN KEY (dhostid) REFERENCES dhosts (dhostid) ON DELETE CASCADE;
+REORG TABLE dservices;
+ALTER TABLE dservices ADD CONSTRAINT c_dservices_2 FOREIGN KEY (dcheckid) REFERENCES dchecks (dcheckid) ON DELETE CASCADE;
+REORG TABLE dservices;
