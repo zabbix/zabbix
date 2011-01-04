2c057508d71bee21dccee875c2a5c1a268feefdc
vladimir <vladimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-04 15:52:18 +0000

    - [ZBXNEXT-602] create
    git-svn-id: svn://svn.zabbix.com/branches/dev@16608 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-602/frontends/php/include/classes/class.cbuttoncancel.php
@@ -0,0 +1,39 @@
+<?php
+/*
+** ZABBIX
+** Copyright (C) 2000-2009 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+?>
+<?php
+class CButtonCancel extends CButton{
+	public function __construct($vars=NULL, $action=NULL, $class=null){
+		parent::__construct('cancel',S_CANCEL, $action, $class);
+
+		if(is_null($action)) $this->setVars($vars);
+	}
+
+	public function setVars($value=NULL){
+		$url = '?cancel=1';
+		if(!is_null($value)) $url.= $value;
+
+		$uri = new Curl($url);
+		$url = $uri->getUrl();
+
+		return $this->setAttribute('onclick', "javascript: return redirect('".$url."');");
+	}
+}
+?>
---
+++ ZBXNEXT-602/frontends/php/include/classes/class.ctextarea.php
@@ -0,0 +1,58 @@
+<?php
+/*
+** ZABBIX
+** Copyright (C) 2000-2009 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+?>
+<?php
+class CTextArea extends CTag{
+/* public */
+	public function __construct($name='textarea',$value='',$cols=77,$rows=7, $readonly=false){
+		parent::__construct('textarea','yes');
+		$this->attributes['class'] = 'input';
+
+		$this->attributes['id'] = $name;
+		$this->attributes['name'] = $name;
+
+		$this->attributes['rows'] = $rows;
+		$this->attributes['cols'] = $cols;
+		$this->setReadonly($readonly);
+
+		$this->addItem($value);
+	}
+
+	public function setReadonly($value=true){
+		if($value)
+			$this->attributes['readonly'] = 'readonly';
+		else
+			$this->removeAttribute('readonly');
+	}
+
+	public function setValue($value=''){
+		return $this->addItem($value);
+	}
+
+	public function setRows($value){
+		return $this->attributes['rows'] = $value;
+	}
+
+	public function setCols($value){
+		return $this->attributes['cols'] = $value;
+
+	}
+}
+?>
