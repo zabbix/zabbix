fcca303ed7273e8f4fc9b6957f6b348599d400ca
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-04 12:24:30 +0000

    - [DEV-493] updated version and release date
    git-svn-id: svn://svn.zabbix.com/branches/1.8@16601 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -117,8 +117,8 @@
 #define OFF	0
 
 #define	APPLICATION_NAME	"Zabbix Agent"
-#define	ZABBIX_REVDATE		"30 December 2010"
-#define	ZABBIX_VERSION		"1.8.4rc4"
+#define	ZABBIX_REVDATE		"4 January 2011"
+#define	ZABBIX_VERSION		"1.8.4"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
 #if defined(_WINDOWS)
