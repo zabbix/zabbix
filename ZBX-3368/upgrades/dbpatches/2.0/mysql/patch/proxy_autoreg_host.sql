f3a63967acea5b77a95b17495b1e2332f49702dd
vladimir <vladimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-05 19:10:49 +0000

    - [ZBX-3368] create
    git-svn-id: svn://svn.zabbix.com/branches/dev@16642 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-3368/upgrades/dbpatches/2.0/mysql/patch/proxy_autoreg_host.sql
@@ -0,0 +1,2 @@
+ALTER TABLE proxy_autoreg_host ADD listen_ip varchar(39) DEFAULT '' NOT NULL,
+			       ADD listen_port integer DEFAULT '0' NOT NULL;
---
+++ ZBX-3368/upgrades/dbpatches/2.0/postgresql/patch/scripts.sql
@@ -0,0 +1,11 @@
+ALTER TABLE ONLY scripts ALTER scriptid DROP DEFAULT,
+			 ALTER usrgrpid DROP DEFAULT,
+			 ALTER usrgrpid DROP NOT NULL,
+			 ALTER groupid DROP DEFAULT,
+			 ALTER groupid DROP NOT NULL;
+UPDATE scripts SET usrgrpid=NULL WHERE usrgrpid=0;
+UPDATE scripts SET groupid=NULL WHERE groupid=0;
+DELETE FROM scripts WHERE usrgrpid IS NOT NULL AND NOT EXISTS (SELECT 1 FROM usrgrp WHERE usrgrp.usrgrpid=scripts.usrgrpid);
+DELETE FROM scripts WHERE groupid IS NOT NULL AND NOT EXISTS (SELECT 1 FROM groups WHERE groups.groupid=scripts.groupid);
+ALTER TABLE ONLY scripts ADD CONSTRAINT c_scripts_1 FOREIGN KEY (usrgrpid) REFERENCES usrgrp (usrgrpid);
+ALTER TABLE ONLY scripts ADD CONSTRAINT c_scripts_2 FOREIGN KEY (groupid) REFERENCES groups (groupid);
