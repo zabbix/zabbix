5215801e9e672eab7be6aa04524ff923045a5d35
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-06 09:37:38 +0000

    - [ZBX-3355] typos + comments
    git-svn-id: svn://svn.zabbix.com/branches/dev@16655 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3355/frontends/php/tests/general/function_gettext.php
+++ ZBX-3355/frontends/php/tests/general/function_gettext.php
@@ -19,22 +19,32 @@
 **/
 ?>
 <?php
+/**
+ * Testing gettext related functions
+ */
+
 require_once 'PHPUnit/Framework.php';
 
 require_once(dirname(__FILE__).'/../../include/gettextwrapper.inc.php');
 
 class function_gettext extends PHPUnit_Framework_TestCase
 {
+	/**
+	 * Data provider from for _s() function
+	 */
 	public static function provider_s()
 	{
 		return array(
 			array('host "%s" has been deleted', array('Host 1'), 'host "Host 1" has been deleted'),
 			array('id=%d', array(8), 'id=8'),
 			array('roses are %1$s, violets are %2$s, all by %3$s are belong to %4$s', array('red','blue','base','you'), 'roses are red, violets are blue, all by base are belong to you'),
-			array('reverse argument oreder: %2$s, %1$s', array('first','second'), 'reverse argument oreder: second, first'),
+			array('reverse argument order: %2$s, %1$s', array('first','second'), 'reverse argument order: second, first'),
 		);
 	}
 
+	/**
+	 * Data provider from for _n() function
+	 */
 	public static function provider_n()
 	{
 		return array(
@@ -45,8 +55,8 @@ class function_gettext extends PHPUnit_Framework_TestCase
 			array('deleted %d host from template "%s"', 'deleted %d hosts from template "%s"', array(1, 'my_template'), 'deleted 1 host from template "my_template"'),
 			array('deleted %d host from template "%s"', 'deleted %d hosts from template "%s"', array(90, 'my_template'), 'deleted 90 hosts from template "my_template"'),
 
-			array('on table %2$s, there is %1$d cake', 'on table %2$s, there is %1$d cakes', array(1, 'table_name'), 'on table table_name, there is 1 cake'),
-			array('on table %2$s, there is %1$d cake', 'on table %2$s, there is %1$d cakes', array(2, 'table_name'), 'on table table_name, there is 2 cakes'),
+			array('on table %2$s, there is %1$d cake', 'on table %2$s, there are %1$d cakes', array(1, 'table_name'), 'on table table_name, there is 1 cake'),
+			array('on table %2$s, there is %1$d cake', 'on table %2$s, there are %1$d cakes', array(2, 'table_name'), 'on table table_name, there are 2 cakes'),
 
 			array('%3$s, %4$s, %2$s, %1$d thing', '%3$s, %4$s, %2$s, %1$d things', array(1, 'foo', 'bar', 'bla'), 'bar, bla, foo, 1 thing'),
 			array('%3$s, %4$s, %2$s, %1$d thing', '%3$s, %4$s, %2$s, %1$d things', array(56, 'foo', 'bar', 'bla'), 'bar, bla, foo, 56 things'),
@@ -54,6 +64,7 @@ class function_gettext extends PHPUnit_Framework_TestCase
 	}
 
 	/**
+	* Test _s() function
 	* @dataProvider provider_s
 	*/
 	public function test_gettext_s($string, $parameters, $expected_result)
@@ -64,6 +75,7 @@ class function_gettext extends PHPUnit_Framework_TestCase
 	}
 
 	/**
+	* Test _n() function
 	* @dataProvider provider_n
 	*/
 	public function test_gettext_n($string1, $string2, $parameters, $expected_result)
