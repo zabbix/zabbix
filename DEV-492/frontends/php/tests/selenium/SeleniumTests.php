af37ec0ccd2e7c3a7bdaefb2b67418e2491437d1
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-07 17:08:16 +0000

    - [DEV-492] new branch created
    git-svn-id: svn://svn.zabbix.com/branches/dev@16711 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ DEV-492/frontends/php/tests/selenium/SeleniumTests.php
@@ -0,0 +1,53 @@
+<?php
+/*
+** ZABBIX
+** Copyright (C) 2000-2010 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+?>
+<?php
+require_once(dirname(__FILE__).'/testGeneric.php');
+require_once(dirname(__FILE__).'/testClicks.php');
+require_once(dirname(__FILE__).'/testPageDashboard.php');
+require_once(dirname(__FILE__).'/testPageHosts.php');
+require_once(dirname(__FILE__).'/testPageSearch.php');
+require_once(dirname(__FILE__).'/testFormHost.php');
+require_once(dirname(__FILE__).'/testFormHostGroup.php');
+require_once(dirname(__FILE__).'/testFormProfile.php');
+require_once(dirname(__FILE__).'/testFormScreen.php');
+// ...
+
+class SeleniumTests
+{
+	public static function suite()
+	{
+		$suite = new PHPUnit_Framework_TestSuite('selenium');
+
+		$suite->addTestSuite('testGeneric');
+		$suite->addTestSuite('testClicks');
+		$suite->addTestSuite('testPageDashboard');
+		$suite->addTestSuite('testPageSearch');
+		$suite->addTestSuite('testFormHost');
+		$suite->addTestSuite('testFormHostGroup');
+		$suite->addTestSuite('testFormProfile');
+		$suite->addTestSuite('testFormScreen');
+		$suite->addTestSuite('testPageHosts');
+		// ...
+
+		return $suite;
+	}
+}
+?>
