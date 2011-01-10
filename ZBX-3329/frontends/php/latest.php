dc9d93a2f851037b1106e2d7cbd14f21351bf581
vladimir <vladimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-10 12:16:44 +0000

    - [ZBX-3329] fixed lastvalue
    git-svn-id: svn://svn.zabbix.com/branches/dev@16738 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3329/frontends/php/latest.php
+++ ZBX-3329/frontends/php/latest.php
@@ -279,7 +279,11 @@ include_once('include/page_header.php');
 		else
 			$lastclock = ' - ';
 
-		$lastvalue = format_lastvalue($db_item);
+		if(isset($db_item["lastclock"]) && $db_item["lastclock"] != 0){
+			$lastvalue = format_lastvalue($db_item);
+		}else{
+			$lastvalue = ' - ';
+		}
 
 		if(isset($db_item['lastvalue']) && isset($db_item['prevvalue'])
 				&& in_array($db_item['value_type'], array(ITEM_VALUE_TYPE_FLOAT, ITEM_VALUE_TYPE_UINT64))
