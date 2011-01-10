d6eb022392f32b13a5d0dd8d312baa9530a8023b
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-10 00:00:57 +0000

    - [DEV-492] removed dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@16735 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- DEV-492/upgrades/dbpatches/2.0/postgresql/patch/proxy_dhistory.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM proxy_dhistory WHERE NOT EXISTS (SELECT 1 FROM drules WHERE drules.druleid=proxy_dhistory.druleid);
-DELETE FROM proxy_dhistory WHERE NOT EXISTS (SELECT 1 FROM dchecks WHERE dchecks.dcheckid=proxy_dhistory.dcheckid);
-ALTER TABLE ONLY proxy_dhistory ALTER druleid DROP DEFAULT;
-ALTER TABLE ONLY proxy_dhistory ALTER dcheckid DROP DEFAULT;
