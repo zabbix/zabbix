0f80a3b436f1414f7b7893ee1324694e7dc86a0e
vladimir <vladimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-13 10:43:33 +0000

    - [ZBX-3391] create
    git-svn-id: svn://svn.zabbix.com/branches/dev@16854 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-3391/frontends/php/tests/selenium/testPageTemplates.php
@@ -0,0 +1,118 @@
+<?php
+/*
+** ZABBIX
+** Copyright (C) 2000-2010 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+?>
+<?php
+require_once(dirname(__FILE__).'/class.ctest.php');
+
+class testPageTemplates extends CTest
+{
+	// Returns all templates
+	public static function allTemplates()
+	{
+		return DBdata("select * from hosts where status in (".HOST_STATUS_TEMPLATE.')');
+	}
+
+	/**
+	* @dataProvider allTemplates
+	*/
+	public function testPageTemplates_SimpleTest($template)
+	{
+		$this->login('templates.php');
+		$this->dropdown_select('groupid','Templates');
+//		$this->wait();
+		$this->assertTitle('Templates');
+		$this->ok('CONFIGURATION OF TEMPLATES');
+		$this->ok('Displaying');
+		// Header
+		$this->ok(array('Templates','Applications','Items','Triggers','Graphs','Screens','Discovery','Linked templates','Linked to'));
+		// Data
+		$this->ok(array($template['host']));
+		$this->dropdown_select('go','Export selected');
+		$this->dropdown_select('go','Delete selected');
+		$this->dropdown_select('go','Delete selected with linked elements');
+	}
+
+	/**
+	* @dataProvider allTemplates
+	*/
+	public function testPageTemplates_SimpleUpdate($template)
+	{
+		$name=$template['host'];
+
+		$sql1="select * from hosts where host='$name'";
+		$oldHashTemplate=$this->DBhash($sql1);
+		$sql2="select * from hosts order by hostid";
+		$oldHashHosts=$this->DBhash($sql2);
+		$sql3="select * from items order by itemid";
+		$oldHashItems=$this->DBhash($sql3);
+		$sql4="select * from triggers order by triggerid";
+		$oldHashTriggers=$this->DBhash($sql4);
+
+		$this->login('templates.php');
+		$this->dropdown_select('groupid','all');
+
+		$this->assertTitle('Templates');
+
+		$this->click("link=$name");
+		$this->wait();
+		$this->button_click('save');
+		$this->wait();
+		$this->assertTitle('Templates');
+		$this->ok('Template updated');
+		$this->ok("$name");
+		$this->ok('CONFIGURATION OF TEMPLATES');
+
+		$this->assertEquals($oldHashTemplate,$this->DBhash($sql1));
+		$this->assertEquals($oldHashHosts,$this->DBhash($sql2));
+		$this->assertEquals($oldHashItems,$this->DBhash($sql3));
+		$this->assertEquals($oldHashTriggers,$this->DBhash($sql4));
+	}
+
+	public function testPageTemplates_Create()
+	{
+// TODO
+		$this->markTestIncomplete();
+	}
+
+	public function testPageTemplates_Import()
+	{
+// TODO
+		$this->markTestIncomplete();
+	}
+
+	public function testPageTemplates_MassExport()
+	{
+// TODO
+		$this->markTestIncomplete();
+	}
+
+	public function testPageTemplates_MassDelete()
+	{
+// TODO
+		$this->markTestIncomplete();
+	}
+
+	public function testPageTemplates_MassDeleteWithLinkedElements()
+	{
+// TODO
+		$this->markTestIncomplete();
+	}
+}
+?>
---
+++ ZBX-3391/frontends/php/tests/selenium/testPageUserGroups.php
@@ -0,0 +1,124 @@
+<?php
+/*
+** ZABBIX
+** Copyright (C) 2000-2010 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+?>
+<?php
+require_once(dirname(__FILE__).'/class.ctest.php');
+
+class testPageUserGroups extends CTest
+{
+	// Returns all user groups
+	public static function allGroups()
+	{
+		return DBdata("select * from usrgrp where name<>'Disabled' order by usrgrpid");
+	}
+
+	/**
+	* @dataProvider allGroups
+	*/
+	public function testPageUserGroups_SimpleTest($group)
+	{
+		$this->login('usergrps.php');
+		$this->assertTitle('User groups');
+		$this->ok('CONFIGURATION OF USERS AND USER GROUPS');
+		$this->ok('Displaying');
+		// Header
+		$this->ok(array('Name','#','Members','Users status','GUI access','API access','Debug mode'));
+		// Data
+		$this->ok(array($group['name']));
+		$this->dropdown_select('go','Enable selected');
+		$this->dropdown_select('go','Disable selected');
+		$this->dropdown_select('go','Enable API');
+		$this->dropdown_select('go','Disable API');
+		$this->dropdown_select('go','Enable DEBUG');
+		$this->dropdown_select('go','Disable DEBUG');
+		$this->dropdown_select('go','Delete selected');
+	}
+
+	/**
+	* @dataProvider allGroups
+	*/
+	public function testPageUserGroups_SimpleUpdate($group)
+	{
+		$usrgrpid=$group['usrgrpid'];
+		$name=$group['name'];
+
+		$sql1="select * from usrgrp where name='$name' order by usrgrpid";
+		$oldHashGroup=$this->DBhash($sql1);
+		$sql2="select * from users_groups where usrgrpid=$usrgrpid order by id";
+		$oldHashUsersGroups=$this->DBhash($sql2);
+
+		$this->login('usergrps.php');
+		$this->assertTitle('User groups');
+		$this->click("link=$name");
+		$this->wait();
+		$this->button_click('save');
+		$this->wait();
+		$this->assertTitle('User groups');
+		$this->ok('Group updated');
+		$this->ok("$name");
+		$this->ok('CONFIGURATION OF USERS AND USER GROUPS');
+
+		$this->assertEquals($oldHashGroup,$this->DBhash($sql1));
+		$this->assertEquals($oldHashUsersGroups,$this->DBhash($sql2));
+	}
+
+	public function testPageUserGroups_MassEnable()
+	{
+// TODO
+		$this->markTestIncomplete();
+	}
+
+	public function testPageUserGroups_MassDisable()
+	{
+// TODO
+		$this->markTestIncomplete();
+	}
+
+	public function testPageUserGroups_MassEnableAPI()
+	{
+// TODO
+		$this->markTestIncomplete();
+	}
+
+	public function testPageUserGroups_MassDisableAPI()
+	{
+// TODO
+		$this->markTestIncomplete();
+	}
+
+	public function testPageUserGroups_MassEnableDEBUG()
+	{
+// TODO
+		$this->markTestIncomplete();
+	}
+
+	public function testPageUserGroups_MassDisableDEBUG()
+	{
+// TODO
+		$this->markTestIncomplete();
+	}
+
+	public function testPageUserGroups_MassDelete()
+	{
+// TODO
+		$this->markTestIncomplete();
+	}
+}
+?>
