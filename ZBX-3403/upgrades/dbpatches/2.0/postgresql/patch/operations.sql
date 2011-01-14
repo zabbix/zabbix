c7087cb63867eff5a26d76b40300a2ac9316969e
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-14 10:40:32 +0000

    - [ZBX-3403] deleted development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@16910 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3403/upgrades/dbpatches/2.0/postgresql/patch/operations.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE ONLY operations ALTER operationid DROP DEFAULT,
-			    ALTER actionid DROP DEFAULT,
-			    ADD mediatypeid bigint NULL;
-DELETE FROM operations WHERE NOT EXISTS (SELECT 1 FROM actions WHERE actions.actionid=operations.actionid);
-ALTER TABLE ONLY operations ADD CONSTRAINT c_operations_1 FOREIGN KEY (actionid) REFERENCES actions (actionid) ON DELETE CASCADE;
-ALTER TABLE ONLY operations ADD CONSTRAINT c_operations_2 FOREIGN KEY (mediatypeid) REFERENCES media_type (mediatypeid) ON DELETE CASCADE;
-UPDATE operations SET mediatypeid=(SELECT mediatypeid FROM opmediatypes WHERE operationid = operations.operationid);
-DROP TABLE opmediatypes;
