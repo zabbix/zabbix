2436dfde531ab28430c665017bf548c4122d6db2
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-19 09:04:58 +0000

    - [ZBX-3434] Removing branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@17014 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3434/upgrades/dbpatches/1.8/oracle/patch/autoreg_host.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE autoreg_host (
-        autoreg_hostid          number(20)              DEFAULT '0'     NOT NULL,
-        proxy_hostid            number(20)              DEFAULT '0'     NOT NULL,
-        host            nvarchar2(64)           DEFAULT ''      ,
-        PRIMARY KEY (autoreg_hostid)
-);
-CREATE INDEX autoreg_host_1 on autoreg_host (proxy_hostid,host);
