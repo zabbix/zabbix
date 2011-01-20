4962bca1c4f2f51e31cec17f4a464cc0d3fa6fe4
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-20 22:24:51 +0000

    - [DEV-492] more tests for the list of actions
    git-svn-id: svn://svn.zabbix.com/trunk@17063 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/tests/selenium/testPageActionsTriggers.php
+++ frontends/php/tests/selenium/testPageActionsTriggers.php
@@ -89,10 +89,35 @@ class testPageActionsTriggers extends CWebTest
 	/**
 	* @dataProvider allActions
 	*/
-	public function testPageActionsTriggers_SingleEnable($action)
+	public function testPageActionsTriggers_SingleEnableDisable($action)
 	{
-// TODO
-		$this->markTestIncomplete();
+		$actionid=$action['actionid'];
+		$name=$action['name'];
+
+		$this->login('actionconf.php?eventsource='.EVENT_SOURCE_TRIGGERS);
+		$this->assertTitle('Configuration of actions');
+		switch($action['status']){
+			case ACTION_STATUS_ENABLED:
+				$this->click("xpath=//a[contains(@href,'actionconf.php?go=disable&g_actionid%5B%5D=$actionid&')]");
+			break;
+			case ACTION_STATUS_DISABLED:
+				$this->click("xpath=//a[contains(@href,'actionconf.php?go=activate&g_actionid%5B%5D=$actionid&')]");
+			break;
+		}
+		$this->wait();
+
+		$this->assertTitle('Configuration of actions');
+		$this->ok('Status updated');
+
+		switch($action['status']){
+			case ACTION_STATUS_ENABLED:
+				$sql="select * from actions where actionid=$actionid and status=".ACTION_STATUS_DISABLED;
+			break;
+			case ACTION_STATUS_DISABLED:
+				$sql="select * from actions where actionid=$actionid and status=".ACTION_STATUS_ENABLED;
+			break;
+		}
+		$this->assertEquals(1,DBcount($sql));
 	}
 
 	public function testPageActionsTriggers_Create()
