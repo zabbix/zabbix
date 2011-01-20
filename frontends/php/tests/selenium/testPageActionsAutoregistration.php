bfdcfc17d0e6932778782488068df6371a23a859
Alexei Vladishev <alexei.vladishev@zabbix.com>
2011-01-20 22:24:51 +0000

    - [DEV-492] more tests for the list of actions

--- frontends/php/tests/selenium/testPageActionsAutoregistration.php
+++ frontends/php/tests/selenium/testPageActionsAutoregistration.php
@@ -88,10 +88,35 @@ class testPageActionsAutoregistration extends CWebTest
 	/**
 	* @dataProvider allActions
 	*/
-	public function testPageActionsAutoregistration_SingleEnable($action)
+	public function testPageActionsAutoregistration_SingleEnableDisable($action)
 	{
-// TODO
-		$this->markTestIncomplete();
+		$actionid=$action['actionid'];
+		$name=$action['name'];
+
+		$this->login('actionconf.php?eventsource='.EVENT_SOURCE_AUTO_REGISTRATION);
+		$this->assertTitle('Configuration of actions');
+		switch($action['status']){
+			case ACTION_STATUS_ENABLED:
+				$this->click("xpath=//a[contains(@href,'actionconf.php?go=disable&g_actionid%5B%5D=$actionid&')]");
+			break;
+			case ACTION_STATUS_DISABLED:
+				$this->click("xpath=//a[contains(@href,'actionconf.php?go=activate&g_actionid%5B%5D=$actionid&')]");
+			break;
+		}
+		$this->wait();
+
+		$this->assertTitle('Configuration of actions');
+		$this->ok('Status updated');
+
+		switch($action['status']){
+			case ACTION_STATUS_ENABLED:
+				$sql="select * from actions where actionid=$actionid and status=".ACTION_STATUS_DISABLED;
+			break;
+			case ACTION_STATUS_DISABLED:
+				$sql="select * from actions where actionid=$actionid and status=".ACTION_STATUS_ENABLED;
+			break;
+		}
+		$this->assertEquals(1,DBcount($sql));
 	}
 
 	public function testPageActionsAutoregistration_Create()
