291f05f11793192373bf2ceed80e72c171d4a491
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-24 13:18:02 +0000

    - [ZBXNEXT-575] partial action form commit (Aly)
    git-svn-id: svn://svn.zabbix.com/branches/dev@17110 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-575/frontends/php/tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copy this file to bootstrap.php and replace URL parameter with
+ * location of the front-end files. For example
+ * http://127.0.0.1/trunk/frontends/php/
+ *
+ * Note that file conf/zabbix.conf.php mus exist and point to the same
+ * database used by the front-end for successful testing.
+ */
+	define('PHPUNIT_URL','http://192.168.3.67/branches/work/frontends/php/');
+?>
