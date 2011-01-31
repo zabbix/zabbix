1fa25709c3dfb9a8d165593f953ca5bdf2dc1788
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-01-31 10:48:49 +0000

    - [ZBXNEXT-587] added missed config table DB patches (Aly)
    git-svn-id: svn://svn.zabbix.com/trunk@17288 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/2.0/postgresql/patch/config.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/config.sql
@@ -2,6 +2,7 @@ ALTER TABLE ONLY config ALTER configid DROP DEFAULT,
 			ALTER alert_usrgrpid DROP DEFAULT,
 			ALTER alert_usrgrpid DROP NOT NULL,
 			ALTER discovery_groupid DROP DEFAULT,
+			ALTER default_theme SET DEFAULT 'css_ob.css',
 			ADD ns_support integer DEFAULT '0' NOT NULL;
 UPDATE config SET alert_usrgrpid=NULL WHERE NOT EXISTS (SELECT 1 FROM usrgrp WHERE usrgrp.usrgrpid=config.alert_usrgrpid);
 UPDATE config SET discovery_groupid=NULL WHERE NOT EXISTS (SELECT 1 FROM groups WHERE groups.groupid=config.discovery_groupid);
