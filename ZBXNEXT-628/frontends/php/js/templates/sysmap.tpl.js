e8c4f8744edee3662e91977b4f7872b6fdf10761
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-02-02 12:41:43 +0000

    - [ZBXNEXT-628] new dev branch trunk (Aly)
    git-svn-id: svn://svn.zabbix.com/branches/dev@17362 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-628/frontends/php/js/templates/sysmap.tpl.js
@@ -0,0 +1,21 @@
+if(typeof(ZBX_TPL) == 'undefined'){
+	var ZBX_TPL = {};
+}
+
+Object.extend(ZBX_TPL,{
+'selementFormUrlContainer': '<td class="form_row_l">'+locale['S_LINKS']+'</td>'+
+		'<td class="form_row_r">'+
+		'<table><tbody id="urlContainer">'+
+			'<tr class="header"><td>'+locale['S_NAME']+'</td><td>'+locale['S_URL']+'</td><td></td></tr>'+
+			'<tr id="urlfooter"><td colspan="3"><span id="newSelementUrl" class="link_menu" title="Add">'+locale['S_ADD']+'</span></td></tr>'+
+		'</tbody></table>'+
+		'</td>',
+'selementFormUrls': '<tr id="urlrow_#{sysmapelementurlid}">'+
+			'<td>'+
+				'<input class="input" name="name" id="url_name_#{sysmapelementurlid}" type="text" size="16" value="#{name}">'+
+			'</td>'+
+			'<td><input class="input" name="url" id="url_url_#{sysmapelementurlid}" type="text" size="32" value="#{url}"></td>'+
+			'<td><span class="link_menu" onclick="$(\'urlrow_#{sysmapelementurlid}\').remove();">'+locale['S_REMOVE']+'</span></td>'+
+		'</tr>'
+}
+);
---
+++ ZBXNEXT-628/upgrades/dbpatches/2.0/ibm_db2/patch/escalations.sql
@@ -0,0 +1,16 @@
+ALTER TABLE escalations ALTER COLUMN escalationid SET WITH DEFAULT NULL;
+REORG TABLE escalations;
+ALTER TABLE escalations ALTER COLUMN actionid SET WITH DEFAULT NULL;
+REORG TABLE escalations;
+ALTER TABLE escalations ALTER COLUMN triggerid SET WITH DEFAULT NULL;
+REORG TABLE escalations;
+ALTER TABLE escalations ALTER COLUMN triggerid DROP NOT NULL;
+REORG TABLE escalations;
+ALTER TABLE escalations ALTER COLUMN eventid SET WITH DEFAULT NULL;
+REORG TABLE escalations;
+ALTER TABLE escalations ALTER COLUMN r_eventid SET WITH DEFAULT NULL;
+REORG TABLE escalations;
+ALTER TABLE escalations ALTER COLUMN r_eventid DROP NOT NULL;
+REORG TABLE escalations;
+UPDATE escalations SET triggerid=NULL WHERE triggerid=0;
+UPDATE escalations SET r_eventid=NULL WHERE r_eventid=0;
