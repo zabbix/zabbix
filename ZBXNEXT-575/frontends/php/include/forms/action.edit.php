657e82ce63afe2cf2174569aa7e88953f2dad39c
artem <artem@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-02-03 11:27:22 +0000

    - [ZBXNEXT-575] form design (Aly)
    git-svn-id: svn://svn.zabbix.com/branches/dev@17385 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-575/frontends/php/include/forms/action.edit.php
+++ ZBXNEXT-575/frontends/php/include/forms/action.edit.php
@@ -457,7 +457,7 @@ require_once('include/templates/action.js.php');
 				_('Period'),
 				new CCol(array(
 					new CNumericBox('new_operation[esc_period]', $new_operation['esc_period'], 5),
-					_('( mininum 60, 0 - use action default)')))
+					_('(mininum 60, 0 - use action default)')))
 			));
 
 			$tblOper->addRow(array(S_STEP, $tblStep));
