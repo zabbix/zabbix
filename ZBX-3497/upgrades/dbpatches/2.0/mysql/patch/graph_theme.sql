c1465c4e3d1b21b4e28995188af8be8826bd4838
vladimir <vladimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-02-07 10:23:05 +0000

    - [ZBX-3497] delete
    git-svn-id: svn://svn.zabbix.com/branches/dev@17474 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3497/upgrades/dbpatches/2.0/mysql/patch/graph_theme.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE graph_theme MODIFY graphthemeid bigint unsigned NOT NULL,
-			CHANGE noneworktimecolor nonworktimecolor varchar(6) DEFAULT 'CCCCCC' NOT NULL;
--- ZBX-3497/upgrades/dbpatches/2.0/oracle/patch/screens.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE screens MODIFY screenid DEFAULT NULL;
-ALTER TABLE screens MODIFY name DEFAULT NULL;
-ALTER TABLE screens ADD templateid number(20) NULL;
-ALTER TABLE screens ADD CONSTRAINT c_screens_1 FOREIGN KEY (templateid) REFERENCES hosts (hostid) ON DELETE CASCADE;
