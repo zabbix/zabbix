bf1a0854b6d1757bcd29e47b5d9b6b3b787a9a14
vladimir <vladimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-02-07 13:34:53 +0000

    - [ZBXNEXT-631] fixed database schema, data and patches.
    git-svn-id: svn://svn.zabbix.com/branches/dev@17493 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-631/upgrades/dbpatches/2.0/postgresql/patch/config.sql
+++ ZBXNEXT-631/upgrades/dbpatches/2.0/postgresql/patch/config.sql
@@ -4,18 +4,18 @@ ALTER TABLE ONLY config ALTER configid DROP DEFAULT,
 			ALTER discovery_groupid DROP DEFAULT,
 			ALTER default_theme SET DEFAULT 'css_ob.css',
 			ADD ns_support integer DEFAULT '0' NOT NULL;
-			ADD severity_color_0 varchar(6) DEFAULT 'information' NOT NULL;
-			ADD severity_color_1 varchar(6) DEFAULT 'information' NOT NULL;
-			ADD severity_color_2 varchar(6) DEFAULT 'warning' NOT NULL;
-			ADD severity_color_3 varchar(6) DEFAULT 'average' NOT NULL;
-			ADD severity_color_4 varchar(6) DEFAULT 'high' NOT NULL;
-			ADD severity_color_5 varchar(6) DEFAULT 'disaster' NOT NULL;
-			ADD severity_name_0 varchar(6) DEFAULT 'Not classified' NOT NULL;
-			ADD severity_name_1 varchar(6) DEFAULT 'Information' NOT NULL;
-			ADD severity_name_2 varchar(6) DEFAULT 'Warning' NOT NULL;
-			ADD severity_name_3 varchar(6) DEFAULT 'Average' NOT NULL;
-			ADD severity_name_4 varchar(6) DEFAULT 'High' NOT NULL;
-			ADD severity_name_5 varchar(6) DEFAULT 'Disaster' NOT NULL;
+			ADD severity_color_0 varchar(6) DEFAULT 'AADDAA' NOT NULL;
+			ADD severity_color_1 varchar(6) DEFAULT 'CCE2CC' NOT NULL;
+			ADD severity_color_2 varchar(6) DEFAULT 'EFEFCC' NOT NULL;
+			ADD severity_color_3 varchar(6) DEFAULT 'DDAAAA' NOT NULL;
+			ADD severity_color_4 varchar(6) DEFAULT 'FF8888' NOT NULL;
+			ADD severity_color_5 varchar(6) DEFAULT 'FF0000' NOT NULL;
+			ADD severity_name_0 varchar(32) DEFAULT 'Not classified' NOT NULL;
+			ADD severity_name_1 varchar(32) DEFAULT 'Information' NOT NULL;
+			ADD severity_name_2 varchar(32) DEFAULT 'Warning' NOT NULL;
+			ADD severity_name_3 varchar(32) DEFAULT 'Average' NOT NULL;
+			ADD severity_name_4 varchar(32) DEFAULT 'High' NOT NULL;
+			ADD severity_name_5 varchar(32) DEFAULT 'Disaster' NOT NULL;
 UPDATE config SET alert_usrgrpid=NULL WHERE NOT EXISTS (SELECT 1 FROM usrgrp WHERE usrgrp.usrgrpid=config.alert_usrgrpid);
 UPDATE config SET discovery_groupid=NULL WHERE NOT EXISTS (SELECT 1 FROM groups WHERE groups.groupid=config.discovery_groupid);
 UPDATE config SET default_theme='css_ob.css' WHERE default_theme='default.css';
