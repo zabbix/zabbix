8e835c4d6af0a772b55f92eb8e65202251318512
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2011-02-09 14:36:55 +0000

    - [ZBXNEXT-575] fixed END-OF-STATEMENT delimiter in IBM DB2 patches

--- upgrades/dbpatches/2.0/ibm_db2/patch/config.sql
+++ upgrades/dbpatches/2.0/ibm_db2/patch/config.sql
@@ -1,20 +1,38 @@
-ALTER TABLE config ALTER COLUMN configid SET WITH DEFAULT NULL;
-REORG TABLE config;
-ALTER TABLE config ALTER COLUMN alert_usrgrpid SET WITH DEFAULT NULL;
-REORG TABLE config;
-ALTER TABLE config ALTER COLUMN alert_usrgrpid DROP NOT NULL;
-REORG TABLE config;
-ALTER TABLE config ALTER COLUMN discovery_groupid SET WITH DEFAULT NULL;
-REORG TABLE config;
-ALTER TABLE config ALTER COLUMN default_theme SET WITH DEFAULT 'css_ob.css';
-REORG TABLE config;
-ALTER TABLE config ADD ns_support integer DEFAULT '0' NOT NULL;
-REORG TABLE config;
-UPDATE config SET alert_usrgrpid=NULL WHERE NOT alert_usrgrpid IN (SELECT usrgrpid FROM usrgrp);
-UPDATE config SET discovery_groupid=NULL WHERE NOT discovery_groupid IN (SELECT groupid FROM groups);
-UPDATE config SET default_theme='css_ob.css' WHERE default_theme='default.css';
-ALTER TABLE config ADD CONSTRAINT c_config_1 FOREIGN KEY (alert_usrgrpid) REFERENCES usrgrp (usrgrpid);
-REORG TABLE config;
-ALTER TABLE config ADD CONSTRAINT c_config_2 FOREIGN KEY (discovery_groupid) REFERENCES groups (groupid);
-REORG TABLE config;
-
+ALTER TABLE config ALTER COLUMN configid SET WITH DEFAULT NULL
+/
+REORG TABLE config
+/
+ALTER TABLE config ALTER COLUMN alert_usrgrpid SET WITH DEFAULT NULL
+/
+REORG TABLE config
+/
+ALTER TABLE config ALTER COLUMN alert_usrgrpid DROP NOT NULL
+/
+REORG TABLE config
+/
+ALTER TABLE config ALTER COLUMN discovery_groupid SET WITH DEFAULT NULL
+/
+REORG TABLE config
+/
+ALTER TABLE config ALTER COLUMN default_theme SET WITH DEFAULT 'css_ob.css'
+/
+REORG TABLE config
+/
+ALTER TABLE config ADD ns_support integer WITH DEFAULT '0' NOT NULL
+/
+REORG TABLE config
+/
+UPDATE config SET alert_usrgrpid=NULL WHERE NOT alert_usrgrpid IN (SELECT usrgrpid FROM usrgrp)
+/
+UPDATE config SET discovery_groupid=NULL WHERE NOT discovery_groupid IN (SELECT groupid FROM groups)
+/
+UPDATE config SET default_theme='css_ob.css' WHERE default_theme='default.css'
+/
+ALTER TABLE config ADD CONSTRAINT c_config_1 FOREIGN KEY (alert_usrgrpid) REFERENCES usrgrp (usrgrpid)
+/
+REORG TABLE config
+/
+ALTER TABLE config ADD CONSTRAINT c_config_2 FOREIGN KEY (discovery_groupid) REFERENCES groups (groupid)
+/
+REORG TABLE config
+/
