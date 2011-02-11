534b710c4c03dbf980d33ca50860d837322d5930
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-02-11 09:34:09 +0000

    - [ZBX-3516] Removed development branch.
    git-svn-id: svn://svn.zabbix.com/branches/dev@17652 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3516/upgrades/dbpatches/1.4/oracle/patch/slides.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE slides (
-	slideid		number(20)	DEFAULT '0'	NOT NULL,
-	slideshowid	number(20)	DEFAULT '0'	NOT NULL,
-	screenid	number(20)	DEFAULT '0'	NOT NULL,
-	step		number(10)	DEFAULT '0'	NOT NULL,
-	delay		number(10)	DEFAULT '0'	NOT NULL,
-	PRIMARY KEY (slideid)
-);
-CREATE INDEX slides_slides_1 on slides (slideshowid);
