996dd5ca3cf31bb2d3b8acc00d12a6110d123274
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-02-14 12:15:27 +0000

    - [ZBX-3532] another fix
    git-svn-id: svn://svn.zabbix.com/branches/dev@17703 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3532/frontends/php/include/js.inc.php
+++ ZBX-3532/frontends/php/include/js.inc.php
@@ -512,13 +512,21 @@ function insert_js_function($fnct_name){
 					var parentDocument = window.opener.document;
 					if(!parentDocument) return close_window();
 
+					var parentDocumentForm = $(parentDocument.body).select("form[name="+frame+"]");
+
 					var submitParent = submitParent || false;
 
 					var tmpStorage = null;
 					for(var key in values){
 						if(is_null(values[key])) continue;
 
-						tmpStorage = parentDocument.getElementById(key);
+						if(parentDocumentForm.length){
+							// .parentNode, to make prototype .select work
+							tmpStorage = $(parentDocumentForm[0].parentNode).select("#"+key).first();
+						}
+
+						if(typeof(tmpStorage) == "undefined" || is_null(tmpStorage))
+							tmpStorage = parentDocument.getElementById(key);
 
 						tmpStorage.value = values[key];
 					}
