fc4c9639ede5dde97daaa54ca09997415d604083
konstantin <konstantin@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2011-02-15 14:17:36 +0000

    - [ZBXNEXT-575] Updated test cases after redesign of actions
    git-svn-id: svn://svn.zabbix.com/trunk@17754 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/tests/selenium/testPageMediaTypes.php
+++ frontends/php/tests/selenium/testPageMediaTypes.php
@@ -90,7 +90,7 @@ class testPageMediaTypes extends CWebTest
 		$row=DBfetch(DBselect("select count(*) as cnt from opmessage where mediatypeid=$id"));
 		$used_by_operations = ($row['cnt'] > 0);
 
-		DBsave_tables(array('media_type','media','operations'));
+		DBsave_tables(array('opmessage','media_type','media'));
 
 		$this->chooseOkOnNextConfirmation();
 
@@ -116,7 +116,7 @@ class testPageMediaTypes extends CWebTest
 			break;
 		}
 
-		DBrestore_tables(array('media_type','media','operations'));
+		DBrestore_tables(array('opmessage','media_type','media'));
 	}
 
 	public function testPageMediaTypes_Sorting()
