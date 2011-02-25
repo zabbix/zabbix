e3a36e357e6cd2af7258abf5f2c8f0662bd7e8dc
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-02-25 09:01:11 +0000

    - [ZBXNEXT-631] reverted t_char to t_varchar for colors
    git-svn-id: svn://svn.zabbix.com/branches/dev@18092 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-631/upgrades/dbpatches/2.0/ibm_db2/patch/config.sql
+++ ZBXNEXT-631/upgrades/dbpatches/2.0/ibm_db2/patch/config.sql
@@ -22,27 +22,27 @@ ALTER TABLE config ADD ns_support integer WITH DEFAULT '0' NOT NULL
 /
 REORG TABLE config
 /
-ALTER TABLE config ADD severity_color_0 char(6) SET WITH DEFAULT 'DBDBDB' NOT NULL
+ALTER TABLE config ADD severity_color_0 varchar(6) SET WITH DEFAULT 'DBDBDB' NOT NULL
 /
 REORG TABLE config
 /
-ALTER TABLE config ADD severity_color_1 char(6) SET WITH DEFAULT 'D6F6FF' NOT NULL
+ALTER TABLE config ADD severity_color_1 varchar(6) SET WITH DEFAULT 'D6F6FF' NOT NULL
 /
 REORG TABLE config
 /
-ALTER TABLE config ADD severity_color_2 char(6) SET WITH DEFAULT 'FFF6A5' NOT NULL
+ALTER TABLE config ADD severity_color_2 varchar(6) SET WITH DEFAULT 'FFF6A5' NOT NULL
 /
 REORG TABLE config
 /
-ALTER TABLE config ADD severity_color_3 char(6) SET WITH DEFAULT 'FFB689' NOT NULL
+ALTER TABLE config ADD severity_color_3 varchar(6) SET WITH DEFAULT 'FFB689' NOT NULL
 /
 REORG TABLE config
 /
-ALTER TABLE config ADD severity_color_4 char(6) SET WITH DEFAULT 'FF9999' NOT NULL
+ALTER TABLE config ADD severity_color_4 varchar(6) SET WITH DEFAULT 'FF9999' NOT NULL
 /
 REORG TABLE config
 /
-ALTER TABLE config ADD severity_color_5 char(6) SET WITH DEFAULT 'FF3838' NOT NULL
+ALTER TABLE config ADD severity_color_5 varchar(6) SET WITH DEFAULT 'FF3838' NOT NULL
 /
 REORG TABLE config
 /
