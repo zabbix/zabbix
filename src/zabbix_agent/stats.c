c8d220d5b2ab986fa172b0ab4cceddd92bd74892
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-01 09:56:37 +0000

    - [ZBXNEXT-408] better error reporting for internal checks zabbix[process,...]
    git-svn-id: svn://svn.zabbix.com/branches/1.8@18135 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_agent/stats.c
+++ src/zabbix_agent/stats.c
@@ -23,7 +23,6 @@
 #include "log.h"
 #include "zbxconf.h"
 
-#include "interfaces.h"
 #include "diskdevices.h"
 #include "cpustat.h"
 #include "perfstat.h"
@@ -270,8 +269,7 @@ ZBX_THREAD_ENTRY(collector_thread, args)
 		collect_perfstat();
 #endif /* _WINDOWS */
 
-		collect_stats_interfaces(&(collector->interfaces)); /* TODO */
-		collect_stats_diskdevices(&(collector->diskdevices)); /* TODO */
+		collect_stats_diskdevices(&(collector->diskdevices));
 #ifdef _AIX
 		collect_vmstat_data(&collector->vmstat);
 #endif
