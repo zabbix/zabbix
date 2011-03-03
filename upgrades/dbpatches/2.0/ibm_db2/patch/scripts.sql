7edf7f0c58f2ac6da0a266b47f100733f82d8fc2
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2011-03-03 13:50:10 +0000

    - [ZBXNEXT-673] database patches

--- upgrades/dbpatches/2.0/ibm_db2/patch/scripts.sql
+++ upgrades/dbpatches/2.0/ibm_db2/patch/scripts.sql
@@ -26,13 +26,23 @@ ALTER TABLE scripts ADD confirmation varchar(255) WITH DEFAULT '' NOT NULL
 /
 REORG TABLE scripts
 /
+ALTER TABLE scripts ADD type integer WITH DEFAULT '0' NOT NULL
+/
+REORG TABLE scripts
+/
+ALTER TABLE scripts ADD execute_on integer WITH DEFAULT '1' NOT NULL
+/
+REORG TABLE scripts
+/
 UPDATE scripts SET usrgrpid=NULL WHERE usrgrpid=0
 /
 UPDATE scripts SET groupid=NULL WHERE groupid=0
 /
-DELETE FROM scripts WHERE NOT usrgrpid IS NULL AND NOT usrgrpid IN (SELECT usrgrpid FROM usrgrp)
+UPDATE scripts SET type=1,command=TRIM(SUBSTR(command, 5)) WHERE SUBSTR(command, 1, 4)='IPMI'
 /
-DELETE FROM scripts WHERE NOT groupid IS NULL AND NOT groupid IN (SELECT groupid FROM groups)
+DELETE FROM scripts WHERE usrgrpid IS NOT NULL AND usrgrpid NOT IN (SELECT usrgrpid FROM usrgrp)
+/
+DELETE FROM scripts WHERE groupid IS NOT NULL AND groupid NOT IN (SELECT groupid FROM groups)
 /
 ALTER TABLE scripts ADD CONSTRAINT c_scripts_1 FOREIGN KEY (usrgrpid) REFERENCES usrgrp (usrgrpid)
 /
