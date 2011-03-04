3a58b40388cd559b9b788fdac0efe55385debc21
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-04 09:02:56 +0000

    - [ZBX-3538] removed dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@18232 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3538/upgrades/dbpatches/2.0/mysql/patch/hosts_profiles.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE hosts_profiles MODIFY hostid bigint unsigned NOT NULL;
-DELETE FROM hosts_profiles WHERE NOT hostid IN (SELECT hostid FROM hosts);
-ALTER TABLE hosts_profiles ADD CONSTRAINT c_hosts_profiles_1 FOREIGN KEY (hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
