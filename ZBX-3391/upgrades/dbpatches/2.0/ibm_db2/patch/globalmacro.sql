f70780fe5f0c01638490fc4ff6a7f57d6ceea795
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-07 12:36:55 +0000

    - [ZBX-3391] remove
    git-svn-id: svn://svn.zabbix.com/branches/dev@18286 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3391/upgrades/dbpatches/2.0/ibm_db2/patch/globalmacro.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE globalmacro ALTER COLUMN globalmacroid SET WITH DEFAULT NULL;
-REORG TABLE globalmacro;
--- ZBX-3391/upgrades/dbpatches/2.0/mysql/patch/proxy_dhistory.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM proxy_dhistory WHERE NOT druleid IN (SELECT druleid FROM drules);
-DELETE FROM proxy_dhistory WHERE NOT dcheckid IN (SELECT dcheckid FROM dchecks);
-ALTER TABLE proxy_dhistory MODIFY druleid bigint unsigned NOT NULL;
-ALTER TABLE proxy_dhistory MODIFY dcheckid bigint unsigned NOT NULL;
