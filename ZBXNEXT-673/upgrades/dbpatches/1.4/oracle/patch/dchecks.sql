2e8f0e98cb9175cd649f2791bc90fd6c0573b35e
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-08 09:15:58 +0000

    - [ZBXNEXT-673] remove
    git-svn-id: svn://svn.zabbix.com/branches/dev@18313 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-673/upgrades/dbpatches/1.4/oracle/patch/dchecks.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE dchecks (
-	dcheckid                number(20)              DEFAULT '0'     NOT NULL,
-	druleid         number(20)              DEFAULT '0'     NOT NULL,
-	type            number(10)              DEFAULT '0'     NOT NULL,
-	key_            varchar2(255)           DEFAULT '0'     ,
-	snmp_community          varchar2(255)           DEFAULT '0'     ,
-	ports           varchar2(255)           DEFAULT '0'     ,
-	PRIMARY KEY (dcheckid)
-);
