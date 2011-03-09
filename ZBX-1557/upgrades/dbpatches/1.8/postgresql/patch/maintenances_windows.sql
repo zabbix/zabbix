4cee80935b0be025300da448142c0535bfce171a
rudolfs <rudolfs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-09 09:42:31 +0000

    - [ZBX-1557] Removing dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@18341 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-1557/upgrades/dbpatches/1.8/postgresql/patch/maintenances_windows.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE maintenances_windows (
-        maintenance_timeperiodid                bigint          DEFAULT '0'     NOT NULL,
-        maintenanceid           bigint          DEFAULT '0'     NOT NULL,
-        timeperiodid            bigint          DEFAULT '0'     NOT NULL,
-        PRIMARY KEY (maintenance_timeperiodid)
-) with OIDS;
-CREATE INDEX maintenances_windows_1 on maintenances_windows (maintenanceid,timeperiodid);
