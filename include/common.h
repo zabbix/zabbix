ce3e3d152f0bfd312aabd9eaf08abe505f730c67
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-11 14:07:42 +0000

    - [DEV-492] updated version number to 1.9.3
    git-svn-id: svn://svn.zabbix.com/trunk@18407 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -118,7 +118,7 @@
 
 #define	APPLICATION_NAME	"Zabbix Agent"
 #define	ZABBIX_REVDATE		"11 March 2011"
-#define	ZABBIX_VERSION		"1.9.2"
+#define	ZABBIX_VERSION		"1.9.3"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
 #if defined(_WINDOWS)
