6f1e8e927ae72f7509083925cf384410fe51c077
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-17 13:57:09 +0000

    - [ZBX-3623] improved function zbx_timespec()
    git-svn-id: svn://svn.zabbix.com/branches/dev@18508 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3623/src/libs/zbxcommon/misc.c
+++ ZBX-3623/src/libs/zbxcommon/misc.c
@@ -89,12 +89,24 @@ int	get_nodeid_by_id(zbx_uint64_t id)
  ******************************************************************************/
 void	zbx_timespec(zbx_timespec_t *ts)
 {
+	static zbx_timespec_t	*last_ts = NULL;
+	static unsigned short	corr = 0;
 #ifdef _WINDOWS
-
 	LARGE_INTEGER	tickPerSecond, tick;
 	static int	boottime = 0;
 	BOOL		rc = FALSE;
+#else	/* not _WINDOWS */
+	struct timeval	tv;
+	int		rc = -1;
+#ifdef HAVE_TIME_CLOCK_GETTIME
+	struct timespec	tp;
+#endif	/* HAVE_TIME_CLOCK_GETTIME */
+#endif	/* not _WINDOWS */
 
+	if (NULL == last_ts)
+		last_ts = zbx_malloc(last_ts, sizeof(zbx_timespec_t));
+
+#ifdef _WINDOWS
 	if (TRUE == (rc = QueryPerformanceFrequency(&tickPerSecond)))
 	{
 		if (TRUE == (rc = QueryPerformanceCounter(&tick)))
@@ -119,14 +131,8 @@ void	zbx_timespec(zbx_timespec_t *ts)
 		ts->sec = (int)tb.time;
 		ts->ns = tb.millitm * 1000000;
 	}
-
 #else	/* not _WINDOWS */
-
-	struct timeval	tv;
-	int		rc = -1;
 #ifdef HAVE_TIME_CLOCK_GETTIME
-	struct timespec	tp;
-
 	if (0 == (rc = clock_gettime(CLOCK_REALTIME, &tp)))
 	{
 		ts->sec = (int)tp.tv_sec;
@@ -145,8 +151,26 @@ void	zbx_timespec(zbx_timespec_t *ts)
 		ts->sec = (int)time(NULL);
 		ts->ns = 0;
 	}
-
 #endif	/* not _WINDOWS */
+
+	if (last_ts->ns == ts->ns && last_ts->sec == ts->sec)
+	{
+		corr++;
+
+		while (ts->ns + corr > 1000000000)
+		{
+			ts->sec++;
+			corr -= 1000000000;
+		}
+
+		ts->ns += corr;
+	}
+	else
+	{
+		last_ts->sec = ts->sec;
+		last_ts->ns = ts->ns;
+		corr = 0;
+	}
 }
 
 /******************************************************************************
