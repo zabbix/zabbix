05e0b9b726e764f2c783e3124584d581781586cd
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-18 23:42:10 +0000

    - [ZBXNEXT-675] change status message style  - only generate png if it is missing or older than the corresponding svg
    git-svn-id: svn://svn.zabbix.com/trunk@18565 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- create/images/svg_to_pngsql.sh
+++ create/images/svg_to_pngsql.sh
@@ -26,27 +26,32 @@ for svgfile in $elementdir/*.svg; do
 	((elementfilesdone++))
 	for size in 24 48 64 96 128; do
 		pngoutfile="$outputdir/$(basename ${svgfile%.svg}) ($size).png"
-		echo -n " to size $size..."
-		# we have to query image dimensions first, because export dimensions are used "as-is", resulting in a aquare rackmountable server, for example
-		# inkscape option --query-all could be used, but it's not fully clear which layer is supposed to be "whole image"
-		# crudely dropping decimal part, bash fails on it
-		[[ "$(inkscape --without-gui --query-width $svgfile | cut -d. -f1)" -gt "$(inkscape --without-gui --query-height $svgfile | cut -d. -f1)" ]] && {
-			dimension=width
+		[[ "$(stat -c "%Y" "$pngoutfile" 2>/dev/null)" -lt "$(stat -c "%Y" "$svgfile")" ]] && {
+			# if png file modification time is older than svg file modification time
+			echo -n " to $size..."
+			# we have to query image dimensions first, because export dimensions are used "as-is", resulting in a aquare rackmountable server, for example
+			# inkscape option --query-all could be used, but it's not fully clear which layer is supposed to be "whole image"
+			# crudely dropping decimal part, bash fails on it
+			[[ "$(inkscape --without-gui --query-width $svgfile | cut -d. -f1)" -gt "$(inkscape --without-gui --query-height $svgfile | cut -d. -f1)" ]] && {
+				dimension=width
+			} || {
+				dimension=height
+			}
+			inkscape --without-gui --export-$dimension=$size $svgfile --export-png="$pngoutfile" >> inkscape.log.txt|| exit 1
+			echo -n " compress..."
+			$pngcrushbin -brute -reduce -e .2.png "$pngoutfile" >> $pngcrushoutput || exit 1
+			echo "$pngoutfile : $(echo "$(stat -c %s "${pngoutfile%png}2.png")/$(stat -c %s "${pngoutfile}")*100" | bc -l)" >> $pngcrushlog
+			mv "${pngoutfile%png}2.png" "$pngoutfile"
 		} || {
-			dimension=height
+			echo -n " skip $size..."
 		}
-		inkscape --without-gui --export-$dimension=$size $svgfile --export-png="$pngoutfile" >> inkscape.log.txt|| exit 1
-		$pngcrushbin -brute -reduce -e .2.png "$pngoutfile" >> $pngcrushoutput || exit 1
-		echo "$pngoutfile : $(echo "$(stat -c %s "${pngoutfile%png}2.png")/$(stat -c %s "${pngoutfile}")*100" | bc -l)" >> $pngcrushlog
-		mv "${pngoutfile%png}2.png" "$pngoutfile"
 	done
 	echo "[$[$elementfilesdone*100/$svgelementcount]%]"
 done
 
-echo "Compressing images with pngcrush"
+echo "Generating SQL file"
 
 for imagefile in $outputdir/*.png; do
 	((imagecount++))
-	echo "$imagefile"
 	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagecount,1,'${imagefile%.png}','$(hexdump -ve '"" 1/1 "%02X"' "$imagefile")';" >> $sqlfile
 done
