734607efa482a60aaf807933ac4ca758ade9a2cf
Rihards Olups <git-no-reply@zabbix.com>
2011-03-18 23:45:16 +0000

    - [ZBXNEXT-675] fix modification time comparison

--- create/images/svg_to_pngsql.sh
+++ create/images/svg_to_pngsql.sh
@@ -27,7 +27,7 @@ for svgfile in $elementdir/*.svg; do
 	svgfilemod=$(stat -c "%Y" "$svgfile")
 	for size in 24 48 64 96 128; do
 		pngoutfile="$outputdir/$(basename ${svgfile%.svg}) ($size).png"
-		[[ "$(stat -c "%Y" "$pngoutfile" 2>/dev/null)" -lt "$svgfilemod")" ]] && {
+		[[ "$(stat -c "%Y" "$pngoutfile" 2>/dev/null)" -lt "$svgfilemod" ]] && {
 			# if png file modification time is older than svg file modification time
 			echo -n " to $size..."
 			# we have to query image dimensions first, because export dimensions are used "as-is", resulting in a aquare rackmountable server, for example
