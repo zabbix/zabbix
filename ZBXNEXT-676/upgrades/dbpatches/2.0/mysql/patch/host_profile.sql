477be95842d05b6bc51b4cd893f80aa7b094abac
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-18 11:18:19 +0000

    - [ZBXNEXT-676] added CR+LF for MySQL and PostgreSQL patches
    git-svn-id: svn://svn.zabbix.com/branches/dev@18526 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-676/upgrades/dbpatches/2.0/mysql/patch/host_profile.sql
+++ ZBXNEXT-676/upgrades/dbpatches/2.0/mysql/patch/host_profile.sql
@@ -251,7 +251,7 @@ UPDATE t_host_profile SET poc_2_notes='' WHERE poc_2_notes IS NULL;
 
 -- merge notes field
 UPDATE t_host_profile SET notes_ext='' WHERE notes_ext IS NULL;
-UPDATE t_host_profile SET notes=CONCAT(notes, '\n', notes_ext) WHERE notes<>'' AND notes_ext<>'';
+UPDATE t_host_profile SET notes=CONCAT(notes, '\r\n', notes_ext) WHERE notes<>'' AND notes_ext<>'';
 UPDATE t_host_profile SET notes=notes_ext WHERE notes='';
 ALTER TABLE t_host_profile DROP COLUMN notes_ext;
 
