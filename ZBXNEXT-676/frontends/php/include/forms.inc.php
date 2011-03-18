b5c4a8d7b8e6506aff3f3c93dd6965cb84a915f3
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-18 08:40:15 +0000

    - [ZBXNEXT-676] fixed new lines, field sizes, patch names
    git-svn-id: svn://svn.zabbix.com/branches/dev@18520 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-676/frontends/php/include/forms.inc.php
+++ ZBXNEXT-676/frontends/php/include/forms.inc.php
@@ -3760,7 +3760,7 @@ ITEM_TYPE_CALCULATED $key = ''; $params = '';
 		$row = DBfetch($result);
 		foreach($row as $key => $value){
 			if(!zbx_empty($value)){
-				$frmHostP->addRow($table_titles[$key], new CSpan(zbx_str2links($value)));
+				$frmHostP->addRow($table_titles[$key], new CSpan(zbx_str2links($value), 'pre'));
 			}
 		}
 
