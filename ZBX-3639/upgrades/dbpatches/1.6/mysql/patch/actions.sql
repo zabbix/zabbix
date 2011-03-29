1c3eff281c93068fae176db358cc36cab3e46962
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-29 10:36:16 +0000

    - [ZBX-3639] Removing development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@18755 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3639/upgrades/dbpatches/1.6/mysql/patch/actions.sql
+++
@@ -1,8 +0,0 @@
-alter table actions add	esc_period	integer		DEFAULT '0'	NOT NULL;
-alter table actions add	def_shortdata	varchar(255)	DEFAULT ''	NOT NULL;
-alter table actions add	def_longdata	blob				NOT NULL;
-alter table actions add	recovery_msg	integer		DEFAULT '0'	NOT NULL;
-alter table actions add	r_shortdata	varchar(255)	DEFAULT ''	NOT NULL;
-alter table actions add	r_longdata	blob				NOT NULL;
-
-CREATE INDEX actions_1 on actions (eventsource,status);
