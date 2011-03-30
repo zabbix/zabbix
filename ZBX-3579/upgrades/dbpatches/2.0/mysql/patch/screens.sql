f33804d715b6b259ac2fbe443f112fdf6a6dfed5
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-30 11:36:42 +0000

    - [ZBX-3579] remove
    git-svn-id: svn://svn.zabbix.com/branches/dev@18792 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3579/upgrades/dbpatches/2.0/mysql/patch/screens.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE screens MODIFY screenid bigint unsigned NOT NULL,
-		    MODIFY name varchar(255) NOT NULL,
-		    ADD templateid bigint unsigned NULL;
-ALTER TABLE screens ADD CONSTRAINT c_screens_1 FOREIGN KEY (templateid) REFERENCES hosts (hostid) ON DELETE CASCADE;
