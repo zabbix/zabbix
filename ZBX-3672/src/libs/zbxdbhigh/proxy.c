3c169617d8b76ff305f53e03cba7bae9536e51bd
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-03-31 16:08:27 +0000

    - [ZBX-3672] removed unnecessary the function zabbix_syslog() calls
    git-svn-id: svn://svn.zabbix.com/branches/dev@18841 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3672/src/libs/zbxdbhigh/proxy.c
+++ ZBX-3672/src/libs/zbxdbhigh/proxy.c
@@ -1619,18 +1619,14 @@ void	process_dhis_data(struct zbx_json_parse *jp)
 
 		continue;
 json_parse_error:
-		zabbix_log(LOG_LEVEL_WARNING, "Invalid discovery data. %s",
-				zbx_json_strerror());
-		zabbix_syslog("Invalid discovery data. %s",
-				zbx_json_strerror());
+		zabbix_log(LOG_LEVEL_WARNING, "Invalid discovery data. %s", zbx_json_strerror());
+		zabbix_syslog("Invalid discovery data. %s", zbx_json_strerror());
 	}
 exit:
 	if (SUCCEED != ret)
 	{
-		zabbix_log(LOG_LEVEL_WARNING, "Invalid discovery data. %s",
-				zbx_json_strerror());
-		zabbix_syslog("Invalid discovery data. %s",
-				zbx_json_strerror());
+		zabbix_log(LOG_LEVEL_WARNING, "Invalid discovery data. %s", zbx_json_strerror());
+		zabbix_syslog("Invalid discovery data. %s", zbx_json_strerror());
 	}
 
 	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%s",
@@ -1693,18 +1689,14 @@ void	process_areg_data(struct zbx_json_parse *jp, zbx_uint64_t proxy_hostid)
 
 		continue;
 json_parse_error:
-		zabbix_log(LOG_LEVEL_WARNING, "Invalid auto registration data. %s",
-				zbx_json_strerror());
-		zabbix_syslog("Invalid auto registration data. %s",
-				zbx_json_strerror());
+		zabbix_log(LOG_LEVEL_WARNING, "Invalid auto registration data. %s", zbx_json_strerror());
+		zabbix_syslog("Invalid auto registration data. %s", zbx_json_strerror());
 	}
 exit:
 	if (SUCCEED != ret)
 	{
-		zabbix_log(LOG_LEVEL_WARNING, "Invalid auto registration data. %s",
-				zbx_json_strerror());
-		zabbix_syslog("Invalid auto registration data. %s",
-				zbx_json_strerror());
+		zabbix_log(LOG_LEVEL_WARNING, "Invalid auto registration data. %s", zbx_json_strerror());
+		zabbix_syslog("Invalid auto registration data. %s", zbx_json_strerror());
 	}
 
 	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%s",
