7d5b2ed2aa7011177c128bdd67610e9f828471fd
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-04-13 07:53:33 +0000

    - [ZBX-3503] Removing development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@19012 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3503-trunk/upgrades/dbpatches/2.0/oracle/patch/users_groups.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE users_groups MODIFY id DEFAULT NULL;
-ALTER TABLE users_groups MODIFY usrgrpid DEFAULT NULL;
-ALTER TABLE users_groups MODIFY userid DEFAULT NULL;
-DROP INDEX users_groups_1;
-DELETE FROM users_groups WHERE usrgrpid NOT IN (SELECT usrgrpid FROM usrgrp);
-DELETE FROM users_groups WHERE userid NOT IN (SELECT userid FROM users);
-CREATE UNIQUE INDEX users_groups_1 ON users_groups (usrgrpid,userid);
-ALTER TABLE users_groups ADD CONSTRAINT c_users_groups_1 FOREIGN KEY (usrgrpid) REFERENCES usrgrp (usrgrpid) ON DELETE CASCADE;
-ALTER TABLE users_groups ADD CONSTRAINT c_users_groups_2 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
