e6960f5ef005917cf5d8e762072308760bb7cbcd
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-04-18 08:59:53 +0000

    - [ZBX-524] updated to the latest 1.8 branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@19072 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-524/include/common.h
+++ ZBX-524/include/common.h
@@ -117,8 +117,8 @@
 #define OFF	0
 
 #define	APPLICATION_NAME	"Zabbix Agent"
-#define	ZABBIX_REVDATE		"1 April 2011"
-#define	ZABBIX_VERSION		"1.8.5rc1"
+#define	ZABBIX_REVDATE		"15 April 2011"
+#define	ZABBIX_VERSION		"1.8.5"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
 #if defined(_WINDOWS)
