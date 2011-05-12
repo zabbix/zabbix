ffb6d5312815f39a188b16fd4eb82d3703e711ac
rudolfs <rudolfs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-05-12 08:00:16 +0000

    - [ZBX-3747] Typos
    git-svn-id: svn://svn.zabbix.com/branches/dev@19515 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3747/src/libs/zbxcommon/str.c
+++ ZBX-3747/src/libs/zbxcommon/str.c
@@ -232,8 +232,8 @@ int	zbx_vsnprintf(char *str, size_t count, const char *fmt, va_list args)
 	assert(str);
 
 #ifdef _WINDOWS
-	if (-1 == (written_len = vsnprintf_s(str, count, _TRUNCATE, fmt, args))
-		return count -1;	/* result was truncated */
+	if (-1 == (written_len = vsnprintf_s(str, count, _TRUNCATE, fmt, args)))
+		return count - 1;	/* result was truncated */
 #else
 	written_len = vsnprintf(str, count, fmt, args);
 	written_len = MIN(written_len, (int)count - 1);
