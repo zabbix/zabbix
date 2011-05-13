c57077cc98094bee9f7e1671e9aafa27ac32958d
alex <alex@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2011-05-13 19:56:30 +0000

    - [DEV-492] simplified save and restore procedure for database tables
    git-svn-id: svn://svn.zabbix.com/trunk@19599 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/tests/selenium/testPageActionsAutoregistration.php
+++ frontends/php/tests/selenium/testPageActionsAutoregistration.php
@@ -23,21 +23,6 @@ require_once(dirname(__FILE__).'/../include/class.cwebtest.php');
 
 class testPageActionsAutoregistration extends CWebTest
 {
-	public $affectedTables = array(
-		'actions',
-		'operations',
-		'conditions',
-		'opmessage',
-		'opgroup',
-		'optemplate',
-		'opcommand',
-		'opcommand_grp',
-		'opcommand_hst',
-		'opconditions',
-		'opmessage_grp',
-		'opmessage_usr'
-	);
-
 	// Returns all trigger actions
 	public static function allActions()
 	{
@@ -224,7 +209,7 @@ class testPageActionsAutoregistration extends CWebTest
 
 		$this->chooseOkOnNextConfirmation();
 
-		DBsave_tables($this->affectedTables);
+		DBsave_tables('actions');
 
 		$this->login('actionconf.php?eventsource='.EVENT_SOURCE_AUTO_REGISTRATION);
 		$this->assertTitle('Configuration of actions');
@@ -246,7 +231,7 @@ class testPageActionsAutoregistration extends CWebTest
 		$sql="select * from conditions where actionid=$actionid";
 		$this->assertEquals(0,DBcount($sql));
 
-		DBrestore_tables($this->affectedTables);
+		DBrestore_tables('actions');
 	}
 
 	public function testPageActionsAutoregistration_Sorting()
--- frontends/php/tests/selenium/testPageActionsDiscovery.php
+++ frontends/php/tests/selenium/testPageActionsDiscovery.php
@@ -24,21 +24,6 @@ require_once(dirname(__FILE__).'/../include/class.cwebtest.php');
 
 class testPageActionsDiscovery extends CWebTest
 {
-	public $affectedTables = array(
-		'actions',
-		'operations',
-		'conditions',
-		'opmessage',
-		'opgroup',
-		'optemplate',
-		'opcommand',
-		'opcommand_grp',
-		'opcommand_hst',
-		'opconditions',
-		'opmessage_grp',
-		'opmessage_usr'
-	);
-
 	// Returns all trigger actions
 	public static function allActions()
 	{
@@ -223,7 +208,7 @@ class testPageActionsDiscovery extends CWebTest
 
 		$this->chooseOkOnNextConfirmation();
 
-		DBsave_tables($this->affectedTables);
+		DBsave_tables('actions');
 
 		$this->login('actionconf.php?eventsource='.EVENT_SOURCE_DISCOVERY);
 		$this->assertTitle('Configuration of actions');
@@ -245,7 +230,7 @@ class testPageActionsDiscovery extends CWebTest
 		$sql="select * from conditions where actionid=$actionid";
 		$this->assertEquals(0,DBcount($sql));
 
-		DBrestore_tables($this->affectedTables);
+		DBrestore_tables('actions');
 	}
 
 	public function testPageActionsDiscovery_Sorting()
