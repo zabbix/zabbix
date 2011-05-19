0a00606e46429a65ab68b907318fe09925aea605
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-05-19 08:10:38 +0000

    - [ZBX-3818] Removing development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@19728 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3818/upgrades/dbpatches/1.6/mysql/patch/graphs.sql
+++
@@ -1,4 +0,0 @@
-alter table graphs add show_legend int(11) NOT NULL default '0';
-alter table graphs add show_3d int(11) NOT NULL default '0';
-alter table graphs add percent_left            double(16,4)            DEFAULT '0'     NOT NULL;
-alter table graphs add percent_right           double(16,4)            DEFAULT '0'     NOT NULL;
