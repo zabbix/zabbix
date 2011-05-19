260bb82f45f466f49e03ee603584735c45147184
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-05-19 08:19:34 +0000

    - [ZBXNEXT-783] Creating development branch (copy from trunk)
    git-svn-id: svn://svn.zabbix.com/branches/dev@19732 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-783/upgrades/dbpatches/2.0/oracle/patch/proxy_dhistory.sql
@@ -0,0 +1,5 @@
+DELETE FROM proxy_dhistory WHERE NOT druleid IN (SELECT druleid FROM drules);
+DELETE FROM proxy_dhistory WHERE NOT dcheckid IN (SELECT dcheckid FROM dchecks);
+ALTER TABLE proxy_dhistory MODIFY druleid DEFAULT NULL;
+ALTER TABLE proxy_dhistory MODIFY dcheckid DEFAULT NULL;
+ALTER TABLE proxy_dhistory ADD dns nvarchar2(64) DEFAULT '';
