6b7e3be53e37f2fa532d22722869b74fac960f38
rudolfs <rudolfs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-05-24 07:40:02 +0000

    - [ZBXNEXT-747] Changed to monitoring a single file with log rotation
    git-svn-id: svn://svn.zabbix.com/branches/dev@19809 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-747/src/zabbix_server/snmptrapper/snmptrapper.c
+++ ZBXNEXT-747/src/zabbix_server/snmptrapper/snmptrapper.c
@@ -25,7 +25,9 @@
 
 #include "snmptrapper.h"
 
-static char		tmp_trap_file[MAX_STRING_LEN];
+static int	trap_fd = -1;
+static off_t	trap_lastsize = 0;
+static ino_t	trap_ino = 0;
 
 /******************************************************************************
  *                                                                            *
@@ -41,48 +43,92 @@ static char		tmp_trap_file[MAX_STRING_LEN];
 static void process_traps()
 {
 	const char	*__function_name = "process_traps";
-	int		fd, ret = FAIL;
+	int		nbytes;
 	char		buffer[MAX_BUFFER_LEN];
 
-	zabbix_log(LOG_LEVEL_DEBUG, "In %s(), tmp file [%s]", __function_name, tmp_trap_file);
+	zabbix_log(LOG_LEVEL_DEBUG, "In %s(), lastsize [%lu]", __function_name, trap_lastsize);
 
 	*buffer = 0;
 
-	if (-1 == (fd = open(tmp_trap_file, O_RDONLY)))
+	if (FAIL == (nbytes = read(trap_fd, buffer, sizeof(buffer))))
 	{
-		zabbix_log(LOG_LEVEL_ERR, "could not open [%s]: %s",tmp_trap_file, zbx_strerror(errno));
-		goto close;
+		zabbix_log(LOG_LEVEL_WARNING, "%s(): cannot read from [%s]: %s",
+				__function_name, CONFIG_SNMPTRAP_FILE, zbx_strerror(errno));
+		goto exit;
 	}
 
-	if (-1 == flock(fd, LOCK_EX))	/* do no read the file before it is closed by zabbix_handler */
-	{
-		zabbix_log(LOG_LEVEL_ERR, "could lock [%s]: %s",tmp_trap_file, zbx_strerror(errno));
-		goto close;
-	}
+	trap_lastsize += (off_t)nbytes;
 
-
-	if (-1 == read(fd, buffer, sizeof(buffer)))
-	{
-		zabbix_log(LOG_LEVEL_ERR, "could not read from [%s]: %s",tmp_trap_file, zbx_strerror(errno));
-		goto close;
-	}
-
-	zbx_rtrim(buffer, " \r\n");
+	buffer[nbytes] = '\0';
+	zbx_rtrim(buffer + MAX(nbytes - 3, 0), " \r\n");
 
 	if ('\0' != *buffer)
-		ret = SUCCEED;
-close:
-	close(fd);
-
-	if (0 != remove(tmp_trap_file))
-		zabbix_log(LOG_LEVEL_ERR, "could not remove [%s]: %s",tmp_trap_file, zbx_strerror(errno));
-
-	if (SUCCEED == ret)
-		zabbix_log(LOG_LEVEL_ERR, "%s", buffer);
-	else
-		zabbix_log(LOG_LEVEL_ERR, "%s() FAILED", __function_name);
-
+		zabbix_log(LOG_LEVEL_ERR, "trap: %s", buffer);
+exit:
 	zabbix_log(LOG_LEVEL_DEBUG, "End of %s()", __function_name);
+}
+
+static int	open_trap_file()
+{
+	const char	*__function_name = "open_trap_file";
+	struct stat	file_buf;
+
+	if(-1 == (trap_fd = open(CONFIG_SNMPTRAP_FILE, O_RDONLY)))
+	{
+		if (errno != ENOENT)	/* file exists but cannot be opened */
+		{
+			zabbix_log(LOG_LEVEL_CRIT, "%s(): cannot open [%s]: %s",
+					__function_name, CONFIG_SNMPTRAP_FILE, zbx_strerror(errno));
+		}
+	}
+	else if (FAIL == stat(CONFIG_SNMPTRAP_FILE, &file_buf))
+	{
+		zabbix_log(LOG_LEVEL_CRIT, "%s(): cannot stat [%s]: %s",
+				__function_name, CONFIG_SNMPTRAP_FILE, zbx_strerror(errno));
+		close(trap_fd);
+		trap_fd = -1;
+	}
+	else
+	{
+		trap_ino = file_buf.st_ino;
+		trap_lastsize = 0;
+	}
+
+	return trap_fd;
+}
+
+static int	get_latest_data()
+{
+	const char	*__function_name = "get_latest_data";
+	struct stat	file_buf;
+
+	if (-1 != trap_fd)	/* a trap file is open */
+	{
+		if (FAIL == stat(CONFIG_SNMPTRAP_FILE, &file_buf))	/* file might have been renamed or deleted */
+		{
+			if  (errno != ENOENT)
+			{
+				zabbix_log(LOG_LEVEL_CRIT, "%s(): cannot stat [%s]: %s",
+						__function_name, CONFIG_SNMPTRAP_FILE, zbx_strerror(errno));
+			}
+			process_traps();
+			close(trap_fd);
+			trap_fd = -1;
+		}
+		else if (file_buf.st_ino != trap_ino || file_buf.st_size < trap_lastsize)
+		{
+			process_traps();
+			close(trap_fd);
+			trap_fd = -1;
+		}
+		else if (file_buf.st_size == trap_lastsize)
+			return FAIL;	/* no new traps */
+	}
+
+	if (-1 == trap_fd && -1 == open_trap_file())
+		return FAIL;
+
+	return SUCCEED;
 }
 
 void	main_snmptrapper_loop(int server_num)
@@ -90,8 +136,6 @@ void	main_snmptrapper_loop(int server_num)
 	const char	*__function_name = "main_snmptrapper_loop";
 
 	zabbix_log(LOG_LEVEL_ERR, "In %s(), trapfile [%s]", __function_name, CONFIG_SNMPTRAP_FILE);
-
-	zbx_snprintf(tmp_trap_file, sizeof(tmp_trap_file), "%s_%d", CONFIG_SNMPTRAP_FILE, server_num);
 
 	set_child_signal_handler();
 
@@ -104,15 +148,14 @@ void	main_snmptrapper_loop(int server_num)
 		update_selfmon_counter(ZBX_PROCESS_STATE_BUSY);
 		zbx_setproctitle("%s [processing data]", get_process_type_string(process_type));
 
-		/* if file exists, process the new traps */
-		if (0 == rename(CONFIG_SNMPTRAP_FILE, tmp_trap_file))
+		while (SUCCEED == get_latest_data())	/* there are unprocessed traps */
 			process_traps();
-		else
-			zabbix_log(LOG_LEVEL_ERR, "could not rename [%s]: %s",tmp_trap_file, zbx_strerror(errno));
 
 		update_selfmon_counter(ZBX_PROCESS_STATE_IDLE);
-		zbx_setproctitle("%s [waiting for traps]", get_process_type_string(process_type));
 		zbx_setproctitle("snmptrapper [sleeping for 1 second]");
 		zbx_sleep(1);
 	}
+
+	if (-1 != trap_fd)
+		close(trap_fd);
 }
