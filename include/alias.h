e7fbc4e96cc10dca602e468f65620281e3c69d77
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-05-24 09:36:24 +0000

    - [ZBX-3105] Processing garbage in configuration files.  - disallow invalid or duplicate-key Aliase, UserParameter and    PerfCounter entries  - exit if garbage (non-parameter entry) in configuration file  - exit if unknown parameter in configuration file (except sender)  - separate parsing for agent and agentd
    git-svn-id: svn://svn.zabbix.com/branches/1.8@19813 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/alias.h
+++ include/alias.h
@@ -31,7 +31,6 @@ typedef struct zbx_alias
 ALIAS;
 
 int	add_alias(const char *name, const char *value);
-int	add_alias_from_config(char *value);
 void	alias_list_free();
 void	alias_expand(const char *orig, char *expanded, int exp_buf_len);
 
