a724873797a572a20d1e81beedb54e9bac550d78
rudolfs <rudolfs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-05-25 13:04:35 +0000

    - [ZBX-3741] Style fixes
    git-svn-id: svn://svn.zabbix.com/branches/dev@19859 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3741/src/libs/zbxnix/pid.c
+++ ZBX-3741/src/libs/zbxnix/pid.c
@@ -19,10 +19,8 @@
 
 #include "common.h"
 #include "pid.h"
-
 #include "log.h"
 #include "threads.h"
-
 #include <sysinc.h>
 
 static FILE	*fpid = NULL;
@@ -30,28 +28,27 @@ static int	fdpid = -1;
 
 int	create_pid_file(const char *pidfile)
 {
-	struct  stat    buf;
-	int fd = 0;
+	int		fd = 0;
+	struct stat	buf;
+	struct flock	fl;
 
-	struct flock fl;
-
-	fl.l_type   = F_WRLCK;  /* F_RDLCK, F_WRLCK, F_UNLCK    */
-	fl.l_whence = SEEK_SET; /* SEEK_SET, SEEK_CUR, SEEK_END */
-	fl.l_start  = 0;        /* Offset from l_whence         */
-	fl.l_len    = 0;        /* length, 0 = to EOF           */
-	fl.l_pid    = zbx_get_thread_id(); /* our PID           */
+	fl.l_type = F_WRLCK;		/* F_RDLCK, F_WRLCK, F_UNLCK */
+	fl.l_whence = SEEK_SET;		/* SEEK_SET, SEEK_CUR, SEEK_END */
+	fl.l_start = 0;			/* offset from l_whence */
+	fl.l_len = 0;			/* length, 0 = to EOF */
+	fl.l_pid = zbx_get_thread_id();	/* our PID */
 
 	/* check if pid file already exists */
-	if(stat(pidfile,&buf) == 0)
+	if (0 == stat(pidfile, &buf))
 	{
-		if( -1 == (fd = open(pidfile, O_WRONLY | O_APPEND)))
+		if (-1 == (fd = open(pidfile, O_WRONLY | O_APPEND)))
 		{
 			zbx_error("cannot open PID file [%s]: %s", pidfile, zbx_strerror(errno));
 			zabbix_log(LOG_LEVEL_CRIT, "cannot open PID file [%s]: %s", pidfile, zbx_strerror(errno));
 			return FAIL;
 		}
 
-		if(-1 == fcntl(fd, F_SETLK, &fl) && EAGAIN == errno)
+		if (-1 == fcntl(fd, F_SETLK, &fl) && EAGAIN == errno)
 		{
 			zbx_error("File [%s] exists and is locked. Is this process already running?", pidfile);
 			zabbix_log(LOG_LEVEL_CRIT, "File [%s] exists and is locked. Is this process already running?", pidfile);
@@ -62,18 +59,16 @@ int	create_pid_file(const char *pidfile)
 	}
 
 	/* open pid file */
-	if( NULL == (fpid = fopen(pidfile, "w")))
+	if (NULL == (fpid = fopen(pidfile, "w")))
 	{
 		zbx_error("cannot create PID file [%s]: %s", pidfile, zbx_strerror(errno));
 		zabbix_log(LOG_LEVEL_CRIT, "cannot create PID file [%s]: %s", pidfile, zbx_strerror(errno));
-
 		return FAIL;
 	}
 
 	/* lock file */
 	fdpid = fileno(fpid);
-
-	if(-1 != fdpid)
+	if (-1 != fdpid)
 	{
 		fcntl(fdpid, F_SETLK, &fl);
 		fcntl(fdpid, F_SETFD, FD_CLOEXEC);
@@ -90,21 +85,18 @@ void	drop_pid_file(const char *pidfile)
 {
 	struct flock fl;
 
-	fl.l_type   = F_UNLCK;  /* tell it to unlock the region */
-	fl.l_whence = SEEK_SET; /* SEEK_SET, SEEK_CUR, SEEK_END */
-	fl.l_start  = 0;        /* Offset from l_whence         */
-	fl.l_len    = 0;        /* length, 0 = to EOF           */
-	fl.l_pid    = zbx_get_thread_id(); /* our PID           */
+	fl.l_type = F_UNLCK;		/* tell it to unlock the region */
+	fl.l_whence = SEEK_SET;		/* SEEK_SET, SEEK_CUR, SEEK_END */
+	fl.l_start = 0;			/* offset from l_whence */
+	fl.l_len = 0;			/* length, 0 = to EOF */
+	fl.l_pid = zbx_get_thread_id();	/* our PID */
 
 	/* unlock file */
-	if(-1 != fdpid)
+	if (-1 != fdpid)
 		fcntl(fdpid, F_SETLK, &fl);
 
-	/* close pid file */
 	zbx_fclose(fpid);
 
-	if(-1 == unlink(pidfile))
-	{
+	if (-1 == unlink(pidfile))
 		zabbix_log(LOG_LEVEL_DEBUG, "cannot remove PID file [%s]: %s", pidfile, zbx_strerror(errno));
-	}
 }
