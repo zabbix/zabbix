d4006953fe8f74af2c83a9d98673a425a92d5049
konstantin <konstantin@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2011-06-06 07:52:14 +0000

    - [ZBX-3854] added saving of state of dynamic/fixed setting for time lines on all pages  - [Merged: branches/1.8 20079]
    git-svn-id: svn://svn.zabbix.com/trunk@20080 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/screens.inc.php
+++ frontends/php/include/screens.inc.php
@@ -1138,7 +1138,8 @@ require_once('include/js.inc.php');
 						'loadSBox' => 0,
 						'loadImage' => 1,
 						'loadScroll' => 0,
-						'dynamic' => 0
+						'dynamic' => 0,
+						'periodFixed' => CProfile::get('web.screens.timelinefixed', 1)
 					);
 
 					$default = false;
@@ -1220,7 +1221,8 @@ require_once('include/js.inc.php');
 						'loadSBox' => 0,
 						'loadImage' => 1,
 						'loadScroll' => 0,
-						'dynamic' => 0
+						'dynamic' => 0,
+						'periodFixed' => CProfile::get('web.screens.timelinefixed', 1)
 					);
 
 // Host feature
