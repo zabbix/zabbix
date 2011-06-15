8382c9203917c1b08e3ffdccb15de3cfb39c68f4
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-06-15 15:26:19 +0000

    - [ZBXNEXT-807] time-based triggers should now be taken from the trigger cache, too
    git-svn-id: svn://svn.zabbix.com/branches/dev@20231 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-807/include/dbcache.h
+++ ZBXNEXT-807/include/dbcache.h
@@ -209,6 +209,7 @@ int	DCconfig_get_item_by_itemid(DC_ITEM *item, zbx_uint64_t itemid);
 int	DCconfig_get_function_by_functionid(DC_FUNCTION *function, zbx_uint64_t functionid);
 void	DCconfig_get_triggers_by_itemids(zbx_hashset_t *trigger_info, zbx_vector_ptr_t *trigger_order,
 		const zbx_uint64_t *itemids, const zbx_timespec_t *timespecs, int item_num);
+void	DCconfig_get_time_based_triggers(DC_TRIGGER **trigger_info, zbx_vector_ptr_t *trigger_order);
 int	DCconfig_get_interface_by_type(DC_INTERFACE *interface, zbx_uint64_t hostid, unsigned char type);
 int	DCconfig_get_poller_nextcheck(unsigned char poller_type);
 int	DCconfig_get_poller_items(unsigned char poller_type, DC_ITEM *items, int max_items);
