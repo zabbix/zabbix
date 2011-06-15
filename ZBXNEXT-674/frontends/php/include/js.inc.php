15970cc9b3b80e467330bd6e4785bcf700221425
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-06-15 14:20:30 +0000

    - [ZBXNEXT-674] sync trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@20229 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-674/frontends/php/include/js.inc.php
+++ ZBXNEXT-674/frontends/php/include/js.inc.php
@@ -20,7 +20,7 @@ function zbx_jsvalue($value, $asObject=false, $addQuotes=true){
 			$escaped = str_replace("\n", '\n', $escaped); // changing LF to '\n' string
 			$escaped = str_replace('\'', '\\\'', $escaped); // escaping single quotes: ' => \'
 			if($addQuotes){
-				$escaped = '"'.$escaped.'"';
+				$escaped = '\''.$escaped.'\'';
 			}
 			return $escaped;
 		}
