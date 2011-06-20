f49a32b6fe3e7b4871146ac70d785dd99971d938
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-06-20 13:52:16 +0000

    - [NTP-373] removed the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@20297 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- NTP-373/upgrades/dbpatches/1.4/postgresql/patch/history_str_sync.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE history_str_sync (
-	id		serial			,
-	nodeid		bigint DEFAULT '0'	NOT NULL,
-	itemid		bigint DEFAULT '0'	NOT NULL,
-	clock		integer		DEFAULT '0'	NOT NULL,
-	value		varchar(255)		DEFAULT ''	NOT NULL,
-	PRIMARY KEY (id)
-) with OIDS;
-CREATE INDEX history_str_sync_1 on history_str_sync (nodeid,id);
