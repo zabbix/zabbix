f98e122e6f451843baa39715060753e3d84798c6
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-06-21 10:10:21 +0000

    - [ZBX-3063] improved db version comparison
    git-svn-id: svn://svn.zabbix.com/branches/dev@20311 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3063/frontends/php/include/db.inc.php
+++ ZBX-3063/frontends/php/include/db.inc.php
@@ -86,7 +86,7 @@ if(!isset($DB)){
 					}
 					elseif(false !== ($server_version = pg_parameter_status('server_version'))){
 						// if server_version exist and begins with 9 then set bytea_output = 'escape', as PG9 new hex bytea default type output
-						if(preg_match('/^9.*/', $server_version)){
+						if((int) $server_version[0] >= 9){
 							DBexecute('set bytea_output = escape');
 						}
 					}
