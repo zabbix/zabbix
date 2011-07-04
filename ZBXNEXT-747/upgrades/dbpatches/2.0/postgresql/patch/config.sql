9e0e8b4e743aa6c6540cbaa471dee55dae596295
rudolfs <rudolfs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-07-04 12:27:38 +0000

    - [ZBXNEXT-747] Log unmatched traps by default
    git-svn-id: svn://svn.zabbix.com/branches/dev@20412 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-747/upgrades/dbpatches/2.0/postgresql/patch/config.sql
+++ ZBXNEXT-747/upgrades/dbpatches/2.0/postgresql/patch/config.sql
@@ -27,7 +27,7 @@ ALTER TABLE ONLY config
 	ADD problem_ack_style integer DEFAULT '0' NOT NULL,
 	ADD ok_unack_style integer DEFAULT '0' NOT NULL,
 	ADD ok_ack_style integer DEFAULT '0' NOT NULL,
-	ADD snmptrap_logging integer DEFAULT '0' NOT NULL;
+	ADD snmptrap_logging integer DEFAULT '1' NOT NULL;
 
 UPDATE config SET alert_usrgrpid=NULL WHERE NOT EXISTS (SELECT 1 FROM usrgrp WHERE usrgrp.usrgrpid=config.alert_usrgrpid);
 UPDATE config SET discovery_groupid=NULL WHERE NOT EXISTS (SELECT 1 FROM groups WHERE groups.groupid=config.discovery_groupid);
