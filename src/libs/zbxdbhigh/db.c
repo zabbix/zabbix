bc535d75a38e517e91737957486a008641d665dd
Rudolfs Kreicbergs <git-no-reply@zabbix.com>
2011-07-08 12:10:36 +0000

    - [ZBXNEXT-807] Commented on the ID structure

--- src/libs/zbxdbhigh/db.c
+++ src/libs/zbxdbhigh/db.c
@@ -27,6 +27,10 @@
 #include "dbcache.h"
 #include "zbxalgo.h"
 
+/* ID structure: NNNSSSDDDDDDDDDDD, where                      */
+/*      NNN - nodeid (to which node the id belongs to)         */
+/*      SSS - source nodeid (in which node was the id created) */
+/*      DDDDDDDDDDD - the ID itself                            */
 const char	*DBnode(const char *fieldid, int nodeid)
 {
 	static char	dbnode[128];
