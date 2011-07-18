378728ab54f55652932355a5b34e88fa0ec43862
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-07-18 08:35:39 +0000

    - [ZBX-3962] the new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@20626 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-3962/upgrades/dbpatches/2.0/oracle/patch/autoreg_host.sql
@@ -0,0 +1,11 @@
+DROP INDEX autoreg_host_1;
+CREATE INDEX autoreg_host_1 ON autoreg_host (proxy_hostid,host);
+ALTER TABLE autoreg_host MODIFY autoreg_hostid DEFAULT NULL;
+ALTER TABLE autoreg_host MODIFY proxy_hostid DEFAULT NULL;
+ALTER TABLE autoreg_host MODIFY proxy_hostid NULL;
+ALTER TABLE autoreg_host ADD listen_ip nvarchar2(39) DEFAULT '';
+ALTER TABLE autoreg_host ADD listen_port number(10) DEFAULT '0' NOT NULL;
+ALTER TABLE autoreg_host ADD listen_dns nvarchar2(64) DEFAULT '';
+UPDATE autoreg_host SET proxy_hostid=NULL WHERE proxy_hostid=0;
+DELETE FROM autoreg_host WHERE proxy_hostid IS NOT NULL AND proxy_hostid NOT IN (SELECT hostid FROM hosts);
+ALTER TABLE autoreg_host ADD CONSTRAINT c_autoreg_host_1 FOREIGN KEY (proxy_hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
