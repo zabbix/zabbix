ce84d4ec4a34998aaaf8979ecb6948259f444dd8
rudolfs <rudolfs@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2011-07-18 10:15:33 +0000

    - [ZBXNEXT-831] macros in interface IP addresses and DNS names are resolved in cache
    git-svn-id: svn://svn.zabbix.com/trunk@20634 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/zabbix_server/poller/poller.c
+++ src/zabbix_server/poller/poller.c
@@ -461,7 +461,7 @@ static int	get_values(unsigned char poller_type)
 	int		errcodes[MAX_BUNCH_ITEMS];
 	zbx_timespec_t	timespecs[MAX_BUNCH_ITEMS];
 	int		i, num;
-	char		*addr, *port;
+	char		*port;
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
 
@@ -480,15 +480,7 @@ static int	get_values(unsigned char poller_type)
 		ZBX_STRDUP(items[i].key, items[i].key_orig);
 		substitute_simple_macros(NULL, NULL, &items[i].host, NULL, &items[i].key, MACRO_TYPE_ITEM_KEY, NULL, 0);
 
-		items[i].interface.addr = (items[i].interface.useip ? items[i].interface.ip_orig : items[i].interface.dns_orig);
-
-		if (INTERFACE_TYPE_AGENT != items[i].interface.type || 1 != items[i].interface.main)
-		{
-			addr = zbx_strdup(NULL, items[i].interface.addr);
-			substitute_simple_macros(NULL, NULL, &items[i].host, NULL,
-					&addr, MACRO_TYPE_INTERFACE_ADDR, NULL, 0);
-			items[i].interface.addr = addr;
-		}
+		items[i].interface.addr = (1 == items[i].interface.useip ? items[i].interface.ip_orig : items[i].interface.dns_orig);
 
 		switch (items[i].type)
 		{
@@ -635,9 +627,6 @@ static int	get_values(unsigned char poller_type)
 		{
 			DCrequeue_unreachable_item(items[i].itemid);
 		}
-
-		if (INTERFACE_TYPE_AGENT != items[i].interface.type || 1 != items[i].interface.main)
-			zbx_free(items[i].interface.addr);
 
 		switch (items[i].type)
 		{
