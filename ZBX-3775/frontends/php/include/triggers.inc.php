508175f8b3192912d20ebaf2145b45b7e5faf099
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-07-21 10:00:51 +0000

    - [ZBX-3775] fixed html formatting
    git-svn-id: svn://svn.zabbix.com/branches/dev@20709 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3775/frontends/php/include/triggers.inc.php
+++ ZBX-3775/frontends/php/include/triggers.inc.php
@@ -2362,8 +2362,8 @@ function utf8RawUrlDecode($source){
 		if(isset($tr_ov_menu)){
 			$tr_ov_menu  = new CPUMenu($tr_ov_menu,170);
 			$status_col->OnClick($tr_ov_menu->GetOnActionJS());
-			$status_col->addAction('onmouseover', 'this.style.border=\'1px dotted #0C0CF0\'');
-			$status_col->addAction('onmouseout', 'this.style.border=\'1px solid #F2F7FA\'');
+			$status_col->addAction('onmouseover', 'jQuery(this).css({border:\'1px dotted #0C0CF0\', padding: \'0px\'})');
+			$status_col->addAction('onmouseout', 'jQuery(this).css({border:\'\'})');
 		}
 		array_push($table_row,$status_col);
 
