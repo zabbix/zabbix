35a5988ac7dfccc5010c37237c71877c86977ca9
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-07-22 09:42:42 +0000

    - [ZBX-3968] removed redundant form fields
    git-svn-id: svn://svn.zabbix.com/branches/dev@20737 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3968/frontends/php/events.php
+++ ZBX-3968/frontends/php/events.php
@@ -196,8 +196,6 @@
 // HEADER {{{
 	$r_form = new CForm(null, 'get');
 	$r_form->addVar('fullscreen',$_REQUEST['fullscreen']);
-	$r_form->addVar('stime', get_request('stime'));
-	$r_form->addVar('period', get_request('period'));
 
 	if(EVENT_SOURCE_TRIGGERS == $source){
 
