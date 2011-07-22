b4851cf3d144c2fefef718d3c61ff7192fa78a2f
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2011-07-22 07:08:36 +0000

    - [ZBXNEXT-747] fixed delimiter in the IBM DB2 patch; removed unused variable

--- upgrades/dbpatches/2.0/ibm_db2/patch/globalvars.sql
+++ upgrades/dbpatches/2.0/ibm_db2/patch/globalvars.sql
@@ -2,4 +2,5 @@ CREATE TABLE globalvars (
 	globalvarid              bigint                                    NOT NULL,
 	snmp_lastsize            integer         WITH DEFAULT '0'          NOT NULL,
 	PRIMARY KEY (globalvarid)
-);
+)
+/
