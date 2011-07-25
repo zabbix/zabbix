a8949166b1326539bb8796a0e8e1c27b5b226272
Konstantin Buravcov <git-no-reply@zabbix.com>
2011-07-25 12:00:59 +0000

    - [ZBX-3980] Changed default value of trigger blinking from 0 to 1

--- upgrades/dbpatches/2.0/mysql/patch/config.sql
+++ upgrades/dbpatches/2.0/mysql/patch/config.sql
@@ -22,10 +22,10 @@ ALTER TABLE config
 	ADD problem_ack_color varchar(6) DEFAULT 'DC0000' NOT NULL,
 	ADD ok_unack_color varchar(6) DEFAULT '00AA00' NOT NULL,
 	ADD ok_ack_color varchar(6) DEFAULT '00AA00' NOT NULL,
-	ADD problem_unack_style integer DEFAULT '0' NOT NULL,
-	ADD problem_ack_style integer DEFAULT '0' NOT NULL,
-	ADD ok_unack_style integer DEFAULT '0' NOT NULL,
-	ADD ok_ack_style integer DEFAULT '0' NOT NULL;
+	ADD problem_unack_style integer DEFAULT '1' NOT NULL,
+	ADD problem_ack_style integer DEFAULT '1' NOT NULL,
+	ADD ok_unack_style integer DEFAULT '1' NOT NULL,
+	ADD ok_ack_style integer DEFAULT '1' NOT NULL;
 
 UPDATE config SET alert_usrgrpid=NULL WHERE NOT alert_usrgrpid IN (SELECT usrgrpid FROM usrgrp);
 UPDATE config SET discovery_groupid=NULL WHERE NOT discovery_groupid IN (SELECT groupid FROM groups);
