cdd163a5b3310b5e1c63eabf9a5584b2101a2819
konstantin <konstantin@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-07-26 09:31:19 +0000

    - [ZBX-3978] Commented previous change (2)
    git-svn-id: svn://svn.zabbix.com/branches/dev@20783 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3978/frontends/php/include/graphs.inc.php
+++ ZBX-3978/frontends/php/include/graphs.inc.php
@@ -1354,7 +1354,7 @@
 			}
 
 			if($next_h >= 0 && $next_m >= 0){
-				// end date does not include upper bound ("-1" makes 23:00 become 22:59)
+				// end date does not include upper bound ("-1" makes 23:00:00 become 22:59:59)
 				$new_time = mktime($next_h, $next_m, 0, $date['mon'], $date['mday'], $date['year']) - 1;
 
 				if($new_time == $time) return $time;
