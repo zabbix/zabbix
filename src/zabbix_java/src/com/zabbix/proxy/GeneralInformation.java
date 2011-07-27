1731d113afa37b5e689192b59431f7eb5610bcf5
Rihards Olups <git-no-reply@zabbix.com>
2011-07-27 17:39:58 +0000

    - [ZBXNEXT-826] bump version to 1.9.6

--- src/zabbix_java/src/com/zabbix/proxy/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/proxy/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	public static final String APPLICATION_NAME = "Zabbix Java Proxy";
 	public static final String REVISION_DATE = "26 July 2011";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "1.9.5";
+	public static final String VERSION = "1.9.6";
 
 	public static void printVersion()
 	{
