57ce7819ea685367edc00e3e5a7b6d898443b930
rich <rich@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2011-07-27 17:39:58 +0000

    - [ZBXNEXT-826] bump version to 1.9.6
    git-svn-id: svn://svn.zabbix.com/trunk@20826 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,7 +19,7 @@
 **/
 ?>
 <?php
-	define('ZABBIX_VERSION','1.9.5');
+	define('ZABBIX_VERSION','1.9.6');
 	define('ZABBIX_API_VERSION','1.2');
 /* USER DEFINES */
 
--- include/common.h
+++ include/common.h
@@ -122,7 +122,7 @@
 
 #define	APPLICATION_NAME	"Zabbix Agent"
 #define	ZABBIX_REVDATE		"26 July 2011"
-#define	ZABBIX_VERSION		"1.9.5"
+#define	ZABBIX_VERSION		"1.9.6"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
 #if defined(_WINDOWS)
