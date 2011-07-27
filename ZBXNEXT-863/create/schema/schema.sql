31bfec62c3cf8a16a7c5e297fffd8915e4b749af
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-07-27 16:46:10 +0000

    - [ZBXNEXT-863] Do not rename user profile to inventory in schema.
    git-svn-id: svn://svn.zabbix.com/branches/dev@20824 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-863/create/schema/schema.sql
+++ ZBXNEXT-863/create/schema/schema.sql
@@ -861,8 +861,8 @@ FIELD		|gsm_modem	|t_varchar(255)	|''	|NOT NULL	|ZBX_SYNC
 FIELD		|username	|t_varchar(255)	|''	|NOT NULL	|ZBX_SYNC
 FIELD		|passwd		|t_varchar(255)	|''	|NOT NULL	|ZBX_SYNC
 
-TABLE|inventories|inventoryid|0
-FIELD		|inventoryid	|t_id		|	|NOT NULL	|0
+TABLE|profiles|profileid|0
+FIELD		|profileid	|t_id		|	|NOT NULL	|0
 FIELD		|userid		|t_id		|	|NOT NULL	|0			|1|users
 FIELD		|idx		|t_varchar(96)	|''	|NOT NULL	|0
 FIELD		|idx2		|t_id		|'0'	|NOT NULL	|0
@@ -872,7 +872,7 @@ FIELD		|value_str	|t_varchar(255)	|''	|NOT NULL	|0
 FIELD		|source		|t_varchar(96)	|''	|NOT NULL	|0
 FIELD		|type		|t_integer	|'0'	|NOT NULL	|0
 INDEX		|1		|userid,idx,idx2
-INDEX		|2		|userid,inventoryid
+INDEX		|2		|userid,profileid
 
 TABLE|rights|rightid|ZBX_SYNC
 FIELD		|rightid	|t_id		|	|NOT NULL	|0
