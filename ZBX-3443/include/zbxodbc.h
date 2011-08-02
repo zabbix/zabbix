be526db715568c1bed6b70be17d76e2a3b6d0c67
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-08-02 14:59:22 +0000

    - [ZBX-3443] Created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@20899 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-3443/include/zbxodbc.h
@@ -0,0 +1,52 @@
+/*
+** ZABBIX
+** Copyright (C) 2000-2005 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+
+
+#ifndef ZABBIX_ZODBC_H
+#define ZABBIX_ZODBC_H
+
+#include <sql.h>
+#include <sqlext.h>
+#include <sqltypes.h>
+
+typedef char**		ZBX_ODBC_ROW;
+
+typedef struct zbx_odbc_dbh_s
+{
+	SQLHENV		henv;
+	SQLHDBC		hdbc;
+	unsigned short	connected;
+	SQLHSTMT	hstmt;
+	SQLSMALLINT     col_num;
+	ZBX_ODBC_ROW	row_data;
+	SQLINTEGER	*data_len;
+} ZBX_ODBC_DBH;
+
+typedef ZBX_ODBC_DBH*		ZBX_ODBC_RESULT;
+
+int		odbc_DBconnect(ZBX_ODBC_DBH *pdbh, const char *db_name, const char *user, const char *pass);
+void		odbc_DBclose(ZBX_ODBC_DBH *pdbh);
+
+int	odbc_DBexecute(ZBX_ODBC_DBH *pdbh, const char *query);
+ZBX_ODBC_RESULT odbc_DBselect(ZBX_ODBC_DBH *pdbh, const char *query);
+ZBX_ODBC_ROW    odbc_DBfetch(ZBX_ODBC_RESULT pdbh);
+
+const char	*get_last_odbc_strerror();
+
+#endif /* ZABBIX_ZODBC_H */
