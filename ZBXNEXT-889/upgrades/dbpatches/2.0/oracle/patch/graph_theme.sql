d8122f9695ac301a7d2453fa69ab4fd8897abdc1
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-08-17 11:39:19 +0000

    - [ZBXNEXT-889] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@21172 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-889/upgrades/dbpatches/2.0/oracle/patch/graph_theme.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE graph_theme MODIFY graphthemeid DEFAULT NULL;
-ALTER TABLE graph_theme MODIFY noneworktimecolor DEFAULT 'CCCCCC';
-ALTER TABLE graph_theme RENAME COLUMN noneworktimecolor TO nonworktimecolor;
