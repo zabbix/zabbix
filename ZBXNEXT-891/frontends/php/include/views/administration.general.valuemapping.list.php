5791dd1f14c5fdba6c9abcb545086ba2d62a35c0
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-08-19 07:30:45 +0000

    - [ZBXNEXT-891] redesign general->Value mapping
    git-svn-id: svn://svn.zabbix.com/branches/dev@21218 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-891/frontends/php/include/views/administration.general.valuemapping.list.php
@@ -0,0 +1,47 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2011 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+?>
+<?php
+
+$this->data['cnf_wdgt']->addHeader(_('Value mapping'));
+
+$valueMappingForm = new CForm();
+$valueMappingForm->setName('valuemappingForm');
+$valueMappingForm->addVar('config', get_request('config', 6));
+$valueMappingForm->addItem(BR());
+
+$valueMappingTable = new CTableInfo();
+$valueMappingTable->setHeader(array(_('Name'), _('Value map')));
+
+foreach($this->data['valuemaps'] as $valuemap) {
+	$maps = $valuemap['maps'];
+	order_result($maps, 'value');
+
+	$mappings_row = array();
+	foreach($maps as $map) {
+		array_push($mappings_row, $map['value'], SPACE.RARR.SPACE, $map['newvalue'], BR());
+	}
+	$valueMappingTable->addRow(array(new CLink($valuemap['name'],'config.php?form=update&valuemapid='.$valuemap['valuemapid'].url_param('config')), empty($mappings_row) ? SPACE : $mappings_row));
+}
+
+$valueMappingForm->addItem($valueMappingTable);
+
+return $valueMappingForm;
+?>
