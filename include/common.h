d77e9f22449646735a8c046a966a9898e83f010a
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-08-23 19:37:57 +0000

    - [DEV-493] updated release date
    git-svn-id: svn://svn.zabbix.com/branches/1.8@21307 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -121,7 +121,7 @@
 #define OFF	0
 
 #define	APPLICATION_NAME	"Zabbix Agent"
-#define	ZABBIX_REVDATE		"4 August 2011"
+#define	ZABBIX_REVDATE		"24 August 2011"
 #define	ZABBIX_VERSION		"1.8.7rc1"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
