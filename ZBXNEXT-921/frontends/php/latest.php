de5ed7f26f234aa0fa81dcec2febdca6ad844490
Vedmak <Vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-08-25 09:22:45 +0000

    - [ZBXNEXT-921] added ability to see items without data in latest data
    git-svn-id: svn://svn.zabbix.com/branches/dev@21327 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-921/frontends/php/latest.php
+++ ZBXNEXT-921/frontends/php/latest.php
@@ -46,7 +46,6 @@ include_once('include/page_header.php');
 		'applicationid'=>	array(T_ZBX_INT, O_OPT,	NULL,	DB_ID,		NULL),
 		'close'=>			array(T_ZBX_INT, O_OPT,	NULL,	IN('1'),	NULL),
 		'open'=>			array(T_ZBX_INT, O_OPT,	NULL,	IN('1'),	NULL),
-		'groupbyapp'=>		array(T_ZBX_INT, O_OPT,	NULL,	IN('1'),	NULL),
 
 		'groupid'=>		array(T_ZBX_INT, O_OPT,	P_SYS,	DB_ID,		NULL),
 		'hostid'=>		array(T_ZBX_INT, O_OPT,	P_SYS,	DB_ID,		NULL),
@@ -54,6 +53,7 @@ include_once('include/page_header.php');
 		'fullscreen'=>	array(T_ZBX_INT, O_OPT,	P_SYS,	IN('0,1'),		NULL),
 // filter
 		'select'=>			array(T_ZBX_STR, O_OPT, NULL,	NULL,		NULL),
+		'show_without_data'=>array(T_ZBX_INT, O_OPT, NULL,	IN('1'),		NULL),
 		'filter_rst'=>		array(T_ZBX_INT, O_OPT,	P_SYS,	IN(array(0,1)),	NULL),
 		'filter_set'=>		array(T_ZBX_STR, O_OPT,	P_SYS,	null,	NULL),
 //ajax
@@ -63,11 +63,6 @@ include_once('include/page_header.php');
 	);
 
 	check_fields($fields);
-
-// HEADER REQUEST
-	$_REQUEST['select'] = get_request('select',CProfile::get('web.latest.filter.select', ''));
-	CProfile::update('web.latest.filter.select', $_REQUEST['select'], PROFILE_TYPE_STR);
-//----------------
 ?>
 <?php
 /* AJAX */
@@ -75,15 +70,6 @@ include_once('include/page_header.php');
 		if('filter' == $_REQUEST['favobj']){
 			CProfile::update('web.latest.filter.state',$_REQUEST['state'], PROFILE_TYPE_INT);
 		}
-/*
-		else if('refresh' == $_REQUEST['favobj']){
-			switch($_REQUEST['favref']){
-				case ZBX_PAGE_MAIN_HAT:
-					include_once('blocks/latest.page.php');
-					break;
-			}
-		}
-//*/
 	}
 
 	if((PAGE_TYPE_JS == $page['type']) || (PAGE_TYPE_HTML_BLOCK == $page['type'])){
@@ -93,12 +79,24 @@ include_once('include/page_header.php');
 //--------
 
 /* FILTER */
+	if (!isset($_REQUEST['show_without_data'])) {
+		$_REQUEST['show_without_data'] = 0;
+	}
+
 	if(isset($_REQUEST['filter_rst'])){
 		$_REQUEST['select'] = '';
+		$_REQUEST['show_without_data'] = 0;
 	}
+
 	if(isset($_REQUEST['filter_set']) || isset($_REQUEST['filter_rst'])){
 		CProfile::update('web.latest.filter.select',$_REQUEST['select'], PROFILE_TYPE_STR);
+		CProfile::update('web.latest.filter.show_without_data',$_REQUEST['show_without_data'], PROFILE_TYPE_INT);
 	}
+	else {
+		$_REQUEST['select'] = CProfile::get('web.latest.filter.select', '');
+		$_REQUEST['show_without_data'] = CProfile::get('web.latest.filter.show_without_data', 0);
+	}
+
 // --------------
 
 	$latest_wdgt = new CWidget();
@@ -110,17 +108,12 @@ include_once('include/page_header.php');
 // 2nd header
 	$r_form = new CForm(null, 'get');
 
-//	$cmbGroup = new CComboBox('groupid',$_REQUEST['groupid'],"javascript: return updater.onetime_update('".ZBX_PAGE_MAIN_HAT."',this.form);");
-//	$cmbHosts = new CComboBox('hostid',$_REQUEST['hostid'],"javascript: return updater.onetime_update('".ZBX_PAGE_MAIN_HAT."',this.form);");
-
 	$options = array(
 		'groups' => array(
 			'monitored_hosts' => 1,
-			'with_historical_items' => 1,
 		),
 		'hosts' => array(
 			'monitored_hosts' => 1,
-			'with_historical_items' => 1,
 		),
 		'hostid' => get_request('hostid', null),
 		'groupid' => get_request('groupid', null),
@@ -144,6 +137,7 @@ include_once('include/page_header.php');
 	$filterForm->setAttribute('id','zbx_filter');
 
 	$filterForm->addRow(S_SHOW_ITEMS_WITH_DESCRIPTION_LIKE, new CTextBox('select',$_REQUEST['select'],20));
+	$filterForm->addRow(S_SHOW_ITEMS_WITHOUT_DATA, new CCheckBox('show_without_data', $_REQUEST['show_without_data'], null, 1));
 
 	$reset = new CButton("filter_rst",S_RESET);
 	$reset->setType('button');
@@ -156,9 +150,6 @@ include_once('include/page_header.php');
 //-------
 
 	validate_sort_and_sortorder('i.description',ZBX_SORT_UP);
-
-	$_REQUEST['groupbyapp'] = get_request('groupbyapp',CProfile::get('web.latest.groupbyapp',1));
-	CProfile::update('web.latest.groupbyapp',$_REQUEST['groupbyapp'],PROFILE_TYPE_INT);
 
 	$_REQUEST['apps'] = get_request('apps', array());
 	$apps = zbx_toHash($_REQUEST['apps']);
@@ -178,12 +169,6 @@ include_once('include/page_header.php');
 		$apps = array_slice($apps, -35);
 	}
 
-	/* limit opened application count */
-	// while(count($apps) > 25){
-		// array_shift($apps);
-	// }
-
-	// CProfile::update('web.latest.applications',$apps,PROFILE_TYPE_ARRAY_ID);
 ?>
 <?php
 	if(isset($showAll)){
@@ -255,7 +240,8 @@ include_once('include/page_header.php');
 	$sql = 'SELECT DISTINCT i.*, ia.applicationid '.
 			' FROM items i,items_applications ia'.
 			' WHERE '.DBcondition('ia.applicationid',$db_appids).
-				' AND i.itemid=ia.itemid AND i.lastvalue IS NOT NULL'.
+				' AND i.itemid=ia.itemid'.
+				($_REQUEST['show_without_data'] ? '' : ' AND i.lastvalue IS NOT NULL').
 				' AND (i.status='.ITEM_STATUS_ACTIVE.' OR i.status='.ITEM_STATUS_NOTSUPPORTED.')'.
 			order_by('i.description,i.itemid,i.lastclock');
 
@@ -386,7 +372,7 @@ include_once('include/page_header.php');
 			' WHERE ia.itemid is NULL '.
 				$sql_where.
 				' AND h.hostid=i.hostid '.
-				' AND '.DBcondition('h.hostid',$available_hosts).
+				' AND '.DBcondition('h.hostid', $available_hosts).
 			' ORDER BY h.host';
 
 	$db_host_res = DBselect($sql);
@@ -406,7 +392,7 @@ include_once('include/page_header.php');
 			' WHERE ia.itemid is NULL '.
 				$sql_where.
 				' AND h.hostid=i.hostid '.
-				' AND i.lastvalue IS NOT NULL '.
+				($_REQUEST['show_without_data'] ? '' : ' AND i.lastvalue IS NOT NULL').
 				' AND (i.status='.ITEM_STATUS_ACTIVE.' OR i.status='.ITEM_STATUS_NOTSUPPORTED.')'.
 				' AND '.DBcondition('h.hostid', $db_hostids).
 			' ORDER BY i.description,i.itemid';
