32c1d7fb9807e25654fedfbab24b06048d482761
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-09-01 13:26:31 +0000

    - [ZBXNEXT-826] bump version to 1.8.8rc1
    git-svn-id: svn://svn.zabbix.com/branches/1.8@21420 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -122,7 +122,7 @@
 
 #define	APPLICATION_NAME	"Zabbix Agent"
 #define	ZABBIX_REVDATE		"1 September 2011"
-#define	ZABBIX_VERSION		"1.8.7"
+#define	ZABBIX_VERSION		"1.8.8rc1"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
 #if defined(_WINDOWS)
