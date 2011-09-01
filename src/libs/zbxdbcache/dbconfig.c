8f6f6981ec7df0ed17c4cf2bc18bcfa5985066b5
Rudolfs Kreicbergs <git-no-reply@zabbix.com>
2011-09-01 08:29:00 +0000

    - [ZBX-4045] added check for duplicate proxies for merging to trunk

--- src/libs/zbxdbcache/dbconfig.c
+++ src/libs/zbxdbcache/dbconfig.c
@@ -1577,11 +1577,12 @@ static void	DCsync_hosts(DB_RESULT result)
 
 			if (NULL != host_ph)
 			{
-				if (HOST_STATUS_MONITORED == host_ph->status && (0 == found || sync_num == host_ph->sync_num))
+				if (0 == found || sync_num == host_ph->sync_num)
 				{
-					/* duplicate hosts found */
+					/* duplicate hosts or proxies found */
 
-					zabbix_log(LOG_LEVEL_CRIT, "Error: duplicate hosts [%s] found. Exiting...",
+					zabbix_log(LOG_LEVEL_CRIT, "Error: duplicate %s [%s] found. Exiting...",
+							HOST_STATUS_MONITORED == host_ph->status ? "hosts" : "proxies",
 							host_ph->host);
 					exit(FAIL);
 				}
