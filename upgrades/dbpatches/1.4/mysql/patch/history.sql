48e7b9fbd8d61e14b8a025388ed2fefbff836c0b
Rihards Olups <git-no-reply@zabbix.com>
2011-09-04 19:04:30 +0000

    - [ZBX-3843] revert a bad merge in revision 18402 from ZBX-1419; thanks to sasha for spotting

--- upgrades/dbpatches/1.4/mysql/patch/history.sql
+++ upgrades/dbpatches/1.4/mysql/patch/history.sql
@@ -3,17 +3,8 @@ CREATE TABLE history_tmp (
 	clock		integer		DEFAULT '0'	NOT NULL,
 	value		double(16,4)		DEFAULT '0.0000'	NOT NULL
 ) ENGINE=InnoDB;
-
-CREATE TABLE history (
-  itemid                int(4)          DEFAULT '0' NOT NULL,
-  clock                 int(4)          DEFAULT '0' NOT NULL,
-  value                 double(16,4)    DEFAULT '0.0000' NOT NULL,
---  PRIMARY KEY (itemid,clock)
-  KEY itemidclock (itemid, clock)
-) type=InnoDB;
-
+CREATE INDEX history_1 on history_tmp (itemid,clock);
 
 insert into history_tmp select * from history;
-CREATE INDEX history_1 on history_tmp (itemid,clock);
 drop table history;
 alter table history_tmp rename history;
