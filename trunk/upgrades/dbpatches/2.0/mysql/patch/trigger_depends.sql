d8d3ad34a7961b333be8ce9be45cecd0024bf1d9
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-09-12 09:47:18 +0000

    - [ZBX-1207] branch created
    git-svn-id: svn://svn.zabbix.com/branches/dev@21607 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ trunk/upgrades/dbpatches/2.0/mysql/patch/trigger_depends.sql
@@ -0,0 +1,9 @@
+ALTER TABLE trigger_depends MODIFY triggerdepid bigint unsigned NOT NULL,
+			    MODIFY triggerid_down bigint unsigned NOT NULL,
+			    MODIFY triggerid_up bigint unsigned NOT NULL;
+DROP INDEX trigger_depends_1 ON trigger_depends;
+DELETE FROM trigger_depends WHERE triggerid_down NOT IN (SELECT triggerid FROM triggers);
+DELETE FROM trigger_depends WHERE triggerid_up NOT IN (SELECT triggerid FROM triggers);
+CREATE UNIQUE INDEX trigger_depends_1 ON trigger_depends (triggerid_down,triggerid_up);
+ALTER TABLE trigger_depends ADD CONSTRAINT c_trigger_depends_1 FOREIGN KEY (triggerid_down) REFERENCES triggers (triggerid) ON DELETE CASCADE;
+ALTER TABLE trigger_depends ADD CONSTRAINT c_trigger_depends_2 FOREIGN KEY (triggerid_up) REFERENCES triggers (triggerid) ON DELETE CASCADE;
