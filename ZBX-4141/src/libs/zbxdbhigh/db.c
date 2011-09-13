ffe19a91ee51f97c9bb2dd843106ae0b260a7c67
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-09-13 09:42:19 +0000

    - [ZBX-4141] the changes created under ZBX-3970 are partially returned
    git-svn-id: svn://svn.zabbix.com/branches/dev@21625 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4141/src/libs/zbxdbhigh/db.c
+++ ZBX-4141/src/libs/zbxdbhigh/db.c
@@ -118,12 +118,12 @@ void	DBbegin()
 
 	rc = zbx_db_begin();
 
-	while (ZBX_DB_OK > rc)
+	while (ZBX_DB_DOWN == rc)
 	{
 		DBclose();
 		DBconnect(ZBX_DB_CONNECT_NORMAL);
 
-		if (ZBX_DB_OK > (rc = zbx_db_begin()))
+		if (ZBX_DB_DOWN == (rc = zbx_db_begin()))
 		{
 			zabbix_log(LOG_LEVEL_WARNING, "Database is down. Retrying in 10 seconds.");
 			sleep(10);
@@ -148,12 +148,12 @@ void	DBcommit()
 
 	rc = zbx_db_commit();
 
-	while (ZBX_DB_OK > rc)
+	while (ZBX_DB_DOWN == rc)
 	{
 		DBclose();
 		DBconnect(ZBX_DB_CONNECT_NORMAL);
 
-		if (ZBX_DB_OK > (rc = zbx_db_commit()))
+		if (ZBX_DB_DOWN == (rc = zbx_db_commit()))
 		{
 			zabbix_log(LOG_LEVEL_WARNING, "Database is down. Retrying in 10 seconds.");
 			sleep(10);
@@ -178,12 +178,12 @@ void	DBrollback()
 
 	rc = zbx_db_rollback();
 
-	while (ZBX_DB_OK > rc)
+	while (ZBX_DB_DOWN == rc)
 	{
 		DBclose();
 		DBconnect(ZBX_DB_CONNECT_NORMAL);
 
-		if (ZBX_DB_OK > (rc = zbx_db_rollback()))
+		if (ZBX_DB_DOWN == (rc = zbx_db_rollback()))
 		{
 			zabbix_log(LOG_LEVEL_WARNING, "Database is down. Retrying in 10 seconds.");
 			sleep(10);
@@ -209,12 +209,12 @@ int	__zbx_DBexecute(const char *fmt, ...)
 
 	rc = zbx_db_vexecute(fmt, args);
 
-	while (ZBX_DB_OK > rc)
+	while (ZBX_DB_DOWN == rc)
 	{
 		DBclose();
 		DBconnect(ZBX_DB_CONNECT_NORMAL);
 
-		if (ZBX_DB_OK > (rc = zbx_db_vexecute(fmt, args)))
+		if (ZBX_DB_DOWN == (rc = zbx_db_vexecute(fmt, args)))
 		{
 			zabbix_log(LOG_LEVEL_WARNING, "Database is down. Retrying in 10 seconds.");
 			sleep(10);
