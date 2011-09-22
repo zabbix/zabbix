43b78f9bfb27b180c5ea0b860fd7a9120ad285c6
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-09-22 14:19:20 +0000

    - [ZBXNEXT-918] redesign Administration->Media types
    git-svn-id: svn://svn.zabbix.com/trunk@21851 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/classes/class.cpassbox.php
+++ frontends/php/include/classes/class.cpassbox.php
@@ -20,8 +20,9 @@
 ?>
 <?php
 class CPassBox extends CInput {
-	public function __construct($name = 'password', $value = '', $maxlength = 255) {
+	public function __construct($name = 'password', $value = '', $size = 50, $maxlength = 255) {
 		parent::__construct('password', $name, $value);
+		$this->setAttribute('size', $size);
 		$this->setAttribute('maxlength', $maxlength);
 	}
 }
