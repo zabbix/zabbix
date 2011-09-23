05396b85fe74d9347a595865530232ad35d4759c
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-09-23 16:07:28 +0000

    - [ZBX-3493] merge from trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@21899 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3493/frontends/php/include/media.inc.php
+++ ZBX-3493/frontends/php/include/media.inc.php
@@ -19,42 +19,41 @@
 **/
 ?>
 <?php
+function media_type2str($type = null) {
+	$media_types = array(
+		MEDIA_TYPE_EMAIL => _('Email'),
+		MEDIA_TYPE_EXEC => _('Script'),
+		MEDIA_TYPE_SMS => _('SMS'),
+		MEDIA_TYPE_JABBER => _('Jabber'),
+		MEDIA_TYPE_EZ_TEXTING => _('Ez Texting'),
+	);
 
-	function media_type2str($type=null){
-		$media_types = array(
-			MEDIA_TYPE_EMAIL => S_EMAIL,
-			MEDIA_TYPE_EXEC => S_SCRIPT,
-			MEDIA_TYPE_SMS => S_SMS,
-			MEDIA_TYPE_JABBER => S_JABBER,
-			MEDIA_TYPE_EZ_TEXTING => S_EZ_TEXTING,
-		);
-
-		if(is_null($type)){
-			natsort($media_types);
-			return $media_types;
-		}
-		else if(isset($media_types[$type]))
-			return $media_types[$type];
-		else
-			return S_UNKNOWN;
+	if (is_null($type)) {
+		natsort($media_types);
+		return $media_types;
 	}
+	elseif (isset($media_types[$type])) {
+		return $media_types[$type];
+	}
+	else {
+		return _('Unknown');
+	}
+}
 
-	function media_severity2str($severity){
-		$mapping = array(
-			0 => array('letter' => 'N', 'style' => (($severity & 1)  ? 'enabled' : NULL)),
-			1 => array('letter' => 'I', 'style' => (($severity & 2)  ? 'enabled' : NULL)),
-			2 => array('letter' => 'W', 'style' => (($severity & 4)  ? 'enabled' : NULL)),
-			3 => array('letter' => 'A', 'style' => (($severity & 8)  ? 'enabled' : NULL)),
-			4 => array('letter' => 'H', 'style' => (($severity & 16) ? 'enabled' : NULL)),
-			5 => array('letter' => 'D', 'style' => (($severity & 32) ? 'enabled' : NULL))
-		);
+function media_severity2str($severity) {
+	$mapping = array(
+		0 => array('letter' => 'N', 'style' => (($severity & 1)  ? 'enabled' : NULL)),
+		1 => array('letter' => 'I', 'style' => (($severity & 2)  ? 'enabled' : NULL)),
+		2 => array('letter' => 'W', 'style' => (($severity & 4)  ? 'enabled' : NULL)),
+		3 => array('letter' => 'A', 'style' => (($severity & 8)  ? 'enabled' : NULL)),
+		4 => array('letter' => 'H', 'style' => (($severity & 16) ? 'enabled' : NULL)),
+		5 => array('letter' => 'D', 'style' => (($severity & 32) ? 'enabled' : NULL))
+	);
 
-		foreach($mapping as $id => $map){
-			$result[$id] = new CSpan($map['letter'], $map['style']);
-			$result[$id]->SetHint(getSeverityCaption($id)." (".(isset($map['style']) ? "on" : "off").")");
-		}
-
+	foreach ($mapping as $id => $map) {
+		$result[$id] = new CSpan($map['letter'], $map['style']);
+		$result[$id]->setHint(getSeverityCaption($id).' ('.(isset($map['style']) ? 'on' : 'off').')');
+	}
 	return $result;
-	}
-
+}
 ?>
