14da9aefc6d081096078353a4d72e6710fa8e9ed
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-10-04 10:58:53 +0000

    - [ZBXNEXT-951] added possibility to calculate CPU load per processor: system.cpu.load[percpu,avgX]
    git-svn-id: svn://svn.zabbix.com/trunk@22115 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxsysinfo/osf/cpu.c
+++ src/libs/zbxsysinfo/osf/cpu.c
@@ -19,151 +19,52 @@
 
 #include "common.h"
 #include "sysinfo.h"
-
-static int	SYSTEM_CPU_IDLE1(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
-{
-	return EXECUTE_DBL(cmd, "iostat 1 2 | tail -n 1 | awk '{printf(\"%s\",$(NF))}'", flags, result);
-}
-
-static int	SYSTEM_CPU_SYS1(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
-{
-	return EXECUTE_DBL(cmd, "iostat 1 2 | tail -n 1 | awk '{printf(\"%s\",$(NF-1))}'", flags, result);
-}
-
-static int	SYSTEM_CPU_NICE1(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
-{
-	return EXECUTE_DBL(cmd, "iostat 1 2 | tail -n 1 | awk '{printf(\"%s\",$(NF-2))}'", flags, result);
-}
-
-static int	SYSTEM_CPU_USER1(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
-{
-	return EXECUTE_DBL(cmd, "iostat 1 2 | tail -n 1 | awk '{printf(\"%s\",$(NF-3))}'", flags, result);
-}
+#include "../common/common.h"
 
 int	SYSTEM_CPU_UTIL(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	TYPE_MODE_FUNCTION fl[] =
-	{
-		{"idle",	"avg1" ,	SYSTEM_CPU_IDLE1},
-		{"nice",	"avg1" ,	SYSTEM_CPU_NICE1},
-		{"user",	"avg1" ,	SYSTEM_CPU_USER1},
-		{"system",	"avg1" ,	SYSTEM_CPU_SYS1},
-		{0,		0,		0}
-	};
+	char	tmp[16];
 
-	char cpuname[MAX_STRING_LEN];
-	char type[MAX_STRING_LEN];
-	char mode[MAX_STRING_LEN];
-	int i;
-
-        if(num_param(param) > 3)
-        {
-                return SYSINFO_RET_FAIL;
-        }
-
-        if(get_param(param, 1, cpuname, sizeof(cpuname)) != 0)
-        {
-                return SYSINFO_RET_FAIL;
-        }
-	if(cpuname[0] == '\0')
-	{
-		/* default parameter */
-		zbx_snprintf(cpuname, sizeof(cpuname), "all");
-	}
-	if(strncmp(cpuname, "all", sizeof(cpuname)))
-	{
+	if (3 < num_param(param))
 		return SYSINFO_RET_FAIL;
-	}
 
-	if(get_param(param, 2, type, sizeof(type)) != 0)
-        {
-                type[0] = '\0';
-        }
-        if(type[0] == '\0')
-	{
-		/* default parameter */
-		zbx_snprintf(type, sizeof(type), "user");
-	}
+	/* only "all" (default) for parameter "cpu" is supported */
+	if (0 == get_param(param, 1, tmp, sizeof(tmp)) && '\0' != *tmp && 0 != strcmp(tmp, "all"))
+		return SYSINFO_RET_FAIL;
 
-	if(get_param(param, 3, mode, sizeof(mode)) != 0)
-        {
-                mode[0] = '\0';
-        }
+	/* only "avg1" (default) for parameter "mode" is supported */
+	if (0 == get_param(param, 3, tmp, sizeof(tmp)) && '\0' != *tmp && 0 != strcmp(tmp, "avg1"))
+		return SYSINFO_RET_FAIL;
 
-        if(mode[0] == '\0')
-	{
-		/* default parameter */
-		zbx_snprintf(mode, sizeof(mode), "avg1");
-	}
-
-	for(i=0; fl[i].type!=0; i++)
-		if(strncmp(type, fl[i].type, MAX_STRING_LEN)==0)
-			if(strncmp(mode, fl[i].mode, MAX_STRING_LEN)==0)
-				return (fl[i].function)(cmd, param, flags, result);
+	if (0 != get_param(param, 2, tmp, sizeof(tmp)) || '\0' == *tmp || 0 == strcmp(tmp, "user"))
+		return EXECUTE_DBL(cmd, "iostat 1 2 | tail -n 1 | awk '{printf(\"%s\",$(NF-3))}'", flags, result);
+	else if (0 == strcmp(tmp, "nice"))
+		return EXECUTE_DBL(cmd, "iostat 1 2 | tail -n 1 | awk '{printf(\"%s\",$(NF-2))}'", flags, result);
+	else if (0 == strcmp(tmp, "system"))
+		return EXECUTE_DBL(cmd, "iostat 1 2 | tail -n 1 | awk '{printf(\"%s\",$(NF-1))}'", flags, result);
+	else if (0 == strcmp(tmp, "idle"))
+		return EXECUTE_DBL(cmd, "iostat 1 2 | tail -n 1 | awk '{printf(\"%s\",$(NF))}'", flags, result);
 
 	return SYSINFO_RET_FAIL;
 }
 
-static int	SYSTEM_CPU_LOAD1(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
-{
-	return EXECUTE_DBL(cmd, "uptime | awk '{printf(\"%s\", $(NF))}' | sed 's/[ ,]//g'", flags, result);
-}
-
-static int	SYSTEM_CPU_LOAD5(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
-{
-	return EXECUTE_DBL(cmd, "uptime | awk '{printf(\"%s\", $(NF-1))}' | sed 's/[ ,]//g'", flags, result);
-}
-
-static int	SYSTEM_CPU_LOAD15(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
-{
-	return EXECUTE_DBL(cmd, "uptime | awk '{printf(\"%s\", $(NF-2))}' | sed 's/[ ,]//g'", flags, result);
-}
-
 int	SYSTEM_CPU_LOAD(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	MODE_FUNCTION fl[] =
-	{
-		{"avg1" ,	SYSTEM_CPU_LOAD1},
-		{"avg5" ,	SYSTEM_CPU_LOAD5},
-		{"avg15",	SYSTEM_CPU_LOAD15},
-		{0,		0}
-	};
+	char	tmp[16];
 
-	char cpuname[MAX_STRING_LEN];
-	char mode[MAX_STRING_LEN];
-	int i;
-
-        if(num_param(param) > 2)
-        {
-                return SYSINFO_RET_FAIL;
-        }
-
-        if(get_param(param, 1, cpuname, sizeof(cpuname)) != 0)
-        {
-                return SYSINFO_RET_FAIL;
-        }
-	if(cpuname[0] == '\0')
-	{
-		/* default parameter */
-		zbx_snprintf(cpuname, sizeof(cpuname), "all");
-	}
-	if(strncmp(cpuname, "all", MAX_STRING_LEN))
-	{
+	if (2 < num_param(param))
 		return SYSINFO_RET_FAIL;
-	}
 
-	if(get_param(param, 2, mode, sizeof(mode)) != 0)
-        {
-                mode[0] = '\0';
-        }
-        if(mode[0] == '\0')
-	{
-		/* default parameter */
-		zbx_snprintf(mode, sizeof(mode), "avg1");
-	}
-	for(i=0; fl[i].mode!=0; i++)
-		if(strncmp(mode, fl[i].mode, MAX_STRING_LEN)==0)
-			return (fl[i].function)(cmd, param, flags, result);
+	/* only "all" (default) for parameter "cpu" is supported */
+	if (0 == get_param(param, 1, tmp, sizeof(tmp)) && '\0' != *tmp && 0 != strcmp(tmp, "all"))
+		return SYSINFO_RET_FAIL;
+
+	if (0 != get_param(param, 2, tmp, sizeof(tmp)) || '\0' == *tmp || 0 == strcmp(tmp, "avg1"))
+		return EXECUTE_DBL(cmd, "uptime | awk '{printf(\"%s\", $(NF))}' | sed 's/[ ,]//g'", flags, result);
+	else if (0 == strcmp(tmp, "avg5"))
+		return EXECUTE_DBL(cmd, "uptime | awk '{printf(\"%s\", $(NF-1))}' | sed 's/[ ,]//g'", flags, result);
+	else if (0 == strcmp(tmp, "avg15"))
+		return EXECUTE_DBL(cmd, "uptime | awk '{printf(\"%s\", $(NF-2))}' | sed 's/[ ,]//g'", flags, result);
 
 	return SYSINFO_RET_FAIL;
 }
