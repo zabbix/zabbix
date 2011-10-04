4af8293bc71231317f62d38f92555023c9e60187
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-10-04 12:56:07 +0000

    - [ZBX-3942] Disable collecting perfstat data on AIX in agent until    the data is requested using system.stat .
    git-svn-id: svn://svn.zabbix.com/branches/1.8@22129 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxsysinfo/aix/uptime.c
+++ src/libs/zbxsysinfo/aix/uptime.c
@@ -20,16 +20,26 @@
 #include "common.h"
 #include "sysinfo.h"
 
+static long	hertz = 0;
+
 int	SYSTEM_UPTIME(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
 #if defined(HAVE_LIBPERFSTAT)
 	perfstat_cpu_total_t	ps_cpu_total;
 
+	if (0 == hertz)
+	{
+		hertz = sysconf(_SC_CLK_TCK);
+
+		/* make sure we do not divide by 0 */
+		assert(hertz);
+	}
+
 	/* AIX 6.1 */
 	if (-1 == perfstat_cpu_total(NULL, &ps_cpu_total, sizeof(ps_cpu_total), 1))
 		return SYSINFO_RET_FAIL;
 
-	SET_UI64_RESULT(result, (zbx_uint64_t)((double)ps_cpu_total.lbolt / 100));
+	SET_UI64_RESULT(result, (zbx_uint64_t)((double)ps_cpu_total.lbolt / hertz));
 
 	return SYSINFO_RET_OK;
 #else
