823717e04aab077fa1338d2118be5f5b88fae7f3
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-10-04 09:00:45 +0000

    - [ZBX-3342] branch created
    git-svn-id: svn://svn.zabbix.com/branches/dev@22099 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-3342/upgrades/dbpatches/1.6/mysql/patch/graphs_items.sql
@@ -0,0 +1,15 @@
+update graphs_items set color='FF0000' where color='Red';
+update graphs_items set color='960000' where color='Dark Red';
+update graphs_items set color='00FF00' where color='Green';
+update graphs_items set color='009600' where color='Dark Green';
+update graphs_items set color='0000FF' where color='Blue';
+update graphs_items set color='000096' where color='Dark Blue';
+update graphs_items set color='FFFF00' where color='Yellow';
+update graphs_items set color='969600' where color='Dark Yellow';
+update graphs_items set color='00FFFF' where color='Cyan';
+update graphs_items set color='000000' where color='Black';
+update graphs_items set color='969696' where color='Gray';
+update graphs_items set color='FFFFFF' where color='White';
+alter table graphs_items change color color varchar(6) DEFAULT '009600' NOT NULL;
+CREATE INDEX graphs_items_1 on graphs_items (itemid);
+CREATE INDEX graphs_items_2 on graphs_items (graphid);
