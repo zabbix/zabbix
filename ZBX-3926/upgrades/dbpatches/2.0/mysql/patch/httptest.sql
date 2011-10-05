1ce003ad7fbcc1fc3becfe1e37e30c12992f1007
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-10-05 12:50:29 +0000

    - [ZBX-3926] branch removed
    git-svn-id: svn://svn.zabbix.com/branches/dev@22160 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3926/upgrades/dbpatches/2.0/mysql/patch/httptest.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE httptest MODIFY httptestid bigint unsigned NOT NULL,
-		     MODIFY applicationid bigint unsigned NOT NULL;
-DELETE FROM httptest WHERE NOT applicationid IN (SELECT applicationid FROM applications);
-ALTER TABLE httptest ADD CONSTRAINT c_httptest_1 FOREIGN KEY (applicationid) REFERENCES applications (applicationid) ON DELETE CASCADE;
