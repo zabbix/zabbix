a3e87576f73ea976c21d51eb7b5b2a9651a541ba
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-10-05 14:03:12 +0000

    - [ZBX-3165] branch removed
    git-svn-id: svn://svn.zabbix.com/branches/dev@22166 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3165/upgrades/dbpatches/2.0/mysql/patch/services.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE services MODIFY serviceid bigint unsigned NOT NULL;
-ALTER TABLE services ADD CONSTRAINT c_services_1 FOREIGN KEY (triggerid) REFERENCES triggers (triggerid) ON DELETE CASCADE;
