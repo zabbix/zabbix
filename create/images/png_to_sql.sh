42336b238c04089a9b1c656987003a1c3ebba03f
Rihards Olups <git-no-reply@zabbix.com>
2011-10-11 17:59:31 +0000

    - [ZBXNEXT-675] use 'decode' for pgsql images

--- create/images/png_to_sql.sh
+++ create/images/png_to_sql.sh
@@ -27,19 +27,19 @@ done
 
 echo "Generating SQL files"
 
-# for pgsql something like '"\\\\" 1/1 "%_c"' could be used
-# but it also precedes printing characters with double backslashes and prints out things like \r instead of their octal values
-
 imagecount=$(ls $outputdir/*.png | wc -l)
 for imagefile in $outputdir/*.png; do
 	((imagesdone++))
 	imagename=$(basename ${imagefile%.png})
 	# ----- MySQL
 	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagecount,1,'$imagename',0x$(hexdump -ve '"" 1/1 "%02X"' "$imagefile"));" >> $imagefile_mysql
+	# ----- PostgreSQL
+	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagecount,1,'$imagename',decode('$(hexdump -ve '"" 1/1 "%02X"' "$imagefile")','hex'));" >> $imagefile_pgsql
 	# ----- SQLite
 	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagecount,1,'$imagename','$(hexdump -ve '"" 1/1 "%02X"' "$imagefile")');" >> $imagefile_sqlite3
 	# ----- DB2
 	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagecount,1,'$imagename',blob(x'$(hexdump -ve '"" 1/1 "%02X"' "$imagefile")'));" >> $imagefile_db2
+
 	echo -n "$[$imagesdone*100/$imagecount]% "
 done
 echo
