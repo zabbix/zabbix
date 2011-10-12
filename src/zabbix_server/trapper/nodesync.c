e6839eb85f0678d7f1061b772997ef0fa61b87d6
Aleksandrs Saveljevs <git-no-reply@zabbix.com>
2011-10-12 12:38:14 +0000

    - [DEV-502] fixed compilation with PostgreSQL

--- src/zabbix_server/trapper/nodesync.c
+++ src/zabbix_server/trapper/nodesync.c
@@ -303,8 +303,11 @@ static void	process_updated_records(int nodeid, char *data, int sender_nodetype)
 						else
 						{
 #if defined(HAVE_POSTGRESQL)
+							size_t	len;
+
 							len = zbx_hex2binary(buf);
 							zbx_pg_escape_bytea((u_char *)buf, len, &tmp, &tmp_alloc);
+
 							if (NODE_CONFIGLOG_OP_UPDATE == op)
 								zbx_snprintf_alloc(&ufld, &ufld_alloc, &ufld_offset,
 										"'%s',", tmp);
