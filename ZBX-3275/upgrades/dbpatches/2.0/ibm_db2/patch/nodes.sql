aa6fa1017eb1556f3c18bab92696bb747bc80319
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-10-12 13:30:20 +0000

    - [ZBX-3275] fixed deleting of master node, it shouldn't delete other nodes
    Also now it is impossible to change node id of already existing node.
    
    
    
    git-svn-id: svn://svn.zabbix.com/branches/dev@22317 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3275/upgrades/dbpatches/2.0/ibm_db2/patch/nodes.sql
+++ ZBX-3275/upgrades/dbpatches/2.0/ibm_db2/patch/nodes.sql
@@ -12,5 +12,5 @@ REORG TABLE nodes
 /
 UPDATE nodes SET masterid=NULL WHERE masterid=0
 /
-ALTER TABLE nodes ADD CONSTRAINT c_nodes_1 FOREIGN KEY (masterid) REFERENCES nodes (nodeid) ON DELETE CASCADE
+ALTER TABLE nodes ADD CONSTRAINT c_nodes_1 FOREIGN KEY (masterid) REFERENCES nodes (nodeid)
 /
