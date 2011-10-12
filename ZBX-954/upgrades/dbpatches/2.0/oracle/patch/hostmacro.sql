2f22671d90ed9af10addb5c8719d3b503ef4ed4d
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-10-12 13:32:00 +0000

    - [ZBX-954] remove merged dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@22319 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-954/upgrades/dbpatches/2.0/oracle/patch/hostmacro.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE hostmacro MODIFY hostmacroid DEFAULT NULL;
-ALTER TABLE hostmacro MODIFY hostid DEFAULT NULL;
-DROP INDEX hostmacro_1;
-DELETE FROM hostmacro WHERE NOT hostid IN (SELECT hostid FROM hosts);
-CREATE UNIQUE INDEX hostmacro_1 ON hostmacro (hostid,macro);
-ALTER TABLE hostmacro ADD CONSTRAINT c_hostmacro_1 FOREIGN KEY (hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
