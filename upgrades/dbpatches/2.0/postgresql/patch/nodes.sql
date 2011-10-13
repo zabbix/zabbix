fb4e4e0805d24239a4d02d4faf1522d724140f50
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-10-13 14:00:48 +0000

    - [ZBX-3275] fixed deleting of master node, it shouldn't delete other nodes
    git-svn-id: svn://svn.zabbix.com/trunk@22376 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/2.0/postgresql/patch/nodes.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/nodes.sql
@@ -2,4 +2,4 @@ ALTER TABLE ONLY nodes ALTER nodeid DROP DEFAULT,
 		       ALTER masterid DROP DEFAULT,
 		       ALTER masterid DROP NOT NULL;
 UPDATE nodes SET masterid=NULL WHERE masterid=0;
-ALTER TABLE ONLY nodes ADD CONSTRAINT c_nodes_1 FOREIGN KEY (masterid) REFERENCES nodes (nodeid) ON DELETE CASCADE;
+ALTER TABLE ONLY nodes ADD CONSTRAINT c_nodes_1 FOREIGN KEY (masterid) REFERENCES nodes (nodeid);
