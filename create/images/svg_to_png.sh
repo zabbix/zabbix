384ce6374538e43268c564a6de87fdaa925503ab
Rihards Olups <git-no-reply@zabbix.com>
2011-10-17 15:29:43 +0000

    - [ZBXNEXT-675] split out conversion in a function as well; initial conversion for rack images (fixed sizes); only report on pngcrush statistics if the stat file is there

--- create/images/svg_to_png.sh
+++ create/images/svg_to_png.sh
@@ -23,6 +23,27 @@ crushpng() {
 			mv "${1%png}2.png" "$1"
 }
 
+svgtopng() {
+	pngoutfile="$outputdir/$(basename ${1%.svg}) ($2).png"
+	[[ "$(stat -c "%Y" "$pngoutfile" 2>/dev/null)" -lt "$svgfilemod" ]] && {
+		# if png file modification time is older than svg file modification time
+		echo -n " to $size..."
+		# we have to query image dimensions first, because export dimensions are used "as-is", resulting in a square rackmountable server, for example
+		# inkscape option --query-all could be used, but it's not fully clear which layer is supposed to be "whole image"
+		# crudely dropping decimal part, bash fails on it
+		[[ "$(inkscape --without-gui --query-width $1 | cut -d. -f1)" -gt "$(inkscape --without-gui --query-height $1 | cut -d. -f1)" ]] && {
+			dimension=width
+		} || {
+			dimension=height
+		}
+		inkscape --without-gui --export-$dimension=$size $1 --export-png="$pngoutfile" >> "$inkscapelog" || exit 1
+		echo -n " compress..."
+		crushpng "$pngoutfile"
+	} || {
+		echo -n " skip $2..."
+	}
+}
+
 mkdir -p "$outputdir"
 
 > "$pngcrushoutput"
@@ -38,28 +59,22 @@ for svgfile in $elementdir/*.svg; do
 	for size in 24 48 64 96 128; do
 		# rackmountable device icons don't make much sense below size 64
 		[[ "$svgfile" =~ Rackmountable_.* || "$svgfile" =~ Zabbix_server_.* ]] && [ "$size" -lt "64" ] && continue
-		pngoutfile="$outputdir/$(basename ${svgfile%.svg}) ($size).png"
-		[[ "$(stat -c "%Y" "$pngoutfile" 2>/dev/null)" -lt "$svgfilemod" ]] && {
-			# if png file modification time is older than svg file modification time
-			echo -n " to $size..."
-			# we have to query image dimensions first, because export dimensions are used "as-is", resulting in a square rackmountable server, for example
-			# inkscape option --query-all could be used, but it's not fully clear which layer is supposed to be "whole image"
-			# crudely dropping decimal part, bash fails on it
-			[[ "$(inkscape --without-gui --query-width $svgfile | cut -d. -f1)" -gt "$(inkscape --without-gui --query-height $svgfile | cut -d. -f1)" ]] && {
-				dimension=width
-			} || {
-				dimension=height
-			}
-			inkscape --without-gui --export-$dimension=$size $svgfile --export-png="$pngoutfile" >> "$inkscapelog" || exit 1
-			echo -n " compress..."
-			crushpng "$pngoutfile"
-		} || {
-			echo -n " skip $size..."
-		}
+		svgtopng "$svgfile" "$size"
 	done
 	echo "[$[$elementfilesdone*100/$svgelementcount]%]"
 done
 
-echo "Biggest gain from pngcrush:"
-sort -n -r -t : -k 2 $pngcrushlog | tail -n 1
-awk 'BEGIN {FS=":"}; {sum+=$2} END { print "Average gain:",sum/NR}' $pngcrushlog
+rackimages=([64]=66 [96]=99 [128]=132)
+
+echo -n "Converting Rack_42.svg"
+for rackimagesize in "${!rackimages[@]}"; do
+	svgtopng "equipment_rack/Rack_42.svg" "${rackimages[$rackimagesize]}"
+done
+
+echo
+
+[[ -s "$pngcrushlog" ]] && {
+	echo "Biggest gain from pngcrush:"
+	sort -n -r -t : -k 2 $pngcrushlog | tail -n 1
+	awk 'BEGIN {FS=":"}; {sum+=$2} END { print "Average gain:",sum/NR}' $pngcrushlog
+}
