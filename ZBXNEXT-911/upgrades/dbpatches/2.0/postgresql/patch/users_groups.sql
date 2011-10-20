6d5d19da7702edc64c06c3cc75a149f37be69355
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-10-20 12:53:04 +0000

    - [ZBXNEXT-911] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@22575 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-911/upgrades/dbpatches/2.0/postgresql/patch/users_groups.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE ONLY users_groups ALTER id DROP DEFAULT,
-			      ALTER usrgrpid DROP DEFAULT,
-			      ALTER userid DROP DEFAULT;
-DROP INDEX users_groups_1;
-DELETE FROM users_groups WHERE NOT EXISTS (SELECT 1 FROM usrgrp WHERE usrgrp.usrgrpid=users_groups.usrgrpid);
-DELETE FROM users_groups WHERE NOT EXISTS (SELECT 1 FROM users WHERE users.userid=users_groups.userid);
-CREATE UNIQUE INDEX users_groups_1 ON users_groups (usrgrpid,userid);
-ALTER TABLE ONLY users_groups ADD CONSTRAINT c_users_groups_1 FOREIGN KEY (usrgrpid) REFERENCES usrgrp (usrgrpid) ON DELETE CASCADE;
-ALTER TABLE ONLY users_groups ADD CONSTRAINT c_users_groups_2 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
