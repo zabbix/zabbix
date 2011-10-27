ecada7d399c6d28fc58ff15e12d4bd796a61a161
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-10-27 08:16:17 +0000

    - [ZBX-1020] Remove re-created development branch.
    git-svn-id: svn://svn.zabbix.com/branches/dev@22725 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-1020/upgrades/dbpatches/2.0/mysql/patch/items_applications.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE items_applications MODIFY itemappid bigint unsigned NOT NULL,
-			       MODIFY applicationid bigint unsigned NOT NULL,
-			       MODIFY itemid bigint unsigned NOT NULL;
-DROP INDEX items_applications_1 ON items_applications;
-DELETE FROM items_applications WHERE applicationid NOT IN (SELECT applicationid FROM applications);
-DELETE FROM items_applications WHERE itemid NOT IN (SELECT itemid FROM items);
-CREATE UNIQUE INDEX items_applications_1 ON items_applications (applicationid,itemid);
-ALTER TABLE items_applications ADD CONSTRAINT c_items_applications_1 FOREIGN KEY (applicationid) REFERENCES applications (applicationid) ON DELETE CASCADE;
-ALTER TABLE items_applications ADD CONSTRAINT c_items_applications_2 FOREIGN KEY (itemid) REFERENCES items (itemid) ON DELETE CASCADE;
