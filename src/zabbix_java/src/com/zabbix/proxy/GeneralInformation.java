970b7a6e518d77f6db9148538a02255b0572405d
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-10-27 16:54:33 +0000

    - [ZBXNEXT-826] bump version to 1.9.8
    git-svn-id: svn://svn.zabbix.com/trunk@22764 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_java/src/com/zabbix/proxy/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/proxy/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	public static final String APPLICATION_NAME = "Zabbix Java Proxy";
 	public static final String REVISION_DATE = "27 October 2011";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "1.9.7";
+	public static final String VERSION = "1.9.8";
 
 	public static void printVersion()
 	{
