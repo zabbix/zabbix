4efa06473a2bf5d70f696650f153afc78cb62f23
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-11-02 07:42:45 +0000

    - [ZBX-2919] fixed custom multiplier becoming 0 when creating an item
    git-svn-id: svn://svn.zabbix.com/branches/1.8@22875 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/forms.inc.php
+++ frontends/php/include/forms.inc.php
@@ -1952,7 +1952,7 @@ ITEM_TYPE_CALCULATED $key = ''; $params = '';
 			$mltpbox[] = $ctb;
 		}
 		else {
-			$mltpbox[] = new CCheckBox('multiplier',$multiplier == 1 ? 'yes':'no', 'var editbx = document.getElementById(\'formula\'); if(editbx) editbx.disabled = !this.checked;', 1);
+			$mltpbox[] = new CCheckBox('multiplier',$multiplier == 1 ? 'yes':'no', 'var editbx = document.getElementById(\'formula\'); if(editbx) editbx.readOnly = !this.checked;', 1);
 			$mltpbox[] = SPACE;
 			$ctb = new CTextBox('formula', $formula, 10);
 			$ctb->setAttribute('style', 'text-align: right;');
