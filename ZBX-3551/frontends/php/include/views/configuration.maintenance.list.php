e4f3400ba663e4da164ede52b0f285004c9848d7
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-11-03 12:16:18 +0000

    - [ZBX-3551] dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@22935 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-3551/frontends/php/include/views/configuration.maintenance.list.php
@@ -0,0 +1,92 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2011 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+?>
+<?php
+zbx_add_post_js('chkbxRange.pageGoName = "maintenanceids";');
+$maintenanceWidget = new CWidget();
+
+// create new maintenance button
+$createForm = new CForm('get');
+$createForm->cleanItems();
+$createForm->addItem(new CSubmit('form', _('Create maintenance period')));
+$maintenanceWidget->addPageHeader(_('CONFIGURATION OF MAINTENANCE PERIODS'), $createForm);
+
+// header
+$filterForm = new CForm('get');
+$filterForm->addItem(array(_('Group').SPACE, $this->data['pageFilter']->getGroupsCB()));
+$numRows = new CDiv();
+$numRows->setAttribute('name', 'numrows');
+$maintenanceWidget->addHeader(_('Maintenance periods'), $filterForm);
+$maintenanceWidget->addHeader($numRows);
+
+// create form
+$maintenanceForm = new CForm();
+$maintenanceForm->setName('maintenanceForm');
+
+// create table
+$maintenanceTable = new CTableInfo(_('No maintenance defined'));
+$maintenanceTable->setHeader(array(
+	new CCheckBox('all_maintenances', null, "checkAll('".$maintenanceForm->getName()."', 'all_maintenances', 'maintenanceids');"),
+	make_sorting_header(_('Name'), 'name'),
+	make_sorting_header(_('Type'), 'maintenance_type'),
+	make_sorting_header(_('State'), 'status'),
+	_('Description')
+));
+
+foreach ($this->data['maintenances'] as $maintenance) {
+	$maintenanceid = $maintenance['maintenanceid'];
+
+	switch ($maintenance['status']) {
+		case MAINTENANCE_STATUS_EXPIRED:
+			$maintenanceStatus = new CSpan(_('Expired'), 'red');
+			break;
+		case MAINTENANCE_STATUS_APPROACH:
+			$maintenanceStatus = new CSpan(_('Approaching'), 'blue');
+			break;
+		case MAINTENANCE_STATUS_ACTIVE:
+			$maintenanceStatus = new CSpan(_('Active'), 'green');
+			break;
+	}
+
+	$maintenanceTable->addRow(array(
+		new CCheckBox('maintenanceids['.$maintenanceid.']', null, null, $maintenanceid),
+		new CLink($maintenance['name'], 'maintenance.php?form=update&maintenanceid='.$maintenanceid.'#form'),
+		$maintenance['maintenance_type'] ? _('No data collection') : _('With data collection'),
+		$maintenanceStatus,
+		$maintenance['description']
+	));
+}
+
+// create go button
+$goComboBox = new CComboBox('go');
+$goOption = new CComboItem('delete', _('Delete selected'));
+$goOption->setAttribute('confirm', _('Delete selected maintenance periods?'));
+$goComboBox->addItem($goOption);
+$goButton = new CSubmit('goButton', _('Go').' (0)');
+$goButton->setAttribute('id', 'goButton');
+
+// append table to form
+$maintenanceForm->addItem(array($this->data['paging'], $maintenanceTable, $this->data['paging'], get_table_header(array($goComboBox, $goButton))));
+
+// append form to widget
+$maintenanceWidget->addItem($maintenanceForm);
+
+return $maintenanceWidget;
+?>
