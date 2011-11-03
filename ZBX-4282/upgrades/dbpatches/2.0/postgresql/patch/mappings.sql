6db9fa2636c8c69c4224122b2f4a6a05d2f66509
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-11-03 10:25:18 +0000

    - [ZBX-4282] branch removed
    git-svn-id: svn://svn.zabbix.com/branches/dev@22925 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4282/upgrades/dbpatches/2.0/postgresql/patch/mappings.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ONLY mappings ALTER mappingid DROP DEFAULT,
-			  ALTER valuemapid DROP DEFAULT;
-DELETE FROM mappings WHERE NOT EXISTS (SELECT 1 FROM valuemaps WHERE valuemaps.valuemapid=mappings.valuemapid);
-ALTER TABLE ONLY mappings ADD CONSTRAINT c_mappings_1 FOREIGN KEY (valuemapid) REFERENCES valuemaps (valuemapid) ON DELETE CASCADE;
