d6e33ec8ac1bbbce2ca565d07ba07161ef31f02b
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-11-03 09:17:06 +0000

    - [ZBX-3498] removed items from unmonitored hosts from the 'Add graph' and 'Add simple graph' pop-ups in the 'Favourite graphs' widget  - [ZBX-3498] removed the 'Status' column from the 'Add simple graph' pop-up
    git-svn-id: svn://svn.zabbix.com/branches/1.8@22918 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/blocks.inc.php
+++ frontends/php/include/blocks.inc.php
@@ -1175,7 +1175,7 @@ function make_graph_menu(&$menu,&$submenu){
 				"PopUp('popup.php?srctbl=graphs".
 					'&srcfld1=graphid'.
 					'&reference=graphid'.
-					'&real_hosts=1'.
+					'&monitored_hosts=1'.
 					"&multiselect=1',800,450);".
 				"void(0);",
 				null,
@@ -1186,8 +1186,8 @@ function make_graph_menu(&$menu,&$submenu){
 				'javascript: '.
 				"PopUp('popup.php?srctbl=simple_graph".
 					'&srcfld1=itemid'.
-					'&real_hosts=1'.
 					'&reference=itemid'.
+					'&monitored_hosts=1'.
 					"&multiselect=1',800,450);".
 				"void(0);",
 				null,
