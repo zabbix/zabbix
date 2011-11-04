970a18b87a78744dfb1fa8c158f69577d81b0ba3
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-11-04 12:06:55 +0000

    - [ZBX-3468] formatting; coding style
    git-svn-id: svn://svn.zabbix.com/branches/dev@22976 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3468/src/libs/zbxdb/db.c
+++ ZBX-3468/src/libs/zbxdb/db.c
@@ -22,6 +22,7 @@
 #include "db.h"
 #include "zbxdb.h"
 #include "log.h"
+#include "mutexs.h"
 
 static int	txn_level = 0;	/* transaction level, nested transactions are not supported */
 static int	txn_init = 0;
@@ -39,7 +40,7 @@ static int			ZBX_PG_BYTEAOID = 0;
 int				ZBX_PG_SVERSION = 0;
 #elif defined(HAVE_SQLITE3)
 static sqlite3			*conn = NULL;
-PHP_MUTEX			sqlite_access;
+static PHP_MUTEX		sqlite_access;
 #endif
 
 #if defined(HAVE_ORACLE)
@@ -369,6 +370,11 @@ void	zbx_create_sqlite3_mutex(const char *dbname)
 		zbx_error("cannot create mutex for SQLite3");
 		exit(FAIL);
 	}
+}
+
+void	zbx_remove_sqlite3_mutex()
+{
+	php_sem_remove(&sqlite_access);
 }
 #endif	/* HAVE_SQLITE3 */
 
