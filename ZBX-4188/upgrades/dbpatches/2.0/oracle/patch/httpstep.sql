0082cd2b341c2020ffdc50b1b89e4e4416087cca
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-11-10 13:11:27 +0000

    - [ZBX-4188] removed t_blob type
    git-svn-id: svn://svn.zabbix.com/branches/dev@23101 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4188/upgrades/dbpatches/2.0/oracle/patch/httpstep.sql
+++ ZBX-4188/upgrades/dbpatches/2.0/oracle/patch/httpstep.sql
@@ -1,4 +1,5 @@
 ALTER TABLE httpstep MODIFY httpstepid DEFAULT NULL;
 ALTER TABLE httpstep MODIFY httptestid DEFAULT NULL;
+ALTER TABLE httpstep MODIFY posts nvarchar2(2000) DEFAULT '';
 DELETE FROM httpstep WHERE NOT httptestid IN (SELECT httptestid FROM httptest);
 ALTER TABLE httpstep ADD CONSTRAINT c_httpstep_1 FOREIGN KEY (httptestid) REFERENCES httptest (httptestid) ON DELETE CASCADE;
