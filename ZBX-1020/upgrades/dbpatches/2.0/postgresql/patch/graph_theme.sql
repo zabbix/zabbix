58e3a535dcf01cac71af0c289ed1c4f2ae476bcf
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-11-17 15:07:18 +0000

    - [ZBX-1020] Remove development branch.
    git-svn-id: svn://svn.zabbix.com/branches/dev@23320 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-1020/upgrades/dbpatches/2.0/postgresql/patch/graph_theme.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE ONLY graph_theme ALTER graphthemeid DROP DEFAULT,
-			     ALTER noneworktimecolor SET DEFAULT 'CCCCCC';
-ALTER TABLE ONLY graph_theme RENAME noneworktimecolor TO nonworktimecolor;
