c94e0dde0d8d8e506b99cbb3b460055ffa0abc1e
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-11-22 15:13:56 +0000

    - [ZBX-2806] provide db patch for 2.0
     Change current statuses in DB in the next way:
       recovery -> sleep
       superseded_active -> active
       superseded_recovery -> active
    
    
    git-svn-id: svn://svn.zabbix.com/branches/dev@23433 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-2806-trunk/upgrades/dbpatches/2.0/mysql/patch/escalations.sql
+++ ZBX-2806-trunk/upgrades/dbpatches/2.0/mysql/patch/escalations.sql
@@ -5,3 +5,10 @@ ALTER TABLE escalations MODIFY escalationid bigint unsigned NOT NULL,
 			MODIFY r_eventid bigint unsigned NULL;
 UPDATE escalations SET triggerid=NULL WHERE triggerid=0;
 UPDATE escalations SET r_eventid=NULL WHERE r_eventid=0;
+-- 0: ESCALATION_STATUS_ACTIVE
+-- 1: ESCALATION_STATUS_RECOVERY
+-- 2: ESCALATION_STATUS_SLEEP
+-- 4: ESCALATION_STATUS_SUPERSEDED_ACTIVE
+-- 5: ESCALATION_STATUS_SUPERSEDED_RECOVERY
+UPDATE escalations SET status=2 WHERE status=1;
+UPDATE escalations SET status=0 WHERE status in (4,5);
--- ZBX-2806-trunk/upgrades/dbpatches/2.0/postgresql/patch/escalations.sql
+++ ZBX-2806-trunk/upgrades/dbpatches/2.0/postgresql/patch/escalations.sql
@@ -7,3 +7,10 @@ ALTER TABLE ONLY escalations ALTER escalationid DROP DEFAULT,
 			     ALTER r_eventid DROP NOT NULL;
 UPDATE escalations SET triggerid=NULL WHERE triggerid=0;
 UPDATE escalations SET r_eventid=NULL WHERE r_eventid=0;
+-- 0: ESCALATION_STATUS_ACTIVE
+-- 1: ESCALATION_STATUS_RECOVERY
+-- 2: ESCALATION_STATUS_SLEEP
+-- 4: ESCALATION_STATUS_SUPERSEDED_ACTIVE
+-- 5: ESCALATION_STATUS_SUPERSEDED_RECOVERY
+UPDATE escalations SET status=2 WHERE status=1;
+UPDATE escalations SET status=0 WHERE status in (4,5);
