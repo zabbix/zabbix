80060b8bb153dc3a95fd407bff9893c36c4293b1
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-11-22 20:25:42 +0000

    - [NTP-321] fixed displaying of "History of events" screen element
    git-svn-id: svn://svn.zabbix.com/branches/1.8.ns@23442 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- 1.8.6.patched/frontends/php/include/events.inc.php
+++ 1.8.6.patched/frontends/php/include/events.inc.php
@@ -494,7 +494,6 @@ function getLastEvents($options){
 		'skipDependent'	=> 1,
 		'select_hosts' => array('hostid', 'host'),
 		'output' => API_OUTPUT_EXTEND,
-//		'expandDescription' => 0,
 		'sortfield' => 'lastchange',
 		'sortorder' => ZBX_SORT_DOWN,
 		'limit' => $options['limit']
@@ -543,7 +542,7 @@ function getLastEvents($options){
 
 		//expanding description for the state where event was
 		$merged_event = array_merge($event, $triggers[$event['objectid']]);
-		$events[$enum]['trigger']['description'] = expandTriggerDescription($merged_event, ZBX_FLAG_EVENT);
+		$events[$enum]['trigger']['description'] = expand_trigger_description_by_data($merged_event, ZBX_FLAG_EVENT);
 	}
 
 	array_multisort($sortClock, SORT_DESC, $sortEvent, SORT_DESC, $events);
