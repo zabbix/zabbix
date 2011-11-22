910c5b54dfa5c0dc1f6e6608c80c8fbe1ae2f361
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-11-22 09:34:46 +0000

    - [DEV-484] branch removed
    git-svn-id: svn://svn.zabbix.com/branches/dev@23385 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- DEV-484/upgrades/dbpatches/1.4/postgresql/patch/history_sync.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE history_sync (
-	id		serial			,
-	nodeid		bigint DEFAULT '0'	NOT NULL,
-	itemid		bigint DEFAULT '0'	NOT NULL,
-	clock		integer		DEFAULT '0'	NOT NULL,
-	value		numeric(16,4)		DEFAULT '0.0000'	NOT NULL,
-	PRIMARY KEY (id)
-) with OIDS;
-CREATE INDEX history_sync_1 on history_sync (nodeid,id);
--- DEV-484/upgrades/dbpatches/1.6/oracle/patch/sysmaps_elements.sql
+++
@@ -1,1 +0,0 @@
-alter table sysmaps_elements add iconid_disabled number(20) DEFAULT '0' NOT NULL;
