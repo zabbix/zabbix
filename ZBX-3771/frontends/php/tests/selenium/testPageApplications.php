eb6a5c0ef33200aab92a2af79d2793777dd509f1
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-11-23 11:47:37 +0000

    - [ZBX-3771] updated to the latest trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@23472 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3771/frontends/php/tests/selenium/testPageApplications.php
+++ ZBX-3771/frontends/php/tests/selenium/testPageApplications.php
@@ -1,7 +1,7 @@
 <?php
 /*
 ** Zabbix
-** Copyright (C) 2000-2011 Zabbix SIA
+** Copyright (C) 2001-2011 Zabbix SIA
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
 **
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
-** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 ?>
 <?php
