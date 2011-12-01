e2bec8483164f10dad0a2d59901e40293f29073b
pavels <pavels@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2011-12-01 10:13:17 +0000

    A.F....... [ZBX-3104] API refactoring
    git-svn-id: svn://svn.zabbix.com/trunk@23677 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/api/classes/class.czbxapi.php
+++ frontends/php/api/classes/class.czbxapi.php
@@ -22,6 +22,144 @@
 class CZBXAPI {
 	public static $userData;
 
+	/**
+	 * The name of the table.
+	 *
+	 * @var string
+	 */
+	protected $tableName;
+
+	/**
+	 * The alias of the table.
+	 *
+	 * @var string
+	 */
+	protected $tableAlias;
+
+
+	/**
+	 * The name of the field used as a private key. If the PK consists of multiple
+	 * fields, the names will be stored as an array.
+	 *
+	 * @var mixed
+	 */
+	protected $pk;
+
+
+	public function __construct() {
+		$schema = $this->getTableSchema();
+
+		// set the PK of the table
+		$pk = explode(',', $schema['key']);
+		$this->pk = (count($pk) > 1) ? $pk : $pk[0];
+	}
+
+
+	/**
+	 * Returns the name of the database table that contains the objects.
+	 *
+	 * @return string
+	 */
+	protected function tableName() {
+		return $this->tableName;
+	}
+
+
+	/**
+	 * Returns the alias of the database table that contains the objects.
+	 *
+	 * @return string
+	 */
+	protected function tableAlias() {
+		return $this->tableAlias;
+	}
+
+
+	/**
+	 * Returns the table name with the table alias.
+	 *
+	 * @return string
+	 */
+	protected function tableId() {
+		return $this->tableName().' '.$this->tableAlias();
+	}
+
+
+	/**
+	 * Prepends the table alias to the given field name.
+	 *
+	 * @return string
+	 */
+	protected function fieldId($fieldName) {
+		return $this->tableAlias().'.'.$fieldName;
+	}
+
+
+	/**
+	 * Returns the columns that contain the private key.
+	 *
+	 * @see CZBXAPI::$pk
+	 *
+	 * @return mixed
+	 */
+	protected function pk() {
+		return $this->pk;
+	}
+
+
+	/**
+	 * Returns an array that described the schema of the database table.
+	 *
+	 * @return array
+	 */
+	protected function getTableSchema() {
+		return DB::getSchema($this->tableName());
+	}
+
+
+	/**
+	 * Returns true if the table has the given field.
+	 *
+	 * @param string $fieldName
+	 *
+	 * @return boolean
+	 */
+	protected function hasField($fieldName) {
+		$schema = $this->getTableSchema();
+
+		return isset($schema['fields'][$fieldName]);
+	}
+
+
+	/**
+	 * Unsets the fields that haven't been explicitly asked for by the user, but
+	 * have been included in the resulting object for whatever reasons.
+	 *
+	 * @param array $object    The object from the database
+	 * @param array $sqlWhere
+	 * @param array $options
+	 *
+	 * @return array           The resulting object
+	 */
+	protected function unsetExtraFields(array $object, array $options, array $sqlParts) {
+
+		// unset the pk forced by the 'preservedkeys' option
+		if ($options['preservekeys'] !== null && in_array($this->fieldId($this->pk()), $sqlParts['select'])
+			&& is_array($options['output']) && !in_array($this->pk(), $options['output'])) {
+
+			unset($object[$this->pk()]);
+		}
+
+		return $object;
+	}
+
+
+	/**
+	 * Throws an API exception.
+	 *
+	 * @param type $code
+	 * @param type $error
+	 */
 	protected static function exception($code = ZBX_API_ERROR_INTERNAL, $error = '') {
 		throw new APIException($code, $error);
 	}
