f5673eacae00df4e0f382755cfa566797f202a04
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-12-02 08:08:28 +0000

    A......... [ZBX-3366] add patches to delete sort settings from profile
    git-svn-id: svn://svn.zabbix.com/branches/dev@23713 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3366/upgrades/dbpatches/2.0/ibm_db2/patch/profiles.sql
+++ ZBX-3366/upgrades/dbpatches/2.0/ibm_db2/patch/profiles.sql
@@ -10,3 +10,5 @@ DELETE FROM profiles WHERE NOT userid IN (SELECT userid FROM users)
 /
 ALTER TABLE profiles ADD CONSTRAINT c_profiles_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE
 /
+DELETE FROM profiles WHERE idx LIKE '%.sort%'
+/
--- ZBX-3366/upgrades/dbpatches/2.0/oracle/patch/profiles.sql
+++ ZBX-3366/upgrades/dbpatches/2.0/oracle/patch/profiles.sql
@@ -2,3 +2,4 @@ ALTER TABLE profiles MODIFY profileid DEFAULT NULL;
 ALTER TABLE profiles MODIFY userid DEFAULT NULL;
 DELETE FROM profiles WHERE NOT userid IN (SELECT userid FROM users);
 ALTER TABLE profiles ADD CONSTRAINT c_profiles_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
+DELETE FROM profiles WHERE idx LIKE '%.sort%';
