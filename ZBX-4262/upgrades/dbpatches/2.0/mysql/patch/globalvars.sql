e2e38d26cbbeed864140710307c66b14608c225b
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-12-05 17:09:35 +0000

    .......... [ZBX-4262] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@23781 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4262/upgrades/dbpatches/2.0/mysql/patch/globalvars.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE globalvars (
-	globalvarid              bigint unsigned                           NOT NULL,
-	snmp_lastsize            integer         DEFAULT '0'               NOT NULL,
-	PRIMARY KEY (globalvarid)
-) ENGINE=InnoDB;
