f38341ada74971846db317b1eb93eaf95249ef55
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-12-05 17:08:56 +0000

    A.F.....S. [ZBX-4262] added support of item prototypes for graph y axis min/max
    git-svn-id: svn://svn.zabbix.com/trunk@23780 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/forms.inc.php
+++ frontends/php/include/forms.inc.php
@@ -2819,7 +2819,7 @@
 					$ymin_name = $min_host['host'].':'.itemName($min_item);
 				}
 
-				if(count($items)){
+				if (count($items)) {
 					$yaxis_min[] = new CTextBox("ymin_name",$ymin_name,80,'yes');
 					$yaxis_min[] = new CButton('yaxis_min',S_SELECT,'javascript: '.
 						"return PopUp('popup.php?dstfrm=".$frmGraph->getName().
@@ -2830,8 +2830,20 @@
 							"&srctbl=items".
 							"&srcfld1=itemid".
 							"&srcfld2=name',0,0,'zbx_popup_item');");
+
+					// select prototype button
+					if ($parent_discoveryid) {
+						$yaxis_min[] = new CButton('yaxis_min', _('Select prototype'),'javascript: '.
+							"return PopUp('popup.php?dstfrm=".$frmGraph->getName().
+								"&parent_discoveryid=".$parent_discoveryid.
+								"&dstfld1=ymin_itemid".
+								"&dstfld2=ymin_name".
+								"&srctbl=prototypes".
+								"&srcfld1=itemid".
+								"&srcfld2=name',0,0,'zbx_popup_item');");
+					}
 				}
-				else{
+				else {
 					$yaxis_min[] = S_ADD_GRAPH_ITEMS;
 				}
 			}
@@ -2863,7 +2875,7 @@
 					$ymax_name = $max_host['host'].':'.itemName($max_item);
 				}
 
-				if(count($items)){
+				if (count($items)) {
 					$yaxis_max[] = new CTextBox("ymax_name",$ymax_name,80,'yes');
 					$yaxis_max[] = new CButton('yaxis_max',S_SELECT,'javascript: '.
 							"return PopUp('popup.php?dstfrm=".$frmGraph->getName().
@@ -2875,8 +2887,20 @@
 							"&srcfld1=itemid".
 							"&srcfld2=name',0,0,'zbx_popup_item');"
 					);
+
+					// select prototype button
+					if ($parent_discoveryid) {
+						$yaxis_max[] = new CButton('yaxis_min', _('Select prototype'),'javascript: '.
+							"return PopUp('popup.php?dstfrm=".$frmGraph->getName().
+								"&parent_discoveryid=".$parent_discoveryid.
+								"&dstfld1=ymax_itemid".
+								"&dstfld2=ymax_name".
+								"&srctbl=prototypes".
+								"&srcfld1=itemid".
+								"&srcfld2=name',0,0,'zbx_popup_item');");
+					}
 				}
-				else{
+				else {
 					$yaxis_max[] = S_ADD_GRAPH_ITEMS;
 				}
 			}
