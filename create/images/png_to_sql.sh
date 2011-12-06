aea0e49e5cef191c3a3e7c053cb4cf2f26a67879
Rihards Olups <git-no-reply@zabbix.com>
2011-12-06 22:46:27 +0000

    ....I..... [ZBXNEXT-675] by default, write to the new db file location; deal with directory names with spaces a bit better; fix indenting; improve variable naming; support executing png_to_sql.sh from any directory

--- create/images/png_to_sql.sh
+++ create/images/png_to_sql.sh
@@ -3,16 +3,19 @@
 # A script to generate SQL from PNG images
 # depends on hexdump
 
-outputdir=${1:-png_modern}
+scriptdir="$(basename $0)"
+pngdir="${1:-png_modern}"
+sqlbasedir="$scriptdir/../../database"
+imagefile="images.sql"
 
-imagefile_mysql=images_mysql.sql
-imagefile_pgsql=images_postgresql.sql
-imagefile_sqlite3=images_sqlite3.sql
-imagefile_oracle=images_oracle.sql
-imagefile_db2=images_ibm_db2.sql
+imagefile_mysql="$sqlbasedir/mysql/$imagefile"
+imagefile_pgsql="$sqlbasedir/postgresql/$imagefile"
+imagefile_sqlite3="$sqlbasedir/sqlite3/$imagefile"
+imagefile_oracle="$sqlbasedir/oracle/$imagefile"
+imagefile_ibm_db2="$sqlbasedir/ibm_db2/$imagefile"
 
-for imagefile in $imagefile_mysql $imagefile_pgsql $imagefile_sqlite3 $imagefile_oracle $imagefile_db2; do
-	[[ -s $imagefile ]] && {
+for imagefile in "$imagefile_mysql" "$imagefile_pgsql" "$imagefile_sqlite3" "$imagefile_oracle" "$imagefile_ibm_db2"; do
+	[[ -s "$imagefile" ]] && {
 		echo "Non-empty $imagefile already exists, stopping"
 		exit 1
 	}
@@ -20,25 +23,27 @@ done
 
 echo "Generating SQL files"
 
-cat images_oracle_start.txt > $imagefile_oracle
+cat images_oracle_start.txt > "$imagefile_oracle"
 
-imagecount=$(ls $outputdir/*.png | wc -l)
+imagecount=$(ls $pngdir/*.png | wc -l)
 
-for imagefile in $outputdir/*.png; do
+# TODO: this loop won't work with directory names, containing spaces
+# using 'find' here seems to be a bit excessive for now
+for imagefile in $pngdir/*.png; do
 	((imagesdone++))
-	imagename=$(basename ${imagefile%.png})
+	imagename="$(basename "${imagefile%.png}")"
 	# ----- MySQL
-	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagesdone,1,'$imagename',0x$(hexdump -ve '"" 1/1 "%02X"' "$imagefile"));" >> $imagefile_mysql
+	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagesdone,1,'$imagename',0x$(hexdump -ve '"" 1/1 "%02X"' "$imagefile"));" >> "$imagefile_mysql"
 	# ----- PostgreSQL
-	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagesdone,1,'$imagename',decode('$(hexdump -ve '"" 1/1 "%02X"' "$imagefile")','hex'));" >> $imagefile_pgsql
+	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagesdone,1,'$imagename',decode('$(hexdump -ve '"" 1/1 "%02X"' "$imagefile")','hex'));" >> "$imagefile_pgsql"
 	# ----- Oracle
-	echo -e "\tLOAD_IMAGE($imagesdone,1,'$imagename','$imagefile');" >> $imagefile_oracle
+	echo -e "\tLOAD_IMAGE($imagesdone,1,'$imagename','$imagefile');" >> "$imagefile_oracle"
 	# ----- SQLite
-	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagesdone,1,'$imagename','$(hexdump -ve '"" 1/1 "%02X"' "$imagefile")');" >> $imagefile_sqlite3
+	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagesdone,1,'$imagename','$(hexdump -ve '"" 1/1 "%02X"' "$imagefile")');" >> "$imagefile_sqlite3"
 	# ----- DB2
-	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagesdone,1,'$imagename',blob(x'$(hexdump -ve '"" 1/1 "%02X"' "$imagefile")'));" >> $imagefile_db2
+	echo "INSERT INTO images (imageid,imagetype,name,image) VALUES ($imagesdone,1,'$imagename',blob(x'$(hexdump -ve '"" 1/1 "%02X"' "$imagefile")'));" >> "$imagefile_db2"
 
 	echo -n "$[$imagesdone*100/$imagecount]% "
 done
-cat images_oracle_end.txt >> $imagefile_oracle
+cat images_oracle_end.txt >> "$imagefile_oracle"
 echo
