55ebb1b545c344dc38ffd05a07888893d95f6eea
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-12-07 10:38:00 +0000

    A.F....... [ZBX-4260] replaced event sorting by 'clock' with 'eventid'
    git-svn-id: svn://svn.zabbix.com/branches/dev@23838 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4260/frontends/php/events.php
+++ ZBX-4260/frontends/php/events.php
@@ -540,7 +540,7 @@ require_once('include/page_header.php');
 					'time_from' => $from,
 					'time_till' => $till,
 					'output' => API_OUTPUT_SHORTEN,
-					'sortfield' => 'clock',
+					'sortfield' => 'eventid',
 					'sortorder' => ZBX_SORT_DOWN,
 					'limit' => ($config['search_limit']+1)
 				);
@@ -580,11 +580,6 @@ require_once('include/page_header.php');
 					'nopermissions' => 1
 				);
 				$events = API::Event()->get($options);
-				$sortFields = array(
-					array('field' => 'clock', 'order' => ZBX_SORT_DOWN),
-					array('field' => 'ns', 'order' => ZBX_SORT_DOWN)
-				);
-				ArraySorter::sort($events, $sortFields);
 
 				$csv_disabled = zbx_empty($events);
 
