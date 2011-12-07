a5aa4a84fc6abe9e56ff3ae37f69f57f7e3dd7f7
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-12-07 14:05:08 +0000

    A.F....... [ZBX-3219] minor code refactoring
    git-svn-id: svn://svn.zabbix.com/branches/dev@23848 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3219/frontends/php/include/locales/en_gb.inc.php
+++ ZBX-3219/frontends/php/include/locales/en_gb.inc.php
@@ -1910,15 +1910,12 @@
 	'S_TOOLS'=>				'Tools',
 	'S_RESULT'=>				'Result',
 	'S_CLOSE'=>				'Close',
-		'S_SCRIPT_ERROR_BIG'=>			' Possible reasons:
-		1. Incorrect server IP/DNS in the "zabbix.conf.php".
-		2. Security environment like SELinux is blocking connection.
-		3. Zabbix server daemon not running. ',
+	'S_SCRIPT_ERROR_BIG'=>			'Possible reasons: 1. incorrect server IP/DNS in the "zabbix.conf.php"; 2. security environment like SELinux is blocking connection; 3. Zabbix server daemon not running.',
 	'S_DELETE_SELECTED_SCRIPTS_Q'=>		'Delete selected scripts?',
-	'S_NOT_RUN'=>'Server is "%s" and daemon not running',
-	'S_IP_NOT_AVAILABLE'=>'IP address in the same LAN and not available',
-	'S_TIME_OUT'=>'Correct DNS name or IP "%s" but unavailable outside the LAN',
-	'S_WRONG_DNS'=> 'Wrong DNS name',
+	'S_NOT_RUN'=>'Server is "%s" and daemon not running.',
+	'S_IP_NOT_AVAILABLE'=>'IP address in the same LAN and not available.',
+	'S_TIME_OUT'=>'Correct DNS name or IP "%s" but unavailable outside the LAN.',
+	'S_WRONG_DNS'=> 'Wrong DNS name.',
 	'S_SCRIPT_ERROR'=>			'Cannot connect to the trapper port of zabbix server daemon, but it should be available to run the script.',
 	'S_SCRIPT_ERROR_DESCRIPTION'=>		'Error Description',
 	'S_SCRIPT_SEND_ERROR'=>			'Can\'t send command, check connection',
