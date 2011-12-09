97f25e92ec8990c20a62edf697d660cd6c37921e
Vladimir Levijev <vladimir.levijev@zabbix.com>
2011-12-09 17:05:14 +0000

    ........S. [ZBX-2806] reduce the amount of work on dbsyncer and fix superseded escalations removal
    dbsyncer process only adds 1 record to escalations table on every
    event:
    
    - escalation with eventid (start escalation)
    - escalation with r_eventid (stop escalation, recovery)
    
    escalator processes all escalations in every iteration, even the ones
    with nextcheck in the future. This improves deletion of superseded
    escalations and also allows to get last eventid when processing
    recovery.
    
    This commit also fixes db patches for escalations, during upgrade
    escalations table is emptied.

--- upgrades/dbpatches/2.0/postgresql/patch/escalations.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/escalations.sql
@@ -1,16 +1,9 @@
 ALTER TABLE ONLY escalations ALTER escalationid DROP DEFAULT,
-			     ALTER actionid DROP DEFAULT,
-			     ALTER triggerid DROP DEFAULT,
-			     ALTER triggerid DROP NOT NULL,
-			     ALTER eventid DROP DEFAULT,
-			     ALTER r_eventid DROP DEFAULT,
-			     ALTER r_eventid DROP NOT NULL;
-UPDATE escalations SET triggerid=NULL WHERE triggerid=0;
-UPDATE escalations SET r_eventid=NULL WHERE r_eventid=0;
--- 0: ESCALATION_STATUS_ACTIVE
--- 1: ESCALATION_STATUS_RECOVERY
--- 2: ESCALATION_STATUS_SLEEP
--- 4: ESCALATION_STATUS_SUPERSEDED_ACTIVE
--- 5: ESCALATION_STATUS_SUPERSEDED_RECOVERY
-UPDATE escalations SET status=2 WHERE status=1;
-UPDATE escalations SET status=0 WHERE status in (4,5);
+		ALTER actionid DROP DEFAULT,
+		ALTER triggerid DROP DEFAULT,
+		ALTER triggerid DROP NOT NULL,
+		ALTER eventid DROP DEFAULT,
+		ALTER eventid DROP NOT NULL,
+		ALTER r_eventid DROP DEFAULT,
+		ALTER r_eventid DROP NOT NULL;
+DELETE FROM escalations;
