278d056607effd215b10285ec17662d51085b2fe
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-12-09 13:58:23 +0000

    A.F....... [ZBX-3366] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@23903 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3366/upgrades/dbpatches/1.6/oracle/patch/opconditions.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE opconditions (
-	opconditionid	number(20)	DEFAULT '0'	NOT NULL,
-	operationid	number(20)	DEFAULT '0'	NOT NULL,
-	conditiontype	number(10)	DEFAULT '0'	NOT NULL,
-	operator	number(10)	DEFAULT '0'	NOT NULL,
-	value		varchar2(255)	DEFAULT '',
-	PRIMARY KEY (opconditionid)
-);
-CREATE INDEX opconditions_1 on opconditions (operationid);
--- ZBX-3366/upgrades/dbpatches/2.0/postgresql/patch/services.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE ONLY services ALTER serviceid DROP DEFAULT;
-ALTER TABLE ONLY services ADD CONSTRAINT c_services_1 FOREIGN KEY (triggerid) REFERENCES triggers (triggerid) ON DELETE CASCADE;
