3150b0aaee91e1812d7f05832ebb0a4b1329bf64
eduards <eduards@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2011-12-09 13:54:08 +0000

    A......... [ZBX-3366] fixed sorting in history
    git-svn-id: svn://svn.zabbix.com/trunk@23902 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- upgrades/dbpatches/2.0/mysql/patch/profiles.sql
+++ upgrades/dbpatches/2.0/mysql/patch/profiles.sql
@@ -1,4 +1,6 @@
-ALTER TABLE profiles MODIFY profileid bigint unsigned NOT NULL,
-		     MODIFY userid bigint unsigned NOT NULL;
+ALTER TABLE profiles
+	MODIFY profileid bigint unsigned NOT NULL,
+	MODIFY userid bigint unsigned NOT NULL;
 DELETE FROM profiles WHERE NOT userid IN (SELECT userid FROM users);
+DELETE FROM profiles WHERE idx LIKE 'web.%.sort' OR idx LIKE 'web.%.sortorder';
 ALTER TABLE profiles ADD CONSTRAINT c_profiles_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
--- upgrades/dbpatches/2.0/oracle/patch/profiles.sql
+++ upgrades/dbpatches/2.0/oracle/patch/profiles.sql
@@ -1,4 +1,5 @@
 ALTER TABLE profiles MODIFY profileid DEFAULT NULL;
 ALTER TABLE profiles MODIFY userid DEFAULT NULL;
 DELETE FROM profiles WHERE NOT userid IN (SELECT userid FROM users);
+DELETE FROM profiles WHERE idx LIKE 'web.%.sort' OR idx LIKE 'web.%.sortorder';
 ALTER TABLE profiles ADD CONSTRAINT c_profiles_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
