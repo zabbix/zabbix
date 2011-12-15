a81be290ed469defc8c161a59a60ec304b5ee90e
slava <slava@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-12-15 08:08:32 +0000

    ..F....... [ZBX-4436] fixed wrong sql query in discovery popup
    git-svn-id: svn://svn.zabbix.com/branches/dev@23993 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4436/frontends/php/js/menu.js
+++ ZBX-4436/frontends/php/js/menu.js
@@ -83,16 +83,34 @@ function get_real_text_width(text, id) {
 	return tmp_len;
 }
 
+function truncateText(text, len) {
+	return (text.length > len) ? text.substring(0, len) + '...' : text;
+}
+
 function show_popup_menu(e, content, width){
 	var cursor = get_cursor_position(e);
 	var tmp_width = 0;
 	var max_width = 0;
+	var menuTextLength = 35;
 
-	for (i = 0; i < content.length; i++) {
+	for (var i = 0; i < content.length; i++) {
+		content[i][0] = truncateText(content[i][0], menuTextLength);
 		tmp_width = get_real_text_width(content[i][0], i);
 
 		if (max_width < tmp_width) {
 			max_width = tmp_width;
+		}
+
+		// truncate sub menu text
+		if (content[i].length > 4) {
+			for (var j = 4; j < content[i].length; j++) {
+				content[i][j][0] = truncateText(content[i][j][0], menuTextLength);
+				tmp_width = get_real_text_width(content[i][j][0], i);
+
+				if (max_width < tmp_width) {
+					max_width = tmp_width;
+				}
+			}
 		}
 	}
 
@@ -104,8 +122,8 @@ function show_popup_menu(e, content, width){
 		width = 220;
 
 	var pos = [
-		{'block_top' : -12, 'block_left' : -5, 'width' : width},
-		{'block_top' : 5, 'block_left' : width - 5, 'width' : width}
+		{'block_top': -12, 'block_left': -5, 'width': width},
+		{'block_top': 5, 'block_left': width-5, 'width': width}
 	];
 
 	new popup_menu (content, pos, cursor.x, cursor.y);
