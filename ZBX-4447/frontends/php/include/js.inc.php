f7de8dbfb47a13358c64b792c6e3cd72a0da407f
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-12-16 14:00:35 +0000

    ..F....... [ZBX-4447] fixed trigger popup in IE6
    git-svn-id: svn://svn.zabbix.com/branches/dev@24036 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4447/frontends/php/include/js.inc.php
+++ ZBX-4447/frontends/php/include/js.inc.php
@@ -529,7 +529,7 @@ function insert_js_function($fnct_name){
 						// in internet explorer prototype filters by form name does not work
 						var pForms = parentDocument.body.getElementsByTagName("form");
 						for (i = 0; i < pForms.length; i++) {
-							if (pForms[i].getAttribute("name") === frame) {
+							if (pForms[i].attributes.name && pForms[i].attributes.name.value === frame) {
 								parentDocumentForms = [pForms[i]];
 								break;
 							}
