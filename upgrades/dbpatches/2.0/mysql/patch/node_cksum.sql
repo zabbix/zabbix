9251ebab354b71f6b65e5882405c27d9ae742794
Vladimir Levijev <vladimir.levijev@zabbix.com>
2011-12-20 15:07:28 +0000

    ....I..... [ZBX-4471] drop unneeded indexes without a "does not exist" 	   warning

--- upgrades/dbpatches/2.0/mysql/patch/node_cksum.sql
+++ upgrades/dbpatches/2.0/mysql/patch/node_cksum.sql
@@ -1,3 +1,19 @@
+delimiter $
+CREATE PROCEDURE zbx_drop_indexes()
+BEGIN
+	DECLARE dummy INT DEFAULT 0;
+	DECLARE CONTINUE HANDLER FOR SQLSTATE '42000' set dummy = 1;
+
+	DROP INDEX node_cksum_233 on node_cksum;
+	DROP INDEX node_cksum_2 on node_cksum;
+	DROP INDEX node_cksum_3 on node_cksum;
+	DROP INDEX node_cksum_344 on node_cksum;
+END$
+delimiter ;
+
+CALL zbx_drop_indexes();
+DROP PROCEDURE zbx_drop_indexes;
+
 ALTER TABLE node_cksum MODIFY nodeid integer NOT NULL,
 		       MODIFY recordid bigint unsigned NOT NULL;
 DELETE FROM node_cksum WHERE NOT nodeid IN (SELECT nodeid FROM nodes);
--- upgrades/dbpatches/2.0/postgresql/patch/node_cksum.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/node_cksum.sql
@@ -1,3 +1,5 @@
+DROP INDEX IF EXISTS node_cksum_1;
+DROP INDEX IF EXISTS node_cksum_cksum_1;
 ALTER TABLE ONLY node_cksum ALTER nodeid DROP DEFAULT,
 			    ALTER recordid DROP DEFAULT;
 DELETE FROM node_cksum WHERE NOT EXISTS (SELECT 1 FROM nodes WHERE nodes.nodeid=node_cksum.nodeid);
