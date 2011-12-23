04cec7ea6641e46b49aa8e011ed1a2fa65785c69
slava <slava@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-12-23 10:32:40 +0000

    ..F....... [ZBX-3909] fixed link to null on menu and non-hiding menu in maps (Slava)
    git-svn-id: svn://svn.zabbix.com/branches/dev@24217 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3909/frontends/php/js/menu.js
+++ ZBX-3909/frontends/php/js/menu.js
@@ -84,6 +84,10 @@ function get_real_text_width(text, id) {
 }
 
 function show_popup_menu(e, content, width){
+
+	if (A_MENUS[0] !== undefined) {
+		A_MENUS[0].collapse();
+	}
 	var cursor = get_cursor_position(e);
 	var tmp_width = 0;
 	var max_width = 0;
@@ -453,10 +457,12 @@ function menu_item (o_parent, n_order) {
 	// generate item's HMTL
 	var el = document.createElement('a');
 	el.setAttribute('id', 'e' + o_root.n_id + '_' + this.n_id + 'o');
-	el.setAttribute('href', this.a_config[1]);
 
-	if(this.a_config[2] && this.a_config[2]['tw'])
-		el.setAttribute('target', this.a_config[2]['tw']);
+	if (this.a_config[1] != null) {
+		el.setAttribute('href', this.a_config[1]);
+		if(this.a_config[2] && this.a_config[2]['tw'])
+			el.setAttribute('target', this.a_config[2]['tw']);
+	}
 
 	el.className = this.getstyle(0, 0);
 	el.style.position = 'absolute';
