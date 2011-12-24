d3634bc3a37a755ed51ae3e3ae3d797b93585d92
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2011-12-24 18:53:56 +0000

    .......... [ZBXNEXT-686] added tests for global macros
    git-svn-id: svn://svn.zabbix.com/trunk@24256 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ frontends/php/tests/selenium/testFormAdministrationGeneralMacro.php
@@ -0,0 +1,191 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2011 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+?>
+<?php
+require_once(dirname(__FILE__).'/../include/class.cwebtest.php');
+
+class testFormAdministrationGeneralMacro extends CWebTest {
+	private $newmacro = '{$NEW_MACRO}';
+	private $newmacrovalue = 'Value of the new macro';
+
+	public function testFormAdministrationGeneralRegexp_CheckLayout() {
+
+		$count = DBcount('select * from globalmacro');
+
+		$this->login('config.php');
+		$this->dropdown_select_wait('configDropDown','Macros');
+		$this->assertTitle('Configuration of Zabbix');
+		$this->ok('CONFIGURATION OF ZABBIX');
+		$this->ok('Macros');
+		$this->ok('Macro');
+		$this->ok('Value');
+		$this->assertElementPresent('macros[0][macro]');
+		$this->assertAttribute("//input[@id='macros_0_macro']/@maxlength", '64');
+		$this->assertAttribute("//input[@id='macros_0_macro']/@size", '30');
+		for ($i=0; $i<$count; $i++) {
+			$this->assertElementPresent("macros[$i][value]");
+		}
+		$this->assertElementPresent('macro_add');		// button "Add"
+		$this->assertElementPresent('macros_del');		// button "Delete selected"
+		$this->assertElementPresent('save');
+	}
+
+	public function testFormAdministrationGeneralMacro_Create() {
+
+		$count = DBcount('select * from globalmacro');
+
+		// create new Global macro
+		$this->login('config.php');
+		$this->dropdown_select_wait('configDropDown','Macros');
+		$this->assertTitle('Configuration of Zabbix');
+		$this->ok('CONFIGURATION OF ZABBIX');
+		$this->ok('Macros');
+		$this->click('id=macro_add');
+		$this->waitForVisible("macros[$count][macro]");
+		$this->input_type("macros[$count][macro]", $this->newmacro);		// id= macros_N_macro
+		$this->input_type("macros[$count][value]", $this->newmacrovalue);	// id= macros_N_macro
+		$this->button_click('save');
+		$this->wait();
+		$this->ok('Macros updated');
+		$this->ok('CONFIGURATION OF ZABBIX');
+		$this->ok('Macros');
+
+		$sql = "SELECT * FROM globalmacro WHERE macro='".$this->newmacro."' and value='".$this->newmacrovalue."'";
+		$this->assertEquals(1, DBcount($sql), 'Chuck Norris: Macro has not been created in the DB.');
+	}
+
+	public function testFormAdministrationGeneralMacro_Update() {
+		$row = DBfetch(DBselect('select * from globalmacro order by macro limit 1'));
+		$macro = '{$UPDATEDMACRO}';
+		$macrovalue = 'Value of the updated macro';
+
+		$this->login('config.php');
+		$this->dropdown_select_wait('configDropDown','Macros');
+		$this->assertTitle('Configuration of Zabbix');
+		$this->ok('CONFIGURATION OF ZABBIX');
+		$this->ok('Macros');
+		$this->ok('Macro');
+		$this->ok('Value');
+		// id=macros_0_macro
+		$this->input_type('macros[0][macro]', $macro);
+		$this->input_type('macros[0][value]', $macrovalue);
+		$this->button_click('save');
+		$this->wait();
+		// checking that value of the macro has been updated in the DB
+		$sql = "SELECT * FROM globalmacro WHERE macro='$macro' AND value='$macrovalue' and globalmacroid=".$row['globalmacroid'];
+		$this->assertEquals(1, DBcount($sql), 'Chuck Norris: Value of the macro has not been updated in the DB. Perhaps it was saved with different globalmacroid.');
+	}
+
+	public function testFormAdministrationGeneralMacro_CreateDuplicate() {
+		$sql = "SELECT * FROM globalmacro order by globalmacroid";
+		$oldHashGlobalmacro = DBhash($sql);
+
+		$row = DBfetch(DBselect('select * from globalmacro order by macro limit 1'));
+
+		$this->login('config.php');
+		$this->dropdown_select_wait('configDropDown','Macros');
+		$this->assertTitle('Configuration of Zabbix');
+		$this->ok('CONFIGURATION OF ZABBIX');
+		$this->ok('Macros');
+
+		// creating already existing macro
+		$this->button_click('macro_add');
+		$this->input_type('macros[1][macro]', $row['macro']);
+		$this->input_type('macros[1][value]', $row['value']);
+		$this->button_click('save');
+		$this->wait();
+		$this->ok('ERROR: Cannot update macros');
+		$this->ok('More than one macro with same name found:');
+		$this->ok('Macros');
+		$this->ok('Macro');
+		$this->ok('Value');
+		$newHashGlobalmacro = DBhash($sql);
+		$this->assertEquals($oldHashGlobalmacro, $newHashGlobalmacro, 'Data in the DB table "globalmacro" has been changed');
+	}
+
+	public static function wrongMacros() {
+		return array(
+			array('1','value'),
+			array('AAA','value'),
+			array('{','value'),
+			array('}','value'),
+			array('{}','value'),
+			array('{AAA}','value'),
+			array('{$}','value'),
+			array('{$$}','value'),
+			array('{$$A}','value'),
+			array('{$A$}','value'),
+			array('{$ABCEFG}','')
+		);
+	}
+
+	/**
+	 * @dataProvider wrongMacros
+	 */
+	public function testFormAdministrationGeneralMacro_CreateWrong($macro, $value) {
+
+		$sql = "SELECT * FROM globalmacro order by globalmacroid";
+		$oldHashGlobalmacro = DBhash($sql);
+
+		$count = DBcount('select * from globalmacro');
+
+		// create new Global macro
+		$this->login('config.php');
+		$this->dropdown_select_wait('configDropDown','Macros');
+		$this->assertTitle('Configuration of Zabbix');
+		$this->ok('CONFIGURATION OF ZABBIX');
+		$this->ok('Macros');
+		$this->click('id=macro_add');
+		$this->waitForVisible("macros[$count][macro]");
+		$this->input_type("macros[$count][macro]", $macro);
+		$this->input_type("macros[$count][value]", $value);
+		$this->button_click('save');
+		$this->wait();
+		$this->ok('ERROR: Cannot update macros');
+		$this->ok('[CUserMacro.createGlobal -> CUserMacro.validate]');
+		$this->ok('Cannot add macro');
+
+		$newHashGlobalmacro = DBhash($sql);
+		$this->assertEquals($oldHashGlobalmacro, $newHashGlobalmacro, 'Data in the DB table "globalmacro" has been changed');
+	}
+
+	public function testFormAdministrationGeneralMacro_Delete() {
+		$this->chooseOkOnNextConfirmation();
+
+		$this->login('config.php');
+		$this->dropdown_select_wait('configDropDown','Macros');
+		$this->assertTitle('Configuration of Zabbix');
+		$this->ok('CONFIGURATION OF ZABBIX');
+		$this->ok('Macros');
+		$this->ok('Macro');
+		$this->ok('Value');
+		// No way to select what macro to remove currently
+		$this->click("id=macros_del");
+		$this->click("id=save");
+		$this->waitForConfirmation();
+		$this->wait();
+		$this->ok('Macros updated');
+		$this->ok('CONFIGURATION OF ZABBIX');
+		$this->ok('Macros');
+		$this->ok('Macro');
+		$this->ok('Value');
+	}
+}
+?>
