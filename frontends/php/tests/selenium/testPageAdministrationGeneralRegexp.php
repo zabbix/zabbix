22ebdc0fda75d13e5325a1a7d8a36ee2f94a54cd
igor <igor@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2011-12-28 12:44:11 +0000

    .......... [ZBXNEXT-686] Added Selenium test for testing Administration->General->Regexp main report form
    git-svn-id: svn://svn.zabbix.com/trunk@24352 97f52cf1-0a1b-0410-bd0e-c28be96e8082

---
+++ frontends/php/tests/selenium/testPageAdministrationGeneralRegexp.php
@@ -0,0 +1,145 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2011 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+?>
+<?php
+require_once(dirname(__FILE__).'/../include/class.cwebtest.php');
+
+class testPageAdministrationGeneralRegexp extends CWebTest {
+
+	public static function allRegexps(){
+		return DBdata('select * from regexps');
+	}
+
+	/**
+	* @dataProvider allRegexps
+	*/
+	public function testPageAdministrationGeneralRegexp_CheckLayout($regexp) {
+
+		$this->login('config.php');
+		$this->dropdown_select_wait('configDropDown', 'Regular expressions');
+		$this->assertTitle('Configuration of Zabbix');
+		$this->ok('CONFIGURATION OF ZABBIX');
+		$this->ok('Regular expressions');
+		$this->ok(array('Name', 'Expressions'));
+		// checking that all elements exists
+		$this->assertElementPresent('go');
+		// single quotes does not work here
+		$this->assertElementPresent("//select[@id='go']/option[text()='Delete selected']");
+		$this->assertElementPresent('goButton');
+
+		// checking that all regexps are present in the report
+		$this->ok(array($regexp['name']));
+		$this->dropdown_select('go', 'Delete selected');
+	}
+
+	/**
+	* @dataProvider allRegexps
+	*/
+	public function testPageAdministrationGeneralRegexp_SimpleUpdate($regexp) {
+
+		$sql_hash="select * from regexps order by regexpid";
+		$oldHashRegexp=DBhash($sql_hash);
+
+		$sql_hash2="select * from expressions order by expressionid";
+		$oldHashRegexp2=DBhash($sql_hash2);
+
+		$this->login('config.php');
+		$this->dropdown_select_wait('configDropDown', 'Regular expressions');
+		$this->ok('Regular expressions');
+		// checking that can click on each regexp and then save it without any changes
+		$this->click('link='.$regexp['name']);
+		$this->wait();
+		$this->button_click('save');
+		$this->wait();
+		$this->ok('Regular expression updated');
+
+		// $newHash=DBhash($sql_hash);
+		$this->assertEquals($oldHashRegexp, DBhash($sql_hash), "Chuck Norris: no-change regexp update should not update data in table 'regexps'");
+
+		// $newHash2=DBhash($sql_hash2);
+		$this->assertEquals($oldHashRegexp2, DBhash($sql_hash2), "Chuck Norris: no-change regexp update should not update data in table 'expressions'");
+		$this->dropdown_select('go','Delete selected');
+	}
+
+	/**
+	* @dataProvider allRegexps
+	*/
+	public function testPageAdministrationGeneralRegexp_MassDelete($regexp) {
+		$name = $regexp['name'];
+
+		DBsave_tables('regexps');
+
+		$this->login('config.php');
+		$this->dropdown_select_wait('configDropDown', 'Regular expressions');
+		$this->assertTitle('Configuration of Zabbix');
+		$this->ok('CONFIGURATION OF ZABBIX');
+		$this->ok('Regular expressions');
+		$this->ok(array('Name','Expressions'));
+
+		// detecting "regexpid" values for clicking checkboxes "regexpids[$regexpid]"
+		$sql = "SELECT regexpid FROM regexps WHERE name='$name'";
+		$result = DBfetch(DBselect($sql));
+		$regexpid = $result['regexpid'];
+		$this->checkbox_select("regexpids[$regexpid]");
+		$this->dropdown_select('go', 'Delete selected');
+		$this->chooseOkOnNextConfirmation();
+		$this->click('goButton');
+		$this->waitForConfirmation();
+		$this->wait();
+		$this->ok('Regular expression deleted');
+
+		// $sql = "SELECT * FROM regexps r WHERE r.name='$name'";
+		$sql1 = "SELECT * FROM regexps r WHERE r.name='$name'";
+		$this->assertEquals(0, DBcount($sql1), 'Chuck Norris: Regexp has not been deleted from the DB');
+
+		$sql2 = "SELECT * FROM expressions e WHERE e.regexpid=$regexpid";
+		$this->assertEquals(0, DBcount($sql2), 'Chuck Norris: Regexp expressions has not been deleted from the DB');
+		DBrestore_tables('regexps');
+	}
+
+	public function testPageAdministrationGeneralRegexp_MassDeleteAll() {
+
+		// DBsave_tables('regexps');
+
+		$this->login('config.php');
+		$this->dropdown_select_wait('configDropDown', 'Regular expressions');
+		$this->assertTitle('Configuration of Zabbix');
+		$this->ok('CONFIGURATION OF ZABBIX');
+		$this->ok('Regular expressions');
+		$this->ok(array('Name', 'Expressions'));
+
+		$this->checkbox_select("all_regexps");
+		$this->dropdown_select('go', 'Delete selected');
+		$this->chooseOkOnNextConfirmation();
+		$this->click('goButton');
+		$this->waitForConfirmation();
+		$this->wait();
+		$this->ok('Regular expression deleted');
+
+		$sql1 = "SELECT * FROM regexps";
+		$this->assertEquals(0, DBcount($sql1), 'Chuck Norris: Regexp has not been deleted from the DB');
+
+		$sql2 = "SELECT * FROM regexps r, expressions e WHERE r.regexpid =e.regexpid";
+		$this->assertEquals(0, DBcount($sql2), 'Chuck Norris: Regexp expressions has not been deleted from the DB');
+
+		// DBrestore_tables('regexps');
+	}
+
+}
