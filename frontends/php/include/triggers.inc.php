755c9753780c63b42b21719aa713b00739864209
slava <slava@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2011-12-30 10:29:25 +0000

    ..F....... [ZBX-4391] fixed suffix support in trigger testing interface and deleted YZEP
    git-svn-id: svn://svn.zabbix.com/trunk@24449 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/triggers.inc.php
+++ frontends/php/include/triggers.inc.php
@@ -2620,7 +2620,7 @@ function evalExpressionData($expression, $rplcts, $oct = false) {
 	$result = false;
 
 	$evStr = str_replace(array_keys($rplcts), array_values($rplcts), $expression);
-	preg_match_all("/[0-9\.]+[KMGTPEZYhmdw]?/", $evStr, $arr);
+	preg_match_all("/[0-9\.]+[KMGThmdw]?/", $evStr, $arr);
 	$evStr = str_replace(array($arr[0][0], $arr[0][1]), array(convert($arr[0][0]), convert($arr[0][1])), $evStr);
 
 	if (!preg_match("/^[0-9.\s=!()><+*\/&E|\-]+$/is", $evStr)) {
@@ -2789,24 +2789,12 @@ function resolveTriggerUrl($trigger) {
 
 function convert($value) {
 	$value = trim($value);
-	if (!preg_match('/(?P<value>[\-+]?[0-9]+[.]?[0-9]*)(?P<mult>[YZEPTGMKsmhdw]?)/', $value, $arr)) {
+	if (!preg_match('/(?P<value>[\-+]?[0-9]+[.]?[0-9]*)(?P<mult>[TGMKsmhdw]?)/', $value, $arr)) {
 		return $value;
 	}
 
 	$value = $arr['value'];
 	switch ($arr['mult']) {
-		case 'Y':
-			$value *= 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024;
-			break;
-		case 'Z':
-			$value *= 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024;
-			break;
-		case 'E':
-			$value *= 1024 * 1024 * 1024 * 1024 * 1024 * 1024;
-			break;
-		case 'P':
-			$value *= 1024 * 1024 * 1024 * 1024 * 1024;
-			break;
 		case 'T':
 			$value *= 1024 * 1024 * 1024 * 1024;
 			break;
