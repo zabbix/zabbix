009f4c818b0541b7aeab709046659e616b93e961
rich <rich@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2012-01-02 22:59:41 +0000

    .......... [ZBX-954] some coding style fixes
    git-svn-id: svn://svn.zabbix.com/trunk@24482 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/tests/include/dbfunc.php
+++ frontends/php/tests/include/dbfunc.php
@@ -26,8 +26,7 @@ require_once(dirname(__FILE__).'/../../include/copt.lib.php');
 require_once(dirname(__FILE__).'/../../include/func.inc.php');
 require_once(dirname(__FILE__).'/../../include/db.inc.php');
 
-function error($error)
-{
+function error($error) {
 	echo "\nError reported: $error\n";
 	return true;
 }
@@ -35,15 +34,13 @@ function error($error)
 /**
  * Returns database data suitable for PHPUnit data provider functions
  */
-function DBdata($query)
-{
+function DBdata($query) {
 	DBconnect($error);
 
 	$objects=array();
 
 	$result=DBselect($query);
-	while($object=DBfetch($result))
-	{
+	while($object=DBfetch($result)) {
 		$objects[]=array($object);
 	}
 
@@ -56,9 +53,8 @@ function DBdata($query)
  * For example: DBget_tables('users')
  * Result: array(users,alerts,acknowledges,auditlog,auditlog_details,opmessage_usr,media,profiles,sessions,users_groups)
  */
-function DBget_tables(&$tables, $topTable)
-{
-	if(in_array($topTable, $tables))
+function DBget_tables(&$tables, $topTable) {
+	if (in_array($topTable, $tables))
 		return;
 
 	$schema = include(dirname(__FILE__).'/../../include/schema.inc.php');
@@ -66,25 +62,24 @@ function DBget_tables(&$tables, $topTable)
 	$tableData = $schema[$topTable];
 
 	$fields = $tableData['fields'];
-	foreach($fields as $field => $fieldData){
-		if(isset($fieldData['ref_table'])){
+	foreach ($fields as $field => $fieldData) {
+		if (isset($fieldData['ref_table'])) {
 			$refTable = $fieldData['ref_table'];
-			if($refTable != $topTable)
+			if ($refTable != $topTable)
 				DBget_tables($tables, $refTable);
 		}
 	}
 
-	if(!in_array($topTable, $tables))
+	if (!in_array($topTable, $tables))
 		$tables[] = $topTable;
 
-	foreach($schema as $table => $tableData)
-	{
+	foreach ($schema as $table => $tableData) {
 		$fields = $schema[$table]['fields'];
 		$referenced = false;
-		foreach($fields as $field => $fieldData){
-			if(isset($fieldData['ref_table'])){
+		foreach ($fields as $field => $fieldData) {
+			if (isset($fieldData['ref_table'])) {
 				$refTable = $fieldData['ref_table'];
-				if($refTable == $topTable && $topTable != $table){
+				if ($refTable == $topTable && $topTable != $table) {
 					DBget_tables($tables, $table);
 				}
 			}
@@ -96,17 +91,15 @@ function DBget_tables(&$tables, $topTable)
  * Saves data of the specified table and all dependent tables in temporary storage.
  * For example: DBsave_tables('users')
  */
-function DBsave_tables($topTable)
-{
+function DBsave_tables($topTable) {
 	global $DB;
 
 	$tables = array();
 
 	DBget_tables($tables, $topTable);
 
-	foreach($tables as $table)
-	{
-		switch($DB['TYPE']) {
+	foreach ($tables as $table) {
+		switch ($DB['TYPE']) {
 		case ZBX_DB_MYSQL:
 			DBexecute("drop table if exists ${table}_tmp");
 			DBexecute("create table ${table}_tmp like $table");
@@ -127,8 +120,7 @@ function DBsave_tables($topTable)
  * Restores data from temporary storage. DBsave_tables() must be called first.
  * For example: DBrestore_tables('users')
  */
-function DBrestore_tables($topTable)
-{
+function DBrestore_tables($topTable) {
 	global $DB;
 
 	$tables = array();
@@ -137,13 +129,11 @@ function DBrestore_tables($topTable)
 
 	$tables_reversed = array_reverse($tables);
 
-	foreach($tables_reversed as $table)
-	{
+	foreach ($tables_reversed as $table) {
 		DBexecute("delete from $table");
 	}
 
-	foreach($tables as $table)
-	{
+	foreach ($tables as $table) {
 		DBexecute("insert into $table select * from ${table}_tmp");
 		DBexecute("drop table ${table}_tmp");
 	}
@@ -152,17 +142,14 @@ function DBrestore_tables($topTable)
 /**
  * Returns md5 hash sum of database result.
  */
-function DBhash($sql)
-{
+function DBhash($sql) {
 	global $DB;
 
 	$hash = '<empty hash>';
 
 	$result=DBselect($sql);
-	while($row = DBfetch($result))
-	{
-		foreach($row as $key => $value)
-		{
+	while ($row = DBfetch($result)) {
+		foreach ($row as $key => $value) {
 			$hash = md5($hash.$value);
 		}
 	}
@@ -173,24 +160,23 @@ function DBhash($sql)
 /**
  * Returns number of records in database result.
  */
-function DBcount($sql, $limit = null, $offset = null){
+function DBcount($sql, $limit = null, $offset = null) {
 	$cnt = 0;
 
-	if(isset($limit) && isset($offset)){
+	if (isset($limit) && isset($offset)) {
 		$result = DBselect($sql, $limit, $offset);
 	}
-	else if(isset($limit)){
-		$result = DBselect($sql,$limit);
+	elseif (isset($limit)) {
+		$result = DBselect($sql, $limit);
 	}
-	else{
+	else {
 		$result = DBselect($sql);
 	}
 
-	while(DBfetch($result)){
+	while (DBfetch($result)) {
 		$cnt++;
 	}
 
 	return $cnt;
 }
-
 ?>
