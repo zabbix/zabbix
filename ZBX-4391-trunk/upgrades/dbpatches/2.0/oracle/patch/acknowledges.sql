1aa70960e5f2cb120f6af469809b55f09131ddf7
slava <slava@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-01-04 07:32:50 +0000

    .......... [ZBX-4391] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@24505 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4391-trunk/upgrades/dbpatches/2.0/oracle/patch/acknowledges.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE acknowledges MODIFY acknowledgeid DEFAULT NULL;
-ALTER TABLE acknowledges MODIFY userid DEFAULT NULL;
-ALTER TABLE acknowledges MODIFY eventid DEFAULT NULL;
-DELETE FROM acknowledges WHERE NOT userid IN (SELECT userid FROM users);
-DELETE FROM acknowledges WHERE NOT eventid IN (SELECT eventid FROM events);
-ALTER TABLE acknowledges ADD CONSTRAINT c_acknowledges_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
-ALTER TABLE acknowledges ADD CONSTRAINT c_acknowledges_2 FOREIGN KEY (eventid) REFERENCES events (eventid) ON DELETE CASCADE;
