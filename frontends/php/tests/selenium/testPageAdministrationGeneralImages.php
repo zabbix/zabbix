b6739b9c3cc345bda3708eaae49a4725b1ec7ea5
Igor Danoshaites <git-no-reply@zabbix.com>
2012-01-09 10:20:34 +0000

    .......... [ZBX-954] some additional minor coding style fixes

--- frontends/php/tests/selenium/testPageAdministrationGeneralImages.php
+++ frontends/php/tests/selenium/testPageAdministrationGeneralImages.php
@@ -23,11 +23,11 @@ require_once(dirname(__FILE__).'/../include/class.cwebtest.php');
 
 class testPageAdministrationGeneralImages extends CWebTest {
 
-	public static function allIcons(){
+	public static function allIcons() {
 		return DBdata('SELECT name FROM images WHERE imagetype=1 limit 5');
 	}
 
-	public static function allBgImages(){
+	public static function allBgImages() {
 		return DBdata('SELECT name FROM images WHERE imagetype=2 limit 5');
 	}
 
@@ -84,7 +84,7 @@ class testPageAdministrationGeneralImages extends CWebTest {
 		$this->assertElementPresent('configDropDown');
 		$this->dropdown_select_wait('configDropDown', 'Images');
 		$this->assertElementPresent('form');
-		$this->dropdown_select_wait('imagetype','Icon');
+		$this->dropdown_select_wait('imagetype', 'Icon');
 		$this->assertTitle('Configuration of Zabbix');
 		$this->ok(array('CONFIGURATION OF ZABBIX', 'Images', 'Type'));
 		$this->click('link='.$icon_name['name']);
@@ -111,9 +111,9 @@ class testPageAdministrationGeneralImages extends CWebTest {
 
 		$this->login('config.php');
 		$this->assertElementPresent('configDropDown');
-		$this->dropdown_select_wait('configDropDown','Images');
+		$this->dropdown_select_wait('configDropDown', 'Images');
 		$this->assertElementPresent('form');
-		$this->dropdown_select_wait('imagetype','Background');
+		$this->dropdown_select_wait('imagetype', 'Background');
 		$this->assertTitle('Configuration of Zabbix');
 		$this->ok(array('CONFIGURATION OF ZABBIX', 'Images', 'Type'));
 		$this->click('link='.$bgimage_name['name']);
