347cb98c826f0ddc983cbe1947f2ba48b74578de
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-01-23 13:32:31 +0000

    .......... [ZBX-3531] branch created
    git-svn-id: svn://svn.zabbix.com/branches/dev@24962 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-3531/upgrades/dbpatches/1.1/1.1alpha3_to_1.1alpha4/mysql/patch.sql
@@ -0,0 +1,3 @@
+alter table alerts add	error	varchar(128)	DEFAULT '' NOT NULL;
+alter table items add	error	varchar(128)	DEFAULT '' NOT NULL;
+alter table hosts add	error	varchar(128)	DEFAULT '' NOT NULL;
