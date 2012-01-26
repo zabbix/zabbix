zabbix/zabbix

commit ee3d19418616bf995b83068d0b85ae71a0eacb81

..F....... [ZBX-4558] added an info message when deleting inherited templated triggers
git-svn-id: svn://svn.zabbix.com/branches/dev@24960 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

ZBX-4558/frontends/php/api/classes/class.ctrigger.php
@@ -1457,7 +1457,7 @@ class CTrigger extends CZBXAPI {
 		foreach ($triggers as $trigger) {
 			// pass the full trigger so the children can inherit all of the data
 			$dbTrigger = $dbTriggers[$trigger['triggerid']];
-			if (isset($trigger['exrpession'])) {
+			if (isset($trigger['expression'])) {
 				$dbTrigger['expression'] = $trigger['expression'];
 			}
 
@@ -1799,13 +1799,22 @@ class CTrigger extends CZBXAPI {
 		));
 
 		// fetch the existing child triggers
-		$templatedTriggers = $this->select('triggers', array(
-			'filter' => array('templateid' => $trigger['triggerid'])
+		$templatedTriggers = $this->get(array(
+			'output' => array('triggerid', 'description'),
+			'filter' => array(
+				'templateid' => $trigger['triggerid']
+			),
+			'selectHosts' => array('name'),
+			'preservekeys' => true
 		));
 
 		if (empty($triggerTemplates)) {
 			// no templates found, delete any child triggers, that may exist
 			if ($templatedTriggers) {
+				foreach ($templatedTriggers as $trigger) {
+					info(_s('Deleted: Trigger "%1$s" on "%2$s".', $trigger['description'],
+						implode(', ', zbx_objectValues($trigger['hosts'], 'name'))));
+				}
 				$this->deleteByPks(zbx_objectValues($templatedTriggers, 'triggerid'));
 			}
 
@@ -1843,7 +1852,6 @@ class CTrigger extends CZBXAPI {
 			'templated_hosts' => true
 		));
 
-		$childTriggerIds = array();
 		foreach ($chd_hosts as $chd_host) {
 			$newTrigger = $trigger;
 			$newTrigger['templateid'] = $trigger['triggerid'];
@@ -1940,13 +1948,16 @@ class CTrigger extends CZBXAPI {
 			}
 			$this->inherit($newTrigger);
 
-			$childTriggerIds[] = $newTrigger['triggerid'];
+			unset($templatedTriggers[$newTrigger['triggerid']]);
 		}
 
 		// delete remaining child triggers
-		$remainingChildTriggerIds = array_diff(zbx_objectValues($templatedTriggers, 'triggerid'), $childTriggerIds);
-		if ($remainingChildTriggerIds) {
-			$this->deleteByPks($remainingChildTriggerIds);
+		if ($templatedTriggers) {
+			foreach ($templatedTriggers as $trigger) {
+				info(_s('Deleted: Trigger "%1$s" on "%2$s".', $trigger['description'],
+					implode(', ', zbx_objectValues($trigger['hosts'], 'name'))));
+			}
+			$this->deleteByPks(zbx_objectValues($templatedTriggers, 'triggerid'));
 		}
 
 		return true;

commit 03d1afdab64a3c391677cb5ffe79acf9511da37e

.......... [ZBX-4558] fixed coding style broken by r24960
git-svn-id: svn://svn.zabbix.com/branches/dev@25032 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

ZBX-4558/frontends/php/api/classes/class.ctrigger.php
@@ -1489,11 +1489,11 @@ class CTrigger extends CZBXAPI {
 		// get child triggers
 		$parentTriggerids = $triggerids;
 		do {
-			$db_items = DBselect('SELECT triggerid FROM triggers WHERE '.DBcondition('templateid', $parentTriggerids));
+			$dbItems = DBselect('SELECT triggerid FROM triggers WHERE '.DBcondition('templateid', $parentTriggerids));
 			$parentTriggerids = array();
-			while ($db_trigger = DBfetch($db_items)) {
-				$parentTriggerids[] = $db_trigger['triggerid'];
-				$triggerids[] = $db_trigger['triggerid'];
+			while ($dbTrigger = DBfetch($dbItems)) {
+				$parentTriggerids[] = $dbTrigger['triggerid'];
+				$triggerids[] = $dbTrigger['triggerid'];
 			}
 		} while (!empty($parentTriggerids));
 
@@ -1507,9 +1507,9 @@ class CTrigger extends CZBXAPI {
 		// TODO: REMOVE info
 		foreach ($delTriggers as $trigger) {
 			info(_s('Deleted: Trigger "%1$s" on "%2$s".', $trigger['description'],
-				implode(', ', zbx_objectValues($trigger['hosts'], 'name'))));
+					implode(', ', zbx_objectValues($trigger['hosts'], 'name'))));
 			add_audit_ext(AUDIT_ACTION_DELETE, AUDIT_RESOURCE_TRIGGER, $trigger['triggerid'],
-				$trigger['description'], null, null, null);
+					$trigger['description'], null, null, null);
 		}
 
 		// execute delete
@@ -1534,9 +1534,9 @@ class CTrigger extends CZBXAPI {
 		$actionids = array();
 		$dbActions = DBselect(
 			'SELECT DISTINCT actionid'.
-				' FROM conditions'.
-				' WHERE conditiontype='.CONDITION_TYPE_TRIGGER.
-				' AND '.DBcondition('value', $pks, false, true)
+			' FROM conditions'.
+			' WHERE conditiontype='.CONDITION_TYPE_TRIGGER.
+			' AND '.DBcondition('value', $pks, false, true)
 		);
 		while ($dbAction = DBfetch($dbActions)) {
 			$actionids[$dbAction['actionid']] = $dbAction['actionid'];
@@ -2097,7 +2097,7 @@ class CTrigger extends CZBXAPI {
 				$upTriggerids = array();
 				while ($upTrigger = DBfetch($dbUpTriggers)) {
 					if (bccomp($upTrigger['triggerid_up'], $trigger['triggerid']) == 0) {
-						self::exception(ZBX_API_ERROR_PARAMETERS, S_INCORRECT_DEPENDENCY);
+						self::exception(ZBX_API_ERROR_PARAMETERS, _('Incorrect dependency'));
 					}
 					$upTriggerids[] = $upTrigger['triggerid_up'];
 				}