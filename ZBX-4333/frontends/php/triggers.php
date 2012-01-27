afc2ac6f6b8e649f39a1345ee910aa270f516755
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-01-27 09:29:47 +0000

    A......... [ZBX-4333] fixed SQL error when trying to copy a trigger with dependencies to a host without the nesessary item key A......... [ZBX-4333] fixed empty error messages
    git-svn-id: svn://svn.zabbix.com/branches/dev@25043 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4333/frontends/php/triggers.php
+++ ZBX-4333/frontends/php/triggers.php
@@ -429,10 +429,12 @@ include_once('include/page_header.php');
 					$new_triggerids[$trigger_id] = $newtrigid;
 					$go_result |= (bool) $newtrigid;
 				}
-				foreach ($_REQUEST['g_triggerid'] as $trigger_id) {
-					$deps = replace_template_dependencies(get_trigger_dependencies_by_triggerid($trigger_id), $host_id);
-					foreach ($deps as $dep_id) {
-						add_trigger_dependency($new_triggerids[$trigger_id], $dep_id);
+				if ($go_result) {
+					foreach ($_REQUEST['g_triggerid'] as $trigger_id) {
+						$deps = replace_template_dependencies(get_trigger_dependencies_by_triggerid($trigger_id), $host_id);
+						foreach ($deps as $dep_id) {
+							add_trigger_dependency($new_triggerids[$trigger_id], $dep_id);
+						}
 					}
 				}
 			}
