fbcf2c75c820caee576e657e3fc3c3a448ea771f
Pavels Jelisejevs <git-no-reply@zabbix.com>
2012-01-31 14:20:40 +0000

    A......... [ZBX-4586] implemented item's application validation in the Item.update and Item.create API methods

--- frontends/php/api/classes/class.citemgeneral.php
+++ frontends/php/api/classes/class.citemgeneral.php
@@ -116,10 +116,11 @@ abstract class CItemGeneral extends CZBXAPI{
 			));
 
 			$dbHosts = API::Host()->get(array(
-				'output' => array('hostid', 'host', 'status'),
+				'output' => array('hostid', 'host', 'status', 'name'),
 				'hostids' => zbx_objectValues($dbItems, 'hostid'),
 				'templated_hosts' => true,
 				'editable' => true,
+				'selectApplications' => API_OUTPUT_REFER,
 				'preservekeys' => true
 			));
 		}
@@ -129,10 +130,11 @@ abstract class CItemGeneral extends CZBXAPI{
 			);
 
 			$dbHosts = API::Host()->get(array(
-				'output' => array('hostid', 'host', 'status'),
+				'output' => array('hostid', 'host', 'status', 'name'),
 				'hostids' => zbx_objectValues($items, 'hostid'),
 				'templated_hosts' => true,
 				'editable' => true,
+				'selectApplications' => API_OUTPUT_REFER,
 				'preservekeys' => true
 			));
 		}
@@ -160,6 +162,7 @@ abstract class CItemGeneral extends CZBXAPI{
 				}
 
 				check_db_fields($dbItems[$item['itemid']], $fullItem);
+				$host = $dbHosts[$fullItem['hostid']];
 
 				// apply rules
 				foreach ($this->fieldRules as $field => $rules) {
@@ -271,6 +274,17 @@ abstract class CItemGeneral extends CZBXAPI{
 			if (isset($fullItem['port']) && !zbx_empty($fullItem['port']) && !validatePortNumberOrMacro($fullItem['port'])) {
 				self::exception(ZBX_API_ERROR_PARAMETERS,
 					_s('Item "%1$s:%2$s" has invalid port: "%3$s".', $fullItem['name'], $fullItem['key_'], $fullItem['port']));
+			}
+
+			// check that the given applications belong to the item's host
+			if (isset($item['applications']) && $item['applications']) {
+				$dbApplicationIds = zbx_objectValues($host['applications'], 'applicationid');
+				foreach($item['applications'] as $appId) {
+					if (!in_array($appId, $dbApplicationIds)) {
+						$error = _s('Application with ID "%1$s" is not available on host "%2$s"', $appId, $host['name']);
+						self::exception(ZBX_API_ERROR_PARAMETERS, $error);
+					}
+				}
 			}
 
 			$this->checkSpecificFields($fullItem);
