7b5a9b58a756683593953f5a1e30a8134c32a228
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2012-02-01 20:21:03 +0000

    .......... [ZBXNEXT-826] updated version number

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	public static final String REVISION_DATE = "01 February 2012";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "1.9.9";
+	public static final String VERSION = "1.9.10";
 
 	public static void printVersion()
 	{
