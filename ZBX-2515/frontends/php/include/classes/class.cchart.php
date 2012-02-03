74edd9963a8ad101a9e0295cb964082e3eaf5d88
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-02-03 08:28:21 +0000

    .......... [ZBX-2515] merge from trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@25181 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-2515/frontends/php/include/classes/class.cchart.php
+++ ZBX-2515/frontends/php/include/classes/class.cchart.php
@@ -1785,9 +1785,7 @@ class CChart extends CGraphDraw{
 	protected function limitToBounds(&$value1, &$value2, $min, $max, $drawtype) {
 // Fixes graph out of bounds problem
 		if ( (($value1 > ($max+$min)) && ($value2 > ($max+$min))) || (($value1 < $min) && ($value2 < $min)) ) {
-			if (!in_array($drawtype, array(GRAPH_ITEM_DRAWTYPE_FILLED_REGION, GRAPH_ITEM_DRAWTYPE_GRADIENT_LINE))) {
-				return false;
-			};
+			if ($drawtype != GRAPH_ITEM_DRAWTYPE_FILLED_REGION) return false;
 		}
 
 		$y_first = (($value1 > ($max+$min)) || ($value1 < $min));
