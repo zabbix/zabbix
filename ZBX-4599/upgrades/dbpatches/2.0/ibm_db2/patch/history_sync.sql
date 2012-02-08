5e45f90bd1627818965d654a331b2c72d3a08a89
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-02-08 11:55:15 +0000

    .......... [ZBX-4599] created dev brach
    git-svn-id: svn://svn.zabbix.com/branches/dev@25247 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-4599/upgrades/dbpatches/2.0/ibm_db2/patch/history_sync.sql
@@ -0,0 +1,16 @@
+ALTER TABLE history_sync ALTER COLUMN itemid SET WITH DEFAULT NULL
+/
+REORG TABLE history_sync
+/
+ALTER TABLE history_sync ALTER COLUMN nodeid SET WITH DEFAULT NULL
+/
+REORG TABLE history_sync
+/
+ALTER TABLE history_sync ALTER COLUMN nodeid SET DATA TYPE integer
+/
+REORG TABLE history_sync
+/
+ALTER TABLE history_sync ADD ns integer WITH DEFAULT '0' NOT NULL
+/
+REORG TABLE history_sync
+/
