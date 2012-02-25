430627cda94c539901bf2a9858fc8d24e5420da5
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-02-25 10:59:14 +0000

    ....I..... [ZBX-4693] fix foreign key constraint error during upgrade to 2.0
    git-svn-id: svn://svn.zabbix.com/branches/dev@25605 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4693/upgrades/dbpatches/2.0/oracle/patch/hosts.sql
+++ ZBX-4693/upgrades/dbpatches/2.0/oracle/patch/hosts.sql
@@ -213,7 +213,7 @@ ALTER TABLE hosts ADD jmx_errors_from number(10) DEFAULT '0' NOT NULL;
 ALTER TABLE hosts ADD jmx_error nvarchar2(128) DEFAULT '';
 ALTER TABLE hosts ADD name nvarchar2(64) DEFAULT '';
 UPDATE hosts SET proxy_hostid=NULL WHERE proxy_hostid=0;
-UPDATE hosts SET maintenanceid=NULL WHERE maintenanceid=0;
+UPDATE hosts SET maintenanceid=NULL;
 UPDATE hosts SET name=host WHERE status in (0,1,3)	-- MONITORED, NOT_MONITORED, TEMPLATE
 /
 CREATE INDEX hosts_4 on hosts (name);
