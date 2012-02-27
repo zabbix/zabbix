71a5106002d305abf1d5de5be69807f7fb29ed3c
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-02-27 08:07:26 +0000

    .......... [ZBX-4467] removed merged branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@25610 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4467/upgrades/dbpatches/2.0/oracle/patch/maintenances_groups.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE maintenances_groups MODIFY maintenance_groupid DEFAULT NULL;
-ALTER TABLE maintenances_groups MODIFY maintenanceid DEFAULT NULL;
-ALTER TABLE maintenances_groups MODIFY groupid DEFAULT NULL;
-DROP INDEX maintenances_groups_1;
-DELETE FROM maintenances_groups WHERE maintenanceid NOT IN (SELECT maintenanceid FROM maintenances);
-DELETE FROM maintenances_groups WHERE groupid NOT IN (SELECT groupid FROM groups);
-CREATE UNIQUE INDEX maintenances_groups_1 ON maintenances_groups (maintenanceid,groupid);
-ALTER TABLE maintenances_groups ADD CONSTRAINT c_maintenances_groups_1 FOREIGN KEY (maintenanceid) REFERENCES maintenances (maintenanceid) ON DELETE CASCADE;
-ALTER TABLE maintenances_groups ADD CONSTRAINT c_maintenances_groups_2 FOREIGN KEY (groupid) REFERENCES groups (groupid) ON DELETE CASCADE;
