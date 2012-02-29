fb16f4eded536764951a33f6055f7874be3d8957
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-02-29 13:59:18 +0000

    ........S. [ZBX-4549] fix nested db transaction error when stopping server
    git-svn-id: svn://svn.zabbix.com/branches/1.8@25698 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxdb/db.c
+++ src/libs/zbxdb/db.c
@@ -23,8 +23,8 @@
 #include "zbxdb.h"
 #include "log.h"
 
-int		txn_level = 0;	/* transaction level, nested transactions are not supported */
-int		txn_error = 0;	/* failed transaction */
+static int	txn_level = 0;	/* transaction level, nested transactions are not supported */
+static int	txn_error = 0;	/* failed transaction */
 static int	txn_init = 0;	/* connecting to db */
 
 #if defined(HAVE_IBM_DB2)
@@ -507,8 +507,7 @@ int	zbx_db_begin()
 
 	if (txn_level > 0)
 	{
-		zabbix_log(LOG_LEVEL_CRIT, "ERROR: nested transaction detected."
-				" Please report it to Zabbix Team.");
+		zabbix_log(LOG_LEVEL_CRIT, "ERROR: nested transaction detected. Please report it to Zabbix Team.");
 		assert(0);
 	}
 
@@ -647,6 +646,16 @@ int	zbx_db_rollback()
 		txn_error = last_txn_error;	/* in case of DB down we will repeat this operation */
 
 	return rc;
+}
+
+int	zbx_db_txn_level()
+{
+	return txn_level;
+}
+
+int	zbx_db_txn_error()
+{
+	return txn_error;
 }
 
 /*
