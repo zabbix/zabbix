99703246490af34a5040e7c5f7502e4297438b7e
tomtom <tomtom@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-03-01 08:55:29 +0000

    ..F....... [ZBX-4253] Fixed popupmenu links in tr_status.php for "Configurations of triggers" and "URL"
    git-svn-id: svn://svn.zabbix.com/branches/dev@25710 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4253/frontends/php/tr_status.php
+++ ZBX-4253/frontends/php/tr_status.php
@@ -456,14 +456,13 @@ include_once('include/page_header.php');
 
 		$menu_trigger_conf = 'null';
 		if($admin_links){
-			$menu_trigger_conf = "['".S_CONFIGURATION_OF_TRIGGERS."',\"javascript:
-				redirect('triggers.php?form=update&triggerid=".$trigger['triggerid'].'&switch_node='.id2nodeid($trigger['triggerid'])."')\",
+			$menu_trigger_conf = "['".S_CONFIGURATION_OF_TRIGGERS."','triggers.php?form=update&triggerid=".$trigger['triggerid']."&switch_node=".id2nodeid($trigger['triggerid'])."',
 				null, {'outer' : ['pum_o_item'],'inner' : ['pum_i_item']}]";
 		}
 		$menu_trigger_url = 'null';
 		if (!zbx_empty($trigger['url'])) {
-			// double zbx_jsvalue is required to prevent XSS attacks
-			$menu_trigger_url = "['".S_URL."',\"javascript: window.location.href=".zbx_jsvalue(zbx_jsvalue(resolveTriggerUrl($trigger), null, false))."\",
+			// double htmlentities is required to prevent XSS attacks
+			$menu_trigger_url = "['".S_URL."','".htmlentities(htmlentities(resolveTriggerUrl($trigger), ENT_QUOTES, "UTF"), ENT_QUOTES, "UTF")."',
 				null, {'outer' : ['pum_o_item'],'inner' : ['pum_i_item']}]";
 		}
 		$description->addAction('onclick',
@@ -471,7 +470,6 @@ include_once('include/page_header.php');
 				"', 'lastchange': '".$trigger['lastchange']."'}, ".$menu_trigger_conf.", ".$menu_trigger_url."),".
 			zbx_jsvalue($items, true).");"
 		);
-
 
 // }}} trigger description js menu
 
