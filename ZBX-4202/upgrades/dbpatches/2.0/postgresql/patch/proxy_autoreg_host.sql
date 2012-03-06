daa41e157c44a6b1170f4d2e9681a53c335b019d
tomtom <tomtom@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-03-06 11:20:04 +0000

    .......... [ZBX-4202] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@25832 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4202/upgrades/dbpatches/2.0/postgresql/patch/proxy_autoreg_host.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE ONLY proxy_autoreg_host ADD listen_ip varchar(39) DEFAULT '' NOT NULL,
-				    ADD listen_port integer DEFAULT '0' NOT NULL,
-				    ADD listen_dns varchar(64) DEFAULT '' NOT NULL;
