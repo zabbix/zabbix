6732ed9efa4f50268261beaad0e582cafc8e34b8
Alexey Fukalov <git-no-reply@zabbix.com>
2012-03-09 09:32:47 +0000

    ..F....... [ZBX-3531] moved method fileExt2ImportFormat to CImportReaderFactory

--- frontends/php/include/classes/import/readers/CImportReaderFactory.php
+++ frontends/php/include/classes/import/readers/CImportReaderFactory.php
@@ -44,4 +44,26 @@ class CImportReaderFactory {
 				throw new Exception(_s('Unsupported import format "%1$s".', $format));
 		}
 	}
+
+	/**
+	 * Converts file extenion to associated import format.
+	 *
+	 * @static
+	 * @throws Exception
+	 *
+	 * @param string $ext
+	 *
+	 * @return string
+	 */
+	public static function fileExt2ImportFormat($ext) {
+		switch ($ext) {
+			case 'xml':
+				return CImportReaderFactory::XML;
+			case 'json':
+				return CImportReaderFactory::JSON;
+			default:
+				throw new Exception(_s('Unsupported import file extension "%1$s".', $ext));
+		}
+
+	}
 }
