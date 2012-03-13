5e5c00ced79b7c82485d2daccb16b1efb9304f6e
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2012-03-13 14:12:20 +0000

    ....I..PS. [ZBX-4751] removed "WITH OIDS" option from PostgreSQL database scheme

--- upgrades/dbpatches/2.0/postgresql/patch/graph_discovery.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/graph_discovery.sql
@@ -4,7 +4,7 @@ CREATE TABLE graph_discovery (
 	parent_graphid           bigint                                    NOT NULL,
 	name                     varchar(128)    DEFAULT ''                NOT NULL,
 	PRIMARY KEY (graphdiscoveryid)
-) with OIDS;
+);
 CREATE UNIQUE INDEX graph_discovery_1 on graph_discovery (graphid,parent_graphid);
 ALTER TABLE ONLY graph_discovery ADD CONSTRAINT c_graph_discovery_1 FOREIGN KEY (graphid) REFERENCES graphs (graphid) ON DELETE CASCADE;
 ALTER TABLE ONLY graph_discovery ADD CONSTRAINT c_graph_discovery_2 FOREIGN KEY (parent_graphid) REFERENCES graphs (graphid) ON DELETE CASCADE;
