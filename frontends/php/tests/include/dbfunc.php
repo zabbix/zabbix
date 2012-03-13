611d34264f50ec5544d9d506999a9ff16e54dd59
Alexey Fukalov <git-no-reply@zabbix.com>
2012-03-13 12:59:02 +0000

    .......... [ZBXNEXT-686] reverted last change to test, added require of CProfiler class in dbfunc.php

--- frontends/php/tests/include/dbfunc.php
+++ frontends/php/tests/include/dbfunc.php
@@ -24,6 +24,7 @@ require_once dirname(__FILE__).'/../../include/defines.inc.php';
 require_once dirname(__FILE__).'/../../conf/zabbix.conf.php';
 require_once dirname(__FILE__).'/../../include/func.inc.php';
 require_once dirname(__FILE__).'/../../include/db.inc.php';
+require_once dirname(__FILE__).'/../../include/classes/debug/CProfiler.php';
 
 function error($error) {
 	echo "\nError reported: $error\n";
