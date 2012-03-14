5c255007d30d27c6347b307c8fbca17668e6f3a7
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-03-14 09:30:01 +0000

    .......... [ZBX-4574] branch created
    git-svn-id: svn://svn.zabbix.com/branches/dev@26057 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-4574/frontends/php/scripts_exec.php
@@ -0,0 +1,70 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2011 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+?>
+<?php
+require_once 'include/config.inc.php';
+require_once 'include/hosts.inc.php';
+require_once 'include/forms.inc.php';
+
+$page['title'] = _('Scripts');
+$page['file'] = 'scripts_exec.php';
+
+define('ZBX_PAGE_NO_MENU', 1);
+
+require_once ('include/page_header.php');
+
+//	VAR		TYPE	OPTIONAL FLAGS	VALIDATION	EXCEPTION
+$fields = array(
+	'hostid' =>		array(T_ZBX_INT, O_OPT, P_SYS,	DB_ID,		'isset({execute})'),
+	'scriptid' =>	array(T_ZBX_INT, O_OPT, P_SYS,	DB_ID,		'isset({execute})'),
+	'execute' =>	array(T_ZBX_INT, O_OPT, P_ACT,	IN('0,1'),	null),
+);
+check_fields($fields);
+
+if (isset($_REQUEST['execute'])) {
+	$scriptid = $_REQUEST['scriptid'];
+	$hostid = $_REQUEST['hostid'];
+
+	$script_info = DBfetch(DBselect(
+		'SELECT s.name'.
+		' FROM scripts s'.
+		' WHERE s.scriptid='.$scriptid
+	));
+
+	$result = API::Script()->execute(array('hostid' => $hostid, 'scriptid' => $scriptid));
+	if ($result === false) {
+		show_messages(false, '', _('Cannot connect to the trapper port of zabbix server daemon, but it should be available to run the script.'));
+	}
+	else {
+		$message = $result['value'];
+		if ($result['response'] == 'failed') {
+			error($message);
+			show_messages(false, '', _('Cannot connect to the trapper port of zabbix server daemon, but it should be available to run the script.'));
+			$message = '';
+		}
+		$frmResult = new CFormTable($script_info['name']);
+		$frmResult->addRow(_('Result'), new CTextArea('message', $message, 25, ZBX_TEXTAREA_BIG_WIDTH, 'yes'));
+		$frmResult->addItemToBottomRow(new CButton('close', S_CLOSE, 'window.close();'));
+		$frmResult->show();
+	}
+}
+
+require_once "include/page_footer.php";
+?>
