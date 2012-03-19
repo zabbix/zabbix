82645366bfe63fc0328050433001033b6584f0f8
Alexey Fukalov <git-no-reply@zabbix.com>
2012-03-19 09:46:19 +0000

    ..F....... [ZBX-4621] added replaceMacros method CUserMacro class

--- frontends/php/include/locales/en_gb.inc.php
+++ frontends/php/include/locales/en_gb.inc.php
@@ -122,8 +122,6 @@
 
 	'S_TEMPLATES'=>				_('Templates'),
 	'S_UNLINK'=>				_('Unlink'),
-	'S_CANNOT_UNLINK_TEMPLATE'=>	_('Cannot unlink template'),
-	'S_CANNOT_LINK_TEMPLATE'=>	_('Cannot link template'),
 	'S_UNLINK_AND_CLEAR'=>			_('Unlink and clear'),
 
 //	items.php
