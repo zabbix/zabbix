31c0064750f27e0df52a64fde81bf2e1d264227e
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-03-20 19:31:50 +0000

    .......... [ZBXNEXT-744] updated to the latest 1.8
    git-svn-id: svn://svn.zabbix.com/branches/dev@26241 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-744/frontends/php/include/classes/class.cobject.php
+++ ZBXNEXT-744/frontends/php/include/classes/class.cobject.php
@@ -19,97 +19,73 @@
 **/
 ?>
 <?php
-class CObject{
- public $items;
+class CObject {
+	public $items;
 
-	public function __construct($items=null){
+	public function __construct($items = null) {
 		$this->items = array();
-		if(isset($items)){
+		if (isset($items)) {
 			$this->addItem($items);
 		}
 	}
 
-	public function toString($destroy=true){
-		$res = implode('',$this->items);
-		if($destroy) $this->destroy();
-	return $res;
+	public function toString($destroy = true) {
+		$res = implode('', $this->items);
+		if ($destroy) {
+			$this->destroy();
+		}
+
+		return $res;
 	}
 
-	public function show($destroy=true){
+	public function show($destroy = true) {
 		echo $this->toString($destroy);
 	}
 
-	public function destroy(){
+	public function destroy() {
 		$this->cleanItems();
 	}
 
-	public function cleanItems(){
+	public function cleanItems() {
 		$this->items = array();
 	}
 
-	public function itemsCount(){
+	public function itemsCount() {
 		return count($this->items);
 	}
 
-	public function addItem($value){
-		if(is_object($value)){
-			array_push($this->items,unpack_object($value));
+	public function addItem($value) {
+		if (is_object($value)) {
+			array_push($this->items, unpack_object($value));
 		}
-		else if(is_string($value)){
-			array_push($this->items, $this->sanitize($value));
-//				array_push($this->items,htmlspecialchars($value));
+		elseif (is_string($value)) {
+			array_push($this->items, $value);
 		}
-		else if(is_array($value)){
-			foreach($value as $item){
-				$this->addItem($item);			 // Attention, recursion !!!
+		elseif (is_array($value)) {
+			foreach ($value as $item) {
+				$this->addItem($item); // Attention, recursion !!!
 			}
 		}
-		else if(!is_null($value)){
-			array_push($this->items,unpack_object($value));
+		elseif (!is_null($value)) {
+			array_push($this->items, unpack_object($value));
 		}
 	}
-
-
-	/**
-	 * Sanitezes a string before outputting it to the browser.
-	 *
-	 * @param string $str
-	 * @return string
-	 */
-	protected function sanitize($value) {
-		return zbx_htmlstr($value);
-	}
 }
 
-function destroy_objects(){
-	if(isset($GLOBALS)) foreach($GLOBALS as $name => $value){
-		if(!is_object($GLOBALS[$name])) continue;
-		unset($GLOBALS[$name]);
-	}
-}
-
-function unpack_object(&$item){
+function unpack_object(&$item) {
 	$res = '';
 
-	if(is_object($item)){
+	if (is_object($item)) {
 		$res = $item->toString(false);
 	}
-	else if(is_array($item)){
-		foreach($item as $id => $dat)
+	else if (is_array($item)) {
+		foreach ($item as $id => $dat) {
 			$res .= unpack_object($item[$id]); // Attention, recursion !!!
+		}
 	}
-	else if(!is_null($item)){
+	else if (!is_null($item)) {
 		$res = strval($item);
 		unset($item);
 	}
-return $res;
-}
-
-function implode_objects($glue, &$pieces){
-	if( !is_array($pieces) )	return unpack_object($pieces);
-
-	foreach($pieces as $id => $piece)
-		$pieces[$id] = unpack_object($piece);
-
-return implode($glue, $pieces);
+	return $res;
 }
