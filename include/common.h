88ff6b393951dd3c6c2348dbae6d1dbe0a85acb7
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-03-20 09:12:45 +0000

    .......... [ZBXNEXT-826] updated release date (fixed typo)
    git-svn-id: svn://svn.zabbix.com/branches/1.8@26203 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -121,7 +121,7 @@
 #define OFF	0
 
 #define	APPLICATION_NAME	"Zabbix Agent"
-#define	ZABBIX_REVDATE		"20 Match 2012"
+#define	ZABBIX_REVDATE		"20 March 2012"
 #define	ZABBIX_VERSION		"1.8.11"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
