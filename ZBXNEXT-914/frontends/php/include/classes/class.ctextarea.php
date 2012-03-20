05b067932b0ea4b0523998407e925a868be799fb
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-03-20 12:27:15 +0000

    ..F....... [ZBXNEXT-914] merged from upstream and resolve conflicts
    git-svn-id: svn://svn.zabbix.com/branches/dev@26225 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-914/frontends/php/include/classes/class.ctextarea.php
+++ ZBXNEXT-914/frontends/php/include/classes/class.ctextarea.php
@@ -22,6 +22,13 @@
 
 class CTextArea extends CTag {
 
+	/**
+	 * The "&" symbol in the textarea should be encoded.
+	 *
+	 * @var int
+	 */
+	protected $encStrategy = self::ENC_ALL;
+
 	public function __construct($name = 'textarea', $value = '', $rows = ZBX_TEXTAREA_STANDARD_ROWS, $width = ZBX_TEXTAREA_STANDARD_WIDTH, $readonly = false) {
 		parent::__construct('textarea', 'yes');
 		$this->attr('class', 'input');
