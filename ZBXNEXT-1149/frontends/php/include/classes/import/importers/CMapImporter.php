e988478f644de760b55f679d55096888e0ecb274
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-03-20 14:47:11 +0000

    ..F....... [ZBXNEXT-1149] added license comment to new files, fixed import of map image elements
    git-svn-id: svn://svn.zabbix.com/branches/dev@26236 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1149/frontends/php/include/classes/import/importers/CMapImporter.php
+++ ZBXNEXT-1149/frontends/php/include/classes/import/importers/CMapImporter.php
@@ -1,4 +1,23 @@
 <?php
+/*
+** Zabbix
+** Copyright (C) 2000-2012 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
 
 class CMapImporter extends CImporter {
 
@@ -171,6 +190,10 @@ class CMapImporter extends CImporter {
 							throw new Exception(_s('Cannot find trigger "%1$s" used in map %2$s".',
 								$selement['element']['description'], $map['name']));
 						}
+						break;
+
+					case SYSMAP_ELEMENT_TYPE_IMAGE:
+						$selement['elementid'] = 0;
 						break;
 				}
 
