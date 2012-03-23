054f5ca06c566590e9620d81289316ecef6d53d3
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-03-23 10:56:09 +0000

    ..F....... [ZBXNEXT-1153] nested templates import
    git-svn-id: svn://svn.zabbix.com/branches/dev@26359 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1153/frontends/php/include/classes/import/CImportReferencer.php
+++ ZBXNEXT-1153/frontends/php/include/classes/import/CImportReferencer.php
@@ -25,6 +25,7 @@
  */
 class CImportReferencer {
 
+	protected $processedHosts = array();
 	protected $groups = array();
 	protected $templates = array();
 	protected $hosts = array();
@@ -43,6 +44,28 @@ class CImportReferencer {
 	protected $triggersRefs;
 	protected $iconMapsRefs;
 	protected $mapsRefs;
+
+
+	/**
+	 * Add host/template that was updated or created, i.e. all items, discovery rules, etc,
+	 * related to these host/template should be created or updated too.
+	 *
+	 * @param $host
+	 */
+	public function addProcessedHost($host) {
+		$this->processedHosts[$host] = $host;
+	}
+
+	/**
+	 * Checks if host/template was create or updated during current import.
+	 *
+	 * @param $host
+	 *
+	 * @return bool
+	 */
+	public function isProcessedHost($host) {
+		return isset($this->processedHosts[$host]);
+	}
 
 	/**
 	 * Get group id by name.
