1d698c6695b460db266af89b7c692021e50c2232
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-03-28 11:34:23 +0000

    .......... [ZBX-3991] branch created
    git-svn-id: svn://svn.zabbix.com/branches/dev@26451 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-3991/src/zabbix_java/tests/com/zabbix/gateway/IntegerValidatorTest.java
@@ -0,0 +1,50 @@
+/*
+** Zabbix
+** Copyright (C) 2000-2011 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+package com.zabbix.gateway;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class IntegerValidatorTest
+{
+	@Test
+	public void testCorrectValidation()
+	{
+		InputValidator validator = new IntegerValidator(3, 7);
+
+		assertFalse(validator.validate(Integer.valueOf(2)));
+		assertTrue(validator.validate(Integer.valueOf(3)));
+		assertTrue(validator.validate(Integer.valueOf(5)));
+		assertTrue(validator.validate(Integer.valueOf(7)));
+		assertFalse(validator.validate(Integer.valueOf(8)));
+	}
+
+	@Test
+	public void testMinimumInterval()
+	{
+		new IntegerValidator(5, 5);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidInterval()
+	{
+		new IntegerValidator(7, 3);
+	}
+}
