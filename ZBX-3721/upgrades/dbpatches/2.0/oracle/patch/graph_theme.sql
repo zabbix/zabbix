e07ef5b40a6181261cf8f878ccd12600f7f4c1a8
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-03-29 09:18:09 +0000

    ..F....... [ZBX-3721] added the "Dark orange" graph theme ..F....... [ZBX-3721] implemented graph theme name patches
    git-svn-id: svn://svn.zabbix.com/branches/dev@26458 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3721/upgrades/dbpatches/2.0/oracle/patch/graph_theme.sql
+++ ZBX-3721/upgrades/dbpatches/2.0/oracle/patch/graph_theme.sql
@@ -1,3 +1,6 @@
 ALTER TABLE graph_theme MODIFY graphthemeid DEFAULT NULL;
 ALTER TABLE graph_theme MODIFY noneworktimecolor DEFAULT 'CCCCCC';
 ALTER TABLE graph_theme RENAME COLUMN noneworktimecolor TO nonworktimecolor;
+
+UPDATE graph_theme SET theme = 'darkblue' WHERE theme = 'css_bb.css';
+UPDATE graph_theme SET theme = 'originalblue' WHERE theme = 'css_ob.css';
