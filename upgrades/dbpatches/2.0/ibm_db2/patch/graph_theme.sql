6cb97b3e0a7585e2220fc2bc7441e238ac14de2d
Pavels Jelisejevs <git-no-reply@zabbix.com>
2012-03-30 09:01:03 +0000

    ....I..... [ZBX-3721] fixed new id generation in the patches

--- upgrades/dbpatches/2.0/ibm_db2/patch/graph_theme.sql
+++ upgrades/dbpatches/2.0/ibm_db2/patch/graph_theme.sql
@@ -14,6 +14,8 @@ UPDATE graph_theme SET theme = 'darkblue' WHERE theme = 'css_bb.css'
 /
 UPDATE graph_theme SET theme = 'originalblue' WHERE theme = 'css_ob.css'
 /
-INSERT INTO graph_theme (description, theme, backgroundcolor, graphcolor, graphbordercolor, gridcolor, maingridcolor, gridbordercolor, textcolor, highlightcolor, leftpercentilecolor, rightpercentilecolor, nonworktimecolor, gridview, legendview)
-VALUES ('Dark orange', 'darkorange', '333333', '0A0A0A', '888888', '222222', '4F4F4F', 'EFEFEF', 'DFDFDF', 'FF5500', 'FF5500', 'FF1111', '1F1F1F', 1, 1);
+-- Insert new graph theme
+INSERT INTO graph_theme (graphthemeid, description, theme, backgroundcolor, graphcolor, graphbordercolor, gridcolor, maingridcolor, gridbordercolor, textcolor, highlightcolor, leftpercentilecolor, rightpercentilecolor, nonworktimecolor, gridview, legendview) VALUES ((SELECT MAX(graphthemeid) + 1 FROM graph_theme), 'Dark orange', 'darkorange', '333333', '0A0A0A', '888888', '222222', '4F4F4F', 'EFEFEF', 'DFDFDF', 'FF5500', 'FF5500', 'FF1111', '1F1F1F', 1, 1)
+/
+DELETE FROM ids WHERE table_name = 'graph_theme'
 /
