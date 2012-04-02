334fb1830191b051f5afbbdd8be1fb0aae106184
tomtom <tomtom@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-04-02 12:57:21 +0000

    ..F....... [ZBX-4806] fixed small mistakes
    git-svn-id: svn://svn.zabbix.com/branches/dev@26529 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4806/frontends/php/include/views/js/configuration.item.edit.js.php
+++ ZBX-4806/frontends/php/include/views/js/configuration.item.edit.js.php
@@ -143,11 +143,11 @@
 
 	jQuery(document).ready(function() {
 		<?php //sdii ($this->data);
-		if (0&&!empty($this->data['valueTypeVisibility'])) { ?>
+		if (!empty($this->data['valueTypeVisibility'])) { ?>
 			var valueTypeSwitcher = new CViewSwitcher('value_type', 'change',
 				<?php echo zbx_jsvalue($this->data['valueTypeVisibility'], true); ?>);
 		<?php }
-		if (0&&!empty($this->data['authTypeVisibility'])) { ?>
+		if (!empty($this->data['authTypeVisibility'])) { ?>
 			var authTypeSwitcher = new CViewSwitcher('authtype', 'change',
 				<?php echo zbx_jsvalue($this->data['authTypeVisibility'], true); ?>);
 		<?php }
@@ -155,11 +155,11 @@
 			var typeSwitcher = new CViewSwitcher('type', 'change',
 				<?php echo zbx_jsvalue($this->data['typeVisibility'], true); ?>);
 		<?php }
-		if (0&&!empty($this->data['securityLevelVisibility'])) { ?>
+		if (!empty($this->data['securityLevelVisibility'])) { ?>
 			var securityLevelSwitcher = new CViewSwitcher('snmpv3_securitylevel', 'change',
 				<?php echo zbx_jsvalue($this->data['securityLevelVisibility'], true); ?>);
 		<?php }
-		if (0&&!empty($this->data['dataTypeVisibility'])) { ?>
+		if (!empty($this->data['dataTypeVisibility'])) { ?>
 			var dataTypeSwitcher = new CViewSwitcher('data_type', 'change',
 				<?php echo zbx_jsvalue($this->data['dataTypeVisibility'], true); ?>);
 		<?php } ?>
