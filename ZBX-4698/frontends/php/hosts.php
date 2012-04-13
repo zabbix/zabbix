dd92aa8800f5ab9074d4d63f6e11386712ffe982
tomtom <tomtom@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-04-13 08:11:27 +0000

    ..F....... [ZBX-4698] fixed selected host group forgotten if "remember selected" is disabled
    git-svn-id: svn://svn.zabbix.com/branches/dev@26804 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4698/frontends/php/hosts.php
+++ ZBX-4698/frontends/php/hosts.php
@@ -583,7 +583,7 @@ $options = array(
 		'real_hosts' => 1,
 		'editable' => true
 	),
-	'groupid' => get_request('groupid', null),
+	'groupid' => get_request('groupid', null)
 );
 $pageFilter = new CPageFilter($options);
 
@@ -636,9 +636,7 @@ else {
 		array(bold(_('Port').': '), new CTextBox('filter_port', $_REQUEST['filter_port'], 20))
 	));
 
-	$filter = new CButton('filter', _('Filter'),
-		"javascript: create_var('zbx_filter', 'groupid',".$_REQUEST['groupid'].");
-		create_var('zbx_filter', 'filter_set', '1', true);");
+	$filter = new CButton('filter', _('Filter'), "javascript: create_var('zbx_filter', 'filter_set', '1', true);");
 	$filter->useJQueryStyle('main');
 
 	$reset = new CButton('reset', _('Reset'), "javascript: clearAllForm('zbx_filter');");
