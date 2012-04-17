0f38f539931742c22c679c40808e3a88752940d8
Pavels Jelisejevs <git-no-reply@zabbix.com>
2012-04-17 07:23:40 +0000

    ....I..... [ZBX-3721] fixed typo

--- upgrades/dbpatches/2.0/mysql/patch/graph_theme.sql
+++ upgrades/dbpatches/2.0/mysql/patch/graph_theme.sql
@@ -5,7 +5,7 @@ UPDATE graph_theme SET theme = 'darkblue' WHERE theme = 'css_bb.css';
 UPDATE graph_theme SET theme = 'originalblue' WHERE theme = 'css_ob.css';
 
 -- Insert new graph theme
-SET @grapththemeid = (SELECT MAX(graphthemeid) FROM graph_theme);
+SET @graphthemeid = (SELECT MAX(graphthemeid) FROM graph_theme);
 INSERT INTO graph_theme (graphthemeid, description, theme, backgroundcolor, graphcolor, graphbordercolor, gridcolor, maingridcolor, gridbordercolor, textcolor, highlightcolor, leftpercentilecolor, rightpercentilecolor, nonworktimecolor, gridview, legendview)
-VALUES (@grapththemeid + 1, 'Dark orange', 'darkorange', '333333', '0A0A0A', '888888', '222222', '4F4F4F', 'EFEFEF', 'DFDFDF', 'FF5500', 'FF5500', 'FF1111', '1F1F1F', 1, 1);
+VALUES (@graphthemeid + 1, 'Dark orange', 'darkorange', '333333', '0A0A0A', '888888', '222222', '4F4F4F', 'EFEFEF', 'DFDFDF', 'FF5500', 'FF5500', 'FF1111', '1F1F1F', 1, 1);
 DELETE FROM ids WHERE table_name = 'graph_theme';
