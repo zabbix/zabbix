212c4a05a597471a97d76f23d68ff308e69a54ea
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-04-19 15:20:24 +0000

    .......... [ZBXNEXT-826] updated release date
    git-svn-id: svn://svn.zabbix.com/trunk@26979 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -121,7 +121,7 @@
 #define OFF	0
 
 #define	APPLICATION_NAME	"Zabbix Agent"
-#define	ZABBIX_REVDATE		"22 March 2012"
+#define	ZABBIX_REVDATE		"19 April 2012"
 #define	ZABBIX_VERSION		"2.0.0rc3"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
