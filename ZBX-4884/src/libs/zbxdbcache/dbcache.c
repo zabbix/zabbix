a13e534af1878e862fbaa748e74527bc33caad94
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-04-24 15:44:55 +0000

    ........S. [ZBX-4884] coding style fixes
    git-svn-id: svn://svn.zabbix.com/branches/dev@27083 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4884/src/libs/zbxdbcache/dbcache.c
+++ ZBX-4884/src/libs/zbxdbcache/dbcache.c
@@ -997,7 +997,7 @@ static void	DCadd_update_item_sql(int *sql_offset, DB_ITEM *item, ZBX_DC_HISTORY
 			switch (item->delta)
 			{
 				case ITEM_STORE_AS_IS:
-					if (1 != item->prevorgvalue_null)
+					if (0 == item->prevorgvalue_null)
 					{
 						zbx_snprintf_alloc(&sql, &sql_allocated, sql_offset, 19,
 								",prevorgvalue=null");
@@ -1010,6 +1010,7 @@ static void	DCadd_update_item_sql(int *sql_offset, DB_ITEM *item, ZBX_DC_HISTORY
 					{
 						h->status = ITEM_STATUS_NOTSUPPORTED;
 						h->value_null = 1;
+						goto notsupported;
 					}
 					break;
 				case ITEM_STORE_SPEED_PER_SECOND:
@@ -1024,17 +1025,16 @@ static void	DCadd_update_item_sql(int *sql_offset, DB_ITEM *item, ZBX_DC_HISTORY
 						{
 							h->status = ITEM_STATUS_NOTSUPPORTED;
 							h->value_null = 1;
+							goto notsupported;
 						}
 					}
 					else
 						h->value_null = 1;
 
-					if (ITEM_STATUS_NOTSUPPORTED != h->status)
-					{
-						zbx_snprintf_alloc(&sql, &sql_allocated, sql_offset, 512,
-								",prevorgvalue='" ZBX_FS_DBL "'", h->value_orig.dbl);
-						item->prevorgvalue_null = 0;
-					}
+					zbx_snprintf_alloc(&sql, &sql_allocated, sql_offset, 512,
+							",prevorgvalue='" ZBX_FS_DBL "'", h->value_orig.dbl);
+					item->prevorgvalue_null = 0;
+
 					break;
 				case ITEM_STORE_SIMPLE_CHANGE:
 					if (0 == item->prevorgvalue_null && item->prevorgvalue.dbl <= h->value_orig.dbl)
@@ -1046,21 +1046,22 @@ static void	DCadd_update_item_sql(int *sql_offset, DB_ITEM *item, ZBX_DC_HISTORY
 						{
 							h->status = ITEM_STATUS_NOTSUPPORTED;
 							h->value_null = 1;
+							goto notsupported;
 						}
 					}
 					else
 						h->value_null = 1;
 
-					if (ITEM_STATUS_NOTSUPPORTED != h->status)
-					{
-						zbx_snprintf_alloc(&sql, &sql_allocated, sql_offset, 512,
-								",prevorgvalue='" ZBX_FS_DBL "'", h->value_orig.dbl);
-						item->prevorgvalue_null = 0;
-					}
+					zbx_snprintf_alloc(&sql, &sql_allocated, sql_offset, 512,
+							",prevorgvalue='" ZBX_FS_DBL "'", h->value_orig.dbl);
+					item->prevorgvalue_null = 0;
+
 					break;
+				default:
+					THIS_SHOULD_NEVER_HAPPEN;
 			}
 
-			if (1 != h->value_null)
+			if (0 == h->value_null)
 			{
 				zbx_snprintf_alloc(&sql, &sql_allocated, sql_offset, 512,
 						",prevvalue=lastvalue,lastvalue='" ZBX_FS_DBL "'",
@@ -1118,9 +1119,11 @@ static void	DCadd_update_item_sql(int *sql_offset, DB_ITEM *item, ZBX_DC_HISTORY
 							",prevorgvalue='" ZBX_FS_UI64 "'", h->value_orig.ui64);
 					item->prevorgvalue_null = 0;
 					break;
+				default:
+					THIS_SHOULD_NEVER_HAPPEN;
 			}
 
-			if (1 != h->value_null)
+			if (0 == h->value_null)
 			{
 				zbx_snprintf_alloc(&sql, &sql_allocated, sql_offset, 64,
 						",prevvalue=lastvalue,lastvalue='" ZBX_FS_UI64 "'",
@@ -1138,6 +1141,8 @@ static void	DCadd_update_item_sql(int *sql_offset, DB_ITEM *item, ZBX_DC_HISTORY
 					value_esc);
 			zbx_free(value_esc);
 			break;
+		default:
+			THIS_SHOULD_NEVER_HAPPEN;
 	}
 notsupported:
 	if (ITEM_STATUS_NOTSUPPORTED == h->status)
