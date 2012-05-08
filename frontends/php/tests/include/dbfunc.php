06f0fce5631119dfad1ede5602ae85d1bd08a1fe
igor <igor@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2012-05-08 15:10:41 +0000

    .......... [ZBXNEXT-686] Fixed all unit tests by adding class "gettextwrapper.inc.php" before calling defines.inc.php
    git-svn-id: svn://svn.zabbix.com/trunk@27355 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/tests/include/dbfunc.php
+++ frontends/php/tests/include/dbfunc.php
@@ -20,6 +20,7 @@
 ?>
 <?php
 
+require_once dirname(__FILE__).'/../../include/gettextwrapper.inc.php';
 require_once dirname(__FILE__).'/../../include/defines.inc.php';
 require_once dirname(__FILE__).'/../../conf/zabbix.conf.php';
 require_once dirname(__FILE__).'/../../include/func.inc.php';
