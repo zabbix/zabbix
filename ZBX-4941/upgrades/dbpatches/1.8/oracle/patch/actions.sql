27f0bcd6bf135fe05f5fc60644b4f365898ced81
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-05-09 13:39:36 +0000

    .......... [ZBX-4941] remove development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@27392 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4941/upgrades/dbpatches/1.8/oracle/patch/actions.sql
+++
@@ -1,8 +0,0 @@
-CREATE INDEX actions_1 on actions (eventsource,status);
-
-alter table actions modify name            nvarchar2(255)          DEFAULT '';
-alter table actions modify def_shortdata           nvarchar2(255)          DEFAULT '';
-alter table actions modify def_longdata            nvarchar2(2048)         DEFAULT '';
-alter table actions modify r_shortdata             nvarchar2(255)          DEFAULT '';
-alter table actions modify r_longdata              nvarchar2(2048)         DEFAULT '';
-
