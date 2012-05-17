7701c4efd2e94b47989a1943525189f583433bec
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-05-17 07:41:20 +0000

    .......... [ZBX-5005] branch created
    git-svn-id: svn://svn.zabbix.com/branches/dev@27580 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-5005/src/zabbix_server/dbconfig/dbconfig.c
@@ -0,0 +1,73 @@
+/*
+** Zabbix
+** Copyright (C) 2000-2011 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+
+#include "db.h"
+#include "log.h"
+#include "daemon.h"
+#include "zbxself.h"
+
+#include "dbconfig.h"
+#include "dbcache.h"
+
+extern int		CONFIG_CONFSYNCER_FREQUENCY;
+extern unsigned char	process_type;
+
+/******************************************************************************
+ *                                                                            *
+ * Function: main_dbconfig_loop                                               *
+ *                                                                            *
+ * Purpose: periodically synchronises database data with memory cache         *
+ *                                                                            *
+ * Parameters:                                                                *
+ *                                                                            *
+ * Return value:                                                              *
+ *                                                                            *
+ * Author: Alexander Vladishev                                                *
+ *                                                                            *
+ * Comments: never returns                                                    *
+ *                                                                            *
+ ******************************************************************************/
+void	main_dbconfig_loop()
+{
+	double	sec;
+
+	zabbix_log(LOG_LEVEL_DEBUG, "In main_dbconfig_loop()");
+
+	zbx_setproctitle("%s [connecting to the database]", get_process_type_string(process_type));
+
+	DBconnect(ZBX_DB_CONNECT_NORMAL);
+
+	for (;;)
+	{
+		zbx_setproctitle("%s [syncing configuration]", get_process_type_string(process_type));
+
+		zabbix_log(LOG_LEVEL_DEBUG, "Syncing ...");
+
+		sec = zbx_time();
+		DCsync_configuration();
+		sec = zbx_time() - sec;
+
+		zabbix_log(LOG_LEVEL_DEBUG, "%s spent " ZBX_FS_DBL " second while processing configuration data",
+				get_process_type_string(process_type), sec);
+
+		zbx_sleep_loop(CONFIG_CONFSYNCER_FREQUENCY);
+	}
+}
