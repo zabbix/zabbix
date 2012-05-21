6275eafe5fc97e11cafc5928117f8b13c72e99d3
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2012-05-21 14:02:42 +0000

    .......... [ZBXNEXT-826] updated release date

--- include/common.h
+++ include/common.h
@@ -121,7 +121,7 @@
 #define OFF	0
 
 #define	APPLICATION_NAME	"Zabbix Agent"
-#define	ZABBIX_REVDATE		"18 May 2012"
+#define	ZABBIX_REVDATE		"21 May 2012"
 #define	ZABBIX_VERSION		"2.0.0"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
