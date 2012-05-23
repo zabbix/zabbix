55f0e423fe4854148ca22e1d11d543fb99b4ada3
Pavels Jelisejevs <git-no-reply@zabbix.com>
2012-05-23 13:15:47 +0000

    ..F....... [ZBX-4864] fixed graph Y axis label alignment

--- frontends/php/include/func.inc.php
+++ frontends/php/include/func.inc.php
@@ -595,7 +595,7 @@ function convert_units($value, $units, $convert = ITEM_CONVERT_WITH_UNITS) {
 	$value = preg_replace('/^([\-0-9]+)(\.)([0-9]*)[0]+$/U','$1$2$3', round($valUnit['value'], ZBX_UNITS_ROUNDOFF_UPPER_LIMIT));
 	$value = rtrim($value, '.');
 
-	return sprintf('%s %s%s', $value, $desc, $units);
+	return rtrim(sprintf('%s %s%s', $value, $desc, $units));
 }
 
 /************* ZBX MISC *************/
