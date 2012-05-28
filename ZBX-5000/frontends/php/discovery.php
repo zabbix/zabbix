c98b6c898ff0a086f9402a6669aa2fcd9bdcbedc
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-05-28 11:04:34 +0000

    ..F....... [ZBX-5000] minor code refactoring
    git-svn-id: svn://svn.zabbix.com/branches/dev@27870 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5000/frontends/php/discovery.php
+++ ZBX-5000/frontends/php/discovery.php
@@ -74,23 +74,22 @@ if (!empty($data['drules_all'])) {
 	order_result($data['drules_all'], 'name');
 }
 
-// discovery rules
-$options = array(
-	'filter' => array('status' => DRULE_STATUS_ACTIVE),
-	'selectDHosts' => API_OUTPUT_EXTEND,
-	'output' => API_OUTPUT_EXTEND
-);
-
 $config = select_config();
 
-if (!empty($data['druleid'])) {
-	$options['druleids'] = $data['druleid']; // set selected discovery rule id
-}
-elseif ($config['dropdown_first_entry'] == ZBX_DROPDOWN_FIRST_NONE) {
-	$options['druleids'] = array(); // set empty array to select no discovery rules
-}
+// do not retrieve data when select first none and discovery rule not selected
+if (!($config['dropdown_first_entry'] == ZBX_DROPDOWN_FIRST_NONE && empty($data['druleid']))) {
 
-if (!($config['dropdown_first_entry'] == ZBX_DROPDOWN_FIRST_NONE && empty($data['druleid']))) { // do not retrieve data when select first none and discovery rule not selected
+	// discovery rules
+	$options = array(
+		'filter' => array('status' => DRULE_STATUS_ACTIVE),
+		'selectDHosts' => API_OUTPUT_EXTEND,
+		'output' => API_OUTPUT_EXTEND
+	);
+
+	// set selected discovery rule id
+	if (!empty($data['druleid'])) {
+		$options['druleids'] = $data['druleid'];
+	}
 
 	$data['drules'] = API::DRule()->get($options);
 	if (!empty($data['drules'])) {
