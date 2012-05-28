95fbc47c0d0ddd7e3e7a151e917ca0536a9c8fd7
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-05-28 09:50:11 +0000

    .......... [ZBX-5032] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@27867 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5032/upgrades/dbpatches/2.0/postgresql/patch/applications.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE ONLY applications ALTER applicationid DROP DEFAULT,
-			      ALTER hostid DROP DEFAULT,
-			      ALTER templateid DROP DEFAULT,
-			      ALTER templateid DROP NOT NULL;
-DELETE FROM applications WHERE NOT EXISTS (SELECT 1 FROM hosts WHERE hosts.hostid=applications.hostid);
-UPDATE applications SET templateid=NULL WHERE templateid=0;
-UPDATE applications SET templateid=NULL WHERE templateid IS NOT NULL AND NOT EXISTS (SELECT 1 FROM applications a WHERE a.applicationid=applications.templateid);
-ALTER TABLE ONLY applications ADD CONSTRAINT c_applications_1 FOREIGN KEY (hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
-ALTER TABLE ONLY applications ADD CONSTRAINT c_applications_2 FOREIGN KEY (templateid) REFERENCES applications (applicationid) ON DELETE CASCADE;
