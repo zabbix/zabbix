c0f9b20390f280ab95134905097c75b936ac6e44
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-05-31 08:08:09 +0000

    ..F....... [ZBX-5080] fixed incorrect JavaScript parameter encoding in the map editor
    git-svn-id: svn://svn.zabbix.com/branches/2.0@27963 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/sysmap.php
+++ frontends/php/sysmap.php
@@ -257,9 +257,11 @@ while ($row = DBfetch($result)) {
 }
 order_result($iconList, 'name');
 
-zbx_add_post_js('ZABBIX.apps.map.run("sysmap_cnt", '.zbx_jsvalue(array(
-			'sysmap' => $sysmap, 'iconList' => $iconList, 'defaultAutoIconId' => $defaultAutoIconId), true).');'
-);
+zbx_add_post_js('ZABBIX.apps.map.run("sysmap_cnt", '.CJs::encodeJson(array(
+	'sysmap' => $sysmap,
+	'iconList' => $iconList,
+	'defaultAutoIconId' => $defaultAutoIconId)
+).');');
 
 require_once dirname(__FILE__).'/include/page_footer.php';
 ?>
