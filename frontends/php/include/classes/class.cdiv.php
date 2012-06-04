da266cfb9ae67ffb0025003a2756369903af28c7
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-04 07:09:36 +0000

    A.F....... [ZBX-5074] redesign configuration of action
    git-svn-id: svn://svn.zabbix.com/branches/2.0@28020 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/classes/class.cdiv.php
+++ frontends/php/include/classes/class.cdiv.php
@@ -17,8 +17,7 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
-?>
-<?php
+
 
 class CDiv extends CTag {
 
@@ -48,4 +47,3 @@ class CDiv extends CTag {
 		return $this;
 	}
 }
-?>
--- frontends/php/include/classes/class.cnumericbox.php
+++ frontends/php/include/classes/class.cnumericbox.php
@@ -17,8 +17,7 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
-?>
-<?php
+
 
 class CNumericBox extends CInput {
 
@@ -31,4 +30,3 @@ class CNumericBox extends CInput {
 		$this->addAction('onchange', 'validateNumericBox(this, '.($allowempty ? 'true' : 'false').', '.($allownegative ? 'true' : 'false').');');
 	}
 }
-?>
