11bf2636f628bcabd81378ebee0dacce389d265a
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-04 07:13:10 +0000

    A.F....... [ZBX-5074] redesign configuration of action
    git-svn-id: svn://svn.zabbix.com/trunk@28021 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/classes/class.cspan.php
+++ frontends/php/include/classes/class.cspan.php
@@ -17,8 +17,7 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
-?>
-<?php
+
 
 class CSpan extends CTag {
 
@@ -34,4 +33,3 @@ class CSpan extends CTag {
 		$this->tag_body_end = '';
 	}
 }
-?>
