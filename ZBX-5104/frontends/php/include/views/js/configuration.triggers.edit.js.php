37cf7d5a11bdb495fb715b26905f30c87492b4f6
tomtom <tomtom@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-05 08:21:42 +0000

    ..F....... [ZBX-5104] minor code refactoring
    git-svn-id: svn://svn.zabbix.com/branches/dev@28055 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5104/frontends/php/include/views/js/configuration.triggers.edit.js.php
+++ ZBX-5104/frontends/php/include/views/js/configuration.triggers.edit.js.php
@@ -1,28 +1,26 @@
 <script type="text/javascript">
 	jQuery(function($) {
 		$('#severity_0, #severity_1, #severity_2, #severity_3, #severity_4, #severity_5').change(function() {
-			var label = $('#severity_label_' + $(this).val());
-
 			// remove classes from all labels
 			$('div.control-severity label').each(function(i, obj) {
 				obj = $(obj);
 				obj.removeClass(obj.data('severityStyle'));
 			});
 
+			var label = $('#severity_label_' + $(this).val());
 			label.addClass(label.data('severityStyle'));
 		});
 
 		$('#severity_label_0, #severity_label_1, #severity_label_2, #severity_label_3, #severity_label_4, #severity_label_5').mouseenter(function() {
 			var obj = $(this);
-
-			$('#severity_label_' + obj.data('severity')).addClass(obj.data('severityStyle'));
+			obj.addClass(obj.data('severityStyle'));
 		});
 
 		$('#severity_label_0, #severity_label_1, #severity_label_2, #severity_label_3, #severity_label_4, #severity_label_5').mouseleave(function() {
 			var obj = $(this);
 
 			if (!$('#' + obj.attr('for')).prop('checked')) {
-				$('#severity_label_' + obj.data('severity')).removeClass(obj.data('severityStyle'));
+				obj.removeClass(obj.data('severityStyle'));
 			}
 		});
 
@@ -31,10 +29,8 @@
 
 
 		// create jQuery buttonset object when VisibilityBox is switched on
-		jQuery('#visible_priority').click(function() {
-			if (!jQuery('#priority_div').hasClass('ui-buttonset')) {
-				jQuery('#priority_div').buttonset();
-			}
+		jQuery('#visible_priority').one('click', function() {
+			jQuery('#priority_div').buttonset();
 		});
 	});
 
