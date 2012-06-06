2f04e990beb3f45a6be8f27b11b6a5d065ab4bbc
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-06 09:06:44 +0000

    ..F....... [ZBXNEXT-829] fixed session cookie path
    git-svn-id: svn://svn.zabbix.com/branches/dev@28098 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-829/frontends/php/include/classes/core/CSession.php
+++ ZBXNEXT-829/frontends/php/include/classes/core/CSession.php
@@ -2,8 +2,18 @@
 
 class CSession implements ArrayAccess {
 
+	/**
+	 * Initialize session.
+	 * Set cookie path to path to current URI without file.
+	 *
+	 * @throw Exception if cannot start session
+	 */
 	public function __construct() {
-		session_set_cookie_params(0, parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+		$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+		// remove file name from path
+		$path = substr($path, 0, strrpos($path, '/'));
+
+		session_set_cookie_params(0, $path);
 		if (!session_start()) {
 			throw new Exception('Cannot start session.');
 		}
