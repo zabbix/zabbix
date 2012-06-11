ade339f6a8808f64caad93bc1f8d3237812a9811
vedmak <vedmak@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2012-06-11 08:05:45 +0000

    ..F....... [ZBX-5143] fixed regular expression delete action ignores cancelation in confirmation dialog
    git-svn-id: svn://svn.zabbix.com/trunk@28182 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/views/administration.mediatypes.list.php
+++ frontends/php/include/views/administration.mediatypes.list.php
@@ -118,7 +118,7 @@ $goOption = new CComboItem('delete', _('Delete selected'));
 $goOption->setAttribute('confirm', _('Delete selected media types?'));
 $goComboBox->addItem($goOption);
 
-$goButton = new CSubmit('goButton', _('Go'));
+$goButton = new CSubmit('goButton', _('Go').' (0)');
 $goButton->setAttribute('id', 'goButton');
 zbx_add_post_js('chkbxRange.pageGoName = "mediatypeids";');
 
