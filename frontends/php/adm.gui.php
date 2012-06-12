b967291f424e2f27627c9e0c41312672ac8d9cc7
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-12 14:06:12 +0000

    ..F....... [ZBXNEXT-829] improved zabbix server availability check layout, cache check status to php session
    git-svn-id: svn://svn.zabbix.com/branches/2.0@28205 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/adm.gui.php
+++ frontends/php/adm.gui.php
@@ -29,18 +29,17 @@ require_once dirname(__FILE__).'/include/page_header.php';
 
 // VAR	TYPE	OPTIONAL	FLAGS	VALIDATION	EXCEPTION
 $fields = array(
-	'default_theme'           => array(T_ZBX_STR, O_OPT, null,        NOT_EMPTY,                 'isset({save})'),
-	'event_ack_enable'        => array(T_ZBX_INT, O_OPT, null,        IN('1'),                   null),
-	'event_expire'            => array(T_ZBX_INT, O_OPT, null,        BETWEEN(1, 99999),         'isset({save})', _('Show events not older than (in days)')),
-	'event_show_max'          => array(T_ZBX_INT, O_OPT, null,        BETWEEN(1, 99999),         'isset({save})', _('Max count of events per trigger to show')),
-	'dropdown_first_entry'    => array(T_ZBX_INT, O_OPT, null,        IN('0,1,2'),               'isset({save})'),
-	'dropdown_first_remember' => array(T_ZBX_INT, O_OPT, null,        IN('1'),                   null),
-	'max_in_table'            => array(T_ZBX_INT, O_OPT, null,        BETWEEN(1, 99999),         'isset({save})', _('Max count of elements to show inside table cell')),
-	'search_limit'            => array(T_ZBX_INT, O_OPT, null,        BETWEEN(1, 999999),        'isset({save})', _('Search/Filter elements limit')),
-	'server_check_interval'   => array(T_ZBX_INT, O_OPT, null,        BETWEEN(30, SEC_PER_HOUR), null, _('Zabbix server activity check interval')),
-	'server_check_enabled'    => array(T_ZBX_INT, O_OPT, null,        null,                      null),
-	'save'                    => array(T_ZBX_STR, O_OPT, P_SYS|P_ACT, null,                      null),
-	'form_refresh'            => array(T_ZBX_INT, O_OPT, null,        null,                      null)
+	'default_theme'           => array(T_ZBX_STR, O_OPT, null,        NOT_EMPTY,          'isset({save})'),
+	'event_ack_enable'        => array(T_ZBX_INT, O_OPT, null,        IN('1'),            null),
+	'event_expire'            => array(T_ZBX_INT, O_OPT, null,        BETWEEN(1, 99999),  'isset({save})', _('Show events not older than (in days)')),
+	'event_show_max'          => array(T_ZBX_INT, O_OPT, null,        BETWEEN(1, 99999),  'isset({save})', _('Max count of events per trigger to show')),
+	'dropdown_first_entry'    => array(T_ZBX_INT, O_OPT, null,        IN('0,1,2'),        'isset({save})'),
+	'dropdown_first_remember' => array(T_ZBX_INT, O_OPT, null,        IN('1'),            null),
+	'max_in_table'            => array(T_ZBX_INT, O_OPT, null,        BETWEEN(1, 99999),  'isset({save})', _('Max count of elements to show inside table cell')),
+	'search_limit'            => array(T_ZBX_INT, O_OPT, null,        BETWEEN(1, 999999), 'isset({save})', _('Search/Filter elements limit')),
+	'server_check_interval'   => array(T_ZBX_INT, O_OPT, null,        null,               null, _('Zabbix server activity check interval')),
+	'save'                    => array(T_ZBX_STR, O_OPT, P_SYS|P_ACT, null,               null),
+	'form_refresh'            => array(T_ZBX_INT, O_OPT, null,        null,               null)
 );
 check_fields($fields);
 
@@ -116,15 +115,10 @@ if ($data['form_refresh']) {
 	$data['config']['event_expire'] = get_request('event_expire');
 	$data['config']['event_show_max'] = get_request('event_show_max');
 	$data['config']['server_check_enabled'] = get_request('server_check_enabled');
-	$data['config']['server_check_interval'] = get_request('server_check_interval', 60);
+	$data['config']['server_check_interval'] = get_request('server_check_interval', 0);
 }
 else {
 	$data['config'] = select_config(false);
-	$data['config']['server_check_enabled'] = (bool) $data['config']['server_check_interval'];
-
-	if (!$data['config']['server_check_interval']) {
-		$data['config']['server_check_interval'] = 60;
-	}
 }
 
 $guiForm = new CView('administration.general.gui.edit', $data);
--- frontends/php/js/servercheck.js
+++ frontends/php/js/servercheck.js
@@ -1,8 +1,13 @@
-var checkServerStatus = (function ($) {
+jQuery(function($) {
 	"use strict";
 
+	/**
+	 * Object that sends ajax request for server status and show/hide warning messages.
+	 *
+	 * @type {Object}
+	 */
 	var checker = {
-		timeout: 0,
+		timeout: 10000, // 10 seconds
 		warning: false,
 
 		/**
@@ -31,29 +36,56 @@ var checkServerStatus = (function ($) {
 
 		showWarning: function(message) {
 			if (!this.warning) {
-				$('#message-global').text(message).addClass('warning-global');
+				$('#message-global').text(message);
+				$('#message-global-wrap').fadeIn(100);
 				this.warning = true;
 			}
 		},
 
 		hideWarning: function() {
 			if (this.warning) {
-				$('#message-global').text('').removeClass('warning-global');
+				$('#message-global-wrap').fadeOut(100);
 				this.warning = false;
 			}
 		}
 	};
 
+	// looping function that check for server status every 10 seconds
 	function checkStatus(nocache) {
 		checker.check(nocache);
 
 		window.setTimeout(checkStatus, checker.timeout);
 	}
 
-	return function(timeout) {
-		checker.timeout = timeout * 1000;
-
+	// start server status checks with 5 sec dealy after page is loaded
+	window.setTimeout(function() {
 		checkStatus(true);
-	}
-}(jQuery));
+	}, 5000);
+
+
+	// event that hide warning message when mouse hover it
+	$('#message-global-wrap').on('mouseenter', function() {
+		var obj = $(this),
+			offset = obj.offset(),
+			x1 = offset.left,
+			x2 = x1 + obj.outerWidth(),
+			y1 = offset.top,
+			y2 = y1 + obj.outerHeight();
+
+		obj.fadeOut(100);
+
+		$(document).on('mousemove.messagehide', function(e) {
+			if (e.pageX < x1 || e.pageX > x2 || e.pageY < y1 || e.pageY > y2) {
+				obj.fadeIn(100);
+				$(document).off('mousemove.messagehide');
+				$(document).off('mouseleave.messagehide');
+			}
+		});
+		$(document).on('mouseleave.messagehide', function() {
+			obj.fadeIn(100);
+			$(document).off('mouseleave.messagehide');
+			$(document).off('mousemove.messagehide');
+		});
+	});
+});
 
