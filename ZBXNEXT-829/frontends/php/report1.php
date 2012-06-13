30a7a983268775e6ca9036ca3f7cceeafe92bbc8
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-13 09:18:25 +0000

    .......... [ZBXNEXT-829] branch created
    git-svn-id: svn://svn.zabbix.com/branches/dev@28219 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-829/frontends/php/report1.php
@@ -0,0 +1,36 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2011 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+require_once dirname(__FILE__).'/include/config.inc.php';
+require_once dirname(__FILE__).'/include/blocks.inc.php';
+
+$page['title'] = _('Status of Zabbix');
+$page['file'] = 'report1.php';
+$page['hist_arg'] = array();
+
+require_once dirname(__FILE__).'/include/page_header.php';
+
+$reportWidget = new CWidget();
+$reportWidget->addPageHeader(_('STATUS OF ZABBIX'));
+$reportWidget->addItem(make_status_of_zbx());
+$reportWidget->show();
+
+require_once dirname(__FILE__).'/include/page_footer.php';
---
+++ ZBXNEXT-829/src/zabbix_agent/eventlog.h
@@ -0,0 +1,31 @@
+/*
+** Zabbix
+** Copyright (C) 2000-2011 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#ifndef ZABBIX_EVENTLOG_H
+#define ZABBIX_EVENTLOG_H
+
+#ifndef _WINDOWS
+#	error "This module is only available for Windows OS"
+#endif
+
+int	process_eventlog(const char *source, zbx_uint64_t *lastlogsize, unsigned long *out_timestamp,
+		char **out_source, unsigned short *out_severity, char **out_message, unsigned long *out_eventid,
+		unsigned char skip_old_data);
+
+#endif /* ZABBIX_EVENTLOG_H */
