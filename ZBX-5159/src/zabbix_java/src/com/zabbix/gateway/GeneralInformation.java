e33fa677d4b9120f529d7ffea32573fc4bdcbcb6
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-15 08:34:22 +0000

    .......... [ZBX-5159] sync 2.0
    git-svn-id: svn://svn.zabbix.com/branches/dev@28283 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5159/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ ZBX-5159/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,7 +22,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	public static final String REVISION_DATE = "21 May 2012";
+	public static final String REVISION_DATE = "14 June 2012";
 	public static final String REVISION = "{ZABBIX_REVISION}";
 	public static final String VERSION = "2.0.1rc1";
 
