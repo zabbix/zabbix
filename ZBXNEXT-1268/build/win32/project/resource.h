a323d81e683d86c0796df2fc77a312ce97e212cc
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-22 07:25:56 +0000

    ...GI..... [ZBXNEXT-1268] add version-information resource to Windows binaries
    Windows binaries can have version-information resource. The resource
    contains information about the file as its version, description and so
    on. This commit changes Windows binaries compilation procedure to
    include that information.
    
    Also in this commit the 32-bit and 64-bit Makefiles for building
    Windows binaries are unified using "include" directive to avoid lots
    of duplicate information.
    
    Some files were missing svn:eol-style properties which were added.
    
    Zabbix version information was moved from include/common.h to
    include/version.h so that this information is available for building
    Windows binaries.
    
    
    
    git-svn-id: svn://svn.zabbix.com/branches/dev@28400 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1268/build/win32/project/resource.h
+++ ZBXNEXT-1268/build/win32/project/resource.h
@@ -2,14 +2,34 @@
 // Microsoft Developer Studio generated include file.
 // Used by resource.rc
 //
+#ifndef _RESOURCE_H_
+#define _RESOURCE_H_
+
+#include "..\..\..\include\version.h"
+
+#if defined(ZABBIX_AGENT_DESC)
+#	include "zabbix_agent_desc.h"
+#elif defined(ZABBIX_GET_DESC)
+#	include "zabbix_get_desc.h"
+#elif defined(ZABBIX_SENDER_DESC)
+#	include "zabbix_sender_desc.h"
+#endif
+
+#define VER_FILEVERSION			{ZABBIX_FILEVERSION}
+#define VER_PRODUCTVERSION_STR		ZABBIX_VERSION "\0"
+#define VER_LEGALCOPYRIGHT_STR		"Copyright (C) 2000-2012 Zabbix SIA"
+#define VER_PRODUCTNAME_STR		"Zabbix"
+
 
 // Next default values for new objects
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_RESOURCE_VALUE	105
+#define _APS_NEXT_COMMAND_VALUE		40001
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		101
 #endif
 #endif
+
+#endif	/* _RESOURCE_H_ */
---
+++ ZBXNEXT-1268/build/win32/project/zabbix_agent_desc.h
@@ -0,0 +1,6 @@
+#ifndef _ZABBIX_AGENT_DESC_H_
+#define _ZABBIX_AGENT_DESC_H_
+
+#define VER_FILEDESCRIPTION_STR		"A command line utility to control Zabbix Agent service."
+
+#endif	/* _ZABBIX_AGENT_DESC_H_ */
