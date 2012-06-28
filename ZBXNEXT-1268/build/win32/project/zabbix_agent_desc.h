9bf9cdf22ac535c3f7de8fc1e192cf10e1dae4b3
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-28 09:54:58 +0000

    ...GI..... [ZBXNEXT-1268] fix descriptions of Zabbix binaries for Windows
    git-svn-id: svn://svn.zabbix.com/branches/dev@28469 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1268/build/win32/project/zabbix_agent_desc.h
+++ ZBXNEXT-1268/build/win32/project/zabbix_agent_desc.h
@@ -1,6 +1,6 @@
 #ifndef _ZABBIX_AGENT_DESC_H_
 #define _ZABBIX_AGENT_DESC_H_
 
-#define VER_FILEDESCRIPTION_STR		"Zabbix Agent is deployed on a host to monitor resources and applications and send data to Zabbix Server."
+#define VER_FILEDESCRIPTION_STR		"Zabbix agent is deployed on a host to monitor resources and applications and send data to Zabbix server."
 
 #endif	/* _ZABBIX_AGENT_DESC_H_ */
--- ZBXNEXT-1268/build/win32/project/zabbix_sender_desc.h
+++ ZBXNEXT-1268/build/win32/project/zabbix_sender_desc.h
@@ -1,6 +1,6 @@
 #ifndef _ZABBIX_SENDER_DESC_H_
 #define _ZABBIX_SENDER_DESC_H_
 
-#define VER_FILEDESCRIPTION_STR		"A command line utility to send data to Zabbix Server."
+#define VER_FILEDESCRIPTION_STR		"A command line utility to send data to Zabbix server."
 
 #endif	/* _ZABBIX_SENDER_DESC_H_ */
