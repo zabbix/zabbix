d8971dbe2eb00b3249e0cd7b51abbe8143baa5c1
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-28 13:15:14 +0000

    .......... [ZBX-5159] sync 2.0
    git-svn-id: svn://svn.zabbix.com/branches/dev@28507 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5159/frontends/php/events.php
+++ ZBX-5159/frontends/php/events.php
@@ -198,10 +198,6 @@ if(isset($_REQUEST['filter_set']) || isset($_REQUEST['filter_rst'])){
 
 CProfile::update('web.events.source',$source, PROFILE_TYPE_INT);
 
-
-// js templates
-require_once dirname(__FILE__).'/include/views/js/general.script.confirm.js.php';
-
 $events_wdgt = new CWidget();
 
 // header
@@ -735,6 +731,9 @@ $objData = array(
 zbx_add_post_js('jqBlink.blink();');
 zbx_add_post_js('timeControl.addObject("'.$dom_graph_id.'",'.zbx_jsvalue($timeline).','.zbx_jsvalue($objData).');');
 zbx_add_post_js('timeControl.processObjects();');
+
+// js templates
+require_once dirname(__FILE__).'/include/views/js/general.script.confirm.js.php';
 
 $events_wdgt->show();
 if($csv_disabled) zbx_add_post_js('document.getElementById(\'csv_export\').disabled=true;');
