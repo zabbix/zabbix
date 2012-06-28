1aab49b3718d7ba3aad86e59fcd5d98b15b16c65
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-28 09:04:52 +0000

    ...GI..... [ZBXNEXT-1268] fix zabbix_agent.exe description
    git-svn-id: svn://svn.zabbix.com/branches/dev@28464 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1268/build/win32/project/zabbix_agent_desc.h
+++ ZBXNEXT-1268/build/win32/project/zabbix_agent_desc.h
@@ -1,6 +1,6 @@
 #ifndef _ZABBIX_AGENT_DESC_H_
 #define _ZABBIX_AGENT_DESC_H_
 
-#define VER_FILEDESCRIPTION_STR		"An agent deployed on a monitoring target to collect local resources data and send it to Zabbix Server."
+#define VER_FILEDESCRIPTION_STR		"Zabbix Agent is deployed on a host to monitor resources and applications and send data to Zabbix Server."
 
 #endif	/* _ZABBIX_AGENT_DESC_H_ */
