ec479fee83e38e86d8208228f7d42d8f9bd4c8c9
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-28 13:28:20 +0000

    .......... [ZBX-5108] removed merged branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@28516 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5108/upgrades/dbpatches/2.0/postgresql/patch/auditlog.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ONLY auditlog ALTER auditid DROP DEFAULT,
-			  ALTER userid DROP DEFAULT;
-DELETE FROM auditlog WHERE NOT EXISTS (SELECT 1 FROM users WHERE users.userid=auditlog.userid);
-ALTER TABLE ONLY auditlog ADD CONSTRAINT c_auditlog_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
