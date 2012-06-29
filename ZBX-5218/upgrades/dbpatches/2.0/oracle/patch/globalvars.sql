a5a74a8dc3a4ad5f926ac9fb50a9b915759d8954
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-29 12:45:16 +0000

    .......... [ZBX-5218] remove merged development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@28568 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5218/upgrades/dbpatches/2.0/oracle/patch/globalvars.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE globalvars (
-	globalvarid              number(20)                                NOT NULL,
-	snmp_lastsize            number(10)      DEFAULT '0'               NOT NULL,
-	PRIMARY KEY (globalvarid)
-);
