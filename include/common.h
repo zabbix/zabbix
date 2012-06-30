72ebe765a412631a95838254a96b81f9faf113dc
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-06-30 15:08:49 +0000

    .......... [ZBXNEXT-826] updated release version
    git-svn-id: svn://svn.zabbix.com/branches/1.8@28587 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -122,7 +122,7 @@
 
 #define	APPLICATION_NAME	"Zabbix Agent"
 #define	ZABBIX_REVDATE		"27 June 2012"
-#define	ZABBIX_VERSION		"1.8.14"
+#define	ZABBIX_VERSION		"1.8.15rc1"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
 #if defined(_WINDOWS)
