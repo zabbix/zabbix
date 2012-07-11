391d34741091910ad046d1a184106c9136c0198b
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-07-11 09:07:26 +0000

    ....I..... [ZBX-5168] add missing and fix existing help_items as per documentation
    This includes:
      - fix net.dns* items
      - fix icmpping* items
      - lowercase grpfunc item parameters
      - add missing internal check items
      - add vfs.file.contents item
      - add third parameter for vfs.file.reg* items
      - add second parameter for perf_counter item
    
    
    git-svn-id: svn://svn.zabbix.com/branches/2.0@28790 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/2.0/ibm_db2/patch/help_items.sql
+++ upgrades/dbpatches/2.0/ibm_db2/patch/help_items.sql
@@ -17,9 +17,9 @@ INSERT INTO help_items (itemtype,key_,description) values ('0','kernel.maxfiles'
 /
 INSERT INTO help_items (itemtype,key_,description) values ('0','kernel.maxproc','Maximum number of processes supported by OS.')
 /
-INSERT INTO help_items (itemtype,key_,description) values ('0','net.dns.record[&lt;ip&gt;,&lt;zone&gt;,&lt;type&gt;,&lt;timeout&gt;,&lt;count&gt;]','Performs a DNS query. On success returns a character string with the required type of information.')
+INSERT INTO help_items (itemtype,key_,description) values ('0','net.dns.record[&lt;ip&gt;,zone,&lt;type&gt;,&lt;timeout&gt;,&lt;count&gt;]','Performs a DNS query. On success returns a character string with the required type of information.')
 /
-INSERT INTO help_items (itemtype,key_,description) values ('0','net.dns[&lt;ip&gt;,&lt;zone&gt;,&lt;type&gt;,&lt;timeout&gt;,&lt;count&gt;]','Checks if DNS service is up. 0 - DNS is down (server did not respond or DNS resolution failed), 1 - DNS is up.')
+INSERT INTO help_items (itemtype,key_,description) values ('0','net.dns[&lt;ip&gt;,zone,&lt;type&gt;,&lt;timeout&gt;,&lt;count&gt;]','Checks if DNS service is up. 0 - DNS is down (server did not respond or DNS resolution failed), 1 - DNS is up.')
 /
 INSERT INTO help_items (itemtype,key_,description) values ('0','net.if.collisions[if]','Out-of-window collision. Collisions count.')
 /
@@ -127,11 +127,11 @@ INSERT INTO help_items (itemtype,key_,description) values ('0','web.page.perf[ho
 /
 INSERT INTO help_items (itemtype,key_,description) values ('0','web.page.regexp[host,&lt;path&gt;,&lt;port&gt;,&lt;regexp&gt;,&lt;length&gt;]','Get first occurence of regexp in WEB page. Default path is /')
 /
-INSERT INTO help_items (itemtype,key_,description) values ('3','icmppingloss[&lt;ip&gt;,&lt;count&gt;,&lt;interval&gt;,&lt;size&gt;,&lt;timeout&gt;]','Returns percentage of lost ICMP ping packets.')
+INSERT INTO help_items (itemtype,key_,description) values ('3','icmppingloss[&lt;target&gt;,&lt;packets&gt;,&lt;interval&gt;,&lt;size&gt;,&lt;timeout&gt;]','Returns percentage of lost ICMP ping packets.')
 /
-INSERT INTO help_items (itemtype,key_,description) values ('3','icmppingsec[&lt;ip&gt;,&lt;count&gt;,&lt;interval&gt;,&lt;size&gt;,&lt;timeout&gt;,&lt;type&gt;]','Returns ICMP ping response time in seconds. Example: 0.02')
+INSERT INTO help_items (itemtype,key_,description) values ('3','icmppingsec[&lt;target&gt;,&lt;packets&gt;,&lt;interval&gt;,&lt;size&gt;,&lt;timeout&gt;,&lt;mode&gt;]','Returns ICMP ping response time in seconds. Example: 0.02')
 /
-INSERT INTO help_items (itemtype,key_,description) values ('3','icmpping[&lt;ip&gt;,&lt;count&gt;,&lt;interval&gt;,&lt;size&gt;,&lt;timeout&gt;]','Checks if server is accessible by ICMP ping. 0 - ICMP ping fails. 1 - ICMP ping successful. One of zabbix_server processes performs ICMP pings once per PingerFrequency seconds.')
+INSERT INTO help_items (itemtype,key_,description) values ('3','icmpping[&lt;target&gt;,&lt;packets&gt;,&lt;interval&gt;,&lt;size&gt;,&lt;timeout&gt;]','Checks if server is accessible by ICMP ping. 0 - ICMP ping fails. 1 - ICMP ping successful. One of zabbix_server processes performs ICMP pings once per PingerFrequency seconds.')
 /
 INSERT INTO help_items (itemtype,key_,description) values ('3','net.tcp.service.perf[service,&lt;ip&gt;,&lt;port&gt;]','Check performance of service. 0 - service is down, sec - number of seconds spent on connection to the service. If &lt;ip&gt; is missing, IP or DNS name is taken from host definition. If &lt;port&gt; is missing, default service port is used.')
 /
@@ -191,9 +191,9 @@ INSERT INTO help_items (itemtype,key_,description) values ('7','logrt[file_forma
 /
 INSERT INTO help_items (itemtype,key_,description) values ('7','log[file,&lt;pattern&gt;,&lt;encoding&gt;,&lt;maxlines&gt;,&lt;mode&gt;]','Monitoring of log file. pattern - regular expression')
 /
-INSERT INTO help_items (itemtype,key_,description) values ('7','net.dns.record[&lt;ip&gt;,&lt;zone&gt;,&lt;type&gt;,&lt;timeout&gt;,&lt;count&gt;]','Performs a DNS query. On success returns a character string with the required type of information.')
+INSERT INTO help_items (itemtype,key_,description) values ('7','net.dns.record[&lt;ip&gt;,zone,&lt;type&gt;,&lt;timeout&gt;,&lt;count&gt;]','Performs a DNS query. On success returns a character string with the required type of information.')
 /
-INSERT INTO help_items (itemtype,key_,description) values ('7','net.dns[&lt;ip&gt;,&lt;zone&gt;,&lt;type&gt;,&lt;timeout&gt;,&lt;count&gt;]','Checks if DNS service is up. 0 - DNS is down (server did not respond or DNS resolution failed), 1 - DNS is up.')
+INSERT INTO help_items (itemtype,key_,description) values ('7','net.dns[&lt;ip&gt;,zone,&lt;type&gt;,&lt;timeout&gt;,&lt;count&gt;]','Checks if DNS service is up. 0 - DNS is down (server did not respond or DNS resolution failed), 1 - DNS is up.')
 /
 INSERT INTO help_items (itemtype,key_,description) values ('7','net.if.collisions[if]','Out-of-window collision. Collisions count.')
 /
