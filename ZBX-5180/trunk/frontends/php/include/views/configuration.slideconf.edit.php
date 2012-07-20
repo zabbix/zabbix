7c7590bb0b74baf58c474c4d3bdfca3a419a5ea5
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-07-20 14:36:41 +0000

    .......... [ZBX-5180] branch created
    git-svn-id: svn://svn.zabbix.com/branches/dev@29003 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-5180/trunk/frontends/php/include/views/configuration.slideconf.edit.php
@@ -0,0 +1,123 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2011 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+?>
+<?php
+require_once dirname(__FILE__).'/js/configuration.slideconf.edit.js.php';
+
+$slideWidget = new CWidget();
+$slideWidget->addPageHeader(_('CONFIGURATION OF SLIDE SHOWS'));
+
+// create form
+$slideForm = new CForm();
+$slideForm->setName('slideForm');
+$slideForm->addVar('form', $this->data['form']);
+$slideForm->addVar('slides', $this->data['slides_without_delay']);
+if (!empty($this->data['slideshowid'])) {
+	$slideForm->addVar('slideshowid', $this->data['slideshowid']);
+}
+
+// create slide form list
+$slideFormList = new CFormList('slideFormList');
+$slideFormList->addRow(_('Name'), new CTextBox('name', $this->data['name'], ZBX_TEXTBOX_STANDARD_SIZE));
+$slideFormList->addRow(_('Default delay (in seconds)'), new CNumericBox('delay', $this->data['delay'], 5, 'no', false, false));
+
+// append slide table
+$slideTable = new CTableInfo(_('No slides defined.'), 'formElementTable');
+$slideTable->setAttribute('style', 'min-width: 500px;');
+$slideTable->setAttribute('id', 'slideTable');
+$slideTable->setHeader(array(
+	new CCol(SPACE, null, null, '15'),
+	new CCol(SPACE, null, null, '15'),
+	new CCol(_('Screen'), null, null, '140'),
+	new CCol(_('Delay'), null, null, '70'),
+	new CCol(_('Action'), null, null, '50')
+));
+
+$i = 1;
+foreach ($this->data['slides'] as $step => $slides) {
+	$name = '';
+	if (!empty($slides['screenid'])) {
+		$screen = get_screen_by_screenid($slides['screenid']);
+		if (!empty($screen['name'])) {
+			$name = $screen['name'];
+		}
+	}
+
+	$delay = new CNumericBox('slides['.$step.'][delay]', !empty($slides['delay']) ? $slides['delay'] : '', 5, 'no', true, false);
+	$delay->setAttribute('placeholder', _('default'));
+
+	$removeButton = new CButton('remove_'.$step, _('Remove'), 'javascript: removeSlide(this);', 'link_menu');
+	$removeButton->setAttribute('remove_slide', $step);
+
+	$row = new CRow(
+		array(
+			new CSpan(null, 'ui-icon ui-icon-arrowthick-2-n-s move'),
+			new CSpan($i++.':', 'rowNum', 'current_slide_'.$step),
+			$name,
+			$delay,
+			$removeButton
+		),
+		'sortable',
+		'slides_'.$step
+	);
+	$slideTable->addRow($row);
+}
+
+$addButtonColumn = new CCol(
+	empty($this->data['work_slide'])
+		? new CButton('add', _('Add'),
+			'return PopUp("popup.php?srctbl=screens&srcfld1=screenid&dstfrm='.$slideForm->getName().
+				'&multiselect=1", 450, 450)',
+			'link_menu')
+		: null,
+	null,
+	5
+);
+$addButtonColumn->setAttribute('style', 'vertical-align: middle;');
+$slideTable->addRow(new CRow($addButtonColumn, null, 'screenListFooter'));
+
+$slideFormList->addRow(_('Slides'), new CDiv($slideTable, 'objectgroup inlineblock border_dotted ui-corner-all'));
+
+// append tabs to form
+$slideTab = new CTabView();
+$slideTab->addTab('slideTab', _('Slide'), $slideFormList);
+$slideForm->addItem($slideTab);
+
+// append buttons to form
+if (empty($this->data['slideshowid'])) {
+	$slideForm->addItem(makeFormFooter(
+		new CSubmit('save', _('Save')),
+		new CButtonCancel()
+	));
+}
+else {
+	$slideForm->addItem(makeFormFooter(
+		new CSubmit('save', _('Save')),
+		array(
+			new CSubmit('clone', _('Clone')),
+			new CButtonDelete(_('Delete slide show?'), url_param('form').url_param('slideshowid').url_param('config')),
+			new CButtonCancel()
+		)
+	));
+}
+
+$slideWidget->addItem($slideForm);
+return $slideWidget;
+?>
---
+++ ZBX-5180/trunk/src/libs/zbxnix/ipc.c
@@ -0,0 +1,111 @@
+/*
+** Zabbix
+** Copyright (C) 2000-2011 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+#include "ipc.h"
+#include "log.h"
+
+/******************************************************************************
+ *                                                                            *
+ * Function: zbx_ftok                                                         *
+ *                                                                            *
+ * Purpose: Create IPC id                                                     *
+ *                                                                            *
+ * Parameters:  path - filename                                               *
+ *              id - user selectable ID                                       *
+ *                                                                            *
+ * Return value: If the function succeeds, then return unique ID              *
+ *               -1 on an error                                               *
+ *                                                                            *
+ * Author: Alexei Vladishev                                                   *
+ *                                                                            *
+ * Comments:                                                                  *
+ *                                                                            *
+ ******************************************************************************/
+key_t	zbx_ftok(char *path, int id)
+{
+	key_t	ipc_key;
+
+	if (-1 == (ipc_key = ftok(path, id)))
+	{
+		zbx_error("cannot create IPC key for path [%s] id [%c]: %s",
+			path, (char)id, zbx_strerror(errno));
+	}
+
+	return ipc_key;
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Function: zbx_shmget                                                       *
+ *                                                                            *
+ * Purpose: Create block of shared memory                                     *
+ *                                                                            *
+ * Parameters:  key - IPC key                                                 *
+ *              size - size                                                   *
+ *                                                                            *
+ * Return value: If the function succeeds, then return SHM ID                 *
+ *               -1 on an error                                               *
+ *                                                                            *
+ * Author: Alexei Vladishev                                                   *
+ *                                                                            *
+ * Comments:                                                                  *
+ *                                                                            *
+ ******************************************************************************/
+int	zbx_shmget(key_t key, size_t size)
+{
+	int	shm_id, ret = SUCCEED;
+
+	if (-1 != (shm_id = shmget(key, size, IPC_CREAT | IPC_EXCL | 0600)))
+		return shm_id;
+
+	/* if shared memory block exists, try to remove and re-create it */
+	if (EEXIST == errno)
+	{
+		/* get ID of existing memory */
+		if (-1 == (shm_id = shmget(key, 0 /* get reference */, 0600)))
+		{
+			zbx_error("cannot attach to existing shared memory: %s", zbx_strerror(errno));
+			ret = FAIL;
+		}
+
+		zabbix_log(LOG_LEVEL_DEBUG, "zbx_shmget() removing existing shm_id:%d", shm_id);
+
+		if (SUCCEED == ret && -1 == shmctl(shm_id, IPC_RMID, 0))
+		{
+			zbx_error("cannot remove existing shared memory: %s", zbx_strerror(errno));
+			ret = FAIL;
+		}
+
+		if (SUCCEED == ret && -1 == (shm_id = shmget(key, size, IPC_CREAT | IPC_EXCL | 0600)))
+		{
+			zabbix_log(LOG_LEVEL_CRIT, "cannot allocate shared memory of size " ZBX_FS_SIZE_T ": %s",
+					(zbx_fs_size_t)size, zbx_strerror(errno));
+			ret = FAIL;
+		}
+	}
+	else
+	{
+		zabbix_log(LOG_LEVEL_CRIT, "cannot allocate shared memory of size " ZBX_FS_SIZE_T ": %s",
+				(zbx_fs_size_t)size, zbx_strerror(errno));
+		ret = FAIL;
+	}
+
+	return (ret == SUCCEED) ? shm_id : -1;
+}
