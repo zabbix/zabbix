6475faadb563216f44a69732bd6d03702a85fc1d
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-07-23 06:41:48 +0000

    .......... [ZBX-3859] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@29034 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3859/upgrades/dbpatches/2.0/mysql/optional/history_uint_sync.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE history_uint_sync MODIFY itemid bigint unsigned NOT NULL;
-ALTER TABLE history_uint_sync MODIFY nodeid integer NOT NULL;
-ALTER TABLE history_uint_sync ADD ns integer DEFAULT '0' NOT NULL;
