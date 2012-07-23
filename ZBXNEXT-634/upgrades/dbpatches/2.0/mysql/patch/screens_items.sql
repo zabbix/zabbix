3980201d3f65685a5a58a03f5cb03a6a73337a18
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-07-23 06:20:36 +0000

    .......... [ZBXNEXT-634] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@29026 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-634/upgrades/dbpatches/2.0/mysql/patch/screens_items.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE screens_items MODIFY screenitemid bigint unsigned NOT NULL,
-			  MODIFY screenid bigint unsigned NOT NULL;
-DELETE FROM screens_items WHERE screenid NOT IN (SELECT screenid FROM screens);
-ALTER TABLE screens_items ADD CONSTRAINT c_screens_items_1 FOREIGN KEY (screenid) REFERENCES screens (screenid) ON DELETE CASCADE;
