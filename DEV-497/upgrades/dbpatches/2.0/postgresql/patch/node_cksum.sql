588d402a57eaab66f9768a95323115547154b7a8
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-07-23 06:17:46 +0000

    .......... [DEV-497] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@29025 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- DEV-497/upgrades/dbpatches/2.0/postgresql/patch/node_cksum.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ONLY node_cksum ALTER nodeid DROP DEFAULT,
-			    ALTER recordid DROP DEFAULT;
-DELETE FROM node_cksum WHERE NOT EXISTS (SELECT 1 FROM nodes WHERE nodes.nodeid=node_cksum.nodeid);
-ALTER TABLE ONLY node_cksum ADD CONSTRAINT c_node_cksum_1 FOREIGN KEY (nodeid) REFERENCES nodes (nodeid) ON DELETE CASCADE;
