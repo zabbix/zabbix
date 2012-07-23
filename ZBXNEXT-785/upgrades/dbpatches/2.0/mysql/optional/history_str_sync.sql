fd2a34eb70cf32450b7f9bb0546e1bfbca01e6c9
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-07-23 06:57:46 +0000

    .......... [ZBXNEXT-785] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@29049 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-785/upgrades/dbpatches/2.0/mysql/optional/history_str_sync.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE history_str_sync MODIFY itemid bigint unsigned NOT NULL;
-ALTER TABLE history_str_sync MODIFY nodeid integer NOT NULL;
-ALTER TABLE history_str_sync ADD ns integer DEFAULT '0' NOT NULL;
