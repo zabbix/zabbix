d9edc19362661d9adefe644d683732dcfb447dfe
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-07-25 12:34:16 +0000

    A.F....... [ZBX-5351] replaced CZBXAPI::objectModified() with DB::recordModified()
    git-svn-id: svn://svn.zabbix.com/branches/dev@29150 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5351/frontends/php/api/classes/CGraphGeneral.php
+++ ZBX-5351/frontends/php/api/classes/CGraphGeneral.php
@@ -166,7 +166,7 @@ abstract class CGraphGeneral extends CZBXAPI {
 		$dbGitemIds = zbx_objectValues($dbGitems, 'gitemid');
 
 		// update the graph if it's modified
-		if ($this->objectModified($graph, $dbGraph)) {
+		if (DB::recordModified('graphs', $dbGraph, $graph)) {
 			DB::updateByPk($this->tableName(), $graph['graphid'], $graph);
 		}
 
@@ -176,7 +176,7 @@ abstract class CGraphGeneral extends CZBXAPI {
 		foreach ($graph['gitems'] as $gitem) {
 			// updating an existing item
 			if (isset($gitem['gitemid'], $dbGitems[$gitem['gitemid']])) {
-				if ($this->objectModified($gitem, $dbGitems[$gitem['gitemid']], 'graphs_items')) {
+				if (DB::recordModified('graphs_items', $dbGitems[$gitem['gitemid']], $gitem)) {
 					DB::updateByPk('graphs_items', $gitem['gitemid'], $gitem);
 				}
 
