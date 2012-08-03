e6fe4d65f0332e7996370d4040d1715e022f0d04
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-08-03 11:19:48 +0000

    ..F....... [ZBXNEXT-1253] fixed IE8 time control rendering
    git-svn-id: svn://svn.zabbix.com/branches/dev@29402 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1253/frontends/php/js/gtlc.js
+++ ZBXNEXT-1253/frontends/php/js/gtlc.js
@@ -171,7 +171,7 @@ var timeControl = {
 		imgUrl.setArgument('stime', stime);
 		imgUrl = this.getFormattedUrl(objid, imgUrl);
 
-		jQuery('<img />', {id: obj.domid + '_tmp', src: imgUrl.getUrl(), class: 'borderless'}).load(function() {
+		jQuery('<img />', {id: obj.domid + '_tmp', src: imgUrl.getUrl(), 'class': 'borderless'}).load(function() {
 			var id = jQuery(this).attr('id').substring(0, jQuery(this).attr('id').indexOf('_tmp'));
 			jQuery('#' + id).replaceWith(jQuery(this));
 			jQuery(this).attr('id', id);
