93e2ed2337ad6388eaebda5025594e79a970dcd6
tomtom <tomtom@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-08-03 08:27:49 +0000

    .......... [ZBX-5393] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@29394 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-5393/build/win32/project/zabbix_agent_desc.h
@@ -0,0 +1,6 @@
+#ifndef _ZABBIX_AGENT_DESC_H_
+#define _ZABBIX_AGENT_DESC_H_
+
+#define VER_FILEDESCRIPTION_STR		"Zabbix agent is deployed on a host to monitor resources and applications and send data to Zabbix server."
+
+#endif
