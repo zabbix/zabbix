11fa182f7ae5167a1f334feaaaffb8165429ac77
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-08-08 12:39:04 +0000

    A.F....... [ZBXNEXT-1253] implemented the flicked-free screens
    git-svn-id: svn://svn.zabbix.com/trunk@29459 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/2.0/postgresql/patch/profiles.sql
+++ upgrades/dbpatches/2.0/postgresql/patch/profiles.sql
@@ -4,3 +4,7 @@ ALTER TABLE ONLY profiles
 DELETE FROM profiles WHERE NOT EXISTS (SELECT 1 FROM users WHERE users.userid=profiles.userid);
 DELETE FROM profiles WHERE idx LIKE 'web.%.sort' OR idx LIKE 'web.%.sortorder';
 ALTER TABLE ONLY profiles ADD CONSTRAINT c_profiles_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
+
+UPDATE profiles SET idx = 'web.screens.period' WHERE idx = 'web.charts.period';
+UPDATE profiles SET idx = 'web.screens.stime' WHERE idx = 'web.charts.stime';
+UPDATE profiles SET idx = 'web.screens.timelinefixed' WHERE idx = 'web.charts.timelinefixed';
