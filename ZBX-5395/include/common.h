98f5a86cdbcb9f35ba08af262de7fd646c4ece0d
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-08-22 10:39:30 +0000

    .......... [ZBX-5395] updated to the latest /branches/1.8
    git-svn-id: svn://svn.zabbix.com/branches/dev@29727 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5395/include/common.h
+++ ZBX-5395/include/common.h
@@ -122,7 +122,7 @@
 
 #define	APPLICATION_NAME	"Zabbix Agent"
 #define	ZABBIX_REVDATE		"20 August 2012"
-#define	ZABBIX_VERSION		"1.8.15"
+#define	ZABBIX_VERSION		"1.8.16rc1"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
 #if defined(_WINDOWS)
