9f83094725d70f6f799ac23c4a36a90b5a3bdef5
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2012-08-27 20:06:08 +0000

    .......PS. [ZBX-5483] fixed response to requests of the sender with missing fields

--- src/libs/zbxdbhigh/proxy.c
+++ src/libs/zbxdbhigh/proxy.c
@@ -1594,6 +1594,8 @@ int	process_hist_data(zbx_sock_t *sock, struct zbx_json_parse *jp,
 		if (FAIL == (ret = zbx_json_brackets_open(p, &jp_row)))
 			break;
 
+		total_num++;
+
 		av = &values[value_num];
 
 		memset(av, 0, sizeof(AGENT_VALUE));
@@ -1660,7 +1662,6 @@ int	process_hist_data(zbx_sock_t *sock, struct zbx_json_parse *jp,
 			process_mass_data(sock, proxy_hostid, values, value_num, &processed);
 
 			clean_agent_values(values, value_num);
-			total_num += value_num;
 			value_num = 0;
 		}
 	}
@@ -1671,7 +1672,6 @@ int	process_hist_data(zbx_sock_t *sock, struct zbx_json_parse *jp,
 		process_mass_data(sock, proxy_hostid, values, value_num, &processed);
 
 	clean_agent_values(values, value_num);
-	total_num += value_num;
 
 	if (NULL != info)
 	{
