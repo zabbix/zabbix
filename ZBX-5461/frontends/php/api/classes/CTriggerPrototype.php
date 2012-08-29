54cd0826c96bf27df072a8adf107e646232fff66
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-08-29 11:28:34 +0000

    ..F....... [ZBX-5461] fixed error when import template with triggers with dependencies
    git-svn-id: svn://svn.zabbix.com/branches/dev@29923 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5461/frontends/php/api/classes/CTriggerPrototype.php
+++ ZBX-5461/frontends/php/api/classes/CTriggerPrototype.php
@@ -17,11 +17,8 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
-?>
-<?php
-/**
- * @package API
- */
+
+
 class CTriggerPrototype extends CTriggerGeneral {
 
 	protected $tableName = 'triggers';
@@ -1270,4 +1267,3 @@ class CTriggerPrototype extends CTriggerGeneral {
 		return true;
 	}
 }
-?>
