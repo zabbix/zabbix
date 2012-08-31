d09bc8aa1a02295535c7f7de79488c40e2b810d3
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-08-31 12:39:00 +0000

    .......... [ZBX-5383] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@30021 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5383-2.0/upgrades/dbpatches/1.8/postgresql/patch/hostmacro.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE hostmacro (
-        hostmacroid             bigint          DEFAULT '0'     NOT NULL,
-        hostid          bigint          DEFAULT '0'     NOT NULL,
-        macro           varchar(64)             DEFAULT ''      NOT NULL,
-        value           varchar(255)            DEFAULT ''      NOT NULL,
-        PRIMARY KEY (hostmacroid)
-) with OIDS;
-CREATE INDEX hostmacro_1 on hostmacro (hostid,macro);
