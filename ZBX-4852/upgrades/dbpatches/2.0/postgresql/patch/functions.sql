068ebbd9392f6b426a0c9eb5f3960db772c61444
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-09-04 13:34:36 +0000

    .......... [ZBX-4852] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@30100 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4852/upgrades/dbpatches/2.0/postgresql/patch/functions.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE ONLY functions ALTER functionid DROP DEFAULT,
-			   ALTER itemid DROP DEFAULT,
-			   ALTER triggerid DROP DEFAULT,
-			   DROP COLUMN lastvalue;
-DELETE FROM functions WHERE NOT EXISTS (SELECT 1 FROM items WHERE items.itemid=functions.itemid);
-DELETE FROM functions WHERE NOT EXISTS (SELECT 1 FROM triggers WHERE triggers.triggerid=functions.triggerid);
-ALTER TABLE ONLY functions ADD CONSTRAINT c_functions_1 FOREIGN KEY (itemid) REFERENCES items (itemid) ON DELETE CASCADE;
-ALTER TABLE ONLY functions ADD CONSTRAINT c_functions_2 FOREIGN KEY (triggerid) REFERENCES triggers (triggerid) ON DELETE CASCADE;
