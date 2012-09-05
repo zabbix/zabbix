2e56611f77810167f80769b47eb3904ee0e3c478
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-09-05 08:21:34 +0000

    .......... [ZBX-5513] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@30108 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5513/upgrades/dbpatches/2.0/postgresql/patch/screens.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ONLY screens ALTER screenid DROP DEFAULT,
-			 ALTER name DROP DEFAULT,
-			 ADD templateid bigint NULL;
-ALTER TABLE ONLY screens ADD CONSTRAINT c_screens_1 FOREIGN KEY (templateid) REFERENCES hosts (hostid) ON DELETE CASCADE;
