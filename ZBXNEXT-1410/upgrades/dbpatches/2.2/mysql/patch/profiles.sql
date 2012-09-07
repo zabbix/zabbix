192b5a4b6d0261970276e97e51f2c067ede1f7f9
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-09-07 06:04:33 +0000

    .......... [ZBXNEXT-1410] created a new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@30144 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-1410/upgrades/dbpatches/2.2/mysql/patch/profiles.sql
@@ -0,0 +1,3 @@
+UPDATE profiles SET idx = 'web.screens.period' WHERE idx = 'web.charts.period';
+UPDATE profiles SET idx = 'web.screens.stime' WHERE idx = 'web.charts.stime';
+UPDATE profiles SET idx = 'web.screens.timelinefixed' WHERE idx = 'web.charts.timelinefixed';
