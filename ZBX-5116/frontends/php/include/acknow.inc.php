d55aac7a0461f316877eadb2f5b5ff6ce6818da2
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-09-11 11:31:21 +0000

    .......... [ZBX-5116] created dev brach
    git-svn-id: svn://svn.zabbix.com/branches/dev@30178 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-5116/frontends/php/include/acknow.inc.php
@@ -0,0 +1,57 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2012 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+function get_last_event_by_triggerid($triggerid) {
+	$dbEvents = DBfetch(DBselect(
+		'SELECT e.*'.
+		' FROM events e'.
+		' WHERE e.objectid='.$triggerid.
+			' AND e.object='.EVENT_OBJECT_TRIGGER.
+		' ORDER BY e.objectid DESC,e.object DESC,e.eventid DESC',
+		1
+	));
+
+	return !empty($dbEvents) ? $dbEvents : false;
+}
+
+function get_acknowledges_by_eventid($eventid) {
+	return DBselect('SELECT a.*,u.alias FROM acknowledges a LEFT JOIN users u ON u.userid=a.userid WHERE a.eventid='.$eventid);
+}
+
+function make_acktab_by_eventid($event) {
+	if (!empty($event['acknowledges']) && is_array($event['acknowledges'])) {
+		$table = new CTableInfo(_('No acknowledges defined.'));
+		$table->setHeader(array(_('Time'), _('User'), _('Comments')));
+
+		foreach ($event['acknowledges'] as $ack) {
+			$table->addRow(array(
+				zbx_date2str(_('d M Y H:i:s'), $ack['clock']),
+				$ack['alias'],
+				new CCol(zbx_nl2br($ack['message']), 'wraptext')
+			));
+		}
+
+		return $table;
+	}
+	else {
+		return null;
+	}
+}
