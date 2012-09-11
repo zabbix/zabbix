7412bbece98abab558d6db05c95be0426ea709a9
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-09-11 14:57:32 +0000

    .......... [ZBXNEXT-1343] updated to the latest /trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@30194 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1343/frontends/php/profile.php
+++ ZBXNEXT-1343/frontends/php/profile.php
@@ -134,9 +134,6 @@ elseif (isset($_REQUEST['save'])) {
 		if (!isset($messages['enabled'])) {
 			$messages['enabled'] = 0;
 		}
-		if (!isset($messages['sounds.recovery'])) {
-			$messages['sounds.recovery'] = 0;
-		}
 		if (!isset($messages['triggers.recovery'])) {
 			$messages['triggers.recovery'] = 0;
 		}
