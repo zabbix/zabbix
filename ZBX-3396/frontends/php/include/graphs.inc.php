803c9e15bc33b2d38af5e7a4e3ff6f5170b4c8d1
tomtom <tomtom@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-09-18 06:51:39 +0000

    ..F....... [ZBX-3396] Removed unnecessary code; added error message if FreeType support is missing.
    git-svn-id: svn://svn.zabbix.com/branches/dev@30253 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3396/frontends/php/include/graphs.inc.php
+++ ZBX-3396/frontends/php/include/graphs.inc.php
@@ -709,51 +709,8 @@ function imageTextSize($fontsize, $angle, $string) {
 		$result['baseline'] = $ar[1];
 	}
 	else {
-		switch($fontsize) {
-			case 5:
-				$fontsize = 1;
-				break;
-			case 6:
-				$fontsize = 1;
-				break;
-			case 7:
-				$fontsize = 2;
-				break;
-			case 8:
-				$fontsize = 2;
-				break;
-			case 9:
-				$fontsize = 3;
-				break;
-			case 10:
-				$fontsize = 3;
-				break;
-			case 11:
-				$fontsize = 4;
-				break;
-			case 12:
-				$fontsize = 4;
-				break;
-			case 13:
-				$fontsize = 5;
-				break;
-			case 14:
-				$fontsize = 5;
-				break;
-			default:
-				$fontsize = 2;
-				break;
-		}
-
-		if ($angle) {
-			$result['width'] = imagefontheight($fontsize);
-			$result['height'] = imagefontwidth($fontsize) * zbx_strlen($string);
-		}
-		else {
-			$result['height'] = imagefontheight($fontsize);
-			$result['width'] = imagefontwidth($fontsize) * zbx_strlen($string);
-		}
-		$result['baseline'] = 0;
+		show_error_message(_('PHP gd FreeType support missing'));
+		return false;
 	}
 
 	return $result;
