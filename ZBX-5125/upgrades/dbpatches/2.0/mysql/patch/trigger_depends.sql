09f1bc7befa9c645cef929274588df02d87e8586
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-09-25 14:44:39 +0000

    ....I..... [ZBX-5125] allow unique index of trigger dependencies in 1.8 -> 2.0 upgrade
    git-svn-id: svn://svn.zabbix.com/branches/dev@30344 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5125/upgrades/dbpatches/2.0/mysql/patch/trigger_depends.sql
+++ ZBX-5125/upgrades/dbpatches/2.0/mysql/patch/trigger_depends.sql
@@ -4,6 +4,16 @@ ALTER TABLE trigger_depends MODIFY triggerdepid bigint unsigned NOT NULL,
 DROP INDEX trigger_depends_1 ON trigger_depends;
 DELETE FROM trigger_depends WHERE triggerid_down NOT IN (SELECT triggerid FROM triggers);
 DELETE FROM trigger_depends WHERE triggerid_up NOT IN (SELECT triggerid FROM triggers);
+-- remove duplicates to allow unique index
+DELETE trigger_depends
+	FROM trigger_depends
+	LEFT OUTER JOIN (
+		SELECT MIN(triggerdepid) AS triggerdepid
+		FROM trigger_depends
+		GROUP BY triggerid_down,triggerid_up
+	) keep_rows ON
+		trigger_depends.triggerdepid=keep_rows.triggerdepid
+	WHERE keep_rows.triggerdepid IS NULL;
 CREATE UNIQUE INDEX trigger_depends_1 ON trigger_depends (triggerid_down,triggerid_up);
 ALTER TABLE trigger_depends ADD CONSTRAINT c_trigger_depends_1 FOREIGN KEY (triggerid_down) REFERENCES triggers (triggerid) ON DELETE CASCADE;
 ALTER TABLE trigger_depends ADD CONSTRAINT c_trigger_depends_2 FOREIGN KEY (triggerid_up) REFERENCES triggers (triggerid) ON DELETE CASCADE;
