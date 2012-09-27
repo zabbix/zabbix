5d5aa205d9311fc85aecdb486f5473c7e4742664
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-09-27 07:03:50 +0000

    ..F....... [ZBX-2579] Y-scale, Bps fix
    git-svn-id: svn://svn.zabbix.com/branches/dev@30375 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-2579/frontends/php/include/classes/class.cchart.php
+++ ZBX-2579/frontends/php/include/classes/class.cchart.php
@@ -719,7 +719,7 @@ class CChart extends CGraphDraw {
 		}
 
 		foreach (array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18) as $num) {
-			if ($this->items[0]['units'] == 'B') {
+			if ($this->items[0]['units'] == 'B' || $this->items[0]['units'] == 'Bps') {
 				$dec = bcpow(8, $num);
 			}
 			else {
