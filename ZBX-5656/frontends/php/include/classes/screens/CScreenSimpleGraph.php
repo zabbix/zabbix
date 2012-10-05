8b15f6784ab3a696fb08dd34bb937a8aeacee32c
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-10-05 09:27:39 +0000

    .......... [ZBX-5656] Created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@30643 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-5656/frontends/php/include/classes/screens/CScreenSimpleGraph.php
@@ -0,0 +1,96 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2012 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CScreenSimpleGraph extends CScreenBase {
+
+	/**
+	 * Process screen.
+	 *
+	 * @return CDiv (screen inside container)
+	 */
+	public function get() {
+		$this->dataId = 'graph_'.$this->screenitem['screenitemid'].'_'.$this->screenitem['screenid'];
+		$resourceid = !empty($this->screenitem['real_resourceid']) ? $this->screenitem['real_resourceid'] : $this->screenitem['resourceid'];
+		$containerid = 'graph_container_'.$this->screenitem['screenitemid'].'_'.$this->screenitem['screenid'];
+		$graphDims = getGraphDims();
+		$graphDims['graphHeight'] = $this->screenitem['height'];
+		$graphDims['width'] = $this->screenitem['width'];
+
+		// get time control
+		$timeControlData = array(
+			'id' => $this->getDataId(),
+			'containerid' => $containerid,
+			'objDims' => $graphDims,
+			'loadImage' => 1,
+			'periodFixed' => CProfile::get('web.screens.timelinefixed', 1),
+			'sliderMaximumTimePeriod' => ZBX_MAX_PERIOD
+		);
+
+		// host feature
+		if ($this->screenitem['dynamic'] == SCREEN_DYNAMIC_ITEM && !empty($this->hostid)) {
+			$newitemid = get_same_item_for_host($resourceid, $this->hostid);
+			$resourceid = !empty($newitemid) ? $newitemid : '';
+		}
+
+		if ($this->mode == SCREEN_MODE_PREVIEW && !empty($resourceid)) {
+			$this->action = 'history.php?action=showgraph&itemid='.$resourceid.'&period='.$this->timeline['period'].
+					'&stime='.$this->timeline['stimeNow'].'&updateProfile='.(int) $this->updateProfile;
+		}
+
+		if (!zbx_empty($resourceid) && $this->mode != SCREEN_MODE_EDIT) {
+			if ($this->mode == SCREEN_MODE_PREVIEW) {
+				$timeControlData['loadSBox'] = 1;
+			}
+		}
+
+		$timeControlData['src'] = zbx_empty($resourceid)
+			? 'chart3.php?updateProfile='.(int) $this->updateProfile
+			: 'chart.php?itemid='.$resourceid.'&'.$this->screenitem['url'].'&width='.$this->screenitem['width'].'&height='.$this->screenitem['height'];
+
+		$timeControlData['src'] .= ($this->mode == SCREEN_MODE_EDIT)
+			? '&period=3600&stime='.date('YmdHis', time())
+			: '&period='.$this->timeline['period'].'&stime='.$this->timeline['stimeNow'];
+
+		// output
+		if ($this->mode == SCREEN_MODE_JS) {
+			return 'timeControl.addObject("'.$this->getDataId().'", '.zbx_jsvalue($this->timeline).', '.zbx_jsvalue($timeControlData).')';
+		}
+		else {
+			if ($this->mode == SCREEN_MODE_SLIDESHOW) {
+				insert_js('timeControl.addObject("'.$this->getDataId().'", '.zbx_jsvalue($this->timeline).', '.zbx_jsvalue($timeControlData).');');
+			}
+			else {
+				zbx_add_post_js('timeControl.addObject("'.$this->getDataId().'", '.zbx_jsvalue($this->timeline).', '.zbx_jsvalue($timeControlData).');');
+			}
+
+			if ($this->mode == SCREEN_MODE_EDIT || $this->mode == SCREEN_MODE_SLIDESHOW) {
+				$item = new CDiv();
+			}
+			elseif ($this->mode == SCREEN_MODE_PREVIEW) {
+				$item = new CLink(null, 'charts.php?graphid='.$resourceid.'&period='.$this->timeline['period'].
+						'&stime='.$this->timeline['stimeNow'].'&updateProfile='.(int) $this->updateProfile);
+			}
+			$item->setAttribute('id', $containerid);
+
+			return $this->getOutput($item);
+		}
+	}
+}
