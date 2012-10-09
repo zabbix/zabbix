1c9a2afb0231136cab155f43b03c89d53bb2f614
Alexey Fukalov <git-no-reply@zabbix.com>
2012-10-09 13:26:17 +0000

    ..F....... [ZBX-5565] fixed errors in tests

--- frontends/php/tests/api/classes/class.cdrule.php
+++ frontends/php/tests/api/classes/class.cdrule.php
@@ -17,9 +17,7 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
-?>
-<?php
-require_once 'PHPUnit/Framework.php';
+
 
 require_once dirname(__FILE__).'/../../include/gettextwrapper.inc.php';
 require_once dirname(__FILE__).'/../../../include/defines.inc.php';
@@ -578,4 +576,3 @@ class CDRuleTest extends PHPUnit_Framework_TestCase {
 	}
 
 }
-?>
