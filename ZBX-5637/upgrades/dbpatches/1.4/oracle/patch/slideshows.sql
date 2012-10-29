6998ef7ad2109be6113e1d0e4f8e680b604079d5
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-10-29 14:29:26 +0000

    .......... [ZBX-5637] branch removed
    git-svn-id: svn://svn.zabbix.com/branches/dev@31134 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5637/upgrades/dbpatches/1.4/oracle/patch/slideshows.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE slideshows (
-	slideshowid             number(20)              DEFAULT '0'     NOT NULL,
-	name            varchar2(255)           DEFAULT ''      ,
-	delay           number(10)              DEFAULT '0'     NOT NULL,
-	PRIMARY KEY (slideshowid)
-);
