baf7463e53431d33f106208035cfb3d2ba404574
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2012-10-31 12:11:57 +0000

    ....I..... [ZBX-5125] fixed possible SQL errors while upgrading DB

--- upgrades/dbpatches/2.0/mysql/patch/hostmacro.sql
+++ upgrades/dbpatches/2.0/mysql/patch/hostmacro.sql
@@ -1,16 +1,17 @@
 ALTER TABLE hostmacro MODIFY hostmacroid bigint unsigned NOT NULL,
 		      MODIFY hostid bigint unsigned NOT NULL;
+DROP INDEX hostmacro_1 ON hostmacro;
 DELETE FROM hostmacro WHERE NOT hostid IN (SELECT hostid FROM hosts);
+
 -- remove duplicates to allow unique index
-DELETE hostmacro
-	FROM hostmacro
-	LEFT OUTER JOIN (
-		SELECT MIN(hostmacroid) AS hostmacroid
+CREATE TEMPORARY TABLE tmp_hostmacro (hostmacroid bigint unsigned PRIMARY KEY);
+INSERT INTO tmp_hostmacro (hostmacroid) (
+	SELECT MIN(hostmacroid)
 		FROM hostmacro
 		GROUP BY hostid,macro
-	) keep_rows ON
-		hostmacro.hostmacroid=keep_rows.hostmacroid
-	WHERE keep_rows.hostmacroid IS NULL;
-DROP INDEX hostmacro_1 ON hostmacro;
+);
+DELETE FROM hostmacro WHERE hostmacroid NOT IN (SELECT hostmacroid FROM tmp_hostmacro);
+DROP TABLE tmp_hostmacro;
+
 CREATE UNIQUE INDEX hostmacro_1 ON hostmacro (hostid,macro);
 ALTER TABLE hostmacro ADD CONSTRAINT c_hostmacro_1 FOREIGN KEY (hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
