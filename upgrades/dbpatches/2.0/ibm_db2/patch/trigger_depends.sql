a408ca7dddc1c587b3f35b53a103c168593f77a9
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-10-31 12:11:25 +0000

    ....I..... [ZBX-5125] fixed possible SQL errors while upgrading DB
    git-svn-id: svn://svn.zabbix.com/branches/2.0@31167 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- upgrades/dbpatches/2.0/ibm_db2/patch/trigger_depends.sql
+++ upgrades/dbpatches/2.0/ibm_db2/patch/trigger_depends.sql
@@ -16,6 +16,20 @@ DELETE FROM trigger_depends WHERE triggerid_down NOT IN (SELECT triggerid FROM t
 /
 DELETE FROM trigger_depends WHERE triggerid_up NOT IN (SELECT triggerid FROM triggers)
 /
+-- remove duplicates to allow unique index
+DELETE FROM trigger_depends
+	WHERE triggerdepid IN (
+		SELECT td1.triggerdepid
+		FROM trigger_depends td1
+		LEFT OUTER JOIN (
+			SELECT MIN(td2.triggerdepid) AS triggerdepid
+			FROM trigger_depends td2
+			GROUP BY td2.triggerid_down,td2.triggerid_up
+		) keep_rows ON
+			td1.triggerdepid=keep_rows.triggerdepid
+		WHERE keep_rows.triggerdepid IS NULL
+	)
+/
 CREATE UNIQUE INDEX trigger_depends_1 ON trigger_depends (triggerid_down,triggerid_up)
 /
 ALTER TABLE trigger_depends ADD CONSTRAINT c_trigger_depends_1 FOREIGN KEY (triggerid_down) REFERENCES triggers (triggerid) ON DELETE CASCADE
