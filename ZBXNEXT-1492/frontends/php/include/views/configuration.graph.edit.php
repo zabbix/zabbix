27b1410769e843fc8dccefc15a0ee4f0e65dbfc9
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-11-05 14:48:16 +0000

    .......... [ZBXNEXT-1492] updated to the latest /trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@31257 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1492/frontends/php/include/views/configuration.graph.edit.php
+++ ZBXNEXT-1492/frontends/php/include/views/configuration.graph.edit.php
@@ -306,7 +306,7 @@ if (!empty($this->data['graphid'])) {
 	$deleteButton = new CButtonDelete(_('Delete graph?'), url_param('graphid').url_param('parent_discoveryid'));
 	$cloneButton = new CSubmit('clone', _('Clone'));
 
-	if (!empty($this->data['templates'])) {
+	if (!empty($this->data['templateid'])) {
 		$saveButton->setEnabled(false);
 		$deleteButton->setEnabled(false);
 		$cloneButton->setEnabled(false);
