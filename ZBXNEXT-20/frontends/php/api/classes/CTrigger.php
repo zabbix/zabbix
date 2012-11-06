34a7b03dc187d292bca7c6c95138ea4e6b65ec57
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-11-06 13:10:06 +0000

    .......... [ZBXNEXT-20] sync trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@31293 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-20/frontends/php/api/classes/CTrigger.php
+++ ZBXNEXT-20/frontends/php/api/classes/CTrigger.php
@@ -1305,6 +1305,7 @@ class CTrigger extends CTriggerGeneral {
 	public function delete($triggerids, $nopermissions = false) {
 		$triggerids = zbx_toArray($triggerids);
 		$triggers = zbx_toObject($triggerids, 'triggerid');
+		$delTriggerIds = $triggerids;
 
 		if (empty($triggerids)) {
 			self::exception(ZBX_API_ERROR_PARAMETERS, _('Empty input parameter.'));
@@ -1344,7 +1345,7 @@ class CTrigger extends CTriggerGeneral {
 		// execute delete
 		$this->deleteByPks($triggerids);
 
-		return array('triggerids' => $triggerids);
+		return array('triggerids' => $delTriggerIds);
 	}
 
 
