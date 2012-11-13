db6854470979ba3c441e1c5cb551750ea61bbaa8
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2012-11-13 08:55:10 +0000

    ........S. [ZBXNEXT-1286] added support of low-level discovery macros in trigger prototype expressions

--- src/libs/zbxdbhigh/lld.c
+++ src/libs/zbxdbhigh/lld.c
@@ -504,14 +504,14 @@ static int	DBlld_trigger_exists(zbx_uint64_t hostid, zbx_uint64_t triggerid, con
 }
 
 static int	DBlld_make_trigger(zbx_uint64_t hostid, zbx_uint64_t parent_triggerid, zbx_vector_ptr_t *triggers,
-		const char *description_proto, const char *expression, const char *comments_proto,
+		const char *description_proto, const char *expression_proto, const char *comments_proto,
 		struct zbx_json_parse *jp_row, char **error)
 {
 	const char		*__function_name = "DBlld_make_trigger";
 
 	DB_RESULT		result;
 	DB_ROW			row;
-	char			*description_esc, *full_expression;
+	char			*description_esc, *expression, *full_expression, err[64];
 	zbx_vector_ptr_t	functions;
 	zbx_lld_trigger_t	*trigger;
 	zbx_lld_function_t	*function;
@@ -525,14 +525,23 @@ static int	DBlld_make_trigger(zbx_uint64_t hostid, zbx_uint64_t parent_triggerid
 	trigger = zbx_calloc(NULL, 1, sizeof(zbx_lld_trigger_t));
 	trigger->update_expression = 1;
 	trigger->description = zbx_strdup(NULL, description_proto);
-	substitute_discovery_macros(&trigger->description, jp_row);
+	substitute_discovery_macros(&trigger->description, jp_row, ZBX_MACRO_ANY, NULL, 0);
+
+	expression = zbx_strdup(NULL, expression_proto);
+	if (SUCCEED != substitute_discovery_macros(&expression, jp_row, ZBX_MACRO_NUMERIC, err, sizeof(err)))
+	{
+		*error = zbx_strdcatf(*error, "Cannot create trigger \"%s\": %s.\n",
+				trigger->description, err);
+		res = FAIL;
+		goto out;
+	}
 
 	zbx_vector_ptr_create(&trigger->functions);
 	DBlld_get_trigger_functions(parent_triggerid, jp_row, &trigger->functions);
 	trigger->full_expression = DBlld_expand_trigger_expression(expression, &trigger->functions);
 
 	trigger->comments = zbx_strdup(NULL, comments_proto);
-	substitute_discovery_macros(&trigger->comments, jp_row);
+	substitute_discovery_macros(&trigger->comments, jp_row, ZBX_MACRO_ANY, NULL, 0);
 
 	description_esc = DBdyn_escape_string_len(trigger->description, TRIGGER_DESCRIPTION_LEN);
 
@@ -543,6 +552,8 @@ static int	DBlld_make_trigger(zbx_uint64_t hostid, zbx_uint64_t parent_triggerid
 				" and td.parent_triggerid=" ZBX_FS_UI64
 				" and t.description='%s'",
 			parent_triggerid, description_esc);
+
+	zbx_free(description_esc);
 
 	while (NULL != (row = DBfetch(result)))
 	{
@@ -581,7 +592,7 @@ static int	DBlld_make_trigger(zbx_uint64_t hostid, zbx_uint64_t parent_triggerid
 			ZBX_STR2UINT64(h_triggerid, row[0]);
 
 			old_name = zbx_strdup(old_name, row[2]);
-			substitute_discovery_macros(&old_name, jp_row);
+			substitute_discovery_macros(&old_name, jp_row, ZBX_MACRO_ANY, NULL, 0);
 
 			if (0 == strcmp(old_name, row[3]))
 			{
@@ -612,7 +623,7 @@ static int	DBlld_make_trigger(zbx_uint64_t hostid, zbx_uint64_t parent_triggerid
 	if (SUCCEED == DBlld_trigger_exists(hostid, trigger->triggerid, trigger->description,
 			trigger->full_expression, triggers))
 	{
-		*error = zbx_strdcatf(*error, "Cannot %s trigger [%s]: trigger already exists\n",
+		*error = zbx_strdcatf(*error, "Cannot %s trigger \"%s\": trigger already exists\n",
 				0 != trigger->triggerid ? "update" : "create", trigger->description);
 		res = FAIL;
 		goto out;
@@ -650,7 +661,7 @@ out:
 		zbx_free(trigger);
 	}
 
-	zbx_free(description_esc);
+	zbx_free(expression);
 	zbx_vector_ptr_destroy(&functions);
 
 	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%s", __function_name, zbx_result_string(res));
@@ -919,14 +930,14 @@ static void	DBlld_update_triggers(zbx_uint64_t hostid, zbx_uint64_t discovery_it
 	while (NULL != (row = DBfetch(result)))
 	{
 		zbx_uint64_t	parent_triggerid;
-		const char	*description_proto, *expression, *comments_proto;
+		const char	*description_proto, *expression_proto, *comments_proto;
 		char		*description_proto_esc, *url_esc;
 		unsigned char	status, type, priority;
 
 		ZBX_STR2UINT64(parent_triggerid, row[0]);
 		description_proto = row[1];
 		description_proto_esc = DBdyn_escape_string(description_proto);
-		expression = row[2];
+		expression_proto = row[2];
 		status = (unsigned char)atoi(row[3]);
 		type = (unsigned char)atoi(row[4]);
 		priority = (unsigned char)atoi(row[5]);
@@ -946,7 +957,7 @@ static void	DBlld_update_triggers(zbx_uint64_t hostid, zbx_uint64_t discovery_it
 			if (SUCCEED != DBlld_check_record(&jp_row, f_macro, f_regexp, regexps, regexps_num))
 				continue;
 
-			DBlld_make_trigger(hostid, parent_triggerid, &triggers, description_proto, expression,
+			DBlld_make_trigger(hostid, parent_triggerid, &triggers, description_proto, expression_proto,
 					comments_proto, &jp_row, error);
 		}
 
@@ -1082,14 +1093,14 @@ static int	DBlld_make_item(zbx_uint64_t hostid, zbx_uint64_t parent_itemid, zbx_
 
 	if (SUCCEED == DBlld_item_exists(hostid, item->itemid, item->key, items))
 	{
-		*error = zbx_strdcatf(*error, "Cannot %s item [%s]: item already exists\n",
+		*error = zbx_strdcatf(*error, "Cannot %s item \"%s\": item already exists\n",
 				0 != item->itemid ? "update" : "create", item->key);
 		res = FAIL;
 		goto out;
 	}
 
 	item->name = zbx_strdup(NULL, name_proto);
-	substitute_discovery_macros(&item->name, jp_row);
+	substitute_discovery_macros(&item->name, jp_row, ZBX_MACRO_ANY, NULL, 0);
 
 	item->snmp_oid = zbx_strdup(NULL, snmp_oid_proto);
 	substitute_key_macros(&item->snmp_oid, NULL, NULL, jp_row, MACRO_TYPE_SNMP_OID, NULL, 0);
@@ -1098,11 +1109,11 @@ static int	DBlld_make_item(zbx_uint64_t hostid, zbx_uint64_t parent_itemid, zbx_
 	if (ITEM_TYPE_DB_MONITOR == type || ITEM_TYPE_SSH == type ||
 			ITEM_TYPE_TELNET == type || ITEM_TYPE_CALCULATED == type)
 	{
-		substitute_discovery_macros(&item->params, jp_row);
+		substitute_discovery_macros(&item->params, jp_row, ZBX_MACRO_ANY, NULL, 0);
 	}
 
 	item->description = zbx_strdup(NULL, description_proto);
-	substitute_discovery_macros(&item->description, jp_row);
+	substitute_discovery_macros(&item->description, jp_row, ZBX_MACRO_ANY, NULL, 0);
 
 	zbx_vector_ptr_append(items, item);
 
@@ -1581,7 +1592,7 @@ static int	DBlld_make_graph(zbx_uint64_t hostid, zbx_uint64_t parent_graphid, zb
 
 	graph = zbx_calloc(NULL, 1, sizeof(zbx_lld_graph_t));
 	graph->name = zbx_strdup(NULL, name_proto);
-	substitute_discovery_macros(&graph->name, jp_row);
+	substitute_discovery_macros(&graph->name, jp_row, ZBX_MACRO_ANY, NULL, 0);
 
 	name_esc = DBdyn_escape_string_len(graph->name, GRAPH_NAME_LEN);
 
@@ -1611,7 +1622,7 @@ static int	DBlld_make_graph(zbx_uint64_t hostid, zbx_uint64_t parent_graphid, zb
 			char	*old_name = NULL;
 
 			old_name = zbx_strdup(old_name, row[1]);
-			substitute_discovery_macros(&old_name, jp_row);
+			substitute_discovery_macros(&old_name, jp_row, ZBX_MACRO_ANY, NULL, 0);
 
 			if (0 == strcmp(old_name, row[2]))
 				ZBX_STR2UINT64(graph->graphid, row[0]);
@@ -1626,7 +1637,7 @@ static int	DBlld_make_graph(zbx_uint64_t hostid, zbx_uint64_t parent_graphid, zb
 
 	if (SUCCEED == DBlld_graph_exists(hostid, graph->graphid, graph->name, graphs))
 	{
-		*error = zbx_strdcatf(*error, "Cannot %s graph [%s]: graph already exists\n",
+		*error = zbx_strdcatf(*error, "Cannot %s graph \"%s\": graph already exists\n",
 				0 != graph->graphid ? "update" : "create", graph->name);
 		res = FAIL;
 		goto out;
