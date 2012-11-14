ce82a9483a10c8576777373faf76a88dfcef4def
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-11-14 12:47:29 +0000

    .......... [ZBX-5732] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@31447 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-5732/upgrades/dbpatches/1.6/oracle/patch/node_cksum.sql
@@ -0,0 +1,11 @@
+drop table node_cksum;
+
+CREATE TABLE node_cksum (
+	nodeid		number(10)	DEFAULT '0'	NOT NULL,
+	tablename	varchar2(64)	DEFAULT '',
+	recordid	number(20)	DEFAULT '0'	NOT NULL,
+	cksumtype	number(10)	DEFAULT '0'	NOT NULL,
+	cksum		clob		DEFAULT ''	NOT NULL,
+	sync		varchar2(128)	DEFAULT ''
+);
+CREATE INDEX node_cksum_cksum_1 on node_cksum (nodeid,tablename,recordid,cksumtype);
