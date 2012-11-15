6f586a301f2c233aa8739d18204ef74f115e8bfb
vedmak <vedmak@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-11-15 10:47:58 +0000

    .......... [ZBXNEXT-20] sync trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@31465 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-20/frontends/php/include/classes/import/CConfigurationImport.php
+++ ZBXNEXT-20/frontends/php/include/classes/import/CConfigurationImport.php
@@ -520,10 +520,12 @@ class CConfigurationImport {
 		}
 
 		// create the applications and create a hash hostid->name->applicationid
-		$newApplicationsIds = API::Application()->create($applicationsToCreate);
-		foreach ($newApplicationsIds['applicationids'] as $anum => $applicationId) {
-			$application = $applicationsToCreate[$anum];
-			$this->referencer->addApplicationRef($application['hostid'], $application['name'], $applicationId);
+		if (!empty($applicationsToCreate)) {
+			$newApplicationsIds = API::Application()->create($applicationsToCreate);
+			foreach ($newApplicationsIds['applicationids'] as $anum => $applicationId) {
+				$application = $applicationsToCreate[$anum];
+				$this->referencer->addApplicationRef($application['hostid'], $application['name'], $applicationId);
+			}
 		}
 
 		// refresh applications beacuse templated ones can be inherited to host and used in items
