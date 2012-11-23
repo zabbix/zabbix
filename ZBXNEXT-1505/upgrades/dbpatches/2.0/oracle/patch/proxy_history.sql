578bc82c687a566bd3c40610f8b0d41b56363c0f
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-11-23 13:39:31 +0000

    .......... [ZBXNEXT-1505] removed merged branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@31664 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1505/upgrades/dbpatches/2.0/oracle/patch/proxy_history.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE proxy_history MODIFY itemid DEFAULT NULL;
-ALTER TABLE proxy_history ADD ns number(10) DEFAULT '0' NOT NULL;
-ALTER TABLE proxy_history ADD status number(10) DEFAULT '0' NOT NULL;
