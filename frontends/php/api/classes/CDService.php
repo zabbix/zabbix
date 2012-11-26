d219154cc44ba9445165b120db67a03ffe102581
Pavels Jelisejevs <git-no-reply@zabbix.com>
2012-11-26 14:38:59 +0000

    A......... [ZBXNEXT-1491] removed unused code

--- frontends/php/api/classes/CDService.php
+++ frontends/php/api/classes/CDService.php
@@ -104,7 +104,6 @@ class CDService extends CZBXAPI{
 			'output'					=> API_OUTPUT_REFER,
 			'selectDRules'				=> null,
 			'selectDHosts'				=> null,
-			'selectDChecks'				=> null,
 			'selectHosts'				=> null,
 			'countOutput'				=> null,
 			'groupCount'				=> null,
@@ -270,10 +269,6 @@ class CDService extends CZBXAPI{
 					$result[$dservice['dserviceid']]['dhosts'] = array();
 				}
 
-				if (!is_null($options['selectDChecks']) && !isset($result[$dservice['dserviceid']]['dchecks'])) {
-					$result[$dservice['dserviceid']]['dchecks'] = array();
-				}
-
 				if (!is_null($options['selectHosts']) && !isset($result[$dservice['dserviceid']]['hosts'])) {
 					$result[$dservice['dserviceid']]['hosts'] = array();
 				}
@@ -292,14 +287,6 @@ class CDService extends CZBXAPI{
 						$result[$dservice['dserviceid']]['dhosts'] = array();
 
 					$result[$dservice['dserviceid']]['dhosts'][] = array('dhostid' => $dservice['dhostid']);
-				}
-
-				// dcheckids
-				if (isset($dservice['dcheckid']) && is_null($options['selectDChecks'])) {
-					if (!isset($result[$dservice['dserviceid']]['dchecks']))
-						$result[$dservice['dserviceid']]['dchecks'] = array();
-
-					$result[$dservice['dserviceid']]['dchecks'][] = array('dcheckid' => $dservice['dcheckid']);
 				}
 
 				$result[$dservice['dserviceid']] += $dservice;
