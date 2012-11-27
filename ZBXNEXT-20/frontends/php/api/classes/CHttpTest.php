c150506ec084553cfa205efc33d4b96418987ef9
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-11-27 09:54:43 +0000

    A.F....... [ZBXNEXT-20] fixed errors when requesting web scenarios with a non super admin user
    git-svn-id: svn://svn.zabbix.com/branches/dev@31732 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-20/frontends/php/api/classes/CHttpTest.php
+++ ZBXNEXT-20/frontends/php/api/classes/CHttpTest.php
@@ -87,7 +87,7 @@ class CHttpTest extends CZBXAPI {
 		if (USER_TYPE_SUPER_ADMIN == $userType || $options['nopermissions']) {
 		}
 		else {
-			$permission = $options['editable']?PERM_READ_WRITE:PERM_READ_ONLY;
+			$permission = $options['editable'] ? PERM_READ_WRITE : PERM_READ;
 
 			$sqlParts['from']['hosts_groups'] = 'hosts_groups hg';
 			$sqlParts['from']['rights'] = 'rights r';
