d4c9db08130d813eb2169ac03e8ec1b965267279
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-11-28 09:36:24 +0000

    .......... [ZBX-5900] created a new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@31757 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-5900/upgrades/dbpatches/2.0/oracle/patch/sysmaps_links.sql
@@ -0,0 +1,10 @@
+ALTER TABLE sysmaps_links MODIFY linkid DEFAULT NULL;
+ALTER TABLE sysmaps_links MODIFY sysmapid DEFAULT NULL;
+ALTER TABLE sysmaps_links MODIFY selementid1 DEFAULT NULL;
+ALTER TABLE sysmaps_links MODIFY selementid2 DEFAULT NULL;
+DELETE FROM sysmaps_links WHERE sysmapid NOT IN (SELECT sysmapid FROM sysmaps);
+DELETE FROM sysmaps_links WHERE selementid1 NOT IN (SELECT selementid FROM sysmaps_elements);
+DELETE FROM sysmaps_links WHERE selementid2 NOT IN (SELECT selementid FROM sysmaps_elements);
+ALTER TABLE sysmaps_links ADD CONSTRAINT c_sysmaps_links_1 FOREIGN KEY (sysmapid) REFERENCES sysmaps (sysmapid) ON DELETE CASCADE;
+ALTER TABLE sysmaps_links ADD CONSTRAINT c_sysmaps_links_2 FOREIGN KEY (selementid1) REFERENCES sysmaps_elements (selementid) ON DELETE CASCADE;
+ALTER TABLE sysmaps_links ADD CONSTRAINT c_sysmaps_links_3 FOREIGN KEY (selementid2) REFERENCES sysmaps_elements (selementid) ON DELETE CASCADE;
