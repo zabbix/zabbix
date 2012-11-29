1c21302ac7fddafb86b762e3b8a04c02b326d025
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-11-29 13:50:08 +0000

    ..F....... [ZBX-5904] fixed history filtering by items
    git-svn-id: svn://svn.zabbix.com/branches/dev@31791 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5904/frontends/php/include/views/js/monitoring.history.js.php
+++ ZBX-5904/frontends/php/include/views/js/monitoring.history.js.php
@@ -17,4 +17,27 @@
 			$('zbx_filter').submit();
 		}
 	}
+
+	function removeSelectedItems(formobject, name) {
+		formobject = $(formobject);
+		if (is_null(formobject)) {
+			return false;
+		}
+
+		for (var i = 0; i < formobject.options.length; i++) {
+			if (formobject.options[i].selected) {
+				if (formobject.options.length > 1) {
+					var obj = $(name + '_' + formobject.options[i].value);
+					if (!is_null(obj)) {
+						obj.remove();
+					}
+					$(formobject.options[i]).remove();
+				}
+				else {
+					alert(<?php echo CJs::encodeJson(_('Cannot remove all items, at least one item should remain.')); ?>);
+					return false;
+				}
+			}
+		}
+	}
 </script>
