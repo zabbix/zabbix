0cf85948410c7e61211f3e82c022353839e4a01a
alex <alex@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2012-12-03 09:09:41 +0000

    A.F.I..PS. [ZBXNEXT-1526] added support of configurable number of retries per WEB scenario
    git-svn-id: svn://svn.zabbix.com/trunk@31829 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/zabbix_server/httppoller/httptest.c
+++ src/zabbix_server/httppoller/httptest.c
@@ -419,16 +419,17 @@ static void	process_httptest(DC_HOST *host, DB_HTTPTEST *httptest)
 			goto httpstep_error;
 		}
 
-		memset(&page, 0, sizeof(page));
-
-		if (CURLE_OK != (err = curl_easy_perform(easyhandle)))
+		/* try to retrieve page several times depending on number of retries */
+		do
 		{
-			err_str = zbx_strdup(err_str, curl_easy_strerror(err));
-			zabbix_log(LOG_LEVEL_ERR, "web scenario step \"%s:%s\" error:"
-					" error doing curl_easy_perform: %s",
-					httptest->name, httpstep.name, err_str);
+			memset(&page, 0, sizeof(page));
+
+			if (CURLE_OK == (err = curl_easy_perform(easyhandle)))
+				break;
 		}
-		else
+		while (0 != --httptest->retries);
+
+		if (CURLE_OK == err)
 		{
 			if ('\0' != *httpstep.required && NULL == zbx_regexp_match(page.data, httpstep.required, NULL))
 			{
@@ -476,6 +477,13 @@ static void	process_httptest(DC_HOST *host, DB_HTTPTEST *httptest)
 				speed_download += stat.speed_download;
 				speed_download_num++;
 			}
+		}
+		else
+		{
+			err_str = zbx_strdup(err_str, curl_easy_strerror(err));
+			zabbix_log(LOG_LEVEL_ERR, "web scenario step \"%s:%s\" error:"
+					" error doing curl_easy_perform: %s",
+					httptest->name, httpstep.name, err_str);
 		}
 
 		zbx_free(page.data);
@@ -565,7 +573,7 @@ void	process_httptests(int httppoller_num, int now)
 
 	result = DBselect(
 			"select h.hostid,h.host,h.name,t.httptestid,t.name,t.macros,t.agent,"
-				"t.authentication,t.http_user,t.http_password,t.http_proxy"
+				"t.authentication,t.http_user,t.http_password,t.http_proxy,t.retries"
 			" from httptest t,hosts h"
 			" where t.hostid=h.hostid"
 				" and t.nextcheck<=%d"
@@ -613,6 +621,8 @@ void	process_httptests(int httppoller_num, int now)
 		httptest.http_proxy = zbx_strdup(NULL, row[10]);
 		substitute_simple_macros(NULL, &host.hostid, NULL, NULL, NULL,
 				&httptest.http_proxy, MACRO_TYPE_COMMON, NULL, 0);
+
+		httptest.retries = atoi(row[11]);
 
 		process_httptest(&host, &httptest);
 
