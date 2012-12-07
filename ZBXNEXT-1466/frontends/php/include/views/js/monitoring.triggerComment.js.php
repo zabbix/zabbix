b09c4ed2f8a216b74e3f1d5aa1400fd152cb7708
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-12-07 15:25:09 +0000

    ..F....... [ZBXNEXT-1466] fixed edit button in trigger comment page
    git-svn-id: svn://svn.zabbix.com/branches/dev@31975 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1466/frontends/php/include/views/js/monitoring.triggerComment.js.php
+++ ZBXNEXT-1466/frontends/php/include/views/js/monitoring.triggerComment.js.php
@@ -5,6 +5,7 @@
 		jQuery('#edit').click(function() {
 			jQuery('#comments').val(<?php echo CJs::encodeJson($this->data['trigger']['comments']); ?>);
 			jQuery('#comments').removeAttr('readonly');
+			jQuery('#edit').button('disable');
 			jQuery('#save').button('enable');
 		});
 	});
