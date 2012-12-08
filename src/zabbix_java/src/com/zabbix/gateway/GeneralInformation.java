4801399c364f075bb8ca21b95eacf704d10aedc2
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-12-08 13:45:01 +0000

    .D........ [ZBXNEXT-826] bump version to 2.0.5rc1
    git-svn-id: svn://svn.zabbix.com/branches/2.0@31985 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	public static final String REVISION_DATE = "08 December 2012";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "2.0.4";
+	public static final String VERSION = "2.0.5rc1";
 
 	public static void printVersion()
 	{
