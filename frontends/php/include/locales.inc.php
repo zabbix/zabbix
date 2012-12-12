d6aa6092b8f865233c1f98119ca34f3e5b07db90
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-12-12 08:39:08 +0000

    A.F....... [ZBX-1357] fix portuguese being listed as pt_BR, remove extra comma; thanks to pavels & vedmak
    git-svn-id: svn://svn.zabbix.com/branches/2.0@32079 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/locales.inc.php
+++ frontends/php/include/locales.inc.php
@@ -60,7 +60,7 @@ function set_zbx_locales() {
 		'fa_IR' => _('Persian (fa_IR)'),
 		'pl_PL' => _('Polish (pl_PL)'),
 		'pt_BR' => _('Portuguese (pt_BR)'),
-		'pt_PT' => _('Portuguese (pt_BR)'),
+		'pt_PT' => _('Portuguese (pt_PT)'),
 		'ru_RU' => _('Russian (ru_RU)'),
 		'sk_SK' => _('Slovak (sk_SK)'),
 		'es_ES' => _('Spanish (es_ES)'),
@@ -160,7 +160,7 @@ function zbx_locale_variants_win($language) {
 		'es_es' => array('Spanish_Spain.1252', 'spanish'),
 		'sv_se' => array('Swedish_Sweden.1252', 'swedish'),
 		'tr_tr' => array('Turkish_Turkey.1254', 'turkish'),
-		'uk_ua' => array('Ukrainian_Ukraine.1251', 'ukrainian'),
+		'uk_ua' => array('Ukrainian_Ukraine.1251', 'ukrainian')
 	);
 	return $winLanguageName[strtolower($language)];
 }
