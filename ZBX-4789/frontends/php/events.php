c4ec14c1a054e8b9c83afb28b76999e7f3d7253d
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-12-12 11:34:21 +0000

    ..F....... [ZBX-4789] the code improvements
    git-svn-id: svn://svn.zabbix.com/branches/dev@32099 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4789/frontends/php/events.php
+++ ZBX-4789/frontends/php/events.php
@@ -724,9 +724,12 @@ else {
 				);
 
 				// host JS menu link
-				$hostSpan = new CSpan($host['name'], 'link_menu menu-host');
-				$scripts = isset($hostScripts[$host['hostid']]) ? $hostScripts[$host['hostid']] : array();
-				$hostSpan->setAttribute('data-menu', hostMenuData($host, $scripts));
+				$hostSpan = null;
+				if ($_REQUEST['hostid'] == 0) {
+					$hostSpan = new CSpan($host['name'], 'link_menu menu-host');
+					$scripts = $hostScripts[$host['hostid']];
+					$hostSpan->setAttribute('data-menu', hostMenuData($host, $scripts));
+				}
 
 				$table->addRow(array(
 					new CLink(zbx_date2str(EVENTS_ACTION_TIME_FORMAT, $event['clock']),
@@ -734,7 +737,7 @@ else {
 						'action'
 					),
 					is_show_all_nodes() ? get_node_name_by_elid($event['objectid']) : null,
-					$_REQUEST['hostid'] ? null : $hostSpan,
+					$hostSpan,
 					new CSpan($tr_desc, 'link_menu'),
 					$statusSpan,
 					getSeverityCell($trigger['priority'], null, !$event['value']),
