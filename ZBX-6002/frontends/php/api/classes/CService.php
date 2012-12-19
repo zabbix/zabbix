38bc7ca5f0b37f71b560af67f732f4347d273e12
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2012-12-19 09:19:26 +0000

    A......... [ZBX-6002] optimizes sql query in Service API to get latest service alarms
    git-svn-id: svn://svn.zabbix.com/branches/dev@32226 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6002/frontends/php/api/classes/CService.php
+++ ZBX-6002/frontends/php/api/classes/CService.php
@@ -1008,14 +1008,16 @@ class CService extends CZBXAPI {
 		// since multiple alarms can have the same timestamp, we only need to save the last one
 		$query = DBSelect(
 			'SELECT sa.serviceid,sa.value
-				FROM service_alarms sa
-					LEFT OUTER JOIN service_alarms sa2 ON (sa.serviceid=sa2.serviceid AND sa.clock<sa2.clock AND sa2.clock<'.zbx_dbstr($beforeTime).')
-				WHERE sa2.servicealarmid IS NULL
-					AND sa.clock<'.zbx_dbstr($beforeTime).'
-					AND '.DBcondition('sa.serviceid', $serviceIds).'
-				ORDER BY sa.servicealarmid'
+				FROM (SELECT MAX(sa3.servicealarmid) AS servicealarmid
+						FROM (SELECT sa2.serviceid,MAX(sa2.clock) AS clock
+								FROM service_alarms sa2
+								WHERE sa2.clock<'.zbx_dbstr($beforeTime).
+								' AND '.DBcondition('sa2.serviceid', $serviceIds).
+							' GROUP BY sa2.serviceid) AS ss
+						JOIN service_alarms sa3 ON sa3.serviceid = ss.serviceid and sa3.clock = ss.clock
+					GROUP BY sa3.serviceid) AS ss2
+				JOIN service_alarms sa ON sa.servicealarmid = ss2.servicealarmid'
 		);
-
 		$rs = array();
 		while ($alarm = DBfetch($query)) {
 			$rs[$alarm['serviceid']] = $alarm['value'];
