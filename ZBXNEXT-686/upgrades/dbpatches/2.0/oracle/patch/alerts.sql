14edbf25d9b1998b62ea1b3291a922718d6ebb0e
egita <egita@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-03 10:22:38 +0000

    .......... [ZBXNEXT-686] created branch for CTriggerExpressionTest update
    git-svn-id: svn://svn.zabbix.com/branches/dev@32400 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-686/upgrades/dbpatches/2.0/oracle/patch/alerts.sql
@@ -0,0 +1,17 @@
+ALTER TABLE alerts MODIFY alertid DEFAULT NULL;
+ALTER TABLE alerts MODIFY actionid DEFAULT NULL;
+ALTER TABLE alerts MODIFY eventid DEFAULT NULL;
+ALTER TABLE alerts MODIFY userid DEFAULT NULL;
+ALTER TABLE alerts MODIFY userid NULL;
+ALTER TABLE alerts MODIFY mediatypeid DEFAULT NULL;
+ALTER TABLE alerts MODIFY mediatypeid NULL;
+UPDATE alerts SET userid=NULL WHERE userid=0;
+UPDATE alerts SET mediatypeid=NULL WHERE mediatypeid=0;
+DELETE FROM alerts WHERE NOT actionid IN (SELECT actionid FROM actions);
+DELETE FROM alerts WHERE NOT eventid IN (SELECT eventid FROM events);
+DELETE FROM alerts WHERE NOT userid IN (SELECT userid FROM users);
+DELETE FROM alerts WHERE NOT mediatypeid IN (SELECT mediatypeid FROM media_type);
+ALTER TABLE alerts ADD CONSTRAINT c_alerts_1 FOREIGN KEY (actionid) REFERENCES actions (actionid) ON DELETE CASCADE;
+ALTER TABLE alerts ADD CONSTRAINT c_alerts_2 FOREIGN KEY (eventid) REFERENCES events (eventid) ON DELETE CASCADE;
+ALTER TABLE alerts ADD CONSTRAINT c_alerts_3 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
+ALTER TABLE alerts ADD CONSTRAINT c_alerts_4 FOREIGN KEY (mediatypeid) REFERENCES media_type (mediatypeid) ON DELETE CASCADE;
