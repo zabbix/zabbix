a17387b1a10c5738fd5a8ed960d5413ce542b845
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-04 14:54:47 +0000

    ..F....... [ZBXNEXT-1399] fixed chosen text entering
    git-svn-id: svn://svn.zabbix.com/branches/dev@32460 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1399/frontends/php/js/vendors/chosen/chosen.jquery.js
+++ ZBXNEXT-1399/frontends/php/js/vendors/chosen/chosen.jquery.js
@@ -564,7 +564,6 @@ Copyright (c) 2011 by Harvest
       }
       this.search_field_disabled();
       this.show_search_field_default();
-      this.search_field_scale();
       this.search_results.html(content);
       return this.parsing = false;
     };
