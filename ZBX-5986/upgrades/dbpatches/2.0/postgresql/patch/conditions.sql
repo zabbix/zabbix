9c95b1da414838e400df93145a2ca5975bba8981
tomtom <tomtom@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-04 10:17:01 +0000

    .......... [ZBX-5986] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@32435 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5986/upgrades/dbpatches/2.0/postgresql/patch/conditions.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ONLY conditions ALTER conditionid DROP DEFAULT,
-			    ALTER actionid DROP DEFAULT;
-DELETE FROM conditions WHERE NOT EXISTS (SELECT 1 FROM actions WHERE actions.actionid=conditions.actionid);
-ALTER TABLE ONLY conditions ADD CONSTRAINT c_conditions_1 FOREIGN KEY (actionid) REFERENCES actions (actionid) ON DELETE CASCADE;
