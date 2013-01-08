02762c7571ebfe3d5cb16cd23c3d3ba1d3a69ac9
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-08 09:26:09 +0000

    ...G...... [ZBX-5201] handle perfcounter error PDH_CALC_NEGATIVE_DENOMINATOR
    The counter to calculate rate values such as system.cpu.util shows the
    average percentage of active time observed during the sample
    interval. This is an inverse counter. Inverse counters are calculated
    by monitoring the percentage of time that the service was inactive and
    then subtracting that value from 100 percent.
    
    The formula used to calculate the value is:
    
      (1 - (inactive time delta) / (total time delta)) x 100
    
    where delta is a result of subtraction of new value and previous
    value.
    
    It's logical to assume that inactive time is always less than or equal
    to the total time. For some unknown reason sometimes performance
    counter return values in such a way that inactive delta is greater
    than total. There must be some kind of bug in how performance counters
    work. When this happens the function PdhCalculateCounterFromRawValue()
    returns error PDH_CALC_NEGATIVE_DENOMINATOR.
    
    As tests show when this happens the inactive time equals total time
    which means zero result.
    
    Before the fix:
      - item becames unsupported if performance counter receives "negative
        denominator" error
    
    After the fix:
      - zero (0.0) is returned and item does not become unsupported if
        performance counter receives "negative denominator" error
    
    More info: technet.microsoft.com/en-us/library/cc757283(WS.10).aspx
    
    
    git-svn-id: svn://svn.zabbix.com/branches/1.8@32540 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_agent/perfstat.c
+++ src/zabbix_agent/perfstat.c
@@ -36,6 +36,8 @@ static ZBX_MUTEX		perfstat_access;
  ******************************************************************************/
 static void	deactivate_perf_counter(PERF_COUNTER_DATA *counter)
 {
+	zabbix_log(LOG_LEVEL_DEBUG, "deactivate_perf_counter() counterpath:'%s'", counter->counterpath);
+
 	counter->status = PERF_COUNTER_NOTSUPPORTED;
 	counter->value_count = 0;
 	counter->value_current = -1;
@@ -99,7 +101,8 @@ PERF_COUNTER_DATA	*add_perf_counter(const char *name, const char *counterpath, i
 
 			if (ERROR_SUCCESS != pdh_status && PDH_CSTATUS_NO_INSTANCE != pdh_status)
 			{
-				zabbix_log(LOG_LEVEL_WARNING, "PerfCounter '%s' FAILED: invalid format", counterpath);
+				zabbix_log(LOG_LEVEL_WARNING, "cannot add performance counter \"%s\": invalid format",
+						counterpath);
 				cptr = NULL;	/* indicate a failure */
 			}
 
@@ -116,7 +119,7 @@ PERF_COUNTER_DATA	*add_perf_counter(const char *name, const char *counterpath, i
 
 	if (FAIL == result)
 	{
-		zabbix_log(LOG_LEVEL_DEBUG, "%s() counter '%s' (interval: %d) already exists", __function_name, counterpath, interval);
+		zabbix_log(LOG_LEVEL_DEBUG, "%s() counter '%s' already exists", __function_name, counterpath);
 	}
 	else if (NULL != name)
 	{
@@ -219,6 +222,7 @@ double	compute_average_value(const char *function, PERF_COUNTER_DATA *counter, i
 int	init_perf_collector(ZBX_PERF_STAT_DATA *pperf)
 {
 	const char	*__function_name = "init_perf_collector";
+	int		ret = FAIL;
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
 
@@ -231,13 +235,15 @@ int	init_perf_collector(ZBX_PERF_STAT_DATA *pperf)
 	}
 
 	if (ERROR_SUCCESS != zbx_PdhOpenQuery(__function_name, &ppsd->pdh_query))
-		return FAIL;
+		goto out;
 
 	ppsd->nextcheck = time(NULL) + UNSUPPORTED_REFRESH_PERIOD;
 
-	zabbix_log(LOG_LEVEL_DEBUG, "End of %s()", __function_name);
+	ret = SUCCEED;
+out:
+	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%s", __function_name, zbx_result_string(ret));
 
-	return SUCCEED;
+	return ret;
 }
 
 void	free_perf_collector()
@@ -305,10 +311,10 @@ void	collect_perfstat()
 				deactivate_perf_counter(cptr);
 		}
 
-		zabbix_log(LOG_LEVEL_DEBUG, "call to PdhCollectQueryData() failed: %s",
-				strerror_from_module(pdh_status, L"PDH.DLL"));
+		zabbix_log(LOG_LEVEL_DEBUG, "%s() call to PdhCollectQueryData() failed: %s",
+				__function_name, strerror_from_module(pdh_status, L"PDH.DLL"));
 
-		return;
+		goto out;
 	}
 
 	/* get the raw values */
@@ -324,36 +330,95 @@ void	collect_perfstat()
 			continue;
 		}
 
+		if (PERF_COUNTER_INITIALIZED < cptr->status)
+		{
+			zabbix_log(LOG_LEVEL_DEBUG, "%s() counterpath:'%s' old first:%I64d second:%I64d",
+					__function_name, cptr->counterpath,
+					cptr->rawValues[(cptr->olderRawValue + 1) % 2].FirstValue,
+					cptr->rawValues[(cptr->olderRawValue + 1) % 2].SecondValue);
+
+			zabbix_log(LOG_LEVEL_DEBUG, "%s() counterpath:'%s' new first:%I64d second:%I64d",
+					__function_name, cptr->counterpath,
+					cptr->rawValues[cptr->olderRawValue].FirstValue,
+					cptr->rawValues[cptr->olderRawValue].SecondValue);
+		}
+
 		cptr->olderRawValue = (cptr->olderRawValue + 1) % 2;
 
-		pdh_status = PdhCalculateCounterFromRawValue(
-				cptr->handle,
-				PDH_FMT_DOUBLE,
+		pdh_status = PdhCalculateCounterFromRawValue(cptr->handle, PDH_FMT_DOUBLE,
 				&cptr->rawValues[(cptr->olderRawValue + 1) % 2],						/* the new value */
 				(PERF_COUNTER_INITIALIZED < cptr->status ? &cptr->rawValues[cptr->olderRawValue] : NULL),	/* the older value */
 				&value);
 
 		if (ERROR_SUCCESS == pdh_status)
 		{
-			cptr->status = PERF_COUNTER_ACTIVE;
-			cptr->value_current = (cptr->value_current + 1) % cptr->interval;
-			if (cptr->value_count == cptr->interval)
-				cptr->sum -= cptr->value_array[cptr->value_current];	/* remove the oldest value, value_count will not increase */
-			cptr->value_array[cptr->value_current] = value.doubleValue;
-			cptr->sum += cptr->value_array[cptr->value_current];
-			if (cptr->value_count < cptr->interval)
-				cptr->value_count++;
-		}
-		else if (PDH_CSTATUS_INVALID_DATA == pdh_status)
-		{
-			/* some (e.g., rate) counters require two raw values, MSDN lacks documentation */
-			/* about what happens but tests show that PDH_CSTATUS_INVALID_DATA is returned */
+			if (PDH_CSTATUS_INVALID_DATA == value.CStatus)
+			{
+				/* some (e.g., rate) counters require two raw values, MSDN lacks documentation */
+				/* about what happens but tests show that PDH_CSTATUS_INVALID_DATA is returned */
 
-			cptr->status = PERF_COUNTER_GET_SECOND_VALUE;
+				cptr->status = PERF_COUNTER_GET_SECOND_VALUE;
+				continue;
+			}
+
+			if (PDH_CALC_NEGATIVE_DENOMINATOR == value.CStatus)
+			{
+				/* This counter type shows the average percentage of active time observed during the  */
+				/* sample interval. This is an inverse counter. Inverse counters are calculated by    */
+				/* monitoring the percentage of time that the service was inactive and then           */
+				/* subtracting that value from 100 percent. The formula used to calculate the counter */
+				/* value is: (1 - (inactive time delta) / (total time delta)) x 100                   */
+				/* For some unknown reason sometimes the collected row values indicate that inactive  */
+				/* delta is greater than total time delta. There must be some kind of bug in how      */
+				/* performance counters work. When this happens function                              */
+				/* PdhCalculateCounterFromRawValue() returns error PDH_CALC_NEGATIVE_DENOMINATOR.     */
+				/* Basically this means that an item was inactive all the time so we set the return   */
+				/* value to 0.0 and change status to indicate success.                                */
+				/* More info: technet.microsoft.com/en-us/library/cc757283(WS.10).aspx                */
+
+				zabbix_log(LOG_LEVEL_DEBUG, "%s() counterpath:'%s'"
+						" negative denominator error is treated as 0.0",
+						__function_name, cptr->counterpath);
+
+				value.doubleValue = 0.0;	/* 100% inactive */
+			}
+
+			if (PDH_CSTATUS_VALID_DATA == value.CStatus || PDH_CSTATUS_NEW_DATA == value.CStatus ||
+					PDH_CALC_NEGATIVE_DENOMINATOR == value.CStatus)
+			{
+				zabbix_log(LOG_LEVEL_DEBUG, "%s() '%s' calculated value:" ZBX_FS_DBL, __function_name,
+						cptr->counterpath, value.doubleValue);
+
+				cptr->status = PERF_COUNTER_ACTIVE;
+				cptr->value_current = (cptr->value_current + 1) % cptr->interval;
+
+				/* remove the oldest value, value_count will not increase */
+				if (cptr->value_count == cptr->interval)
+					cptr->sum -= cptr->value_array[cptr->value_current];
+
+				cptr->value_array[cptr->value_current] = value.doubleValue;
+				cptr->sum += cptr->value_array[cptr->value_current];
+				if (cptr->value_count < cptr->interval)
+					cptr->value_count++;
+			}
+			else
+			{
+				zabbix_log(LOG_LEVEL_WARNING, "cannot calculate performance counter value \"%s\": %s",
+						cptr->counterpath, strerror_from_module(value.CStatus, L"PDH.DLL"));
+
+				deactivate_perf_counter(cptr);
+			}
 		}
 		else
+		{
+			zabbix_log(LOG_LEVEL_WARNING, "cannot calculate performance counter value \"%s\": %s",
+					cptr->counterpath, strerror_from_module(pdh_status, L"PDH.DLL"));
+
 			deactivate_perf_counter(cptr);
+		}
 	}
+out:
+	zabbix_log(LOG_LEVEL_DEBUG, "End of %s()", __function_name);
 }
 
 #endif	/* _WINDOWS */
