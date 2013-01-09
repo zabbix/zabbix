9d614c9ebc81a966b310a670659aad12f46583f3
egita <egita@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-09 13:40:31 +0000

    .......... [ZBXNEXT-686] fixed tests in testFormAction
    git-svn-id: svn://svn.zabbix.com/trunk@32616 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/tests/selenium/testFormAction.php
+++ frontends/php/tests/selenium/testFormAction.php
@@ -63,7 +63,7 @@ class testFormAction extends CWebTest {
 	 * @dataProvider providerNewActions
 	 */
 	public function testActionCreateSimple($action) {
-		DBsave_tables('actions');
+//		DBsave_tables('actions');
 
 		$this->login('actionconf.php?form=1&eventsource=0');
 		$this->checkTitle('Configuration of actions');
@@ -137,16 +137,19 @@ class testFormAction extends CWebTest {
 			$this->wait();
 		}
 		$this->type('esc_period', $action['esc_period']);
+		$this->button_click('operationTab');
+
+		$this->wait();
 		$this->button_click('save');
 
 		$this->wait();
 		$this->ok('Action added');
 
-		DBrestore_tables('actions');
+//		DBrestore_tables('actions');
 	}
 
 	public function testActionCreate() {
-		DBsave_tables('actions');
+	//	DBsave_tables('actions');
 
 		$this->login('actionconf.php?form=1&eventsource=0');
 		$this->checkTitle('Configuration of actions');
@@ -215,7 +218,7 @@ class testFormAction extends CWebTest {
 		$this->waitForPopUp("zbx_popup", "30000");
 		$this->selectWindow("name=zbx_popup");
 		$this->dropdown_select_wait('groupid', 'Zabbix servers');
-		$this->button_click("spanid10053");
+		$this->button_click("spanid10084");
 		$this->selectWindow("null");
 		$this->button_click("//input[@name='save']");
 
@@ -259,7 +262,7 @@ class testFormAction extends CWebTest {
 		$this->wait();
 		$this->ok("Action added");
 
-		DBrestore_tables('actions');
+	//	DBrestore_tables('actions');
 	}
 }
 ?>
