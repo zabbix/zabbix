923bbd520a30d995292fd49a7c19eb11f4d287f3
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-14 16:15:52 +0000

    ....I..... [ZBX-5532] added MySQL patch
    git-svn-id: svn://svn.zabbix.com/branches/dev@32746 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5532/upgrades/dbpatches/2.0/mysql/patch/services_times.sql
+++ ZBX-5532/upgrades/dbpatches/2.0/mysql/patch/services_times.sql
@@ -2,3 +2,8 @@ ALTER TABLE services_times MODIFY timeid bigint unsigned NOT NULL,
 			   MODIFY serviceid bigint unsigned NOT NULL;
 DELETE FROM services_times WHERE NOT serviceid IN (SELECT serviceid FROM services);
 ALTER TABLE services_times ADD CONSTRAINT c_services_times_1 FOREIGN KEY (serviceid) REFERENCES services (serviceid) ON DELETE CASCADE;
+
+UPDATE services_times
+	SET ts_from = 86400 * DAYOFWEEK(FROM_UNIXTIME(ts_from)) + (ts_from - UNIX_TIMESTAMP(STR_TO_DATE(DATE_FORMAT(FROM_UNIXTIME(ts_from), '%Y%m%d'), '%Y%m%d'))),
+		ts_to = 86400 * DAYOFWEEK(FROM_UNIXTIME(ts_to)) + (ts_to - UNIX_TIMESTAMP(STR_TO_DATE(DATE_FORMAT(FROM_UNIXTIME(ts_to), '%Y%m%d'), '%Y%m%d')))
+	WHERE type IN (0,1);
