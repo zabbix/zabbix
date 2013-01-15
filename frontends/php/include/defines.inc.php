9865ee994a7ebd3a12b954b7a596ecde7778ae96
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-15 19:49:36 +0000

    A......... [ZBXNEXT-826] bump api version to 2.0.5
    git-svn-id: svn://svn.zabbix.com/branches/2.0@32775 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -20,7 +20,7 @@
 
 
 define('ZABBIX_VERSION',		'2.0.5rc1');
-define('ZABBIX_API_VERSION',	'2.0.4');
+define('ZABBIX_API_VERSION',	'2.0.5');
 
 define('ZABBIX_COPYRIGHT_FROM', '2001');
 define('ZABBIX_COPYRIGHT_TO', '2012');
