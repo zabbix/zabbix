a1a484229a1b93089d431ed8e9aed60db7edda6e
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-21 12:58:43 +0000

    ..F....... [ZBX-6109] fixed type of *.isnow profile records
    git-svn-id: svn://svn.zabbix.com/branches/dev@32969 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6109/src/libs/zbxdbupgrade/dbupgrade.c
+++ ZBX-6109/src/libs/zbxdbupgrade/dbupgrade.c
@@ -665,6 +665,21 @@ static int	DBpatch_02010029()
 
 	return FAIL;
 }
+
+static int	DBpatch_02010030()
+{
+	const char	*sql =
+			"update profiles"
+			" set value_int=case when value_str='1' then 1 else 0 end,"
+				"value_str='',"
+				"type=2"	/* PROFILE_TYPE_INT */
+			" where idx like '%isnow'";
+
+	if (ZBX_DB_OK <= DBexecute("%s", sql))
+		return SUCCEED;
+
+	return FAIL;
+}
 #endif	/* not HAVE_SQLITE3 */
 
 static void	DBget_version(int *mandatory, int *optional)
@@ -732,6 +747,7 @@ int	DBcheck_version()
 		{DBpatch_02010027, 2010027, 0, 1},
 		{DBpatch_02010028, 2010028, 0, 0},
 		{DBpatch_02010029, 2010029, 0, 0},
+		{DBpatch_02010030, 2010030, 0, 0},
 		/* IMPORTANT! When adding a new mandatory DBPatch don't forget to update it for SQLite, too. */
 		{NULL}
 	};
