d6aa6a1acd7d7526397dda7a3f2c39dc1da21bbd
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-21 14:53:43 +0000

    .......... [ZBX-6057] updated to the latest 1.8
    git-svn-id: svn://svn.zabbix.com/branches/dev@32977 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6057-1.8/include/common.h
+++ ZBX-6057-1.8/include/common.h
@@ -121,8 +121,8 @@
 #define OFF	0
 
 #define	APPLICATION_NAME	"Zabbix Agent"
-#define	ZABBIX_REVDATE		"10 January 2013"
-#define	ZABBIX_VERSION		"1.8.16"
+#define	ZABBIX_REVDATE		"16 January 2013"
+#define	ZABBIX_VERSION		"1.8.17rc1"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
 #if defined(_WINDOWS)
