80afea461bea0cc9d72839cd11827c15f523a62e
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-21 12:56:48 +0000

    .......... [ZBX-6109] created the new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@32968 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-6109/frontends/php/include/classes/core/ZBase.php
@@ -0,0 +1,366 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2012 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+require_once dirname(__FILE__).'/CAutoloader.php';
+
+class ZBase {
+	const EXEC_MODE_DEFAULT = 'default';
+	const EXEC_MODE_SETUP = 'setup';
+	const EXEC_MODE_API = 'api';
+
+	/**
+	 * An instance of the current Z object.
+	 *
+	 * @var Z
+	 */
+	protected static $instance;
+
+	/**
+	 * The absolute path to the root directory.
+	 *
+	 * @var string
+	 */
+	protected $rootDir;
+
+	/**
+	 * Session object.
+	 *
+	 * @var CSession
+	 */
+	protected $session;
+
+	/**
+	 * @var array of config data from zabbix config file
+	 */
+	protected $config = array();
+
+	/**
+	 * Returns the current instance of Z.
+	 *
+	 * @static
+	 *
+	 * @return Z
+	 */
+	public static function getInstance() {
+		if (self::$instance === null) {
+			self::$instance = new Z();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Init modules required to run frontend.
+	 */
+	protected function init() {
+		$this->rootDir = $this->findRootDir();
+		$this->registerAutoloader();
+
+		// system includes
+		require_once $this->getRootDir().'/include/debug.inc.php';
+		require_once $this->getRootDir().'/include/gettextwrapper.inc.php';
+		require_once $this->getRootDir().'/include/defines.inc.php';
+		require_once $this->getRootDir().'/include/func.inc.php';
+		require_once $this->getRootDir().'/include/html.inc.php';
+		require_once $this->getRootDir().'/include/perm.inc.php';
+		require_once $this->getRootDir().'/include/audit.inc.php';
+		require_once $this->getRootDir().'/include/js.inc.php';
+		require_once $this->getRootDir().'/include/users.inc.php';
+		require_once $this->getRootDir().'/include/validate.inc.php';
+		require_once $this->getRootDir().'/include/profiles.inc.php';
+		require_once $this->getRootDir().'/include/locales.inc.php';
+		require_once $this->getRootDir().'/include/db.inc.php';
+		require_once $this->getRootDir().'/include/nodes.inc.php';
+
+		// page specific includes
+		require_once $this->getRootDir().'/include/acknow.inc.php';
+		require_once $this->getRootDir().'/include/actions.inc.php';
+		require_once $this->getRootDir().'/include/discovery.inc.php';
+		require_once $this->getRootDir().'/include/events.inc.php';
+		require_once $this->getRootDir().'/include/graphs.inc.php';
+		require_once $this->getRootDir().'/include/hosts.inc.php';
+		require_once $this->getRootDir().'/include/httptest.inc.php';
+		require_once $this->getRootDir().'/include/ident.inc.php';
+		require_once $this->getRootDir().'/include/images.inc.php';
+		require_once $this->getRootDir().'/include/items.inc.php';
+		require_once $this->getRootDir().'/include/maintenances.inc.php';
+		require_once $this->getRootDir().'/include/maps.inc.php';
+		require_once $this->getRootDir().'/include/media.inc.php';
+		require_once $this->getRootDir().'/include/services.inc.php';
+		require_once $this->getRootDir().'/include/sounds.inc.php';
+		require_once $this->getRootDir().'/include/triggers.inc.php';
+		require_once $this->getRootDir().'/include/valuemap.inc.php';
+	}
+
+	/**
+	 * Initializes the application.
+	 */
+	public function run($mode = self::EXEC_MODE_DEFAULT) {
+		$this->init();
+
+		$this->setMaintenanceMode();
+		$this->setErrorHandler();
+
+		switch ($mode) {
+			case self::EXEC_MODE_DEFAULT:
+				$this->loadConfigFile();
+				$this->initDB();
+				$this->initNodes();
+				$this->authenticateUser();
+				// init nodes after user is authenticated
+				init_nodes();
+				$this->initLocales();
+				break;
+			case self::EXEC_MODE_API:
+				$this->loadConfigFile();
+				$this->initDB();
+				$this->initNodes();
+				$this->initLocales();
+				break;
+			case self::EXEC_MODE_SETUP:
+				try {
+					// try to load config file, if it exists we need to init db and authenticate user to check permissions
+					$this->loadConfigFile();
+					$this->initDB();
+					$this->initNodes();
+					$this->authenticateUser();
+					// init nodes after user is authenticated
+					init_nodes();
+					$this->initLocales();
+					DBclose();
+				}
+				catch (ConfigFileException $e) {}
+				break;
+		}
+	}
+
+	/**
+	 * Returns the absolute path to the root dir.
+	 *
+	 * @return string
+	 */
+	public static function getRootDir() {
+		return self::getInstance()->rootDir;
+	}
+
+	/**
+	 * Returns the path to the frontend's root dir.
+	 *
+	 * @return string
+	 */
+	private function findRootDir() {
+		return realpath(dirname(__FILE__).'/../../..');
+	}
+
+	/**
+	 * Register autoloader.
+	 */
+	private function registerAutoloader() {
+		$autoloader = new CAutoloader($this->getIncludePaths());
+		$autoloader->register();
+	}
+
+	/**
+	 * An array of directories to add to the autoloader include paths.
+	 *
+	 * @return array
+	 */
+	private function getIncludePaths() {
+		return array(
+			$this->rootDir.'/include/classes',
+			$this->rootDir.'/include/classes/core',
+			$this->rootDir.'/include/classes/api',
+			$this->rootDir.'/include/classes/db',
+			$this->rootDir.'/include/classes/debug',
+			$this->rootDir.'/include/classes/validators',
+			$this->rootDir.'/include/classes/export',
+			$this->rootDir.'/include/classes/export/writers',
+			$this->rootDir.'/include/classes/export/elements',
+			$this->rootDir.'/include/classes/import',
+			$this->rootDir.'/include/classes/import/importers',
+			$this->rootDir.'/include/classes/import/readers',
+			$this->rootDir.'/include/classes/import/formatters',
+			$this->rootDir.'/include/classes/screens',
+			$this->rootDir.'/include/classes/sysmaps',
+			$this->rootDir.'/include/classes/helpers',
+			$this->rootDir.'/include/classes/helpers/trigger',
+			$this->rootDir.'/include/classes/macros',
+			$this->rootDir.'/include/classes/tree',
+			$this->rootDir.'/include/classes/html',
+			$this->rootDir.'/include/classes/parsers',
+			$this->rootDir.'/api/classes',
+			$this->rootDir.'/api/classes/managers',
+			$this->rootDir.'/api/rpc'
+		);
+	}
+
+	/**
+	 * An array of available themes.
+	 *
+	 * @return array
+	 */
+	public static function getThemes() {
+		return array(
+			'classic' => _('Classic'),
+			'originalblue' => _('Original blue'),
+			'darkblue' => _('Black & Blue'),
+			'darkorange' => _('Dark orange')
+		);
+	}
+
+	/**
+	 * Return session object.
+	 *
+	 * @return CSession
+	 */
+	public function getSession() {
+		if ($this->session === null) {
+			$this->session = new CSession();
+		}
+
+		return $this->session;
+	}
+
+	/**
+	 * Set custom error handler for PHP errors.
+	 */
+	protected function setErrorHandler() {
+		function zbx_err_handler($errno, $errstr, $errfile, $errline) {
+			// necessary to surpress errors when calling with error control operator like @function_name()
+			if (error_reporting() === 0) {
+				return true;
+			}
+
+			$pathLength = strlen(__FILE__);
+
+			$pathLength -= 22;
+			$errfile = substr($errfile, $pathLength);
+
+			error($errstr.' ['.$errfile.':'.$errline.']');
+		}
+
+		set_error_handler('zbx_err_handler');
+	}
+
+	/**
+	 * Check if maintenance mode is enabled.
+	 *
+	 * @throws Exception
+	 */
+	protected function setMaintenanceMode() {
+		require_once $this->getRootDir().'/conf/maintenance.inc.php';
+
+		if (defined('ZBX_DENY_GUI_ACCESS')) {
+			$user_ip = (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+					? $_SERVER['HTTP_X_FORWARDED_FOR']
+					: $_SERVER['REMOTE_ADDR'];
+			if (!isset($ZBX_GUI_ACCESS_IP_RANGE) || !in_array($user_ip, $ZBX_GUI_ACCESS_IP_RANGE)) {
+				throw new Exception($_REQUEST['warning_msg']);
+			}
+		}
+	}
+
+	/**
+	 * Load zabbix config file.
+	 */
+	protected function loadConfigFile() {
+		$configFile = $this->getRootDir().CConfigFile::CONFIG_FILE_PATH;
+		$config = new CConfigFile($configFile);
+		$this->config = $config->load();
+	}
+
+	/**
+	 * Check if frontend can connect to DB.
+	 * @throws DBException
+	 */
+	protected function initDB() {
+		$error = null;
+		if (!DBconnect($error)) {
+			throw new DBException($error);
+		}
+	}
+
+	/**
+	 * Check if distributed monitoring is enabled.
+	 */
+	protected function initNodes() {
+		global $ZBX_LOCALNODEID, $ZBX_LOCMASTERID, $ZBX_NODES;
+
+		if ($local_node_data = DBfetch(DBselect('SELECT n.* FROM nodes n WHERE n.nodetype=1 ORDER BY n.nodeid'))) {
+			$ZBX_LOCALNODEID = $local_node_data['nodeid'];
+			$ZBX_LOCMASTERID = $local_node_data['masterid'];
+			$ZBX_NODES[$local_node_data['nodeid']] = $local_node_data;
+			define('ZBX_DISTRIBUTED', true);
+		}
+		else {
+			define('ZBX_DISTRIBUTED', false);
+		}
+	}
+
+	/**
+	 * Initilaize translations.
+	 */
+	protected function initLocales() {
+		init_mbstrings();
+
+		if (function_exists('bindtextdomain')) {
+			// initializing gettext translations depending on language selected by user
+			$locales = zbx_locale_variants(CWebUser::$data['lang']);
+			$locale_found = false;
+			foreach ($locales as $locale) {
+				putenv('LC_ALL='.$locale);
+				putenv('LANG='.$locale);
+				putenv('LANGUAGE='.$locale);
+
+				if (setlocale(LC_ALL, $locale)) {
+					$locale_found = true;
+					CWebUser::$data['locale'] = $locale;
+					break;
+				}
+			}
+
+			if (!$locale_found && CWebUser::$data['lang'] != 'en_GB' && CWebUser::$data['lang'] != 'en_gb') {
+				error('Locale for language "'.CWebUser::$data['lang'].'" is not found on the web server. Tried to set: '.implode(', ', $locales).'. Unable to translate Zabbix interface.');
+			}
+			bindtextdomain('frontend', 'locale');
+			bind_textdomain_codeset('frontend', 'UTF-8');
+			textdomain('frontend');
+		}
+		else {
+			error('Your PHP has no gettext support. Zabbix translations are not available.');
+		}
+
+		// should be after locale initialization
+		require_once $this->getRootDir().'/include/translateDefines.inc.php';
+
+		// numeric Locale to default
+		setlocale(LC_NUMERIC, array('C', 'POSIX', 'en', 'en_US', 'en_US.UTF-8', 'English_United States.1252', 'en_GB', 'en_GB.UTF-8'));
+	}
+
+	/**
+	 * Authenticate user.
+	 */
+	protected function authenticateUser() {
+		if (!CWebUser::checkAuthentication(get_cookie('zbx_sessionid'))) {
+			CWebUser::setDefault();
+		}
+	}
+}
---
+++ ZBX-6109/frontends/php/include/page_footer.php
@@ -0,0 +1,117 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2012 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+require_once dirname(__FILE__).'/config.inc.php';
+
+// if we include footer in some function
+if (!isset($page)) {
+	global $page;
+}
+if (!isset($ZBX_PAGE_POST_JS)) {
+	global $ZBX_PAGE_POST_JS;
+}
+
+if (!defined('PAGE_HEADER_LOADED')) {
+	define('PAGE_HEADER_LOADED', 1);
+}
+
+// history
+if (isset($page['hist_arg']) && CWebUser::$data['alias'] != ZBX_GUEST_USER && $page['type'] == PAGE_TYPE_HTML && !defined('ZBX_PAGE_NO_MENU')) {
+	add_user_history($page);
+}
+
+// last page
+if (!defined('ZBX_PAGE_NO_MENU') && $page['file'] != 'profile.php') {
+	CProfile::update('web.paging.lastpage', $page['file'], PROFILE_TYPE_STR);
+}
+
+CProfile::flush();
+
+// end transactions if they have not been closed already
+if (isset($DB) && isset($DB['TRANSACTIONS']) && $DB['TRANSACTIONS'] != 0) {
+	error(_('Transaction has not been closed. Aborting...'));
+	DBend(false);
+}
+
+show_messages();
+
+$post_script = '';
+if (in_array($page['type'], array(PAGE_TYPE_HTML_BLOCK, PAGE_TYPE_HTML))) {
+	if (!is_null(CWebUser::$data) && isset(CWebUser::$data['debug_mode']) && CWebUser::$data['debug_mode'] == GROUP_DEBUG_MODE_ENABLED) {
+		CProfiler::getInstance()->stop();
+		CProfiler::getInstance()->show();
+	}
+}
+
+if ($page['type'] == PAGE_TYPE_HTML) {
+	$post_script .= 'var page_refresh = null;'."\n";
+
+	$post_script .= "jQuery(function() {\n";
+
+	if (isset($ZBX_PAGE_POST_JS)) {
+		foreach ($ZBX_PAGE_POST_JS as $script) {
+			$post_script .= $script."\n";
+		}
+	}
+
+	if (defined('ZBX_PAGE_DO_REFRESH') && CWebUser::$data['refresh']) {
+		$post_script .= 'PageRefresh.init('.(CWebUser::$data['refresh'] * 1000).');'."\n";
+	}
+
+	if (in_array('flickerfreescreen.js', $page['scripts'])) {
+		$post_script .= 'window.flickerfreeScreenShadow.timeout = '.SCREEN_REFRESH_TIMEOUT.' * 1000;'."\n";
+		$post_script .= 'window.flickerfreeScreenShadow.responsiveness = '.SCREEN_REFRESH_RESPONSIVENESS.' * 1000;'."\n";
+	}
+
+	// the chkbxRange.init() method must be called after the inserted post scripts
+	$post_script .= "chkbxRange.init();\n";
+	$post_script .= '});'."\n";
+
+	if (!defined('ZBX_PAGE_NO_MENU') && !defined('ZBX_PAGE_NO_FOOTER')) {
+		$table = new CTable(null, 'textwhite bold maxwidth ui-widget-header ui-corner-all page_footer');
+
+		if (CWebUser::$data['userid'] == 0) {
+			$conString = _('Not connected');
+		}
+		elseif (ZBX_DISTRIBUTED) {
+			$conString = _s('Connected as \'%1$s\' from \'%2$s\'', CWebUser::$data['alias'], CWebUser::$data['node']['name']);
+		}
+		else {
+			$conString = _s('Connected as \'%1$s\'', CWebUser::$data['alias']);
+		}
+
+		$table->addRow(array(
+			new CCol(new CLink(
+				_s('Zabbix %s Copyright %s-%s by Zabbix SIA', ZABBIX_VERSION, ZABBIX_COPYRIGHT_FROM, ZABBIX_COPYRIGHT_TO),
+				'http://www.zabbix.com', 'highlight', null, true), 'center'),
+			new CCol(array(
+				new CSpan(SPACE.SPACE.'|'.SPACE.SPACE, 'divider'),
+				new CSpan($conString, 'footer_sign')
+			), 'right')
+		));
+		$table->show();
+	}
+	insert_js($post_script);
+
+	echo '</body>'."\n".
+		'</html>'."\n";
+}
+exit;
