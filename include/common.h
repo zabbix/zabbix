3756ccd6b395fcdb29281e3db61bce129da2cad6
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-22 09:38:25 +0000

    ...G...... [ZBXNEXT-1334] remove whitespace surrounding server list item delimiters in conf file
    git-svn-id: svn://svn.zabbix.com/trunk@33003 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -1041,6 +1041,8 @@ int	calculate_sleeptime(int nextcheck, int max_sleeptime);
 
 void	zbx_replace_string(char **data, size_t l, size_t *r, const char *value);
 
+void	zbx_trim_str_list(char *list, char delimiter);
+
 int	parse_serveractive_element(char *str, char **host, unsigned short *port, unsigned short port_default);
 
 #endif
