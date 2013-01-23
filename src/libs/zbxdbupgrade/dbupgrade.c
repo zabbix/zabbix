153263b72a655321efe56777732061bcc3172a91
Eduards Samersovs <git-no-reply@zabbix.com>
2013-01-23 11:36:22 +0000

    ....I..... [ZBX-6127] fixed default value for type column in users table

--- src/libs/zbxdbupgrade/dbupgrade.c
+++ src/libs/zbxdbupgrade/dbupgrade.c
@@ -188,6 +188,21 @@ static void	DBset_not_null_sql(char **sql, size_t *sql_alloc, size_t *sql_offset
 #endif
 }
 
+static void	DBset_default_sql(char **sql, size_t *sql_alloc, size_t *sql_offset,
+		const char *table_name, const ZBX_FIELD *field)
+{
+	zbx_snprintf_alloc(sql, sql_alloc, sql_offset, "alter table" ZBX_DB_ONLY " %s" ZBX_DB_ALTER_COLUMN " ",
+			table_name);
+
+#if defined(HAVE_MYSQL)
+	DBfield_definition_string(sql, sql_alloc, sql_offset, field);
+#elif defined(HAVE_ORACLE)
+	zbx_snprintf_alloc(sql, sql_alloc, sql_offset, "%s default '%s'", field->name, field->default_value);
+#else
+	zbx_snprintf_alloc(sql, sql_alloc, sql_offset, "%s set default '%s'", field->name, field->default_value);
+#endif
+}
+
 static void	DBadd_field_sql(char **sql, size_t *sql_alloc, size_t *sql_offset,
 		const char *table_name, const ZBX_FIELD *field)
 {
@@ -305,6 +320,24 @@ static int	DBset_not_null(const char *table_name, const ZBX_FIELD *field)
 	sql = zbx_malloc(sql, sql_alloc);
 
 	DBset_not_null_sql(&sql, &sql_alloc, &sql_offset, table_name, field);
+
+	if (ZBX_DB_OK <= DBexecute("%s", sql))
+		ret = DBreorg_table(table_name);
+
+	zbx_free(sql);
+
+	return ret;
+}
+
+static int	DBset_default(const char *table_name, const ZBX_FIELD *field)
+{
+	char	*sql = NULL;
+	size_t	sql_alloc = 64, sql_offset = 0;
+	int	ret = FAIL;
+
+	sql = zbx_malloc(sql, sql_alloc);
+
+	DBset_default_sql(&sql, &sql_alloc, &sql_offset, table_name, field);
 
 	if (ZBX_DB_OK <= DBexecute("%s", sql))
 		ret = DBreorg_table(table_name);
@@ -690,6 +723,13 @@ static int	DBpatch_02010031()
 
 	return FAIL;
 }
+
+static int	DBpatch_02010032()
+{
+	const ZBX_FIELD	field = {"type", "1", NULL, NULL, 0, ZBX_TYPE_INT, ZBX_NOTNULL, 0};
+
+	return DBset_default("users", &field);
+}
 #endif	/* not HAVE_SQLITE3 */
 
 static void	DBget_version(int *mandatory, int *optional)
@@ -759,11 +799,12 @@ int	DBcheck_version()
 		{DBpatch_02010029, 2010029, 0, 0},
 		{DBpatch_02010030, 2010030, 0, 0},
 		{DBpatch_02010031, 2010031, 0, 0},
+		{DBpatch_02010032, 2010032, 0, 1},
 		/* IMPORTANT! When adding a new mandatory DBPatch don't forget to update it for SQLite, too. */
 		{NULL}
 	};
 #else
-	required = 2010027;	/* <---- Update mandatory DBpatch for SQLite here. */
+	required = 2010032;	/* <---- Update mandatory DBpatch for SQLite here. */
 #endif
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
