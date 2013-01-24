2e0c8a8225a7eecf462e98fa49d1a0a051b69ac0
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-24 08:36:07 +0000

    ....I..... [ZBX-5532] add services_times ugrade patch for postgresql
    git-svn-id: svn://svn.zabbix.com/branches/dev@33097 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5532/upgrades/dbpatches/2.0/postgresql/patch/services_times.sql
+++ ZBX-5532/upgrades/dbpatches/2.0/postgresql/patch/services_times.sql
@@ -2,3 +2,8 @@ ALTER TABLE ONLY services_times ALTER timeid DROP DEFAULT,
 				ALTER serviceid DROP DEFAULT;
 DELETE FROM services_times WHERE NOT EXISTS (SELECT 1 FROM services WHERE services.serviceid=services_times.serviceid);
 ALTER TABLE ONLY services_times ADD CONSTRAINT c_services_times_1 FOREIGN KEY (serviceid) REFERENCES services (serviceid) ON DELETE CASCADE;
+
+UPDATE services_times
+	SET ts_from = 86400 * EXTRACT(dow FROM to_timestamp(ts_from)::timestamp) + (ts_from - EXTRACT(EPOCH FROM to_timestamp(ts_from)::date)),
+		ts_to = 86400 * EXTRACT(dow FROM to_timestamp(ts_to)::timestamp) + (ts_to - EXTRACT(EPOCH FROM to_timestamp(ts_to)::date))
+	WHERE type IN (0,1);
