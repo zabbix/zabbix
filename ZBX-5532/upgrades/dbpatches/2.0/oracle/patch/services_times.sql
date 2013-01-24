b601d0dd3556953fba2b599cf67a9732fc52e1b5
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-24 10:13:04 +0000

    ....I..... [ZBX-5532] add services_times ugrade patch for Oracle
    git-svn-id: svn://svn.zabbix.com/branches/dev@33100 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5532/upgrades/dbpatches/2.0/oracle/patch/services_times.sql
+++ ZBX-5532/upgrades/dbpatches/2.0/oracle/patch/services_times.sql
@@ -2,3 +2,12 @@ ALTER TABLE services_times MODIFY timeid DEFAULT NULL;
 ALTER TABLE services_times MODIFY serviceid DEFAULT NULL;
 DELETE FROM services_times WHERE NOT serviceid IN (SELECT serviceid FROM services);
 ALTER TABLE services_times ADD CONSTRAINT c_services_times_1 FOREIGN KEY (serviceid) REFERENCES services (serviceid) ON DELETE CASCADE;
+
+UPDATE services_times
+	SET ts_from = to_char(to_timestamp_tz('197001010000000000','yyyymmddhh24missTZHTZM')+NumToDSInterval(ts_from,'SECOND'), 'D') * 86400 - 86400
+			+ (ts_from -
+				(to_date(to_char(to_timestamp_tz('197001010000000000','yyyymmddhh24missTZHTZM')+NumToDSInterval(ts_from,'SECOND'), 'yyyymmdd'), 'yyyymmdd') - TO_DATE('19700101000000','YYYYMMDDHH24MISS')) / (1/86400)),
+		ts_to = to_char(to_timestamp_tz('197001010000000000','yyyymmddhh24missTZHTZM')+NumToDSInterval(ts_to,'SECOND'), 'D') * 86400 - 86400
+			+ (ts_to -
+				(to_date(to_char(to_timestamp_tz('197001010000000000','yyyymmddhh24missTZHTZM')+NumToDSInterval(ts_to,'SECOND'), 'yyyymmdd'), 'yyyymmdd') - TO_DATE('19700101000000','YYYYMMDDHH24MISS')) / (1/86400))
+	WHERE type IN (0,1);
