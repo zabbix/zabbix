8df3f7bb7e3719da400282c55222c958903f5ab0
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-25 16:15:44 +0000

    .......... [ZBX-5968] coding style: zbx_vector_uint64_t instead of zbx_uint64_t *
    git-svn-id: svn://svn.zabbix.com/branches/dev@33151 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5968/include/db.h
+++ ZBX-5968/include/db.h
@@ -539,8 +539,9 @@ int	DBadd_graph_item_to_linked_hosts(int gitemid,int hostid);
 int	DBcopy_template_elements(zbx_uint64_t hostid, zbx_vector_uint64_t *lnk_templateids);
 int	DBdelete_template_elements(zbx_uint64_t hostid, zbx_vector_uint64_t *del_templateids);
 
-void	DBdelete_graphs(zbx_vector_uint64_t *graphids);
 void	DBdelete_items(zbx_vector_uint64_t *itemids);
+void	DBdelete_triggers(zbx_vector_uint64_t *triggerids);
+void	DBdelete_graphs(zbx_vector_uint64_t *graphids);
 void	DBdelete_host(zbx_uint64_t hostid);
 void	DBget_graphitems(const char *sql, ZBX_GRAPH_ITEMS **gitems, size_t *gitems_alloc, size_t *gitems_num);
 void	DBupdate_services(zbx_uint64_t triggerid, int status, int clock);
