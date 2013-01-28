40119269f6be11ec845979672324eb32c1d9fabf
tomtom <tomtom@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-28 09:15:45 +0000

    ..F....... [ZBXNEXT-1] renamed CMacrosResolverHelper resolveGraphLabel() to resolveGraphName(); for consistency implement type support for CMacrosResolver::resolveGraph(); formated PHPDoc comments; made resolveGraph() private.
    git-svn-id: svn://svn.zabbix.com/branches/dev@33168 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1/frontends/php/include/classes/class.cgraphdraw.php
+++ ZBXNEXT-1/frontends/php/include/classes/class.cgraphdraw.php
@@ -233,7 +233,7 @@ class CGraphDraw {
 			$str = $this->items[0]['hostname'].': '.$this->items[0]['name'];
 		}
 		else {
-			$str = CMacrosResolverHelper::resolveGraphLabel($this->header, $this->items);
+			$str = CMacrosResolverHelper::resolveGraphName($this->header, $this->items);
 		}
 
 		$str .= $this->period2str($this->period);
