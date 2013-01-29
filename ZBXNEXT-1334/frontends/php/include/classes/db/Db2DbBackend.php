5093e706d7181b0474ab4fd43103b901bbf678a6
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-29 15:21:32 +0000

    .......... [ZBXNEXT-1334] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@33228 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-1334/frontends/php/include/classes/db/Db2DbBackend.php
@@ -0,0 +1,47 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2012 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+/**
+ * Database backend class for DB2.
+ */
+class Db2DbBackend extends DbBackend {
+
+	/**
+	 * Check if 'dbversion' table exists.
+	 *
+	 * @return boolean
+	 */
+	protected function checkDbVersionTable() {
+		global $DB;
+
+		$tableExists = DBfetch(DBselect('SELECT TABNAME'.
+			' FROM SYSCAT.TABLES'.
+			" WHERE TABNAME='DBVERSION'".
+				' AND TABSCHEMA='.zbx_dbstr($DB['SCHEMA'])));
+
+		if (!$tableExists) {
+			$this->setError(_('The frontend does not match Zabbix database.'));
+			return false;
+		}
+
+		return true;
+	}
+}
