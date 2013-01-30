44f77297dcee991897745ac3040cd169f7ab3bc2
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-01-30 09:32:57 +0000

    .......... [ZBX-6169] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@33246 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6169/frontends/php/include/views/js/administration.general.gui.php
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-	jQuery(document).ready(function($) {
-		'use strict';
-
-		$('#server_check_enabled').change(function() {
-			$('#server_check_interval').prop('disabled', !this.checked);
-		}).change();
-	});
-</script>
--- ZBX-6169/upgrades/dbpatches/2.0/postgresql/patch/history_uint.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE ONLY history_uint
-	ALTER itemid DROP DEFAULT,
-	ADD ns integer DEFAULT '0' NOT NULL;
