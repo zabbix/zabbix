8504da7421f48fd3f68c1506587758cc69552f6b
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-02-01 14:27:23 +0000

    A......... [ZBX-6084] fixed favorites removing
    git-svn-id: svn://svn.zabbix.com/branches/dev@33352 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6084/frontends/php/api/classes/CMap.php
+++ ZBX-6084/frontends/php/api/classes/CMap.php
@@ -1,7 +1,7 @@
 <?php
 /*
 ** Zabbix
-** Copyright (C) 2000-2011 Zabbix SIA
+** Copyright (C) 2000-2013 Zabbix SIA
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -10,15 +10,15 @@
 **
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ** GNU General Public License for more details.
 **
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
-?>
-<?php
+
+
 /**
  * @package API
  */
@@ -28,7 +28,6 @@
 class CMap extends CMapElement {
 
 	protected $tableName = 'sysmaps';
-
 	protected $tableAlias = 's';
 
 /**
@@ -930,38 +929,38 @@ class CMap extends CMapElement {
 		return array('sysmapids' => $sysmapids);
 	}
 
-
-/**
- * Delete Map
- *
- * @param array $sysmaps
- * @param array $sysmaps['sysmapid']
- * @return boolean
- */
-	public function delete($sysmapids) {
-
-		$maps = zbx_toObject($sysmapids, 'sysmapid');
+	/**
+	 * Delete Map.
+	 *
+	 * @param array $sysmaps
+	 * @param array $sysmaps['sysmapid']
+	 *
+	 * @return array
+	 */
+	public function delete($sysmapIds) {
+		$maps = zbx_toObject($sysmapIds, 'sysmapid');
 		$this->checkInput($maps, __FUNCTION__);
 
-	// delete maps from selements of other maps
 		DB::delete('sysmaps_elements', array(
-			'elementid' => $sysmapids,
+			'elementid' => $sysmapIds,
 			'elementtype' => SYSMAP_ELEMENT_TYPE_MAP
 		));
-
 		DB::delete('screens_items', array(
-			'resourceid' => $sysmapids,
+			'resourceid' => $sysmapIds,
 			'resourcetype' => SCREEN_RESOURCE_MAP
 		));
-
 		DB::delete('profiles', array(
 			'idx' => 'web.maps.sysmapid',
-			'value_id' => $sysmapids
+			'value_id' => $sysmapIds
 		));
-		//----
-		DB::delete('sysmaps', array('sysmapid' => $sysmapids));
+		DB::delete('profiles', array(
+			'idx' => 'web.favorite.sysmapids',
+			'source' => 'sysmapid',
+			'value_id' => $sysmapIds
+		));
+		DB::delete('sysmaps', array('sysmapid' => $sysmapIds));
 
-		return array('sysmapids' => $sysmapids);
+		return array('sysmapids' => $sysmapIds);
 	}
 
 	private function expandUrlMacro($url, $selement) {
@@ -1023,4 +1022,3 @@ class CMap extends CMapElement {
 		return $sqlParts;
 	}
 }
-?>
