1aa57e9454273bcaa3b5353e922928d90ec94a2a
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-02-05 14:13:06 +0000

    .......PS. [ZBX-5549] removed references to unused PERM_READ_LIST define
    git-svn-id: svn://svn.zabbix.com/branches/dev@33422 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5549/include/common.h
+++ ZBX-5549/include/common.h
@@ -651,10 +651,9 @@ typedef enum
 typedef enum
 {
 	PERM_DENY = 0,
-	PERM_READ_LIST,
 	PERM_READ_ONLY,
 	PERM_READ_WRITE,
-	PERM_MAX = 3
+	PERM_MAX = PERM_READ_WRITE
 } zbx_user_permission_t;
 
 const char	*zbx_permission_string(int perm);
