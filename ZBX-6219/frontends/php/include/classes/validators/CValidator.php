17b59fa143a6a5d7642a5ee201c94c88c464b56e
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-02-07 08:39:41 +0000

    .......... [ZBX-6219] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@33466 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-6219/frontends/php/include/classes/validators/CValidator.php
@@ -0,0 +1,64 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2012 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+abstract class CValidator {
+
+	/**
+	 * Validation errors.
+	 *
+	 * @var array
+	 */
+	private $error;
+	protected $options = array();
+
+	public function __construct(array $options = array()) {
+		$this->initOptions();
+		$this->options = zbx_array_merge($this->options, $options);
+	}
+	/**
+	 * @abstract
+	 *
+	 * @param $value
+	 *
+	 * @return bool
+	 */
+	abstract public function validate($value);
+
+	/**
+	 * Get first validation error.
+	 *
+	 * @return string
+	 */
+	public function getError() {
+		return $this->error;
+	}
+
+	/**
+	 * Add validation error.
+	 *
+	 * @param $error
+	 */
+	protected function setError($error) {
+		$this->error = $error;
+	}
+
+	protected function initOptions() {}
+}
