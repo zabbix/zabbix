c7f61f8b97e818f430af6a5447d626211c95bd7a
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-02-12 11:14:16 +0000

    .......PS. [ZBXNEXT-300] added sensor info logging
    git-svn-id: svn://svn.zabbix.com/branches/dev@33557 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-300-trunk/src/zabbix_server/poller/checks_ipmi.c
+++ ZBXNEXT-300-trunk/src/zabbix_server/poller/checks_ipmi.c
@@ -237,6 +237,7 @@ static zbx_ipmi_sensor_t	*allocate_ipmi_sensor(zbx_ipmi_host_t *h, ipmi_sensor_t
 	char			id[IPMI_SENSOR_ID_SZ];
 	enum ipmi_str_type_e	id_type;
 	int			id_sz, sz;
+	char			full_name[MAX_STRING_LEN];
 
 	id_sz = ipmi_sensor_get_id_length(sensor);
 	memset(id, 0, sizeof(id));
@@ -263,6 +264,12 @@ static zbx_ipmi_sensor_t	*allocate_ipmi_sensor(zbx_ipmi_host_t *h, ipmi_sensor_t
 	s->reading_type = ipmi_sensor_get_event_reading_type(sensor);
 	s->type = ipmi_sensor_get_sensor_type(sensor);
 
+	ipmi_sensor_get_name(s->sensor, full_name, sizeof(full_name));
+	zabbix_log(LOG_LEVEL_DEBUG, "Added sensor: id_type:%d id_sz:%d id:'%s'"
+			" reading_type:0x%x ('%s') type:0x%x ('%s') full_name:'%s'",
+			s->id_type, s->id_sz, sensor_id_to_str(id_str, sizeof(id_str), s->id, s->id_type, s->id_sz),
+			s->reading_type, ipmi_sensor_get_event_reading_type_string(s->sensor), s->type,
+			ipmi_sensor_get_sensor_type_string(s->sensor), full_name);
 	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%p", __function_name, s);
 
 	return s;
