eef22d00c05cac913e3b9bd4e2009576848a69ea
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-02-12 13:53:52 +0000

    .......... [ZBX-6216] merged form upstream
    git-svn-id: svn://svn.zabbix.com/branches/dev@33566 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6216/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ ZBX-6216/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	public static final String REVISION_DATE = "06 February 2013";
+	public static final String REVISION_DATE = "12 February 2013";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "2.0.5rc1";
+	public static final String VERSION = "2.0.6rc1";
 
 	public static void printVersion()
 	{
