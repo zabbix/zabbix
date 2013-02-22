abffebaff16672abea428409d3b923762c4e8051
Pavels Jelisejevs <git-no-reply@zabbix.com>
2013-02-22 14:08:14 +0000

    A......... [ZBXNEXT-1633] implemented the base of the hostinterface API ..F....... [ZBXNEXT-1633] updated the PHP DB schema

--- frontends/php/include/schema.inc.php
+++ frontends/php/include/schema.inc.php
@@ -225,6 +225,12 @@ return array(
 				'length' => 64,
 				'default' => '',
 			),
+			'flags' => array(
+				'null' => false,
+				'type' => DB::FIELD_TYPE_INT,
+				'length' => 10,
+				'default' => '0',
+			),
 		),
 	),
 	'groups' => array(
