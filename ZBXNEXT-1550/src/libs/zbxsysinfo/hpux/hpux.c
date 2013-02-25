475d7ccbbca5b36e765046ef2b0d90ffa6a0e8ec
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-02-25 08:59:00 +0000

    ...G...... [ZBXNEXT-1550] hpux metrics: coding style, formatting, bugs
    git-svn-id: svn://svn.zabbix.com/branches/dev@33921 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1550/src/libs/zbxsysinfo/hpux/hpux.c
+++ ZBXNEXT-1550/src/libs/zbxsysinfo/hpux/hpux.c
@@ -35,5 +35,5 @@ ZBX_METRIC	parameters_specific[] =
 	{"system.cpu.load",	CF_HAVEPARAMS,	SYSTEM_CPU_LOAD,	"all,avg1"},
 	{"system.cpu.num",	CF_HAVEPARAMS,	SYSTEM_CPU_NUM,		"online"},
 
-	{0}
+	{NULL}
 };
