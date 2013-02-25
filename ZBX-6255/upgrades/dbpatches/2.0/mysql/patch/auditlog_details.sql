e005196aff8f456749062ed967380309d7740297
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-02-25 08:00:04 +0000

    .......... [ZBX-6255] removed merged branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@33907 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6255/upgrades/dbpatches/2.0/mysql/patch/auditlog_details.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE auditlog_details
-	MODIFY auditdetailid bigint unsigned NOT NULL,
-	MODIFY auditid bigint unsigned NOT NULL,
-	MODIFY oldvalue text NOT NULL,
-	MODIFY newvalue text NOT NULL;
-DELETE FROM auditlog_details WHERE NOT auditid IN (SELECT auditid FROM auditlog);
-ALTER TABLE auditlog_details ADD CONSTRAINT c_auditlog_details_1 FOREIGN KEY (auditid) REFERENCES auditlog (auditid) ON DELETE CASCADE;
