79b5004c104e93751647154393203123473150f0
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-02-28 09:18:53 +0000

    .......... [ZBX-5990] deleted development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@34028 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5990/upgrades/dbpatches/1.1/1.1beta12_to_1.1/mysql/patch.sql
+++
@@ -1,3 +0,0 @@
-alter table graphs_items add calc_fnc int(1) DEFAULT '2' NOT NULL;
-alter table graphs_items add type int(1) DEFAULT '0' NOT NULL;
-alter table graphs_items add periods_cnt int(4) DEFAULT '5' NOT NULL;
