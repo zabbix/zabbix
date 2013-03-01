4c7af48c9391732d7c00357368267e53a4c457e0
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-03-01 15:08:32 +0000

    ..F....... [ZBXNEXT-1399] fixed multiselect formatting
    git-svn-id: svn://svn.zabbix.com/branches/dev@34066 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1399-ms2/frontends/php/js/multiselect.js
+++ ZBXNEXT-1399-ms2/frontends/php/js/multiselect.js
@@ -116,6 +116,7 @@ jQuery(function($) {
 											url: options.url + '&curtime=' + new CDate().getTime(),
 											type: 'GET',
 											dataType: 'json',
+											cache: false,
 											data: {
 												search: values.search,
 												limit: getLimit(values, options)
@@ -290,270 +291,270 @@ jQuery(function($) {
 				loadSelected(options.data, obj, values, options);
 			}
 		});
+	};
 
-		function loadSelected(data, obj, values, options) {
+	function loadSelected(data, obj, values, options) {
+		$.each(data, function(i, item) {
+			addSelected(item, obj, values, options);
+		});
+	}
+
+	function loadAvailable(data, obj, values, options) {
+		cleanAvailable(obj, values);
+
+		if (!empty(data)) {
 			$.each(data, function(i, item) {
-				addSelected(item, obj, values, options);
+				addAvailable(item, obj, values, options);
 			});
 		}
 
-		function loadAvailable(data, obj, values, options) {
-			cleanAvailable(obj, values);
+		// write empty result label
+		if (objectLength(values.available) == 0) {
+			var div = $('<div>', {
+				'class': 'label-empty-result',
+				text: options.labels.emptyResult
+			})
+			.click(function() {
+				$('input[type="text"]', obj).focus();
+			});
 
-			if (!empty(data)) {
-				$.each(data, function(i, item) {
-					addAvailable(item, obj, values, options);
-				});
-			}
-
-			// write empty result label
-			if (objectLength(values.available) == 0) {
-				var div = $('<div>', {
-					'class': 'label-empty-result',
-					text: options.labels.emptyResult
-				})
-				.click(function() {
-					$('input[type="text"]', obj).focus();
-				});
-
-				$('.available', obj).append(div);
-			}
-
-			// write more matches found label
-			if (values.isMoreMatchesFound) {
-				var div = $('<div>', {
-					'class': 'label-more-matches-found',
-					text: options.labels.moreMatchesFound
-				})
-				.click(function() {
-					$('input[type="text"]', obj).focus();
-				});
-
-				$('.available', obj).prepend(div);
-			}
-
-			showAvailable(obj, values);
+			$('.available', obj).append(div);
 		}
 
-		function addSelected(item, obj, values, options) {
-			if (typeof(values.selected[item.id]) == 'undefined') {
-				values.selected[item.id] = item;
+		// write more matches found label
+		if (values.isMoreMatchesFound) {
+			var div = $('<div>', {
+				'class': 'label-more-matches-found',
+				text: options.labels.moreMatchesFound
+			})
+			.click(function() {
+				$('input[type="text"]', obj).focus();
+			});
 
-				// add hidden input
-				obj.append($('<input>', {
-					type: 'hidden',
-					name: options.name,
-					value: item.id
-				}));
+			$('.available', obj).prepend(div);
+		}
 
-				// add list item
+		showAvailable(obj, values);
+	}
+
+	function addSelected(item, obj, values, options) {
+		if (typeof(values.selected[item.id]) == 'undefined') {
+			values.selected[item.id] = item;
+
+			// add hidden input
+			obj.append($('<input>', {
+				type: 'hidden',
+				name: options.name,
+				value: item.id
+			}));
+
+			// add list item
+			var li = $('<li>', {
+				'data-id': item.id
+			});
+
+			var text = $('<span>', {
+				'class': 'text',
+				text: empty(item.prefix) ? item.name : item.prefix + item.name
+			});
+
+			if (!options.disabled) {
+				var arrow = $('<span>', {
+					'class': 'arrow',
+					'data-id': item.id
+				})
+				.click(function() {
+					removeSelected($(this).data('id'), obj, values, options);
+				});
+
+				$('.selected ul', obj).append(li.append(text, arrow));
+
+				// resize
+				resizeSelected(obj, values);
+			}
+			else {
+				$('.selected ul', obj).append(li.append(text));
+			}
+		}
+	}
+
+	function removeSelected(id, obj, values, options) {
+		// remove
+		$('.selected li[data-id="' + id + '"]', obj).remove();
+		$('input[value="' + id + '"]', obj).remove();
+
+		delete values.selected[id];
+
+		// resize
+		resizeSelected(obj, values);
+
+		// clean
+		cleanAvailable(obj, values);
+		cleanLastSearch(obj);
+		$('input[type="text"]', obj).focus();
+	}
+
+	function addAvailable(item, obj, values, options) {
+		if (empty(options.limit) || (options.limit > 0 && $('.available li', obj).length < options.limit)) {
+			if (typeof(values.available[item.id]) == 'undefined' && typeof(values.selected[item.id]) == 'undefined') {
+				values.available[item.id] = item;
+
+				var prefix = $('<span>', {
+					'class': 'prefix',
+					text: item.prefix
+				});
+
+				var matchedText = $('<span>', {
+					'class': 'matched',
+					text: item.name.substr(0, values.search.length)
+				});
+
+				var unMatchedText = $('<span>', {
+					text: item.name.substr(values.search.length, item.name.length)
+				});
+
 				var li = $('<li>', {
 					'data-id': item.id
-				});
+				})
+				.click(function() {
+					select($(this).data('id'), obj, values, options);
+				})
+				.hover(function() {
+					$('.available li.hover', obj).removeClass('hover');
+					li.addClass('hover');
+				})
+				.append(prefix, matchedText, unMatchedText);
 
-				var text = $('<span>', {
-					'class': 'text',
-					text: empty(item.prefix) ? item.name : item.prefix + item.name
-				});
-
-				if (!options.disabled) {
-					var arrow = $('<span>', {
-						'class': 'arrow',
-						'data-id': item.id
-					})
-					.click(function() {
-						removeSelected($(this).data('id'), obj, values, options);
-					});
-
-					$('.selected ul', obj).append(li.append(text, arrow));
-
-					// resize
-					resizeSelected(obj, values);
-				}
-				else {
-					$('.selected ul', obj).append(li.append(text));
-				}
+				$('.available ul', obj).append(li);
 			}
 		}
+		else {
+			values.isMoreMatchesFound = true;
+		}
+	}
 
-		function removeSelected(id, obj, values, options) {
-			// remove
-			$('.selected li[data-id="' + id + '"]', obj).remove();
-			$('input[value="' + id + '"]', obj).remove();
-
-			delete values.selected[id];
-
-			// resize
-			resizeSelected(obj, values);
-
-			// clean
+	function select(id, obj, values, options) {
+		if (values.isAjaxLoaded && !values.isWaiting) {
+			addSelected(values.available[id], obj, values, options);
+			hideAvailable(obj);
 			cleanAvailable(obj, values);
 			cleanLastSearch(obj);
-			$('input[type="text"]', obj).focus();
+		}
+	}
+
+	function showAvailable(obj, values) {
+		if ($('.label-empty-result', obj).length > 0 || objectLength(values.available) > 0) {
+			$('.selected ul', obj).addClass('active');
+			$('.available', obj).fadeIn(0);
+
+			// remove selected item pressed state
+			$('.selected li.pressed', obj).removeClass('pressed');
+
+			// pre-select first available
+			$('.available li.hover', obj).removeClass('hover');
+			$('.available li:first-child', obj).addClass('hover');
+		}
+	}
+
+	function hideAvailable(obj) {
+		$('.available', obj).fadeOut(0);
+	}
+
+	function cleanAvailable(obj, values) {
+		$('.label-empty-result', obj).remove();
+		$('.label-more-matches-found', obj).remove();
+		$('.available li', obj).remove();
+		values.available = {};
+		values.isMoreMatchesFound = false;
+	}
+
+	function cleanLastSearch(obj) {
+		var input = $('input[type="text"]', obj);
+
+		input.attr('data-lastSearch', '');
+		input.val('');
+	}
+
+	function cleanSearchInput(obj) {
+		$('input[type="text"]', obj).val('');
+	}
+
+	function resizeSelected(obj, values) {
+		// settings
+		var searchInputMinWidth = 50,
+			searchInputLeftPaddings = 4,
+			searchInputTopPaddings = IE8 ? 7 : 0;
+
+		// calculate
+		var top, left, height;
+
+		if ($('.selected li', obj).length > 0) {
+			var position = $('.selected li:last-child .arrow', obj).position();
+			top = position.top + searchInputTopPaddings;
+			left = position.left + 20;
+			height = $('.selected li:last-child', obj).height();
+		}
+		else {
+			top = 3 + searchInputTopPaddings;
+			left = searchInputLeftPaddings;
+			height = 0;
 		}
 
-		function addAvailable(item, obj, values, options) {
-			if (empty(options.limit) || (options.limit > 0 && $('.available li', obj).length < options.limit)) {
-				if (typeof(values.available[item.id]) == 'undefined' && typeof(values.selected[item.id]) == 'undefined') {
-					values.available[item.id] = item;
+		if (left + searchInputMinWidth > values.width) {
+			var topPaddings = (height > 20) ? height / 2 : height;
 
-					var prefix = $('<span>', {
-						'class': 'prefix',
-						text: item.prefix
-					});
+			top += topPaddings;
+			left = searchInputLeftPaddings;
 
-					var matchedText = $('<span>', {
-						'class': 'matched',
-						text: item.name.substr(0, values.search.length)
-					});
-
-					var unMatchedText = $('<span>', {
-						text: item.name.substr(values.search.length, item.name.length)
-					});
-
-					var li = $('<li>', {
-						'data-id': item.id
-					})
-					.click(function() {
-						select($(this).data('id'), obj, values, options);
-					})
-					.hover(function() {
-						$('.available li.hover', obj).removeClass('hover');
-						li.addClass('hover');
-					})
-					.append(prefix, matchedText, unMatchedText);
-
-					$('.available ul', obj).append(li);
-				}
-			}
-			else {
-				values.isMoreMatchesFound = true;
-			}
+			$('.selected ul', obj).css({
+				'padding-bottom': topPaddings
+			});
 		}
-
-		function select(id, obj, values, options) {
-			if (values.isAjaxLoaded && !values.isWaiting) {
-				addSelected(values.available[id], obj, values, options);
-				hideAvailable(obj);
-				cleanAvailable(obj, values);
-				cleanLastSearch(obj);
-			}
-		}
-
-		function showAvailable(obj, values) {
-			if ($('.label-empty-result', obj).length > 0 || objectLength(values.available) > 0) {
-				$('.selected ul', obj).addClass('active');
-				$('.available', obj).fadeIn(0);
-
-				// remove selected item pressed state
-				$('.selected li.pressed', obj).removeClass('pressed');
-
-				// pre-select first available
-				$('.available li.hover', obj).removeClass('hover');
-				$('.available li:first-child', obj).addClass('hover');
-			}
-		}
-
-		function hideAvailable(obj) {
-			$('.available', obj).fadeOut(0);
-		}
-
-		function cleanAvailable(obj, values) {
-			$('.label-empty-result', obj).remove();
-			$('.label-more-matches-found', obj).remove();
-			$('.available li', obj).remove();
-			values.available = {};
-			values.isMoreMatchesFound = false;
-		}
-
-		function cleanLastSearch(obj) {
-			var input = $('input[type="text"]', obj);
-
-			input.attr('data-lastSearch', '');
-			input.val('');
-		}
-
-		function cleanSearchInput(obj) {
-			$('input[type="text"]', obj).val('');
-		}
-
-		function resizeSelected(obj, values) {
-			// settings
-			var searchInputMinWidth = 50,
-				searchInputLeftPaddings = 4,
-				searchInputTopPaddings = IE8 ? 7 : 0;
-
-			// calculate
-			var top, left, height;
-
-			if ($('.selected li', obj).length > 0) {
-				var position = $('.selected li:last-child .arrow', obj).position();
-				top = position.top + searchInputTopPaddings;
-				left = position.left + 20;
-				height = $('.selected li:last-child', obj).height();
-			}
-			else {
-				top = 3 + searchInputTopPaddings;
-				left = searchInputLeftPaddings;
-				height = 0;
-			}
-
-			if (left + searchInputMinWidth > values.width) {
-				var topPaddings = (height > 20) ? height / 2 : height;
-
-				top += topPaddings;
-				left = searchInputLeftPaddings;
-
-				$('.selected ul', obj).css({
-					'padding-bottom': topPaddings
-				});
-			}
-			else {
-				$('.selected ul', obj).css({
-					'padding-bottom': 0
-				});
-			}
-
-			$('input[type="text"]', obj).css({
-				'padding-top': top,
-				'padding-left': left,
-				width: values.width - left
+		else {
+			$('.selected ul', obj).css({
+				'padding-bottom': 0
 			});
 		}
 
-		function scrollAvailable(obj) {
-			var hover = $('.available li.hover', obj);
+		$('input[type="text"]', obj).css({
+			'padding-top': top,
+			'padding-left': left,
+			width: values.width - left
+		});
+	}
 
-			if (hover.length > 0) {
-				var available = $('.available ul', obj),
-					offset = hover.position().top;
+	function scrollAvailable(obj) {
+		var hover = $('.available li.hover', obj);
 
-				if (offset < 0 || offset > available.height()) {
-					if (offset < 0) {
-						offset = available.scrollTop() + offset;
-					}
+		if (hover.length > 0) {
+			var available = $('.available ul', obj),
+				offset = hover.position().top;
 
-					available.animate({ scrollTop: offset }, 300);
+			if (offset < 0 || offset > available.height()) {
+				if (offset < 0) {
+					offset = available.scrollTop() + offset;
 				}
+
+				available.animate({ scrollTop: offset }, 300);
+			}
+		}
+	}
+
+	function getLimit(values, options) {
+		return (options.limit > 0)
+			? options.limit + objectLength(values.selected) + 1
+			: null;
+	}
+
+	function objectLength(obj) {
+		var length = 0;
+
+		for (var key in obj) {
+			if (obj.hasOwnProperty(key)) {
+				length++;
 			}
 		}
 
-		function getLimit(values, options) {
-			return (options.limit > 0)
-				? options.limit + objectLength(values.selected) + 1
-				: null;
-		}
-
-		function objectLength(obj) {
-			var length = 0;
-
-			for (var key in obj) {
-				if (obj.hasOwnProperty(key)) {
-					length++;
-				}
-			}
-
-			return length;
-		}
-	};
+		return length;
+	}
 });
