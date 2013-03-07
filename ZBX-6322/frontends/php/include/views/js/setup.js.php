5a878054766f9b799e2b53a1379c62716a81b7a1
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-03-07 14:54:56 +0000

    .......... [ZBX-6322] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@34188 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-6322/frontends/php/include/views/js/setup.js.php
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+	jQuery(document).ready(function() {
+
+		jQuery(document.body).addClass('setupBG');
+
+		jQuery(":submit").button();
+
+		jQuery("#agree").change(function(){
+			if (this.checked) {
+				jQuery("#next_1").button("enable");
+			}
+			else {
+				jQuery("#next_1").button("disable");
+			}
+		})
+	});
+</script>
