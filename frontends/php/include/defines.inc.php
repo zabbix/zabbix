aa464200c4bee29b506b0f1f4faf2d5d7a2b9494
Oleg Egorov <git-no-reply@zabbix.com>
2013-03-21 12:59:29 +0000

    .......... [ZBX-954] fixed copyright years

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 ** Zabbix
-** Copyright (C) 2000-2011 Zabbix SIA
+** Copyright (C) 2001-2013 Zabbix SIA
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@ define('ZABBIX_API_VERSION', '2.1.0');
 define('ZABBIX_DB_VERSION',	 2010034);
 
 define('ZABBIX_COPYRIGHT_FROM', '2001');
-define('ZABBIX_COPYRIGHT_TO',   '2012');
+define('ZABBIX_COPYRIGHT_TO',   '2013');
 
 define('ZBX_LOGIN_ATTEMPTS',	5);
 define('ZBX_LOGIN_BLOCK',		30); // sec
