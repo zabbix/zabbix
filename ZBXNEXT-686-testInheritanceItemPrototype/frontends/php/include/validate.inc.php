25ca3a98043a4e47c34c4857e1a5aa5c9e1b71b7
egita <egita@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-03-22 12:09:09 +0000

    .......... [ZBXNEXT-686] updated to the latest trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@34594 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-686-testInheritanceItemPrototype/frontends/php/include/validate.inc.php
+++ ZBXNEXT-686-testInheritanceItemPrototype/frontends/php/include/validate.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 ** Zabbix
-** Copyright (C) 2000-2011 Zabbix SIA
+** Copyright (C) 2001-2013 Zabbix SIA
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -10,7 +10,7 @@
 **
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ** GNU General Public License for more details.
 **
 ** You should have received a copy of the GNU General Public License
@@ -481,7 +481,7 @@ function check_field(&$fields, &$field, $checks) {
 		return $err;
 	}
 
-	if ((is_null($exception) || $except) && $validation && !calc_exp($fields, $field, $validation)){
+	if ((is_null($exception) || $except) && $validation && !calc_exp($fields, $field, $validation)) {
 		if ($validation == NOT_EMPTY) {
 			if ($flags&P_SYS) {
 				info(_s('Critical error. Incorrect value for field "%1$s": cannot be empty.', $caption));
@@ -512,6 +512,7 @@ function check_field(&$fields, &$field, $checks) {
 
 		return ($flags&P_SYS) ? ZBX_VALID_ERROR : ZBX_VALID_WARNING;
 	}
+
 	return ZBX_VALID_OK;
 }
 
