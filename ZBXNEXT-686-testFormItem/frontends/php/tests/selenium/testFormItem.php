504ace8c031439601f378bcd29083a43da3c1e66
egita <egita@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-03-22 12:01:32 +0000

    .......... [ZBXNEXT-686] fix ZBX-9397
    git-svn-id: svn://svn.zabbix.com/branches/dev@34591 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-686-testFormItem/frontends/php/tests/selenium/testFormItem.php
+++ ZBXNEXT-686-testFormItem/frontends/php/tests/selenium/testFormItem.php
@@ -1,7 +1,7 @@
 <?php
 /*
 ** Zabbix
-** Copyright (C) 2000-2013 Zabbix SIA
+** Copyright (C) 2001-2013 Zabbix SIA
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -1682,7 +1682,7 @@ class testFormItem extends CWebTest {
 					'key' => 'item-ssh-agent-error',
 					'errors' => array(
 							'ERROR: Page received incorrect data',
-							'Warning. Incorrect value for field "username".',
+							'Warning. Incorrect value for field "User name": cannot be empty.',
 							'Warning. Incorrect value for field "Executed script": cannot be empty.'
 					)
 				)
@@ -1695,7 +1695,7 @@ class testFormItem extends CWebTest {
 					'key' => 'item-telnet-agent-error',
 					'errors' => array(
 							'ERROR: Page received incorrect data',
-							'Warning. Incorrect value for field "username".',
+							'Warning. Incorrect value for field "User name": cannot be empty.',
 							'Warning. Incorrect value for field "Executed script": cannot be empty.'
 					)
 				)
