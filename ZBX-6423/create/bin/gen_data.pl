176e9b3d619bbabfce992aa2f445d5442a9d75fb
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-03-26 10:18:05 +0000

    .......... [ZBX-6423] merge from upstream
    git-svn-id: svn://svn.zabbix.com/branches/dev@34642 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6423/create/bin/gen_data.pl
+++ ZBX-6423/create/bin/gen_data.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 #
 # Zabbix
-# Copyright (C) 2000-2011 Zabbix SIA
+# Copyright (C) 2001-2013 Zabbix SIA
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -9,7 +9,7 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
