c3f8a987ab547220e9b03cdc076feac367030ee9
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-03-28 12:47:42 +0000

    .......... [ZBXNEXT-1575] updated to the latest trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@34689 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1575/frontends/php/include/views/configuration.item.edit.php
+++ ZBXNEXT-1575/frontends/php/include/views/configuration.item.edit.php
@@ -1,7 +1,7 @@
 <?php
 /*
 ** Zabbix
-** Copyright (C) 2000-2011 Zabbix SIA
+** Copyright (C) 2001-2013 Zabbix SIA
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -10,7 +10,7 @@
 **
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ** GNU General Public License for more details.
 **
 ** You should have received a copy of the GNU General Public License
@@ -321,23 +321,24 @@ $itemFormList->addRow(_('Flexible intervals'),
 );
 
 // append new flexible interval to form list
-$itemFormList->addRow(
-	_('New flexible interval'),
-	array(
-		_('Interval (in sec)'),
-		SPACE,
-		new CNumericBox('new_delay_flex[delay]', $this->data['new_delay_flex']['delay'], 5),
-		SPACE,
-		_('Period'),
-		SPACE,
-		new CTextBox('new_delay_flex[period]', $this->data['new_delay_flex']['period'], 20),
-		SPACE,
-		new CSubmit('add_delay_flex', _('Add'), null, 'formlist')
-	),
-	$this->data['maxReached'],
-	'row_new_delay_flex',
-	'new'
-);
+$newFlexInt = new CSpan(array(
+	_('Interval (in sec)'),
+	SPACE,
+	new CNumericBox('new_delay_flex[delay]', $this->data['new_delay_flex']['delay'], 5),
+	SPACE,
+	_('Period'),
+	SPACE,
+	new CTextBox('new_delay_flex[period]', $this->data['new_delay_flex']['period'], 20),
+	SPACE,
+	new CSubmit('add_delay_flex', _('Add'), null, 'formlist')
+));
+$newFlexInt->setAttribute('id', 'row-new-delay-flex-fields');
+
+$maxFlexMsg = new CSpan(_('Maximum number of flexible intervals added'), 'red');
+$maxFlexMsg->setAttribute('id', 'row-new-delay-flex-max-reached');
+$maxFlexMsg->setAttribute('style', 'display: none;');
+
+$itemFormList->addRow(_('New flexible interval'), array($newFlexInt, $maxFlexMsg), false, 'row_new_delay_flex', 'new');
 
 if ($this->data['is_discovery_rule']) {
 	$itemFormList->addRow(_('Keep lost resources period (in days)'), new CTextBox('lifetime', $this->data['lifetime'], ZBX_TEXTBOX_SMALL_SIZE, false, 64));
