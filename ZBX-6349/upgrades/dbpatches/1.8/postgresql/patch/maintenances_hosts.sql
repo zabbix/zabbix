44ad831e1b7c4c247686dff23a400a2bed721108
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-04-08 09:21:42 +0000

    .......... [ZBX-6349] deleted development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@34870 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6349/upgrades/dbpatches/1.8/postgresql/patch/maintenances_hosts.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE maintenances_hosts (
-        maintenance_hostid              bigint          DEFAULT '0'     NOT NULL,
-        maintenanceid           bigint          DEFAULT '0'     NOT NULL,
-        hostid          bigint          DEFAULT '0'     NOT NULL,
-        PRIMARY KEY (maintenance_hostid)
-) with OIDS;
-CREATE INDEX maintenances_hosts_1 on maintenances_hosts (maintenanceid,hostid);
