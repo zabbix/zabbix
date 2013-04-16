a7818bbec0d9935dc1c3a03a902e02a6d9ebbd6c
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-04-16 10:58:33 +0000

    ...G...... [ZBX-6470] fix swap calculation on Solaris
    Previously we used swapctl() call with parameters that gave incorrect
    values on some versions of Solaris. New approach is to use swapctl()
    call with SC_AINFO command and using struct anoninfo which guarantees
    proper results on all versions.
    
    The calculations are made the same way swap -s works:
      total = total swap memory
      used = allocated + reserved
      free = total - used
    
    
    git-svn-id: svn://svn.zabbix.com/branches/dev@35054 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6470/src/libs/zbxsysinfo/solaris/swap.c
+++ ZBX-6470/src/libs/zbxsysinfo/solaris/swap.c
@@ -20,145 +20,93 @@
 #include "common.h"
 #include "sysinfo.h"
 
-static void	get_swapinfo(double *total, double *fr)
+/******************************************************************************
+ *                                                                            *
+ * Function: get_swapinfo                                                     *
+ *                                                                            *
+ * Purpose: get swap usage statistics                                         *
+ *                                                                            *
+ * Return value: SUCCEED if swap usage statistics retrieved successfully      *
+ *               FAIL otherwise                                               *
+ *                                                                            *
+ * Author: Vladimir Levijev                                                   *
+ *                                                                            *
+ * Comments: we make calculations the same way swap -s works:                 *
+ *           total = total swap memory                                        *
+ *           used = allocated + reserved                                      *
+ *           free = total - used                                              *
+ *                                                                            *
+ ******************************************************************************/
+static int	get_swapinfo(zbx_uint64_t *total, zbx_uint64_t *used)
 {
-	register int cnt, i, page_size;
-/* Support for >2Gb */
-/*	register int t, f;*/
-	double	t, f;
-	struct swaptable *swt;
-	struct swapent *ste;
-	static char path[256];
+	static int	pagesize = 0;
 
-	/* get total number of swap entries */
-	cnt = swapctl(SC_GETNSWP, 0);
+	struct anoninfo	ai;
 
-	/* allocate enough space to hold count + n swapents */
-	swt = (struct swaptable *)malloc(sizeof(int) +
-		cnt * sizeof(struct swapent));
+	if (-1 == swapctl(SC_AINFO, &ai))
+		return FAIL;
 
-	if (swt == NULL)
-	{
-		*total = 0;
-		*fr = 0;
-		return;
-	}
-	swt->swt_n = cnt;
+	if (0 == pagesize)
+		pagesize = getpagesize();
 
-/* fill in ste_path pointers: we don't care about the paths, so we
-point them all to the same buffer */
-	ste = &(swt->swt_ent[0]);
-	i = cnt;
-	while (--i >= 0)
-	{
-		ste++->ste_path = path;
-	}
+	*total = ai.ani_max * pagesize;
+	*used = ai.ani_resv * pagesize;
 
-	/* grab all swap info */
-	swapctl(SC_LIST, swt);
-
-	/* walk through the structs and sum up the fields */
-	t = f = 0;
-	ste = &(swt->swt_ent[0]);
-	i = cnt;
-	while (--i >= 0)
-	{
-		/* don't count slots being deleted */
-		if (!(ste->ste_flags & ST_INDEL) &&
-		!(ste->ste_flags & ST_DOINGDEL))
-		{
-			t += ste->ste_pages;
-			f += ste->ste_free;
-		}
-		ste++;
-	}
-
-	page_size=getpagesize();
-
-	/* fill in the results */
-	*total = page_size*t;
-	*fr = page_size*f;
-	free(swt);
+	return SUCCEED;
 }
 
-int	SYSTEM_SWAP_FREE(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
+static int	SYSTEM_SWAP_TOTAL(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	double	swaptotal, swapfree;
+	zbx_uint64_t	total, used;
 
-	get_swapinfo(&swaptotal, &swapfree);
+	if (SUCCEED != get_swapinfo(&total, &used))
+		return SYSINFO_RET_FAIL;
 
-	SET_UI64_RESULT(result, swapfree);
+	SET_UI64_RESULT(result, total);
+
 	return SYSINFO_RET_OK;
 }
 
-int	SYSTEM_SWAP_TOTAL(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
+static int	SYSTEM_SWAP_FREE(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	double	swaptotal, swapfree;
+	zbx_uint64_t	total, used;
 
-	get_swapinfo(&swaptotal, &swapfree);
-
-	SET_UI64_RESULT(result, swaptotal);
-	return SYSINFO_RET_OK;
-}
-
-static int	SYSTEM_SWAP_PFREE(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
-{
-	AGENT_RESULT	result_tmp;
-	zbx_uint64_t	tot_val = 0;
-	zbx_uint64_t	free_val = 0;
-
-        init_result(&result_tmp);
-
-	if(SYSTEM_SWAP_TOTAL(cmd, param, flags, &result_tmp) != SYSINFO_RET_OK || !(result_tmp.type & AR_UINT64))
+	if (SUCCEED != get_swapinfo(&total, &used))
 		return SYSINFO_RET_FAIL;
-	tot_val = result_tmp.ui64;
 
-	/* Check for division by zero */
-	if(tot_val == 0)
-	{
-		free_result(&result_tmp);
-		return SYSINFO_RET_FAIL;
-	}
-
-	if(SYSTEM_SWAP_FREE(cmd, param, flags, &result_tmp) != SYSINFO_RET_OK || !(result_tmp.type & AR_UINT64))
-		return SYSINFO_RET_FAIL;
-	free_val = result_tmp.ui64;
-
-	free_result(&result_tmp);
-
-	SET_DBL_RESULT(result, (100.0 * (double)free_val) / (double)tot_val);
+	SET_UI64_RESULT(result, total - used);
 
 	return SYSINFO_RET_OK;
 }
 
 static int	SYSTEM_SWAP_PUSED(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
-	AGENT_RESULT	result_tmp;
-	zbx_uint64_t	tot_val = 0;
-	zbx_uint64_t	free_val = 0;
+	zbx_uint64_t	total, used;
 
-        init_result(&result_tmp);
-
-	if(SYSTEM_SWAP_TOTAL(cmd, param, flags, &result_tmp) != SYSINFO_RET_OK || !(result_tmp.type & AR_UINT64))
+	if (SUCCEED != get_swapinfo(&total, &used))
 		return SYSINFO_RET_FAIL;
-	tot_val = result_tmp.ui64;
 
-	/* Check for division by zero */
-	if(tot_val == 0)
-	{
-		free_result(&result_tmp);
+	if (0 == total)
 		return SYSINFO_RET_FAIL;
-	}
 
-	if(SYSTEM_SWAP_FREE(cmd, param, flags, &result_tmp) != SYSINFO_RET_OK || !(result_tmp.type & AR_UINT64))
+	SET_DBL_RESULT(result, 100.0 * (double)used / (double)total);
+
+	return SYSINFO_RET_OK;
+}
+
+static int	SYSTEM_SWAP_PFREE(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
+{
+	zbx_uint64_t	total, used;
+
+	if (SUCCEED != get_swapinfo(&total, &used))
 		return SYSINFO_RET_FAIL;
-	free_val = result_tmp.ui64;
 
-	free_result(&result_tmp);
+	if (0 == total)
+		return SYSINFO_RET_FAIL;
 
-	SET_DBL_RESULT(result, 100.0 - (100.0 * (double)free_val) / (double)tot_val);
+	SET_DBL_RESULT(result, 100.0 * (double)(total - used) / (double)total);
 
-        return SYSINFO_RET_OK;
+	return SYSINFO_RET_OK;
 }
 
 int	SYSTEM_SWAP_SIZE(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
@@ -167,50 +115,31 @@ int	SYSTEM_SWAP_SIZE(const char *cmd, const char *param, unsigned flags, AGENT_R
 	{
 		{"total",	SYSTEM_SWAP_TOTAL},
 		{"free",	SYSTEM_SWAP_FREE},
-		{"pfree",       SYSTEM_SWAP_PFREE},
 		{"pused",       SYSTEM_SWAP_PUSED},
+		{"pfree",       SYSTEM_SWAP_PFREE},
 		{NULL,		0}
 	};
 
-	char swapdev[MAX_STRING_LEN];
-	char mode[MAX_STRING_LEN];
-	int i;
+	char	swapdev[MAX_STRING_LEN], mode[MAX_STRING_LEN];
+	int	i;
 
-        if(num_param(param) > 2)
-        {
-                return SYSINFO_RET_FAIL;
-        }
-
-        if(get_param(param, 1, swapdev, sizeof(swapdev)) != 0)
-        {
-                return SYSINFO_RET_FAIL;
-        }
-
-        if(swapdev[0] == '\0')
-	{
-		/* default parameter */
-		zbx_snprintf(swapdev, sizeof(swapdev), "all");
-	}
-
-	if(strncmp(swapdev, "all", sizeof(swapdev)))
-	{
+	if (2 < num_param(param))
 		return SYSINFO_RET_FAIL;
-	}
 
-	if(get_param(param, 2, mode, sizeof(mode)) != 0)
-        {
-                mode[0] = '\0';
-        }
+	if (0 != get_param(param, 1, swapdev, sizeof(swapdev)))
+		return SYSINFO_RET_FAIL;
 
-        if(mode[0] == '\0')
+	if ('\0' != *swapdev)
+		return SYSINFO_RET_FAIL;
+
+	if (0 != get_param(param, 2, mode, sizeof(mode)))
+		zbx_snprintf(mode, sizeof(mode), "free");	/* default parameter */
+
+	for (i = 0; 0 != fl[i].mode; i++)
 	{
-		/* default parameter */
-		zbx_snprintf(mode, sizeof(mode), "free");
-	}
-
-	for(i=0; fl[i].mode!=0; i++)
-		if(strncmp(mode, fl[i].mode, MAX_STRING_LEN)==0)
+		if (0 == strncmp(mode, fl[i].mode, MAX_STRING_LEN))
 			return (fl[i].function)(cmd, param, flags, result);
+	}
 
 	return SYSINFO_RET_FAIL;
 }
