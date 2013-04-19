b715fd57243a2baf0fd205927b1271adb2b972bf
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-04-19 15:19:02 +0000

    ..F....... [ZBXNEXT-1649] fixed housekeeper form
    git-svn-id: svn://svn.zabbix.com/branches/dev@35138 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1649/frontends/php/include/views/administration.general.housekeeper.edit.php
+++ ZBXNEXT-1649/frontends/php/include/views/administration.general.housekeeper.edit.php
@@ -22,9 +22,16 @@ require_once dirname(__FILE__).'/js/administration.general.housekeeper.edit.js.p
 
 $houseKeeperTab = new CFormList('scriptsTab');
 
-// remove events and alerts block
-$houseKeeperTab->addRow(_('Enable housekeeping'), new CCheckBox('hk_events_mode',
-	$this->data['config']['hk_events_mode'], null, 1));
+// events and alerts
+$eventAlertTab = new CTable(null, 'formElementTable');
+
+$eventAlertTab->addRow(
+	array(
+		new CDiv(_('Enable housekeeping'), 'hkElementLabel'),
+		new CCheckBox('hk_events_mode', $this->data['config']['hk_events_mode'], null, 1)
+	)
+);
+
 $houseKeeperEventsTrigger = new CNumericBox('hk_events_trigger', $this->data['config']['hk_events_trigger'], 5);
 $houseKeeperEventsInternal = new CNumericBox('hk_events_internal', $this->data['config']['hk_events_internal'], 5);
 $houseKeeperEventsDiscovery = new CNumericBox('hk_events_discovery', $this->data['config']['hk_events_discovery'], 5);
@@ -35,42 +42,74 @@ if (!$this->data['config']['hk_events_mode']) {
 	$houseKeeperEventsDiscovery->setAttribute('disabled', 'disabled');
 	$houseKeeperEventsAutoreg->setAttribute('disabled', 'disabled');
 }
-$houseKeeperTab->addRow(_('Keep trigger data for (in days)'), $houseKeeperEventsTrigger);
-$houseKeeperTab->addRow(_('Keep internal data for (in days)'), $houseKeeperEventsInternal);
-$houseKeeperTab->addRow(_('Keep network discovery data for (in days)'), $houseKeeperEventsDiscovery);
-$houseKeeperTab->addRow(_('Keep auto-registration data for (in days)'),
-	$houseKeeperEventsAutoreg);
+$eventAlertTab->addRow(array(_('Keep trigger data for (in days)'), $houseKeeperEventsTrigger));
+$eventAlertTab->addRow(array(_('Keep internal data for (in days)'), $houseKeeperEventsInternal));
+$eventAlertTab->addRow(array(_('Keep network discovery data for (in days)'), $houseKeeperEventsDiscovery));
+$eventAlertTab->addRow(array(_('Keep auto-registration data for (in days)'),
+	$houseKeeperEventsAutoreg));
+$eventAlertTab->addClass('border_dotted objectgroup interface-group');
+$houseKeeperTab->addRow(_('Events and alerts'), new CDiv($eventAlertTab));
 
-// remove IT service history block
-$houseKeeperTab->addRow(_('Enable housekeeping'), new CCheckBox('hk_services_mode',
-	$this->data['config']['hk_services_mode'], null, 1));
+
+// IT services
+$itServicesTab = new CTable(null, 'formElementTable');
+
+$itServicesTab->addRow(
+	array(
+		new CDiv(_('Enable housekeeping'), 'hkElementLabel'),
+		new CCheckBox('hk_services_mode', $this->data['config']['hk_services_mode'], null, 1)
+	)
+);
+
 $houseKeeperServicesMode = new CNumericBox('hk_services', $this->data['config']['hk_services'], 5);
 if (!$this->data['config']['hk_services_mode']) {
 	$houseKeeperServicesMode->setAttribute('disabled', 'disabled');
 }
-$houseKeeperTab->addRow(_('Keep data for (in days)'), $houseKeeperServicesMode);
+$itServicesTab->addRow(array(_('Keep data for (in days)'), $houseKeeperServicesMode));
+$itServicesTab->addClass('border_dotted objectgroup interface-group');
+$houseKeeperTab->addRow(_('IT services'), new CDiv($itServicesTab));
 
-// remove audit history block
-$houseKeeperTab->addRow(_('Enable housekeeping'), new CCheckBox('hk_audit_mode',
-	$this->data['config']['hk_audit_mode'], null, 1));
+// audit
+$auditTab = new CTable(null, 'formElementTable');
+
+$auditTab->addRow(
+	array(
+		new CDiv(_('Enable housekeeping'), 'hkElementLabel'),
+		new CCheckBox('hk_audit_mode', $this->data['config']['hk_audit_mode'], null, 1)
+	)
+);
+
 $houseKeeperAuditMode = new CNumericBox('hk_audit', $this->data['config']['hk_audit'], 5);
 if (!$this->data['config']['hk_audit_mode']) {
 	$houseKeeperAuditMode->setAttribute('disabled', 'disabled');
 }
-$houseKeeperTab->addRow(_('Keep data for (in days)'), $houseKeeperAuditMode);
+$auditTab->addRow(array(_('Keep data for (in days)'), $houseKeeperAuditMode));
+$auditTab->addClass('border_dotted objectgroup interface-group');
+$houseKeeperTab->addRow(_('Audit'), new CDiv($auditTab));
 
-// remove audit history block
-$houseKeeperTab->addRow(_('Enable housekeeping'), new CCheckBox('hk_sessions_mode',
-	$this->data['config']['hk_sessions_mode'], null, 1));
+// user session
+$userSessionTab = new CTable(null, 'formElementTable');
+
+$userSessionTab->addRow(
+	array(
+		new CDiv(_('Enable housekeeping'), 'hkElementLabel'),
+		new CCheckBox('hk_sessions_mode', $this->data['config']['hk_sessions_mode'], null, 1)
+	)
+);
+
 $houseKeeperSessionsMode = new CNumericBox('hk_sessions', $this->data['config']['hk_sessions'], 5);
 if (!$this->data['config']['hk_sessions_mode']) {
 	$houseKeeperSessionsMode->setAttribute('disabled', 'disabled');
 }
-$houseKeeperTab->addRow(_('Keep data for (in days)'), $houseKeeperSessionsMode);
+$userSessionTab->addRow(array(_('Keep data for (in days)'), $houseKeeperSessionsMode));
+$userSessionTab->addClass('border_dotted objectgroup interface-group');
+$houseKeeperTab->addRow(_('User sessions'), new CDiv($userSessionTab));
 
-// remove historical data block
-$houseKeeperTab->addRow(_('Enable housekeeping'), new CCheckBox('hk_history_mode',
-	$this->data['config']['hk_history_mode'], null, 1));
+// history
+$histortTab = new CTable(null, 'formElementTable');
+
+$histortTab->addRow(array(new CDiv(_('Enable housekeeping'), 'hkElementLabel'), new CCheckBox('hk_history_mode',
+	$this->data['config']['hk_history_mode'], null, 1)));
 $houseKeeperHistoryGlobal = new CCheckBox('hk_history_global',
 	$this->data['config']['hk_history_global'], null, 1);
 if (!$this->data['config']['hk_history_mode']) {
@@ -80,23 +119,29 @@ $houseKeeperHistoryModeGlobal = new CNumericBox('hk_history', $this->data['confi
 if (!$this->data['config']['hk_history_mode'] || !$this->data['config']['hk_history_global']) {
 	$houseKeeperHistoryModeGlobal->setAttribute('disabled', 'disabled');
 }
-$houseKeeperTab->addRow(_('Override item history period'), $houseKeeperHistoryGlobal);
-$houseKeeperTab->addRow(_('Keep data for (in days)'), $houseKeeperHistoryModeGlobal);
+$histortTab->addRow(array(_('Override item history period'), $houseKeeperHistoryGlobal));
+$histortTab->addRow(array(_('Keep data for (in days)'), $houseKeeperHistoryModeGlobal));
+$histortTab->addClass('border_dotted objectgroup interface-group');
+$houseKeeperTab->addRow(_('History'), new CDiv($histortTab));
 
-// remove trend data block
-$houseKeeperTab->addRow(_('Enable housekeeping'), new CCheckBox('hk_trends_mode',
-	$this->data['config']['hk_trends_mode'], null, 1));
-$houseKeeperTrendsGlobal= new CCheckBox('hk_trends_global',
+// trend
+$trendTab = new CTable(null, 'formElementTable');
+
+$trendTab->addRow(array(new CDiv(_('Enable housekeeping'), 'hkElementLabel'), new CCheckBox('hk_trends_mode',
+	$this->data['config']['hk_trends_mode'], null, 1)));
+$houseKeeperTrendGlobal = new CCheckBox('hk_trends_global',
 	$this->data['config']['hk_trends_global'], null, 1);
 if (!$this->data['config']['hk_trends_mode']) {
-	$houseKeeperTrendsGlobal->setAttribute('disabled', 'disabled');
+	$houseKeeperTrendGlobal->setAttribute('disabled', 'disabled');
 }
-$houseKeeperTrendsModeGlobal = new CNumericBox('hk_trends', $this->data['config']['hk_trends'], 5);
+$houseKeeperTrendModeGlobal = new CNumericBox('hk_trends', $this->data['config']['hk_trends'], 5);
 if (!$this->data['config']['hk_trends_mode'] || !$this->data['config']['hk_trends_global']) {
-	$houseKeeperTrendsModeGlobal->setAttribute('disabled', 'disabled');
+	$houseKeeperTrendModeGlobal->setAttribute('disabled', 'disabled');
 }
-$houseKeeperTab->addRow(_('Override item trends period'), $houseKeeperTrendsGlobal);
-$houseKeeperTab->addRow(_('Keep data for (in days)'), $houseKeeperTrendsModeGlobal);
+$trendTab->addRow(array(_('Override item trends period'), $houseKeeperTrendGlobal));
+$trendTab->addRow(array(_('Keep data for (in days)'), $houseKeeperTrendModeGlobal));
+$trendTab->addClass('border_dotted objectgroup interface-group');
+$houseKeeperTab->addRow(_('Trends'), new CDiv($trendTab));
 
 $houseKeeperView = new CTabView();
 $houseKeeperView->addTab('houseKeeper', _('Housekeeper'), $houseKeeperTab);
