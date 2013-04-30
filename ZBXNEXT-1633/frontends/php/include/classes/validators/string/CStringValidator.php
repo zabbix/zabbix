98a003960da009f38e39935cea1b7a5983b6de1a
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-04-30 07:21:11 +0000

    A.F....... [ZBXNEXT-1633] added code used for group prototype validation
    git-svn-id: svn://svn.zabbix.com/branches/dev@35338 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-1633/frontends/php/include/classes/validators/string/CStringValidator.php
@@ -0,0 +1,107 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2000-2012 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CStringValidator extends CValidator {
+
+	/**
+	 * If set to false, the string cannot be empty.
+	 *
+	 * @var bool
+	 */
+	public $empty = false;
+
+	/**
+	 * Maximum string length.
+	 *
+	 * @var int
+	 */
+	public $maxLength;
+
+	/**
+	 * Regex to match the string against.
+	 *
+	 * @var string
+	 */
+	public $regex;
+
+	/**
+	 * Error message if the string is empty.
+	 *
+	 * @var string
+	 */
+	public $messageEmpty = 'Empty string.';
+
+	/**
+	 * Error message if the string is too long.
+	 *
+	 * @var string
+	 */
+	public $messageMaxLength = 'String "%1$s" is too long, it must have less then %2$d characters.';
+
+	/**
+	 * Error message if the string doesn't match the regex.
+	 *
+	 * @var string
+	 */
+	public $messageRegex = 'Incorrect characters used for string "%1$s".';
+
+
+	/**
+	 * Checks if the given string is:
+	 * - empty
+	 * - not too long
+	 * - matches a certain regex
+	 *
+	 * @param string $value
+	 *
+	 * @return bool
+	 */
+	public function validate($value)
+	{
+		if (zbx_empty($value)) {
+			if ($this->empty) {
+				return true;
+			}
+			else {
+				$this->setError($this->messageEmpty);
+
+				return false;
+			}
+		}
+
+		if ($this->maxLength && zbx_strlen($value) > $this->maxLength) {
+			$this->setError(
+				_s($this->messageMaxLength, $value, $this->maxLength)
+			);
+
+			return false;
+		}
+
+		if ($this->regex && !preg_match($this->regex, $value)) {
+			$this->setError(_s($this->messageRegex, $value));
+
+			return false;
+		}
+
+		return true;
+	}
+
+}
