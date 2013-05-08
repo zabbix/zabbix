0ba52c209d527e0ade57edbd37a97726110e8a33
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-05-08 10:28:08 +0000

    ..F....... [ZBXNEXT-1712] fixed parent item editing
    git-svn-id: svn://svn.zabbix.com/branches/dev@35486 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1712/frontends/php/items.php
+++ ZBXNEXT-1712/frontends/php/items.php
@@ -323,7 +323,7 @@ else {
 	$_REQUEST['filter_ipmi_sensor'] = CProfile::get('web.items.filter_ipmi_sensor');
 }
 
-if (isset($_REQUEST['filter_hostname']) && !zbx_empty($_REQUEST['filter_hostname'])) {
+if (empty($_REQUEST['hostid']) && isset($_REQUEST['filter_hostname']) && !zbx_empty($_REQUEST['filter_hostname'])) {
 	$host = API::Host()->getObjects(array('name' => $_REQUEST['filter_hostname']));
 	if (empty($host)) {
 		$host = API::Template()->getObjects(array('name' => $_REQUEST['filter_hostname']));
