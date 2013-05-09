49e42dad3c2586bae5da2dd4e21bea84f966ba03
Andris Zeila <andris.zeila@zabbix.com>
2013-05-09 06:21:32 +0000

    ...G...PS. [ZBXNEXT-928] removed misc.c dependency on db.h

--- src/libs/zbxcommon/misc.c
+++ src/libs/zbxcommon/misc.c
@@ -19,7 +19,6 @@
 
 #include "common.h"
 #include "log.h"
-#include "db.h"
 
 #ifdef _WINDOWS
 char	ZABBIX_SERVICE_NAME[ZBX_SERVICE_NAME_LEN] = APPLICATION_NAME;
@@ -65,7 +64,7 @@ const char	*get_program_name(const char *path)
  ******************************************************************************/
 int	get_nodeid_by_id(zbx_uint64_t id)
 {
-	return (int)(id / ZBX_DM_MAX_HISTORY_IDS);
+	return (int)(id / __UINT64_C(100000000000000));
 }
 
 /******************************************************************************
