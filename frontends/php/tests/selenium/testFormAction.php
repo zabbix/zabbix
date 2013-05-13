381f0199c0da246727600b199dfa32f407ad55aa
egita <egita@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2013-05-13 13:10:27 +0000

    .......... [ZBXNEXT-686] fixed timout issue when new host added on operation for action page
    git-svn-id: svn://svn.zabbix.com/trunk@35604 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/tests/selenium/testFormAction.php
+++ frontends/php/tests/selenium/testFormAction.php
@@ -195,7 +195,8 @@ class testFormAction extends CWebTest {
 		$this->zbxTestClick('add');
 		sleep(1);
 		$this->select("opCmdTarget", "label=Host");
-		sleep(1);
+		$this->zbxTestTextPresent(array('Target list', 'Target', 'Action'));
+		$this->assertElementPresent("//input[@placeholder='type here to search']");
 		$this->input_type("//input[@placeholder='type here to search']", 'Simple form test host');
 		sleep(1);
 		$this->zbxTestClick("//span[@class='matched']");
@@ -206,7 +207,8 @@ class testFormAction extends CWebTest {
 		$this->zbxTestClick('add');
 		sleep(1);
 		$this->select("opCmdTarget", "label=Host group");
-		sleep(1);
+		$this->zbxTestTextPresent(array('Target list', 'Target', 'Action'));
+		$this->assertElementPresent("//input[@placeholder='type here to search']");
 		$this->input_type("//input[@placeholder='type here to search']", 'Zabbix servers');
 		sleep(1);
 		$this->zbxTestClick("//span[@class='matched']");
