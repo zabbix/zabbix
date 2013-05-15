24480884e04c736528c40878ed7892f8dd7684d1
Egita Sidorova <git-no-reply@zabbix.com>
2013-05-15 13:15:04 +0000

    .......... [ZBXNEXT-686] removed layout dublicated layout checks and added some changes to simple create checks

--- frontends/php/tests/selenium/testInheritanceTrigger.php
+++ frontends/php/tests/selenium/testInheritanceTrigger.php
@@ -56,177 +56,6 @@ class testInheritanceTrigger extends CWebTest {
 		DBsave_tables('triggers');
 	}
 
-	// Returns layout data
-	public static function layout() {
-		return array(
-			array(
-				array('constructor' => 'open')
-			),
-			array(
-				array('constructor' => 'open_close')
-			),
-			array(
-				array('constructor' => 'open', 'severity' => 'Warning')
-			),
-			array(
-				array('constructor' => 'open_close', 'severity' => 'Disaster')
-			),
-			array(
-				array('severity' => 'Not classified')
-			),
-			array(
-				array('severity' => 'Information')
-			),
-			array(
-				array('severity' => 'Warning')
-			),
-			array(
-				array('severity' => 'Average')
-			),
-			array(
-				array('severity' => 'High')
-			),
-			array(
-				array('severity' => 'Disaster')
-			)
-		);
-	}
-
-	/**
-	 * @dataProvider layout
-	 */
-	public function testInheritanceTrigger_CheckLayout($data) {
-		$this->zbxTestLogin('templates.php');
-
-		$this->zbxTestClickWait('link='.$this->template);
-		$this->zbxTestClickWait("//div[@class='w']//a[text()='Triggers']");
-		$this->checkTitle('Configuration of triggers');
-		$this->zbxTestTextPresent('CONFIGURATION OF TRIGGERS');
-
-		$this->zbxTestClickWait('form');
-		$this->checkTitle('Configuration of triggers');
-
-		if (isset($data['constructor'])) {
-			switch ($data['constructor']) {
-				case 'open':
-					$this->zbxTestClick("//span[text()='Expression constructor']");
-					sleep(1);
-					break;
-				case 'open_close':
-					$this->zbxTestClick("//span[text()='Expression constructor']");
-					sleep(1);
-					$this->zbxTestClick("//span[text()='Close expression constructor']");
-					sleep(1);
-					break;
-			}
-		}
-
-		$this->zbxTestTextPresent('Trigger');
-		$this->zbxTestTextPresent('Name');
-		$this->assertVisible('description');
-		$this->assertAttribute("//input[@id='description']/@maxlength", '255');
-		$this->assertAttribute("//input[@id='description']/@size", '50');
-
-		if (!(isset($data['constructor'])) || $data['constructor'] == 'open_close') {
-			$this->zbxTestTextPresent(array('Expression', 'Expression constructor'));
-			$this->assertVisible('expression');
-			$this->assertAttribute("//textarea[@id='expression']/@rows", '7');
-			$this->assertVisible('insert');
-			$this->assertAttribute("//input[@id='insert']/@value", 'Add');
-
-			$this->assertElementNotPresent('add_expression');
-			$this->assertElementNotPresent('insert_macro');
-			$this->assertElementNotPresent('exp_list');
-		} else {
-			$this->zbxTestTextPresent('Expression');
-			$this->assertVisible('expr_temp');
-			$this->assertAttribute("//textarea[@id='expr_temp']/@rows", '7');
-			$this->assertAttribute("//textarea[@id='expr_temp']/@readonly", 'readonly');
-			$this->zbxTestTextNotPresent('Expression constructor');
-			$this->assertNotVisible('expression');
-
-			$this->assertVisible('add_expression');
-			$this->assertAttribute("//input[@id='add_expression']/@value", 'Add');
-
-			$this->assertVisible('insert');
-			$this->assertAttribute("//input[@id='insert']/@value", 'Edit');
-
-			$this->assertVisible('insert_macro');
-			$this->assertAttribute("//input[@id='insert_macro']/@value", 'Insert macro');
-
-			$this->zbxTestTextPresent(array('Target', 'Expression', 'Error', 'Action', 'Close expression constructor'));
-			$this->assertVisible('exp_list');
-			$this->zbxTestTextPresent('Close expression constructor');
-		}
-
-		$this->zbxTestTextPresent('Multiple PROBLEM events generation');
-		$this->assertVisible('type');
-		$this->assertAttribute("//input[@id='type']/@type", 'checkbox');
-
-		$this->zbxTestTextPresent('Description');
-		$this->assertVisible('comments');
-		$this->assertAttribute("//textarea[@id='comments']/@rows", '7');
-
-		$this->zbxTestTextPresent('URL');
-		$this->assertVisible('url');
-		$this->assertAttribute("//input[@id='url']/@maxlength", '255');
-		$this->assertAttribute("//input[@id='url']/@size", '50');
-
-		$this->assertVisible('severity_0');
-		$this->assertAttribute("//*[@id='severity_0']/@checked", 'checked');
-		$this->assertElementPresent("//*[@id='severity_label_0']/span[text()='Not classified']");
-		$this->assertVisible('severity_1');
-		$this->assertElementPresent("//*[@id='severity_label_1']/span[text()='Information']");
-		$this->assertVisible('severity_2');
-		$this->assertElementPresent("//*[@id='severity_label_2']/span[text()='Warning']");
-		$this->assertVisible('severity_3');
-		$this->assertElementPresent("//*[@id='severity_label_3']/span[text()='Average']");
-		$this->assertVisible('severity_4');
-		$this->assertElementPresent("//*[@id='severity_label_4']/span[text()='High']");
-		$this->assertVisible('severity_5');
-		$this->assertElementPresent("//*[@id='severity_label_5']/span[text()='Disaster']");
-
-		if (isset($data['severity'])) {
-			switch ($data['severity']) {
-				case 'Not classified':
-					$this->zbxTestClick('severity_0');
-					break;
-				case 'Information':
-					$this->zbxTestClick('severity_1');
-					break;
-				case 'Warning':
-					$this->zbxTestClick('severity_2');
-					break;
-				case 'Average':
-					$this->zbxTestClick('severity_3');
-					break;
-				case 'High':
-					$this->zbxTestClick('severity_4');
-					break;
-				case 'Disaster':
-					$this->zbxTestClick('severity_5');
-					break;
-				default:
-					break;
-			}
-		}
-
-		$this->zbxTestTextPresent('Enabled');
-		$this->assertVisible('status');
-		$this->assertAttribute("//input[@id='status']/@type", 'checkbox');
-
-		$this->assertVisible('save');
-		$this->assertAttribute("//input[@id='save']/@value", 'Save');
-
-		$this->assertVisible('cancel');
-		$this->assertAttribute("//input[@id='cancel']/@value", 'Cancel');
-
-		$this->zbxTestClick('link=Dependencies');
-		$this->zbxTestTextPresent(array('Dependencies', 'Name', 'Action', 'No dependencies defined'));
-		$this->assertElementPresent('bnt1');
-		$this->assertAttribute("//input[@id='bnt1']/@value", 'Add');
-	}
-
 	// Returns update data
 	public static function update() {
 		return DBdata("select * from triggers t left join functions f on f.triggerid=t.triggerid where f.itemid='23329' and t.description LIKE 'testInheritanceTrigger%'");
@@ -302,7 +131,7 @@ class testInheritanceTrigger extends CWebTest {
 					'errors' => array(
 						'ERROR: Page received incorrect data',
 						'Warning. Incorrect value for field "Name": cannot be empty.',
-						'Warning. Incorrect value for field "expression": cannot be empty.'
+						'Warning. Incorrect value for field "Expression": cannot be empty.'
 					)
 				)
 			),
@@ -312,7 +141,7 @@ class testInheritanceTrigger extends CWebTest {
 					'description' => 'MyTrigger',
 					'errors' => array(
 						'ERROR: Page received incorrect data',
-						'Warning. Incorrect value for field "expression": cannot be empty.'
+						'Warning. Incorrect value for field "Expression": cannot be empty.'
 					)
 				)
 			),
@@ -465,17 +294,6 @@ class testInheritanceTrigger extends CWebTest {
 					'errors' => array(
 						'ERROR: Cannot add trigger',
 						'Incorrect item key "someItem.uptime" provided for trigger expression on "Inheritance test template".'
-					)
-				)
-			),
-			array(
-				array(
-					'expected' => TRIGGER_BAD,
-					'description' => 'MyTrigger',
-					'expression' => '{Inheritance test template:key-item-inheritance.somefunc(0)}<0',
-					'errors' => array(
-						'ERROR: Cannot add trigger',
-						'Cannot implode expression "{Inheritance test template:key-item-inheritance.somefunc(0)}<0". Incorrect trigger function "somefunc" provided in expression. Unknown function.'
 					)
 				)
 			),
