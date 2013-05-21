a464961f19e6fb4ac9300d29d20a0045da6ceaac
egita <egita@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-05-21 10:42:54 +0000

    .......... [ZBXNEXT-686] deleted testFormTrigger dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@35735 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-686-testFormTrigger/upgrades/dbpatches/1.6/oracle/patch/graphs.sql
+++
@@ -1,4 +0,0 @@
-alter table graphs add show_legend	number(10)	DEFAULT '0'	NOT NULL;
-alter table graphs add show_3d		number(10)	DEFAULT '0'	NOT NULL;
-alter table graphs add percent_left	number(5,2)	DEFAULT '0'	NOT NULL;
-alter table graphs add percent_right	number(5,2)	DEFAULT '0'	NOT NULL;
