3098ce48d219500eacee179497b444800aec7143
Pavels Jelisejevs <git-no-reply@zabbix.com>
2013-05-24 11:46:01 +0000

    A......... [ZBXNEXT-928] deleting an application used in a HTTP test will now unset the application from the test instead of raising an error

--- frontends/php/api/classes/managers/CApplicationManager.php
+++ frontends/php/api/classes/managers/CApplicationManager.php
@@ -317,6 +317,13 @@ class CApplicationManager {
 	 * @param array $applicationIds
 	 */
 	public function delete(array $applicationIds) {
+		// unset applications from http tests
+		DBexecute(
+			'UPDATE httptest ht'.
+			' SET ht.applicationid=NULL'.
+			' WHERE '.dbConditionInt('ht.applicationid', $applicationIds)
+		);
+
 		// remove Monitoring > Latest data toggle profile values related to given aplications
 		CProfile::delete('web.latest.toggle', $applicationIds);
 
