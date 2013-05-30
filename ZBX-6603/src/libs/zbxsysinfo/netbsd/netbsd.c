24ce0ed60826e8e846c8f1a7f4bbcfcaf051ef4c
tomtom <tomtom@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-05-30 12:00:08 +0000

    .......... [ZBX-6603] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@35968 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-6603/src/libs/zbxsysinfo/netbsd/netbsd.c
@@ -0,0 +1,53 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2013 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+#include "sysinfo.h"
+
+ZBX_METRIC	parameters_specific[] =
+/* 	KEY			FLAG		FUNCTION 	ADD_PARAM	TEST_PARAM */
+{
+	{"kernel.maxfiles",	0,		KERNEL_MAXFILES,	0,	0},
+	{"kernel.maxproc",	0,		KERNEL_MAXPROC, 	0,	0},
+
+	{"vfs.fs.size",		CF_USEUPARAM,	VFS_FS_SIZE, 		0,	"/"},
+	{"vfs.fs.inode",	CF_USEUPARAM,	VFS_FS_INODE,		0,	"/,free"},
+	{"vfs.fs.discovery",	0,		VFS_FS_DISCOVERY,	0,	0},
+
+	{"net.if.in",		CF_USEUPARAM,	NET_IF_IN,		0,	"lo0,bytes"},
+	{"net.if.out",		CF_USEUPARAM,	NET_IF_OUT,		0,	"lo0,bytes"},
+	{"net.if.total",	CF_USEUPARAM,	NET_IF_TOTAL,		0,	"lo0,bytes"},
+	{"net.if.collisions",   CF_USEUPARAM,   NET_IF_COLLISIONS,      0,      "lo0"},
+
+	{"vm.memory.size",	CF_USEUPARAM,	VM_MEMORY_SIZE,		0,	"free"},
+
+	{"proc.num",		CF_USEUPARAM,	PROC_NUM,		0,	"inetd,,"},
+	{"proc.mem",		CF_USEUPARAM,	PROC_MEM,		0,	"inetd,,"},
+
+	{"system.cpu.switches", 0,              SYSTEM_CPU_SWITCHES,    0,      0},
+	{"system.cpu.intr",     0,              SYSTEM_CPU_INTR,        0,      0},
+	{"system.cpu.util",	CF_USEUPARAM,	SYSTEM_CPU_UTIL,	0,	"all,user,avg1"},
+	{"system.cpu.load",	CF_USEUPARAM,	SYSTEM_CPU_LOAD,	0,	"all,avg1"},
+	{"system.cpu.num",	CF_USEUPARAM,	SYSTEM_CPU_NUM,		0,	"online"},
+
+	{"system.uptime",	0,		SYSTEM_UPTIME,		0,	0},
+	{"system.boottime",     0,      	SYSTEM_BOOTTIME,        0,      0},
+
+	{0}
+};
