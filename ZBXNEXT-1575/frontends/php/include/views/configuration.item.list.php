9ceca98e959d4d21a187ace1d14aee05f6c7696c
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-06-06 13:34:04 +0000

    ..F....... [ZBXNEXT-1575] fixed "Status" filter selection being remembered incorrectly when switching states multiple times ..F....... [ZBXNEXT-1575] fixed the "Reset" button not re-enabling the status filter .......... [ZBXNEXT-1575] minor code refactoring
    git-svn-id: svn://svn.zabbix.com/branches/dev@36139 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1575/frontends/php/include/views/configuration.item.list.php
+++ ZBXNEXT-1575/frontends/php/include/views/configuration.item.list.php
@@ -18,6 +18,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
+
 require_once dirname(__FILE__).'/js/configuration.item.list.js.php';
 
 $itemsWidget = new CWidget(null, 'item-list');
