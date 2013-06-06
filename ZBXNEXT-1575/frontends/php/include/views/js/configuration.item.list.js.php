4a9dd65d4834742eb638aa33cd4742ed9223e8ea
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-06-06 14:03:42 +0000

    ..F....... [ZBXNEXT-1575] changed the state filter to not restore the last remembered status filter value if no value has been chosen
    git-svn-id: svn://svn.zabbix.com/branches/dev@36143 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1575/frontends/php/include/views/js/configuration.item.list.js.php
+++ ZBXNEXT-1575/frontends/php/include/views/js/configuration.item.list.js.php
@@ -1,23 +1,26 @@
 <script type="text/javascript">
 	jQuery(function() {
 		// disable the status filter when using the state filter
-		jQuery('#filter_state').change(function() {
-			var stateObj = jQuery(this);
-			var statusObj = jQuery('#filter_status');
+		jQuery('#filter_state').change(function(event, saveValue) {
+			var stateObj = jQuery(this),
+				statusObj = jQuery('#filter_status'),
+				saveValue = (saveValue != undefined) ? saveValue : true;
 
 			if (stateObj.val() == -1) {
-				if (statusObj.prop('disabled')) {
+				// restore the last remembered status filter value
+				if (statusObj.prop('disabled') && statusObj.data('last-value') != undefined) {
 					statusObj.val(statusObj.data('last-value'));
 				}
 				statusObj.prop('disabled', false);
 			}
 			else {
-				if (!statusObj.prop('disabled')) {
+				// remember the last status filter value
+				if (!statusObj.prop('disabled') && saveValue) {
 					statusObj.data('last-value', statusObj.val());
 				}
 				statusObj.prop('disabled', true).val(<?php echo ITEM_STATUS_ACTIVE ?>);
 			}
 		})
-		.trigger('change');
+		.trigger('change', false);
 	});
 </script>
