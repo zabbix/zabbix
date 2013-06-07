ac666dd4a318003e2cf228ef32e8ddcc592ff0e0
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-06-07 07:15:14 +0000

    ..F....... [ZBXNEXT-1575] fixed js formatting
    git-svn-id: svn://svn.zabbix.com/branches/dev@36153 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1575/frontends/php/include/views/js/configuration.item.list.js.php
+++ ZBXNEXT-1575/frontends/php/include/views/js/configuration.item.list.js.php
@@ -4,11 +4,11 @@
 		jQuery('#filter_state').change(function(event, saveValue) {
 			var stateObj = jQuery(this),
 				statusObj = jQuery('#filter_status'),
-				saveValue = (saveValue != undefined) ? saveValue : true;
+				saveValue = (saveValue === undefined) ? true : saveValue;
 
 			if (stateObj.val() == -1) {
 				// restore the last remembered status filter value
-				if (statusObj.prop('disabled') && statusObj.data('last-value') != undefined) {
+				if (statusObj.prop('disabled') && statusObj.data('last-value') !== undefined) {
 					statusObj.val(statusObj.data('last-value'));
 				}
 				statusObj.prop('disabled', false);
