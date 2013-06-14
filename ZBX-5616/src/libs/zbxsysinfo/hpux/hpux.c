65389898f669c8dbb466192edd0a225c177d7863
igors <igors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-06-14 13:09:52 +0000

    ...GI..PS. [ZBX-5616] fixed usage of system.hostname in the configuration parameter HostnameItem on proxy
    git-svn-id: svn://svn.zabbix.com/branches/dev@36372 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5616/src/libs/zbxsysinfo/hpux/hpux.c
+++ ZBX-5616/src/libs/zbxsysinfo/hpux/hpux.c
@@ -35,7 +35,6 @@ ZBX_METRIC	parameters_specific[] =
 	{"system.cpu.load",	CF_HAVEPARAMS,	SYSTEM_CPU_LOAD,	"all,avg1"},
 	{"system.cpu.num",	CF_HAVEPARAMS,	SYSTEM_CPU_NUM,		"online"},
 
-	{"system.hostname",     0,              SYSTEM_HOSTNAME,        NULL},
 	{"system.uname",	0,		SYSTEM_UNAME,		NULL},
 	{"system.sw.arch",	0,		SYSTEM_SW_ARCH,		NULL},
 
--- ZBX-5616/src/libs/zbxsysinfo/win32/win32.c
+++ ZBX-5616/src/libs/zbxsysinfo/win32/win32.c
@@ -44,7 +44,6 @@ ZBX_METRIC	parameters_specific[] =
 	{"system.cpu.load",	CF_HAVEPARAMS,	SYSTEM_CPU_LOAD,	"all,avg1"},
 	{"system.cpu.num",	CF_HAVEPARAMS,	SYSTEM_CPU_NUM,		"online"},
 
-	{"system.hostname",     CF_HAVEPARAMS,  SYSTEM_HOSTNAME,        NULL},
 	{"system.sw.arch",	0,		SYSTEM_SW_ARCH,		NULL},
 
 	{"system.swap.size",	CF_HAVEPARAMS,	SYSTEM_SWAP_SIZE,	"all,free"},
