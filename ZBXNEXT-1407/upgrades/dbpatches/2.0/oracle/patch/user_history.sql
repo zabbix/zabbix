f0f9f9e0951c96ff198c8b6c241d6a5cf6c312d4
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-06-18 06:13:37 +0000

    .......... [ZBXNEXT-1407] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@36422 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1407/upgrades/dbpatches/2.0/oracle/patch/user_history.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE user_history MODIFY userhistoryid DEFAULT NULL;
-ALTER TABLE user_history MODIFY userid DEFAULT NULL;
-DELETE FROM user_history WHERE NOT userid IN (SELECT userid FROM users);
-ALTER TABLE user_history ADD CONSTRAINT c_user_history_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
