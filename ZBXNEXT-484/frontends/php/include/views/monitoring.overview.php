026ff4c5496efa078bc7fb045e4ecd3b07643ea3
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-06-18 08:11:31 +0000

    ..F....... [ZBXNEXT-484] fixed monitoring overview if no applications defined
    git-svn-id: svn://svn.zabbix.com/branches/dev@36426 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-484/frontends/php/include/views/monitoring.overview.php
+++ ZBXNEXT-484/frontends/php/include/views/monitoring.overview.php
@@ -91,24 +91,19 @@ $hostLocationForm->additem(array(_('Hosts location'), SPACE, $styleComboBox));
 
 $overviewWidget->addHeader($hostLocationForm);
 
-if ($this->data['pageFilter']->applicationsSelected) {
-	if ($this->data['type'] == SHOW_DATA) {
-		$dataTable = get_items_data_overview(
-			array_keys($this->data['pageFilter']->hosts),
-			$this->data['pageFilter']->application,
-			$this->data['view_style']
-		);
-	}
-	elseif ($this->data['type'] == SHOW_TRIGGERS) {
-		$dataTable = get_triggers_overview(
-			array_keys($this->data['pageFilter']->hosts),
-			$this->data['pageFilter']->application,
-			$this->data['view_style']
-		);
-	}
+if ($this->data['type'] == SHOW_DATA) {
+	$dataTable = get_items_data_overview(
+		array_keys($this->data['pageFilter']->hosts),
+		$this->data['pageFilter']->application,
+		$this->data['view_style']
+	);
 }
-else {
-	$dataTable = new CTableInfo(_('No items defined.'));
+elseif ($this->data['type'] == SHOW_TRIGGERS) {
+	$dataTable = get_triggers_overview(
+		array_keys($this->data['pageFilter']->hosts),
+		$this->data['pageFilter']->application,
+		$this->data['view_style']
+	);
 }
 
 $overviewWidget->addItem($dataTable);
