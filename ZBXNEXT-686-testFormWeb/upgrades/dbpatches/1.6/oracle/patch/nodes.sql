b1201ee147196b668c1f04b2c259dfe17ef29dc3
egita <egita@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-07-04 13:48:26 +0000

    .......... [ZBXNEXT-686] deleted testFormWeb dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@36746 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-686-testFormWeb/upgrades/dbpatches/1.6/oracle/patch/nodes.sql
+++
@@ -1,5 +0,0 @@
-alter table nodes modify ip varchar2(39) DEFAULT '';
-alter table nodes drop column event_lastid;
-alter table nodes drop column history_lastid;
-alter table nodes drop column history_str_lastid;
-alter table nodes drop column history_uint_lastid;
