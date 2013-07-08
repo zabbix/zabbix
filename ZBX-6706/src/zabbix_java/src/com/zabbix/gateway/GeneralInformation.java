fa9b656936d31ef5e18ef6e7b377d3f89cca758a
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-07-08 13:14:01 +0000

    .......... [ZBX-6706] merged from upstream
    git-svn-id: svn://svn.zabbix.com/branches/dev@36786 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6706/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ ZBX-6706/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,7 +22,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	public static final String REVISION_DATE = "21 May 2012";
+	public static final String REVISION_DATE = "05 July 2013";
 	public static final String REVISION = "{ZABBIX_REVISION}";
 	public static final String VERSION = "2.1.0";
 
