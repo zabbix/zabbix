ee3363353d9551ae8cbf03e1e617dad0fe8da85f
guntis <guntis@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-07-15 09:28:41 +0000

    ..F....... [ZBX-6776] Added sorting by icon names.
    git-svn-id: svn://svn.zabbix.com/branches/dev@36955 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6776/frontends/php/sysmap.php
+++ ZBX-6776/frontends/php/sysmap.php
@@ -184,10 +184,15 @@ $icons = DBselect(
 	' WHERE i.imagetype='.IMAGE_TYPE_ICON.
 		andDbNode('i.imageid')
 );
+$tempArray=array();
 while ($icon = DBfetch($icons)) {
-	$data['iconList'][] = array('imageid' => $icon['imageid'], 'name' => $icon['name']);
+	$tempArray[] = array('imageid' => $icon['imageid'], 'name' => $icon['name']);
 }
-order_result($data['iconList'], 'name');
+CArrayHelper::sort($tempArray,array('name'));
+foreach($tempArray as $value){
+	$data['iconList'][]=$value;
+}
+unset($tempArray);
 
 $data['sysmap']['selements'] = zbx_toHash($data['sysmap']['selements'], 'selementid');
 $data['sysmap']['links'] = zbx_toHash($data['sysmap']['links'], 'linkid');
