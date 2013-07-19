6144da8556d6a489a0d2b58b07b6447ee86f2c87
guntis <guntis@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-07-19 08:52:48 +0000

    ..F....... [ZBX-6586] Merged r37054 through r37166
    git-svn-id: svn://svn.zabbix.com/branches/dev@37167 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6586/frontends/php/js/multiselect.js
+++ ZBX-6586/frontends/php/js/multiselect.js
@@ -136,6 +136,18 @@ jQuery(function($) {
 			};
 
 			/**
+			 * Rezise multiselect selected text
+			 */
+			$.fn.multiSelect.resize = function() {
+				$.each(window.multiSelect, function(i) {
+					var ms = window.multiSelect[i];
+					if (!empty(ms.options.data)) {
+						resizeAllSelectedTexts(ms.obj, ms.options, ms.values);
+					}
+				});
+			}
+
+			/**
 			 * MultiSelect object.
 			 */
 			if (empty(window.multiSelect)) {
@@ -406,8 +418,7 @@ jQuery(function($) {
 			}
 
 			// resize
-			resizeSelected(obj, values, options);
-			resizeAvailable(obj);
+			resize(obj, values, options);
 		});
 	};
 
@@ -551,10 +562,11 @@ jQuery(function($) {
 				text: empty(item.prefix) ? item.name : item.prefix + item.name
 			});
 
-			if (options.disabled) {
-				$('.selected ul', obj).append(li.append(text));
-			}
-			else {
+			$('.selected ul', obj).append(li.append(text));
+
+			resizeSelectedText(li, text, obj, options);
+
+			if (!options.disabled) {
 				var arrow = $('<span>', {
 					'class': 'arrow',
 					'data-id': item.id
@@ -563,14 +575,13 @@ jQuery(function($) {
 					removeSelected($(this).data('id'), obj, values, options);
 				});
 
-				$('.selected ul', obj).append(li.append(text, arrow));
+				$('.selected ul', obj).append(li.append(arrow));
 			}
 
 			removePlaceholder(obj);
 
 			// resize
-			resizeSelected(obj, values, options);
-			resizeAvailable(obj);
+			resize(obj, values, options);
 
 			// set readonly
 			if (options.selectedLimit > 0 && $('.selected li', obj).length == options.selectedLimit) {
@@ -587,8 +598,7 @@ jQuery(function($) {
 		delete values.selected[id];
 
 		// resize
-		resizeSelected(obj, values, options);
-		resizeAvailable(obj);
+		resize(obj, values, options);
 
 		// remove readonly
 		if ($('.selected li', obj).length == 0) {
@@ -704,12 +714,19 @@ jQuery(function($) {
 		}
 	}
 
+	function resize(obj, values, options) {
+		if (!options.selectedLimit || $('.selected li', obj).length < options.selectedLimit) {
+			resizeSelected(obj, values, options);
+			resizeAvailable(obj);
+		}
+	}
+
 	function resizeSelected(obj, values, options) {
 		// settings
-		var settingTopPaddings = IE8 ? 1 : 0,
+		var settingTopPaddingsEmpty = IE8 ? 1 : 0,
+			settingTopPaddingsExist = IE8 ? 1 : 2,
 			settingTopPaddingsInit = 3,
 			settingRightPaddings = 4,
-			settingLeftPaddings = 13,
 			settingMinimumWidth = 50,
 			settingMinimumHeight = 12,
 			settingNewLineTopPaddings = 6;
@@ -720,16 +737,15 @@ jQuery(function($) {
 			height = 0;
 
 		if ($('.selected li', obj).length > 0) {
-			var position = options.disabled
-				? $('.selected li:last-child', obj).position()
-				: $('.selected li:last-child .arrow', obj).position();
+			var lastItem = $('.selected li:last-child', obj),
+				position = lastItem.position();
 
-			top = position.top + settingTopPaddings - 1;
-			left = position.left + settingLeftPaddings;
+			top = position.top + settingTopPaddingsExist;
+			left = position.left + lastItem.width();
 			height = $('.selected li:last-child', obj).height();
 		}
 		else {
-			top = settingTopPaddingsInit + settingTopPaddings;
+			top = settingTopPaddingsInit + settingTopPaddingsEmpty;
 			height = 0;
 		}
 
@@ -779,6 +795,60 @@ jQuery(function($) {
 
 		$('.available', obj).css({
 			top: (selectedHeight > 0) ? selectedHeight : 20
+		});
+	}
+
+	function resizeSelectedText(item, text, obj, options) {
+		// settings
+		var settingLineHeight = 15,
+			settingArrowSpace = options.disabled ? 22 : 32,
+			settingPaddings = 3,
+			settingTextMax = 510;
+
+		// calculate
+		var maxWidth = $('.selected ul', obj).width() - settingArrowSpace;
+
+		if (text.width() > maxWidth || text.height() > settingLineHeight) {
+			var i = 0;
+
+			while (text.width() > maxWidth || text.height() > settingLineHeight) {
+				var t = text.text();
+
+				text.text(t.substring(0, t.length - 1));
+
+				i++;
+
+				if (i > settingTextMax) {
+					break;
+				}
+			}
+
+			text.text(text.text() + '...');
+
+			item.css('width', $('.selected ul', obj).width() - settingPaddings);
+		}
+		else {
+			item.css('width', '');
+
+			if (!options.disabled) {
+				text.css('padding-right', 16);
+			}
+		}
+	}
+
+	function resizeAllSelectedTexts(obj, options, values) {
+		$('.selected li', obj).each(function() {
+			var item = $(this),
+				id = item.data('id'),
+				text = $(item.children()[0]),
+				t = empty(values.selected[id].prefix)
+					? values.selected[id].name
+					: values.selected[id].prefix + values.selected[id].name;
+
+			// rewrite previous text to original
+			text.text(t);
+
+			resizeSelectedText(item, text, obj, options);
 		});
 	}
 
