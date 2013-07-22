841ef77757c7c5f0454a47aa0929f59b69f5b835
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-07-22 07:26:38 +0000

    ....I..... [ZBX-6587] replaced windows file descriptions with executable names.
    Not pretty, but less confusing.
    
    
    git-svn-id: svn://svn.zabbix.com/branches/dev@37196 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6587/build/win32/project/zabbix_get_desc.h
+++ ZBX-6587/build/win32/project/zabbix_get_desc.h
@@ -1,6 +1,6 @@
 #ifndef _ZABBIX_GET_DESC_H_
 #define _ZABBIX_GET_DESC_H_
 
-#define VER_FILEDESCRIPTION_STR		"Zabbix get"
+#define VER_FILEDESCRIPTION_STR		"zabbix_get.exe"
 
 #endif
--- ZBX-6587/build/win32/project/zabbix_sender_desc.h
+++ ZBX-6587/build/win32/project/zabbix_sender_desc.h
@@ -1,6 +1,6 @@
 #ifndef _ZABBIX_SENDER_DESC_H_
 #define _ZABBIX_SENDER_DESC_H_
 
-#define VER_FILEDESCRIPTION_STR		"Zabbix sender"
+#define VER_FILEDESCRIPTION_STR		"zabbix_sender.exe"
 
 #endif
