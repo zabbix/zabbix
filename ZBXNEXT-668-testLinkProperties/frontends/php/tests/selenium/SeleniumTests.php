71534763f6fff8c9067821cb932e0831d28b0bcd
egita <egita@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-07-23 06:39:55 +0000

    .......... [ZBXNEXT-686] added testLinkProperties to SeleniumTests.php
    git-svn-id: svn://svn.zabbix.com/branches/dev@37225 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-668-testLinkProperties/frontends/php/tests/selenium/SeleniumTests.php
+++ ZBXNEXT-668-testLinkProperties/frontends/php/tests/selenium/SeleniumTests.php
@@ -100,6 +100,7 @@ require_once dirname(__FILE__).'/testInheritanceGraphPrototype.php';
 require_once dirname(__FILE__).'/testZBX6339.php';
 require_once dirname(__FILE__).'/testZBX6648.php';
 require_once dirname(__FILE__).'/testZBX6663.php';
+require_once dirname(__FILE__).'/testLinkProperties.php';
 
 class SeleniumTests {
 	public static function suite() {
@@ -186,6 +187,7 @@ class SeleniumTests {
 		$suite->addTestSuite('testZBX6339');
 		$suite->addTestSuite('testZBX6648');
 		$suite->addTestSuite('testZBX6663');
+		$suite->addTestSuite('testLinkProperties');
 
 		return $suite;
 	}
