c239e3d66933d517bd668ff370b7baa429527c9e
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-07-24 10:10:42 +0000

    .......... [ZBX-6646] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@37260 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6646/upgrades/dbpatches/2.0/ibm_db2/patch/history_uint.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE history_uint ALTER COLUMN itemid SET WITH DEFAULT NULL
-/
-REORG TABLE history_uint
-/
-ALTER TABLE history_uint ADD ns integer WITH DEFAULT '0' NOT NULL
-/
-REORG TABLE history_uint
-/
