528e0ee4043d7faae3f6f9e2c5152386224ff5ba
Andris Zeila <andris.zeila@zabbix.com>
2013-07-26 11:15:49 +0000

    ....I..... [ZBXNEXT-1689] added db patch to update items history field from 0 to 1

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -20,7 +20,7 @@
 
 define('ZABBIX_VERSION',     '2.1.2');
 define('ZABBIX_API_VERSION', '2.1.2');
-define('ZABBIX_DB_VERSION',	 2010091);
+define('ZABBIX_DB_VERSION',	 2010092);
 
 define('ZABBIX_COPYRIGHT_FROM', '2001');
 define('ZABBIX_COPYRIGHT_TO',   '2013');
