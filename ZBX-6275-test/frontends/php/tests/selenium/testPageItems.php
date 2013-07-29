9d8a3138c42db0abc3d4d0e608f6889ebfcd17dd
egita <egita@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-07-29 12:28:58 +0000

    .......... [ZBXNEXT-686] added page layout checks for templated items, plus check for column Error not present on the list as for changes on ZBX-6275
    git-svn-id: svn://svn.zabbix.com/branches/dev@37384 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6275-test/frontends/php/tests/selenium/testPageItems.php
+++ ZBX-6275-test/frontends/php/tests/selenium/testPageItems.php
@@ -22,56 +22,106 @@ require_once dirname(__FILE__) . '/../include/class.cwebtest.php';
 
 class testPageItems extends CWebTest {
 	// Returns all hosts
-	public static function allHosts() {
-		return DBdata('select * from hosts where status in ('.HOST_STATUS_MONITORED.','.HOST_STATUS_NOT_MONITORED.')');
+	public static function data() {
+		return DBdata('SELECT * FROM hosts
+					WHERE status IN ('
+						.HOST_STATUS_MONITORED.','
+						.HOST_STATUS_NOT_MONITORED.','
+						.HOST_STATUS_TEMPLATE.')');
 	}
 
 	/**
-	* @dataProvider allHosts
+	* @dataProvider data
 	*/
 
-	public function testPageItems_CheckLayout($host) {
-		$hostid = $host['hostid'];
+	public function testPageItems_CheckLayout($data) {
+		if ($data['status'] == HOST_STATUS_MONITORED || $data['status'] == HOST_STATUS_NOT_MONITORED) {
+			$hostid = $data['hostid'];
 
-		$this->zbxTestLogin('hosts.php');
-		$this->zbxTestDropdownSelectWait('groupid', 'all');
+			$this->zbxTestLogin('hosts.php');
+			$this->zbxTestDropdownSelectWait('groupid', 'all');
 
-		$this->checkTitle('Configuration of hosts');
-		$this->zbxTestTextPresent('HOSTS');
-		// Go to the list of items
-		$this->href_click("items.php?filter_set=1&hostid=$hostid&sid=");
-		$this->wait();
-		// We are in the list of items
-		$this->checkTitle('Configuration of items');
-		$this->zbxTestTextPresent('CONFIGURATION OF ITEMS');
-		$this->zbxTestTextPresent('Items');
-		$this->zbxTestTextPresent('Displaying');
-		$this->zbxTestTextPresent('Host list');
-		// Header
-		$this->zbxTestTextPresent(
-			array(
-				'Wizard',
-				'Name',
-				'Triggers',
-				'Key',
-				'Interval',
-				'History',
-				'Trends',
-				'Type',
-				'Applications',
-				'Status',
-				'Error'
-			)
-		);
-		// someday should check that interval is not shown for trapper items, trends not shown for non-numeric items etc
+			$this->checkTitle('Configuration of hosts');
+			$this->zbxTestTextPresent('HOSTS');
+			// Go to the list of items
+			$this->href_click("items.php?filter_set=1&hostid=$hostid&sid=");
+			$this->wait();
+			// We are in the list of items
+			$this->checkTitle('Configuration of items');
+			$this->zbxTestTextPresent('CONFIGURATION OF ITEMS');
+			$this->zbxTestTextPresent('Items');
+			$this->zbxTestTextPresent('Displaying');
+			$this->zbxTestTextPresent('Host list');
+			// Header
+			$this->zbxTestTextPresent(
+				array(
+					'Wizard',
+					'Name',
+					'Triggers',
+					'Key',
+					'Interval',
+					'History',
+					'Trends',
+					'Type',
+					'Applications',
+					'Status',
+					'Error'
+				)
+			);
+			// someday should check that interval is not shown for trapper items, trends not shown for non-numeric items etc
 
-		$this->zbxTestDropdownHasOptions('go', array(
-				'Enable selected',
-				'Disable selected',
-				'Mass update',
-				'Copy selected to ...',
-				'Clear history for selected',
-				'Delete selected'
-		));
+			$this->zbxTestDropdownHasOptions('go', array(
+					'Enable selected',
+					'Disable selected',
+					'Mass update',
+					'Copy selected to ...',
+					'Clear history for selected',
+					'Delete selected'
+			));
+		}
+		if ($data['status'] == HOST_STATUS_TEMPLATE) {
+			$templateid = $data['hostid'];
+
+			$this->zbxTestLogin('templates.php');
+			$this->zbxTestDropdownSelectWait('groupid', 'all');
+
+			$this->checkTitle('Configuration of templates');
+			$this->zbxTestTextPresent('TEMPLATES');
+			// Go to the list of items
+			$this->href_click("items.php?filter_set=1&groupid=0&hostid=$templateid&sid=");
+			$this->wait();
+			// We are in the list of items
+			$this->checkTitle('Configuration of items');
+			$this->zbxTestTextPresent('CONFIGURATION OF ITEMS');
+			$this->zbxTestTextPresent('Items');
+			$this->zbxTestTextPresent('Displaying');
+			$this->zbxTestTextPresent('Template list');
+			// Header
+			$this->zbxTestTextPresent(
+				array(
+					'Wizard',
+					'Name',
+					'Triggers',
+					'Key',
+					'Interval',
+					'History',
+					'Trends',
+					'Type',
+					'Applications',
+					'Status'
+				)
+			);
+			$this->zbxTestTextNotPresent('Error');
+			// someday should check that interval is not shown for trapper items, trends not shown for non-numeric items etc
+
+			$this->zbxTestDropdownHasOptions('go', array(
+					'Enable selected',
+					'Disable selected',
+					'Mass update',
+					'Copy selected to ...',
+					'Clear history for selected',
+					'Delete selected'
+			));
+		}
 	}
 }
