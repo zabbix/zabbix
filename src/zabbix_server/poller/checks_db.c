6bc1440353e23e90b8c4cfe8aab1ec0aa1583349
Andris Zeila <andris.zeila@zabbix.com>
2013-08-07 10:57:42 +0000

    .......PS. [ZBXNEXT-817] reworked database monitoring ODBC parameter storage and added db upgrade patch
    * store DSN into item key as the second parameter
    * store username,password into item username,password fields
    * store sql query into item params field

--- src/zabbix_server/poller/checks_db.c
+++ src/zabbix_server/poller/checks_db.c
@@ -26,90 +26,6 @@
 #include "checks_db.h"
 #include "log.h"
 
-#ifdef HAVE_ODBC
-/******************************************************************************
- *                                                                            *
- * Function: get_param_value                                                  *
- *                                                                            *
- * Purpose: retrieve parameter value by name                                  *
- *                                                                            *
- * Parameters: params - list of params                                        *
- *             param_name - name of requested parameter                       *
- *                                                                            *
- * Return value: NULL - if parameter missing param_name,                      *
- *               else return value in new allocated memory                    *
- *                                                                            *
- * Author: Eugene Grigorjev                                                   *
- *                                                                            *
- * Comments: this function allocates memory, required zbx_free for result!!!  *
- *           one parameter format: param1_name=param1_value                   *
- *           parameters separated by '\n'                                     *
- *                                                                            *
- ******************************************************************************/
-static char	*get_param_value(char *params, const char *param_name)
-{
-	char	*p, *l, *n, *r, *buf = NULL;
-
-	assert(NULL != params);
-
-	for (p = params; '\0' != *p; p++)
-	{
-		r = NULL;
-
-		/* trim left spaces */
-		for (; 0 != isspace(*p); p++)
-			;
-
-		/* find '=' */
-		for (n = p; '\0' != *n && '\n' != *n; n++)
-		{
-			if ('=' == *n)
-			{
-				/* trim right spaces */
-				for (l = n - 1; 0 != isspace(*l); l--)
-					;
-
-				l++;
-
-				/* compare parameter name */
-				if (l - p != strlen(param_name) || 0 != strncmp(p, param_name, l - p))
-					break;
-
-				r = n + 1;
-
-				break;
-			}
-		}
-
-		/* find EOL */
-		for (p = n; '\0' != *p && '\n' != *p; p++)
-			;
-
-		/* allocate result */
-		if (NULL != r)
-		{
-			/* trim right EOL characters */
-			while ('\r' == *p || '\n' == *p || '\0' == *p)
-				p--;
-
-			p++;
-
-			/* allocate result */
-			buf = zbx_malloc(buf, p - r + 1);
-			memmove(buf, r, p - r);
-			buf[p - r] = '\0';
-
-			break;
-		}
-	}
-
-	if (NULL == buf)
-		buf = zbx_strdup(buf, "");
-
-	return buf;
-}
-#endif	/* HAVE_ODBC */
-
 /******************************************************************************
  *                                                                            *
  * Function: get_value_db                                                     *
@@ -132,7 +48,7 @@ int	get_value_db(DC_ITEM *item, AGENT_RESULT *result)
 #ifdef HAVE_ODBC
 	ZBX_ODBC_DBH	dbh;
 	ZBX_ODBC_ROW	row;
-	char		*db_dsn = NULL, *db_user = NULL, *db_pass = NULL, *db_sql = NULL;
+	char		params[MAX_STRING_LEN], db_dsn[MAX_STRING_LEN];
 #endif
 	int		ret = NOTSUPPORTED;
 
@@ -146,14 +62,17 @@ int	get_value_db(DC_ITEM *item, AGENT_RESULT *result)
 
 	if (0 == strncmp(item->key, DB_ODBC_SELECT_KEY, strlen(DB_ODBC_SELECT_KEY)))
 	{
-		db_dsn = get_param_value(item->params, "DSN");
-		db_user = get_param_value(item->params, "user");
-		db_pass = get_param_value(item->params, "password");
-		db_sql = get_param_value(item->params, "sql");
 
-		if (SUCCEED == odbc_DBconnect(&dbh, db_dsn, db_user, db_pass, CONFIG_TIMEOUT))
+		if (ZBX_COMMAND_WITH_PARAMS != parse_command(item->key, NULL, 0, params, sizeof(params)) ||
+				0 != get_param(params, 2, db_dsn, sizeof(db_dsn)))
 		{
-			if (NULL != odbc_DBselect(&dbh, db_sql))
+			SET_MSG_RESULT(result, zbx_strdup(NULL, "Failed to parse item key."));
+			goto out;
+		}
+
+		if (SUCCEED == odbc_DBconnect(&dbh, db_dsn, item->username, item->password, CONFIG_TIMEOUT))
+		{
+			if (NULL != odbc_DBselect(&dbh, item->params))
 			{
 				if (NULL != (row = odbc_DBfetch(&dbh)))
 				{
@@ -186,16 +105,12 @@ int	get_value_db(DC_ITEM *item, AGENT_RESULT *result)
 		}
 		else
 			SET_MSG_RESULT(result, zbx_strdup(NULL, get_last_odbc_strerror()));
-
-		zbx_free(db_dsn);
-		zbx_free(db_user);
-		zbx_free(db_pass);
-		zbx_free(db_sql);
 	}
 #undef DB_ODBC_SELECT_KEY
 
 #endif	/* HAVE_ODBC */
 
+out:
 	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%s", __function_name, zbx_result_string(ret));
 
 	return ret;
--- src/zabbix_server/poller/poller.c
+++ src/zabbix_server/poller/poller.c
@@ -662,6 +662,13 @@ static int	get_values(unsigned char poller_type)
 						NULL, &items[i].password, MACRO_TYPE_COMMON, NULL, 0);
 				/* break; is not missing here */
 			case ITEM_TYPE_DB_MONITOR:
+				items[i].username = zbx_strdup(items[i].username, items[i].username_orig);
+				items[i].password = zbx_strdup(items[i].password, items[i].password_orig);
+
+				substitute_simple_macros(NULL, NULL, NULL, NULL, NULL, NULL, &items[i],
+						NULL, &items[i].username, MACRO_TYPE_COMMON, NULL, 0);
+				substitute_simple_macros(NULL, NULL, NULL, NULL, NULL, NULL, &items[i],
+						NULL, &items[i].password, MACRO_TYPE_COMMON, NULL, 0);
 				substitute_simple_macros(NULL, NULL, NULL, NULL, NULL, NULL, &items[i],
 						NULL, &items[i].params, MACRO_TYPE_PARAMS_FIELD, NULL, 0);
 				break;
@@ -749,6 +756,10 @@ static int	get_values(unsigned char poller_type)
 				zbx_free(items[i].privatekey);
 				/* break; is not missing here */
 			case ITEM_TYPE_TELNET:
+				zbx_free(items[i].username);
+				zbx_free(items[i].password);
+				break;
+			case ITEM_TYPE_DB_MONITOR:
 				zbx_free(items[i].username);
 				zbx_free(items[i].password);
 				break;
