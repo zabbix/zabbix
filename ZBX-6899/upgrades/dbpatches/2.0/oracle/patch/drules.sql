0a98566b9a5fceaf27e88ff1bfaed436226075a9
vitalij <vitalij@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-08-16 06:45:23 +0000

    .......... [ZBX-6899] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@37870 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-6899/upgrades/dbpatches/2.0/oracle/patch/drules.sql
@@ -0,0 +1,15 @@
+ALTER TABLE dchecks MODIFY dcheckid DEFAULT NULL;
+ALTER TABLE dchecks MODIFY druleid DEFAULT NULL;
+ALTER TABLE dchecks MODIFY key_ DEFAULT '';
+ALTER TABLE dchecks MODIFY snmp_community DEFAULT '';
+ALTER TABLE dchecks ADD uniq number(10) DEFAULT '0' NOT NULL;
+DELETE FROM dchecks WHERE NOT druleid IN (SELECT druleid FROM drules);
+ALTER TABLE dchecks ADD CONSTRAINT c_dchecks_1 FOREIGN KEY (druleid) REFERENCES drules (druleid) ON DELETE CASCADE;
+UPDATE dchecks SET uniq=1 WHERE dcheckid IN (SELECT unique_dcheckid FROM drules);
+ALTER TABLE drules MODIFY druleid DEFAULT NULL;
+ALTER TABLE drules MODIFY proxy_hostid DEFAULT NULL;
+ALTER TABLE drules MODIFY proxy_hostid NULL;
+ALTER TABLE drules MODIFY delay DEFAULT '3600';
+ALTER TABLE drules DROP COLUMN unique_dcheckid;
+UPDATE drules SET proxy_hostid=NULL WHERE NOT proxy_hostid IN (SELECT hostid FROM hosts);
+ALTER TABLE drules ADD CONSTRAINT c_drules_1 FOREIGN KEY (proxy_hostid) REFERENCES hosts (hostid);
