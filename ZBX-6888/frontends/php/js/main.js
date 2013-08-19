c90c991cc1fd993d8d267033ca733c6579f0fb27
vitalij <vitalij@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-08-19 13:15:31 +0000

    ..F....... [ZBX-6888] if element coding style fix
    git-svn-id: svn://svn.zabbix.com/branches/dev@37935 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6888/frontends/php/js/main.js
+++ ZBX-6888/frontends/php/js/main.js
@@ -383,8 +383,12 @@ var chkbxRange = {
 
 			cookie.createJSON(this.cookieName, this.selectedIds);
 
-			if(jQuery('#go').length) jQuery('#go')[0].disabled = (countChecked == 0);
-			if(jQuery('#goButton').length) jQuery('#goButton')[0].disabled = (countChecked == 0);
+			if (jQuery('#go').length) {
+				jQuery('#go')[0].disabled = (countChecked == 0);
+			}
+			if (jQuery('#goButton').length) {
+				jQuery('#goButton')[0].disabled = (countChecked == 0);
+			}
 
 			this.pageGoCount = countChecked;
 		}
