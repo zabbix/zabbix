1e2129c9feb37aaa2dfe77292c2e613c4188ab49
igors <igors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-08-23 13:12:32 +0000

    ...G...PS. [ZBXNEXT-1855] minor code improvements
    git-svn-id: svn://svn.zabbix.com/branches/dev@38095 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1855/src/libs/zbxcommon/setproctitle.c
+++ ZBXNEXT-1855/src/libs/zbxcommon/setproctitle.c
@@ -24,11 +24,6 @@
 #include <crt_externs.h>
 #endif
 
-#ifdef HAVE_PS_STRINGS
-#include <machine/vmparam.h>
-#include <sys/exec.h>
-#endif
-
 #ifdef HAVE_SYS_PSTAT_H
 #include <sys/pstat.h>
 #endif
@@ -39,10 +34,6 @@ extern char **environ;
 #	define PS_USE_SETPROCTITLE	/* use the function setproctitle() (newer BSD systems) */
 #elif defined(HAVE_SYS_PSTAT_H) && defined(PSTAT_SETCMD)
 #	define PS_USE_PSTAT		/* use the pstat() (HPUX)*/
-#elif defined(HAVE_PS_STRINGS)
-#	define PS_USE_PS_STRINGS	/* assign PS_STRINGS->ps_argvstr = "string" (some BSD systems) */
-#elif (defined(BSD) || defined(__bsdi__)) && !defined(MAC_OS_X)
-#	define PS_USE_CHANGE_ARGV	/* assign argv[0] = "string" (some other BSD systems) */
 #elif defined(__linux__) || defined(_AIX) || (defined(sun) && !defined(BSD)) || defined(__osf__) || defined(MAC_OS_X)
 #	define PS_USE_CLOBBER_ARGV	/* write over the argv and environment area (most SysV-like systems) */
 #else
@@ -50,7 +41,7 @@ extern char **environ;
 #endif
 
 /* Different systems want the buffer padded differently */
-#if defined(_AIX) || defined(__linux__)
+#if defined(_AIX) || defined(__linux__) || defined(MAC_OS_X)
 #	define PS_PADDING	'\0'
 #else
 #	define PS_PADDING	' '
@@ -87,84 +78,65 @@ void save_ps_display_args(int argc, char **argv)
 #if defined(PS_USE_CLOBBER_ARGV)
 	char	*end_of_area = NULL;
 	char	**new_environ = NULL;
-#endif   /* PS_USE_CLOBBER_ARGV */
-
-#if defined(PS_USE_CHANGE_ARGV) || defined(PS_USE_CLOBBER_ARGV)
 	char	**new_argv = NULL;
 	int	i;
-#endif   /* PS_USE_CHANGE_ARGV or PS_USE_CLOBBER_ARGV */
+#endif /* PS_USE_CLOBBER_ARGV */
 
 	save_argc = argc;
 	save_argv = argv;
 
 #if defined(PS_USE_CLOBBER_ARGV)
-/* count the available space before overwriting argv area and move the environment to make additional room */
+	/* count the available space before overwriting argv area and move the environment to make additional room */
+	/* check for contiguous argv strings */
+	for (i = 0; i < argc; i++)
 	{
-		/* check for contiguous argv strings */
-		for (i = 0; i < argc; i++)
-		{
-			if (i == 0 || end_of_area + 1 == argv[i])
-				end_of_area = argv[i] + strlen(argv[i]);
+		if (i == 0 || end_of_area + 1 == argv[i])
+			end_of_area = argv[i] + strlen(argv[i]);
 
-			strcat(argv_full, argv[i]);
-			strcat(argv_full, " ");
-		}
-
-		argv_full[strlen(argv_full)-1] = '\0';
-
-		if (end_of_area == NULL)
-		{
-			ps_buffer = NULL;
-			ps_buffer_size = 0;
-			return;
-		}
-
-		/* check for contiguous environ strings following argv */
-		for (i = 0; environ[i] != NULL; i++)
-		{
-			if (end_of_area + 1 == environ[i])
-				end_of_area = environ[i] + strlen(environ[i]);
-		}
-
-		ps_buffer = argv[0];
-		ps_buffer_size = end_of_area - argv[0];
-
-		/* move the environment out of the way */
-		new_environ = (char **) zbx_malloc(new_environ, (i + 1) * sizeof(char *));
-		for (i = 0; environ[i] != NULL; i++)
-			new_environ[i] = strdup(environ[i]);
-		new_environ[i] = NULL;
-		environ = new_environ;
+		strcat(argv_full, argv[i]);
+		strcat(argv_full, " ");
 	}
-#endif   /* PS_USE_CLOBBER_ARGV */
 
-#if defined(PS_USE_CHANGE_ARGV) || defined(PS_USE_CLOBBER_ARGV)
+	argv_full[strlen(argv_full)-1] = '\0';
 
-/*
-* If we're going to change the original argv[] then make a copy for
-* argument parsing purposes.
-*
-* (NB: do NOT think to remove the copying of argv[], even though
-* postmaster.c finishes looking at argv[] long before we ever consider
-* changing the ps display.  On some platforms, getopt() keeps pointers
-* into the argv array, and will get horribly confused when it is
-* re-called to analyze a subprocess' argument string if the argv storage
-* has been clobbered meanwhile.  Other platforms have other dependencies
-* on argv[].
-*/
+	if (end_of_area == NULL)
 	{
-		new_argv = (char **) zbx_malloc(new_argv, (argc + 1) * sizeof(char *));
-		for (i = 0; i < argc; i++)
-			new_argv[i] = zbx_strdup(new_argv[i], argv[i]);
-		new_argv[argc] = NULL;
+		ps_buffer = NULL;
+		ps_buffer_size = 0;
+		return;
+	}
+
+	/* check for contiguous environ strings following argv */
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (end_of_area + 1 == environ[i])
+			end_of_area = environ[i] + strlen(environ[i]);
+	}
+
+	ps_buffer = argv[0];
+	ps_buffer_size = end_of_area - argv[0];
+
+	/* move the environment out of the way */
+	new_environ = (char **) zbx_malloc(new_environ, (i + 1) * sizeof(char *));
+	for (i = 0; environ[i] != NULL; i++)
+		new_environ[i] = zbx_strdup(NULL, environ[i]);
+	new_environ[i] = NULL;
+	environ = new_environ;
+
+/* Before changing the original argv[] make a copy for argument parsing purposes. */
+/* Some platforms have various dependencies on argv[]. */
+
+	new_argv = (char **) zbx_malloc(new_argv, (argc + 1) * sizeof(char *));
+	for (i = 0; i < argc; i++)
+		new_argv[i] = zbx_strdup(NULL, argv[i]);
+	new_argv[argc] = NULL;
 
 #if defined(MAC_OS_X)
-		/* Darwin has a static copy of the argv pointer */
-		*_NSGetArgv() = new_argv;
+	/* Darwin has a static copy of the argv pointer */
+	*_NSGetArgv() = new_argv;
 #endif
-		argv = new_argv;
-	}
-#endif   /* PS_USE_CHANGE_ARGV or PS_USE_CLOBBER_ARGV */
+	argv = new_argv;
+#endif /* PS_USE_CLOBBER_ARGV */
 
 	return;
 }
@@ -173,13 +145,9 @@ void save_ps_display_args(int argc, char **argv)
 void set_ps_display(const char *activity)
 {
 #ifndef PS_USE_NONE
-
 #ifdef PS_USE_CLOBBER_ARGV
 	int	buflen;
-#endif   /* PS_USE_CLOBBER_ARGV */
 
-#ifdef PS_USE_CLOBBER_ARGV
-	/* If ps_buffer is a pointer, it might still be null */
 	if (!ps_buffer)
 		return;
 #endif
@@ -193,78 +161,58 @@ void set_ps_display(const char *activity)
 #ifdef PS_USE_SETPROCTITLE
 	zbx_strlcpy(ps_buffer + strlen(prog_prefix), activity, ps_buffer_size - strlen(prog_prefix));
 #elif defined(sun)
-/* SUN requires new ps name to be longer than the initial one, */
-/* therefore activity is added to the initial ps name*/
+	/* SUN requires new ps name to be longer than the initial one, */
+	/* therefore activity is added to the initial ps name*/
 	zbx_snprintf(ps_buffer, ps_buffer_size, "%s: %s", argv_full, activity);
 #else
 	zbx_snprintf(ps_buffer, ps_buffer_size, "%s: %s", prog_prefix, activity);
 #endif
 
-	/* Transmit new setting to kernel, if necessary */
 #ifdef PS_USE_SETPROCTITLE
 	setproctitle("%s", ps_buffer);
 #endif
 
 #ifdef PS_USE_PSTAT
-	{
 	union	pstun pst;
 
 	pst.pst_command = ps_buffer;
 	pstat(PSTAT_SETCMD, pst, strlen(ps_buffer), 0, 0);
-	}
-#endif   /* PS_USE_PSTAT */
-
-#ifdef PS_USE_PS_STRINGS
-	PS_STRINGS->ps_nargvstr = 1;
-	PS_STRINGS->ps_argvstr = ps_buffer;
-#endif   /* PS_USE_PS_STRINGS */
+#endif /* PS_USE_PSTAT */
 
 #ifdef PS_USE_CLOBBER_ARGV
-	{
 	/* pad unused memory */
 	buflen = strlen(ps_buffer);
 	memset(ps_buffer + buflen, PS_PADDING, ps_buffer_size - buflen);
-	}
-#endif   /* PS_USE_CLOBBER_ARGV */
+#endif
 
-#endif   /* not PS_USE_NONE */
+#endif /* not PS_USE_NONE */
 }
 
 /* Call this once during subprocess startup to set the identification values */
 void init_ps_display(void)
 {
 #ifndef PS_USE_NONE
-
 #if defined(PS_USE_CLOBBER_ARGV)
 	int	i;
-	/* If ps_buffer is a pointer, it might still be null */
+
 	if (!ps_buffer)
 		return;
 #endif
-	/* no ps display if you didn't call save_ps_display_args() */
+
 	if (!save_argv)
 		return;
 
-	/* Overwrite argv[] to point at appropriate space, if needed */
-#if defined(PS_USE_CHANGE_ARGV)
-	save_argv[0] = ps_buffer;
-	save_argv[1] = NULL;
-#endif   /* PS_USE_CHANGE_ARGV */
-
 #if defined(PS_USE_CLOBBER_ARGV)
-	{
 	/* make extra argv slots point at end_of_area (a NUL) */
 	for (i = 1; i < save_argc; i++)
 		save_argv[i] = ps_buffer + ps_buffer_size;
-	}
-#endif   /* PS_USE_CLOBBER_ARGV */
+#endif
 
-	/* Make fixed prefix of ps display. */
 #ifdef PS_USE_SETPROCTITLE /* setproctitle() already adds a 'progname:' prefix to the ps line */
 	zbx_snprintf(ps_buffer, ps_buffer_size, " ");
 #else
 	zbx_strlcat(prog_prefix, progname, ps_buffer_size);
-#endif /* PS_USE_SETPROCTITLE */
+#endif
 
-#endif   /* not PS_USE_NONE */
+#endif /* not PS_USE_NONE */
 }
