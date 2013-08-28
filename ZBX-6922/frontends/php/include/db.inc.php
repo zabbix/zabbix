e0c413c6da9b7443bc1f35cf5ab05f98cc41533a
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-08-28 06:44:49 +0000

    ..F....... [ZBX-6922] optimized dbConditionInt()
    git-svn-id: svn://svn.zabbix.com/branches/dev@38164 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6922/frontends/php/include/db.inc.php
+++ ZBX-6922/frontends/php/include/db.inc.php
@@ -1084,9 +1084,9 @@ function dbConditionInt($fieldName, array $values, $notIn = false, $sort = true)
 
 	if ($sort) {
 		natsort($values);
-	}
 
-	$values = array_values($values);
+		$values = array_values($values);
+	}
 
 	$betweens = array();
 	$data = array();
