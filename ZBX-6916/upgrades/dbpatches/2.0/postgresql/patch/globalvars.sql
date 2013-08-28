d8e55cf92498a70c3a4330989e2d517cb6a18c67
igors <igors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-08-28 11:21:44 +0000

    .......... [ZBX-6916] removed merged development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@38184 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6916/upgrades/dbpatches/2.0/postgresql/patch/globalvars.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE globalvars (
-	globalvarid              bigint                                    NOT NULL,
-	snmp_lastsize            integer         DEFAULT '0'               NOT NULL,
-	PRIMARY KEY (globalvarid)
-);
