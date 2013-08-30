cf6efa339a0be196f45a8c0f9e4334ea272c8805
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2013-08-30 15:57:01 +0000

    ....I..... [ZBX-6774] fixed default value for "hk_trends_mode" field

--- src/libs/zbxdbupgrade/dbupgrade.c
+++ src/libs/zbxdbupgrade/dbupgrade.c
@@ -1212,7 +1212,7 @@ static int	DBpatch_2010064(void)
 
 static int	DBpatch_2010065(void)
 {
-	const ZBX_FIELD field = {"hk_trends_mode", "1 ", NULL, NULL, 0, ZBX_TYPE_INT, ZBX_NOTNULL, 0};
+	const ZBX_FIELD field = {"hk_trends_mode", "1", NULL, NULL, 0, ZBX_TYPE_INT, ZBX_NOTNULL, 0};
 
 	return DBadd_field("config", &field);
 }
