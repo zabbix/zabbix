7a5b3ade4f79c2e79e77d9d8a385f55f1275f451
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-09-11 06:28:55 +0000

    .......... [ZBX-2166] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@38400 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-2166/upgrades/dbpatches/2.0/mysql/patch/hostmacro.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE hostmacro MODIFY hostmacroid bigint unsigned NOT NULL,
-		      MODIFY hostid bigint unsigned NOT NULL;
-DROP INDEX hostmacro_1 ON hostmacro;
-DELETE FROM hostmacro WHERE NOT hostid IN (SELECT hostid FROM hosts);
-CREATE UNIQUE INDEX hostmacro_1 ON hostmacro (hostid,macro);
-ALTER TABLE hostmacro ADD CONSTRAINT c_hostmacro_1 FOREIGN KEY (hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
--- ZBX-2166/upgrades/dbpatches/2.0/oracle/patch/hosts_groups.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE hosts_groups MODIFY hostgroupid DEFAULT NULL;
-ALTER TABLE hosts_groups MODIFY hostid DEFAULT NULL;
-ALTER TABLE hosts_groups MODIFY groupid DEFAULT NULL;
-DROP INDEX hosts_groups_1;
-DELETE FROM hosts_groups WHERE NOT hostid IN (SELECT hostid FROM hosts);
-DELETE FROM hosts_groups WHERE NOT groupid IN (SELECT groupid FROM groups);
-CREATE UNIQUE INDEX hosts_groups_1 ON hosts_groups (hostid,groupid);
-ALTER TABLE hosts_groups ADD CONSTRAINT c_hosts_groups_1 FOREIGN KEY (hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
-ALTER TABLE hosts_groups ADD CONSTRAINT c_hosts_groups_2 FOREIGN KEY (groupid) REFERENCES groups (groupid) ON DELETE CASCADE;
