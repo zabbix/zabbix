586cf554d9c37cf1282b0f729a499536fbc319c8
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-09-17 09:57:38 +0000

    A.F....... [ZBX-6952] fixed XSS vulnerability in Administration > General and user profile page
    git-svn-id: svn://svn.zabbix.com/branches/dev@38553 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6952-trunk/frontends/php/profile.php
+++ ZBX-6952-trunk/frontends/php/profile.php
@@ -37,12 +37,15 @@ if (CWebUser::$data['alias'] == ZBX_GUEST_USER) {
 	access_deny();
 }
 
+$themes = array_keys(Z::getThemes());
+array_push($themes, THEME_DEFAULT);
+
 //	VAR			TYPE	OPTIONAL FLAGS	VALIDATION	EXCEPTION
 $fields = array(
 	'password1' =>			array(T_ZBX_STR, O_OPT, null, null, 'isset({save})&&isset({form})&&({form}!="update")&&isset({change_password})'),
 	'password2' =>			array(T_ZBX_STR, O_OPT, null, null, 'isset({save})&&isset({form})&&({form}!="update")&&isset({change_password})'),
 	'lang' =>				array(T_ZBX_STR, O_OPT, null, NOT_EMPTY, 'isset({save})'),
-	'theme' =>				array(T_ZBX_STR, O_OPT, null, NOT_EMPTY, 'isset({save})'),
+	'theme' =>				array(T_ZBX_STR, O_OPT, null, IN('"'.implode('","', $themes).'"'), 'isset({save})'),
 	'autologin' =>			array(T_ZBX_INT, O_OPT, null, IN('1'), null),
 	'autologout' =>	array(T_ZBX_INT, O_OPT, null, BETWEEN(90, 10000), null, _('Auto-logout (min 90 seconds)')),
 	'url' =>				array(T_ZBX_STR, O_OPT, null, null, 'isset({save})'),
