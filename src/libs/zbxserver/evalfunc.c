e2d5fd11ecd39f89d0e19dc94d054913aa9bcacb
Andris Zeila <andris.zeila@zabbix.com>
2013-09-18 13:46:17 +0000

    ........S. [ZBX-4476] take in account the time item was added to configuration cache when calculating nodata period
    This will prevent new items immediatelly triggering nodata() check.

--- src/libs/zbxserver/evalfunc.c
+++ src/libs/zbxserver/evalfunc.c
@@ -1122,12 +1122,9 @@ out:
 static int	evaluate_NODATA(char *value, DB_ITEM *item, const char *function, const char *parameters)
 {
 	const char			*__function_name = "evaluate_NODATA";
-	int				arg1, flag, now, ret = FAIL;
-	zbx_vector_history_record_t	values;
+	int				arg1, flag, seconds, ret = FAIL;
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
-
-	zbx_history_record_vector_create(&values);
 
 	if (1 < num_param(parameters))
 		goto out;
@@ -1138,25 +1135,13 @@ static int	evaluate_NODATA(char *value, DB_ITEM *item, const char *function, con
 	if (ZBX_FLAG_SEC != flag)
 		goto out;
 
-	now = (int)time(NULL);
-
-	if (SUCCEED != zbx_vc_get_value_range(item->itemid, item->value_type, &values, 0, 1, now) ||
-			1 > values.values_num)
+	if (SUCCEED != DCget_item_nodata_seconds(item->itemid, &seconds))
 		goto out;
 
-	if (values.values[0].timestamp.sec + arg1 > now)
-		zbx_strlcpy(value, "0", MAX_BUFFER_LEN);
-	else
-	{
-		if (CONFIG_SERVER_STARTUP_TIME + arg1 > now)
-			goto out;
-
-		zbx_strlcpy(value, "1", MAX_BUFFER_LEN);
-	}
+	zbx_strlcpy(value, seconds < arg1 ? "0" : "1", MAX_BUFFER_LEN);
 
 	ret = SUCCEED;
 out:
-	zbx_history_record_vector_destroy(&values, item->value_type);
 
 	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%s", __function_name, zbx_result_string(ret));
 
