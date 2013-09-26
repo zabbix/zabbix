fbc9324d4cff5ddbb991db00eb3172fb2a7a465f
egita <egita@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-09-26 09:28:44 +0000

    .......... [ZBXNEXT-686] updated to the latest trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@38814 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6808-test/frontends/php/tests/selenium/testFormAction.php
+++ ZBX-6808-test/frontends/php/tests/selenium/testFormAction.php
@@ -1971,6 +1971,117 @@ class testFormAction extends CWebTest {
 				}
 				break;
 			}
+		$this->zbxTestLogin('actionconf.php?form=1&eventsource=0');
+		$this->checkTitle('Configuration of actions');
+
+		$this->type("name", "action test");
+		$this->type("def_shortdata", "subject");
+		$this->type("def_longdata", "message");
+
+// adding conditions
+		$this->zbxTestClick('link=Conditions');
+		$this->type("new_condition_value", "trigger");
+		$this->zbxTestClickWait('add_condition');
+		$this->zbxTestTextPresent("Trigger name like trigger");
+
+		$this->select("new_condition_conditiontype", "label=Trigger severity");
+		$this->wait();
+		$this->select("new_condition_value", "label=Average");
+		$this->zbxTestClickWait('add_condition');
+		$this->zbxTestTextPresent("Trigger severity = Average");
+
+		$this->select("new_condition_conditiontype", "label=Application");
+		$this->wait();
+		$this->type("new_condition_value", "app");
+		$this->zbxTestClickWait('add_condition');
+		$this->zbxTestTextPresent("Application = app");
+
+// adding operations
+		$this->zbxTestClick('link=Operations');
+		$this->zbxTestClickWait('new_operation');
+		sleep(1);
+		$this->zbxTestLaunchPopup('addusrgrpbtn');
+		$this->zbxTestClick('usrgrps_7');
+		$this->zbxTestClick('usrgrps_11');
+		$this->zbxTestClick('select');
+		$this->selectWindow('null');
+		sleep(1);
+		$this->zbxTestLaunchPopup('adduserbtn');
+		$this->zbxTestClick("users_'1'");
+		$this->zbxTestClick('select');
+		$this->selectWindow('null');
+		$this->select("new_operation_opmessage_mediatypeid", "label=Jabber");
+		$this->zbxTestClickWait('add_operation');
+		$this->zbxTestTextPresent("Send message to users: Admin (Zabbix Administrator) via Jabber");
+		$this->zbxTestTextPresent("Send message to user groups: Enabled debug mode, Zabbix administrators via Jabber");
+		$this->zbxTestClickWait('new_operation');
+		$this->select("new_operation_operationtype", "label=Remote command");
+		$this->wait();
+// add target current host
+		$this->zbxTestClick('add');
+		$this->zbxTestClick("//input[@name='save']");
+
+// add target host Zabbix server
+		$this->zbxTestClick('add');
+		sleep(1);
+		$this->select("opCmdTarget", "label=Host");
+		$this->zbxTestTextPresent(array('Target list', 'Target', 'Action'));
+		sleep(1);
+		$this->assertElementPresent("//div[@id='opCmdTargetObject']/input");
+		$this->input_type("//div[@id='opCmdTargetObject']/input", 'Simple form test host');
+		sleep(1);
+		$this->zbxTestClick("//span[@class='matched']");
+		$this->zbxTestClick("//input[@name='save']");
+
+		sleep(1);
+// add target group Zabbix servers
+		$this->zbxTestClick('add');
+		sleep(1);
+		$this->select("opCmdTarget", "label=Host group");
+		$this->zbxTestTextPresent(array('Target list', 'Target', 'Action'));
+		sleep(1);
+		$this->assertElementPresent("//div[@id='opCmdTargetObject']/input");
+		$this->input_type("//div[@id='opCmdTargetObject']/input", 'Zabbix servers');
+		sleep(1);
+		$this->zbxTestClick("//span[@class='matched']");
+		$this->zbxTestClick("//input[@name='save']");
+
+		sleep(1);
+
+		$this->type("new_operation_opcommand_command", "command");
+		$this->zbxTestClickWait('add_operation');
+		$this->zbxTestTextPresent('Send message to users: Admin (Zabbix Administrator) via Jabber');
+		$this->zbxTestTextPresent("Send message to user groups: Enabled debug mode, Zabbix administrators via Jabber");
+		$this->zbxTestTextPresent("Run remote commands on current host");
+		$this->zbxTestTextPresent('Run remote commands on hosts: Simple form test host');
+		$this->zbxTestTextPresent('Run remote commands on host groups: Zabbix servers');
+
+		$this->zbxTestClickWait('new_operation');
+		$this->type("new_operation_esc_step_to", "2");
+		$this->select("new_operation_operationtype", "label=Remote command");
+		$this->wait();
+		$this->zbxTestClick('add');
+		$this->zbxTestClick("//input[@name='save']");
+		$this->select("new_operation_opcommand_type", "label=SSH");
+		$this->type("new_operation_opcommand_username", "user");
+		$this->type("new_operation_opcommand_password", "pass");
+		$this->type("new_operation_opcommand_port", "123");
+		$this->type("new_operation_opcommand_command", "command ssh");
+		$this->zbxTestClickWait('add_operation');
+
+		$this->zbxTestTextPresent('Send message to users: Admin (Zabbix Administrator) via Jabber');
+		$this->zbxTestTextPresent("Send message to user groups: Enabled debug mode, Zabbix administrators via Jabber");
+		$this->zbxTestTextPresent("Run remote commands on current host");
+		$this->zbxTestTextPresent('Run remote commands on hosts: Simple form test host');
+		$this->zbxTestTextPresent('Run remote commands on host groups: Zabbix servers');
+
+		$this->type("esc_period", "123");
+		sleep(1);
+		$this->type('new_condition_value', '');
+		sleep(1);
+
+		$this->zbxTestClickWait('save');
+		$this->zbxTestTextPresent('Action added');
 	}
 
 	public function testFormAction_Teardown() {
