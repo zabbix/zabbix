22d492b4ef745df8231d5e6c66cbbe1bb78db89b
Vitalij Romanovskij <git-no-reply@zabbix.com>
2013-09-30 08:06:47 +0000

    .......... [ZBX-6911] updated to the latest trunk

--- frontends/php/tests/selenium/testPageActions.php
+++ frontends/php/tests/selenium/testPageActions.php
@@ -20,7 +20,8 @@
 
 require_once dirname(__FILE__).'/../include/class.cwebtest.php';
 
-class testPageActionsTriggers extends CWebTest {
+class testPageActions extends CWebTest {
+
 	private $sqlHashAction = '';
 	private $oldHashAction = '';
 	private $sqlHashConditions = '';
@@ -136,38 +137,71 @@ class testPageActionsTriggers extends CWebTest {
 				'Chuck Norris: Action update changed data in table "opconditions".');
 	}
 
-	// Returns all trigger actions
+	public static function allEventSources() {
+		return array(
+			array(EVENT_SOURCE_TRIGGERS),
+			array(EVENT_SOURCE_DISCOVERY),
+			array(EVENT_SOURCE_AUTO_REGISTRATION),
+			array(EVENT_SOURCE_INTERNAL)
+		);
+	}
+
 	public static function allActions() {
-		return DBdata('SELECT * FROM actions WHERE eventsource='.EVENT_SOURCE_TRIGGERS.' ORDER BY actionid');
+		return DBdata(
+			'SELECT actionid,eventsource,name,status'.
+			' FROM actions'.
+			' ORDER BY actionid'
+		);
 	}
 
 	/**
-	* @dataProvider allActions
+	* @dataProvider allEventSources
 	*/
-	public function testPageActionsTriggers_CheckLayout($action) {
-		$this->zbxTestLogin('actionconf.php?eventsource='.EVENT_SOURCE_TRIGGERS);
+	public function testPageActions_CheckLayout($eventsource) {
+		$this->zbxTestLogin('actionconf.php?eventsource='.$eventsource);
 		$this->checkTitle('Configuration of actions');
 
-// eventsource is used for a hidden field, so it does not work. See above: ?eventsource=0 is used instead
-//		$this->zbxTestDropdownSelect('eventsource', 'Triggers');
-
+		$this->zbxTestTextPresent('CONFIGURATION OF ACTIONS');
 		$this->zbxTestTextPresent('Event source');
+		$this->zbxTestTextPresent('Actions');
 		$this->zbxTestTextPresent('Displaying');
-		// Header
+
+		$eventsources = array(
+			EVENT_SOURCE_TRIGGERS => 'Triggers',
+			EVENT_SOURCE_DISCOVERY => 'Discovery',
+			EVENT_SOURCE_AUTO_REGISTRATION => 'Auto registration',
+			EVENT_SOURCE_INTERNAL => 'Internal'
+		);
+
+		$this->zbxTestDrowpdownAssertSelected('eventsource', $eventsources[$eventsource]);
+		$this->zbxTestDropdownHasOptions('eventsource', $eventsources);
+
 		$this->zbxTestTextPresent(array('Name', 'Conditions', 'Operations', 'Status'));
-		// Data
-		$this->zbxTestTextPresent(array($action['name']));
+
+		$dbResult = DBselect(
+			'SELECT name,status'.
+			' FROM actions'.
+			' WHERE eventsource='.$eventsource.
+			' ORDER BY actionid'
+		);
+
+		while ($dbRow = DBfetch($dbResult)) {
+			$statusStr = ($dbRow['status'] == ACTION_STATUS_ENABLED ? 'Enabled' : 'Disabled');
+
+			$this->zbxTestTextPresent(array($dbRow['name'], $statusStr));
+		}
+
 		$this->zbxTestDropdownHasOptions('go', array('Enable selected', 'Disable selected', 'Delete selected'));
+		$this->assertElementValue('goButton', 'Go (0)');
 	}
 
 	/**
 	* @dataProvider allActions
 	*/
-	public function testPageActionsTriggers_SimpleUpdate($action) {
+	public function testPageActions_SimpleUpdate($action) {
 		$this->calculateHash($action['actionid']);
 
-		$this->zbxTestLogin('actionconf.php?eventsource='.EVENT_SOURCE_TRIGGERS);
-		$this->checkTitle('Configuration of actions');
+		$this->zbxTestLogin('actionconf.php?eventsource='.$action['eventsource']);
 		$this->zbxTestClickWait('link='.$action['name']);
 		$this->zbxTestClickWait('save');
 		$this->checkTitle('Configuration of actions');
@@ -180,56 +214,52 @@ class testPageActionsTriggers extends CWebTest {
 	/**
 	* @dataProvider allActions
 	*/
-	public function testPageActionsTriggers_SingleEnableDisable($action) {
-		$actionid = $action['actionid'];
+	public function testPageActions_SingleEnableDisable($action) {
+		$this->sqlHashAction = 'SELECT * FROM actions WHERE actionid<>'.$action['actionid'].' ORDER BY actionid';
+		$this->oldHashAction = DBhash($this->sqlHashAction);
 
-		$this->zbxTestLogin('actionconf.php?eventsource='.EVENT_SOURCE_TRIGGERS);
+		$this->zbxTestLogin('actionconf.php?eventsource='.$action['eventsource']);
 		$this->checkTitle('Configuration of actions');
+
 		switch ($action['status']) {
 			case ACTION_STATUS_ENABLED:
-				$this->href_click("actionconf.php?go=disable&g_actionid%5B%5D=$actionid&");
+				$this->zbxTestHrefClickWait('actionconf.php?go=disable&g_actionid%5B%5D='.$action['actionid'].'&');
+				$newStatus = ACTION_STATUS_DISABLED;
 				break;
 			case ACTION_STATUS_DISABLED:
-				$this->href_click("actionconf.php?go=activate&g_actionid%5B%5D=$actionid&");
+				$this->zbxTestHrefClickWait('actionconf.php?go=activate&g_actionid%5B%5D='.$action['actionid'].'&');
+				$newStatus = ACTION_STATUS_ENABLED;
 				break;
+			default:
+				$this->assertTrue(false);
 		}
-		$this->wait();
 
 		$this->checkTitle('Configuration of actions');
 		$this->zbxTestTextPresent('Status updated');
 
-		switch ($action['status']) {
-			case ACTION_STATUS_ENABLED:
-				$sql = "SELECT * FROM actions WHERE actionid=$actionid AND status=".ACTION_STATUS_DISABLED;
-				break;
-			case ACTION_STATUS_DISABLED:
-				$sql = "SELECT * FROM actions WHERE actionid=$actionid AND status=".ACTION_STATUS_ENABLED;
-				break;
-		}
-		$this->assertEquals(1, DBcount($sql));
-	}
+		$this->assertEquals(1, DBcount(
+			'SELECT NULL'.
+			' FROM actions'.
+			' WHERE actionid='.$action['actionid'].
+				' AND status='.$newStatus
+		));
 
-	public function testPageActionsTriggers_Create() {
-// TODO
-		$this->markTestIncomplete();
-	}
-
-	public function testPageActionsTriggers_MassDisableAll() {
-// TODO
-		$this->markTestIncomplete();
+		$this->assertEquals($this->oldHashAction, DBhash($this->sqlHashAction));
 	}
 
 	/**
 	* @dataProvider allActions
 	*/
-	public function testPageActionsTriggers_MassDisable($action) {
-		$actionid = $action['actionid'];
+	public function testPageActions_MassDisable($action) {
+		$this->sqlHashAction = 'SELECT * FROM actions WHERE actionid<>'.$action['actionid'].' ORDER BY actionid';
+		$this->oldHashAction = DBhash($this->sqlHashAction);
 
 		$this->chooseOkOnNextConfirmation();
 
-		$this->zbxTestLogin('actionconf.php?eventsource='.EVENT_SOURCE_TRIGGERS);
+		$this->zbxTestLogin('actionconf.php?eventsource='.$action['eventsource']);
 		$this->checkTitle('Configuration of actions');
-		$this->zbxTestCheckboxSelect('g_actionid['.$actionid.']');
+
+		$this->zbxTestCheckboxSelect('g_actionid['.$action['actionid'].']');
 		$this->zbxTestDropdownSelect('go', 'Disable selected');
 		$this->zbxTestClickWait('goButton');
 
@@ -239,26 +269,29 @@ class testPageActionsTriggers extends CWebTest {
 		$this->zbxTestTextPresent('Status updated');
 		$this->zbxTestTextPresent('Disabled');
 
-		$sql = "SELECT * FROM actions WHERE actionid=$actionid AND status=1";
-		$this->assertEquals(1, DBcount($sql));
-	}
+		$this->assertEquals(1, DBcount(
+			'SELECT NULL'.
+			' FROM actions'.
+			' WHERE actionid='.$action['actionid'].
+				' AND status='.ACTION_STATUS_DISABLED
+		));
 
-	public function testPageActionsTriggers_MassEnableAll() {
-// TODO
-		$this->markTestIncomplete();
+		$this->assertEquals($this->oldHashAction, DBhash($this->sqlHashAction));
 	}
 
 	/**
 	* @dataProvider allActions
 	*/
-	public function testPageActionsTriggers_MassEnable($action) {
-		$actionid = $action['actionid'];
+	public function testPageActions_MassEnable($action) {
+		$this->sqlHashAction = 'SELECT * FROM actions WHERE actionid<>'.$action['actionid'].' ORDER BY actionid';
+		$this->oldHashAction = DBhash($this->sqlHashAction);
 
 		$this->chooseOkOnNextConfirmation();
 
-		$this->zbxTestLogin('actionconf.php?eventsource='.EVENT_SOURCE_TRIGGERS);
+		$this->zbxTestLogin('actionconf.php?eventsource='.$action['eventsource']);
 		$this->checkTitle('Configuration of actions');
-		$this->zbxTestCheckboxSelect('g_actionid['.$actionid.']');
+
+		$this->zbxTestCheckboxSelect('g_actionid['.$action['actionid'].']');
 		$this->zbxTestDropdownSelect('go', 'Enable selected');
 		$this->zbxTestClickWait('goButton');
 
@@ -268,28 +301,33 @@ class testPageActionsTriggers extends CWebTest {
 		$this->zbxTestTextPresent('Status updated');
 		$this->zbxTestTextPresent('Enabled');
 
-		$sql = "SELECT * FROM actions WHERE actionid=$actionid AND status=0";
-		$this->assertEquals(1, DBcount($sql));
+		$this->assertEquals(1, DBcount(
+			'SELECT NULL'.
+			' FROM actions'.
+			' WHERE actionid='.$action['actionid'].
+				' AND status='.ACTION_STATUS_ENABLED
+		));
+
+		$this->assertEquals($this->oldHashAction, DBhash($this->sqlHashAction));
 	}
 
-	public function testPageActionsTriggers_MassDeleteAll() {
-// TODO
-		$this->markTestIncomplete();
+	public function testPageActions_Backup() {
+		DBsave_tables('actions');
 	}
 
 	/**
 	* @dataProvider allActions
 	*/
-	public function testPageActionsTriggers_MassDelete($action) {
-		$actionid = $action['actionid'];
+	public function testPageActions_MassDelete($action) {
+		$this->sqlHashAction = 'SELECT * FROM actions WHERE actionid<>'.$action['actionid'].' ORDER BY actionid';
+		$this->oldHashAction = DBhash($this->sqlHashAction);
 
 		$this->chooseOkOnNextConfirmation();
 
-		DBsave_tables('actions');
-
-		$this->zbxTestLogin('actionconf.php?eventsource='.EVENT_SOURCE_TRIGGERS);
+		$this->zbxTestLogin('actionconf.php?eventsource='.$action['eventsource']);
 		$this->checkTitle('Configuration of actions');
-		$this->zbxTestCheckboxSelect('g_actionid['.$actionid.']');
+
+		$this->zbxTestCheckboxSelect('g_actionid['.$action['actionid'].']');
 		$this->zbxTestDropdownSelect('go', 'Delete selected');
 		$this->zbxTestClickWait('goButton');
 
@@ -298,18 +336,13 @@ class testPageActionsTriggers extends CWebTest {
 		$this->checkTitle('Configuration of actions');
 		$this->zbxTestTextPresent('Selected actions deleted');
 
-		$sql = "SELECT * FROM actions WHERE actionid=$actionid";
-		$this->assertEquals(0, DBcount($sql));
-		$sql = "SELECT * FROM operations WHERE actionid=$actionid";
-		$this->assertEquals(0, DBcount($sql));
-		$sql = "SELECT * FROM conditions WHERE actionid=$actionid";
-		$this->assertEquals(0, DBcount($sql));
+		$this->assertEquals(0, DBcount('SELECT * FROM actions WHERE actionid='.$action['actionid']));
 
+		$this->assertEquals($this->oldHashAction, DBhash($this->sqlHashAction));
+	}
+
+	public function testPageActions_Restore() {
 		DBrestore_tables('actions');
 	}
 
-	public function testPageActionsTriggers_Sorting() {
-// TODO
-		$this->markTestIncomplete();
-	}
 }
