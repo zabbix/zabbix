291471c2ec0978c8eb603997efa6f15fc13989bd
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-10-04 09:19:19 +0000

    .......... [ZBX-7036] removed merged branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@38999 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7036/2.0/upgrades/dbpatches/1.6/mysql/patch/trends_uint.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE trends_uint (
-        itemid          bigint unsigned         DEFAULT '0'     NOT NULL,
-        clock           integer         DEFAULT '0'     NOT NULL,
-        num             integer         DEFAULT '0'     NOT NULL,
-        value_min               bigint unsigned         DEFAULT '0'     NOT NULL,
-        value_avg               bigint unsigned         DEFAULT '0'     NOT NULL,
-        value_max               bigint unsigned         DEFAULT '0'     NOT NULL,
-        PRIMARY KEY (itemid,clock)
-) ENGINE=InnoDB;
