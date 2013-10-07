1b6446bfe3f7a26af65278a5921ed1323e7c1d3c
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-10-07 06:43:54 +0000

    .......... [ZBX-7058] Created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@39032 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-7058/upgrades/dbpatches/1.8/postgresql/patch/dhosts.sql
@@ -0,0 +1,8 @@
+alter table dservices add dcheckid                bigint          DEFAULT '0'     NOT NULL;
+alter table dservices add ip              varchar(39)             DEFAULT ''      NOT NULL;
+
+update dservices set ip=(select dhosts.ip from dhosts where dservices.dhostid=dhosts.dhostid);
+
+alter table dhosts drop ip;
+
+CREATE INDEX dhosts_1 on dhosts (druleid);
