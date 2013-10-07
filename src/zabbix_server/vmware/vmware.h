15b856e63c458c7e3c77321a675087e0fb882aed
Andris Zeila <andris.zeila@zabbix.com>
2013-10-07 13:15:05 +0000

    .......PS. [ZBXNEXT-1633] create service data object on heap and copy it during vmware lock

--- src/zabbix_server/vmware/vmware.h
+++ src/zabbix_server/vmware/vmware.h
@@ -16,8 +16,8 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
-#ifndef VMWARE_H_
-#define VMWARE_H_
+#ifndef ZABBIX_VMWARE_H
+#define ZABBIX_VMWARE_H
 
 #include "common.h"
 
