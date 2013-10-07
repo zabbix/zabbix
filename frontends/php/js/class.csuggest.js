cfd126b276a6e71832adae0d71501d1c727ccd3c
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-10-07 08:06:54 +0000

    ..F....... [ZBX-7058] fixed repeated global search with same value
    git-svn-id: svn://svn.zabbix.com/trunk@39036 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/js/class.csuggest.js
+++ frontends/php/js/class.csuggest.js
@@ -79,6 +79,7 @@ initialize: function($super, id, objid){
 //	addListener(window, 'keypress', this.searchFocus.bindAsEventListener(this));
 
 	this.timeoutNeedle = null;
+	this.userNeedle = this.dom.input.value;
 },
 
 needleChange: function(e){
