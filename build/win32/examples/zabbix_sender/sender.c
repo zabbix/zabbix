f6fff71ae8ae7568d8ec347715af2b328f5d4adc
Aleksandrs Saveljevs <git-no-reply@zabbix.com>
2013-10-09 08:49:06 +0000

    ....I..... [ZBXNEXT-1942] fixed compilation warnings in sender.c, made the output look more like that of zabbix_sender

--- build/win32/examples/zabbix_sender/sender.c
+++ build/win32/examples/zabbix_sender/sender.c
@@ -33,7 +33,7 @@
  * environment with the following command: nmake /f Makefile
  */
 
-int main(int argc, const char *argv[])
+int main(int argc, char *argv[])
 {
 	if (5 == argc)
 	{
@@ -45,23 +45,21 @@ int main(int argc, const char *argv[])
 		/* send one value to the argv[1] IP address and the default trapper port 10051 */
 		if (-1 == zabbix_sender_send_values(argv[1], 10051, NULL, &value, 1, &result))
 		{
-			printf("Sending failed: %s\n", result);
+			printf("sending failed: %s\n", result);
 		}
 		else
 		{
-			printf("Sending succeeded:\n");
+			printf("sending succeeded:\n");
 
 			/* parse the server response */
 			if (0 == zabbix_sender_parse_result(result, &response, &info))
 			{
-				printf("\tResult: %s\n", 0 == response ? "OK" : "failed");
-				printf("\tTotal: %d; failed: %d; time spent: %lf\n",
-						info.total, info.failed, info.time_spent);
+				printf("  response: %s\n", 0 == response ? "success" : "failed");
+				printf("  info from server: \"processed: %d; total: %d; failed: %d; time spent: %lf\"\n",
+						info.total - info.failed, info.total, info.failed, info.time_spent);
 			}
 			else
-			{
-				printf("\tFailed to parse server response\n");
-			}
+				printf("  failed to parse server response\n");
 		}
 
 		/* free the server response */
@@ -69,12 +67,13 @@ int main(int argc, const char *argv[])
 	}
 	else
 	{
-		printf("Simple zabbix_sender implementation with zabbix_sender library\n");
-		printf("\tUsage: %s <address> <host> <key> <value>\n", argv[0]);
-		printf("\t\t<address> - Zabbix server IP address\n");
-		printf("\t\t<host> - host name\n");
-		printf("\t\t<key> - item key\n");
-		printf("\t\t<value> - item value\n");
+		printf("Simple zabbix_sender implementation with zabbix_sender library\n\n");
+		printf("usage: %s <server> <hostname> <key> <value>\n\n", argv[0]);
+		printf("Options:\n");
+		printf("  <server>    Hostname or IP address of Zabbix server\n");
+		printf("  <hostname>  Host name\n");
+		printf("  <key>       Item key\n");
+		printf("  <value>     Item value\n");
 	}
 
 	return EXIT_SUCCESS;
