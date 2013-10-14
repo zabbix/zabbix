23b6cc37cc979e9eeec5fd5db4d251e415f39be1
Chris Christensen <cchristensen@llnw.com>
2013-10-14 13:03:01 -0500

    [fix #MON-2344] Update performance link and layout for LLNW specific general search page.

--- frontends/php/include/views/general.searchpage.php
+++ frontends/php/include/views/general.searchpage.php
@@ -3,13 +3,15 @@
 ** Zabbix
 ** copy of include/views/general.searchpage.php
 **/
+
+global $LLNW_PERFORMANCE_LINK_ID;
 ?>
-<div style="display: table; position: absolute; height: 99%; width: 99%;">
+<div style="display: table; height: 99%; width: 99%;">
 <div class="vertical-middle">
 <div class="loginForm">
 	<div style="position: relative; color: #F0F0F0; height: 100%;">
 		<!-- Search Form -->
-		<div style="display: table; height: 99%; width: 99%;">
+		<div style="height: 100%; padding-top: 100px; padding-right: 40px; margin-left: 275px;">
 			<div style="float: right;">
 				<?php
 					$searchForm = new CView('general.search');
@@ -17,7 +19,9 @@
 				?>
 				<div style="height: 45px;"></div>
 				<span style="margin-left: 5px;">
-					<a class="highlight underline" href="screens.php?form_refresh=1&fullscreen=0&elementid=100100000000004"><?php echo _('Performance'); ?></a>
+				<?php if (!empty($LLNW_PERFORMANCE_LINK_ID)): ?>
+					<a class="highlight underline" href="screens.php?form_refresh=1&fullscreen=0&elementid=<?php echo $LLNW_PERFORMANCE_LINK_ID; ?>"><?php echo _('Performance'); ?></a>
+				<?php endif;?>
 				</span>
 			</div>
 		</div>
