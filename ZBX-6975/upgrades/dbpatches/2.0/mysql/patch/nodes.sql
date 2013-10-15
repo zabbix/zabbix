e93b41a0fd4ca22f650cc3815f5188aaf4ab66b5
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-10-15 07:21:54 +0000

    .......... [ZBX-6975] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@39282 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6975/upgrades/dbpatches/2.0/mysql/patch/nodes.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE nodes
-	MODIFY nodeid integer NOT NULL,
-	MODIFY masterid integer NULL,
-	DROP COLUMN timezone,
-	DROP COLUMN slave_history,
-	DROP COLUMN slave_trends;
-UPDATE nodes SET masterid=NULL WHERE masterid=0;
-ALTER TABLE nodes ADD CONSTRAINT c_nodes_1 FOREIGN KEY (masterid) REFERENCES nodes (nodeid);
