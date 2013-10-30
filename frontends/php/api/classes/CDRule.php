ccba5672bc01e59b5420cb4c4ed7dffa02ce3a01
Pavels Jelisejevs <git-no-reply@zabbix.com>
2013-10-30 16:09:14 +0000

    A......... [ZBX-7238] fixed permission checks when creating, updating and deleting discovery rules

--- frontends/php/api/classes/CDRule.php
+++ frontends/php/api/classes/CDRule.php
@@ -244,10 +244,8 @@ class CDRule extends CZBXAPI {
 			self::exception(ZBX_API_ERROR_PARAMETERS, _('Empty input.'));
 		}
 
-		if (self::$userData['type'] >= USER_TYPE_ZABBIX_ADMIN) {
-			if (!count(get_accessible_nodes_by_user(self::$userData, PERM_READ_WRITE, PERM_RES_IDS_ARRAY))) {
-				self::exception(ZBX_API_ERROR_PARAMETERS, _('No permissions to referred object or it does not exist!'));
-			}
+		if (CWebUser::getType() < USER_TYPE_ZABBIX_ADMIN) {
+			self::exception(ZBX_API_ERROR_PARAMETERS, _('No permissions to referred object or it does not exist!'));
 		}
 
 		$proxies = array();
@@ -567,10 +565,8 @@ class CDRule extends CZBXAPI {
 	public function delete(array $druleIds) {
 		$druleIds = zbx_toArray($druleIds);
 
-		if (self::$userData['type'] >= USER_TYPE_ZABBIX_ADMIN) {
-			if (!count(get_accessible_nodes_by_user(self::$userData, PERM_READ_WRITE, PERM_RES_IDS_ARRAY))) {
-				self::exception(ZBX_API_ERROR_PARAMETERS, _('No permissions to referred object or it does not exist!'));
-			}
+		if (CWebUser::getType() < USER_TYPE_ZABBIX_ADMIN) {
+			self::exception(ZBX_API_ERROR_PARAMETERS, _('No permissions to referred object or it does not exist!'));
 		}
 
 		$actionIds = array();
