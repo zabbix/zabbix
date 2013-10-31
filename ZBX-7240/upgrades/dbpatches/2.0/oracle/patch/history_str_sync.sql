4f1f5be2ed39ac06447ed777701affa6f3b1b111
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-10-31 17:21:20 +0000

    .......... [ZBX-7240] removed merged branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@39740 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7240/upgrades/dbpatches/2.0/oracle/patch/history_str_sync.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE history_str_sync MODIFY itemid DEFAULT NULL;
-ALTER TABLE history_str_sync MODIFY nodeid DEFAULT NULL;
-ALTER TABLE history_str_sync MODIFY nodeid number(10);
-ALTER TABLE history_str_sync ADD ns number(10) DEFAULT '0' NOT NULL;
