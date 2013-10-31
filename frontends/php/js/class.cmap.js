a35e5643584de795a860906f95a1a7b8522eec68
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-10-31 09:10:44 +0000

    .......... [ZBX-7247] reverted incorrectly commited changes
    git-svn-id: svn://svn.zabbix.com/trunk@39675 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/js/class.cmap.js
+++ frontends/php/js/class.cmap.js
@@ -498,8 +498,7 @@ ZABBIX.apps.map = (function($) {
 							$('#elementNameHost').multiSelectHelper({
 								objectName: 'hosts',
 								name: 'elementValue',
-								selectedLimit: 1,
-								objectOptions: { editable: true }
+								selectedLimit: 1
 							});
 							break;
 
@@ -508,8 +507,7 @@ ZABBIX.apps.map = (function($) {
 							$('#elementNameHostGroup').multiSelectHelper({
 								objectName: 'hostGroup',
 								name: 'elementValue',
-								selectedLimit: 1,
-								objectOptions: { editable: true }
+								selectedLimit: 1
 							});
 							break;
 
@@ -1335,8 +1333,7 @@ ZABBIX.apps.map = (function($) {
 							objectName: 'hosts',
 							name: 'elementValue',
 							data: [{id: selement.elementid, name: selement.elementName}],
-							selectedLimit: 1,
-							objectOptions: { editable: true }
+							selectedLimit: 1
 						});
 						break;
 
@@ -1346,8 +1343,7 @@ ZABBIX.apps.map = (function($) {
 							objectName: 'hostGroup',
 							name: 'elementValue',
 							data: [{id: selement.elementid, name: selement.elementName}],
-							selectedLimit: 1,
-							objectOptions: { editable: true }
+							selectedLimit: 1
 						});
 						break;
 				}
