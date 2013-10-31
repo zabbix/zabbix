e7452bf408fc80b675418b54fb338f3137e94790
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-10-31 07:15:20 +0000

    .......... [ZBX-7224] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@39673 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7224/upgrades/dbpatches/2.0/oracle/patch/service_alarms.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE service_alarms MODIFY servicealarmid DEFAULT NULL;
-ALTER TABLE service_alarms MODIFY serviceid DEFAULT NULL;
-DELETE FROM service_alarms WHERE NOT serviceid IN (SELECT serviceid FROM services);
-ALTER TABLE service_alarms ADD CONSTRAINT c_service_alarms_1 FOREIGN KEY (serviceid) REFERENCES services (serviceid) ON DELETE CASCADE;
