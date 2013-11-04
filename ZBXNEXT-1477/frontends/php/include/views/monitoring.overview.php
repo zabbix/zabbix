872fbf7e64879dc8a5f1fd7b25f38958245aaa90
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-11-04 16:08:57 +0000

    .......... [ZBXNEXT-1477] updated to the latest trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@39859 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1477/frontends/php/include/views/monitoring.overview.php
+++ ZBXNEXT-1477/frontends/php/include/views/monitoring.overview.php
@@ -100,9 +100,12 @@ if ($config['dropdown_first_entry'] || $this->data['pageFilter']->applicationsSe
 		);
 	}
 	elseif ($this->data['type'] == SHOW_TRIGGERS) {
+		global $page;
+
 		$dataTable = getTriggersOverview(
 			array_keys($this->data['pageFilter']->hosts),
 			$this->data['pageFilter']->application,
+			$page['file'],
 			$this->data['view_style']
 		);
 	}
