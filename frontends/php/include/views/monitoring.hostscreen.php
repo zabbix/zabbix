54298c0f86d61b8d0045b101c08fe16e9de0a7ba
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-11-12 12:51:46 +0000

    ..F....... [ZBX-7325] fixed parent LLD rule link color in the darkblue and darkorange themes
    git-svn-id: svn://svn.zabbix.com/branches/2.2@40215 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/views/monitoring.hostscreen.php
+++ frontends/php/include/views/monitoring.hostscreen.php
@@ -51,7 +51,7 @@ else {
 			$screenComboBox->addItem('host_screen.php?hostid='.$this->data['hostid'].'&screenid='.$screen['screenid'], $screen['name']);
 		}
 
-		$screenWidget->addHeader(array($this->data['screen']['name'], SPACE, _('on'), SPACE, new CSpan($this->data['host']['name'], 'gold')), $screenComboBox);
+		$screenWidget->addHeader(array($this->data['screen']['name'], SPACE, _('on'), SPACE, new CSpan($this->data['host']['name'], 'parent-discovery')), $screenComboBox);
 	}
 
 	// append screens to widget
