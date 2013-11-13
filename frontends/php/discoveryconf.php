018fa8576ef5b0a7e875fb543623191b43bbfa54
ivo <ivo@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2013-11-13 13:56:28 +0000

    A.F....... [ZBX-7316] fixed drule.delete method
    Before the fix:
    - drule.delete method in API was not working;
    - deleting unexisting discovery rule in API resulted in success and returned passed IDs;
    - deleting unexisting discovery rule in frontend (i.e. refreshing page after delete) page stopped with fatal error.
    
    After the fix:
    - before deleting, discovery rules are validated whether such ID exists and trows error if not;
    - SQL error in drule.delete method is also fixed and deleting works as intended;
    - deleting missing discovery rules in frontend, trows same error but now also displays complete page.
    
    git-svn-id: svn://svn.zabbix.com/trunk@40259 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/discoveryconf.php
+++ frontends/php/discoveryconf.php
@@ -73,20 +73,7 @@ if (isset($_REQUEST['druleid'])) {
 		access_deny();
 	}
 }
-if (isset($_REQUEST['go'])) {
-	if (!isset($_REQUEST['g_druleid']) || !is_array($_REQUEST['g_druleid'])) {
-		access_deny();
-	}
-	else {
-		$dbDRules = API::DRule()->get(array(
-			'druleids' => $_REQUEST['g_druleid'],
-			'countOutput' => true
-		));
-		if ($dbDRules != count($_REQUEST['g_druleid'])) {
-			access_deny();
-		}
-	}
-}
+
 $_REQUEST['go'] = get_request('go', 'none');
 
 // ajax
@@ -167,12 +154,11 @@ if (isset($_REQUEST['save'])) {
 	}
 }
 elseif (isset($_REQUEST['delete']) && isset($_REQUEST['druleid'])) {
-	$result = delete_discovery_rule($_REQUEST['druleid']);
+	$result = API::DRule()->delete(array($_REQUEST['druleid']));
+
 	show_messages($result, _('Discovery rule deleted'), _('Cannot delete discovery rule'));
 
 	if ($result) {
-		add_audit(AUDIT_ACTION_DELETE, AUDIT_RESOURCE_DISCOVERY_RULE, '['.$_REQUEST['druleid'].']');
-
 		unset($_REQUEST['form'], $_REQUEST['druleid']);
 		clearCookies($result);
 	}
@@ -193,16 +179,10 @@ elseif (str_in_array($_REQUEST['go'], array('activate', 'disable')) && isset($_R
 	clearCookies($goResult);
 }
 elseif ($_REQUEST['go'] == 'delete' && isset($_REQUEST['g_druleid'])) {
-	$goResult = false;
-	foreach ($_REQUEST['g_druleid'] as $drid) {
-		if (delete_discovery_rule($drid)) {
-			add_audit(AUDIT_ACTION_DELETE, AUDIT_RESOURCE_DISCOVERY_RULE, '['.$drid.']');
-			$goResult = true;
-		}
-	}
+	$result = API::DRule()->delete($_REQUEST['g_druleid']);
 
-	show_messages($goResult, _('Discovery rules deleted'));
-	clearCookies($goResult);
+	show_messages($result, _('Discovery rules deleted'), _('Cannot delete discovery rules'));
+	clearCookies($result);
 }
 
 /*
