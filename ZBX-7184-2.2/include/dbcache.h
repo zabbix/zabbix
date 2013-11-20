a29bf8bb17f298c9e0c88ba9af1bdb6b3ed4b938
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-11-20 09:45:47 +0000

    ........S. [ZBX-7184] added check to prevent host from being switched to unavailable state multiple times in a row
    git-svn-id: svn://svn.zabbix.com/branches/dev@40375 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7184-2.2/include/dbcache.h
+++ ZBX-7184-2.2/include/dbcache.h
@@ -326,7 +326,11 @@ void	DCrequeue_proxy(zbx_uint64_t hostid, unsigned char update_nextcheck);
 
 void	DCget_user_macro(zbx_uint64_t *hostids, int host_num, const char *macro, char **replace_to);
 
-int	DCconfig_update_host_availability(const zbx_host_availability_t *availability, int availability_num);
+int	DChost_activate(zbx_host_availability_t *in, zbx_host_availability_t *out);
+
+int	DChost_deactivate(zbx_timespec_t *ts, zbx_host_availability_t *in, zbx_host_availability_t *out);
+
+void	DChost_update_availability(const zbx_host_availability_t *availability, int availability_num);
 
 void	DCget_delta_items(zbx_hashset_t *items, const zbx_vector_uint64_t *ids);
 void	DCset_delta_items(zbx_hashset_t *items);
