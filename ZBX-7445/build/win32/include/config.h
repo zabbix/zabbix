5de1cbdf1a1b63f8b9d394151f8301ac10df3814
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-11-29 10:13:55 +0000

    .......... [ZBX-7445] Created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@40608 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-7445/build/win32/include/config.h
@@ -0,0 +1,74 @@
+/* Define to os name for code managing */
+#define ARCH "windows"
+
+/* Define to 1 if you have the <windows.h> header file. */
+#define HAVE_WINDOWS_H 1
+
+/* Define to 1 if you have the <winsock2.h> header file. */
+#define HAVE_WINSOCK2_H 1
+
+/* Define to 1 if you have the <ws2tcpip.h> header file. */
+#define HAVE_WS2TCPIP_H 1
+
+/* Define to 1 if you have the <wspiapi.h> header file. */
+#define HAVE_WSPIAPI_H 1
+
+/* Define to 1 if you have the <conio.h> header file. */
+#define HAVE_CONIO_H 1
+
+/* Define to 1 if you have the <process.h> header file. */
+#define HAVE_PROCESS_H 1
+
+/* Define to 1 if you have the <pdh.h> header file. */
+#define HAVE_PDH_H 1
+
+/* Define to 1 if you have the <pdhmsg.h> header file. */
+#define HAVE_PDHMSG_H 1
+
+/* Define to 1 if you have the <psapi.h> header file. */
+#define HAVE_PSAPI_H 1
+
+/* Define to 1 if you have the <sys/timeb.h> header file. */
+#define HAVE_SYS_TIMEB_H 1
+
+/* Define to 1 if you have the <Winldap.h> header file. */
+#define HAVE_WINLDAP_H 1
+
+/* Define to 1 if you have the <Winber.h> header file. */
+#define HAVE_WINBER_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <math.h> header file. */
+#define HAVE_MATH_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <time.h> header file. */
+#define HAVE_TIME_H 1
+
+/* Define to 1 if you have the <assert.h> header file. */
+#define HAVE_ASSERT_H 1
+
+/* Define to 1 if you have the <signal.h> header file. */
+#define HAVE_SIGNAL_H 1
+
+/* Define to 1 if you have the <io.h> header file. */
+#define HAVE_IO_H 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the <Iphlpapi.h> header file. */
+#define HAVE_IPHLPAPI_H 1
+
+/* Define to 1 if 'sockaddr_storage.ss_family' exists. */
+#define HAVE_SOCKADDR_STORAGE_SS_FAMILY 1
+
+/* define to 1 if you have the <errno.h> header file */
+#define HAVE_ERRNO_H 1
