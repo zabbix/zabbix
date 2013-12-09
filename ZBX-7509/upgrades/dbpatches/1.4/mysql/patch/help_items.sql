7b8edcfd75752c1443a8b4faa951a6d31eacba8a
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-12-09 14:19:58 +0000

    .......... [ZBX-7509] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@40854 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-7509/upgrades/dbpatches/1.4/mysql/patch/help_items.sql
@@ -0,0 +1,10 @@
+CREATE TABLE help_items_tmp (
+	itemtype		integer		DEFAULT '0'	NOT NULL,
+	key_		varchar(255)		DEFAULT ''	NOT NULL,
+	description		varchar(255)		DEFAULT ''	NOT NULL,
+	PRIMARY KEY (itemtype,key_)
+) ENGINE=InnoDB;
+
+insert into help_items_tmp select * from help_items;
+drop table help_items;
+alter table help_items_tmp rename help_items;
