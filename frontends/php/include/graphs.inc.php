2460036b5545996d367d8535f23876462296f3ef
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-12-09 12:14:10 +0000

    A......... [ZBX-7338] fixed screen item validation
    git-svn-id: svn://svn.zabbix.com/branches/2.2@40827 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/graphs.inc.php
+++ frontends/php/include/graphs.inc.php
@@ -159,7 +159,7 @@ function get_graphs_by_hostid($hostid) {
 }
 
 function get_realhosts_by_graphid($graphid) {
-	$graph = get_graph_by_graphid($graphid);
+	$graph = getGraphByGraphId($graphid);
 	if (!empty($graph['templateid'])) {
 		return get_realhosts_by_graphid($graph['templateid']);
 	}
@@ -284,14 +284,14 @@ function get_min_itemclock_by_itemid($itemIds) {
 	return $min ? $min: $result;
 }
 
-function get_graph_by_graphid($graphid) {
-	$dbGraphs = DBselect('SELECT g.* FROM graphs g WHERE g.graphid='.zbx_dbstr($graphid));
-	$dbGraphs = DBfetch($dbGraphs);
-	if (!empty($dbGraphs)) {
-		return $dbGraphs;
+function getGraphByGraphId($graphId) {
+	$dbGraph = DBfetch(DBselect('SELECT g.* FROM graphs g WHERE g.graphid='.zbx_dbstr($graphId)));
+
+	if ($dbGraph) {
+		return $dbGraph;
 	}
 
-	error(_s('No graph item with graphid "%s".', $graphid));
+	error(_s('No graph item with graphid "%s".', $graphId));
 
 	return false;
 }
