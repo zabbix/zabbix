162b461dd719b13b3cc08115d07b5311cfe17c78
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-12-10 08:32:54 +0000

    ..F....... [ZBX-7486] code formating
    git-svn-id: svn://svn.zabbix.com/branches/dev@40865 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7486/frontends/php/js/main.js
+++ ZBX-7486/frontends/php/js/main.js
@@ -1140,27 +1140,33 @@ function switch_mute(icon) {
 function createPlaceholders() {
 	if (IE) {
 		jQuery(document).ready(function() {
-			jQuery('[placeholder]').focus(function() {
-				var obj = jQuery(this);
-				if (obj.val() == obj.attr('placeholder')) {
-					obj.val('');
-					obj.removeClass('placeholder');
-				}
-			}).blur(function() {
-				var obj = jQuery(this);
-				if (obj.val() == '' || obj.val() == obj.attr('placeholder')) {
-					obj.val(obj.attr('placeholder'));
-					obj.addClass('placeholder');
-				}
-			}).blur();
-		});
+			jQuery('[placeholder]')
+				.focus(function() {
+					var obj = jQuery(this);
 
-		// clear the placeholder values so they don't get submitted
-		jQuery(document).delegate('form', 'submit.placeholder', function() {
-			jQuery('.placeholder').each(function(){
-				if (jQuery(this).val() == jQuery(this).attr('placeholder')) {
-					jQuery(this).val('');
-				}
+					if (obj.val() == obj.attr('placeholder')) {
+						obj.val('');
+						obj.removeClass('placeholder');
+					}
+				})
+				.blur(function() {
+					var obj = jQuery(this);
+
+					if (obj.val() == '' || obj.val() == obj.attr('placeholder')) {
+						obj.val(obj.attr('placeholder'));
+						obj.addClass('placeholder');
+					}
+				})
+				.blur();
+
+			jQuery('form').submit(function() {
+				jQuery('.placeholder').each(function() {
+					var obj = jQuery(this);
+
+					if (obj.val() == obj.attr('placeholder')) {
+						obj.val('');
+					}
+				});
 			});
 		});
 	}
