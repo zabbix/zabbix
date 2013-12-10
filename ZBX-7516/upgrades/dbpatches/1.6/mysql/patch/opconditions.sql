78233918866015ebcf6414e7babda510ba85aaa3
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-12-10 14:49:53 +0000

    .......... [ZBX-7516] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@40891 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7516/upgrades/dbpatches/1.6/mysql/patch/opconditions.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE opconditions (
-        opconditionid           bigint unsigned         DEFAULT '0'     NOT NULL,
-        operationid             bigint unsigned         DEFAULT '0'     NOT NULL,
-        conditiontype           integer         DEFAULT '0'     NOT NULL,
-        operator                integer         DEFAULT '0'     NOT NULL,
-        value           varchar(255)            DEFAULT ''      NOT NULL,
-        PRIMARY KEY (opconditionid)
-) ENGINE=InnoDB;
-CREATE INDEX opconditions_1 on opconditions (operationid);
