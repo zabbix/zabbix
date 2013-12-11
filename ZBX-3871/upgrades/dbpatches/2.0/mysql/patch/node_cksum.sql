e90c83584fa3353ca67c908e98edaf734719153f
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-12-11 08:03:28 +0000

    .......... [ZBX-3871,ZBX-4344] removed outdated development branches
    git-svn-id: svn://svn.zabbix.com/branches/dev@40906 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3871/upgrades/dbpatches/2.0/mysql/patch/node_cksum.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE node_cksum MODIFY nodeid integer NOT NULL,
-		       MODIFY recordid bigint unsigned NOT NULL;
-DELETE FROM node_cksum WHERE NOT nodeid IN (SELECT nodeid FROM nodes);
-ALTER TABLE node_cksum ADD CONSTRAINT c_node_cksum_1 FOREIGN KEY (nodeid) REFERENCES nodes (nodeid) ON DELETE CASCADE;
