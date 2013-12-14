cf00bf6f72889200adb3faafb189100f56d30f60
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-12-14 09:59:22 +0000

    .......... [ZBX-7288] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@40980 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7288/upgrades/dbpatches/1.8/oracle/patch/maintenances_hosts.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE maintenances_hosts (
-        maintenance_hostid              number(20)              DEFAULT '0'     NOT NULL,
-        maintenanceid           number(20)              DEFAULT '0'     NOT NULL,
-        hostid          number(20)              DEFAULT '0'     NOT NULL,
-        PRIMARY KEY (maintenance_hostid)
-);
-CREATE INDEX maintenances_hosts_1 on maintenances_hosts (maintenanceid,hostid);
-
