84ef9820ec24f39e7a5d6609944766d209d0ddcd
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-12-14 15:36:06 +0000

    ....I...S. [ZBXNEXT-581] created database patch to export items.filter field into item_condition table
    git-svn-id: svn://svn.zabbix.com/branches/dev@41006 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-581/src/libs/zbxdbupgrade/dbupgrade_2030.c
+++ ZBXNEXT-581/src/libs/zbxdbupgrade/dbupgrade_2030.c
@@ -35,6 +35,100 @@ static int	DBpatch_2030000(void)
 	return SUCCEED;
 }
 
+static int	DBpatch_2030001(void)
+{
+	const ZBX_TABLE	table =
+			{"item_condition", "item_conditionid", 0,
+				{
+					{"item_conditionid", NULL, NULL, NULL, 0, ZBX_TYPE_ID, ZBX_NOTNULL, 0},
+					{"itemid", NULL, NULL, NULL, 0, ZBX_TYPE_ID, ZBX_NOTNULL, 0},
+					{"templateid", NULL, NULL, NULL, 0, ZBX_TYPE_ID, 0, 0},
+					{"operator", "8", NULL, NULL, 0, ZBX_TYPE_INT, ZBX_NOTNULL, 0},
+					{"macro", "", NULL, NULL, 64, ZBX_TYPE_CHAR, ZBX_NOTNULL, 0},
+					{"value", "", NULL, NULL, 255, ZBX_TYPE_CHAR, ZBX_NOTNULL, 0},
+					{NULL}
+				}
+			};
+
+	return DBcreate_table(&table);
+}
+
+static int	DBpatch_2030002(void)
+{
+	return DBcreate_index("item_condition", "item_condition_1", "itemid", 0);
+}
+
+static int	DBpatch_2030003(void)
+{
+	return DBcreate_index("item_condition", "item_condition_2", "templateid", 0);
+}
+
+static int	DBpatch_2030004(void)
+{
+	const ZBX_FIELD	field = {"itemid", NULL, "items", "itemid", 0, 0, 0, ZBX_FK_CASCADE_DELETE};
+
+	return DBadd_foreign_key("item_condition", 1, &field);
+}
+
+static int	DBpatch_2030005(void)
+{
+	const ZBX_FIELD	field = {"templateid", NULL, "item_condition", "item_conditionid", 0, 0, 0,
+			ZBX_FK_CASCADE_DELETE};
+
+	return DBadd_foreign_key("item_condition", 2, &field);
+}
+
+static int	DBpatch_2030006(void)
+{
+	DB_RESULT	result;
+	DB_ROW		row;
+	char		*value, *macro_esc, *value_esc, *templateid;
+	int		ret = FAIL, rc;
+
+	result = DBselect("select itemid,filter,templateid from items where filter<>''");
+
+	while (NULL != (row = DBfetch(result)))
+	{
+		if (NULL == (value = strchr(row[1], ':')) || 0 == strcmp(row[1], ":"))
+			continue;
+
+		*value++ = '\0';
+
+		macro_esc = DBdyn_escape_string(row[1]);
+		value_esc = DBdyn_escape_string(value);
+		templateid = (SUCCEED == DBis_null(row[2])) ? "null" : row[2];
+
+		rc = DBexecute("insert into item_condition"
+				" (item_conditionid,itemid,templateid,macro,value)"
+				" values (%s,%s,%s,'%s','%s')",
+				row[0], row[0], templateid, macro_esc, value_esc);
+
+		zbx_free(value_esc);
+		zbx_free(macro_esc);
+
+		if (ZBX_DB_OK > rc)
+			goto out;
+	}
+
+	ret = SUCCEED;
+out:
+	DBfree_result(result);
+
+	return ret;
+}
+
+static int	DBpatch_2030007(void)
+{
+	const ZBX_FIELD field = {"evaltype", "0", NULL, NULL, 0, ZBX_TYPE_INT, ZBX_NOTNULL, 0};
+
+	return DBadd_field("items", &field);
+}
+
+static int	DBpatch_2030008(void)
+{
+	return DBdrop_field("items", "filter");
+}
+
 #endif
 
 DBPATCH_START(2030)
@@ -42,5 +136,16 @@ DBPATCH_START(2030)
 /* version, duplicates flag, mandatory flag */
 
 DBPATCH_ADD(2030000, 0, 1)
+
+/* ZBXNEXT-581 */
+DBPATCH_ADD(2030001, 0, 1)
+DBPATCH_ADD(2030002, 0, 1)
+DBPATCH_ADD(2030003, 0, 1)
+DBPATCH_ADD(2030004, 0, 1)
+DBPATCH_ADD(2030005, 0, 1)
+DBPATCH_ADD(2030006, 0, 1)
+DBPATCH_ADD(2030007, 0, 1)
+DBPATCH_ADD(2030008, 0, 1)
+/* */
 
 DBPATCH_END()
