29a61ad314aa45689cd4e0f6a1549a9d5e759a70
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2013-12-17 09:46:23 +0000

    .......... [ZBX-7483] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@41066 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7483/upgrades/dbpatches/1.8/oracle/patch/regexps.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE regexps (
-        regexpid                number(20)              DEFAULT '0'     NOT NULL,
-        name            nvarchar2(128)          DEFAULT ''      ,
-        test_string             nvarchar2(2048)         DEFAULT ''      ,
-        PRIMARY KEY (regexpid)
-);
-CREATE INDEX regexps_1 on regexps (name);
-
