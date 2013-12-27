b80d02d2e779026c78835b841df2401749d8936e
Chris Christensen <cchristensen@llnw.com>
2013-12-27 16:45:53 -0600

    Push ezsql to composer config and add additional API dep.

--- frontends/php/llnw/config.php
+++ frontends/php/llnw/config.php
@@ -44,8 +44,6 @@ include($base_dir.'toolbox.php');
 
 
 // for ezSQL help:  http://justinvincent.com/docs/ezsql/ez_sql_help.htm
-include_once($base_dir."ezsql/shared/ez_sql_core.php");
-include_once($base_dir."ezsql/mysql/ez_sql_mysql.php");
 $db = new ezSQL_mysql($db_user, $db_pass, $db_name, $db_host);
 $ldb = new ezSQL_mysql($ldb_user, $ldb_pass, $ldb_name, $ldb_host);
 $cdb = new ezSQL_mysql($cdb_user, $cdb_pass, $cdb_name, $cdb_host);
