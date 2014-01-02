d9614270ffd42afe7147bfcb365922a4fa838268
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-01-02 14:00:20 +0000

    .......... [ZBX-7576] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@41225 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-7576/src/libs/zbxsysinfo/openbsd/cpu.c
@@ -0,0 +1,183 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2013 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+#include "sysinfo.h"
+#include "stats.h"
+
+static int	get_cpu_num()
+{
+#ifdef HAVE_FUNCTION_SYSCTL_HW_NCPU	/* OpenBSD 4.2,4.3 i386 */
+	size_t	len;
+	int	mib[] = {CTL_HW, HW_NCPU}, ncpu;
+
+	len = sizeof(ncpu);
+
+	if (-1 == sysctl(mib, 2, &ncpu, &len, NULL, 0))
+		return -1;
+
+	return ncpu;
+#else
+	return -1;
+#endif
+}
+
+int	SYSTEM_CPU_NUM(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+	char	*tmp;
+	int	cpu_num;
+
+	if (1 < request->nparam)
+		return SYSINFO_RET_FAIL;
+
+	tmp = get_rparam(request, 0);
+
+	/* only "online" (default) for parameter "type" is supported */
+	if (NULL != tmp && '\0' != *tmp && 0 != strcmp(tmp, "online"))
+		return SYSINFO_RET_FAIL;
+
+	if (-1 == (cpu_num = get_cpu_num()))
+		return SYSINFO_RET_FAIL;
+
+	SET_UI64_RESULT(result, cpu_num);
+
+	return SYSINFO_RET_OK;
+}
+
+int	SYSTEM_CPU_UTIL(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+	char	*tmp;
+	int	cpu_num, state, mode;
+
+	if (3 < request->nparam)
+		return SYSINFO_RET_FAIL;
+
+	tmp = get_rparam(request, 0);
+
+	if (NULL == tmp || '\0' == *tmp || 0 == strcmp(tmp, "all"))
+		cpu_num = 0;
+	else if (SUCCEED != is_uint31_1(tmp, &cpu_num))
+		return SYSINFO_RET_FAIL;
+	else
+		cpu_num++;
+
+	tmp = get_rparam(request, 1);
+
+	if (NULL == tmp || '\0' == *tmp || 0 == strcmp(tmp, "user"))
+		state = ZBX_CPU_STATE_USER;
+	else if (0 == strcmp(tmp, "nice"))
+		state = ZBX_CPU_STATE_NICE;
+	else if (0 == strcmp(tmp, "system"))
+		state = ZBX_CPU_STATE_SYSTEM;
+	else if (0 == strcmp(tmp, "idle"))
+		state = ZBX_CPU_STATE_IDLE;
+	else if (0 == strcmp(tmp, "interrupt"))
+		state = ZBX_CPU_STATE_INTERRUPT;
+	else
+		return SYSINFO_RET_FAIL;
+
+	tmp = get_rparam(request, 2);
+
+	if (NULL == tmp || '\0' == *tmp || 0 == strcmp(tmp, "avg1"))
+		mode = ZBX_AVG1;
+	else if (0 == strcmp(tmp, "avg5"))
+		mode = ZBX_AVG5;
+	else if (0 == strcmp(tmp, "avg15"))
+		mode = ZBX_AVG15;
+	else
+		return SYSINFO_RET_FAIL;
+
+	return get_cpustat(result, cpu_num, state, mode);
+}
+
+int	SYSTEM_CPU_LOAD(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+	char	*tmp;
+	int	mode, per_cpu = 1, cpu_num;
+	double	load[ZBX_AVG_COUNT], value;
+
+	if (2 < request->nparam)
+		return SYSINFO_RET_FAIL;
+
+	tmp = get_rparam(request, 0);
+
+	if (NULL == tmp || '\0' == *tmp || 0 == strcmp(tmp, "all"))
+		per_cpu = 0;
+	else if (0 != strcmp(tmp, "percpu"))
+		return SYSINFO_RET_FAIL;
+
+	tmp = get_rparam(request, 1);
+
+	if (NULL == tmp || '\0' == *tmp || 0 == strcmp(tmp, "avg1"))
+		mode = ZBX_AVG1;
+	else if (0 == strcmp(tmp, "avg5"))
+		mode = ZBX_AVG5;
+	else if (0 == strcmp(tmp, "avg15"))
+		mode = ZBX_AVG15;
+	else
+		return SYSINFO_RET_FAIL;
+
+	if (mode >= getloadavg(load, 3))
+		return SYSINFO_RET_FAIL;
+
+	value = load[mode];
+
+	if (1 == per_cpu)
+	{
+		if (0 >= (cpu_num = get_cpu_num()))
+			return SYSINFO_RET_FAIL;
+		value /= cpu_num;
+	}
+
+	SET_DBL_RESULT(result, value);
+
+	return SYSINFO_RET_OK;
+}
+
+int     SYSTEM_CPU_SWITCHES(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+	int		mib[] = {CTL_VM, VM_UVMEXP};
+	size_t		len;
+	struct uvmexp	v;
+
+	len = sizeof(struct uvmexp);
+
+	if (0 != sysctl(mib, 2, &v, &len, NULL, 0))
+		return SYSINFO_RET_FAIL;
+
+	SET_UI64_RESULT(result, v.swtch);
+
+	return SYSINFO_RET_OK;
+}
+
+int     SYSTEM_CPU_INTR(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+	int		mib[] = {CTL_VM, VM_UVMEXP};
+	size_t		len;
+	struct uvmexp	v;
+
+	len = sizeof(struct uvmexp);
+
+	if (0 != sysctl(mib, 2, &v, &len, NULL, 0))
+		return SYSINFO_RET_FAIL;
+
+	SET_UI64_RESULT(result, v.intrs);
+
+	return SYSINFO_RET_OK;
+}
