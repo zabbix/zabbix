3a49b8252a65fa377e9be12368b7fcf34dd9d5b1
Chris Christensen <cchristensen@llnw.com>
2014-01-03 17:15:54 -0600

    [#MON-2822] Refactor Hostlist class.

--- frontends/php/llnw/lib/LLNW/Zabbix/Hostlist.php
+++ frontends/php/llnw/lib/LLNW/Zabbix/Hostlist.php
@@ -1,56 +1,60 @@
 <?php
-include_once('config.php');
+namespace LLNW\Zabbix;
 
+class Hostlist {
+    public function get($name, $output = 'json') {
+        $this->_getpull($name, $output, 'hostlist.get');
+    }
 
+    public function pull($name, $output = 'json') {
+        $this->_getpull($name, $output, 'hostlist.pull');
+    }
 
-if ($json['method'] == 'hostlist.get' || $json['method'] == 'hostlist.pull') {
+    private function _getpull($list_name, $output, $method) {
+        global $logger;
 
-   // this section returns the current list of hosts in the given hostlist
-   // below is where we determine if we have to truncate the file or not.
-   
-   $list_name = (isset($json['params']['name']) && $json['params']['name'] != '') ? addslashes($json['params']['name']) : '';
-   $output    = (isset($json['params']['output']) && $json['params']['output'] == 'raw') ? 'raw' : 'json';
+        $list_name = (isset($list_name) && $list_name != '') ? addslashes($list_name) : '';
+        $output    = (isset($output) && $output == 'raw') ? 'raw' : 'json';
 
-   if ($list_name == '') {
-      error_log("Error: no hostlist name supplied in request");
-      sendErrorResponse('235','Invalid params','no hostlist name provided');
-   }
+        if ($list_name == '') {
+            $logger->log("Error: no hostlist name supplied in request");
+            sendErrorResponse('235','Invalid params','no hostlist name provided');
+        }
 
-   $list_dir = '/tmp/';
+        $list_dir = '/tmp/';
 
-   if (!is_readable($list_dir.$list_name)) {
-      error_log("Error: hostlist file $list_dir$list_name does not exist");
-      sendErrorResponse('151','Internal error','hostlist file does not exist');
-   }
+        if (!is_readable($list_dir.$list_name)) {
+            $logger->log("Error: hostlist file $list_dir$list_name does not exist");
+            sendErrorResponse('151','Internal error','hostlist file does not exist');
+        }
 
-   $hostlist = file($list_dir.$list_name);
+        $hostlist = file($list_dir.$list_name);
 
-   // removes the newlines from each item in array
-   $hostlist = array_map('trim', $hostlist);
+        // removes the newlines from each item in array
+        $hostlist = array_map('trim', $hostlist);
 
-   $resp['result'] = 'success';
-   $resp['hosts'] = $hostlist;
+        $resp['result'] = 'success';
+        $resp['hosts'] = $hostlist;
 
-   // method 'pull' truncates the list.
-   if ($json['method'] == 'hostlist.pull') {
-      if (!is_writable($list_dir.$list_name)) {
-         error_log("Error: hostlist file $list_dir$list_name cannot be truncated");
-      }
-      else {
-         $fh = fopen($list_dir.$list_name, 'w');
-         fclose($fh);
-      }
-   }
+        // method 'pull' truncates the list.
+        if ($method == 'hostlist.pull') {
+            if (!is_writable($list_dir.$list_name)) {
+                $logger->log("Error: hostlist file $list_dir$list_name cannot be truncated");
+            }
+            else {
+                $fh = fopen($list_dir.$list_name, 'w');
+                fclose($fh);
+            }
+        }
 
-   if ($output == 'json') {
-      sendResponse($resp);
-   }
-   else {
-      foreach ($hostlist as $host) {
-         print $host."\n";
-      }
-      exit;
-   }
+        if ($output == 'json') {
+            sendResponse($resp);
+        }
+        else {
+            foreach ($hostlist as $host) {
+                print $host."\n";
+            }
+            exit;
+        }
+    }
 }
-
-?>
