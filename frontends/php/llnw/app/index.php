2aabd54ad6b7e848e8eb3fff72d12575d04e4c8a
Chris Christensen <cchristensen@llnw.com>
2014-01-03 14:19:36 -0600

    [#MON-2822] Refactor JSONRPC callback and class structure (beginning with Ack).

--- frontends/php/llnw/app/index.php
+++ frontends/php/llnw/app/index.php
@@ -1,18 +1,17 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use LLNW\Zabbix;
+use LLNW\Zabbix\Config;
+use LLNW\Zabbix\RPC;
 
-LLNW\Zabbix\Config::includeLLNWZabbix();
+Config::includeLLNWZabbix();
 
 // TODO: Refactor to silex before processor: http://silex.sensiolabs.org/doc/cookbook/json_request_body.html
 $json = json_decode(file_get_contents("php://input"), true);  // true makes this fn return associative array, instead of object.
 
-// TODO: Refactor to jsonrpc/jsonrpc error class
-if (json_last_error() != 0) {
-   sendErrorResponse('123','Invalid JSON', 'data received was not formatted properly');
-   exit;
-}
+// NEWHOTNESS!! --> SETUP
+$jsonrpc = new RPC($json);
+
 
 // TODO: convert this to an actual zbx user authentication using the Zabbix API login() and userGroup() functions
 if (($json['key'] != 'sjE4i') && (strlen($json['key']) != 32)) {
@@ -24,11 +23,6 @@ if (($json['key'] != 'sjE4i') && (strlen($json['key']) != 32)) {
 // you could use this to consolidate authentications, responses, error handling, etc.
 // check for the master variable above so only the included scripts will run if $master is defined and equal to 1.
 
-// TODO: add.squelch,clear.squelch :: Squelch.php
-// TODO: get.ack,ack.get,add.ack,ack.add :: Ack.php
-// TODO: hostlist.get,hostlist.pull :: Hostlist.php
-// TODO: proxymap.get :: proxy.php --> Proxymap.php
-// TODO: alertqueue.create :: alert-queue.php --> AlertQueue.php
-// TODO: proxy.status, proxy.reassign :: proxy-assign.php --> Proxy.php
-// TODO: dm.info :: dm-info.php --> DM.php
-// TODO: queue.info :: queue-info.php --> Queue.php
+
+// TODO: this would be the default callback for application type jsonrpc
+return $jsonrpc->receive();
--- frontends/php/llnw/lib/LLNW/Zabbix/Ack.php
+++ frontends/php/llnw/lib/LLNW/Zabbix/Ack.php
@@ -1,64 +1,59 @@
 <?php
-include('config.php');
 
-if ($_POST['key'] != 'sjE4i') {
-   exit;
+namespace LLNW\Zabbix;
+
+class Ack {
+    protected $db = '';
+    protected $logger = '';
+
+    public function get($eventids) {
+        // HACK: SQL injection!
+        $sql_where = (isset($eventids) && $eventids != '') ? 'WHERE eventid IN ('.$eventids.')' : '';
+        $q = "SELECT * FROM acknowledges $sql_where ORDER BY clock ASC";
+
+        $logger->log("get.ack query: $q");
+        return $db->get_results($q);
+    }
+
+    public function add($userid, $triggerid, $message = '') {
+        // TODO: Act on behalf of userid, lookup trigger eventid
+        //  (trigger.get, selectLastEvent) -> event.acknowledge
+        // objectid in the events table have to use triggerid because checkboxes
+        //  are trigger ids.
+
+        $clock   = strtotime('now'); //$_POST['clock'];
+        $message = urldecode($message);
+        $message = addslashes($message);
+
+        $logger->log("received triggerid: $triggerid");
+
+        $q = "SELECT eventid FROM events ".
+                "WHERE objectid=$triggerid AND value=1 ".
+                "ORDER BY clock DESC LIMIT 1";
+
+        $eventid = $db->get_var($q);
+        $logger->log("got eventid from events table: $eventid");
+
+        if (isset($eventid) && $eventid > 0) {
+
+            $ackid = $db->get_var("SELECT nextid FROM ids WHERE table_name='acknowledges' LIMIT 1");
+            $logger->log("got nextid: $ackid");
+            if (isset($ackid) && $ackid > 0) {
+                $next_id = $ackid + 1;
+                $logger->log("adding $next_id to ids table");
+                $db->get_var("UPDATE ids SET nextid=$next_id WHERE table_name='acknowledges'");
+
+                $q = "UPDATE events SET acknowledged=1 WHERE eventid=$eventid";
+                $db->query($q);
+                $logger->log($q);
+
+                $q = "INSERT INTO acknowledges SET acknowledgeid='$ackid', userid='$userid', eventid='$eventid', clock='$clock', message='$message'";
+                $db->query($q);
+                $logger->log($q);
+
+                return 'success';
+            }
+        }
+        return 'error';
+    }
 }
-
-$_POST['method'] = urldecode($_POST['method']);
-
-if ($_POST['method'] == 'get.ack' || $_POST['method'] == 'ack.get') {
-   $sql_where = (isset($_POST['eventids']) && $_POST['eventids'] != '') ? 'WHERE eventid IN ('.$_POST['eventids'].')' : '';
-
-   $q = "SELECT * FROM acknowledges $sql_where ORDER BY clock ASC";
-
-   error_log("get.ack query: $q");
-   $resp = $db->get_results($q);
-
-}
-elseif ($_POST['method'] == 'add.ack' || $_POST['method'] == 'ack.add') {
-   $userid  = $_POST['userid'];
-   $triggerid = $_POST['triggerid']; // this is the objectid in the events table
-                                     // have to use triggerid because checkboxes are trigger ids.
-
-   $clock   = strtotime('now'); //$_POST['clock'];
-   $message = urldecode($_POST['message']);
-   $message = addslashes($message);
-
-   error_log("received triggerid: $triggerid");
-
-   $q = "SELECT eventid FROM events ".
-        "WHERE objectid=$triggerid AND value=1 ".
-        "ORDER BY clock DESC LIMIT 1";
-
-   $eventid = $db->get_var($q);
-   error_log("got eventid from events table: $eventid");
-
-   $resp['result'] = 'error';
-   if (isset($eventid) && $eventid > 0) {
-
-      $ackid = $db->get_var("SELECT nextid FROM ids WHERE table_name='acknowledges' LIMIT 1");
-      error_log("got nextid: $ackid");
-      if (isset($ackid) && $ackid > 0) {
-         $next_id = $ackid + 1;
-         error_log("adding $next_id to ids table");
-         $db->get_var("UPDATE ids SET nextid=$next_id WHERE table_name='acknowledges'");
-
-         $q = "UPDATE events SET acknowledged=1 WHERE eventid=$eventid";
-         $db->query($q);
-         error_log($q);
-
-         $q = "INSERT INTO acknowledges SET acknowledgeid='$ackid', userid='$userid', eventid='$eventid', clock='$clock', message='$message'";
-         $db->query($q);
-         error_log($q);
-
-         $resp['result'] = 'success';
-      }
-   }
-}
-
-header('Content-type: application/json');
-$json = json_encode($resp);
-//error_log("response sent: ".$json);
-print $json;
-?>
