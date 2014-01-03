60434683354dcf49289315691f69e21c7d589068
Chris Christensen <cchristensen@llnw.com>
2014-01-03 16:07:56 -0600

    [#MON-2822] Refactor Zabbix GUI config file inclusion from /etc/zabbix.

--- frontends/php/llnw/app/index.php
+++ frontends/php/llnw/app/index.php
@@ -8,11 +8,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 Config::includeLLNWZabbix();
 
-// TODO: at some point we should use this as the central landing point for all llnw custom api requests
-// an example below shows how just checking the method and doing file includes would make this easy to maintain.
-// you could use this to consolidate authentications, responses, error handling, etc.
-// check for the master variable above so only the included scripts will run if $master is defined and equal to 1.
-
 $app = new Application();
 
 // TODO: convert this to an actual zbx user authentication using the Zabbix API login() and userGroup() functions
