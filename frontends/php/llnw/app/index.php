65eddf248d9c49355752790aae470c17e4776102
Chris Christensen <cchristensen@llnw.com>
2014-01-03 10:53:38 -0600

    [#MON-2822] Refactor index usage, begin pushing scripts to namespaces.

--- frontends/php/llnw/app/index.php
+++ frontends/php/llnw/app/index.php
@@ -1,15 +1,14 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$d='/var/www/zabbix/llnw/';
-include($d.'config.php');
+use LLNW\Zabbix;
 
-// THIS PAGE IS LIVE!!!
+LLNW\Zabbix\Config::includeLLNWZabbix();
 
-$master=1;
-
+// TODO: Refactor to silex before processor: http://silex.sensiolabs.org/doc/cookbook/json_request_body.html
 $json = json_decode(file_get_contents("php://input"), true);  // true makes this fn return associative array, instead of object.
 
+// TODO: Refactor to jsonrpc/jsonrpc error class
 if (json_last_error() != 0) {
    sendErrorResponse('123','Invalid JSON', 'data received was not formatted properly');
    exit;
@@ -20,40 +19,16 @@ if (($json['key'] != 'sjE4i') && (strlen($json['key']) != 32)) {
    exit;
 }
 
-$trans_id = (isset($json['id'])) ? $json['id'] : '';
-
-
 // TODO: at some point we should use this as the central landing point for all llnw custom api requests
 // an example below shows how just checking the method and doing file includes would make this easy to maintain.
 // you could use this to consolidate authentications, responses, error handling, etc.
 // check for the master variable above so only the included scripts will run if $master is defined and equal to 1.
 
-
-if ($json['method'] == 'add.squelch') {
-   include($base_dir.'squelch.php');
-}
-elseif ($json['method'] == 'get.ack') {
-   include($base_dir.'ack.php');
-}
-elseif ($json['method'] == 'hostlist.get' || $json['method'] == 'hostlist.pull') {
-   include($base_dir.'hostlist.php');
-}
-elseif ($json['method'] == 'proxymap.get') {
-   include($base_dir.'proxy.php');
-}
-elseif ($json['method'] == 'alertqueue.create') {
-   include($base_dir.'alert-queue.php');
-}
-elseif ($json['method'] == 'proxy.status' || $json['method'] == 'proxy.reassign') {
-   include($base_dir.'proxy-assign.php');
-}
-elseif ($json['method'] == 'dm.info') {
-   include($base_dir.'dm-info.php');
-}
-elseif ($json['method'] == 'queue.info') {
-   include($base_dir.'queue-info.php');
-}
-else {
-   sendErrorResponse('235', "Invalid method", "Invalid method");
-}
-?>
+// TODO: add.squelch,clear.squelch :: Squelch.php
+// TODO: get.ack,ack.get,add.ack,ack.add :: Ack.php
+// TODO: hostlist.get,hostlist.pull :: Hostlist.php
+// TODO: proxymap.get :: proxy.php --> Proxymap.php
+// TODO: alertqueue.create :: alert-queue.php --> AlertQueue.php
+// TODO: proxy.status, proxy.reassign :: proxy-assign.php --> Proxy.php
+// TODO: dm.info :: dm-info.php --> DM.php
+// TODO: queue.info :: queue-info.php --> Queue.php
