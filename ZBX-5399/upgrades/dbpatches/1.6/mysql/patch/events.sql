e17a980659414ccc480fc3663724b877588feb39
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-01-06 07:15:31 +0000

    .......... [ZBX-5399] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@41281 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5399/upgrades/dbpatches/1.6/mysql/patch/events.sql
+++
@@ -1,5 +0,0 @@
-alter table events drop index events_1;
-alter table events drop index events_2;
-
-CREATE INDEX events_1 on events (object,objectid,eventid);
-CREATE INDEX events_2 on events (clock);
