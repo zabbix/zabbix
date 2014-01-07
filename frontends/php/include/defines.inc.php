6e589a9bc25212f8febb095f825c9dfef4b0a946
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-01-07 08:21:51 +0000

    .......... [ZBX-954] updated copyright to 2014
    git-svn-id: svn://svn.zabbix.com/branches/2.0@41314 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 ** Zabbix
-** Copyright (C) 2001-2013 Zabbix SIA
+** Copyright (C) 2001-2014 Zabbix SIA
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@ define('ZABBIX_VERSION',		'2.0.11rc1');
 define('ZABBIX_API_VERSION',	'2.0.11');
 
 define('ZABBIX_COPYRIGHT_FROM', '2001');
-define('ZABBIX_COPYRIGHT_TO', '2013');
+define('ZABBIX_COPYRIGHT_TO', '2014');
 
 define('ZBX_LOGIN_ATTEMPTS',	5);
 define('ZBX_LOGIN_BLOCK',		30); // sec
