01bfc1738eafc3aadc47c5eb11ba1b665618d7d0
juris <juris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-01-20 16:23:22 +0000

    ...G...... [ZBX-7668] Removed validation for key values when read from files to remain consistent with command line options. Empty key values are allowed.
    git-svn-id: svn://svn.zabbix.com/branches/dev@41678 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7668/src/zabbix_sender/zabbix_sender.c
+++ ZBX-7668/src/zabbix_sender/zabbix_sender.c
@@ -479,16 +479,7 @@ int	main(int argc, char **argv)
 				}
 			}
 
-			if ('\0' != *p && '"' != *p)
-			{
-				zbx_strlcpy(key_value, p, sizeof(key_value));
-			}
-			else if ('\0' == *p || NULL == (p = get_string(p, key_value, sizeof(key_value))) || '\0' == *key_value)
-			{
-				zabbix_log(LOG_LEVEL_WARNING, "[line %d] 'Key value' required", total_count);
-				ret = FAIL;
-				break;
-			}
+			p = get_string(p, key_value, sizeof(key_value));
 
 			zbx_rtrim(key_value, "\r\n");
 
