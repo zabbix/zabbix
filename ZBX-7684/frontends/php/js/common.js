451c5ea9107903fa7b283a8003e957324002e446
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-01-23 13:22:26 +0000

    ..F....... [ZBX-7684] clean old popup content before opening new popup window
    git-svn-id: svn://svn.zabbix.com/branches/dev@41810 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7684/frontends/php/js/common.js
+++ ZBX-7684/frontends/php/js/common.js
@@ -489,18 +489,26 @@ function insertInElement(element_name, text, tagName) {
 	}
 }
 
-function openWinCentered(loc, winname, iwidth, iheight, params) {
-	var tp = Math.ceil((screen.height - iheight) / 2);
-	var lf = Math.ceil((screen.width - iwidth) / 2);
+function openWinCentered(url, name, width, height, params) {
+	var top = Math.ceil((screen.height - height) / 2),
+		left = Math.ceil((screen.width - width) / 2);
 
 	if (params.length > 0) {
 		params = ', ' + params;
 	}
 
-	loc = new Curl(loc).getUrl();
+	// open empty window
+	window.open(new Curl(url.substring(0, url.indexOf('?'))).getUrl(), name,
+		'width=' + width + ', height=' + height + ', top=' + top + ', left=' + left + params
+	);
 
-	var WinObjReferer = window.open(loc, winname, 'width=' + iwidth + ', height=' + iheight + ', top=' + tp + ', left=' + lf + params);
-	WinObjReferer.focus();
+	// open script window
+	setTimeout(function() {
+		var windowObj = window.open(new Curl(url).getUrl(), name,
+			'width=' + width + ', height=' + height + ', top=' + top + ', left=' + left + params
+		);
+		windowObj.focus();
+	}, 100);
 }
 
 function PopUp(url, width, height, form_name) {
