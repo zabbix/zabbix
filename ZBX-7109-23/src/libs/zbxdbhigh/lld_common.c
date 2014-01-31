9b0b5714e070e2be0b39b5126a9ff6098b2bf39b
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-01-31 07:39:43 +0000

    ........S. [ZBX-7109] fixed typos and formatting during code review [merge from branches/dev/ZBX-7109-22, r41984]
    git-svn-id: svn://svn.zabbix.com/branches/dev@42009 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7109-23/src/libs/zbxdbhigh/lld_common.c
+++ ZBX-7109-23/src/libs/zbxdbhigh/lld_common.c
@@ -1,6 +1,6 @@
 /*
 ** Zabbix
-** Copyright (C) 2000-2011 Zabbix SIA
+** Copyright (C) 2001-2014 Zabbix SIA
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -21,14 +21,12 @@
 #include "db.h"
 #include "log.h"
 #include "zbxserver.h"
-#include "zbxregexp.h"
 
 /******************************************************************************
  *                                                                            *
  * Function: lld_field_str_rollback                                           *
  *                                                                            *
- * Purpose: finds item in the selected host by an item prototype key and      *
- *          discovered data                                                   *
+ * Author: Alexander Vladishev                                                *
  *                                                                            *
  ******************************************************************************/
 void	lld_field_str_rollback(char **field, char **field_orig, zbx_uint64_t *flags, zbx_uint64_t flag)
