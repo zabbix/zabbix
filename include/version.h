2f6cc4b3a4ed77c498c77f4ad7c4072ef5378ea0
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-02-03 15:43:19 +0000

    .......... [ZBXNEXT-826] updated release date
    git-svn-id: svn://svn.zabbix.com/branches/2.2@42183 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/version.h
+++ include/version.h
@@ -24,7 +24,7 @@
 #define ZBX_STR(str)	ZBX_STR2(str)
 
 #define APPLICATION_NAME	"Zabbix Agent"
-#define ZABBIX_REVDATE		"09 December 2013"
+#define ZABBIX_REVDATE		"03 February 2014"
 #define ZABBIX_VERSION_MAJOR	2
 #define ZABBIX_VERSION_MINOR	2
 #define ZABBIX_VERSION_PATCH	2
--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,7 +22,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	public static final String REVISION_DATE = "09 December 2013";
+	public static final String REVISION_DATE = "03 February 2014";
 	public static final String REVISION = "{ZABBIX_REVISION}";
 	public static final String VERSION = "2.2.2rc1";
 
