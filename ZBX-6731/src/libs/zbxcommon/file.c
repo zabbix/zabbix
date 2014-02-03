ef8f364be7d385a2ed14974c90d1ba071ed9ab5f
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-02-03 13:03:50 +0000

    ...G...... [ZBX-6731] added zbx_lseek() error checking
    git-svn-id: svn://svn.zabbix.com/branches/dev@42137 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6731/src/libs/zbxcommon/file.c
+++ ZBX-6731/src/libs/zbxcommon/file.c
@@ -124,7 +124,8 @@ int	zbx_read(int fd, char *buf, size_t count, const char *encoding)
 	int		nbytes;
 	zbx_offset_t	offset;
 
-	offset = zbx_lseek(fd, 0, SEEK_CUR);
+	if ((zbx_offset_t)-1 == (offset = zbx_lseek(fd, 0, SEEK_CUR)))
+		return -1;
 
 	if (0 >= (nbytes = (int)read(fd, buf, count)))
 		return nbytes;
@@ -150,7 +151,8 @@ int	zbx_read(int fd, char *buf, size_t count, const char *encoding)
 		}
 	}
 
-	zbx_lseek(fd, offset + (zbx_offset_t)i, SEEK_SET);
+	if ((zbx_offset_t)-1 == (zbx_lseek(fd, offset + (zbx_offset_t)i, SEEK_SET)))
+		return -1;
 
 	return (int)i;
 }
@@ -247,7 +249,11 @@ int	zbx_read2(int fd, zbx_uint64_t *lastlogsize, int *mtime, unsigned char *skip
 			goto out;
 		}
 
-		offset = zbx_lseek(fd, 0, SEEK_CUR);
+		if ((zbx_offset_t)-1 == (offset = zbx_lseek(fd, 0, SEEK_CUR)))
+		{
+			*big_rec = 0;
+			goto out;
+		}
 
 		nbytes = (int)read(fd, buf, (size_t)BUF_SIZE);
 
