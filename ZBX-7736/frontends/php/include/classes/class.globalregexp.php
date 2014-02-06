656cff2833bc4e0cc32672c1f623d46e1e0f323e
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-02-06 16:05:12 +0000

    ..F....... [ZBX-7736] fixed global regular expression type "Any character string included"
    git-svn-id: svn://svn.zabbix.com/branches/dev@42380 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7736/frontends/php/include/classes/class.globalregexp.php
+++ ZBX-7736/frontends/php/include/classes/class.globalregexp.php
@@ -196,17 +196,21 @@ class GlobalRegExp {
 			$paterns = array($expression['expression']);
 		}
 
-		$expectedResult = $expression['expression_type'] != EXPRESSION_TYPE_NOT_INCLUDED;
+		$expectedResult = ($expression['expression_type'] != EXPRESSION_TYPE_NOT_INCLUDED);
 
-
-		if ($expression['case_sensitive']) {
-			foreach ($paterns as $patern) {
-				$result = $result && ((zbx_strstr($string, $patern) !== false) == $expectedResult);
+		foreach ($paterns as $patern) {
+			if ($expression['case_sensitive']) {
+				$tmp = ((zbx_strstr($string, $patern) !== false) == $expectedResult);
 			}
-		}
-		else {
-			foreach ($paterns as $patern) {
-				$result = $result && ((zbx_stristr($string, $patern) !== false) == $expectedResult);
+			else {
+				$tmp = ((zbx_stristr($string, $patern) !== false) == $expectedResult);
+			}
+
+			if ($expression['expression_type'] == EXPRESSION_TYPE_ANY_INCLUDED && $tmp) {
+				return true;
+			}
+			else {
+				$result = ($result && $tmp);
 			}
 		}
 
