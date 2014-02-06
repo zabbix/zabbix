eaab85de5e60365cb85974afb19ed3c144ffa8b5
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-02-06 11:41:59 +0000

    .......... [ZBX-7693] created dev brach
    git-svn-id: svn://svn.zabbix.com/branches/dev@42336 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-7693-1.8/src/zabbix_server/httppoller/httpmacro.h
@@ -0,0 +1,25 @@
+/*
+** ZABBIX
+** Copyright (C) 2000-2005 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+
+#ifndef ZABBIX_HTTPMACRO_H
+#define ZABBIX_HTTPMACRO_H
+
+void	http_substitute_macros(DB_HTTPTEST *httptest, char *data, int data_max_len);
+
+#endif
