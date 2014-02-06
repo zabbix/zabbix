ef25b2b1c6b2be5f267ce3fa8a8c916f97bfa750
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-02-06 11:17:23 +0000

    .......... [ZBX-7693] removed unnecessary branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@42327 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7693-2.2/upgrades/dbpatches/1.4/mysql/patch/dchecks.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE dchecks (
-        dcheckid                bigint unsigned         DEFAULT '0'     NOT NULL,
-        druleid         bigint unsigned         DEFAULT '0'     NOT NULL,
-        type            integer         DEFAULT '0'     NOT NULL,
-        key_            varchar(255)            DEFAULT '0'     NOT NULL,
-        snmp_community  varchar(255)            DEFAULT '0'     NOT NULL,
-        ports           varchar(255)            DEFAULT '0'     NOT NULL,
-        PRIMARY KEY (dcheckid)
-) ENGINE=InnoDB;
