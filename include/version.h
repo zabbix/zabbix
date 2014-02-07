9b862464e1edf7be77cce6beed5929dffa7d2a7c
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-02-07 14:20:10 +0000

    .......... [ZBXNEXT-826] updated release date
    git-svn-id: svn://svn.zabbix.com/branches/2.2@42430 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/version.h
+++ include/version.h
@@ -24,7 +24,7 @@
 #define ZBX_STR(str)	ZBX_STR2(str)
 
 #define APPLICATION_NAME	"Zabbix Agent"
-#define ZABBIX_REVDATE		"04 February 2014"
+#define ZABBIX_REVDATE		"07 February 2014"
 #define ZABBIX_VERSION_MAJOR	2
 #define ZABBIX_VERSION_MINOR	2
 #define ZABBIX_VERSION_PATCH	2
