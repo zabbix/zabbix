b473687222e5ed0c8d1e34cc03fbe27b23631783
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-02-10 12:22:53 +0000

    ..F....... [ZBX-7773] fixed jquery buttonset initializing
    git-svn-id: svn://svn.zabbix.com/branches/dev@42461 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7773/frontends/php/include/classes/class.cdiv.php
+++ ZBX-7773/frontends/php/include/classes/class.cdiv.php
@@ -24,7 +24,7 @@ class CDiv extends CTag {
 	public function __construct($items = null, $class = null, $id = null) {
 		parent::__construct('div', 'yes');
 		$this->attr('class', $class);
-		if (!empty($id)) {
+		if ($id) {
 			$this->attr('id', zbx_formatDomId($id));
 		}
 		$this->addItem($items);
@@ -42,7 +42,8 @@ class CDiv extends CTag {
 		$this->setAttribute('class', $this->getAttribute('class').' jqueryinputset');
 		if (!defined('ZBX_JQUERY_INPUTSET')) {
 			define('ZBX_JQUERY_INPUTSET', true);
-			zbx_add_post_js('setTimeout(function() { jQuery("div.jqueryinputset").buttonset(); }, 10);');
+
+			zbx_add_post_js('jQuery("div.jqueryinputset").buttonset();');
 		}
 
 		return $this;
