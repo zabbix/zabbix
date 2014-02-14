1b5edda7c769f632cdfa5df7b4df472da81162fb
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2014-02-14 12:29:28 +0000

    ........S. [ZBX-5789] removed unused fields graph_discovery.graphdiscoveryid from server code

--- src/libs/zbxdbhigh/db.c
+++ src/libs/zbxdbhigh/db.c
@@ -1031,8 +1031,7 @@ zbx_uint64_t	DBget_maxid_num(const char *tablename, int num)
 			0 == strcmp(tablename, "dhosts") ||
 			0 == strcmp(tablename, "alerts") ||
 			0 == strcmp(tablename, "escalations") ||
-			0 == strcmp(tablename, "autoreg_host") ||
-			0 == strcmp(tablename, "graph_discovery"))
+			0 == strcmp(tablename, "autoreg_host"))
 		return DCget_nextid(tablename, num);
 
 	return DBget_nextid(tablename, num);
