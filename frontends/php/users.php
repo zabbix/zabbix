9dc3fd86726182581807e9779cbf0ca00aa36cdc
eduards <eduards@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2014-02-17 09:23:47 +0000

    ..F....... [ZBX-7786] fixed cell limit formating with ellipsis
    git-svn-id: svn://svn.zabbix.com/trunk@42723 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/users.php
+++ frontends/php/users.php
@@ -332,7 +332,8 @@ if (!empty($_REQUEST['form'])) {
 }
 else {
 	$data = array(
-		'displayNodes' => is_array(get_current_nodeid())
+		'displayNodes' => is_array(get_current_nodeid()),
+		'config' => $config
 	);
 
 	// get user groups
