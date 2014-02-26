f15dc7d90735192212530fe160b3a2363a6b214f
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-02-26 12:00:31 +0000

    .......... [ZBXNEXT-421] Created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@43010 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-421/frontends/php/auditacts.php
@@ -0,0 +1,177 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+require_once dirname(__FILE__).'/include/config.inc.php';
+require_once dirname(__FILE__).'/include/audit.inc.php';
+require_once dirname(__FILE__).'/include/actions.inc.php';
+require_once dirname(__FILE__).'/include/users.inc.php';
+
+$page['title'] = _('Action log');
+$page['file'] = 'auditacts.php';
+$page['hist_arg'] = array();
+$page['scripts'] = array('class.calendar.js', 'gtlc.js');
+$page['type'] = detect_page_type(PAGE_TYPE_HTML);
+
+require_once dirname(__FILE__).'/include/page_header.php';
+
+// VAR	TYPE	OPTIONAL	FLAGS	VALIDATION	EXCEPTION
+$fields = array(
+	// filter
+	'filter_rst' =>	array(T_ZBX_INT, O_OPT, P_SYS,	IN('0,1'),	null),
+	'filter_set' =>	array(T_ZBX_STR, O_OPT, P_SYS,	null,		null),
+	'alias' =>		array(T_ZBX_STR, O_OPT, P_SYS,	null,		null),
+	'period' =>		array(T_ZBX_INT, O_OPT, null,	null,		null),
+	'dec' =>		array(T_ZBX_INT, O_OPT, null,	null,		null),
+	'inc' =>		array(T_ZBX_INT, O_OPT, null,	null,		null),
+	'left' =>		array(T_ZBX_INT, O_OPT, null,	null,		null),
+	'right' =>		array(T_ZBX_INT, O_OPT, null,	null,		null),
+	'stime' =>		array(T_ZBX_STR, O_OPT, null,	null,		null),
+	// ajax
+	'filterState' => array(T_ZBX_INT, O_OPT, P_ACT, null,		null),
+	'favobj' =>		array(T_ZBX_STR, O_OPT, P_ACT,	null,		null),
+	'favid' =>		array(T_ZBX_INT, O_OPT, P_ACT,	null,		null)
+);
+check_fields($fields);
+
+/*
+ * Ajax
+ */
+if (hasRequest('filterState')) {
+	CProfile::update('web.auditacts.filter.state', getRequest('filterState'), PROFILE_TYPE_INT);
+}
+if (isset($_REQUEST['favobj'])) {
+	// saving fixed/dynamic setting to profile
+	if ($_REQUEST['favobj'] == 'timelinefixedperiod') {
+		if (isset($_REQUEST['favid'])) {
+			CProfile::update('web.auditacts.timelinefixed', $_REQUEST['favid'], PROFILE_TYPE_INT);
+		}
+	}
+}
+
+if ($page['type'] == PAGE_TYPE_JS || $page['type'] == PAGE_TYPE_HTML_BLOCK) {
+	require_once dirname(__FILE__).'/include/page_footer.php';
+	exit;
+}
+
+/*
+ * Filter
+ */
+$_REQUEST['alias'] = isset($_REQUEST['filter_rst'])
+	? ''
+	: get_request('alias', CProfile::get('web.auditacts.filter.alias', ''));
+
+if (isset($_REQUEST['filter_set']) || isset($_REQUEST['filter_rst'])) {
+	CProfile::update('web.auditacts.filter.alias', $_REQUEST['alias'], PROFILE_TYPE_STR);
+}
+
+/*
+ * Display
+ */
+$effectivePeriod = navigation_bar_calc('web.auditacts.timeline', 0, true);
+
+$data = array(
+	'stime' => getRequest('stime'),
+	'alias' => getRequest('alias'),
+	'users' => array(),
+	'alerts' => array()
+);
+
+if ($data['alias']) {
+	$data['users'] = API::User()->get(array(
+		'output' => array('userid', 'alias', 'name', 'surname'),
+		'filter' => array('alias' => $data['alias']),
+		'preservekeys' => true
+	));
+}
+
+if (!$data['alias'] || $data['users']) {
+	$userIds = array_keys($data['users']);
+
+	$from = zbxDateToTime($data['stime']);
+	$till = $from + $effectivePeriod;
+
+	// fetch alerts for different objects and sources and combine them in a single stream
+	foreach (eventSourceObjects() as $eventSource) {
+		$data['alerts'] = array_merge($data['alerts'], API::Alert()->get(array(
+			'output' => API_OUTPUT_EXTEND,
+			'selectMediatypes' => API_OUTPUT_EXTEND,
+			'userids' => $userIds ? $userIds : null,
+			'time_from' => $from,
+			'time_till' => $till,
+			'eventsource' => $eventSource['source'],
+			'eventobject' => $eventSource['object'],
+			'limit' => $config['search_limit'] + 1
+		)));
+	}
+
+	CArrayHelper::sort($data['alerts'], array(
+		array('field' => 'alertid', 'order' => ZBX_SORT_DOWN)
+	));
+
+	$data['alerts'] = array_slice($data['alerts'], 0, $config['search_limit'] + 1);
+
+	// get users
+	if (!$data['alias']) {
+		$data['users'] = API::User()->get(array(
+			'output' => array('userid', 'alias', 'name', 'surname'),
+			'userids' => zbx_objectValues($data['alerts'], 'userid'),
+			'preservekeys' => true
+		));
+	}
+
+	// get first alert clock
+	$firstAlert = DBfetch(DBselect(
+		'SELECT MIN(a.clock) AS clock'.
+		' FROM alerts a'.
+		' WHERE '.dbConditionInt('a.userid', array_keys($data['users']))
+	));
+
+	$minStartTime = $firstAlert['clock'];
+}
+else {
+	$minStartTime = ZBX_MAX_PERIOD;
+}
+
+// padding
+$data['paging'] = getPagingLine($data['alerts']);
+
+// get actions names
+if ($data['alerts']) {
+	$data['actions'] = API::Action()->get(array(
+		'output' => array('actionid	', 'name'),
+		'actionids' => array_unique(zbx_objectValues($data['alerts'], 'actionid')),
+		'preservekeys' => true
+	));
+}
+
+// timeline
+$data['timeline'] = array(
+	'period' => $effectivePeriod,
+	'starttime' => date(TIMESTAMP_FORMAT, $minStartTime),
+	'usertime' => $data['stime'] ? date(TIMESTAMP_FORMAT, zbxDateToTime($data['stime']) + $effectivePeriod) : null
+);
+
+// render view
+$auditView = new CView('administration.auditacts.list', $data);
+$auditView->render();
+$auditView->show();
+
+require_once dirname(__FILE__).'/include/page_footer.php';
---
+++ ZBXNEXT-421/frontends/php/include/classes/import/formatters/C20ImportFormatter.php
@@ -0,0 +1,437 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+/**
+ * Import formatter for version 2.0
+ */
+class C20ImportFormatter extends CImportFormatter {
+
+	public function getGroups() {
+		if (!isset($this->data['groups'])) {
+			return array();
+		}
+		return array_values($this->data['groups']);
+	}
+
+	public function getTemplates() {
+		$templatesData = array();
+
+		if (!empty($this->data['templates'])) {
+			foreach ($this->data['templates'] as $template) {
+				$template = $this->renameData($template, array('template' => 'host'));
+
+				CArrayHelper::convertFieldToArray($template, 'templates');
+				if (empty($template['templates'])) {
+					unset($template['templates']);
+				}
+				CArrayHelper::convertFieldToArray($template, 'macros');
+				CArrayHelper::convertFieldToArray($template, 'groups');
+
+				CArrayHelper::convertFieldToArray($template, 'screens');
+				if (!empty($template['screens'])) {
+					foreach ($template['screens'] as &$screen) {
+						$screen = $this->renameData($screen, array('screen_items' => 'screenitems'));
+					}
+					unset($screen);
+				}
+
+
+				$templatesData[] = CArrayHelper::getByKeys($template, array(
+					'groups', 'macros', 'screens', 'templates', 'host', 'status', 'name'
+				));
+			}
+		}
+
+		return $templatesData;
+	}
+
+	public function getHosts() {
+		$hostsData = array();
+
+		if (!empty($this->data['hosts'])) {
+			foreach ($this->data['hosts'] as $host) {
+				$host = $this->renameData($host, array('proxyid' => 'proxy_hostid'));
+
+				CArrayHelper::convertFieldToArray($host, 'interfaces');
+				if (!empty($host['interfaces'])) {
+					foreach ($host['interfaces'] as $inum => $interface) {
+						$host['interfaces'][$inum] = $this->renameData($interface, array('default' => 'main'));
+					}
+				}
+
+				CArrayHelper::convertFieldToArray($host, 'templates');
+				if (empty($host['templates'])) {
+					unset($host['templates']);
+				}
+				CArrayHelper::convertFieldToArray($host, 'macros');
+				CArrayHelper::convertFieldToArray($host, 'groups');
+
+				if (!empty($host['inventory']) && isset($host['inventory']['inventory_mode'])) {
+					$host['inventory_mode'] = $host['inventory']['inventory_mode'];
+					unset($host['inventory']['inventory_mode']);
+				}
+				else {
+					$host['inventory_mode'] = HOST_INVENTORY_DISABLED;
+				}
+
+				$hostsData[] = CArrayHelper::getByKeys($host, array(
+					'inventory', 'proxy', 'groups', 'templates', 'macros', 'interfaces', 'host', 'status',
+					'ipmi_authtype', 'ipmi_privilege', 'ipmi_username', 'ipmi_password', 'name', 'inventory_mode'
+				));
+			}
+		}
+
+		return $hostsData;
+	}
+
+	public function getApplications() {
+		$applicationsData = array();
+
+		if (isset($this->data['hosts'])) {
+			foreach ($this->data['hosts'] as $host) {
+				if (!empty($host['applications'])) {
+					foreach ($host['applications'] as $application) {
+						$applicationsData[$host['host']][$application['name']] = $application;
+					}
+				}
+			}
+		}
+		if (isset($this->data['templates'])) {
+			foreach ($this->data['templates'] as $template) {
+				if (!empty($template['applications'])) {
+					foreach ($template['applications'] as $application) {
+						$applicationsData[$template['template']][$application['name']] = $application;
+					}
+				}
+			}
+		}
+
+		return $applicationsData;
+	}
+
+	public function getItems() {
+		$itemsData = array();
+
+		if (isset($this->data['hosts'])) {
+			foreach ($this->data['hosts'] as $host) {
+				if (!empty($host['items'])) {
+					foreach ($host['items'] as $item) {
+						// if a host item has the "Not supported" status, convert it to "Active"
+						if ($item['status'] == ITEM_STATUS_NOTSUPPORTED) {
+							$item['status'] = ITEM_STATUS_ACTIVE;
+						}
+
+						$item = $this->formatItem($item);
+						$itemsData[$host['host']][$item['key_']] = $item;
+					}
+				}
+			}
+		}
+		if (isset($this->data['templates'])) {
+			foreach ($this->data['templates'] as $template) {
+				if (!empty($template['items'])) {
+					foreach ($template['items'] as $item) {
+						$item = $this->formatItem($item);
+						$itemsData[$template['template']][$item['key_']] = $item;
+					}
+				}
+			}
+		}
+
+		return $itemsData;
+	}
+
+	public function getDiscoveryRules() {
+		$discoveryRulesData = array();
+
+		if (isset($this->data['hosts'])) {
+			foreach ($this->data['hosts'] as $host) {
+				if (!empty($host['discovery_rules'])) {
+					foreach ($host['discovery_rules'] as $item) {
+						// if a discovery rule has the "Not supported" status, convert it to "Active"
+						if ($item['status'] == ITEM_STATUS_NOTSUPPORTED) {
+							$item['status'] = ITEM_STATUS_ACTIVE;
+						}
+
+						$item = $this->formatDiscoveryRule($item);
+
+						$discoveryRulesData[$host['host']][$item['key_']] = $item;
+					}
+				}
+			}
+		}
+
+		if (isset($this->data['templates'])) {
+			foreach ($this->data['templates'] as $template) {
+				if (!empty($template['discovery_rules'])) {
+					foreach ($template['discovery_rules'] as $item) {
+						$item = $this->formatDiscoveryRule($item);
+
+						$discoveryRulesData[$template['template']][$item['key_']] = $item;
+					}
+				}
+			}
+		}
+
+		return $discoveryRulesData;
+	}
+
+	public function getGraphs() {
+		$graphsData = array();
+
+		if (isset($this->data['graphs']) && $this->data['graphs']) {
+			foreach ($this->data['graphs'] as $graph) {
+				$graph = $this->renameGraphFields($graph);
+
+				if (isset($graph['gitems']) && $graph['gitems']) {
+					$graph['gitems'] = array_values($graph['gitems']);
+				}
+
+				$graphsData[] = $graph;
+			}
+		}
+
+		return $graphsData;
+	}
+
+	public function getTriggers() {
+		$triggersData = array();
+
+		if (!empty($this->data['triggers'])) {
+			foreach ($this->data['triggers'] as $trigger) {
+				CArrayHelper::convertFieldToArray($trigger, 'dependencies');
+				$triggersData[] = $this->renameTriggerFields($trigger);
+
+			}
+		}
+
+		return $triggersData;
+	}
+
+	public function getImages() {
+		$imagesData = array();
+
+		if (!empty($this->data['images'])) {
+			foreach ($this->data['images'] as $image) {
+				$imagesData[] = $this->renameData($image, array('encodedImage' => 'image'));
+			}
+		}
+
+		return $imagesData;
+	}
+
+	public function getMaps() {
+		$mapsData = array();
+
+		if (!empty($this->data['maps'])) {
+			foreach ($this->data['maps'] as $map) {
+				CArrayHelper::convertFieldToArray($map, 'selements');
+				foreach ($map['selements'] as &$selement) {
+					CArrayHelper::convertFieldToArray($selement, 'urls');
+				}
+				unset($selement);
+
+				CArrayHelper::convertFieldToArray($map, 'links');
+				foreach ($map['links'] as &$link) {
+					CArrayHelper::convertFieldToArray($link, 'linktriggers');
+				}
+				unset($link);
+
+				CArrayHelper::convertFieldToArray($map, 'urls');
+
+				$mapsData[] = $map;
+			}
+		}
+
+		return $mapsData;
+	}
+
+	public function getScreens() {
+		$screensData = array();
+
+		if (!empty($this->data['screens'])) {
+			foreach ($this->data['screens'] as $screen) {
+				$screen = $this->renameData($screen, array('screen_items' => 'screenitems'));
+				CArrayHelper::convertFieldToArray($screen, 'screenitems');
+				$screensData[] = $screen;
+			}
+		}
+
+		return $screensData;
+	}
+
+	public function getTemplateScreens() {
+		$screensData = array();
+
+		if (isset($this->data['templates'])) {
+			foreach ($this->data['templates'] as $template) {
+				if (!empty($template['screens'])) {
+					foreach ($template['screens'] as $screen) {
+						$screen = $this->renameData($screen, array('screen_items' => 'screenitems'));
+						CArrayHelper::convertFieldToArray($screen, 'screenitems');
+						$screensData[$template['template']][$screen['name']] = $screen;
+					}
+				}
+			}
+		}
+
+		return $screensData;
+	}
+
+	/**
+	 * Format item.
+	 *
+	 * @param array $item
+	 *
+	 * @return array
+	 */
+	protected function formatItem(array $item) {
+		$item = $this->renameItemFields($item);
+
+		if (empty($item['applications'])) {
+			$item['applications'] = array();
+		}
+
+		return $item;
+	}
+
+	/**
+	 * Format discovery rule.
+	 *
+	 * @param array $discoveryRule
+	 *
+	 * @return array
+	 */
+	protected function formatDiscoveryRule(array $discoveryRule) {
+		$discoveryRule = $this->renameItemFields($discoveryRule);
+
+		if (!empty($discoveryRule['item_prototypes'])) {
+			foreach ($discoveryRule['item_prototypes'] as &$prototype) {
+				$prototype = $this->renameItemFields($prototype);
+				CArrayHelper::convertFieldToArray($prototype, 'applications');
+			}
+			unset($prototype);
+		}
+		else {
+			$discoveryRule['item_prototypes'] = array();
+		}
+
+		if (!empty($discoveryRule['trigger_prototypes'])) {
+			foreach ($discoveryRule['trigger_prototypes'] as &$trigger) {
+				$trigger = $this->renameTriggerFields($trigger);
+			}
+			unset($trigger);
+		}
+		else {
+			$discoveryRule['trigger_prototypes'] = array();
+		}
+
+		if (!empty($discoveryRule['graph_prototypes'])) {
+			foreach ($discoveryRule['graph_prototypes'] as &$graph) {
+				$graph = $this->renameGraphFields($graph);
+			}
+			unset($graph);
+		}
+		else {
+			$discoveryRule['graph_prototypes'] = array();
+		}
+
+		if (!empty($discoveryRule['host_prototypes'])) {
+			foreach ($discoveryRule['host_prototypes'] as &$hostPrototype) {
+				CArrayHelper::convertFieldToArray($hostPrototype, 'group_prototypes');
+				CArrayHelper::convertFieldToArray($hostPrototype, 'templates');
+			}
+			unset($hostPrototype);
+		}
+		else {
+			$discoveryRule['host_prototypes'] = array();
+		}
+
+		if (!empty($discoveryRule['filter'])) {
+			// array filter structure
+			if (is_array($discoveryRule['filter'])) {
+				CArrayHelper::convertFieldToArray($discoveryRule['filter'], 'conditions');
+			}
+			// string {#MACRO}:value syntax
+			else {
+				list ($filterMacro, $filterValue) = explode(':', $discoveryRule['filter']);
+				if ($filterMacro) {
+					$discoveryRule['filter'] = array(
+						'evaltype' => CONDITION_EVAL_TYPE_AND_OR,
+						'formula' => '',
+						'conditions' => array(
+							array(
+								'macro' => $filterMacro,
+								'value' => $filterValue,
+								'operator' => CONDITION_OPERATOR_REGEXP,
+							)
+						)
+					);
+				}
+				// if the macro is empty, ignore the filter
+				else {
+					unset($discoveryRule['filter']);
+				}
+			}
+		}
+
+		return $discoveryRule;
+	}
+
+	/**
+	 * Rename items, discovery rules, item prototypes fields.
+	 *
+	 * @param array $item
+	 *
+	 * @return array
+	 */
+	protected function renameItemFields(array $item) {
+		return $this->renameData($item, array('key' => 'key_', 'allowed_hosts' => 'trapper_hosts'));
+	}
+
+	/**
+	 * Rename triggers, trigger prototypes fields.
+	 *
+	 * @param array $trigger
+	 *
+	 * @return array
+	 */
+	protected function renameTriggerFields(array $trigger) {
+		$trigger = $this->renameData($trigger, array('description' => 'comments'));
+		return $this->renameData($trigger, array('name' => 'description', 'severity' => 'priority'));
+	}
+
+	/**
+	 * Rename graphs, graph prototypes fields.
+	 *
+	 * @param array $graph
+	 *
+	 * @return array
+	 */
+	protected function renameGraphFields(array $graph) {
+		return $this->renameData($graph, array(
+			'type' => 'graphtype',
+			'ymin_type_1' => 'ymin_type',
+			'ymax_type_1' => 'ymax_type',
+			'graph_items' => 'gitems'
+		));
+	}
+}
