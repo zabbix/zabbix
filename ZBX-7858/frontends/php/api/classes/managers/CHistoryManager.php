64993f00e1a31a91a44b1c3b1a3df6ee39e531dc
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-02-28 07:28:26 +0000

    .......... [ZBX-7858] updated to the latest /branches/2.2
    git-svn-id: svn://svn.zabbix.com/branches/dev@43077 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7858/frontends/php/api/classes/managers/CHistoryManager.php
+++ ZBX-7858/frontends/php/api/classes/managers/CHistoryManager.php
@@ -33,22 +33,63 @@ class CHistoryManager {
 	 * @return array    an array with items IDs as keys and arrays of history objects as values
 	 */
 	public function getLast(array $items, $limit = 1) {
-		$rs = array();
+		$periods = array(SEC_PER_HOUR, SEC_PER_DAY, SEC_PER_WEEK, SEC_PER_MONTH, null);
+		$periods = array(time() - strtotime('15.12.2013'));
+//		var_dump(time() - strtotime('14.12.2013'));
+
+		$items = zbx_toHash($items, 'itemid');
+
+		$values = array();
+		$startTime = time();
+		foreach ($periods as $period) {
+			$periodValues = $this->getLastByPeriod($items, $startTime, $limit, $period);
+			foreach ($periodValues as $value) {
+				$values[$value['itemid']][] = $value;
+			}
+
+			foreach ($values as $itemId => $itemValues) {
+				if (count($itemValues) == $limit) {
+					unset($items[$itemId]);
+				}
+			}
+
+			if (!$items) {
+				break;
+			}
+
+			$startTime -= $period;
+		}
+
+		return $values;
+	}
+
+	public function getLastByPeriod(array $items, $start, $limit = 1, $period = null) {
+		$queries = array();
 		foreach ($items as $item) {
 			$table = self::getTableName($item['value_type']);
-			$query = DBselect(
+			$queries[$table][] = DBaddLimit(
 				'SELECT *'.
 				' FROM '.$table.' h'.
 				' WHERE h.itemid='.zbx_dbstr($item['itemid']).
+					' AND h.clock<='.$start.
+					($period ? ' AND h.clock>'.($start - $period) : '').
 				' ORDER BY h.clock DESC',
 				$limit
 			);
-			while ($history = DBfetch($query)) {
-				$rs[$history['itemid']][] = $history;
+		}
+
+		$values = array();
+		foreach ($queries as $tableQueries) {
+			$chunks = array_chunk($tableQueries, 100);
+			foreach ($chunks as $queries) {
+				$query = DBunion($queries);
+				while ($history = DBfetch($query)) {
+					$values[] = $history;
+				}
 			}
 		}
 
-		return $rs;
+		return $values;
 	}
 
 	/**
