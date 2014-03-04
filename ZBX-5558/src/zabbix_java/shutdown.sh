1e09e5265ab45ecd02d5b596b3699a2bb76a6c8b
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-03-04 14:56:06 +0000

    ....IJ.... [ZBX-5558] made shutdown.sh script wait for the gateway to stop
    git-svn-id: svn://svn.zabbix.com/branches/dev@43186 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-5558/src/zabbix_java/shutdown.sh
+++ ZBX-5558/src/zabbix_java/shutdown.sh
@@ -8,7 +8,15 @@ if [ -n "$PID_FILE" ]; then
 		PID=`cat "$PID_FILE"`
 		if ps -p "$PID" > /dev/null 2>&1; then
 			kill `cat $PID_FILE`
-			exit 0
+			for i in 1 2 3 4 5; do
+				sleep 1
+				ps -p "$PID" > /dev/null 2>&1
+				if [ $? -ne 0 ]; then
+					exit 0
+				fi
+			done
+			echo "Zabbix Java Gateway did not stop"
+			exit 1
 		fi
 		rm -f "$PID_FILE"
 	fi
