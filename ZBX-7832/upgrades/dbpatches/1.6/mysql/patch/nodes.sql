7f11cc531f55295c67fefc0ade4499d21f8b54ad
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-03-12 11:27:23 +0000

    A.F....... [ZBX-7832] deleted discarded branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@43417 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7832/upgrades/dbpatches/1.6/mysql/patch/nodes.sql
+++
@@ -1,6 +0,0 @@
-alter table nodes modify ip varchar(39) DEFAULT '' NOT NULL;
-
-alter table nodes drop event_lastid;
-alter table nodes drop history_lastid;
-alter table nodes drop history_str_lastid;
-alter table nodes drop history_uint_lastid;
