19ef54a0f57161d31f6c1c8dea55f7ffa77ea2e6
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-03-12 10:57:32 +0000

    .......... [ZBX-7832] updated to the latest /branches/2.2
    git-svn-id: svn://svn.zabbix.com/branches/dev@43413 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7832-2/frontends/php/include/func.inc.php
+++ ZBX-7832-2/frontends/php/include/func.inc.php
@@ -2538,7 +2538,8 @@ function get_status() {
 				' WHERE i.status='.ITEM_STATUS_ACTIVE.
 				' AND i.hostid=h.hostid'.
 				' AND h.status='.HOST_STATUS_MONITORED.
-				' AND i.delay<>0'
+				' AND i.delay<>0'.
+				' AND i.flags<>'.ZBX_FLAG_DISCOVERY_PROTOTYPE
 	));
 	$status['qps_total'] = round($row['qps'], 2);
 
