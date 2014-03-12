f27dbb031898fc96cac80ff52e6f86b980aec51c
Mark Broyles <mbroyles@llnw.com>
2014-03-11 19:13:11 -0500

    [#MON-2836] Refactor circuit-cache function.

--- frontends/php/llnw/doc/example/zabbix-llnw.conf.php
+++ frontends/php/llnw/doc/example/zabbix-llnw.conf.php
@@ -50,4 +50,3 @@ include($base_dir.'toolbox.php');
 $db = new ezSQL_mysql($db_user, $db_pass, $db_name, $db_host);
 $ldb = new ezSQL_mysql($ldb_user, $ldb_pass, $ldb_name, $ldb_host);
 $cdb = new ezSQL_mysql($cdb_user, $cdb_pass, $cdb_name, $cdb_host);
-
