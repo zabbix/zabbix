7a50f812a577582a966bbb3877a46a3e8d79d7d2
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-03-21 12:20:22 +0000

    .......... [ZBX-7409] updated to the latest /branches/2.2
    git-svn-id: svn://svn.zabbix.com/branches/dev@43629 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7409/frontends/php/maintenance.php
+++ ZBX-7409/frontends/php/maintenance.php
@@ -507,16 +507,18 @@ if (!empty($data['form'])) {
 	$maintenanceView->show();
 }
 else {
-	// get maintenances
 	$sortfield = getPageSortField('name');
 	$sortorder = getPageSortOrder();
+
+	// get only maintenance IDs for paging
 	$options = array(
-		'output' => API_OUTPUT_EXTEND,
+		'output' => array('maintenanceid'),
 		'editable' => true,
 		'sortfield' => $sortfield,
 		'sortorder' => $sortorder,
 		'limit' => $config['search_limit'] + 1
 	);
+
 	if ($pageFilter->groupsSelected) {
 		if ($pageFilter->groupid > 0) {
 			$options['groupids'] = $pageFilter->groupid;
@@ -528,7 +530,16 @@ else {
 	else {
 		$options['groupids'] = array();
 	}
+
 	$data['maintenances'] = API::Maintenance()->get($options);
+	$data['paging'] = getPagingLine($data['maintenances'], array('maintenanceid'));
+
+	// get list of maintenances
+	$data['maintenances'] = API::Maintenance()->get(array(
+		'maintenanceids' => zbx_objectValues($data['maintenances'], 'maintenanceid'),
+		'output' => API_OUTPUT_EXTEND
+	));
+
 	foreach ($data['maintenances'] as $number => $maintenance) {
 		if ($maintenance['active_till'] < time()) {
 			$data['maintenances'][$number]['status'] = MAINTENANCE_STATUS_EXPIRED;
@@ -541,7 +552,7 @@ else {
 		}
 	}
 	order_result($data['maintenances'], $sortfield, $sortorder);
-	$data['paging'] = getPagingLine($data['maintenances'], array('maintenanceid'));
+
 	$data['pageFilter'] = $pageFilter;
 
 	// nodes
