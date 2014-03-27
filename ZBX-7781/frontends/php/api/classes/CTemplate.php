ab18d7c3ec4446685e9eb2ebec7f5d7dad7aa7aa
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-03-27 14:00:02 +0000

    .......... [ZBX-7781] updated to the latest /trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@43799 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7781/frontends/php/api/classes/CTemplate.php
+++ ZBX-7781/frontends/php/api/classes/CTemplate.php
@@ -495,7 +495,7 @@ class CTemplate extends CHostGeneral {
 
 			if (!preg_match('/^'.ZBX_PREG_HOST_FORMAT.'$/', $template['host'])) {
 				self::exception(ZBX_API_ERROR_PARAMETERS, _s(
-					'Incorrect characters used for Template name "%1$s".',
+					'Incorrect characters used for template name "%1$s".',
 					$template['host']
 				));
 			}
