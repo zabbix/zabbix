3d3833547664442c531e0f29d75f14f633429060
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-03-28 17:58:08 +0000

    .......... [RSM-1] epp_rsm_enc: allow passing a file name as sensitive data instead of reading it from STDIN
    git-svn-id: svn://svn.zabbix.com/branches/2.0.rsm@43858 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_rsm/enc.c
+++ src/zabbix_rsm/enc.c
@@ -10,29 +10,57 @@
 const char      *progname = NULL;
 const char	title_message[] = "Zabbix encrypter";
 const char	*help_message[] = {NULL};
-const char	usage_message[] = "<secretkey_enc_b64> <secretkey_salt_b64>\n"
+const char	usage_message[] = "<secretkey_enc_b64> <secretkey_salt_b64> [optional parameters]\n"
 		"Encrypt sensitive data using secret key.\n"
 		"Parameters:\n"
 		"    <secretkey_enc_b64>         secret key, encrypted with passphrase, base64-encoded\n"
 		"    <secretkey_salt_b64>        secret key salt, base64-encoded\n"
-		"Optional:\n"
-		"    <noprompt>                  do not print prompts when requesting passphrase and sensitive data";
+		"Optional parameters:\n"
+		"    -n                          do not print prompts when requesting passphrase and sensitive data\n"
+		"    -f <file>                   do not request sensitive data from STDIN, instead read it from file";
 
 int	main(int argc, char *argv[])
 {
-	const char	*secretkey_enc_b64, *secretkey_salt_b64, *prompt = 0;
+	const char	*secretkey_enc_b64, *secretkey_salt_b64, *prompt = "";
 	char		*sensdata_enc_b64 = NULL, *sensdata_salt_b64 = NULL, passphrase[128],
-			sensdata[RSM_EPP_SENSDATA_MAX], err[128];
+			sensdata[RSM_EPP_SENSDATA_MAX], *file = NULL, err[128];
+	int		i, noprompt = 0;
 
 	progname = get_program_name(argv[0]);
+	secretkey_enc_b64 = argv[1];
+	secretkey_salt_b64 = argv[2];
 
-	if (argc < 3)
+	if (3 > argc)
 	{
 		usage();
-		exit(1);
+		goto out;
 	}
 
-	if (3 == argc || 0 != strcmp("noprompt", argv[3]))
+	for (i = 3; i < argc; i++)
+	{
+		if (0 == strcmp("-n", argv[i]))
+		{
+			noprompt = 1;
+		}
+		else if (0 == strcmp("-f", argv[i]))
+		{
+			if (i == argc - 1)
+			{
+				usage();
+				goto out;
+			}
+
+			file = argv[++i];
+		}
+		else
+		{
+			printf("%s: invalid argument\n", argv[i]);
+			usage();
+			goto out;
+		}
+	}
+
+	if (0 == noprompt)
 		prompt = "Enter EPP passphrase: ";
 	if (SUCCEED != zbx_read_stdin(prompt, passphrase, sizeof(passphrase), err, sizeof(err)))
 	{
@@ -40,16 +68,42 @@ int	main(int argc, char *argv[])
 		goto out;
 	}
 
-	if (3 == argc || 0 != strcmp("noprompt", argv[3]))
-		prompt = "Enter EPP sensitive data to encrypt: ";
-	if (SUCCEED != zbx_read_stdin(prompt, sensdata, sizeof(sensdata), err, sizeof(err)))
+	if (NULL != file)
 	{
-		fprintf(stderr, "cannot get EPP sensitive data: %s\n", err);
-		goto out;
-	}
+		char	buf[128], *contents = NULL;
+		size_t	contents_alloc = 512, contents_offset = 0;
+		int	f, nbytes;
 
-	secretkey_enc_b64 = argv[1];
-	secretkey_salt_b64 = argv[2];
+		if (-1 == (f = zbx_open(file, O_RDONLY)))
+		{
+			fprintf(stderr, "cannot read file \"%s\": %s\n", file, strerror(errno));
+			goto out;
+		}
+
+		contents = zbx_malloc(contents, contents_alloc);
+		*contents = '\0';
+		while (0 < (nbytes = zbx_read(f, buf, sizeof(buf), "")))
+			zbx_strncpy_alloc(&contents, &contents_alloc, &contents_offset, buf, nbytes);
+
+		zbx_strlcpy(sensdata, contents, sizeof(sensdata));
+		zbx_free(contents);
+
+		if (-1 == nbytes)
+		{
+			fprintf(stderr, "an error occured while reading file \"%s\": %s", file, strerror(errno));
+			goto out;
+		}
+	}
+	else
+	{
+		if (0 == noprompt)
+			prompt = "Enter EPP sensitive data to encrypt: ";
+		if (SUCCEED != zbx_read_stdin(prompt, sensdata, sizeof(sensdata), err, sizeof(err)))
+		{
+			fprintf(stderr, "cannot get EPP sensitive data: %s\n", err);
+			goto out;
+		}
+	}
 
 	/* initialize the library */
 	if (SUCCEED != rsm_ssl_init())
