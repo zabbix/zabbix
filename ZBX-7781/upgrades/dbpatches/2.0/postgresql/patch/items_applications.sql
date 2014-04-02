86d7227ee44e116c94e465c7d6851bfb11381658
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-04-02 13:01:22 +0000

    .......... [ZBX-7781] removed merged branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@43982 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7781/upgrades/dbpatches/2.0/postgresql/patch/items_applications.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE ONLY items_applications ALTER itemappid DROP DEFAULT,
-				    ALTER applicationid DROP DEFAULT,
-				    ALTER itemid DROP DEFAULT;
-DROP INDEX items_applications_1;
-DELETE FROM items_applications WHERE NOT EXISTS (SELECT 1 FROM applications WHERE applications.applicationid=items_applications.applicationid);
-DELETE FROM items_applications WHERE NOT EXISTS (SELECT 1 FROM items WHERE items.itemid=items_applications.itemid);
-CREATE UNIQUE INDEX items_applications_1 ON items_applications (applicationid,itemid);
-ALTER TABLE ONLY items_applications ADD CONSTRAINT c_items_applications_1 FOREIGN KEY (applicationid) REFERENCES applications (applicationid) ON DELETE CASCADE;
-ALTER TABLE ONLY items_applications ADD CONSTRAINT c_items_applications_2 FOREIGN KEY (itemid) REFERENCES items (itemid) ON DELETE CASCADE;
