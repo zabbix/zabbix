66811b125ec09da6569cd7648d68dff929a9d5df
juris <juris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-04-07 06:42:55 +0000

    ...G...PS. [ZBXNEXT-2225] added commentary block in the Windows swap size calculation file explaining the reasoning behind the code
    git-svn-id: svn://svn.zabbix.com/branches/dev@44089 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2225/src/libs/zbxsysinfo/win32/swap.c
+++ ZBXNEXT-2225/src/libs/zbxsysinfo/win32/swap.c
@@ -38,6 +38,38 @@ int	SYSTEM_SWAP_SIZE(AGENT_REQUEST *request, AGENT_RESULT *result)
 	if (NULL != swapdev && '\0' != *swapdev && 0 != strcmp(swapdev, "all"))
 		return SYSINFO_RET_FAIL;
 
+	/***************************************************************************
+	 *                                                                         *
+	 * Due to the way Windows API functions report memory metrics,             *
+	 * it is impossible to accurately retrieve swap (virtual memory)           *
+	 * sizes as Windows either does not report them properly or                *
+	 * reports swap along with actual physical memory as page file             *
+	 * sizes. The only resolution that could be applied was calculatively      *
+	 * deducting the swap sizes knowing the page and physical memory           *
+	 * sizes.                                                                  *
+	 *                                                                         *
+	 * While developing this solution, it was found that in virtualized        *
+	 * environments Windows reports incorrect values for various memory        *
+	 * types. For example, the free memory size may be larger than the         *
+	 * total memory size of the system, or, under certain circumstances,       *
+	 * these functions may return negative values.                             *
+	 *                                                                         *
+	 * Taking these fallacious conditions into account, these calculations     *
+	 * guarantee that the available swap size is never larger than the total   *
+	 * available (if it is reported as such, it is lowered to the total        *
+	 * as there is a higher probability of that number staying consistent      *
+	 * than the other way around). In case of the system reporting negative    *
+	 * values, the appropriate metric is returned as 0.                        *
+	 *                                                                         *
+	 * The returned values are not guaranteed to be accurate and may report    *
+	 * inaccurate results which may depend on the system and environment.      *
+	 * In a proper system environment, this should logically work just as any  *
+	 * arithmetic calculation, but the problem lies within the way Windows     *
+	 * itself operates. Although this guestimate should work most of the time, *
+	 * there are no guarantees that it will.                                   *
+	 *                                                                         *
+	 ***************************************************************************/
+
 	if (NULL != zbx_GlobalMemoryStatusEx)
 	{
 		ms_ex.dwLength = sizeof(MEMORYSTATUSEX);
