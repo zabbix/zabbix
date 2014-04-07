zabbix/zabbix

commit 9cae71083b850584c7ea4ddbd9a81729d52d61fd

A.F....... [ZBXNEXT-2017] dropped support of output refer for all API get methods
Before the fix:
- Get method "output" parameter supported the "refer" value, which was unintuitive and unreliable. 

After the fix:
- Output "refer" is no longer supported. All of its usages are replaced with arrays of specific fields. No backward compatibility is provided.

git-svn-id: svn://svn.zabbix.com/trunk@41620 97f52cf1-0a1b-0410-bd0e-c28be96e8082

frontends/php/srv_status.php
@@ -54,6 +54,7 @@ check_fields($fields);
 
 if (isset($_REQUEST['serviceid']) && isset($_REQUEST['showgraph'])) {
 	$service = API::Service()->get(array(
+		'output' => array('serviceid'),
 		'serviceids' => $_REQUEST['serviceid'],
 		'preservekeys' => true
 	));

commit 1e747d8095ec1161b14eca85a3aeeb0dd058eba8

..F....... [ZBXNEXT-2017] fixed IT service availability graphs not being displayed; broken in r41620
git-svn-id: svn://svn.zabbix.com/trunk@44112 97f52cf1-0a1b-0410-bd0e-c28be96e8082

frontends/php/srv_status.php
@@ -55,13 +55,13 @@ check_fields($fields);
 if (isset($_REQUEST['serviceid']) && isset($_REQUEST['showgraph'])) {
 	$service = API::Service()->get(array(
 		'output' => array('serviceid'),
-		'serviceids' => $_REQUEST['serviceid'],
-		'preservekeys' => true
+		'serviceids' => getRequest('serviceid')
 	));
+	$service = reset($service);
 
 	if ($service) {
 		$table = new CTable(null, 'chart');
-		$table->addRow(new CImg('chart5.php?serviceid='.key($service).url_param('path')));
+		$table->addRow(new CImg('chart5.php?serviceid='.$service['serviceid'].url_param('path')));
 		$table->show();
 	}
 	else {