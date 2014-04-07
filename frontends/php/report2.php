a5e56a4ceb537cb6cc182b2a95a1c2f0957ed8c4
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-04-07 10:10:41 +0000

    ..F....... [ZBX-7864] fixed filter by host group in "Availability reports" page
    git-svn-id: svn://svn.zabbix.com/trunk@44117 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/report2.php
+++ frontends/php/report2.php
@@ -304,7 +304,8 @@ elseif (isset($_REQUEST['filter_hostid'])) {
 		$hostGroups = API::HostGroup()->get(array(
 			'output' => array('groupid', 'name'),
 			'hostids' => $triggerOptions['hostids'],
-			'monitored_hosts' => true
+			'monitored_hosts' => true,
+			'preservekeys' => true
 		));
 		order_result($hostGroups, 'name');
 
@@ -314,6 +315,7 @@ elseif (isset($_REQUEST['filter_hostid'])) {
 				get_node_name_by_elid($hostGroup['groupid'], null, NAME_DELIMITER).$hostGroup['name']
 			);
 		}
+
 		if (isset($_REQUEST['hostgroupid']) && !isset($hostGroups[$_REQUEST['hostgroupid']])) {
 			unset($triggerOptions['groupids']);
 		}
