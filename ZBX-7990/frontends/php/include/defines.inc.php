9e5f01a7e53b4df5bdf28ffe93353b62ff25e0d9
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-04-08 05:39:25 +0000

    .......... [ZBX-7990] updated to the latest 2.2
    git-svn-id: svn://svn.zabbix.com/branches/dev@44174 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7990/frontends/php/include/defines.inc.php
+++ ZBX-7990/frontends/php/include/defines.inc.php
@@ -18,7 +18,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-define('ZABBIX_VERSION',     '2.2.3rc2');
+define('ZABBIX_VERSION',     '2.2.3');
 define('ZABBIX_API_VERSION', '2.2.3');
 define('ZABBIX_DB_VERSION',	 2020000);
 
@@ -126,6 +126,7 @@ define('T_ZBX_IP',			4);
 define('T_ZBX_CLR',			5);
 define('T_ZBX_IP_RANGE',	7);
 define('T_ZBX_INT_RANGE',	8);
+define('T_ZBX_DBL_BIG',		9);
 
 define('O_MAND',	0);
 define('O_OPT',		1);
--- ZBX-7990/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ ZBX-7990/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	public static final String REVISION_DATE = "3 April 2014";
+	public static final String REVISION_DATE = "7 April 2014";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "2.2.3rc2";
+	public static final String VERSION = "2.2.3";
 
 	public static void printVersion()
 	{
