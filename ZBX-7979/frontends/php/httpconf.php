1367151a270d7a00e9addffb03f2ba14d1b54e7f
krists <krists@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-04-14 08:07:30 +0000

    ..F....... [ZBX-7979] Add "del_history" (with title "Clear history and trends") button to scenario form. Add code to handle "del_history" button action.
    git-svn-id: svn://svn.zabbix.com/branches/dev@44352 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7979/frontends/php/httpconf.php
+++ ZBX-7979/frontends/php/httpconf.php
@@ -60,6 +60,7 @@ $fields = array(
 	// actions
 	'go'				=> array(T_ZBX_STR, O_OPT, P_SYS|P_ACT, null,			null),
 	'clone'				=> array(T_ZBX_STR, O_OPT, P_SYS|P_ACT, null,			null),
+	'del_history'		=> array(T_ZBX_STR, O_OPT, P_SYS|P_ACT, null,			null),
 	'save'				=> array(T_ZBX_STR, O_OPT, P_SYS|P_ACT, null,			null),
 	'delete'			=> array(T_ZBX_STR, O_OPT, P_SYS|P_ACT, null,			null),
 	'cancel'			=> array(T_ZBX_STR, O_OPT, P_SYS,	null,				null),
@@ -140,6 +141,35 @@ elseif (isset($_REQUEST['clone']) && isset($_REQUEST['httptestid'])) {
 	unset($_REQUEST['httptestid']);
 	unset($_REQUEST['templated']);
 	$_REQUEST['form'] = 'clone';
+}
+elseif (isset($_REQUEST['del_history']) && isset($_REQUEST['httptestid'])) {
+	$dbResult = true;
+
+	DBstart();
+
+	$httptestId = getRequest('httptestid');
+
+	$httpTest = get_httptest_by_httptestid($httptestId);
+	if ($httpTest) {
+		$dbResult &= delete_history_by_httptestid($httptestId);
+		if ($dbResult) {
+
+			$dbResult &= DBexecute('UPDATE httptest SET nextcheck=0 WHERE httptestid='.zbx_dbstr($httptestId));
+
+			$host = DBfetch(DBselect(
+				'SELECT h.host FROM hosts h,httptest ht WHERE ht.hostid=h.hostid AND ht.httptestid='.zbx_dbstr($httptestId)
+			));
+
+			add_audit(AUDIT_ACTION_UPDATE, AUDIT_RESOURCE_SCENARIO,
+				'Scenario ['.$httpTest['name'].'] ['.$httptestId.'] Host ['.$host['host'].'] history cleared'
+			);
+		}
+	}
+
+	$dbResult = DBend($dbResult);
+
+	show_messages($dbResult, _('History cleared'), _('Cannot clear history'));
+	clearCookies($dbResult, getRequest('hostid'));
 }
 elseif (isset($_REQUEST['save'])) {
 	try {
