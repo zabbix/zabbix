bf006ecd18544e7b02907f96263f32a88563d33b
eduards <eduards@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-04-15 07:49:43 +0000

    .......... [ZBX-7809] Created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@44414 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-7809/upgrades/dbpatches/2.0/postgresql/patch/proxy_autoreg_host.sql
@@ -0,0 +1,3 @@
+ALTER TABLE ONLY proxy_autoreg_host ADD listen_ip varchar(39) DEFAULT '' NOT NULL,
+				    ADD listen_port integer DEFAULT '0' NOT NULL,
+				    ADD listen_dns varchar(64) DEFAULT '' NOT NULL;
