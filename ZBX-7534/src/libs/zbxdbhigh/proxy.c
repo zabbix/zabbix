8dc6af4ce233d545ecbe4a62e880e45b3a970dc4
nikolaj <nikolaj@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-04-16 09:03:44 +0000

    .......PS. [ZBX-7534] init value to null
    git-svn-id: svn://svn.zabbix.com/branches/dev@44500 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7534/src/libs/zbxdbhigh/proxy.c
+++ ZBX-7534/src/libs/zbxdbhigh/proxy.c
@@ -2402,7 +2402,7 @@ void	process_dhis_data(struct zbx_json_parse *jp)
 	int			port, status, ret;
 	const char		*p = NULL;
 	char			last_ip[INTERFACE_IP_LEN_MAX], ip[INTERFACE_IP_LEN_MAX], key_[ITEM_KEY_LEN * 4 + 1],
-				tmp[MAX_STRING_LEN], *value, dns[INTERFACE_DNS_LEN_MAX];
+				tmp[MAX_STRING_LEN], *value = NULL, dns[INTERFACE_DNS_LEN_MAX];
 	time_t			now, hosttime, itemtime;
 	size_t			value_alloc = 0;
 
