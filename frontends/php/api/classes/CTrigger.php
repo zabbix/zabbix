5dbf7afa23d15097cd932879fe1bfd5f4d725812
Eduards Samersovs <git-no-reply@zabbix.com>
2014-04-16 11:59:13 +0000

    ..F....... [ZBX-6596] fixed trigger sorting by hostname

--- frontends/php/api/classes/CTrigger.php
+++ frontends/php/api/classes/CTrigger.php
@@ -1782,11 +1782,24 @@ class CTrigger extends CTriggerGeneral {
 	protected function applyQuerySortOptions($tableName, $tableAlias, array $options, array $sqlParts) {
 		$sqlParts = parent::applyQuerySortOptions($tableName, $tableAlias, $options, $sqlParts);
 
-		// if the parent method call adds a hostname column to the select clause, replace it with "h.name"
-		// since column "t.hostname" doesn't exist
-		if (!zbx_empty($options['sortfield'])
-				&& ($options['sortfield'] === 'hostname' || isset($sqlParts['select']['hostname']))) {
-			$sqlParts['select']['hostname'] = 'h.name as hostname';
+		if (!zbx_empty($options['sortfield'])) {
+			// if the parent method call adds a hostname column to the select clause, replace it with "h.name"
+			// since column "t.hostname" doesn't exist
+			if (isset($sqlParts['select']['hostname'])) {
+				$sqlParts['select']['hostname'] = 'h.name as hostname';
+			}
+			else {
+				$options['sortfield'] = is_array($options['sortfield'])
+					? array_unique($options['sortfield'])
+					: array($options['sortfield']);
+
+				foreach ($options['sortfield'] as $sortfield) {
+					if ($sortfield === 'hostname') {
+						$sqlParts['select']['hostname'] = 'h.name as hostname';
+						break;
+					}
+				}
+			}
 		}
 
 		return $sqlParts;
