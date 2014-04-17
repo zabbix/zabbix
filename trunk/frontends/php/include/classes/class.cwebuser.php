959e4923e805c59cf0929a0f13cf1a196e0c1299
Krists Krigers <git-no-reply@zabbix.com>
2014-04-17 07:47:12 +0000

    .......... [ZBX-7941] created new development branch

---
+++ trunk/frontends/php/include/classes/class.cwebuser.php
@@ -0,0 +1,165 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CWebUser {
+
+	public static $data = null;
+
+	public static function login($login, $password) {
+		try {
+			self::setDefault();
+
+			self::$data = API::User()->login(array(
+				'user' => $login,
+				'password' => $password,
+				'userData' => true
+			));
+
+			if (!self::$data) {
+				throw new Exception();
+			}
+
+			if (self::$data['gui_access'] == GROUP_GUI_ACCESS_DISABLED) {
+				error(_('GUI access disabled.'));
+				throw new Exception();
+			}
+
+			if (empty(self::$data['url'])) {
+				self::$data['url'] = CProfile::get('web.menu.view.last', 'index.php');
+			}
+
+			$result = (bool) self::$data;
+
+			if (isset(self::$data['attempt_failed']) && self::$data['attempt_failed']) {
+				CProfile::init();
+				CProfile::update('web.login.attempt.failed', self::$data['attempt_failed'], PROFILE_TYPE_INT);
+				CProfile::update('web.login.attempt.ip', self::$data['attempt_ip'], PROFILE_TYPE_STR);
+				CProfile::update('web.login.attempt.clock', self::$data['attempt_clock'], PROFILE_TYPE_INT);
+				$result &= CProfile::flush();
+			}
+
+			// remove guest session after successful login
+			$result &= DBexecute('DELETE FROM sessions WHERE sessionid='.zbx_dbstr(get_cookie('zbx_sessionid')));
+
+			if ($result) {
+				zbx_setcookie('zbx_sessionid', self::$data['sessionid'],
+					self::$data['autologin'] ? time() + SEC_PER_DAY * 31 : 0
+				);
+
+				add_audit_ext(AUDIT_ACTION_LOGIN, AUDIT_RESOURCE_USER, self::$data['userid'], '', null, null, null);
+			}
+
+			return $result;
+		}
+		catch (Exception $e) {
+			self::setDefault();
+			return false;
+		}
+	}
+
+	public static function logout() {
+		self::$data['sessionid'] = get_cookie('zbx_sessionid');
+		self::$data = API::User()->logout();
+		zbx_unsetcookie('zbx_sessionid');
+	}
+
+	public static function checkAuthentication($sessionid) {
+		try {
+			if ($sessionid !== null) {
+				self::$data = API::User()->checkAuthentication(array($sessionid));
+			}
+
+			if ($sessionid === null || empty(self::$data)) {
+				self::setDefault();
+				self::$data = API::User()->login(array(
+					'user' => ZBX_GUEST_USER,
+					'password' => '',
+					'userData' => true
+				));
+
+				if (empty(self::$data)) {
+					clear_messages(1);
+					throw new Exception();
+				}
+				$sessionid = self::$data['sessionid'];
+			}
+
+			if (self::$data['gui_access'] == GROUP_GUI_ACCESS_DISABLED) {
+				throw new Exception();
+			}
+
+			zbx_setcookie('zbx_sessionid', $sessionid, self::$data['autologin'] ? time() + SEC_PER_DAY * 31 : 0);
+
+			return true;
+		}
+		catch (Exception $e) {
+			self::setDefault();
+			return false;
+		}
+	}
+
+	public static function setDefault() {
+		self::$data = array(
+			'alias' => ZBX_GUEST_USER,
+			'userid' => 0,
+			'lang' => 'en_gb',
+			'type' => '0'
+		);
+	}
+
+	/**
+	 * Returns the type of the current user.
+	 *
+	 * @static
+	 *
+	 * @return int
+	 */
+	public static function getType() {
+		return self::$data['type'];
+	}
+
+	/**
+	 * Returns true if debug mode is enabled.
+	 *
+	 * @return bool
+	 */
+	public static function getDebugMode() {
+		return (self::$data['debug_mode']);
+	}
+
+	/**
+	 * Returns true if the current user is logged in.
+	 *
+	 * @return bool
+	 */
+	public static function isLoggedIn() {
+		return (self::$data['userid']);
+	}
+
+	/**
+	 * Returns true if the user is not logged in or logged in as Guest.
+	 *
+	 * @return bool
+	 */
+	public static function isGuest() {
+		return (self::$data['alias'] == ZBX_GUEST_USER);
+	}
+}
---
+++ trunk/frontends/php/include/views/inventory.host.list.php
@@ -0,0 +1,115 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+$hostInventoryWidget = new CWidget();
+
+$rForm = new CForm('get');
+$rForm->addItem(array(_('Group'), SPACE, $this->data['pageFilter']->getGroupsCB()));
+$hostInventoryWidget->addPageHeader(_('HOST INVENTORY'), SPACE);
+$hostInventoryWidget->addHeader(_('Hosts'), $rForm);
+
+$filterTable = new CTable('', 'filter');
+// getting inventory fields to make a drop down
+$inventoryFields = getHostInventories(true); // 'true' means list should be ordered by title
+$inventoryFieldsComboBox = new CComboBox('filter_field', $this->data['filterField']);
+foreach ($inventoryFields as $inventoryField) {
+	$inventoryFieldsComboBox->addItem(
+		$inventoryField['db_field'],
+		$inventoryField['title']
+	);
+}
+$exactComboBox = new CComboBox('filter_exact', $this->data['filterExact']);
+$exactComboBox->addItem('0', _('like'));
+$exactComboBox->addItem('1', _('exactly'));
+$filterTable->addRow(array(
+	array(
+		array(bold(_('Field')), SPACE, $inventoryFieldsComboBox),
+		array(
+			$exactComboBox,
+			new CTextBox('filter_field_value', $this->data['filterFieldValue'], 20)
+		),
+	),
+), 'host-inventories');
+
+$filter = new CButton('filter', _('Filter'),
+	"javascript: create_var('zbx_filter', 'filter_set', '1', true); chkbxRange.clearSelectedOnFilterChange();"
+);
+$filter->useJQueryStyle('main');
+
+$reset = new CButton('reset', _('Reset'), "javascript: clearAllForm('zbx_filter');");
+$reset->useJQueryStyle();
+
+$divButtons = new CDiv(array($filter, SPACE, $reset));
+$divButtons->setAttribute('style', 'padding: 4px 0px;');
+
+$footerCol = new CCol($divButtons, 'controls');
+
+$filterTable->addRow($footerCol);
+
+$filterForm = new CForm('get');
+$filterForm->setAttribute('name', 'zbx_filter');
+$filterForm->setAttribute('id', 'zbx_filter');
+$filterForm->addItem($filterTable);
+$hostInventoryWidget->addFlicker($filterForm, CProfile::get('web.hostinventories.filter.state', 0));
+$hostInventoryWidget->addHeaderRowNumber();
+
+$table = new CTableInfo(_('No hosts found.'));
+$table->setHeader(array(
+	make_sorting_header(_('Host'), 'name'),
+	_('Group'),
+	make_sorting_header(_('Name'), 'pr_name'),
+	make_sorting_header(_('Type'), 'pr_type'),
+	make_sorting_header(_('OS'), 'pr_os'),
+	make_sorting_header(_('Serial number A'), 'pr_serialno_a'),
+	make_sorting_header(_('Tag'), 'pr_tag'),
+	make_sorting_header(_('MAC address A'), 'pr_macaddress_a'))
+);
+
+foreach ($this->data['hosts'] as $host) {
+	$hostGroups = array();
+	foreach ($host['groups'] as $group) {
+		$hostGroups[] = $group['name'];
+	}
+	natsort($hostGroups);
+	$hostGroups = implode(', ', $hostGroups);
+
+	$row = array(
+		new CLink(
+			$host['name'],
+			'?hostid='.$host['hostid'].url_param('groupid'),
+			($host['status'] == HOST_STATUS_NOT_MONITORED) ? 'not-monitored' : ''
+		),
+		$hostGroups,
+		zbx_str2links($host['inventory']['name']),
+		zbx_str2links($host['inventory']['type']),
+		zbx_str2links($host['inventory']['os']),
+		zbx_str2links($host['inventory']['serialno_a']),
+		zbx_str2links($host['inventory']['tag']),
+		zbx_str2links($host['inventory']['macaddress_a'])
+	);
+
+	$table->addRow($row);
+}
+
+$table = array($this->data['paging'], $table, $this->data['paging']);
+$hostInventoryWidget->addItem($table);
+
+return $hostInventoryWidget;
