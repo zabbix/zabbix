198b809d049648a40636a698d9873ab99ff5f369
krists <krists@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-04-24 08:17:47 +0000

    .......... [ZBX-8118] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@44741 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-8118/frontends/php/include/classes/api/CApiClientResponse.php
@@ -0,0 +1,54 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+/**
+ * This class is used by the API client to return the results of an API call.
+ */
+class CApiClientResponse {
+
+	/**
+	 * Data returned by the service method.
+	 *
+	 * @var mixed
+	 */
+	public $data;
+
+	/**
+	 * Error code.
+	 *
+	 * @var	int
+	 */
+	public $errorCode;
+
+	/**
+	 * Error message.
+	 *
+	 * @var	string
+	 */
+	public $errorMessage;
+
+	/**
+	 * Debug information.
+	 *
+	 * @var	array
+	 */
+	public $debug;
+}
