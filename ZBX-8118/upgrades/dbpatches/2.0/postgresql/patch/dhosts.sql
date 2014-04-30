beda4c19d4ecae0b2c88d5ff7c023e44c5851ea0
krists <krists@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-04-30 09:12:27 +0000

    .......... [ZBX-8118] remove development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@45023 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8118/upgrades/dbpatches/2.0/postgresql/patch/dhosts.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ONLY dhosts ALTER dhostid DROP DEFAULT,
-			ALTER druleid DROP DEFAULT;
-DELETE FROM dhosts WHERE NOT EXISTS (SELECT 1 FROM drules WHERE drules.druleid=dhosts.druleid);
-ALTER TABLE ONLY dhosts ADD CONSTRAINT c_dhosts_1 FOREIGN KEY (druleid) REFERENCES drules (druleid) ON DELETE CASCADE;
