f92d76373108f294218d52d36dd2584c0039ee9d
nikolaj <nikolaj@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-06 09:36:33 +0000

    .......... [ZBX-8172] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@45092 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-8172/frontends/php/include/views/configuration.graph.list.php
@@ -0,0 +1,184 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+$graphWidget = new CWidget();
+
+// create new graph button
+$createForm = new CForm('get');
+$createForm->cleanItems();
+if (!empty($this->data['parent_discoveryid'])) {
+	$createForm->addVar('parent_discoveryid', $this->data['parent_discoveryid']);
+	$createForm->addItem(new CSubmit('form', _('Create graph prototype')));
+
+	$graphWidget->addPageHeader(_('CONFIGURATION OF GRAPH PROTOTYPES'), $createForm);
+	$graphWidget->addHeader(array(_('Graph prototypes of').SPACE, new CSpan($this->data['discovery_rule']['name'], 'parent-discovery')));
+
+	if (!empty($this->data['hostid'])) {
+		$graphWidget->addItem(get_header_host_table('graphs', $this->data['hostid'], $this->data['parent_discoveryid']));
+	}
+}
+else {
+	$createForm->addVar('hostid', $this->data['hostid']);
+
+	if (!empty($this->data['hostid'])) {
+		$createForm->addItem(new CSubmit('form', _('Create graph')));
+	}
+	else {
+		$createGraphButton = new CSubmit('form', _('Create graph (select host first)'));
+		$createGraphButton->setEnabled(false);
+		$createForm->addItem($createGraphButton);
+	}
+
+	$graphWidget->addPageHeader(_('CONFIGURATION OF GRAPHS'), $createForm);
+
+	$filterForm = new CForm('get');
+	$filterForm->addItem(array(_('Group').SPACE, $this->data['pageFilter']->getGroupsCB()));
+	$filterForm->addItem(array(SPACE._('Host').SPACE, $this->data['pageFilter']->getHostsCB()));
+
+	$graphWidget->addHeader(_('Graphs'), $filterForm);
+
+	if (!empty($this->data['hostid'])) {
+		$graphWidget->addItem(get_header_host_table('graphs', $this->data['hostid']));
+	}
+}
+$graphWidget->addHeaderRowNumber();
+
+// create form
+$graphForm = new CForm();
+$graphForm->setName('graphForm');
+$graphForm->addVar('hostid', $this->data['hostid']);
+if (!empty($this->data['parent_discoveryid'])) {
+	$graphForm->addVar('parent_discoveryid', $this->data['parent_discoveryid']);
+}
+
+// create table
+$graphTable = new CTableInfo(!empty($this->data['parent_discoveryid']) ? _('No graph prototypes found.') : _('No graphs found.'));
+$graphTable->setHeader(array(
+	new CCheckBox('all_graphs', null, "checkAll('".$graphForm->getName()."', 'all_graphs', 'group_graphid');"),
+	!empty($this->data['hostid']) ? null : _('Hosts'),
+	make_sorting_header(_('Name'), 'name'),
+	_('Width'),
+	_('Height'),
+	make_sorting_header(_('Graph type'), 'graphtype')
+));
+
+foreach ($this->data['graphs'] as $graph) {
+	$graphid = $graph['graphid'];
+
+	$hostList = null;
+	if (empty($this->data['hostid'])) {
+		$hostList = array();
+		foreach ($graph['hosts'] as $host) {
+			$hostList[$host['name']] = $host['name'];
+		}
+
+		foreach ($graph['templates'] as $template) {
+			$hostList[$template['name']] = $template['name'];
+		}
+		$hostList = implode(', ', $hostList);
+	}
+
+	$isCheckboxEnabled = true;
+	$name = array();
+	if (!empty($graph['templateid'])) {
+		$realHosts = get_realhosts_by_graphid($graph['templateid']);
+		$realHosts = DBfetch($realHosts);
+		$name[] = new CLink($realHosts['name'], 'graphs.php?hostid='.$realHosts['hostid'], 'unknown');
+		$name[] = NAME_DELIMITER;
+		$name[] = new CLink(
+			$graph['name'],
+			'graphs.php?'.
+				'form=update'.
+				'&graphid='.$graphid.url_param('parent_discoveryid').
+				'&hostid='.$this->data['hostid']
+		);
+
+		if ($graph['discoveryRule']) {
+			$isCheckboxEnabled = false;
+		}
+	}
+	elseif (!empty($graph['discoveryRule']) && empty($this->data['parent_discoveryid'])) {
+		$name[] = new CLink(
+			$graph['discoveryRule']['name'],
+			'host_discovery.php?form=update&itemid='.$graph['discoveryRule']['itemid'],
+			'parent-discovery'
+		);
+		$name[] = NAME_DELIMITER;
+		$name[] = new CSpan($graph['name']);
+
+		$isCheckboxEnabled = false;
+	}
+	else {
+		$name[] = new CLink(
+			$graph['name'],
+			'graphs.php?'.
+				'form=update'.
+				'&graphid='.$graphid.url_param('parent_discoveryid').
+				'&hostid='.$this->data['hostid']
+		);
+	}
+
+	$checkBox = new CCheckBox('group_graphid['.$graphid.']', null, null, $graphid);
+	$checkBox->setEnabled($isCheckboxEnabled);
+
+	$graphTable->addRow(array(
+		$checkBox,
+		$hostList,
+		$name,
+		$graph['width'],
+		$graph['height'],
+		$graph['graphtype']
+	));
+}
+
+// create go buttons
+$goComboBox = new CComboBox('go');
+if (!$this->data['parent_discoveryid']) {
+	$goComboBox->addItem('copy_to', _('Copy selected to ...'));
+}
+
+$goOption = new CComboItem('delete', _('Delete selected'));
+$goOption->setAttribute(
+	'confirm',
+	$this->data['parent_discoveryid'] ? _('Delete selected graph prototypes?') : _('Delete selected graphs?')
+);
+$goComboBox->addItem($goOption);
+
+$goButton = new CSubmit('goButton', _('Go').' (0)');
+$goButton->attr('id', 'goButton');
+
+zbx_add_post_js('chkbxRange.pageGoName = "group_graphid";');
+if ($this->data['parent_discoveryid']) {
+	zbx_add_post_js('chkbxRange.prefix = "'.$this->data['parent_discoveryid'].'";');
+	zbx_add_post_js('cookie.prefix = "'.$this->data['parent_discoveryid'].'";');
+}
+else {
+	zbx_add_post_js('chkbxRange.prefix = "'.$this->data['hostid'].'";');
+	zbx_add_post_js('cookie.prefix = "'.$this->data['hostid'].'";');
+}
+
+// append table to form
+$graphForm->addItem(array($this->data['paging'], $graphTable, $this->data['paging'], get_table_header(array($goComboBox, $goButton))));
+
+// append form to widget
+$graphWidget->addItem($graphForm);
+
+return $graphWidget;
---
+++ ZBX-8172/src/libs/zbxmedia/sms.c
@@ -0,0 +1,260 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+#include "log.h"
+
+#include "zbxmedia.h"
+
+#include <termios.h>
+
+static int	write_gsm(int fd, const char *str, char *error, int max_error_len)
+{
+	int	i = 0, wlen = 0, len, ret = SUCCEED;
+
+	len = strlen(str);
+
+	zabbix_log(LOG_LEVEL_DEBUG, "write to GSM modem [%s]", str);
+
+	for (wlen = 0; wlen < len; wlen += i)
+	{
+		if (-1 == (i = write(fd, str + wlen, len - wlen)))
+		{
+			i = 0;
+
+			if (EAGAIN == errno)
+				continue;
+
+			zabbix_log(LOG_LEVEL_DEBUG, "error writing to GSM modem: %s", zbx_strerror(errno));
+			if (NULL != error)
+				zbx_snprintf(error, max_error_len, "error writing to GSM modem: %s", zbx_strerror(errno));
+
+			ret = FAIL;
+			break;
+		}
+	}
+
+	return ret;
+}
+
+static int	check_modem_result(char *buffer, char **ebuf, char **sbuf, const char *expect, char *error, int max_error_len)
+{
+	const char	*__function_name = "check_modem_result";
+	char		rcv[0xff];
+	int		i, len, ret = SUCCEED;
+
+	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
+
+	zbx_strlcpy(rcv, *sbuf, sizeof(rcv));
+
+	do
+	{
+		len = *ebuf - *sbuf;
+		for (i = 0; i < len && (*sbuf)[i] != '\n' && (*sbuf)[i] != '\r'; i++)
+			; /* find first '\r' & '\n' */
+
+		if (i < len)
+			(*sbuf)[i++] = '\0';
+
+		ret = (NULL == strstr(*sbuf, expect)) ? FAIL : SUCCEED;
+
+		*sbuf += i;
+
+		if (*sbuf != buffer)
+		{
+			memmove(buffer, *sbuf, *ebuf - *sbuf + 1); /* +1 for '\0' */
+			*ebuf -= *sbuf - buffer;
+			*sbuf = buffer;
+		}
+	}
+	while (*sbuf < *ebuf && ret == FAIL);
+
+	if (ret == FAIL && error)
+		zbx_snprintf(error, max_error_len, "Expected [%s] received [%s]", expect, rcv);
+
+	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%s", __function_name, zbx_result_string(ret));
+
+	return ret;
+}
+
+static int	read_gsm(int fd, const char *expect, char *error, int max_error_len, int timeout_sec)
+{
+	const char	*__function_name = "read_gsm";
+	static char	buffer[0xff], *ebuf = buffer, *sbuf = buffer;
+	fd_set		fdset;
+	struct timeval  tv;
+	int		i, nbytes, ret = SUCCEED;
+
+	zabbix_log(LOG_LEVEL_DEBUG, "In %s() [%s] [%s] [%s] [%s]", __function_name, expect,
+			ebuf != buffer ? buffer : "NULL", ebuf != buffer ? ebuf : "NULL", ebuf != buffer ? sbuf : "NULL");
+
+	if ('\0' != *expect && ebuf != buffer &&
+			SUCCEED == check_modem_result(buffer, &ebuf, &sbuf, expect, error, max_error_len))
+	{
+		goto out;
+	}
+
+	tv.tv_sec = timeout_sec;
+	tv.tv_usec = 0;
+
+	/* wait for response from modem */
+	FD_ZERO(&fdset);
+	FD_SET(fd, &fdset);
+	while (1)
+	{
+		i = select(fd + 1, &fdset, NULL, NULL, &tv);
+		if (-1 == i)
+		{
+			if (EINTR == errno)
+				continue;
+
+			zabbix_log(LOG_LEVEL_DEBUG, "error select() for GSM modem: %s", zbx_strerror(errno));
+			if (NULL != error)
+				zbx_snprintf(error, max_error_len, "error select() for GSM modem: %s", zbx_strerror(errno));
+
+			ret = FAIL;
+			goto out;
+		}
+		else if (0 == i)
+		{
+			/* timeout exceeded */
+			zabbix_log(LOG_LEVEL_DEBUG, "error during wait for GSM modem");
+			if (NULL != error)
+				zbx_snprintf(error, max_error_len, "error during wait for GSM modem");
+
+			goto check_result;
+		}
+		else
+			break;
+	}
+
+	/* read characters into our string buffer */
+	while (0 < (nbytes = read(fd, ebuf, buffer + sizeof(buffer) - 1 - ebuf)))
+		ebuf += nbytes;
+	/* nul terminate the string and see if we got an OK response */
+check_result:
+	*ebuf = '\0';
+
+	zabbix_log(LOG_LEVEL_DEBUG, "Read from GSM modem [%s]", sbuf);
+
+	if ('\0' == *expect) /* empty */
+	{
+		sbuf = ebuf = buffer;
+		*ebuf = '\0';
+		goto out;
+	}
+
+	ret = check_modem_result(buffer, &ebuf, &sbuf, expect, error, max_error_len);
+out:
+	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%s", __function_name, zbx_result_string(ret));
+
+	return ret;
+}
+
+typedef struct
+{
+	const char	*message;
+	const char	*result;
+	int		timeout_sec;
+}
+zbx_sms_scenario;
+
+int	send_sms(const char *device, const char *number, const char *message, char *error, int max_error_len)
+{
+	const char	*__function_name = "send_sms";
+#define	ZBX_AT_ESC	"\x1B"
+#define ZBX_AT_CTRL_Z	"\x1A"
+
+	zbx_sms_scenario scenario[] =
+	{
+		{ZBX_AT_ESC	, NULL		, 0},	/* Send <ESC> */
+		{"AT+CMEE=2\r"	, ""/*"OK"*/	, 5},	/* verbose error values */
+		{"ATE0\r"	, "OK"		, 5},	/* Turn off echo */
+		{"AT\r"		, "OK"		, 5},	/* Init modem */
+		{"AT+CMGF=1\r"	, "OK"		, 5},	/* Switch to text mode */
+		{"AT+CMGS=\""	, NULL		, 0},	/* Set phone number */
+		{number		, NULL		, 0},	/* Write phone number */
+		{"\"\r"		, "> "		, 5},	/* Set phone number */
+		{message	, NULL		, 0},	/* Write message */
+		{ZBX_AT_CTRL_Z	, "+CMGS: "	, 40},	/* Send message */
+		{NULL		, "OK"		, 1},	/* ^Z */
+		{NULL		, NULL		, 0}
+	};
+
+	zbx_sms_scenario	*step = NULL;
+	struct termios		options, old_options;
+	int			f, ret = SUCCEED;
+
+	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
+
+	if (-1 == (f = open(device, O_RDWR | O_NOCTTY | O_NDELAY)))
+	{
+		zabbix_log(LOG_LEVEL_DEBUG, "error in open(%s): %s", device, zbx_strerror(errno));
+		if (NULL != error)
+			zbx_snprintf(error, max_error_len, "error in open(%s): %s", device, zbx_strerror(errno));
+		return FAIL;
+	}
+	fcntl(f, F_SETFL, 0);	/* set the status flag to 0 */
+
+	/* set ta parameters */
+	tcgetattr(f, &old_options);
+
+	memset(&options, 0, sizeof(options));
+
+	options.c_iflag = IGNCR | INLCR | ICRNL;
+
+#ifdef ONOCR
+	options.c_oflag = ONOCR;
+#endif	/* ONOCR */
+
+	options.c_cflag = old_options.c_cflag | CRTSCTS | CS8 | CLOCAL | CREAD;
+	options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+	options.c_cc[VMIN] = 0;
+	options.c_cc[VTIME] = 1;
+
+	tcsetattr(f, TCSANOW, &options);
+
+	for (step = scenario; NULL != step->message || NULL != step->result; step++)
+	{
+		if (NULL != step->message)
+		{
+			if (FAIL == (ret = write_gsm(f, step->message, error, max_error_len)))
+				break;
+		}
+
+		if (NULL != step->result)
+		{
+			if (FAIL == (ret = read_gsm(f, step->result, error, max_error_len, step->timeout_sec)))
+				break;
+		}
+	}
+
+	if (FAIL == ret)
+	{
+		write_gsm(f, "\r" ZBX_AT_ESC ZBX_AT_CTRL_Z, NULL, 0); /* cancel all */
+		read_gsm(f, "", NULL, 0, 0); /* clear buffer */
+	}
+
+	tcsetattr(f, TCSANOW, &old_options);
+	close(f);
+
+	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%s", __function_name, zbx_result_string(ret));
+
+	return ret;
+}
