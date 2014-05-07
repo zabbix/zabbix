b7596da9d88df8c40fe6aab1762d9a28c143424d
krists <krists@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-07 13:43:01 +0000

    .......... [ZBX-8165] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@45146 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-8165/frontends/php/include/classes/validators/CSetValidator.php
@@ -0,0 +1,58 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CSetValidator extends CValidator {
+
+	/**
+	 * Allowed values.
+	 *
+	 * @var array
+	 */
+	public $values = array();
+
+	/**
+	 * Error message if the value is invalid.
+	 *
+	 * @var string
+	 */
+	public $messageInvalid;
+
+	/**
+	 * Checks if the given value belongs to some set.
+	 *
+	 * @param $value
+	 *
+	 * @return bool
+	 */
+	public function validate($value)
+	{
+		$values = array_flip($this->values);
+
+		if (!isset($values[$value])) {
+			$this->error($this->messageInvalid, $value);
+
+			return false;
+		}
+
+		return true;
+	}
+
+}
