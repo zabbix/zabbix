6e96fcaa0f8a8b1a5bc3fd11a734a7301e156f8a
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-08 10:28:32 +0000

    .......... [ZBXNEXT-497] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@45182 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-497/upgrades/dbpatches/1.4/mysql/patch/slideshows.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE slideshows (
-        slideshowid             bigint unsigned         DEFAULT '0'     NOT NULL,
-        name            varchar(255)            DEFAULT ''      NOT NULL,
-        delay           integer         DEFAULT '0'     NOT NULL,
-        PRIMARY KEY (slideshowid)
-) ENGINE=InnoDB;
