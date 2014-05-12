667c6006b0c83537b182f055916e03c670749644
nikolaj <nikolaj@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-12 13:47:25 +0000

    .......PS. [ZBX-8188] fixed IPMI in not supported state
    git-svn-id: svn://svn.zabbix.com/branches/dev@45370 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8188/src/zabbix_server/poller/checks_ipmi.c
+++ ZBX-8188/src/zabbix_server/poller/checks_ipmi.c
@@ -28,6 +28,7 @@
 #include <OpenIPMI/ipmi_lan.h>
 #include <OpenIPMI/ipmi_sdr.h>
 #include <OpenIPMI/ipmi_msgbits.h>
+#include <OpenIPMI/internal/ipmi_int.h>
 
 typedef struct
 {
@@ -372,6 +373,15 @@ static void	got_thresh_reading(ipmi_sensor_t *sensor, int err, enum ipmi_value_p
 	{
 		THIS_SHOULD_NEVER_HAPPEN;
 		h->err = zbx_dsprintf(h->err, "fatal error");
+		h->ret = NOTSUPPORTED;
+		h->done = 1;
+		goto out;
+	}
+
+	if (0 != states->__states)
+	{
+		zabbix_log(LOG_LEVEL_DEBUG, "IPMI sensor in 'not supported' state."
+				" State value: %i", states->__states);
 		h->ret = NOTSUPPORTED;
 		h->done = 1;
 		goto out;
