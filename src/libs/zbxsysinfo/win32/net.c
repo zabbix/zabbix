93bff0c01cfa1befbb851c2eb20b22c518313936
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-13 12:49:20 +0000

    ...G...... [ZBX-7875] fixed wrongly reported system version for Windows 8.1 and Server 2012 R2
    git-svn-id: svn://svn.zabbix.com/branches/2.2@45422 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxsysinfo/win32/net.c
+++ src/libs/zbxsysinfo/win32/net.c
@@ -33,11 +33,11 @@ static LPSTR	get_if_description(MIB_IFROW *pIfRow)
 
 	if (NULL == mb_to_unicode)
 	{
-		OSVERSIONINFO	version_info = {sizeof(OSVERSIONINFO)};
+		const OSVERSIONINFOEX	*vi;
 
 		/* starting with Windows Vista (Windows Server 2008) the interface description */
 		/* is encoded in OEM codepage while earlier versions used ANSI codepage */
-		if (TRUE == GetVersionEx(&version_info) && 6 <= version_info.dwMajorVersion)
+		if (NULL != (vi = zbx_win_getversion()) && 6 <= vi->dwMajorVersion)
 			mb_to_unicode = zbx_oemcp_to_unicode;
 		else
 			mb_to_unicode = zbx_acp_to_unicode;
