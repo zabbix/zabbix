585f837a0867377f50947b59f9b562e0431ba60e
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-13 13:14:11 +0000

    ....I..... [ZBXNEXT-2033] updated script to import service class into host_inventory.serialno_b field
    git-svn-id: svn://svn.zabbix.com/branches/dev@45431 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2033/scripts/host-reconid-import.pl
+++ ZBXNEXT-2033/scripts/host-reconid-import.pl
@@ -16,22 +16,25 @@ sub display_usage
 	printf "    --url=<url>             - Zabbix frontend URL\n";
 	printf "    --username=<username>   - Zabbix user name\n";
 	printf "    --password=<password>   - Zabbix user password\n";
-	printf "    --ci=<pos>              - CI field position (default: 2)\n";
+	printf "    --ci=<pos>              - CI field position (default: 3)\n";
 	printf "    --reconid=<pos>         - Recon ID field position (default: 4)\n";
+	printf "    --serviceclass=<pos>    - Service class field position (default: 5)\n";
 }
 
 my $URL;
 my $USER;
 my $PASSWORD;
-my $CI = 2;
+my $CI = 3;
 my $RECONID = 4;
+my $SERVICECLASS = 5;
 
 GetOptions(
 	"url=s" => \$URL,
 	"username=s" => \$USER,
 	"password=s" => \$PASSWORD,
 	"ci=s" => \$CI,
-	"reconid=s" => \$RECONID
+	"reconid=s" => \$RECONID,
+	"serviceclasss=s" => \$SERVICECLASS
 ) or die("Error in command line arguments\n");
 
 $URL = $URL . "/api_jsonrpc.php";
@@ -46,6 +49,7 @@ if (!$filename or !$URL or !$USER or !$PASSWORD)
 
 $CI--;
 $RECONID--;
+$SERVICECLASS--;
 
 # Update host inventory in database
 open (FP, $filename) or die "Failed to open input file '" . $filename . "'";
@@ -102,7 +106,7 @@ $json = {
 	{	
 		hostid => 0,
 		inventory_mode => '1',
-		inventory => { tag => ''},
+		inventory => { tag => '', serialno_b => ''},
 	},
 	id => 3,
 	auth => "$authID",
@@ -122,10 +126,12 @@ FILE_LOOP: while (<FP>) {
 	
 	foreach $i (1, 2) {
 		if (exists $hosts{$hostname}) {
-			printf "Importing CI '%s' ReconID '%s' to host '%s'\n", $fields[$CI] , $fields[$RECONID], $hostname;
+			printf "Importing CI '%s' ReconID '%s', service class '%s' to host '%s'\n", 
+					$fields[$CI] , $fields[$RECONID], $fields[$SERVICECLASS], $hostname;
 			
 			$json->{params}->{hostid} = $hosts{$hostname};
 			$json->{params}->{inventory}->{tag} = $fields[$RECONID];
+			$json->{params}->{inventory}->{serialno_b} = $fields[$SERVICECLASS];
 			
 			$response = $client->call($URL, $json);
 
