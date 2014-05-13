524ee2a42ede8c944a5c2574cbb3a7035cb9aa99
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-13 12:50:20 +0000

    .......... [ZBX-8157] updated to latest from trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@45423 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8157/src/libs/zbxsysinfo/hpux/net.c
+++ ZBX-8157/src/libs/zbxsysinfo/hpux/net.c
@@ -49,7 +49,7 @@ static struct strbuf	ctlbuf =
 
 #define ZBX_IF_SEP	','
 
-void	add_if_name(char **if_list, size_t *if_list_alloc, size_t *if_list_offset, const char *name)
+static void	add_if_name(char **if_list, size_t *if_list_alloc, size_t *if_list_offset, const char *name)
 {
 	if (FAIL == str_in_list(*if_list, name, ZBX_IF_SEP))
 	{
@@ -60,7 +60,7 @@ void	add_if_name(char **if_list, size_t *if_list_alloc, size_t *if_list_offset, 
 	}
 }
 
-int	get_if_names(char **if_list, size_t *if_list_alloc, size_t *if_list_offset)
+static int	get_if_names(char **if_list, size_t *if_list_alloc, size_t *if_list_offset)
 {
 	int			s, ifreq_size, numifs, i, family = AF_INET;
 	struct sockaddr		*from;
@@ -148,6 +148,7 @@ end:
 #endif
 	return SUCCEED;
 }
+
 #endif	/* HPUX_VERSION < 1131 */
 
 int	NET_IF_DISCOVERY(AGENT_REQUEST *request, AGENT_RESULT *result)
@@ -294,6 +295,7 @@ static int	dlpi_get_stats(int fd, Ext_mib_t *mib)
 	stat_msg = *(dl_get_statistics_ack_t *)buf_ctl;
 
 	memcpy(mib, (Ext_mib_t *)(buf_ctl + stat_msg.dl_stat_offset), sizeof(Ext_mib_t));
+
 	return SUCCEED;
 }
 
@@ -360,9 +362,11 @@ static int get_ppa(int fd, const char *if_name, int *ppa)
 				break;
 			}
 		}
+
 		zbx_free(buf);
 		zbx_free(ppa_data_buf);
 	}
+
 	return ret;
 }
 
