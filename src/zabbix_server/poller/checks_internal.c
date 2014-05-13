b71d540413183fe56c026fc8bff7a4f0939655ed
Aleksandrs Saveljevs <git-no-reply@zabbix.com>
2014-05-13 12:15:42 +0000

    ...G...... [ZBXNEXT-2203] implemented suggestions for error messages for src/zabbix_server/poller

--- src/zabbix_server/poller/checks_internal.c
+++ src/zabbix_server/poller/checks_internal.c
@@ -124,7 +124,7 @@ int	get_value_internal(DC_ITEM *item, AGENT_RESULT *result)
 
 		if (3 < nparams)
 		{
-			error = zbx_strdup(error, "Invalid number of parameters.");
+			error = zbx_strdup(error, "Too many parameters.");
 			goto notsupported;
 		}
 
@@ -154,7 +154,7 @@ int	get_value_internal(DC_ITEM *item, AGENT_RESULT *result)
 
 		if ((unsigned int)-1 != to && from > to)
 		{
-			error = zbx_strdup(error, "Parameters represent an invalid interval");
+			error = zbx_strdup(error, "Parameters represent an invalid interval.");
 			goto notsupported;
 		}
 
@@ -245,13 +245,13 @@ int	get_value_internal(DC_ITEM *item, AGENT_RESULT *result)
 
 		if (4 < nparams)
 		{
-			error = zbx_strdup(error, "Invalid number of parameters");
+			error = zbx_strdup(error, "Too many parameters.");
 			goto notsupported;
 		}
 
 		if (0 != get_param(params, 2, tmp, sizeof(tmp)))
 		{
-			error = zbx_strdup(error, "Required second parameter missing");
+			error = zbx_strdup(error, "Invalid number of parameters.");
 			goto notsupported;
 		}
 
@@ -278,7 +278,7 @@ int	get_value_internal(DC_ITEM *item, AGENT_RESULT *result)
 
 		if (ZBX_PROCESS_TYPE_COUNT == process_type)
 		{
-			error = zbx_strdup(error, "Invalid second parameter");
+			error = zbx_strdup(error, "Invalid second parameter.");
 			goto notsupported;
 		}
 
@@ -318,13 +318,13 @@ int	get_value_internal(DC_ITEM *item, AGENT_RESULT *result)
 
 			if (0 == process_forks)
 			{
-				error = zbx_dsprintf(error, "No \"%s\" processes started",
+				error = zbx_dsprintf(error, "No \"%s\" processes started.",
 						get_process_type_string(process_type));
 				goto notsupported;
 			}
 			else if (process_num > process_forks)
 			{
-				error = zbx_dsprintf(error, "\"%s\" #%d is not started",
+				error = zbx_dsprintf(error, "\"%s\" #%d is not started.",
 						get_process_type_string(process_type), process_num);
 				goto notsupported;
 			}
@@ -458,7 +458,7 @@ int	get_value_internal(DC_ITEM *item, AGENT_RESULT *result)
 
 		if (FAIL == zbx_vc_get_statistics(&stats))
 		{
-			error = zbx_strdup(error, "Value cache is disabled");
+			error = zbx_strdup(error, "Value cache is disabled.");
 			goto notsupported;
 		}
 
@@ -517,7 +517,7 @@ int	get_value_internal(DC_ITEM *item, AGENT_RESULT *result)
 
 		if (FAIL == zbx_vmware_get_statistics(&stats))
 		{
-			error = zbx_dsprintf(error, "No \"%s\" processes started",
+			error = zbx_dsprintf(error, "No \"%s\" processes started.",
 					get_process_type_string(ZBX_PROCESS_TYPE_VMWARE));
 			goto notsupported;
 		}
