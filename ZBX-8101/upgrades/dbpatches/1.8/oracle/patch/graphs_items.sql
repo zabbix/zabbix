e550db1952c1b1256101a56f09d946d392b779be
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-16 16:42:37 +0000

    .......... [ZBX-8101] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@45587 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8101/upgrades/dbpatches/1.8/oracle/patch/graphs_items.sql
+++
@@ -1,6 +0,0 @@
-alter table graphs_items modify color           nvarchar2(6)            DEFAULT '009600';
-
-CREATE INDEX graphs_items_1 on graphs_items (itemid);
-CREATE INDEX graphs_items_2 on graphs_items (graphid);
-
-alter table graphs_items modify color           nvarchar2(6)            DEFAULT '009600';
