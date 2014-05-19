ee3c72f16c5b757fbdeb1e0feaba42734323eda8
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-19 14:23:05 +0000

    .......... [ZBXNEXT-734] updated to latest from trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@45638 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-734/src/libs/zbxdbupgrade/dbupgrade_2030.c
+++ ZBXNEXT-734/src/libs/zbxdbupgrade/dbupgrade_2030.c
@@ -870,6 +870,13 @@ static int	DBpatch_2030091(void)
 
 	return DBcreate_index("valuemaps", "valuemaps_1", "name", 1);
 }
+
+static int      DBpatch_2030092(void)
+{
+	const ZBX_FIELD field = {"timeout", "15", NULL, NULL, 0, ZBX_TYPE_INT, ZBX_NOTNULL, 0};
+
+	return DBset_default("httpstep", &field);
+}
 #endif
 
 DBPATCH_START(2030)
@@ -967,5 +974,6 @@ DBPATCH_ADD(2030088, 0, 1)
 DBPATCH_ADD(2030089, 0, 1)
 DBPATCH_ADD(2030090, 0, 1)
 DBPATCH_ADD(2030091, 0, 1)
+DBPATCH_ADD(2030092, 0, 1)
 
 DBPATCH_END()
