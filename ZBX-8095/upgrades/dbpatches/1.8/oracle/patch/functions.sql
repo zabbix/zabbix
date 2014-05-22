a16877431c1d609750b3b4058f4bff3c4b7e1a2d
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-22 09:52:37 +0000

    .......... [ZBX-8095] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@45743 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-8095/upgrades/dbpatches/1.8/oracle/patch/functions.sql
@@ -0,0 +1,4 @@
+alter table functions modify lastvalue               nvarchar2(255);
+alter table functions modify function                nvarchar2(12);
+alter table functions modify parameter               nvarchar2(255);
+
