79f14cfc896a07145d4aafe1f893b8c3c698bf7f
andrejs <andrejs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-22 15:30:45 +0000

    ..F....... [ZBX-8241] created new developemnt branch to fix media types vs multiple actions bug
    git-svn-id: svn://svn.zabbix.com/tags/2.0.12@45761 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ trunk/src/libs/zbxsysinfo/openbsd/uptime.c
@@ -0,0 +1,51 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+#include "sysinfo.h"
+#include "log.h"
+
+int	SYSTEM_UPTIME(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+#ifdef HAVE_FUNCTION_SYSCTL_KERN_BOOTTIME
+	int		mib[2], now;
+	size_t		len;
+	struct timeval	uptime;
+
+	mib[0] = CTL_KERN;
+	mib[1] = KERN_BOOTTIME;
+
+	len = sizeof(struct timeval);
+
+	if (0 != sysctl(mib, 2, &uptime, &len, NULL, 0))
+	{
+		SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot obtain system information: %s", zbx_strerror(errno)));
+		return SYSINFO_RET_FAIL;
+	}
+
+	now = time(NULL);
+
+	SET_UI64_RESULT(result, now - uptime.tv_sec);
+
+	return SYSINFO_RET_OK;
+#else
+	SET_MSG_RESULT(result, zbx_strdup(NULL, "Agent was compiled without support for uptime information."));
+	return SYSINFO_RET_FAIL;
+#endif
+}
