7876155a3df55ef358e29cc9effc1bb6ade38c30
andrejs <andrejs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-23 11:51:31 +0000

    ..F....... [ZBX-8212] it is not possible to add triggers from templates to maps anymore (Andrejs)
    git-svn-id: svn://svn.zabbix.com/trunk@45803 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/views/js/configuration.sysmaps.js.php
+++ frontends/php/include/views/js/configuration.sysmaps.js.php
@@ -106,7 +106,7 @@
 				<td>
 					<input readonly="readonly" size="50" id="elementNameTrigger" name="elementName" class="input">
 					<input type="hidden" id="elementExpressionTrigger" name="elementExpressionTrigger">
-					<span class="link" onclick="PopUp('popup.php?writeonly=1&dstfrm=selementForm&dstfld1=elementid&dstfld2=elementNameTrigger&dstfld3=elementExpressionTrigger&srctbl=triggers&srcfld1=triggerid&srcfld2=description&srcfld3=expression&with_triggers=1',450,450)"><?php echo _('Select'); ?></span>
+					<span class="link" onclick="PopUp('popup.php?writeonly=1&dstfrm=selementForm&dstfld1=elementid&dstfld2=elementNameTrigger&dstfld3=elementExpressionTrigger&srctbl=triggers&srcfld1=triggerid&srcfld2=description&srcfld3=expression&with_triggers=1&real_hosts=1',450,450)"><?php echo _('Select'); ?></span>
 				</td>
 			</tr>
 			<tr id="mapSelectRow">
