b2dbbec4d18bea4e091b3b0c8f37ca0e5f285282
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-26 08:30:36 +0000

    ........S. [ZBXNEXT-1133] add support of custom expressions to action condition checks
    git-svn-id: svn://svn.zabbix.com/branches/dev@45842 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1133/src/libs/zbxdbhigh/lld.c
+++ ZBXNEXT-1133/src/libs/zbxdbhigh/lld.c
@@ -374,7 +374,7 @@ static int	filter_evaluate_expression(lld_filter_t *filter, struct zbx_json_pars
 	}
 
 	if (SUCCEED == evaluate(&result, expression, error, sizeof(error)))
-		ret = (0 == result ? FAIL : SUCCEED);
+		ret = zbx_double_compare(result, 1.0);
 
 	zbx_free(expression);
 
