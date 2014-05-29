a7a856c829951c9e400bdb3078347c43e543b3a5
juris <juris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-29 10:40:06 +0000

    A......... [ZBX-6531] averted memory leak in filesystem discovery cause by memory traversal via the origin pointer
    git-svn-id: svn://svn.zabbix.com/branches/dev@45958 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6531/src/libs/zbxsysinfo/aix/diskspace.c
+++ ZBX-6531/src/libs/zbxsysinfo/aix/diskspace.c
@@ -176,14 +176,14 @@ static const char	*zbx_get_vfs_name_by_type(int type)
 int	VFS_FS_DISCOVERY(const char *cmd, const char *param, unsigned flags, AGENT_RESULT *result)
 {
 	int		rc, sz, i, ret = SYSINFO_RET_FAIL;
-	struct vmount	*vm = NULL;
+	struct vmount	*vms = NULL, *vm;
 	struct zbx_json	j;
 
 	/* check how many bytes to allocate for the mounted filesystems */
 	if (-1 == (rc = mntctl(MCTL_QUERY, sizeof(sz), (char *)&sz)))
 		return ret;
 
-	vm = zbx_malloc(vm, (size_t)sz);
+	vms = zbx_malloc(vms, (size_t)sz);
 
 	/* get the list of mounted filesystems */
 	/* return code is number of filesystems returned */
@@ -194,7 +194,7 @@ int	VFS_FS_DISCOVERY(const char *cmd, const char *param, unsigned flags, AGENT_R
 
 	zbx_json_addarray(&j, ZBX_PROTO_TAG_DATA);
 
-	for (i = 0; i < rc; i++)
+	for (i = 0, vm = vms; i < rc; i++)
 	{
 		zbx_json_addobject(&j, NULL);
 		zbx_json_addstring(&j, "{#FSNAME}", (char *)vm + vm->vmt_data[VMT_STUB].vmt_off, ZBX_JSON_TYPE_STRING);
@@ -213,7 +213,7 @@ int	VFS_FS_DISCOVERY(const char *cmd, const char *param, unsigned flags, AGENT_R
 
 	ret = SYSINFO_RET_OK;
 error:
-	zbx_free(vm);
+	zbx_free(vms);
 
 	return ret;
 }
