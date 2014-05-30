9ab60643b2fa563b296649c2941bea2da9bd0f6f
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-05-30 15:12:13 +0000

    ...G...... [ZBXNEXT-2203] returning of error messages added to other logrt[]-related functions
    git-svn-id: svn://svn.zabbix.com/branches/dev@46057 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2203-logrt/src/zabbix_agent/logfiles.h
+++ ZBXNEXT-2203-logrt/src/zabbix_agent/logfiles.h
@@ -40,7 +40,7 @@ struct	st_logfile
 };
 
 int	process_log(char *filename, zbx_uint64_t *lastlogsize, int *mtime, unsigned char *skip_old_data, int *big_rec,
-		int *incomplete, const char *encoding, zbx_vector_ptr_t *regexps, const char *pattern,
+		int *incomplete, char **err_msg, const char *encoding, zbx_vector_ptr_t *regexps, const char *pattern,
 		const char *output_template, int *p_count, int *s_count, zbx_process_value_func_t process_value,
 		const char *server, unsigned short port, const char *hostname, const char *key);
 
