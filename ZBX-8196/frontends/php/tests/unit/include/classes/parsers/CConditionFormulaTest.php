2db17a9e8043e5bf231f31103f9181c779dd639e
andrejs <andrejs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-02 08:08:35 +0000

    ..F....... [ZBX-8196] created dev branch to validate sort parameter
    git-svn-id: svn://svn.zabbix.com/branches/dev@46072 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-8196/frontends/php/tests/unit/include/classes/parsers/CConditionFormulaTest.php
@@ -0,0 +1,85 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CConditionFormulaTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var CConditionFormula
+	 */
+	protected $conditionFormula;
+
+	public function setUp() {
+		$this->conditionFormula = new CConditionFormula();
+	}
+
+	public function testParseValidProvider() {
+		return array(
+			array('A'),
+			array('A and B'),
+			array('A or B'),
+			array('(A)'),
+			array('((A))'),
+			array('A and (B and C)'),
+			array('A and(B and C)'),
+			array('(A and B)and C'),
+		);
+	}
+
+	/**
+	 * @dataProvider testParseValidProvider
+	 *
+	 * @param $string
+	 */
+	public function testParseValid($string) {
+		$result = $this->conditionFormula->parse($string);
+
+		$this->assertSame(true, $result);
+	}
+
+	public function testParseInvalidProvider() {
+		return array(
+			array('a'),
+			array('A B'),
+			array('A and'),
+			array('(A'),
+			array('((A)'),
+			array('A and (B and C'),
+			array('AandB'),
+			array('A and BandC'),
+		);
+	}
+
+	/**
+	 * @dataProvider testParseInvalidProvider
+	 *
+	 * @param $string
+	 */
+	public function testParseInvalid($string) {
+		$result = $this->conditionFormula->parse($string);
+
+		$this->assertSame(false, $result);
+	}
+
+	public function testParseConstants() {
+		// test whether the $constants property is populated correctly
+		$this->markTestIncomplete();
+	}
+}
