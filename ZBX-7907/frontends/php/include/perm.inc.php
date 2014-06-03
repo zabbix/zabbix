158b84f2edf86c4d73a5ae8fc0cad72c6905af2f
andrejs <andrejs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-03 14:43:36 +0000

    ..F....... [ZBX-7907] created new developemnt branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@46166 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-7907/frontends/php/include/perm.inc.php
@@ -0,0 +1,311 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+/**
+ * Get permission label.
+ *
+ * @param int $permission
+ *
+ * @return string
+ */
+function permission2str($permission) {
+	$permissions = array(
+		PERM_READ_WRITE => _('Read-write'),
+		PERM_READ => _('Read only'),
+		PERM_DENY => _('Deny')
+	);
+
+	return isset($permissions[$permission]) ? $permissions[$permission] : _('Unknown');
+}
+
+/**
+ * Get authentication label.
+ *
+ * @param int $type
+ *
+ * @return string
+ */
+function authentication2str($type) {
+	$authentications = array(
+		ZBX_AUTH_INTERNAL => _('Zabbix internal authentication'),
+		ZBX_AUTH_LDAP => _('LDAP authentication'),
+		ZBX_AUTH_HTTP => _('HTTP authentication')
+	);
+
+	return isset($authentications[$type]) ? $authentications[$type] : _('Unknown');
+}
+
+/***********************************************
+	CHECK USER ACCESS TO SYSTEM STATUS
+************************************************/
+/* Function: check_perm2system()
+ *
+ * Description:
+ * 		Checking user permissions to access system (affects server side: no notification will be sent)
+ *
+ * Comments:
+ *		return true if permission is positive
+ *
+ * Author: Aly
+ */
+function check_perm2system($userid) {
+	$sql = 'SELECT g.usrgrpid'.
+			' FROM usrgrp g,users_groups ug'.
+			' WHERE ug.userid='.zbx_dbstr($userid).
+				' AND g.usrgrpid=ug.usrgrpid'.
+				' AND g.users_status='.GROUP_STATUS_DISABLED;
+	if ($res = DBfetch(DBselect($sql, 1))) {
+		return false;
+	}
+	return true;
+}
+
+/**
+ * Checking user permissions to login in frontend.
+ *
+ * @param string $userId
+ *
+ * @return bool
+ */
+function check_perm2login($userId) {
+	return (getUserGuiAccess($userId) != GROUP_GUI_ACCESS_DISABLED);
+}
+
+/**
+ * Get user gui access.
+ *
+ * @param string $userId
+ * @param int    $maxGuiAccess
+ *
+ * @return int
+ */
+function getUserGuiAccess($userId, $maxGuiAccess = null) {
+	if (bccomp($userId, CWebUser::$data['userid']) == 0 && isset(CWebUser::$data['gui_access'])) {
+		return CWebUser::$data['gui_access'];
+	}
+
+	$guiAccess = DBfetch(DBselect(
+		'SELECT MAX(g.gui_access) AS gui_access'.
+		' FROM usrgrp g,users_groups ug'.
+		' WHERE ug.userid='.zbx_dbstr($userId).
+			' AND g.usrgrpid=ug.usrgrpid'.
+			(($maxGuiAccess === null) ? '' : ' AND g.gui_access<='.$maxGuiAccess)
+	));
+
+	return $guiAccess ? $guiAccess['gui_access'] : GROUP_GUI_ACCESS_SYSTEM;
+}
+
+/**
+ * Get user authentication type.
+ *
+ * @param string $userId
+ * @param int    $maxGuiAccess
+ *
+ * @return int
+ */
+function getUserAuthenticationType($userId, $maxGuiAccess = null) {
+	$config = select_config();
+
+	switch (getUserGuiAccess($userId, $maxGuiAccess)) {
+		case GROUP_GUI_ACCESS_SYSTEM:
+			return $config['authentication_type'];
+
+		case GROUP_GUI_ACCESS_INTERNAL:
+			return ($config['authentication_type'] == ZBX_AUTH_HTTP) ? ZBX_AUTH_HTTP : ZBX_AUTH_INTERNAL;
+
+		default:
+			return $config['authentication_type'];
+	}
+}
+
+/**
+ * Get groups gui access.
+ *
+ * @param array $groupIds
+ * @param int   $maxGuiAccess
+ *
+ * @return int
+ */
+function getGroupsGuiAccess($groupIds, $maxGuiAccess = null) {
+	$guiAccess = DBfetch(DBselect(
+		'SELECT MAX(g.gui_access) AS gui_access'.
+		' FROM usrgrp g'.
+		' WHERE '.dbConditionInt('g.usrgrpid', $groupIds).
+			(($maxGuiAccess === null) ? '' : ' AND g.gui_access<='.$maxGuiAccess)
+	));
+
+	return $guiAccess ? $guiAccess['gui_access'] : GROUP_GUI_ACCESS_SYSTEM;
+}
+
+/**
+ * Get group authentication type.
+ *
+ * @param array $groupIds
+ * @param int   $maxGuiAccess
+ *
+ * @return int
+ */
+function getGroupAuthenticationType($groupIds, $maxGuiAccess = null) {
+	$config = select_config();
+
+	switch (getGroupsGuiAccess($groupIds, $maxGuiAccess)) {
+		case GROUP_GUI_ACCESS_SYSTEM:
+			return $config['authentication_type'];
+
+		case GROUP_GUI_ACCESS_INTERNAL:
+			return ($config['authentication_type'] == ZBX_AUTH_HTTP) ? ZBX_AUTH_HTTP : ZBX_AUTH_INTERNAL;
+
+		default:
+			return $config['authentication_type'];
+	}
+}
+
+/***********************************************
+	GET ACCESSIBLE RESOURCES BY RIGHTS
+************************************************/
+/* NOTE: right structure is
+	$rights[i]['type']	= type of resource
+	$rights[i]['permission']= permission for resource
+	$rights[i]['id']	= resource id
+*/
+function get_accessible_hosts_by_rights(&$rights, $user_type, $perm) {
+	$result = array();
+	$res_perm = array();
+
+	foreach ($rights as $id => $right) {
+		$res_perm[$right['id']] = $right['permission'];
+	}
+
+	$host_perm = array();
+	$where = array();
+
+	array_push($where, 'h.status in ('.HOST_STATUS_MONITORED.','.HOST_STATUS_NOT_MONITORED.','.HOST_STATUS_TEMPLATE.')');
+	array_push($where, dbConditionInt('h.flags', array(ZBX_FLAG_DISCOVERY_NORMAL, ZBX_FLAG_DISCOVERY_CREATED)));
+
+	$where = count($where) ? $where = ' WHERE '.implode(' AND ', $where) : '';
+
+	$perm_by_host = array();
+
+	$dbHosts = DBselect(
+		'SELECT hg.groupid AS groupid,h.hostid,h.host,h.name AS host_name,h.status'.
+		' FROM hosts h'.
+			' LEFT JOIN hosts_groups hg ON hg.hostid=h.hostid'.
+			$where
+	);
+	while ($dbHost = DBfetch($dbHosts)) {
+		if (isset($dbHost['groupid']) && isset($res_perm[$dbHost['groupid']])) {
+			if (!isset($perm_by_host[$dbHost['hostid']])) {
+				$perm_by_host[$dbHost['hostid']] = array();
+			}
+			$perm_by_host[$dbHost['hostid']][] = $res_perm[$dbHost['groupid']];
+			$host_perm[$dbHost['hostid']][$dbHost['groupid']] = $res_perm[$dbHost['groupid']];
+		}
+		$host_perm[$dbHost['hostid']]['data'] = $dbHost;
+	}
+
+	foreach ($host_perm as $hostid => $dbHost) {
+		$dbHost = $dbHost['data'];
+
+		// select min rights from groups
+		if (USER_TYPE_SUPER_ADMIN == $user_type) {
+			$dbHost['permission'] = PERM_READ_WRITE;
+		}
+		else {
+			if (isset($perm_by_host[$hostid])) {
+				$dbHost['permission'] = (min($perm_by_host[$hostid]) == PERM_DENY)
+					? PERM_DENY
+					: max($perm_by_host[$hostid]);
+			}
+			else {
+				$dbHost['permission'] = PERM_DENY;
+			}
+		}
+
+		if ($dbHost['permission'] < $perm) {
+			continue;
+		}
+
+		$result[$dbHost['hostid']] = $dbHost;
+	}
+
+	CArrayHelper::sort($result, array(
+		array('field' => 'host_name', 'order' => ZBX_SORT_UP)
+	));
+
+	return $result;
+}
+
+function get_accessible_groups_by_rights(&$rights, $user_type, $perm) {
+	$result = array();
+
+	$group_perm = array();
+	foreach ($rights as $right) {
+		$group_perm[$right['id']] = $right['permission'];
+	}
+
+	$dbHostGroups = DBselect('SELECT g.*,'.PERM_DENY.' AS permission FROM groups g');
+
+	while ($dbHostGroup = DBfetch($dbHostGroups)) {
+		if ($user_type == USER_TYPE_SUPER_ADMIN) {
+			$dbHostGroup['permission'] = PERM_READ_WRITE;
+		}
+		elseif (isset($group_perm[$dbHostGroup['groupid']])) {
+			$dbHostGroup['permission'] = $group_perm[$dbHostGroup['groupid']];
+		}
+		else {
+			$dbHostGroup['permission'] = PERM_DENY;
+		}
+
+		if ($dbHostGroup['permission'] < $perm) {
+			continue;
+		}
+
+		$result[$dbHostGroup['groupid']] = $dbHostGroup;
+	}
+
+	CArrayHelper::sort($result, array(
+		array('field' => 'name', 'order' => ZBX_SORT_UP)
+	));
+
+	return $result;
+}
+
+/**
+ * Returns array of user groups by $userId
+ *
+ * @param int $userId
+ *
+ * @return array
+ */
+function getUserGroupsByUserId($userId) {
+	static $userGroups;
+
+	if (!isset($userGroups[$userId])) {
+		$userGroups[$userId] = array();
+
+		$result = DBselect('SELECT usrgrpid FROM users_groups WHERE userid='.zbx_dbstr($userId));
+		while ($row = DBfetch($result)) {
+			$userGroups[$userId][] = $row['usrgrpid'];
+		}
+	}
+
+	return $userGroups[$userId];
+}
---
+++ ZBX-7907/frontends/php/include/views/configuration.triggers.edit.php
@@ -0,0 +1,359 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+require_once dirname(__FILE__).'/js/configuration.triggers.edit.js.php';
+
+$triggersWidget = new CWidget();
+
+// append host summary to widget header
+if (!empty($this->data['hostid'])) {
+	if (!empty($this->data['parent_discoveryid'])) {
+		$triggersWidget->addItem(get_header_host_table('triggers', $this->data['hostid'], $this->data['parent_discoveryid']));
+	}
+	else {
+		$triggersWidget->addItem(get_header_host_table('triggers', $this->data['hostid']));
+	}
+}
+
+if (!empty($this->data['parent_discoveryid'])) {
+	$triggersWidget->addPageHeader(_('CONFIGURATION OF TRIGGER PROTOTYPES'));
+}
+else {
+	$triggersWidget->addPageHeader(_('CONFIGURATION OF TRIGGERS'));
+}
+
+// create form
+$triggersForm = new CForm();
+$triggersForm->setName('triggersForm');
+$triggersForm->addVar('form', $this->data['form']);
+$triggersForm->addVar('hostid', $this->data['hostid']);
+$triggersForm->addVar('parent_discoveryid', $this->data['parent_discoveryid']);
+$triggersForm->addVar('input_method', $this->data['input_method']);
+$triggersForm->addVar('toggle_input_method', '');
+$triggersForm->addVar('remove_expression', '');
+if (!empty($this->data['triggerid'])) {
+	$triggersForm->addVar('triggerid', $this->data['triggerid']);
+}
+
+// create form list
+$triggersFormList = new CFormList('triggersFormList');
+if (!empty($this->data['templates'])) {
+	$triggersFormList->addRow(_('Parent triggers'), $this->data['templates']);
+}
+$nameTextBox = new CTextBox('description', $this->data['description'], ZBX_TEXTBOX_STANDARD_SIZE, $this->data['limited']);
+$nameTextBox->attr('autofocus', 'autofocus');
+$triggersFormList->addRow(_('Name'), $nameTextBox);
+
+// append expression to form list
+$expressionTextBox = new CTextArea(
+	$this->data['expression_field_name'],
+	$this->data['expression_field_value'],
+	array(
+		'rows' => ZBX_TEXTAREA_STANDARD_ROWS,
+		'width' => ZBX_TEXTAREA_STANDARD_WIDTH,
+		'readonly' => $this->data['expression_field_readonly']
+	)
+);
+if ($this->data['expression_field_readonly'] == 'yes') {
+	$triggersForm->addVar('expression', $this->data['expression']);
+}
+
+$addExpressionButton = new CButton(
+	'insert',
+	($this->data['input_method'] == IM_TREE) ? _('Edit') : _('Add'),
+	'return PopUp("popup_trexpr.php?dstfrm='.$triggersForm->getName().
+		'&dstfld1='.$this->data['expression_field_name'].'&srctbl=expression'.url_param('parent_discoveryid').
+		'&srcfld1=expression&expression=" + escape('.$this->data['expression_field_params'].'), 800, 265);',
+	'formlist'
+);
+if ($this->data['limited'] == 'yes') {
+	$addExpressionButton->setAttribute('disabled', 'disabled');
+}
+$expressionRow = array($expressionTextBox, $addExpressionButton);
+
+if ($this->data['input_method'] == IM_TREE) {
+	// insert macro button
+	$insertMacroButton = new CButton('insert_macro', _('Insert expression'), null, 'formlist');
+	$insertMacroButton->setMenuPopup(CMenuPopupHelper::getTriggerMacro());
+	if ($this->data['limited']) {
+		$insertMacroButton->setAttribute('disabled', 'disabled');
+	}
+	$expressionRow[] = $insertMacroButton;
+
+	array_push($expressionRow, BR());
+	if (empty($this->data['outline'])) {
+		// add button
+		$addExpressionButton = new CSubmit('add_expression', _('Add'), null, 'formlist');
+		if ($this->data['limited'] == 'yes') {
+			$addExpressionButton->setAttribute('disabled', 'disabled');
+		}
+		array_push($expressionRow, $addExpressionButton);
+	}
+	else {
+		// add button
+		$addExpressionButton = new CSubmit('and_expression', _('And'), null, 'formlist');
+		if ($this->data['limited'] == 'yes') {
+			$addExpressionButton->setAttribute('disabled', 'disabled');
+		}
+		array_push($expressionRow, $addExpressionButton);
+
+		// or button
+		$orExpressionButton = new CSubmit('or_expression', _('Or'), null, 'formlist');
+		if ($this->data['limited'] == 'yes') {
+			$orExpressionButton->setAttribute('disabled', 'disabled');
+		}
+		array_push($expressionRow, $orExpressionButton);
+
+		// replace button
+		$replaceExpressionButton = new CSubmit('replace_expression', _('Replace'), null, 'formlist');
+		if ($this->data['limited'] == 'yes') {
+			$replaceExpressionButton->setAttribute('disabled', 'disabled');
+		}
+		array_push($expressionRow, $replaceExpressionButton);
+	}
+}
+elseif ($this->data['input_method'] != IM_FORCED) {
+	$inputMethodToggle = new CSpan(_('Expression constructor'), 'link');
+	$inputMethodToggle->setAttribute('onclick', 'javascript: '.
+		'document.getElementById("toggle_input_method").value=1;'.
+		'document.getElementById("input_method").value='.(($this->data['input_method'] == IM_TREE) ? IM_ESTABLISHED : IM_TREE).';'.
+		'document.forms["'.$triggersForm->getName().'"].submit();'
+	);
+	$expressionRow[] = array(BR(), $inputMethodToggle);
+}
+$triggersFormList->addRow(_('Expression'), $expressionRow);
+
+// append expression table to form list
+if ($this->data['input_method'] == IM_TREE) {
+	$expressionTable = new CTable(null, 'formElementTable');
+	$expressionTable->setAttribute('style', 'min-width: 500px;');
+	$expressionTable->setAttribute('id', 'exp_list');
+	$expressionTable->setOddRowClass('even_row');
+	$expressionTable->setEvenRowClass('even_row');
+	$expressionTable->setHeader(array(
+		($this->data['limited'] == 'yes') ? null : _('Target'),
+		_('Expression'),
+		empty($this->data['parent_discoveryid']) ? _('Error') : null,
+		($this->data['limited'] == 'yes') ? null : _('Action')
+	));
+
+	$allowedTesting = true;
+	if (!empty($this->data['eHTMLTree'])) {
+		foreach ($this->data['eHTMLTree'] as $i => $e) {
+			if ($this->data['limited'] != 'yes') {
+				$deleteUrl = new CSpan(_('Delete'), 'link');
+				$deleteUrl->setAttribute('onclick', 'javascript:'.
+					' if (Confirm("'._('Delete expression?').'")) {'.
+						' delete_expression("'.$e['id'] .'");'.
+						' document.forms["'.$triggersForm->getName().'"].submit();'.
+					' }'
+				);
+				$triggerCheckbox = new CCheckbox('expr_target_single', ($i == 0) ? 'yes' : 'no', 'check_target(this);', $e['id']);
+			}
+			else {
+				$triggerCheckbox = null;
+			}
+
+			if (empty($this->data['parent_discoveryid'])) {
+				if (!isset($e['expression']['levelErrors'])) {
+					$errorImg = new CImg('images/general/ok_icon.png', 'expression_no_errors');
+					$errorImg->setHint(_('No errors found.'));
+				}
+				else {
+					$allowedTesting = false;
+					$errorImg = new CImg('images/general/error2.png', 'expression_errors');
+					$errorTexts = array();
+					if (is_array($e['expression']['levelErrors'])) {
+						foreach ($e['expression']['levelErrors'] as $expVal => $errTxt) {
+							if (count($errorTexts) > 0) {
+								array_push($errorTexts, BR());
+							}
+							array_push($errorTexts, $expVal, ':', $errTxt);
+						}
+					}
+					$errorImg->setHint($errorTexts, '', 'left');
+				}
+				$errorColumn = new CCol($errorImg, 'center');
+			}
+			else {
+				$errorColumn = null;
+			}
+
+			// templated trigger
+			if ($this->data['limited'] == 'yes') {
+				// make all links inside inactive
+				$listSize = count($e['list']);
+				for ($i = 0; $i < $listSize; $i++) {
+					if (gettype($e['list'][$i]) == 'object' && get_class($e['list'][$i]) == 'CSpan' && $e['list'][$i]->getAttribute('class') == 'link') {
+						$e['list'][$i]->removeAttribute('class');
+						$e['list'][$i]->setAttribute('onclick', '');
+					}
+				}
+			}
+
+			$row = new CRow(array($triggerCheckbox, $e['list'], $errorColumn, isset($deleteUrl) ? $deleteUrl : null));
+			$expressionTable->addRow($row);
+		}
+	}
+	else {
+		$allowedTesting = false;
+		$this->data['outline'] = '';
+	}
+
+	$testButton = new CButton('test_expression', _('Test'),
+		'openWinCentered("tr_testexpr.php?expression=" + encodeURIComponent(this.form.elements["expression"].value),'.
+		'"ExpressionTest", 850, 400, "titlebar=no, resizable=yes, scrollbars=yes"); return false;',
+		'link_menu'
+	);
+	if (!$allowedTesting) {
+		$testButton->setAttribute('disabled', 'disabled');
+	}
+	if (empty($this->data['outline'])) {
+		$testButton->setAttribute('disabled', 'disabled');
+	}
+
+	$wrapOutline = new CSpan(array($this->data['outline']));
+	$triggersFormList->addRow(SPACE, array(
+		$wrapOutline,
+		BR(),
+		BR(),
+		new CDiv(array($expressionTable, $testButton), 'objectgroup inlineblock border_dotted ui-corner-all')
+	));
+
+	$inputMethodToggle = new CSpan(_('Close expression constructor'), 'link');
+	$inputMethodToggle->setAttribute('onclick', 'javascript: '.
+		'document.getElementById("toggle_input_method").value=1;'.
+		'document.getElementById("input_method").value='.IM_ESTABLISHED.';'.
+		'document.forms["'.$triggersForm->getName().'"].submit();'
+	);
+	$triggersFormList->addRow(SPACE, array($inputMethodToggle, BR()));
+}
+
+$triggersFormList->addRow(_('Multiple PROBLEM events generation'), new CCheckBox('type', (($this->data['type'] == TRIGGER_MULT_EVENT_ENABLED) ? 'yes' : 'no'), null, 1));
+$triggersFormList->addRow(_('Description'), new CTextArea('comments', $this->data['comments']));
+$triggersFormList->addRow(_('URL'), new CTextBox('url', $this->data['url'], ZBX_TEXTBOX_STANDARD_SIZE));
+$triggersFormList->addRow(_('Severity'), new CSeverity(array('name' => 'priority', 'value' => $this->data['priority'])));
+
+// append status to form list
+if (empty($this->data['triggerid']) && empty($this->data['form_refresh'])) {
+	$status = 'yes';
+}
+else {
+	$status = ($this->data['status'] == 0) ? 'yes' : 'no';
+}
+$triggersFormList->addRow(_('Enabled'), new CCheckBox('status', $status, null, 1));
+
+// append tabs to form
+$triggersTab = new CTabView();
+if (!$this->data['form_refresh']) {
+	$triggersTab->setSelected(0);
+}
+$triggersTab->addTab(
+	'triggersTab',
+	empty($this->data['parent_discoveryid']) ? _('Trigger') : _('Trigger prototype'), $triggersFormList
+);
+
+/*
+ * Dependencies tab
+ */
+if (empty($this->data['parent_discoveryid'])) {
+	$dependenciesFormList = new CFormList('dependenciesFormList');
+	$dependenciesTable = new CTable(_('No dependencies defined.'), 'formElementTable');
+	$dependenciesTable->setAttribute('style', 'min-width: 500px;');
+	$dependenciesTable->setAttribute('id', 'dependenciesTable');
+	$dependenciesTable->setHeader(array(_('Name'), _('Action')));
+
+	foreach ($this->data['db_dependencies'] as $dependency) {
+		$triggersForm->addVar('dependencies[]', $dependency['triggerid'], 'dependencies_'.$dependency['triggerid']);
+
+		$hostNames = array();
+		foreach ($dependency['hosts'] as $host) {
+			$hostNames[] = CHtml::encode($host['name']);
+			$hostNames[] = ', ';
+		}
+		array_pop($hostNames);
+
+		if ($dependency['flags'] == ZBX_FLAG_DISCOVERY_NORMAL) {
+			$description = new CLink(
+				array($hostNames, NAME_DELIMITER, CHtml::encode($dependency['description'])),
+				'triggers.php?form=update&hostid='.$dependency['hostid'].'&triggerid='.$dependency['triggerid']
+			);
+			$description->setAttribute('target', '_blank');
+		}
+		else {
+			$description = array($hostNames, NAME_DELIMITER, $dependency['description']);
+		}
+
+		$row = new CRow(array($description, new CButton('remove', _('Remove'),
+			'javascript: removeDependency("'.$dependency['triggerid'].'");',
+			'link_menu'
+		)));
+
+		$row->setAttribute('id', 'dependency_'.$dependency['triggerid']);
+		$dependenciesTable->addRow($row);
+	}
+	$dependenciesFormList->addRow(
+		_('Dependencies'),
+		new CDiv(
+			array(
+				$dependenciesTable,
+				new CButton('bnt1', _('Add'),
+					'return PopUp("popup.php?'.
+						'srctbl=triggers'.
+						'&srcfld1=triggerid'.
+						'&reference=deptrigger'.
+						'&multiselect=1'.
+						'&with_triggers=1", 1000, 700);',
+					'link_menu'
+				)
+			),
+			'objectgroup inlineblock border_dotted ui-corner-all'
+		)
+	);
+	$triggersTab->addTab('dependenciesTab', _('Dependencies'), $dependenciesFormList);
+}
+
+// append tabs to form
+$triggersForm->addItem($triggersTab);
+
+// append buttons to form
+$buttons = array();
+if (!empty($this->data['triggerid'])) {
+	$buttons[] = new CSubmit('clone', _('Clone'));
+
+	$deleteButton = new CButtonDelete(
+		$this->data['parent_discoveryid'] ? _('Delete trigger prototype?') : _('Delete trigger?'),
+		url_params(array('form', 'groupid', 'hostid', 'triggerid', 'parent_discoveryid'))
+	);
+	if ($this->data['limited']) {
+		$deleteButton->setAttribute('disabled', 'disabled');
+	}
+	$buttons [] = $deleteButton;
+}
+$buttons[] = new CButtonCancel(url_params(array('groupid', 'hostid', 'parent_discoveryid')));
+$triggersForm->addItem(makeFormFooter(
+	new CSubmit('save', _('Save')),
+	array($buttons)
+));
+
+$triggersWidget->addItem($triggersForm);
+
+return $triggersWidget;
