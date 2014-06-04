1543d5ea19f2093bb4860e79887461359bc53507
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-04 08:47:50 +0000

    ...G...PS. [ZBX-8172] added allocation for NULL values
    git-svn-id: svn://svn.zabbix.com/branches/dev@46174 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8172/src/libs/zbxjson/json.c
+++ ZBX-8172/src/libs/zbxjson/json.c
@@ -738,7 +738,15 @@ static const char	*zbx_json_decodevalue_dyn(const char *p, char **string, size_t
 		case ZBX_JSON_TYPE_NULL:
 			if (NULL != is_null)
 				*is_null = 1;
+
+			if (*string_alloc < 1)
+			{
+				*string_alloc = 1;
+				*string = zbx_realloc(*string, *string_alloc);
+			}
+
 			**string = '\0';
+
 			return zbx_json_decodenull(p);
 		default:
 			return NULL;
