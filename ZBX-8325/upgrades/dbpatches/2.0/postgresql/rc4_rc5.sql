169d9a4c7ee9b087e9aa79a33ba5848d70e4e250
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-10 14:40:46 +0000

    .......... [ZBX-8325] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@46366 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-8325/upgrades/dbpatches/2.0/postgresql/rc4_rc5.sql
@@ -0,0 +1,5 @@
+ALTER TABLE ONLY config ADD server_check_interval integer DEFAULT '60' NOT NULL;
+ALTER TABLE ONLY items ALTER lastlogsize TYPE numeric(20);
+INSERT INTO graph_theme (graphthemeid, description, theme, backgroundcolor, graphcolor, graphbordercolor, gridcolor, maingridcolor, gridbordercolor, textcolor, highlightcolor, leftpercentilecolor, rightpercentilecolor, nonworktimecolor, gridview, legendview) VALUES ((SELECT MAX(graphthemeid) + 1 FROM graph_theme), 'Dark orange', 'darkorange', '333333', '0A0A0A', '888888', '222222', '4F4F4F', 'EFEFEF', 'DFDFDF', 'FF5500', 'FF5500', 'FF1111', '1F1F1F', 1, 1);
+INSERT INTO graph_theme (graphthemeid, description, theme, backgroundcolor, graphcolor, graphbordercolor, gridcolor, maingridcolor, gridbordercolor, textcolor, highlightcolor, leftpercentilecolor, rightpercentilecolor, nonworktimecolor, gridview, legendview) VALUES ((SELECT MAX(graphthemeid) + 1 FROM graph_theme), 'Classic', 'classic', 'F0F0F0', 'FFFFFF', '333333', 'CCCCCC', 'AAAAAA', '000000', '222222', 'AA4444', '11CC11', 'CC1111', 'E0E0E0', 1, 1);
+DELETE FROM ids WHERE table_name = 'graph_theme';
