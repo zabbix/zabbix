ed7724b3fa00a0336abe866419c1659b96d89116
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-10 10:20:38 +0000

    .......... [ZBX-8287] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@46349 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8287-22/upgrades/dbpatches/1.4/mysql/patch/httpstepitem.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE httpstepitem (
-	httpstepitemid		bigint unsigned		DEFAULT '0'	NOT NULL,
-	httpstepid		bigint unsigned		DEFAULT '0'	NOT NULL,
-	itemid		bigint unsigned		DEFAULT '0'	NOT NULL,
-	type		integer		DEFAULT '0'	NOT NULL,
-	PRIMARY KEY (httpstepitemid)
-) ENGINE=InnoDB;
-CREATE UNIQUE INDEX httpstepitem_httpstepitem_1 on httpstepitem (httpstepid,itemid);
