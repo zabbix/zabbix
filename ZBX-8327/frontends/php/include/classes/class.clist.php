f947f60638b2f385eb3dcd16bf38365fc3ff82c4
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-10 07:24:31 +0000

    .......... [ZBX-8327] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@46330 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-8327/frontends/php/include/classes/class.clist.php
@@ -0,0 +1,54 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CList extends CTag {
+
+	public $emptyList;
+
+	public function __construct($value = null, $class = null) {
+		parent::__construct('ul', 'yes');
+		$this->tag_end = '';
+		$this->addItem($value);
+		$this->addClass($class);
+
+		if (is_null($value)) {
+			$this->addItem(_('List is empty'), 'empty');
+			$this->emptyList = true;
+		}
+	}
+
+	public function prepareItem($value = null, $class = null, $id = null) {
+		if (!is_null($value)) {
+			$value = new CListItem($value, $class, $id);
+		}
+
+		return $value;
+	}
+
+	public function addItem($value, $class = null, $id = null) {
+		if (!is_null($value) && $this->emptyList) {
+			$this->emptyList = false;
+			$this->items = array();
+		}
+
+		parent::addItem($this->prepareItem($value, $class, $id));
+	}
+}
---
+++ ZBX-8327/src/zabbix_proxy/servercomms.h
@@ -0,0 +1,36 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#ifndef ZABBIX_SERVERCOMMS_H
+#define ZABBIX_SERVERCOMMS_H
+
+extern char	*CONFIG_SOURCE_IP;
+extern char	*CONFIG_SERVER;
+extern int	CONFIG_SERVER_PORT;
+extern char	*CONFIG_HOSTNAME;
+
+#include "comms.h"
+
+int	connect_to_server(zbx_sock_t *sock, int timeout, int retry_interval);
+void	disconnect_server(zbx_sock_t *sock);
+
+int	get_data_from_server(zbx_sock_t *sock, const char *request, char **data);
+int	put_data_to_server(zbx_sock_t *sock, struct zbx_json *j);
+
+#endif
