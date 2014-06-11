6083ae1adb2acebd001043ad0843216e64f86555
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-11 13:45:45 +0000

    ...G...PS. [ZBX-8343] fixed a bug in binary heap that could cause a slightly inconsistent ordering of elements in the queue
    git-svn-id: svn://svn.zabbix.com/branches/1.8@46411 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxalgo/binaryheap.c
+++ src/libs/zbxalgo/binaryheap.c
@@ -293,8 +293,9 @@ void	zbx_binary_heap_remove_direct(zbx_binary_heap_t *heap, zbx_uint64_t key)
 		{
 			heap->elems[index] = heap->elems[heap->elems_num];
 
-			if (index == __binary_heap_bubble_down(heap, index))
-				zbx_hashmap_set(heap->key_index, heap->elems[index].key, index);
+			if (index == __binary_heap_bubble_up(heap, index))
+				if (index == __binary_heap_bubble_down(heap, index))
+					zbx_hashmap_set(heap->key_index, heap->elems[index].key, index);
 		}
 	}
 	else
