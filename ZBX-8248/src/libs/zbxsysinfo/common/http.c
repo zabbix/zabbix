1216feee18ef5cef15fa0069606e287f486b4d04
juris <juris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-11 10:02:06 +0000

    ...G...... [ZBX-8248] corrected style; removed unused variable
    git-svn-id: svn://svn.zabbix.com/branches/dev@46385 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8248/src/libs/zbxsysinfo/common/http.c
+++ ZBX-8248/src/libs/zbxsysinfo/common/http.c
@@ -141,7 +141,7 @@ int	WEB_PAGE_PERF(AGENT_REQUEST *request, AGENT_RESULT *result)
 int	WEB_PAGE_REGEXP(AGENT_REQUEST *request, AGENT_RESULT *result)
 {
 	char		*hostname, *path_str, *port_str, *regexp, *length_str, path[MAX_STRING_LEN],
-			*buffer = NULL, *ptr = NULL, *s, *n, *p;
+			*buffer = NULL, *ptr = NULL, *s, *p;
 	int		length;
 	const char	*output;
 	unsigned short	port_number;
@@ -196,7 +196,7 @@ int	WEB_PAGE_REGEXP(AGENT_REQUEST *request, AGENT_RESULT *result)
 				if ('\n' == *(s + 1))
 					offset++;
 
-				if ((ptr = zbx_regexp_sub(p, regexp, output)) != NULL)
+				if (NULL != (ptr = zbx_regexp_sub(p, regexp, output)))
 					break;
 
 				s += offset;
