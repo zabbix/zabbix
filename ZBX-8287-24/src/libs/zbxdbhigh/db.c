0dfa9491449743914947454fbc753eb91df0c628
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-11 15:20:54 +0000

    .......PS. [ZBX-8287] updated to the latest trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@46427 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8287-24/src/libs/zbxdbhigh/db.c
+++ ZBX-8287-24/src/libs/zbxdbhigh/db.c
@@ -2440,7 +2440,7 @@ out:
 
 /******************************************************************************
  *                                                                            *
- * Function: zbx_db_insert_execute                                            *
+ * Function: zbx_db_insert_autoincrement                                      *
  *                                                                            *
  * Purpose: executes the prepared database bulk insert operation              *
  *                                                                            *
