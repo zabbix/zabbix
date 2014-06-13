f2916c12171b601a4fcb31c96d771c67f4698b73
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-13 14:26:56 +0000

    ........S. [DEV-531] fixed bug when it was theoretically possible for a single value request to return no value
    git-svn-id: svn://svn.zabbix.com/branches/dev@46490 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- DEV-531/src/libs/zbxdbcache/valuecache.c
+++ DEV-531/src/libs/zbxdbcache/valuecache.c
@@ -414,7 +414,11 @@ static int	vc_db_read_values_by_count(zbx_uint64_t itemid, int value_type, zbx_v
 
 	while (-1 != periods[step] && 0 < count)
 	{
-		clock_from = clock_to - periods[step];
+		if (0 > (clock_from = clock_to - periods[step]))
+		{
+			clock_from = clock_to;
+			step = 4;
+		}
 
 		sql_offset = 0;
 		zbx_snprintf_alloc(&sql, &sql_alloc, &sql_offset,
@@ -1974,7 +1978,10 @@ static int	vch_item_cache_values_by_count(zbx_vc_item_t *item, int count, int ti
 		zbx_uint64_t			queries = 0;
 
 		if (ZBX_ITEM_STATUS_RELOAD_FIRST == item->status)
-			cache_records -= vch_item_drop_first_second(item);
+		{
+			if (0 > (cache_records -= vch_item_drop_first_second(item)))
+				cache_records = 0;
+		}
 
 		zbx_vector_history_record_create(&records);
 
@@ -2054,7 +2061,7 @@ static int	vch_item_cache_value(zbx_vc_item_t *item, const zbx_timespec_t *ts)
 	}
 
 	/* update cache if necessary */
-	if (0 < update_seconds && ZBX_ITEM_STATUS_CACHED_ALL != item->status)
+	if (ZBX_ITEM_STATUS_CACHED_ALL != item->status)
 	{
 		zbx_vector_history_record_t	records;
 		zbx_uint64_t			queries = 0;
@@ -2066,11 +2073,14 @@ static int	vch_item_cache_value(zbx_vc_item_t *item, const zbx_timespec_t *ts)
 
 		vc_try_unlock();
 
-		/* first try to find the requested value in target second interval */
-		ret = vc_db_read_values_by_time(item->itemid, item->value_type, &records, update_seconds, update_end,
-				&queries);
+		if (0 < update_seconds)
+		{
+			/* first try to find the requested value in target second interval */
+			ret = vc_db_read_values_by_time(item->itemid, item->value_type, &records, update_seconds,
+					update_end, &queries);
 
-		zbx_vector_history_record_sort(&records, (zbx_compare_func_t)vc_history_record_compare_asc_func);
+			zbx_vector_history_record_sort(&records, (zbx_compare_func_t)vc_history_record_compare_asc_func);
+		}
 
 		/* the target second does not contain the required value, read first value before it */
 		if (0 == records.values_num || 0 > zbx_timespec_compare(ts, &records.values[0].timestamp))
@@ -2759,6 +2769,8 @@ out:
 			vc_update_statistics(NULL, 0, 1);
 			*found = 1;
 		}
+		else
+			ret = SUCCEED;
 	}
 
 	if (NULL != item)
