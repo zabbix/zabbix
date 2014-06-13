4098be696d22edfd052e7aec9e5cdec1a5bb50dd
andrejs <andrejs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-13 14:18:58 +0000

    .......... [ZBXNEXT-686] created epic development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@46489 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-686/upgrades/dbpatches/2.0/oracle/patch/hostmacro.sql
@@ -0,0 +1,19 @@
+ALTER TABLE hostmacro MODIFY hostmacroid DEFAULT NULL;
+ALTER TABLE hostmacro MODIFY hostid DEFAULT NULL;
+DELETE FROM hostmacro WHERE NOT hostid IN (SELECT hostid FROM hosts);
+-- remove duplicates to allow unique index
+DELETE FROM hostmacro
+	WHERE hostmacroid IN (
+		SELECT hm1.hostmacroid
+		FROM hostmacro hm1
+		LEFT OUTER JOIN (
+			SELECT MIN(hm2.hostmacroid) AS hostmacroid
+			FROM hostmacro hm2
+			GROUP BY hm2.hostid,hm2.macro
+		) keep_rows ON
+			hm1.hostmacroid=keep_rows.hostmacroid
+		WHERE keep_rows.hostmacroid IS NULL
+	);
+DROP INDEX hostmacro_1;
+CREATE UNIQUE INDEX hostmacro_1 ON hostmacro (hostid,macro);
+ALTER TABLE hostmacro ADD CONSTRAINT c_hostmacro_1 FOREIGN KEY (hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
