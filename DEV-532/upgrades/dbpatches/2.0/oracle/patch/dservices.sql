a3a5289eed50524c396a039c641b0455a7b55413
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-13 14:30:15 +0000

    .......... [DEV-532] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@46492 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ DEV-532/upgrades/dbpatches/2.0/oracle/patch/dservices.sql
@@ -0,0 +1,10 @@
+ALTER TABLE dservices MODIFY dserviceid DEFAULT NULL;
+ALTER TABLE dservices MODIFY dhostid DEFAULT NULL;
+ALTER TABLE dservices MODIFY dcheckid DEFAULT NULL;
+ALTER TABLE dservices MODIFY key_ DEFAULT '';
+ALTER TABLE dservices MODIFY value DEFAULT '';
+ALTER TABLE dservices ADD dns nvarchar2(64) DEFAULT '';
+DELETE FROM dservices WHERE NOT dhostid IN (SELECT dhostid FROM dhosts);
+DELETE FROM dservices WHERE NOT dcheckid IN (SELECT dcheckid FROM dchecks);
+ALTER TABLE dservices ADD CONSTRAINT c_dservices_1 FOREIGN KEY (dhostid) REFERENCES dhosts (dhostid) ON DELETE CASCADE;
+ALTER TABLE dservices ADD CONSTRAINT c_dservices_2 FOREIGN KEY (dcheckid) REFERENCES dchecks (dcheckid) ON DELETE CASCADE;
