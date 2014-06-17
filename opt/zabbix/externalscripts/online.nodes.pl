3bb1762b153e1553c0251eb45d486e9fd164510d
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-17 14:16:19 +0000

    ....I..... [RSM-1] online.nodes.pl: fix calculation of number of online probes for the particular service
    Item zabbix[proxy,{$RSM.PROXY_NAME},lastaccess] contains the probe
    last access time. Last access time should not be over limit defined in
    a {$RSM.PROBE.AVAIL.LIMIT} macro (currently 60 seconds). Before this
    fix last access time was calculated by subtracting lastaccess from
    current time while instead of current time we should use the timestamp
    of the value.
    
    
    git-svn-id: svn://svn.zabbix.com/branches/2.0.rsm@46620 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- opt/zabbix/externalscripts/online.nodes.pl
+++ opt/zabbix/externalscripts/online.nodes.pl
@@ -10,13 +10,44 @@ use RSM;
 use Data::Dumper;
 
 my $command = shift || 'total';
-
 my $type = shift || 'dns';
+
+die("$command: invalid command") if ($command ne 'total' and $command ne 'online');
+die("$type: invalid type") if ($type ne 'dns' and $type ne 'epp' and $type ne 'rdds' and $type ne 'ipv4' and $type ne 'ipv6');
+
+use constant DEBUG => 0;
+use constant LOGFILE => '/tmp/online.nodes.debug.log';
+
+sub ts_str
+{
+    my $ts = shift;
+    $ts = time() unless ($ts);
+
+    my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime($ts);
+
+    $year += 1900;
+    $mon++;
+    return sprintf("%4.2d/%2.2d/%2.2d-%2.2d:%2.2d:%2.2d", $year, $mon, $mday, $hour, $min, $sec);
+}
+
+sub dbg
+{
+    return unless (DEBUG == 1);
+
+    my $msg = join('', @_);
+
+    my $OUTFILE;
+
+    open $OUTFILE, '>>', LOGFILE or die("cannot open file ", LOGFILE, ": $!");
+
+    print {$OUTFILE} ts_str(), " ", $msg, "\n" or die("cannot write to file ", LOGFILE, ": $!");
+
+    close $OUTFILE or die("cannot close file ", LOGFILE, ": $!");
+}
 
 my $hosts;
 
 my $config = get_rsm_config();
-
 
 my $dbh = DBI->connect('DBI:mysql:'.$config->{'db'}->{'name'}.':'.$config->{'db'}->{'host'},
                                            $config->{'db'}->{'user'},
@@ -34,6 +65,8 @@ $sth->finish;
 
 my $max_diff = $macro[0];
 
+dbg("probe availability limit: $max_diff seconds");
+
 my $sql = "select hostid, host FROM hosts h JOIN hosts_groups hg USING(hostid) JOIN groups g USING(groupid) WHERE g.name = ?";
 
 my $sth = $dbh->prepare($sql) or die $dbh->errstr;
@@ -43,17 +76,19 @@ $sth->execute('Probes') or die $dbh->errstr;
 while (my $row = $sth->fetchrow_hashref) {
     my $hostid = $row->{'hostid'};
     my @templates;
-    push(@templates, $row->{'hostid'});
+    push(@templates, $hostid);
+
+    dbg("checking probe ", $row->{'host'}, "...");
 
     $hosts->{$hostid}->{'name'} = $row->{'host'};
 
-    @templates = (@templates, get_parent_templateids($row->{'hostid'}));
+    @templates = (@templates, get_parent_templateids($hostid));
 
     foreach my $templateid (@templates) {
 	my $sql = "SELECT hostid, macro, value FROM hostmacro WHERE hostid = ?";
 
-	my $sth = $dbh->prepare($sql) or die $dbh->errstr;               
-                                                                 
+	my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+
 	$sth->execute($templateid) or die $dbh->errstr;
 
 	while (my $macro = $sth->fetchrow_hashref) {
@@ -63,13 +98,14 @@ while (my $row = $sth->fetchrow_hashref) {
 	$sth->finish;
     }
 
-    my $sql = "SELECT IFNULL(min(lastvalue),1) as value FROM items where hostid = ? AND key_ LIKE 'rsm.probe.status[%]'";
+    my $sql = "SELECT key_,IFNULL(min(lastvalue),1) as value FROM items where hostid = ? AND key_ LIKE 'rsm.probe.status[%]'";
 
     my $sth = $dbh->prepare($sql) or die $dbh->errstr;
 
     $sth->execute($hostid) or die $dbh->errstr;
 
     while (my $item = $sth->fetchrow_hashref) {
+        dbg("  ", $item->{'key_'}, ": ", $item->{'value'});
 	$hosts->{$hostid}->{'status'} = $item->{'value'};
     }
 
@@ -82,7 +118,8 @@ while (my $row = $sth->fetchrow_hashref) {
     $sth->execute($row->{'host'}.' - mon') or die $dbh->errstr;
 
     while (my $item = $sth->fetchrow_hashref) {
-	$hosts->{$hostid}->{'status'} = 0 if (time - $item->{'lastvalue'} > $max_diff);
+        dbg("  last seen: ", ($item->{'lastclock'} - $item->{'lastvalue'}), " seconds ago (lastvalue: ", $item->{'lastvalue'}, ")");
+	$hosts->{$hostid}->{'status'} = 0 if ($item->{'lastclock'} - $item->{'lastvalue'} > $max_diff);
     }
 
     $sth->finish;
@@ -93,7 +130,7 @@ my $online = 0;
 
 foreach my $hostid (keys %{$hosts}) {
     my $status = $hosts->{$hostid}->{'status'};
-    
+
     if ($type eq 'dns') {
 	$online++ if $status == 1;
 	$total++;
@@ -115,6 +152,9 @@ foreach my $hostid (keys %{$hosts}) {
         $total++ if $hosts->{$hostid}->{'{$RSM.IP6.ENABLED}'} == 1;
     }
 }
+
+dbg($total, " total probes available for $type tests") if ($command eq 'total');
+dbg($online, " online probes available for $type tests") if ($command eq 'online');
 
 print $total if $command eq 'total';
 print $online if $command eq 'online';
