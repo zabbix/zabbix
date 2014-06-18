5c816639e2ec0de6ac63217569f67f6b77a15188
andrejs <andrejs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-18 13:34:50 +0000

    A.F....... [ZBX-8151] added comment
    git-svn-id: svn://svn.zabbix.com/branches/dev@46655 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8151-20/frontends/php/include/defines.inc.php
+++ ZBX-8151-20/frontends/php/include/defines.inc.php
@@ -836,6 +836,11 @@ define('XML_TAG_DEPENDENCY',		'dependency');
 define('ZBX_DEFAULT_IMPORT_HOST_GROUP', 'Imported hosts');
 
 // XML import flags
+// See ZBX-8151. Old version of libxml suffered from setting DTDLOAD and NOENT flags by default, which allowed
+// performing XXE attacks. Calling libxml_disable_entity_loader(true) also had no affect if flags passed to libxml
+// calls were 0 - so for better security with legacy libxml we need to call libxml_disable_entity_loader(true) AND
+// pass the LIBXML_NONET flag. Please keep in mind that LIBXML_NOENT actually EXPANDS entities, opposite to it's name -
+// so this flag is not needed here.
 define('LIBXML_IMPORT_FLAGS', LIBXML_NONET);
 
 // API errors
