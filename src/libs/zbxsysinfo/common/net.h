5a62686bcf991bc1b67ec9c756718c05b889acaf
Andris Zeila <andris.zeila@zabbix.com>
2014-06-19 06:27:49 +0000

    .......PS. [ZBX-8040] coding style fixes

--- src/libs/zbxsysinfo/common/net.h
+++ src/libs/zbxsysinfo/common/net.h
@@ -88,9 +88,9 @@
 
 extern char	*CONFIG_SOURCE_IP;
 
-#define ZBX_TCP_EXPECT_FAIL		(-1)
-#define ZBX_TCP_EXPECT_OK		(0)
-#define ZBX_TCP_EXPECT_IGNORE		(1)
+#define ZBX_TCP_EXPECT_FAIL	-1
+#define ZBX_TCP_EXPECT_OK	0
+#define ZBX_TCP_EXPECT_IGNORE	1
 
 int	tcp_expect(const char *host, unsigned short port, int timeout, const char *request,
 		int(*validate_func)(const char *), const char *sendtoclose, int *value_int);
