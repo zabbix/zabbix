b2c80c3fe14238779783ecddbd8cc7e94e5ee7d0
andrejs <andrejs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-19 13:10:19 +0000

    ..F....... [ZBX-8278] refactored checking of mbstring extension, first php 5.6 adaptation
    git-svn-id: svn://svn.zabbix.com/branches/dev@46697 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8278/frontends/php/include/locales.inc.php
+++ ZBX-8278/frontends/php/include/locales.inc.php
@@ -21,19 +21,25 @@
 
 function init_mbstrings() {
 	$res = true;
-	$res &= mbstrings_available();
-	ini_set('mbstring.internal_encoding', 'UTF-8');
-	$res &= (ini_get('mbstring.internal_encoding') == 'UTF-8');
+	$res &= extension_loaded('mbstring');
+
+	if (version_compare(PHP_VERSION, '5.6', '<')) {
+		ini_set('mbstring.internal_encoding', 'UTF-8');
+		$res &= (ini_get('mbstring.internal_encoding') == 'UTF-8');
+	} else {
+		ini_set('default_charset', 'UTF-8');
+		$res &= (ini_get('default_charset') == 'UTF-8');
+		ini_set('php.internal_encoding', 'UTF-8');
+		$res &= (ini_get('php.internal_encoding') == 'UTF-8');
+	}
+
 	ini_set('mbstring.detect_order', 'UTF-8, ISO-8859-1, JIS, SJIS');
 	$res &= (ini_get('mbstring.detect_order') == 'UTF-8, ISO-8859-1, JIS, SJIS');
 	if ($res) {
 		define('ZBX_MBSTRINGS_ENABLED', true);
 	}
+	var_dump($res);
 	return $res;
-}
-
-function mbstrings_available() {
-	return function_exists('mb_strlen') && function_exists('mb_strtoupper') && function_exists('mb_strpos') && function_exists('mb_substr');
 }
 
 /**
