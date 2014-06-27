b068dea6035ec45e42a4e57bd891062d57f5288e
krists <krists@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-06-27 14:27:54 +0000

    ..F....... [ZBXNEXT-2124] Fix CSS for application filter input.
    git-svn-id: svn://svn.zabbix.com/branches/2.2.filter@46906 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/views/js/configuration.sysmaps.js.php
+++ frontends/php/include/views/js/configuration.sysmaps.js.php
@@ -117,7 +117,7 @@
 			<tr id="application-select-row">
 				<td><?php echo _('Application'); ?></td>
 				<td>
-					<input size="50" id="application" name="application" style="width: 312px;">
+					<input size="50" id="application" name="application" class="input">
 					<input id="application-select" type="button" class="input link_menu select-popup" value="<?php echo _('Select'); ?>">
 				</td>
 			</tr>
