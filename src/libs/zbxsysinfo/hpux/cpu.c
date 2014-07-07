e1c3a470a78e41b854f9be02fab33713f525eb8e
Juris Miščenko <git-no-reply@zabbix.com>
2014-07-07 13:21:26 +0000

    ...G...... [ZBXNEXT-1300] added system.cpu.discovery to HPUX

--- src/libs/zbxsysinfo/hpux/cpu.c
+++ src/libs/zbxsysinfo/hpux/cpu.c
@@ -21,6 +21,51 @@
 #include "sysinfo.h"
 #include "stats.h"
 #include "log.h"
+#include "zbxalgo.h"
+#include "zbxjson.h"
+
+int	SYSTEM_CPU_DISCOVERY(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+	int			ret = SYSINFO_RET_FAIL, i;
+	zbx_vector_uint64_t	cpus;
+	struct zbx_json		json;
+
+	do
+	{
+		zbx_vector_uint64_create(&cpus);
+
+		if (SYSINFO_RET_OK != get_cpu_statuses(&cpus))
+		{
+			SET_MSG_RESULT(result, zbx_strdup(NULL, "Collector is not started."));
+			zbx_vector_uint64_destroy(&cpus);
+			break;
+		}
+
+		zbx_json_init(&json, ZBX_JSON_STAT_BUF_LEN);
+		zbx_json_addarray(&json, ZBX_PROTO_TAG_DATA);
+
+		for (i = 0; i < cpus.values_num; i++)
+		{
+			zbx_json_addobject(&json, NULL);
+
+			zbx_json_adduint64(&json, "{#CPUID}", i);
+			zbx_json_addstring(&json, "{#STATUS}", (SYSINFO_RET_OK == cpus.values[i] ?
+					"online" : "offline"), ZBX_JSON_TYPE_STRING);
+
+			zbx_json_close(&json);
+		}
+
+		zbx_json_close(&json);
+		SET_STR_RESULT(result, zbx_strdup(result->str, json.buffer));
+
+		zbx_json_free(&json);
+		zbx_vector_uint64_destroy(&cpus);
+
+		ret = SYSINFO_RET_OK;
+	} while(0);
+
+	return ret;
+}
 
 int	SYSTEM_CPU_NUM(AGENT_REQUEST *request, AGENT_RESULT *result)
 {
