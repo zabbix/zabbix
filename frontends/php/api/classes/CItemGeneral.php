d73939cfe613ffa2b91fbbe83b3cee5b16b74e68
krists <krists@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2014-07-08 14:05:17 +0000

    A.F....... [ZBX-8397] Remove outdated queue related code from frontend and API.
    git-svn-id: svn://svn.zabbix.com/trunk@47134 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/api/classes/CItemGeneral.php
+++ frontends/php/api/classes/CItemGeneral.php
@@ -321,8 +321,8 @@ abstract class CItemGeneral extends CApiService {
 
 			// update interval
 			if ($fullItem['type'] != ITEM_TYPE_TRAPPER && $fullItem['type'] != ITEM_TYPE_SNMPTRAP) {
-				$res = calculateItemNextcheck(0, $fullItem['type'], $fullItem['delay'], $fullItem['delay_flex'], time());
-				if ($res == ZBX_JAN_2038) {
+				$nextCheck = calculateItemNextCheck(0, $fullItem['delay'], $fullItem['delay_flex'], time());
+				if ($nextCheck == ZBX_JAN_2038) {
 					self::exception(ZBX_API_ERROR_PARAMETERS,
 						_('Item will not be refreshed. Please enter a correct update interval.'));
 				}
