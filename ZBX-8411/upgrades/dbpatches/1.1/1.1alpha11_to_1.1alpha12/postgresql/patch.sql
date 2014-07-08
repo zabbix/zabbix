45b2ec6b1f0a43f43b3adca420b4e7079946b74c
krists <krists@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-07-08 14:21:58 +0000

    .......... [ZBX-8411] remove development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@47140 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8411/upgrades/dbpatches/1.1/1.1alpha11_to_1.1alpha12/postgresql/patch.sql
+++
@@ -1,3 +0,0 @@
-alter table history_log add timestamp int4 DEFAULT '0' NOT NULL;
-alter table history_log add source varchar(64) DEFAULT '' NOT NULL;
-alter table history_log add severity int4 DEFAULT '0' NOT NULL;
