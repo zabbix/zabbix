2091f99a2e7a0a72a01ff341d92e9ea3c0409c16
juris <juris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-07-09 08:27:02 +0000

    ...G...... [ZBXNEXT-1300] added an `unknown' state to cpu discovery on Windows when the collector has accounted for a processor but has no data on it.
    git-svn-id: svn://svn.zabbix.com/branches/dev@47150 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1300_2/src/libs/zbxsysinfo/win32/cpu.c
+++ ZBXNEXT-1300_2/src/libs/zbxsysinfo/win32/cpu.c
@@ -57,7 +57,9 @@ int	SYSTEM_CPU_DISCOVERY(AGENT_REQUEST *request, AGENT_RESULT *result)
 
 			zbx_json_adduint64(&json, "{#CPUID}", i);
 			zbx_json_addstring(&json, "{#STATUS}", (PERF_COUNTER_ACTIVE == cpus.values[i]) ?
-					"online" : "offline", ZBX_JSON_TYPE_STRING);
+					"online" :
+					(PERF_COUNTER_INITIALIZED == cpus.values[i]) ? "unknown" : "offline",
+					ZBX_JSON_TYPE_STRING);
 
 			zbx_json_close(&json);
 		}
