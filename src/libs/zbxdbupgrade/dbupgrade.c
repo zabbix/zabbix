f0a1367e8163f1a69f87bf1a3f7118e7288edce8
Juris Miščenko <git-no-reply@zabbix.com>
2014-07-11 13:50:38 +0000

    .......PS. [ZBX-8307] made sqlite upgrade message appear only when the manditory version is high than the binary uses.

--- src/libs/zbxdbupgrade/dbupgrade.c
+++ src/libs/zbxdbupgrade/dbupgrade.c
@@ -740,7 +740,9 @@ int	DBcheck_version(void)
 				" Required mandatory version: %08d.",
 				ZBX_DAEMON_TYPE_SERVER == daemon_type ? "server" : "proxy",
 				db_mandatory, db_optional, required);
-		zabbix_log(LOG_LEVEL_CRIT, "Zabbix does not support SQLite3 database upgrade.");
+
+		if (required < db_mandatory)
+			zabbix_log(LOG_LEVEL_CRIT, "Zabbix does not support SQLite3 database upgrade.");
 
 		goto out;
 	}
