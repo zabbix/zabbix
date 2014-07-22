bd9595d0dba3802d711953ffc73d4a6707ecdbab
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-07-22 13:00:34 +0000

    .......... [ZBX-7623] removed outdated development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@47513 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7623/build/win32/project/zabbix_agent_desc.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ZABBIX_AGENT_DESC_H_
-#define _ZABBIX_AGENT_DESC_H_
-
-#define VER_FILEDESCRIPTION_STR		"Zabbix agent is deployed on a host to monitor resources and applications and send data to Zabbix server."
-
-#endif
