eaae8cad108e56cb351a663f56b7b82fadc2c40d
krists <krists@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-07-24 12:34:27 +0000

    ..F....... [ZBXNEXT-282] Fixed hiding and showing authentication input rows depending on authentication type.
    git-svn-id: svn://svn.zabbix.com/branches/dev@47582 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-282/frontends/php/include/views/js/configuration.httpconf.edit.js.php
+++ ZBXNEXT-282/frontends/php/include/views/js/configuration.httpconf.edit.js.php
@@ -199,13 +199,17 @@
 		$('#authentication').on('change', function() {
 			var httpFieldsDisabled = $(this).val() == 0; // 0 == HTTPTEST_AUTH_NONE
 
-			$('#http_user')
-				.attr('disabled', httpFieldsDisabled)
-				.closest('li').toggleClass('hidden');
+			$('#http_user').attr('disabled', httpFieldsDisabled);
+			$('#http_password').attr('disabled', httpFieldsDisabled);
 
-			$('#http_password')
-				.attr('disabled', httpFieldsDisabled)
-				.closest('li').toggleClass('hidden');
+			if(httpFieldsDisabled) {
+				$('#http_user').closest('li').addClass('hidden');
+				$('#http_password').closest('li').addClass('hidden');
+			}
+			else {
+				$('#http_user').closest('li').removeClass('hidden');
+				$('#http_password').closest('li').removeClass('hidden');
+			}
 		});
 	});
 </script>
