78d45a8d3b924bff0f86af8d519f208c35cc4722
Krists Krigers <git-no-reply@zabbix.com>
2014-07-25 13:48:58 +0000

    A......... [ZBXNEXT-1583] Add constants for new screen resource types.

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -520,6 +520,8 @@ define('SCREEN_RESOURCE_HOST_TRIGGERS',		16);
 // used in Monitoring > Latest data > Graph (history.php)
 define('SCREEN_RESOURCE_HISTORY',			17);
 define('SCREEN_RESOURCE_CHART',				18);
+define('SCREEN_RESOURCE_LLD_SIMPLE_GRAPH',	19);
+define('SCREEN_RESOURCE_LLD_GRAPH',			20);
 
 define('SCREEN_SORT_TRIGGERS_DATE_DESC',			0);
 define('SCREEN_SORT_TRIGGERS_SEVERITY_DESC',		1);
