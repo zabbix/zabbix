1cce82bdbdc0d52d387225da737d2c9edc59dc3a
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-07-27 20:05:46 +0000

    .......... [ZBX-6910] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@47627 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6910/upgrades/dbpatches/2.0/mysql/rc4_rc5.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE config ADD server_check_interval integer DEFAULT '60' NOT NULL;
-ALTER TABLE items MODIFY lastlogsize bigint unsigned DEFAULT '0' NOT NULL;
-SET @graphthemeid = (SELECT MAX(graphthemeid) FROM graph_theme);
-INSERT INTO graph_theme (graphthemeid, description, theme, backgroundcolor, graphcolor, graphbordercolor, gridcolor, maingridcolor, gridbordercolor, textcolor, highlightcolor, leftpercentilecolor, rightpercentilecolor, nonworktimecolor, gridview, legendview) VALUES (@graphthemeid + 1, 'Dark orange', 'darkorange', '333333', '0A0A0A', '888888', '222222', '4F4F4F', 'EFEFEF', 'DFDFDF', 'FF5500', 'FF5500', 'FF1111', '1F1F1F', 1, 1), (@graphthemeid + 2, 'Classic', 'classic', 'F0F0F0', 'FFFFFF', '333333', 'CCCCCC', 'AAAAAA', '000000', '222222', 'AA4444', '11CC11', 'CC1111', 'E0E0E0', 1, 1);
-DELETE FROM ids WHERE table_name = 'graph_theme';
