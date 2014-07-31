7dd2462c6a559e448015206b9e896a04371f14aa
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-07-31 12:48:21 +0000

    .......... [DEV-535] created new dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@47694 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ DEV-535/frontends/php/include/views/administration.general.triggerDisplayOptions.edit.php
@@ -0,0 +1,121 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+include('include/views/js/administration.general.triggerDisplayOptions.js.php');
+
+$triggerDOFormList = new CFormList('scriptsTab');
+
+$headerDiv = new CDiv(_('Colour'), 'inlineblock trigger_displaying_form_col');
+$headerDiv->addStyle('margin-left: 2px;');
+$triggerDOFormList->addRow(SPACE, array($headerDiv, _('Blinking')));
+
+// Unacknowledged problem events
+$triggerDOFormList->addRow(
+	_('Unacknowledged PROBLEM events'),
+	array(
+		new CDiv(
+			new CColor('problem_unack_color', $this->data['problem_unack_color']),
+			'inlineblock trigger_displaying_form_col'
+		),
+		new CCheckBox(
+			'problem_unack_style',
+			$this->data['problem_unack_style'] == 1,
+			null,
+			1
+		)
+	)
+);
+
+// Acknowledged problem events
+$triggerDOFormList->addRow(
+	_('Acknowledged PROBLEM events'),
+	array(
+		new CDiv(
+			new CColor('problem_ack_color', $this->data['problem_ack_color']),
+			'inlineblock trigger_displaying_form_col'
+		),
+		new CCheckBox(
+			'problem_ack_style',
+			$this->data['problem_ack_style'] == 1,
+			null,
+			1
+		)
+	)
+);
+
+// Unacknowledged recovery events
+$triggerDOFormList->addRow(
+	_('Unacknowledged OK events'),
+	array(
+		new CDiv(
+			new CColor('ok_unack_color', $this->data['ok_unack_color']),
+			'inlineblock trigger_displaying_form_col'
+		),
+		new CCheckBox(
+			'ok_unack_style',
+			$this->data['ok_unack_style'] == 1,
+			null,
+			1
+		)
+	)
+);
+
+// Acknowledged recovery events
+$triggerDOFormList->addRow(
+	_('Acknowledged OK events'),
+	array(
+		new CDiv(
+			new CColor('ok_ack_color', $this->data['ok_ack_color']),
+			'inlineblock trigger_displaying_form_col'
+		),
+		new CCheckBox(
+			'ok_ack_style',
+			$this->data['ok_ack_style'] == 1,
+			null,
+			1
+		)
+	)
+);
+
+// some air between the sections
+$triggerDOFormList->addRow(BR());
+
+// Display OK triggers
+$okPeriodTextBox = new CTextBox('ok_period', $this->data['ok_period']);
+$okPeriodTextBox->addStyle('width: 4em;');
+$okPeriodTextBox->setAttribute('maxlength', '6');
+$triggerDOFormList->addRow(_('Display OK triggers for'), array($okPeriodTextBox, SPACE, _('seconds')));
+
+// Triggers blink on status change
+$okPeriodTextBox = new CTextBox('blink_period', $this->data['blink_period']);
+$okPeriodTextBox->addStyle('width: 4em;');
+$okPeriodTextBox->setAttribute('maxlength', '6');
+$triggerDOFormList->addRow(_('On status change triggers blink for'), array($okPeriodTextBox, SPACE, _('seconds')));
+
+$severityView = new CTabView();
+$severityView->addTab('triggerdo', _('Trigger displaying options'), $triggerDOFormList);
+
+$severityForm = new CForm();
+$severityForm->setName('triggerDisplayOptions');
+$severityForm->addItem($severityView);
+$severityForm->addItem(makeFormFooter(new CSubmit('save', _('Save')), new CButton('resetDefaults', _('Reset defaults'))));
+
+return $severityForm;
---
+++ DEV-535/src/libs/zbxsysinfo/hpux/hpux.c
@@ -0,0 +1,48 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+#include "sysinfo.h"
+
+ZBX_METRIC	parameters_specific[] =
+/* 	KEY			FLAG		FUNCTION 		TEST PARAMETERS */
+{
+	{"vfs.fs.size",		CF_HAVEPARAMS,	VFS_FS_SIZE,		"/,free"},
+	{"vfs.fs.inode",	CF_HAVEPARAMS,	VFS_FS_INODE,		"/,free"},
+	{"vfs.fs.discovery",	0,		VFS_FS_DISCOVERY,	NULL},
+
+	{"net.if.discovery",	0,		NET_IF_DISCOVERY,	NULL},
+	{"net.if.in",		CF_HAVEPARAMS,	NET_IF_IN,		"lan0,bytes"},
+	{"net.if.out",		CF_HAVEPARAMS,	NET_IF_OUT,		"lan0,bytes"},
+	{"net.if.total",	CF_HAVEPARAMS,	NET_IF_TOTAL,		"lan0,bytes"},
+
+	{"vm.memory.size",	CF_HAVEPARAMS,	VM_MEMORY_SIZE,		"free"},
+
+	{"proc.num",            CF_HAVEPARAMS,  PROC_NUM,               "inetd"},
+
+	{"system.cpu.util",	CF_HAVEPARAMS,	SYSTEM_CPU_UTIL,	"all,user,avg1"},
+	{"system.cpu.load",	CF_HAVEPARAMS,	SYSTEM_CPU_LOAD,	"all,avg1"},
+	{"system.cpu.num",	CF_HAVEPARAMS,	SYSTEM_CPU_NUM,		"online"},
+	{"system.cpu.discovery",0,		SYSTEM_CPU_DISCOVERY,	NULL},
+
+	{"system.uname",	0,		SYSTEM_UNAME,		NULL},
+	{"system.sw.arch",	0,		SYSTEM_SW_ARCH,		NULL},
+
+	{NULL}
+};
