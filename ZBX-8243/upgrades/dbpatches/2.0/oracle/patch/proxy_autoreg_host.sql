4433a34fff8c05446b62b357735970379953b653
juris <juris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-08-01 07:55:29 +0000

    .......... [ZBX-8243] Removed development branch.
    git-svn-id: svn://svn.zabbix.com/branches/dev@47732 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8243/upgrades/dbpatches/2.0/oracle/patch/proxy_autoreg_host.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE proxy_autoreg_host ADD listen_ip nvarchar2(39) DEFAULT '';
-ALTER TABLE proxy_autoreg_host ADD listen_port number(10) DEFAULT '0' NOT NULL;
-ALTER TABLE proxy_autoreg_host ADD listen_dns nvarchar2(64) DEFAULT '';
