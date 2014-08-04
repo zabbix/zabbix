a8571415303d4671628530221dc1cdc63f9e5b36
Andrejs Cirkovs <git-no-reply@zabbix.com>
2014-08-04 06:34:06 +0000

    ..F....... [ZBX-8458] fixed inline block for trigger selector

--- frontends/php/include/views/configuration.action.edit.php
+++ frontends/php/include/views/configuration.action.edit.php
@@ -349,7 +349,7 @@ $conditionTable = new CTable(null, 'newActionConditionTable');
 $conditionTable->addRow(array($conditionTypeComboBox, $conditionOperatorsComboBox, $condition));
 $conditionTable->addRow(array(new CSubmit('add_condition', _('Add'), null, 'link_menu'), SPACE, SPACE));
 
-$conditionFormList->addRow(_('New condition'), new CDiv($conditionTable, 'objectgroup inlineblock border_dotted ui-corner-all'));
+$conditionFormList->addRow(_('New condition'), new CDiv($conditionTable, 'objectgroup floatleft border_dotted ui-corner-all'));
 
 /*
  * Operation tab
