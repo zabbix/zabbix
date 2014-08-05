804d6e517cb2c18c662308900a97508e70d58313
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-08-05 11:28:04 +0000

    ..F....... [ZBX-8510] minor coding style fixes
    git-svn-id: svn://svn.zabbix.com/branches/dev@47816 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8510-trunk/frontends/php/api/classes/CService.php
+++ ZBX-8510-trunk/frontends/php/api/classes/CService.php
@@ -186,7 +186,7 @@ class CService extends CApiService {
 			$this->addTimes($serviceTimes);
 		}
 
-		updateITServices();
+		updateItServices();
 
 		return array('serviceids' => $serviceIds);
 	}
@@ -322,7 +322,7 @@ class CService extends CApiService {
 			$this->addTimes($serviceTimes);
 		}
 
-		updateITServices();
+		updateItServices();
 
 		return array('serviceids' => zbx_objectValues($services, 'serviceid'));
 	}
@@ -357,7 +357,7 @@ class CService extends CApiService {
 
 		DB::delete($this->tableName(), array('serviceid' => $serviceIds));
 
-		updateITServices();
+		updateItServices();
 
 		return array('serviceids' => $serviceIds);
 	}
