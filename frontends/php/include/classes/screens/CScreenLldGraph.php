4f4acb411fde5a2030a3f38ebe1ac68b08134ac0
Krists Krigers <git-no-reply@zabbix.com>
2014-08-11 13:24:03 +0000

    A......... [ZBXNEXT-1583] Implement graph previews to be shown when screen is in configuration mode or there are no discovered graphs or items for selected graph or item prototype.

--- frontends/php/include/classes/screens/CScreenLldGraph.php
+++ frontends/php/include/classes/screens/CScreenLldGraph.php
@@ -22,11 +22,21 @@
 class CScreenLldGraph extends CScreenLldGraphBase {
 
 	/**
+	 * @var array
+	 */
+	protected $createdGraphIds = array();
+
+	/**
+	 * @var array
+	 */
+	protected $graphPrototype = null;
+
+	/**
 	 * Adds graph items to surrogate screen.
 	 */
 	public function addSurrogateScreenItems() {
-		$discoveredGraphIds = $this->getCreatedGraphIds();
-		$this->addGraphsToSurrogateScreen($discoveredGraphIds);
+		$createdGraphIds = $this->getCreatedGraphIds();
+		$this->addGraphsToSurrogateScreen($createdGraphIds);
 	}
 
 	/**
@@ -36,27 +46,28 @@ class CScreenLldGraph extends CScreenLldGraphBase {
 	 * @return array
 	 */
 	protected function getCreatedGraphIds() {
-		$graphPrototypeId = $this->screenitem['resourceid'];
+		if (!$this->createdGraphIds) {
+			$graphPrototypeId = $this->getGraphPrototypeId();
 
-		$hostId = $this->getCurrentHostId();
+			$hostId = $this->getCurrentHostId();
 
-		// get all created (discovered) graphs for current graph host
-		$allCreatedGraphs = API::Graph()->get(array(
-			'hostids' => array($hostId),
-			'output' => array('graphid'),
-			'selectGraphDiscovery' => array('graphid', 'parent_graphid'),
-			'filter' => array('flags' => ZBX_FLAG_DISCOVERY_CREATED)
-		));
+			// get all created (discovered) graphs for current graph host
+			$allCreatedGraphs = API::Graph()->get(array(
+				'hostids' => array($hostId),
+				'output' => array('graphid'),
+				'selectGraphDiscovery' => array('graphid', 'parent_graphid'),
+				'filter' => array('flags' => ZBX_FLAG_DISCOVERY_CREATED)
+			));
 
-		// collect those graph IDs where parent graph is graph prototype selected for this screen item as resource
-		$createdGraphIds = array();
-		foreach ($allCreatedGraphs as $graph) {
-			if ($graph['graphDiscovery']['parent_graphid'] == $graphPrototypeId) {
-				$createdGraphIds[] = $graph['graphid'];
+			// collect those graph IDs where parent graph is graph prototype selected for this screen item as resource
+			foreach ($allCreatedGraphs as $graph) {
+				if ($graph['graphDiscovery']['parent_graphid'] == $graphPrototypeId) {
+					$this->createdGraphIds[] = $graph['graphid'];
+				}
 			}
 		}
 
-		return $createdGraphIds;
+		return $this->createdGraphIds;
 	}
 
 	/**
@@ -67,29 +78,117 @@ class CScreenLldGraph extends CScreenLldGraphBase {
 	protected function addGraphsToSurrogateScreen(array $graphIds) {
 		$screenItemTemplate = $this->getScreenItemTemplate(SCREEN_RESOURCE_GRAPH);
 
+		$screenItems = array();
 		foreach ($graphIds as $graphId) {
 			$screenItem = $screenItemTemplate;
 
 			$screenItem['resourceid'] = $graphId;
 			$screenItem['screenitemid'] = 'z' . $graphId;
 
-			$this->surrogateScreen['screenitems'][] = $screenItem;
+			$screenItems[] = $screenItem;
 		}
+
+		$this->addItemsToSurrogateScreen($screenItems);
 	}
 
 	/**
 	 * @return integer
 	 */
 	function getHostIdFromScreenItemResource() {
-		$graphPrototype = API::GraphPrototype()->get(array(
-			'graphids' => $this->screenitem['resourceid'],
-			'output' => array('graphid'),
-			'selectDiscoveryRule' => array('hostid')
-		));
-		$graphPrototype = reset($graphPrototype);
+		$graphPrototype = $this->getGraphPrototype();
 
-		$hostId = $graphPrototype['discoveryRule']['hostid'];
+		return $graphPrototype['discoveryRule']['hostid'];
+	}
 
-		return $hostId;
+	/**
+	 * @return mixed
+	 */
+	protected function getGraphPrototypeId() {
+		return $this->screenitem['resourceid'];
+	}
+
+	/**
+	 * @return boolean
+	 */
+	protected function mustShowPreview() {
+		$createdGraphIds = $this->getCreatedGraphIds();
+
+		if ($createdGraphIds) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+
+	/**
+	 * @return CDiv
+	 */
+	protected function getPreview() {
+		$graphPrototype = $this->getGraphPrototype();
+
+		unset($graphPrototype['graphid']);
+
+		switch ($graphPrototype['graphtype']) {
+			case GRAPH_TYPE_NORMAL:
+			case GRAPH_TYPE_STACKED:
+				$url = 'chart3.php';
+				break;
+
+			case GRAPH_TYPE_EXPLODED:
+			case GRAPH_TYPE_3D_EXPLODED:
+			case GRAPH_TYPE_3D:
+			case GRAPH_TYPE_PIE:
+				$url = 'chart7.php';
+				break;
+
+			case GRAPH_TYPE_BAR:
+			case GRAPH_TYPE_COLUMN:
+			case GRAPH_TYPE_BAR_STACKED:
+			case GRAPH_TYPE_COLUMN_STACKED:
+				$url = 'chart_bar.php';
+				break;
+
+			default:
+				show_error_message(_('Graph prototype not found.'));
+				exit;
+		}
+
+		$queryParams = array(
+			'items' => $graphPrototype['gitems'],
+			'graphtype' => $graphPrototype['graphtype'],
+			'period' => 3600,
+			'legend' => $graphPrototype['show_legend'],
+			'graph3d' => $graphPrototype['show_3d'],
+			'width' => $this->screenitem['width'],
+			'height' => $this->screenitem['height'],
+			'name' => $graphPrototype['name']
+		);
+
+		$url .= '?'.http_build_query($queryParams);
+
+		$img = new CImg($url);
+
+		return $img;
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getGraphPrototype() {
+		if (!$this->graphPrototype) {
+			$options = array(
+				'output' => array('name', 'graphtype', 'show_legend', 'show_3d'),
+				'graphids' => array($this->getGraphPrototypeId()),
+				'selectDiscoveryRule' => array('hostid'),
+				'selectGraphItems' => array(
+					'gitemid', 'itemid', 'sortorder', 'flags', 'type', 'calc_fnc',  'drawtype', 'yaxisside', 'color'
+				)
+			);
+			$graphPrototype = API::GraphPrototype()->get($options);
+			$this->graphPrototype = reset($graphPrototype);
+		}
+
+		return $this->graphPrototype;
 	}
 }
