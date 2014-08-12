9ffcc832780f7356740d290c17c75ac96fd8f4e5
Pavels Jelisejevs <git-no-reply@zabbix.com>
2014-08-12 07:30:10 +0000

    ..F....... [ZBXNEXT-570] changed the default latest data action dropdown value to GRAPH_TYPE_STACKED

--- frontends/php/latest.php
+++ frontends/php/latest.php
@@ -841,9 +841,9 @@ foreach ($hosts as $hostId => $dbHost) {
 
 zbx_add_post_js('chkbxRange.pageGoName = "itemids";');
 
-$goBox = new CComboBox('graphtype', null, null, array(
-	GRAPH_TYPE_NORMAL => _('Display graph'),
-	GRAPH_TYPE_STACKED => _('Display stacked graph')
+$goBox = new CComboBox('graphtype', GRAPH_TYPE_STACKED, null, array(
+	GRAPH_TYPE_STACKED => _('Display stacked graph'),
+	GRAPH_TYPE_NORMAL => _('Display graph')
 ));
 $goBox->setAttribute('id', 'go');
 $goButton = new CSubmit('goButton', _('Go').' (0)');
