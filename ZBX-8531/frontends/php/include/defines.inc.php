13b2783ff34158fcefbd93e23b9394a88a56b06a
krists <krists@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-08-13 10:07:15 +0000

    .......... [ZBX-8531] merge from trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@48053 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8531/frontends/php/include/defines.inc.php
+++ ZBX-8531/frontends/php/include/defines.inc.php
@@ -19,9 +19,9 @@
 **/
 
 
-define('ZABBIX_VERSION',     '2.3.3');
-define('ZABBIX_API_VERSION', '2.3.3');
-define('ZABBIX_DB_VERSION',	 2030112);
+define('ZABBIX_VERSION',     '2.3.4');
+define('ZABBIX_API_VERSION', '2.3.4');
+define('ZABBIX_DB_VERSION',	 2030117);
 
 define('ZABBIX_COPYRIGHT_FROM', '2001');
 define('ZABBIX_COPYRIGHT_TO',   '2014');
