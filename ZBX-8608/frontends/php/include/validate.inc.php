3809db21b6dd3cb1de924d9be3d80804225a1ce8
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-08-14 07:26:18 +0000

    ..F....PS. [ZBX-8608] added support of IP masks for action conditions
    git-svn-id: svn://svn.zabbix.com/branches/dev@48083 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8608/frontends/php/include/validate.inc.php
+++ ZBX-8608/frontends/php/include/validate.inc.php
@@ -106,7 +106,7 @@
 
 	/**
 	 * Validate IP mask. IP/bits.
-	 * bits range for IPv4: 16 - 32
+	 * bits range for IPv4: 16 - 30
 	 * bits range for IPv6: 112 - 128
 	 *
 	 * @param string $ip_range
@@ -123,7 +123,7 @@
 		$bits = $parts[1];
 
 		if (validate_ipv4($ip, $arr)) {
-			return preg_match('/^\d{1,2}$/', $bits) && $bits >= 16 && $bits <= 32;
+			return preg_match('/^\d{1,2}$/', $bits) && $bits >= 16 && $bits <= 30;
 		}
 		elseif (defined('ZBX_HAVE_IPV6') && validate_ipv6($ip, $arr)) {
 			return preg_match('/^\d{1,3}$/', $bits) && $bits >= 112 && $bits <= 128;
