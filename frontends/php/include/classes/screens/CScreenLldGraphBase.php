65615a2b41d41ec652344f822a9f7bc314cde826
Krists Krigers <git-no-reply@zabbix.com>
2014-08-15 09:49:21 +0000

    A......... [ZBXNEXT-1583] Add class description.

--- frontends/php/include/classes/screens/CScreenLldGraphBase.php
+++ frontends/php/include/classes/screens/CScreenLldGraphBase.php
@@ -18,7 +18,9 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-
+/**
+ * Base class for screen elements containing surrogate screen with graphs from sources generated from prototypes.
+ */
 abstract class CScreenLldGraphBase extends CScreenBase {
 
 	/**
