67cb5f8c2f6fac0e7818c948033df150312a1faa
igors <igors@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2014-08-18 12:22:08 +0000

    ........S. [ZBX-8444] database queries for enabled items, triggers and hosts replaced with queries from configuration cache to improve performance for processing sleeping escalations
    git-svn-id: svn://svn.zabbix.com/trunk@48166 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- include/dbcache.h
+++ include/dbcache.h
@@ -170,7 +170,9 @@ typedef struct _DC_TRIGGER
 	zbx_uint64_t	triggerid;
 	char		*description;
 	char		*expression_orig;
+	/* temporary value, allocated during processing and freed right after */
 	char		*expression;
+
 	char		*error;
 	char		*new_error;
 	zbx_timespec_t	timespec;
@@ -298,14 +300,17 @@ void	init_configuration_cache();
 void	free_configuration_cache();
 void	DCload_config();
 
+void	DCconfig_get_triggers_by_triggerids(DC_TRIGGER *triggers, const zbx_uint64_t *triggerids, int *errcode,
+		size_t num);
 void	DCconfig_clean_items(DC_ITEM *items, int *errcodes, size_t num);
 int	DCget_host_by_hostid(DC_HOST *host, zbx_uint64_t hostid);
 void	DCconfig_get_items_by_keys(DC_ITEM *items, zbx_host_key_t *keys, int *errcodes, size_t num);
-void	DCconfig_get_items_by_itemids(DC_ITEM *items, zbx_uint64_t *itemids, int *errcodes, size_t num);
+void	DCconfig_get_items_by_itemids(DC_ITEM *items, const zbx_uint64_t *itemids, int *errcodes, size_t num);
 void	DCconfig_set_item_db_state(zbx_uint64_t itemid, unsigned char state, const char *error);
 void	DCconfig_get_functions_by_functionids(DC_FUNCTION *functions,
 		zbx_uint64_t *functionids, int *errcodes, size_t num);
 void	DCconfig_clean_functions(DC_FUNCTION *functions, int *errcodes, size_t num);
+void	DCconfig_clean_triggers(DC_TRIGGER *triggers, int *errcodes, size_t num);
 void	DCconfig_lock_triggers_by_itemids(zbx_uint64_t *itemids, int itemids_num, zbx_vector_uint64_t *triggerids);
 void	DCconfig_unlock_triggers(const zbx_vector_uint64_t *triggerids);
 void	DCconfig_get_triggers_by_itemids(zbx_hashset_t *trigger_info, zbx_vector_ptr_t *trigger_order,
