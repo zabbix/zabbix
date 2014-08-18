7e04f9897b8094cfa8f5874339a799d113c73da2
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-08-18 06:27:48 +0000

    .......PS. [ZBX-8608] fixed formatting
    git-svn-id: svn://svn.zabbix.com/branches/dev@48153 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8608/src/libs/zbxcommon/misc.c
+++ ZBX-8608/src/libs/zbxcommon/misc.c
@@ -909,7 +909,8 @@ static int	ip6_in_list(char *list, const char *ip)
 			goto next;
 
 		if (i[0] != j[0] || i[1] != j[1] || i[2] != j[2] || i[3] != j[3] ||
-				i[4] != j[4] || i[5] != j[5] || i[6] != j[6]) {
+				i[4] != j[4] || i[5] != j[5] || i[6] != j[6])
+		{
 			goto next;
 		}
 
