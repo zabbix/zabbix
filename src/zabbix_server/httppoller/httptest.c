17c53b4b4120051a5f51ef271890515cdecb9600
Igors Homjakovs <git-no-reply@zabbix.com>
2014-08-19 11:46:06 +0000

    .......PS. [ZBXNEXT-101] fixed web content logging

--- src/zabbix_server/httppoller/httptest.c
+++ src/zabbix_server/httppoller/httptest.c
@@ -445,6 +445,8 @@ static void	process_httptest(DC_HOST *host, zbx_httptest_t *httptest)
 		{
 			char	*var_err_str = NULL;
 
+			zabbix_log(LOG_LEVEL_TRACE, "page data received from %s: %s", httpstep.url, page.data);
+
 			/* first get the data that is needed even if step fails */
 			if (CURLE_OK != (err = curl_easy_getinfo(easyhandle, CURLINFO_RESPONSE_CODE, &stat.rspcode)))
 			{
@@ -453,12 +455,8 @@ static void	process_httptest(DC_HOST *host, zbx_httptest_t *httptest)
 			else if ('\0' != *httpstep.status_codes &&
 					FAIL == int_in_list(httpstep.status_codes, stat.rspcode))
 			{
-				err_str = zbx_strdup(err_str, "status code did not match");
-
-				zabbix_log(LOG_LEVEL_TRACE, "In %s() httptestid:" ZBX_FS_UI64 " name:'%s'."
-						" Status code \"%s\" did not match with response code \"%d\" ",
-						__function_name, httptest->httptest.httptestid,
-						httptest->httptest.name, httpstep.status_codes, stat.rspcode);
+				err_str = zbx_dsprintf(err_str, "status code \"%s\" did not match with"
+						" response code \"%d\"", httpstep.status_codes, stat.rspcode);
 			}
 
 			if (CURLE_OK != (err = curl_easy_getinfo(easyhandle, CURLINFO_TOTAL_TIME, &stat.total_time)) &&
@@ -482,13 +480,8 @@ static void	process_httptest(DC_HOST *host, zbx_httptest_t *httptest)
 			if (NULL == err_str && '\0' != *httpstep.required && NULL == zbx_regexp_match(page.data,
 					httpstep.required, NULL))
 			{
-				err_str = zbx_strdup(err_str, "required pattern not found");
-
-				zabbix_log(LOG_LEVEL_TRACE, "In %s() httptestid:" ZBX_FS_UI64 " name:'%s'."
-						" Required pattern \"%s\" not found on %s",
-						__function_name, httptest->httptest.httptestid,
-						httptest->httptest.name, httpstep.required, httpstep.url);
-				zabbix_log(LOG_LEVEL_TRACE, "page data: %s", page.data);
+				err_str = zbx_dsprintf(err_str, "required pattern \"%s\" was not found on %s",
+						httpstep.required, httpstep.url);
 			}
 
 			/* variables defined in scenario */
@@ -500,12 +493,6 @@ static void	process_httptest(DC_HOST *host, zbx_httptest_t *httptest)
 				variables = string_replace(httptest->httptest.variables, "\r\n", " ");
 				err_str = zbx_dsprintf(err_str, "error in scenario variables \"%s\": %s",
 						variables, var_err_str);
-
-				zabbix_log(LOG_LEVEL_TRACE, "In %s() httptestid:" ZBX_FS_UI64 " name:'%s'."
-						" Error in scenario variables \"%s\": %s",
-						__function_name, httptest->httptest.httptestid,
-						httptest->httptest.name, variables, var_err_str);
-				zabbix_log(LOG_LEVEL_TRACE, "page data: %s", page.data);
 
 				zbx_free(variables);
 			}
@@ -519,12 +506,6 @@ static void	process_httptest(DC_HOST *host, zbx_httptest_t *httptest)
 				variables = string_replace(httpstep.variables, "\r\n", " ");
 				err_str = zbx_dsprintf(err_str, "error in step variables \"%s\": %s",
 						variables, var_err_str);
-
-				zabbix_log(LOG_LEVEL_TRACE, "In %s() httptestid:" ZBX_FS_UI64 " name:'%s'."
-						" Error in step variables \"%s\": %s",
-						__function_name, httptest->httptest.httptestid,
-						httptest->httptest.name, variables, var_err_str);
-				zabbix_log(LOG_LEVEL_TRACE, "page data: %s", page.data);
 
 				zbx_free(variables);
 			}
