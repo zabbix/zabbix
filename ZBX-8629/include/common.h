d2384973bbf1c500fbbc4a733239e6da385dbcc1
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-08-21 10:16:58 +0000

    ...G...PS. [ZBX-8629] moved get_string() from str.c to zabbix_sender.c and made it a static function
    git-svn-id: svn://svn.zabbix.com/branches/dev@48283 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8629/include/common.h
+++ ZBX-8629/include/common.h
@@ -813,7 +813,6 @@ int	get_param(const char *param, int num, char *buf, size_t max_len);
 int	num_param(const char *param);
 char	*get_param_dyn(const char *param, int num);
 void	remove_param(char *param, int num);
-const char	*get_string(const char *p, char *buf, size_t bufsize);
 int	get_key_param(char *param, int num, char *buf, size_t max_len);
 int	num_key_param(char *param);
 size_t	zbx_get_escape_string_len(const char *src, const char *charlist);
