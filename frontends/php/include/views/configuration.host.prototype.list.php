49ad8e7edb331f031a0bad32b6e3c97ec88bfe22
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2014-08-22 07:47:35 +0000

    ..F....... [DEV-539] host_prototypes.php: fixed formatting

--- frontends/php/include/views/configuration.host.prototype.list.php
+++ frontends/php/include/views/configuration.host.prototype.list.php
@@ -98,8 +98,13 @@ foreach ($this->data['hostPrototypes'] as $hostPrototype) {
 
 	// status
 	$status = new CLink(item_status2str($hostPrototype['status']),
-		'?group_hostid='.$hostPrototype['hostid'].'&parent_discoveryid='.$discoveryRule['itemid'].
-		'&action='.($hostPrototype['status'] ? 'hostprototype.massenable' : 'hostprototype.massdisable'), itemIndicatorStyle($hostPrototype['status'])
+		'?group_hostid='.$hostPrototype['hostid'].
+			'&parent_discoveryid='.$discoveryRule['itemid'].
+			'&action='.($hostPrototype['status'] == HOST_STATUS_NOT_MONITORED
+				? 'hostprototype.massenable'
+				: 'hostprototype.massdisable'
+			),
+		itemIndicatorStyle($hostPrototype['status'])
 	);
 
 	$hostTable->addRow(array(
