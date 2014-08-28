9f9504c061aea8ebf5e1c764fdfe743846aad0be
Andris Zeila <andris.zeila@zabbix.com>
2014-08-28 13:18:24 +0000

    ...G...PS. [ZBXNEXT-101] removed SIG_CHECKED_FIELD_UINT, forced SIG_CHECKED_FIELD field to int (compilation warnings)

--- src/libs/zbxnix/sigcommon.h
+++ src/libs/zbxnix/sigcommon.h
@@ -23,8 +23,7 @@
 extern int	sig_parent_pid;
 extern int	sig_exiting;
 
-#define SIG_CHECKED_FIELD(siginfo, field)		(NULL == siginfo ? -1 : siginfo->field)
-#define SIG_CHECKED_FIELD_UINT(siginfo, field)		(NULL == siginfo ? (unsigned)-1 : siginfo->field)
+#define SIG_CHECKED_FIELD(siginfo, field)		(NULL == siginfo ? -1 : (int)siginfo->field)
 #define SIG_CHECKED_FIELD_TYPE(siginfo, field, type)	(NULL == siginfo ? (type)-1 : siginfo->field)
 #define SIG_PARENT_PROCESS				(sig_parent_pid == (int)getpid())
 
