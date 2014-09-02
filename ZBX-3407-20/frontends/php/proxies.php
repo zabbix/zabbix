76f04b1fbae1970ba89e6c8aaec9904343cc73bf
krists <krists@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-09-02 10:54:40 +0000

    ..F....... [ZBX-3407] Fix SQL queries to not fail on DB2 by casting to division result to DECIMAL(20,10).
    git-svn-id: svn://svn.zabbix.com/branches/dev@48672 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-3407-20/frontends/php/proxies.php
+++ ZBX-3407-20/frontends/php/proxies.php
@@ -261,7 +261,7 @@ else {
 
 	// calculate performance
 	$dbPerformance = DBselect(
-		'SELECT h.proxy_hostid,SUM(1.0/(i.delay + 0.0)) AS qps'.
+		'SELECT h.proxy_hostid,SUM(CAST(1.0/i.delay AS DECIMAL(20,10))) AS qps'.
 		' FROM items i,hosts h'.
 		' WHERE i.status='.ITEM_STATUS_ACTIVE.
 			' AND i.hostid=h.hostid '.
