ef130cdc3f639883531cee893d2fd402d3901aa8
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-09-04 13:23:42 +0000

    .......... [ZBX-8650] updated to latest from /branches/2.2
    git-svn-id: svn://svn.zabbix.com/branches/dev@48765 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8650/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ ZBX-8650/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	public static final String REVISION_DATE = "27 August 2014";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "2.2.6";
+	public static final String VERSION = "2.2.7rc1";
 
 	public static void printVersion()
 	{
