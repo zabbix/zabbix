163ba25f1aab0db2aff1643e30eb070c50d53f9e
krists <krists@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-09-05 12:41:09 +0000

    .......... [ZBX-8717] merge from trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@48815 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8717/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ ZBX-8717/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	public static final String REVISION_DATE = "4 September 2014";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "2.3.5";
+	public static final String VERSION = "2.3.6";
 
 	public static void printVersion()
 	{
