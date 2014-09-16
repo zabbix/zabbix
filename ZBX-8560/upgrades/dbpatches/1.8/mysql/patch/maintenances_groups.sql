74f152981f20157380bc1b1874b58720017b3887
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-09-16 14:42:03 +0000

    .......... [ZBX-8560] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@49077 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8560/upgrades/dbpatches/1.8/mysql/patch/maintenances_groups.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE maintenances_groups (
-      maintenance_groupid             bigint unsigned         DEFAULT '0'     NOT NULL,
-      maintenanceid           bigint unsigned         DEFAULT '0'     NOT NULL,
-      groupid         bigint unsigned         DEFAULT '0'     NOT NULL,
-      PRIMARY KEY (maintenance_groupid)
-) ENGINE=InnoDB;
-CREATE INDEX maintenances_groups_1 on maintenances_groups (maintenanceid,groupid);
