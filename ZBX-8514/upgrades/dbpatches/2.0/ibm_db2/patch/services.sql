08b2096ed90a819ccf064fea668920ca99c5bc34
juris <juris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-09-24 14:05:38 +0000

    .......... [ZBX-8514] Removed development branch.
    git-svn-id: svn://svn.zabbix.com/branches/dev@49314 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8514/upgrades/dbpatches/2.0/ibm_db2/patch/services.sql
+++
@@ -1,8 +0,0 @@
-UPDATE services SET triggerid = NULL WHERE NOT EXISTS (SELECT 1 FROM triggers t WHERE t.triggerid = services.triggerid)
-/
-ALTER TABLE services ALTER COLUMN serviceid SET WITH DEFAULT NULL
-/
-REORG TABLE services
-/
-ALTER TABLE services ADD CONSTRAINT c_services_1 FOREIGN KEY (triggerid) REFERENCES triggers (triggerid) ON DELETE CASCADE
-/
