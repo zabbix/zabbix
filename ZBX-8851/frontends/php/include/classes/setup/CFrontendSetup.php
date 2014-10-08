a0c498e08a1b32f92530efc209fe77cb148d57b9
krists <krists@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-10-08 14:05:22 +0000

    ..F....... [ZBX-8851] Fix check conditions for "max_execution_time" and "max_input_time" to allow unlimited time.
    git-svn-id: svn://svn.zabbix.com/branches/dev@49690 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8851/frontends/php/include/classes/setup/CFrontendSetup.php
+++ ZBX-8851/frontends/php/include/classes/setup/CFrontendSetup.php
@@ -168,11 +168,13 @@ class CFrontendSetup {
 	public function checkPhpMaxExecutionTime() {
 		$current = ini_get('max_execution_time');
 
+		$currentIsValid = ($current === "0") || ($current === "-1") || ($current >= self::MIN_PHP_MAX_EXECUTION_TIME);
+
 		return array(
 			'name' => _('PHP option max_execution_time'),
 			'current' => $current,
 			'required' => self::MIN_PHP_MAX_EXECUTION_TIME,
-			'result' => ($current >= self::MIN_PHP_MAX_EXECUTION_TIME) ? self::CHECK_OK : self::CHECK_FATAL,
+			'result' => $currentIsValid ? self::CHECK_OK : self::CHECK_FATAL,
 			'error' => _s('Minimum required limit on execution time of PHP scripts is %s (configuration option "max_execution_time").', self::MIN_PHP_MAX_EXECUTION_TIME)
 		);
 	}
@@ -185,11 +187,13 @@ class CFrontendSetup {
 	public function checkPhpMaxInputTime() {
 		$current = ini_get('max_input_time');
 
+		$currentIsValid = ($current === "-1") || ($current >= self::MIN_PHP_MAX_INPUT_TIME);
+
 		return array(
 			'name' => _('PHP option max_input_time'),
 			'current' => $current,
 			'required' => self::MIN_PHP_MAX_INPUT_TIME,
-			'result' => ($current >= self::MIN_PHP_MAX_INPUT_TIME) ? self::CHECK_OK : self::CHECK_FATAL,
+			'result' => $currentIsValid ? self::CHECK_OK : self::CHECK_FATAL,
 			'error' => _s('Minimum required limit on input parse time for PHP scripts is %s (configuration option "max_input_time").', self::MIN_PHP_MAX_INPUT_TIME)
 		);
 	}
