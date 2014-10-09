d33a2a56556bc5f5266d6b250e74944068afbc62
pavels <pavels@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-10-09 12:14:09 +0000

    .......... [ZBXNEXT-686] corrected bootstrap file
    git-svn-id: svn://svn.zabbix.com/branches/dev@49711 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-686/frontends/php/tests/api/bootstrap.php
+++ ZBXNEXT-686/frontends/php/tests/api/bootstrap.php
@@ -2,7 +2,6 @@
 
 // we mostly just need to require autoloader and save test directory
 define('ZABBIX_NEW_TEST_DIR', __DIR__);
-require __DIR__.'/vendor/autoload.php';
 
 // register autoloader
 require_once dirname(__FILE__).'/../../include/classes/core/Z.php';
