3e3e6388640969b55692f89a12d42b4877bcaa6b
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-10-10 15:18:01 +0000

    .......... [ZBX-8072] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@49760 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-8072/frontends/php/include/classes/import/readers/CImportReader.php
@@ -0,0 +1,34 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+abstract class CImportReader {
+
+	/**
+	 * convert string with data in format supported by reader to php array.
+	 *
+	 * @abstract
+	 *
+	 * @param $string
+	 *
+	 * @return array
+	 */
+	abstract public function read($string);
+}
