d54ba54cf33094e31a5c91be355339c2d917cbf2
igors <igors@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2014-10-23 13:17:49 +0000

    .......... [ZBXNEXT-207] undoing incorrectly merged changes
    git-svn-id: svn://svn.zabbix.com/trunk@50125 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/zabbix_proxy/housekeeper/housekeeper.c
+++ src/zabbix_proxy/housekeeper/housekeeper.c
@@ -28,25 +28,6 @@
 extern unsigned char	process_type, daemon_type;
 extern int		server_num, process_num;
 
-static int	hk_period;
-
-/* the maximum number of housekeeping periods to be removed per single housekeeping cycle */
-#define HK_MAX_DELETE_PERIODS	4
-
-void	zbx_housekeeper_sigusr_handler(int flags)
-{
-	if (ZBX_RTC_HOUSEKEEPER_EXECUTE == ZBX_RTC_GET_MSG(flags))
-	{
-		if (0 < zbx_sleep_get_remainder())
-		{
-			zabbix_log(LOG_LEVEL_WARNING, "forced execution of the housekeeper");
-			zbx_wakeup();
-		}
-		else
-			zabbix_log(LOG_LEVEL_WARNING, "housekeeping procedure is already in progress");
-	}
-}
-
 /******************************************************************************
  *                                                                            *
  * Function: delete_history                                                   *
@@ -115,7 +96,7 @@ static int	delete_history(const char *table, const char *fieldname, int now)
 			now - CONFIG_PROXY_OFFLINE_BUFFER * SEC_PER_HOUR,
 			lastid,
 			MIN(now - CONFIG_PROXY_LOCAL_BUFFER * SEC_PER_HOUR,
-					minclock + HK_MAX_DELETE_PERIODS * hk_period));
+					minclock + 4 * CONFIG_HOUSEKEEPING_FREQUENCY * SEC_PER_HOUR));
 
 	DBcommit();
 
@@ -157,21 +138,10 @@ static int	housekeeping_history(int now)
         return records;
 }
 
-static int	get_housekeeper_period(double time_slept)
-{
-	if (SEC_PER_HOUR > time_slept)
-		return SEC_PER_HOUR;
-	else if (24 * SEC_PER_HOUR < time_slept)
-		return 24 * SEC_PER_HOUR;
-	else
-		return (int)time_slept;
-}
-
 ZBX_THREAD_ENTRY(housekeeper_thread, args)
 {
 	int	records, start, sleeptime;
-	double	sec, time_slept;
-	char	sleeptext[25];
+	double	sec;
 
 	process_type = ((zbx_thread_args_t *)args)->process_type;
 	server_num = ((zbx_thread_args_t *)args)->server_num;
@@ -180,34 +150,13 @@ ZBX_THREAD_ENTRY(housekeeper_thread, args)
 	zabbix_log(LOG_LEVEL_INFORMATION, "%s #%d started [%s #%d]", get_daemon_type_string(daemon_type),
 			server_num, get_process_type_string(process_type), process_num);
 
-	if (0 == CONFIG_HOUSEKEEPING_FREQUENCY)
-	{
-		zbx_setproctitle("%s [waiting for user command]", get_process_type_string(process_type));
-		zbx_snprintf(sleeptext, sizeof(sleeptext), "waiting for user command");
-	}
-	else
-	{
-		sleeptime = HOUSEKEEPER_STARTUP_DELAY * SEC_PER_MIN;
-		zbx_setproctitle("%s [startup idle for %d minutes]", get_process_type_string(process_type),
-				HOUSEKEEPER_STARTUP_DELAY);
-		zbx_snprintf(sleeptext, sizeof(sleeptext), "idle for %d hour(s)", CONFIG_HOUSEKEEPING_FREQUENCY);
-	}
+	zbx_setproctitle("%s [startup idle for %d minutes]", get_process_type_string(process_type),
+			HOUSEKEEPER_STARTUP_DELAY);
 
-	zbx_set_sigusr_handler(zbx_housekeeper_sigusr_handler);
+	zbx_sleep_loop(HOUSEKEEPER_STARTUP_DELAY * SEC_PER_MIN);
 
 	for (;;)
 	{
-		sec = zbx_time();
-
-		if (0 == CONFIG_HOUSEKEEPING_FREQUENCY)
-			zbx_sleep_forever();
-		else
-			zbx_sleep_loop(sleeptime);
-
-		time_slept = zbx_time() - sec;
-
-		hk_period = get_housekeeper_period(time_slept);
-
 		start = time(NULL);
 
 		zabbix_log(LOG_LEVEL_WARNING, "executing housekeeper");
@@ -224,13 +173,15 @@ ZBX_THREAD_ENTRY(housekeeper_thread, args)
 
 		DBclose();
 
-		zabbix_log(LOG_LEVEL_WARNING, "%s [deleted %d records in " ZBX_FS_DBL " sec, %s]",
-				get_process_type_string(process_type), records, sec, sleeptext);
+		if (0 > (sleeptime = CONFIG_HOUSEKEEPING_FREQUENCY * SEC_PER_HOUR - (time(NULL) - start)))
+			sleeptime = 0;
 
-		zbx_setproctitle("%s [deleted %d records in " ZBX_FS_DBL " sec, %s]",
-				get_process_type_string(process_type), records, sec, sleeptext);
+		zabbix_log(LOG_LEVEL_WARNING, "%s [deleted %d records in " ZBX_FS_DBL " sec, idle %d sec]",
+				get_process_type_string(process_type), records, sec, sleeptime);
 
-		if (0 != CONFIG_HOUSEKEEPING_FREQUENCY)
-			sleeptime = CONFIG_HOUSEKEEPING_FREQUENCY * SEC_PER_HOUR;
+		zbx_setproctitle("%s [deleted %d records in " ZBX_FS_DBL " sec, idle %d sec]",
+				get_process_type_string(process_type), records, sec, sleeptime);
+
+		zbx_sleep_loop(sleeptime);
 	}
 }
