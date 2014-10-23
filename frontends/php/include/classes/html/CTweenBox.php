10a349003df0536a279d7ec8293949fb7f985381
Pavels Jelisejevs <git-no-reply@zabbix.com>
2014-10-23 14:38:34 +0000

    ..F....... [ZBX-8896] fixed form buttons for the originalblue theme ..F....... [ZBX-8896] removed unnecessary styles ..F....... [ZBX-8896] increased the distance between the host group tween box buttons

--- frontends/php/include/classes/html/CTweenBox.php
+++ frontends/php/include/classes/html/CTweenBox.php
@@ -86,9 +86,9 @@ class CTweenBox {
 			$grp_tab->addRow(array($caption_l, SPACE, $caption_r));
 		}
 
-		$add_btn = new CButton('add', '  &laquo;  ', null, 'formlist');
+		$add_btn = new CButton('add', '  &laquo;  ', null, 'button-form');
 		$add_btn->setAttribute('onclick', 'moveListBoxSelectedItem("'.$this->form->getName().'", "'.$this->varname.'", "'.$this->id_r.'", "'.$this->id_l.'", "add");');
-		$rmv_btn = new CButton('remove', '  &raquo;  ', null, 'formlist');
+		$rmv_btn = new CButton('remove', '  &raquo;  ', null, 'button-form');
 		$rmv_btn->setAttribute('onclick', 'moveListBoxSelectedItem("'.$this->form->getName().'", "'.$this->varname.'", "'.$this->id_l.'", "'.$this->id_r.'", "rmv");');
 
 		$grp_tab->addRow(array($this->lbox, new CCol(array($add_btn, BR(), $rmv_btn)), $this->rbox));
