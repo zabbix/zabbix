ce122b4563c977f2c7a641c8d77a61a27d5e2b2d
igors <igors@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2014-10-24 12:22:06 +0000

    .......PS. [ZBXNEXT-207] undoing chenges in r50125
    git-svn-id: svn://svn.zabbix.com/trunk@50151 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- include/common.h
+++ include/common.h
@@ -380,6 +380,7 @@ zbx_graph_yaxis_types_t;
 
 /* runtime control options */
 #define ZBX_CONFIG_CACHE_RELOAD	"config_cache_reload"
+#define ZBX_HOUSEKEEPER_EXECUTE	"housekeeper_execute"
 #define ZBX_LOG_LEVEL_INCREASE	"log_level_increase"
 #define ZBX_LOG_LEVEL_DECREASE	"log_level_decrease"
 
@@ -728,6 +729,7 @@ zbx_task_t;
 
 #define ZBX_RTC_LOG_LEVEL_INCREASE	1
 #define ZBX_RTC_LOG_LEVEL_DECREASE	2
+#define ZBX_RTC_HOUSEKEEPER_EXECUTE	3
 #define ZBX_RTC_CONFIG_CACHE_RELOAD	8
 
 typedef enum
