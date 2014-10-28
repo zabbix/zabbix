32d215e46ce1d7158976ac7733ef21b8b45716c5
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-10-28 13:32:19 +0000

    .......... [ZBX-8833] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@50244 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-8833/frontends/php/include/views/js/configuration.triggers.edit.js.php
@@ -0,0 +1,33 @@
+<script type="text/javascript">
+	jQuery(document).ready(function() {
+		// create jQuery buttonset object when VisibilityBox is switched on
+		jQuery('#visible_priority').one('click', function() {
+			jQuery('#priority_div').buttonset();
+		});
+	});
+
+	/**
+	 * @see init.js add.popup event
+	 */
+	function addPopupValues(list) {
+		if (!isset('object', list)) {
+			return false;
+		}
+
+		if (list.object == 'deptrigger') {
+			for (var i = 0; i < list.values.length; i++) {
+				create_var('triggersForm', 'new_dependency[' + i + ']', list.values[i].triggerid, false);
+			}
+
+			// return to the same form after it has been submitted
+			jQuery('#action').val(<?php echo CJs::encodeJson(getRequest('action')) ?>);
+
+			create_var('triggersForm', 'add_dependency', 1, true);
+		}
+	}
+
+	function removeDependency(triggerid) {
+		jQuery('#dependency_' + triggerid).remove();
+		jQuery('#dependencies_' + triggerid).remove();
+	}
+</script>
