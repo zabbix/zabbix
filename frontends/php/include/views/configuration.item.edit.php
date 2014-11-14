b11c66ec25a72258dc9bd5432e073fa9e92b577c
Pavels Jelisejevs <git-no-reply@zabbix.com>
2014-11-14 08:38:33 +0000

    ..F....... [ZBX-8816] corrected code formatting

--- frontends/php/include/views/configuration.item.edit.php
+++ frontends/php/include/views/configuration.item.edit.php
@@ -316,7 +316,9 @@ $itemFormList->addRow(_('New flexible interval'), array($newFlexInt, $maxFlexMsg
 
 $keepHistory = array();
 $keepHistory[] =  new CNumericBox('history', $this->data['history'], 8);
-if ($data['config']['hk_history_global'] && $data['parent_discoveryid'] == 0 && $host['status'] != HOST_STATUS_TEMPLATE) {
+if ($data['config']['hk_history_global'] && $data['parent_discoveryid'] == 0
+		&& $host['status'] != HOST_STATUS_TEMPLATE) {
+
 	$keepHistory[] = ' '._x('Overridden by', 'item_form').' ';
 	if (CWebUser::getType() == USER_TYPE_SUPER_ADMIN) {
 		$link = new CLink(_x('global housekeeping settings', 'item_form'), 'adm.housekeeper.php');
@@ -332,7 +334,9 @@ $itemFormList->addRow(_('History storage period (in days)'), $keepHistory);
 
 $keepTrend = array();
 $keepTrend[] =  new CNumericBox('trends', $this->data['trends'], 8);
-if ($data['config']['hk_trends_global'] && $data['parent_discoveryid'] == 0 && $host['status'] != HOST_STATUS_TEMPLATE) {
+if ($data['config']['hk_trends_global'] && $data['parent_discoveryid'] == 0
+		&& $host['status'] != HOST_STATUS_TEMPLATE) {
+
 	$keepTrend[] = ' '._x('Overridden by', 'item_form').' ';
 	if (CWebUser::getType() == USER_TYPE_SUPER_ADMIN) {
 		$link = new CLink(_x('global housekeeping settings', 'item_form'), 'adm.housekeeper.php');
