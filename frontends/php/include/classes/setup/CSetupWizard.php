dff7a3ac64cd341905021603aea2da577ffc0f32
Pavels Jelisejevs <git-no-reply@zabbix.com>
2014-11-19 09:04:00 +0000

    ..F....... [ZBX-8896] corrected setup button styles

--- frontends/php/include/classes/setup/CSetupWizard.php
+++ frontends/php/include/classes/setup/CSetupWizard.php
@@ -115,17 +115,17 @@ class CSetupWizard extends CForm {
 		}
 
 		if (isset($this->stage[$this->getStep() + 1])) {
-			$next = new CSubmit('next['.$this->getStep().']', _('Next').SPACE.'&raquo;');
+			$next = new CSubmit('next['.$this->getStep().']', _('Next').SPACE.'&raquo;', null, 'jqueryinput');
 		}
 		else {
-			$next = new CSubmit('finish', _('Finish'));
+			$next = new CSubmit('finish', _('Finish'), null, 'jqueryinput');
 		}
 
 		if (isset($this->HIDE_CANCEL_BUTTON) && $this->HIDE_CANCEL_BUTTON) {
 			$cancel = null;
 		}
 		else {
-			$cancel = new CDiv(new CSubmit('cancel', _('Cancel')), 'footer_left');
+			$cancel = new CDiv(new CSubmit('cancel', _('Cancel'), null, 'jqueryinput'), 'footer_left');
 		}
 
 		if ($this->DISABLE_NEXT_BUTTON) {
@@ -135,7 +135,7 @@ class CSetupWizard extends CForm {
 		// if the user is not logged in (first setup run) hide the "previous" button on the final step
 		if ($this->getStep()
 				&& ((CWebUser::$data && CWebUser::getType() == USER_TYPE_SUPER_ADMIN) || $this->getStep() < 5)) {
-			$back = new CSubmit('back['.$this->getStep().']', '&laquo;'.SPACE._('Previous'));
+			$back = new CSubmit('back['.$this->getStep().']', '&laquo;'.SPACE._('Previous'), null, 'jqueryinput');
 		}
 		else {
 			$back = null;
@@ -351,7 +351,7 @@ class CSetupWizard extends CForm {
 					new CSpan(array(_('OK'), BR()), 'ok')
 					: new CSpan(array(_('Fail'), BR()), 'fail')
 					: null,
-				new  CSubmit('retry', 'Test connection')
+				new  CSubmit('retry', 'Test connection', null, 'jqueryinput')
 			), 'info_bar')
 
 		);
