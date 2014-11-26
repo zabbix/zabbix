5775c88f5b143b59ad0840dfb5be1c67bae877a3
arvids <arvids@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-11-26 12:08:05 +0000

    .......... [ZBX-5077] Created dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@50862 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-5077/frontends/php/tests/general/class_CDescription.php
@@ -0,0 +1,103 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+require_once dirname(__FILE__).'/../../include/gettextwrapper.inc.php';
+require_once dirname(__FILE__).'/../../include/defines.inc.php';
+require_once dirname(__FILE__).'/../../conf/zabbix.conf.php';
+require_once dirname(__FILE__).'/../../include/func.inc.php';
+require_once dirname(__FILE__).'/../../include/db.inc.php';
+require_once dirname(__FILE__).'/../../include/items.inc.php';
+require_once dirname(__FILE__).'/../../include/classes/core/Z.php';
+
+function error($error) {
+	echo "\nError reported: $error\n";
+	return true;
+}
+
+class class_CDescription extends PHPUnit_Framework_TestCase {
+
+	public static function setUpBeforeClass() {
+		$z = new Z();
+		$z->run();
+		DBconnect($error);
+	}
+
+	public static function providerTriggers() {
+		return array(
+			array(
+				'13517',
+				'trigger host.host:Host for trigger description macros | '.
+				'host.host2:{HOST.HOST2} | host.name:Host for trigger description macros | '.
+				'item.value:5 | item.value1:5 | item.lastvalue:5 | host.ip:127.0.0.1 | '.
+				'host.dns: | host.conn:127.0.0.1'
+			),
+		);
+	}
+
+	public static function providerReferenceMacros() {
+		return array(
+			array(
+				array(
+					'expression' => '{123}=1',
+					'description' => 'd $1'
+				),
+				'd 1',
+			),
+			array(
+				array(
+					'expression' => '{1}=1&{2}>2',
+					'description' => 'd $1 $2 $3'
+				),
+				'd 1 2 ',
+			),
+			array(
+				array(
+					'expression' => '{1}=123&{2}>{$MACRO}',
+					'description' => 'd $1 $2 $3'
+				),
+				'd 123  ',
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider providerTriggers
+	 */
+	public function test_expandTrigger($triggerId, $expectedDescription) {
+		$trigger = DBfetch(DBselect(
+			'SELECT t.triggerid,t.expression,t.description'.
+				' FROM triggers t'.
+				' WHERE t.triggerid='.$triggerId
+		));
+		$description = CTriggerHelper::expandDescription($trigger);
+
+		$this->assertEquals($expectedDescription, $description);
+	}
+
+	/**
+	 * @dataProvider providerReferenceMacros
+	 */
+	public function test_expandReferenceMacros($trigger, $expectedDescription) {
+		$description = CTriggerHelper::expandReferenceMacros($trigger);
+
+		$this->assertEquals($expectedDescription, $description);
+	}
+}
---
+++ ZBX-5077/upgrades/dbpatches/2.0/oracle/patch/hosts_groups.sql
@@ -0,0 +1,22 @@
+ALTER TABLE hosts_groups MODIFY hostgroupid DEFAULT NULL;
+ALTER TABLE hosts_groups MODIFY hostid DEFAULT NULL;
+ALTER TABLE hosts_groups MODIFY groupid DEFAULT NULL;
+DELETE FROM hosts_groups WHERE NOT hostid IN (SELECT hostid FROM hosts);
+DELETE FROM hosts_groups WHERE NOT groupid IN (SELECT groupid FROM groups);
+-- remove duplicates to allow unique index
+DELETE FROM hosts_groups
+	WHERE hostgroupid IN (
+		SELECT hg1.hostgroupid
+		FROM hosts_groups hg1
+		LEFT OUTER JOIN (
+			SELECT MIN(hg2.hostgroupid) AS hostgroupid
+			FROM hosts_groups hg2
+			GROUP BY hostid,groupid
+		) keep_rows ON
+			hg1.hostgroupid=keep_rows.hostgroupid
+		WHERE keep_rows.hostgroupid IS NULL
+	);
+DROP INDEX hosts_groups_1;
+CREATE UNIQUE INDEX hosts_groups_1 ON hosts_groups (hostid,groupid);
+ALTER TABLE hosts_groups ADD CONSTRAINT c_hosts_groups_1 FOREIGN KEY (hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
+ALTER TABLE hosts_groups ADD CONSTRAINT c_hosts_groups_2 FOREIGN KEY (groupid) REFERENCES groups (groupid) ON DELETE CASCADE;
