07c34c85f5f605e565ac9b1354dd6ac05dca7b12
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-11-26 09:12:44 +0000

    ..F....... [ZBXNEXT-1036] code refactoring and minor fixes
    git-svn-id: svn://svn.zabbix.com/branches/dev@50845 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1036/frontends/php/js/class.calendar.js
+++ ZBXNEXT-1036/frontends/php/js/class.calendar.js
@@ -279,7 +279,7 @@ calendar.prototype = {
 			result = true;
 		}
 		else if (d > 28 && result) {
-			if (d <= this.daysInMonth(this.sdt.getMonth(), this.sdt.getFullYear())) {
+			if (d <= daysInMonth(this.sdt.getMonth(), this.sdt.getFullYear())) {
 				this.sdt.setDate(d);
 				result = true;
 			}
@@ -502,18 +502,6 @@ calendar.prototype = {
 		// February has 29 days in any year evenly divisible by four,
 		// EXCEPT for centurial years which are not also divisible by 400.
 		return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 29 : 28;
-	},
-
-	daysInMonth: function(m, y) {
-		m++;
-		var days = 31;
-		if (m == 4 || m == 6 || m == 9 || m == 11) {
-			days = 30;
-		}
-		else if (m == 2) {
-			days = this.daysInFeb(y);
-		}
-		return days;
 	},
 
 	createDaysTab: function() {
