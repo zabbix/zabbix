ddec3710e52a8f8c8da0810359826a9e398d6663
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-12-04 13:09:26 +0000

    .......... [ZBX-9016] updated to the latest 2.2
    git-svn-id: svn://svn.zabbix.com/branches/dev@50996 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9016/frontends/php/include/classes/macros/CMacrosResolver.php
+++ ZBX-9016/frontends/php/include/classes/macros/CMacrosResolver.php
@@ -631,19 +631,19 @@ class CMacrosResolver extends CMacrosResolverGeneral {
 		$allowedItems = API::Item()->get(array(
 			'itemids' => array_keys($items),
 			'webitems' => true,
-			'output' => array('itemid', 'value_type'),
+			'output' => array('itemid', 'value_type', 'lastvalue', 'lastclock'),
 			'preservekeys' => true
 		));
 
 		// map item data only for allowed items
 		foreach ($items as $item) {
 			if (isset($allowedItems[$item['itemid']])) {
+				$item['lastvalue'] = $allowedItems[$item['itemid']]['lastvalue'];
+				$item['lastclock'] = $allowedItems[$item['itemid']]['lastclock'];
 				$hostKeyPairs[$item['host']][$item['key_']] = $item;
 			}
 		}
 
-		// fetch history
-		$history = Manager::History()->getLast($items);
 
 		// replace macros with their corresponding values in graph strings
 		$matches = reset($matchesList);
@@ -664,8 +664,8 @@ class CMacrosResolver extends CMacrosResolverGeneral {
 
 					// macro function is "last"
 					if ($function == 'last') {
-						$value = isset($history[$item['itemid']])
-							? formatHistoryValue($history[$item['itemid']][0]['value'], $item)
+						$value = ($item['lastclock'] > 0)
+							? formatHistoryValue($item['lastvalue'], $item)
 							: UNRESOLVED_MACRO_STRING;
 					}
 					// macro function is "max", "min" or "avg"
