8035594e73ce2b91fd42ae07633b58465679dcec
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-12-08 14:43:54 +0000

    .......... [ZBXNEXT-756] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@51071 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-756/upgrades/dbpatches/1.4/postgresql/patch/slideshows.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE slideshows (
-        slideshowid     bigint DEFAULT '0'     NOT NULL,
-        name            varchar(255)            DEFAULT ''      NOT NULL,
-        delay           integer         DEFAULT '0'     NOT NULL,
-        PRIMARY KEY (slideshowid)
-) with OIDS;
