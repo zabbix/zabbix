ca79fd48cf5d471361a5db8ccdb9de124df52560
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-12-15 15:00:08 +0000

    .......... [ZBXNEXT-826] updated release version [1.8.23rc1]
    git-svn-id: svn://svn.zabbix.com/branches/1.8@51177 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,7 +19,7 @@
 **/
 ?>
 <?php
-	define('ZABBIX_VERSION',		'1.8.22');
+	define('ZABBIX_VERSION',		'1.8.23rc1');
 	define('ZABBIX_API_VERSION',	'1.3');
 /* USER DEFINES */
 
--- include/common.h
+++ include/common.h
@@ -122,7 +122,7 @@
 
 #define	APPLICATION_NAME	"Zabbix Agent"
 #define	ZABBIX_REVDATE		"15 December 2014"
-#define	ZABBIX_VERSION		"1.8.22"
+#define	ZABBIX_VERSION		"1.8.23rc1"
 #define	ZABBIX_REVISION		"{ZABBIX_REVISION}"
 
 #if defined(_WINDOWS)
