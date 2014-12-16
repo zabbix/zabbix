2db227205724e81be464293b5b9692d36ceece9c
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-12-16 13:00:23 +0000

    ..F....... [ZBX-9042] fix typo depenencies; unify mass update form titles
    git-svn-id: svn://svn.zabbix.com/branches/dev@51212 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9042/frontends/php/include/views/configuration.triggers.massupdate.php
+++ ZBX-9042/frontends/php/include/views/configuration.triggers.massupdate.php
@@ -128,7 +128,7 @@ if (empty($this->data['parent_discoveryid'])) {
 
 	$triggersFormList->addRow(
 		array(
-			_('Replace depenencies'),
+			_('Replace dependencies'),
 			SPACE,
 			new CVisibilityBox(
 				'visible[dependencies]',
@@ -143,7 +143,7 @@ if (empty($this->data['parent_discoveryid'])) {
 
 // append tabs to form
 $triggersTab = new CTabView();
-$triggersTab->addTab('triggersTab', _('Triggers massupdate'), $triggersFormList);
+$triggersTab->addTab('triggersTab', _('Mass update'), $triggersFormList);
 $triggersForm->addItem($triggersTab);
 
 // append buttons to form
