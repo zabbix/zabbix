f28a63f80a51ef0311ea183f7acf1f12406f759e
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-12-18 16:00:24 +0000

    ...G...PS. [ZBXNEXT-1263] added loading/releasing of certificate, key, and PSK
    git-svn-id: svn://svn.zabbix.com/branches/dev@51261 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1263-1/src/libs/zbxcrypto/tls.c
+++ ZBXNEXT-1263-1/src/libs/zbxcrypto/tls.c
@@ -32,9 +32,25 @@
 #	include <openssl/ssl.h>
 #endif
 
+#if defined(HAVE_POLARSSL) || defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
+extern char	*CONFIG_TLS_CA_FILE;
+extern char	*CONFIG_TLS_CA_PATH;
+extern char	*CONFIG_TLS_CERT_FILE;
+extern char	*CONFIG_TLS_KEY_FILE;
+extern char	*CONFIG_TLS_PSK_FILE;
+extern char	*CONFIG_TLS_PSK_IDENTITY;
+#endif
+
 #if defined(HAVE_POLARSSL)
-static entropy_context	entropy;
-static ctr_drbg_context	ctr_drbg;
+static x509_crt		*ca_cert		= NULL;
+static x509_crt		*my_cert		= NULL;
+static pk_context	*my_priv_key		= NULL;
+static unsigned char	*my_psk			= NULL;
+static size_t		my_psk_len		= 0;
+static char		*my_psk_identity	= NULL;
+static entropy_context	*entropy		= NULL;
+static ctr_drbg_context	*ctr_drbg		= NULL;
+static char		*err_msg		= NULL;
 #endif
 
 #if defined(HAVE_POLARSSL)
@@ -95,7 +111,7 @@ static void	polarssl_debug(ssl_context *tls_ctx, int level, const char *str)
  * Purpose: compose a TLS error message                                       *
  *                                                                            *
  ******************************************************************************/
-static void	zbx_tls_error_msg(int error_code, char *msg, char **error)
+static void	zbx_tls_error_msg(int error_code, const char *msg, char **error)
 {
 	char	err[128];	/* 128 bytes are enough for PolarSSL error messages */
 
@@ -133,7 +149,7 @@ int	zbx_tls_init_parent(void)
 int	zbx_tls_init_child(void)
 {
 	const char	*__function_name = "zbx_tls_init_child";
-	int		ret = SUCCEED;
+	int		ret = SUCCEED, res;
 
 #if defined(HAVE_POLARSSL)
 	char	*pers = NULL;
@@ -141,19 +157,185 @@ int	zbx_tls_init_child(void)
 #endif
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
-
 #if defined(HAVE_POLARSSL)
-	entropy_init(&entropy);
+	/* try to configure the CA path first, it overrides the CA file parameter */
+	if (NULL != CONFIG_TLS_CA_PATH && '\0' != *CONFIG_TLS_CA_PATH)
+	{
+		ca_cert = zbx_malloc(ca_cert, sizeof(x509_crt));
+		x509_crt_init(ca_cert);
+
+		if (0 != (res = x509_crt_parse_path(ca_cert, CONFIG_TLS_CA_PATH)))
+		{
+			if (0 > res)
+			{
+				zbx_tls_error_msg(res, "", &err_msg);
+				zabbix_log(LOG_LEVEL_CRIT, "cannot parse CA certificate(s) in directory \"%s\": %s",
+						CONFIG_TLS_CA_PATH, err_msg);
+				zbx_free(err_msg);
+			}
+			else
+			{
+				zabbix_log(LOG_LEVEL_CRIT, "cannot parse %d CA certificate(s) in directory \"%s\"", res,
+						CONFIG_TLS_CA_PATH);
+			}
+
+			zbx_tls_free();
+			exit(EXIT_FAILURE);
+		}
+
+		zabbix_log(LOG_LEVEL_DEBUG, "%s(): successfully loaded CA certificate(s) from directory",
+				__function_name);
+	}
+
+	/* load CA file if CA path is not configured */
+	if (NULL == ca_cert && NULL != CONFIG_TLS_CA_FILE && '\0' != *CONFIG_TLS_CA_FILE)
+	{
+		ca_cert = zbx_malloc(ca_cert, sizeof(x509_crt));
+		x509_crt_init(ca_cert);
+
+		if (0 != (res = x509_crt_parse_file(ca_cert, CONFIG_TLS_CA_FILE)))
+		{
+			if (0 > res)
+			{
+				zbx_tls_error_msg(res, "", &err_msg);
+				zabbix_log(LOG_LEVEL_CRIT, "cannot parse CA certificate(s) in file \"%s\": %s",
+						CONFIG_TLS_CA_FILE, err_msg);
+				zbx_free(err_msg);
+			}
+			else
+			{
+				zabbix_log(LOG_LEVEL_CRIT, "cannot parse %d CA certificate(s) in file \"%s\"", res,
+						CONFIG_TLS_CA_FILE);
+			}
+
+			zbx_tls_free();
+			exit(EXIT_FAILURE);
+		}
+
+		zabbix_log(LOG_LEVEL_DEBUG, "%s(): successfully loaded CA certificate(s) from file", __function_name);
+	}
+
+	/* load certificate */
+	if (NULL != CONFIG_TLS_CERT_FILE && '\0' != *CONFIG_TLS_CERT_FILE)
+	{
+		my_cert = zbx_malloc(my_cert, sizeof(x509_crt));
+		x509_crt_init(my_cert);
+
+		if (0 != (res = x509_crt_parse_file(my_cert, CONFIG_TLS_CERT_FILE)))
+		{
+			if (0 > res)
+			{
+				zbx_tls_error_msg(res, "", &err_msg);
+				zabbix_log(LOG_LEVEL_CRIT, "cannot parse certificate(s) in file \"%s\": %s",
+						CONFIG_TLS_CERT_FILE, err_msg);
+				zbx_free(err_msg);
+			}
+			else
+			{
+				zabbix_log(LOG_LEVEL_CRIT, "cannot parse %d certificate(s) in file \"%s\"", res,
+						CONFIG_TLS_CERT_FILE);
+			}
+
+			zbx_tls_free();
+			exit(EXIT_FAILURE);
+		}
+
+		zabbix_log(LOG_LEVEL_DEBUG, "%s(): successfully loaded certificate", __function_name);
+	}
+
+	/* load private key */
+	if (NULL != CONFIG_TLS_KEY_FILE && '\0' != *CONFIG_TLS_KEY_FILE)
+	{
+		my_priv_key = zbx_malloc(my_priv_key, sizeof(pk_context));
+		pk_init(my_priv_key);
+
+		/* The 3rd argument of pk_parse_keyfile() is password for decrypting the private key. */
+		/* Currently the password is not used, it is empty. */
+		if (0 > (res = pk_parse_keyfile(my_priv_key, CONFIG_TLS_KEY_FILE, "")))
+		{
+			zbx_tls_error_msg(res, "", &err_msg);
+			zabbix_log(LOG_LEVEL_CRIT, "cannot parse the private key in file \"%s\": %s",
+					CONFIG_TLS_KEY_FILE, err_msg);
+			zbx_free(err_msg);
+			zbx_tls_free();
+			exit(EXIT_FAILURE);
+		}
+
+		zabbix_log(LOG_LEVEL_DEBUG, "%s(): successfully loaded private key", __function_name);
+	}
+
+	/* load pre-shared key */
+	if (NULL != CONFIG_TLS_PSK_FILE && '\0' != *CONFIG_TLS_PSK_FILE)
+	{
+		int	fd;
+		ssize_t	nbytes;
+
+		my_psk = zbx_malloc(my_psk, POLARSSL_PSK_MAX_LEN);
+
+		if (-1 == (fd = zbx_open(CONFIG_TLS_PSK_FILE, O_RDONLY)))
+		{
+			zabbix_log(LOG_LEVEL_CRIT, "cannot open file \"%s\": %s", CONFIG_TLS_PSK_FILE,
+					zbx_strerror(errno));
+			zbx_tls_free();
+			exit(EXIT_FAILURE);
+		}
+
+		nbytes = read(fd, my_psk, (size_t)POLARSSL_PSK_MAX_LEN);
+
+		if (-1 == nbytes)
+		{
+			zabbix_log(LOG_LEVEL_CRIT, "cannot read from file \"%s\": %s", CONFIG_TLS_PSK_FILE,
+					zbx_strerror(errno));
+			zbx_tls_free();
+			exit(EXIT_FAILURE);
+		}
+
+		if (0 == nbytes)
+		{
+			zabbix_log(LOG_LEVEL_CRIT, "file \"%s\" is empty", CONFIG_TLS_PSK_FILE);
+			zbx_tls_free();
+			exit(EXIT_FAILURE);
+		}
+
+		my_psk_len = (size_t)nbytes;
+
+		if (0 != close(fd))
+		{
+			zabbix_log(LOG_LEVEL_CRIT, "cannot close file \"%s\": %s", CONFIG_TLS_PSK_FILE,
+					zbx_strerror(errno));
+			zbx_tls_free();
+			exit(EXIT_FAILURE);
+		}
+
+		zabbix_log(LOG_LEVEL_DEBUG, "%s(): successfully loaded pre-shared key", __function_name);
+
+		/* configure identity to be used with the pre-shared key */
+		if (NULL != CONFIG_TLS_PSK_IDENTITY && '\0' != *CONFIG_TLS_PSK_IDENTITY)
+		{
+			/* TODO implement validation for PSK identity */
+			my_psk_identity = CONFIG_TLS_PSK_IDENTITY;
+		}
+	}
+
+	entropy = zbx_malloc(entropy, sizeof(entropy_context));
+	entropy_init(entropy);
 	zbx_tls_make_personalization_string(&pers, &pers_len);
 
-	if (0 != (ret = ctr_drbg_init(&ctr_drbg, entropy_func, &entropy, (unsigned char *)pers, pers_len)))
+	ctr_drbg = zbx_malloc(ctr_drbg, sizeof(ctr_drbg_context));
+	if (0 != (res = ctr_drbg_init(ctr_drbg, entropy_func, entropy, (unsigned char *)pers, pers_len)))
 	{
-		zabbix_log(LOG_LEVEL_CRIT, "cannot initialize random number generator: %d", ret);
+		zbx_tls_error_msg(res, "", &err_msg);
+		zabbix_log(LOG_LEVEL_CRIT, "cannot initialize random number generator: %s", err_msg);
+		zbx_free(err_msg);
+		zbx_tls_free();
 		exit(EXIT_FAILURE);
 	}
 
 	zbx_guaranteed_memset(pers, 0, pers_len);
 	zbx_free(pers);
+
+	/* TODO validate configuration parameters (allowed combinations etc.) */
+
 #elif defined(HAVE_GNUTLS)
 	if (GNUTLS_E_SUCCESS != gnutls_global_init())
 	{
@@ -183,10 +365,43 @@ int	zbx_tls_free(void)
 	int		ret = SUCCEED;
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
-
 #if defined(HAVE_POLARSSL)
-	ctr_drbg_free(&ctr_drbg);
-	entropy_free(&entropy);
+	if (NULL != ctr_drbg)
+	{
+		ctr_drbg_free(ctr_drbg);
+		zbx_free(ctr_drbg);
+	}
+
+	if (NULL != entropy)
+	{
+		entropy_free(entropy);
+		zbx_free(entropy);
+	}
+
+	if (NULL != my_psk)
+	{
+		zbx_guaranteed_memset(my_psk, 0, my_psk_len);
+		my_psk_len = 0;
+		zbx_free(my_psk);
+	}
+
+	if (NULL != my_priv_key)
+	{
+		pk_free(my_priv_key);
+		zbx_free(my_priv_key);
+	}
+
+	if (NULL != my_cert)
+	{
+		x509_crt_free(my_cert);
+		zbx_free(my_cert);
+	}
+
+	if (NULL != ca_cert)
+	{
+		x509_crt_free(ca_cert);
+		zbx_free(ca_cert);
+	}
 #elif defined(HAVE_GNUTLS)
 	gnutls_global_deinit();
 #elif defined(HAVE_OPENSSL)
