0d00a85733b9133221e673e6547124ed102263f6
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2014-12-19 13:12:42 +0000

    .......... [ZBX-8448] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@51279 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-8448-24/src/zabbix_server/dbconfig/dbconfig.c
@@ -0,0 +1,83 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+
+#include "db.h"
+#include "daemon.h"
+#include "zbxself.h"
+#include "log.h"
+#include "dbconfig.h"
+#include "dbcache.h"
+
+extern int		CONFIG_CONFSYNCER_FREQUENCY;
+extern unsigned char	process_type, daemon_type;
+extern int		server_num, process_num;
+
+/******************************************************************************
+ *                                                                            *
+ * Function: main_dbconfig_loop                                               *
+ *                                                                            *
+ * Purpose: periodically synchronises database data with memory cache         *
+ *                                                                            *
+ * Parameters:                                                                *
+ *                                                                            *
+ * Return value:                                                              *
+ *                                                                            *
+ * Author: Alexander Vladishev                                                *
+ *                                                                            *
+ * Comments: never returns                                                    *
+ *                                                                            *
+ ******************************************************************************/
+ZBX_THREAD_ENTRY(dbconfig_thread, args)
+{
+	double	sec = 0.0;
+
+	process_type = ((zbx_thread_args_t *)args)->process_type;
+	server_num = ((zbx_thread_args_t *)args)->server_num;
+	process_num = ((zbx_thread_args_t *)args)->process_num;
+
+	zabbix_log(LOG_LEVEL_INFORMATION, "%s #%d started [%s #%d]", get_daemon_type_string(daemon_type),
+			server_num, get_process_type_string(process_type), process_num);
+
+	zbx_setproctitle("%s [waiting %d sec for processes]", get_process_type_string(process_type),
+			CONFIG_CONFSYNCER_FREQUENCY);
+
+	/* the initial configuration sync is done by server before worker processes are forked */
+	zbx_sleep_loop(CONFIG_CONFSYNCER_FREQUENCY);
+
+	zbx_setproctitle("%s [connecting to the database]", get_process_type_string(process_type));
+
+	DBconnect(ZBX_DB_CONNECT_NORMAL);
+
+	for (;;)
+	{
+		zbx_setproctitle("%s [synced configuration in " ZBX_FS_DBL " sec, syncing configuration]",
+				get_process_type_string(process_type), sec);
+
+		sec = zbx_time();
+		DCsync_configuration();
+		sec = zbx_time() - sec;
+
+		zbx_setproctitle("%s [synced configuration in " ZBX_FS_DBL " sec, idle %d sec]",
+				get_process_type_string(process_type), sec, CONFIG_CONFSYNCER_FREQUENCY);
+
+		zbx_sleep_loop(CONFIG_CONFSYNCER_FREQUENCY);
+	}
+}
