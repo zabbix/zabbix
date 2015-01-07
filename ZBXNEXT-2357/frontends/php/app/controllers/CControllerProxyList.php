2670db498d5d5ae9332b74a29170d01312f9f3c6
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-01-07 14:06:14 +0000

    ..F....... [ZBXNEXT-2357] fixed formatting; code simplifying; removing useless code
    git-svn-id: svn://svn.zabbix.com/branches/dev@51424 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2357/frontends/php/app/controllers/CControllerProxyList.php
+++ ZBXNEXT-2357/frontends/php/app/controllers/CControllerProxyList.php
@@ -22,9 +22,9 @@ class CControllerProxyList extends CController {
 
 	protected function checkInput() {
 		$fields = array(
-			'sort' =>			'fatal|in_str:host',
-			'sortorder' =>		'fatal|in_str:'.ZBX_SORT_DOWN.','.ZBX_SORT_UP,
-			'uncheck' =>		'fatal|in_int:1'
+			'sort' =>		'fatal|in_str:host',
+			'sortorder' =>	'fatal|in_str:'.ZBX_SORT_DOWN.','.ZBX_SORT_UP,
+			'uncheck' =>	'fatal|in_int:1'
 		);
 
 		$result = $this->validateInput($fields);
@@ -43,52 +43,52 @@ class CControllerProxyList extends CController {
 	}
 
 	protected function doAction() {
-		$data['uncheck'] = $this->hasInput('uncheck');
+		$sortField = $this->getInput('sort', CProfile::get('web.proxies.php.sort', 'host'));
+		$sortOrder = $this->getInput('sortorder', CProfile::get('web.proxies.php.sortorder', ZBX_SORT_UP));
 
-		$sortField = $this->getInput('sort', CProfile::get('web.proxy.php.sort', 'host'));
-		$sortOrder = $this->getInput('sortorder', CProfile::get('web.proxy.php.sortorder', ZBX_SORT_UP));
-
-		CProfile::update('web.proxy.php.sort', $sortField, PROFILE_TYPE_STR);
-		CProfile::update('web.proxy.php.sortorder', $sortOrder, PROFILE_TYPE_STR);
+		CProfile::update('web.proxies.php.sort', $sortField, PROFILE_TYPE_STR);
+		CProfile::update('web.proxies.php.sortorder', $sortOrder, PROFILE_TYPE_STR);
 
 		$config = select_config();
 
-		$data['sort'] = $sortField;
-		$data['sortorder'] = $sortOrder;
-		$data['config']['max_in_table'] = $config['max_in_table'];
+		$data = array(
+			'uncheck' => $this->hasInput('uncheck'),
+			'sort' => $sortField,
+			'sortorder' => $sortOrder,
+			'config' => array(
+				'max_in_table' => $config['max_in_table']
+			)
+		);
 
 		$data['proxies'] = API::Proxy()->get(array(
-			'editable' => true,
-			'selectHosts' => array('hostid', 'host', 'name', 'status'),
 			'output' => array('proxyid', 'host', 'status', 'lastaccess'),
+			'selectHosts' => array('hostid', 'name', 'status'),
 			'sortfield' => $sortField,
-			'limit' => $config['search_limit'] + 1
+			'limit' => $config['search_limit'] + 1,
+			'editable' => true,
+			'preservekeys' => true
 		));
-
-		$data['proxies'] = zbx_toHash($data['proxies'], 'proxyid');
-
-		$proxyIds = array_keys($data['proxies']);
-
 		// sorting & paging
 		order_result($data['proxies'], $sortField, $sortOrder);
 		$data['paging'] = getPagingLine($data['proxies']);
 
+		// get proxy IDs for a *selected* page
+		$proxyIds = array_keys($data['proxies']);
+
 		// calculate performance
 		$dbPerformance = DBselect(
 			'SELECT h.proxy_hostid,SUM(1.0/i.delay) AS qps'.
-			' FROM items i,hosts h'.
-			' WHERE i.status='.ITEM_STATUS_ACTIVE.
-				' AND i.hostid=h.hostid'.
+			' FROM hosts h,items i'.
+			' WHERE h.hostid=i.hostid'.
 				' AND h.status='.HOST_STATUS_MONITORED.
+				' AND i.status='.ITEM_STATUS_ACTIVE.
 				' AND i.delay<>0'.
 				' AND i.flags<>'.ZBX_FLAG_DISCOVERY_PROTOTYPE.
 				' AND '.dbConditionInt('h.proxy_hostid', $proxyIds).
 			' GROUP BY h.proxy_hostid'
 		);
 		while ($performance = DBfetch($dbPerformance)) {
-			if (isset($data['proxies'][$performance['proxy_hostid']])) {
-				$data['proxies'][$performance['proxy_hostid']]['perf'] = round($performance['qps'], 2);
-			}
+			$data['proxies'][$performance['proxy_hostid']]['perf'] = round($performance['qps'], 2);
 		}
 
 		// get items
@@ -100,13 +100,7 @@ class CControllerProxyList extends CController {
 			'monitored' => true
 		));
 		foreach ($items as $item) {
-			if (isset($data['proxies'][$item['proxy_hostid']])) {
-				if (!isset($data['proxies'][$item['proxy_hostid']]['item_count'])) {
-					$data['proxies'][$item['proxy_hostid']]['item_count'] = 0;
-				}
-
-				$data['proxies'][$item['proxy_hostid']]['item_count'] += $item['rowscount'];
-			}
+			$data['proxies'][$item['proxy_hostid']]['item_count'] = $item['rowscount'];
 		}
 
 		$response = new CControllerResponseData($data);
