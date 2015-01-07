3ef62e219764b5271358d2fe9a1e5aebbd1052cc
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-01-07 14:07:40 +0000

    .......... [ZBX-9163] updated to the latest 2.2
    git-svn-id: svn://svn.zabbix.com/branches/dev@51425 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9163/src/libs/zbxcommon/str.c
+++ ZBX-9163/src/libs/zbxcommon/str.c
@@ -2531,6 +2531,10 @@ const char	*zbx_dservice_type_string(zbx_dservice_type_t service)
 			return "SNMPv3 agent";
 		case SVC_ICMPPING:
 			return "ICMP ping";
+		case SVC_HTTPS:
+			return "HTTPS";
+		case SVC_TELNET:
+			return "Telnet";
 		default:
 			return "unknown";
 	}
