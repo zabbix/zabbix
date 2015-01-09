89a15742556e5590d9ddfc526d1e494549ee2d99
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-01-09 14:43:41 +0000

    .......... [ZBX-9195] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@51472 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-9195/frontends/php/scripts.php
@@ -0,0 +1,287 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+require_once dirname(__FILE__).'/include/config.inc.php';
+
+$page['title'] = _('Configuration of scripts');
+$page['file'] = 'scripts.php';
+$page['hist_arg'] = array('scriptid');
+
+require_once dirname(__FILE__).'/include/page_header.php';
+
+// VAR	TYPE	OPTIONAL	FLAGS	VALIDATION	EXCEPTION
+$fields = array(
+	'scriptid' =>			array(T_ZBX_INT, O_OPT, P_SYS,			DB_ID,		null),
+	'scripts' =>			array(T_ZBX_INT, O_OPT, P_SYS,			DB_ID,		null),
+	'name' =>				array(T_ZBX_STR, O_OPT, null,			NOT_EMPTY,	'isset({save})'),
+	'type' =>				array(T_ZBX_INT, O_OPT, null,			IN('0,1'),	'isset({save})'),
+	'execute_on' =>			array(T_ZBX_INT, O_OPT, null,			IN('0,1'),	'isset({save})&&{type}=='.ZBX_SCRIPT_TYPE_CUSTOM_SCRIPT),
+	'command' =>			array(T_ZBX_STR, O_OPT, null,			null,		'isset({save})'),
+	'commandipmi' =>		array(T_ZBX_STR, O_OPT, null,			null,		'isset({save})'),
+	'description' =>		array(T_ZBX_STR, O_OPT, null,			null,		'isset({save})'),
+	'access' =>				array(T_ZBX_INT, O_OPT, null,			IN('0,1,2,3'), 'isset({save})'),
+	'groupid' =>			array(T_ZBX_INT, O_OPT, P_SYS,			DB_ID,		'isset({save})'),
+	'usrgrpid' =>			array(T_ZBX_INT, O_OPT, P_SYS,			DB_ID,		'isset({save})'),
+	'confirmation' =>		array(T_ZBX_STR, O_OPT, null,			null,		null),
+	'enableConfirmation' =>	array(T_ZBX_STR, O_OPT, null,			null,		null),
+	// actions
+	'go' =>					array(T_ZBX_STR, O_OPT, P_SYS|P_ACT,	null,		null),
+	'action' =>				array(T_ZBX_INT, O_OPT, P_ACT,			IN('0,1'),	null),
+	'save' =>				array(T_ZBX_STR, O_OPT, P_SYS|P_ACT,	null,		null),
+	'delete' =>				array(T_ZBX_STR, O_OPT, P_ACT,			null,		null),
+	'clone' =>				array(T_ZBX_STR, O_OPT, P_SYS|P_ACT,	null,		null),
+	'form' =>				array(T_ZBX_STR, O_OPT, null,			null,		null),
+	'form_refresh' =>		array(T_ZBX_INT, O_OPT, null,			null,		null)
+);
+check_fields($fields);
+
+$_REQUEST['go'] = get_request('go', 'none');
+
+validate_sort_and_sortorder('name', ZBX_SORT_UP);
+
+/*
+ * Permissions
+ */
+$sid = get_request('scriptid');
+if ($sid) {
+	$scripts = API::Script()->get(array(
+		'scriptids' => $sid,
+		'output' => API_OUTPUT_SHORTEN
+	));
+	if (empty($scripts)) {
+		access_deny();
+	}
+}
+
+/*
+ * Actions
+ */
+if (isset($_REQUEST['clone']) && isset($_REQUEST['scriptid'])) {
+	unset($_REQUEST['scriptid']);
+	$_REQUEST['form'] = 'clone';
+}
+elseif (isset($_REQUEST['save'])) {
+	$confirmation = get_request('confirmation', '');
+	$enableConfirmation = get_request('enableConfirmation', false);
+
+	$command = ($_REQUEST['type'] == ZBX_SCRIPT_TYPE_IPMI) ? $_REQUEST['commandipmi'] : $_REQUEST['command'];
+	if ($enableConfirmation && zbx_empty($confirmation)) {
+		error(_('Please enter confirmation text.'));
+		show_messages(null, null, _('Cannot add script'));
+	}
+	elseif (zbx_empty($command)) {
+		error(_('Command cannot be empty.'));
+		show_messages(null, null, _('Cannot add script'));
+	}
+	else {
+		$script = array(
+			'name' => $_REQUEST['name'],
+			'type' => $_REQUEST['type'],
+			'execute_on' => $_REQUEST['execute_on'],
+			'command' => $command,
+			'description' => $_REQUEST['description'],
+			'usrgrpid' => $_REQUEST['usrgrpid'],
+			'groupid' => $_REQUEST['groupid'],
+			'host_access' => $_REQUEST['access'],
+			'confirmation' => get_request('confirmation', '')
+		);
+
+		if (isset($_REQUEST['scriptid'])) {
+			$script['scriptid'] = $_REQUEST['scriptid'];
+
+			$result = API::Script()->update($script);
+			show_messages($result, _('Script updated'), _('Cannot update script'));
+
+			$audit_action = AUDIT_ACTION_UPDATE;
+		}
+		else {
+			$result = API::Script()->create($script);
+
+			show_messages($result, _('Script added'), _('Cannot add script'));
+
+			$audit_action = AUDIT_ACTION_ADD;
+		}
+
+		$scriptid = isset($result['scriptids']) ? reset($result['scriptids']) : null;
+
+		if ($result) {
+			add_audit($audit_action, AUDIT_RESOURCE_SCRIPT, ' Name ['.$_REQUEST['name'].'] id ['.$scriptid.']');
+			unset($_REQUEST['action'], $_REQUEST['form'], $_REQUEST['scriptid']);
+		}
+	}
+}
+elseif (isset($_REQUEST['delete'])) {
+	$scriptid = get_request('scriptid', 0);
+
+	$result = API::Script()->delete($scriptid);
+
+	if ($result) {
+		add_audit(AUDIT_ACTION_DELETE, AUDIT_RESOURCE_SCRIPT, _('Script').' ['.$scriptid.']');
+	}
+
+	show_messages($result, _('Script deleted'), _('Cannot delete script'));
+
+	if ($result) {
+		unset($_REQUEST['form'], $_REQUEST['scriptid']);
+	}
+}
+elseif ($_REQUEST['go'] == 'delete' && isset($_REQUEST['scripts'])) {
+	$scriptids = $_REQUEST['scripts'];
+
+	$go_result = API::Script()->delete($scriptids);
+	if ($go_result) {
+		foreach ($scriptids as $scriptid) {
+			add_audit(AUDIT_ACTION_DELETE, AUDIT_RESOURCE_SCRIPT, _('Script').' ['.$scriptid.']');
+		}
+	}
+
+	show_messages($go_result, _('Script deleted'), _('Cannot delete script'));
+
+	if ($go_result) {
+		unset($_REQUEST['form'], $_REQUEST['scriptid']);
+	}
+}
+
+if ($_REQUEST['go'] != 'none' && isset($go_result) && $go_result) {
+	$url = new CUrl();
+	$path = $url->getPath();
+	insert_js('cookie.eraseArray("'.$path.'")');
+}
+
+/*
+ * Display
+ */
+if (isset($_REQUEST['form'])) {
+	$data = array(
+		'form' => get_request('form', 1),
+		'form_refresh' => get_request('form_refresh', 0),
+		'scriptid' => get_request('scriptid')
+	);
+
+	if (!$data['scriptid'] || isset($_REQUEST['form_refresh'])) {
+		$data['name'] = get_request('name', '');
+		$data['type'] = get_request('type', ZBX_SCRIPT_TYPE_CUSTOM_SCRIPT);
+		$data['execute_on'] = get_request('execute_on', ZBX_SCRIPT_EXECUTE_ON_SERVER);
+		$data['command'] = get_request('command', '');
+		$data['commandipmi'] = get_request('commandipmi', '');
+		$data['description'] = get_request('description', '');
+		$data['usrgrpid'] = get_request('usrgrpid', 0);
+		$data['groupid'] = get_request('groupid', 0);
+		$data['access'] = get_request('host_access', 0);
+		$data['confirmation'] = get_request('confirmation', '');
+		$data['enableConfirmation'] = get_request('enableConfirmation', false);
+	}
+	elseif ($data['scriptid']) {
+		$script = API::Script()->get(array(
+			'scriptids' => $data['scriptid'],
+			'output' => API_OUTPUT_EXTEND
+		));
+		$script = reset($script);
+
+		$data['name'] = $script['name'];
+		$data['type'] = $script['type'];
+		$data['execute_on'] = $script['execute_on'];
+		$data['command'] = $data['commandipmi'] = $script['command'];
+		$data['description'] = $script['description'];
+		$data['usrgrpid'] = $script['usrgrpid'];
+		$data['groupid'] = $script['groupid'];
+		$data['access'] = $script['host_access'];
+		$data['confirmation'] = $script['confirmation'];
+		$data['enableConfirmation'] = !empty($script['confirmation']);
+	}
+
+	$scriptView = new CView('administration.script.edit');
+
+	$scriptView->set('form', $data['form']);
+	$scriptView->set('form_refresh', $data['form_refresh']);
+	$scriptView->set('scriptid', $data['scriptid']);
+	$scriptView->set('name', $data['name']);
+	$scriptView->set('type', $data['type']);
+	$scriptView->set('execute_on', $data['execute_on']);
+	$scriptView->set('command', $data['command']);
+	$scriptView->set('commandipmi', $data['commandipmi']);
+	$scriptView->set('description', $data['description']);
+	$scriptView->set('usrgrpid', $data['usrgrpid']);
+	$scriptView->set('groupid', $data['groupid']);
+	$scriptView->set('access', $data['access']);
+	$scriptView->set('confirmation', $data['confirmation']);
+	$scriptView->set('enableConfirmation', $data['enableConfirmation']);
+
+	// get list of all groups
+	$groups = API::HostGroup()->get(array(
+		'output' => array('groupid', 'name')
+	));
+	order_result($groups, 'name');
+	$scriptView->set('groups', $groups);
+
+	// get list of user groups
+	$usergroups = API::UserGroup()->get(array(
+		'output' => array('usrgrpid', 'name')
+	));
+	order_result($usergroups, 'name');
+	$scriptView->set('usergroups', $usergroups);
+
+	// render view
+	$scriptView->render();
+	$scriptView->show();
+}
+else {
+	$data = array();
+
+	// list of scripts
+	$data['scripts'] = API::Script()->get(array(
+		'output' => array('scriptid', 'name', 'command', 'host_access', 'usrgrpid', 'groupid', 'type', 'execute_on'),
+		'editable' => true,
+		'selectGroups' => API_OUTPUT_EXTEND
+	));
+
+	// find script host group name and user group name. set to '' if all host/user groups used.
+	foreach ($data['scripts'] as $snum => $script) {
+		$scriptid = $script['scriptid'];
+
+		if ($script['usrgrpid'] > 0) {
+			$user_group = API::UserGroup()->get(array('usrgrpids' => $script['usrgrpid'], 'output' => API_OUTPUT_EXTEND));
+			$user_group = reset($user_group);
+
+			$data['scripts'][$snum]['userGroupName'] = $user_group['name'];
+		}
+		else {
+			$data['scripts'][$snum]['userGroupName'] = ''; // all user groups
+		}
+
+		if ($script['groupid'] > 0) {
+			$group = array_pop($script['groups']);
+			$data['scripts'][$snum]['hostGroupName'] = $group['name'];
+		}
+		else {
+			$data['scripts'][$snum]['hostGroupName'] = ''; // all host groups
+		}
+	}
+	order_result($data['scripts'], getPageSortField('name'), getPageSortOrder());
+
+	$data['paging'] = getPagingLine($data['scripts']);
+
+	// render view
+	$scriptView = new CView('administration.script.list', $data);
+	$scriptView->render();
+	$scriptView->show();
+}
+
+require_once dirname(__FILE__).'/include/page_footer.php';
---
+++ ZBX-9195/frontends/php/tests/include/class.capitest.php
@@ -0,0 +1,277 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+require_once dirname(__FILE__).'/../../include/validate.inc.php';
+require_once dirname(__FILE__).'/../../include/gettextwrapper.inc.php';
+require_once dirname(__FILE__).'/../../include/nodes.inc.php';
+require_once dirname(__FILE__).'/../../include/hosts.inc.php';
+require_once dirname(__FILE__).'/../../include/items.inc.php';
+require_once dirname(__FILE__).'/dbfunc.php';
+
+// locales
+require_once dirname(__FILE__).'/../../include/locales.inc.php';
+
+// classes
+require_once dirname(__FILE__).'/../../include/classes/class.cwebuser.php';
+
+// APIs
+require_once dirname(__FILE__).'/../../include/classes/api/APIException.php';
+require_once dirname(__FILE__).'/../../include/classes/api/CZBXAPI.php';
+require_once dirname(__FILE__).'/../../api/classes/CItemGeneral.php';
+require_once dirname(__FILE__).'/../../api/classes/CItemKey.php';
+require_once dirname(__FILE__).'/../../api/classes/CItem.php';
+require_once dirname(__FILE__).'/../../api/classes/CHost.php';
+require_once dirname(__FILE__).'/../../api/classes/CHostGroup.php';
+require_once dirname(__FILE__).'/../../api/classes/CTemplate.php';
+require_once dirname(__FILE__).'/../../api/classes/CHostInterface.php';
+require_once dirname(__FILE__).'/../../api/classes/CProxy.php';
+require_once dirname(__FILE__).'/../../api/classes/CGraph.php';
+require_once dirname(__FILE__).'/../../api/classes/CTrigger.php';
+
+if (!function_exists('info')) {
+	function info($data) {
+	}
+}
+
+require_once 'PHPUnit/Autoload.php';
+
+/**
+ * A base class for creating API method tests.
+ */
+abstract class CApiTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * API object.
+	 *
+	 * @var CZBXAPI
+	 */
+	protected $api;
+
+	/**
+	 * An array of private keys of the created during the tests.
+	 *
+	 * @var array
+	 */
+	protected $createdPks = array();
+
+
+	/**
+	 * A test host object.
+	 *
+	 * @var array
+	 */
+	protected $testHost;
+
+
+	/**
+	 * A method that provides valid objects for creating.
+	 *
+	 * @abstract
+	 *
+	 * @return array
+	 */
+	public abstract function providerCreateValid();
+
+
+	/**
+	 * Test case setup.
+	 *
+	 * @static
+	 */
+	public static function setUpBeforeClass() {
+
+		// some variable defines not to include config.inc.php
+		global $ZBX_CURRENT_NODEID;
+		$ZBX_CURRENT_NODEID = 0;
+		define('ZBX_DISTRIBUTED', false);
+
+		// add some user data
+		CZBXAPI::$userData = array(
+			'userid' => null,
+			'type' => USER_TYPE_SUPER_ADMIN
+		);
+
+		// skip the RPC calls
+		API::setReturnAPI();
+	}
+
+
+	/**
+	 * Test setup.
+	 */
+	public function setUp() {
+		DBconnect($error);
+	}
+
+
+	/**
+	 * A test for creating valid objects.
+	 *
+	 * @dataProvider providerCreateValid
+	 *
+	 * @param array $object
+	 */
+	public function testCreateValid(array $object) {
+		$rs = $this->createTestObject($object);
+
+		$this->assertArrayHasKey($this->api->pkOption(), $rs, get_class($this->api).'->create() result does not contain "'.$this->api->pkOption().'"');
+
+		$this->createdPks = array_merge($this->createdPks, $rs[$this->api->pkOption()]);
+	}
+
+	/**
+	 * A test for retrieving objects.
+	 *
+	 * @dataProvider providerCreateValid
+	 *
+	 * @param array $object
+	 */
+	public function testGet(array $object) {
+		$this->createTestObject($object);
+
+		$rs = $this->api->get(
+			array(
+				$this->api->pkOption() => $this->createdPks
+			)
+		);
+
+		$this->assertCount(1, $rs, 'One of the objects has not been retrieved.');
+	}
+
+
+	/**
+	 * A test for deleting object.
+	 *
+	 * @dataProvider providerCreateValid
+	 *
+	 * @param array $object
+	 */
+	public function testDelete(array $object) {
+		$rs = $this->createTestObject($object);
+		$rs = $this->api->delete($rs[$this->api->pkOption()]);
+
+		$this->assertCount(1, $rs[$this->api->pkOption()], 'One of the objects has not been deleted.');
+	}
+
+
+	/**
+	 * Reverts the database to the initial test.
+	 */
+	public function tearDown() {
+
+		// delete the created objects
+		if ($this->createdPks) {
+			$this->tearDownTestObjects();
+		}
+
+		// delete test host
+		if ($this->testHost) {
+			$this->tearDownTestHost();
+		}
+	}
+
+
+	/**
+	 * Saves an object to the database and returns the result. The objects private key will be added to
+	 * $this->createdPks.
+	 *
+	 * @param array $object
+	 *
+	 * @return mixed
+	 */
+	protected function createTestObject(array $object) {
+		$rs = $this->api->create($object);
+
+		$this->createdPks = array_merge($this->createdPks, $rs[$this->api->pkOption()]);
+
+		return $rs;
+	}
+
+
+	/**
+	 * Deletes the objects, that have been created during the test.
+	 *
+	 * TODO: delete related objects.
+	 */
+	protected function tearDownTestObjects() {
+		DB::delete($this->api->tableName(), array(
+			$this->api->pk() => $this->createdPks
+		));
+	}
+
+
+	/**
+	 * Sets up a test host group and a host. The created host is saved to $this->testHost.
+	 */
+	protected function setUpTestHost() {
+
+		// create a test group
+		$hostGroupApi = API::HostGroup();
+		$hostGroupRs = $hostGroupApi->create(
+			array(
+				'name' => 'Test host group'
+			)
+		);
+
+		// create a test host
+		$hostApi = new CHost();
+		$hostRs = $hostApi->create(array(
+			'host' => 'Test host 1',
+			'interfaces' => array(
+				array(
+					'type' => INTERFACE_TYPE_AGENT,
+					'ip' => '127.0.0.1',
+					'dns' => '',
+					'useip' => 1,
+					'port' => 10050,
+					'main' => 1
+				)
+			),
+			'groups' => array(
+				array(
+					'groupid' => $hostGroupRs['groupids'][0]
+				)
+			)
+		));
+
+		// fetch the new host
+		$hosts = $hostApi->get(array(
+			'output' => API_OUTPUT_EXTEND,
+			'hostids' => $hostRs['hostids'][0],
+			'selectGroups' => API_OUTPUT_EXTEND,
+			'selectInterfaces' => API_OUTPUT_EXTEND
+		));
+		$this->testHost = reset($hosts);
+	}
+
+
+	/**
+	 * Deletes the created test host group and host.
+	 */
+	protected function tearDownTestHost() {
+		DB::delete('hosts', array(
+			'hostid' => $this->testHost['hostid']
+		));
+		DB::delete('groups', array(
+			'groupid' => zbx_objectValues($this->testHost['groups'], 'groupid')
+		));
+		$this->testHost = null;
+	}
+}
