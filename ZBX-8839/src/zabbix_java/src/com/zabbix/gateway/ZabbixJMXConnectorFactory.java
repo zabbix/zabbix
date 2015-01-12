516de32cf00579564027b5ec759c4d44166cb08a
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-01-12 12:48:49 +0000

    .....J.... [ZBX-8839] implemented connection timeout solution as suggested on the Java blog
    git-svn-id: svn://svn.zabbix.com/branches/dev@51518 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8839/src/zabbix_java/src/com/zabbix/gateway/ZabbixJMXConnectorFactory.java
+++ ZBX-8839/src/zabbix_java/src/com/zabbix/gateway/ZabbixJMXConnectorFactory.java
@@ -19,7 +19,16 @@
 
 package com.zabbix.gateway;
 
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.SocketTimeoutException;
 import java.util.HashMap;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
@@ -32,10 +41,100 @@ class ZabbixJMXConnectorFactory
 {
 	private static final Logger logger = LoggerFactory.getLogger(ZabbixJMXConnectorFactory.class);
 
-	static JMXConnector connect(JMXServiceURL url, HashMap<String, String[]> env) throws java.io.IOException
+	private static final DaemonThreadFactory daemonThreadFactory = new DaemonThreadFactory();
+
+	private static class DaemonThreadFactory implements ThreadFactory
+	{
+		@Override
+		public Thread newThread(Runnable r)
+		{
+			Thread t = Executors.defaultThreadFactory().newThread(r);
+
+			t.setDaemon(true);
+
+			return t;
+		}
+	}
+
+	static JMXConnector connect(final JMXServiceURL url, final HashMap<String, String[]> env) throws IOException
 	{
 		logger.debug("connecting to JMX agent at {}", url);
 
-		return JMXConnectorFactory.connect(url, env);
+		final BlockingQueue<Object> queue = new ArrayBlockingQueue<Object>(1);
+
+		ExecutorService executor = Executors.newSingleThreadExecutor(daemonThreadFactory);
+
+		Runnable task = new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					JMXConnector jmxc = JMXConnectorFactory.connect(url, env);
+
+					if (!queue.offer(jmxc))
+						jmxc.close();
+				}
+				catch (Throwable t)
+				{
+					queue.offer(t);
+				}
+			}
+		};
+
+		executor.submit(task);
+
+		Object result;
+
+		try
+		{
+			result = queue.poll(1, TimeUnit.SECONDS);
+
+			if (null == result)
+			{
+				if (!queue.offer(""))
+					result = queue.take();
+			}
+		}
+		catch (InterruptedException e)
+		{
+			InterruptedIOException e2 = new InterruptedIOException(e.getMessage());
+
+			e2.initCause(e);
+
+			throw e2;
+		}
+		finally
+		{
+			executor.shutdown();
+		}
+
+		if (null == result)
+			throw new SocketTimeoutException("connection timed out: " + url);
+
+		if (result instanceof JMXConnector)
+			return (JMXConnector)result;
+
+		try
+		{
+			throw (Throwable)result;
+		}
+		catch (IOException e)
+		{
+			throw e;
+		}
+		catch (RuntimeException e)
+		{
+			throw e;
+		}
+		catch (Error e)
+		{
+			throw e;
+		}
+		catch (Throwable e)
+		{
+			throw new IOException(e.toString(), e);
+		}
 	}
 }
