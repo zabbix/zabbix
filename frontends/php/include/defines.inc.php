cd3ce0e2922b3a5c83cb56f3e301b11573163e7a
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-01-14 17:26:27 +0000

    .......... [ZBXNEXT-826] updated copyright to 2015
    git-svn-id: svn://svn.zabbix.com/trunk@51612 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 ** Zabbix
-** Copyright (C) 2001-2014 Zabbix SIA
+** Copyright (C) 2001-2015 Zabbix SIA
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@ define('ZABBIX_API_VERSION',	'2.5.0');
 define('ZABBIX_DB_VERSION',		2050001);
 
 define('ZABBIX_COPYRIGHT_FROM',	'2001');
-define('ZABBIX_COPYRIGHT_TO',	'2014');
+define('ZABBIX_COPYRIGHT_TO',	'2015');
 
 define('ZBX_LOGIN_ATTEMPTS',	5);
 define('ZBX_LOGIN_BLOCK',		30); // sec
