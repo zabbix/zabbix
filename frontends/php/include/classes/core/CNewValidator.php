5bd897714eece7f706948a427e1acf32845d3dc4
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2015-01-20 09:09:29 +0000

    ..F....... [ZBXNEXT-2357] removed unused code

--- frontends/php/include/classes/core/CNewValidator.php
+++ frontends/php/include/classes/core/CNewValidator.php
@@ -21,7 +21,6 @@
 
 class CNewValidator {
 
-	private $fields;
 	private $rules;
 	private $input = array();
 	private $output = array();
@@ -231,9 +230,6 @@ class CNewValidator {
 					$this->addError($fatal, 'Invalid validation rule "'.$rule.'".');
 					return false;
 			}
-//			if (!$this->validateRule($field, $, $fatal)) {
-//				return false;
-//			}
 		}
 
 		return true;
@@ -255,10 +251,6 @@ class CNewValidator {
 
 		// between INT_MIN and INT_MAX
 		return (bccomp($value, '-2147483648') >= 0 && bccomp($value, '2147483647') <= 0);
-	}
-
-	public static function is_array($value) {
-		return is_array($value);
 	}
 
 	private function check_db_value($field_schema, $value) {
@@ -309,158 +301,6 @@ class CNewValidator {
 		$table_schema = DB::getSchema($table);
 
 		return (is_string($value) && $this->check_db_value($table_schema['fields'][$field], $value));
-	}
-
-// $fatal: false - non fatal, true - any error is considered fatal
-	private function validateRule($field, $rule, $fatal) {
-
-		$field_parts = explode('/', $field);
-		$field_major = $field_parts[0];
-		$field_minor = isset($field_parts[1]) ? $field_parts[1] : null;
-
-		$parts = explode(':', $rule);
-
-		$command = $parts[0];
-		$params = isset($parts[1]) ? explode(',', $parts[1]) : array();
-
-		// simple field name
-		$result = true;
-		if ($field_minor === null) {
-			switch ($command) {
-			case 'required':
-				if (!array_key_exists($field, $this->input)) {
-					$this->addError($fatal, _s('Field "%1$s" is mandatory.', $field));
-					$result = false;
-				}
-				break;
-			// required_if:field,value1,value2,...
-			case 'required_if':
-				if (array_key_exists($field, $this->input) && !array_key_exists($params[0], $this->input)) {
-					$this->addError($fatal, _s('Field "%1$s" must present only if "%2$s" exists and equal to "%3$s".', $field, $params[0], $params[1]));
-					$result = false;
-				}
-//				else if (!array_key_exists($field, $this->input) && (array_key_exists($params[0], $this->input) && $this->input[$params[0]] == $params[1])) {
-//					$this->addError($fatal, _s('Field "%1$s" must present only if "%2$s" exists and equal to "%3$s".', $field, $params[0], $params[1]));
-//					$result = false;
-//				}
-				break;
-			// required_if_not:field,value1,value2,...
-			case 'required_if_not':
-				if (array_key_exists($field, $this->input) && array_key_exists($params[0], $this->input) && $this->input[$params[0]] == $params[1]) {
-					$this->addError($fatal, _s('Field "%1$s" must present if "%2$s" exists and equal to "%3$s".', $field, $params[0], $params[1]));
-					$result = false;
-				}
-				break;
-			case 'array':
-				if (array_key_exists($field, $this->input)) {
-					if(!CNewValidator::is_array($this->input[$field])) {
-						$this->addError($fatal, _s('Incorrect value "%1$s" for "%2$s" field.', $this->input[$field], $field));
-						$result = false;
-					}
-				}
-				break;
-			case 'not_empty':
-				if (array_key_exists($field, $this->input)) {
-					if($this->input[$field] === '') {
-						$this->addError($fatal, _s('Incorrect value for field "%1$s": cannot be empty.', $field));
-						$result = false;
-					}
-				}
-				break;
-			// in_int:value_int1,value_int2,value_int3,...
-			case 'in_int':
-				if (array_key_exists($field, $this->input)) {
-					if (!in_array($this->input[$field], $params, true)) {
-						$this->addError($fatal, _s('Incorrect value "%1$s" for "%2$s" field.', $this->input[$field], $field));
-						$result = false;
-					}
-				}
-				break;
-			case 'ip':
-				if (array_key_exists($field, $this->input)) {
-					if (!validate_ip($this->input[$field], $arr)) {
-						$this->addError($fatal, _s('Field "%1$s" is not IP.', $field));
-						$result = false;
-					}
-				}
-				break;
-			// in_str:value1,value2,value3,...
-			case 'in_str':
-				if (array_key_exists($field, $this->input)) {
-					if (!in_array($this->input[$field], $params, true)) {
-						$this->addError($fatal, _s('Incorrect value "%1$s" for "%2$s" field.', $this->input[$field], $field));
-						$result = false;
-					}
-				}
-				break;
-			// db:table.field
-			case 'db':
-				if (array_key_exists($field, $this->input)) {
-					if (!CNewValidator::is_db($this->input[$field], $params[0])) {
-						$this->addError($fatal, _s('Incorrect value "%1$s" for "%2$s" field.', $this->input[$field], $field));
-						$result = false;
-					}
-				}
-				break;
-			// db_array:table.field
-			case 'array_db':
-				if (array_key_exists($field, $this->input)) {
-					foreach ($this->input[$field] as $value) {
-						if (!CNewValidator::is_db_type($value, $params[0])) {
-							$this->addError($fatal, _s('Incorrect value "%1$s" for "%2$s" field.', $this->input[$field], $field));
-							$result = false;
-						}
-					}
-				}
-				break;
-			default:
-				break;
-			}
-		}
-		// compound field name like field/subfield
-		else {
-			switch ($command) {
-			case 'required':
-/*				if (!array_key_exists($field_major, $this->input)) {
-					$this->addError($fatal, _s('Field "%1$s" is mandatory.', $field));
-				}
-				else if (!CNewValidator::is_array($this->input[$field_major])) {
-					$this->addError($fatal, _s('Field "%1$s" is mandatory.', $field));
-				}
-				else {
-					foreach ($this->input[$field_major] as $array_element) {
-						if (!array_key_exists($field_major, $array_element)) {
-							$this->addError($fatal, _s('Field "%1$s" is mandatory.', $field));
-						}
-					}
-				}*/
-				break;
-			// in_int:value_int1,value_int2,value_int3,...
-			case 'in_int':
-			// required_if:field,value
-			case 'required_if':
-			case 'ip':
-			case 'db':
-/*				if (!array_key_exists($field_major, $this->input)) {
-					$this->addError($fatal, _s('Field "%1$s" is mandatory.', $field));
-				}
-				else if (!CNewValidator::is_array($this->input[$field_major])) {
-					$this->addError($fatal, _s('Field "%1$s" is mandatory.', $field));
-				} else {
-					if (array_key_exists($field, $this->input) && !array_key_exists($params[0], $this->input)) {
-						$this->addError($fatal, _s('Field "%1$s" must present only if "%2$s" exists and equal to "%3$s".', $field, $params[0], $params[1]));
-					}
-					if (!array_key_exists($field, $this->input) && (array_key_exists($params[0], $this->input) && $this->input[$params[0]] == $params[1])) {
-						$this->addError($fatal, _s('Field "%1$s" must present only if "%2$s" exists and equal to "%3$s".', $field, $params[0], $params[1]));
-					}
-				}*/
-				break;
-			default:
-				break;
-			}
-		}
-
-		return $result;
 	}
 
 	/**
