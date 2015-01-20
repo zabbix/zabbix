caf16fd214ac518fb2caf010fb59368961e7ba85
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-01-20 15:14:07 +0000

    .......... [ZBXNEXT-1493] created dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@51742 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-1493/frontends/php/include/classes/html/CButton.php
@@ -0,0 +1,82 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CButton extends CTag implements CButtonInterface {
+
+	/**
+	 * Button class that will be added to the other classes of the element.
+	 *
+	 * @var string
+	 */
+	protected $buttonClass;
+
+	public function __construct($name = 'button', $caption = '', $action = null,
+			$buttonClass = 'button-plain shadow ui-corner-all') {
+		parent::__construct('button', 'yes', $caption, 'button');
+		$this->setAttribute('type', 'button');
+
+		if ($name !== null) {
+			$this->setAttribute('id', zbx_formatDomId($name));
+			$this->setAttribute('name', $name);
+		}
+
+		$this->addAction('onclick', $action);
+
+		$this->buttonClass = $buttonClass;
+	}
+
+	/**
+	 * @see CButtonInterface::main()
+	 */
+	public function main() {
+		$this->addClass('main');
+	}
+
+	/**
+	 * Enable or disable the element.
+	 *
+	 * @param bool $value
+	 */
+	public function setEnabled($value) {
+		if ($value) {
+			$this->removeAttribute('disabled');
+		}
+		else {
+			$this->attr('disabled', 'disabled');
+		}
+	}
+
+	/**
+	 * @see CButtonInterface::setButtonClass()
+	 */
+	public function setButtonClass($class) {
+		$this->buttonClass = $class;
+	}
+
+	public function toString($destroy = true) {
+		// append the button class
+		if ($this->buttonClass !== null) {
+			$this->addClass($this->buttonClass);
+		}
+
+		return parent::toString($destroy);
+	}
+}
