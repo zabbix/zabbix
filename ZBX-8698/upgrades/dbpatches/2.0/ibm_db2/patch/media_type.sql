cba260d94b7e9059afa40fb7bc7ea0b2a5b37663
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-01-23 16:25:27 +0000

    .......... [ZBX-8698] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@51810 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8698/upgrades/dbpatches/2.0/ibm_db2/patch/media_type.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE media_type ADD status integer WITH DEFAULT '0' NOT NULL
-/
-REORG TABLE media_type
-/
-ALTER TABLE media_type ALTER COLUMN mediatypeid SET WITH DEFAULT NULL
-/
-REORG TABLE media_type
-/
