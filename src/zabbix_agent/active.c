93b100af7671365ccb2503c0e9820b01c9b29dc0
igors <igors@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2015-01-26 11:42:57 +0000

    ...G...PS. [ZBX-8993] added check for valid global regular expressions in snmptrap, log, logrt and eventlog items, trigger expressions and lld rules
    git-svn-id: svn://svn.zabbix.com/trunk@51831 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/zabbix_agent/active.c
+++ src/zabbix_agent/active.c
@@ -858,6 +858,24 @@ out:
 	return ret;
 }
 
+static int	global_regexp_exists(const char *name)
+{
+	int	i;
+
+	if (0 == regexps.values_num)
+		return FAIL;
+
+	for (i = 0; i < regexps.values_num; i++)
+	{
+		zbx_expression_t	*regexp = (zbx_expression_t *)regexps.values[i];
+
+		if (0 == strcmp(regexp->name, name))
+			break;
+	}
+
+	return (i == regexps.values_num ? FAIL : SUCCEED);
+}
+
 #define ZBX_ACTIVE_CHECK_LOG	0
 #define ZBX_ACTIVE_CHECK_LOGRT	1
 
@@ -896,7 +914,14 @@ static int	process_log_check(char *server, unsigned short port, ZBX_ACTIVE_METRI
 	}
 
 	if (NULL == (pattern = get_rparam(&request, 1)))
+	{
 		pattern = "";
+	}
+	else if ('@' == *pattern && SUCCEED != global_regexp_exists(pattern + 1))
+	{
+		*error = zbx_dsprintf(*error, "Global regular expression \"%s\" does not exist.", pattern + 1);
+		goto out;
+	}
 
 	if (NULL == (encoding = get_rparam(&request, 2)))
 	{
@@ -1016,16 +1041,44 @@ static int	process_eventlog_check(char *server, unsigned short port, ZBX_ACTIVE_
 	}
 
 	if (NULL == (pattern = get_rparam(&request, 1)))
+	{
 		pattern = "";
+	}
+	else if ('@' == *pattern && SUCCEED != global_regexp_exists(pattern + 1))
+	{
+		*error = zbx_dsprintf(*error, "Global regular expression \"%s\" does not exist.", pattern + 1);
+		goto out;
+	}
 
 	if (NULL == (key_severity = get_rparam(&request, 2)))
+	{
 		key_severity = "";
+	}
+	else if ('@' == *key_severity && SUCCEED != global_regexp_exists(key_severity + 1))
+	{
+		*error = zbx_dsprintf(*error, "Global regular expression \"%s\" does not exist.", key_severity + 1);
+		goto out;
+	}
 
 	if (NULL == (key_source = get_rparam(&request, 3)))
+	{
 		key_source = "";
+	}
+	else if ('@' == *key_source && SUCCEED != global_regexp_exists(key_source + 1))
+	{
+		*error = zbx_dsprintf(*error, "Global regular expression \"%s\" does not exist.", key_source + 1);
+		goto out;
+	}
 
 	if (NULL == (key_logeventid = get_rparam(&request, 4)))
+	{
 		key_logeventid = "";
+	}
+	else if ('@' == *key_logeventid && SUCCEED != global_regexp_exists(key_logeventid + 1))
+	{
+		*error = zbx_dsprintf(*error, "Global regular expression \"%s\" does not exist.", key_logeventid + 1);
+		goto out;
+	}
 
 	if (NULL == (maxlines_persec = get_rparam(&request, 5)) || '\0' == *maxlines_persec)
 	{
