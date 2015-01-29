af8d4dc5ef524ed5fd19fa10f1a804228af07e71
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-01-29 08:53:08 +0000

    A.F....... [ZBX-1357] enabled Chinese (China) locale to be displayed by default
    git-svn-id: svn://svn.zabbix.com/branches/2.4@51898 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/locales.inc.php
+++ frontends/php/include/locales.inc.php
@@ -52,7 +52,7 @@ function getLocales() {
 		'en_GB' => array('name' => _('English (en_GB)'),	'display' => true),
 		'en_US' => array('name' => _('English (en_US)'),	'display' => true),
 		'bg_BG' => array('name' => _('Bulgarian (bg_BG)'),	'display' => false),
-		'zh_CN' => array('name' => _('Chinese (zh_CN)'),	'display' => false),
+		'zh_CN' => array('name' => _('Chinese (zh_CN)'),	'display' => true),
 		'zh_TW' => array('name' => _('Chinese (zh_TW)'),	'display' => false),
 		'cs_CZ' => array('name' => _('Czech (cs_CZ)'),		'display' => false),
 		'nl_NL' => array('name' => _('Dutch (nl_NL)'),		'display' => false),
