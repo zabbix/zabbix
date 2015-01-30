3d291c75e855a7f5eb1045b4e99c364c8390be33
igors <igors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-01-30 10:01:55 +0000

    .......... [ZBX-9233] removed merged development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@51928 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9233/upgrades/dbpatches/1.6/mysql/patch/users.sql
+++
@@ -1,6 +0,0 @@
-alter table users add theme varchar(128) DEFAULT 'default.css' NOT NULL;
-alter table users add attempt_failed          integer         DEFAULT 0       NOT NULL;
-alter table users add attempt_ip              varchar(39)             DEFAULT ''      NOT NULL;
-alter table users add attempt_clock           integer         DEFAULT 0       NOT NULL;
-alter table users add autologin integer DEFAULT '0' NOT NULL after url;
-update users set passwd='5fce1b3e34b520afeffb37ce08c7cd66' where alias<>'guest' and passwd='d41d8cd98f00b204e9800998ecf8427e';
