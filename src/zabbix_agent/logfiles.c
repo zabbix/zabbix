68282a37db79dcd5e0b35ef948ee4f611fd7ea56
igors <igors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-02-06 09:25:55 +0000

    ...G...... [ZBX-9248] fixed code formatting
    git-svn-id: svn://svn.zabbix.com/branches/2.4@52039 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_agent/logfiles.c
+++ src/zabbix_agent/logfiles.c
@@ -1763,7 +1763,7 @@ out:
 static int	process_log(char *filename, zbx_uint64_t *lastlogsize, int *mtime, unsigned char *skip_old_data,
 		int *big_rec, int *incomplete, char **err_msg, const char *encoding, zbx_vector_ptr_t *regexps,
 		const char *pattern, const char *output_template, int *p_count, int *s_count,
-		zbx_process_value_func_t process_value,	const char *server, unsigned short port, const char *hostname,
+		zbx_process_value_func_t process_value, const char *server, unsigned short port, const char *hostname,
 		const char *key)
 {
 	const char	*__function_name = "process_log";
