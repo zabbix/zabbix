288c107d688bcada342feda1ffbd03a15afc60ce
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-02-10 08:36:23 +0000

    ..F....... [ZBXNEXT-2357] fixed clone function in proxy edit form
    git-svn-id: svn://svn.zabbix.com/branches/dev@52118 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2357/frontends/php/app/views/administration.proxy.edit.js.php
+++ ZBXNEXT-2357/frontends/php/app/views/administration.proxy.edit.js.php
@@ -15,13 +15,17 @@
 					.removeClass('hidden');
 			}
 		});
-		// clone button
+		// clone button, special processing because of list of hosts
 		jQuery('#clone').click(function() {
-			jQuery('#proxyid, #delete, #clone').remove();
-			jQuery('#update span').text(<?php echo CJs::encodeJson(_('Add')); ?>);
-			jQuery('#update').val('proxy.create').attr({id: 'add'});
-			jQuery('#cancel').addClass('ui-corner-left');
-			jQuery('#host').focus();
+			var url = new Curl('zabbix.php?action=proxy.edit');
+			url.setArgument('host', jQuery('#host').val());
+			url.setArgument('status', jQuery('#status').val());
+			url.setArgument('description', jQuery('#description').val());
+			url.setArgument('ip', jQuery('#ip').val());
+			url.setArgument('dns', jQuery('#dns').val());
+			url.setArgument('useip', jQuery('input[name=useip]:checked').val());
+			url.setArgument('port', jQuery('#port').val());
+			redirect(url.getUrl(), 'post', 'action');
 		});
 	});
 </script>
