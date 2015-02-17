e67240d4d0e43863ee3ac38961d5daf99348ee97
Andris Mednis <Andris.Mednis@zabbix.com>
2015-02-17 08:54:45 +0000

    ...G...... [ZBXNEXT-1263] added TLS support to 'zabbix_get'

--- src/zabbix_get/zabbix_get.c
+++ src/zabbix_get/zabbix_get.c
@@ -24,12 +24,17 @@
 #include "cfg.h"
 #include "log.h"
 #include "zbxgetopt.h"
+#include "../libs/zbxcrypto/tls.h"
 
 const char	*progname = NULL;
 const char	title_message[] = "zabbix_get";
 const char	syslog_app_name[] = "zabbix_get";
 const char	*usage_message[] = {
 	"-s host-name-or-IP [-p port-number] [-I IP-address] -k item-key",
+#if defined(HAVE_POLARSSL) || defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
+	"-s host-name-or-IP [-p port-number] [-I IP-address] --tls-connect=cert (--tls-ca-file=ca_file | --tls-ca-path=ca_path) [--tls-crl-file=crl_file] --tls-cert-file=cert_file --tls-key-file=key_file -k item-key",
+	"-s host-name-or-IP [-p port-number] [-I IP-address] --tls-connect=psk --tls-psk-identity=psk_identity --tls-psk-file=psk_file -k item-key",
+#endif
 	"-h",
 	"-V",
 	NULL	/* end of text */
@@ -40,7 +45,7 @@ unsigned char	program_type	= ZBX_PROGRAM_TYPE_GET;
 const char	*help_message[] = {
 	"Get data from Zabbix agent.",
 	"",
-	"Options:",
+	"General options:",
 	"  -s --host host-name-or-IP          Specify host name or IP address of a host",
 	"  -p --port port-number              Specify port number of agent running on the host. Default is " ZBX_DEFAULT_AGENT_PORT_STR,
 	"  -I --source-address IP-address     Specify source IP address",
@@ -50,7 +55,42 @@ const char	*help_message[] = {
 	"  -h --help                          Display this help message",
 	"  -V --version                       Display version number",
 	"",
-	"Example: zabbix_get -s 127.0.0.1 -p " ZBX_DEFAULT_AGENT_PORT_STR " -k \"system.cpu.load[all,avg1]\"",
+	"TLS connection options:",
+#if defined(HAVE_POLARSSL) || defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
+	"  --tls-connect                      How to connect to agent. Values:",
+	"                                         unencrypted - connect without encryption",
+	"                                         psk         - connect using TLS and a pre-shared key",
+	"                                         cert        - connect using TLS and a certificate",
+	"",
+	"  --tls-ca-file                      Full pathname of a file containing the top-level CA(s) certificates for",
+	"                                     peer certificate verification",
+	"",
+	"  --tls-ca-path                      Full path of a directory containing the top-level CA(s) certificates for",
+	"                                     peer certificate verification. Overrides '--tls-ca-file' parameter",
+	"  --tls-crl-file                     Full pathname of a file containing revoked certificates",
+	"",
+	"  --tls-cert-file                    Full pathname of a file containing the certificate or certificate chain",
+	"",
+	"  --tls-key-file                     Full pathname of a file containing the private key",
+	"",
+	"  --tls-psk-identity                 Unique, case sensitive string used to identify the pre-shared key",
+	"",
+	"  --tls-psk-file                     Full pathname of a file containing the pre-shared key",
+#else
+	"  Not available. This 'zabbix_get' was compiled without TLS support",
+#endif
+	"",
+	"Example(s):",
+	"    zabbix_get -s 127.0.0.1 -p " ZBX_DEFAULT_AGENT_PORT_STR " -k \"system.cpu.load[all,avg1]\"",
+#if defined(HAVE_POLARSSL) || defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
+	"",
+	"    zabbix_get -s 127.0.0.1 -p " ZBX_DEFAULT_AGENT_PORT_STR " -k \"system.cpu.load[all,avg1]\" --tls-connect=psk \\",
+	"        --tls-psk-identity=\"PSK ID Zabbix agentd\" --tls-psk-file=/home/zabbix/zabbix_agentd.psk ",
+	"",
+	"    zabbix_get -s 127.0.0.1 -p " ZBX_DEFAULT_AGENT_PORT_STR " -k \"system.cpu.load[all,avg1]\" --tls-connect=cert \\",
+	"        --tls-ca-file=/home/zabbix/zabbix_ca_file --tls-cert-file=/home/zabbix/zabbix_get.crt \\",
+	"        --tls-key-file=/home/zabbix/zabbix_get.key",
+#endif
 	NULL	/* end of text */
 };
 
@@ -58,8 +98,7 @@ const char	*help_message[] = {
 unsigned int	configured_tls_connect_mode = ZBX_TCP_SEC_UNENCRYPTED;
 unsigned int	configured_tls_accept_modes = ZBX_TCP_SEC_UNENCRYPTED;	/* not used in zabbix_get, just for linking */
 									/* with tls.c */
-#if defined(HAVE_POLARSSL) || defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
-char	*CONFIG_TLS_CONNECT		= NULL;	/* not used in zabbix_get, just for linking with tls.c */
+char	*CONFIG_TLS_CONNECT		= NULL;
 char	*CONFIG_TLS_ACCEPT		= NULL;	/* not used in zabbix_get, just for linking with tls.c */
 char	*CONFIG_TLS_CA_FILE		= NULL;
 char	*CONFIG_TLS_CA_PATH		= NULL;
@@ -68,7 +107,6 @@ char	*CONFIG_TLS_CERT_FILE		= NULL;
 char	*CONFIG_TLS_KEY_FILE		= NULL;
 char	*CONFIG_TLS_PSK_FILE		= NULL;
 char	*CONFIG_TLS_PSK_IDENTITY	= NULL;
-#endif
 
 /* COMMAND LINE OPTIONS */
 
@@ -81,6 +119,14 @@ struct zbx_option	longopts[] =
 	{"source-address",	1,	NULL,	'I'},
 	{"help",		0,	NULL,	'h'},
 	{"version",		0,	NULL,	'V'},
+	{"tls-connect",		1,	NULL,	'1'},
+	{"tls-ca-file",		1,	NULL,	'2'},
+	{"tls-ca-path",		1,	NULL,	'3'},
+	{"tls-crl-file",	1,	NULL,	'4'},
+	{"tls-cert-file",	1,	NULL,	'5'},
+	{"tls-key-file",	1,	NULL,	'6'},
+	{"tls-psk-identity",	1,	NULL,	'7'},
+	{"tls-psk-file",	1,	NULL,	'8'},
 	{NULL}
 };
 
@@ -131,8 +177,12 @@ static void	get_value(const char *source_ip, const char *host, unsigned short po
 	int		ret;
 	char		request[1024];
 
-	if (SUCCEED == (ret = zbx_tcp_connect(&s, source_ip, host, port, GET_SENDER_TIMEOUT, ZBX_TCP_SEC_UNENCRYPTED,
-			NULL, NULL)))
+	/* The connect mode can specify TLS with PSK but here we do not know PSK details. Therefore we put NULL in */
+	/* the last 2 arguments. zbx_tls_connect() will find out PSK in this case. If the connect mode specifies TLS */
+	/* with certificate then also NULLs are ok, as the sender does not verify server certificate issuer and */
+	/* subject. */
+	if (SUCCEED == (ret = zbx_tcp_connect(&s, source_ip, host, port, GET_SENDER_TIMEOUT,
+			configured_tls_connect_mode, NULL, NULL)))
 	{
 		zbx_snprintf(request, sizeof(request), "%s\n", key);
 
@@ -216,6 +266,45 @@ int	main(int argc, char **argv)
 				version();
 				exit(EXIT_SUCCESS);
 				break;
+#if defined(HAVE_POLARSSL) || defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
+			case '1':
+				CONFIG_TLS_CONNECT = zbx_strdup(CONFIG_TLS_CONNECT, zbx_optarg);
+				break;
+			case '2':
+				CONFIG_TLS_CA_FILE = zbx_strdup(CONFIG_TLS_CA_FILE, zbx_optarg);
+				break;
+			case '3':
+				CONFIG_TLS_CA_PATH = zbx_strdup(CONFIG_TLS_CA_PATH, zbx_optarg);
+				break;
+			case '4':
+				CONFIG_TLS_CRL_FILE = zbx_strdup(CONFIG_TLS_CRL_FILE, zbx_optarg);
+				break;
+			case '5':
+				CONFIG_TLS_CERT_FILE = zbx_strdup(CONFIG_TLS_CERT_FILE, zbx_optarg);
+				break;
+			case '6':
+				CONFIG_TLS_KEY_FILE = zbx_strdup(CONFIG_TLS_KEY_FILE, zbx_optarg);
+				break;
+			case '7':
+				CONFIG_TLS_PSK_IDENTITY = zbx_strdup(CONFIG_TLS_PSK_IDENTITY, zbx_optarg);
+				break;
+			case '8':
+				CONFIG_TLS_PSK_FILE = zbx_strdup(CONFIG_TLS_PSK_FILE, zbx_optarg);
+				break;
+#else
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+				zbx_error("TLS parameters cannot be used: 'zabbix_get' was compiled without TLS "
+						"support.");
+				exit(EXIT_FAILURE);
+				break;
+#endif
 			default:
 				usage();
 				exit(EXIT_FAILURE);
@@ -259,6 +348,17 @@ int	main(int argc, char **argv)
 	if (FAIL == ret)
 		printf("Try '%s --help' for more information.\n", progname);
 
+#if defined(HAVE_POLARSSL) || defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
+	zbx_tls_init_child();
+#else
+	if (NULL != CONFIG_TLS_CONNECT || NULL != CONFIG_TLS_CA_FILE || NULL != CONFIG_TLS_CA_PATH ||
+			NULL != CONFIG_TLS_CRL_FILE || NULL != CONFIG_TLS_CERT_FILE || NULL != CONFIG_TLS_KEY_FILE ||
+			NULL != CONFIG_TLS_PSK_IDENTITY || NULL != CONFIG_TLS_PSK_FILE)
+	{
+		zbx_error("TLS parameters cannot be used: 'zabbix_get' was compiled without TLS support.");
+		ret = FAIL;
+	}
+#endif
 	if (SUCCEED == ret)
 	{
 #if !defined(_WINDOWS)
@@ -273,6 +373,8 @@ int	main(int argc, char **argv)
 	zbx_free(host);
 	zbx_free(key);
 	zbx_free(source_ip);
-
+#if defined(HAVE_POLARSSL) || defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
+	zbx_tls_free();
+#endif
 	return ret;
 }
