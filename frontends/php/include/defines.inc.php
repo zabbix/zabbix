9df09a1e6f6c80504c964d5cfe4ef6176851f780
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-02-23 12:30:20 +0000

    .......... [ZBXNEXT-826] updated release version [2.4.5rc1]; updated API version [2.4.5]
    git-svn-id: svn://svn.zabbix.com/branches/2.4@52343 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,8 +19,8 @@
 **/
 
 
-define('ZABBIX_VERSION',		'2.4.4');
-define('ZABBIX_API_VERSION',	'2.4.4');
+define('ZABBIX_VERSION',		'2.4.5rc1');
+define('ZABBIX_API_VERSION',	'2.4.5');
 define('ZABBIX_DB_VERSION',		2040000);
 
 define('ZABBIX_COPYRIGHT_FROM',	'2001');
--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	public static final String REVISION_DATE = "23 February 2015";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "2.4.4";
+	public static final String VERSION = "2.4.5rc1";
 
 	public static void printVersion()
 	{
