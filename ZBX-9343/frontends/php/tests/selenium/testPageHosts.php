4da7aea840d8cefff391ba0af20d8deae668c3ef
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-03-10 09:38:52 +0000

    .......... [ZBX-9343] updated to latest /branches/2.4
    git-svn-id: svn://svn.zabbix.com/branches/dev@52647 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9343/frontends/php/tests/selenium/testPageHosts.php
+++ ZBX-9343/frontends/php/tests/selenium/testPageHosts.php
@@ -134,58 +134,38 @@ class testPageHosts extends CWebTest {
 	*/
 	public function testPageHosts_FilterHost($host) {
 		$this->zbxTestLogin('hosts.php');
-		$this->zbxTestClick('flicker_icon_l');
+		$this->zbxTestDropdownSelectWait('groupid', 'all');
+		//$this->zbxTestClick('flicker_icon_l');
+		$this->zbxTestClickWait('filter_rst');
 		$this->input_type('filter_host', $host['name']);
-		$this->input_type('filter_ip', '');
-		$this->input_type('filter_port', '');
-		$this->zbxTestClickWait('filter');
+		$this->zbxTestClickWait('filter_set');
 		$this->zbxTestTextPresent($host['name']);
 	}
 
-	// Filter returns nothing
+	// filter returns nothing
 	public function testPageHosts_FilterNone() {
 		$this->zbxTestLogin('hosts.php');
+		$this->zbxTestDropdownSelectWait('groupid', 'all');
 
 		// Reset filter
-		$this->zbxTestClick('reset');
+		$this->zbxTestClickWait('filter_rst');
 
 		$this->input_type('filter_host', '1928379128ksdhksdjfh');
-		$this->zbxTestClickWait('filter');
+		$this->zbxTestClickWait('filter_set');
 		$this->zbxTestTextPresent('Displaying 0 of 0 found');
-	}
-
-	public function testPageHosts_FilterNone1() {
-		$this->zbxTestLogin('hosts.php');
-
-		// Reset filter
-		$this->zbxTestClick('reset');
-
-		$this->input_type('filter_host', '_');
-		$this->zbxTestClickWait('filter');
-		$this->zbxTestTextPresent('Displaying 0 of 0 found');
-	}
-
-	public function testPageHosts_FilterNone2() {
-		$this->zbxTestLogin('hosts.php');
-
-		// Reset filter
-		$this->zbxTestClick('reset');
 
 		$this->input_type('filter_host', '%');
-		$this->zbxTestClickWait('filter');
+		$this->zbxTestClickWait('filter_set');
 		$this->zbxTestTextPresent('Displaying 0 of 0 found');
 	}
 
-	// Filter reset
-
-	/**
-	* @dataProvider allHosts
-	*/
-	public function testPageHosts_FilterReset($host) {
+	// filter reset
+	public function testPageHosts_FilterReset() {
 		$this->zbxTestLogin('hosts.php');
-		$this->zbxTestClick('reset');
-		$this->zbxTestClickWait('filter');
-		$this->zbxTestTextPresent($host['name']);
+		$this->zbxTestDropdownSelectWait('groupid', 'all');
+		$this->zbxTestClickWait('filter_rst');
+		$this->zbxTestClickWait('filter_set');
+		$this->zbxTestTextNotPresent('Displaying 0 of 0 found');
 	}
 
 	/**
--- ZBX-9343/src/libs/zbxcomms/comms.c
+++ ZBX-9343/src/libs/zbxcomms/comms.c
@@ -291,7 +291,8 @@ int	zbx_tcp_connect(zbx_sock_t *s, const char *source_ip, const char *ip, unsign
 
 	if (ZBX_SOCK_ERROR == (s->socket = socket(ai->ai_family, ai->ai_socktype | SOCK_CLOEXEC, ai->ai_protocol)))
 	{
-		zbx_set_tcp_strerror("cannot create socket [[%s]:%d]: %s", ip, port, strerror_from_system(zbx_sock_last_error()));
+		zbx_set_tcp_strerror("cannot create socket [[%s]:%d]: %s",
+				ip, port, strerror_from_system(zbx_sock_last_error()));
 		goto out;
 	}
 
@@ -310,12 +311,14 @@ int	zbx_tcp_connect(zbx_sock_t *s, const char *source_ip, const char *ip, unsign
 		if (0 != getaddrinfo(source_ip, NULL, &hints, &ai_bind))
 		{
 			zbx_set_tcp_strerror("invalid source IP address [%s]", source_ip);
+			zbx_tcp_close(s);
 			goto out;
 		}
 
 		if (ZBX_TCP_ERROR == bind(s->socket, ai_bind->ai_addr, ai_bind->ai_addrlen))
 		{
 			zbx_set_tcp_strerror("bind() failed: %s", strerror_from_system(zbx_sock_last_error()));
+			zbx_tcp_close(s);
 			goto out;
 		}
 	}
@@ -325,7 +328,8 @@ int	zbx_tcp_connect(zbx_sock_t *s, const char *source_ip, const char *ip, unsign
 
 	if (ZBX_TCP_ERROR == connect(s->socket, ai->ai_addr, ai->ai_addrlen))
 	{
-		zbx_set_tcp_strerror("cannot connect to [[%s]:%d]: %s", ip, port, strerror_from_system(zbx_sock_last_error()));
+		zbx_set_tcp_strerror("cannot connect to [[%s]:%d]: %s",
+				ip, port, strerror_from_system(zbx_sock_last_error()));
 		zbx_tcp_close(s);
 		goto out;
 	}
@@ -389,6 +393,7 @@ int	zbx_tcp_connect(zbx_sock_t *s, const char *source_ip, const char *ip, unsign
 		if (ZBX_TCP_ERROR == bind(s->socket, (struct sockaddr *)&source_addr, sizeof(source_addr)))
 		{
 			zbx_set_tcp_strerror("bind() failed: %s", strerror_from_system(zbx_sock_last_error()));
+			zbx_tcp_close(s);
 			return FAIL;
 		}
 	}
@@ -398,7 +403,8 @@ int	zbx_tcp_connect(zbx_sock_t *s, const char *source_ip, const char *ip, unsign
 
 	if (ZBX_TCP_ERROR == connect(s->socket, (struct sockaddr *)&servaddr_in, sizeof(servaddr_in)))
 	{
-		zbx_set_tcp_strerror("cannot connect to [[%s]:%d]: %s", ip, port, strerror_from_system(zbx_sock_last_error()));
+		zbx_set_tcp_strerror("cannot connect to [[%s]:%d]: %s",
+				ip, port, strerror_from_system(zbx_sock_last_error()));
 		zbx_tcp_close(s);
 		return FAIL;
 	}
