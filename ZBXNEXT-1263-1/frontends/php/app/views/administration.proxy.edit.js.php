d7fb5ef654d2f14dc03e1d1d3ca0597992695d53
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-03-11 14:10:14 +0000

    ..F....... [ZBXNEXT-1263] support of tls related information in proxy form
    git-svn-id: svn://svn.zabbix.com/branches/dev@52677 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1263-1/frontends/php/app/views/administration.proxy.edit.js.php
+++ ZBXNEXT-1263-1/frontends/php/app/views/administration.proxy.edit.js.php
@@ -5,9 +5,13 @@
 			var active = (jQuery(this).val() == 5);
 			if (active) {
 				jQuery('#ip').closest('li').addClass('hidden');
+				jQuery('#tls_in_none').closest('li').addClass('hidden');
+				jQuery('#tls_connect').closest('li').removeClass('hidden');
 			}
 			else {
 				jQuery('#ip').closest('li').removeClass('hidden');
+				jQuery('#tls_in_none').closest('li').removeClass('hidden');
+				jQuery('#tls_connect').closest('li').addClass('hidden');
 			}
 		});
 
@@ -31,6 +35,43 @@
 			jQuery('#dns').val(jQuery.trim(jQuery('#dns').val()));
 			jQuery('#port').val(jQuery.trim(jQuery('#port').val()));
 			jQuery('#description').val(jQuery.trim(jQuery('#description').val()));
+
+			var tls_accept = 0;
+
+			if (jQuery('#tls_in_none').is(":checked")) {
+				tls_accept += 1;
+			}
+			if (jQuery('#tls_in_psk').is(":checked")) {
+				tls_accept += 2;
+			}
+			if (jQuery('#tls_in_cert').is(":checked")) {
+				tls_accept += 4;
+			}
+
+			jQuery('#tls_accept').val(tls_accept);
 		});
+
+		jQuery('#status, #tls_connect, #tls_in_psk, #tls_in_cert').change(function() {
+			var active = (jQuery('#status').val() == 5);
+				is_certificate = (active && jQuery('#tls_connect').val() == 4) || (!active && jQuery('#tls_in_cert').is(":checked")),
+				is_psk = (active && jQuery('#tls_connect').val() == 2) || (!active && jQuery('#tls_in_psk').is(":checked"));
+
+			if (is_certificate) {
+				jQuery('#tls_issuer, #tls_subject').prop('disabled', false);
+			}
+			else {
+				jQuery('#tls_issuer, #tls_subject').prop('disabled', true);
+			}
+
+			if (is_psk) {
+				jQuery('#tls_psk, #tls_psk_identity').prop('disabled', false);
+			}
+			else {
+				jQuery('#tls_psk, #tls_psk_identity').prop('disabled', true);
+			}
+		});
+
+		// refresh field visibility on document load
+		jQuery('#status, #tls_connect, #tls_psk_out').trigger('change');
 	});
 </script>
