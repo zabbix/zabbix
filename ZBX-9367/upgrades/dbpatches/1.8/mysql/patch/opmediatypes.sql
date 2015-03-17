aa276179fe6701da2e42afe6b571778435e2d564
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-03-17 16:52:01 +0000

    .......... [ZBX-9367] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@52769 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9367/upgrades/dbpatches/1.8/mysql/patch/opmediatypes.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE opmediatypes (
-      opmediatypeid           bigint unsigned         DEFAULT '0'     NOT NULL,
-      operationid             bigint unsigned         DEFAULT '0'     NOT NULL,
-      mediatypeid             bigint unsigned         DEFAULT '0'     NOT NULL,
-      PRIMARY KEY (opmediatypeid)
-) ENGINE=InnoDB;
-CREATE UNIQUE INDEX opmediatypes_1 on opmediatypes (operationid);
