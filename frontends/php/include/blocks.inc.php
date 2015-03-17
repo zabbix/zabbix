b7644b32da402662ea382b3570a0e276a829ac0c
ivo <ivo@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2015-03-17 16:00:45 +0000

    A.F....... [ZBX-9102] reverted changes made in r52747
    git-svn-id: svn://svn.zabbix.com/trunk@52760 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/blocks.inc.php
+++ frontends/php/include/blocks.inc.php
@@ -325,15 +325,14 @@ function make_system_status($filter) {
 		'skipDependent' => true,
 		'withLastEventUnacknowledged' => ($filter['extAck'] == EXTACK_OPTION_UNACK) ? true : null,
 		'selectLastEvent' => array('eventid', 'acknowledged', 'objectid'),
+		'expandDescription' => true,
 		'filter' => array(
 			'priority' => $filter['severity'],
 			'value' => TRIGGER_VALUE_TRUE
 		),
 		'sortfield' => 'lastchange',
 		'sortorder' => ZBX_SORT_DOWN,
-		'output' => array('triggerid', 'priority', 'state', 'description', 'error', 'value', 'lastchange',
-			'expression'
-		),
+		'output' => array('triggerid', 'priority', 'state', 'description', 'error', 'value', 'lastchange'),
 		'selectHosts' => array('name'),
 		'selectGroups' => array('groupid'),
 		'preservekeys' => true
@@ -828,8 +827,6 @@ function makeTriggersPopup(array $triggers, array $ackParams, array $actions, ar
 	));
 
 	CArrayHelper::sort($triggers, array(array('field' => 'lastchange', 'order' => ZBX_SORT_DOWN)));
-
-	$triggers = CMacrosResolverHelper::resolveTriggerNames($triggers);
 
 	foreach ($triggers as $trigger) {
 		// unknown triggers
