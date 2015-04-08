68a3887d8d65b8a844e4cd1ce2908bcd9da786d1
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-04-08 11:30:51 +0000

    .......... [ZBXNEXT-2683] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@53007 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-2683/frontends/php/include/views/administration.general.workingtime.edit.php
@@ -0,0 +1,35 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+$workingTimeTab = new CFormList('scriptsTab');
+$wtTextBox = new CTextBox('work_period', $data['work_period'], ZBX_TEXTBOX_STANDARD_SIZE);
+$wtTextBox->attr('autofocus', 'autofocus');
+$workingTimeTab->addRow(_('Working time'), $wtTextBox);
+
+$workingTimeView = new CTabView();
+$workingTimeView->addTab('workingTime', _('Working time'), $workingTimeTab);
+
+$workingTimeForm = new CForm();
+$workingTimeForm->setName('workingTimeForm');
+$workingTimeForm->addItem($workingTimeView);
+$workingTimeForm->addItem(makeFormFooter(new CSubmit('update', _('Update'))));
+
+return $workingTimeForm;
