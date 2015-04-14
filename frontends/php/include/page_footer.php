60f8c3ceab4770fa6f4686bb3cdaa04963820fd3
Alexei Vladishev <alexei.vladishev@zabbix.com>
2015-04-14 09:09:34 +0000

    ..F....... [ZBXNEXT-2662] fixed loading of js scripts when in fullscreen; it fixed displaying of graphs, screens and maps, also fixed remembering of checkbox selection in fullscreen mode

--- frontends/php/include/page_footer.php
+++ frontends/php/include/page_footer.php
@@ -56,12 +56,14 @@ if (in_array($page['type'], array(PAGE_TYPE_HTML_BLOCK, PAGE_TYPE_HTML))) {
 	}
 }
 
-if ($page['type'] == PAGE_TYPE_HTML && !defined('ZBX_PAGE_NO_MENU')) {
+if ($page['type'] == PAGE_TYPE_HTML) {
 	echo '</article>'."\n";
-	echo '<footer>'."\n";
-	echo '<a href="http://www.zabbix.com" target="_blank" class="logo"></a>'."\n";
-	echo 'Zabbix 2.4.1. &copy; 2001&ndash;2015, <a href="http://www.zabbix.com" target="_blank">Zabbix SIA</a>'."\n";
-	echo '</footer>'."\n";
+	if (!defined('ZBX_PAGE_NO_MENU')) {
+		echo '<footer>'."\n";
+		echo '<a href="http://www.zabbix.com" target="_blank" class="logo"></a>'."\n";
+		echo 'Zabbix 2.4.1. &copy; 2001&ndash;2015, <a href="http://www.zabbix.com" target="_blank">Zabbix SIA</a>'."\n";
+		echo '</footer>'."\n";
+	}
 	insertPagePostJs();
 	require_once 'include/views/js/common.init.js.php';
 	echo '</body>'."\n".
