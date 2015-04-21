4fb5c91a2d6060c6fda55d3d5ec0b7a4f3a46ccf
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-04-21 19:19:19 +0000

    .......... [ZBXNEXT-2495] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@53276 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2495/upgrades/dbpatches/2.0/postgresql/patch/services.sql
+++
@@ -1,3 +0,0 @@
-UPDATE services SET triggerid = NULL WHERE NOT EXISTS (SELECT 1 FROM triggers t WHERE t.triggerid = services.triggerid);
-ALTER TABLE ONLY services ALTER serviceid DROP DEFAULT;
-ALTER TABLE ONLY services ADD CONSTRAINT c_services_1 FOREIGN KEY (triggerid) REFERENCES triggers (triggerid) ON DELETE CASCADE;
