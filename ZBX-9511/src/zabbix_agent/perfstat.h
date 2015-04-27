27da41593ea7b5070d7ee14ff743b2f7f83b2cf5
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-04-27 09:43:01 +0000

    .......... [ZBX-9511] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@53367 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-9511/src/zabbix_agent/perfstat.h
@@ -0,0 +1,55 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#ifndef ZABBIX_PERFSTAT_H
+#define ZABBIX_PERFSTAT_H
+
+#ifndef _WINDOWS
+#	error "This module is only available for Windows OS"
+#endif
+
+#include "perfmon.h"
+
+#define UNSUPPORTED_REFRESH_PERIOD		600
+
+typedef struct
+{
+	PERF_COUNTER_DATA	*pPerfCounterList;
+	PDH_HQUERY		pdh_query;
+	time_t			nextcheck;	/* refresh time of not supported counters */
+}
+ZBX_PERF_STAT_DATA;
+
+extern ZBX_PERF_STAT_DATA	ppsd;
+
+PERF_COUNTER_DATA	*add_perf_counter(const char *name, const char *counterpath, int interval, char **error);
+void			remove_perf_counter(PERF_COUNTER_DATA *counter);
+
+double	compute_average_value(PERF_COUNTER_DATA *counter, int interval);
+
+int	init_perf_collector(int multithreaded);
+void	free_perf_collector();
+int	perf_collector_started();
+void	collect_perfstat();
+
+int	get_perf_counter_value_by_name(const char *name, double *value, char **error);
+int	get_perf_counter_value_by_path(const char *counterpath, int interval, double *value, char **error);
+int	get_perf_counter_value(PERF_COUNTER_DATA *counter, int interval, double *value, char **error);
+
+#endif
