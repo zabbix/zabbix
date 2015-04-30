066cc7becab08e100da854fcd36b795b856dd0c7
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-04-30 12:44:41 +0000

    A......... [ZBX-7654] separated CXmlValidateGeneral class from the child classes (no functional changes)
    git-svn-id: svn://svn.zabbix.com/branches/dev@53461 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7654/frontends/php/include/defines.inc.php
+++ ZBX-7654/frontends/php/include/defines.inc.php
@@ -890,6 +890,12 @@ define('ZBX_DEFAULT_IMPORT_HOST_GROUP', 'Imported hosts');
 // so this flag is not needed here.
 define('LIBXML_IMPORT_FLAGS', LIBXML_NONET);
 
+// XML validation
+define(XML_STRING,			0x01);
+define(XML_ARRAY,			0x02);
+define(XML_INDEXED_ARRAY,	0x04);
+define(XML_REQUIRED,		0x08);
+
 // API errors
 define('ZBX_API_ERROR_INTERNAL',	111);
 define('ZBX_API_ERROR_PARAMETERS',	100);
