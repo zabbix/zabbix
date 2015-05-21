f0a81f9dff5cd1776f52ce2a45c222f91536ba0e
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-05-21 13:39:11 +0000

    .......... [ZBXNEXT-786] updated unit tests
    git-svn-id: svn://svn.zabbix.com/branches/dev@53717 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-786/frontends/php/tests/unit/include/classes/import/converters/C20ImportConverterTest.php
+++ ZBXNEXT-786/frontends/php/tests/unit/include/classes/import/converters/C20ImportConverterTest.php
@@ -21,265 +21,229 @@
 
 class C20ImportConverterTest extends CImportConverterTest {
 
-	public function testConvertHosts() {
-		$source = $this->createSource([
-			'hosts' => [
+	public function testConvertProvider() {
+		return [
+			[
 				[],
-				[
-					'interfaces' => [
-						[
-							'type' => INTERFACE_TYPE_AGENT,
-						],
-						[
-							'type' => INTERFACE_TYPE_SNMP,
-						]
-					]
-				]
-			]
-		]);
-
-		$expectedResult = $this->createExpectedResult([
-			'hosts' => [
-				[],
-				[
-					'interfaces' => [
-						[
-							'type' => INTERFACE_TYPE_AGENT,
-							'bulk' => SNMP_BULK_ENABLED
-						],
-						[
-							'type' => INTERFACE_TYPE_SNMP,
-							'bulk' => SNMP_BULK_ENABLED
-						]
-					]
-				]
-			]
-		]);
-
-		$this->assertConvert($expectedResult, $source);
-	}
-
-	public function testConvertItems() {
-		$source = $this->createSource([
-			'hosts' => [
-				[],
-				[
-					'items' => ''
-				],
-				[
-					'items' => [
-						[],
-						[
-							'status' => ITEM_STATUS_NOTSUPPORTED
-						]
-					]
-				]
-			]
-		]);
-
-		$expectedResult = $this->createExpectedResult([
-			'hosts' => [
-				[],
-				[
-					'items' => ''
-				],
-				[
-					'items' => [
-						[],
-						[
-							'status' => ITEM_STATUS_ACTIVE
-						]
-					]
-				]
-			]
-		]);
-
-		$this->assertConvert($expectedResult, $source);
-	}
-
-	public function testConvertTriggers() {
-		$this->assertConvert(
-			$this->createExpectedResult([]),
-			$this->createSource([])
-		);
-		$this->assertConvert(
-			$this->createExpectedResult(['triggers' => '']),
-			$this->createSource(['triggers' => ''])
-		);
-
-		$source = $this->createSource([
-			'triggers' => [
-				[
-					'expression' => '{host:item.last(0)}#0|{host:item.last(0)}#1'
-				]
-			]
-		]);
-
-		$expectedResult = $this->createExpectedResult([
-			'triggers' => [
-				[
-					'expression' => '{host:item.last(0)}<>0 or {host:item.last(0)}<>1'
-				]
-			]
-		]);
-
-		$this->assertConvert($expectedResult, $source);
-	}
-
-	public function testConvertDiscoveryRules() {
-		$source = $this->createSource([
-			'hosts' => [
-				[],
-				[
-					'discovery_rules' => ''
-				],
-				[
-					'discovery_rules' => [
-						[],
-						[
-							'status' => ITEM_STATUS_NOTSUPPORTED,
-							'filter' => []
-						],
-						[
-							'filter' => ''
-						],
-						[
-							'filter' => ':'
-						],
-						[
-							'filter' => '{#MACRO}:regex'
-						],
-					]
-				]
+				[]
 			],
-			'templates' => [
+			[
 				[
-					'discovery_rules' => [
+					'hosts' => [
 						[],
 						[
-							'filter' => []
+							'interfaces' => [
+								['type' => INTERFACE_TYPE_AGENT],
+								['type' => INTERFACE_TYPE_SNMP]
+							]
 						],
 						[
-							'filter' => ''
+							'items' => [
+								['key' => 'agent.ping', 'status' => ITEM_STATUS_ACTIVE],
+								['key' => 'net.tcp.service[ntp]', 'status' => ITEM_STATUS_DISABLED],
+								['key' => 'net.tcp.service[tcp,,5432]', 'status' => ITEM_STATUS_NOTSUPPORTED]
+							]
 						],
 						[
-							'filter' => ':'
-						],
-						[
-							'filter' => '{#MACRO}:regex'
-						],
+							'discovery_rules' => [
+								[
+									'status' => ITEM_STATUS_ACTIVE,
+									'filter' => [],
+									'trigger_prototypes' => [
+										[
+											'expression' => '{host:item.last(0)}#0|{host:item.last(0)}#1'
+										]
+									]
+								],
+								[
+									'status' => ITEM_STATUS_DISABLED,
+									'filter' => ''
+								],
+								[
+									'status' => ITEM_STATUS_NOTSUPPORTED,
+									'filter' => ':'
+								],
+								[
+									'status' => ITEM_STATUS_ACTIVE,
+									'filter' => '{#MACRO}:regex'
+								],
+							]
+						]
 					]
-				]
-			]
-		]);
-
-		$expectedResult = $this->createExpectedResult([
-			'hosts' => [
-				[],
-				[
-					'discovery_rules' => ''
 				],
 				[
-					'discovery_rules' => [
+					'hosts' => [
 						[],
 						[
-							'status' => ITEM_STATUS_ACTIVE,
-							'filter' => []
+							'interfaces' => [
+								['type' => INTERFACE_TYPE_AGENT],
+								['type' => INTERFACE_TYPE_SNMP, 'bulk' => SNMP_BULK_ENABLED]
+							]
 						],
 						[
-							'filter' => ''
+							'items' => [
+								['key' => 'agent.ping', 'status' => ITEM_STATUS_ACTIVE],
+								['key' => 'net.udp.service[ntp]', 'status' => ITEM_STATUS_DISABLED],
+								['key' => 'net.tcp.service[tcp,,5432]', 'status' => ITEM_STATUS_ACTIVE]
+							]
 						],
-						[],
 						[
-							'filter' => [
-								'evaltype' => CONDITION_EVAL_TYPE_AND_OR,
-								'formula' => '',
-								'conditions' => [
-									[
-										'macro' => '{#MACRO}',
-										'value' => 'regex',
-										'operator' => CONDITION_OPERATOR_REGEXP,
-									]
+							'discovery_rules' => [
+								[
+									'status' => ITEM_STATUS_ACTIVE,
+									'filter' => [],
+									'trigger_prototypes' => [
+										[
+											'expression' => '{host:item.last(0)}<>0 or {host:item.last(0)}<>1'
+										]
+									],
+									'host_prototypes' => []
+								],
+								[
+									'status' => ITEM_STATUS_DISABLED,
+									'filter' => '',
+									'host_prototypes' => []
+								],
+								[
+									'status' => ITEM_STATUS_ACTIVE,
+									'host_prototypes' => []
+								],
+								[
+									'status' => ITEM_STATUS_ACTIVE,
+									'filter' => [
+										'evaltype' => CONDITION_EVAL_TYPE_AND_OR,
+										'formula' => '',
+										'conditions' => [
+											[
+												'macro' => '{#MACRO}',
+												'value' => 'regex',
+												'operator' => CONDITION_OPERATOR_REGEXP,
+											]
+										]
+									],
+									'host_prototypes' => []
 								]
 							]
 						]
 					]
 				]
 			],
-			'templates' => [
+			[
 				[
-					'discovery_rules' => [
+					'triggers' => [
+						[
+							'expression' => '{host:item.last(0)}#0|{host:item.last(0)}#1'
+						]
+					]
+				],
+				[
+					'triggers' => [
+						[
+							'expression' => '{host:item.last(0)}<>0 or {host:item.last(0)}<>1'
+						]
+					]
+				]
+			],
+			[
+				[
+					'templates' => [
 						[],
 						[
-							'filter' => []
+							'items' => [
+								['key' => 'agent.ping', 'status' => ITEM_STATUS_ACTIVE],
+								['key' => 'net.tcp.service[ntp]', 'status' => ITEM_STATUS_DISABLED],
+								['key' => 'net.tcp.service[tcp,,5432]', 'status' => ITEM_STATUS_NOTSUPPORTED]
+							]
 						],
 						[
-							'filter' => ''
-						],
-						[],
-						[
-							'filter' => [
-								'evaltype' => CONDITION_EVAL_TYPE_AND_OR,
-								'formula' => '',
-								'conditions' => [
-									[
-										'macro' => '{#MACRO}',
-										'value' => 'regex',
-										'operator' => CONDITION_OPERATOR_REGEXP,
+							'discovery_rules' => [
+								[
+									'status' => ITEM_STATUS_ACTIVE,
+									'filter' => [],
+									'trigger_prototypes' => [
+										[
+											'expression' => '{host:item.last(0)}#0|{host:item.last(0)}#1'
+										]
 									]
+								],
+								[
+									'status' => ITEM_STATUS_DISABLED,
+									'filter' => ''
+								],
+								[
+									'status' => ITEM_STATUS_NOTSUPPORTED,
+									'filter' => ':'
+								],
+								[
+									'status' => ITEM_STATUS_ACTIVE,
+									'filter' => '{#MACRO}:regex'
+								],
+							]
+						]
+					]
+				],
+				[
+					'templates' => [
+						[],
+						[
+							'items' => [
+								['key' => 'agent.ping', 'status' => ITEM_STATUS_ACTIVE],
+								['key' => 'net.udp.service[ntp]', 'status' => ITEM_STATUS_DISABLED],
+								['key' => 'net.tcp.service[tcp,,5432]', 'status' => ITEM_STATUS_ACTIVE]
+							]
+						],
+						[
+							'discovery_rules' => [
+								[
+									'status' => ITEM_STATUS_ACTIVE,
+									'filter' => [],
+									'trigger_prototypes' => [
+										[
+											'expression' => '{host:item.last(0)}<>0 or {host:item.last(0)}<>1'
+										]
+									],
+									'host_prototypes' => []
+								],
+								[
+									'status' => ITEM_STATUS_DISABLED,
+									'filter' => '',
+									'host_prototypes' => []
+								],
+								[
+									'status' => ITEM_STATUS_ACTIVE,
+									'host_prototypes' => []
+								],
+								[
+									'status' => ITEM_STATUS_ACTIVE,
+									'filter' => [
+										'evaltype' => CONDITION_EVAL_TYPE_AND_OR,
+										'formula' => '',
+										'conditions' => [
+											[
+												'macro' => '{#MACRO}',
+												'value' => 'regex',
+												'operator' => CONDITION_OPERATOR_REGEXP,
+											]
+										]
+									],
+									'host_prototypes' => []
 								]
 							]
 						]
 					]
 				]
 			]
-		]);
-
-		$this->assertConvert($expectedResult, $source);
+		];
 	}
 
-	public function testConvertTriggerPrototypes() {
-		$source = $this->createSource([
-			'hosts' => [
-				[
-					'discovery_rules' => [
-						[],
-						[
-							'trigger_prototypes' => ''
-						],
-						[
-							'trigger_prototypes' => [
-								[
-									'expression' => '{host:item.last(0)}#0|{host:item.last(0)}#1'
-								]
-							]
-						]
-					]
-				]
-			]
-		]);
-
-		$expectedResult = $this->createExpectedResult([
-			'hosts' => [
-				[
-					'discovery_rules' => [
-						[],
-						[
-							'trigger_prototypes' => ''
-						],
-						[
-							'trigger_prototypes' => [
-								[
-									'expression' => '{host:item.last(0)}<>0 or {host:item.last(0)}<>1'
-								]
-							]
-						]
-					]
-				]
-			]
-		]);
-
-		$this->assertConvert($expectedResult, $source);
+	/**
+	 * @dataProvider testConvertProvider
+	 *
+	 * @param $data
+	 * @param $expected
+	 */
+	public function testConvert(array $data, array $expected) {
+		$this->assertConvert($this->createExpectedResult($expected), $this->createSource($data));
 	}
 
 	protected function createSource(array $data = []) {
@@ -300,9 +264,9 @@ class C20ImportConverterTest extends CImportConverterTest {
 		];
 	}
 
-	protected function assertConvert(array $expectedResult, array $source) {
+	protected function assertConvert(array $expected, array $source) {
 		$result = $this->createConverter()->convert($source);
-		$this->assertEquals($expectedResult, $result);
+		$this->assertEquals($expected, $result);
 	}
 
 
