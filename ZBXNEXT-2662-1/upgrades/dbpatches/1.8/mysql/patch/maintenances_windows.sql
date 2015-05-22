511af53cfb6a52f776883495cadbb1b2fffb2183
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-05-22 08:38:02 +0000

    .......... [ZBXNEXT-2662] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@53739 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2662-1/upgrades/dbpatches/1.8/mysql/patch/maintenances_windows.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE maintenances_windows (
-      maintenance_timeperiodid                bigint unsigned         DEFAULT '0'     NOT NULL,
-      maintenanceid           bigint unsigned         DEFAULT '0'     NOT NULL,
-      timeperiodid            bigint unsigned         DEFAULT '0'     NOT NULL,
-      PRIMARY KEY (maintenance_timeperiodid)
-) ENGINE=InnoDB;
-CREATE INDEX maintenances_windows_1 on maintenances_windows (maintenanceid,timeperiodid);
