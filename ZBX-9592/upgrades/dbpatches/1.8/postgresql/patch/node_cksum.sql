e31a13f8919f07fdf4491a275cd9862b3749a0f0
igors <igors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-05-28 07:45:37 +0000

    .......... [ZBX-9592] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@53833 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-9592/upgrades/dbpatches/1.8/postgresql/patch/node_cksum.sql
@@ -0,0 +1,10 @@
+DROP TABLE node_cksum;
+CREATE TABLE node_cksum (
+	nodeid		integer		DEFAULT '0'	NOT NULL,
+	tablename		varchar(64)		DEFAULT ''	NOT NULL,
+	recordid		bigint		DEFAULT '0'	NOT NULL,
+	cksumtype		integer		DEFAULT '0'	NOT NULL,
+	cksum		text		DEFAULT ''	NOT NULL,
+	sync		char(128)		DEFAULT ''	NOT NULL
+) with OIDS;
+CREATE INDEX node_cksum_1 on node_cksum (nodeid,cksumtype,tablename,recordid);
