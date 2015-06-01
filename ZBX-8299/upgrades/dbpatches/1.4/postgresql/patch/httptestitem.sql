ceb13bfb7127f3c9aaa628c0616c0b5a1c3739bb
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-06-01 06:35:06 +0000

    .......... [ZBX-8299] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@53872 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8299/upgrades/dbpatches/1.4/postgresql/patch/httptestitem.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE httptestitem (
-	httptestitemid	bigint DEFAULT '0'	NOT NULL,
-	httptestid	bigint DEFAULT '0'	NOT NULL,
-	itemid		bigint DEFAULT '0'	NOT NULL,
-	type		integer		DEFAULT '0'	NOT NULL,
-	PRIMARY KEY (httptestitemid)
-) with OIDS;
