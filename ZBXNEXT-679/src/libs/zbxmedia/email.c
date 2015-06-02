aa3a2d9d730abc071b86d99b182af75adfcf6deb
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-06-02 08:30:37 +0000

    ........S. [ZBXNEXT-679] lowered libcurl version requirement to 7.20.0 by omitting explicit login options
    git-svn-id: svn://svn.zabbix.com/branches/dev@53897 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-679/src/libs/zbxmedia/email.c
+++ ZBXNEXT-679/src/libs/zbxmedia/email.c
@@ -603,20 +603,26 @@ static int	send_email_curl(const char *smtp_server, unsigned short smtp_port, co
 
 	if (SMTP_AUTHENTICATION_NORMAL_PASSWORD == smtp_authentication)
 	{
-#if 0x072200 <= LIBCURL_VERSION_NUM	/* versions 7.34.0 and above support CURLOPT_LOGIN_OPTIONS */
+#if 0x071e00 >= LIBCURL_VERSION_NUM	/* versions 7.20.0 to 7.30.0 do not support specifying login options */
 		if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_USERNAME, username)) ||
-				CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_PASSWORD, password)) ||
-				CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_LOGIN_OPTIONS, "AUTH=PLAIN")))
+				CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_PASSWORD, password)))
 		{
 			goto error;
 		}
-#else					/* versions 7.31.0 to 7.33.0 support login options in CURLOPT_USERPWD */
+#elif 0x072100 >= LIBCURL_VERSION_NUM	/* versions 7.31.0 to 7.33.0 support login options in CURLOPT_USERPWD */
 		char	userpwd[523];
 
 		zbx_snprintf(userpwd, sizeof(userpwd), "%s:%s;AUTH=PLAIN", username, password);
 
 		if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_USERPWD, userpwd)))
 			goto error;
+#else					/* versions 7.34.0 and above support explicit CURLOPT_LOGIN_OPTIONS */
+		if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_USERNAME, username)) ||
+				CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_PASSWORD, password)) ||
+				CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_LOGIN_OPTIONS, "AUTH=PLAIN")))
+		{
+			goto error;
+		}
 #endif
 	}
 
