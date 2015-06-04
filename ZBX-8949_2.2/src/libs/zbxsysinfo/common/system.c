c2a4221259c72c28d3b5882d7f43382f42e406b3
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-06-04 12:57:44 +0000

    .......... [ZBX-8949] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@53942 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-8949_2.2/src/libs/zbxsysinfo/common/system.c
@@ -0,0 +1,116 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+#include "sysinfo.h"
+#include "log.h"
+
+#ifdef _WINDOWS
+#	include "perfmon.h"
+#	pragma comment(lib, "user32.lib")
+#endif
+
+int	SYSTEM_LOCALTIME(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+	char		*type, buf[32];
+	struct tm	*tm;
+	size_t		offset;
+	int		gmtoff, ms;
+	unsigned short	h, m;
+#ifdef _WINDOWS
+        struct _timeb	tv;
+#else
+	struct timeval	tv;
+	struct timezone	tz;
+#endif
+
+	if (1 < request->nparam)
+		return SYSINFO_RET_FAIL;
+
+	type = get_rparam(request, 0);
+
+	if (NULL == type || '\0' == *type || 0 == strcmp(type, "utc"))
+	{
+		SET_UI64_RESULT(result, time(NULL));
+	}
+	else if (0 == strcmp(type, "local"))
+	{
+#ifdef _WINDOWS
+	        _ftime(&tv);
+		tm = localtime(&tv.time);
+		ms = tv.millitm;
+#else
+		gettimeofday(&tv, &tz);
+		tm = localtime(&tv.tv_sec);
+		ms = (int)(tv.tv_usec / 1000);
+#endif
+		offset = zbx_snprintf(buf, sizeof(buf), "%04d-%02d-%02d,%02d:%02d:%02d.%03d,",
+				1900 + tm->tm_year, 1 + tm->tm_mon, tm->tm_mday,
+				tm->tm_hour, tm->tm_min, tm->tm_sec, ms);
+
+		/* timezone offset */
+#if defined(HAVE_TM_TM_GMTOFF)
+		gmtoff = tm->tm_gmtoff;
+#else
+		gmtoff = -timezone;
+#endif
+#ifdef _WINDOWS
+		if (0 < tm->tm_isdst)		/* daylight saving time */
+			gmtoff += SEC_PER_HOUR;	/* assume DST is one hour */
+#endif
+		h = (unsigned short)(abs(gmtoff) / SEC_PER_HOUR);
+		m = (unsigned short)((abs(gmtoff) - h * SEC_PER_HOUR) / SEC_PER_MIN);
+
+		if (0 <= gmtoff)
+			offset += zbx_snprintf(buf + offset, sizeof(buf) - offset, "+");
+		else
+			offset += zbx_snprintf(buf + offset, sizeof(buf) - offset, "-");
+
+		offset += zbx_snprintf(buf + offset, sizeof(buf) - offset, "%02d:%02d", (int)h, (int)m);
+
+		SET_STR_RESULT(result, strdup(buf));
+	}
+	else
+		return SYSINFO_RET_FAIL;
+
+	return SYSINFO_RET_OK;
+}
+
+int	SYSTEM_USERS_NUM(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+#ifdef _WINDOWS
+	char		counter_path[64];
+	AGENT_REQUEST	request_tmp;
+	int		ret;
+
+	zbx_snprintf(counter_path, sizeof(counter_path), "\\%d\\%d", PCI_TERMINAL_SERVICES, PCI_TOTAL_SESSIONS);
+
+	request_tmp.nparam = 1;
+	request_tmp.params = zbx_malloc(NULL, request_tmp.nparam * sizeof(char *));
+	request_tmp.params[0] = counter_path;
+
+	ret = PERF_COUNTER(&request_tmp, result);
+
+	zbx_free(request_tmp.params);
+
+	return ret;
+#else
+	return EXECUTE_INT("who | wc -l", result);
+#endif
+}
---
+++ ZBX-8949_2.2/src/zabbix_proxy/heart/heart.c
@@ -0,0 +1,146 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+#include "daemon.h"
+#include "log.h"
+#include "zbxjson.h"
+#include "zbxself.h"
+
+#include "heart.h"
+#include "../servercomms.h"
+
+extern unsigned char	process_type;
+
+/******************************************************************************
+ *                                                                            *
+ * Function: send_heartbeat                                                   *
+ *                                                                            *
+ * Purpose:                                                                   *
+ *                                                                            *
+ * Parameters:                                                                *
+ *                                                                            *
+ * Return value:                                                              *
+ *                                                                            *
+ * Author: Alexander Vladishev                                                *
+ *                                                                            *
+ * Comments:                                                                  *
+ *                                                                            *
+ ******************************************************************************/
+static int	send_heartbeat(void)
+{
+	zbx_sock_t	sock;
+	struct zbx_json	j;
+	int		ret = SUCCEED;
+	char		*error = NULL;
+
+	zabbix_log(LOG_LEVEL_DEBUG, "In send_heartbeat()");
+
+	zbx_json_init(&j, 128);
+	zbx_json_addstring(&j, "request", ZBX_PROTO_VALUE_PROXY_HEARTBEAT, ZBX_JSON_TYPE_STRING);
+	zbx_json_addstring(&j, "host", CONFIG_HOSTNAME, ZBX_JSON_TYPE_STRING);
+
+	if (FAIL == connect_to_server(&sock, CONFIG_HEARTBEAT_FREQUENCY, 0)) /* do not retry */
+		return FAIL;
+
+	if (SUCCEED != put_data_to_server(&sock, &j, &error))
+	{
+		zabbix_log(LOG_LEVEL_WARNING, "sending heartbeat message to server failed: %s", error);
+		ret = FAIL;
+	}
+
+	zbx_free(error);
+	disconnect_server(&sock);
+
+	return ret;
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Function: main_heart_loop                                                  *
+ *                                                                            *
+ * Purpose: periodically send heartbeat message to the server                 *
+ *                                                                            *
+ * Parameters:                                                                *
+ *                                                                            *
+ * Return value:                                                              *
+ *                                                                            *
+ * Author: Alexander Vladishev                                                *
+ *                                                                            *
+ * Comments:                                                                  *
+ *                                                                            *
+ ******************************************************************************/
+void	main_heart_loop(void)
+{
+	int	start, sleeptime = 0, res;
+	double	sec, total_sec = 0.0, old_total_sec = 0.0;
+	time_t	last_stat_time;
+
+#define STAT_INTERVAL	5	/* if a process is busy and does not sleep then update status not faster than */
+				/* once in STAT_INTERVAL seconds */
+
+	last_stat_time = time(NULL);
+
+	zbx_setproctitle("%s [sending heartbeat message]", get_process_type_string(process_type));
+
+	for (;;)
+	{
+		if (0 != sleeptime)
+		{
+			zbx_setproctitle("%s [sending heartbeat message %s in " ZBX_FS_DBL " sec, "
+					"sending heartbeat message]",
+					get_process_type_string(process_type),
+					SUCCEED == res ? "success" : "failed", old_total_sec);
+		}
+
+		start = time(NULL);
+		sec = zbx_time();
+		res = send_heartbeat();
+		total_sec += zbx_time() - sec;
+
+		sleeptime = CONFIG_HEARTBEAT_FREQUENCY - (time(NULL) - start);
+
+		if (0 != sleeptime || STAT_INTERVAL <= time(NULL) - last_stat_time)
+		{
+			if (0 == sleeptime)
+			{
+				zbx_setproctitle("%s [sending heartbeat message %s in " ZBX_FS_DBL " sec, "
+						"sending heartbeat message]",
+						get_process_type_string(process_type),
+						SUCCEED == res ? "success" : "failed", total_sec);
+
+			}
+			else
+			{
+				zbx_setproctitle("%s [sending heartbeat message %s in " ZBX_FS_DBL " sec, "
+						"idle %d sec]",
+						get_process_type_string(process_type),
+						SUCCEED == res ? "success" : "failed", total_sec, sleeptime);
+
+				old_total_sec = total_sec;
+			}
+			total_sec = 0.0;
+			last_stat_time = time(NULL);
+		}
+
+		zbx_sleep_loop(sleeptime);
+	}
+
+#undef STAT_INTERVAL
+}
