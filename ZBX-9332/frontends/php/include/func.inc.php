9cf00295b18cc593bc4e636f4de2bd9909dcfc46
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-06-04 09:32:39 +0000

    .......... [ZBX-9332] updated to latest from /branches/2.2
    git-svn-id: svn://svn.zabbix.com/branches/dev@53937 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9332/frontends/php/include/func.inc.php
+++ ZBX-9332/frontends/php/include/func.inc.php
@@ -1776,7 +1776,13 @@ function getPagingLine(&$items, array $removeUrlParams = array(), array $urlPara
 
 	$searchLimit = '';
 	if ($config['search_limit'] < count($items)) {
-		array_pop($items);
+		if (getPageSortOrder() == ZBX_SORT_UP) {
+			array_pop($items);
+		}
+		else {
+			array_shift($items);
+		}
+
 		$searchLimit = '+';
 	}
 
