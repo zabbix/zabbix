184f3c4682473445392cf00abfbd47a1d67aef52
sasha <sasha@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2015-06-09 08:17:01 +0000

    A.F....... [ZBX-9540] fixed import/export of the "Host time" clock screen element
    git-svn-id: svn://svn.zabbix.com/trunk@54002 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/views/configuration.screen.constructor.edit.php
+++ frontends/php/include/views/configuration.screen.constructor.edit.php
@@ -573,9 +573,10 @@ elseif ($resourceType == SCREEN_RESOURCE_CLOCK) {
 
 	if (zbx_empty($caption) && TIME_TYPE_HOST == $style && $resourceId > 0) {
 		$items = API::Item()->get([
-			'itemids' => $resourceId,
+			'output' => ['itemid', 'hostid', 'key_', 'name'],
 			'selectHosts' => ['name'],
-			'output' => ['itemid', 'hostid', 'key_', 'name']
+			'itemids' => $resourceId,
+			'webitems' => true
 		]);
 
 		if ($items) {
@@ -642,7 +643,7 @@ elseif (in_array($resourceType, [SCREEN_RESOURCE_TRIGGERS_OVERVIEW, SCREEN_RESOU
 	];
 	$screenFormList->addRow(_('Hosts location'), new CDiv($styleRadioButton, 'jqueryinputset radioset'));
 }
-else {
+elseif ($resourceType != SCREEN_RESOURCE_CLOCK) {
 	$screenFormList->addVar('style', 0);
 }
 
