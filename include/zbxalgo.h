394a13d2dab16b070995af738955982e7326a226
Andris Zeila <andris.zeila@zabbix.com>
2015-06-16 06:50:01 +0000

    ........S. [ZBXNEXT-1219] itroduced ZBX_DEFAULT_HASH_ALGO define to use instead of directly calling zbx_hash_modfnv

--- include/zbxalgo.h
+++ include/zbxalgo.h
@@ -32,6 +32,7 @@ zbx_hash_t	zbx_hash_murmur2(const void *data, size_t len, zbx_hash_t seed);
 zbx_hash_t	zbx_hash_sdbm(const void *data, size_t len, zbx_hash_t seed);
 zbx_hash_t	zbx_hash_djb2(const void *data, size_t len, zbx_hash_t seed);
 
+#define ZBX_DEFAULT_HASH_ALGO		zbx_hash_modfnv
 #define ZBX_DEFAULT_UINT64_HASH_ALGO	zbx_hash_modfnv
 #define ZBX_DEFAULT_STRING_HASH_ALGO	zbx_hash_modfnv
 
