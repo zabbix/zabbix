55cf68bd54f976591256e2417ba488c52789c40e
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-06-17 05:40:35 +0000

    ..F....... [ZBXNEXT-1263] added host encryptions import/export
    git-svn-id: svn://svn.zabbix.com/branches/dev@54089 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1263-1/frontends/php/include/classes/export/CConfigurationExport.php
+++ ZBXNEXT-1263-1/frontends/php/include/classes/export/CConfigurationExport.php
@@ -302,7 +302,8 @@ class CConfigurationExport {
 			'hostids' => $hostIds,
 			'output' => [
 				'proxy_hostid', 'host', 'status', 'ipmi_authtype', 'ipmi_privilege', 'ipmi_username', 'ipmi_password',
-				'name', 'description'
+				'name', 'description', 'tls_connect', 'tls_accept', 'tls_issuer', 'tls_subject', 'tls_psk_identity',
+				'tls_psk'
 			],
 			'selectInventory' => true,
 			'selectInterfaces' => ['interfaceid', 'main', 'type', 'useip', 'ip', 'dns', 'port', 'bulk'],
