2f75466a5d0ef514897ad7bff816d8b0aab6a570
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-06-21 05:48:39 +0000

    ..F....... [ZBXNEXT-2662] more compact and readable code for creation of dashboard widgets
    git-svn-id: svn://svn.zabbix.com/branches/dev@54149 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2662-4/frontends/php/app/views/monitoring.dashboard.view.php
+++ ZBXNEXT-2662-4/frontends/php/app/views/monitoring.dashboard.view.php
@@ -21,7 +21,7 @@
 
 $this->addJSfile('js/class.pmaster.js');
 
-$widget = (new CWidget())
+$dashboard = (new CWidget())
 	->setTitle(_('Dashboard'))
 	->setControls((new CForm())
 		->cleanItems()
@@ -96,169 +96,76 @@ $col = CProfile::get('web.dashboard.widget.'.WIDGET_FAVOURITE_SCREENS.'.col', 0)
 $row = CProfile::get('web.dashboard.widget.'.WIDGET_FAVOURITE_SCREENS.'.row', 1);
 $dashboardGrid[$col][$row] = $favouriteScreens;
 
-// status of Zabbix
+$widgets = [
+	WIDGET_SYSTEM_STATUS => [
+		'header' => _('System status'),
+		'action' => 'widget.system.view',
+		'col' => 1,
+		'row' => 1
+	],
+	WIDGET_HOST_STATUS => [
+		'header' => _('Host status'),
+		'action' => 'widget.hosts.view',
+		'col' => 1,
+		'row' => 2
+	],
+	WIDGET_LAST_ISSUES => [
+		'header' => _n('Last %1$d issue', 'Last %1$d issues', DEFAULT_LATEST_ISSUES_CNT),
+		'action' => 'widget.issues.view',
+		'col' => 1,
+		'row' => 3
+	],
+	WIDGET_WEB_OVERVIEW => [
+		'header' => _('Web monitoring'),
+		'action' => 'widget.web.view',
+		'col' => 1,
+		'row' => 4
+	],
+];
+
 if ($data['show_status_widget']) {
-	$rate = CProfile::get('web.dashboard.widget.'.WIDGET_ZABBIX_STATUS.'.rf_rate', 60);
-
-	$icon = (new CRedirectButton(SPACE, null))
-		->addClass(ZBX_STYLE_BTN_WIDGET_ACTION)
-		->setTitle(_('Action'))
-		->setMenuPopup(CMenuPopupHelper::getRefresh(WIDGET_ZABBIX_STATUS, $rate));
-
-	$zabbixStatus = (new CCollapsibleUiWidget(WIDGET_ZABBIX_STATUS, (new CDiv())->addClass('preloader')))
-	->setExpanded((bool) CProfile::get('web.dashboard.widget.'.WIDGET_ZABBIX_STATUS.'.state', true))
-	->setHeader(_('Status of Zabbix'), [$icon])
-	->setFooter(new CList([
-		(new CDiv())->addClass('textwhite')->setId(WIDGET_ZABBIX_STATUS.'_footer')
-	]));
-
-	$col = CProfile::get('web.dashboard.widget.'.WIDGET_ZABBIX_STATUS.'.col', 1);
-	$row = CProfile::get('web.dashboard.widget.'.WIDGET_ZABBIX_STATUS.'.row', 0);
-	$dashboardGrid[$col][$row] = $zabbixStatus;
-
-	$widgetRefreshParams[WIDGET_ZABBIX_STATUS] = [
-		'frequency' => $rate,
-		'url' => 'zabbix.php?action=widget.status.view',
-		'counter' => 0,
-		'darken' => 0,
-		'params' => ['widgetRefresh' => WIDGET_ZABBIX_STATUS]
+	$widgets[WIDGET_ZABBIX_STATUS] = [
+		'header' => _('Status of Zabbix'),
+		'action' => 'widget.status.view',
+		'col' => 1,
+		'row' => 0
+	];
+}
+if ($data['show_discovery_widget']) {
+	$widgets[WIDGET_DISCOVERY_STATUS] = [
+		'header' => _('Discovery status'),
+		'action' => 'widget.discovery.view',
+		'col' => 1,
+		'row' => 5
 	];
 }
 
-// system status
-$rate = CProfile::get('web.dashboard.widget.'.WIDGET_SYSTEM_STATUS.'.rf_rate', 60);
+foreach ($widgets as $widgetid => $widget) {
+	$profile = 'web.dashboard.widget.'.$widgetid;
 
-$icon = (new CRedirectButton(SPACE, null))
-	->addClass(ZBX_STYLE_BTN_WIDGET_ACTION)
-	->setTitle(_('Action'))
-	->setMenuPopup(CMenuPopupHelper::getRefresh(WIDGET_SYSTEM_STATUS, $rate));
-
-$systemStatus = (new CCollapsibleUiWidget(WIDGET_SYSTEM_STATUS, (new CDiv())->addClass('preloader')))
-	->setExpanded((bool) CProfile::get('web.dashboard.widget.'.WIDGET_SYSTEM_STATUS.'.state', true))
-	->setHeader(_('System status'), [$icon])
-	->setFooter(new CList([
-		(new CDiv())->addClass('textwhite')->setId(WIDGET_SYSTEM_STATUS.'_footer')
-	]));
-
-$col = CProfile::get('web.dashboard.widget.'.WIDGET_SYSTEM_STATUS.'.col', 1);
-$row = CProfile::get('web.dashboard.widget.'.WIDGET_SYSTEM_STATUS.'.row', 1);
-$dashboardGrid[$col][$row] = $systemStatus;
-
-$widgetRefreshParams[WIDGET_SYSTEM_STATUS] = [
-	'frequency' => $rate,
-	'url' => 'zabbix.php?action=widget.system.view',
-	'counter' => 0,
-	'darken' => 0,
-	'params' => ['widgetRefresh' => WIDGET_SYSTEM_STATUS]
-];
-
-// host status
-$rate = CProfile::get('web.dashboard.widget.'.WIDGET_HOST_STATUS.'.rf_rate', 60);
-
-$icon = (new CRedirectButton(SPACE, null))
-	->addClass(ZBX_STYLE_BTN_WIDGET_ACTION)
-	->setTitle(_('Action'))
-	->setMenuPopup(CMenuPopupHelper::getRefresh(WIDGET_HOST_STATUS, $rate));
-
-$hostStatus = (new CCollapsibleUiWidget(WIDGET_HOST_STATUS, (new CDiv())->addClass('preloader')))
-	->setExpanded((bool) CProfile::get('web.dashboard.widget.'.WIDGET_HOST_STATUS.'.state', true))
-	->setHeader(_('Host status'), [$icon])
-	->setFooter(new CList([
-		(new CDiv())->addClass('textwhite')->setId(WIDGET_HOST_STATUS.'_footer')
-	]));
-
-$col = CProfile::get('web.dashboard.widget.'.WIDGET_HOST_STATUS.'.col', 1);
-$row = CProfile::get('web.dashboard.widget.'.WIDGET_HOST_STATUS.'.row', 2);
-$dashboardGrid[$col][$row] = $hostStatus;
-
-$widgetRefreshParams[WIDGET_HOST_STATUS] = [
-	'frequency' => $rate,
-	'url' => 'zabbix.php?action=widget.hosts.view',
-	'counter' => 0,
-	'darken' => 0,
-	'params' => ['widgetRefresh' => WIDGET_HOST_STATUS]
-];
-
-// last issues
-$rate = CProfile::get('web.dashboard.widget.'.WIDGET_LAST_ISSUES.'.rf_rate', 60);
-
-$icon = (new CRedirectButton(SPACE, null))
-	->addClass(ZBX_STYLE_BTN_WIDGET_ACTION)
-	->setTitle(_('Action'))
-	->setMenuPopup(CMenuPopupHelper::getRefresh(WIDGET_LAST_ISSUES, $rate));
-
-$lastIssues = (new CCollapsibleUiWidget(WIDGET_LAST_ISSUES, (new CDiv())->addClass('preloader')))
-	->setExpanded((bool) CProfile::get('web.dashboard.widget.'.WIDGET_LAST_ISSUES.'.state', true))
-	->setHeader(_n('Last %1$d issue', 'Last %1$d issues', DEFAULT_LATEST_ISSUES_CNT), [$icon])
-	->setFooter(new CList([
-		(new CDiv())->addClass('textwhite')->setId(WIDGET_LAST_ISSUES.'_footer')
-	]));
-
-$col = CProfile::get('web.dashboard.widget.'.WIDGET_LAST_ISSUES.'.col', 1);
-$row = CProfile::get('web.dashboard.widget.'.WIDGET_LAST_ISSUES.'.row', 3);
-$dashboardGrid[$col][$row] = $lastIssues;
-
-$widgetRefreshParams[WIDGET_LAST_ISSUES] = [
-	'frequency' => $rate,
-	'url' => 'zabbix.php?action=widget.issues.view',
-	'counter' => 0,
-	'darken' => 0,
-	'params' => ['widgetRefresh' => WIDGET_LAST_ISSUES]
-];
-
-// web monitoring
-$rate = CProfile::get('web.dashboard.widget.'.WIDGET_WEB_OVERVIEW.'.rf_rate', 60);
-
-$icon = (new CRedirectButton(SPACE, null))
-	->addClass(ZBX_STYLE_BTN_WIDGET_ACTION)
-	->setTitle(_('Action'))
-	->setMenuPopup(CMenuPopupHelper::getRefresh(WIDGET_WEB_OVERVIEW, $rate));
-
-$webMonitoring = (new CCollapsibleUiWidget(WIDGET_WEB_OVERVIEW, (new CDiv())->addClass('preloader')))
-	->setExpanded((bool) CProfile::get('web.dashboard.widget.'.WIDGET_WEB_OVERVIEW.'.state', true))
-	->setHeader(_('Web monitoring'), [$icon])
-	->setFooter(new CList([
-		(new CDiv())->addClass('textwhite')->setId(WIDGET_WEB_OVERVIEW.'_footer')
-	]));
-
-$col = CProfile::get('web.dashboard.widget.'.WIDGET_WEB_OVERVIEW.'.col', 1);
-$row = CProfile::get('web.dashboard.widget.'.WIDGET_WEB_OVERVIEW.'.row', 4);
-$dashboardGrid[$col][$row] = $webMonitoring;
-
-$widgetRefreshParams[WIDGET_WEB_OVERVIEW] = [
-	'frequency' => $rate,
-	'url' => 'zabbix.php?action=widget.web.view',
-	'counter' => 0,
-	'darken' => 0,
-	'params' => ['widgetRefresh' => WIDGET_WEB_OVERVIEW]
-];
-
-// discovery rules
-if ($data['show_discovery_widget']) {
-	$rate = CProfile::get('web.dashboard.widget.'.WIDGET_DISCOVERY_STATUS.'.rf_rate', 60);
+	$rate = CProfile::get($profile.'.rf_rate', 60);
+	$expanded = (bool) CProfile::get($profile.'.state', true);
+	$col = CProfile::get($profile.'.col', $widget['col']);
+	$row = CProfile::get($profile.'.row', $widget['row']);
 
 	$icon = (new CRedirectButton(SPACE, null))
 		->addClass(ZBX_STYLE_BTN_WIDGET_ACTION)
 		->setTitle(_('Action'))
-		->setMenuPopup(CMenuPopupHelper::getRefresh(WIDGET_DISCOVERY_STATUS, $rate));
+		->setMenuPopup(CMenuPopupHelper::getRefresh($widgetid, $rate));
 
-	$discoveryStatus = (new CCollapsibleUiWidget(WIDGET_DISCOVERY_STATUS, (new CDiv())->addClass('preloader')))
-		->setExpanded((bool) CProfile::get('web.dashboard.widget.'.WIDGET_DISCOVERY_STATUS.'.state', true))
-		->setHeader(_('Discovery status'), [$icon])
+	$dashboardGrid[$col][$row] = (new CCollapsibleUiWidget($widgetid, (new CDiv())->addClass('preloader')))
+		->setExpanded($expanded)
+		->setHeader($widget['header'], [$icon])
 		->setFooter(new CList([
-			(new CDiv())->addClass('textwhite')->setId(WIDGET_DISCOVERY_STATUS.'_footer')
+			(new CDiv())->addClass('textwhite')->setId($widgetid.'_footer')
 		]));
 
-	$col = CProfile::get('web.dashboard.widget.'.WIDGET_DISCOVERY_STATUS.'.col', 1);
-	$row = CProfile::get('web.dashboard.widget.'.WIDGET_DISCOVERY_STATUS.'.row', 5);
-	$dashboardGrid[$col][$row] = $discoveryStatus;
-
-	$widgetRefreshParams[WIDGET_DISCOVERY_STATUS] = [
+	$widgetRefreshParams[$widgetid] = [
 		'frequency' => $rate,
-		'url' => 'zabbix.php?action=widget.discovery.view',
+		'url' => 'zabbix.php?action='.$widget['action'],
 		'counter' => 0,
 		'darken' => 0,
-		'params' => ['widgetRefresh' => WIDGET_DISCOVERY_STATUS]
+		'params' => ['widgetRefresh' => $widgetid]
 	];
 }
 
@@ -279,7 +186,7 @@ $dashboardTable = (new CDiv($dashboardRow))
 	->addClass('table')
 	->addClass('widget-placeholder');
 
-$widget->addItem($dashboardTable)->show();
+$dashboard->addItem($dashboardTable)->show();
 
 /*
  * Javascript
