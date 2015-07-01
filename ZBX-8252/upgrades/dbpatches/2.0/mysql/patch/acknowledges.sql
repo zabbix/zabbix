cbb9a8abd2614f6a6ab9e7c7a6310976cabcd0fe
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-07-01 14:00:49 +0000

    .......... [ZBX-8252] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@54240 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8252/upgrades/dbpatches/2.0/mysql/patch/acknowledges.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE acknowledges MODIFY acknowledgeid bigint unsigned NOT NULL,
-			 MODIFY userid bigint unsigned NOT NULL,
-			 MODIFY eventid bigint unsigned NOT NULL;
-DELETE FROM acknowledges WHERE NOT userid IN (SELECT userid FROM users);
-DELETE FROM acknowledges WHERE NOT eventid IN (SELECT eventid FROM events);
-ALTER TABLE acknowledges ADD CONSTRAINT c_acknowledges_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
-ALTER TABLE acknowledges ADD CONSTRAINT c_acknowledges_2 FOREIGN KEY (eventid) REFERENCES events (eventid) ON DELETE CASCADE;
