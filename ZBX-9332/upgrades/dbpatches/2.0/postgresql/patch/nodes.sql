2510b58706c93e89141605dc2a81ac369f5ec231
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-07-14 15:07:12 +0000

    .......... [ZBX-9332] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@54399 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9332/upgrades/dbpatches/2.0/postgresql/patch/nodes.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE ONLY nodes
-	ALTER nodeid DROP DEFAULT,
-	ALTER masterid DROP DEFAULT,
-	ALTER masterid DROP NOT NULL,
-	DROP COLUMN timezone,
-	DROP COLUMN slave_history,
-	DROP COLUMN slave_trends;
-UPDATE nodes SET masterid=NULL WHERE masterid=0;
-ALTER TABLE ONLY nodes ADD CONSTRAINT c_nodes_1 FOREIGN KEY (masterid) REFERENCES nodes (nodeid);
