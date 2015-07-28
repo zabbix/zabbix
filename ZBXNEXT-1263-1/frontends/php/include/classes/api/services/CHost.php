bf1d5c5a09d4eaf8b724550e3924caab34603ac3
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-07-28 15:14:55 +0000

    A.F....... [ZBXNEXT-1219] fixed discovery host update for encryption field
    git-svn-id: svn://svn.zabbix.com/branches/dev@54592 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1263-1/frontends/php/include/classes/api/services/CHost.php
+++ ZBXNEXT-1263-1/frontends/php/include/classes/api/services/CHost.php
@@ -554,7 +554,9 @@ class CHost extends CHostGeneral {
 		]);
 
 		$updateDiscoveredValidator = new CUpdateDiscoveredValidator([
-			'allowed' => ['hostid', 'status', 'inventory', 'description'],
+			'allowed' => ['hostid', 'status', 'inventory', 'description', 'tls_connect', 'tls_accept', 'tls_issuer',
+				'tls_subject', 'tls_psk_identity', 'tls_psk'
+			],
 			'messageAllowedField' => _('Cannot update "%2$s" for a discovered host "%1$s".')
 		]);
 
