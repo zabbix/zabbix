6e6460926deabdb068ddc026a875698a648c915f
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-07-28 07:44:14 +0000

    .......... [ZBXNEXT-826] undoing changes in r54563
    git-svn-id: svn://svn.zabbix.com/branches/2.2@54564 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	public static final String REVISION_DATE = "22 July 2015";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "2.2.11rc1";
+	public static final String VERSION = "2.2.10";
 
 	public static void printVersion()
 	{
