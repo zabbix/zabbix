f5fc6e52b61cb1ca88d44bff206bdbbfe729d8f8
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-08-13 15:07:44 +0000

    .......... [ZBXNEXT-2662] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@54932 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-2662-8/frontends/php/include/classes/html/CRadioButton.php
@@ -0,0 +1,48 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CRadioButton extends CTag {
+
+	public function __construct($name, $value, $checked = false) {
+		parent::__construct('input');
+		$this->setAttribute('name', $name);
+		$this->setAttribute('value', $value);
+		$this->setAttribute('type', 'radio');
+		if ($checked) {
+			$this->setAttribute('checked', 'checked');
+		}
+	}
+
+	/**
+	 * Enable or disable the element.
+	 *
+	 * @param bool $value
+	 */
+	public function setEnabled($value) {
+		if ($value) {
+			$this->removeAttribute('disabled');
+		}
+		else {
+			$this->setAttribute('disabled', 'disabled');
+		}
+		return $this;
+	}
+}
