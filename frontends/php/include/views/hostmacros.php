a5d0cf0c1b9d045357b14a1ebf79c58167ac31a2
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2015-08-27 09:29:51 +0000

    ..F....... [ZBXNEXT-210] fixed formatting

--- frontends/php/include/views/hostmacros.php
+++ frontends/php/include/views/hostmacros.php
@@ -163,7 +163,9 @@ $macros_form_list
 	->addRow(null,
 		(new CRadioButtonList('show_inherited_macros', (int) $data['show_inherited_macros']))
 			->addValue($data['is_template'] ? _('Template macros') : _('Host macros'), 0, null, 'this.form.submit()')
-			->addValue($data['is_template'] ? _('Inherited and template macros') : _('Inherited and host macros'), 1, null, 'this.form.submit()')
+			->addValue($data['is_template'] ? _('Inherited and template macros') : _('Inherited and host macros'), 1,
+				null, 'this.form.submit()'
+			)
 			->setModern(true)
 	)
 	->addRow(null, $table);
