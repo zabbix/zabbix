a4086f2e87ec5fc0d635c52ca05b3553ebf7f71e
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-08-28 10:16:48 +0000

    .......... [ZBXNEXT-494] merged from upstream
    git-svn-id: svn://svn.zabbix.com/branches/dev@55220 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-494/frontends/php/chart6.php
+++ ZBXNEXT-494/frontends/php/chart6.php
@@ -35,12 +35,14 @@ $fields = [
 	'profileIdx' =>		[T_ZBX_STR, O_OPT, null,		null,		null],
 	'profileIdx2' =>	[T_ZBX_STR, O_OPT, null,		null,		null],
 	'updateProfile' =>	[T_ZBX_STR, O_OPT, null,		null,		null],
-	'width' =>			[T_ZBX_INT, O_OPT, P_NZERO,	'{} > 0',	null],
+	'width' =>			[T_ZBX_INT, O_OPT, P_NZERO,	BETWEEN(20, 65535),	null],
 	'height' =>			[T_ZBX_INT, O_OPT, P_NZERO,	'{} > 0',	null],
 	'graph3d' =>		[T_ZBX_INT, O_OPT, P_NZERO,	IN('0,1'),	null],
 	'legend' =>			[T_ZBX_INT, O_OPT, P_NZERO,	IN('0,1'),	null]
 ];
-check_fields($fields);
+if (!check_fields($fields)) {
+	exit();
+}
 
 /*
  * Permissions
