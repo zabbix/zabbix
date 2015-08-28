d84e1abce9473eee29c3dcb3c3b45ad39c39c505
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-08-28 10:17:08 +0000

    A.F....... [ZBXNEXT-2128] improved several comments in code
    git-svn-id: svn://svn.zabbix.com/branches/dev@55221 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2128/frontends/php/disc_prototypes.php
+++ ZBXNEXT-2128/frontends/php/disc_prototypes.php
@@ -252,7 +252,10 @@ elseif (hasRequest('add') || hasRequest('update')) {
 		}
 	}
 
-	// Convert array "delay_flex" to string glued with ";" which is later validated through API.
+	/*
+	 * Intially validate "delay_flex" field one by one to make sure it does not have interval separator ";".
+	 * Skip empty fields and convert "delay_flex" array to string glued with ";" which is later validated through API.
+	 */
 	$delay_flex = '';
 	$intervals = [];
 
