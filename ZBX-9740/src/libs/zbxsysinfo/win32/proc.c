55e72843d8f48ee8ba5f6eacdb193f942a06f021
gleb <gleb@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-08-31 09:34:23 +0000

    ...G...... [ZBX-9740] added specific process counting for Windows Server 2012 R2
    git-svn-id: svn://svn.zabbix.com/branches/dev@55281 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9740/src/libs/zbxsysinfo/win32/proc.c
+++ ZBX-9740/src/libs/zbxsysinfo/win32/proc.c
@@ -139,9 +139,88 @@ int	old_proc_num_walk(char *procName, char *userName, DWORD access, AGENT_RESULT
 
 int	new_proc_num_walk(char *procName, char *userName, AGENT_RESULT *result)
 {
-	SET_MSG_RESULT(result, zbx_strdup(NULL, "Congratulations, you are the lucky Windows Server 2012 user! That's all I can tell now."));
+	HANDLE	hProcess;
+	HPSS	SnapshotHandle;
+	HPSSWALK	WalkMarkerHandle;
+	PSS_HANDLE_INFORMATION	HandleInformation;
+	PSS_HANDLE_ENTRY	HandleEntry;
+	DWORD	error;
+	int	proccount = 0,
+		proc_ok;
+	char	baseName[MAX_PATH],
+		uname[MAX_NAME];
 
-	return SYSINFO_RET_FAIL;
+	if (ERROR_SUCCESS != PssCaptureSnapshot(GetCurrentProcess(), PSS_CAPTURE_HANDLES |
+			PSS_CAPTURE_HANDLE_NAME_INFORMATION | PSS_CAPTURE_HANDLE_TYPE_SPECIFIC_INFORMATION, 0,
+			&SnapshotHandle))
+	{
+		SET_MSG_RESULT(result, zbx_strdup(NULL, "Unable to capture snapshot."));
+		return SYSINFO_RET_FAIL;
+	}
+
+	/* take the shortcut if no process or user name provided */
+	if ((NULL == procName || '\0' == *procName) && (NULL == userName || '\0' == *userName))
+	{
+		if (ERROR_SUCCESS == PssQuerySnapshot(SnapshotHandle, PSS_QUERY_HANDLE_INFORMATION, HandleInformation,
+				sizeof(PSS_HANDLE_INFORMATION)))
+		{
+			SET_UI64_RESULT(result, HandleInformation.HandlesCaptured);
+			return SYSINFO_RET_OK;
+		}
+	}
+
+	/* proceed the usual way */
+	if (ERROR_SUCCESS != PssWalkMarkerCreate(NULL, &WalkMarkerHandle))
+	{
+		SET_MSG_RESULT(result, zbx_strdup(NULL, "Unable to create walk marker."));
+		return SYSINFO_RET_FAIL;
+	}
+
+	while (ERROR_SUCCESS == (error = PssWalkSnapshot(SnapshotHandle, PSS_WALK_HANDLES, WalkMarkerHandle,
+			&HandleEntry, sizeof(PSS_HANDLE_ENTRY))))
+	{
+		proc_ok = 1;
+
+		if (NULL != procName && '\0' != *procName && 0 != HandleEntry.Flags | PSS_HANDLE_HAVE_NAME)
+		{
+			zbx_unicode_to_utf8_static(HandleEntry.ObjectName, baseName, MIN(MAX_NAME,
+					zbx_strlen_utf8_nchars(HandleEntry.ObjectName, HandleEntry.ObjectNameLength)));
+
+			if (0 != stricmp(baseName, procName))
+				proc_ok = 0;
+		}
+
+		if (0 != proc_ok && NULL != userName && '\0' != *userName &&
+				HandleEntry.Flags | PSS_HANDLE_HAVE_TYPE_SPECIFIC_INFORMATION)
+		{
+			hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE,
+					HandleEntry.TypeSpecificInformation.ProcessId);
+
+			if (NULL == hProcess || SUCCEED != zbx_get_process_username(hProcess, uname) ||
+					0 != stricmp(uname, userName))
+			{
+				proc_ok = 0;
+			}
+
+			if (NULL != hProcess)
+				CloseHandle(hProcess);
+		}
+
+		if (0 != proc_ok)
+			proccount++;
+	}
+
+	if (ERROR_NO_MORE_ITEMS != error)
+	{
+		SET_MSG_RESULT(result, zbx_strdup(NULL, "Process snapshot walking stopped short."));
+		return SYSINFO_RET_FAIL;
+	}
+
+	PssWalkMarkerFree(WalkMarkerHandle);
+
+	SET_UI64_RESULT(result, proccount);
+
+	return SYSINFO_RET_OK;
 }
 
 int	PROC_NUM(AGENT_REQUEST *request, AGENT_RESULT *result)
@@ -171,7 +250,7 @@ int	PROC_NUM(AGENT_REQUEST *request, AGENT_RESULT *result)
 		return old_proc_num_walk(procName, userName, PROCESS_QUERY_INFORMATION, result);
 	}
 
-	if (2 > vi->dwMinorVersion)
+	if (3 > vi->dwMinorVersion)
 		return old_proc_num_walk(procName, userName, PROCESS_QUERY_LIMITED_INFORMATION, result);
 
 	return new_proc_num_walk(procName, userName, result);
