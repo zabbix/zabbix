e4755b479bc32af4707d0b691a8bdd64926564bc
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-09-04 11:21:19 +0000

    .......... [ZBX-9655] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@55408 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9655/upgrades/dbpatches/1.8/mysql/patch/maintenances_hosts.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE maintenances_hosts (
-      maintenance_hostid              bigint unsigned         DEFAULT '0'     NOT NULL,
-      maintenanceid           bigint unsigned         DEFAULT '0'     NOT NULL,
-      hostid          bigint unsigned         DEFAULT '0'     NOT NULL,
-      PRIMARY KEY (maintenance_hostid)
-) ENGINE=InnoDB;
-CREATE INDEX maintenances_hosts_1 on maintenances_hosts (maintenanceid,hostid);
