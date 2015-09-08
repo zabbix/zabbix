dc48c0284541e95d700707aee77a5791fc84590c
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-09-08 12:02:26 +0000

    .......... [ZBX-9807] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@55481 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9807/upgrades/dbpatches/1.6/oracle/patch/history_log.sql
+++
@@ -1,1 +0,0 @@
-CREATE UNIQUE INDEX history_log_2 on history_log (itemid,id);
--- ZBX-9807/upgrades/dbpatches/2.0/mysql/patch/history_log.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE history_log
-	MODIFY id bigint unsigned NOT NULL,
-	MODIFY itemid bigint unsigned NOT NULL,
-	ADD ns integer DEFAULT '0' NOT NULL;
