9b51ba2fe4aa327d3e0ca83ce6965826710f8dd7
vitalijs <vitalijs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-09-23 15:46:54 +0000

    ..F....... [ZBX-8820] added input message triming for triger acknowledge
    git-svn-id: svn://svn.zabbix.com/branches/dev@55727 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-8820/frontends/php/app/views/monitoring.acknowledge.edit.php
+++ ZBX-8820/frontends/php/app/views/monitoring.acknowledge.edit.php
@@ -17,8 +17,7 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
-
-
+$this->includeJSfile('app/views/monitoring.acknowledge.edit.js');
 $form_list = (new CFormList())
 	->addRow(_('Message'),
 		(new CTextArea('message', '', ['maxlength' => 255]))
