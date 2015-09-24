9223caf0007452a5287db4ca6bf5b0aba59efb5f
Vitalijs Cemeris <git-no-reply@zabbix.com>
2015-09-24 12:58:35 +0000

    ..F....... [ZBX-8820] triming values functionality moved to common js

--- frontends/php/app/views/monitoring.acknowledge.edit.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-	jQuery(document).ready(function($) {
-		// type of media
-		$('form').submit(function() {
-			var obj = $(this).find('#message');
-			obj.val($.trim(obj.val()));
-		});
-	});
-</script>
