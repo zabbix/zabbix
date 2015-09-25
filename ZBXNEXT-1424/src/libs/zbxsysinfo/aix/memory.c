d03676bd90f8d89291d0d14a4d535fa1a533a59b
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-09-25 14:09:18 +0000

    .......... [ZBXNEXT-1424] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@55769 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-1424/src/libs/zbxsysinfo/aix/memory.c
@@ -0,0 +1,166 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+#include "sysinfo.h"
+#include "log.h"
+
+#ifdef HAVE_LIBPERFSTAT
+
+static perfstat_memory_total_t	m;
+
+#define ZBX_PERFSTAT_PAGE_SHIFT	12	/* 4 KB */
+
+#define ZBX_PERFSTAT_MEMORY_TOTAL()									\
+													\
+	if (-1 == perfstat_memory_total(NULL, &m, sizeof(m), 1))					\
+	{												\
+		SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot obtain system information: %s",	\
+				zbx_strerror(errno)));							\
+		return SYSINFO_RET_FAIL;								\
+	}
+
+static int	VM_MEMORY_TOTAL(AGENT_RESULT *result)
+{
+	ZBX_PERFSTAT_MEMORY_TOTAL();
+
+	SET_UI64_RESULT(result, m.real_total << ZBX_PERFSTAT_PAGE_SHIFT);	/* total real memory in pages */
+
+	return SYSINFO_RET_OK;
+}
+
+static int	VM_MEMORY_PINNED(AGENT_RESULT *result)
+{
+	ZBX_PERFSTAT_MEMORY_TOTAL();
+
+	SET_UI64_RESULT(result, m.real_pinned << ZBX_PERFSTAT_PAGE_SHIFT);	/* real memory which is pinned in pages */
+
+	return SYSINFO_RET_OK;
+}
+
+static int	VM_MEMORY_FREE(AGENT_RESULT *result)
+{
+	ZBX_PERFSTAT_MEMORY_TOTAL();
+
+	SET_UI64_RESULT(result, m.real_free << ZBX_PERFSTAT_PAGE_SHIFT);	/* free real memory in pages */
+
+	return SYSINFO_RET_OK;
+}
+
+static int	VM_MEMORY_USED(AGENT_RESULT *result)
+{
+	ZBX_PERFSTAT_MEMORY_TOTAL();
+
+	SET_UI64_RESULT(result, m.real_inuse << ZBX_PERFSTAT_PAGE_SHIFT);	/* real memory which is in use in pages */
+
+	return SYSINFO_RET_OK;
+}
+
+static int	VM_MEMORY_PUSED(AGENT_RESULT *result)
+{
+	ZBX_PERFSTAT_MEMORY_TOTAL();
+
+	if (0 == m.real_total)
+	{
+		SET_MSG_RESULT(result, zbx_strdup(NULL, "Cannot calculate percentage because total is zero."));
+		return SYSINFO_RET_FAIL;
+	}
+
+	SET_DBL_RESULT(result, m.real_inuse / (double)m.real_total * 100);
+
+	return SYSINFO_RET_OK;
+}
+
+static int	VM_MEMORY_AVAILABLE(AGENT_RESULT *result)
+{
+	ZBX_PERFSTAT_MEMORY_TOTAL();
+
+	SET_UI64_RESULT(result, (m.real_free + m.numperm) << ZBX_PERFSTAT_PAGE_SHIFT);
+
+	return SYSINFO_RET_OK;
+}
+
+static int	VM_MEMORY_PAVAILABLE(AGENT_RESULT *result)
+{
+	ZBX_PERFSTAT_MEMORY_TOTAL();
+
+	if (0 == m.real_total)
+	{
+		SET_MSG_RESULT(result, zbx_strdup(NULL, "Cannot calculate percentage because total is zero."));
+		return SYSINFO_RET_FAIL;
+	}
+
+	SET_DBL_RESULT(result, (m.real_free + m.numperm) / (double)m.real_total * 100);
+
+	return SYSINFO_RET_OK;
+}
+
+static int	VM_MEMORY_CACHED(AGENT_RESULT *result)
+{
+	ZBX_PERFSTAT_MEMORY_TOTAL();
+
+	SET_UI64_RESULT(result, m.numperm << ZBX_PERFSTAT_PAGE_SHIFT);	/* number of pages used for files */
+
+	return SYSINFO_RET_OK;
+}
+
+#endif
+
+int	VM_MEMORY_SIZE(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+#ifdef HAVE_LIBPERFSTAT
+	int	ret;
+	char	*mode;
+
+	if (1 < request->nparam)
+	{
+		SET_MSG_RESULT(result, zbx_strdup(NULL, "Too many parameters."));
+		return SYSINFO_RET_FAIL;
+	}
+
+	mode = get_rparam(request, 0);
+
+	if (NULL == mode || '\0' == *mode || 0 == strcmp(mode, "total"))
+		ret = VM_MEMORY_TOTAL(result);
+	else if (0 == strcmp(mode, "pinned"))
+		ret = VM_MEMORY_PINNED(result);
+	else if (0 == strcmp(mode, "free"))
+		ret = VM_MEMORY_FREE(result);
+	else if (0 == strcmp(mode, "used"))
+		ret = VM_MEMORY_USED(result);
+	else if (0 == strcmp(mode, "pused"))
+		ret = VM_MEMORY_PUSED(result);
+	else if (0 == strcmp(mode, "available"))
+		ret = VM_MEMORY_AVAILABLE(result);
+	else if (0 == strcmp(mode, "pavailable"))
+		ret = VM_MEMORY_PAVAILABLE(result);
+	else if (0 == strcmp(mode, "cached"))
+		ret = VM_MEMORY_CACHED(result);
+	else
+	{
+		SET_MSG_RESULT(result, zbx_strdup(NULL, "Invalid first parameter."));
+		return SYSINFO_RET_FAIL;
+	}
+
+	return ret;
+#else
+	SET_MSG_RESULT(result, zbx_strdup(NULL, "Agent was compiled without support for Perfstat API."));
+	return SYSINFO_RET_FAIL;
+#endif
+}
