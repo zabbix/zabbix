c21a9171e4beb2a81b6a8a73814b56b3bea4e805
gleb <gleb@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-10-01 07:54:06 +0000

    .......... [ZBX-8899] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@55877 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-8899-22/upgrades/dbpatches/1.4/postgresql/patch/services.sql
@@ -0,0 +1,15 @@
+CREATE TABLE services_tmp (
+	serviceid	bigint DEFAULT '0'	NOT NULL,
+	name		varchar(128)		DEFAULT ''	NOT NULL,
+	status		integer		DEFAULT '0'	NOT NULL,
+	algorithm	integer		DEFAULT '0'	NOT NULL,
+	triggerid	bigint ,
+	showsla		integer		DEFAULT '0'	NOT NULL,
+	goodsla		numeric(5,2)		DEFAULT '99.9'	NOT NULL,
+	sortorder	integer		DEFAULT '0'	NOT NULL,
+	PRIMARY KEY (serviceid)
+) with OIDS;
+
+insert into services_tmp select * from services;
+drop table services;
+alter table services_tmp rename to services;
---
+++ ZBX-8899-22/upgrades/dbpatches/2.0/ibm_db2/patch/opcommand_grp.sql
@@ -0,0 +1,1 @@
+-- See operations.sql
