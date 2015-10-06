377df5d09298ea06c403517aeacf64bb623af496
vitalijs <vitalijs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-10-06 14:17:22 +0000

    ..F....... [ZBX-9919] fixed y scale calculation for linear graphs; changed item type to constant value for linear graphs
    git-svn-id: svn://svn.zabbix.com/branches/dev@55959 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9919/frontends/php/include/classes/graphdraw/CLineGraphDraw.php
+++ ZBX-9919/frontends/php/include/classes/graphdraw/CLineGraphDraw.php
@@ -95,7 +95,7 @@ class CLineGraphDraw extends CGraphDraw {
 		$this->m_showTriggers = ($value == 1) ? 1 : 0;
 	}
 
-	public function addItem($itemid, $axis = GRAPH_YAXIS_SIDE_DEFAULT, $calc_fnc = CALC_FNC_AVG, $color = null, $drawtype = null, $type = null) {
+	public function addItem($itemid, $axis = GRAPH_YAXIS_SIDE_DEFAULT, $calc_fnc = CALC_FNC_AVG, $color = null, $drawtype = null) {
 		if ($this->type == GRAPH_TYPE_STACKED) {
 			$drawtype = GRAPH_ITEM_DRAWTYPE_FILLED_REGION;
 		}
@@ -125,7 +125,7 @@ class CLineGraphDraw extends CGraphDraw {
 		$this->items[$this->num]['drawtype'] = is_null($drawtype) ? GRAPH_ITEM_DRAWTYPE_LINE : $drawtype;
 		$this->items[$this->num]['axisside'] = is_null($axis) ? GRAPH_YAXIS_SIDE_DEFAULT : $axis;
 		$this->items[$this->num]['calc_fnc'] = is_null($calc_fnc) ? CALC_FNC_AVG : $calc_fnc;
-		$this->items[$this->num]['calc_type'] = is_null($type) ? GRAPH_ITEM_SIMPLE : $type;
+		$this->items[$this->num]['calc_type'] = GRAPH_ITEM_SIMPLE;
 
 		if ($this->items[$this->num]['axisside'] == GRAPH_YAXIS_SIDE_LEFT) {
 			$this->yaxisleft = 1;
@@ -217,7 +217,7 @@ class CLineGraphDraw extends CGraphDraw {
 				$this->axis_valuetype[$this->items[$i]['axisside']] = ITEM_VALUE_TYPE_FLOAT;
 			}
 
-			$type = $this->items[$i]['calc_type'];
+			$item_type = $this->items[$i]['calc_type'];
 			$from_time = $this->from_time;
 			$to_time = $this->to_time;
 			$calc_field = 'round('.$x.'*'.zbx_sql_mod(zbx_dbcast_2bigint('clock').'+'.$z, $p).'/('.$p.'),0)'; // required for 'group by' support of Oracle
@@ -286,11 +286,7 @@ class CLineGraphDraw extends CGraphDraw {
 				$this->data[$this->items[$i]['itemid']] = array();
 			}
 
-			if (!isset($this->data[$this->items[$i]['itemid']][$type])) {
-				$this->data[$this->items[$i]['itemid']][$type] = array();
-			}
-
-			$curr_data = &$this->data[$this->items[$i]['itemid']][$type];
+			$curr_data = &$this->data[$this->items[$i]['itemid']][$item_type];
 
 			$curr_data['count'] = null;
 			$curr_data['min'] = null;
@@ -2467,10 +2463,6 @@ class CLineGraphDraw extends CGraphDraw {
 				}
 				else {
 					$draw = (boolean) ($diff < (ZBX_GRAPH_MAX_SKIP_DELAY * $delay));
-				}
-
-				if ($this->items[$item]['type'] == ITEM_TYPE_TRAPPER) {
-					$draw = true;
 				}
 
 				if (!$draw && !$prevDraw) {
