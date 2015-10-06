109b89d485ddba6c86e8176411bd2b6061f35e6f
Gunars Pujats <git-no-reply@zabbix.com>
2015-10-06 09:28:21 +0000

    A.F....... [ZBX-9888] validate triggers comparison in action conditions

--- frontends/php/include/classes/parsers/CConditionFormula.php
+++ frontends/php/include/classes/parsers/CConditionFormula.php
@@ -69,6 +69,34 @@ class CConditionFormula {
 	private $pos;
 
 	/**
+	 * Array of formula conditions.
+	 *
+	 * @var array
+	 */
+	private $conditions_map = [];
+
+	/**
+	 * Path to constant in conditions map.
+	 *
+	 * @var array
+	 */
+	private $formula_path = [0 => 'L1'];
+
+	/**
+	 * Last discovered operator.
+	 *
+	 * @var string
+	 */
+	private $last_operator = '';
+
+	/**
+	 * Count of entrances for current level in the conditions map.
+	 *
+	 * @var array
+	 */
+	private $level_entrances = [0 => 1];
+
+	/**
 	 * Parses the given condition formula.
 	 *
 	 * @param string $formula
@@ -177,6 +205,7 @@ class CConditionFormula {
 
 			$afterSpace = false;
 			$this->pos++;
+			$this->fillConditionsMap($state, $level);
 		}
 
 		if ($this->pos == 0) {
@@ -248,6 +277,8 @@ class CConditionFormula {
 			return false;
 		}
 
+		$this->last_operator = $operator;
+
 		return true;
 	}
 
@@ -271,5 +302,194 @@ class CConditionFormula {
 	 */
 	protected function isOperatorChar($c) {
 		return ($c >= 'a' && $c <= 'z');
+	}
+
+	/**
+	 * Fill conditions map with values.
+	 *
+	 * @param int $state
+	 * @param int $level
+	 */
+	protected function fillConditionsMap($state, $level) {
+		switch ($state) {
+			case self::STATE_AFTER_OPEN_BRACE :
+				if (!array_key_exists($level, $this->level_entrances)) {
+					$this->level_entrances[$level] = 0;
+				}
+				else {
+					$this->level_entrances[$level]++;
+				}
+				array_push($this->formula_path, 'L'.$this->level_entrances[$level]);
+				break;
+
+			case self::STATE_AFTER_OPERATOR :
+				break;
+
+			case self::STATE_AFTER_CLOSE_BRACE :
+				array_pop($this->formula_path);
+				break;
+
+			case self::STATE_AFTER_CONSTANT :
+				$this->updateConditionsMap($this->conditions_map, end($this->constants)['value'], $this->formula_path);
+				break;
+		}
+	}
+
+	/**
+	 * Update conditions map values.
+	 *
+	 * @param array $target_element
+	 * @param string $constant
+	 * @param array $path
+	 */
+	protected function updateConditionsMap(&$target_element, $constant, $path) {
+		$step = array_shift($path);
+
+		if (!array_key_exists($step, $target_element)) {
+			$target_element[$step] = ['operator' => $this->last_operator, 'constant' => []];
+			$this->last_operator = '';
+		}
+
+		if(count($path) > 0) {
+			$this->updateConditionsMap($target_element[$step]['constant'],$constant,$path);
+		}
+		else {
+			array_push($target_element[$step]['constant'], ['operator' => $this->last_operator, 'constant' => $constant]);
+			$this->last_operator = '';
+		}
+
+		return;
+	}
+
+	/**
+	 * Get array with constants that are compared by and operator
+	 *
+	 * @param array $conditions
+	 *
+	 * @return array $constants
+	 */
+	protected function getAndConditions($conditions) {
+		$isand = false;
+		$previous_is_first = false;
+		$previous_constant = [];
+		$andcounter = 0;
+		$constants = ['all' => [], 'and' => []];
+
+		foreach($conditions as $level_key => $condition) {
+			$isand = false;
+			if ($condition['operator'] === ''){
+				// empty operator is only for firt constant in formula / scope
+				$previous_is_first = true;
+			}
+			else if($condition['operator'] === 'and') {
+				$isand = true;
+			}
+			else {
+				// increase andcounter for next and operator
+				$andcounter++;
+			}
+
+			if (is_array($condition['constant'])) {
+				// get conditions from sub-scope
+				$constant = $this->getAndConditions($condition['constant']);
+
+				if ($isand) {
+					if ($previous_constant) {
+						// add previous constant to and comparables
+						$constants['and'][$andcounter][] = array_key_exists('all', $previous_constant)
+							? $previous_constant['all']
+							: $previous_constant;
+						$constants['and'][$andcounter] = array_unique($constants['and'][$andcounter], SORT_REGULAR);
+					}
+
+					// add sub-scope constants to current level "and" comparables
+					$constants['and'][$andcounter][] = $constant['all'];
+					$previous_constant = [];
+				}
+				else {
+					$previous_constant = $constant;
+				}
+
+				// add sub-scope constants to result array
+				$constants[] = $constant;
+
+				// add sub-scope constants to current level "all" items, to pass those to parent level
+				$constants['all'] = array_merge($constants['all'], $constant['all']);
+			}
+			else {
+				$constants['all'][] = $condition['constant'];
+				if ($isand) {
+					if ($previous_constant) {
+						// add previous constant to and comparables
+						$constants['and'][$andcounter][] = $previous_constant;
+					}
+
+					$constants['and'][$andcounter][] = [0 => $condition['constant']];
+					$constants['and'][$andcounter] = array_unique($constants['and'][$andcounter], SORT_REGULAR);
+				}
+				$previous_constant = [0 => $condition['constant']];
+			}
+		}
+
+		return $constants;
+	}
+
+	/**
+	 * Check if triggers comparison in formula is valid.
+	 *
+	 * Comparing several triggers with "and" operator is not allowed.
+	 *
+	 * @param unknown $triggers
+	 *
+	 * return bool
+	 */
+	public function validTriggersComparison(&$triggers) {
+		$and_comparables = $this->getAndConditions($this->conditions_map);
+		$triggers_and_count = $this->getCountTriggersAnd($triggers, $and_comparables);
+
+		return ($triggers_and_count > 1) ? false : true;
+	}
+
+	/**
+	 * Get count of triggers compared with "and".
+	 *
+	 * @param unknown $triggers
+	 * @param unknown $and_comparables
+	 *
+	 * return int $triggers_and_count
+	 */
+	protected function getCountTriggersAnd(&$triggers, $and_comparables) {
+		if (array_key_exists('all', $and_comparables)) {
+			unset($and_comparables['all']);
+		}
+
+		$triggers_and_count = 0;
+		foreach ($and_comparables as $key => $conditions) {
+			if($key === 'and') {
+				// and conditions
+				foreach ($conditions as $and_condition) {
+					$triggers_and_count = 0;
+
+					foreach ($and_condition as $constant_key => $constant) {
+						if (array_intersect($constant, $triggers)) {
+							$triggers_and_count++;
+						}
+					}
+
+					if ($triggers_and_count > 1) {
+						return $triggers_and_count;
+					}
+				}
+			}
+			else {
+				// subquery scope
+				$triggers_and_count = $this->getCountTriggersAnd($triggers, $conditions);
+				if ($triggers_and_count > 1) {
+					return $triggers_and_count;
+				}
+			}
+		}
+
+		return $triggers_and_count;
 	}
 }
