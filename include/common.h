8ec4912bf08f23d33017ae2b605a7131b4e4c7ee
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-10-06 14:20:11 +0000

    ........S. [ZBX-9912] fixed ITEM.LASTVALUE macro value being truncated at bytes rather than utf-8 characters            [merge ^/branches/2.4 -c r55960]
    git-svn-id: svn://svn.zabbix.com/trunk@55961 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- include/common.h
+++ include/common.h
@@ -927,6 +927,7 @@ void	zbx_chrcpy_alloc(char **str, size_t *alloc_len, size_t *offset, char c);
 #define strscat(x, y)	zbx_strlcat(x, y, sizeof(x))
 size_t	zbx_strlcpy(char *dst, const char *src, size_t siz);
 void	zbx_strlcat(char *dst, const char *src, size_t siz);
+size_t	zbx_strlcpy_utf8(char *dst, const char *src, size_t size);
 
 char	*zbx_dvsprintf(char *dest, const char *f, va_list args);
 
