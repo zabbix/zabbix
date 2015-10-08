edd3ff15b66da81b03e76e5ed0c96cbb10dac30f
gunars <gunars@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-10-08 14:08:38 +0000

    .......... [ZBX-9883] updated to the latest trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@56010 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9883/src/libs/zbxsysinfo/linux/sensors.c
+++ ZBX-9883/src/libs/zbxsysinfo/linux/sensors.c
@@ -24,6 +24,7 @@
 #define DEVICE_DIR	"/proc/sys/dev/sensors"
 #else
 #define DEVICE_DIR	"/sys/class/hwmon"
+static char	*locations[] = {"", "/device", NULL};
 #endif
 
 #define ATTR_MAX	128
@@ -78,46 +79,56 @@ static void	count_sensor(int do_task, const char *filename, double *aggr, int *c
  *                                                                               *
  * Function: sysfs_read_attr                                                     *
  *                                                                               *
- * Purpose: read the name attribute of a sensor from sysf                        *
+ * Purpose: locate and read the name attribute of a sensor from sysfs            *
  *                                                                               *
- * Parameters:  device   - [IN] the path to sensor data in sysf                  *
+ * Parameters:  device        - [IN] the path to sensor data in sysfs            *
+ *              attribute     - [OUT] the sensor name                            *
  *                                                                               *
- * Return value: The seonsor name or NULL pointer                                *
+ * Return value: Subfolder where the sensor name file was found or NULL          *
  *                                                                               *
- * Comments: The returned string must be freed by caller after it's been used.   *
+ * Comments: attribute string must be freed by caller after it's been used.      *
  *                                                                               *
  *********************************************************************************/
-static char	*sysfs_read_attr(const char *device)
+static const char	*sysfs_read_attr(const char *device, char **attribute)
 {
-	char	path[MAX_STRING_LEN], buf[ATTR_MAX], *p;
+	char	path[MAX_STRING_LEN], buf[ATTR_MAX], *p, **location;
 	FILE	*f;
 
-	zbx_snprintf(path, MAX_STRING_LEN, "%s/%s", device, "name");
+	for (location = locations; NULL != *location; location++)
+	{
+		zbx_snprintf(path, MAX_STRING_LEN, "%s%s/name", device, *location);
 
-	if (NULL == (f = fopen(path, "r")))
-		return NULL;
+		if (NULL != (f = fopen(path, "r")))
+		{
+			p = fgets(buf, ATTR_MAX, f);
+			zbx_fclose(f);
 
-	p = fgets(buf, ATTR_MAX, f);
-	zbx_fclose(f);
+			if (NULL == p)
+				break;
 
-	if (NULL == p)
-		return NULL;
+			/* Last byte is a '\n'; chop that off */
+			buf[strlen(buf) - 1] = '\0';
 
-	/* Last byte is a '\n'; chop that off */
-	buf[strlen(buf) - 1] = '\0';
+			if (NULL != attribute)
+				*attribute = zbx_strdup(*attribute, buf);
 
-	return zbx_strdup(NULL, buf);
+			return *location;
+		}
+	}
+
+	return NULL;
 }
 
-static int	get_device_info(const char *dev_path, const char *dev_name, char *device_info)
+static int	get_device_info(const char *dev_path, const char *dev_name, char *device_info, const char **name_subfolder)
 {
 	char		bus_path[MAX_STRING_LEN], linkpath[MAX_STRING_LEN], subsys_path[MAX_STRING_LEN];
-	char		*subsys, *prefix, *bus_attr = NULL;
+	char		*subsys, *prefix = NULL, *bus_attr = NULL;
+	const char	*bus_subfolder;
 	int		domain, bus, slot, fn, addr, vendor, product, sub_len, ret = FAIL;
 	short int	bus_spi, bus_i2c;
 
 	/* ignore any device without name attribute */
-	if (NULL == (prefix = sysfs_read_attr(dev_path)))
+	if (NULL == (*name_subfolder = sysfs_read_attr(dev_path, &prefix)))
 		goto out;
 
 	if (NULL == dev_name)
@@ -132,14 +143,14 @@ static int	get_device_info(const char *dev_path, const char *dev_name, char *dev
 	}
 
 	/* Find bus type */
-	zbx_snprintf(linkpath, MAX_STRING_LEN, "%s/subsystem", dev_path);
+	zbx_snprintf(linkpath, MAX_STRING_LEN, "%s/device/subsystem", dev_path);
 
 	sub_len = readlink(linkpath, subsys_path, MAX_STRING_LEN - 1);
 
 	if (0 > sub_len && ENOENT == errno)
 	{
 		/* Fallback to "bus" link for kernels <= 2.6.17 */
-		zbx_snprintf(linkpath, MAX_STRING_LEN, "%s/bus", dev_path);
+		zbx_snprintf(linkpath, MAX_STRING_LEN, "%s/device/bus", dev_path);
 		sub_len = readlink(linkpath, subsys_path, MAX_STRING_LEN - 1);
 	}
 
@@ -169,9 +180,10 @@ static int	get_device_info(const char *dev_path, const char *dev_name, char *dev
 		}
 		else
 		{
-			zbx_snprintf(bus_path, sizeof(bus_path), "/sys/class/i2c-adapter/i2c-%d/device", bus_i2c);
+			zbx_snprintf(bus_path, sizeof(bus_path), "/sys/class/i2c-adapter/i2c-%d", bus_i2c);
+			bus_subfolder = sysfs_read_attr(bus_path, &bus_attr);
 
-			if (NULL != (bus_attr = sysfs_read_attr(bus_path)))
+			if (NULL != bus_subfolder && '\0' != *bus_subfolder)
 			{
 				if (0 != strncmp(bus_attr, "ISA ", 4))
 					goto out;
@@ -292,6 +304,7 @@ static void	get_device_sensors(int do_task, const char *device, const char *name
 	struct dirent	*sensorent, *deviceent;
 	char		hwmon_dir[MAX_STRING_LEN], devicepath[MAX_STRING_LEN], deviced[MAX_STRING_LEN],
 			device_info[MAX_STRING_LEN], regex[MAX_STRING_LEN], *device_p;
+	const char	*subfolder;
 	int		err, dev_len;
 
 	zbx_snprintf(hwmon_dir, sizeof(hwmon_dir), "%s", DEVICE_DIR);
@@ -306,12 +319,12 @@ static void	get_device_sensors(int do_task, const char *device, const char *name
 
 		zbx_snprintf(devicepath, sizeof(devicepath), "%s/%s/device", DEVICE_DIR, deviceent->d_name);
 		dev_len = readlink(devicepath, deviced, MAX_STRING_LEN - 1);
+		zbx_snprintf(devicepath, sizeof(devicepath), "%s/%s", DEVICE_DIR, deviceent->d_name);
 
 		if (0 > dev_len)
 		{
 			/* No device link? Treat device as virtual */
-			zbx_snprintf(devicepath, sizeof(devicepath), "%s/%s", DEVICE_DIR, deviceent->d_name);
-			err = get_device_info(devicepath, NULL, device_info);
+			err = get_device_info(devicepath, NULL, device_info, &subfolder);
 		}
 		else
 		{
@@ -321,14 +334,17 @@ static void	get_device_sensors(int do_task, const char *device, const char *name
 			if (0 == strcmp(device, device_p))
 			{
 				zbx_snprintf(device_info, sizeof(device_info), "%s", device);
-				err = SUCCEED;
+				err = (NULL != (subfolder = sysfs_read_attr(devicepath, NULL)) ? SUCCEED : FAIL);
 			}
 			else
-				err = get_device_info(devicepath, device_p, device_info);
+				err = get_device_info(devicepath, device_p, device_info, &subfolder);
 		}
 
 		if (SUCCEED == err && 0 == strcmp(device_info, device))
 		{
+			zbx_snprintf(devicepath, sizeof(devicepath), "%s/%s%s", DEVICE_DIR, deviceent->d_name,
+					subfolder);
+
 			if (ZBX_DO_ONE == do_task)
 			{
 				zbx_snprintf(sensorname, sizeof(sensorname), "%s/%s_input", devicepath, name);
--- ZBX-9883/src/zabbix_server/poller/poller.c
+++ ZBX-9883/src/zabbix_server/poller/poller.c
@@ -514,7 +514,7 @@ static int	get_value(DC_ITEM *item, AGENT_RESULT *result)
  * Comments:                                                                  *
  *                                                                            *
  ******************************************************************************/
-static int	get_values(unsigned char poller_type)
+static int	get_values(unsigned char poller_type, int *nextcheck)
 {
 	const char	*__function_name = "get_values";
 	DC_ITEM		items[MAX_POLLER_ITEMS];
@@ -531,7 +531,10 @@ static int	get_values(unsigned char poller_type)
 	num = DCconfig_get_poller_items(poller_type, items);
 
 	if (0 == num)
+	{
+		*nextcheck = DCconfig_get_poller_nextcheck(poller_type);
 		goto exit;
+	}
 
 	/* prepare items */
 	for (i = 0; i < num; i++)
@@ -559,7 +562,7 @@ static int	get_values(unsigned char poller_type)
 			case ITEM_TYPE_JMX:
 				ZBX_STRDUP(port, items[i].interface.port_orig);
 				substitute_simple_macros(NULL, NULL, NULL, NULL, &items[i].host.hostid, NULL, NULL,
-						NULL, &port, MACRO_TYPE_COMMON, NULL, 0);
+						NULL, NULL, &port, MACRO_TYPE_COMMON, NULL, 0);
 				if (FAIL == is_ushort(port, &items[i].interface.port))
 				{
 					SET_MSG_RESULT(&results[i], zbx_dsprintf(NULL, "Invalid port number [%s]",
@@ -579,13 +582,13 @@ static int	get_values(unsigned char poller_type)
 				ZBX_STRDUP(items[i].snmpv3_contextname, items[i].snmpv3_contextname_orig);
 
 				substitute_simple_macros(NULL, NULL, NULL, NULL, &items[i].host.hostid, NULL, NULL,
-						NULL, &items[i].snmpv3_securityname, MACRO_TYPE_COMMON, NULL, 0);
+						NULL, NULL, &items[i].snmpv3_securityname, MACRO_TYPE_COMMON, NULL, 0);
 				substitute_simple_macros(NULL, NULL, NULL, NULL, &items[i].host.hostid, NULL, NULL,
-						NULL, &items[i].snmpv3_authpassphrase, MACRO_TYPE_COMMON, NULL, 0);
+						NULL, NULL, &items[i].snmpv3_authpassphrase, MACRO_TYPE_COMMON, NULL, 0);
 				substitute_simple_macros(NULL, NULL, NULL, NULL, &items[i].host.hostid, NULL, NULL,
-						NULL, &items[i].snmpv3_privpassphrase, MACRO_TYPE_COMMON, NULL, 0);
+						NULL, NULL, &items[i].snmpv3_privpassphrase, MACRO_TYPE_COMMON, NULL, 0);
 				substitute_simple_macros(NULL, NULL, NULL, NULL, &items[i].host.hostid, NULL, NULL,
-						NULL, &items[i].snmpv3_contextname, MACRO_TYPE_COMMON, NULL, 0);
+						NULL, NULL, &items[i].snmpv3_contextname, MACRO_TYPE_COMMON, NULL, 0);
 				/* break; is not missing here */
 			case ITEM_TYPE_SNMPv1:
 			case ITEM_TYPE_SNMPv2c:
@@ -593,7 +596,7 @@ static int	get_values(unsigned char poller_type)
 				ZBX_STRDUP(items[i].snmp_oid, items[i].snmp_oid_orig);
 
 				substitute_simple_macros(NULL, NULL, NULL, NULL, &items[i].host.hostid, NULL, NULL,
-						NULL, &items[i].snmp_community, MACRO_TYPE_COMMON, NULL, 0);
+						NULL, NULL, &items[i].snmp_community, MACRO_TYPE_COMMON, NULL, 0);
 				if (SUCCEED != substitute_key_macros(&items[i].snmp_oid, &items[i].host.hostid, NULL,
 						NULL, MACRO_TYPE_SNMP_OID, error, sizeof(error)))
 				{
@@ -607,14 +610,14 @@ static int	get_values(unsigned char poller_type)
 				ZBX_STRDUP(items[i].privatekey, items[i].privatekey_orig);
 
 				substitute_simple_macros(NULL, NULL, NULL, NULL, &items[i].host.hostid, NULL, NULL,
-						NULL, &items[i].publickey, MACRO_TYPE_COMMON, NULL, 0);
+						NULL, NULL, &items[i].publickey, MACRO_TYPE_COMMON, NULL, 0);
 				substitute_simple_macros(NULL, NULL, NULL, NULL, &items[i].host.hostid, NULL, NULL,
-						NULL, &items[i].privatekey, MACRO_TYPE_COMMON, NULL, 0);
+						NULL, NULL, &items[i].privatekey, MACRO_TYPE_COMMON, NULL, 0);
 				/* break; is not missing here */
 			case ITEM_TYPE_TELNET:
 			case ITEM_TYPE_DB_MONITOR:
 				substitute_simple_macros(NULL, NULL, NULL, NULL, NULL, NULL, &items[i],
-						NULL, &items[i].params, MACRO_TYPE_PARAMS_FIELD, NULL, 0);
+						NULL, NULL, &items[i].params, MACRO_TYPE_PARAMS_FIELD, NULL, 0);
 				/* break; is not missing here */
 			case ITEM_TYPE_SIMPLE:
 			case ITEM_TYPE_JMX:
@@ -622,9 +625,9 @@ static int	get_values(unsigned char poller_type)
 				items[i].password = zbx_strdup(items[i].password, items[i].password_orig);
 
 				substitute_simple_macros(NULL, NULL, NULL, NULL, &items[i].host.hostid, NULL, NULL,
-						NULL, &items[i].username, MACRO_TYPE_COMMON, NULL, 0);
+						NULL, NULL, &items[i].username, MACRO_TYPE_COMMON, NULL, 0);
 				substitute_simple_macros(NULL, NULL, NULL, NULL, &items[i].host.hostid, NULL, NULL,
-						NULL, &items[i].password, MACRO_TYPE_COMMON, NULL, 0);
+						NULL, NULL, &items[i].password, MACRO_TYPE_COMMON, NULL, 0);
 				break;
 		}
 	}
@@ -726,8 +729,8 @@ static int	get_values(unsigned char poller_type)
 					items[i].state, results[i].msg);
 		}
 
-		DCrequeue_items(&items[i].itemid, &items[i].state, &timespec.sec, &lastlogsizes[i], NULL,
-				&errcodes[i], 1);
+		DCpoller_requeue_items(&items[i].itemid, &items[i].state, &timespec.sec, &lastlogsizes[i], NULL,
+				&errcodes[i], 1, poller_type, nextcheck);
 
 		zbx_free(items[i].key);
 
@@ -806,7 +809,7 @@ ZBX_THREAD_ENTRY(poller_thread, args)
 		}
 
 		sec = zbx_time();
-		processed += get_values(poller_type);
+		processed += get_values(poller_type, &nextcheck);
 		total_sec += zbx_time() - sec;
 #ifdef HAVE_OPENIPMI
 		if (ZBX_POLLER_TYPE_IPMI == poller_type && SEC_PER_HOUR < time(NULL) - last_ipmi_host_check)
@@ -815,7 +818,6 @@ ZBX_THREAD_ENTRY(poller_thread, args)
 			delete_inactive_ipmi_hosts(last_ipmi_host_check);
 		}
 #endif
-		nextcheck = DCconfig_get_poller_nextcheck(poller_type);
 		sleeptime = calculate_sleeptime(nextcheck, POLLER_DELAY);
 
 		if (0 != sleeptime || STAT_INTERVAL <= time(NULL) - last_stat_time)
