64d5c18a88a4964e25797dbd693ce2b70e7a3d38
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-10-12 11:27:21 +0000

    .......... [ZBXNEXT-2844] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@56075 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-2844/frontends/php/include/classes/html/CCollapsibleUiWidget.php
@@ -0,0 +1,96 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+/**
+ * A class for rendering a widget that can be collapsed or expanded.
+ */
+class CCollapsibleUiWidget extends CUiWidget {
+
+	/**
+	 * Expand/collapse widget.
+	 *
+	 * Supported values:
+	 * - true - expanded;
+	 * - false - collapsed.
+	 *
+	 * @var bool
+	 */
+	private $expanded = true;
+
+	/**
+	 * Sets the header and adds a default expand-collapse icon.
+	 *
+	 * @param string|array|CTag $caption
+	 * @param array              $icons
+	 */
+	public function setHeader($caption = null, array $icons = [], $cursor_move = false, $url = '') {
+		$icon = (new CRedirectButton(null, null))
+			->setId($this->id.'_icon')
+			->onClick('changeWidgetState(this, "'.$this->id.'", "'.$url.'");');
+		if ($this->expanded) {
+			$icon->addClass(ZBX_STYLE_BTN_WIDGET_COLLAPSE)
+				->setTitle(_('Collapse'));
+		}
+		else {
+			$icon->addClass(ZBX_STYLE_BTN_WIDGET_EXPAND)
+				->setTitle(_('Expand'));
+		}
+		$icons[] = $icon;
+
+		parent::setHeader($caption, $icons, $cursor_move);
+
+		return $this;
+	}
+
+	/**
+	 * Display the widget in expanded or collapsed state.
+	 */
+	protected function build() {
+		$body = (new CDiv($this->body))
+			->addClass('body')
+			->setId($this->id);
+
+		if (!$this->expanded) {
+			$body->setAttribute('style', 'display: none;');
+
+			if ($this->footer) {
+				$this->footer->setAttribute('style', 'display: none;');
+			}
+		}
+
+		$this->cleanItems();
+
+		$this->addItem($this->header);
+		$this->addItem($body);
+		$this->addItem($this->footer);
+		return $this;
+	}
+
+	/**
+	 * Sets expanded or collapsed state of the widget.
+	 *
+	 * @param bool
+	 */
+	public function setExpanded($expanded) {
+		$this->expanded = $expanded;
+		return $this;
+	}
+}
---
+++ ZBXNEXT-2844/frontends/php/include/classes/html/widget/CWidget.php
@@ -0,0 +1,145 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CWidget {
+
+	private $css_class;
+	private $title = null;
+	private $controls = null;
+	private $headers;
+
+	/**
+	 * The contents of the body of the widget.
+	 *
+	 * @var array
+	 */
+	protected $body = [];
+
+	public function __construct() {
+		$this->css_class = 'header_wide';
+	}
+
+	public function setClass($class) {
+		$this->css_class = $class;
+		return $this;
+	}
+
+	public function setTitle($title) {
+		$this->title = $title;
+		return $this;
+	}
+
+	public function setControls($controls) {
+		zbx_value2array($controls);
+		$this->controls = $controls;
+		return $this;
+	}
+
+	public function addHeader($left = SPACE, $right = SPACE) {
+		zbx_value2array($right);
+		$this->headers[] = ['left' => $left, 'right' => $right];
+		return $this;
+	}
+
+	public function addItem($items = null) {
+		if (!is_null($items)) {
+			$this->body[] = $items;
+		}
+		return $this;
+	}
+
+	public function get() {
+		$widget = [];
+		if ($this->title !== null || $this->controls !== null) {
+			$widget[] = $this->createTopHeader();
+		}
+		if (!empty($this->headers)) {
+			$widget[] = $this->createHeader();
+		}
+
+		return [$widget, $this->body];
+	}
+
+	public function show() {
+		echo $this->toString();
+		return $this;
+	}
+
+	public function toString() {
+		$tab = $this->get();
+
+		return unpack_object($tab);
+	}
+
+	private function createTopHeader() {
+		$body = [new CTag('h1', true, $this->title), $this->controls];
+
+		return (new CDiv($body))->addClass('header-title');
+	}
+
+	private function createHeader() {
+		$header = reset($this->headers);
+
+		$columnRights = [];
+
+		if (!is_null($header['right'])) {
+			foreach ($header['right'] as $right) {
+				$columnRights[] = (new CDiv($right))->addClass('floatright');
+			}
+		}
+
+		if ($columnRights) {
+			$columnRights = array_reverse($columnRights);
+		}
+
+		// header table
+		$table = (new CTable())
+			->addClass($this->css_class);
+		$table->setCellSpacing(0);
+		$table->setCellPadding(1);
+		$table->addRow($this->createHeaderRow($header['left'], $columnRights), 'first');
+
+		if ($this->css_class != 'header_wide') {
+			$table->addClass('ui-widget-header');
+		}
+
+		foreach ($this->headers as $num => $header) {
+			if ($num > 0) {
+				$table->addRow($this->createHeaderRow($header['left'], $header['right']), 'next');
+			}
+		}
+
+		return new CDiv($table);
+	}
+
+	private function createHeaderRow($col1, $col2 = SPACE) {
+		$td_r = (new CCol($col2))
+			->addClass('header_r')
+			->addClass('right');
+		$row = [
+			(new CCol($col1))
+				->addClass('header_l')
+				->addClass('left'),
+			$td_r
+		];
+		return $row;
+	}
+}
