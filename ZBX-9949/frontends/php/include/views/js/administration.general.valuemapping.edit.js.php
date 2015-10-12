a57b2f26fc8273da766475e4e335183699d5dc33
gunars <gunars@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-10-12 14:47:11 +0000

    .......... [ZBX-9949] created new dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@56084 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-9949/frontends/php/include/views/js/administration.general.valuemapping.edit.js.php
@@ -0,0 +1,72 @@
+<script type="text/x-jquery-tmpl" id="mappingRow">
+	<tr>
+		<td>
+			<input type="text" name="mappings[#{mappingNum}][value]" value="#{value}" style="width: <?= ZBX_TEXTAREA_SMALL_WIDTH ?>px" maxlength="64">
+		</td>
+		<td>&rArr;</td>
+		<td>
+			<input type="text" name="mappings[#{mappingNum}][newvalue]" value="#{newvalue}" style="width: <?= ZBX_TEXTAREA_SMALL_WIDTH ?>px" maxlength="64">
+		</td>
+		<td class="<?= ZBX_STYLE_NOWRAP ?>">
+			<button class="<?= ZBX_STYLE_BTN_LINK ?> removeMapping" type="button"><?= _('Remove') ?></button>
+		</td>
+	</tr>
+</script>
+<script type="text/javascript">
+	var mappingsManager = (function() {
+		var tpl = new Template(jQuery('#mappingRow').html()),
+			mappingsCount = 0,
+			nextMappingNum = 0;
+
+		function renderMappingRow(mapping) {
+			mapping.mappingNum = nextMappingNum++;
+			jQuery(tpl.evaluate(mapping)).insertBefore('#mappingsTable tr:last-child');
+
+			if (mapping.mappingid !== void(0)) {
+				jQuery('#mappingsTable tr:last-child')
+					.prev()
+					.find('td')
+					.first()
+					.append('<input type="hidden" name="mappings[' + mapping.mappingNum + '][mappingid]" value="' + mapping.mappingid + '">');
+			}
+
+			mappingsCount++;
+			toggleSaveButton();
+		}
+
+		function toggleSaveButton() {
+			if (mappingsCount === 0) {
+				jQuery('#save').button('disable');
+			}
+			else if (mappingsCount === 1) {
+				jQuery('#save').button('enable');
+			}
+		}
+
+		return {
+			addNew: function() {
+				renderMappingRow({});
+				toggleSaveButton();
+			},
+
+			addExisting: function(mappings) {
+				for (var i = 0, ln = mappings.length; i < ln; i++) {
+					renderMappingRow(mappings[i]);
+				}
+			},
+
+			remove: function() {
+				jQuery(this).closest('tr').remove();
+
+				mappingsCount--;
+				toggleSaveButton();
+			}
+		};
+	}());
+
+	jQuery(document).ready(function() {
+		jQuery('#save').button();
+		jQuery('#addMapping').click(mappingsManager.addNew);
+		jQuery('#mappingsTable tbody').on('click', '.removeMapping', mappingsManager.remove);
+	});
+</script>
