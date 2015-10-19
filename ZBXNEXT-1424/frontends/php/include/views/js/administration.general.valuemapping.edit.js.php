d57585337087ba03d22da9b27af99cb962073efe
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-10-19 15:23:05 +0000

    ..F....... [ZBXNEXT-1424] fixed coding style for value map dynamic rows template
    git-svn-id: svn://svn.zabbix.com/branches/dev@56266 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1424/frontends/php/include/views/js/administration.general.valuemapping.edit.js.php
+++ ZBXNEXT-1424/frontends/php/include/views/js/administration.general.valuemapping.edit.js.php
@@ -1,14 +1,14 @@
 <script type="text/x-jquery-tmpl" id="mapping_row">
 	<?= (new CRow([
-		(new CTextBox('mappings[#{rowNum}][value]', '', false, 64))->setWidth(ZBX_TEXTAREA_SMALL_WIDTH),
-		'&rArr;',
-		(new CTextBox('mappings[#{rowNum}][newvalue]', '', false, 64))->setWidth(ZBX_TEXTAREA_SMALL_WIDTH),
-		(new CButton('mappings[#{rowNum}][remove]', _('Remove')))
-			->addClass(ZBX_STYLE_BTN_LINK)
-			->addClass('element-table-remove')
-	]))
-		->addClass('form_row')
-		->toString()
+			(new CTextBox('mappings[#{rowNum}][value]', '', false, 64))->setWidth(ZBX_TEXTAREA_SMALL_WIDTH),
+			'&rArr;',
+			(new CTextBox('mappings[#{rowNum}][newvalue]', '', false, 64))->setWidth(ZBX_TEXTAREA_SMALL_WIDTH),
+			(new CButton('mappings[#{rowNum}][remove]', _('Remove')))
+				->addClass(ZBX_STYLE_BTN_LINK)
+				->addClass('element-table-remove')
+		]))
+			->addClass('form_row')
+			->toString()
 	?>
 </script>
 <script type="text/javascript">
