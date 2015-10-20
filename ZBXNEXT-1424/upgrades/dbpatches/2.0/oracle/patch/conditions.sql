6443a6eb045320a89fa9512a8272390ba994ecf7
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-10-20 08:42:45 +0000

    .......... [ZBXNEXT-1424] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@56277 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1424/upgrades/dbpatches/2.0/oracle/patch/conditions.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE conditions MODIFY conditionid DEFAULT NULL;
-ALTER TABLE conditions MODIFY actionid DEFAULT NULL;
-DELETE FROM conditions WHERE NOT actionid IN (SELECT actionid FROM actions);
-ALTER TABLE conditions ADD CONSTRAINT c_conditions_1 FOREIGN KEY (actionid) REFERENCES actions (actionid) ON DELETE CASCADE;
