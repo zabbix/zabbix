a775c0ce7a97f52c552966ba288e69cacb6ef735
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-10-21 05:52:26 +0000

    .......... [ZBX-9347] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@56290 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9347/upgrades/dbpatches/2.0/mysql/patch/proxy_history.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE proxy_history
-	MODIFY itemid bigint unsigned NOT NULL,
-	MODIFY value longtext NOT NULL,
-	ADD ns integer DEFAULT '0' NOT NULL,
-	ADD status integer DEFAULT '0' NOT NULL;
