0296a08db9cde22d914b4940945c06ce96c9414d
Sandis Neilands <git-no-reply@zabbix.com>
2015-10-28 10:02:11 +0000

    .D.G...... [ZBXNEXT-1263] specified in zabbix_get, zabbix_sender manpages, help that by default connections will be unencrypted

--- src/zabbix_sender/zabbix_sender.c
+++ src/zabbix_sender/zabbix_sender.c
@@ -129,6 +129,7 @@ const char	*help_message[] = {
 #if defined(HAVE_POLARSSL) || defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
 	"  --tls-connect value        How to connect to server or proxy. Values:",
 	"                               unencrypted - connect without encryption",
+	"                                             (default)",
 	"                               psk         - connect using TLS and a pre-shared",
 	"                                             key",
 	"                               cert        - connect using TLS and a",
