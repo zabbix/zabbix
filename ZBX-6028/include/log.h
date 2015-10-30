94756c89f39dbb4815629c0c5825672701adc6d8
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-10-30 17:22:00 +0000

    ...G...... [ZBX-6028] allow agent redirecting stdio by calling zbx_redirect_stdio()
    git-svn-id: svn://svn.zabbix.com/branches/dev@56453 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6028/include/log.h
+++ ZBX-6028/include/log.h
@@ -69,6 +69,11 @@ char		*strerror_from_system(unsigned long error);
 
 #ifdef _WINDOWS
 char		*strerror_from_module(unsigned long error, const wchar_t *module);
+
+#define zbx_redirect_stdio(filename)
+
+#else
+void		zbx_redirect_stdio(const char *filename);
 #endif
 
 void		zbx_handle_log(const char *log_filename);
