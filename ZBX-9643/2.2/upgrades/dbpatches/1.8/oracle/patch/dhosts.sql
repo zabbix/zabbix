1e0f0bb8fdfedc5ca8649854bfd88b50292b7b01
gunars <gunars@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-11-03 11:48:57 +0000

    .......... [ZBX-9643] undo copy
    git-svn-id: svn://svn.zabbix.com/branches/dev@56506 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9643/2.2/upgrades/dbpatches/1.8/oracle/patch/dhosts.sql
+++
@@ -1,9 +0,0 @@
-alter table dservices add dcheckid                number(20)              DEFAULT '0'     NOT NULL;
-alter table dservices add ip              nvarchar2(39)           DEFAULT '';
-
-update dservices set ip=(select dhosts.ip from dhosts where dservices.dhostid=dhosts.dhostid);
-
-alter table dhosts drop column ip;
-
-CREATE INDEX dhosts_1 on dhosts (druleid);
-
