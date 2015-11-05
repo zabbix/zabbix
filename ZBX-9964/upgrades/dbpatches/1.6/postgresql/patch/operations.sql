7211d004dc7273420c1f26a8ea181f4af3557089
gunars <gunars@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-11-05 14:55:27 +0000

    .......... [ZBX-9964] removed dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@56573 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9964/upgrades/dbpatches/1.6/postgresql/patch/operations.sql
+++
@@ -1,5 +0,0 @@
-alter table operations add        esc_period              integer         DEFAULT '0'     NOT NULL;
-alter table operations add        esc_step_from           integer         DEFAULT '0'     NOT NULL;
-alter table operations add        esc_step_to             integer         DEFAULT '0'     NOT NULL;
-alter table operations add        default_msg             integer         DEFAULT '0'     NOT NULL;
-alter table operations add        evaltype                integer         DEFAULT '0'     NOT NULL;
