d1ab8e05f0fc2162e2496e298617cb64e85f4671
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-11-09 08:36:03 +0000

    .......... [ZBX-9928] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@56604 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9928/upgrades/dbpatches/1.8/postgresql/patch/trends_uint.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE trends_uint ALTER COLUMN value_min TYPE numeric(20);
-ALTER TABLE trends_uint ALTER COLUMN value_avg TYPE numeric(20);
-ALTER TABLE trends_uint ALTER COLUMN value_max TYPE numeric(20);
-ALTER TABLE trends_uint ALTER COLUMN value_min SET DEFAULT '0';
-ALTER TABLE trends_uint ALTER COLUMN value_avg SET DEFAULT '0';
-ALTER TABLE trends_uint ALTER COLUMN value_max SET DEFAULT '0';
-ALTER TABLE trends_uint ALTER COLUMN value_min SET NOT NULL;
-ALTER TABLE trends_uint ALTER COLUMN value_avg SET NOT NULL;
-ALTER TABLE trends_uint ALTER COLUMN value_max SET NOT NULL;
