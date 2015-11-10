991db5217d7c1a016c0bdc7069d5993d2616c7f4
viktors <viktors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-11-10 15:16:33 +0000

    ........S. [ZBX-9931] improved buffer from static to dynamic for item key sending from server to agent
    git-svn-id: svn://svn.zabbix.com/branches/dev@56641 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9931/src/zabbix_server/poller/checks_agent.c
+++ ZBX-9931/src/zabbix_server/poller/checks_agent.c
@@ -47,7 +47,7 @@ int	get_value_agent(DC_ITEM *item, AGENT_RESULT *result)
 {
 	const char	*__function_name = "get_value_agent";
 	zbx_sock_t	s;
-	char		*buf, buffer[MAX_STRING_LEN];
+	char		*buf, *buffer = NULL;
 	int		ret = SUCCEED;
 	ssize_t		received_len;
 
@@ -56,7 +56,7 @@ int	get_value_agent(DC_ITEM *item, AGENT_RESULT *result)
 
 	if (SUCCEED == (ret = zbx_tcp_connect(&s, CONFIG_SOURCE_IP, item->interface.addr, item->interface.port, 0)))
 	{
-		zbx_snprintf(buffer, sizeof(buffer), "%s\n", item->key);
+		buffer = zbx_dsprintf(buffer, "%s\n", item->key);
 		zabbix_log(LOG_LEVEL_DEBUG, "Sending [%s]", buffer);
 
 		/* send requests using old protocol */
@@ -79,19 +79,19 @@ int	get_value_agent(DC_ITEM *item, AGENT_RESULT *result)
 
 		if (0 == strcmp(buf, ZBX_NOTSUPPORTED))
 		{
-			zbx_snprintf(buffer, sizeof(buffer), "Not supported by Zabbix Agent");
+			buffer = zbx_dsprintf(buffer, "Not supported by Zabbix Agent", NULL);
 			SET_MSG_RESULT(result, strdup(buffer));
 			ret = NOTSUPPORTED;
 		}
 		else if (0 == strcmp(buf, ZBX_ERROR))
 		{
-			zbx_snprintf(buffer, sizeof(buffer), "Zabbix Agent non-critical error");
+			buffer = zbx_dsprintf(buffer, "Zabbix Agent non-critical error", NULL);
 			SET_MSG_RESULT(result, strdup(buffer));
 			ret = AGENT_ERROR;
 		}
 		else if (0 == received_len)
 		{
-			zbx_snprintf(buffer, sizeof(buffer), "Received empty response from Zabbix Agent at [%s]."
+			buffer = zbx_dsprintf(buffer, "Received empty response from Zabbix Agent at [%s]."
 					" Assuming that agent dropped connection because of access permissions.",
 					item->interface.addr);
 			SET_MSG_RESULT(result, strdup(buffer));
@@ -102,10 +102,11 @@ int	get_value_agent(DC_ITEM *item, AGENT_RESULT *result)
 	}
 	else
 	{
-		zbx_snprintf(buffer, sizeof(buffer), "Get value from agent failed: %s",
-				zbx_tcp_strerror());
+		buffer = zbx_dsprintf(buffer, "Get value from agent failed: %s", zbx_tcp_strerror());
 		SET_MSG_RESULT(result, strdup(buffer));
 	}
+
+	zbx_free(buffer);
 
 	zbx_tcp_close(&s);
 
