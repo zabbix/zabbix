3dd1469dd4de35b5bfb5015f827b04590b8eaba3
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-11-12 12:24:32 +0000

    .......... [ZBXNEXT-826] updated release version [2.0.17rc1]; updated API version [2.0.17]
    git-svn-id: svn://svn.zabbix.com/branches/2.0@56700 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,8 +19,8 @@
 **/
 
 
-define('ZABBIX_VERSION',		'2.0.16');
-define('ZABBIX_API_VERSION',	'2.0.16');
+define('ZABBIX_VERSION',		'2.0.17rc1');
+define('ZABBIX_API_VERSION',	'2.0.17');
 
 define('ZABBIX_COPYRIGHT_FROM', '2001');
 define('ZABBIX_COPYRIGHT_TO', '2015');
