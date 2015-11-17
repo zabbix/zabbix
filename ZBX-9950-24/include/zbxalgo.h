ae22669fd46d727dca18bff2fee99ac7e3fd2715
gleb <gleb@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-11-17 10:51:27 +0000

    ........S. [ZBX-9950] added declaration
    git-svn-id: svn://svn.zabbix.com/branches/dev@56767 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9950-24/include/zbxalgo.h
+++ ZBX-9950-24/include/zbxalgo.h
@@ -291,6 +291,8 @@ int	zbx_vector_ ## __id ## _lsearch(zbx_vector_ ## __id ## _t *vector, __type va
 									zbx_compare_func_t compare_func);	\
 int	zbx_vector_ ## __id ## _search(zbx_vector_ ## __id ## _t *vector, __type value,				\
 									zbx_compare_func_t compare_func);	\
+void	zbx_vector_ ## __id ## _setdiff(zbx_vector_ ## __id ## _t *left, zbx_vector_ ## __id ## _t *right,	\
+									zbx_compare_func_t compare_func);	\
 														\
 void	zbx_vector_ ## __id ## _reserve(zbx_vector_ ## __id ## _t *vector, size_t size);			\
 void	zbx_vector_ ## __id ## _clear(zbx_vector_ ## __id ## _t *vector);
