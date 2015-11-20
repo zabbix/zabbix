4691d96979ecd936a5401d6facd6e86892bd9bbb
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-11-20 12:09:04 +0000

    .......... [ZBX-9928] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@56823 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9928/upgrades/dbpatches/2.0/oracle/patch/httptestitem.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE httptestitem MODIFY httptestitemid DEFAULT NULL;
-ALTER TABLE httptestitem MODIFY httptestid DEFAULT NULL;
-ALTER TABLE httptestitem MODIFY itemid DEFAULT NULL;
-DELETE FROM httptestitem WHERE NOT httptestid IN (SELECT httptestid FROM httptest);
-DELETE FROM httptestitem WHERE NOT itemid IN (SELECT itemid FROM items);
-ALTER TABLE httptestitem ADD CONSTRAINT c_httptestitem_1 FOREIGN KEY (httptestid) REFERENCES httptest (httptestid) ON DELETE CASCADE;
-ALTER TABLE httptestitem ADD CONSTRAINT c_httptestitem_2 FOREIGN KEY (itemid) REFERENCES items (itemid) ON DELETE CASCADE;
