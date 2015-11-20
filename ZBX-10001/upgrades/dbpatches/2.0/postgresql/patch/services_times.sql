8772f9335a5b3a0f3781ae12e4eb4100820a6ae4
viktors <viktors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-11-20 14:34:26 +0000

    .......... [ZBX-10001] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@56834 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10001/upgrades/dbpatches/2.0/postgresql/patch/services_times.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ONLY services_times ALTER timeid DROP DEFAULT,
-				ALTER serviceid DROP DEFAULT;
-DELETE FROM services_times WHERE NOT EXISTS (SELECT 1 FROM services WHERE services.serviceid=services_times.serviceid);
-ALTER TABLE ONLY services_times ADD CONSTRAINT c_services_times_1 FOREIGN KEY (serviceid) REFERENCES services (serviceid) ON DELETE CASCADE;
