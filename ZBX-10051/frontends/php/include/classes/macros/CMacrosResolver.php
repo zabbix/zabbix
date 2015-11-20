e085762838383ab1516ff8ffa5024a327bfc2601
gunars <gunars@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-11-20 09:21:58 +0000

    ..F....... [ZBX-10051] reverted changes made in r56756
    git-svn-id: svn://svn.zabbix.com/branches/dev@56807 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10051/frontends/php/include/classes/macros/CMacrosResolver.php
+++ ZBX-10051/frontends/php/include/classes/macros/CMacrosResolver.php
@@ -414,14 +414,8 @@ class CMacrosResolver extends CMacrosResolverGeneral {
 				$references = $this->resolveTriggerReferences($trigger['expression'], $matched_macros['references']);
 
 				$macro_values[$triggerid] = array_key_exists($triggerid, $macro_values)
-					? array_merge($macro_values[$triggerid], $references['references'])
-					: $references['references'];
-
-				if ($references['usermacros']) {
-					$usermacros[$triggerid]['macros'] = array_key_exists($triggerid, $usermacros)
-						? $usermacros[$triggerid]['macros'] + $references['usermacros']
-						: $references['usermacros'];
-				}
+					? array_merge($macro_values[$triggerid], $references)
+					: $references;
 			}
 		}
 
@@ -460,12 +454,6 @@ class CMacrosResolver extends CMacrosResolverGeneral {
 		// Replace macros to value.
 		foreach ($macro_values as $triggerid => $macro) {
 			$trigger = &$triggers[$triggerid];
-
-			foreach ($macro as $macro_name => $macro_value) {
-				if (array_key_exists($macro_value, $macro_values[$triggerid])) {
-					$macro_values[$triggerid][$macro_name] = $macro_values[$triggerid][$macro_value];
-				}
-			}
 
 			$matched_macros = $this->getMacroPositions($trigger['description'], $types);
 
