b2a8fbcf11039860c389e2cc41b9dfe2333655c8
sandis <sandis@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-11-23 09:48:50 +0000

    ...G...... [ZBX-10033] created new development branch to fix handling of abandoned locks on Windows
    git-svn-id: svn://svn.zabbix.com/branches/dev@56854 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-10033/frontends/php/app/controllers/CControllerDiscoveryView.php
@@ -0,0 +1,163 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CControllerDiscoveryView extends CController {
+
+	private $sysmapid;
+
+	protected function init() {
+		$this->disableSIDValidation();
+	}
+
+	protected function checkInput() {
+		$fields = [
+			'druleid' =>	'db drules.druleid',
+			'sort' =>		'in ip',
+			'sortorder' =>	'in '.ZBX_SORT_DOWN.','.ZBX_SORT_UP,
+			'fullscreen' =>	'in 0,1'
+		];
+
+		$ret = $this->validateInput($fields);
+
+		if (!$ret) {
+			$this->setResponse(new CControllerResponseFatal());
+		}
+
+		return $ret;
+	}
+
+	protected function checkPermissions() {
+		if ($this->getUserType() < USER_TYPE_ZABBIX_ADMIN) {
+			return false;
+		}
+
+		if ($this->hasInput('druleid') && $this->getInput('druleid') != 0) {
+			$drules = API::DRule()->get([
+				'output' => [],
+				'druleids' => [$this->getInput('druleid')],
+				'filter' => ['status' => DRULE_STATUS_ACTIVE]
+			]);
+			if (!$drules) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	protected function doAction() {
+		$sortField = $this->getInput('sort', CProfile::get('web.discovery.php.sort', 'ip'));
+		$sortOrder = $this->getInput('sortorder', CProfile::get('web.discovery.php.sortorder', ZBX_SORT_UP));
+
+		CProfile::update('web.discovery.php.sort', $sortField, PROFILE_TYPE_STR);
+		CProfile::update('web.discovery.php.sortorder', $sortOrder, PROFILE_TYPE_STR);
+
+		/*
+		 * Display
+		 */
+		$data = [
+			'fullscreen' => $this->getInput('fullscreen', 0),
+			'druleid' => $this->getInput('druleid', 0),
+			'sort' => $sortField,
+			'sortorder' => $sortOrder,
+			'services' => [],
+			'drules' => []
+		];
+
+		$data['pageFilter'] = new CPageFilter([
+			'drules' => ['filter' => ['status' => DRULE_STATUS_ACTIVE]],
+			'druleid' => $data['druleid']
+		]);
+
+		if ($data['pageFilter']->drulesSelected) {
+
+			// discovery rules
+			$options = [
+				'output' => API_OUTPUT_EXTEND,
+				'selectDHosts' => API_OUTPUT_EXTEND,
+				'filter' => ['status' => DRULE_STATUS_ACTIVE]
+			];
+
+			if ($data['pageFilter']->druleid > 0) {
+				$options['druleids'] = $data['pageFilter']->druleid; // set selected discovery rule id
+			}
+
+			$data['drules'] = API::DRule()->get($options);
+			if (!empty($data['drules'])) {
+				order_result($data['drules'], 'name');
+			}
+
+			// discovery services
+			$options = [
+				'selectHosts' => ['hostid', 'name', 'status'],
+				'output' => API_OUTPUT_EXTEND,
+				'sortfield' => $sortField,
+				'sortorder' => $sortOrder,
+				'limitSelects' => 1
+			];
+			if (!empty($data['druleid'])) {
+				$options['druleids'] = $data['druleid'];
+			}
+			else {
+				$options['druleids'] = zbx_objectValues($data['drules'], 'druleid');
+			}
+			$dservices = API::DService()->get($options);
+
+			// user macros
+			$data['macros'] = API::UserMacro()->get([
+				'output' => API_OUTPUT_EXTEND,
+				'globalmacro' => true
+			]);
+			$data['macros'] = zbx_toHash($data['macros'], 'macro');
+
+			// services
+			$data['services'] = [];
+			foreach ($dservices as $dservice) {
+				$key_ = $dservice['key_'];
+				if (!zbx_empty($key_)) {
+					if (isset($data['macros'][$key_])) {
+						$key_ = $data['macros'][$key_]['value'];
+					}
+					$key_ = ': '.$key_;
+				}
+				$serviceName = discovery_check_type2str($dservice['type']).
+					discovery_port2str($dservice['type'], $dservice['port']).$key_;
+				$data['services'][$serviceName] = 1;
+			}
+			ksort($data['services']);
+
+			// discovery services to hash
+			$data['dservices'] = zbx_toHash($dservices, 'dserviceid');
+
+			// discovery hosts
+			$data['dhosts'] = API::DHost()->get([
+				'druleids' => zbx_objectValues($data['drules'], 'druleid'),
+				'selectDServices' => ['dserviceid', 'ip', 'dns', 'type', 'status', 'key_'],
+				'output' => ['dhostid', 'lastdown', 'lastup', 'druleid']
+			]);
+			$data['dhosts'] = zbx_toHash($data['dhosts'], 'dhostid');
+		}
+
+		$response = new CControllerResponseData($data);
+		$response->setTitle(_('Status of discovery'));
+		$this->setResponse($response);
+	}
+}
---
+++ ZBX-10033/frontends/php/include/classes/screens/CScreenBase.php
@@ -0,0 +1,430 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CScreenBase {
+
+	/**
+	 * @see CScreenBuilder::isFlickerfree
+	 */
+	public $isFlickerfree;
+
+	/**
+	 * Page file.
+	 *
+	 * @var string
+	 */
+	public $pageFile;
+
+	/**
+	 * @see CScreenBuilder::mode
+	 */
+	public $mode;
+
+	/**
+	 * @see CScreenBuilder::timestamp
+	 */
+	public $timestamp;
+
+	/**
+	 * Resource (screen) type
+	 *
+	 * @var int
+	 */
+	public $resourcetype;
+
+	/**
+	 * Is templated screen
+	 *
+	 * @var bool
+	 */
+	public $isTemplatedScreen;
+
+	/**
+	 * Screen id
+	 *
+	 * @var int
+	 */
+	public $screenid;
+
+	/**
+	 * Screen item data
+	 *
+	 * @var array
+	 */
+	public $screenitem;
+
+	/**
+	 * Action
+	 *
+	 * @var string
+	 */
+	public $action;
+
+	/**
+	 * Group id
+	 *
+	 * @var int
+	 */
+	public $groupid;
+
+	/**
+	 * Host id
+	 *
+	 * @var int
+	 */
+	public $hostid;
+
+	/**
+	 * Time control timeline
+	 *
+	 * @var array
+	 */
+	public $timeline;
+
+	/**
+	 * @see CScreenBuilder::profileIdx
+	 */
+	public $profileIdx;
+
+	/**
+	 * @see CScreenBuilder::profileIdx2
+	 */
+	public $profileIdx2;
+
+	/**
+	 * @see CScreenBuilder::updateProfile
+	 */
+	public $updateProfile;
+
+	/**
+	 * Time control dom element id
+	 *
+	 * @var string
+	 */
+	public $dataId;
+
+	/**
+	 * Init screen data.
+	 *
+	 * @param array		$options
+	 * @param boolean	$options['isFlickerfree']
+	 * @param string	$options['pageFile']
+	 * @param int		$options['mode']
+	 * @param int		$options['timestamp']
+	 * @param int		$options['resourcetype']
+	 * @param bool		$options['isTemplatedScreen']
+	 * @param int		$options['screenid']
+	 * @param array		$options['screenitem']
+	 * @param string	$options['action']
+	 * @param int		$options['groupid']
+	 * @param int		$options['hostid']
+	 * @param int		$options['period']
+	 * @param int		$options['stime']
+	 * @param string	$options['profileIdx']
+	 * @param int		$options['profileIdx2']
+	 * @param boolean	$options['updateProfile']
+	 * @param array		$options['timeline']
+	 * @param string	$options['dataId']
+	 */
+	public function __construct(array $options = []) {
+		$this->isFlickerfree = isset($options['isFlickerfree']) ? $options['isFlickerfree'] : true;
+		$this->mode = isset($options['mode']) ? $options['mode'] : SCREEN_MODE_SLIDESHOW;
+		$this->timestamp = !empty($options['timestamp']) ? $options['timestamp'] : time();
+		$this->resourcetype = isset($options['resourcetype']) ? $options['resourcetype'] : null;
+		$this->isTemplatedScreen = isset($options['isTemplatedScreen']) ? $options['isTemplatedScreen'] : false;
+		$this->screenid = !empty($options['screenid']) ? $options['screenid'] : null;
+		$this->action = !empty($options['action']) ? $options['action'] : null;
+		$this->groupid = !empty($options['groupid']) ? $options['groupid'] : null;
+		$this->hostid = isset($options['hostid']) ? $options['hostid'] : 0;
+		$this->dataId = !empty($options['dataId']) ? $options['dataId'] : null;
+
+		// get page file
+		if (!empty($options['pageFile'])) {
+			$this->pageFile = $options['pageFile'];
+		}
+		else {
+			global $page;
+			$this->pageFile = $page['file'];
+		}
+
+		// calculate timeline
+		$this->profileIdx = !empty($options['profileIdx']) ? $options['profileIdx'] : '';
+		$this->profileIdx2 = !empty($options['profileIdx2']) ? $options['profileIdx2'] : null;
+		$this->updateProfile = isset($options['updateProfile']) ? $options['updateProfile'] : true;
+		$this->timeline = !empty($options['timeline']) ? $options['timeline'] : null;
+		if (empty($this->timeline)) {
+			$this->timeline = $this->calculateTime([
+				'profileIdx' => $this->profileIdx,
+				'profileIdx2' => $this->profileIdx2,
+				'updateProfile' => $this->updateProfile,
+				'period' => !empty($options['period']) ? $options['period'] : null,
+				'stime' => !empty($options['stime']) ? $options['stime'] : null
+			]);
+		}
+
+		// get screenitem
+		if (!empty($options['screenitem'])) {
+			$this->screenitem = $options['screenitem'];
+		}
+		elseif (!empty($options['screenitemid'])) {
+			if ($this->hostid != 0) {
+				$this->screenitem = API::TemplateScreenItem()->get([
+					'screenitemids' => $options['screenitemid'],
+					'hostids' => $this->hostid,
+					'output' => API_OUTPUT_EXTEND
+				]);
+			}
+			else {
+				$this->screenitem = API::ScreenItem()->get([
+					'screenitemids' => $options['screenitemid'],
+					'output' => API_OUTPUT_EXTEND
+				]);
+			}
+
+			$this->screenitem = reset($this->screenitem);
+		}
+
+		// get screenid
+		if (empty($this->screenid) && !empty($this->screenitem)) {
+			$this->screenid = $this->screenitem['screenid'];
+		}
+
+		// get resourcetype
+		if (is_null($this->resourcetype) && !empty($this->screenitem['resourcetype'])) {
+			$this->resourcetype = $this->screenitem['resourcetype'];
+		}
+
+		// create action url
+		if (empty($this->action)) {
+			$this->action = 'screenedit.php?form=update&screenid='.$this->screenid.'&screenitemid='.$this->screenitem['screenitemid'];
+		}
+	}
+
+	/**
+	 * Create and get unique screen id for time control.
+	 *
+	 * @return string
+	 */
+	public function getDataId() {
+		if (empty($this->dataId)) {
+			$this->dataId = !empty($this->screenitem) ? $this->screenitem['screenitemid'].'_'.$this->screenitem['screenid'] : 1;
+		}
+
+		return $this->dataId;
+	}
+
+	/**
+	 * Get unique screen container id.
+	 *
+	 * @return string
+	 */
+	public function getScreenId() {
+		return 'flickerfreescreen_'.$this->getDataId();
+	}
+
+	/**
+	 * Get profile url params.
+	 *
+	 * @return string
+	 */
+	public function getProfileUrlParams() {
+		return '&updateProfile='.(int) $this->updateProfile.'&profileIdx='.$this->profileIdx.'&profileIdx2='.$this->profileIdx2;
+	}
+
+	/**
+	 * Get enveloped screen inside container.
+	 *
+	 * @param object	$item
+	 * @param boolean	$insertFlickerfreeJs
+	 * @param array		$flickerfreeData
+	 *
+	 * @return CDiv
+	 */
+	public function getOutput($item = null, $insertFlickerfreeJs = true, $flickerfreeData = []) {
+		if ($insertFlickerfreeJs) {
+			$this->insertFlickerfreeJs($flickerfreeData);
+		}
+
+		if ($this->mode == SCREEN_MODE_EDIT) {
+			$div = (new CDiv([$item, BR(), new CLink(_('Change'), $this->action)]))
+				->addClass('flickerfreescreen')
+				->setId($this->getScreenId());
+		}
+		else {
+			$div = (new CDiv($item))
+				->addClass('flickerfreescreen')
+				->setId($this->getScreenId());
+		}
+
+		$div->setAttribute('data-timestamp', $this->timestamp);
+		$div->addStyle('position: relative;');
+
+		return $div;
+	}
+
+	/**
+	 * Insert javascript flicker-free screen data.
+	 *
+	 * @param array $data
+	 */
+	public function insertFlickerfreeJs($data = []) {
+		$jsData = [
+			'id' => $this->getDataId(),
+			'isFlickerfree' => $this->isFlickerfree,
+			'pageFile' => $this->pageFile,
+			'resourcetype' => $this->resourcetype,
+			'mode' => $this->mode,
+			'timestamp' => $this->timestamp,
+			'interval' => CWebUser::$data['refresh'],
+			'screenitemid' => !empty($this->screenitem['screenitemid']) ? $this->screenitem['screenitemid'] : null,
+			'screenid' => !empty($this->screenitem['screenid']) ? $this->screenitem['screenid'] : $this->screenid,
+			'groupid' => $this->groupid,
+			'hostid' => $this->hostid,
+			'timeline' => $this->timeline,
+			'profileIdx' => $this->profileIdx,
+			'profileIdx2' => $this->profileIdx2,
+			'updateProfile' => $this->updateProfile,
+			'data' => !empty($data) ? $data : null
+		];
+
+		zbx_add_post_js('window.flickerfreeScreen.add('.zbx_jsvalue($jsData).');');
+	}
+
+	/**
+	 * Insert javascript flicker-free screen data.
+	 *
+	 * @static
+	 *
+	 * @param array		$options
+	 * @param string	$options['profileIdx']
+	 * @param int		$options['profileIdx2']
+	 * @param boolean	$options['updateProfile']
+	 * @param int		$options['period']
+	 * @param string	$options['stime']
+	 *
+	 * @return array
+	 */
+	public static function calculateTime(array $options = []) {
+		if (!array_key_exists('updateProfile', $options)) {
+			$options['updateProfile'] = true;
+		}
+		if (empty($options['profileIdx2'])) {
+			$options['profileIdx2'] = 0;
+		}
+
+		// show only latest data without update is set only period
+		if (!empty($options['period']) && empty($options['stime'])) {
+			$options['updateProfile'] = false;
+			$options['profileIdx'] = '';
+		}
+
+		// period
+		if (empty($options['period'])) {
+			$options['period'] = !empty($options['profileIdx'])
+				? CProfile::get($options['profileIdx'].'.period', ZBX_PERIOD_DEFAULT, $options['profileIdx2'])
+				: ZBX_PERIOD_DEFAULT;
+		}
+		else {
+			if ($options['period'] < ZBX_MIN_PERIOD) {
+				show_error_message(_n('Minimum time period to display is %1$s minute.',
+					'Minimum time period to display is %1$s minutes.',
+					(int) ZBX_MIN_PERIOD / SEC_PER_MIN
+				));
+				$options['period'] = ZBX_MIN_PERIOD;
+			}
+			elseif ($options['period'] > ZBX_MAX_PERIOD) {
+				show_error_message(_n('Maximum time period to display is %1$s day.',
+					'Maximum time period to display is %1$s days.',
+					(int) ZBX_MAX_PERIOD / SEC_PER_DAY
+				));
+				$options['period'] = ZBX_MAX_PERIOD;
+			}
+		}
+		if ($options['updateProfile'] && !empty($options['profileIdx'])) {
+			CProfile::update($options['profileIdx'].'.period', $options['period'], PROFILE_TYPE_INT, $options['profileIdx2']);
+		}
+
+		// stime
+		$time = time();
+		$usertime = null;
+		$stimeNow = null;
+		$isNow = 0;
+
+		if (!empty($options['stime'])) {
+			$stimeUnix = zbxDateToTime($options['stime']);
+
+			if ($stimeUnix > $time || zbxAddSecondsToUnixtime($options['period'], $stimeUnix) > $time) {
+				$stimeNow = $options['stime'];
+				$options['stime'] = date(TIMESTAMP_FORMAT, $time - $options['period']);
+				$usertime = date(TIMESTAMP_FORMAT, $time);
+				$isNow = 1;
+			}
+			else {
+				$usertime = date(TIMESTAMP_FORMAT, zbxAddSecondsToUnixtime($options['period'], $stimeUnix));
+				$isNow = 0;
+			}
+
+			if ($options['updateProfile'] && !empty($options['profileIdx'])) {
+				CProfile::update($options['profileIdx'].'.stime', $options['stime'], PROFILE_TYPE_STR, $options['profileIdx2']);
+				CProfile::update($options['profileIdx'].'.isnow', $isNow, PROFILE_TYPE_INT, $options['profileIdx2']);
+			}
+		}
+		else {
+			if (!empty($options['profileIdx'])) {
+				$isNow = CProfile::get($options['profileIdx'].'.isnow', null, $options['profileIdx2']);
+				if ($isNow) {
+					$options['stime'] = date(TIMESTAMP_FORMAT, $time - $options['period']);
+					$usertime = date(TIMESTAMP_FORMAT, $time);
+					$stimeNow = date(TIMESTAMP_FORMAT, zbxAddSecondsToUnixtime(SEC_PER_YEAR, $options['stime']));
+
+					if ($options['updateProfile']) {
+						CProfile::update($options['profileIdx'].'.stime', $options['stime'], PROFILE_TYPE_STR, $options['profileIdx2']);
+					}
+				}
+				else {
+					$options['stime'] = CProfile::get($options['profileIdx'].'.stime', null, $options['profileIdx2']);
+					$usertime = date(TIMESTAMP_FORMAT, zbxAddSecondsToUnixtime($options['period'], $options['stime']));
+				}
+			}
+
+			if (empty($options['stime'])) {
+				$options['stime'] = date(TIMESTAMP_FORMAT, $time - $options['period']);
+				$usertime = date(TIMESTAMP_FORMAT, $time);
+				$stimeNow = date(TIMESTAMP_FORMAT, zbxAddSecondsToUnixtime(SEC_PER_YEAR, $options['stime']));
+				$isNow = 1;
+
+				if ($options['updateProfile'] && !empty($options['profileIdx'])) {
+					CProfile::update($options['profileIdx'].'.stime', $options['stime'], PROFILE_TYPE_STR, $options['profileIdx2']);
+					CProfile::update($options['profileIdx'].'.isnow', $isNow, PROFILE_TYPE_INT, $options['profileIdx2']);
+				}
+			}
+		}
+
+		return [
+			'period' => $options['period'],
+			'stime' => $options['stime'],
+			'stimeNow' => !empty($stimeNow) ? $stimeNow : $options['stime'],
+			'starttime' => date(TIMESTAMP_FORMAT, $time - ZBX_MAX_PERIOD),
+			'usertime' => $usertime,
+			'isNow' => $isNow
+		];
+	}
+}
