bd9e670a7cab190166b0a0abae8576000a43a702
rich <rich@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-11-29 03:27:52 +0000

    .......... [ZBX-10079] created new development branch to include system.run description updates in the item key helper
    git-svn-id: svn://svn.zabbix.com/branches/dev@56924 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-10079/frontends/php/include/classes/html/CHostsInfo.php
@@ -0,0 +1,134 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CHostsInfo extends CTable {
+
+	public $style;
+
+	public function __construct($groupid = 0, $style = STYLE_HORIZONTAL) {
+		$this->groupid = $groupid;
+		$this->style = null;
+
+		parent::__construct();
+		$this->addClass('hosts_info');
+		$this->setOrientation($style);
+	}
+
+	public function setOrientation($value) {
+		$this->style = $value;
+		return $this;
+	}
+
+	public function bodyToString() {
+		$this->cleanItems();
+
+		$total = 0;
+
+		// fetch accessible host ids
+		$hosts = API::Host()->get([
+			'output' => ['hostid'],
+			'preservekeys' => true
+		]);
+		$hostIds = array_keys($hosts);
+
+		if ($this->groupid != 0) {
+			$cond_from = ',hosts_groups hg';
+			$cond_where = ' AND hg.hostid=h.hostid AND hg.groupid='.zbx_dbstr($this->groupid);
+		}
+		else {
+			$cond_from = '';
+			$cond_where = '';
+		}
+
+		$db_host_cnt = DBselect(
+			'SELECT COUNT(DISTINCT h.hostid) AS cnt'.
+			' FROM hosts h'.$cond_from.
+			' WHERE h.available='.HOST_AVAILABLE_TRUE.
+				' AND h.status IN ('.HOST_STATUS_MONITORED.','.HOST_STATUS_NOT_MONITORED.')'.
+				' AND '.dbConditionInt('h.hostid', $hostIds).
+				$cond_where
+		);
+
+		$host_cnt = DBfetch($db_host_cnt);
+		$avail = $host_cnt['cnt'];
+		$total += $host_cnt['cnt'];
+
+		$db_host_cnt = DBselect(
+			'SELECT COUNT(DISTINCT h.hostid) AS cnt'.
+			' FROM hosts h'.$cond_from.
+			' WHERE h.available='.HOST_AVAILABLE_FALSE.
+				' AND h.status IN ('.HOST_STATUS_MONITORED.','.HOST_STATUS_NOT_MONITORED.')'.
+				' AND '.dbConditionInt('h.hostid', $hostIds).
+				$cond_where
+		);
+
+		$host_cnt = DBfetch($db_host_cnt);
+		$notav = $host_cnt['cnt'];
+		$total += $host_cnt['cnt'];
+
+		$db_host_cnt = DBselect(
+			'SELECT COUNT(DISTINCT h.hostid) AS cnt'.
+			' FROM hosts h'.$cond_from.
+			' WHERE h.available='.HOST_AVAILABLE_UNKNOWN.
+				' AND h.status IN ('.HOST_STATUS_MONITORED.','.HOST_STATUS_NOT_MONITORED.')'.
+				' AND '.dbConditionInt('h.hostid', $hostIds).
+				$cond_where
+		);
+
+		$host_cnt = DBfetch($db_host_cnt);
+		$uncn = $host_cnt['cnt'];
+		$total += $host_cnt['cnt'];
+
+		$header_str = _('Hosts info').SPACE;
+
+		if ($this->groupid != 0) {
+			$group = get_hostgroup_by_groupid($this->groupid);
+			$header_str .= _('Group').SPACE.'&quot;'.$group['name'].'&quot;';
+		}
+		else {
+			$header_str .= _('All groups');
+		}
+
+		$header = new CCol($header_str);
+		if ($this->style == STYLE_HORIZONTAL) {
+			$header->setColspan(4);
+		}
+
+		$this->addRow($header);
+
+		$avail = (new CCol($avail.'  '._('Available')))->addClass('avail');
+		$notav = (new CCol($notav.'  '._('Not available')))->addClass('notav');
+		$uncn = (new CCol($uncn.'  '._('Unknown')))->addClass('uncn');
+		$total = (new CCol($total.'  '._('Total')))->addClass('total');
+
+		if ($this->style == STYLE_HORIZONTAL) {
+			$this->addRow([$avail, $notav, $uncn, $total]);
+		}
+		else {
+			$this->addRow($avail);
+			$this->addRow($notav);
+			$this->addRow($uncn);
+			$this->addRow($total);
+		}
+
+		return parent::bodyToString();
+	}
+}
---
+++ ZBX-10079/frontends/php/include/views/administration.general.valuemapping.list.php
@@ -0,0 +1,75 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+$widget = (new CWidget())
+	->setTitle(_('Value mapping'))
+	->setControls((new CForm())
+		->cleanItems()
+		->addItem((new CList())
+			->addItem(makeAdministrationGeneralMenu('adm.valuemapping.php'))
+			->addItem(new CSubmit('form', _('Create value map')))
+			->addItem((new CButton('form', _('Import')))->onClick('redirect("conf.import.php?rules_preset=valuemap")'))
+		)
+	);
+
+$form = (new CForm())
+	->setName('valuemap_form');
+
+$table = (new CTableInfo())
+	->setHeader([
+		(new CColHeader(
+			(new CCheckBox('all_valuemaps'))
+				->onClick("checkAll('".$form->getName()."', 'all_valuemaps', 'valuemapids');")
+		))->addClass(ZBX_STYLE_CELL_WIDTH),
+		make_sorting_header(_('Name'), 'name', $data['sort'], $data['sortorder']),
+		_('Value map'),
+		_('Used in items')
+	]);
+
+foreach ($data['valuemaps'] as $valuemap) {
+	$mappings = [];
+
+	foreach ($valuemap['mappings'] as $mapping) {
+		$mappings[] = $mapping['value'].' &rArr; '.$mapping['newvalue'];
+		$mappings[] = BR();
+	}
+	array_pop($mappings);
+
+	$table->addRow([
+		new CCheckBox('valuemapids['.$valuemap['valuemapid'].']', $valuemap['valuemapid']),
+		new CLink($valuemap['name'], 'adm.valuemapping.php?form=update&valuemapid='.$valuemap['valuemapid']),
+		$mappings,
+		$valuemap['used_in_items'] ? (new CCol(_('Yes')))->addClass(ZBX_STYLE_GREEN) : ''
+	]);
+}
+
+$form->addItem([
+	$table,
+	$data['paging'],
+	new CActionButtonList('action', 'valuemapids', [
+		'valuemap.export' => ['name' => _('Export')],
+		'valuemap.delete' => ['name' => _('Delete'), 'confirm' => _('Delete selected value maps?')]
+	])
+]);
+
+$widget->addItem($form);
+
+return $widget;
