2e168d863e253aee9528a2f7804cc5fedcd0c71c
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-12-17 14:35:53 +0000

    A.F.I..... [ZBXNEXT-3007] implemented support of public and private maps with ability to share them with other users and user groups
    git-svn-id: svn://svn.zabbix.com/trunk@57262 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/app/views/monitoring.map.view.php
+++ frontends/php/app/views/monitoring.map.view.php
@@ -25,50 +25,41 @@ $headerMapForm = (new CForm('get'))->cleanItems();
 
 $controls = new CList();
 
-if ($data['maps']) {
-	$maps = [];
-	foreach ($data['maps'] as $sysmapid => $map) {
-		$maps[$sysmapid] = $map['name'];
+$headerMapForm->addVar('action', 'map.view')
+	->addVar('sysmapid', $data['sysmapid'])
+	->addVar('fullscreen', $data['fullscreen']);
+
+$controls
+	->addItem(new CLink(_('All maps'), 'sysmaps.php'))
+	->addItem([_('Minimum severity'), SPACE, $data['pageFilter']->getSeveritiesMinCB()]);
+
+// get map parent maps
+$parentMaps = [];
+foreach (getParentMaps($data['sysmapid']) as $parent) {
+	// check for permissions
+	if (isset($data['maps'][$parent['sysmapid']])) {
+		$parentMaps[] = SPACE.SPACE;
+		$parentMaps[] = new CLink($parent['name'], 'zabbix.php?action=map.view&sysmapid='.$parent['sysmapid'].'&fullscreen='.$data['fullscreen'].'&severity_min='.$data['severity_min']);
 	}
-
-	$headerMapForm->addVar('action', 'map.view')
-		->addVar('fullscreen', $data['fullscreen']);
-
-	$controls
-		->addItem([_('Map'), SPACE, new CComboBox('sysmapid', $data['sysmapid'], 'submit()', $maps)])
-		->addItem([_('Minimum severity'), SPACE, $data['pageFilter']->getSeveritiesMinCB()]);
-
-	// get map parent maps
-	$parentMaps = [];
-	foreach (getParentMaps($data['sysmapid']) as $parent) {
-		// check for permissions
-		if (isset($data['maps'][$parent['sysmapid']])) {
-			$parentMaps[] = SPACE.SPACE;
-			$parentMaps[] = new CLink($parent['name'], 'zabbix.php?action=map.view&sysmapid='.$parent['sysmapid'].'&fullscreen='.$data['fullscreen'].'&severity_min='.$data['severity_min']);
-		}
-	}
-	if (!empty($parentMaps)) {
-		array_unshift($parentMaps, _('Upper level maps').':');
-		$controls->addItem($parentMaps);
-	}
-
-	$actionMap = getActionMapBySysmap($data['map'], ['severity_min' => $data['severity_min']]);
-	$imgMap = (new CImg('map.php?sysmapid='.$data['sysmapid'].'&severity_min='.$data['severity_min']))
-		->setMap($actionMap->getName());
-
-	$mapTable = (new CTable())
-		->addRow($actionMap)
-		->addRow($imgMap);
-
-	$controls->addItem(get_icon('favourite', [
-		'fav' => 'web.favorite.sysmapids',
-		'elname' => 'sysmapid',
-		'elid' => $data['sysmapid']
-	]));
 }
-else {
-	$mapTable = (new CTable())->setNoDataMessage(_('No maps found.'));
+if (!empty($parentMaps)) {
+	array_unshift($parentMaps, _('Upper level maps').':');
+	$controls->addItem($parentMaps);
 }
+
+$actionMap = getActionMapBySysmap($data['map'], ['severity_min' => $data['severity_min']]);
+$imgMap = (new CImg('map.php?sysmapid='.$data['sysmapid'].'&severity_min='.$data['severity_min']))
+	->setMap($actionMap->getName());
+
+$mapTable = (new CTable())
+	->addRow($actionMap)
+	->addRow($imgMap);
+
+$controls->addItem(get_icon('favourite', [
+	'fav' => 'web.favorite.sysmapids',
+	'elname' => 'sysmapid',
+	'elid' => $data['sysmapid']
+]));
 
 $controls->addItem(get_icon('fullscreen', ['fullscreen' => $data['fullscreen']]));
 
