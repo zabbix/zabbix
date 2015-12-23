308a080548bb1dfa785c3e7ecb6b17e23ff427b6
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2015-12-23 07:28:00 +0000

    ...G...PS. [ZBXNEXT-1263] turned get_ip_by_socket() into a static function
    git-svn-id: svn://svn.zabbix.com/branches/dev@57343 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1263-2/include/comms.h
+++ ZBXNEXT-1263-2/include/comms.h
@@ -150,7 +150,6 @@ void	zbx_tcp_unaccept(zbx_socket_t *s);
 ssize_t		zbx_tcp_recv_ext(zbx_socket_t *s, unsigned char flags, int timeout);
 const char	*zbx_tcp_recv_line(zbx_socket_t *s);
 
-char	*get_ip_by_socket(zbx_socket_t *s);
 int	zbx_tcp_check_security(zbx_socket_t *s, const char *ip_list, int allow_if_empty);
 
 int	zbx_udp_connect(zbx_socket_t *s, const char *source_ip, const char *ip, unsigned short port, int timeout);
