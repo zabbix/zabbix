60407256ba4b2d2dcdb80155c960935cb81dcee8
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-01-07 07:58:24 +0000

    .......... [ZBX-10066] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@57459 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10066/upgrades/dbpatches/2.0/ibm_db2/patch/history_str.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE history_str ALTER COLUMN itemid SET WITH DEFAULT NULL
-/
-REORG TABLE history_str
-/
-ALTER TABLE history_str ADD ns integer WITH DEFAULT '0' NOT NULL
-/
-REORG TABLE history_str
-/
