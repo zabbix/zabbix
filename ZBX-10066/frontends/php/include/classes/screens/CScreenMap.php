f1cc3efd11e53f1307003303a4f6c3ca2f700192
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-01-13 08:38:01 +0000

    .......... [ZBX-10066] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@57573 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-10066/frontends/php/include/classes/screens/CScreenMap.php
@@ -0,0 +1,78 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2015 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CScreenMap extends CScreenBase {
+
+	/**
+	 * Params for monitoring maps js.
+	 *
+	 * @var array
+	 */
+	private $data = [];
+
+	/**
+	 * Process screen.
+	 *
+	 * @return CDiv (screen inside container)
+	 */
+	public function get() {
+		$image = (new CImg('map.php?noedit=1&sysmapid='.$this->screenitem['resourceid'].'&width='.$this->screenitem['width']
+			.'&height='.$this->screenitem['height'].'&curtime='.time()))
+			->setId('map_'.$this->screenitem['screenitemid']);
+
+		if ($this->mode == SCREEN_MODE_PREVIEW) {
+			$sysmap = API::Map()->get([
+				'sysmapids' => $this->screenitem['resourceid'],
+				'output' => API_OUTPUT_EXTEND,
+				'selectSelements' => API_OUTPUT_EXTEND,
+				'selectLinks' => API_OUTPUT_EXTEND,
+				'expandUrls' => true,
+				'nopermissions' => true,
+				'preservekeys' => true
+			]);
+			$sysmap = reset($sysmap);
+
+			$image->setSrc($image->getAttribute('src').'&severity_min='.$sysmap['severity_min']);
+
+			$actionMap = getActionMapBySysmap($sysmap, ['severity_min' => $sysmap['severity_min']]);
+			$image->setMap($actionMap->getName());
+
+			$output = [$actionMap, $image];
+		}
+		elseif ($this->mode == SCREEN_MODE_EDIT) {
+			$output = [$image, BR(), new CLink(_('Change'), $this->action)];
+		}
+		else {
+			$output = [$image];
+		}
+
+		$this->insertFlickerfreeJs();
+
+		$div = (new CDiv($output))
+			->addClass('map-container')
+			->addClass('flickerfreescreen')
+			->setId($this->getScreenId())
+			->setAttribute('data-timestamp', $this->timestamp)
+			->addStyle('position: relative;');
+
+		return $div;
+	}
+}
