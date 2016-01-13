2a93682a578523ce74f030f9a9ab440d0dceb62c
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-01-13 14:45:37 +0000

    .......... [ZBXNEXT-3073] removed dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@57600 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-3073/frontends/php/include/views/js/configuration.triggers.list.js.php
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-	jQuery(function($) {
-		$('#filter_state').change(function() {
-			$('input[name=filter_status]').prop('disabled', $('input[name=filter_state]:checked').val() != -1);
-		})
-		.trigger('change', false);
-	});
-</script>
