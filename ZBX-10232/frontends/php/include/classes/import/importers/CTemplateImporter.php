d08f2a7a2e20c1b4b41bfac1c28c3ebb87c3e9b9
gunars <gunars@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-01-18 14:03:10 +0000

    .......... [ZBX-10232] created new dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@57759 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-10232/frontends/php/include/classes/import/importers/CTemplateImporter.php
@@ -0,0 +1,248 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CTemplateImporter extends CImporter {
+
+	/**
+	 * Import templates.
+	 *
+	 * @throws Exception
+	 *
+	 * @param array $templates
+	 *
+	 * @return void
+	 */
+	public function import(array $templates) {
+		$templates = zbx_toHash($templates, 'host');
+
+		$this->checkCircularTemplateReferences($templates);
+
+		foreach ($templates as &$template) {
+			// screens are imported separately
+			unset($template['screens']);
+
+			if (!$this->options['templateLinkage']['createMissing']) {
+				unset($template['templates']);
+			}
+		}
+		unset($template);
+
+		do {
+			$independentTemplates = $this->getIndependentTemplates($templates);
+
+			$templatesToCreate = array();
+			$templatesToUpdate = array();
+			$templateLinkage = array();
+			foreach ($independentTemplates as $name) {
+				$template = $templates[$name];
+				unset($templates[$name]);
+
+				$template = $this->resolveTemplateReferences($template);
+
+				// if we need to add linkages, save linked templates to massAdd later
+				if ($this->options['templateLinkage']['createMissing'] && !empty($template['templates'])) {
+					$templateLinkage[$template['host']] = $template['templates'];
+					unset($template['templates']);
+				}
+
+				if (!empty($template['templateid'])) {
+					$templatesToUpdate[] = $template;
+				}
+				else {
+					$templatesToCreate[] = $template;
+				}
+			}
+
+			if ($this->options['templates']['createMissing'] && $templatesToCreate) {
+				$newHostIds = API::Template()->create($templatesToCreate);
+
+				foreach ($templatesToCreate as $num => $createdTemplate) {
+					$hostId = $newHostIds['templateids'][$num];
+					$this->referencer->addTemplateRef($createdTemplate['host'], $hostId);
+					$this->referencer->addProcessedHost($createdTemplate['host']);
+
+					if (!empty($templateLinkage[$createdTemplate['host']])) {
+						API::Template()->massAdd(array(
+							'templates' => array('templateid' => $hostId),
+							'templates_link' => $templateLinkage[$createdTemplate['host']]
+						));
+					}
+				}
+			}
+			if ($this->options['templates']['updateExisting'] && $templatesToUpdate) {
+				API::Template()->update($templatesToUpdate);
+
+				foreach ($templatesToUpdate as $updatedTemplate) {
+					$this->referencer->addProcessedHost($updatedTemplate['host']);
+
+					if (!empty($templateLinkage[$updatedTemplate['host']])) {
+						API::Template()->massAdd(array(
+							'templates' => $updatedTemplate,
+							'templates_link' => $templateLinkage[$updatedTemplate['host']]
+						));
+					}
+				}
+			}
+		} while (!empty($independentTemplates));
+
+		// if there are templates left in $templates, then they have unresolved references
+		foreach ($templates as $template) {
+			$unresolvedReferences = array();
+			foreach ($template['templates'] as $linkedTemplate) {
+				if (!$this->referencer->resolveTemplate($linkedTemplate['name'])) {
+					$unresolvedReferences[] = $linkedTemplate['name'];
+				}
+			}
+			throw new Exception(_n('Cannot import template "%2$s", linked template "%3$s" does not exist.',
+				'Cannot import template "%2$s", linked templates "%3$s" does not exist.',
+				count($unresolvedReferences), $template['host'], implode(', ', $unresolvedReferences)));
+		}
+	}
+
+	/**
+	 * Check if templates have circular references.
+	 *
+	 * @throws Exception
+	 * @see checkCircularRecursive
+	 *
+	 * @param array $templates
+	 *
+	 * @return void
+	 */
+	protected function checkCircularTemplateReferences(array $templates) {
+		foreach ($templates as $name => $template) {
+			if (empty($template['templates'])) {
+				continue;
+			}
+
+			foreach ($template['templates'] as $linkedTemplate) {
+				$checked = array($name);
+				if ($circTemplates = $this->checkCircularRecursive($linkedTemplate, $templates, $checked)) {
+					throw new Exception(_s('Circular reference in templates: %1$s.', implode(' - ', $circTemplates)));
+				}
+			}
+		}
+	}
+
+	/**
+	 * Recursive function for searching for circular template references.
+	 * If circular reference exist it return array with template names with circular reference.
+	 *
+	 * @param array $linkedTemplate template element to inspect on current recursive loop
+	 * @param array $templates      all templates where circular references should be searched
+	 * @param array $checked        template names that already were processed,
+	 *                              should contain unique values if no circular references exist
+	 *
+	 * @return array|bool
+	 */
+	protected function checkCircularRecursive(array $linkedTemplate, array $templates, array $checked) {
+		$linkedTemplateName = $linkedTemplate['name'];
+
+		// if current element map name is already in list of checked map names,
+		// circular reference exists
+		if (in_array($linkedTemplateName, $checked)) {
+			// to have nice result containing only maps that have circular reference,
+			// remove everything that was added before repeated map name
+			$checked = array_slice($checked, array_search($linkedTemplateName, $checked));
+			// add repeated name to have nice loop like m1->m2->m3->m1
+			$checked[] = $linkedTemplateName;
+			return $checked;
+		}
+		else {
+			$checked[] = $linkedTemplateName;
+		}
+
+		// we need to find map that current element reference to
+		// and if it has selements check all them recursively
+		if (!empty($templates[$linkedTemplateName]['templates'])) {
+			foreach ($templates[$linkedTemplateName]['templates'] as $tpl) {
+				return $this->checkCircularRecursive($tpl, $templates, $checked);
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get templates that don't have not existing linked templates i.e. all templates that must be linked to these templates exist.
+	 * Returns array with template names (host).
+	 *
+	 * @param array $templates
+	 *
+	 * @return array
+	 */
+	protected function getIndependentTemplates(array $templates) {
+		foreach ($templates as $num => $template) {
+			if (empty($template['templates'])) {
+				continue;
+			}
+
+			foreach ($template['templates'] as $linkedTpl) {
+				if (!$this->referencer->resolveTemplate($linkedTpl['name'])) {
+					unset($templates[$num]);
+					continue 2;
+				}
+			}
+		}
+
+		return zbx_objectValues($templates, 'host');
+	}
+
+	/**
+	 * Change all references in template to database ids.
+	 *
+	 * @throws Exception
+	 *
+	 * @param array $template
+	 *
+	 * @return array
+	 */
+	protected function resolveTemplateReferences(array $template) {
+		if ($templateId = $this->referencer->resolveTemplate($template['host'])) {
+			$template['templateid'] = $templateId;
+
+			// if we update template, existing macros should have hostmacroid
+			foreach ($template['macros'] as &$macro) {
+				if ($hostMacroId = $this->referencer->resolveMacro($templateId, $macro['macro'])) {
+					$macro['hostmacroid'] = $hostMacroId;
+				}
+			}
+			unset($macro);
+		}
+
+		foreach ($template['groups'] as $gnum => $group) {
+			if (!$this->referencer->resolveGroup($group['name'])) {
+				throw new Exception(_s('Group "%1$s" does not exist.', $group['name']));
+			}
+			$template['groups'][$gnum] = array('groupid' => $this->referencer->resolveGroup($group['name']));
+		}
+
+		if (isset($template['templates'])) {
+			foreach ($template['templates'] as $tnum => $parentTemplate) {
+				$template['templates'][$tnum] = array(
+					'templateid' => $this->referencer->resolveTemplate($parentTemplate['name'])
+				);
+			}
+		}
+
+		return $template;
+	}
+}
---
+++ ZBX-10232/frontends/php/setup.php
@@ -0,0 +1,139 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+require_once dirname(__FILE__).'/include/config.inc.php';
+require_once dirname(__FILE__).'/include/setup.inc.php';
+
+$page['title'] = _('Installation');
+$page['file'] = 'setup.php';
+
+if (!defined('PAGE_HEADER_LOADED') && !defined('ZBX_PAGE_NO_MENU')) {
+	define('ZBX_PAGE_NO_MENU', 1);
+}
+
+define('ZBX_PAGE_NO_THEME', true); // don't load any themes for this page
+
+// VAR	TYPE	OPTIONAL	FLAGS	VALIDATION	EXCEPTION
+$fields = array(
+	'distributed' =>		array(T_ZBX_STR, O_OPT, null,	null,				null),
+	'trouble' =>			array(T_ZBX_STR, O_OPT, null,	null,				null),
+	'type' =>				array(T_ZBX_STR, O_OPT, null,	IN('"'.ZBX_DB_MYSQL.'","'.ZBX_DB_POSTGRESQL.'","'.ZBX_DB_ORACLE.'","'.ZBX_DB_DB2.'","'.ZBX_DB_SQLITE3.'"'), null),
+	'server' =>				array(T_ZBX_STR, O_OPT, null,	null,				null),
+	'port' =>				array(T_ZBX_INT, O_OPT, null,	BETWEEN(0, 65535),	null, _('Port')),
+	'database' =>			array(T_ZBX_STR, O_OPT, null,	NOT_EMPTY,			null, _('Database name')),
+	'user' =>				array(T_ZBX_STR, O_OPT, null,	null,				null),
+	'password' =>			array(T_ZBX_STR, O_OPT, null,	null, 				null),
+	'schema' =>				array(T_ZBX_STR, O_OPT, null,	null, 				null),
+	'zbx_server' =>			array(T_ZBX_STR, O_OPT, null,	null,				null),
+	'zbx_server_name' =>	array(T_ZBX_STR, O_OPT, null,	null,				null),
+	'zbx_server_port' =>	array(T_ZBX_INT, O_OPT, null,	BETWEEN(0, 65535),	null, _('Port')),
+	'message' =>			array(T_ZBX_STR, O_OPT, null,	null,				null),
+	'nodename' =>			array(T_ZBX_STR, O_OPT, null,	NOT_EMPTY,			null),
+	'nodeid' =>				array(T_ZBX_INT, O_OPT, null,	BETWEEN(0, 999),	null),
+	// actions
+	'save_config' =>		array(T_ZBX_STR, O_OPT, P_SYS,	null,				null),
+	'retry' =>				array(T_ZBX_STR, O_OPT, P_SYS,	null,				null),
+	'cancel' =>				array(T_ZBX_STR, O_OPT, P_SYS,	null,				null),
+	'finish' =>				array(T_ZBX_STR, O_OPT, P_SYS,	null,				null),
+	'next' =>				array(T_ZBX_STR, O_OPT, P_SYS,	null,				null),
+	'back' =>				array(T_ZBX_STR, O_OPT, P_SYS,	null,				null),
+	'form' =>				array(T_ZBX_STR, O_OPT, P_SYS,	null,				null),
+	'form_refresh' =>		array(T_ZBX_INT, O_OPT, null,	null,				null)
+);
+check_fields($fields, false);
+
+global $ZBX_CONFIG;
+
+if (isset($_REQUEST['cancel']) || isset($_REQUEST['finish'])) {
+	zbx_unsetcookie('ZBX_CONFIG');
+	redirect('index.php');
+}
+
+$ZBX_CONFIG = get_cookie('ZBX_CONFIG', null);
+$ZBX_CONFIG = isset($ZBX_CONFIG) ? unserialize($ZBX_CONFIG) : array();
+
+if (!isset($ZBX_CONFIG['step'])) {
+	$ZBX_CONFIG['step'] = 0;
+}
+if (!isset($ZBX_CONFIG['agree'])) {
+	$ZBX_CONFIG['agree'] = false;
+}
+
+$ZBX_CONFIG['allowed_db'] = array();
+
+// MYSQL
+if (zbx_is_callable(array('mysql_pconnect', 'mysql_select_db', 'mysql_error', 'mysql_select_db', 'mysql_query',
+		'mysql_fetch_array', 'mysql_fetch_row', 'mysql_data_seek', 'mysql_insert_id'))) {
+	$ZBX_CONFIG['allowed_db']['MYSQL'] = 'MySQL';
+}
+
+// POSTGRESQL
+if (zbx_is_callable(array('pg_pconnect', 'pg_fetch_array', 'pg_fetch_row', 'pg_exec', 'pg_getlastoid'))) {
+	$ZBX_CONFIG['allowed_db']['POSTGRESQL'] = 'PostgreSQL';
+}
+
+// ORACLE
+if (zbx_is_callable(array('ocilogon', 'ocierror', 'ociparse', 'ociexecute', 'ocifetchinto'))) {
+	$ZBX_CONFIG['allowed_db']['ORACLE'] = 'Oracle';
+}
+
+// IBM_DB2
+if (zbx_is_callable(array('db2_connect', 'db2_set_option', 'db2_prepare', 'db2_execute', 'db2_fetch_assoc'))) {
+	$ZBX_CONFIG['allowed_db']['IBM_DB2'] = 'IBM DB2';
+}
+
+// SQLITE3. The false is here to avoid autoloading of the class.
+if (class_exists('SQLite3', false) && zbx_is_callable(array('ftok', 'sem_acquire', 'sem_release', 'sem_get'))) {
+	$ZBX_CONFIG['allowed_db']['SQLITE3'] = 'SQLite3';
+}
+
+if (count($ZBX_CONFIG['allowed_db']) == 0) {
+	$ZBX_CONFIG['allowed_db']['no'] = 'No';
+}
+
+/*
+ * Setup wizard
+ */
+global $ZBX_SETUP_WIZARD;
+
+$ZBX_SETUP_WIZARD = new CSetupWizard($ZBX_CONFIG);
+
+zbx_set_post_cookie('ZBX_CONFIG', serialize($ZBX_CONFIG));
+
+require_once dirname(__FILE__).'/include/page_header.php';
+
+include('include/views/js/setup.js.php');
+
+/*
+ * Check configuration
+ */
+global $ZBX_CONFIGURATION_FILE;
+
+if (file_exists($ZBX_CONFIGURATION_FILE)) {
+	if (isset($_REQUEST['message'])) {
+		show_error_message($_REQUEST['message']);
+	}
+}
+
+$ZBX_SETUP_WIZARD->show();
+unset($_POST);
+
+require_once dirname(__FILE__).'/include/page_footer.php';
