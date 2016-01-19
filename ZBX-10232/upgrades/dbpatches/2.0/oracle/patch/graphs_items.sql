7a15bb0b8e76a083afeb08e5d740600863178444
gunars <gunars@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-01-19 08:38:25 +0000

    .......... [ZBX-10232] removed dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@57803 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10232/upgrades/dbpatches/2.0/oracle/patch/graphs_items.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE graphs_items MODIFY gitemid DEFAULT NULL;
-ALTER TABLE graphs_items MODIFY graphid DEFAULT NULL;
-ALTER TABLE graphs_items MODIFY itemid DEFAULT NULL;
-ALTER TABLE graphs_items DROP COLUMN periods_cnt;
-UPDATE graphs_items SET type=0 WHERE type=1;
-DELETE FROM graphs_items WHERE NOT graphid IN (SELECT graphid FROM graphs);
-DELETE FROM graphs_items WHERE NOT itemid IN (SELECT itemid FROM items);
-ALTER TABLE graphs_items ADD CONSTRAINT c_graphs_items_1 FOREIGN KEY (graphid) REFERENCES graphs (graphid) ON DELETE CASCADE;
-ALTER TABLE graphs_items ADD CONSTRAINT c_graphs_items_2 FOREIGN KEY (itemid) REFERENCES items (itemid) ON DELETE CASCADE;
