23646fb6f9dbf4ac366cc413069579ba332c31cd
Sandis Neilands <git-no-reply@zabbix.com>
2016-01-20 16:18:18 +0000

    ...G...... [ZBX-9877] changed system.uname on Windows to get data from WMI isntead of volatile Windows APIs, fixed wmi.get to return UTF-8 encoded strings

--- src/libs/zbxsysinfo/win32/system.c
+++ src/libs/zbxsysinfo/win32/system.c
@@ -22,193 +22,7 @@
 #include "perfmon.h"
 #pragma comment(lib, "user32.lib")
 
-static void	get_50_version(char **os, size_t *os_alloc, size_t *os_offset, const OSVERSIONINFOEX *vi)
-{
-	zbx_strcpy_alloc(os, os_alloc, os_offset, " Microsoft Windows 2000");
-
-	if (VER_NT_WORKSTATION != vi->wProductType)
-	{
-		if (0 != (vi->wSuiteMask & VER_SUITE_DATACENTER))
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Datacenter Server");
-		else if (0 != (vi->wSuiteMask & VER_SUITE_ENTERPRISE))
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Advanced Server");
-		else
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Server");
-	}
-	else
-		zbx_strcpy_alloc(os, os_alloc, os_offset, " Professional");
-
-}
-
-static void	get_51_version(char **os, size_t *os_alloc, size_t *os_offset, const OSVERSIONINFOEX *vi)
-{
-	zbx_strcpy_alloc(os, os_alloc, os_offset, " Microsoft Windows XP");
-
-	if (0 != GetSystemMetrics(87))		/* SM_MEDIACENTER */
-		zbx_strcpy_alloc(os, os_alloc, os_offset, " Media Center Edition");
-	else if (0 != GetSystemMetrics(88))	/* SM_STARTER */
-		zbx_strcpy_alloc(os, os_alloc, os_offset, " Starter Edition");
-	else if (0 != GetSystemMetrics(86))	/* SM_TABLETPC */
-		zbx_strcpy_alloc(os, os_alloc, os_offset, " Tablet PC Edition");
-	else if (0 != (vi->wSuiteMask & VER_SUITE_PERSONAL))
-		zbx_strcpy_alloc(os, os_alloc, os_offset, " Home Edition");
-	else
-		zbx_strcpy_alloc(os, os_alloc, os_offset, " Professional");
-}
-
-static void	get_52_version(char **os, size_t *os_alloc, size_t *os_offset, const OSVERSIONINFOEX *vi,
-		const SYSTEM_INFO *si)
-{
-	zbx_strcpy_alloc(os, os_alloc, os_offset, " Microsoft Windows");
-
-	if (0 != GetSystemMetrics(89))			/* SM_SERVERR2 */
-		zbx_strcpy_alloc(os, os_alloc, os_offset, " Server 2003 R2");
-	else if (0 != (vi->wSuiteMask & 0x8000))	/* VER_SUITE_WH_SERVER */
-		zbx_strcpy_alloc(os, os_alloc, os_offset, " Home Server");
-	else if (VER_NT_WORKSTATION == vi->wProductType && PROCESSOR_ARCHITECTURE_AMD64 == si->wProcessorArchitecture)
-		zbx_strcpy_alloc(os, os_alloc, os_offset, " XP Professional");
-	else
-		zbx_strcpy_alloc(os, os_alloc, os_offset, " Server 2003");
-
-	if (VER_NT_WORKSTATION != vi->wProductType)
-	{
-		if (vi->wSuiteMask & VER_SUITE_COMPUTE_SERVER)
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Compute Cluster Edition");
-		else if (vi->wSuiteMask & VER_SUITE_DATACENTER)
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Datacenter Edition");
-		else if (vi->wSuiteMask & VER_SUITE_ENTERPRISE)
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Enterprise Edition");
-		else if (vi->wSuiteMask & VER_SUITE_BLADE)
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Web Edition");
-		else
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Standard Edition");
-	}
-}
-
-static void	get_6x_version(char **os, size_t *os_alloc, size_t *os_offset, const OSVERSIONINFOEX *vi,
-		const SYSTEM_INFO *si)
-{
-	typedef BOOL (WINAPI *PGPI)(DWORD, DWORD, DWORD, DWORD, PDWORD);
-
-	PGPI	pGPI;
-	DWORD	product_type;
-
-	zbx_strcpy_alloc(os, os_alloc, os_offset, " Microsoft Windows");
-
-	if (VER_NT_WORKSTATION == vi->wProductType)
-	{
-		switch (vi->dwMinorVersion)
-		{
-			case 0:
-				zbx_strcpy_alloc(os, os_alloc, os_offset, " Vista");
-				break;
-			case 1:
-				zbx_strcpy_alloc(os, os_alloc, os_offset, " 7");
-				break;
-			case 2:
-				zbx_strcpy_alloc(os, os_alloc, os_offset, " 8");
-				break;
-			case 3:
-				zbx_strcpy_alloc(os, os_alloc, os_offset, " 8.1");
-				break;
-		}
-	}
-	else
-	{
-		switch (vi->dwMinorVersion)
-		{
-			case 0:
-				zbx_strcpy_alloc(os, os_alloc, os_offset, " Server 2008");
-				break;
-			case 1:
-				zbx_strcpy_alloc(os, os_alloc, os_offset, " Server 2008 R2");
-				break;
-			case 2:
-				zbx_strcpy_alloc(os, os_alloc, os_offset, " Server 2012");
-				break;
-			case 3:
-				zbx_strcpy_alloc(os, os_alloc, os_offset, " Server 2012 R2");
-				break;
-		}
-	}
-
-	pGPI = (PGPI)GetProcAddress(GetModuleHandle(TEXT("kernel32.dll")), "GetProductInfo");
-
-	pGPI(vi->dwMajorVersion, vi->dwMinorVersion, 0, 0, &product_type);
-
-	/* use constants in order to support Windows 2000 */
-	switch (product_type)
-	{
-		case 0x0001:	/* PRODUCT_ULTIMATE */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Ultimate Edition");
-			break;
-		case 0x0030:	/* PRODUCT_PROFESSIONAL */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Professional");
-			break;
-		case 0x0003:	/* PRODUCT_HOME_PREMIUM */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Home Premium Edition");
-			break;
-		case 0x0002:	/* PRODUCT_HOME_BASIC */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Home Basic Edition");
-			break;
-		case 0x0004:	/* PRODUCT_ENTERPRISE */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Enterprise Edition");
-			break;
-		case 0x0006:	/* PRODUCT_BUSINESS */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Business Edition");
-			break;
-		case 0x000B:	/* PRODUCT_STARTER */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Starter Edition");
-			break;
-		case 0x0012:	/* PRODUCT_CLUSTER_SERVER */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Cluster Server Edition");
-			break;
-		case 0x0008:	/* PRODUCT_DATACENTER_SERVER */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Datacenter Edition");
-			break;
-		case 0x000C:	/* PRODUCT_DATACENTER_SERVER_CORE */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Datacenter Edition (core installation)");
-			break;
-		case 0x000A:	/* PRODUCT_ENTERPRISE_SERVER */
-		case 0x000F:	/* PRODUCT_ENTERPRISE_SERVER_IA64 */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Enterprise Edition");
-			break;
-		case 0x000E:	/* PRODUCT_ENTERPRISE_SERVER_CORE */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Enterprise Edition (core installation)");
-			break;
-		case 0x0009:	/* PRODUCT_SMALLBUSINESS_SERVER */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Small Business Server");
-			break;
-		case 0x0019:	/* PRODUCT_SMALLBUSINESS_SERVER_PREMIUM */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Small Business Server Premium Edition");
-			break;
-		case 0x0007:	/* PRODUCT_STANDARD_SERVER */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Standard Edition");
-			break;
-		case 0x000D:	/* PRODUCT_STANDARD_SERVER_CORE */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Standard Edition (core installation)");
-			break;
-		case 0x0011:	/* PRODUCT_WEB_SERVER */
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Web Server Edition");
-			break;
-	}
-}
-
-static void	get_cpu_type(char **os, size_t *os_alloc, size_t *os_offset, const SYSTEM_INFO *si)
-{
-	switch (si->wProcessorArchitecture)
-	{
-		case PROCESSOR_ARCHITECTURE_INTEL:
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " x86");
-			break;
-		case PROCESSOR_ARCHITECTURE_AMD64:
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " x64");
-			break;
-		case PROCESSOR_ARCHITECTURE_IA64:
-			zbx_strcpy_alloc(os, os_alloc, os_offset, " Intel Itanium-based");
-			break;
-	}
-}
+void	zbx_wmi_get(char *wmi_namespace, char *wmi_query, char **utf8_value);
 
 /******************************************************************************
  *                                                                            *
@@ -341,81 +155,36 @@ out:
 
 int	SYSTEM_UNAME(AGENT_REQUEST *request, AGENT_RESULT *result)
 {
-	typedef void (WINAPI *PGNSI)(LPSYSTEM_INFO);
+	char	*os = NULL;
+	size_t	os_alloc = 0, os_offset = 0;
+	char	*sysname = "Windows";
+	char	*nodename_csname = NULL;
+	char	*release_version = NULL;
+	char	*version_caption = NULL;
+	char	*version_csdversion = NULL;
+	char	*machine_osarchitecture = NULL;
+	char	*wmi_namespace = "root\\cimv2";
 
-	DWORD			dwSize = 256;
-	wchar_t			computer_name[256];
-	SYSTEM_INFO		si;
-	const OSVERSIONINFOEX	*vi;
-	char			*os = NULL, *utf8;
-	size_t			os_alloc = 256, os_offset = 0;
-	PGNSI			pGNSI;
+	zbx_wmi_get(wmi_namespace, "select CSName from Win32_OperatingSystem", &nodename_csname);
+	zbx_wmi_get(wmi_namespace, "select Version from Win32_OperatingSystem", &release_version);
+	zbx_wmi_get(wmi_namespace, "select Caption from Win32_OperatingSystem", &version_caption);
+	zbx_wmi_get(wmi_namespace, "select CSDVersion from Win32_OperatingSystem", &version_csdversion);
+	zbx_wmi_get(wmi_namespace, "select OSArchitecture from Win32_OperatingSystem", &machine_osarchitecture);
 
-	/* Buffer size is chosen large enough to contain any DNS name, not just MAX_COMPUTERNAME_LENGTH + 1 */
-	/* characters. MAX_COMPUTERNAME_LENGTH is usually less than 32, but it varies among systems, so we  */
-	/* cannot use the constant in a precompiled Windows agent, which is expected to work on any system. */
-	if (0 == GetComputerName(computer_name, &dwSize))
-		*computer_name = '\0';
+	zbx_snprintf_alloc(&os, &os_alloc, &os_offset, "%s %s %s %s%s%s %s",
+		sysname,
+		nodename_csname ? nodename_csname : "<unknown nodename>",
+		release_version ? release_version : "<unknown release>",
+		version_caption ? version_caption : "<unknown version>",
+		version_csdversion ? " " : "",
+		version_csdversion ? version_csdversion : "",
+		machine_osarchitecture ? machine_osarchitecture : "32-bit");
 
-	memset(&si, 0, sizeof(si));
-
-	if (NULL == (vi = zbx_win_getversion()))
-	{
-		SET_MSG_RESULT(result, zbx_strdup(NULL, "Cannot obtain system information."));
-		return SYSINFO_RET_FAIL;
-	}
-
-	if (NULL != (pGNSI = (PGNSI)GetProcAddress(GetModuleHandle(TEXT("kernel32.dll")), "GetNativeSystemInfo")))
-		pGNSI(&si);
-	else
-		GetSystemInfo(&si);
-
-	os = zbx_malloc(os, os_alloc);
-
-	zbx_strcpy_alloc(&os, &os_alloc, &os_offset, "Windows");
-
-	if ('\0' != *computer_name)
-	{
-		utf8 = zbx_unicode_to_utf8(computer_name);
-		zbx_snprintf_alloc(&os, &os_alloc, &os_offset, " %s", utf8);
-		zbx_free(utf8);
-	}
-
-	zbx_snprintf_alloc(&os, &os_alloc, &os_offset, " %d.%d.%d",
-			vi->dwMajorVersion, vi->dwMinorVersion, vi->dwBuildNumber);
-
-	if (VER_PLATFORM_WIN32_NT == vi->dwPlatformId)
-	{
-		switch (vi->dwMajorVersion)
-		{
-			case 5:
-				switch (vi->dwMinorVersion)
-				{
-					case 0:
-						get_50_version(&os, &os_alloc, &os_offset, vi);
-						break;
-					case 1:
-						get_51_version(&os, &os_alloc, &os_offset, vi);
-						break;
-					case 2:
-						get_52_version(&os, &os_alloc, &os_offset, vi, &si);
-						break;
-				}
-				break;
-			case 6:
-				get_6x_version(&os, &os_alloc, &os_offset, vi, &si);
-				break;
-		}
-	}
-
-	if ('\0' != *vi->szCSDVersion)
-	{
-		utf8 = zbx_unicode_to_utf8(vi->szCSDVersion);
-		zbx_snprintf_alloc(&os, &os_alloc, &os_offset, " %s", utf8);
-		zbx_free(utf8);
-	}
-
-	get_cpu_type(&os, &os_alloc, &os_offset, &si);
+	zbx_free(nodename_csname);
+	zbx_free(release_version);
+	zbx_free(version_caption);
+	zbx_free(version_csdversion);
+	zbx_free(machine_osarchitecture);
 
 	SET_STR_RESULT(result, os);
 
