9ebb16bc780e4e8d977843a0c2ff6eecdca8f163
viktors <viktors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-02-08 07:52:22 +0000

    ...G...... [ZBX-9839] improved retrieving the time zone value
    git-svn-id: svn://svn.zabbix.com/branches/dev@58294 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9839/src/libs/zbxsysinfo/common/system.c
+++ ZBX-9839/src/libs/zbxsysinfo/common/system.c
@@ -70,7 +70,8 @@ int	SYSTEM_LOCALTIME(AGENT_REQUEST *request, AGENT_RESULT *result)
 		gmtoff = tm->tm_gmtoff;
 #else
 #ifdef _WINDOWS
-		gmtoff = -_timezone;
+		_get_timezone(&tv.timezone);
+		gmtoff = -tv.timezone;
 #else
 		gmtoff = -timezone;
 #endif
