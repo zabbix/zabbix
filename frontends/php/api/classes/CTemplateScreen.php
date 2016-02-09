c20c0300a7cf28629add4efd52cfc9c0f2f0eac8
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-02-09 15:42:24 +0000

    A......... [ZBX-10358] fixed error message in template screen cloning
    git-svn-id: svn://svn.zabbix.com/branches/2.0@58360 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/api/classes/CTemplateScreen.php
+++ frontends/php/api/classes/CTemplateScreen.php
@@ -815,7 +815,7 @@ class CTemplateScreen extends CScreen {
 		));
 		foreach ($existingScreens as $existingScreen) {
 			$template = DBfetch(DBselect('SELECT h.name FROM hosts h WHERE h.hostid='.$existingScreen['templateid']));
-			self::exception(ZBX_API_ERROR_PARAMETERS, _s('Screen "%1$s" already exists on template "%2$2".',
+			self::exception(ZBX_API_ERROR_PARAMETERS, _s('Screen "%1$s" already exists on template "%2$s".',
 				$existingScreen['name'], $template['name']));
 		}
 	}
