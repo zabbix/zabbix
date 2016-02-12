50754274a0bec766593b21fdf027fb1b5fd4f89d
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-02-12 08:50:07 +0000

    ..F....... [ZBX-10319] fixed graphs consuming 100% CPU when given incorrect "stime" parameter in URL
    git-svn-id: svn://svn.zabbix.com/branches/2.4@58407 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/classes/screens/CScreenBase.php
+++ frontends/php/include/classes/screens/CScreenBase.php
@@ -364,7 +364,7 @@ class CScreenBase {
 			$stimeUnix = zbxDateToTime($options['stime']);
 
 			if ($stimeUnix > $time || zbxAddSecondsToUnixtime($options['period'], $stimeUnix) > $time) {
-				$stimeNow = $options['stime'];
+				$stimeNow = zbxAddSecondsToUnixtime(SEC_PER_YEAR, $options['stime']);
 				$options['stime'] = date(TIMESTAMP_FORMAT, $time - $options['period']);
 				$usertime = date(TIMESTAMP_FORMAT, $time);
 				$isNow = 1;
@@ -412,8 +412,10 @@ class CScreenBase {
 
 		return array(
 			'period' => $options['period'],
-			'stime' => $options['stime'],
-			'stimeNow' => !empty($stimeNow) ? $stimeNow : $options['stime'],
+			'stime' => date(TIMESTAMP_FORMAT, zbxDateToTime($options['stime'])),
+			'stimeNow' => ($stimeNow === null)
+				? date(TIMESTAMP_FORMAT, zbxAddSecondsToUnixtime(SEC_PER_YEAR, $options['stime']))
+				: $stimeNow,
 			'starttime' => date(TIMESTAMP_FORMAT, $time - ZBX_MAX_PERIOD),
 			'usertime' => $usertime,
 			'isNow' => $isNow
