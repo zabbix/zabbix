18cd4cf9ab0139093e671a9a4e69c9c3097c702a
vitalijs <vitalijs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-02-16 14:41:26 +0000

    ..F....... [ZBX-9953] reverted unnecessary changes in CLocalApiClient.php
    git-svn-id: svn://svn.zabbix.com/branches/dev@58500 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9953/frontends/php/include/classes/api/clients/CLocalApiClient.php
+++ ZBX-9953/frontends/php/include/classes/api/clients/CLocalApiClient.php
@@ -76,20 +76,17 @@ class CLocalApiClient extends CApiClient {
 		// check that no authentication token is passed to methods that don't require it
 		if (!$requiresAuthentication && $auth !== null) {
 			$response->errorCode = ZBX_API_ERROR_PARAMETERS;
-			$response->errorMessage = _s(
-				'The "%1$s.%2$s" method must be called without the "auth" parameter.',
+			$response->errorMessage = _s('The "%1$s.%2$s" method must be called without the "auth" parameter.',
 				$requestApi, $requestMethod
 			);
 
 			return $response;
 		}
 
-		// check for incompatibile API options countOutput, sortfield usage
+		// Check for incompatibile API options countOutput, sortfield usage.
 		if (array_key_exists('countOutput', $params) &&  array_key_exists('sortfield', $params)) {
 			$response->errorCode = ZBX_API_ERROR_INTERNAL;
-			$response->errorMessage = _s('Incompatible options "%1$s", "%2$s" used.',
-				'countOutput', 'sortfield'
-			);
+			$response->errorMessage = _s('Incompatible options "%1$s", "%2$s" used.', 'countOutput', 'sortfield');
 
 			return $response;
 		}
@@ -122,10 +119,8 @@ class CLocalApiClient extends CApiClient {
 		}
 		catch (Exception $e) {
 			if ($newTransaction) {
-				/*
-				 * If we're calling user.login and authentication failed - commit the transaction to
-				 * save the failed attempt data
-				 */
+				// if we're calling user.login and authentication failed - commit the transaction to save the
+				// failed attempt data
 				if ($api === 'user' && $method === 'login') {
 					DBend(true);
 				}
