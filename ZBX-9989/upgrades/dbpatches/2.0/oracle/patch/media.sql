47a4cae236a25320b975a1a5dc760175aacd321f
gunars <gunars@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-02-16 13:20:01 +0000

    .......... [ZBX-9989] removed dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@58494 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9989/upgrades/dbpatches/2.0/oracle/patch/media.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE media MODIFY mediaid DEFAULT NULL;
-ALTER TABLE media MODIFY userid DEFAULT NULL;
-ALTER TABLE media MODIFY mediatypeid DEFAULT NULL;
-ALTER TABLE media MODIFY period DEFAULT '1-7,00:00-24:00';
-DELETE FROM media WHERE NOT userid IN (SELECT userid FROM users);
-DELETE FROM media WHERE NOT mediatypeid IN (SELECT mediatypeid FROM media_type);
-ALTER TABLE media ADD CONSTRAINT c_media_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
-ALTER TABLE media ADD CONSTRAINT c_media_2 FOREIGN KEY (mediatypeid) REFERENCES media_type (mediatypeid) ON DELETE CASCADE;
