e996278d6e8eef1538f9b912ffa612598e2e4409
gleb <gleb@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-02-16 14:52:10 +0000

    ....I..PS. [ZBX-4148] isolated places where libnetsnmp headers are included
    git-svn-id: svn://svn.zabbix.com/branches/dev@58501 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-4148/src/zabbix_server/poller/checks_snmp.c
+++ ZBX-4148/src/zabbix_server/poller/checks_snmp.c
@@ -18,6 +18,14 @@
 **/
 
 #include "checks_snmp.h"
+
+/* Net-SNMP is used */
+#ifdef HAVE_NETSNMP
+#	define SNMP_NO_DEBUGGING		/* disabling debugging messages from Net-SNMP library */
+#	include <net-snmp/net-snmp-config.h>
+#	include <net-snmp/net-snmp-includes.h>
+#endif
+
 #include "comms.h"
 #include "zbxalgo.h"
 #include "zbxjson.h"
