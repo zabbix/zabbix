fd9feb71c554cc392436f88f241ea5edf389c2c6
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-02-19 13:19:19 +0000

    .......... [ZBXNEXT-826] undoing changes in r58609
    git-svn-id: svn://svn.zabbix.com/trunk@58610 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,7 +22,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	public static final String REVISION_DATE = "19 February 2016";
+	public static final String REVISION_DATE = "15 February 2016";
 	public static final String REVISION = "{ZABBIX_REVISION}";
 	public static final String VERSION = "3.1.0";
 
