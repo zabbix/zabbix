0d1133af75f46420974f8e7b531cc92cc2e4615a
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-02-19 12:41:01 +0000

    .......... [ZBXNEXT-3135] merged from upstream
    git-svn-id: svn://svn.zabbix.com/branches/dev@58601 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-3135/src/libs/zbxdb/db.c
+++ ZBXNEXT-3135/src/libs/zbxdb/db.c
@@ -271,6 +271,13 @@ int	zbx_db_connect(char *host, char *user, char *password, char *dbname, char *d
 		ret = ZBX_DB_FAIL;
 	}
 
+	/* set codepage to utf-8 */
+	if (ZBX_DB_OK == ret && SUCCEED != zbx_ibm_db2_success(SQLSetConnectAttr(ibm_db2.hdbc, SQL_ATTR_CLIENT_CODEPAGE,
+			(SQLPOINTER)(SQLUINTEGER)1208, SQL_IS_UINTEGER)))
+	{
+		ret = ZBX_DB_FAIL;
+	}
+
 	/* connect to the database */
 	if (ZBX_DB_OK == ret && SUCCEED != zbx_ibm_db2_success(SQLDriverConnect(ibm_db2.hdbc, NULL, (SQLCHAR *)connect,
 			SQL_NTS, NULL, 0, NULL, SQL_DRIVER_NOPROMPT)))
