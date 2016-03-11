5011b1c53898439ca7b566ec3a1a60a975132198
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-03-11 09:13:11 +0000

    .......... [ZBX-10372] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@58961 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10372/upgrades/dbpatches/1.4/mysql/patch/history_str_sync.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE history_str_sync (
-	id		serial			,
-	nodeid		bigint unsigned		DEFAULT '0'	NOT NULL,
-	itemid		bigint unsigned		DEFAULT '0'	NOT NULL,
-	clock		integer		DEFAULT '0'	NOT NULL,
-	value		varchar(255)		DEFAULT ''	NOT NULL,
-	PRIMARY KEY (id)
-) ENGINE=InnoDB;
-CREATE INDEX history_str_sync_1 on history_str_sync (nodeid,id);
