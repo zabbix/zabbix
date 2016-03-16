fc8696b79eebb1f205b19311caaf36108c5430d8
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-03-16 12:09:02 +0000

    ...G...PS. [ZBXNEXT-1263] fixing (143): moved DCget_psk_by_identity() and DCcheck_proxy_permissions() from src/libs/zbxcrypto/tls_tcp_active.h to include/dbcache.h
    git-svn-id: svn://svn.zabbix.com/branches/dev@59043 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1263-5/src/libs/zbxcrypto/tls_tcp_active.h
+++ ZBXNEXT-1263-5/src/libs/zbxcrypto/tls_tcp_active.h
@@ -36,9 +36,5 @@ int		zbx_check_server_issuer_subject(zbx_socket_t *sock, char **error);
 #endif
 
 const char	*zbx_tls_connection_type_name(unsigned int type);
-int		DCcheck_proxy_permissions(const char *host, const zbx_socket_t *sock, zbx_uint64_t *hostid,
-		char **error);
-size_t		DCget_psk_by_identity(const unsigned char *psk_identity, unsigned char *psk_buf, size_t psk_buf_len);
-
 
 #endif	/* ZABBIX_TLS_TCP_ACTIVE_H */
