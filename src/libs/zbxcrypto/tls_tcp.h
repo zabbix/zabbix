a8561a40069a854025c0cc2617b22bd8de23117a
gleb <gleb@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-03-22 12:52:42 +0000

    ...GI..PS. [ZBX-4148] separated third-party library CFLAGS to avoid header file name conflicts
    git-svn-id: svn://svn.zabbix.com/trunk@59083 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxcrypto/tls_tcp.h
+++ src/libs/zbxcrypto/tls_tcp.h
@@ -21,8 +21,10 @@
 #define ZABBIX_TLS_TCP_H
 
 #if defined(HAVE_POLARSSL) || defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
-int	zbx_tls_connect(zbx_socket_t *s, char **error, unsigned int tls_connect, char *tls_arg1, char *tls_arg2);
-int	zbx_tls_accept(zbx_socket_t *s, char **error, unsigned int tls_accept);
+int	zbx_tls_connect(zbx_socket_t *s, unsigned int tls_connect, char *tls_arg1, char *tls_arg2, char **error);
+int	zbx_tls_accept(zbx_socket_t *s, unsigned int tls_accept, char **error);
+ssize_t	zbx_tls_write(zbx_socket_t *s, const char *buf, size_t len, char **error);
+ssize_t	zbx_tls_read(zbx_socket_t *s, char *buf, size_t len, char **error);
 void	zbx_tls_close(zbx_socket_t *s);
 #endif
 
