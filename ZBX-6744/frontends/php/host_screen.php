86f7b93f4716b3e74433d19fe5792ddabbecc9e9
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-03-27 19:30:48 +0000

    .......... [ZBX-6744] created a development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@59137 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-6744/frontends/php/host_screen.php
@@ -0,0 +1,151 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+require_once dirname(__FILE__).'/include/config.inc.php';
+require_once dirname(__FILE__).'/include/graphs.inc.php';
+require_once dirname(__FILE__).'/include/screens.inc.php';
+require_once dirname(__FILE__).'/include/blocks.inc.php';
+
+$page['title'] = _('Host screens');
+$page['file'] = 'screens.php';
+$page['hist_arg'] = array('elementid');
+$page['scripts'] = array('effects.js', 'dragdrop.js', 'class.calendar.js', 'gtlc.js', 'flickerfreescreen.js');
+$page['type'] = detect_page_type(PAGE_TYPE_HTML);
+
+define('ZBX_PAGE_DO_JS_REFRESH', 1);
+
+require_once dirname(__FILE__).'/include/page_header.php';
+
+// VAR	TYPE	OPTIONAL	FLAGS	VALIDATION	EXCEPTION
+$fields = array(
+	'hostid' =>		array(T_ZBX_INT, O_OPT, P_SYS, DB_ID,		null),
+	'tr_groupid' =>	array(T_ZBX_INT, O_OPT, P_SYS, DB_ID,		null),
+	'tr_hostid' =>	array(T_ZBX_INT, O_OPT, P_SYS, DB_ID,		null),
+	'screenid' =>	array(T_ZBX_INT, O_OPT, P_SYS|P_NZERO, DB_ID, null),
+	'step' =>		array(T_ZBX_INT, O_OPT, P_SYS, BETWEEN(0, 65535), null),
+	'period' =>		array(T_ZBX_INT, O_OPT, P_SYS, null,		null),
+	'stime' =>		array(T_ZBX_STR, O_OPT, P_SYS, null,		null),
+	'reset' =>		array(T_ZBX_STR, O_OPT, P_SYS, IN("'reset'"), null),
+	'fullscreen' =>	array(T_ZBX_INT, O_OPT, P_SYS, IN('0,1'),	null),
+	// ajax
+	'favobj' =>		array(T_ZBX_STR, O_OPT, P_ACT, null,		null),
+	'favref' =>		array(T_ZBX_STR, O_OPT, P_ACT, NOT_EMPTY,	null),
+	'favid' =>		array(T_ZBX_INT, O_OPT, P_ACT, null,		null),
+	'favaction' =>	array(T_ZBX_STR, O_OPT, P_ACT, IN("'add','remove','flop'"), null),
+	'favstate' =>	array(T_ZBX_INT, O_OPT, P_ACT, NOT_EMPTY,	null)
+);
+check_fields($fields);
+
+/*
+ * Ajax
+ */
+if (isset($_REQUEST['favobj'])) {
+	if ($_REQUEST['favobj'] == 'hat') {
+		CProfile::update('web.hostscreen.hats.'.$_REQUEST['favref'].'.state', $_REQUEST['favstate'], PROFILE_TYPE_INT);
+	}
+
+	if ($_REQUEST['favobj'] == 'filter') {
+		CProfile::update('web.hostscreen.filter.state', $_REQUEST['favstate'], PROFILE_TYPE_INT);
+	}
+
+	if ($_REQUEST['favobj'] == 'timeline') {
+		if (isset($_REQUEST['elementid']) && isset($_REQUEST['period'])) {
+			navigation_bar_calc('web.hostscreen', $_REQUEST['elementid'], true);
+		}
+	}
+
+	if (str_in_array($_REQUEST['favobj'], array('screenid', 'slideshowid'))) {
+		$result = false;
+		if ($_REQUEST['favaction'] == 'add') {
+			$result = add2favorites('web.favorite.screenids',$_REQUEST['favid'], $_REQUEST['favobj']);
+			if ($result) {
+				echo '$("addrm_fav").title = "'._('Remove from favourites').'";'."\n";
+				echo '$("addrm_fav").onclick = function() { rm4favorites("'.$_REQUEST['favobj'].'", "'.$_REQUEST['favid'].'", 0); }'."\n";
+			}
+		}
+		elseif ($_REQUEST['favaction'] == 'remove') {
+			$result = rm4favorites('web.favorite.screenids', $_REQUEST['favid'], $_REQUEST['favobj']);
+
+			if ($result) {
+				echo '$("addrm_fav").title = "'._('Add to favourites').'";'."\n";
+				echo '$("addrm_fav").onclick = function() { add2favorites("'.$_REQUEST['favobj'].'", "'.$_REQUEST['favid'].'"); }'."\n";
+			}
+		}
+
+		if ($page['type'] == PAGE_TYPE_JS && $result) {
+			echo 'switchElementsClass("addrm_fav", "iconminus", "iconplus");';
+		}
+	}
+}
+
+if ($page['type'] == PAGE_TYPE_JS || $page['type'] == PAGE_TYPE_HTML_BLOCK) {
+	require_once dirname(__FILE__).'/include/page_footer.php';
+	exit();
+}
+
+/*
+ * Display
+ */
+$data = array(
+	'hostid' => get_request('hostid', 0),
+	'fullscreen' => get_request('fullscreen', 0),
+	'screenid' => get_request('screenid', CProfile::get('web.hostscreen.screenid', null)),
+	'period' => get_request('period'),
+	'stime' => get_request('stime')
+);
+CProfile::update('web.hostscreen.screenid', $data['screenid'], PROFILE_TYPE_ID);
+
+// get screen list
+$data['screens'] = API::TemplateScreen()->get(array(
+	'hostids' => $data['hostid'],
+	'output' => API_OUTPUT_EXTEND
+));
+$data['screens'] = zbx_toHash($data['screens'], 'screenid');
+order_result($data['screens'], 'name');
+
+// get screen
+$screenid = null;
+if (!empty($data['screens'])) {
+	$screen = !isset($data['screens'][$data['screenid']]) ? reset($data['screens']) : $data['screens'][$data['screenid']];
+	if (!empty($screen['screenid'])) {
+		$screenid = $screen['screenid'];
+	}
+}
+
+$data['screen'] = API::TemplateScreen()->get(array(
+	'screenids' => $screenid,
+	'hostids' => $data['hostid'],
+	'output' => API_OUTPUT_EXTEND,
+	'selectScreenItems' => API_OUTPUT_EXTEND
+));
+$data['screen'] = reset($data['screen']);
+
+// get host
+if (!empty($data['screen']['hostid'])) {
+	$data['host'] = get_host_by_hostid($data['screen']['hostid']);
+}
+
+// render view
+$screenView = new CView('monitoring.hostscreen', $data);
+$screenView->render();
+$screenView->show();
+
+require_once dirname(__FILE__).'/include/page_footer.php';
---
+++ ZBX-6744/frontends/php/include/classes/validators/CTriggerFunctionValidator.php
@@ -0,0 +1,325 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+class CTriggerFunctionValidator extends CValidator {
+	/**
+	 * The array containing valid functions and parameres to them
+	 *
+	 * Structure: array(
+	 *   '<function>' => array(
+	 *     'args' => array(
+	 *       array('type' => '<parameter_type>'[, 'mandat' => bool]),
+	 *       ...
+	 *     ),
+	 *     'value_types' => array(<value_type>, <value_type>, ...)
+	 *   )
+	 * )
+	 *
+	 * <parameter_type> can be 'sec', 'sec_num' or 'str'
+	 * <value_type> can be one of ITEM_VALUE_TYPE_*
+	 *
+	 * @var array
+	 */
+	private $allowed;
+
+	/**
+	 * Validate trigger function like last(0), time(), etc.
+	 * Examples:
+	 *   array('functionName' => 'last', 'functionParamList' => array(0 => '#15'), 'valueType' => 3)
+	 *
+	 * @param array $value
+	 * @param string $value['functionName']
+	 * @param array $value['functionParamList']
+	 * @param int $value['valueType']
+	 *
+	 * @return bool
+	 */
+	public function validate($value) {
+		$this->setError('');
+
+		if (!isset($this->allowed[$value['functionName']])) {
+			$this->setError(_s('Incorrect trigger function "%1$s" provided in expression.', $value['functionName']).' '.
+					_('Unknown function.'));
+			return false;
+		}
+
+		if (!isset($this->allowed[$value['functionName']]['value_types'][$value['valueType']])) {
+			$this->setError(_s('Incorrect item value type "%1$s:%2$s" provided for trigger function "%3$s".',
+					'', itemValueTypeString($value['valueType']), $value['functionName']));
+			return false;
+		}
+
+		if (count($this->allowed[$value['functionName']]['args']) < count($value['functionParamList'])) {
+			$this->setError(_s('Incorrect trigger function "%1$s" provided in expression.', $value['functionName']).' '.
+					_s('Function supports "%1$s" parameters.', count($this->allowed[$value['functionName']]['args'])));
+			return false;
+		}
+
+		foreach ($this->allowed[$value['functionName']]['args'] as $aNum => $arg) {
+			// mandatory check
+			if (isset($arg['mandat']) && $arg['mandat'] && !isset($value['functionParamList'][$aNum])) {
+				$this->setError(_s('Incorrect trigger function "%1$s" provided in expression.', $value['functionName']).' '.
+						_('Mandatory parameter is missing.'));
+				return false;
+			}
+
+			// type check
+			if (isset($arg['type']) && isset($value['functionParamList'][$aNum])) {
+				$userMacro = preg_match('/^'.ZBX_PREG_EXPRESSION_USER_MACROS.'$/', $value['functionParamList'][$aNum]);
+
+				if (!$userMacro) {
+					switch ($arg['type']) {
+						case 'str':
+							if (!is_string($value['functionParamList'][$aNum])) {
+								$this->setError(_s('Incorrect trigger function "%1$s" provided in expression.', $value['functionName']).' '.
+										_s('Parameter of type string or user macro expected, "%1$s" given.', $value['functionParamList'][$aNum]));
+								return false;
+							}
+							break;
+						case 'sec':
+							if (!$this->validateSec($value['functionParamList'][$aNum])) {
+								$this->setError(_s('Incorrect trigger function "%1$s" provided in expression.', $value['functionName']).' '.
+										_s('Parameter sec or user macro expected, "%1$s" given.', $value['functionParamList'][$aNum]));
+								return false;
+							}
+							break;
+						case 'sec_num':
+							if (!$this->validateSecNum($value['functionParamList'][$aNum])) {
+								$this->setError(_s('Incorrect trigger function "%1$s" provided in expression.', $value['functionName']).' '.
+										_s('Parameter sec or #num or user macro expected, "%1$s" given.', $value['functionParamList'][$aNum]));
+								return false;
+							}
+							break;
+						case 'num':
+							if (!is_numeric($value['functionParamList'][$aNum])) {
+								$this->setError(_s('Incorrect trigger function "%1$s" provided in expression.', $value['functionName']).' '.
+										_s('Parameter num or user macro expected, "%1$s" given.', $value['functionParamList'][$aNum]));
+								return false;
+							}
+							break;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Validate trigger function parameter which can contain only seconds
+	 * Examples:
+	 *   5
+	 *   1w
+	 *
+	 * @param string $param
+	 *
+	 * @return bool
+	 */
+	private function validateSec($param) {
+		return preg_match('/^\d+['.ZBX_TIME_SUFFIXES.']{0,1}$/', $param) == 1;
+	}
+
+	/**
+	 * Validate trigger function parameter which can contain seconds or count
+	 * Examples:
+	 *   5
+	 *   1w
+	 *   #5
+	 *
+	 * @param string $param
+	 *
+	 * @return bool
+	 */
+	private function validateSecNum($param) {
+		if (preg_match('/^#\d+$/', $param)) {
+			return true;
+		}
+		return $this->validateSec($param);
+	}
+
+	protected function initOptions() {
+		$valueTypesAll = array(
+			ITEM_VALUE_TYPE_FLOAT => true,
+			ITEM_VALUE_TYPE_UINT64 => true,
+			ITEM_VALUE_TYPE_STR => true,
+			ITEM_VALUE_TYPE_TEXT => true,
+			ITEM_VALUE_TYPE_LOG => true
+		);
+		$valueTypesNum = array(
+			ITEM_VALUE_TYPE_FLOAT => true,
+			ITEM_VALUE_TYPE_UINT64 => true
+		);
+		$valueTypesChar = array(
+			ITEM_VALUE_TYPE_STR => true,
+			ITEM_VALUE_TYPE_TEXT => true,
+			ITEM_VALUE_TYPE_LOG => true
+		);
+		$valueTypesLog = array(
+			ITEM_VALUE_TYPE_LOG => true
+		);
+
+		$argsIgnored = array(array('type' => 'str'));
+
+		$this->allowed = array(
+			'abschange' => array(
+				'args' => $argsIgnored,
+				'value_types' => $valueTypesAll
+			),
+			'avg' => array(
+				'args' => array(
+					array('type' => 'sec_num', 'mandat' => true),
+					array('type' => 'sec')
+				),
+				'value_types' => $valueTypesNum
+			),
+			'change' => array(
+				'args' => $argsIgnored,
+				'value_types' => $valueTypesAll
+			),
+			'count' => array(
+				'args' => array(
+					array('type' => 'sec_num', 'mandat' => true),
+					array('type' => 'str'),
+					array('type' => 'str'),
+					array('type' => 'sec')
+				),
+				'value_types' => $valueTypesAll
+			),
+			'date' => array(
+				'args' => $argsIgnored,
+				'value_types' => $valueTypesAll
+			),
+			'dayofmonth' => array(
+				'args' => $argsIgnored,
+				'value_types' => $valueTypesAll
+			),
+			'dayofweek' => array(
+				'args' => $argsIgnored,
+				'value_types' => $valueTypesAll
+			),
+			'delta' => array(
+				'args' => array(
+					array('type' => 'sec_num', 'mandat' => true),
+					array('type' => 'sec')
+				),
+				'value_types' => $valueTypesNum
+			),
+			'diff' => array(
+				'args' => $argsIgnored,
+				'value_types' => $valueTypesAll
+			),
+			'fuzzytime' => array(
+				'args' => array(
+					array('type' => 'sec', 'mandat' => true)
+				),
+				'value_types' => $valueTypesNum
+			),
+			'iregexp' => array(
+				'args' => array(
+					array('type' => 'str', 'mandat' => true),
+					array('type' => 'sec_num')
+				),
+				'value_types' => $valueTypesChar
+			),
+			'last' => array(
+				'args' => array(
+					array('type' => 'sec_num', 'mandat' => true),
+					array('type' => 'sec')
+				),
+				'value_types' => $valueTypesAll
+			),
+			'logeventid' => array(
+				'args' => array(
+					array('type' => 'str', 'mandat' => true)
+				),
+				'value_types' => $valueTypesLog
+			),
+			'logseverity' => array(
+				'args' => $argsIgnored,
+				'value_types' => $valueTypesLog
+			),
+			'logsource' => array(
+				'args' => array(
+					array('type' => 'str', 'mandat' => true)
+				),
+				'value_types' => $valueTypesLog
+			),
+			'max' => array(
+				'args' => array(
+					array('type' => 'sec_num', 'mandat' => true),
+					array('type' => 'sec')
+				),
+				'value_types' => $valueTypesNum
+			),
+			'min' => array(
+				'args' => array(
+					array('type' => 'sec_num', 'mandat' => true),
+					array('type' => 'sec')
+				),
+				'value_types' => $valueTypesNum
+			),
+			'nodata'=> array(
+				'args' => array(
+					array('type' => 'sec', 'mandat' => true)
+				),
+				'value_types' => $valueTypesAll
+			),
+			'now' => array(
+				'args' => $argsIgnored,
+				'value_types' => $valueTypesAll
+			),
+			'prev' => array(
+				'args' => $argsIgnored,
+				'value_types' => $valueTypesAll
+			),
+			'regexp' => array(
+				'args' => array(
+					array('type' => 'str', 'mandat' => true),
+					array('type' => 'sec_num')
+				),
+				'value_types' => $valueTypesChar
+			),
+			'str' => array(
+				'args' => array(
+					array('type' => 'str', 'mandat' => true),
+					array('type' => 'sec_num')
+				),
+				'value_types' => $valueTypesChar
+			),
+			'strlen' => array(
+				'args' => array(
+					array('type' => 'sec_num', 'mandat' => true),
+					array('type' => 'sec')
+				),
+				'value_types' => $valueTypesChar
+			),
+			'sum' => array(
+				'args' => array(
+					array('type' => 'sec_num', 'mandat' => true),
+					array('type' => 'sec')
+				),
+				'value_types' => $valueTypesNum
+			),
+			'time' => array(
+				'args' => $argsIgnored,
+				'value_types' => $valueTypesAll
+			)
+		);
+	}
+}
