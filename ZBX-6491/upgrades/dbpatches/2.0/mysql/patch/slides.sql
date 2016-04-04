fd893712c33629f3f7f0590592f278395ee0c20f
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-04-04 05:50:26 +0000

    .......... [ZBX-6491] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@59248 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-6491/upgrades/dbpatches/2.0/mysql/patch/slides.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE slides MODIFY slideid bigint unsigned NOT NULL,
-		   MODIFY slideshowid bigint unsigned NOT NULL,
-		   MODIFY screenid bigint unsigned NOT NULL;
-DELETE FROM slides WHERE NOT slideshowid IN (SELECT slideshowid FROM slideshows);
-DELETE FROM slides WHERE NOT screenid IN (SELECT screenid FROM screens);
-ALTER TABLE slides ADD CONSTRAINT c_slides_1 FOREIGN KEY (slideshowid) REFERENCES slideshows (slideshowid) ON DELETE CASCADE;
-ALTER TABLE slides ADD CONSTRAINT c_slides_2 FOREIGN KEY (screenid) REFERENCES screens (screenid) ON DELETE CASCADE;
--- ZBX-6491/upgrades/dbpatches/2.0/oracle/patch/media_type.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE media_type MODIFY mediatypeid DEFAULT NULL;
-ALTER TABLE media_type ADD status number(10) DEFAULT '0' NOT NULL;
