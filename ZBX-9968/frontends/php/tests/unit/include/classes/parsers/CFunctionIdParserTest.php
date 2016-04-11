191bb9c7c71e343c719e332e61f4b4c3d6ccba44
gunars <gunars@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-04-11 07:29:33 +0000

    .......... [ZBX-9968] created new dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@59368 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-9968/frontends/php/tests/unit/include/classes/parsers/CFunctionIdParserTest.php
@@ -0,0 +1,42 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CFunctionIdParserTest extends CParserTest {
+
+	protected function getParser() {
+		return new CFunctionIdParser();
+	}
+
+	public function testProvider() {
+		return [
+			['{1}', 0, CParser::PARSE_SUCCESS, '{1}'],
+			['{12345}', 0, CParser::PARSE_SUCCESS, '{12345}'],
+			['{9223372036854775807} = 0', 0, CParser::PARSE_SUCCESS_CONT, '{9223372036854775807}'],
+			['not {34356} = 0', 4, CParser::PARSE_SUCCESS_CONT, '{34356}'],
+
+			['', 0, CParser::PARSE_FAIL, ''],
+			['1', 0, CParser::PARSE_FAIL, ''],
+			['{2Q', 0, CParser::PARSE_FAIL, ''],
+			['{0}', 0, CParser::PARSE_FAIL, ''],
+			['{9223372036854775808}', 0, CParser::PARSE_FAIL, '']
+		];
+	}
+}
