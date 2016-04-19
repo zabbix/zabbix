2b5716e933816771414dc7555f7631e6efba1c19
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-04-19 05:52:50 +0000

    A......... [ZBXNEXT-2118] undoing changes in C30XmlValidator
    git-svn-id: svn://svn.zabbix.com/branches/dev@59477 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2118/frontends/php/include/classes/import/validators/C30XmlValidator.php
+++ ZBXNEXT-2118/frontends/php/include/classes/import/validators/C30XmlValidator.php
@@ -244,9 +244,7 @@ class C30XmlValidator {
 											'name' =>					['type' => XML_STRING | XML_REQUIRED],
 											'expression' =>				['type' => XML_STRING | XML_REQUIRED]
 										]]
-									]],
-									'recovery_mode' =>			['type' => XML_STRING],
-									'recovery_expression' =>	['type' => XML_STRING]
+									]]
 								]]
 							]],
 							'graph_prototypes' =>		['type' => XML_INDEXED_ARRAY | XML_REQUIRED, 'prefix' => 'graph_prototype', 'rules' => [
@@ -682,9 +680,7 @@ class C30XmlValidator {
 							'name' =>					['type' => XML_STRING | XML_REQUIRED],
 							'expression' =>				['type' => XML_STRING | XML_REQUIRED]
 						]]
-					]],
-					'recovery_mode' =>			['type' => XML_STRING],
-					'recovery_expression' =>	['type' => XML_STRING]
+					]]
 				]]
 			]],
 			'graphs' =>					['type' => XML_INDEXED_ARRAY, 'prefix' => 'graph', 'rules' => [
