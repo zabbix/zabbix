c5af9106a52332ad605d795eb014554192890718
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-04-20 08:01:28 +0000

    .......... [ZBXNEXT-826] updated release date and version [2.2.12]
    git-svn-id: svn://svn.zabbix.com/branches/2.2@59513 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	public static final String REVISION_DATE = "11 April 2016";
+	public static final String REVISION_DATE = "20 April 2016";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "2.2.12rc1";
+	public static final String VERSION = "2.2.12";
 
 	public static void printVersion()
 	{
