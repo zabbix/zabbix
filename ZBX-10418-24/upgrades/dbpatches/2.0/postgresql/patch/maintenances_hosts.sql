502102a9e015f1a6d39b8bb8763e4c7509b6f3b4
zalex <zalex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-04-21 16:14:09 +0000

    .......... [ZBX-10418] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@59632 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10418-24/upgrades/dbpatches/2.0/postgresql/patch/maintenances_hosts.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE ONLY maintenances_hosts ALTER maintenance_hostid DROP DEFAULT,
-				    ALTER maintenanceid DROP DEFAULT,
-				    ALTER hostid DROP DEFAULT;
-DROP INDEX maintenances_hosts_1;
-DELETE FROM maintenances_hosts WHERE NOT EXISTS (SELECT 1 FROM maintenances WHERE maintenances.maintenanceid=maintenances_hosts.maintenanceid);
-DELETE FROM maintenances_hosts WHERE NOT EXISTS (SELECT 1 FROM hosts WHERE hosts.hostid=maintenances_hosts.hostid);
-CREATE UNIQUE INDEX maintenances_hosts_1 ON maintenances_hosts (maintenanceid,hostid);
-ALTER TABLE ONLY maintenances_hosts ADD CONSTRAINT c_maintenances_hosts_1 FOREIGN KEY (maintenanceid) REFERENCES maintenances (maintenanceid) ON DELETE CASCADE;
-ALTER TABLE ONLY maintenances_hosts ADD CONSTRAINT c_maintenances_hosts_2 FOREIGN KEY (hostid) REFERENCES hosts (hostid) ON DELETE CASCADE;
