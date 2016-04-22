3735e9a5772ad902137fa0eb921c3210a4553d36
Viktors Tjarve <viktors.tjarve@zabbix.com>
2016-04-22 10:36:59 +0000

    ...G...... [ZBX-9839] improved the work of zbx_get_time(); fixed value check for timestamps

--- include/common.h
+++ include/common.h
@@ -111,8 +111,9 @@ zbx_timespec_t;
 /* time zone offset */
 typedef struct zbx_tz_offset
 {
-	short	hours;
-	short	minutes;
+	int	hours;
+	int	minutes;
+	char	sign;
 }
 zbx_tz_offset_t;
 
@@ -1117,7 +1118,7 @@ int	parse_serveractive_element(char *str, char **host, unsigned short *port, uns
 
 void	zbx_get_time(struct tm *tm, long *milliseconds, zbx_tz_offset_t *tz_offset);
 
-int	zbx_utc_time(int tm_year, int tm_mon, int tm_mday, int tm_hour, int tm_min, int tm_sec);
+int	zbx_utc_time(int year, int mon, int mday, int hour, int min, int sec);
 
 int	zbx_strcmp_null(const char *s1, const char *s2);
 
