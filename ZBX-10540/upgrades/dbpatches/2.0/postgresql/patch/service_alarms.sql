0437e65dd5fc10b6152eb868ee1c0d21ae8d6fa5
gleb <gleb@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-04-25 08:21:38 +0000

    .......... [ZBX-10540] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@59667 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10540/upgrades/dbpatches/2.0/postgresql/patch/service_alarms.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ONLY service_alarms ALTER servicealarmid DROP DEFAULT,
-				ALTER serviceid DROP DEFAULT;
-DELETE FROM service_alarms WHERE NOT EXISTS (SELECT 1 FROM services WHERE services.serviceid=service_alarms.serviceid);
-ALTER TABLE ONLY service_alarms ADD CONSTRAINT c_service_alarms_1 FOREIGN KEY (serviceid) REFERENCES services (serviceid) ON DELETE CASCADE;
