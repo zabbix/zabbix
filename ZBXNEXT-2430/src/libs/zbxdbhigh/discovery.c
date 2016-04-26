0c0ba71521b87c51578023f12f2207c1c41d6f7d
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-04-26 08:00:22 +0000

    .......... [ZBXNEXT-2430] updated to the latest trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@59711 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2430/src/libs/zbxdbhigh/discovery.c
+++ ZBXNEXT-2430/src/libs/zbxdbhigh/discovery.c
@@ -358,7 +358,7 @@ static void	discovery_update_service_status(const DB_DSERVICE *dservice, int sta
 		{
 			discovery_update_dservice(dservice->dserviceid, status, now, 0, value);
 			add_event(0, EVENT_SOURCE_DISCOVERY, EVENT_OBJECT_DSERVICE, dservice->dserviceid, &ts,
-					DOBJECT_STATUS_DISCOVER, NULL, NULL, 0, 0);
+					DOBJECT_STATUS_DISCOVER, NULL, NULL, NULL, 0, 0);
 		}
 		else if (0 != strcmp(dservice->value, value))
 		{
@@ -371,11 +371,11 @@ static void	discovery_update_service_status(const DB_DSERVICE *dservice, int sta
 		{
 			discovery_update_dservice(dservice->dserviceid, status, 0, now, dservice->value);
 			add_event(0, EVENT_SOURCE_DISCOVERY, EVENT_OBJECT_DSERVICE, dservice->dserviceid, &ts,
-					DOBJECT_STATUS_LOST, NULL, NULL, 0, 0);
+					DOBJECT_STATUS_LOST, NULL, NULL, NULL, 0, 0);
 		}
 	}
 	add_event(0, EVENT_SOURCE_DISCOVERY, EVENT_OBJECT_DSERVICE, dservice->dserviceid, &ts, status,
-			NULL, NULL, 0, 0);
+			NULL, NULL, NULL, 0, 0);
 
 	process_events();
 
@@ -420,7 +420,7 @@ static void	discovery_update_host_status(DB_DHOST *dhost, int status, int now)
 
 			discovery_update_dhost(dhost);
 			add_event(0, EVENT_SOURCE_DISCOVERY, EVENT_OBJECT_DHOST, dhost->dhostid, &ts,
-					DOBJECT_STATUS_DISCOVER, NULL, NULL, 0, 0);
+					DOBJECT_STATUS_DISCOVER, NULL, NULL, NULL, 0, 0);
 		}
 	}
 	else	/* DOBJECT_STATUS_DOWN */
@@ -433,10 +433,10 @@ static void	discovery_update_host_status(DB_DHOST *dhost, int status, int now)
 
 			discovery_update_dhost(dhost);
 			add_event(0, EVENT_SOURCE_DISCOVERY, EVENT_OBJECT_DHOST, dhost->dhostid, &ts,
-					DOBJECT_STATUS_LOST, NULL, NULL, 0, 0);
+					DOBJECT_STATUS_LOST, NULL, NULL, NULL, 0, 0);
 		}
 	}
-	add_event(0, EVENT_SOURCE_DISCOVERY, EVENT_OBJECT_DHOST, dhost->dhostid, &ts, status, NULL, NULL, 0, 0);
+	add_event(0, EVENT_SOURCE_DISCOVERY, EVENT_OBJECT_DHOST, dhost->dhostid, &ts, status, NULL, NULL, NULL, 0, 0);
 
 	process_events();
 }
