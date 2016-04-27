62739a19589caee3740e722cdee2fd1ba0b6710a
vitalijs <vitalijs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-04-27 12:33:28 +0000

    .......... [ZBX-10686] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@59759 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10686/upgrades/dbpatches/2.0/oracle/patch/sessions.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE sessions MODIFY userid DEFAULT NULL;
-DELETE FROM sessions WHERE NOT userid IN (SELECT userid FROM users);
-ALTER TABLE sessions ADD CONSTRAINT c_sessions_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
