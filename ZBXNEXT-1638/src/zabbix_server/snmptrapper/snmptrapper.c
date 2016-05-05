47d4b798769469c54929c3e0d953c6e52de0fbd0
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-05-05 07:09:43 +0000

    .......... [ZBXNEXT-1638] merged from upstream
    git-svn-id: svn://svn.zabbix.com/branches/dev@59881 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1638/src/zabbix_server/snmptrapper/snmptrapper.c
+++ ZBXNEXT-1638/src/zabbix_server/snmptrapper/snmptrapper.c
@@ -148,7 +148,7 @@ static int	process_trap_for_interface(zbx_uint64_t interfaceid, char *trap, zbx_
 				}
 			}
 
-			if (SUCCEED != regexp_match_ex(&regexps, trap, regex, ZBX_CASE_SENSITIVE))
+			if (ZBX_REGEXP_MATCH != regexp_match_ex(&regexps, trap, regex, ZBX_CASE_SENSITIVE))
 				goto next;
 		}
 
