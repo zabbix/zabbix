dcbc52c425a4836ed5fda9e12fdda27b035fd766
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-05-12 11:26:22 +0000

    ..F....... [ZBXNEXT-2087] fixed displaying of "Tags" table
    git-svn-id: svn://svn.zabbix.com/branches/dev@60082 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2087/frontends/php/include/views/configuration.trigger.prototype.edit.php
+++ ZBXNEXT-2087/frontends/php/include/views/configuration.trigger.prototype.edit.php
@@ -465,16 +465,13 @@ $triggersFormList
 	->addRow(_('Severity'), new CSeverity(['name' => 'priority', 'value' => (int) $data['priority']]));
 
 // tags
-$tags_table = (new CTable())
-	->setId('tbl_tags')
-	->addClass(ZBX_STYLE_TABLE_FORMS_SEPARATOR);
+$tags_table = (new CTable())->setId('tbl_tags');
 
 foreach ($data['tags'] as $tag_key => $tag) {
 	$tags_table->addRow([
 		(new CTextBox('tags['.$tag_key.'][tag]', $tag['tag'], false, 255))
 			->setWidth(ZBX_TEXTAREA_SMALL_WIDTH)
 			->setAttribute('placeholder', _('tag')),
-		'&nbsp;',
 		(new CTextBox('tags['.$tag_key.'][value]', $tag['value'], false, 255))
 			->setWidth(ZBX_TEXTAREA_SMALL_WIDTH)
 			->setAttribute('placeholder', _('value')),
@@ -491,7 +488,11 @@ $tags_table->setFooter(new CCol(
 		->addClass(ZBX_STYLE_BTN_LINK)
 		->addClass('element-table-add')
 ));
-$triggersFormList->addRow(_('Tags'), $tags_table);
+$triggersFormList->addRow(_('Tags'),
+	(new CDiv($tags_table))
+		->addClass(ZBX_STYLE_TABLE_FORMS_SEPARATOR)
+		->setAttribute('style', 'min-width: '.ZBX_TEXTAREA_STANDARD_WIDTH.'px;')
+);
 
 // append status to form list
 if (empty($data['triggerid']) && empty($data['form_refresh'])) {
