7188835705f0a637356f33fbfc4fee692b1c5c29
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-05-20 07:22:09 +0000

    .......... [ZBXNEXT-3196] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@60214 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-3196/upgrades/dbpatches/1.8/oracle/patch/maintenances_groups.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE maintenances_groups (
-        maintenance_groupid             number(20)              DEFAULT '0'     NOT NULL,
-        maintenanceid           number(20)              DEFAULT '0'     NOT NULL,
-        groupid         number(20)              DEFAULT '0'     NOT NULL,
-        PRIMARY KEY (maintenance_groupid)
-);
-CREATE INDEX maintenances_groups_1 on maintenances_groups (maintenanceid,groupid);
-
