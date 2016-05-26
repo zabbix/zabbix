99b356f8773b84748eb45b27d662f5955997ff91
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-05-26 09:49:55 +0000

    ..F....... [ZBXNEXT-3263] fixed style for "No data found."
    git-svn-id: svn://svn.zabbix.com/branches/dev@60336 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-3263/frontends/php/include/views/administration.general.image.list.php
+++ ZBXNEXT-3263/frontends/php/include/views/administration.general.image.list.php
@@ -39,48 +39,50 @@ $widget = (new CWidget())
 		)
 	);
 
-// header
-$imageTable = (new CDiv())
-	->addClass(ZBX_STYLE_TABLE)
-	->addClass(ZBX_STYLE_ADM_IMG);
+if (!$data['images']) {
+	$widget->addItem(new CTableInfo());
+}
+else {
+	// header
+	$imageTable = (new CDiv())
+		->addClass(ZBX_STYLE_TABLE)
+		->addClass(ZBX_STYLE_ADM_IMG);
 
-$count = 0;
-$imageRow = (new CDiv())->addClass(ZBX_STYLE_ROW);
-foreach ($data['images'] as $image) {
-	$img = ($image['imagetype'] == IMAGE_TYPE_BACKGROUND)
-		? new CLink(
-			new CImg('imgstore.php?width=200&height=200&iconid='.$image['imageid'], 'no image'),
-			'image.php?imageid='.$image['imageid']
-		)
-		: new CImg('imgstore.php?iconid='.$image['imageid'], 'no image');
+	$count = 0;
+	$imageRow = (new CDiv())->addClass(ZBX_STYLE_ROW);
+	foreach ($data['images'] as $image) {
+		$img = ($image['imagetype'] == IMAGE_TYPE_BACKGROUND)
+			? new CLink(
+				new CImg('imgstore.php?width=200&height=200&iconid='.$image['imageid'], 'no image'),
+				'image.php?imageid='.$image['imageid']
+			)
+			: new CImg('imgstore.php?iconid='.$image['imageid'], 'no image');
 
-	$imageRow->addItem(
-		(new CDiv())
-			->addClass(ZBX_STYLE_CELL)
-			->addItem([
-				$img,
-				BR(),
-				new CLink($image['name'], 'adm.images.php?form=update&imageid='.$image['imageid'])
-			])
-	);
+		$imageRow->addItem(
+			(new CDiv())
+				->addClass(ZBX_STYLE_CELL)
+				->addItem([
+					$img,
+					BR(),
+					new CLink($image['name'], 'adm.images.php?form=update&imageid='.$image['imageid'])
+				])
+		);
 
-	if ((++$count % 5) == 0) {
-		$imageTable->addItem($imageRow);
-		$imageRow = (new CDiv())->addClass(ZBX_STYLE_ROW);
+		if ((++$count % 5) == 0) {
+			$imageTable->addItem($imageRow);
+			$imageRow = (new CDiv())->addClass(ZBX_STYLE_ROW);
+		}
 	}
-}
 
-if (($count % 5) != 0) {
-	$imageTable->addItem($imageRow);
-}
-elseif ($count == 0) {
-	$imageTable->addItem(_('No data found.'));
-}
+	if (($count % 5) != 0) {
+		$imageTable->addItem($imageRow);
+	}
 
-$widget->addItem(
-	(new CForm())->addItem(
-		(new CTabView())->addTab('image', null, $imageTable)
-	)
-);
+	$widget->addItem(
+		(new CForm())->addItem(
+			(new CTabView())->addTab('image', null, $imageTable)
+		)
+	);
+}
 
 return $widget;
