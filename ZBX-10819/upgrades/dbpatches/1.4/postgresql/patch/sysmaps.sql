50aee41c19dc4466d7a549ab02dab7f0934635fd
viktors <viktors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-05-27 05:27:45 +0000

    .......... [ZBX-10819] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@60357 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-10819/upgrades/dbpatches/1.4/postgresql/patch/sysmaps.sql
@@ -0,0 +1,16 @@
+CREATE TABLE sysmaps_tmp (
+	sysmapid	bigint DEFAULT '0'	NOT NULL,
+	name		varchar(128)		DEFAULT ''	NOT NULL,
+	width		integer		DEFAULT '0'	NOT NULL,
+	height		integer		DEFAULT '0'	NOT NULL,
+	backgroundid	bigint DEFAULT '0'	NOT NULL,
+	label_type	integer		DEFAULT '0'	NOT NULL,
+	label_location	integer		DEFAULT '0'	NOT NULL,
+	PRIMARY KEY (sysmapid)
+) with OIDS;
+CREATE INDEX sysmaps_1 on sysmaps_tmp (name);
+
+insert into sysmaps_tmp select s.sysmapid,s.name,s.width,s.height,i.imageid,s.label_type,s.label_location from sysmaps s,images i where s.background=i.name;
+insert into sysmaps_tmp select s.sysmapid,s.name,s.width,s.height,0,s.label_type,s.label_location from sysmaps s where s.background='';
+drop table sysmaps;
+alter table sysmaps_tmp rename to sysmaps;
