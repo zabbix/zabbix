1119832f875bcc7fa1e4e1e423ac4647574e9163
vitalijs <vitalijs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-05-30 07:31:05 +0000

    ..F....... [ZBX-10732] added default value for map hostgroup element "show" option
    git-svn-id: svn://svn.zabbix.com/branches/dev@60395 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10732/frontends/php/include/views/js/monitoring.sysmaps.js.php
+++ ZBX-10732/frontends/php/include/views/js/monitoring.sysmaps.js.php
@@ -22,7 +22,7 @@
 						]))->setId('elementType')
 					)
 					->addRow(_('Show'),
-						(new CRadioButtonList('elementsubtype', null))
+						(new CRadioButtonList('elementsubtype', SYSMAP_ELEMENT_SUBTYPE_HOST_GROUP))
 							->addValue(_('Host group'), SYSMAP_ELEMENT_SUBTYPE_HOST_GROUP, 'subtypeHostGroup')
 							->addValue(_('Host group elements'), SYSMAP_ELEMENT_SUBTYPE_HOST_GROUP_ELEMENTS,
 								'subtypeHostGroupElements'
@@ -31,7 +31,7 @@
 						'subtypeRow'
 					)
 					->addRow(_('Area type'),
-						(new CRadioButtonList('areatype', null))
+						(new CRadioButtonList('areatype', SYSMAP_ELEMENT_AREA_TYPE_FIT))
 							->addValue(_('Fit to map'), SYSMAP_ELEMENT_AREA_TYPE_FIT, 'areaTypeAuto')
 							->addValue(_('Custom size'), SYSMAP_ELEMENT_AREA_TYPE_CUSTOM, 'areaTypeCustom')
 							->setModern(true),
