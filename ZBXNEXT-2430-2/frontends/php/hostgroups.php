78299a6d87607c93936809329af51264163bfb3f
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-06-14 08:26:42 +0000

    .......... [ZBXNEXT-2430] updated to the latest trunk
    git-svn-id: svn://svn.zabbix.com/branches/dev@60631 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-2430-2/frontends/php/hostgroups.php
+++ ZBXNEXT-2430-2/frontends/php/hostgroups.php
@@ -48,6 +48,10 @@ $fields = [
 	// other
 	'form' =>			[T_ZBX_STR, O_OPT, P_SYS,	null,		null],
 	'form_refresh' =>	[T_ZBX_INT, O_OPT, null,	null,		null],
+	// filter
+	'filter_set' =>		[T_ZBX_STR, O_OPT, P_SYS,	null,		null],
+	'filter_rst' =>		[T_ZBX_STR, O_OPT, P_SYS,	null,		null],
+	'filter_name' =>	[T_ZBX_STR, O_OPT, null,	null,		null],
 	// sort and sortorder
 	'sort' =>			[T_ZBX_STR, O_OPT, P_SYS, IN('"name"'),								null],
 	'sortorder' =>		[T_ZBX_STR, O_OPT, P_SYS, IN('"'.ZBX_SORT_DOWN.'","'.ZBX_SORT_UP.'"'),	null]
@@ -400,16 +404,32 @@ else {
 	CProfile::update('web.'.$page['file'].'.sort', $sortField, PROFILE_TYPE_STR);
 	CProfile::update('web.'.$page['file'].'.sortorder', $sortOrder, PROFILE_TYPE_STR);
 
+	// filter
+	if (hasRequest('filter_set')) {
+		CProfile::update('web.groups.filter_name', getRequest('filter_name', ''), PROFILE_TYPE_STR);
+	}
+	elseif (hasRequest('filter_rst')) {
+		CProfile::delete('web.groups.filter_name');
+	}
+
+	$filter = [
+		'name' => CProfile::get('web.groups.filter_name', '')
+	];
+
 	$config = select_config();
 
 	$data = [
 		'sort' => $sortField,
 		'sortorder' => $sortOrder,
+		'filter' => $filter,
 		'config' => $config
 	];
 
 	$groups = API::HostGroup()->get([
 		'output' => ['groupid', $sortField],
+		'search' => [
+			'name' => ($filter['name'] === '') ? null : $filter['name']
+		],
 		'editable' => true,
 		'sortfield' => $sortField,
 		'limit' => $config['search_limit'] + 1
