7bb390e52ca285107bce18de1f2a8535e265016e
vitalijs <vitalijs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-06-20 05:44:12 +0000

    .......... [ZBX-10732] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@60691 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10732/upgrades/dbpatches/1.8/oracle/patch/drules.sql
+++
@@ -1,4 +0,0 @@
-alter table drules add unique_dcheckid number(20) DEFAULT '0' NOT NULL;
-
-alter table drules modify name            nvarchar2(255)          DEFAULT '';
-alter table drules modify iprange         nvarchar2(255)          DEFAULT '';
