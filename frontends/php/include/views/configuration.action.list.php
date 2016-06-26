5273ddb1bebe0191af1b01d8491b20117f3abb12
sasha <sasha@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2016-06-26 15:27:44 +0000

    ..F....... [ZBXNEXT-3296] fixed profile index:
     - web.action.filter* => web.actionconf.filter*
    
    git-svn-id: svn://svn.zabbix.com/trunk@60732 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/views/configuration.action.list.php
+++ frontends/php/include/views/configuration.action.list.php
@@ -34,7 +34,7 @@ $widget = (new CWidget())
 			->addItem(new CSubmit('form', _('Create action')))
 		)
 	)
-	->addItem((new CFilter('web.action.filter.state'))
+	->addItem((new CFilter('web.actionconf.filter.state'))
 		->addColumn((new CFormList())->addRow(_('Name'),
 			(new CTextBox('filter_name', $data['filter']['name']))
 				->setWidth(ZBX_TEXTAREA_FILTER_SMALL_WIDTH)
