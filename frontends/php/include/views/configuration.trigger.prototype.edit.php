fc7e11dc5ef75d145a1538c6a1ddb627c488d107
Oleg Egorov <git-no-reply@zabbix.com>
2016-07-05 13:41:14 +0000

    ..F....... [ZBXNEXT-3274] added event correlation support for triggers prototypes

--- frontends/php/include/views/configuration.trigger.prototype.edit.php
+++ frontends/php/include/views/configuration.trigger.prototype.edit.php
@@ -51,7 +51,8 @@ $triggersFormList->addRow(_('Name'),
 	(new CTextBox('description', $data['description'], $data['limited']))
 		->setWidth(ZBX_TEXTAREA_STANDARD_WIDTH)
 		->setAttribute('autofocus', 'autofocus')
-);
+)
+	->addRow(_('Severity'), new CSeverity(['name' => 'priority', 'value' => (int) $data['priority']]));
 
 // append expression to form list
 if ($data['expression_field_readonly']) {
@@ -458,11 +459,15 @@ $triggersFormList
 			->addValue(_('Multiple'), TRIGGER_MULT_EVENT_ENABLED)
 			->setModern(true)
 	)
-	->addRow(_('Description'),
-		(new CTextArea('comments', $data['comments']))->setWidth(ZBX_TEXTAREA_STANDARD_WIDTH)
+	->addRow(_('OK event closes'),
+		(new CRadioButtonList('correlation_mode', (int) $data['correlation_mode']))
+			->addValue(_('All problems'), TRIGGER_MULT_EVENT_DISABLED)
+			->addValue(_('All problems if tag values match'), TRIGGER_MULT_EVENT_ENABLED)
+			->setModern(true)
 	)
-	->addRow(_('URL'), (new CTextBox('url', $data['url']))->setWidth(ZBX_TEXTAREA_STANDARD_WIDTH))
-	->addRow(_('Severity'), new CSeverity(['name' => 'priority', 'value' => (int) $data['priority']]));
+	->addRow(_('Tag for matching'), (new CTextBox('correlation_tag', $data['correlation_tag'], $readonly))
+		->setWidth(ZBX_TEXTAREA_STANDARD_WIDTH)
+	);
 
 // tags
 $tags_table = (new CTable())->setId('tbl_tags');
@@ -501,7 +506,12 @@ if (empty($data['triggerid']) && empty($data['form_refresh'])) {
 else {
 	$status = ($data['status'] == 0);
 }
-$triggersFormList->addRow(_('Enabled'), (new CCheckBox('status'))->setChecked($status));
+$triggersFormList
+	->addRow(_('URL'), (new CTextBox('url', $data['url']))->setWidth(ZBX_TEXTAREA_STANDARD_WIDTH))
+	->addRow(_('Description'),
+		(new CTextArea('comments', $data['comments']))->setWidth(ZBX_TEXTAREA_STANDARD_WIDTH)
+	)
+	->addRow(_('Enabled'), (new CCheckBox('status'))->setChecked($status));
 
 // append tabs to form
 $triggersTab = new CTabView();
