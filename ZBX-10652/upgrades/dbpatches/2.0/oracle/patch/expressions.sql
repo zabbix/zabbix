2098b8e3e142f2f734a5c50bd4b12a957cfb5e18
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-07-18 11:42:16 +0000

    .......... [ZBX-10652] deleted the development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@61082 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10652/upgrades/dbpatches/2.0/oracle/patch/expressions.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE expressions MODIFY expressionid DEFAULT NULL;
-ALTER TABLE expressions MODIFY regexpid DEFAULT NULL;
-DELETE FROM expressions WHERE NOT regexpid IN (SELECT regexpid FROM regexps);
-ALTER TABLE expressions ADD CONSTRAINT c_expressions_1 FOREIGN KEY (regexpid) REFERENCES regexps (regexpid) ON DELETE CASCADE;
