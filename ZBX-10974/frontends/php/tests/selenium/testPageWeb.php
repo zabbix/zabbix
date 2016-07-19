1f8416aeac2ba80e6e8c8213e369322ac5aa0b30
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-07-19 14:51:18 +0000

    .......... [ZBX-10974] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@61114 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-10974/frontends/php/tests/selenium/testPageWeb.php
@@ -0,0 +1,38 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+require_once dirname(__FILE__).'/../include/class.cwebtest.php';
+
+class testPageWeb extends CWebTest {
+	public function testPageWeb_CheckLayout() {
+		$this->zbxTestLogin('zabbix.php?action=web.view');
+		$this->zbxTestCheckTitle('Web monitoring');
+		$this->zbxTestCheckHeader('Web monitoring');
+		$this->zbxTestTextPresent(['Group', 'Host']);
+		$this->zbxTestTextPresent(['Host', 'Name', 'Number of steps', 'Last check', 'Status']);
+	}
+
+// Check that no real host or template names displayed
+	public function testPageWeb_NoHostNames() {
+		$this->zbxTestLogin('zabbix.php?action=web.view');
+		$this->zbxTestCheckTitle('Web monitoring');
+		$this->zbxTestCheckNoRealHostnames();
+	}
+}
