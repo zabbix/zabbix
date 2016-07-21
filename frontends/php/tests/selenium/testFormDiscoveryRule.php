919cbb09503884a80f5be29c64df300efc2f8aa9
natalja <natalja@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2016-07-21 14:54:31 +0000

    .......... [ZBXNEXT-686] updated selenium tests, turned off warning if zabbix server is down
    git-svn-id: svn://svn.zabbix.com/trunk@61152 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/tests/selenium/testFormDiscoveryRule.php
+++ frontends/php/tests/selenium/testFormDiscoveryRule.php
@@ -1431,6 +1431,8 @@ class testFormDiscoveryRule extends CWebTest {
 	 * @dataProvider create
 	 */
 	public function testFormDiscoveryRule_SimpleCreate($data) {
+		DBexecute("UPDATE config SET server_check_interval = 0 WHERE configid = 1");
+
 		$this->zbxTestLogin('hosts.php');
 		$this->zbxTestCheckTitle('Configuration of hosts');
 		$this->zbxTestCheckHeader('Hosts');
@@ -1511,8 +1513,6 @@ class testFormDiscoveryRule extends CWebTest {
 					$this->zbxTestInputType('delay_flex_'.$itemCount.'_delay', $period['flexDelay']);
 				}
 				$itemCount ++;
-				$this->zbxTestWaitUntilElementVisible(WebDriverBy::id('interval_add'));
-				$this->zbxTestCheckFatalErrors();
 				$this->zbxTestClickWait('interval_add');
 
 				$this->zbxTestAssertVisibleId('delay_flex_'.$itemCount.'_delay');
@@ -1620,6 +1620,7 @@ class testFormDiscoveryRule extends CWebTest {
 			$sql = "SELECT itemid FROM items WHERE name = '".$name."' and hostid = ".$this->hostid;
 			$this->assertEquals(0, DBcount($sql), 'Discovery rule has not been deleted from DB.');
 
+			DBexecute("UPDATE config SET server_check_interval = 10 WHERE configid = 1");
 		}
 	}
 
