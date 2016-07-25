36b526a13376e50d65ccd76759b31fb8bbee2780
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-07-25 09:50:31 +0000

    .......... [ZBX-10591] updated to the latest 3.0
    git-svn-id: svn://svn.zabbix.com/branches/dev@61199 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10591/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ ZBX-10591/src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	public static final String REVISION_DATE = "15 July 2016";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "3.0.4rc1";
+	public static final String VERSION = "3.0.5rc1";
 
 	public static void printVersion()
 	{
