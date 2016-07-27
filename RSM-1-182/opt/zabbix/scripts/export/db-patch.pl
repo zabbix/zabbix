7cfde9617766fb57eae53577f1b7d7b36b4e482e
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-07-27 14:41:31 +0000

    .......... [RSM-1] merge from upstream
    git-svn-id: svn://svn.zabbix.com/branches/dev@61243 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- RSM-1-182/opt/zabbix/scripts/export/db-patch.pl
+++ RSM-1-182/opt/zabbix/scripts/export/db-patch.pl
@@ -1,9 +1,15 @@
 #!/usr/bin/perl
 
+BEGIN
+{
+	our $MYDIR = $0; $MYDIR =~ s,(.*)/.*,$1,; $MYDIR = '.' if ($MYDIR eq $0);
+	our $MYDIR2 = $0; $MYDIR2 =~ s,(.*)/.*/.*,$1,; $MYDIR2 = '..' if ($MYDIR2 eq $0);
+}
+use lib $MYDIR;
+use lib $MYDIR2;
+
 use warnings;
 use strict;
-
-use lib '/opt/zabbix/scripts';
 
 use RSM;
 use RSMSLV;
