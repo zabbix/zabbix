bf247e3cab2fa16c1b55582ea9c8d6266749ae9d
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-08-02 12:21:40 +0000

    ........S. [ZBXNEXT-3274] truncate and trim event tags before validation
    git-svn-id: svn://svn.zabbix.com/branches/dev@61328 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-3274-3/src/zabbix_server/events.c
+++ ZBXNEXT-3274-3/src/zabbix_server/events.c
@@ -155,16 +155,16 @@ int	add_event(unsigned char source, unsigned char object, zbx_uint64_t objectid,
 				substitute_simple_macros(NULL, &events[events_num], NULL, NULL, NULL, NULL, NULL, NULL,
 						&tag->value, MACRO_TYPE_TRIGGER_TAG, NULL, 0);
 
-				if (SUCCEED == validate_event_tag(&events[events_num], tag))
-				{
-					/* truncate tag name/value if necessary */
-					if (TAG_NAME_LEN < zbx_strlen_utf8(tag->tag))
-						tag->tag[zbx_strlen_utf8_nchars(tag->tag, TAG_NAME_LEN)] = '\0';
-					if (TAG_VALUE_LEN < zbx_strlen_utf8(tag->value))
-						tag->value[zbx_strlen_utf8_nchars(tag->value, TAG_VALUE_LEN)] = '\0';
+				if (TAG_NAME_LEN < zbx_strlen_utf8(tag->tag))
+					tag->tag[zbx_strlen_utf8_nchars(tag->tag, TAG_NAME_LEN)] = '\0';
+				if (TAG_VALUE_LEN < zbx_strlen_utf8(tag->value))
+					tag->value[zbx_strlen_utf8_nchars(tag->value, TAG_VALUE_LEN)] = '\0';
 
+				zbx_lrtrim(tag->tag, ZBX_WHITESPACE);
+				zbx_lrtrim(tag->value, ZBX_WHITESPACE);
+
+				if (SUCCEED == validate_event_tag(&events[events_num], tag))
 					zbx_vector_ptr_append(&events[events_num].tags, tag);
-				}
 				else
 					zbx_free_tag(tag);
 			}
