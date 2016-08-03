70cfd43ffa9900d7824aed4b6b6995fe05502125
sergejs <sergejs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-08-03 07:23:55 +0000

    .......... [ZBX-10759] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@61345 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-10759/src/zabbix_server/proxypoller/proxypoller.h
@@ -0,0 +1,29 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#ifndef ZABBIX_PROXYPOLLER_H
+#define ZABBIX_PROXYPOLLER_H
+
+extern char	*CONFIG_SOURCE_IP;
+extern int	CONFIG_TRAPPER_TIMEOUT;
+
+void	main_proxypoller_loop(void);
+
+#endif
+
