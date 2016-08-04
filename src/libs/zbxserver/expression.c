1a785e76aa2e49a2a79d829ede4e2f781034c393
sergejs <sergejs@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2016-08-04 13:40:46 +0000

    ........S. [ZBX-10713] fixed incorrect macro {ESC.HISTORY} that gathers history about one event to different actions
    git-svn-id: svn://svn.zabbix.com/trunk@61425 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/libs/zbxserver/expression.c
+++ src/libs/zbxserver/expression.c
@@ -1457,7 +1457,7 @@ out:
  * Purpose: retrieve escalation history                                       *
  *                                                                            *
  ******************************************************************************/
-static void	get_escalation_history(const DB_EVENT *event, const DB_EVENT *r_event, char **replace_to)
+static void	get_escalation_history(zbx_uint64_t actionid, const DB_EVENT *event, const DB_EVENT *r_event, char **replace_to)
 {
 	DB_RESULT	result;
 	DB_ROW		row;
@@ -1480,8 +1480,9 @@ static void	get_escalation_history(const DB_EVENT *event, const DB_EVENT *r_even
 			" left join media_type mt"
 				" on mt.mediatypeid=a.mediatypeid"
 			" where a.eventid=" ZBX_FS_UI64
+				" and a.actionid=" ZBX_FS_UI64
 			" order by a.clock",
-			event->eventid);
+			event->eventid, actionid);
 
 	while (NULL != (row = DBfetch(result)))
 	{
@@ -2596,7 +2597,7 @@ int	substitute_simple_macros(zbx_uint64_t *actionid, const DB_EVENT *event, cons
 				}
 				else if (0 == strcmp(m, MVAR_ESC_HISTORY))
 				{
-					get_escalation_history(event, r_event, &replace_to);
+					get_escalation_history(*actionid, event, r_event, &replace_to);
 				}
 				else if (0 == strncmp(m, MVAR_EVENT_RECOVERY, ZBX_CONST_STRLEN(MVAR_EVENT_RECOVERY)))
 				{
@@ -2858,7 +2859,7 @@ int	substitute_simple_macros(zbx_uint64_t *actionid, const DB_EVENT *event, cons
 				}
 				else if (0 == strcmp(m, MVAR_ESC_HISTORY))
 				{
-					get_escalation_history(event, r_event, &replace_to);
+					get_escalation_history(*actionid, event, r_event, &replace_to);
 				}
 				else if (0 == strncmp(m, MVAR_EVENT_RECOVERY, ZBX_CONST_STRLEN(MVAR_EVENT_RECOVERY)))
 				{
@@ -3238,7 +3239,7 @@ int	substitute_simple_macros(zbx_uint64_t *actionid, const DB_EVENT *event, cons
 				}
 				else if (0 == strcmp(m, MVAR_ESC_HISTORY))
 				{
-					get_escalation_history(event, r_event, &replace_to);
+					get_escalation_history(*actionid, event, r_event, &replace_to);
 				}
 				else if (0 == strncmp(m, MVAR_EVENT_RECOVERY, ZBX_CONST_STRLEN(MVAR_EVENT_RECOVERY)))
 				{
@@ -3347,7 +3348,7 @@ int	substitute_simple_macros(zbx_uint64_t *actionid, const DB_EVENT *event, cons
 				}
 				else if (0 == strcmp(m, MVAR_ESC_HISTORY))
 				{
-					get_escalation_history(event, r_event, &replace_to);
+					get_escalation_history(*actionid, event, r_event, &replace_to);
 				}
 				else if (0 == strncmp(m, MVAR_EVENT_RECOVERY, ZBX_CONST_STRLEN(MVAR_EVENT_RECOVERY)))
 				{
