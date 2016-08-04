3fbc36fe10109a0fc9da5bc2a9f8d53fc1aed826
sergejs <sergejs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-08-04 13:41:49 +0000

    .......... [ZBX-10713] removing unnecessary development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@61426 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10713/upgrades/dbpatches/1.4/mysql/patch/dhosts.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE dhosts (
-        dhostid         bigint unsigned         DEFAULT '0'     NOT NULL,
-        druleid         bigint unsigned         DEFAULT '0'     NOT NULL,
-        ip              varchar(15)             DEFAULT ''      NOT NULL,
-        status          integer         DEFAULT '0'     NOT NULL,
-        lastup          integer         DEFAULT '0'     NOT NULL,
-        lastdown        integer         DEFAULT '0'     NOT NULL,
-        PRIMARY KEY (dhostid)
-) ENGINE=InnoDB;
