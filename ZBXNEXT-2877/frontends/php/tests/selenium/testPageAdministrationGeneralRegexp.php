773ea8c32919b9016f511d6c0c603148e80f0fdb
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-08-12 06:53:03 +0000

    .......... [ZBXNEXT-2877] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@61602 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-2877/frontends/php/tests/selenium/testPageAdministrationGeneralRegexp.php
@@ -0,0 +1,132 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+require_once dirname(__FILE__).'/../include/class.cwebtest.php';
+
+class testPageAdministrationGeneralRegexp extends CWebTest {
+
+	private $sqlHashRegexps = '';
+	private $oldHashRegexps = '';
+	private $sqlHashExpressions = '';
+	private $oldHashExpressions = '';
+
+	private function calculateHash($conditions = null) {
+		$this->sqlHashRegexps =
+			'SELECT * FROM regexps'.
+			($conditions ? ' WHERE '.$conditions : '').
+			' ORDER BY regexpid';
+		$this->oldHashRegexps = DBhash($this->sqlHashRegexps);
+
+		$this->sqlHashExpressions =
+			'SELECT * FROM expressions'.
+			($conditions ? ' WHERE '.$conditions : '').
+			' ORDER BY expressionid';
+		$this->oldHashExpressions = DBhash($this->sqlHashExpressions);
+	}
+
+	private function verifyHash() {
+		$this->assertEquals($this->oldHashRegexps, DBhash($this->sqlHashRegexps));
+		$this->assertEquals($this->oldHashExpressions, DBhash($this->sqlHashExpressions));
+	}
+
+	public static function allRegexps() {
+		return DBdata('SELECT regexpid FROM regexps');
+	}
+
+	public function testPageAdministrationGeneralRegexp_CheckLayout() {
+		$this->zbxTestLogin('adm.regexps.php');
+		$this->zbxTestCheckTitle('Configuration of regular expressions');
+		$this->zbxTestCheckHeader('Regular expressions');
+		$this->zbxTestDropdownHasOptions('configDropDown', [
+			'GUI', 'Housekeeping', 'Images', 'Icon mapping', 'Regular expressions', 'Macros', 'Value mapping',
+			'Working time', 'Trigger severities', 'Trigger displaying options', 'Other'
+		]);
+		$this->zbxTestAssertElementPresentId('form');
+
+		$this->zbxTestTextPresent(['Name', 'Expressions']);
+
+		$dbResult = DBselect('select name from regexps');
+
+		while ($dbRow = DBfetch($dbResult)) {
+			$this->zbxTestTextPresent($dbRow['name']);
+		}
+
+		$this->zbxTestAssertElementPresentXpath("//button[@value='regexp.massdelete' and @disabled]");
+	}
+
+	public function testPageAdministrationGeneralRegexp_MassDeleteAllCancel() {
+		$this->calculateHash();
+
+		$this->zbxTestLogin('adm.regexps.php');
+		$this->zbxTestCheckboxSelect('all_regexps');
+		$this->zbxTestClickButton('regexp.massdelete');
+		$this->webDriver->switchTo()->alert()->dismiss();
+		$this->zbxTestCheckTitle('Configuration of regular expressions');
+		$this->zbxTestTextNotPresent(['Regular expression deleted', 'Regular expressions deleted']);
+
+		$this->verifyHash();
+	}
+
+	public function testPageAdministrationGeneralRegexp_backup_1() {
+		DBsave_tables('regexps');
+	}
+
+	/**
+	 * @dataProvider allRegexps
+	 */
+	public function testPageAdministrationGeneralRegexp_MassDelete($regexp) {
+		$this->calculateHash('regexpid<>'.$regexp['regexpid']);
+
+		$this->zbxTestLogin('adm.regexps.php');
+		$this->zbxTestCheckboxSelect('regexpids_'.$regexp['regexpid']);
+		$this->zbxTestClickButton('regexp.massdelete');
+		$this->webDriver->switchTo()->alert()->accept();
+		$this->zbxTestCheckTitle('Configuration of regular expressions');
+		$this->zbxTestTextPresent('Regular expression deleted');
+
+		$this->assertEquals(0, DBcount('SELECT NULL FROM regexps WHERE regexpid='.$regexp['regexpid']));
+
+		$this->verifyHash();
+	}
+
+	public function testPageAdministrationGeneralRegexp_restore_1() {
+		DBrestore_tables('regexps');
+	}
+
+	public function testPageAdministrationGeneralRegexp_backup_2() {
+		DBsave_tables('regexps');
+	}
+
+	public function testPageAdministrationGeneralRegexp_MassDeleteAll() {
+		$this->zbxTestLogin('adm.regexps.php');
+		$this->zbxTestCheckboxSelect('all_regexps');
+		$this->zbxTestClickButton('regexp.massdelete');
+		$this->webDriver->switchTo()->alert()->accept();
+		$this->zbxTestCheckTitle('Configuration of regular expressions');
+		$this->zbxTestTextPresent('Regular expressions deleted');
+
+		$this->assertEquals(0, DBcount('SELECT NULL FROM regexps'));
+	}
+
+	public function testPageAdministrationGeneralRegexp_restore_2() {
+		DBrestore_tables('regexps');
+	}
+
+}
