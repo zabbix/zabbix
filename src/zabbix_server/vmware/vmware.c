14d0ec99b38f2ee764eb2c8ce53f934568d44eba
Andris Zeila <andris.zeila@zabbix.com>
2016-08-16 09:20:59 +0000

    ........S. [ZBX-11093] reverted previous commit

--- src/zabbix_server/vmware/vmware.c
+++ src/zabbix_server/vmware/vmware.c
@@ -1757,7 +1757,6 @@ static int	vmware_service_get_hv_data(const zbx_vmware_service_t *service, CURL 
 				"<ns0:propSet>"							\
 					"<ns0:type>HostSystem</ns0:type>"			\
 					"<ns0:pathSet>name</ns0:pathSet>"			\
-					"<ns0:pathSet>overallStatus</ns0:pathSet>"		\
 					"<ns0:pathSet>vm</ns0:pathSet>"				\
 					"<ns0:pathSet>summary.quickStats</ns0:pathSet>"		\
 					"<ns0:pathSet>summary.config</ns0:pathSet>"		\
