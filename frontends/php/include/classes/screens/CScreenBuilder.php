b88573c194311fb07a55e31760412a08703243ee
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-08-16 07:50:02 +0000

    ..F....... [ZBX-10901] added missing highlighting for some UI elements in focused state
    git-svn-id: svn://svn.zabbix.com/branches/3.0@61660 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/classes/screens/CScreenBuilder.php
+++ frontends/php/include/classes/screens/CScreenBuilder.php
@@ -565,7 +565,7 @@ class CScreenBuilder {
 						->addClass(ZBX_STYLE_TREEVIEW_PLUS)
 						->addSID();
 					if (!$emptyScreenRow) {
-						$link->addConfirmation(CJs::encodeJson(_('This screen-row is not empty. Delete it?')));
+						$link->addConfirmation(_('This screen row is not empty. Delete it?'));
 					}
 				}
 
@@ -611,7 +611,7 @@ class CScreenBuilder {
 						->addSID();
 
 					if (array_key_exists($i, $emptyScreenColumns)) {
-						$link->addConfirmation(CJs::encodeJson(_('This screen-column is not empty. Delete it?')));
+						$link->addConfirmation(_('This screen column is not empty. Delete it?'));
 					}
 				}
 
