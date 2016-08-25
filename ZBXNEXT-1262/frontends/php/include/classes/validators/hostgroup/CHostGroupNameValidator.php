f909fefa44fec4ec475e819cbb60b39092245323
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-08-25 08:49:03 +0000

    A......... [ZBXNEXT-1262] added host group name validator with new algorithm and added unit test
    git-svn-id: svn://svn.zabbix.com/branches/dev@61930 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-1262/frontends/php/include/classes/validators/hostgroup/CHostGroupNameValidator.php
@@ -0,0 +1,113 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CHostGroupNameValidator extends CValidator {
+
+	/**
+	 * Error message.
+	 *
+	 * @var string
+	 */
+	public $message;
+
+	/**
+	 * Error message details.
+	 *
+	 * @var string
+	 */
+	public $message_details;
+
+	/**
+	 * Checks if host group name contains forward slashes and asterisks. Slashes cannot be first character, last or
+	 * repeat in the middle multiple times. Asterisks are not allowed at all.
+	 *
+	 * @param string $name				Host group name.
+	 *
+	 * @return bool
+	 */
+	public function validate($name) {
+		$error = false;
+		$len = strlen($name);
+
+		foreach (['/', '*'] as $char) {
+			switch ($char) {
+				case '/':
+					$groups = explode($char, $name);
+					$cnt = count($groups);
+					foreach ($groups as $i => $group) {
+						if (!$group) {
+							unset($groups[$i]);
+						}
+					}
+
+					$expected_cnt = count($groups);
+
+					if ($cnt != $expected_cnt) {
+						$str = '';
+						for ($i = 0; $i < $cnt; $i++) {
+							if (array_key_exists($i, $groups)) {
+								$str .= $groups[$i].'/';
+							}
+							else {
+								$pos = strlen($str) - 1;
+								if ($pos == -1) {
+									$pos = 0;
+								}
+
+								$error = true;
+								break 3;
+							}
+						}
+					}
+					break;
+
+				case '*':
+					$pos = strpos($name, $char);
+
+					if ($pos !== false) {
+						$error = true;
+						break 2;
+					}
+			}
+		}
+
+		if ($error) {
+			for ($i = $pos, $chunk = '', $max_chunk_size = 50; isset($name[$i]); $i++) {
+				if (0x80 != (0xc0 & ord($name[$i])) && $max_chunk_size-- == 0) {
+					break;
+				}
+				$chunk .= $name[$i];
+			}
+
+			if (isset($name[$i])) {
+				$chunk .= ' ...';
+			}
+
+			$this->error(_s('Incorrect value for field "%1$s": %2$s.', 'name',
+				_s('incorrect syntax near "%1$s"', $chunk)
+			));
+
+			return false;
+		}
+
+		return true;
+	}
+}
