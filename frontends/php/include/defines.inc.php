2164d946bd9e3caafbcbf46e0bd4a51aa72af2f4
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-08-26 14:44:40 +0000

    ....I..... [ZBX-11132] added database patch to fill all required fields in "problem" table
    git-svn-id: svn://svn.zabbix.com/trunk@62018 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -22,7 +22,7 @@
 define('ZABBIX_VERSION',		'3.2.0beta2');
 define('ZABBIX_API_VERSION',	'3.2.0');
 define('ZABBIX_EXPORT_VERSION',	'3.2');
-define('ZABBIX_DB_VERSION',		3010078);
+define('ZABBIX_DB_VERSION',		3010079);
 
 define('ZABBIX_COPYRIGHT_FROM',	'2001');
 define('ZABBIX_COPYRIGHT_TO',	'2016');
