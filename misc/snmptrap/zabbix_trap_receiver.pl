3c53a7bad0590ed3039fed0ad703f111911381f4
viktors <viktors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-08-30 09:05:27 +0000

    ........S. [ZBX-10830] fixed SNMP trap to convert non-printable values from SNMPv3 to hexadecimal 	   [merge ^/branches/2.2 -c r62053]
    git-svn-id: svn://svn.zabbix.com/branches/3.0@62054 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- misc/snmptrap/zabbix_trap_receiver.pl
+++ misc/snmptrap/zabbix_trap_receiver.pl
@@ -87,6 +87,12 @@ sub zabbix_receiver
 	print OUTPUT_FILE "PDU INFO:\n";
 	foreach my $key(keys(%pdu_info))
 	{
+		if ($pdu_info{$key} !~ /^[[:print:]]*$/)
+		{
+			my $OctetAsHex = unpack('H*',$pdu_info{$key}) ;	# convert octet string to hex
+			$pdu_info{$key} = "0x$OctetAsHex" ;		# apply 0x prefix for consistency
+		}
+
 		printf OUTPUT_FILE "  %-30s %s\n", $key, $pdu_info{$key};
 	}
 
