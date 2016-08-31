96b4fc3b5ff5048979b889237251b60926eb0f16
asaveljevs <asaveljevs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-08-31 10:23:10 +0000

    A......... [ZBX-11020] fixed "host.create" so that both "inventory" and "inventory_mode" are optional
    git-svn-id: svn://svn.zabbix.com/branches/3.0@62107 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/disc_prototypes.php
+++ frontends/php/disc_prototypes.php
@@ -214,7 +214,7 @@ if ($itemPrototypeId && !API::ItemPrototype()->isWritable([$itemPrototypeId])) {
  */
 if (hasRequest('delete') && hasRequest('itemid')) {
 	DBstart();
-	$result = API::Itemprototype()->delete([getRequest('itemid')]);
+	$result = API::ItemPrototype()->delete([getRequest('itemid')]);
 	$result = DBend($result);
 
 	if ($result) {
@@ -380,10 +380,10 @@ elseif (hasRequest('add') || hasRequest('update')) {
 			$item = CArrayHelper::unsetEqualValues($item, $dbItem);
 			$item['itemid'] = $itemId;
 
-			$result = API::Itemprototype()->update($item);
+			$result = API::ItemPrototype()->update($item);
 		}
 		else {
-			$result = API::Itemprototype()->create($item);
+			$result = API::ItemPrototype()->create($item);
 		}
 	}
 
@@ -426,7 +426,7 @@ elseif (hasRequest('action') && str_in_array(getRequest('action'), ['itemprototy
 elseif (hasRequest('action') && getRequest('action') == 'itemprototype.massdelete' && hasRequest('group_itemid')) {
 	DBstart();
 
-	$result = API::Itemprototype()->delete(getRequest('group_itemid'));
+	$result = API::ItemPrototype()->delete(getRequest('group_itemid'));
 	$result = DBend($result);
 
 	if ($result) {
