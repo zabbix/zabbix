651ccdb07c5dec6de5c0a2b0454aed39433168f5
sasha <sasha@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2016-08-31 14:51:09 +0000

    ..F....... [ZBXNEXT-3201] added icon in Monitoring->Problems view to show who close problem; implemented new information icons ..F....... [ZBXNEXT-3201] added blinking on CLOSING state in Monitoring->Problems view ..F....... [ZBXNEXT-3201] added "Show details" filter option in Monitoring->Problems view ..F....... [ZBXNEXT-3201] added displaying of recovery actions in Monitoring->Problem view
    git-svn-id: svn://svn.zabbix.com/trunk@62146 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/views/configuration.triggers.edit.php
+++ frontends/php/include/views/configuration.triggers.edit.php
@@ -180,10 +180,8 @@ if ($data['expression_constructor'] == IM_TREE) {
 	$allowed_testing = true;
 	if ($data['expression_tree']) {
 		foreach ($data['expression_tree'] as $i => $e) {
-			if (!isset($e['expression']['levelErrors'])) {
-				$errorImg = '';
-			}
-			else {
+			$info_icons = [];
+			if (isset($e['expression']['levelErrors'])) {
 				$allowed_testing = false;
 				$errors = [];
 
@@ -196,7 +194,7 @@ if ($data['expression_constructor'] == IM_TREE) {
 					}
 				}
 
-				$errorImg = makeErrorIcon($errors);
+				$info_icons[] = makeErrorIcon($errors);
 			}
 
 			// Templated or discovered trigger.
@@ -232,7 +230,7 @@ if ($data['expression_constructor'] == IM_TREE) {
 								)
 						))->addClass(ZBX_STYLE_NOWRAP)
 						: null,
-					$errorImg
+					makeInformationList($info_icons)
 				])
 			);
 		}
@@ -364,15 +362,13 @@ if ($data['recovery_expression_constructor'] == IM_TREE) {
 
 	if ($data['recovery_expression_tree']) {
 		foreach ($data['recovery_expression_tree'] as $i => $e) {
-			if (!isset($e['recovery_expression']['levelErrors'])) {
-				$errorImg = '';
-			}
-			else {
+			$info_icons = [];
+			if (isset($e['expression']['levelErrors'])) {
 				$allowed_testing = false;
 				$errors = [];
 
-				if (is_array($e['recovery_expression']['levelErrors'])) {
-					foreach ($e['recovery_expression']['levelErrors'] as $expVal => $errTxt) {
+				if (is_array($e['expression']['levelErrors'])) {
+					foreach ($e['expression']['levelErrors'] as $expVal => $errTxt) {
 						if ($errors) {
 							$errors[] = BR();
 						}
@@ -380,7 +376,7 @@ if ($data['recovery_expression_constructor'] == IM_TREE) {
 					}
 				}
 
-				$errorImg = makeErrorIcon($errors);
+				$info_icons[] = makeErrorIcon($errors);
 			}
 
 			// Templated or discovered trigger.
@@ -416,7 +412,7 @@ if ($data['recovery_expression_constructor'] == IM_TREE) {
 								)
 						))->addClass(ZBX_STYLE_NOWRAP)
 						: null,
-					$errorImg
+					makeInformationList($info_icons)
 				])
 			);
 		}
