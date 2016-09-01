0c95bd723b4ba9d67de71c3046ea917e62c09ad4
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-09-01 10:32:45 +0000

    .......... [ZBXNEXT-3101] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@62170 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-3101/upgrades/dbpatches/1.8/postgresql/patch/history_uint_sync.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE history_uint_sync ALTER COLUMN value TYPE numeric(20);
-ALTER TABLE history_uint_sync ALTER COLUMN value SET DEFAULT '0';
-ALTER TABLE history_uint_sync ALTER COLUMN value SET NOT NULL;
