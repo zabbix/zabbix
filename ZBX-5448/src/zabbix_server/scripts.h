2dab89dd352617635b54a6885537cf51c33ae905
vladislavs <vladislavs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-09-02 17:28:00 +0000

    .......... [ZBX-5448] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@62237 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-5448/src/zabbix_server/scripts.h
@@ -0,0 +1,30 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#ifndef ZABBIX_SCRIPTS_H
+#define ZABBIX_SCRIPTS_H
+
+#include "common.h"
+#include "dbcache.h"
+
+void	zbx_script_init(zbx_script_t *script);
+void	zbx_script_clean(zbx_script_t *script);
+int	zbx_execute_script(DC_HOST *host, zbx_script_t *script, char **result, char *error, size_t max_error_len);
+
+#endif
