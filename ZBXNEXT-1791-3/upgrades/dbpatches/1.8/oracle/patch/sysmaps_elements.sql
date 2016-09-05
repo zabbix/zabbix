f6421f14708a50342656345318ad6b0201903118
andris <andris@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-09-05 07:41:43 +0000

    .......... [ZBXNEXT-1791] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@62262 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1791-3/upgrades/dbpatches/1.8/oracle/patch/sysmaps_elements.sql
+++
@@ -1,4 +0,0 @@
-alter table sysmaps_elements  modify label           nvarchar2(255)            DEFAULT '';
-ALTER TABLE sysmaps_elements ADD iconid_maintenance number(20) DEFAULT '0' NOT NULL;
-
-alter table sysmaps_elements modify url             nvarchar2(255)          DEFAULT '';
