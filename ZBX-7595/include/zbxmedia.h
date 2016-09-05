d853c2b351c6ad5289c22e78c9c1f7bfe46877a7
vladislavs <vladislavs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-09-05 09:17:53 +0000

    .......... [ZBX-7595] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@62278 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-7595/include/zbxmedia.h
@@ -0,0 +1,37 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#ifndef ZABBIX_ZBXMEDIA_H
+#define ZABBIX_ZBXMEDIA_H
+
+#include "sysinc.h" /* using "config.h" would be better, but it causes warnings when compiled with Net-SNMP */
+
+extern char	*CONFIG_SOURCE_IP;
+
+int	send_email(const char *smtp_server, const char *smtp_helo, const char *smtp_email, const char *mailto,
+		const char *mailsubject, const char *mailbody, char *error, size_t max_error_len);
+int	send_ez_texting(const char *username, const char *password, const char *sendto,
+		const char *message, const char *limit, char *error, int max_error_len);
+#ifdef HAVE_JABBER
+int	send_jabber(const char *username, const char *password, const char *sendto,
+		const char *subject, const char *message, char *error, int max_error_len);
+#endif
+int	send_sms(const char *device, const char *number, const char *message, char *error, int max_error_len);
+
+#endif
---
+++ ZBX-7595/upgrades/dbpatches/1.8/oracle/patch/images.sql
@@ -0,0 +1,2 @@
+alter table images modify name            nvarchar2(64)           DEFAULT '0';
+
