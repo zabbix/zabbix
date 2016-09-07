0e623cff2031d96b7a53190ee27a2f263717f428
gleb <gleb@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-09-07 06:18:18 +0000

    .......... [ZBXNEXT-3353] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@62343 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-3353/frontends/php/include/acknow.inc.php
@@ -0,0 +1,62 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+function get_last_event_by_triggerid($triggerId) {
+	$dbEvents = DBfetch(DBselect(
+		'SELECT e.*'.
+		' FROM events e'.
+		' WHERE e.objectid='.zbx_dbstr($triggerId).
+			' AND e.source='.EVENT_SOURCE_TRIGGERS.
+			' AND e.object='.EVENT_OBJECT_TRIGGER.
+		' ORDER BY e.objectid DESC,e.object DESC,e.eventid DESC',
+		1
+	));
+
+	return $dbEvents ? $dbEvents : false;
+}
+
+/**
+ * Get acknowledgement table.
+ *
+ * @param array  $acknowledges
+ * @param string $acknowledges[]['clock']
+ * @param string $acknowledges[]['alias']
+ * @param string $acknowledges[]['name']
+ * @param string $acknowledges[]['surname']
+ * @param string $acknowledges[]['message']
+ * @param string $acknowledges[]['action']
+ *
+ * @return CTableInfo
+ */
+function makeAckTab($acknowledges) {
+	$table = (new CTableInfo())->setHeader([_('Time'), _('User'), _('Message'), _('User action')]);
+
+	foreach ($acknowledges as $acknowledge) {
+		$table->addRow([
+			zbx_date2str(DATE_TIME_FORMAT_SECONDS, $acknowledge['clock']),
+			getUserFullname($acknowledge),
+			zbx_nl2br($acknowledge['message']),
+			($acknowledge['action'] == ZBX_ACKNOWLEDGE_ACTION_CLOSE_PROBLEM) ? _('Close problem') : ''
+		]);
+	}
+
+	return $table;
+}
