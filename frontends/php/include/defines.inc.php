7ca77ebc40806c738f97eb1b6a750efcc80662ce
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-09-13 12:26:59 +0000

    .......... [ZBXNEXT-826] updated release version [3.2.1rc1]
    git-svn-id: svn://svn.zabbix.com/branches/3.2@62488 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,8 +19,8 @@
 **/
 
 
-define('ZABBIX_VERSION',		'3.2.0');
-define('ZABBIX_API_VERSION',	'3.2.0');
+define('ZABBIX_VERSION',		'3.2.1rc1');
+define('ZABBIX_API_VERSION',	'3.2.1');
 define('ZABBIX_EXPORT_VERSION',	'3.2');
 define('ZABBIX_DB_VERSION',		3020000);
 
