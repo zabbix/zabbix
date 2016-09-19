5b2af6ed473e61208da1a7e335177c3ca1da307e
viktors <viktors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-09-19 11:36:17 +0000

    .......PS. [ZBXNEXT-3344] fixed issue with creating DB in MySQL where index was too long
    git-svn-id: svn://svn.zabbix.com/branches/dev@62601 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-3344/src/zabbix_server/actions.c
+++ ZBXNEXT-3344/src/zabbix_server/actions.c
@@ -715,9 +715,9 @@ static int	check_discovery_condition(const DB_EVENT *event, DB_CONDITION *condit
 			int	condition_value_i = atoi(condition->value);
 
 			result = DBselect(
-					"select type"
-					" from dservices"
-					" where dserviceid=" ZBX_FS_UI64,
+					"select dc.type"
+					" from dservices ds, dchecks dc"
+					" where ds.dserviceid=" ZBX_FS_UI64,
 					event->objectid);
 
 			if (NULL != (row = DBfetch(result)))
