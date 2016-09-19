ba92e25e1008ebaabf09280f1ff9bbb5f4cba554
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-09-19 09:26:41 +0000

    .......... [ZBX-9068] created development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@62595 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-9068-30/src/zabbix_proxy/events.c
@@ -0,0 +1,31 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+
+void	add_event(zbx_uint64_t eventid, unsigned char source, unsigned char object, zbx_uint64_t objectid,
+		const zbx_timespec_t *timespec, int value, const char *trigger_description,
+		const char *trigger_expression, unsigned char trigger_priority, unsigned char trigger_type)
+{
+}
+
+int	process_events(void)
+{
+	return 0;
+}
