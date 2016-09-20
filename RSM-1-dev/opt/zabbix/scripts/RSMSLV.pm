3ee1fd5573ec846af86580934daf37bedfc1a322
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-09-20 14:22:19 +0000

    ....I..... [RSM-1] fix probe ONLINE status calculation in item rsm.probe.online
    git-svn-id: svn://svn.zabbix.com/branches/dev@62629 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- RSM-1-dev/opt/zabbix/scripts/RSMSLV.pm
+++ RSM-1-dev/opt/zabbix/scripts/RSMSLV.pm
@@ -26,9 +26,6 @@ use constant DOWN		=> 0;
 use constant UP			=> 1;
 use constant UP_INCONCLUSIVE	=> 2;
 
-use constant ONLINE	=> 1;
-use constant OFFLINE	=> 0;
-
 use constant SLV_UNAVAILABILITY_LIMIT	=> 49; # NB! must be in sync with frontend
 
 use constant MIN_SERVICE_ERROR		=> -200;	# -200, -201 ...
@@ -44,10 +41,7 @@ use constant TRIGGER_VALUE_FALSE	=> 0;
 use constant TRIGGER_VALUE_TRUE		=> 1;
 use constant INCIDENT_FALSE_POSITIVE	=> 1; # NB! must be in sync with frontend
 use constant SENDER_BATCH_COUNT		=> 250;
-use constant PROBE_LASTACCESS_ITEM	=> 'zabbix[proxy,{$RSM.PROXY_NAME},lastaccess]';
 use constant PROBE_GROUP_NAME		=> 'Probes';
-use constant PROBE_KEY_MANUAL		=> 'rsm.probe.status[manual]';
-use constant PROBE_KEY_AUTOMATIC	=> 'rsm.probe.status[automatic,%]';	# match all in SQL
 use constant PROBE_KEY_ONLINE		=> 'rsm.probe.online';
 
 # In order to do the calculation we should wait till all the results
@@ -1634,13 +1628,7 @@ sub get_probe_times
 
 	if (scalar(@{$items_ref}) == 0)
 	{
-		wrn("Probe main status items (".PROBE_KEY_ONLINE.") not available, will calculate manually.");
-
-		$result = __get_probe_times_old($from, $till, $probes_ref);
-
-		__print_probe_times($result) if (opt('dry-run'));
-
-		return $result;
+		fail("Probe status item (".PROBE_KEY_ONLINE.") was not found on any of the probes. Please check your installation.");
 	}
 
 	foreach my $item_ref (@{$items_ref})
@@ -1696,65 +1684,6 @@ sub get_probe_times
 	}
 
 	__print_probe_times($result) if (opt('dry-run'));
-
-	return $result;
-}
-
-# Old way of getting online probe statuses. Uses 3 methods
-# - proxy lastaccess
-# - automatic status (calculated by simple check on the probe, self-check)
-# - manual status (manually turn off probe)
-#
-# Must return results the same way get_probe_times() does.
-sub __get_probe_times_old
-{
-	my $from = shift;
-	my $till = shift;
-	my $probes_ref = shift; # { host => hostid, ... }
-
-	my $probe_avail_limit = get_macro_probe_avail_limit();
-
-	dbg("from:$from till:$till probe_avail_limit:$probe_avail_limit");
-
-	my $result;
-
-	# check probe lastaccess time
-	foreach my $probe (keys(%$probes_ref))
-	{
-		my $host = "$probe - mon";
-
-		my $itemid = get_itemid_by_host($host, PROBE_LASTACCESS_ITEM);
-		if (!$itemid)
-		{
-			wrn("configuration error: ", rsm_slv_error());
-			next;
-		}
-
-		my $times_ref = __get_lastaccess_times($itemid, $probe_avail_limit, $from, $till);
-
-		my $hostid = $probes_ref->{$probe};
-
-		if (scalar(@$times_ref) != 0)
-		{
-			dbg("$probe reachable times: ", join(',', @$times_ref)) if (opt('debug'));
-
-			$times_ref = __get_probestatus_times($probe, $hostid, $times_ref, PROBE_KEY_MANUAL);
-		}
-
-		if (scalar(@$times_ref) != 0)
-		{
-			dbg("$probe manual probestatus times: ", join(',', @$times_ref)) if (opt('debug'));
-
-			$times_ref = __get_probestatus_times($probe, $hostid, $times_ref, PROBE_KEY_AUTOMATIC);
-		}
-
-		if (scalar(@$times_ref) != 0)
-		{
-			dbg("$probe automatic probestatus times: ", join(',', @$times_ref)) if (opt('debug'));
-
-			$result->{$probe} = $times_ref;
-		}
-	}
 
 	return $result;
 }
@@ -5100,168 +5029,6 @@ sub __script
 sub __get_pidfile
 {
 	return PID_DIR . '/' . __script() . '.pid';
-}
-
-# Times when probe "lastaccess" within $probe_avail_limit.
-sub __get_lastaccess_times
-{
-	my $itemid = shift;
-	my $probe_avail_limit = shift;
-	my $from = shift;
-	my $till = shift;
-
-	my ($rows_ref, @times, $last_status);
-
-	# get the previous status
-	$rows_ref = db_select(
-		"select clock,value".
-		" from history_uint".
-		" where itemid=$itemid".
-			" and clock between ".($from-3600)." and ".($from-1).
-		" order by itemid desc,clock desc".
-		" limit 1");
-
-	$last_status = UP;
-	if (scalar(@$rows_ref) != 0)
-	{
-		my $clock = $rows_ref->[0]->[0];
-		my $value = $rows_ref->[0]->[1];
-
-		dbg("clock:$clock value:$value");
-
-		$last_status = DOWN if ($clock - $value > $probe_avail_limit);
-	}
-
-	push(@times, truncate_from($from)) if ($last_status == UP);
-
-	$rows_ref = db_select(
-		"select clock,value".
-		" from history_uint".
-		" where itemid=$itemid".
-	    		" and clock between $from and $till".
-	    		" and value!=0".
-		" order by itemid,clock");
-
-	foreach my $row_ref (@$rows_ref)
-	{
-		my $clock = $row_ref->[0];
-		my $value = $row_ref->[1];
-
-		my $status = ($clock - $value > $probe_avail_limit) ? DOWN : UP;
-
-		if ($last_status != $status)
-		{
-			if ($status == UP)
-			{
-				$clock = truncate_from($clock);
-			}
-			else
-			{
-				$clock = truncate_till($clock);
-			}
-
-			push(@times, $clock);
-
-			dbg("clock:$clock diff:", ($clock - $value));
-
-			$last_status = $status;
-		}
-	}
-
-	# push "till" to @times if it contains odd number of elements
-	if (scalar(@times) != 0)
-	{
-		push(@times, $till) if ($last_status == UP);
-	}
-
-	return \@times;
-}
-
-sub __get_probestatus_times
-{
-	my $probe = shift;
-	my $hostid = shift;
-	my $times_ref = shift; # input
-	my $key = shift;
-
-	my ($rows_ref, @times, $last_status);
-
-	my $key_match = "i.key_";
-	$key_match .= ($key =~ m/%/) ? " like '$key'" : "='$key'";
-
-	my $itemid;
-	if ($key =~ m/%/)
-	{
-		$itemid = get_itemid_like_by_hostid($hostid, $key);
-	}
-	else
-	{
-		$itemid = get_itemid_by_hostid($hostid, $key);
-	}
-
-	if (!$itemid)
-	{
-		wrn("configuration error: ", rsm_slv_error());
-		return;
-	}
-
-	$rows_ref = db_select(
-		"select value".
-		" from history_uint".
-		" where itemid=$itemid".
-			" and clock<" . $times_ref->[0].
-		" order by clock desc".
-		" limit 1");
-
-	$last_status = UP;
-	if (scalar(@$rows_ref) != 0)
-	{
-		my $value = $rows_ref->[0]->[0];
-
-		$last_status = DOWN if ($value == OFFLINE);
-	}
-
-	my $idx = 0;
-	my $times_count = scalar(@$times_ref);
-	while ($idx < $times_count)
-	{
-		my $from = $times_ref->[$idx++];
-		my $till = $times_ref->[$idx++];
-
-		$rows_ref = db_select(
-			"select clock,value".
-			" from history_uint".
-			" where itemid=$itemid".
-				" and clock between $from and $till".
-			" order by itemid,clock");
-
-		push(@times, $from) if ($last_status == UP);
-
-		foreach my $row_ref (@$rows_ref)
-		{
-			my $clock = $row_ref->[0];
-			my $value = $row_ref->[1];
-
-			my $status = ($value == OFFLINE) ? DOWN : UP;
-
-			if ($last_status != $status)
-			{
-				push(@times, $clock);
-
-				dbg("clock:$clock value:$value");
-
-				$last_status = $status;
-			}
-		}
-
-		# push "till" to @times if it contains odd number of elements
-		if (scalar(@times) != 0)
-		{
-			push(@times, $till) if ($last_status == UP);
-		}
-	}
-
-	return \@times;
 }
 
 sub __get_configvalue
--- RSM-1-dev/opt/zabbix/scripts/slv/rsm.probe.online.pl
+++ RSM-1-dev/opt/zabbix/scripts/slv/rsm.probe.online.pl
@@ -13,6 +13,13 @@ use warnings;
 use RSM;
 use RSMSLV;
 
+use constant PROBE_LASTACCESS_ITEM	=> 'zabbix[proxy,{$RSM.PROXY_NAME},lastaccess]';
+use constant PROBE_KEY_MANUAL		=> 'rsm.probe.status[manual]';
+use constant PROBE_KEY_AUTOMATIC	=> 'rsm.probe.status[automatic,%]';	# match all in SQL
+
+use constant ONLINE	=> 1;
+use constant OFFLINE	=> 0;
+
 my $cfg_key_out = 'rsm.probe.online';
 
 parse_opts('now=i');
@@ -34,7 +41,7 @@ dbg("selected period: ", selected_period($from, $till), ", with value timestamp:
 
 my $probes_ref = get_probes(ENABLED_DNS);
 
-my $probe_times_ref = get_probe_times($from, $till, $probes_ref);
+my $probe_times_ref = __get_probe_times($from, $till, $probes_ref);
 my @online_probes = keys(%{$probe_times_ref});
 
 init_values();
@@ -58,3 +65,218 @@ foreach my $probe (keys(%$probes_ref))
 send_values();
 
 slv_exit(SUCCESS);
+
+sub __get_probe_times
+{
+	my $from = shift;
+	my $till = shift;
+	my $probes_ref = shift; # { host => hostid, ... }
+
+	my $probe_avail_limit = get_macro_probe_avail_limit();
+
+	dbg("from:$from till:$till probe_avail_limit:$probe_avail_limit");
+
+	my $result;
+
+	# check probe lastaccess time
+	foreach my $probe (keys(%$probes_ref))
+	{
+		my $host = "$probe - mon";
+
+		my $itemid = get_itemid_by_host($host, PROBE_LASTACCESS_ITEM);
+		if (!$itemid)
+		{
+			wrn("configuration error: ", rsm_slv_error());
+			next;
+		}
+
+		my $times_ref = __get_lastaccess_times($itemid, $probe_avail_limit, $from, $till);
+
+		my $hostid = $probes_ref->{$probe};
+
+		if (scalar(@$times_ref) != 0)
+		{
+			dbg("$probe reachable times: ", join(',', @$times_ref)) if (opt('debug'));
+
+			$times_ref = __get_probestatus_times($probe, $hostid, $times_ref, PROBE_KEY_MANUAL);
+		}
+
+		if (scalar(@$times_ref) != 0)
+		{
+			dbg("$probe manual probestatus times: ", join(',', @$times_ref)) if (opt('debug'));
+
+			$times_ref = __get_probestatus_times($probe, $hostid, $times_ref, PROBE_KEY_AUTOMATIC);
+		}
+
+		if (scalar(@$times_ref) != 0)
+		{
+			dbg("$probe automatic probestatus times: ", join(',', @$times_ref)) if (opt('debug'));
+
+			$result->{$probe} = $times_ref;
+		}
+	}
+
+	return $result;
+}
+
+# Times when probe "lastaccess" within $probe_avail_limit.
+sub __get_lastaccess_times
+{
+	my $itemid = shift;
+	my $probe_avail_limit = shift;
+	my $from = shift;
+	my $till = shift;
+
+	my ($rows_ref, @times, $last_status);
+
+	# get the previous status
+	$rows_ref = db_select(
+		"select clock,value".
+		" from history_uint".
+		" where itemid=$itemid".
+			" and clock between ".($from-3600)." and ".($from-1).
+		" order by itemid desc,clock desc".
+		" limit 1");
+
+	$last_status = UP;
+	if (scalar(@$rows_ref) != 0)
+	{
+		my $clock = $rows_ref->[0]->[0];
+		my $value = $rows_ref->[0]->[1];
+
+		dbg("clock:$clock value:$value");
+
+		$last_status = DOWN if ($clock - $value > $probe_avail_limit);
+	}
+
+	push(@times, truncate_from($from)) if ($last_status == UP);
+
+	$rows_ref = db_select(
+		"select clock,value".
+		" from history_uint".
+		" where itemid=$itemid".
+	    		" and clock between $from and $till".
+	    		" and value!=0".
+		" order by itemid,clock");
+
+	foreach my $row_ref (@$rows_ref)
+	{
+		my $clock = $row_ref->[0];
+		my $value = $row_ref->[1];
+
+		my $status = ($clock - $value > $probe_avail_limit) ? DOWN : UP;
+
+		if ($last_status != $status)
+		{
+			if ($status == UP)
+			{
+				$clock = truncate_from($clock);
+			}
+			else
+			{
+				$clock = truncate_till($clock);
+			}
+
+			push(@times, $clock);
+
+			dbg("clock:$clock diff:", ($clock - $value));
+
+			$last_status = $status;
+		}
+	}
+
+	# push "till" to @times if it contains odd number of elements
+	if (scalar(@times) != 0)
+	{
+		push(@times, $till) if ($last_status == UP);
+	}
+
+	return \@times;
+}
+
+sub __get_probestatus_times
+{
+	my $probe = shift;
+	my $hostid = shift;
+	my $times_ref = shift; # input
+	my $key = shift;
+
+	my ($rows_ref, @times, $last_status);
+
+	my $key_match = "i.key_";
+	$key_match .= ($key =~ m/%/) ? " like '$key'" : "='$key'";
+
+	my $itemid;
+	if ($key =~ m/%/)
+	{
+		$itemid = get_itemid_like_by_hostid($hostid, $key);
+	}
+	else
+	{
+		$itemid = get_itemid_by_hostid($hostid, $key);
+	}
+
+	if (!$itemid)
+	{
+		wrn("configuration error: ", rsm_slv_error());
+		return;
+	}
+
+	$rows_ref = db_select(
+		"select value".
+		" from history_uint".
+		" where itemid=$itemid".
+			" and clock<" . $times_ref->[0].
+		" order by clock desc".
+		" limit 1");
+
+	$last_status = UP;
+	if (scalar(@$rows_ref) != 0)
+	{
+		my $value = $rows_ref->[0]->[0];
+
+		$last_status = DOWN if ($value == OFFLINE);
+	}
+
+	my $idx = 0;
+	my $times_count = scalar(@$times_ref);
+	while ($idx < $times_count)
+	{
+		my $from = $times_ref->[$idx++];
+		my $till = $times_ref->[$idx++];
+
+		$rows_ref = db_select(
+			"select clock,value".
+			" from history_uint".
+			" where itemid=$itemid".
+				" and clock between $from and $till".
+			" order by itemid,clock");
+
+		push(@times, $from) if ($last_status == UP);
+
+		foreach my $row_ref (@$rows_ref)
+		{
+			my $clock = $row_ref->[0];
+			my $value = $row_ref->[1];
+
+			my $status = ($value == OFFLINE) ? DOWN : UP;
+
+			if ($last_status != $status)
+			{
+				push(@times, $clock);
+
+				dbg("clock:$clock value:$value");
+
+				$last_status = $status;
+			}
+		}
+
+		# push "till" to @times if it contains odd number of elements
+		if (scalar(@times) != 0)
+		{
+			push(@times, $till) if ($last_status == UP);
+		}
+	}
+
+	return \@times;
+}
