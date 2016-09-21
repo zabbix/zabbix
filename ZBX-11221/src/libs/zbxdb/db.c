a291fe3cc635e171320afdf6d4fc7e3a965ba6a1
vladislavs <vladislavs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-09-21 13:51:22 +0000

    .......PS. [ZBX-11221] don't set auto commit if database connection failed; don't allow to use database if autocommit failed
    git-svn-id: svn://svn.zabbix.com/branches/dev@62650 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-11221/src/libs/zbxdb/db.c
+++ ZBX-11221/src/libs/zbxdb/db.c
@@ -321,8 +321,11 @@ int	zbx_db_connect(char *host, char *user, char *password, char *dbname, char *d
 		ret = ZBX_DB_FAIL;
 	}
 
-	if (0 != mysql_autocommit(conn, 1))
-		zabbix_log(LOG_LEVEL_WARNING, "Cannot set MySQL autocommit option.");
+	if (ZBX_DB_OK == ret && 0 != mysql_autocommit(conn, 1))
+	{
+		zabbix_errlog(ERR_Z3001, dbname, mysql_errno(conn), mysql_error(conn));
+		ret = ZBX_DB_FAIL;
+	}
 
 	if (ZBX_DB_OK == ret && 0 != mysql_select_db(conn, dbname))
 	{
