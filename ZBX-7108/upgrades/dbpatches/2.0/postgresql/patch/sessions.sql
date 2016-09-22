332641964069f8b68085303f628adec006a068ee
vladislavs <vladislavs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-09-22 11:41:15 +0000

    .......... [ZBX-7108] removing merged development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@62687 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-7108/upgrades/dbpatches/2.0/postgresql/patch/sessions.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE ONLY sessions ALTER userid DROP DEFAULT;
-DELETE FROM sessions WHERE NOT EXISTS (SELECT 1 FROM users WHERE users.userid=sessions.userid);
-ALTER TABLE ONLY sessions ADD CONSTRAINT c_sessions_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
