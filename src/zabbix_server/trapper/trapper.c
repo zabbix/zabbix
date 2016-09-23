bf602dae3bf6bab5459a2d39596ce82810d20dd5
vladislavs <vladislavs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-09-23 11:57:42 +0000

    .......PS. [ZBX-5448] fixed possible delay when proxy sends cached history to server
    git-svn-id: svn://svn.zabbix.com/branches/3.0@62731 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_server/trapper/trapper.c
+++ src/zabbix_server/trapper/trapper.c
@@ -113,7 +113,7 @@ static void	send_proxyhistory(zbx_socket_t *sock, zbx_timespec_t *ts)
 
 	struct zbx_json	j;
 	zbx_uint64_t	lastid;
-	int		records, ret = FAIL;
+	int		ret = FAIL;
 	char		*error = NULL;
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
@@ -128,7 +128,7 @@ static void	send_proxyhistory(zbx_socket_t *sock, zbx_timespec_t *ts)
 
 	zbx_json_addarray(&j, ZBX_PROTO_TAG_DATA);
 
-	records = proxy_get_hist_data(&j, &lastid);
+	proxy_get_hist_data(&j, &lastid);
 
 	zbx_json_close(&j);
 
@@ -144,7 +144,7 @@ static void	send_proxyhistory(zbx_socket_t *sock, zbx_timespec_t *ts)
 	if (SUCCEED != zbx_recv_response(sock, CONFIG_TIMEOUT, &error))
 		goto out;
 
-	if (0 != records)
+	if (0 != lastid)
 		proxy_set_hist_lastid(lastid);
 
 	ret = SUCCEED;
