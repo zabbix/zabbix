971cb5e44d1a20f3b9e4f59c9d50d716b5bf5587
sergejs <sergejs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-10-03 06:37:17 +0000

    .......... [ZBX-11122] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@62929 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-11122/frontends/php/app/controllers/CControllerAcknowledgeEdit.php
@@ -0,0 +1,151 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CControllerAcknowledgeEdit extends CController {
+
+	protected function init() {
+		$this->disableSIDValidation();
+	}
+
+	protected function checkInput() {
+		$fields = [
+			'eventids' =>			'required|array_db acknowledges.eventid',
+			'message' =>			'db acknowledges.message',
+			'acknowledge_type' =>	'in '.ZBX_ACKNOWLEDGE_SELECTED.','.ZBX_ACKNOWLEDGE_PROBLEM.','.ZBX_ACKNOWLEDGE_ALL,
+			'backurl' =>			'string'
+		];
+
+		$ret = $this->validateInput($fields);
+
+		if ($ret) {
+			$backurl = $this->getInput('backurl', 'tr_status.php');
+
+			switch (parse_url($backurl, PHP_URL_PATH)) {
+				case 'events.php':
+				case 'overview.php':
+				case 'screenedit.php':
+				case 'screens.php':
+				case 'slides.php':
+				case 'tr_events.php':
+				case 'tr_status.php':
+				case 'zabbix.php':
+					break;
+
+				default:
+					$ret = false;
+			}
+		}
+
+		if (!$ret) {
+			$this->setResponse(new CControllerResponseFatal());
+		}
+
+		return $ret;
+	}
+
+	protected function checkPermissions() {
+		$events = API::Event()->get([
+			'eventids' => $this->getInput('eventids'),
+			'source' => EVENT_SOURCE_TRIGGERS,
+			'object' => EVENT_OBJECT_TRIGGER,
+			'countOutput' => true
+		]);
+
+		return ($events == count($this->getInput('eventids')));
+	}
+
+	protected function doAction() {
+		$data = [
+			'sid' => $this->getUserSID(),
+			'eventids' => $this->getInput('eventids'),
+			'message' => $this->getInput('message', ''),
+			'acknowledge_type' => $this->getInput('acknowledge_type', ZBX_ACKNOWLEDGE_SELECTED),
+			'backurl' => $this->getInput('backurl', 'tr_status.php'),
+			'unack_problem_events_count' => 0,
+			'unack_events_count' => 0
+		];
+
+		if (count($this->getInput('eventids')) == 1) {
+			$events = API::Event()->get([
+				'output' => [],
+				'eventids' => $this->getInput('eventids'),
+				'source' => EVENT_SOURCE_TRIGGERS,
+				'object' => EVENT_OBJECT_TRIGGER,
+				'select_acknowledges' => ['clock', 'message', 'alias', 'name', 'surname']
+			]);
+
+			if ($events) {
+				$data['event'] = [
+					'acknowledges' => $events[0]['acknowledges']
+				];
+				order_result($data['acknowledges'], 'clock', ZBX_SORT_DOWN);
+			}
+		}
+
+		$events = API::Event()->get([
+			'output' => ['objectid', 'acknowledged', 'value'],
+			'eventids' => $this->getInput('eventids'),
+			'source' => EVENT_SOURCE_TRIGGERS,
+			'object' => EVENT_OBJECT_TRIGGER
+		]);
+
+		$triggerids = [];
+
+		foreach ($events as $event) {
+			if ($event['acknowledged'] == EVENT_ACKNOWLEDGED) {
+				$data['unack_problem_events_count']++;
+				$data['unack_events_count']++;
+			}
+			elseif ($event['value'] == TRIGGER_VALUE_FALSE) {
+				$data['unack_problem_events_count']++;
+			}
+			$triggerids[$event['objectid']] = true;
+		}
+
+		$triggerids = array_keys($triggerids);
+
+		$data['unack_problem_events_count'] += API::Event()->get([
+			'countOutput' => true,
+			'source' => EVENT_SOURCE_TRIGGERS,
+			'object' => EVENT_OBJECT_TRIGGER,
+			'objectids' => $triggerids,
+			'filter' => [
+				'acknowledged' => EVENT_NOT_ACKNOWLEDGED,
+				'value' => TRIGGER_VALUE_TRUE
+			]
+		]);
+
+		$data['unack_events_count'] += API::Event()->get([
+			'countOutput' => true,
+			'source' => EVENT_SOURCE_TRIGGERS,
+			'object' => EVENT_OBJECT_TRIGGER,
+			'objectids' => $triggerids,
+			'filter' => [
+				'acknowledged' => EVENT_NOT_ACKNOWLEDGED
+			]
+		]);
+
+		$response = new CControllerResponseData($data);
+		$response->setTitle(_('Alarm acknowledgements'));
+		$this->setResponse($response);
+	}
+}
+
---
+++ ZBX-11122/frontends/php/tests/selenium/testFormAdministrationGeneralImages.php
@@ -0,0 +1,158 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+require_once dirname(__FILE__).'/../include/class.cwebtest.php';
+
+class testFormAdministrationGeneralImages extends CWebTest {
+	public $icon_image_name = '1image1';
+	public $icon_image_name2 = '2image2';
+	public $bg_image_name = '1bgimage1';
+	public $bg_image_name2 = '2bgimage2';
+
+	public function testFormAdministrationGeneralImages_CheckLayout() {
+
+		$this->zbxTestLogin('adm.gui.php');
+		$this->zbxTestAssertElementPresentId('configDropDown');
+		$this->zbxTestDropdownSelectWait('configDropDown', 'Images');
+		$this->zbxTestCheckTitle('Configuration of images');
+		$this->zbxTestCheckHeader('Images');
+		$this->zbxTestTextPresent(['Images', 'Type']);
+		$this->zbxTestAssertElementPresentXpath("//select[@id='imagetype']/option[text()='Icon']");
+		$this->zbxTestAssertElementPresentXpath("//select[@id='imagetype']/option[text()='Background']");
+		$this->zbxTestAssertElementPresentId('form');
+		$this->zbxTestClickWait('form');
+
+		$this->zbxTestAssertElementPresentId('name');
+		$this->zbxTestAssertAttribute("//input[@id='name']", "maxlength", '64');
+		$this->zbxTestAssertElementPresentId('image');
+		$this->zbxTestAssertElementPresentId('add');
+		$this->zbxTestAssertElementPresentId('cancel');
+
+	}
+
+	public function testFormAdministrationGeneralImages_AddImage() {
+
+		$this->zbxTestLogin('adm.images.php');
+		$this->zbxTestClickWait('form');
+
+		$this->zbxTestAssertElementPresentId('name');
+		$this->zbxTestInputType('name', $this->icon_image_name);
+		$this->zbxTestInputType('image', PHPUNIT_BASEDIR.'/tests/images/image.png');
+		$this->zbxTestClickWait('add');
+		$this->zbxTestCheckTitle('Configuration of images');
+		$this->zbxTestCheckHeader('Images');
+		$this->zbxTestTextPresent(['Images', 'Type', 'Image added']);
+
+		$sql = 'SELECT * FROM images WHERE imagetype=1 AND name=\''.$this->icon_image_name.'\'';
+		$this->assertEquals(1, DBcount($sql), 'Chuck Norris: Image with such name has not been added to the DB');
+
+	}
+
+	public function testFormAdministrationGeneralImages_CancelIconImageChanges() {
+
+		$sqlIcons = 'SELECT * FROM images WHERE imagetype=1';
+		$oldHashIcons=DBhash($sqlIcons);
+
+		$this->zbxTestLogin('adm.images.php');
+		$this->zbxTestClickLinkText($this->icon_image_name);
+		$this->zbxTestInputTypeWait('name', $this->icon_image_name2);
+		$this->zbxTestInputTypeWait('image', PHPUNIT_BASEDIR.'/tests/images/image.png');
+		$this->zbxTestClick('cancel');
+
+		// checking that image has not been changed after clicking on the "Cancel" button in the confirm dialog box
+		$this->assertEquals($oldHashIcons, DBhash($sqlIcons), 'Chuck Norris: No-change images update should not update data in table "images"');
+
+	}
+
+	public function testFormAdministrationGeneralImages_UpdateImage() {
+
+		$this->zbxTestLogin('adm.images.php');
+		$this->zbxTestClickLinkText($this->icon_image_name);
+		$this->zbxTestInputTypeOverwrite('name', $this->icon_image_name2);
+		$this->zbxTestInputType('image', PHPUNIT_BASEDIR.'/tests/images/image.png');
+		$this->zbxTestClickWait('update');
+		$this->zbxTestCheckTitle('Configuration of images');
+		$this->zbxTestWaitUntilMessageTextPresent('msg-good', 'Image updated');;
+
+		$sql = 'SELECT * FROM images WHERE imagetype=1 AND name=\''.$this->icon_image_name2.'\'';
+		$this->assertEquals(1, DBcount($sql), 'Chuck Norris: Image with such name does not exist in the DB');
+	}
+
+	public function testFormAdministrationGeneralImages_DeleteImage() {
+
+		$this->zbxTestLogin('adm.images.php');
+		$this->zbxTestClickLinkTextWait($this->icon_image_name2);
+		$this->zbxTestClickWait('delete');
+		$this->webDriver->switchTo()->alert()->accept();
+		$this->zbxTestCheckTitle('Configuration of images');
+		$this->zbxTestWaitUntilMessageTextPresent('msg-good', 'Image deleted');
+		$this->zbxTestTextPresent(['Images', 'Image deleted']);
+
+		$sql = 'SELECT * FROM images WHERE imagetype=1 AND name=\''.$this->icon_image_name2.'\'';
+		$this->assertEquals(0, DBcount($sql), 'Chuck Norris: Image with such name still exist in the DB');
+	}
+
+	public function testFormAdministrationGeneralImages_AddBgImage() {
+
+		$this->zbxTestLogin('adm.images.php');
+		$this->zbxTestDropdownSelectWait('imagetype', 'Background');
+		$this->zbxTestClickWait('form');
+		$this->zbxTestInputType('name', $this->bg_image_name);
+		$this->zbxTestInputType('image', PHPUNIT_BASEDIR.'/tests/images/image.png');
+		$this->zbxTestClickWait('add');
+		$this->zbxTestCheckTitle('Configuration of images');
+		$this->zbxTestTextPresent(['Images', 'Type', 'Image added']);
+
+		$sql = 'SELECT * FROM images WHERE imagetype=2 AND name=\''.$this->bg_image_name.'\'';
+		$this->assertEquals(1, DBcount($sql), 'Chuck Norris: Image with such name has not been added to the DB');
+	}
+
+	public function testFormAdministrationGeneralImages_UpdateBgImage() {
+
+		$this->zbxTestLogin('adm.images.php');
+		$this->zbxTestDropdownSelectWait('imagetype', 'Background');
+		$this->zbxTestTextPresent('Type');
+		$this->zbxTestWaitUntilElementVisible(WebdriverBy::xpath("//div[@class='cell']"));
+		$this->zbxTestClickLinkText($this->bg_image_name);
+		$this->zbxTestInputTypeOverwrite('name', $this->bg_image_name2);
+		$this->zbxTestInputTypeWait('image', PHPUNIT_BASEDIR.'/tests/images/image.png');
+		$this->zbxTestClickWait('update');
+		$this->zbxTestCheckTitle('Configuration of images');
+		$this->zbxTestWaitUntilMessageTextPresent('msg-good', 'Image updated');
+
+		$sql = 'SELECT * FROM images WHERE imagetype=2 AND name=\''.$this->bg_image_name2.'\'';
+		$this->assertEquals(1, DBcount($sql), 'Chuck Norris: Image with such name does not exist in the DB');
+	}
+
+	public function testFormAdministrationGeneralImages_DeleteBgImage() {
+
+		$this->zbxTestLogin('adm.images.php');
+		$this->zbxTestDropdownSelectWait('imagetype', 'Background');
+		$this->zbxTestClickLinkTextWait($this->bg_image_name2);
+		$this->zbxTestClickWait('delete');
+		$this->webDriver->switchTo()->alert()->accept();
+		$this->zbxTestCheckHeader('Images');
+		$this->zbxTestTextPresent(['Images', 'Image deleted']);
+
+		$sql = 'SELECT * FROM images WHERE imagetype=2 AND name=\''.$this->bg_image_name2.'\'';
+		$this->assertEquals(0, DBcount($sql), 'Chuck Norris: Image with such name still exist in the DB');
+	}
+
+}
