6ca14df3ac9f441c932ba5615cbc58bc7c6ce78d
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-10-11 09:45:57 +0000

    .......... [ZBX-11291] removed development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@63087 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-11291/upgrades/dbpatches/2.0/oracle/patch/profiles.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE profiles MODIFY profileid DEFAULT NULL;
-ALTER TABLE profiles MODIFY userid DEFAULT NULL;
-DELETE FROM profiles WHERE NOT userid IN (SELECT userid FROM users);
-DELETE FROM profiles WHERE idx LIKE 'web.%.sort' OR idx LIKE 'web.%.sortorder';
-ALTER TABLE profiles ADD CONSTRAINT c_profiles_1 FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE;
-
-UPDATE profiles SET idx = 'web.screens.period' WHERE idx = 'web.charts.period';
-UPDATE profiles SET idx = 'web.screens.stime' WHERE idx = 'web.charts.stime';
-UPDATE profiles SET idx = 'web.screens.timelinefixed' WHERE idx = 'web.charts.timelinefixed';
