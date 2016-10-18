3655645e268d22f5ddc42137af9a9a55c2242219
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-10-18 13:49:21 +0000

    ........S. [ZBXNEXT-1895] fixed formatting
    git-svn-id: svn://svn.zabbix.com/branches/dev@63245 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-1859/src/libs/zbxdbhigh/db.c
+++ ZBXNEXT-1859/src/libs/zbxdbhigh/db.c
@@ -1732,8 +1732,8 @@ void	DBregister_host_flush(zbx_vector_ptr_t *autoreg_hosts, zbx_uint64_t proxy_h
 
 		ts.sec = autoreg_host->now;
 
-		add_event(0, EVENT_SOURCE_AUTO_REGISTRATION, EVENT_OBJECT_ZABBIX_ACTIVE, autoreg_host->autoreg_hostid, &ts,
-				TRIGGER_VALUE_PROBLEM, NULL, NULL, 0, 0);
+		add_event(0, EVENT_SOURCE_AUTO_REGISTRATION, EVENT_OBJECT_ZABBIX_ACTIVE, autoreg_host->autoreg_hostid,
+				&ts, TRIGGER_VALUE_PROBLEM, NULL, NULL, 0, 0);
 	}
 
 	if (0 != new)
