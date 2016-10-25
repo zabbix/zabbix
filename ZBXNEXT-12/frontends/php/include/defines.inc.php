c2be85472704c00ef3eb5fea6cdb3448f8c10cf0
dimir <dimir@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-10-25 13:12:15 +0000

    .......... [ZBXNEXT-12] merge from upstream
    git-svn-id: svn://svn.zabbix.com/branches/dev@63351 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-12/frontends/php/include/defines.inc.php
+++ ZBXNEXT-12/frontends/php/include/defines.inc.php
@@ -22,7 +22,7 @@
 define('ZABBIX_VERSION',		'3.3.0');
 define('ZABBIX_API_VERSION',	'3.3.0');
 define('ZABBIX_EXPORT_VERSION',	'3.2');
-define('ZABBIX_DB_VERSION',		3020000);
+define('ZABBIX_DB_VERSION',		3030011);
 
 define('ZABBIX_COPYRIGHT_FROM',	'2001');
 define('ZABBIX_COPYRIGHT_TO',	'2016');
