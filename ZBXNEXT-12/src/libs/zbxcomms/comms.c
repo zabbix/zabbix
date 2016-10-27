2570c758095dddfd1aa9f83d7fa899e86f87d106
vladislavs <vladislavs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-10-27 05:59:40 +0000

    ...G...... [ZBXNEXT-12] handle round robin DNS for subnet match
    git-svn-id: svn://svn.zabbix.com/branches/dev@63367 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-12/src/libs/zbxcomms/comms.c
+++ ZBXNEXT-12/src/libs/zbxcomms/comms.c
@@ -1886,7 +1886,7 @@ int	zbx_tcp_check_security(zbx_socket_t *s, const char *ip_list, int allow_if_em
 {
 #if defined(HAVE_IPV6)
 	struct addrinfo	hints, *ai = NULL, *current_ai;
-	int		prefix_size_ipv6;
+	int		prefix_size_ipv6, prefix_tmp;
 #else
 	struct hostent	*hp;
 	int		i;
@@ -1948,10 +1948,8 @@ int	zbx_tcp_check_security(zbx_socket_t *s, const char *ip_list, int allow_if_em
 		{
 			for (current_ai = ai; NULL != current_ai; current_ai = current_ai->ai_next)
 			{
-				if (AF_INET != current_ai->ai_family)
-					prefix_size = prefix_size_ipv6;
-
-				if (SUCCEED == zbx_ip_cmp(prefix_size, current_ai, name))
+				prefix_tmp = current_ai->ai_family == AF_INET ? prefix_size : prefix_size_ipv6;
+				if (SUCCEED == zbx_ip_cmp(prefix_tmp, current_ai, name))
 				{
 					freeaddrinfo(ai);
 					return SUCCEED;
