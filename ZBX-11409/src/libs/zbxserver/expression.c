99568210231f90b9320d76a8c33e1b5752517453
viktors <viktors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-11-02 08:18:30 +0000

    ........S. [ZBX-11409] improved fix for order of operations in substitute_simple_macros()
    git-svn-id: svn://svn.zabbix.com/branches/dev@63486 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-11409/src/libs/zbxserver/expression.c
+++ ZBX-11409/src/libs/zbxserver/expression.c
@@ -3765,28 +3765,27 @@ int	substitute_simple_macros(zbx_uint64_t *actionid, const DB_EVENT *event, cons
 			}
 		}
 
-		if (NULL != replace_to)
+		if (ZBX_TOKEN_FUNC_MACRO == token.type && NULL != replace_to)
 		{
-			if (ZBX_TOKEN_FUNC_MACRO == token.type && 0 != func_macro)
+			if (0 != func_macro)
 			{
 				ret = zbx_calculate_macro_function(*data + token.data.func_macro.func.l,
 						token.data.func_macro.func.r - token.data.func_macro.func.l + 1,
 						&replace_to);
 			}
-
-			if (1 == require_numeric)
-			{
-				if (SUCCEED == (res = is_double_suffix(replace_to, ZBX_FLAG_DOUBLE_SUFFIX)))
-					wrap_negative_double_suffix(&replace_to, NULL);
-				else if (NULL != error)
-					zbx_snprintf(error, maxerrlen, "Macro '%s' value is not numeric", m);
-			}
-
-			if (ZBX_TOKEN_FUNC_MACRO == token.type && 0 == func_macro)
+			else
 			{
 				/* ignore functions with macros not supporting them */
 				zbx_free(replace_to);
 			}
+		}
+
+		if (1 == require_numeric && NULL != replace_to)
+		{
+			if (SUCCEED == (res = is_double_suffix(replace_to, ZBX_FLAG_DOUBLE_SUFFIX)))
+				wrap_negative_double_suffix(&replace_to, NULL);
+			else if (NULL != error)
+				zbx_snprintf(error, maxerrlen, "Macro '%s' value is not numeric", m);
 		}
 
 		if (FAIL == ret)
