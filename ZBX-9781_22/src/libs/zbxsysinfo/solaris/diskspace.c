bee70db896a519d981207e8999405691435ffa80
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-11-09 08:49:21 +0000

    ...G...... [ZBX-9781] fixed vfs.fs.size check on solaris
    git-svn-id: svn://svn.zabbix.com/branches/dev@63633 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-9781_22/src/libs/zbxsysinfo/solaris/diskspace.c
+++ ZBX-9781_22/src/libs/zbxsysinfo/solaris/diskspace.c
@@ -161,7 +161,7 @@ static int	vfs_fs_size(AGENT_REQUEST *request, AGENT_RESULT *result)
 
 int	VFS_FS_SIZE(AGENT_REQUEST *request, AGENT_RESULT *result)
 {
-	return zbx_execute_threaded_metric(vfs_fs_size, request
+	return zbx_execute_threaded_metric(vfs_fs_size, request, result);
 }
 
 int	VFS_FS_DISCOVERY(AGENT_REQUEST *request, AGENT_RESULT *result)
