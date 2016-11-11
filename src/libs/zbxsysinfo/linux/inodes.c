2880b7e69ae6d27b3886960fc377768edcc195fa
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-11-11 10:12:17 +0000

    ...G...... [ZBX-9781] changed vfs.fs.size, vfs.fs.inode checks to be executed in separate process/thread to avoid lockup with stale NFS mounts
    git-svn-id: svn://svn.zabbix.com/branches/2.2@63717 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxsysinfo/linux/inodes.c
+++ src/libs/zbxsysinfo/linux/inodes.c
@@ -20,7 +20,7 @@
 #include "common.h"
 #include "sysinfo.h"
 
-int	VFS_FS_INODE(AGENT_REQUEST *request, AGENT_RESULT *result)
+static int	vfs_fs_inode(AGENT_REQUEST *request, AGENT_RESULT *result)
 {
 #ifdef HAVE_SYS_STATVFS_H
 #	define ZBX_STATFS	statvfs
@@ -80,4 +80,9 @@ int	VFS_FS_INODE(AGENT_REQUEST *request, AGENT_RESULT *result)
 		return SYSINFO_RET_FAIL;
 
 	return SYSINFO_RET_OK;
+}
+
+int	VFS_FS_INODE(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+	return zbx_execute_threaded_metric(vfs_fs_inode, request, result);
 }
