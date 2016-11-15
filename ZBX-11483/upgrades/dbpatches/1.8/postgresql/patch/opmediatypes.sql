e4e9a90e16b74e4c8b97f0a7bb6dc86032ce7a33
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-11-15 16:26:23 +0000

    .......... [ZBX-11483] removed dev branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@63802 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-11483/upgrades/dbpatches/1.8/postgresql/patch/opmediatypes.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE opmediatypes (
-        opmediatypeid           bigint          DEFAULT '0'     NOT NULL,
-        operationid             bigint          DEFAULT '0'     NOT NULL,
-        mediatypeid             bigint          DEFAULT '0'     NOT NULL,
-        PRIMARY KEY (opmediatypeid)
-) with OIDS;
-CREATE UNIQUE INDEX opmediatypes_1 on opmediatypes (operationid);
