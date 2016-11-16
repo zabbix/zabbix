447a5ffac702f2c223ba0e11c364c1acdd6e7e06
viktors <viktors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-11-16 14:19:41 +0000

    ........S. [ZBX-11411] added check for LLD macro values in calculated item formula to be numeric 	   [merge ^/branches/3.2 -c r63821]
    git-svn-id: svn://svn.zabbix.com/trunk@63822 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxdbhigh/lld_item.c
+++ src/libs/zbxdbhigh/lld_item.c
@@ -858,7 +858,9 @@ static int	substitute_formula_macros(char **data, struct zbx_json_parse *jp_row,
 		/* substitute LLD macros in the part of the string preceding function parameters */
 
 		zbx_strncpy_alloc(&tmp, &tmp_alloc, &tmp_offset, e, par_l + 1);
-		substitute_lld_macros(&tmp, jp_row, ZBX_MACRO_ANY, NULL, NULL, 0);
+		if (SUCCEED != substitute_lld_macros(&tmp, jp_row, ZBX_MACRO_NUMERIC, NULL, error, max_error_len))
+			goto out;
+
 		tmp_offset = strlen(tmp);
 		zbx_strncpy_alloc(&exp, &exp_alloc, &exp_offset, tmp, tmp_offset);
 
@@ -931,7 +933,9 @@ static int	substitute_formula_macros(char **data, struct zbx_json_parse *jp_row,
 	/* substitute LLD macros in the remaining part */
 
 	zbx_strcpy_alloc(&tmp, &tmp_alloc, &tmp_offset, e);
-	substitute_lld_macros(&tmp, jp_row, ZBX_MACRO_ANY, NULL, NULL, 0);
+	if (SUCCEED != substitute_lld_macros(&tmp, jp_row, ZBX_MACRO_NUMERIC, NULL, error, max_error_len))
+		goto out;
+
 	zbx_strcpy_alloc(&exp, &exp_alloc, &exp_offset, tmp);
 
 	ret = SUCCEED;
