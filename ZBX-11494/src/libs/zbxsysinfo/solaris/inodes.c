f37ef557d41a586ae17aa1228efa66b098947d0c
viktors <viktors@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-11-17 07:16:16 +0000

    .......... [ZBX-11494] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@63828 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-11494/src/libs/zbxsysinfo/solaris/inodes.c
@@ -0,0 +1,88 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+#include "sysinfo.h"
+
+static int	vfs_fs_inode(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+#ifdef HAVE_SYS_STATVFS_H
+#	define ZBX_STATFS	statvfs
+#	define ZBX_FFREE	f_favail
+#else
+#	define ZBX_STATFS	statfs
+#	define ZBX_FFREE	f_ffree
+#endif
+	char			*fsname, *mode;
+	zbx_uint64_t		total;
+	struct ZBX_STATFS	s;
+
+	if (2 < request->nparam)
+		return SYSINFO_RET_FAIL;
+
+	fsname = get_rparam(request, 0);
+	mode = get_rparam(request, 1);
+
+	if (NULL == fsname || '\0' == *fsname || 0 != ZBX_STATFS(fsname, &s))
+		return SYSINFO_RET_FAIL;
+
+	if (NULL == mode || '\0' == *mode || 0 == strcmp(mode, "total"))	/* default parameter */
+	{
+		SET_UI64_RESULT(result, s.f_files);
+	}
+	else if (0 == strcmp(mode, "free"))
+	{
+		SET_UI64_RESULT(result, s.ZBX_FFREE);
+	}
+	else if (0 == strcmp(mode, "used"))
+	{
+		SET_UI64_RESULT(result, s.f_files - s.f_ffree);
+	}
+	else if (0 == strcmp(mode, "pfree"))
+	{
+		total = s.f_files;
+#ifdef HAVE_SYS_STATVFS_H
+		total -= s.f_ffree - s.f_favail;
+#endif
+		if (0 != total)
+			SET_DBL_RESULT(result, (double)(100.0 * s.ZBX_FFREE) / total);
+		else
+			return SYSINFO_RET_FAIL;
+	}
+	else if (0 == strcmp(mode, "pused"))
+	{
+		total = s.f_files;
+#ifdef HAVE_SYS_STATVFS_H
+		total -= s.f_ffree - s.f_favail;
+#endif
+		if (0 != total)
+			SET_DBL_RESULT(result, 100.0 - (double)(100.0 * s.ZBX_FFREE) / total);
+		else
+			return SYSINFO_RET_FAIL;
+	}
+	else
+		return SYSINFO_RET_FAIL;
+
+	return SYSINFO_RET_OK;
+}
+
+int	VFS_FS_INODE(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+	return zbx_execute_threaded_metric(vfs_fs_inode, request, result);
+}
---
+++ ZBX-11494/upgrades/dbpatches/1.4/mysql/patch/media.sql
@@ -0,0 +1,16 @@
+CREATE TABLE media_tmp (
+	mediaid		bigint unsigned		DEFAULT '0'	NOT NULL,
+	userid		bigint unsigned		DEFAULT '0'	NOT NULL,
+	mediatypeid		bigint unsigned		DEFAULT '0'	NOT NULL,
+	sendto		varchar(100)		DEFAULT ''	NOT NULL,
+	active		integer		DEFAULT '0'	NOT NULL,
+	severity		integer		DEFAULT '63'	NOT NULL,
+	period		varchar(100)		DEFAULT '1-7,00:00-23:59'	NOT NULL,
+	PRIMARY KEY (mediaid)
+) ENGINE=InnoDB;
+CREATE INDEX media_1 on media_tmp (userid);
+CREATE INDEX media_2 on media_tmp (mediatypeid);
+
+insert into media_tmp select mediaid,userid,mediatypeid,sendto,active,severity,period from media;
+drop table media;
+alter table media_tmp rename media;
