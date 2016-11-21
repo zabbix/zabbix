fc8e0d170248e7c19925b9e6fee58cd745c5811a
sergejs <sergejs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-11-21 09:36:26 +0000

    .......... [ZBX-11395] removed unnecessary development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@63870 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-11395/upgrades/dbpatches/2.0/oracle/patch/mappings.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE mappings MODIFY mappingid DEFAULT NULL;
-ALTER TABLE mappings MODIFY valuemapid DEFAULT NULL;
-DELETE FROM mappings WHERE NOT valuemapid IN (SELECT valuemapid FROM valuemaps);
-ALTER TABLE mappings ADD CONSTRAINT c_mappings_1 FOREIGN KEY (valuemapid) REFERENCES valuemaps (valuemapid) ON DELETE CASCADE;
