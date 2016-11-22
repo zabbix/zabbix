33c2193a65dac1a3fb269e332a03c45cb8256d71
ivo <ivo@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-11-22 07:35:49 +0000

    ..F....... [ZBXNEXT-3425] implemented more compact problem view in triggers list, event details and history of events
    git-svn-id: svn://svn.zabbix.com/branches/3.2@63896 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/classes/screens/CScreenEvents.php
+++ frontends/php/include/classes/screens/CScreenEvents.php
@@ -27,30 +27,124 @@ class CScreenEvents extends CScreenBase {
 	 * @return CDiv (screen inside container)
 	 */
 	public function get() {
-		$options = [
+		$config = select_config();
+
+		$table = (new CTableInfo())->setHeader([_('Time'), _('Recovery time'), _('Host'), _('Description'), _('Value'),
+			_('Severity')
+		]);
+
+		$triggers = API::Trigger()->get([
+			'output' => ['triggerid', 'description', 'expression', 'priority'],
+			'selectHosts' => ['hostid', 'name'],
+			'skipDependent' => true,
 			'monitored' => true,
-			'value' => [TRIGGER_VALUE_TRUE, TRIGGER_VALUE_FALSE],
-			'triggerLimit' => $this->screenitem['elements'],
-			'eventLimit' => $this->screenitem['elements']
+			'sortfield' => 'lastchange',
+			'sortorder' => ZBX_SORT_DOWN,
+			'limit' => $this->screenitem['elements'],
+			'preservekeys' => true
+		]);
+
+		$options = [
+			'output' => ['eventid', 'r_eventid', 'objectid', 'clock'],
+			'source' => EVENT_SOURCE_TRIGGERS,
+			'object' => EVENT_OBJECT_TRIGGER,
+			'value' => TRIGGER_VALUE_TRUE,
+			'objectids' => zbx_objectValues($triggers, 'triggerid'),
+			'sortfield' => ['clock', 'eventid'],
+			'sortorder' => ZBX_SORT_DOWN,
+			'limit' => $this->screenitem['elements']
 		];
 
-		$table = (new CTableInfo())->setHeader([_('Time'), _('Host'), _('Description'), _('Value'), _('Severity')]);
+		if ($config['event_ack_enable']) {
+			$options['output'][] = 'acknowledged';
+			$options['select_acknowledges'] = ['userid', 'clock', 'message', 'action'];
+		}
 
-		$events = getLastEvents($options);
+		$events = API::Event()->get($options);
 
-		$config = select_config();
+		$sort_clock = [];
+		$sort_event = [];
+
+		foreach ($events as $key => $event) {
+			if (!array_key_exists($event['objectid'], $triggers)) {
+				continue;
+			}
+
+			$events[$key]['trigger'] = $triggers[$event['objectid']];
+			$events[$key]['host'] = reset($events[$key]['trigger']['hosts']);
+			$sort_clock[$key] = $event['clock'];
+			$sort_event[$key] = $event['eventid'];
+
+			$merged_event = array_merge($event, $triggers[$event['objectid']]);
+			$events[$key]['trigger']['description'] = CMacrosResolverHelper::resolveEventDescription($merged_event);
+		}
+		array_multisort($sort_clock, SORT_DESC, $sort_event, SORT_DESC, $events);
+
+		if ($events) {
+			$r_eventids = [];
+
+			foreach ($events as $event) {
+				$r_eventids[$event['r_eventid']] = true;
+			}
+			unset($r_eventids[0]);
+
+			$r_events = $r_eventids
+				? API::Event()->get([
+					'output' => ['clock'],
+					'source' => EVENT_SOURCE_TRIGGERS,
+					'object' => EVENT_OBJECT_TRIGGER,
+					'eventids' => array_keys($r_eventids),
+					'preservekeys' => true
+				])
+				: [];
+
+			foreach ($events as &$event) {
+				if (array_key_exists($event['r_eventid'], $r_events)) {
+					$event['r_clock'] = $r_events[$event['r_eventid']]['clock'];
+				}
+				else {
+					$event['r_clock'] = 0;
+				}
+			}
+			unset($event);
+		}
 
 		foreach ($events as $event) {
 			$trigger = $event['trigger'];
 			$host = $event['host'];
 
-			$statusSpan = new CSpan(trigger_value2str($event['value']));
+			if ($event['r_eventid'] == 0) {
+				$in_closing = false;
 
-			// add colors and blinking to span depending on configuration and trigger parameters
-			addTriggerValueStyle($statusSpan, $event['value'], $event['clock'], $event['acknowledged']);
+				if ($config['event_ack_enable']) {
+					foreach ($event['acknowledges'] as $acknowledge) {
+						if ($acknowledge['action'] == ZBX_ACKNOWLEDGE_ACTION_CLOSE_PROBLEM) {
+							$in_closing = true;
+							break;
+						}
+					}
+				}
+
+				$value = $in_closing ? TRIGGER_VALUE_FALSE : TRIGGER_VALUE_TRUE;
+				$value_str = $in_closing ? _('CLOSING') : _('PROBLEM');
+				$value_clock = $in_closing ? time() : $event['clock'];
+			}
+			else {
+				$value = TRIGGER_VALUE_FALSE;
+				$value_str = _('RESOLVED');
+				$value_clock = $event['r_clock'];
+			}
+
+			$statusSpan = new CSpan($value_str);
+
+			// Add colors span depending on configuration and trigger parameters.
+			addTriggerValueStyle($statusSpan, $value, $event['clock'],
+				$config['event_ack_enable'] && $event['acknowledged']
+			);
 
 			$table->addRow([
 				zbx_date2str(DATE_TIME_FORMAT_SECONDS, $event['clock']),
+				($event['r_eventid'] == 0) ? '' : zbx_date2str(DATE_TIME_FORMAT_SECONDS, $event['r_clock']),
 				$host['name'],
 				new CLink(
 					$trigger['description'],
