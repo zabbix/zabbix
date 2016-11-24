7f0d0884e24c203467d4fc897a278d13b076dd53
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-11-24 14:32:46 +0000

    .......... [ZBXNEXT-3451] merged from upstream
    git-svn-id: svn://svn.zabbix.com/branches/dev@64017 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBXNEXT-3451_2/frontends/php/items.php
+++ ZBXNEXT-3451_2/frontends/php/items.php
@@ -1156,7 +1156,6 @@ elseif (((hasRequest('action') && getRequest('action') === 'item.massupdateform'
 	$data['hosts'] = API::Host()->get([
 		'output' => ['hostid'],
 		'itemids' => $data['itemids'],
-		'selectItems' => ['itemid'],
 		'selectInterfaces' => API_OUTPUT_EXTEND
 	]);
 	$hostCount = count($data['hosts']);
@@ -1199,7 +1198,7 @@ elseif (((hasRequest('action') && getRequest('action') === 'item.massupdateform'
 
 			// set the initial chosen interface to one of the interfaces the items use
 			$items = API::Item()->get([
-				'itemids' => zbx_objectValues($data['hosts']['items'], 'itemid'),
+				'itemids' => $data['itemids'],
 				'output' => ['itemid', 'type']
 			]);
 			$usedInterfacesTypes = [];
