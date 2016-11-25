5f489d72c0c7de521ab39ae0d3042f2a9e209331
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2016-11-25 07:57:13 +0000

    A.F....... [ZBXNEXT-3570] removed screenitem.iswritable() method

--- frontends/php/include/classes/api/services/CScreenItem.php
+++ frontends/php/include/classes/api/services/CScreenItem.php
@@ -395,32 +395,6 @@ class CScreenItem extends CApiService {
 	}
 
 	/**
-	 * Returns true if the given screen items exist and are available for writing.
-	 *
-	 * @param array $screenItemIds	An array if screen item IDs
-	 *
-	 * @return bool
-	 */
-	public function isWritable(array $screenItemIds) {
-		if (!is_array($screenItemIds)) {
-			return false;
-		}
-		elseif (empty($screenItemIds)) {
-			return true;
-		}
-
-		$screenItemIds = array_unique($screenItemIds);
-
-		$count = $this->get([
-			'screenitemids' => $screenItemIds,
-			'editable' => true,
-			'countOutput' => true
-		]);
-
-		return (count($screenItemIds) == $count);
-	}
-
-	/**
 	 * Validates screen items.
 	 *
 	 * If the $dbScreenItems parameter is given, the screen items will be matched
