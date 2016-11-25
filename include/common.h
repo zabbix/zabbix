76126c48167106cd3e4041a2fbbce13209ef9b03
vladislavs <vladislavs@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2016-11-25 12:26:53 +0000

    ........S. [ZBXNEXT-1667] added support of LLD macros in function parameters of trigger prototype expression, name, description and tags, and in function parameters of graph prototype name
    git-svn-id: svn://svn.zabbix.com/trunk@64054 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- include/common.h
+++ include/common.h
@@ -1217,6 +1217,7 @@ void	zbx_function_param_parse(const char *expr, size_t *param_pos, size_t *lengt
 char	*zbx_function_param_unquote_dyn(const char *param, size_t len, int *quoted);
 int	zbx_function_param_quote(char **param, int forced);
 int	zbx_function_validate(const char *expr, size_t *par_l, size_t *par_r);
+int	zbx_function_validate_parameters(const char *expr, size_t *length);
 int	zbx_function_find(const char *expr, size_t *func_pos, size_t *par_l, size_t *par_r);
 
 void	zbx_alarm_flag_set(void);
@@ -1278,6 +1279,8 @@ typedef struct
 	zbx_strloc_t	macro;
 	/* function + parameters, for example: regsub("([0-9]+)", \1) */
 	zbx_strloc_t	func;
+	/* parameters, for example: ("([0-9]+)", \1) */
+	zbx_strloc_t	func_param;
 }
 zbx_token_func_macro_t;
 
