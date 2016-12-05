89058daa5d4c4dff10e439cdb27b24bdd19cef79
vladislavs <vladislavs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-12-05 13:34:13 +0000

    .......... [ZBXNEXT-3588] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@64194 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-3588/frontends/php/include/views/js/monitoring.sysmap.edit.js.php
@@ -0,0 +1,177 @@
+<script type="text/x-jquery-tmpl" id="user_group_row_tpl">
+	<?= (new CRow([
+			new CCol([
+				(new CTextBox('userGroups[#{usrgrpid}][usrgrpid]', '#{usrgrpid}'))->setAttribute('type', 'hidden'),
+				(new CSpan('#{name}')),
+			]),
+			new CCol(
+				(new CTag('ul', false, [
+					new CTag('li', false, [
+						(new CInput('radio', 'userGroups[#{usrgrpid}][permission]', PERM_READ))
+							->setId('user_group_#{usrgrpid}_permission_'.PERM_READ),
+						(new CTag('label', false, _('Read-only')))
+							->setAttribute('for', 'user_group_#{usrgrpid}_permission_'.PERM_READ)
+					]),
+					new CTag('li', false, [
+						(new CInput('radio', 'userGroups[#{usrgrpid}][permission]', PERM_READ_WRITE))
+							->setId('user_group_#{usrgrpid}_permission_'.PERM_READ_WRITE),
+						(new CTag('label', false, _('Read-write')))
+							->setAttribute('for', 'user_group_#{usrgrpid}_permission_'.PERM_READ_WRITE)
+					])
+				]))->addClass(ZBX_STYLE_RADIO_SEGMENTED)
+			),
+			(new CCol(
+				(new CButton('remove', _('Remove')))
+					->addClass(ZBX_STYLE_BTN_LINK)
+					->onClick('removeUserGroupShares("#{usrgrpid}");')
+			))->addClass(ZBX_STYLE_NOWRAP)
+		]))
+			->setId('user_group_shares_#{usrgrpid}')
+			->toString()
+	?>
+</script>
+
+<script type="text/x-jquery-tmpl" id="user_row_tpl">
+	<?= (new CRow([
+			new CCol([
+				(new CTextBox('users[#{id}][userid]', '#{id}'))->setAttribute('type', 'hidden'),
+				(new CSpan('#{name}')),
+			]),
+			new CCol(
+				(new CTag('ul', false, [
+					new CTag('li', false, [
+						(new CInput('radio', 'users[#{id}][permission]', PERM_READ))
+							->setId('user_#{id}_permission_'.PERM_READ),
+						(new CTag('label', false, _('Read-only')))
+							->setAttribute('for', 'user_#{id}_permission_'.PERM_READ)
+					]),
+					new CTag('li', false, [
+						(new CInput('radio', 'users[#{id}][permission]', PERM_READ_WRITE))
+							->setId('user_#{id}_permission_'.PERM_READ_WRITE),
+						(new CTag('label', false, _('Read-write')))
+							->setAttribute('for', 'user_#{id}_permission_'.PERM_READ_WRITE)
+					])
+				]))->addClass(ZBX_STYLE_RADIO_SEGMENTED)
+			),
+			(new CCol(
+				(new CButton('remove', _('Remove')))
+					->addClass(ZBX_STYLE_BTN_LINK)
+					->onClick('removeUserShares("#{id}");')
+			))->addClass(ZBX_STYLE_NOWRAP)
+		]))
+			->setId('user_shares_#{id}')
+			->toString()
+	?>
+</script>
+
+<script type="text/javascript">
+	jQuery(function($) {
+		var inputs = '#label_type_hostgroup, #label_type_host, #label_type_trigger, #label_type_map, #label_type_image';
+
+		$('#label_format').click(function() {
+			var toggle = $('#label_format').is(':checked');
+
+			$(inputs).each(function() {
+				$(this).parentsUntil('ul')
+					.toggle(toggle)
+					.next().toggle($(this).val() == <?= MAP_LABEL_TYPE_CUSTOM ?> && toggle);
+			});
+
+			$('#label_type').parentsUntil('ul').toggle(!toggle);
+		});
+
+		$(inputs).change(function() {
+			$(this).parentsUntil('ul').next().toggle($(this).val() == <?= MAP_LABEL_TYPE_CUSTOM ?>);
+		});
+
+		// clone button
+		$('#clone').click(function() {
+			// Remove buttons, sharing options and inaccessible user message.
+			$('#sysmapid, #delete, #clone, [id^=user_group_shares_], [id^=user_shares_], #inaccessible_user').remove();
+			$('#update')
+				.text(<?= CJs::encodeJson(_('Add')) ?>)
+				.attr({id: 'add', name: 'add'});
+			$('#name').focus();
+
+			// Set map to private.
+			$('input[name=private][value=' + <?= PRIVATE_SHARING ?> + ']').prop('checked', true);
+
+			// Switch to first tab so multiselect is visible and only then add data and resize.
+			$('#tab_sysmap_tab').trigger('click');
+
+			$('#multiselect_userid_wrapper').show();
+
+			// Set current user as owner.
+			$('#userid').multiSelect('addData', {
+				'id': $('#current_user_userid').val(),
+				'name': $('#current_user_fullname').val()
+			});
+		});
+
+		$('#label_format').triggerHandler('click');
+	});
+
+	/**
+	 * @see init.js add.popup event
+	 */
+	function addPopupValues(list) {
+		var i,
+			value,
+			tpl,
+			container;
+
+		for (i = 0; i < list.values.length; i++) {
+			if (empty(list.values[i])) {
+				continue;
+			}
+
+			value = list.values[i];
+			if (typeof value.permission === 'undefined') {
+				if (jQuery('input[name=private]:checked').val() == <?= PRIVATE_SHARING ?>) {
+					value.permission = <?= PERM_READ ?>;
+				}
+				else {
+					value.permission = <?= PERM_READ_WRITE ?>;
+				}
+			}
+
+			switch (list.object) {
+				case 'usrgrpid':
+					if (jQuery('#user_group_shares_' + value.usrgrpid).length) {
+						continue;
+					}
+
+					tpl = new Template(jQuery('#user_group_row_tpl').html());
+
+					container = jQuery('#user_group_list_footer');
+					container.before(tpl.evaluate(value));
+
+					jQuery('#user_group_' + value.usrgrpid + '_permission_' + value.permission + '')
+						.prop('checked', true);
+					break;
+
+				case 'userid':
+					if (jQuery('#user_shares_' + value.id).length) {
+						continue;
+					}
+
+					tpl = new Template(jQuery('#user_row_tpl').html());
+
+					container = jQuery('#user_list_footer');
+					container.before(tpl.evaluate(value));
+
+					jQuery('#user_' + value.id + '_permission_' + value.permission + '')
+						.prop('checked', true);
+					break;
+			}
+		}
+	}
+
+	function removeUserGroupShares(usrgrpid) {
+		jQuery('#user_group_shares_' + usrgrpid).remove();
+	}
+
+	function removeUserShares(userid) {
+		jQuery('#user_shares_' + userid).remove();
+	}
+</script>
