e245bbd873f556f6b350b501b91da6d6b1e0d3a2
wiper <wiper@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-12-08 07:28:00 +0000

    .......... [ZBX-10979] merged from upstream
    git-svn-id: svn://svn.zabbix.com/branches/dev@64266 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-10979/frontends/php/include/defines.inc.php
+++ ZBX-10979/frontends/php/include/defines.inc.php
@@ -19,7 +19,7 @@
 **/
 
 
-define('ZABBIX_VERSION',		'3.0.6rc1');
+define('ZABBIX_VERSION',		'3.0.6');
 define('ZABBIX_API_VERSION',	'3.0.6');
 define('ZABBIX_EXPORT_VERSION',	'3.0');
 define('ZABBIX_DB_VERSION',		3000000);
