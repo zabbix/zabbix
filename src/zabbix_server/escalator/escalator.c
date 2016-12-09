b9214fea9d4a8935d1d3d66eb95b71c2168e719e
vladislavs <vladislavs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-12-09 14:58:56 +0000

    ........S. [ZBX-11534] fixed default operation step duration to be included in minimal step calculation instead of overriding
    git-svn-id: svn://svn.zabbix.com/branches/2.2@64323 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_server/escalator/escalator.c
+++ src/zabbix_server/escalator/escalator.c
@@ -876,6 +876,12 @@ static void	execute_operations(DB_ESCALATION *escalation, DB_EVENT *event, DB_AC
 		esc_period = atoi(row[2]);
 		evaltype = (unsigned char)atoi(row[3]);
 
+		if (0 == esc_period)
+			esc_period = action->esc_period;
+
+		if (0 == next_esc_period || next_esc_period > esc_period)
+			next_esc_period = esc_period;
+
 		if (SUCCEED == check_operation_conditions(event, operationid, evaltype))
 		{
 			unsigned char	default_msg;
@@ -883,9 +889,6 @@ static void	execute_operations(DB_ESCALATION *escalation, DB_EVENT *event, DB_AC
 			zbx_uint64_t	mediatypeid;
 
 			zabbix_log(LOG_LEVEL_DEBUG, "Conditions match our event. Execute operation.");
-
-			if (0 == next_esc_period || next_esc_period > esc_period)
-				next_esc_period = esc_period;
 
 			switch (operationtype)
 			{
