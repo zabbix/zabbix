c72ba363c0d8b503d8432fac7a01e160a89ae823
sandis <sandis@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-12-14 14:59:27 +0000

    .......... [ZBXNEXT-936] created new development branch to implement remote command execution via proxy
    git-svn-id: svn://svn.zabbix.com/branches/dev@64448 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBXNEXT-936-2/frontends/php/app/controllers/CControllerScriptDelete.php
@@ -0,0 +1,70 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+class CControllerScriptDelete extends CController {
+
+	protected function checkInput() {
+		$fields = [
+			'scriptids' =>	'required|array_db scripts.scriptid'
+		];
+
+		$ret = $this->validateInput($fields);
+
+		if (!$ret) {
+			$this->setResponse(new CControllerResponseFatal());
+		}
+
+		return $ret;
+	}
+
+	protected function checkPermissions() {
+		if ($this->getUserType() != USER_TYPE_SUPER_ADMIN) {
+			return false;
+		}
+
+		$scripts = API::Script()->get([
+			'countOutput' => true,
+			'scriptids' => $this->getInput('scriptids'),
+			'editable' => true
+		]);
+
+		return ($scripts == count($this->getInput('scriptids')));
+	}
+
+	protected function doAction() {
+		$scriptids = $this->getInput('scriptids');
+
+		$result = (bool) API::Script()->delete($scriptids);
+
+		$deleted = count($scriptids);
+
+		$response = new CControllerResponseRedirect('zabbix.php?action=script.list&uncheck=1');
+
+		if ($result) {
+			$response->setMessageOk(_n('Script deleted', 'Scripts deleted', $deleted));
+		}
+		else {
+			$response->setMessageError(_n('Cannot delete script', 'Cannot delete scripts', $deleted));
+		}
+
+		$this->setResponse($response);
+	}
+}
