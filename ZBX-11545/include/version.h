5ad66cf445bf67916a9fd18b28938ba655f266f0
gunars <gunars@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-12-21 06:48:10 +0000

    .......... [ZBX-11545] created new development branch
    git-svn-id: svn://svn.zabbix.com/branches/dev@64600 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

---
+++ ZBX-11545/include/version.h
@@ -0,0 +1,37 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2016 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#ifndef ZABBIX_VERSION_H
+#define ZABBIX_VERSION_H
+
+#define ZBX_STR2(str)	#str
+#define ZBX_STR(str)	ZBX_STR2(str)
+
+#define APPLICATION_NAME	"Zabbix Agent"
+#define ZABBIX_REVDATE		"19 December 2016"
+#define ZABBIX_VERSION_MAJOR	3
+#define ZABBIX_VERSION_MINOR	2
+#define ZABBIX_VERSION_PATCH	3
+#define ZABBIX_VERSION_REVISION	{ZABBIX_REVISION}
+#define ZABBIX_VERSION_RC	"rc2"
+#define ZABBIX_VERSION		ZBX_STR(ZABBIX_VERSION_MAJOR) "." ZBX_STR(ZABBIX_VERSION_MINOR) "." \
+				ZBX_STR(ZABBIX_VERSION_PATCH) ZABBIX_VERSION_RC
+#define ZABBIX_REVISION		ZBX_STR(ZABBIX_VERSION_REVISION)
+
+#endif
