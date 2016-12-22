bd080aaa7c13aafdeed40c5bdb0dd3c8af821ea2
oleg <oleg@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-12-22 08:55:55 +0000

    ..F....... [ZBX-11385] fixed interfaces displaying in host inventory page
    git-svn-id: svn://svn.zabbix.com/branches/3.0@64660 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -983,7 +983,6 @@ define('ZBX_TEXTAREA_2DIGITS_WIDTH',			35);	// please use for date selector only
 define('ZBX_TEXTAREA_4DIGITS_WIDTH',			50);	// please use for date selector only
 define('ZBX_TEXTAREA_INTERFACE_IP_WIDTH',		225);
 define('ZBX_TEXTAREA_INTERFACE_DNS_WIDTH',		175);
-define('ZBX_TEXTAREA_INTERFACE_USEIP_WIDTH',	100);
 define('ZBX_TEXTAREA_INTERFACE_PORT_WIDTH',		100);
 define('ZBX_TEXTAREA_STANDARD_ROWS',			7);
 
