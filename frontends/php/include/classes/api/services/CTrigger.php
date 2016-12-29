34059abfe6cc8cd7b3888baad46558fe4492a91b
gunars <gunars@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2016-12-29 15:31:01 +0000

    A......... [ZBX-11545] fixed dependency validation and update when trigger expression is changed
    git-svn-id: svn://svn.zabbix.com/branches/3.2@64796 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/classes/api/services/CTrigger.php
+++ frontends/php/include/classes/api/services/CTrigger.php
@@ -534,6 +534,27 @@ class CTrigger extends CTriggerGeneral {
 		$db_triggers = [];
 
 		$this->validateUpdate($triggers, $db_triggers);
+
+		$validate_dependencies = [];
+		foreach ($triggers as $tnum => $trigger) {
+			$db_trigger = $db_triggers[$tnum];
+
+			$expressions_changed = ($trigger['expression'] !== $db_trigger['expression']
+				|| $trigger['recovery_expression'] !== $db_trigger['recovery_expression']);
+
+			if ($expressions_changed && $db_trigger['dependencies'] && !array_key_exists('dependencies', $trigger)) {
+				$validate_dependencies[] = [
+					'triggerid' => $trigger['triggerid'],
+					'dependencies' => zbx_objectValues($db_trigger['dependencies'], 'triggerid')
+				];
+			}
+		}
+
+		if ($validate_dependencies) {
+			$this->checkDependencies($validate_dependencies);
+			$this->checkDependencyParents($validate_dependencies);
+		}
+
 		$this->updateReal($triggers, $db_triggers);
 
 		foreach ($triggers as $trigger) {
