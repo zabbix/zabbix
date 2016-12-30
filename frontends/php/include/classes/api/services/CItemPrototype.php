5096c9240e6e9a17913c08a4063a51418589892d
Ivo Kurzemnieks <ivo.kurzemnieks@zabbix.com>
2016-12-30 12:35:35 +0000

    A......... [ZBXNEXT-1443] fixed item option selectPreprocessing returning empty array in case there is no such item

--- frontends/php/include/classes/api/services/CItemPrototype.php
+++ frontends/php/include/classes/api/services/CItemPrototype.php
@@ -1142,7 +1142,9 @@ class CItemPrototype extends CItemGeneral {
 				$itemid = $step['itemid'];
 				unset($step['item_preprocid'], $step['itemid'], $step['step']);
 
-				$result[$itemid]['preprocessing'][] = $step;
+				if (array_key_exists($itemid, $result)) {
+					$result[$itemid]['preprocessing'][] = $step;
+				}
 			}
 		}
 
