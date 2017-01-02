8b5afb8580c68fc6b65297812fa539e9569c3396
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2017-01-02 11:32:59 +0000

    ........S. [ZBX-11470] set token type to reference if found

--- include/common.h
+++ include/common.h
@@ -1243,6 +1243,7 @@ int	zbx_strcmp_natural(const char *s1, const char *s2);
 #define ZBX_TOKEN_USER_MACRO	0x0008
 #define ZBX_TOKEN_FUNC_MACRO	0x0010
 #define ZBX_TOKEN_SIMPLE_MACRO	0x0020
+#define ZBX_TOKEN_REFERENCE	0x0040
 
 /* additional token flags */
 #define ZBX_TOKEN_NUMERIC	0x8000
