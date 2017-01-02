450c36cf8962ad51b4f543643ab00804323647aa
vladislavs <vladislavs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-01-02 07:45:01 +0000

    .......PS. [ZBX-11655] fixed mysql text column limit calculation
    git-svn-id: svn://svn.zabbix.com/branches/dev@64817 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- ZBX-11655/src/libs/zbxdb/db.c
+++ ZBX-11655/src/libs/zbxdb/db.c
@@ -2116,7 +2116,7 @@ char	*zbx_db_dyn_escape_string(const char *src)
 	return dst;
 }
 
-#ifndef HAVE_IBM_DB2
+#if !defined(HAVE_IBM_DB2) && !defined(HAVE_MYSQL)
 /******************************************************************************
  *                                                                            *
  * Function: zbx_db_dyn_escape_string_len                                     *
@@ -2144,9 +2144,7 @@ char	*zbx_db_dyn_escape_string_len(const char *src, size_t max_src_len)
 		if (0x80 != (0xc0 & *s) && 0 == --max_src_len)
 			break;
 
-#if defined(HAVE_MYSQL)
-		if ('\'' == *s || '\\' == *s)
-#elif defined(HAVE_POSTGRESQL)
+#if defined(HAVE_POSTGRESQL)
 		if ('\'' == *s || ('\\' == *s && 1 == ZBX_PG_ESCAPE_BACKSLASH))
 #else
 		if ('\'' == *s)
@@ -2169,8 +2167,8 @@ out:
  *                                                                            *
  * Return value: escaped string                                               *
  *                                                                            *
- * Comments: This function is used to escape strings for IBM DB2 where fields *
- *           are limited by bytes rather than characters.                     *
+ * Comments: This function is used to escape strings where fields are         *
+ *           limited by bytes rather than characters.                         *
  *                                                                            *
  ******************************************************************************/
 char	*zbx_db_dyn_escape_string_len(const char *src, size_t max_src_len)
@@ -2199,7 +2197,11 @@ char	*zbx_db_dyn_escape_string_len(const char *src, size_t max_src_len)
 		if (max_src_len < csize)
 			break;
 
+#if defined(HAVE_MYSQL)
+		if ('\'' == *s || '\\' == *s)
+#else
 		if ('\'' == *s)
+#endif
 			len++;
 
 		s += csize;
