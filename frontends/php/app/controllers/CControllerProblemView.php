3bc78ece24c3e70bab5fd9da24ed5329b9559dbc
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-01-09 07:11:45 +0000

    ..F....... [ZBX-11642] fixed undefined index on Problem page with disabled guest user
    git-svn-id: svn://svn.zabbix.com/branches/3.2@64946 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/app/controllers/CControllerProblemView.php
+++ frontends/php/app/controllers/CControllerProblemView.php
@@ -90,7 +90,7 @@ class CControllerProblemView extends CController {
 	}
 
 	protected function checkPermissions() {
-		return true;
+		return ($this->getUserType() >= USER_TYPE_ZABBIX_USER);
 	}
 
 	protected function doAction() {
