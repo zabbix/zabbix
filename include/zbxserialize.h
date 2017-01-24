4124c90437d724ccbd867a31434fbc4420a1ce51
Andris Zeila <andris.zeila@zabbix.com>
2017-01-24 12:49:23 +0000

    .......PS. [ZBXNEXT-3386] replaced malloc with zbx_malloc in string deserialization macro

--- include/zbxserialize.h
+++ include/zbxserialize.h
@@ -57,7 +57,7 @@
 	(										\
 			memcpy(&value_len, buffer, sizeof(zbx_uint32_t)),		\
 			0 < value_len ? (						\
-			*value = malloc(value_len + 1), 				\
+			*value = zbx_malloc(NULL, value_len + 1), 			\
 			memcpy(*(value), buffer + sizeof(zbx_uint32_t), value_len),	\
 			(*value)[value_len] = '\0'					\
 			) : (*value = NULL, 0),						\
