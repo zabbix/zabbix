bfd3e851964d03cc4bde88ad1ea542feff42920e
Andrea Biscuola <git-no-reply@zabbix.com>
2017-02-15 10:17:29 +0000

    ........S. [ZBX-11805] Fixed the aggregation checks log output
    For fixing the log output i needed to modify the calles related on how to
    compose the log strings. In particular, the common call was removed
    and the two situation where we need aggregate_quote_groups() now
    have a dedicated call.
    This is due to the fact that the new message for indicating that no groups
    are correct, need to be formatted in three different steps, where the
    groups are in the middle.

--- src/zabbix_server/poller/checks_aggregate.c
+++ src/zabbix_server/poller/checks_aggregate.c
@@ -340,7 +340,9 @@ static int	aggregate_get_items(zbx_vector_uint64_t *itemids, const char *groups,
 
 	if (0 == groupids.values_num)
 	{
-		zbx_strcpy_alloc(error, &error_alloc, &error_offset, "No groups in list ");
+		zbx_strcpy_alloc(error, &error_alloc, &error_offset, "None of the groups in list ");
+		aggregate_quote_groups(error, &error_alloc, &error_offset, groups);
+		zbx_strcpy_alloc(error, &error_alloc, &error_offset, " is correct");
 		goto out;
 	}
 
@@ -375,6 +377,7 @@ static int	aggregate_get_items(zbx_vector_uint64_t *itemids, const char *groups,
 	if (0 == itemids->values_num)
 	{
 		zbx_strcpy_alloc(error, &error_alloc, &error_offset, "No items for key \"%s\" in group(s) ");
+		aggregate_quote_groups(error, &error_alloc, &error_offset, groups);
 		goto out;
 	}
 
@@ -384,10 +387,7 @@ static int	aggregate_get_items(zbx_vector_uint64_t *itemids, const char *groups,
 
 out:
 	if (FAIL == ret)
-	{
-		aggregate_quote_groups(error, &error_alloc, &error_offset, groups);
 		zbx_chrcpy_alloc(error, &error_alloc, &error_offset, '.');
-	}
 
 	zbx_vector_uint64_destroy(&groupids);
 
