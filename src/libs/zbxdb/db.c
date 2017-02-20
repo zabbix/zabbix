6b48f5eeec10951cded99ae0990c8a6aa86786f0
sergejs <sergejs@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-02-20 09:59:53 +0000

    .......PS. [ZBX-11765] added savepoint statement for correct rollback on DB2
    git-svn-id: svn://svn.zabbix.com/branches/3.2@65841 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/libs/zbxdb/db.c
+++ src/libs/zbxdb/db.c
@@ -789,11 +789,19 @@ int	zbx_db_begin(void)
 		rc = ZBX_DB_DOWN;
 	}
 
+	if (ZBX_DB_OK == rc)
+	{
+		/* create savepoint for correct rollback on DB2 */
+		if (0 <= (rc = zbx_db_execute("savepoint zbx_begin_savepoint unique on rollback retain cursors;")))
+			rc = ZBX_DB_OK;
+	}
+
 	if (ZBX_DB_OK != rc)
 	{
 		zbx_ibm_db2_log_errors(SQL_HANDLE_DBC, ibm_db2.hdbc, ERR_Z3005, "<begin>");
 		rc = (SQL_CD_TRUE == IBM_DB2server_status() ? ZBX_DB_FAIL : ZBX_DB_DOWN);
 	}
+
 #elif defined(HAVE_MYSQL) || defined(HAVE_POSTGRESQL)
 	rc = zbx_db_execute("%s", "begin;");
 #elif defined(HAVE_SQLITE3)
@@ -841,7 +849,6 @@ int	zbx_db_commit(void)
 	{
 		rc = ZBX_DB_DOWN;
 	}
-
 	if (ZBX_DB_OK != rc)
 	{
 		zbx_ibm_db2_log_errors(SQL_HANDLE_DBC, ibm_db2.hdbc, ERR_Z3005, "<commit>");
@@ -900,8 +907,11 @@ int	zbx_db_rollback(void)
 	txn_error = 0;
 
 #if defined(HAVE_IBM_DB2)
-	if (SUCCEED != zbx_ibm_db2_success(SQLEndTran(SQL_HANDLE_DBC, ibm_db2.hdbc, SQL_ROLLBACK)))
-		rc = ZBX_DB_DOWN;
+
+	/* Rollback to begin that is marked with savepoint. This move undo all transactions. */
+	if (0 <= (rc = zbx_db_execute("rollback to savepoint zbx_begin_savepoint;")))
+		rc = ZBX_DB_OK;
+
 	if (SUCCEED != zbx_ibm_db2_success(SQLSetConnectAttr(ibm_db2.hdbc, SQL_ATTR_AUTOCOMMIT,
 			(SQLPOINTER)SQL_AUTOCOMMIT_ON, SQL_NTS)))
 	{
