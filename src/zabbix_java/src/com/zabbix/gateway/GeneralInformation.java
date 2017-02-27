9553799282c38538e1a856cf540b7097359c27f0
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-02-27 08:49:41 +0000

    .......... [ZBXNEXT-826] updated release date and version [3.2.4]
    git-svn-id: svn://svn.zabbix.com/branches/3.2@65970 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	public static final String REVISION_DATE = "17 February 2017";
+	public static final String REVISION_DATE = "27 February 2017";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "3.2.4rc1";
+	public static final String VERSION = "3.2.4";
 
 	public static void printVersion()
 	{
