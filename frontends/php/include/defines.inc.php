ef44039a8984c561695be3120a1a9b7bdbd59e1d
sasha <sasha@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2017-02-27 07:58:37 +0000

    .......... [ZBXNEXT-826] updated release date and version [2.2.17]
    git-svn-id: svn://svn.zabbix.com/branches/2.2@65967 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -18,7 +18,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-define('ZABBIX_VERSION',		'2.2.17rc1');
+define('ZABBIX_VERSION',		'2.2.17');
 define('ZABBIX_API_VERSION',	'2.2.17');
 define('ZABBIX_DB_VERSION',		2020000);
 
--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	public static final String REVISION_DATE = "17 February 2017";
+	public static final String REVISION_DATE = "27 February 2017";
 	public static final String REVISION = "{ZABBIX_REVISION}";
-	public static final String VERSION = "2.2.17rc1";
+	public static final String VERSION = "2.2.17";
 
 	public static void printVersion()
 	{
