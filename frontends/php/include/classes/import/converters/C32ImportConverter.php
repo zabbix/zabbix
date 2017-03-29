52ae6701a457027da06cc7d4be5dbb48246164cc
Oleg Egorov <git-no-reply@zabbix.com>
2017-03-29 11:50:39 +0000

    ..F....... [ZBXNEXT-2694] fixed auto merge issue

--- frontends/php/include/classes/import/converters/C32ImportConverter.php
+++ frontends/php/include/classes/import/converters/C32ImportConverter.php
@@ -136,22 +136,8 @@ class C32ImportConverter extends CConverter {
 	 */
 	protected function convertMaps(array $maps) {
 		foreach ($maps as &$map) {
-			$map['shapes'] = [];
-		}
-		unset($map);
-
-		return $maps;
-	}
-	/**
-	 * Convert maps.
-	 *
-	 * @param array $maps
-	 *
-	 * @return array
-	 */
-	protected function convertMaps(array $maps) {
-		foreach ($maps as &$map) {
 			$map['selements'] = $this->convertMapElements($map['selements']);
+			$map['shapes'] = [];
 		}
 		unset($map);
 
