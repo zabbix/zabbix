f19fa1c01355d2a21137a13d9d7f96e1e2794006
Glebs Ivanovskis <git-no-reply@zabbix.com>
2017-04-05 15:45:21 +0000

    ........S. [DEV-567] added validation of discovered host IP addresses

--- src/libs/zbxcommon/misc.c
+++ src/libs/zbxcommon/misc.c
@@ -1884,15 +1884,7 @@ int	is_ip6(const char *ip)
  ******************************************************************************/
 int	is_ip(const char *ip)
 {
-	zabbix_log(LOG_LEVEL_DEBUG, "In is_ip() ip:'%s'", ip);
-
-	if (SUCCEED == is_ip4(ip))
-		return SUCCEED;
-#ifdef HAVE_IPV6
-	if (SUCCEED == is_ip6(ip))
-		return SUCCEED;
-#endif
-	return FAIL;
+	return SUCCEED == is_ip4(ip) ? SUCCEED : is_ip6(ip);
 }
 
 /******************************************************************************
