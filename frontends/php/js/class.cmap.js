1b6a2f0439c4cdc62e76a995ac4b187f5c1b4db8
Oleg Egorov <git-no-reply@zabbix.com>
2017-04-25 14:11:03 +0000

    ..F....... [ZBXNEXT-2694] fixed element name for links

--- frontends/php/js/class.cmap.js
+++ frontends/php/js/class.cmap.js
@@ -2718,8 +2718,8 @@ ZABBIX.apps.map = (function($) {
 						}
 
 						list.push({
-							fromElementName: this.sysmap.selements[link.selementid1].data.elementName,
-							toElementName: this.sysmap.selements[link.selementid2].data.elementName,
+							fromElementName: this.sysmap.selements[link.selementid1].data.elements[0].elementName,
+							toElementName: this.sysmap.selements[link.selementid2].data.elements[0].elementName,
 							linkid: link.linkid,
 							linktriggers: linktriggers
 						});
