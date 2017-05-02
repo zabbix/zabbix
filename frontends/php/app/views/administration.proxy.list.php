0e2c452d35cbb6b28cbf704900de76f05e24eeae
Miks Kronkalns <miks.kronkalns@zabbix.com>
2017-05-02 08:25:34 +0000

    ..F....... [ZBX-11309] fixed collision of cookies name responsible for storing selected checkboxes

--- frontends/php/app/views/administration.proxy.list.php
+++ frontends/php/app/views/administration.proxy.list.php
@@ -155,7 +155,7 @@ $proxyForm->addItem([
 			'confirm' => _('Disable hosts monitored by selected proxies?')
 		],
 		'proxy.delete' => ['name' => _('Delete'), 'confirm' => _('Delete selected proxies?')]
-	])
+	], 'proxy')
 ]);
 
 // append form to widget
