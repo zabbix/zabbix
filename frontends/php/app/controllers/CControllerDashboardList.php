82ee6e8052878eddc5244831ce25625e41efee1d
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2017-05-03 12:50:37 +0000

    ..F....... [ZBXNEXT-2102,ZBXNEXT-3808] fixed several bugs and formatting issues in dashboard.list controller and view

--- frontends/php/app/controllers/CControllerDashboardList.php
+++ frontends/php/app/controllers/CControllerDashboardList.php
@@ -24,25 +24,19 @@
  */
 class CControllerDashboardList extends CController {
 
-	const CHECKBOX_COOKIE_SUFFIX = 'selected_dashboard_ids';
-
-	/**
-	 * {@inheritdoc}
-	 */
 	protected function init() {
 		$this->disableSIDValidation();
 	}
 
-	/**
-	 * {@inheritdoc}
-	 */
 	protected function checkInput() {
 		$fields = [
-			'uncheck'        => 'in 1,0',
-			'sort'           => 'in name',
-			'sortorder'      => 'in '.ZBX_SORT_DOWN.','.ZBX_SORT_UP
+			'sort' =>		'in name',
+			'sortorder' =>	'in '.ZBX_SORT_DOWN.','.ZBX_SORT_UP,
+			'uncheck' =>	'in 1'
 		];
+
 		$ret = $this->validateInput($fields);
+
 		if (!$ret) {
 			$this->setResponse(new CControllerResponseFatal());
 		}
@@ -50,71 +44,56 @@ class CControllerDashboardList extends CController {
 		return $ret;
 	}
 
-	/**
-	 * {@inheritdoc}
-	 */
 	protected function checkPermissions() {
 		return ($this->getUserType() >= USER_TYPE_ZABBIX_USER);
 	}
 
-	/**
-	 * Prepare and render list of dashboards
-	 *
-	 * @return void
-	 */
 	protected function doAction() {
-
-		if ($this->getInput('uncheck', false)) {
-			foreach ($_COOKIE as $key => $value) {
-				if (strpos($key, self::CHECKBOX_COOKIE_SUFFIX) !== false) {
-					zbx_unsetcookie($key);
-				}
-			}
-		}
-
 		CProfile::delete('web.dashbrd.dashboardid');
 		CProfile::update('web.dashbrd.list_was_opened', 1, PROFILE_TYPE_INT);
 
-		$sortField = $this->getInput('sort', CProfile::get('web.dashbrd.sort', 'name'));
-		CProfile::update('web.dashbrd.sort', $sortField, PROFILE_TYPE_STR);
+		$sortField = $this->getInput('sort', CProfile::get('web.dashbrd.list.sort', 'name'));
+		$sortOrder = $this->getInput('sortorder', CProfile::get('web.dashbrd.list.sortorder', ZBX_SORT_UP));
 
-		$sortOrder = $this->getInput('sortorder', CProfile::get('web.dashbrd.sortorder', ZBX_SORT_UP));
-		CProfile::update('web.dashbrd.sortorder', $sortOrder, PROFILE_TYPE_STR);
+		CProfile::update('web.dashbrd.list.sort', $sortField, PROFILE_TYPE_STR);
+		CProfile::update('web.dashbrd.list.sortorder', $sortOrder, PROFILE_TYPE_STR);
 
 		$config = select_config();
 
-		$dashboards = API::Dashboard()->get(
-			[
-				'output'       => ['dashboardid', 'name'],
-				'preservekeys' => true,
-				'limit'        => $config['search_limit'] + 1,
-			]
-		);
-
-		if (count($dashboards)) {
-			$write_dashboards = API::Dashboard()->get(['output' => ['dashboardid'], 'editable' => true, 'preservekeys' => true]);
-
-			$curl = (new CUrl('zabbix.php'))->setArgument('action', 'dashboard.view');
-			foreach ($dashboards as $id => &$dashboard) {
-				if (array_key_exists($id, $write_dashboards)) {
-					$dashboard['editable'] = true;
-				}
-				else {
-					$dashboard['editable'] = false;
-				}
-				$dashboard['view_link'] = $curl->setArgument('dashboardid', $id)->getUrl();
-			}
-		}
-
-		order_result($dashboards, $sortField, $sortOrder);
 		$data = [
-			'dashboards'             => $dashboards,
-			'sort'                   => $sortField,
-			'sort_order'             => $sortOrder,
-			'checkbox_cookie_suffix' => self::CHECKBOX_COOKIE_SUFFIX
+			'uncheck' => $this->hasInput('uncheck'),
+			'sort' => $sortField,
+			'sortorder' => $sortOrder
 		];
-		$url = (new CUrl('zabbix.php'))->setArgument('action', 'dashboard.list');
+
+		// list of dashboards
+		$data['dashboards'] = API::Dashboard()->get([
+			'output' => ['dashboardid', 'name'],
+			'limit' => $config['search_limit'] + 1,
+			'preservekeys' => true
+		]);
+
+		// sorting & paging
+		order_result($data['dashboards'], $sortField, $sortOrder);
+
+		$url = (new CUrl('zabbix.php'))
+			->setArgument('action', 'dashboard.list');
+
 		$data['paging'] = getPagingLine($data['dashboards'], $sortOrder, $url);
+
+		if ($data['dashboards']) {
+			$dashboards_rw = API::Dashboard()->get([
+				'output' => [],
+				'dashboardids' => array_keys($data['dashboards']),
+				'editable' => true,
+				'preservekeys' => true
+			]);
+
+			foreach ($data['dashboards'] as &$dashboard) {
+				$dashboard['editable'] = array_key_exists($dashboard['dashboardid'], $dashboards_rw);
+			}
+			unset($dashboard);
+		}
 
 		$response = new CControllerResponseData($data);
 		$response->setTitle(_('Dashboards'));
