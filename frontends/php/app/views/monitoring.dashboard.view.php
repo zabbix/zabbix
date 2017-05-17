c23fefa3efe579d88a520f0647954404cf8a136c
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2017-05-17 15:12:13 +0000

    ..F....... [ZBXNEXT-2102,ZBXNEXT-3812] fixed formatting issues

--- frontends/php/app/views/monitoring.dashboard.view.php
+++ frontends/php/app/views/monitoring.dashboard.view.php
@@ -18,6 +18,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 **/
 
+
 require_once dirname(__FILE__).'/monitoring.dashboard.view.js.php';
 
 $this->addJsFile('dashboard.grid.js');
@@ -32,19 +33,23 @@ if ($data['fullscreen']) {
 	$url_view->setArgument('fullscreen', '1');
 }
 
-$form = (new CForm('post', (new CUrl('zabbix.php'))->setArgument('action', 'dashboard.update')->getUrl()))
+$form = (new CForm('post', (new CUrl('zabbix.php'))
+	->setArgument('action', 'dashboard.update')
+	->getUrl()
+))
 	->setName('dashboard_sharing_form')
-	->setAttribute('style', 'display: none;');
+	->addStyle('display: none;');
 
 $user_group_shares_table = (new CTable())
 	->setHeader([_('User groups'), _('Permissions'), _('Action')])
-	->setAttribute('style', 'width: 100%;');
+	->addStyle('width: 100%;');
 
 $add_user_group_btn = ([(new CButton(null, _('Add')))
-	->onClick(
-		"return PopUp('popup.php?dstfrm=".$form->getName(). "&srctbl=usrgrp&srcfld1=usrgrpid&srcfld2=name&multiselect=1')"
+	->onClick("return PopUp('popup.php?dstfrm=".$form->getName().
+		"&srctbl=usrgrp&srcfld1=usrgrpid&srcfld2=name&multiselect=1')"
 	)
-	->addClass(ZBX_STYLE_BTN_LINK)]);
+	->addClass(ZBX_STYLE_BTN_LINK)
+]);
 
 $user_group_shares_table->addRow(
 	(new CRow(
@@ -55,10 +60,12 @@ $user_group_shares_table->addRow(
 // User sharing table.
 $user_shares_table = (new CTable())
 	->setHeader([_('Users'), _('Permissions'), _('Action')])
-	->setAttribute('style', 'width: 100%;');
+	->addStyle('width: 100%;');
 
 $add_user_btn = ([(new CButton(null, _('Add')))
-	->onClick("return PopUp('popup.php?dstfrm=".$form->getName(). "&srctbl=users&srcfld1=userid&srcfld2=fullname&multiselect=1')")
+	->onClick("return PopUp('popup.php?dstfrm=".$form->getName().
+		"&srctbl=users&srcfld1=userid&srcfld2=fullname&multiselect=1')"
+	)
 	->addClass(ZBX_STYLE_BTN_LINK)]);
 
 $user_shares_table->addRow(
@@ -75,26 +82,23 @@ $form
 	// indicator to help delete all user groups
 	->addItem(new CInput('hidden', 'userGroups[' . CControllerDashboardUpdate::EMPTY_GROUP . ']', '1'))
 	->addItem((new CFormList('sharing_form'))
-	->addRow(
-		_('Type'),
+	->addRow(_('Type'),
 		(new CRadioButtonList('private', (int) $data['dashboard']['private']))
 			->addValue(_('Private'), PRIVATE_SHARING)
 			->addValue(_('Public'), PUBLIC_SHARING)
 			->setModern(true)
 	)
-	->addRow(
-		_('List of user group shares'),
+	->addRow(_('List of user group shares'),
 		(new CDiv($user_group_shares_table))
 			->addClass(ZBX_STYLE_TABLE_FORMS_SEPARATOR)
-			->setAttribute('style', 'min-width: '.ZBX_TEXTAREA_STANDARD_WIDTH.'px;')
+			->addStyle('min-width: '.ZBX_TEXTAREA_STANDARD_WIDTH.'px;')
 	)
 	->addRow(_('List of user shares'),
 		(new CDiv($user_shares_table))
 			->addClass(ZBX_STYLE_TABLE_FORMS_SEPARATOR)
-			->setAttribute('style', 'min-width: '.ZBX_TEXTAREA_STANDARD_WIDTH.'px;')
+			->addStyle('min-width: '.ZBX_TEXTAREA_STANDARD_WIDTH.'px;')
 	)
 );
-
 
 (new CWidget())
 	->setTitle($data['dashboard']['name'])
