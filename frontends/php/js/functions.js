9b118bafc525d86f76b9bbdc86d21156bad13ac4
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2017-05-30 14:13:35 +0000

    ..F....... [ZBXNEXT-2102,ZBXNEXT-3789] fixed formatting

--- frontends/php/js/functions.js
+++ frontends/php/js/functions.js
@@ -723,37 +723,50 @@ function executeScript(hostid, scriptid, confirmation) {
 	};
 
 	$.fn.formToJSON = function() {
-		var $elements = {};
-		var $form = $(this);
-		$form.find('input, select, textarea').each(function(){
-			var name = $(this).attr('name');
-			var type = $(this).attr('type');
-			if(name){
+		var $form = $(this),
+			$elements = {};
+
+		$form.find('input, select, textarea').each(function() {
+			var name = $(this).attr('name'),
+				type = $(this).attr('type');
+
+			if (name) {
 				var $value;
-				if(type == 'radio'){
-					$value = $('input[name='+name+']:checked', $form).val()
-				} else if(type == 'checkbox'){
-					$value = $(this).is(':checked')
-				} else {
-					$value = $(this).val()
+
+				if (type == 'radio') {
+					$value = $('input[name=' + name + ']:checked', $form).val();
 				}
-				$elements[$(this).attr('name')] = $value
+				else if (type == 'checkbox') {
+					$value = $(this).is(':checked');
+				}
+				else {
+					$value = $(this).val();
+				}
+
+				$elements[$(this).attr('name')] = $value;
 			}
 		});
-		return JSON.stringify( $elements )
+
+		return JSON.stringify($elements)
 	};
+
+	// TODO AV: this function is unused
 	$.fn.formFromJSON = function(json_string) {
-		var $form = $(this);
-		var data = JSON.parse(json_string);
+		var $form = $(this),
+			data = JSON.parse(json_string);
+
 		$.each(data, function(key, value) {
-			var $elem = $('[name="'+key+'"]', $form);
-			var type = $elem.first().attr('type');
-			if(type === 'radio'){
-				$('[name="'+key+'"][value="'+value+'"]').prop('checked', true)
-			} else if(type === 'checkbox' && (value === true || value === 'true')){
-				$('[name="'+key+'"]').prop('checked', true)
-			} else {
-				$elem.val(value)
+			var $elem = $('[name="' + key + '"]', $form),
+				type = $elem.first().attr('type');
+
+			if (type === 'radio') {
+				$('[name="' + key + '"][value="' + value + '"]').prop('checked', true);
+			}
+			else if (type === 'checkbox' && (value === true || value === 'true')) {
+				$('[name="' + key + '"]').prop('checked', true);
+			}
+			else {
+				$elem.val(value);
 			}
 		})
 	};
@@ -763,6 +776,7 @@ function makeErrorMessageBox(errors, elementId) {
 	var div = jQuery('<div>').addClass('msg-bad').attr('id', elementId);
 	var details = jQuery('<div>').addClass('msg-details'),
 		ul = jQuery('<ul>');
+
 	errors.each(function (error) {
 		// split long messages
 		var msg = '';
