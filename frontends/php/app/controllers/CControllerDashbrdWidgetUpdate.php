0ceb664ddeee661aba1700293daff1d2fcc0e84d
Maksims Tarleckis <git-no-reply@zabbix.com>
2017-05-31 08:33:22 +0000

    ..F....... [ZBXNEXT-2102,ZBXNEXT-3888] fix sub-issue (1)

--- frontends/php/app/controllers/CControllerDashbrdWidgetUpdate.php
+++ frontends/php/app/controllers/CControllerDashbrdWidgetUpdate.php
@@ -105,7 +105,7 @@ class CControllerDashbrdWidgetUpdate extends CController {
 					$widget_name = (array_key_exists('name', $widget) && $widget['name'] !== '')
 						? $widget['name'] : CWidgetConfig::getKnownWidgetTypes()[$widget['type']];
 					foreach ($errors as $key => $error) {
-						error(_s('Error in widget "%1$s": %2$s.', $widget_name, $error));
+						error(_s('Error in widget "%1$s": %2$s', $widget_name, $error));
 					}
 
 					$ret = false;
