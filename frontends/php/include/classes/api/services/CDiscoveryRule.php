d4c347bb03e8db7176b7efc722cd5aef81a57844
Gregory Chalenko <Gregory.Chalenko@zabbix.com>
2017-06-08 11:19:44 +0000

    A......... [ZBXNEXT-1274] fixed template discovery rule clone

--- frontends/php/include/classes/api/services/CDiscoveryRule.php
+++ frontends/php/include/classes/api/services/CDiscoveryRule.php
@@ -1073,7 +1073,7 @@ class CDiscoveryRule extends CItemGeneral {
 				'snmpv3_securitylevel',	'snmpv3_authpassphrase', 'snmpv3_privpassphrase', 'lastlogsize', 'logtimefmt',
 				'valuemapid', 'params', 'ipmi_sensor', 'authtype', 'username', 'password', 'publickey', 'privatekey',
 				'mtime', 'flags', 'interfaceid', 'port', 'description', 'inventory_link', 'lifetime',
-				'snmpv3_authprotocol', 'snmpv3_privprotocol', 'snmpv3_contextname'
+				'snmpv3_authprotocol', 'snmpv3_privprotocol', 'snmpv3_contextname', 'jmx_endpoint'
 			],
 			'selectFilter' => ['evaltype', 'formula', 'conditions'],
 			'preservekeys' => true
