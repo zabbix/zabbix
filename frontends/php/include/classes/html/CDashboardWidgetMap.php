74df782f767ede45be5651190ed9c23d0c90420d
Valdis Murzins <valdis.murzins@zabbix.com>
2017-06-21 08:55:10 +0000

    ..F....... [ZBXNEXT-2102,ZBXNEXT-3807] minor formatting changes

--- frontends/php/include/classes/html/CDashboardWidgetMap.php
+++ frontends/php/include/classes/html/CDashboardWidgetMap.php
@@ -46,17 +46,17 @@ class CDashboardWidgetMap extends CDiv {
 		$this->sysmap_data = CMapHelper::get($sysmapid, $options);
 
 		if ($sysmapid) {
-			foreach ($this->sysmap_data['elements'] as &$elemnet) {
-				$actions = json_decode($elemnet['actions'], true);
+			foreach ($this->sysmap_data['elements'] as &$element) {
+				$actions = json_decode($element['actions'], true);
 				if ($actions && array_key_exists('gotos', $actions) && array_key_exists('submap', $actions['gotos'])) {
 					$actions['navigatetos']['submap'] = $actions['gotos']['submap'];
 					$actions['navigatetos']['submap']['widget_uniqueid'] = $this->uniqueid;
 					unset($actions['gotos']['submap']);
 				}
 
-				$elemnet['actions'] = json_encode($actions);
+				$element['actions'] = json_encode($actions);
 			}
-			unset($elemnet);
+			unset($element);
 		}
 	}
 
@@ -116,9 +116,8 @@ class CDashboardWidgetMap extends CDiv {
 							$go_back_div = (new CDiv())
 								->setAttribute('style', 'padding:5px 10px; border-bottom: 1px solid #ebeef0;')
 								->addItem(
-									(new CLink(_s('Go back to %1$s', $map['name'])))
-										->onClick('javascript: navigateToSubmap('.$map['sysmapid'].', "' .
-											$this->uniqueid.'", true);')
+									(new CLink(_s('Go back to %1$s', $map['name']), 'javascript: navigateToSubmap('.
+										$map['sysmapid'].', "'.$this->uniqueid.'", true);'))
 								);
 
 							$this->addItem($go_back_div);
