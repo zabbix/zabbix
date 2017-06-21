00d5703988d7c31d791cc820362ddf818df6085e
Valdis Murzins <valdis.murzins@zabbix.com>
2017-06-21 14:48:58 +0000

    ..F....... [ZBXNEXT-2102] fixed undefined label_location index in CMapHelper

--- frontends/php/include/classes/helpers/CMapHelper.php
+++ frontends/php/include/classes/helpers/CMapHelper.php
@@ -68,6 +68,7 @@ class CMapHelper {
 				'height' => 150,
 				'backgroundid' => null,
 				'severity_min' => 0,
+				'label_location' => MAP_LABEL_LOC_BOTTOM,
 				'selements' => [],
 				'links' => [],
 				'shapes' => [[
