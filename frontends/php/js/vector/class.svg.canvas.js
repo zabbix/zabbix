f148045ac290f09cd1b1d53f6693b63dff7705b4
Vjaceslavs Bogdanovs <vjaceslavs.bogdanovs@zabbix.com>
2017-07-21 06:00:08 +0000

    ..F....... [ZBXNEXT-2031] better fix for invalid map element label background offset

--- frontends/php/js/vector/class.svg.canvas.js
+++ frontends/php/js/vector/class.svg.canvas.js
@@ -536,9 +536,8 @@ SVGTextArea.prototype.create = function(attributes, parent, content) {
 	this.text = this.element.add('text', attributes, this.lines);
 
 	size = this.text.element.getBBox();
-	this.delta = size.y;
 	this.width = Math.ceil(size.width);
-	this.height = Math.ceil(size.height);
+	this.height = Math.ceil(size.height + size.y);
 
 	// Workaround for IE/EDGE for proper text height calculation.
 	if ((IE || ED) && this.lines.length > 0
@@ -551,8 +550,7 @@ SVGTextArea.prototype.create = function(attributes, parent, content) {
 	if (this.background !== null) {
 		this.background.update({
 			width: this.width + (this.canvas.textPadding * 2),
-			height: this.height + (this.canvas.textPadding * 2),
-			transform: 'translate(0 ' + this.delta + ')'
+			height: this.height + (this.canvas.textPadding * 2)
 		});
 	}
 
