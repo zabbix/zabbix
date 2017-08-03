1d025cd9d409809235fa7bdde5bcd051e3c681d5
Andris Zeila <andris.zeila@zabbix.com>
2017-08-03 09:39:01 +0000

    .......PS. [ZBXNEXT-3006] fixed broken http tests (leftovers from merge issues)

--- src/zabbix_server/httppoller/httptest.c
+++ src/zabbix_server/httppoller/httptest.c
@@ -199,15 +199,10 @@ static void	process_test_data(zbx_uint64_t httptestid, int lastfailedstep, doubl
 			}
 
 			items[i].state = ITEM_STATE_NORMAL;
-			dc_add_history(items[i].itemid, 0, &value, ts, items[i].state, NULL);
+			zbx_preprocess_item_value(items[i].itemid, 0, &value, ts, items[i].state, NULL);
 
 			free_result(&value);
 		}
-
-		items[i].state = ITEM_STATE_NORMAL;
-		zbx_preprocess_item_value(items[i].itemid, 0, &value, ts, items[i].state, NULL);
-
-		free_result(&value);
 
 		DCconfig_clean_items(items, errcodes, num);
 	}
