57f62525a7688c33696bc4c3b394724404872c0b
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2017-08-04 14:18:44 +0000

    ....I....T [ZBXNEXT-3920] handle multiple self references

--- create/bin/export_data.sh
+++ create/bin/export_data.sh
@@ -47,7 +47,11 @@ for table in `grep TABLE "$schema" | grep $dbflag | awk -F'|' '{print $2}'`; do
 			pri_field=`echo $line | cut -f2 -d'|' | sed -e 's/ //'`
 			ref_field=`echo $line | cut -f9 -d'|' | sed -e 's/ //'`
 			# this strange sort order works fine with MySQL
-			sortorder="order by $pri_field<$ref_field,$ref_field"
+			if [ -z "$sortorder" ]; then
+				sortorder="order by $pri_field<$ref_field,$ref_field"
+			else
+				sortorder="$sortorder,$pri_field<$ref_field,$ref_field"
+			fi
 		fi
 	done
 
