cb9e7bb0c1654254968d5cde4ff236be4238a385
Andris Zeila <andris.zeila@zabbix.com>
2017-08-24 14:02:15 +0000

    .......PS. [ZBX-12549] fixed requeueing of items from unreachable poller to normal poller

--- src/libs/zbxdbcache/dbconfig.c
+++ src/libs/zbxdbcache/dbconfig.c
@@ -381,37 +381,36 @@ static void	DCitem_nextcheck_update(ZBX_DC_ITEM *item, const ZBX_DC_HOST *host, 
 
 static void	DCitem_poller_type_update(ZBX_DC_ITEM *dc_item, const ZBX_DC_HOST *dc_host, int flags)
 {
+	unsigned char	poller_type;
+
 	if (0 != dc_host->proxy_hostid && SUCCEED != is_item_processed_by_server(dc_item->type, dc_item->key))
 	{
 		dc_item->poller_type = ZBX_NO_POLLER;
 		return;
 	}
 
+	poller_type = poller_by_item(dc_item->type, dc_item->key);
+
 	if (0 != (flags & ZBX_HOST_UNREACHABLE))
 	{
-		if (ZBX_POLLER_TYPE_NORMAL == dc_item->poller_type || ZBX_POLLER_TYPE_JAVA == dc_item->poller_type)
-			dc_item->poller_type = ZBX_POLLER_TYPE_UNREACHABLE;
-
-		return;
-	}
-
-	if (0 == (flags & ZBX_ITEM_COLLECTED))
-	{
-		dc_item->poller_type = poller_by_item(dc_item->type, dc_item->key);
-		return;
-	}
-
-	if (ZBX_POLLER_TYPE_UNREACHABLE == dc_item->poller_type)
-	{
-		unsigned char	poller_type;
-
-		poller_type = poller_by_item(dc_item->type, dc_item->key);
-
 		if (ZBX_POLLER_TYPE_NORMAL == poller_type || ZBX_POLLER_TYPE_JAVA == poller_type)
-			return;
+			poller_type = ZBX_POLLER_TYPE_UNREACHABLE;
+
+		dc_item->poller_type = poller_type;
+		return;
 	}
 
-	dc_item->poller_type = poller_by_item(dc_item->type, dc_item->key);
+	if (0 != (flags & ZBX_ITEM_COLLECTED))
+	{
+		dc_item->poller_type = poller_type;
+		return;
+	}
+
+	if (ZBX_POLLER_TYPE_UNREACHABLE != dc_item->poller_type ||
+			(ZBX_POLLER_TYPE_NORMAL != poller_type && ZBX_POLLER_TYPE_JAVA != poller_type))
+	{
+		dc_item->poller_type = poller_type;
+	}
 }
 
 static int	DCget_disable_until(const ZBX_DC_ITEM *item, const ZBX_DC_HOST *host)
