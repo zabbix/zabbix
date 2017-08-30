c4854068a6a9cdc526549347fe37848ca0e5c8d8
Andris Zeila <andris.zeila@zabbix.com>
2017-08-30 09:54:05 +0000

    ........S. [ZBX-12635] fixed item error message handling for lld rules

--- src/zabbix_server/preprocessor/preprocessing.c
+++ src/zabbix_server/preprocessor/preprocessing.c
@@ -653,13 +653,10 @@ void	zbx_preprocess_item_value(zbx_uint64_t itemid, unsigned char item_flags, AG
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
 
-	if (0 != (item_flags & ZBX_FLAG_DISCOVERY_RULE))
+	if (ITEM_STATE_NOTSUPPORTED != state && 0 != (item_flags & ZBX_FLAG_DISCOVERY_RULE))
 	{
 		if (NULL != result && NULL != GET_TEXT_RESULT(result))
 			lld_process_discovery_rule(itemid, result->text, ts);
-
-		if (ITEM_STATE_NOTSUPPORTED == state)
-			dc_local_add_history_notsupported(itemid, ts, error);
 
 		goto out;
 	}
