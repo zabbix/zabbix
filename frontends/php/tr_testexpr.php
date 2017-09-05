de5ab0997d0a4805b9e13aa6c3f701dbc0125fa8
Gregory Chalenko <Gregory.Chalenko@zabbix.com>
2017-09-05 12:44:22 +0000

    ..F....... [ZBX-8997] fixed trigger expression validation test form            [merge ^/branches/3.4 -c 72203]

--- frontends/php/tr_testexpr.php
+++ frontends/php/tr_testexpr.php
@@ -33,7 +33,8 @@ $definedErrorPhrases = [
 	EXPRESSION_HOST_UNKNOWN => _('Unknown host, no such host present in system'),
 	EXPRESSION_HOST_ITEM_UNKNOWN => _('Unknown host item, no such item in selected host'),
 	EXPRESSION_NOT_A_MACRO_ERROR => _('Given expression is not a macro'),
-	EXPRESSION_FUNCTION_UNKNOWN => _('Incorrect function is used')
+	EXPRESSION_FUNCTION_UNKNOWN => _('Incorrect function is used'),
+	EXPRESSION_UNSUPPORTED_VALUE_TYPE => _('Incorrect item value type')
 ];
 
 require_once dirname(__FILE__).'/include/page_header.php';
