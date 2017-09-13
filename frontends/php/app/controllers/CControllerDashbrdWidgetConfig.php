f8f19a3c97926b46073e22505468215641b54ced
Gregory Chalenko <Gregory.Chalenko@zabbix.com>
2017-09-13 07:35:06 +0000

    ..F....... [ZBX-12577] added item select filter for simple graph to allow select only supported item            [merge ^/branches/3.4 -c 72543]

--- frontends/php/app/controllers/CControllerDashbrdWidgetConfig.php
+++ frontends/php/app/controllers/CControllerDashbrdWidgetConfig.php
@@ -100,6 +100,8 @@ class CControllerDashbrdWidgetConfig extends CController {
 
 				if ($id != 0) {
 					switch ($resource_type) {
+						case WIDGET_FIELD_SELECT_RES_SIMPLE_GRAPH:
+							// falls through
 						case WIDGET_FIELD_SELECT_RES_ITEM:
 							$captions['simple'][$resource_type][$id] = _('Inaccessible item');
 							break;
@@ -122,6 +124,8 @@ class CControllerDashbrdWidgetConfig extends CController {
 			}
 
 			switch ($resource_type) {
+				case WIDGET_FIELD_SELECT_RES_SIMPLE_GRAPH:
+					// falls through
 				case WIDGET_FIELD_SELECT_RES_ITEM:
 					$items = API::Item()->get([
 						'output' => ['itemid', 'hostid', 'key_', 'name'],
--- frontends/php/include/classes/widgets/CGraphWidgetForm.php
+++ frontends/php/include/classes/widgets/CGraphWidgetForm.php
@@ -43,7 +43,7 @@ class CGraphWidgetForm extends CWidgetForm {
 		if (array_key_exists('source_type', $this->data)
 				&& $this->data['source_type'] == ZBX_WIDGET_FIELD_RESOURCE_SIMPLE_GRAPH) {
 			// item field
-			$field_item = (new CWidgetFieldSelectResource('itemid', _('Item'), WIDGET_FIELD_SELECT_RES_ITEM))
+			$field_item = (new CWidgetFieldSelectResource('itemid', _('Item'), WIDGET_FIELD_SELECT_RES_SIMPLE_GRAPH))
 				->setFlags(CWidgetField::FLAG_NOT_EMPTY);
 
 			if (array_key_exists('itemid', $this->data)) {
