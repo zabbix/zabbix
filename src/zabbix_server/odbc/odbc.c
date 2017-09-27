7f055dc8324c7afbda02475bf7f3a188b4d40a42
Glebs Ivanovskis <git-no-reply@zabbix.com>
2017-09-27 12:24:16 +0000

    .......PS. [ZBX-10576] moved getting column types to select stage; minor naming, logging, stylistic and formatting improvements

--- src/zabbix_server/odbc/odbc.c
+++ src/zabbix_server/odbc/odbc.c
@@ -36,13 +36,12 @@ struct zbx_odbc_data_source
 	SQLHDBC	hdbc;
 };
 
-typedef char	**zbx_odbc_row_t;
-
 struct zbx_odbc_query_result
 {
 	SQLHSTMT	hstmt;
 	SQLSMALLINT	col_num;
-	zbx_odbc_row_t	row_data;
+	SQLSMALLINT	*c_types;
+	char		**row;
 };
 
 /******************************************************************************
@@ -203,6 +202,24 @@ static void	zbx_log_odbc_connection_info(const char *function, SQLHDBC hdbc)
 	}
 }
 
+/******************************************************************************
+ *                                                                            *
+ * Function: zbx_odbc_connect                                                 *
+ *                                                                            *
+ * Purpose: connect to ODBC data source                                       *
+ *                                                                            *
+ * Parameters: dsn     - [IN] data source name                                *
+ *             user    - [IN] user name                                       *
+ *             pass    - [IN] password                                        *
+ *             timeout - [IN] timeout                                         *
+ *             error   - [OUT] error message                                  *
+ *                                                                            *
+ * Return value: pointer to opaque data source data structure or NULL in case *
+ *               of failure, allocated error message is returned in error     *
+ *                                                                            *
+ * Comments: It is caller's responsibility to free error buffer!              *
+ *                                                                            *
+ ******************************************************************************/
 zbx_odbc_data_source_t	*zbx_odbc_connect(const char *dsn, const char *user, const char *pass, int timeout, char **error)
 {
 	const char		*__function_name = "zbx_odbc_connect";
@@ -265,6 +282,18 @@ out:
 	return data_source;
 }
 
+/******************************************************************************
+ *                                                                            *
+ * Function: zbx_odbc_data_source_free                                        *
+ *                                                                            *
+ * Purpose: free resources allocated by successful zbx_odbc_connect() call    *
+ *                                                                            *
+ * Parameters: data_source - [IN] pointer to data source structure            *
+ *                                                                            *
+ * Comments: Input parameter data_source must be obtained using               *
+ *           zbx_odbc_connect() and must not be NULL.                         *
+ *                                                                            *
+ ******************************************************************************/
 void	zbx_odbc_data_source_free(zbx_odbc_data_source_t *data_source)
 {
 	SQLDisconnect(data_source->hdbc);
@@ -273,6 +302,22 @@ void	zbx_odbc_data_source_free(zbx_odbc_data_source_t *data_source)
 	zbx_free(data_source);
 }
 
+/******************************************************************************
+ *                                                                            *
+ * Function: zbx_odbc_select                                                  *
+ *                                                                            *
+ * Purpose: execute a query to ODBC data source                               *
+ *                                                                            *
+ * Parameters: data_source - [IN] pointer to data source structure            *
+ *             query       - [IN] SQL query                                   *
+ *             error       - [OUT] error message                              *
+ *                                                                            *
+ * Return value: pointer to opaque query result structure or NULL in case of  *
+ *               failure, allocated error message is returned in error        *
+ *                                                                            *
+ * Comments: It is caller's responsibility to free error buffer!              *
+ *                                                                            *
+ ******************************************************************************/
 zbx_odbc_query_result_t	*zbx_odbc_select(const zbx_odbc_data_source_t *data_source, const char *query, char **error)
 {
 	const char		*__function_name = "zbx_odbc_select";
@@ -296,14 +341,43 @@ zbx_odbc_query_result_t	*zbx_odbc_select(const zbx_odbc_data_source_t *data_sour
 
 			if (SUCCEED == zbx_odbc_diag(SQL_HANDLE_STMT, query_result->hstmt, rc, &diag))
 			{
-				query_result->row_data = zbx_malloc(NULL, sizeof(char *) * (size_t)query_result->col_num);
-				memset(query_result->row_data, 0, sizeof(char *) * (size_t)query_result->col_num);
-				zabbix_log(LOG_LEVEL_DEBUG, "%s() selected %i columns", __function_name,
-						query_result->col_num);
-				goto out;
-			}
+				SQLSMALLINT	i;
+				SQLLEN		col_type;
 
-			*error = zbx_dsprintf(*error, "Cannot get number of columns in ODBC result: %s", diag);
+				query_result->c_types = zbx_malloc(NULL,
+						sizeof(SQLSMALLINT) * (size_t)query_result->col_num);
+				query_result->row = zbx_malloc(NULL, sizeof(char *) * (size_t)query_result->col_num);
+
+				for (i = 0; ; i++)
+				{
+					if (i == query_result->col_num)
+					{
+						zabbix_log(LOG_LEVEL_DEBUG, "selected all %d columns",
+								(int)query_result->col_num);
+						goto out;
+					}
+
+					rc = SQLColAttribute(query_result->hstmt, i + 1, SQL_DESC_TYPE, NULL, 0, NULL,
+							&col_type);
+
+					if (SUCCEED != zbx_odbc_diag(SQL_HANDLE_STMT, query_result->hstmt, rc, &diag))
+					{
+						*error = zbx_dsprintf(*error, "Cannot get column type: %s", diag);
+						zbx_free(query_result->c_types);
+						zbx_free(query_result->row);
+						break;
+					}
+
+					zabbix_log(LOG_LEVEL_DEBUG, "column #%d type:%d", (int)i, (int)col_type);
+
+					/* force col_type to integer value for DB2 compatibility */
+					query_result->c_types[i] = (SQL_WLONGVARCHAR == (int)col_type ?
+							SQL_C_DEFAULT : SQL_C_CHAR);
+					query_result->row[i] = NULL;
+				}
+			}
+			else
+				*error = zbx_dsprintf(*error, "Cannot get number of columns in ODBC result: %s", diag);
 		}
 		else
 			*error = zbx_dsprintf(*error, "Cannot execute ODBC query: %s", diag);
@@ -322,26 +396,57 @@ out:
 	return query_result;
 }
 
+/******************************************************************************
+ *                                                                            *
+ * Function: zbx_odbc_query_result_free                                       *
+ *                                                                            *
+ * Purpose: free resources allocated by successful zbx_odbc_select() call     *
+ *                                                                            *
+ * Parameters: query_result - [IN] pointer to query result structure          *
+ *                                                                            *
+ * Comments: Input parameter query_result must be obtained using              *
+ *           zbx_odbc_select() and must not be NULL.                          *
+ *                                                                            *
+ ******************************************************************************/
 void	zbx_odbc_query_result_free(zbx_odbc_query_result_t *query_result)
 {
 	SQLSMALLINT	i;
 
 	SQLFreeHandle(SQL_HANDLE_STMT, query_result->hstmt);
+	zbx_free(query_result->c_types);
 
 	for (i = 0; i < query_result->col_num; i++)
-		zbx_free(query_result->row_data[i]);
+		zbx_free(query_result->row[i]);
 
-	zbx_free(query_result->row_data);
+	zbx_free(query_result->row);
 	zbx_free(query_result);
 }
 
-static zbx_odbc_row_t	zbx_odbc_fetch(zbx_odbc_query_result_t *query_result)
+/******************************************************************************
+ *                                                                            *
+ * Function: zbx_odbc_fetch                                                   *
+ *                                                                            *
+ * Purpose: fetch single row of ODBC query result                             *
+ *                                                                            *
+ * Parameters: query_result - [IN] pointer to query result structure          *
+ *                                                                            *
+ * Return value: array of strings or NULL (see Comments)                      *
+ *                                                                            *
+ * Comments: NULL result can signify both end of rows (which is normal) and   *
+ *           failure. There is currently no way to distinguish these cases.   *
+ *           There is no need to free strings returned by this function.      *
+ *           Lifetime of strings is limited to next call of zbx_odbc_fetch()  *
+ *           or zbx_odbc_query_result_free(), caller needs to make a copy if  *
+ *           result is needed for longer.                                     *
+ *                                                                            *
+ ******************************************************************************/
+static const const char	**zbx_odbc_fetch(zbx_odbc_query_result_t *query_result)
 {
-	const char	*__function_name = "zbx_odbc_fetch";
-	char		*diag = NULL;
-	SQLRETURN	rc;
-	SQLSMALLINT	i;
-	zbx_odbc_row_t	row = NULL;
+	const char		*__function_name = "zbx_odbc_fetch";
+	char			*diag = NULL;
+	SQLRETURN		rc;
+	SQLSMALLINT		i;
+	const const char	**row = NULL;
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
 
@@ -361,33 +466,14 @@ static zbx_odbc_row_t	zbx_odbc_fetch(zbx_odbc_query_result_t *query_result)
 	{
 		size_t		alloc = 0, offset = 0;
 		char		buffer[MAX_STRING_LEN + 1];
-		SQLLEN		len, col_type;
-		SQLSMALLINT	c_type;
+		SQLLEN		len;
 
-		zbx_free(query_result->row_data[i]);
-
-		rc = SQLColAttribute(query_result->hstmt, i + 1, SQL_DESC_TYPE, NULL, 0, NULL, &col_type);
-
-		if (SUCCEED != zbx_odbc_diag(SQL_HANDLE_STMT, query_result->hstmt, rc, &diag))
-		{
-			zabbix_log(LOG_LEVEL_DEBUG, "Cannot get column type: %s", diag);
-			goto out;
-		}
-
-		/* force col_type to integer value for DB2 compatibility */
-		switch ((int)col_type)
-		{
-			case SQL_WLONGVARCHAR:
-				c_type = SQL_C_DEFAULT;
-				break;
-			default:
-				c_type = SQL_C_CHAR;
-		}
+		zbx_free(query_result->row[i]);
 
 		/* force len to integer value for DB2 compatibility */
 		do
 		{
-			rc = SQLGetData(query_result->hstmt, i + 1, c_type, buffer, MAX_STRING_LEN, &len);
+			rc = SQLGetData(query_result->hstmt, i + 1, query_result->c_types[i], buffer, MAX_STRING_LEN, &len);
 
 			if (SUCCEED != zbx_odbc_diag(SQL_HANDLE_STMT, query_result->hstmt, rc, &diag))
 			{
@@ -400,18 +486,17 @@ static zbx_odbc_row_t	zbx_odbc_fetch(zbx_odbc_query_result_t *query_result)
 
 			buffer[(int)len] = '\0';
 
-			zbx_strcpy_alloc(&query_result->row_data[i], &alloc, &offset, buffer);
+			zbx_strcpy_alloc(&query_result->row[i], &alloc, &offset, buffer);
 		}
 		while (SQL_SUCCESS != rc);
 
-		if (NULL != query_result->row_data[i])
-			zbx_rtrim(query_result->row_data[i], " ");
+		if (NULL != query_result->row[i])
+			zbx_rtrim(query_result->row[i], " ");
 
-		zabbix_log(LOG_LEVEL_DEBUG, "%s() fetched [%i col (%d)]: '%s'", __function_name, i, (int)col_type,
-						NULL == query_result->row_data[i] ? "NULL" : query_result->row_data[i]);
+		zabbix_log(LOG_LEVEL_DEBUG, "column #%d value:'%s'", (int)i, ZBX_NULL2STR(query_result->row[i]));
 	}
 
-	row = query_result->row_data;
+	row = (const const char **)query_result->row;
 out:
 	zbx_free(diag);
 
@@ -441,9 +526,9 @@ out:
  ******************************************************************************/
 int	zbx_odbc_query_result_to_string(zbx_odbc_query_result_t *query_result, char **string, char **error)
 {
-	const char	*__function_name = "zbx_odbc_query_result_to_string";
-	zbx_odbc_row_t	row;
-	int		ret = FAIL;
+	const char		*__function_name = "zbx_odbc_query_result_to_string";
+	const const char	**row;
+	int			ret = FAIL;
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
 
@@ -488,7 +573,7 @@ int	zbx_odbc_query_result_to_string(zbx_odbc_query_result_t *query_result, char 
 int	zbx_odbc_query_result_to_lld_json(zbx_odbc_query_result_t *query_result, char **lld_json, char **error)
 {
 	const char		*__function_name = "zbx_odbc_query_result_to_lld_json";
-	zbx_odbc_row_t		row;
+	const const char	**row;
 	struct zbx_json		json;
 	zbx_vector_str_t	macros;
 	int			ret = FAIL, i, j;
