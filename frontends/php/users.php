bb14f8e17d5dc0424443b571e8c6229e4eff27df
vmurzins <vmurzins@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2017-10-02 09:57:18 +0000

    ..F....... [ZBX-12770] fixed links in select popup for user groups; updated group selection field in Administration->Users
    git-svn-id: svn://svn.zabbix.com/trunk@73096 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/users.php
+++ frontends/php/users.php
@@ -28,6 +28,7 @@ require_once dirname(__FILE__).'/include/js.inc.php';
 
 $page['title'] = _('Configuration of users');
 $page['file'] = 'users.php';
+$page['scripts'] = ['multiselect.js'];
 
 require_once dirname(__FILE__).'/include/page_header.php';
 
@@ -47,7 +48,6 @@ $fields = [
 	'password2' =>			[T_ZBX_STR, O_OPT, null,	null,		'(isset({add}) || isset({update})) && isset({form}) && {form} != "update" && isset({change_password})'],
 	'user_type' =>			[T_ZBX_INT, O_OPT, null,	IN('1,2,3'),'isset({add}) || isset({update})'],
 	'user_groups' =>		[T_ZBX_STR, O_OPT, null,	NOT_EMPTY,	null],
-	'user_groups_to_del' =>	[T_ZBX_INT, O_OPT, null,	DB_ID,		null],
 	'user_medias' =>		[T_ZBX_STR, O_OPT, null,	NOT_EMPTY,	null],
 	'user_medias_to_del' =>	[T_ZBX_INT, O_OPT, null,	DB_ID,		null],
 	'new_groups' =>			[T_ZBX_STR, O_OPT, null,	null,		null],
@@ -69,7 +69,6 @@ $fields = [
 	'update' =>				[T_ZBX_STR, O_OPT, P_SYS|P_ACT,	null,	null],
 	'delete' =>				[T_ZBX_STR, O_OPT, P_SYS|P_ACT,	null,	null],
 	'delete_selected' =>	[T_ZBX_STR, O_OPT, P_SYS|P_ACT,	null,	null],
-	'del_user_group' =>		[T_ZBX_STR, O_OPT, P_SYS|P_ACT,	null,	null],
 	'del_user_media' =>		[T_ZBX_STR, O_OPT, P_SYS|P_ACT,	null,	null],
 	'del_group_user' =>		[T_ZBX_STR, O_OPT, P_SYS|P_ACT,	null,	null],
 	'change_password' =>	[T_ZBX_STR, O_OPT, P_SYS|P_ACT,	null,	null],
@@ -273,13 +272,6 @@ elseif (isset($_REQUEST['del_user_media'])) {
 	foreach (getRequest('user_medias_to_del', []) as $mediaId) {
 		if (isset($_REQUEST['user_medias'][$mediaId])) {
 			unset($_REQUEST['user_medias'][$mediaId]);
-		}
-	}
-}
-elseif (isset($_REQUEST['del_user_group'])) {
-	foreach (getRequest('user_groups_to_del', []) as $groupId) {
-		if (isset($_REQUEST['user_groups'][$groupId])) {
-			unset($_REQUEST['user_groups'][$groupId]);
 		}
 	}
 }
