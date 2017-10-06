37ebe49cbaded7376332140c5a4ea032a8fff7fa
Sergejs Paskevics <git-no-reply@zabbix.com>
2017-10-06 04:27:43 +0000

    ....I...S. [ZBXNEXT-3126] changed media.sendto field size

---
+++ src/libs/zbxdbupgrade/dbupgrade_3050.c
@@ -0,0 +1,52 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2017 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "common.h"
+#include "db.h"
+#include "dbupgrade.h"
+
+/*
+ * 4.0 maintenance database patches
+ */
+
+#ifndef HAVE_SQLITE3
+
+static int	DBpatch_3050000(void)
+{
+	return SUCCEED;
+}
+
+#endif
+
+static int	DBpatch_3050001(void)
+{
+	const ZBX_FIELD	field = {"sendto", "", NULL, NULL, 1024, ZBX_TYPE_CHAR, ZBX_NOTNULL, 0};
+
+	return DBmodify_field_type("media", &field, NULL);
+}
+
+
+DBPATCH_START(3050)
+
+/* version, duplicates flag, mandatory flag */
+
+DBPATCH_ADD(3050000, 0, 1)
+DBPATCH_ADD(3050001, 0, 1)
+
+DBPATCH_END()
