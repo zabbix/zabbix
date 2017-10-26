0ad6b4465f36031ff521cfd614093a11f2f0c8d5
ivo <ivo@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2017-10-26 08:26:56 +0000

    A.F.I..... [ZBXNEXT-4118] implemented advanced options for tag-based search of problems
    git-svn-id: svn://svn.zabbix.com/trunk@73938 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/libs/zbxdbupgrade/dbupgrade_3050.c
+++ src/libs/zbxdbupgrade/dbupgrade_3050.c
@@ -34,6 +34,47 @@ static int	DBpatch_3050000(void)
 	return DBadd_field("hosts", &field);
 }
 
+static int	DBpatch_3050001(void)
+{
+	DB_RESULT	result;
+	DB_ROW		row;
+	int		ret = FAIL;
+
+	/* type : 'problem' - WIDGET_PROBLEMS */
+	result = DBselect(
+			"select wf.widgetid,wf.name"
+			" from widget w,widget_field wf"
+			" where w.widgetid=wf.widgetid"
+				" and w.type='problems'"
+				" and wf.name like 'tags.tag.%%'");
+
+	while (NULL != (row = DBfetch(result)))
+	{
+		const char	*p;
+		int		index;
+		zbx_uint64_t	widget_fieldid;
+
+		if (NULL == (p = strrchr(row[1], '.')) || SUCCEED != is_uint31(p + 1, &index))
+			continue;
+
+		widget_fieldid = DBget_maxid_num("widget_field", 1);
+
+		/* type      : 0 - ZBX_WIDGET_FIELD_TYPE_INT32 */
+		/* value_int : 0 - TAG_OPERATOR_LIKE */
+		if (ZBX_DB_OK > DBexecute(
+				"insert into widget_field (widget_fieldid,widgetid,type,name,value_int)"
+				"values (" ZBX_FS_UI64 ",%s,0,'tags.operator.%d',0)", widget_fieldid, row[0], index)) {
+			goto clean;
+		}
+	}
+
+	ret = SUCCEED;
+clean:
+	DBfree_result(result);
+
+	return ret;
+}
+
 #endif
 
 DBPATCH_START(3050)
@@ -41,5 +82,6 @@ DBPATCH_START(3050)
 /* version, duplicates flag, mandatory flag */
 
 DBPATCH_ADD(3050000, 0, 1)
+DBPATCH_ADD(3050001, 0, 1)
 
 DBPATCH_END()
