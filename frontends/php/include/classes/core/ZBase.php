8556ac64f7baa8fef3426e8d1b0995a991b6c095
Valdis Murzins <valdis.murzins@zabbix.com>
2017-10-27 08:14:13 +0000

    ..F....... [ZBXNEXT-4139] added high contrast light and dark themes

--- frontends/php/include/classes/core/ZBase.php
+++ frontends/php/include/classes/core/ZBase.php
@@ -259,6 +259,8 @@ class ZBase {
 		return [
 			'blue-theme' => _('Blue'),
 			'dark-theme' => _('Dark'),
+			'hc-light' => _('High Contrast Light'),
+			'hc-dark' => _('High Contrast Dark'),
 		];
 	}
 
