ed4e94d98cb210802729dccf0c279739b39737f7
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2017-10-31 10:40:45 +0000

    ....I..... [ZBXNEXT-4186] fixed field type

--- src/libs/zbxdbupgrade/dbupgrade_3050.c
+++ src/libs/zbxdbupgrade/dbupgrade_3050.c
@@ -36,7 +36,7 @@ static int	DBpatch_3050000(void)
 
 static int	DBpatch_3050001(void)
 {
-	const ZBX_FIELD field = {"colorpalette", "", NULL, NULL, 0, ZBX_TYPE_TEXT, ZBX_NOTNULL, 0};
+	const ZBX_FIELD field = {"colorpalette", "", NULL, NULL, 255, ZBX_TYPE_CHAR, ZBX_NOTNULL, 0};
 
 	return DBadd_field("graph_theme", &field);
 }
