397149601619c84ea4d0ff9c9454e33e5899b340
Miks Kronkalns <miks.kronkalns@zabbix.com>
2017-11-22 14:09:05 +0000

    ..F....... [ZBXNEXT-4175] fixed email validation

--- frontends/php/include/classes/validators/CEmailValidator.php
+++ frontends/php/include/classes/validators/CEmailValidator.php
@@ -33,7 +33,7 @@ class CEmailValidator extends CStringValidator {
 	 */
 	public function validate($value) {
 		if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-			preg_match('/.*<(?<email>.*[^>])>/i', $value, $match);
+			preg_match('/.*<(?<email>.*[^>])>$/i', $value, $match);
 
 			if (!array_key_exists('email', $match) || !filter_var($match['email'], FILTER_VALIDATE_EMAIL)) {
 				$this->setError(_s('Invalid email address "%1$s".', $value));
