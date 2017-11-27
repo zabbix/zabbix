019916a6910cd2b4c1497d8e73bb640f3fa7c3a1
Andrea Biscuola <git-no-reply@zabbix.com>
2017-11-27 12:40:24 +0000

    ........S. [ZBXNEXT-3480] correct inventory_value assignement
    The "link" field in the zbx_inventory_value_t must
    start from 0 and not 1. So at assignement, we need
    to subtract 1 from the final link value for having the
    right index for a ZBX_DC_HOST_INVENTORY
    structure value in the "fields" array.

--- src/libs/zbxdbcache/dbcache.c
+++ src/libs/zbxdbcache/dbcache.c
@@ -941,7 +941,7 @@ static void	DCinventory_value_add(zbx_vector_ptr_t *inventory_values, DC_ITEM *i
 	inventory_value = zbx_malloc(NULL, sizeof(zbx_inventory_value_t));
 
 	inventory_value->hostid = item->host.hostid;
-	inventory_value->link = item->inventory_link;
+	inventory_value->link = item->inventory_link - 1;
 	inventory_value->field_name = inventory_field;
 	inventory_value->value = zbx_strdup(NULL, value);
 
