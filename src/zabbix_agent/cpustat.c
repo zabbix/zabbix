4354cfd69a8d0648b4c2645d6a86ffbb10c6990a
Valdis Kauķis <git-no-reply@zabbix.com>
2017-12-04 08:37:04 +0000

    ........S. [ZBX-12974] fixed TLS connection to passive proxy error handling

--- src/zabbix_agent/cpustat.c
+++ src/zabbix_agent/cpustat.c
@@ -122,10 +122,11 @@ static int	refresh_kstat(ZBX_CPUS_STAT_DATA *pcpus)
 int	init_cpu_collector(ZBX_CPUS_STAT_DATA *pcpus)
 {
 	const char			*__function_name = "init_cpu_collector";
+	char				*error = NULL;
 	int				idx, ret = FAIL;
 #ifdef _WINDOWS
 	wchar_t				cpu[8];
-	char				counterPath[PDH_MAX_COUNTER_PATH], *error = NULL;
+	char				counterPath[PDH_MAX_COUNTER_PATH];
 	PDH_COUNTER_PATH_ELEMENTS	cpe;
 #endif
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
@@ -174,9 +175,10 @@ clean:
 	}
 
 #else	/* not _WINDOWS */
-	if (SUCCEED != zbx_mutex_create(&cpustats_lock, ZBX_MUTEX_CPUSTATS, NULL))	/* FIXME later */
+	if (SUCCEED != zbx_mutex_create(&cpustats_lock, ZBX_MUTEX_CPUSTATS, &error))
 	{
-		zbx_error("unable to create mutex for cpu collector");
+		zbx_error("unable to create mutex for cpu collector: %s", error);
+		zbx_free(error);
 		exit(EXIT_FAILURE);
 	}
 
