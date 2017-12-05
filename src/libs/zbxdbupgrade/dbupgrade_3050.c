e57c1595125cd52d2328c245851e0cc9c0eb5d2b
Andrea Biscuola <git-no-reply@zabbix.com>
2017-12-05 13:16:28 +0000

    ........S. [ZBXNEXT-4108] fixed dbupgrade breakage
    Backport immediately to trunk a fix for a dbupgrade breakage
    in the case a db does not have events or problems.

--- src/libs/zbxdbupgrade/dbupgrade_3050.c
+++ src/libs/zbxdbupgrade/dbupgrade_3050.c
@@ -139,8 +139,11 @@ static int	DBpatch_3050006(void)
 
 	DBend_multiple_update(&sql, &sql_alloc, &sql_offset);
 
-	if (ZBX_DB_OK > DBexecute("%s", sql))
-		return FAIL;
+	if (16 < sql_offset)	/* in ORACLE always present begin..end; */
+	{
+		if (ZBX_DB_OK > DBexecute("%s", sql))
+			return FAIL;
+	}
 
 	zbx_free(sql);
 
@@ -173,8 +176,11 @@ static int	DBpatch_3050007(void)
 
 	DBend_multiple_update(&sql, &sql_alloc, &sql_offset);
 
-	if (ZBX_DB_OK > DBexecute("%s", sql))
-		return FAIL;
+	if (16 < sql_offset)	/* in ORACLE always present begin..end; */
+	{
+		if (ZBX_DB_OK > DBexecute("%s", sql))
+			return FAIL;
+	}
 
 	zbx_free(sql);
 
