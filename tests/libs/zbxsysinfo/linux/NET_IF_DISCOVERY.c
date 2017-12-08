4c94395a10740173362762c8a7fb653b12a91b7b
Vladimir Levijev <vladimir.levijev@zabbix.com>
2017-12-08 14:20:47 +0000

    ....I..... [DEV-749] rename expected_result -> expected_ret, actual_result -> actual_ret

--- tests/libs/zbxsysinfo/linux/NET_IF_DISCOVERY.c
+++ tests/libs/zbxsysinfo/linux/NET_IF_DISCOVERY.c
@@ -25,6 +25,50 @@
 
 #define FAIL_PARAM(NAME, MOCK_ERR)	fail_msg("Cannot get \"%s\": %s", NAME, zbx_mock_error_string(MOCK_ERR))
 
+static zbx_mock_error_t	get_out_parameter(const char *name, const char **value);
+
+void	zbx_mock_test_entry(void **state)
+{
+	zbx_mock_error_t	error;
+	AGENT_REQUEST		request;
+	AGENT_RESULT		result;
+	const char		*expected_string, *actual_string;
+	int			expected_ret, actual_ret;
+
+	ZBX_UNUSED(state);
+
+	if ()
+
+	if (ZBX_MOCK_NO_PARAMETER == (error = get_out_parameter("json", &expected_string)))
+	{
+		if (ZBX_MOCK_NO_PARAMETER == (error = get_out_parameter("error", &expected_string)))
+			fail_msg("Invalid test case data: expected \"json\" or \"error\" out parameter");
+
+		expected_ret = SYSINFO_RET_FAIL;
+	}
+	else
+		expected_ret = SYSINFO_RET_OK;
+
+	/* NET_IF_DISCOVERY() does not use request */
+	actual_ret = NET_IF_DISCOVERY(&request, &result);
+
+	if (actual_ret != expected_ret)
+	{
+		fail_msg("Unexpected return code from NET_IF_DISCOVERY(): expected %d, got %d", expected_ret,
+				actual_ret);
+	}
+
+	if (SYSINFO_RET_OK == actual_ret)
+		actual_string = *GET_STR_RESULT(&result);
+	else if (SYSINFO_RET_FAIL == actual_ret)
+		actual_string = *GET_MSG_RESULT(&result);
+	else
+		fail_msg("Unsupported return code from NET_IF_DISCOVERY(): %d", actual_ret);
+
+	if (0 != strcmp(expected_string, actual_string))
+		fail_msg("Unexpected result string: expected \"%s\", got \"%s\"", expected_string, actual_string);
+}
+
 /* returns ZBX_MOCK_SUCCESS or ZBX_MOCK_NO_PARAMETER */
 static zbx_mock_error_t	get_out_parameter(const char *name, const char **value)
 {
@@ -37,44 +81,4 @@ static zbx_mock_error_t	get_out_parameter(const char *name, const char **value)
 		FAIL_PARAM(name, error);
 
 	return error;
-}
-
-void	zbx_mock_test_entry(void **state)
-{
-	zbx_mock_error_t	error;
-	AGENT_REQUEST		request;
-	AGENT_RESULT		result;
-	const char		*expected_string, *actual_string;
-	int			expected_result, actual_result;
-
-	ZBX_UNUSED(state);
-
-	if (ZBX_MOCK_NO_PARAMETER == (error = get_out_parameter("json", &expected_string)))
-	{
-		if (ZBX_MOCK_NO_PARAMETER == (error = get_out_parameter("error", &expected_string)))
-			fail_msg("Invalid test case data: expected \"json\" or \"error\" out parameter");
-
-		expected_result = SYSINFO_RET_FAIL;
-	}
-	else
-		expected_result = SYSINFO_RET_OK;
-
-	/* NET_IF_DISCOVERY() does not use request */
-	actual_result = NET_IF_DISCOVERY(&request, &result);
-
-	if (actual_result != expected_result)
-	{
-		fail_msg("Unexpected return code from NET_IF_DISCOVERY(): expected %d, got %d", expected_result,
-				actual_result);
-	}
-
-	if (SYSINFO_RET_OK == actual_result)
-		actual_string = *GET_STR_RESULT(&result);
-	else if (SYSINFO_RET_FAIL == actual_result)
-		actual_string = *GET_MSG_RESULT(&result);
-	else
-		fail_msg("Unsupported  return code from NET_IF_DISCOVERY(): %d", actual_result);
-
-	if (0 != strcmp(expected_string, actual_string))
-		fail_msg("Unexpected result string: expected \"%s\", got \"%s\"", expected_string, actual_string);
 }
