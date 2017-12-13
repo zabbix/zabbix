34d27c4d597da3a0630fdcfe73c5cda65a5fb625
Oleg Egorov <git-no-reply@zabbix.com>
2017-12-13 10:20:28 +0000

    A......... [ZBXNEXT-4119] added additional check

--- frontends/php/include/classes/api/services/CEvent.php
+++ frontends/php/include/classes/api/services/CEvent.php
@@ -209,7 +209,8 @@ class CEvent extends CApiService {
 					foreach ($userGroups as $usrgrpid) {
 						if (array_key_exists($usrgrpid, $tag_filters_tmp)) {
 							foreach ($tag_filters_tmp[$usrgrpid] as $groupid => $tag_filter) {
-								if (array_key_exists($groupid, $rights[$usrgrpid])) {
+								if (array_key_exists($usrgrpid, $rights)
+										&& array_key_exists($groupid, $rights[$usrgrpid])) {
 									$tag_filters[$groupid] = $tag_filter;
 									unset($rights[$usrgrpid][$groupid]);
 								}
