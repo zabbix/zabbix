0fbc4d75c4aa738a04a8594ee0b36c13011d487b
gcalenko <gcalenko@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2017-12-14 10:50:50 +0000

    ..F....... [ZBX-12856] fixed empty host filter when adding dependent trigger in trigger edit form; fixed undefined index in trigger form
    git-svn-id: svn://svn.zabbix.com/trunk@75882 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/trigger_prototypes.php
+++ frontends/php/trigger_prototypes.php
@@ -504,7 +504,7 @@ elseif (isset($_REQUEST['form'])) {
 		'recovery_expression_constructor' => getRequest('recovery_expression_constructor', IM_ESTABLISHED),
 		'limited' => false,
 		'templates' => [],
-		'hostid' => getRequest('hostid', 0),
+		'hostid' => getRequest('hostid', $discoveryRule['hostid']),
 		'expression_action' => $expression_action,
 		'recovery_expression_action' => $recovery_expression_action,
 		'tags' => getRequest('tags', []),
@@ -512,18 +512,6 @@ elseif (isset($_REQUEST['form'])) {
 		'correlation_tag' => getRequest('correlation_tag', ''),
 		'manual_close' => getRequest('manual_close', ZBX_TRIGGER_MANUAL_CLOSE_NOT_ALLOWED)
 	]);
-
-	$data['hostid'] = $discoveryRule['hostid'];
-
-	$db_hostgroup = API::HostGroup()->get([
-		'output' => ['groupid'],
-		'hostids' => $data['hostid'],
-		'templateids' => $data['hostid']
-	]);
-
-	if ($db_hostgroup) {
-		$data['groupid'] = $db_hostgroup[0]['groupid'];
-	}
 
 	$triggersView = new CView('configuration.trigger.prototype.edit', $data);
 	$triggersView->render();
