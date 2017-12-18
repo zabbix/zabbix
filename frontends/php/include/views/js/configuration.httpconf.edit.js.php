83371fee1e4d1b3c61f4e695a7a586d9561d12f1
Valdis Murzins <valdis.murzins@zabbix.com>
2017-12-18 18:46:14 +0000

    ..F....... [ZBXNEXT-4127] fixed js overlapping between scenario pairs and step pairs

--- frontends/php/include/views/js/configuration.httpconf.edit.js.php
+++ frontends/php/include/views/js/configuration.httpconf.edit.js.php
@@ -12,7 +12,7 @@
 				->setAttribute('data-type', 'name')
 				->setAttribute('placeholder', _('name'))
 				->setWidth(ZBX_TEXTAREA_TAG_WIDTH),
-			'⇒',
+			'&rArr;',
 			(new CTextBox('pairs[#{pair.id}][value]', '#{pair.value}'))
 				->setAttribute('data-type', 'value')
 				->setAttribute('placeholder', _('value'))
@@ -41,9 +41,9 @@
 		var rowTemplate = new Template(jQuery('#scenarioPairRow').html()),
 			allPairs = {};
 
-		function renderPairRow(pair) {
+		function renderPairRow(formid, pair) {
 			var parent,
-				target = jQuery(getDomTargetIdForRowInsert(pair.type)),
+				target = jQuery(getDomTargetIdForRowInsert(pair.type), jQuery('#'+formid)),
 				pair_row = jQuery(rowTemplate.evaluate({'pair': pair}));
 
 			if (!target.parents('.pair-container').hasClass('pair-container-sortable')) {
@@ -75,13 +75,14 @@
 			return pair;
 		}
 
-		function createNewPair(typeName) {
-			var newPair = {
+		function createNewPair(options) {
+			var newPair = jQuery.extend({
+				formid: '',
 				isNew: true,
-				type: typeName,
+				type: '',
 				name: '',
 				value: ''
-			};
+			}, options);
 
 			newPair.id = 1;
 			while (allPairs[newPair.id] !== void(0)) {
@@ -91,6 +92,7 @@
 			return addPair(newPair);
 		}
 
+		// Make sortable handler inactive, if nothing to sort.
 		function refreshContainers() {
 			jQuery('.pair-container-sortable').each(function() {
 				jQuery(this).sortable({
@@ -99,82 +101,167 @@
 			});
 		}
 
-		function moveRowToAnotherTypeTable(pairId, type) {
-			jQuery('#pair_type_' + pairId).val(type);
-			jQuery('#pairRow_' + pairId).insertBefore(getDomTargetIdForRowInsert(type));
-
-			refreshContainers();
-		}
-
 		return {
-			add: function(pairs) {
+			// Add when form is beeing created
+			add: function(formid, pairs) {
 				for (var i = 0; i < pairs.length; i++) {
-					renderPairRow(addPair(pairs[i]));
+					pairs[i]['formid'] = formid;
+					renderPairRow(formid, addPair(pairs[i]));
 				}
 
-				jQuery('.pair-container').each(function() {
+				jQuery('.pair-container', jQuery('#'+formid)).each(function() {
 					var rows = jQuery(this).find('.pairRow').length;
 					if (rows === 0) {
-						renderPairRow(createNewPair(this.id));
+						renderPairRow(formid, createNewPair({formid: formid, type: this.id}));
 					}
 				});
 
 				refreshContainers();
 			},
 
-			addNew: function(type) {
-				renderPairRow(createNewPair(type));
+			// Add empty pair, when 'Add new' button is pressed
+			addNew: function(formid, options) {
+				options.formid = formid;
+				renderPairRow(formid, createNewPair(options));
 				refreshContainers();
 			},
 
+			// Delete pair with given ID from allPairs
 			remove: function(pairId) {
 				delete allPairs[pairId];
 				refreshContainers();
 			},
 
-			setType: function(pairId, type) {
-				if (allPairs[pairId].type !== type) {
-					moveRowToAnotherTypeTable(pairId, type);
-					allPairs[pairId].type = type;
+			initControls: function(formid) {
+				var $form = jQuery('#'+formid);
+				$form.on('click', 'button.remove', function() {
+					var pairId = jQuery(this).data('pairid');
+					jQuery('#pairRow_' + pairId).remove();
+					pairManager.remove(pairId);
+				});
+
+				jQuery('.pair-container-sortable', $form).sortable({
+					disabled: (jQuery(this).find('tr.sortable').length < 2),
+					items: 'tr.sortable',
+					axis: 'y',
+					cursor: 'move',
+					containment: 'parent',
+					handle: 'div.<?= ZBX_STYLE_DRAG_ICON ?>',
+					tolerance: 'pointer',
+					opacity: 0.6,
+					helper: function(e, ui) {
+						return ui;
+					},
+					start: function(e, ui) {
+						$(ui.placeholder).height($(ui.helper).height());
+					}
+				});
+
+				jQuery('.pairs-control-add', $form).on('click', function() {
+					pairManager.addNew(formid, {type:jQuery(this).data('type')});
+				});
+
+				jQuery('#retrieve_mode', $form)
+					.on('change', function() {
+						jQuery('#post_fields', $form).toggleClass('disabled',this.checked);
+						jQuery('#required, #posts, #post_fields input[type="text"], #post_fields .btn-link, #post_type input', $form)
+							.attr('disabled', this.checked);
+
+						if (this.checked === false) {
+							pairManager.refresh();
+						}
+					})
+					.trigger('change');
+			},
+
+			/**** Steps popup functions ****/
+			refresh: function() {
+				refreshContainers();
+			},
+
+			// Merge 'query_fields' values with ones from URL
+			merge: function (formid, pairs) {
+				var	pair,
+					queryFields = [],
+					existingPairs = Object.keys(allPairs);
+
+				if (pairs.length > 0) {
+					this.cleanup(formid, 'query_fields');
+				}
+
+				for (var p = 0; p < existingPairs.length; p++) {
+					if (allPairs[existingPairs[p]] !== undefined &&
+						allPairs[existingPairs[p]].type === 'query_fields' &&
+						allPairs[existingPairs[p]].formid === formid &&
+						allPairs[existingPairs[p]].name.indexOf('[]') === -1) {
+						queryFields.push(allPairs[existingPairs[p]]);
+					}
+				}
+
+				for (var i = 0; i < pairs.length; i++) {
+					pair = null;
+					for (var p = 0; p < queryFields.length; p++) {
+						if (queryFields[p].name === pairs[i].name) {
+							pair = queryFields[p];
+							break;
+						}
+					}
+
+					if (pair === null) {
+						renderPairRow(formid, createNewPair({
+							formid: formid,
+							type: 'query_fields',
+							name: pairs[i].name,
+							value: pairs[i].value
+						}));
+					}
+					else {
+						jQuery('#pair_value_' + pair.id).val(pairs[i].value);
+					}
+				}
+
+				refreshContainers();
+			},
+
+			// Removes all new pairs with empty name and value (of given type withing given form)
+			cleanup: function(formid, type) {
+				var pairs = this.getPairsByType(formid, type);
+
+				for (var p = 0; p < pairs.length; p++) {
+					if (pairs[p].isNew === true && pairs[p].name === '' && pairs[p].value === '') {
+						jQuery('#pairRow_' + pairs[p].id).remove();
+						delete allPairs[pairs[p].id];
+					}
 				}
 			},
 
-			refresh: function() {
-				refreshContainers();
+			// Finds all pairs of given type within given form
+			getPairsByType: function(formid, type) {
+				var	pairs = [],
+					existingPairs = Object.keys(allPairs);
+
+				for (var p = 0; p < existingPairs.length; p++) {
+					if (allPairs[existingPairs[p]].type === type && allPairs[existingPairs[p]].formid === formid) {
+						pairs.push(allPairs[existingPairs[p]]);
+					}
+				}
+
+				return pairs;
+			},
+
+			// Removes all pairs and their fields of given type in given form.
+			removeAll: function(formid, type) {
+				var pairs = Object.keys(allPairs);
+
+				for (var p = 0; p < pairs.length; p++) {
+					if (allPairs[pairs[p]].type === type && allPairs[pairs[p]].formid === formid) {
+						jQuery('#pairRow_' + pairs[p]).remove();
+						delete allPairs[pairs[p]];
+					}
+				}
 			}
 		};
 	}());
-
-	jQuery(document).ready(function() {
-		'use strict';
-
-		jQuery('#httpFormList').on('click', 'button.remove', function() {
-			var pairId = jQuery(this).data('pairid');
-			jQuery('#pairRow_' + pairId).remove();
-			pairManager.remove(pairId);
-		});
-
-		jQuery('.pair-container-sortable').sortable({
-			disabled: (jQuery(this).find('tr.sortable').length < 2),
-			items: 'tr.sortable',
-			axis: 'y',
-			cursor: 'move',
-			containment: 'parent',
-			handle: 'div.<?= ZBX_STYLE_DRAG_ICON ?>',
-			tolerance: 'pointer',
-			opacity: 0.6,
-			helper: function(e, ui) {
-				return ui;
-			},
-			start: function(e, ui) {
-				$(ui.placeholder).height($(ui.helper).height());
-			}
-		});
-
-		jQuery('.pairs-control-add').on('click', function() {
-			pairManager.addNew(jQuery(this).data('type'));
-		});
-	});
 
 	function removeStep(obj) {
 		var step = obj.getAttribute('remove_step'),
@@ -193,6 +280,8 @@
 		recalculateSortOrder();
 	}
 
+	// Changes ID's of steps in table (data in row and all hidden fields with step data),
+	// after one of existing steps is deleted.
 	function recalculateSortOrder() {
 		var i = 0;
 
@@ -369,4 +458,294 @@
 		$('#agent').trigger('change');
 		$('#authentication').trigger('change');
 	});
+
+	/**** Steps popup functions ****/
+	// Inital post time selection
+	function setPostType(formid, type) {
+		var $form = jQuery('#'+formid);
+		if (type == <?= ZBX_POSTTYPE_FORM ?>) {
+			jQuery('#post_fields_row', $form).css("display", 'table-row');
+			jQuery('#post_raw_row', $form).css("display", "none");
+		}
+		else {
+			jQuery('#post_fields_row', $form).css("display", "none");
+			jQuery('#post_raw_row', $form).css("display", 'table-row');
+		}
+
+		jQuery('input[name="post_type"][value="' + type + '"]', $form).prop("checked", true);
+	}
+
+	function switchToPostType(formid, type) {
+		if (type == <?= ZBX_POSTTYPE_FORM ?>) {
+			var	posts = jQuery('#posts', jQuery('#'+formid)).val(),
+				fields,
+				parts,
+				pair,
+				pairs = [];
+
+			if (posts !== '') {
+				fields = posts.split('&');
+
+				try {
+					for (var i = 0; i < fields.length; i++) {
+						parts = fields[i].split('=');
+						if (parts.length === 1) {
+							parts.push('');
+						}
+
+						pair = {};
+						try {
+							if (parts.length > 2) {
+								throw null;
+							}
+
+							if (/%[01]/.match(parts[0]) || /%[01]/.match(parts[1]) ) {
+								throw null; /* non-printable characters in data */
+							}
+
+							pair.name = decodeURIComponent(parts[0].replace(/\+/g, " "));
+							pair.value = decodeURIComponent(parts[1].replace(/\+/g, " "));
+						}
+						catch(e) {
+							throw <?= CJs::encodeJson(_('Data is not properly encoded.')); ?>;
+						}
+
+						if (pair.name === '') {
+							throw <?= CJs::encodeJson(_('Values without names are not allowed in form fields.')); ?>;
+						}
+
+						if (pair.name.length > 255) {
+							throw <?= CJs::encodeJson(_('Name of the form field should not exceed 255 characters.')); ?>;
+						}
+
+						pairs.push(pair);
+					}
+				}
+				catch(e) {
+					jQuery('input[name="post_type"][value="<?= ZBX_POSTTYPE_RAW ?>"]', jQuery('#'+formid))
+						.prop("checked", true);
+
+					overlayDialogue({
+						'title': <?= CJs::encodeJson(_('Error')); ?>,
+						'content': jQuery('<span>').html(<?=
+							CJs::encodeJson(
+								_('Cannot convert POST data from raw data format to form field data format.').'<br><br>'
+							); ?> + e),
+						'buttons': [
+							{
+								title: <?= CJs::encodeJson(_('Ok')); ?>,
+								class: 'btn-alt',
+								focused: true,
+								action: function() {}
+							}
+						]
+					});
+
+					return false;
+				}
+			}
+
+			pairManager.removeAll(formid, 'post_fields');
+			for (var i = 0; i < pairs.length; i++) {
+				pairManager.addNew(formid, {
+					type: 'post_fields',
+					name: pairs[i].name,
+					value: pairs[i].value
+				});
+			}
+			pairManager.refresh();
+		}
+		else {
+			var fields = [],
+				parts,
+				pairs = pairManager.getPairsByType(formid, 'post_fields');
+
+			for (var p = 0; p < pairs.length; p++) {
+				parts = [];
+				if (pairs[p].name !== '') {
+					parts.push(encodeURIComponent(pairs[p].name.replace(/'/g,"%27").replace(/"/g,"%22")));
+				}
+				if (pairs[p].value !== '') {
+					parts.push(encodeURIComponent(pairs[p].value.replace(/'/g,"%27").replace(/"/g,"%22")));
+				}
+				if (parts.length > 0) {
+					fields.push(parts.join('='));
+				}
+			}
+
+			jQuery('#posts').val(fields.join('&'));
+		}
+
+		setPostType(formid, type);
+	}
+
+	// Parse actin for URL field
+	function parseUrl(formid) {
+		var i,
+			query,
+			index,
+			fields,
+			pair,
+			hasErrors = false,
+			pairs = [],
+			target = jQuery("#url", jQuery('#'+formid)),
+			url = target.val();
+
+		index = url.indexOf("#");
+		if (index !== -1)
+			url = url.substring(0, index);
+
+		index = url.indexOf("?");
+		if (index !== -1) {
+			query = url.substring(index + 1);
+			url = url.substring(0, index);
+
+			fields = query.split('&');
+			for (i = 0; i < fields.length; i++) {
+				if (fields[i].length === 0 || fields[i] === '=')
+					continue;
+
+				pair = {};
+				index = fields[i].indexOf("=");
+				if (index > 0) {
+					pair.name = fields[i].substring(0, index);
+					pair.value = fields[i].substring(index + 1);
+				}
+				else {
+					if (index === 0) {
+						fields[i] = fields[i].substring(1);
+					}
+					pair.name = fields[i];
+					pair.value = "";
+				}
+
+				try {
+					if (/%[01]/.match(pair.name) || /%[01]/.match(pair.value) ) {
+						throw null; /* non-printable characters in URL */
+					}
+					pair.name = decodeURIComponent(pair.name.replace(/\+/g,  " "));
+					pair.value = decodeURIComponent(pair.value.replace(/\+/g,  " "));
+				}
+				catch( e ) {
+					/* malformed url */
+					hasErrors = true;
+					break;
+				}
+
+				pairs.push(pair);
+			}
+
+			if (true === hasErrors) {
+				overlayDialogue({
+					'title': <?= CJs::encodeJson(_('Error')); ?>,
+					'content': jQuery('<span>').html(<?=
+						CJs::encodeJson(_('Failed to parse URL.').'<br><br>'._('URL is not properly encoded.'));
+					?>),
+					'buttons': [
+						{
+							title: <?= CJs::encodeJson(_('Ok')); ?>,
+							class: 'btn-alt',
+							focused: true,
+							action: function() {}
+						}
+					]
+				});
+
+				return false;
+			}
+
+			pairManager.merge(formid, pairs);
+		}
+
+		target.val(url);
+	}
+
+	function add_httpstep(formname, httpstep) {
+		var form = window.document.forms[formname];
+		if (!form) {
+			return false;
+		}
+
+		add_var_to_opener_obj(form, 'new_httpstep[name]', httpstep.name);
+		add_var_to_opener_obj(form, 'new_httpstep[timeout]', httpstep.timeout);
+		add_var_to_opener_obj(form, 'new_httpstep[url]', httpstep.url);
+		add_var_to_opener_obj(form, 'new_httpstep[posts]', httpstep.posts);
+		add_var_to_opener_obj(form, 'new_httpstep[post_type]', httpstep.post_type);
+		add_var_to_opener_obj(form, 'new_httpstep[required]', httpstep.required);
+		add_var_to_opener_obj(form, 'new_httpstep[status_codes]', httpstep.status_codes);
+		add_var_to_opener_obj(form, 'new_httpstep[follow_redirects]', httpstep.follow_redirects);
+		add_var_to_opener_obj(form, 'new_httpstep[retrieve_mode]', httpstep.retrieve_mode);
+
+		addPairsToOpenerObject(form, 'new_httpstep', httpstep.pairs);
+
+		form.submit();
+		return true;
+	}
+
+	function update_httpstep(formname, list_name, httpstep) {
+		var prefix,
+			form = window.document.forms[formname];
+
+		if (!form) {
+			return false;
+		}
+
+		prefix = list_name + '[' + httpstep.stepid + ']';
+
+		add_var_to_opener_obj(form, prefix + '[name]', httpstep.name);
+		add_var_to_opener_obj(form, prefix + '[timeout]', httpstep.timeout);
+		add_var_to_opener_obj(form, prefix + '[url]', httpstep.url);
+		add_var_to_opener_obj(form, prefix + '[posts]', httpstep.posts);
+		add_var_to_opener_obj(form, prefix + '[post_type]', httpstep.post_type);
+		add_var_to_opener_obj(form, prefix + '[required]', httpstep.required);
+		add_var_to_opener_obj(form, prefix + '[status_codes]', httpstep.status_codes);
+		add_var_to_opener_obj(form, prefix + '[follow_redirects]', httpstep.follow_redirects);
+		add_var_to_opener_obj(form, prefix + '[retrieve_mode]', httpstep.retrieve_mode);
+
+		addPairsToOpenerObject(form, prefix, httpstep.pairs);
+		form.submit();
+		return true;
+	}
+
+	function add_var_to_opener_obj(obj, name, value) {
+		var input = window.document.createElement('input');
+
+		input.value = value;
+		input.type = 'hidden';
+		input.name = name;
+		obj.appendChild(input);
+	}
+
+	function addPairsToOpenerObject(obj, name, stepPairs) {
+		var prefix,
+			keys,
+			pairs,
+			inputs;
+
+		name += '[pairs]';
+		inputs = jQuery(window.document).find('input[name^="' + name + '"]');
+		for (var i = 0; i < inputs.length; i++) {
+			inputs[i].remove();
+		}
+
+		pairs = Object.keys(stepPairs);
+		for (var i = 0; i < pairs.length; i++) {
+			if (!/[0-9]+/.match(pairs[i])) {
+				continue;
+			}
+
+			var pair = stepPairs[pairs[i]];
+			prefix = name + '[' + pair.id + ']';
+
+			/* empty values are ignored */
+			if (pair.name === undefined || (pair.isNew !== undefined && pair.name === '' && pair.value === '')) {
+				continue;
+			}
+
+			keys = Object.keys(pair);
+			for (var p = 0; p < keys.length; p++) {
+				add_var_to_opener_obj(obj, prefix + '[' + keys[p] + ']', pair[keys[p]]);
+			}
+		}
+	}
 </script>
