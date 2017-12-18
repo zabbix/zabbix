2967e8f3832d02c0d8e083fb91c596fc9cd071ca
Oleg Egorov <git-no-reply@zabbix.com>
2017-12-18 08:43:24 +0000

    ..F....... [ZBXNEXT-4119] fixed user group popup

--- frontends/php/include/views/administration.usergroups.edit.php
+++ frontends/php/include/views/administration.usergroups.edit.php
@@ -217,8 +217,13 @@ $new_permissions_table = (new CTable())
 			'objectName' => 'hostGroup',
 			'styles' => ['margin-top' => '-.3em'],
 			'popup' => [
-				'parameters' => 'srctbl=host_groups&dstfrm='.$userGroupForm->getName().
-					'&dstfld1=tag_filter_groupids_&srcfld1=groupid&multiselect=1'
+				'parameters' => [
+					'srctbl' => 'host_groups',
+					'dstfrm' => $userGroupForm->getName(),
+					'dstfld1' => 'tag_filter_groupids_',
+					'srcfld1' => 'groupid',
+					'multiselect' => '1'
+				]
 			]
 		]))->setWidth(ZBX_TEXTAREA_STANDARD_WIDTH),
 		new CCol(
