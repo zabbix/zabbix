8c81e0a661bbccc3f82f298a6e799944a4f64c7d
vladislavs <vladislavs@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2017-12-29 10:04:00 +0000

    ...G...... [ZBX-13264] fixed Zabbix agent compilation on OS/X
    git-svn-id: svn://svn.zabbix.com/trunk@76444 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/libs/zbxsysinfo/common/dir.c
+++ src/libs/zbxsysinfo/common/dir.c
@@ -933,8 +933,8 @@ static int	vfs_dir_count(AGENT_REQUEST *request, AGENT_RESULT *result)
 						(S_ISFIFO(status.st_mode) && 0 != (types & DET_FIFO))) &&
 						(min_size <= (zbx_uint64_t)status.st_size
 								&& (zbx_uint64_t)status.st_size <= max_size) &&
-						(min_time < status.st_mtim.tv_sec &&
-								status.st_mtim.tv_sec <= max_time))
+						(min_time < status.st_mtime &&
+								status.st_mtime <= max_time))
 				{
 					++count;
 				}
