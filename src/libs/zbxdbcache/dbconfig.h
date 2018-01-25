98e469c2aa5ebc6a05808ef61b240f68e9456be5
viktors <viktors@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2018-01-25 15:09:14 +0000

    .......PS. [ZBX-13060] added limitation for meaningless server reconnection attempts to incorrectly configured passive proxy (viktors) 	   [merge ^/branches/3.4 -c r77204]
    git-svn-id: svn://svn.zabbix.com/trunk@77205 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/libs/zbxdbcache/dbconfig.h
+++ src/libs/zbxdbcache/dbconfig.h
@@ -334,6 +334,8 @@ typedef struct
 	int		nextcheck;
 	int		timediff;
 	int		lastaccess;
+	int		last_cfg_error_time;	/* time when passive proxy misconfiguration error was seen */
+						/* or 0 if no error */
 	int		version;
 	unsigned char	location;
 }
