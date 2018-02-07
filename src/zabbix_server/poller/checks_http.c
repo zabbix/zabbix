deb8ee1b944d60974ec17fbd1136f050303e8281
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2018-02-07 09:39:34 +0000

    .......PS. [ZBXNEXT-4358] http check request

--- src/zabbix_server/poller/checks_http.c
+++ src/zabbix_server/poller/checks_http.c
@@ -24,6 +24,7 @@ extern void	add_headers(char *headers, struct curl_slist **headers_slist);
 
 #define HTTP_CHECK_GET	0
 #define HTTP_CHECK_POST	1
+#define HTTP_CHECK_PUT	2
 #define HTTP_CHECK_HEAD	3
 
 extern char	*CONFIG_SOURCE_IP;
@@ -41,6 +42,23 @@ typedef struct
 zbx_httppage_t;
 
 static zbx_httppage_t	page;
+
+static const char	*zbx_request_string(int result)
+{
+	switch (result)
+	{
+		case HTTP_CHECK_GET:
+			return "GET";
+		case HTTP_CHECK_POST:
+			return "POST";
+		case HTTP_CHECK_PUT:
+			return "PUT";
+		case HTTP_CHECK_HEAD:
+			return "HEAD";
+		default:
+			return "unknown";
+	}
+}
 
 static size_t	WRITEFUNCTION2(void *ptr, size_t size, size_t nmemb, void *userdata)
 {
@@ -171,11 +189,16 @@ static int	prepare_https(CURL *easyhandle, const DC_ITEM *item, char **error)
 
 int	get_value_http(const DC_ITEM *item, AGENT_RESULT *result)
 {
+	const char		*__function_name = "get_value_http";
 	CURL			*easyhandle = NULL;
 	CURLcode		err;
 	char			errbuf[CURL_ERROR_SIZE], *error = NULL;
-	int			ret = NOTSUPPORTED;
+	int			ret = NOTSUPPORTED, timeout_seconds;
+	long			response_code;
 	struct curl_slist	*headers_slist = NULL;
+
+	zabbix_log(LOG_LEVEL_DEBUG, "In %s() request '%s' url '%s'", __function_name,
+			zbx_request_string(item->request_method),item->url);
 
 	if (NULL == (easyhandle = curl_easy_init()))
 	{
@@ -185,7 +208,6 @@ int	get_value_http(const DC_ITEM *item, AGENT_RESULT *result)
 	}
 
 	if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_PROXY, item->http_proxy)) ||
-			CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_COOKIEFILE, "")) ||
 			CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, WRITEFUNCTION2)) ||
 			CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_HEADERFUNCTION, HEADERFUNCTION2)) ||
 			CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_ERRORBUFFER, errbuf)))
@@ -217,52 +239,154 @@ int	get_value_http(const DC_ITEM *item, AGENT_RESULT *result)
 		}
 	}
 
-	if (HTTP_CHECK_POST == item->request_method)
+	if (HTTPTEST_AUTH_NONE != item->authtype)
 	{
-		zabbix_log(LOG_LEVEL_DEBUG, "post body '%s'", item->params);
+		long		curlauth = 0;
+		char		auth[MAX_STRING_LEN];
 
-		if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_POSTFIELDS, item->params)))
+		zabbix_log(LOG_LEVEL_DEBUG, "setting HTTPAUTH [%d]", item->authtype);
+
+		switch (item->authtype)
 		{
-			SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot specify data to POST to server: %s",
+			case HTTPTEST_AUTH_BASIC:
+				curlauth = CURLAUTH_BASIC;
+				break;
+			case HTTPTEST_AUTH_NTLM:
+				curlauth = CURLAUTH_NTLM;
+				break;
+			default:
+				THIS_SHOULD_NEVER_HAPPEN;
+				break;
+		}
+
+		if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_HTTPAUTH, curlauth)))
+		{
+			SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot set HTTP server authentication method: %s",
 					curl_easy_strerror(err)));
 			goto clean;
 		}
 
-		if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_POST, 1L)))
+		zbx_snprintf(auth, sizeof(auth), "%s:%s", item->username, item->password);
+		if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_USERPWD, auth)))
 		{
-			SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot specify HTTP POST request: %s",
+			SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot set user name and password: %s",
 					curl_easy_strerror(err)));
 			goto clean;
 		}
 	}
-	else if (HTTP_CHECK_GET == item->request_method)
+
+	switch (item->request_method)
 	{
-		curl_easy_setopt(easyhandle, CURLOPT_HTTPGET, 1L);
-	}
-	else if (HTTP_CHECK_HEAD == item->request_method)
-	{
-		curl_easy_setopt(easyhandle, CURLOPT_NOBODY, 1L);
-	}
-	else
-	{
-		THIS_SHOULD_NEVER_HAPPEN;
-		SET_MSG_RESULT(result, zbx_strdup(NULL, "Unsupported request method"));
-		goto clean;
+		case HTTP_CHECK_POST:
+			if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_POSTFIELDS, item->params)))
+			{
+				SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot specify data to POST to server: %s",
+						curl_easy_strerror(err)));
+				goto clean;
+			}
+			break;
+		case HTTP_CHECK_GET:
+			if ('\0' == *item->params)
+				break;
+
+			if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_POSTFIELDS, item->params)))
+			{
+				SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot specify data to POST to server: %s",
+						curl_easy_strerror(err)));
+				goto clean;
+			}
+
+			if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_CUSTOMREQUEST, "GET")))
+			{
+				SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot specify custom GET request: %s",
+						curl_easy_strerror(err)));
+				goto clean;
+			}
+			break;
+		case HTTP_CHECK_HEAD:
+			if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_NOBODY, 1L)))
+			{
+				SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot specify HEAD request: %s",
+						curl_easy_strerror(err)));
+				goto clean;
+			}
+			break;
+		case HTTP_CHECK_PUT:
+			if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_POSTFIELDS, item->params)))
+			{
+				SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot specify data to POST to server: %s",
+						curl_easy_strerror(err)));
+				goto clean;
+			}
+
+			if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_CUSTOMREQUEST, "PUT")))
+			{
+				SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot specify custom GET request: %s",
+						curl_easy_strerror(err)));
+				goto clean;
+			}
+			break;
+		default:
+			THIS_SHOULD_NEVER_HAPPEN;
+			SET_MSG_RESULT(result, zbx_strdup(NULL, "Unsupported request method"));
+			goto clean;
 	}
 
 	add_headers(item->headers, &headers_slist);
-
 	if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_HTTPHEADER, headers_slist)))
 	{
 		SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot specify headers: %s", curl_easy_strerror(err)));
 		goto clean;
 	}
 
-	curl_slist_free_all(headers_slist);	/* must be called after curl_easy_perform() */
+	if (FAIL == is_time_suffix(item->timeout, &timeout_seconds, strlen(item->timeout)))
+	{
+		SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Invalid timeout: %s", item->timeout));
+		goto clean;
+	}
 
+	if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_TIMEOUT, (long)timeout_seconds)))
+	{
+		SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot specify timeout: %s", curl_easy_strerror(err)));
+		goto clean;
+	}
+
+	if (CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_URL, item->url)))
+	{
+		SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot specify headers: %s", curl_easy_strerror(err)));
+		goto clean;
+	}
+
+	if (CURLE_OK != (err = curl_easy_perform(easyhandle)))
+	{
+		SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot perform request: %s", curl_easy_strerror(err)));
+		goto clean;
+	}
+
+	zabbix_log(LOG_LEVEL_TRACE, "received body '%s'", page.data);
+
+	if (CURLE_OK != (err = curl_easy_getinfo(easyhandle, CURLINFO_RESPONSE_CODE, &response_code)))
+	{
+		SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot get the response code: %s", curl_easy_strerror(err)));
+		goto clean;
+	}
+
+	if ('\0' != *item->status_codes && FAIL == int_in_list(item->status_codes, response_code))
+	{
+		SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Response code \"%ld\" did not match any of the"
+				" required status codes \"%s\"", response_code, item->status_codes));
+		goto clean;
+	}
+
+	SET_TEXT_RESULT(result, page.data);
+	ret = SUCCEED;
 clean:
+	if (NULL != headers_slist)
+		curl_slist_free_all(headers_slist);	/* must be called after curl_easy_perform() */
 	curl_easy_cleanup(easyhandle);
 	zbx_free(page.data);
+
+	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%s", __function_name, zbx_result_string(ret));
 
 	return ret;
 }
