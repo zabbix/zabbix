6ba267bb35022bf4869010742cd4925d11c7102c
sasha <sasha@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2018-02-08 15:36:20 +0000

    ........S. [ZBX-11391] fixed coding style issue
    git-svn-id: svn://svn.zabbix.com/trunk@77513 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/libs/zbxdbhigh/lld_trigger.c
+++ src/libs/zbxdbhigh/lld_trigger.c
@@ -2109,9 +2109,9 @@ static void	lld_triggers_validate(zbx_uint64_t hostid, zbx_vector_ptr_t *trigger
  *                                                                            *
  ******************************************************************************/
 static void	lld_validate_trigger_tag_field(zbx_lld_tag_t *tag, const char *field, zbx_uint64_t flag,
-		size_t field_len_max, char **error)
+		size_t field_len, char **error)
 {
-	size_t	field_len;
+	size_t	len;
 
 	if (0 == (tag->flags & ZBX_FLAG_LLD_TAG_DISCOVERED))
 		return;
@@ -2130,9 +2130,9 @@ static void	lld_validate_trigger_tag_field(zbx_lld_tag_t *tag, const char *field
 				field_utf8);
 		zbx_free(field_utf8);
 	}
-	else if ((field_len = zbx_strlen_utf8(field)) > field_len_max)
+	else if ((len = zbx_strlen_utf8(field)) > field_len)
 		*error = zbx_strdcatf(*error, "Cannot create trigger tag: value \"%s\" is too long.\n", field);
-	else if (0 != (flag & ZBX_FLAG_LLD_TAG_UPDATE_TAG) && 0 == field_len)
+	else if (0 != (flag & ZBX_FLAG_LLD_TAG_UPDATE_TAG) && 0 == len)
 		*error = zbx_strdcatf(*error, "Cannot create trigger tag: empty tag name.\n");
 	else
 		return;
