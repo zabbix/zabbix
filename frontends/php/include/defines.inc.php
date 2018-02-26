ae05831b4dc75eefe146a7307b4a72c100c7289e
natalja <natalja@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2018-02-26 14:20:33 +0000

    .......... [ZBXNEXT-826] updated release version [4.0.0alpha5]
    git-svn-id: svn://svn.zabbix.com/trunk@78035 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,7 +19,7 @@
 **/
 
 
-define('ZABBIX_VERSION',		'4.0.0alpha4');
+define('ZABBIX_VERSION',		'4.0.0alpha5');
 define('ZABBIX_API_VERSION',	'4.0.0');
 define('ZABBIX_EXPORT_VERSION',	'3.4');
 define('ZABBIX_DB_VERSION',		3050037);
