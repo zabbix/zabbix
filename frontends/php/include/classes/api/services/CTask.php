a35ae90332853fd46127596b57e2b7fe97841eb3
Ivo Kurzemnieks <ivo.kurzemnieks@zabbix.com>
2018-03-02 14:50:40 +0000

    A......... [DEV-794] added task API

---
+++ frontends/php/include/classes/api/services/CTask.php
@@ -0,0 +1,216 @@
+<?php
+/*
+** Zabbix
+** Copyright (C) 2001-2018 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+/**
+ * Class containing methods for operations with tasks.
+ */
+class CTask extends CApiService {
+
+	protected $tableName = 'task';
+	protected $tableAlias = 't';
+	protected $sortColumns = ['taskid'];
+
+	/**
+	 * @param array        $tasks             Array of tasks to create.
+	 * @param string|array $tasks['itemids']  Array of item and LLD rule IDs to create tasks for.
+	 *
+	 * @return array
+	 */
+	public function create(array $tasks) {
+		$this->validateCreate($tasks);
+
+		$tasks_to_create = [];
+
+		foreach ($tasks as $task) {
+			foreach ($task['itemids'] as $itemid) {
+				$tasks_to_create[] = [
+					'type' => $task['type'],
+					'status' => ZBX_TM_STATUS_NEW,
+					'clock' => time(),
+					'ttl' => SEC_PER_DAY
+				];
+			}
+		}
+
+		$taskids = DB::insert('task', $tasks_to_create);
+
+		$check_now_to_create = [];
+
+		foreach ($tasks as $idx => $task) {
+			foreach ($task['itemids'] as $itemid) {
+				$check_now_to_create[] = [
+					'taskid' => $taskids[$idx],
+					'itemid' => $itemid
+				];
+			}
+		}
+
+		DB::insert('task_check_now', $check_now_to_create);
+
+		return ['taskids' => $taskids];
+	}
+
+	/**
+	 * Validates the input for create method. Checks if user is at least a regular admin, validates user input, checks
+	 * if user has permissions to given items and LLD rules, checks item and LLD rule types, checks if items and
+	 * LLD rules are enabled, checks if host is monitored and it's not a template. And checks if tasks don't exist.
+	 *
+	 * @param array $tasks  Array of tasks to validate.
+	 *
+	 * @throws APIException if the input is invalid
+	 */
+	protected function validateCreate(array $tasks) {
+		if (self::$userData['type'] < USER_TYPE_ZABBIX_ADMIN) {
+			self::exception(ZBX_API_ERROR_PERMISSIONS, _('You do not have permission to perform this operation.'));
+		}
+
+		$api_input_rules = ['type' => API_OBJECTS, 'flags' => API_NOT_EMPTY | API_NORMALIZE, 'fields' => [
+			'type' =>			['type' => API_INT32, 'flags' => API_REQUIRED | API_NOT_EMPTY, 'in' => implode(',', [ZBX_TM_TASK_CHECK_NOW])],
+			'itemids' =>		['type' => API_IDS, 'flags' => API_REQUIRED | API_NOT_EMPTY | API_NORMALIZE, 'uniq' => true]
+		]];
+
+		if (!CApiInputValidator::validate($api_input_rules, $tasks, '/', $error)) {
+			self::exception(ZBX_API_ERROR_PARAMETERS, $error);
+		}
+
+		// Check if user has permissions to items and LLD rules.
+		$itemids = [];
+
+		foreach ($tasks as $task) {
+			$itemids = array_merge($itemids, $task['itemids']);
+		}
+
+		$items = API::Item()->get([
+			'output' => ['itemid', 'type', 'hostid', 'status'],
+			'itemids' => $itemids,
+			'editable' => true
+		]);
+
+		$discovery_rules = API::DiscoveryRule()->get([
+			'output' => ['itemid', 'type', 'hostid', 'status'],
+			'itemids' => $itemids,
+			'editable' => true
+		]);
+
+		if (!$items && !$discovery_rules) {
+			self::exception(ZBX_API_ERROR_PARAMETERS, _('No permissions to referred object or it does not exist!'));
+		}
+
+		// Validate item and LLD rule types and statuses, and collect host IDs for later.
+		$allowed_item_types = [
+			ITEM_TYPE_ZABBIX,
+			ITEM_TYPE_SNMPV1,
+			ITEM_TYPE_SIMPLE,
+			ITEM_TYPE_SNMPV2C,
+			ITEM_TYPE_INTERNAL,
+			ITEM_TYPE_SNMPV3,
+			ITEM_TYPE_AGGREGATE,
+			ITEM_TYPE_EXTERNAL,
+			ITEM_TYPE_DB_MONITOR,
+			ITEM_TYPE_IPMI,
+			ITEM_TYPE_SSH,
+			ITEM_TYPE_TELNET,
+			ITEM_TYPE_CALCULATED,
+			ITEM_TYPE_JMX
+		];
+
+		$hostids = [];
+
+		foreach ($items as $item) {
+			if (!in_array($item['type'], $allowed_item_types)) {
+				self::exception(ZBX_API_ERROR_PARAMETERS, _s('Cannot send request: %1$s.', _('wrong item type')));
+			}
+
+			if ($item['status'] != ITEM_STATUS_ACTIVE) {
+				self::exception(ZBX_API_ERROR_PARAMETERS, _s('Cannot send request: %1$s.', _('item is disabled')));
+			}
+
+			$hostids[$item['hostid']] = true;
+		}
+
+		foreach ($discovery_rules as $discovery_rule) {
+			if (!in_array($discovery_rule['type'], $allowed_item_types)) {
+				self::exception(ZBX_API_ERROR_PARAMETERS,
+					_s('Cannot send request: %1$s.', _('wrong discovery rule type'))
+				);
+			}
+
+			if ($discovery_rule['status'] != ITEM_STATUS_ACTIVE) {
+				self::exception(ZBX_API_ERROR_PARAMETERS,
+					_s('Cannot send request: %1$s.', _('discovery rule is disabled'))
+				);
+			}
+
+			$hostids[$discovery_rule['hostid']] = true;
+		}
+
+		// Check if those are actually hosts because given hostids could actually be templateids.
+		$hosts = API::Host()->get([
+			'output' => ['status'],
+			'hostids' => array_keys($hostids)
+		]);
+
+		if (!$hosts) {
+			self::exception(ZBX_API_ERROR_PARAMETERS, _('No permissions to referred object or it does not exist!'));
+		}
+
+		// Check host status. Allow only monitored hosts.
+		foreach ($hosts as $host) {
+			if ($host['status'] != HOST_STATUS_MONITORED) {
+				self::exception(ZBX_API_ERROR_PARAMETERS, _s('Cannot send request: %1$s.', _('host is not monitored')));
+			}
+		}
+
+		// Check if task already exists.
+		$itemids = zbx_objectValues($items, 'itemid');
+		$discovery_ruleids = zbx_objectValues($discovery_rules, 'itemid');
+
+		foreach ($items as $item) {
+			if (DBfetch(DBselect(
+					'SELECT t.taskid'.
+					' FROM task t'.
+					' LEFT JOIN task_check_now tcn ON t.taskid = tcn.taskid'.
+					' WHERE t.type = '.ZBX_TM_TASK_CHECK_NOW.
+						' AND '.dbConditionId('tcn.itemid', $itemids).
+						' AND (t.status = '.ZBX_TM_STATUS_NEW.' OR t.status = '.ZBX_TM_STATUS_INPROGRESS.')'))) {
+				self::exception(ZBX_API_ERROR_PARAMETERS,
+					_s('Cannot send request: %1$s.', _('item is already being checked'))
+				);
+			}
+		}
+
+		foreach ($discovery_rules as $discovery_rule) {
+			if (DBfetch(DBselect(
+					'SELECT t.taskid'.
+					' FROM task t'.
+					' LEFT JOIN task_check_now tcn ON t.taskid = tcn.taskid'.
+					' WHERE t.type = '.ZBX_TM_TASK_CHECK_NOW.
+						' AND '.dbConditionId('tcn.itemid', $discovery_ruleids).
+						' AND (t.status = '.ZBX_TM_STATUS_NEW.' OR t.status = '.ZBX_TM_STATUS_INPROGRESS.')'))) {
+				self::exception(ZBX_API_ERROR_PARAMETERS,
+					_s('Cannot send request: %1$s.', _('discovery rule is already being checked'))
+				);
+			}
+		}
+
+		self::exception(ZBX_API_ERROR_PARAMETERS, _('stop'));
+	}
+}
