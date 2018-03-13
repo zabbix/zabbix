140afcca8305ddce53c8349cc4566b2f6c3c8427
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2018-03-13 12:53:04 +0000

    .......PS. [DEV-794] style fixes

--- src/libs/zbxtasks/task.c
+++ src/libs/zbxtasks/task.c
@@ -791,7 +791,6 @@ static zbx_tm_check_now_t	*tm_json_deserialize_check_now(const struct zbx_json_p
 {
 	char			value[MAX_ID_LEN + 1];
 	zbx_uint64_t		itemid;
-	zbx_tm_check_now_t	*data = NULL;
 
 	if (SUCCEED != zbx_json_value_by_name(jp, ZBX_PROTO_TAG_ITEMID, value, sizeof(value)) ||
 			SUCCEED != is_uint64(value, &itemid))
