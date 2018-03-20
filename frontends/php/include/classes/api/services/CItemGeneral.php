6c817aa142a045eade9bbd395d154d6ce31ca6f8
Gregory Chalenko <Gregory.Chalenko@zabbix.com>
2018-03-20 09:23:21 +0000

    A......... [ZBXNEXT-4358,ZBXNEXT-4359] make field ruleid as requireed for item prototype

--- frontends/php/include/classes/api/services/CItemGeneral.php
+++ frontends/php/include/classes/api/services/CItemGeneral.php
@@ -313,7 +313,7 @@ abstract class CItemGeneral extends CApiService {
 					$item['name']
 				);
 
-				if ($this instanceof CItemPrototype	&& (!array_key_exists($fullItem['ruleid'], $discovery_rules)
+				if ($this instanceof CItemPrototype && (!array_key_exists($fullItem['ruleid'], $discovery_rules)
 						|| $discovery_rules[$fullItem['ruleid']]['hostid'] != $fullItem['hostid'])) {
 					self::exception(ZBX_API_ERROR_PARAMETERS,
 						_('No permissions to referred object or it does not exist!')
