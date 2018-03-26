f1ca0f3de1f5d04301abb75dc3be099e8687ab6f
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2018-03-26 17:39:39 +0000

    .......... [ZBXNEXT-826] updated release version [4.0.0alpha6]

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "26 March 2018";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "4.0.0alpha5";
+	static final String VERSION = "4.0.0alpha6";
 
 	static void printVersion()
 	{
