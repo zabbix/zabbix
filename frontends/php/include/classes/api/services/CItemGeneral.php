eb9f997e44b3d8baa75bd5296a9b00095843d3b3
Vjaceslavs Bogdanovs <vjaceslavs.bogdanovs@zabbix.com>
2018-04-12 06:05:06 +0000

    A......... [ZBXNEXT-4458] removed unreachable code

--- frontends/php/include/classes/api/services/CItemGeneral.php
+++ frontends/php/include/classes/api/services/CItemGeneral.php
@@ -428,7 +428,6 @@ abstract class CItemGeneral extends CApiService {
 						_s('Incorrect value for field "%1$s": %2$s.', 'username',
 								_('both username and password should be either present or empty'))
 					);
-					self::exception(ZBX_API_ERROR_PARAMETERS, _('No authentication user name specified.'));
 				}
 			}
 			else {
