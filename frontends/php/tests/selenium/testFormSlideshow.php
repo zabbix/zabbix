289c6326c7c79d3385d91b751f5c74a707c73320
Miks Kronkalns <miks.kronkalns@zabbix.com>
2018-04-18 09:04:24 +0000

    ..F....... [DEV-668] fixed multiple issues in slideshow selenium tests

--- frontends/php/tests/selenium/testFormSlideshow.php
+++ frontends/php/tests/selenium/testFormSlideshow.php
@@ -47,52 +47,69 @@ class testFormSlideshow extends CWebTest {
 		$this->webDriver->findElement(WebDriverBy::xpath('//*[@id="delay"]'))->clear();
 		$this->zbxTestInputTypeWait('delay', $data['delay']);
 
-		// Select slides.
+		// Select slides clicking on the first link that appears in popup window.
 		$this->zbxTestClick('add');
 		$this->zbxTestWaitWindowAndSwitchToIt('zbx_popup');
 		$this->zbxTestCheckHeader('Screens');
-		$this->zbxTestCheckboxSelect('all_screens');
-		$this->zbxTestClick('select');
-		$this->webDriver->switchTo()->window('');
+		$this->zbxTestWaitUntilElementVisible(WebDriverBy::tagName('a'));
+		$this->webDriver->findElement(WebDriverBy::tagName('a'))->click();
+		$this->zbxTestWaitWindowClose();
 
-		// Click on submit button.
+		/**
+		 * Click on submit button.
+		 * Full XPath required because there are multiple [id="add"] elements.
+		 */
 		$submit_btn_selector = WebDriverBy::cssSelector('button[id="add"][type="submit"]');
 		$this->webDriver->findElement($submit_btn_selector)->click();
 
+		// Wait until page is loaded, just to be sure that creation of slideshow is completed on validation.
+		$this->zbxTestWaitUntilElementVisible(WebDriverBy::xpath('//*[@value="Create slide show"]'));
+
 		// Validate.
-		$this->zbxTestWaitUntilMessageTextPresent('msg-good', 'Slide show added');
+		$this->zbxTestTextPresent($data['name']);
+		$this->assertEquals(1, DBcount('SELECT NULL FROM slideshows WHERE name='.zbx_dbstr($data['name'])));
+		$this->zbxTestCheckFatalErrors();
 	}
 
 	/**
 	 * @dataProvider formData
 	 */
 	public function testFormSlideshow_Clone($data) {
-		// Log in.
-		$this->zbxTestLogin('slideconf.php');
-		//$this->zbxTestCheckHeader('Slide shows');
+		// Select slideshow to update.
+		$slideshow = DBfetch(DBSelect('SELECT slideshowid FROM slideshows', 1));
+		if ($slideshow) {
+			// Log in and navigate to new slideshow form.
+			$this->zbxTestLogin('slideconf.php?form=update&slideshowid='.$slideshow['slideshowid']);
 
-		// Click on the Properties link of the first item in the list.
-		$this->zbxTestClickLinkTextWait('Properties');
+			// Test page title.
+			$this->zbxTestWaitUntilElementVisible(WebDriverBy::tagName('h1'));
+			$this->zbxTestCheckHeader('Slide shows');
 
-		// Check the name of screenshow.
-		$this->zbxTestCheckHeader('Slide shows');
+			// Press clone.
+			$this->zbxTestClick('clone');
 
-		// Click on Clone button.
-		$this->zbxTestWaitUntilElementClickable(WebDriverBy::id('clone'));
-		$this->webDriver->findElement(WebDriverBy::id('clone'))->click();
+			// Change slide show name.
+			$name_field = WebDriverBy::xpath('//*[@id="name"]');
+			$new_name = sprintf('Clone of %s (%s)', $this->webDriver->findElement($name_field)->getAttribute('value'),
+							date("H:i"));
+			$this->webDriver->findElement($name_field)->clear();
+			$this->zbxTestInputTypeWait('name', $new_name);
 
-		// Change slide show name.
-		$name_field = WebDriverBy::xpath('//*[@id="name"]');
-		$new_name = sprintf('Clone of %s', $this->webDriver->findElement($name_field)->getAttribute('value'));
-		$this->webDriver->findElement($name_field)->clear();
-		$this->zbxTestInputTypeWait('name', $new_name);
+			/**
+			 * Click on submit button.
+			 * Button 'Add' can be reached using button[@value='Update']
+			 */
+			$this->zbxTestClickButton('Update');
 
-		// Click on submit button.
-		$submit_btn_selector = WebDriverBy::cssSelector('button[id="add"][type="submit"]');
-		$this->webDriver->findElement($submit_btn_selector)->click();
+			// Wait until page is loaded, just to be sure that creation of slideshow is completed on validation.
+			$this->zbxTestWaitUntilElementVisible(WebDriverBy::xpath('//*[@value="Create slide show"]'));
 
-		// Validate.
-		$this->zbxTestWaitUntilMessageTextPresent('msg-good', 'Slide show added');
+			// Test results.
+			$this->zbxTestWaitUntilMessageTextPresent('msg-good', 'Slide show added');
+			$this->assertEquals(1, DBCount('SELECT null FROM slideshows WHERE name='.zbx_dbstr($data['name'])));
+		}
+
+		$this->zbxTestCheckFatalErrors();
 	}
 
 	/**
@@ -106,42 +123,136 @@ class testFormSlideshow extends CWebTest {
 		$data['name'] = $data['name'] . ' for cancel';
 
 		// Click on 'Create slide show' button.
-		$this->zbxTestClick('form');
+		$this->zbxTestClickButton('Create slide show');
 
 		// Test page title.
 		$this->zbxTestCheckTitle('Configuration of slide shows');
 		$this->zbxTestCheckHeader('Slide shows');
 
-		// Fill out the form.
+		// Do some changes in form.
 		$this->zbxTestInputTypeWait('name', $data['name']);
-		$this->webDriver->findElement(WebDriverBy::xpath('//*[@id="delay"]'))->clear();
 
 		// Change your mind and cancel form creation.
 		$this->zbxTestClick('cancel');
 
 		// Test if slideshow is there.
-		$this->zbxTestTextNotPresent($data['name']);
+		$this->assertEquals(0, DBcount('SELECT NULL FROM slideshows WHERE name='.zbx_dbstr($data['name'])));
+		$this->zbxTestCheckFatalErrors();
 	}
 
 	/**
 	 * @dataProvider formData
 	 */
-	public function testFormSlideshow_Delete($data) {
+	public function testFormSlideshow_ValidateOnCreate($data) {
+		// Log in and navigate to new slideshow form.
+		$this->zbxTestLogin('slideconf.php');
+		$this->zbxTestClickButton('Create slide show');
+
+		// Test page title.
+		$this->zbxTestWaitUntilElementVisible(WebDriverBy::tagName('h1'));
+		$this->zbxTestCheckHeader('Slide shows');
+
+		// Clear input fields.
+		$this->webDriver->findElement(WebDriverBy::xpath('//*[@id="name"]'))->clear();
+		$this->webDriver->findElement(WebDriverBy::xpath('//*[@id="delay"]'))->clear();
+
+		// Try to save changes.
+		$submit_btn_selector = WebDriverBy::cssSelector('button[id="add"][type="submit"]');
+		$this->webDriver->findElement($submit_btn_selector)->click();
+
+		// Validate.
+		$this->zbxTestWaitUntilMessageTextPresent('msg-bad', 'Incorrect value for field "Name": cannot be empty.');
+		$this->zbxTestWaitUntilMessageTextPresent('msg-bad', 'Field "Default delay" is not correct: a time unit is expected');
+		$this->zbxTestCheckFatalErrors();
+	}
+
+	/**
+	 * @dataProvider formData
+	 */
+	public function testFormSlideshow_ChangeSlideshowName($data) {
+		// Select slideshow to update.
+		$slideshow = DBfetch(DBSelect('SELECT slideshowid FROM slideshows', 1));
+		if ($slideshow) {
+			$data['name'] = 'Changed name of ' . $data['name'];
+
+			// Log in and navigate to new slideshow form.
+			$this->zbxTestLogin('slideconf.php?form=update&slideshowid='.$slideshow['slideshowid']);
+
+			// Test page title.
+			$this->zbxTestWaitUntilElementVisible(WebDriverBy::tagName('h1'));
+			$this->zbxTestCheckHeader('Slide shows');
+
+			// Set new slideshow name.
+			$this->webDriver->findElement(WebDriverBy::xpath('//*[@id="name"]'))->clear();
+			$this->zbxTestInputTypeWait('name', $data['name']);
+			$this->zbxTestClickButton('Update');
+
+			// Test results.
+			$this->zbxTestWaitUntilMessageTextPresent('msg-good', 'Slide show updated');
+			$new_slideshowid = DBfetch(DBSelect('SELECT slideshowid FROM slideshows WHERE name='.zbx_dbstr($data['name'])));
+			$this->assertEquals($slideshow['slideshowid'], $new_slideshowid['slideshowid']);
+		}
+
+		$this->zbxTestCheckFatalErrors();
+	}
+
+	/**
+	 * @dataProvider formData
+	 */
+	public function testFormSlideshow_DeleteFromForm($data) {
+		// Select slideshow to update.
+		$slideshow = DBfetch(DBSelect('SELECT slideshowid FROM slideshows', 1));
+		if ($slideshow) {
+			// Log in and navigate to new slideshow form.
+			$this->zbxTestLogin('slideconf.php?form=update&slideshowid='.$slideshow['slideshowid']);
+
+			// Test page title.
+			$this->zbxTestWaitUntilElementVisible(WebDriverBy::tagName('h1'));
+			$this->zbxTestCheckHeader('Slide shows');
+
+			// Click on delete button.
+			$delete_btn_selector = WebDriverBy::cssSelector('button[value="Delete"]');
+			$this->zbxTestWaitUntilElementClickable($delete_btn_selector);
+			$this->webDriver->findElement($delete_btn_selector)->click();
+
+			// Confirm deletion.
+			try {
+				$this->webDriver->wait(10)->until(WebDriverExpectedCondition::alertIsPresent());
+				$this->webDriver->switchTo()->alert()->accept();
+			}
+			catch (TimeoutException $ex) {
+				$this->zbxTestWaitUntilElementClickable($delete_btn_selector);
+				$this->webDriver->findElement($delete_btn_selector)->click();
+				$this->webDriver->switchTo()->alert()->accept();
+			}
+
+			// Validate.
+			$this->zbxTestWaitUntilMessageTextPresent('msg-good', 'Slide show deleted');
+			$this->assertEquals(0, DBCount('SELECT null FROM slideshows WHERE slideshowid='.$slideshow['slideshowid']));
+		}
+
+		$this->zbxTestCheckFatalErrors();
+	}
+
+	/**
+	 * @dataProvider formData
+	 */
+	public function testFormSlideshow_DeleteFromList($data) {
 		// Log in.
 		$this->zbxTestLogin('slideconf.php');
 
 		// Test page title.
-		$this->zbxTestCheckTitle('Configuration of slide shows');
 		$this->zbxTestCheckHeader('Slide shows');
 
-		// Click on the Properties link of the first item in the list.
-		$this->zbxTestCheckboxSelect('all_shows');
+		// Select all vsible checkboxes (since checkboxes are not visible, span elements should be clicked).
+		$this->webDriver->findElement(WebDriverBy::className('list-table'))->findElement(WebDriverBy::tagName('span'))->click();
 
 		// Click on submit button.
 		$delete_btn_selector = WebDriverBy::cssSelector('button[value="slideshow.massdelete"][type="submit"]');
 		$this->zbxTestWaitUntilElementClickable($delete_btn_selector);
 		$this->webDriver->findElement($delete_btn_selector)->click();
 
+		// Confirm deletion.
 		try {
 			$this->webDriver->wait(10)->until(WebDriverExpectedCondition::alertIsPresent());
 			$this->webDriver->switchTo()->alert()->accept();
@@ -154,5 +265,6 @@ class testFormSlideshow extends CWebTest {
 
 		// Validate.
 		$this->zbxTestWaitUntilMessageTextPresent('msg-good', 'Slide show deleted');
+		$this->zbxTestCheckFatalErrors();
 	}
 }
