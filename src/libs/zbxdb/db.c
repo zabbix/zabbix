69cda943fd9909861fec852d95de9db5a5e53b3d
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2018-04-24 15:28:35 +0000

    .......PS. [ZBX-12451] improved my_bool removal detection

--- src/libs/zbxdb/db.c
+++ src/libs/zbxdb/db.c
@@ -372,10 +372,10 @@ int	zbx_db_connect(char *host, char *user, char *password, char *dbname, char *d
 #if defined(HAVE_IBM_DB2)
 	char		*connect = NULL;
 #elif defined(HAVE_MYSQL)
-#ifdef my_bool	/* my_bool type is removed in MySQL 8.0 */
-	my_bool		mysql_reconnect = 1;
-#else
+#if LIBMYSQL_VERSION_ID >= 80000	/* my_bool type is removed in MySQL 8.0 */
 	bool		mysql_reconnect = 1;
+#else
+	my_bool		mysql_reconnect = 1;
 #endif
 #elif defined(HAVE_ORACLE)
 	char		*connect = NULL;
