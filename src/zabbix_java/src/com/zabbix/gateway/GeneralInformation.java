46075008a922634c11c4fb6673dee64b5a294db6
natalja <natalja@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2018-04-27 07:43:46 +0000

    .......... [ZBXNEXT-826] updated release date [4.0.0alpha6]
    git-svn-id: svn://svn.zabbix.com/trunk@80279 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,7 +22,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "26 March 2018";
+	static final String REVISION_DATE = "27 April 2018";
 	static final String REVISION = "{ZABBIX_REVISION}";
 	static final String VERSION = "4.0.0alpha6";
 
