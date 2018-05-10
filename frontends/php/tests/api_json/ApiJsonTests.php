553163426934c78c53332252446dda8076429edd
akucenko <akucenko@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2018-05-10 07:08:50 +0000

    .......... [DEV-823] added new Selenium tests for action.delete
    git-svn-id: svn://svn.zabbix.com/trunk@80647 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/tests/api_json/ApiJsonTests.php
+++ frontends/php/tests/api_json/ApiJsonTests.php
@@ -22,6 +22,7 @@
 require_once dirname(__FILE__).'/CHost.php';
 require_once dirname(__FILE__).'/CItem.php';
 require_once dirname(__FILE__).'/testAPIInfo.php';
+require_once dirname(__FILE__).'/testAction.php';
 require_once dirname(__FILE__).'/testApplication.php';
 require_once dirname(__FILE__).'/testConfiguration.php';
 require_once dirname(__FILE__).'/testHostGroup.php';
@@ -43,6 +44,7 @@ class ApiJsonTests {
 //		$suite->addTestSuite('API_JSON_Host');
 //		$suite->addTestSuite('API_JSON_Item');
 		$suite->addTestSuite('testAPIInfo');
+		$suite->addTestSuite('testAction');
 		$suite->addTestSuite('testApplication');
 		$suite->addTestSuite('testConfiguration');
 		$suite->addTestSuite('testHostGroup');
