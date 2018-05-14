0c1e79866aacd9f4dc29e2e6a71df030ab1a319e
Michael Veksler <Mihails.Vekslers@zabbix.com>
2018-05-14 07:22:28 +0000

    ........S. [ZBXNEXT-4163] fixed a flags numeration

--- include/common.h
+++ include/common.h
@@ -1316,8 +1316,8 @@ int	zbx_strcmp_natural(const char *s1, const char *s2);
 #define ZBX_TOKEN_NUMERIC	0x08000
 #define ZBX_TOKEN_JSON		0x10000
 #define ZBX_TOKEN_XML		0x20000
-#define ZBX_TOKEN_REGEXP	0x30000
-#define ZBX_TOKEN_XPATH		0x40000
+#define ZBX_TOKEN_REGEXP	0x40000
+#define ZBX_TOKEN_XPATH		0x80000
 
 /* location of a substring */
 typedef struct
