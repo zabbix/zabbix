46a8d5a9592ee977019b8970a9eae37c36f1e712
Valdis Murzins <valdis.murzins@zabbix.com>
2018-05-23 08:20:10 +0000

    ..F....... [ZBXNEXT-4447] renamed event source details to trigger details in event details page

--- frontends/php/tr_events.php
+++ frontends/php/tr_events.php
@@ -126,7 +126,7 @@ $eventTab = (new CTable())
 	->addRow([
 		new CDiv([
 			(new CUiWidget(WIDGET_HAT_TRIGGERDETAILS, make_trigger_details($trigger)))
-				->setHeader(_('Event source details')),
+				->setHeader(_('Trigger details')),
 			(new CUiWidget(WIDGET_HAT_EVENTDETAILS,
 				make_event_details($event,
 					$page['file'].'?triggerid='.getRequest('triggerid').'&eventid='.getRequest('eventid')
