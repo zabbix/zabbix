291491a1c8fe010498382117e7b89260b0f9eef2
Gregory Chalenko <Gregory.Chalenko@zabbix.com>
2018-06-04 06:43:38 +0000

    ..F....... [ZBXNEXT-4361] fixed firefox returns position as float value

--- frontends/php/js/gtlc.js
+++ frontends/php/js/gtlc.js
@@ -404,8 +404,9 @@ jQuery(function ($){
 	 * @param {object} e    jQuery event object.
 	 */
 	function selectionHandlerDragEnd(e) {
-		var from_offset = (selection.dom.position().left - selection.min) * selection.seconds_per_px,
-			to_offset = (selection.max - selection.dom.width() - selection.dom.position().left) * selection.seconds_per_px;
+		var left = Math.round(selection.dom.position().left),
+			from_offset = (left - selection.min) * selection.seconds_per_px,
+			to_offset = (selection.max - selection.dom.width() - left) * selection.seconds_per_px;
 
 		selection.dom.remove();
 		selection = null;
