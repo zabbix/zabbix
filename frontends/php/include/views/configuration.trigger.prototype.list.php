66ab9e626efc75821d2b9fb7c2a9631ada9f9c67
miks <miks@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2018-06-22 14:38:20 +0000

    ..F....... [ZBX-13779] fixed redirect button link in multiple places
    git-svn-id: svn://svn.zabbix.com/trunk@82132 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/include/views/configuration.trigger.prototype.list.php
+++ frontends/php/include/views/configuration.trigger.prototype.list.php
@@ -24,7 +24,7 @@ $widget = (new CWidget())
 	->setControls(
 		(new CTag('nav', true,
 			(new CList())->addItem(new CRedirectButton(_('Create trigger prototype'),
-				(new CUrl())
+				(new CUrl('trigger_prototypes.php'))
 					->setArgument('parent_discoveryid', $data['parent_discoveryid'])
 					->setArgument('form', 'create')
 			))
