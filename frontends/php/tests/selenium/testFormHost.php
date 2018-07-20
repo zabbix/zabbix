739fc0f9baeaa683603202acd5f6238a4188235f
akucenko <akucenko@97f52cf1-0a1b-0410-bd0e-c28be96e8082>
2018-07-20 09:53:16 +0000

    .......... [DEV-850] added dropdown selection for template group
    git-svn-id: svn://svn.zabbix.com/trunk@82876 97f52cf1-0a1b-0410-bd0e-c28be96e8082

--- frontends/php/tests/selenium/testFormHost.php
+++ frontends/php/tests/selenium/testFormHost.php
@@ -263,6 +263,7 @@ class testFormHost extends CWebTest {
 		$this->zbxTestTabSwitch('Templates');
 		$this->zbxTestClickButtonMultiselect('add_templates_');
 		$this->zbxTestLaunchOverlayDialog('Templates');
+		$this->zbxTestDropdownSelect('groupid', 'Templates');
 		$this->zbxTestClickLinkTextWait('Template OS Linux');
 		$this->zbxTestClickXpathWait("//div[@id='templateTab']//button[contains(@onclick,'add_template')]");
 
@@ -324,6 +325,7 @@ class testFormHost extends CWebTest {
 		$this->zbxTestTabSwitch('Templates');
 		$this->zbxTestClickButtonMultiselect('add_templates_');
 		$this->zbxTestLaunchOverlayDialog('Templates');
+		$this->zbxTestDropdownSelect('groupid', 'Templates');
 		$this->zbxTestClickLinkTextWait('Template OS Linux');
 		$this->zbxTestClickXpathWait("//div[@id='templateTab']//button[contains(@onclick,'add_template')]");
 
