6c9fd514131c51622d63042cf4e15c934dd75603
Andris Zeila <andris.zeila@zabbix.com>
2018-07-31 11:10:01 +0000

    ........S. [ZBXNEXT-413] changed maintenance update statistic value types to int, removed unused variable

--- src/libs/zbxdbcache/dbconfig.h
+++ src/libs/zbxdbcache/dbconfig.h
@@ -694,9 +694,8 @@ typedef struct
 	zbx_uint64_t		maintenance_revision;		/* global maintenance revision incremented by */
 								/* any changes to cached maintenance data     */
 	zbx_uint64_t		maintenance_update_revision;	/* last maintenance update revision */
-	zbx_uint64_t		maintenance_modified_num;	/* number of modified maintenances since last update */
-	zbx_uint64_t		maintenance_deleted_num;	/* number of deleted maintenances since last update */
-	int			maintenance_stopped_num;	/* number of stopped maintenances since last update */
+	int			maintenance_modified_num;	/* number of modified maintenances since last update */
+	int			maintenance_deleted_num;	/* number of deleted maintenances since last update */
 	int			maintenance_event_updates_num;	/* number of event updates done since last */
 								/* maintenance update                      */
 
