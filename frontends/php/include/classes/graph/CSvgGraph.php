846b2efb0876625b2a7f290fa548cdc1009ad978
Miks Kronkalns <miks.kronkalns@zabbix.com>
2018-08-28 13:17:59 +0000

    ..F....... [ZBXNEXT-4588] improved fix for division by zero error

--- frontends/php/include/classes/graph/CSvgGraph.php
+++ frontends/php/include/classes/graph/CSvgGraph.php
@@ -534,7 +534,7 @@ class CSvgGraph extends CSvg {
 			$units = $this->right_y_units;
 		}
 
-		$grid = $this->getValueGrid($min_value, $max_value);
+		$grid = $this->getValueGrid((int) $min_value, (int) $max_value);
 		$delta = (($max_value - $min_value) ? : 1);
 		$grid_values = [];
 
@@ -596,7 +596,7 @@ class CSvgGraph extends CSvg {
 		$res = [];
 
 		for ($base = 10; $base > .01; $base /= 10) {
-			$mul = ($max > 0) ? 1 / pow($base, floor(log10($max))) : 1;
+			$mul = $max ? 1 / pow($base, floor(log10($max))) : 1;
 			$max10 = ceil($mul * $max) / $mul;
 			$min10 = floor($mul * $min) / $mul;
 			$delta = $max10 - $min10;
