99a8fee24573235cdc95113eb9f7553f3649d956
Aleksandrs Subins <aleksandrs.subins@zabbix.com>
2018-08-31 09:13:28 +0000

    ..F....... [ZBXNEXT-4606] fixed PHPDocs in the CView class

--- frontends/php/include/classes/mvc/CView.php
+++ frontends/php/include/classes/mvc/CView.php
@@ -22,56 +22,82 @@
 class CView {
 
 	/**
-	 * @var string - name of the template file without extension, for example 'configuration.item.edit'
+	 * Name of the template file without extension, for example 'configuration.item.edit'.
+	 *
+	 * @var string
 	 */
 	private $filePath;
 
 	/**
-	 * @var array - hash of 'variable_name'=>'variable_value' to be used inside template
+	 * Hash of 'variable_name'=>'variable_value' to be used inside template.
+	 *
+	 * @var array
 	 */
 	private $data;
 
 	/**
-	 * @var CTag - actual template object being shown
+	 * Actual template object being shown.
+	 *
+	 * @var CTag
 	 */
 	private $template;
 
 	/**
-	 * @var string - scripts on page
+	 * Scripts on page.
+	 *
+	 * @var string
+	 *
 	 * @deprecated
 	 */
 	private $scripts;
 
 	/**
-	 * @var array - Java code for inclusions on page
+	 * Java code for inclusions on page.
+	 *
+	 * @var array
 	 */
 	private $jsIncludePost = [];
 
 	/**
-	 * @var array - Java Script files for inclusions on page, pre-processed by PHP
+	 * Java Script files for inclusions on page, pre-processed by PHP.
+	 *
+	 * @var array
 	 */
 	private $jsIncludeFiles = [];
 
 	/**
-	 * @var array - Java Script files for inclusions on page, included as <script src="..."></script>
+	 * Java Script files for inclusions on page, included as <script src="..."></script>.
+	 *
+	 * @var array
 	 */
 	private $jsFiles = [];
 
 	/**
-	 * @array - directories where views are stored, ordered by priority
-	 * include/views should be removed once we fully move to MVC
+	 * Directories where views are stored, ordered by priority include/views should be removed once fully move to MVC.
+	 *
+	 * @static
+	 *
+	 * @var array
 	 */
 	static $viewsDir = ['local/app/views', 'app/views', 'include/views'];
 
 	/**
-	 * @var boolean - Web layout mode enabled flag.
+	 * Web layout mode enabled flag.
+	 *
+	 * @static
+	 *
+	 * @var boolean
 	 */
 	static $has_web_layout_mode = false;
 
 	/**
 	 * Creates a new view based on provided template file.
-	 * @param string $view name of a view, located under include/views
-	 * @param array $data deprecated parameter, use set() and get() methods for passing variables to views
+	 *
+	 * @param string $view  Name of a view, located under include/views.
+	 * @param array  $data  Deprecated parameter, use set() and get() methods for passing variables to views.
+	 *
+	 * @throws Exception if file does not exist.
+	 *
 	 * @example $scriptForm = new CView('administration.script.edit');
 	 */
 	public function __construct($view, $data = []) {
@@ -79,6 +105,13 @@ class CView {
 		$this->data = $data;
 	}
 
+	/**
+	 * Search file and assigns path to the view file.
+	 *
+	 * @param string $view  Name of the template file without extension.
+	 *
+	 * @throws Exception if invalid filename or file does not exist.
+	 */
 	public function assign($view) {
 		if (!preg_match("/[a-z\.]+/", $view)) {
 			throw new Exception(_s('Invalid view name given "%s". Allowed chars: "a-z" and ".".', $view));
@@ -100,8 +133,10 @@ class CView {
 
 	/**
 	 * Assign value to a named variable.
-	 * @param string $var variable name
-	 * @param any $value variable value
+	 *
+	 * @param string $var
+	 * @param mixed  $value
+	 *
 	 * @example set('hostName','Host ABC')
 	 */
 	public function set($var, $value) {
@@ -110,9 +145,13 @@ class CView {
 
 	/**
 	 * Get value by variable name.
-	 * @param string $var name of the variable.
-	 * @return string variable value. Returns empty string if the variable is not defined.
+	 *
+	 * @param string $var
+	 *
+	 * @return mixed|string  Variable value. Returns empty string if the variable is not defined.
+	 *
 	 * @example get('hostName')
+	 *
 	 * @deprecated use $data instead
 	 */
 	public function get($var) {
@@ -121,9 +160,13 @@ class CView {
 
 	/**
 	 * Get variable of type array by variable name.
-	 * @param string $var name of the variable.
-	 * @return array variable value. Returns empty array if the variable is not defined or not an array.
+	 *
+	 * @param string $var
+	 *
+	 * @return array  Variable value. Returns empty array if the variable is not defined or not an array.
+	 *
 	 * @example getArray('hosts')
+	 *
 	 * @deprecated use $data instead
 	 */
 	public function getArray($var) {
@@ -133,8 +176,12 @@ class CView {
 	/**
 	 * Load and execute view.
 	 * TODO It outputs JavaScript code immediately, should be done in show() or processed separately.
-	 * @deprecated will not be supported when we fully move to MVC
-	 * @return object GUI object.
+	 *
+	 * @deprecated  Will not be supported when we fully move to MVC.
+	 *
+	 * @throws Exception if cannot include view file.
+	 *
+	 * @return object  GUI object.
 	 */
 	public function render() {
 		// $data this variable will be used in included file
@@ -152,10 +199,11 @@ class CView {
 	}
 
 	/**
-	 * The method outputs HTML code based on rendered template.
-	 * It calls render() if not called already.
-	 * @deprecated will not be supported when we fully move to MVC
-	 * @return NULL
+	 * The method outputs HTML code based on rendered template. It calls render() if not called already.
+	 *
+	 * @deprecated  Will not be supported when we fully move to MVC.
+	 *
+	 * @throws Exception if view is not rendered.
 	 */
 	public function show() {
 		if (!isset($this->template)) {
@@ -167,7 +215,6 @@ class CView {
 	/**
 	* The method returns HTML/JSON/CVS/etc text based on rendered template.
 	* show() and render() should be made deprecated. View should only output text, no objects, nothing.
-	* @return NULL
 	*/
 	public function getOutput() {
 		$data = $this->data;
@@ -177,32 +224,36 @@ class CView {
 	}
 
 	/**
-	 * Include Java Script code to be executed after page load
-	 * @param string $js Java Script code
+	 * Include Java Script code to be executed after page load.
+	 *
+	 * @param string $js  Java Script code.
 	 */
 	public function addPostJS($js) {
 		$this->jsIncludePost[] = $js;
 	}
 
 	/**
-	 * Include Java Script file required for the view into HTML
-	 * @param string $filename name of java Script file, will be pre-processed by PHP
+	 * Include Java Script file required for the view into HTML.
+	 *
+	 * @param string $filename  Name of java Script file, will be pre-processed by PHP.
 	 */
 	public function includeJSfile($filename) {
 		$this->jsIncludeFiles[] = $filename;
 	}
 
 	/**
-	 * Add Java Script file required for the view as <script src="..."></script>
-	 * @param string $filename name of java Script file
+	 * Add Java Script file required for the view as <script src="..."></script>.
+	 *
+	 * @param string $filename  Name of java Script file.
 	 */
 	public function addJsFile($filename) {
 		$this->jsFiles[] = $filename;
 	}
 
 	/**
-	 * Get content of all Java Script code
-	 * @return string Java Script code
+	 * Get content of all Java Script code.
+	 *
+	 * @return string  Java Script code.
 	 */
 	public function getPostJS() {
 		if ($this->jsIncludePost) {
@@ -213,8 +264,11 @@ class CView {
 	}
 
 	/**
-	 * Get content of all included Java Script files
-	 * @return string empty string or content of included JS files
+	 * Get content of all included Java Script files.
+	 *
+	 * @throws Exception if cannot include JS file.
+	 *
+	 * @return string  Empty string or content of included JS files.
 	 */
 	public function getIncludedJS() {
 		ob_start();
@@ -227,8 +281,9 @@ class CView {
 	}
 
 	/**
-	 * Get content of all included Java Script files
-	 * @return string empty string or array of path of included JS files
+	 * Get content of all included Java Script files.
+	 *
+	 * @return array|string  Empty string or array of path of included JS files.
 	 */
 	public function getAddedJS() {
 		return $this->jsFiles;
