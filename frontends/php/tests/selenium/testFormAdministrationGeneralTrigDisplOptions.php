0cf41037309a84c5546d40e69132310188173263
Ivo Kurzemnieks <ivo.kurzemnieks@zabbix.com>
2018-09-10 08:05:42 +0000

    ..F....... [ZBX-14789] changed US "Color" to UK "Colour"

--- frontends/php/tests/selenium/testFormAdministrationGeneralTrigDisplOptions.php
+++ frontends/php/tests/selenium/testFormAdministrationGeneralTrigDisplOptions.php
@@ -39,7 +39,7 @@ class testFormAdministrationGeneralTrigDisplOptions extends CWebTest {
 		$this->zbxTestTextPresent(
 			[
 				'Trigger displaying options',
-				'Use custom event status colors',
+				'Use custom event status colours',
 				'blinking',
 				'Unacknowledged PROBLEM events',
 				'Acknowledged PROBLEM events',
