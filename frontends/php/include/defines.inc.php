66f945efbfb2bddf26754b59482a3ec21a25f545
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2018-10-01 12:09:33 +0000

    .......... [ZBXNEXT-826] updated release version [4.0.1rc1]

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,8 +19,8 @@
 **/
 
 
-define('ZABBIX_VERSION',		'4.0.0');
-define('ZABBIX_API_VERSION',	'4.0.0');
+define('ZABBIX_VERSION',		'4.0.1rc1');
+define('ZABBIX_API_VERSION',	'4.0.1');
 define('ZABBIX_EXPORT_VERSION',	'4.0');
 define('ZABBIX_DB_VERSION',		4000000);
 
