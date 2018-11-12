02613b92a44b279e352d12a1a92a61994a234e43
Miks Kronkalns <miks.kronkalns@zabbix.com>
2018-11-12 12:30:35 +0000

    ..F....... [ZBX-8396] improved source code comments

--- frontends/php/include/graphs.inc.php
+++ frontends/php/include/graphs.inc.php
@@ -106,7 +106,7 @@ function getGraphDims($graphid = null) {
 		return $graphDims;
 	}
 
-	// zoom featers
+	// Select graph's type and height as well as which Y axes are used by graph items.
 	$dbGraphs = DBselect(
 		'SELECT MAX(g.graphtype) AS graphtype,MIN(gi.yaxisside) AS yaxissidel,MAX(gi.yaxisside) AS yaxissider,MAX(g.height) AS height'.
 		' FROM graphs g,graphs_items gi'.
