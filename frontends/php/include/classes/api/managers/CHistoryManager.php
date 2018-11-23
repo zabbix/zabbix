183e4492b092944dcbe79b22ceafb0643bf002f3
mylinuxsa <tony_wangzt@163.com>
2018-11-23 11:27:46 +0800

    Update CHistoryManager.php

--- frontends/php/include/classes/api/managers/CHistoryManager.php
+++ frontends/php/include/classes/api/managers/CHistoryManager.php
@@ -672,7 +672,11 @@ class CHistoryManager {
 
 			$urls[$value_name] = $url;
 		}
-
+		$username = $HISTORY['username'];
+        	$password = $HISTORY['password'];
+        	$pattern = '/(https?:\/\/)(.+?)/';
+	
+        	$urls[$value_name] = preg_replace($pattern,"$1$username:$password@$2",$urls[$value_name]);
 		return $urls[$value_name];
 	}
 
