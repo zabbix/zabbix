0bd0bfc3c8c08e285f1d9fec15fdc297f56a9e08
Vasily Goncharenko <vasilijs.goncarenko@zabbix.com>
2018-12-03 15:43:34 +0000

    ..F....... [ZBXNEXT-4883] fixed when trigger in problem state hides all dependent triggers in history view

--- frontends/php/include/classes/screens/CScreenProblem.php
+++ frontends/php/include/classes/screens/CScreenProblem.php
@@ -329,7 +329,7 @@ class CScreenProblem extends CScreenBase {
 						'selectItems' => ['itemid', 'hostid', 'name', 'key_', 'value_type'],
 						'triggerids' => array_keys($triggerids),
 						'monitored' => true,
-						'skipDependent' => true,
+						'skipDependent' => $filter['show'] == TRIGGERS_OPTION_ALL ? null : true,
 						'preservekeys' => true
 					];
 
