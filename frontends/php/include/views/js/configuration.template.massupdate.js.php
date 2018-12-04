d0ca0abaceada28b743a2c996413d691ebde4a1d
Andrejs Griscenko <Andrejs.Griscenko@zabbix.com>
2018-12-04 13:18:58 +0000

    ..F....... [ZBXNEXT-4731] fixd possibility to select new host groups using multiselect on template host group mass remove"

--- frontends/php/include/views/js/configuration.template.massupdate.js.php
+++ frontends/php/include/views/js/configuration.template.massupdate.js.php
@@ -4,6 +4,15 @@
 
 <script type="text/javascript">
 	jQuery(function($) {
+		<?php if (CWebUser::getType() == USER_TYPE_SUPER_ADMIN): ?>
+			$('input[name=mass_update_groups]').on('change', function() {
+				$('#groups_').multiSelect('setOption', 'addNew',
+					(this.value == '<?php echo ZBX_MASSUPDATE_ACTION_ADD ?>'
+						|| this.value == '<?php echo ZBX_MASSUPDATE_ACTION_REPLACE ?>')
+				);
+			});
+		<?php endif ?>
+
 		$('#tbl-tags').dynamicRows({
 			template: '#tag-row'
 		});
