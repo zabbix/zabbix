23795e2a2d21d73b314f28dd22ff08c79478a013
Andris Zeila <andris.zeila@zabbix.com>
2018-12-07 15:17:13 +0000

    .......PS. [ZBXNEXT-4087] moved server/proxy specific internal checks to separate libraries

--- src/zabbix_server/poller/checks_internal.h
+++ src/zabbix_server/poller/checks_internal.h
@@ -28,4 +28,6 @@ extern int	CONFIG_SERVER_STARTUP_TIME;
 
 int	get_value_internal(DC_ITEM *item, AGENT_RESULT *result);
 
+int	zbx_get_value_internal_ext(const char *query, const AGENT_REQUEST *request, AGENT_RESULT *result);
+
 #endif
