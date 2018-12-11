589691ff42d6b41aad576e5ba4b19b73423cff31
Andris Mednis <Andris.Mednis@zabbix.com>
2018-12-11 13:59:03 +0000

    ........S. [ZBX-15296] added comment

--- src/libs/zbxdbcache/dbconfig.h
+++ src/libs/zbxdbcache/dbconfig.h
@@ -654,7 +654,7 @@ typedef struct
 {
 	zbx_uint64_t	maintenancetagid;
 	zbx_uint64_t	maintenanceid;
-	unsigned char	op;
+	unsigned char	op;		/* condition operator */
 	const char	*tag;
 	const char	*value;
 }
