afec63d15e92aab9b05df06570996d53c169b2f5
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2018-12-11 09:33:11 +0000

    ........S. [ZBXNEXT-4877] changed vfs.fs.discovery to return array

--- src/libs/zbxsysinfo/linux/diskspace.c
+++ src/libs/zbxsysinfo/linux/diskspace.c
@@ -138,9 +138,7 @@ int	VFS_FS_DISCOVERY(AGENT_REQUEST *request, AGENT_RESULT *result)
 		return SYSINFO_RET_FAIL;
 	}
 
-	zbx_json_init(&j, ZBX_JSON_STAT_BUF_LEN);
-
-	zbx_json_addarray(&j, ZBX_PROTO_TAG_DATA);
+	zbx_json_initarray(&j, ZBX_JSON_STAT_BUF_LEN);
 
 	while (NULL != fgets(line, sizeof(line), f))
 	{
