d807ba97795a35de2fb87f59fb8cddad7d0e79ea
Natalja Karpinska <natalja.karpinska@zabbix.com>
2018-12-12 09:40:24 +0000

    .......... [ZBXNEXT-826] updated release date [4.0.3rc1]

--- include/version.h
+++ include/version.h
@@ -24,7 +24,7 @@
 #define ZBX_STR(str)	ZBX_STR2(str)
 
 #define APPLICATION_NAME	"Zabbix Agent"
-#define ZABBIX_REVDATE		"26 November 2018"
+#define ZABBIX_REVDATE		"12 December 2018"
 #define ZABBIX_VERSION_MAJOR	4
 #define ZABBIX_VERSION_MINOR	0
 #define ZABBIX_VERSION_PATCH	3
