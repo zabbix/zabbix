84f087fb99da56cbd0283a702928244cc37f87b2
Michael Veksler <Mihails.Vekslers@zabbix.com>
2018-12-13 12:44:15 +0000

    ....I..... [ZBX-15211] implemantation of 'bootstrap.sh tests'

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,13 @@
 
 # Run this script to generate 'configure'
 
+if [ "xtests" = "x$1" ]; then
+	cp -f ./tests/conf_tests.m4 ./m4/ || exit $?
+fi
+
 aclocal -I m4
 autoconf
 autoheader
 automake -a
 automake
+rm -f ./m4/conf_tests.m4
