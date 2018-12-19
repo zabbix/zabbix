54c7d85e705ff967cc994bdc7e534f4e4add30a9
Vasily Goncharenko <vasilijs.goncarenko@zabbix.com>
2018-12-19 12:13:40 +0000

    ..F....... [ZBXNEXT-4792] fixed that ignored next line in Latest values

--- frontends/php/include/classes/screens/CScreenProblem.php
+++ frontends/php/include/classes/screens/CScreenProblem.php
@@ -1192,7 +1192,9 @@ class CScreenProblem extends CScreenBase {
 		foreach ($trigger_items as $itemid => $item) {
 			if (array_key_exists($itemid, $history_values)) {
 				$last_value = reset($history_values[$itemid]);
-				$last_value['value'] = formatHistoryValue($last_value['value'], $item, true);
+				$last_value['value'] = formatHistoryValue(str_replace(["\r\n", "\n"], [" "], $last_value['value']),
+					$item, true
+				);
 			}
 			else {
 				$last_value = [
