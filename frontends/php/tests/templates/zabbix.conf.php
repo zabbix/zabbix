25678e65ba2b32ab23c76dc8e769d9f985e5de96
Vjaceslavs Bogdanovs <vjaceslavs.bogdanovs@zabbix.com>
2018-12-20 13:37:52 +0000

    ..F....... [DEV-935] added custom runtime error handler for phpunit tests

--- frontends/php/tests/templates/zabbix.conf.php
+++ frontends/php/tests/templates/zabbix.conf.php
@@ -16,4 +16,25 @@ $ZBX_SERVER_PORT		= '10051';
 $ZBX_SERVER_NAME		= 'TEST_SERVER_NAME';
 
 $IMAGE_FORMAT_DEFAULT	= IMAGE_FORMAT_PNG;
+
+// Runtime error collection block.
+if (!file_exists(PHPUNIT_ERROR_LOG)) {
+	file_put_contents(PHPUNIT_ERROR_LOG, '');
+	chmod(PHPUNIT_ERROR_LOG, 0666);
+}
+
+if (!defined('PHPUNIT_BASEDIR')) {
+	set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+		// Check if error control operator was used.
+		if (error_reporting() & $errno) {
+			file_put_contents(PHPUNIT_ERROR_LOG, $errfile.' ('.$errline.'): '.$errstr."\n", FILE_APPEND);
+		}
+
+		return false;
+	}, E_ALL | E_STRICT);
+
+	set_exception_handler(function ($exception) {
+		file_put_contents(PHPUNIT_ERROR_LOG, $exception."\n", FILE_APPEND);
+	});
+}
 ?>
