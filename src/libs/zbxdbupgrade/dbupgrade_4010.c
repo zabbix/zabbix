f6d4f2d6610395328e2d3ad80dd982fc3e410308
Gregory Chalenko <Gregory.Chalenko@zabbix.com>
2019-01-18 08:43:07 +0000

    ....I...S. [ZBXNEXT-4938] fixed wrong added index on dchecks table

--- src/libs/zbxdbupgrade/dbupgrade_4010.c
+++ src/libs/zbxdbupgrade/dbupgrade_4010.c
@@ -107,10 +107,22 @@ static int	DBpatch_4010009(void)
 
 static int	DBpatch_4010010(void)
 {
-	return DBcreate_index("dchecks", "dchecks_2", "druleid,host_source,name_source", 0);
+	return DBdrop_foreign_key("dchecks", 1);
 }
 
 static int	DBpatch_4010011(void)
+{
+	return DBdrop_index("dchecks", "dchecks_1");
+}
+
+static int	DBpatch_4010012(void)
+{
+	const ZBX_FIELD	field = {"druleid", NULL, "drules", "druleid", 0, 0, 0, ZBX_FK_CASCADE_DELETE};
+
+	return DBadd_foreign_key("dchecks", 1, &field);
+}
+
+static int	DBpatch_4010013(void)
 {
 	return DBcreate_index("proxy_dhistory", "proxy_dhistory_2", "druleid", 0);
 }
@@ -132,5 +144,7 @@ DBPATCH_ADD(4010008, 0, 1)
 DBPATCH_ADD(4010009, 0, 1)
 DBPATCH_ADD(4010010, 0, 1)
 DBPATCH_ADD(4010011, 0, 1)
+DBPATCH_ADD(4010012, 0, 1)
+DBPATCH_ADD(4010013, 0, 1)
 
 DBPATCH_END()
