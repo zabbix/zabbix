0979d7840762ee15e333852fad15296a144b009f
Jurijs Klopovskis <jurijs.klopovskis@zabbix.com>
2019-01-28 13:50:07 +0000

    .......... [ZBXNEXT-826] updated release date [4.0.4rc2]

--- include/version.h
+++ include/version.h
@@ -24,7 +24,7 @@
 #define ZBX_STR(str)	ZBX_STR2(str)
 
 #define APPLICATION_NAME	"Zabbix Agent"
-#define ZABBIX_REVDATE		"22 January 2019"
+#define ZABBIX_REVDATE		"28 January 2019"
 #define ZABBIX_VERSION_MAJOR	4
 #define ZABBIX_VERSION_MINOR	0
 #define ZABBIX_VERSION_PATCH	4
--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,7 +22,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "22 January 2019";
+	static final String REVISION_DATE = "28 January 2019";
 	static final String REVISION = "{ZABBIX_REVISION}";
 	static final String VERSION = "4.0.4rc2";
 
