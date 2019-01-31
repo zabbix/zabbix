a92b77e759216e815afbf3830e84158f0d0051f9
Miks Kronkalns <miks.kronkalns@zabbix.com>
2019-01-31 08:00:24 +0000

    ..F....... [ZBX-15539] fixed validation of  master item in item update form

--- frontends/php/items.php
+++ frontends/php/items.php
@@ -1345,7 +1345,8 @@ if (isset($_REQUEST['form']) && str_in_array($_REQUEST['form'], ['create', 'upda
 			$item['jmx_endpoint'] = ZBX_DEFAULT_JMX_ENDPOINT;
 		}
 
-		if (getRequest('type', $item['type']) == ITEM_TYPE_DEPENDENT) {
+		if (getRequest('type', $item['type']) == ITEM_TYPE_DEPENDENT
+				&& getRequest('master_itemid', $item['master_itemid']) > 0) {
 			$master_item_options = [
 				'output' => ['itemid', 'type', 'hostid', 'name', 'key_'],
 				'itemids' => getRequest('master_itemid', $item['master_itemid']),
