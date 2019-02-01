cc2f81f261c81666a76558067ed636732942b611
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2019-02-01 12:14:49 +0000

    ........S. [ZBXNEXT-4944] use already known length instead of strlen

--- src/libs/zbxembed/embed.c
+++ src/libs/zbxembed/embed.c
@@ -354,7 +354,7 @@ int	zbx_es_compile(zbx_es_t *es, const char *script, char **code, int *size, cha
 	*ptr = '\0';
 
 	duk_push_lstring(es->env->ctx, func, ptr - func);
-	duk_push_string(es->env->ctx, "function");
+	duk_push_lstring(es->env->ctx, "function", ZBX_CONST_STRLEN("function"));
 
 	if (0 != duk_pcompile(es->env->ctx, DUK_COMPILE_FUNCTION))
 	{
