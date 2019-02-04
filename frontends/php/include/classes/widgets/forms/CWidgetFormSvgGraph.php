9a6165a776a2a8c61f47b0c36870ffcb2a6a7ad5
Miks Kronkalns <miks.kronkalns@zabbix.com>
2019-02-04 11:12:55 +0000

    ..F....... [ZBX-15535] fixed translation tring

--- frontends/php/include/classes/widgets/forms/CWidgetFormSvgGraph.php
+++ frontends/php/include/classes/widgets/forms/CWidgetFormSvgGraph.php
@@ -501,11 +501,11 @@ class CWidgetFormSvgGraph extends CWidgetForm {
 			$compare = true;
 
 			if (strlen(substr(strrchr($righty_min, '.'), 1)) > 4) {
-				$errors[] = _s('Invalid parameter "%1$s": %2$s.', _('Min'), _('a number is too small'));
+				$errors[] = _s('Invalid parameter "%1$s": %2$s.', _('Min'), _('too mach decimal places'));
 				$compare = false;
 			}
 			if (strlen(substr(strrchr($righty_max, '.'), 1))  > 4) {
-				$errors[] = _s('Invalid parameter "%1$s": %2$s.', _('Max'), _('a number is too small'));
+				$errors[] = _s('Invalid parameter "%1$s": %2$s.', _('Max'), _('too mach decimal places'));
 				$compare = false;
 			}
 
