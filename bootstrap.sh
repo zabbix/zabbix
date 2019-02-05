c64086c4a08284a8354cfb49f3a5a1a2cfcbe638
Michael Veksler <Mihails.Vekslers@zabbix.com>
2019-02-05 12:13:22 +0000

    ....I..... [ZBX-15211] reverted changes in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,26 +2,8 @@
 
 # Run this script to generate 'configure'
 
-usage()
-{
-	echo "usage: $0 [tests]"
-	exit 1
-}
-
-[ $# -gt 1 ] && usage
-
-if [ $# -eq 1 ]; then
-	[ $1 = "tests" ] || usage
-
-	cp -f tests/conf_tests.m4 m4/ || exit $?
-fi
-
 aclocal -I m4
 autoconf
 autoheader
 automake -a
 automake
-
-if [ -f m4/conf_tests.m4 ]; then
-	rm -f m4/conf_tests.m4
-fi
