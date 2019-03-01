7c2fbcab5f1a3ffd13309a6eef9302c30d6ffaaf
Andris Zeila <andris.zeila@zabbix.com>
2019-03-01 12:59:08 +0000

    ........S. [ZBXNEXT-4865] fixed label comparison

--- src/libs/zbxprometheus/zbxprometheus.c
+++ src/libs/zbxprometheus/zbxprometheus.c
@@ -182,6 +182,33 @@ static char	*str_loc_unquote_dyn(const char *src, const zbx_strloc_t *loc)
 
 /******************************************************************************
  *                                                                            *
+ * Function: str_loc_cmp                                                      *
+ *                                                                            *
+ * Purpose: compares substring at the specified location with the specified   *
+ *          text                                                              *
+ *                                                                            *
+ * Parameters: src  - [IN] the source string                                  *
+ *             loc  - [IN] the substring location                             *
+ *             text - [IN] the text to compare with                           *
+ *                                                                            *
+ * Return value: -1 - the substring is less than the specified text           *
+ *                0 - the substring is equal to the specified text            *
+ *                1 - the substring is greater than the specified text        *
+ *                                                                            *
+ ******************************************************************************/
+static int	str_loc_cmp(const char *src, const zbx_strloc_t *loc, const char *text)
+{
+	size_t	len_text, len_loc;
+
+	len_loc = loc->r - loc->l + 1;
+	len_text = strlen(text);
+
+	ZBX_RETURN_IF_NOT_EQUAL(len_loc, len_text);
+	return memcmp(src + loc->l, text, len_text);
+}
+
+/******************************************************************************
+ *                                                                            *
  * Function: str_loc_op                                                       *
  *                                                                            *
  * Purpose: parses condition operation at the specified location              *
@@ -622,7 +649,7 @@ static int	prometheus_filter_parse_labels(zbx_prometheus_filter_t *filter, const
 			return FAIL;
 		}
 
-		if (0 == strncmp(data + loc_key.l, "__name__", loc_key.r - loc_key.l + 1))
+		if (0 == str_loc_cmp(data, &loc_key, "__name__"))
 		{
 			if (NULL != filter->metric)
 			{
