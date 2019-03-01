c8354299422a23606d3310fbd010835152a2d84f
Vasily Goncharenko <vasilijs.goncarenko@zabbix.com>
2019-03-01 12:41:43 +0000

    A......... [ZBXNEXT-4938] fixed dchecks validation

--- frontends/php/include/classes/api/services/CDRule.php
+++ frontends/php/include/classes/api/services/CDRule.php
@@ -494,9 +494,10 @@ class CDRule extends CApiService {
 					continue;
 				}
 
-				if (!in_array($dcheck['type'], [SVC_AGENT, SVC_SNMPv1, SVC_SNMPv2c, SVC_SNMPv3])) {
+				if (!in_array($dcheck['type'], [SVC_AGENT, SVC_SNMPv1, SVC_SNMPv2c, SVC_SNMPv3])
+						&& $dcheck[$field] == ZBX_DISCOVERY_VALUE) {
 					self::exception(ZBX_API_ERROR_PARAMETERS,
-						_s('Only Zabbix agent, SNMPv1, SNMPv2 and SNMPv3 checks can have the "%1$s" parameter.', $field)
+						_s('Incorrect value for field "%1$s": %2$s.', $field, $dcheck[$field])
 					);
 				}
 
