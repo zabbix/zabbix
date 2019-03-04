5e8dd072a58a90ed3f2cc7be414fb5d6bab14055
Natalja Karpinska <natalja.karpinska@zabbix.com>
2019-03-04 10:29:30 +0000

    .......... [ZBXNEXT-826] updated release date [4.2.0beta2]

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,7 +22,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "18 February 2019";
+	static final String REVISION_DATE = "4 March 2019";
 	static final String REVISION = "{ZABBIX_REVISION}";
 	static final String VERSION = "4.2.0beta2";
 
