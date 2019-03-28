bbef976441ba2387c355e51a794bd7ccffeab32e
Andrejs Griscenko <Andrejs.Griscenko@zabbix.com>
2019-03-28 14:10:29 +0000

    ..F....... [ZBX-10574] replaced string manipulations with preg_match()

--- frontends/php/include/classes/helpers/CBrandHelper.php
+++ frontends/php/include/classes/helpers/CBrandHelper.php
@@ -82,7 +82,7 @@ class CBrandHelper {
 	 */
 	public static function getHelpUrl() {
 		return self::getValue('BRAND_HELP_URL', 'https://www.zabbix.com/documentation/'.
-			implode('.', array_slice(explode('.', ZABBIX_VERSION), 0, 2)).'/'
+			(preg_match('/^\d+\.\d+/', ZABBIX_VERSION, $version) ? $version[0].'/' : '')
 		);
 	}
 
