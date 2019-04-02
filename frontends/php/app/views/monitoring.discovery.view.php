37a44ff9a6868e90afc43fa3edccd3f8206f678e
Ivo Kurzemnieks <ivo.kurzemnieks@zabbix.com>
2019-04-02 13:59:25 +0000

    ..F....... [ZBX-11043] fixed coding style

--- frontends/php/app/views/monitoring.discovery.view.php
+++ frontends/php/app/views/monitoring.discovery.view.php
@@ -38,29 +38,29 @@ $widget = (new CWidget())
 			->setAttribute('aria-label', _('Content controls'))
 	)
 	->addItem((new CFilter((new CUrl('zabbix.php'))->setArgument('action', 'discovery.view')))
-	->setProfile($data['profileIdx'])
-	->setActiveTab($data['active_tab'])
-	->addFilterTab(_('Filter'), [
-		(new CFormList())
-			->addRow(
-				(new CLabel(_('Discovery rule'), 'filter_druleids__ms')),
-				(new CMultiSelect([
-					'name' => 'filter_druleids[]',
-					'object_name' => 'drules',
-					'data' => $data['filter']['drules'],
-					'popup' => [
-						'parameters' => [
-							'srctbl' => 'drules',
-							'srcfld1' => 'druleid',
-							'dstfrm' => 'zbx_filter',
-							'dstfld1' => 'filter_druleids_'
+		->setProfile($data['profileIdx'])
+		->setActiveTab($data['active_tab'])
+		->addFilterTab(_('Filter'), [
+			(new CFormList())
+				->addRow(
+					(new CLabel(_('Discovery rule'), 'filter_druleids__ms')),
+					(new CMultiSelect([
+						'name' => 'filter_druleids[]',
+						'object_name' => 'drules',
+						'data' => $data['filter']['drules'],
+						'popup' => [
+							'parameters' => [
+								'srctbl' => 'drules',
+								'srcfld1' => 'druleid',
+								'dstfrm' => 'zbx_filter',
+								'dstfld1' => 'filter_druleids_'
+							]
 						]
-					]
-				]))->setWidth(ZBX_TEXTAREA_FILTER_STANDARD_WIDTH)
-			)
-	])
-	->addVar('action', 'discovery.view')
-);
+					]))->setWidth(ZBX_TEXTAREA_FILTER_STANDARD_WIDTH)
+				)
+		])
+		->addVar('action', 'discovery.view')
+	);
 
 $discovery_table = CScreenBuilder::getScreen([
 	'resourcetype' => SCREEN_RESOURCE_DISCOVERY,
