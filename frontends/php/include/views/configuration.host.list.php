f3cfbfc178a643a2e44e3e08b84ea2c25aa03b96
Gregory Chalenko <Gregory.Chalenko@zabbix.com>
2019-04-02 14:50:40 +0000

    ..F....... [ZBX-15804] added prefix 'inaccessible discovery rule' to discovered host  with discovery rule inaccessible for current user in hosts list

--- frontends/php/include/views/configuration.host.list.php
+++ frontends/php/include/views/configuration.host.list.php
@@ -214,6 +214,12 @@ foreach ($data['hosts'] as $host) {
 			->addClass(ZBX_STYLE_ORANGE);
 		$description[] = NAME_DELIMITER;
 	}
+	else if ($host['flags'] == ZBX_FLAG_DISCOVERY_CREATED) {
+		// Discovered host which does not contain info about parent discovery rule is inaccessible for current user.
+		$description[] = (new CSpan(_('Inaccessible discovery rule')))
+			->addClass(ZBX_STYLE_ORANGE);
+		$description[] = NAME_DELIMITER;
+	}
 
 	$description[] = new CLink(CHtml::encode($host['name']),
 		'hosts.php?form=update&hostid='.$host['hostid'].url_param('groupid')
