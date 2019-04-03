7aab521148e1f107821cbd6de96f0cadd9e77aa4
Andrejs Kozlovs <andrejs.kozlovs@zabbix.com>
2019-04-03 08:55:32 +0000

    .......PS. [ZBX-15826] fixed unsupported macros parsing optimization with cursor moving to left position of the token

--- src/libs/zbxserver/expression.c
+++ src/libs/zbxserver/expression.c
@@ -2735,6 +2735,7 @@ int	substitute_simple_macros(zbx_uint64_t *actionid, const DB_EVENT *event, cons
 		require_numeric = 0;
 		N_functionid = 1;
 		raw_value = 0;
+		pos = token.loc.l;
 
 		switch (token.type)
 		{
