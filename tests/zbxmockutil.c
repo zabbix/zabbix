f28c6034f65894a789b16b0e1f86dad98135afcd
Viktors Tjarve <viktors.tjarve@zabbix.com>
2019-04-10 10:18:40 +0000

    .......... [DEV-875] fixed return type

--- tests/zbxmockutil.c
+++ tests/zbxmockutil.c
@@ -149,7 +149,7 @@ unsigned char	zbx_mock_str_to_value_type(const char *str)
  * Purpose: converts item type from text format                               *
  *                                                                            *
  ******************************************************************************/
-unsigned char	zbx_mock_str_to_item_type(const char *str)
+int	zbx_mock_str_to_item_type(const char *str)
 {
 	if (0 == strcmp(str, "ITEM_TYPE_ZABBIX"))
 		return ITEM_TYPE_ZABBIX;
