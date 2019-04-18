8689a0ae810e414dc33fdcb52c5a75764c3a607e
Vjaceslavs Bogdanovs <vjaceslavs.bogdanovs@zabbix.com>
2019-04-18 10:42:57 +0000

    .......... [DEV-1026] fixed comments in CTestArrayHelper

--- frontends/php/tests/include/helpers/CTestArrayHelper.php
+++ frontends/php/tests/include/helpers/CTestArrayHelper.php
@@ -4,18 +4,17 @@
  * Helper for array related operations.
  */
 class CTestArrayHelper {
+
 	/**
-	 * TODO
-	 * @param type $array
-	 * @param type $key
-	 * @param type $default
-	 * @return type
+	 * Get value from array by key.
+	 *
+	 * @param mixed $array      array
+	 * @param mixed $key        key to look for
+	 * @param mixed $default    default value to be returned if array key doesn't exist (or if non-array is passed)
+	 *
+	 * @return mixed
 	 */
 	public static function get($array, $key, $default = null) {
 		return (is_array($array) && array_key_exists($key, $array)) ? $array[$key] : $default;
-	}
-
-	public static function fillMultiple($root, $mapping, $data) {
-
 	}
 }
