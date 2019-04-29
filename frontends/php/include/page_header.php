650b771a347a4f219a61f53c1f50379535bf523c
Gregory Chalenko <gregory.chalenko@zabbix.com>
2019-04-29 16:33:34 +0300

    ..F....... [ZBX-15905] fixed map icons dimension detection

--- frontends/php/include/page_header.php
+++ frontends/php/include/page_header.php
@@ -180,13 +180,8 @@ if ($page['type'] == PAGE_TYPE_HTML) {
 			}
 		}
 	}
-	$pageHeader->addCssFile('styles/'.CHtml::encode($theme).'.css');
-
-	if ($page['file'] == 'sysmap.php') {
-		$pageHeader->addCssFile('imgstore.php?css=1&output=css');
-	}
-
 	$pageHeader
+		->addCssFile('styles/'.CHtml::encode($theme).'.css')
 		->addJsFile((new CUrl('js/browsers.js'))->getUrl())
 		->addJsBeforeScripts(
 			'var PHP_TZ_OFFSET = '.date('Z').','.
