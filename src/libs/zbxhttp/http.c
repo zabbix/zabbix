45462dfd4ea2689abd27bdaf2ac413265dc5a1f9
Andrejs Kozlovs <andrejs.kozlovs@zabbix.com>
2019-05-16 18:38:38 +0300

    .......PS. [ZBXNEXT-1856] patch for CURLAUTH_NEGOTIATE applied

--- src/libs/zbxhttp/http.c
+++ src/libs/zbxhttp/http.c
@@ -150,6 +150,16 @@ int	zbx_http_prepare_auth(CURL *easyhandle, unsigned char authtype, const char *
 			case HTTPTEST_AUTH_NTLM:
 				curlauth = CURLAUTH_NTLM;
 				break;
+			case HTTPTEST_AUTH_NEGOTIATE:
+#if LIBCURL_VERSION_NUM >= 0x072600
+				curlauth = CURLAUTH_NEGOTIATE;
+				break;
+#else
+				*error = zbx_dsprintf(*error, "Cannot use CURLAUTH_NEGOTIATE authentication method with"
+						"%s version of cURL library, use 7.38.0 or higher",
+						curl_version_info(0)->version);
+				return FAIL;
+#endif
 			default:
 				THIS_SHOULD_NEVER_HAPPEN;
 				break;
