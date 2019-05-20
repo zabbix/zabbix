2188161729b53dd9e0e80fc74dc5277eae3a22c4
Andrejs Verza <andrejs.verza@zabbix.com>
2019-05-20 09:26:27 +0300

    ..F....... [ZBX-16135] refactoring

--- frontends/php/include/validate.inc.php
+++ frontends/php/include/validate.inc.php
@@ -416,7 +416,7 @@ function check_fields(&$fields, $show_messages = true) {
 	}
 
 	if ($show_messages && $err != ZBX_VALID_OK) {
-		show_messages(($err == ZBX_VALID_OK), null, _('Page received incorrect data'));
+		show_messages(false, null, _('Page received incorrect data'));
 	}
 
 	return ($err == ZBX_VALID_OK);
