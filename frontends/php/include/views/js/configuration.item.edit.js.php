b3a595b74dbfa4057b6ccf701f5ebee863eb3faa
Miks Kronkalns <miks.kronkalns@zabbix.com>
2019-05-23 14:33:22 +0300

    ..F....... [ZBXNEXT-5176,ZBXNEXT-5205] fixed appearance of trends storage period field when change the type of value in item edit form

--- frontends/php/include/views/js/configuration.item.edit.js.php
+++ frontends/php/include/views/js/configuration.item.edit.js.php
@@ -94,10 +94,12 @@ zbx_subarray_push($this->data['valueTypeVisibility'], ITEM_VALUE_TYPE_UINT64, 'r
 
 			if ((old_value == <?= ITEM_VALUE_TYPE_STR ?> || old_value == <?= ITEM_VALUE_TYPE_LOG ?>
 					|| old_value == <?= ITEM_VALUE_TYPE_TEXT ?>)
-					&& ((new_value == <?= ITEM_VALUE_TYPE_FLOAT ?>
-					|| new_value == <?= ITEM_VALUE_TYPE_UINT64 ?>)
-					&& trends.val() == 0)) {
-				trends.val('<?= $this->data['trends_default'] ?>');
+					&& (new_value == <?= ITEM_VALUE_TYPE_FLOAT ?>
+					|| new_value == <?= ITEM_VALUE_TYPE_UINT64 ?>)) {
+				if (trends.val() == 0) {
+					trends.val('<?= $this->data['trends_default'] ?>');
+				}
+				$('#trends_mode_1').prop('checked', true);
 			}
 		});
 
