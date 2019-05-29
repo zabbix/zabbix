d1be66f2e7b13157bbbaa5fc222f67aedffd187d
Natalja Karpinska <natalja.karpinska@zabbix.com>
2019-05-29 11:54:29 +0300

    .......... [ZBX-16122] fixed autotest, changed error message text for trigger dependency

--- frontends/php/tests/selenium/testTriggerDependencies.php
+++ frontends/php/tests/selenium/testTriggerDependencies.php
@@ -49,7 +49,7 @@ class testTriggerDependencies extends CLegacyWebTest {
 
 	public function testTriggerDependenciesFromHost_SimpleTestProvider() {
 		return [
-			['10001', 'Cannot add dependency from a host to a template.'],
+			['10001', 'Not all templates are linked to "Test host".'],
 			['10081', 'Trigger updated']
 		];
 	}
