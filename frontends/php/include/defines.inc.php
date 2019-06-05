ff28b423d0e9a7316461180f6c88bb6ed29bd699
Jurijs Klopovskis <jurijs.klopovskis@zabbix.com>
2019-06-05 15:05:34 +0300

    .......... [ZBXNEXT-826] updated ChangeLog, release date and version [4.0.9]

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,8 +19,8 @@
 **/
 
 
-define('ZABBIX_VERSION',		'4.0.8');
-define('ZABBIX_API_VERSION',	'4.0.8');
+define('ZABBIX_VERSION',		'4.0.9');
+define('ZABBIX_API_VERSION',	'4.0.9');
 define('ZABBIX_EXPORT_VERSION',	'4.0');
 define('ZABBIX_DB_VERSION',		4000000);
 
