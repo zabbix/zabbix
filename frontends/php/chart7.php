2b33c54a0a63cb9a17cb4f830302e6ac4028bdd8
Roberts Lataria <roberts.lataria@zabbix.com>
2019-06-12 11:38:15 +0300

    ..F....... [ZBX-16153] fix unnecessary check

--- frontends/php/chart7.php
+++ frontends/php/chart7.php
@@ -39,7 +39,7 @@ $fields = [
 	'graphtype' =>		[T_ZBX_INT,			O_OPT, null,	IN('2,3'),			null],
 	'graph3d' =>		[T_ZBX_INT,			O_OPT, P_NZERO,	IN('0,1'),			null],
 	'legend' =>			[T_ZBX_INT,			O_OPT, null,	IN('0,1'),			null],
-	'items' =>			[T_ZBX_STR,			O_OPT, null,	null,				null],
+	'items' =>			[T_ZBX_STR,			O_MAND, null,	null,				null],
 	'widget_view' =>	[T_ZBX_INT,			O_OPT, null,	IN('0,1'),			null]
 ];
 if (!check_fields($fields)) {
@@ -49,7 +49,7 @@ validateTimeSelectorPeriod(getRequest('from'), getRequest('to'));
 
 $items = getRequest('items', []);
 
-if (count($items) === 0) {
+if (!$items) {
 	show_error_message(_('No items defined.'));
 	exit();
 }
