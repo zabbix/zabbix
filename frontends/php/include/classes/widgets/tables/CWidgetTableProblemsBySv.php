2f5b7581adae5a2ba9018201addabeed579fe87c
Vasily Goncharenko <vasilijs.goncarenko@zabbix.com>
2019-07-04 17:09:24 +0300

    ..F....... [ZBXNEXT-5251] improved code structure

--- frontends/php/include/classes/widgets/tables/CWidgetTableProblemsBySv.php
+++ frontends/php/include/classes/widgets/tables/CWidgetTableProblemsBySv.php
@@ -1,11 +1,30 @@
 <?php
+/*
+** Zabbix
+** Copyright (C) 2001-2019 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+**/
+
 
 /**
  * Table generation for Problems By Severity widget.
  *
  * Class CWidgetTablesProblemsBySv
  */
-class CWidgetTableProblemsBySv {
+class CWidgetTableProblemsBySv extends CWidgetTable {
 	/**
 	 * Widget filters.
 	 *
@@ -35,11 +54,32 @@ class CWidgetTableProblemsBySv {
 	protected $backurl;
 
 	/**
+	 * CUrl to group problems.
+	 *
+	 * @var CUrl
+	 */
+	protected $groupurl;
+
+	/**
 	 * Way of showing problems (EXTACK_OPTION_ALL, EXTACK_OPTION_UNACK, EXTACK_OPTION_BOTH).
 	 *
 	 * @var integer
 	 */
 	protected $ext_ack;
+
+	/**
+	 * Hide empty groups filter.
+	 *
+	 * @var boolean
+	 */
+	protected $filter_hide_empty_groups;
+
+	/**
+	 * Totals mode.
+	 *
+	 * @var boolean
+	 */
+	protected $total = false;
 
 	/**
 	 * @param array  $filter
@@ -79,13 +119,13 @@ class CWidgetTableProblemsBySv {
 	 *
 	 */
 	public function __construct(array $filter = [], array $data = [], array $config = [], $backurl = '') {
+		parent::__construct($data['groups']);
+
 		$this->filter = $filter;
 		$this->data = $data;
 		$this->severity_names = $config;
 		$this->backurl = $backurl;
 		$this->ext_ack = array_key_exists('ext_ack', $filter) ? $filter['ext_ack'] : EXTACK_OPTION_ALL;
-
-		return $this;
 	}
 
 	/**
@@ -106,7 +146,7 @@ class CWidgetTableProblemsBySv {
 		$filter_severities = (array_key_exists('severities', $this->filter) && $this->filter['severities'])
 			? $this->filter['severities']
 			: range(TRIGGER_SEVERITY_NOT_CLASSIFIED, TRIGGER_SEVERITY_COUNT - 1);
-		$filter_hide_empty_groups = array_key_exists('hide_empty_groups', $this->filter)
+		$this->filter_hide_empty_groups = array_key_exists('hide_empty_groups', $this->filter)
 			? $this->filter['hide_empty_groups']
 			: 0;
 
@@ -118,11 +158,9 @@ class CWidgetTableProblemsBySv {
 			}
 		}
 
-		$table = (new CTableInfo())
-			->setHeader($header)
-			->setHeadingColumn(0);
+		$this->setTable(null, ['header' => $header, 'heading_column' => 0]);
 
-		$url_group = (new CUrl('zabbix.php'))
+		$this->groupurl = (new CUrl('zabbix.php'))
 			->setArgument('action', 'problem.view')
 			->setArgument('filter_set', 1)
 			->setArgument('filter_show', TRIGGERS_OPTION_RECENT_PROBLEM)
@@ -137,63 +175,104 @@ class CWidgetTableProblemsBySv {
 					: null
 			);
 
-		foreach ($this->data['groups'] as $group) {
-			if ($filter_hide_empty_groups && !$group['has_problems']) {
-				continue;
-			}
+		$this->populateNormalTable();
 
-			$url_group->setArgument('filter_groupids', [$group['groupid']]);
-			$row = [new CLink($group['name'], $url_group->getUrl())];
-
-			$row = $this->getGroupSeverity($row, $group, false);
-			$table->addRow($row);
-		}
-
-		return $table;
+		return $this->table;
 	}
 
 	private function getSeverityTotals() {
-		$goups_info = $this->aggregateData($this->data['groups']);
+		$this->total = true;
+		$this->setData($this->aggregateData($this->data['groups']));
 
-		$table = (new CTableInfo())
-			->addClass(ZBX_STYLE_BY_SEVERITY_WIDGET)
-			->addClass(($this->filter['layout'] == STYLE_HORIZONTAL)
+		$this->setTable([
+			ZBX_STYLE_BY_SEVERITY_WIDGET,
+			($this->filter['layout'] == STYLE_HORIZONTAL)
 				? ZBX_STYLE_BY_SEVERITY_LAYOUT_HORIZONTAL
-				: ZBX_STYLE_BY_SEVERITY_LAYOUT_VERTICAL
-			);
+				: ZBX_STYLE_BY_SEVERITY_LAYOUT_VERTICAL,
+		]);
 
-		foreach ($goups_info as $group) {
-			$row = $this->getGroupSeverity([], $group, true, $table);
-
-			if ($this->filter['layout'] == STYLE_HORIZONTAL) {
-				$table->addRow($row);
-			}
+		if ($this->filter['layout'] == STYLE_HORIZONTAL) {
+			$this->populateNormalTable();
+		}
+		else {
+			$this->populateVerticalTotalTable();
 		}
 
-		return $table;
+		return $this->table;
 	}
 
 	/**
-	 * Generate row or column by one group or total.
+	 * Row initialization.
 	 *
-	 * @param array $row          Initial value of the $row
-	 * @param array $group        Group or total data
-	 * @param boolean $aggregate  True for totals mode
-	 * @param CTableInfo $table   Table for column generation in totals mode
+	 * @param array $group  Group data array for initialization
 	 * @return array
 	 */
-	private function getGroupSeverity(array $row, array $group, $aggregate, CTableInfo $table = null) {
+	protected function initRow(array $group) {
+		if ($this->total) {
+			return [];
+		}
+		else {
+			$this->groupurl->setArgument('filter_groupids', [$group['groupid']]);
+			return [new CLink($group['name'], $this->groupurl->getUrl())];
+		}
+	}
 
-		foreach ($group['stats'] as $severity => $stat) {
+	/**
+	 * Extract severities list from group.
+	 *
+	 * @param array $group  Group data array.
+	 * @return array
+	 */
+	protected function extractRowElements(array $group) {
+		return $group['stats'];
+	}
 
-			if ($this->filter['severities'] && !in_array($severity, $this->filter['severities'])) {
-				continue;
-			}
-			if (!$aggregate && $stat['count'] == 0 && $stat['count_unack'] == 0) {
-				$row[] = '';
-				continue;
-			}
-			$severity_name = $aggregate ? SPACE.getSeverityName($severity, $this->severity_names) : '';
+	/**
+	 * Filter groups.
+	 *
+	 * @param array $group  Group data array.
+	 * @return boolean
+	 */
+	protected function rowFilter(array $group) {
+		if ($this->filter_hide_empty_groups && !$group['has_problems']) {
+			// Skip row.
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Filter cells.
+	 *
+	 * @param int   $severity  Severity code.
+	 * @param array $stat      Problem info with this severity.
+	 * @return boolean
+	 */
+	protected function cellFilter($severity, array $stat) {
+		if ($this->filter['severities'] && !in_array($severity, $this->filter['severities'])) {
+			// Skip cell.
+			return false;
+		}
+		if (!$this->total && $stat['count'] == 0 && $stat['count_unack'] == 0) {
+			// Add empty cell.
+			$this->row[] = '';
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Get table cell with this severity.
+	 *
+	 * @param int   $severity  Severity code.
+	 * @param array $stat      Problem info with this severity.
+	 * @return CCol
+	 */
+	protected function getTableCell($severity, array $stat) {
+
+			$severity_name = $this->total ? ' '.getSeverityName($severity, $this->severity_names) : '';
 
 			$allTriggersNum = $stat['count'];
 			if ($allTriggersNum) {
@@ -213,31 +292,26 @@ class CWidgetTableProblemsBySv {
 
 			switch ($this->ext_ack) {
 				case EXTACK_OPTION_ALL:
-					$row[] = getSeverityCell($severity, null, [new CSpan($allTriggersNum), $severity_name]);
+					$cell = getSeverityCell($severity, null, [new CSpan($allTriggersNum), $severity_name]);
 					break;
 
 				case EXTACK_OPTION_UNACK:
-					$row[] = getSeverityCell($severity, null, [new CSpan($unackTriggersNum), $severity_name]);
+					$cell = getSeverityCell($severity, null, [new CSpan($unackTriggersNum), $severity_name]);
 					break;
 
 				case EXTACK_OPTION_BOTH:
-					if ($stat['count_unack'] != 0 || $aggregate) {
-						$row[] = getSeverityCell($severity, $this->severity_names, [
+					if ($stat['count_unack'] != 0 || $this->total) {
+						$cell = getSeverityCell($severity, $this->severity_names, [
 							new CSpan([$unackTriggersNum, ' '._('of').' ', $allTriggersNum]), $severity_name
 						]);
 					}
 					else {
-						$row[] = getSeverityCell($severity, $this->severity_names, [$allTriggersNum, $severity_name]);
+						$cell = getSeverityCell($severity, $this->severity_names, [$allTriggersNum, $severity_name]);
 					}
 					break;
 			}
-			if ($this->filter['layout'] == STYLE_VERTICAL) {
-				$table->addRow($row);
-				$row = [];
-			}
-		}
 
-		return $row;
+		return $cell;
 	}
 
 	/**
