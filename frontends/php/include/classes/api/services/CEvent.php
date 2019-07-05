cc0293e07d2a586cfe40a283f7990cd75107e8b6
Miks Kronkalns <miks.kronkalns@zabbix.com>
2019-07-05 16:43:58 +0300

    A.F....... [ZBX-16031] changed event update form and event API to enable problem closing checkbox if only part of problems can be closed

--- frontends/php/include/classes/api/services/CEvent.php
+++ frontends/php/include/classes/api/services/CEvent.php
@@ -620,13 +620,23 @@ class CEvent extends CApiService {
 
 		$events = $this->get([
 			'output' => ['objectid', 'acknowledged', 'severity', 'r_eventid'],
-			'select_acknowledges' => $has_close_action? ['action'] : null,
+			'select_acknowledges' => $has_close_action ? ['action'] : null,
+			'selectRelatedObject' => $has_close_action ? ['manual_close'] : null,
 			'eventids' => $data['eventids'],
 			'source' => EVENT_SOURCE_TRIGGERS,
 			'object' => EVENT_OBJECT_TRIGGER,
 			'value' => TRIGGER_VALUE_TRUE,
 			'preservekeys' => true
 		]);
+
+		$editable_triggers = $events
+			? API::Trigger()->get([
+				'output' => [],
+				'triggerids' => zbx_objectValues($events, 'objectid'),
+				'editable' => true,
+				'preservekeys' => true
+			])
+			: [];
 
 		$ack_eventids = [];
 		$sev_change_eventids = [];
@@ -640,7 +650,8 @@ class CEvent extends CApiService {
 			$message = '';
 
 			// Perform ZBX_PROBLEM_UPDATE_CLOSE action flag.
-			if ($has_close_action && !$this->isEventClosed($event)) {
+			if ($has_close_action && $event['relatedObject']['manual_close'] == ZBX_TRIGGER_MANUAL_CLOSE_ALLOWED
+					&& array_key_exists($event['objectid'], $editable_triggers) && !$this->isEventClosed($event)) {
 				$action |= ZBX_PROBLEM_UPDATE_CLOSE;
 			}
 
@@ -823,7 +834,7 @@ class CEvent extends CApiService {
 		 * If at least one of following is given, API call should not be processed:
 		 *   - eventid for OK event
 		 *   - eventid with source, that is not trigger
-		 *   - no read rights for related trigger
+		 *   - in case of problem severity change, related trigger is not writable
 		 *   - unexisting eventid
 		 */
 		if (count($data['eventids']) != count($events)) {
@@ -860,21 +871,26 @@ class CEvent extends CApiService {
 	 * @param int   $editable_events_count  Count of editable events.
 	 *
 	 * @throws APIException                 Throws an exception:
-	 *                                        - If at least one event is not editable;
-	 *                                        - If any of given event can be closed manually according the triggers
-	 *                                          configuration.
+	 *                                        - If none of passed events is editable;
+	 *                                        - If none of passed trigger is configured to be closed manually.
 	 */
 	protected function checkCanBeManuallyClosed(array $events, $editable_events_count) {
-		if (count($events) != $editable_events_count) {
+		if ($editable_events_count == 0) {
 			self::exception(ZBX_API_ERROR_PERMISSIONS, _('No permissions to referred object or it does not exist!'));
 		}
 
+		$manually_closable = false;
 		foreach ($events as $event) {
-			if ($event['relatedObject']['manual_close'] != ZBX_TRIGGER_MANUAL_CLOSE_ALLOWED) {
-				self::exception(ZBX_API_ERROR_PERMISSIONS,
-					_s('Cannot close problem: %1$s.', _('trigger does not allow manual closing'))
-				);
+			if ($event['relatedObject']['manual_close'] == ZBX_TRIGGER_MANUAL_CLOSE_ALLOWED) {
+				$manually_closable = true;
+				break;
 			}
+		}
+
+		if (!$manually_closable) {
+			self::exception(ZBX_API_ERROR_PERMISSIONS,
+				_s('Cannot close problem: %1$s.', _('trigger does not allow manual closing'))
+			);
 		}
 	}
 
