67afdb16b87dbf09c288eebd7c876b5acc2ca991
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2019-07-26 14:36:57 +0300

    .......... [DEV-1144] use configuration file to setup mock_server and use logs instead of stdout

--- go/src/zabbix/cmd/mock_server/mock_server.go
+++ go/src/zabbix/cmd/mock_server/mock_server.go
@@ -24,66 +24,134 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strconv"
 	"time"
 	"zabbix/pkg/comms"
+	"zabbix/pkg/conf"
+	"zabbix/pkg/log"
 )
+
+type MockServerOptions struct {
+	LogType          string `conf:",,,console"`
+	LogFile          string `conf:",optional"`
+	DebugLevel       int    `conf:",,0:5,3"`
+	Port             int    `conf:",,1:65535,10051"`
+	Timeout          int    `conf:",,1:30,5"`
+	ActiveChecksFile string `conf:",optional"`
+}
+
+var options MockServerOptions
 
 func handleConnection(c comms.ZbxConnection, activeChecks []byte, tFlag int) {
 	defer c.Close()
 
 	js, err := c.Read(time.Second * time.Duration(tFlag))
 	if err != nil {
-		fmt.Printf("Read failed: %s\n", err)
+		log.Warningf("Read failed: %s\n", err)
 		return
 	}
 
-	var pairs map[string]interface{}
+	log.Debugf("got '%s'", string(js))
 
+	var pairs map[string]interface{}
 	if err := json.Unmarshal(js, &pairs); err != nil {
-		panic(err)
+		log.Warningf("Unmarshal failed: %s\n", err)
+		return
 	}
 
 	switch pairs["request"] {
 	case "active checks":
 		err = c.Write(activeChecks, time.Second*time.Duration(tFlag))
 		if err != nil {
-			fmt.Printf("Write failed: %s\n", err)
+			log.Warningf("Write failed: %s\n", err)
 			return
 		}
 	default:
-		fmt.Printf("Unsupported request: %s\n", pairs["request"])
+		log.Warningf("Unsupported request: %s\n", pairs["request"])
 		return
 	}
+
 }
 
 func main() {
-	var fFlag, pFlag string
-	var tFlag int
-
+	var confFlag string
 	const (
-		fDefault     = "active_checks.json"
-		fDescription = "Path to the json file used in response"
-		pDefault     = "10051"
-		pDescription = "Listen port"
-		tDefault     = 5
-		tDescription = "Timeout in seconds"
+		confDefault     = "mock_server.conf"
+		confDescription = "Path to the configuration file"
 	)
-	flag.StringVar(&fFlag, "f", fDefault, fDescription)
-	flag.StringVar(&pFlag, "p", pDefault, pDescription)
-	flag.IntVar(&tFlag, "t", tDefault, tDescription)
+	flag.StringVar(&confFlag, "config", confDefault, confDescription)
+	flag.StringVar(&confFlag, "c", confDefault, confDescription+" (shorhand)")
+
+	var foregroundFlag bool
+	const (
+		foregroundDefault     = true
+		foregroundDescription = "Run Zabbix agent in foreground"
+	)
+	flag.BoolVar(&foregroundFlag, "foreground", foregroundDefault, foregroundDescription)
+	flag.BoolVar(&foregroundFlag, "f", foregroundDefault, foregroundDescription+" (shorhand)")
 	flag.Parse()
 
-	activeChecks, err := ioutil.ReadFile(fFlag)
+	if err := conf.Load(confFlag, &options); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+
+	if err := conf.Load(confFlag, &options); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+
+	var logType, logLevel int
+	switch options.LogType {
+	case "console":
+		logType = log.Console
+	case "file":
+		logType = log.File
+	}
+	switch options.DebugLevel {
+	case 0:
+		logLevel = log.Info
+	case 1:
+		logLevel = log.Crit
+	case 2:
+		logLevel = log.Err
+	case 3:
+		logLevel = log.Warning
+	case 4:
+		logLevel = log.Debug
+	case 5:
+		logLevel = log.Trace
+	}
+
+	if err := log.Open(logType, logLevel, options.LogFile); err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot initialize logger: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	greeting := fmt.Sprintf("Starting Zabbix Agent [(hostname placeholder)]. (version placeholder)")
+	log.Infof(greeting)
+
+	if foregroundFlag {
+		if options.LogType != "console" {
+			fmt.Println(greeting)
+		}
+		fmt.Println("Press Ctrl+C to exit.")
+	}
+
+	log.Infof("using configuration file: %s", confFlag)
+
+	activeChecks, err := ioutil.ReadFile(options.ActiveChecksFile)
 	if err != nil {
-		fmt.Printf("Cannot read file: %s\n", err)
+		log.Critf("Cannot read active checks file: %s\n", err)
 		return
 	}
 
 	var ln comms.ZbxListener
 
-	err = ln.Listen(":" + pFlag)
+	err = ln.Listen(":" + strconv.Itoa(options.Port))
 	if err != nil {
-		fmt.Printf("Listen failed: %s\n", err)
+		log.Critf("Listen failed: %s\n", err)
 		return
 	}
 	defer ln.Close()
@@ -93,10 +161,10 @@ func main() {
 
 		err = c.Accept(&ln)
 		if err != nil {
-			fmt.Printf("Accept failed: %s\n", err)
+			log.Critf("Accept failed: %s\n", err)
 			return
 		}
 
-		go handleConnection(c, activeChecks, tFlag)
+		go handleConnection(c, activeChecks, options.Timeout)
 	}
 }
