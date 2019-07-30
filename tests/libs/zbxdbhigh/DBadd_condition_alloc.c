028ec746d45b80c11678de67d4027316796f79cf
Michael Veksler <Mihails.Vekslers@zabbix.com>
2019-07-30 10:19:09 +0300

    .......PS. [ZBX-13266] implementation of 'IN' logic for all db except Oracle

--- tests/libs/zbxdbhigh/DBadd_condition_alloc.c
+++ tests/libs/zbxdbhigh/DBadd_condition_alloc.c
@@ -32,12 +32,16 @@ void	zbx_mock_test_entry(void **state)
 #if defined(HAVE_SQLITE3)
 #	define RESULT	"out.sqlite_regex"
 #else
+#if defined(HAVE_ORACLE)
+#	define RESULT	"out.sqlora_regex"
+#else
 #	define RESULT	"out.sql_regex"
+#endif
 #endif
 	const char		*sql_where, *sql_rgx, *field_name;
 	zbx_vector_uint64_t	in_ids;
-	int			i, count;
-	uint64_t		value_id;
+	int			i, count1, count2, shift, repeat;
+	uint64_t		value_id, value;
 	char			*sql;
 	size_t			sql_alloc = 4 * ZBX_KIBIBYTE, sql_offset = 0;
 
@@ -46,13 +50,33 @@ void	zbx_mock_test_entry(void **state)
 	sql_where = zbx_mock_get_parameter_string("in.sql_where");
 	field_name = zbx_mock_get_parameter_string("in.field_name");
 	value_id = zbx_mock_get_parameter_uint64("in.start_id");
-	count = atoi(zbx_mock_get_parameter_string("in.count"));
+	count1 = atoi(zbx_mock_get_parameter_string("in.count1"));
+	count2 = atoi(zbx_mock_get_parameter_string("in.count2"));
+	shift = atoi(zbx_mock_get_parameter_string("in.shift"));
+	repeat = atoi(zbx_mock_get_parameter_string("in.repeat"));
 	sql_rgx = zbx_mock_get_parameter_string(RESULT);
 	sql = (char *)zbx_malloc(NULL, sql_alloc);
 	zbx_vector_uint64_create(&in_ids);
+	value = value_id;
 
-	for (i = 0; i < count; i++)
-		zbx_vector_uint64_append(&in_ids, value_id++);
+	do
+	{
+		for (i = 0; i < count1; i++)
+			zbx_vector_uint64_append(&in_ids, value++);
+
+		value_id += shift;
+		value = value_id;
+
+		for (i = 0; i < count2; i++)
+			zbx_vector_uint64_append(&in_ids, value++);
+
+		if (0 != count2)
+		{
+			value_id += shift;
+			value = value_id;
+		}
+	}
+	while (repeat--);
 
 	zbx_strcpy_alloc(&sql, &sql_alloc, &sql_offset, sql_where);
 	DBadd_condition_alloc(&sql, &sql_alloc, &sql_offset, field_name, in_ids.values, in_ids.values_num);
@@ -60,7 +84,7 @@ void	zbx_mock_test_entry(void **state)
 
 	if (NULL == zbx_regexp_match(sql, sql_rgx, NULL))
 	{
-		int len;
+		unsigned int len;
 
 		if (sql_offset > (len = 4 * ZBX_KIBIBYTE) * 2)
 		{
