ca7a9cfaa01f0e2dae5078f1af5a29ebdf5b5f09
Andris Zeila <andris.zeila@zabbix.com>
2019-08-09 10:04:57 +0300

    .......... [DEV-1100] removed libcurl from hardcoded library list in cgo

--- go/src/zabbix/pkg/zbxlib/globals.go
+++ go/src/zabbix/pkg/zbxlib/globals.go
@@ -39,7 +39,7 @@ package zbxlib
 #cgo LDFLAGS: ${SRCDIR}/../../../../../src/libs/zbxexec/libzbxexec.a
 #cgo LDFLAGS: ${SRCDIR}/../../../../../src/libs/zbxalgo/libzbxalgo.a
 #cgo LDFLAGS: ${SRCDIR}/../../../../../src/libs/zbxjson/libzbxjson.a
-#cgo LDFLAGS: -lz -lpcre -lresolv -lcurl
+#cgo LDFLAGS: -lz -lpcre -lresolv
 #cgo LDFLAGS: -Wl,--end-group
 
 #include "common.h"
