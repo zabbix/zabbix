04bf414b2b0131d985f2f9928dc4d4a62a52ddda
Michael Veksler <Mihails.Vekslers@zabbix.com>
2019-08-12 17:46:44 +0300

    .......... [DEV-1254] c call 'proc.num'

--- go/src/zabbix/pkg/zbxlib/checks.go
+++ go/src/zabbix/pkg/zbxlib/checks.go
@@ -33,6 +33,7 @@ int	SYSTEM_LOCALTIME(AGENT_REQUEST *request, AGENT_RESULT *result);
 int	NET_DNS(AGENT_REQUEST *request, AGENT_RESULT *result);
 int	NET_DNS_RECORD(AGENT_REQUEST *request, AGENT_RESULT *result);
 int	PROC_MEM(AGENT_REQUEST *request, AGENT_RESULT *result);
+int	PROC_NUM(AGENT_REQUEST *request, AGENT_RESULT *result);
 
 static int execute_check(const char *key, zbx_agent_check_t check_func, char **value, char **error)
 {
@@ -90,6 +91,8 @@ func ExecuteCheck(key string, params []string) (result *string, err error) {
 		cfunc = unsafe.Pointer(C.NET_DNS_RECORD)
 	case "proc.mem":
 		cfunc = unsafe.Pointer(C.PROC_MEM)
+	case "proc.num":
+		cfunc = unsafe.Pointer(C.PROC_NUM)
 	default:
 		return nil, fmt.Errorf("Unsupported metric %s", key)
 	}
