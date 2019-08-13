bebf16042bf64aa4c23f97d9b5fcf0ad0c6a8ddf
Andris Zeila <andris.zeila@zabbix.com>
2019-08-13 14:21:57 +0300

    .......... [DEV-1219] added non collector based vfs.dev metrics

--- go/src/zabbix/plugins/vfs/dev/dev.go
+++ go/src/zabbix/plugins/vfs/dev/dev.go
@@ -21,29 +21,184 @@ package vfsdev
 
 import (
 	"errors"
+	"fmt"
+	"sync"
+	"time"
 	"zabbix/internal/plugin"
 )
 
 // Plugin -
 type Plugin struct {
 	plugin.Base
+	devices map[string]*devUnit
+	mutex   sync.Mutex
 }
 
 var impl Plugin
 
+const (
+	maxInactivityPeriod = time.Hour * 3
+	maxHistory          = 60*15 + 1
+)
+
+const (
+	ioModeRead = iota
+	ioModeWrite
+)
+
+const (
+	statTypeSectors = iota
+	statTypeOperations
+	statTypeSPS
+	statTypeOPS
+)
+
+type historyIndex int
+
+func (h historyIndex) inc() historyIndex {
+	h++
+	if h == maxHistory {
+		h = 0
+	}
+	return h
+}
+
+func (h historyIndex) dec() historyIndex {
+	h--
+	if h < 0 {
+		h = maxHistory - 1
+	}
+	return h
+}
+
+func (h historyIndex) sub(value historyIndex) historyIndex {
+	h -= value
+	for h < 0 {
+		h += maxHistory
+	}
+	return h
+}
+
+type devIO struct {
+	sectors    uint64
+	operations uint64
+}
+
+type devStats struct {
+	clock int64
+	rx    devIO
+	tx    devIO
+}
+
+type devUnit struct {
+	name       string
+	head, tail int
+	accessed   time.Time
+	history    []devStats
+}
+
+var typeParams map[string]int = map[string]int{
+	"":           statTypeSPS,
+	"sps":        statTypeSPS,
+	"sectors":    statTypeSectors,
+	"operations": statTypeOperations,
+}
+
+var rangeParams map[string]int = map[string]int{
+	"":      60,
+	"avg1":  60,
+	"avg5":  60 * 5,
+	"avg15": 60 * 15,
+}
+
 func (p *Plugin) Export(key string, params []string, ctx plugin.ContextProvider) (result interface{}, err error) {
+	var mode int
 	switch key {
-	case "vfs.dev.read", "vfs.dev.write":
-		return nil, errors.New("Not implemented")
+	case "vfs.dev.read":
+		mode = ioModeRead
+	case "vfs.dev.write":
+		mode = ioModeWrite
 	case "vfs.dev.discovery":
 		return p.getDiscovery()
 	default:
 		return nil, errors.New("Unsupported metric")
 	}
 
+	var devParam, typeParam, rangeParam string
+	switch len(params) {
+	case 3:
+		rangeParam = params[2]
+		fallthrough
+	case 2:
+		typeParam = params[1]
+		fallthrough
+	case 1:
+		devParam = params[0]
+		if devParam == "all" {
+			devParam = ""
+		}
+	default:
+		return nil, errors.New("Too many parameters.")
+	}
+
+	var ok bool
+	var statType int
+	if statType, ok = typeParams[typeParam]; !ok {
+		return nil, errors.New("Invalid second parameter.")
+	}
+
+	if statType == statTypeSectors || statType == statTypeOperations {
+		if len(params) > 2 {
+			return nil, errors.New("Invalid number of parameters.")
+		}
+		var stats *devStats
+		if stats, err = p.getDeviceStats(devParam); err != nil {
+			return
+		} else {
+			if stats == nil {
+				return nil, errors.New("Device not found.")
+			}
+			var devio *devIO
+			if mode == ioModeRead {
+				devio = &stats.rx
+			} else {
+				devio = &stats.tx
+			}
+			if statType == statTypeSectors {
+				return devio.sectors, nil
+			}
+			return devio.operations, nil
+		}
+	}
+
+	if ctx == nil {
+		return nil, errors.New("This item is available only in daemon mode.")
+	}
+
+	var statRange int
+	if statRange, ok = rangeParams[rangeParam]; !ok {
+		return nil, errors.New("Invalid third parameter.")
+	}
+
+	var devName string
+	if devName, err = p.getDeviceName(devParam); err != nil {
+		return nil, fmt.Errorf("Cannot obtain device name: %s", err)
+	}
+
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	if dev, ok := p.devices[devName]; ok {
+		// TODO: retrieve dev statistics
+		return nil, fmt.Errorf("Not implemented. %v %d %d", *dev, statRange, mode)
+	} else {
+		p.devices[devName] = &devUnit{name: devName, accessed: time.Now(), history: make([]devStats, maxHistory)}
+		return
+	}
 }
 
 func init() {
+	impl.devices = make(map[string]*devUnit)
 	plugin.RegisterMetric(&impl, "vfsdev", "vfs.dev.read", "Disk read statistics.")
 	plugin.RegisterMetric(&impl, "vfsdev", "vfs.dev.write", "Disk write statistics.")
 	plugin.RegisterMetric(&impl, "vfsdev", "vfs.dev.discovery", "List of block devices and their type."+
--- go/src/zabbix/plugins/vfs/dev/dev_linux.go
+++ go/src/zabbix/plugins/vfs/dev/dev_linux.go
@@ -21,10 +21,14 @@ package vfsdev
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 	"syscall"
 
@@ -35,6 +39,7 @@ const (
 	devLocation       = "/dev/"
 	sysBlkdevLocation = "/sys/dev/block/"
 	devtypePrefix     = "DEVTYPE="
+	diskstatLocation  = "/proc/diskstats"
 )
 
 type devRecord struct {
@@ -80,4 +85,145 @@ func (p *Plugin) getDiscovery() (out string, err error) {
 		return
 	}
 	return string(b), nil
+}
+
+func (p *Plugin) getDeviceName(name string) (devName string, err error) {
+	if name == "" {
+		return "", nil
+	}
+	if !strings.HasPrefix(name, devLocation) {
+		name = devLocation + name
+	}
+	var stat os.FileInfo
+	if stat, err = os.Stat(name); err != nil {
+		return
+	}
+	var file *os.File
+	if file, err = os.Open(diskstatLocation); err != nil {
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		if len(fields) < 3 {
+			return "", fmt.Errorf("unexpected %s file format", diskstatLocation)
+		}
+		var major, minor uint64
+		if major, err = strconv.ParseUint(fields[0], 10, 32); err != nil {
+			return
+		}
+		if minor, err = strconv.ParseUint(fields[1], 10, 32); err != nil {
+			return
+		}
+		rdev := stat.Sys().(*syscall.Stat_t).Rdev
+		if uint64(unix.Major(rdev)) == major && uint64(unix.Minor(rdev)) == minor {
+			return fields[2], nil
+		}
+	}
+
+	return "nil", errors.New("no matching record found")
+}
+
+const (
+	diskstatMatchNone = iota
+	diskstatMatchMultiple
+	diskstatMatchSingle
+)
+
+func (p *Plugin) matchDiskstatFields(name string, rdev uint64, fields []string) (match int, err error) {
+	if name == "" {
+		return diskstatMatchMultiple, nil
+	}
+	if name == fields[2] {
+		return diskstatMatchSingle, nil
+	}
+	if rdev != math.MaxUint64 {
+		var major, minor uint64
+		if major, err = strconv.ParseUint(fields[0], 10, 32); err != nil {
+			return
+		}
+		if minor, err = strconv.ParseUint(fields[1], 10, 32); err != nil {
+			return
+		}
+		if uint64(unix.Major(rdev)) == major && uint64(unix.Minor(rdev)) == minor {
+			return diskstatMatchMultiple, nil
+		}
+	}
+	return diskstatMatchNone, nil
+}
+
+func (p *Plugin) scanDeviceStats(name string, buf *bytes.Buffer) (devstats *devStats, err error) {
+	rdev := uint64(math.MaxUint64)
+	if name != "" {
+		if !strings.HasPrefix(name, devLocation) {
+			name = devLocation + name
+		}
+		var stat os.FileInfo
+		if stat, err = os.Stat(name); err == nil {
+			rdev = stat.Sys().(*syscall.Stat_t).Rdev
+		}
+	}
+
+	var stats devStats
+	scanner := bufio.NewScanner(buf)
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		if len(fields) < 7 {
+			return nil, fmt.Errorf("unexpected %s file format", diskstatLocation)
+		}
+		var match int
+		if match, err = p.matchDiskstatFields(name, rdev, fields); err != nil {
+			return
+		}
+		var rxop, rxsec, txop, txsec int
+		if match != diskstatMatchNone {
+			if len(fields) >= 14 {
+				rxop, rxsec, txop, txsec = 3, 5, 7, 9
+			} else {
+				rxop, rxsec, txop, txsec = 3, 4, 5, 6
+			}
+			var n uint64
+			if n, err = strconv.ParseUint(fields[rxop], 10, 64); err != nil {
+				return
+			}
+			stats.rx.operations += n
+
+			if n, err = strconv.ParseUint(fields[rxsec], 10, 64); err != nil {
+				return
+			}
+			stats.rx.sectors += n
+
+			if n, err = strconv.ParseUint(fields[txop], 10, 64); err != nil {
+				return
+			}
+			stats.tx.operations += n
+
+			if n, err = strconv.ParseUint(fields[txsec], 10, 64); err != nil {
+				return
+			}
+			stats.tx.sectors += n
+
+			devstats = &stats
+			if match == diskstatMatchSingle {
+				return
+			}
+		}
+	}
+	return
+}
+
+func (p *Plugin) getDeviceStats(name string) (stats *devStats, err error) {
+	var file *os.File
+	if file, err = os.Open(diskstatLocation); err != nil {
+		return
+	}
+	var buf bytes.Buffer
+	_, err = buf.ReadFrom(file)
+	file.Close()
+	if err != nil {
+		return
+	}
+	return p.scanDeviceStats(name, &buf)
 }
