c82d9e8314fa5ef4d95fbad86081e1e8d956450a
Michael Veksler <Mihails.Vekslers@zabbix.com>
2019-08-13 10:43:57 +0300

    .......... [DEV-1254] c calls 'web.page.*'

--- go/src/zabbix/pkg/zbxlib/checks.go
+++ go/src/zabbix/pkg/zbxlib/checks.go
@@ -35,6 +35,11 @@ int	NET_DNS_RECORD(AGENT_REQUEST *request, AGENT_RESULT *result);
 int	PROC_MEM(AGENT_REQUEST *request, AGENT_RESULT *result);
 int	PROC_NUM(AGENT_REQUEST *request, AGENT_RESULT *result);
 int	SYSTEM_BOOTTIME(AGENT_REQUEST *request, AGENT_RESULT *result);
+int	WEB_PAGE_GET(AGENT_REQUEST *request, AGENT_RESULT *result);
+int	WEB_PAGE_PERF(AGENT_REQUEST *request, AGENT_RESULT *result);
+int	WEB_PAGE_REGEXP(AGENT_REQUEST *request, AGENT_RESULT *result);
+
+
 
 static int execute_check(const char *key, zbx_agent_check_t check_func, char **value, char **error)
 {
@@ -96,6 +101,13 @@ func ExecuteCheck(key string, params []string) (result *string, err error) {
 		cfunc = unsafe.Pointer(C.PROC_NUM)
 	case "system.boottime":
 		cfunc = unsafe.Pointer(C.SYSTEM_BOOTTIME)
+	case "web.page.get":
+		cfunc = unsafe.Pointer(C.WEB_PAGE_GET)
+	case "web.page.perf":
+		cfunc = unsafe.Pointer(C.WEB_PAGE_PERF)
+	case "web.page.regexp":
+		cfunc = unsafe.Pointer(C.WEB_PAGE_REGEXP)
+
 	default:
 		return nil, fmt.Errorf("Unsupported metric %s", key)
 	}
