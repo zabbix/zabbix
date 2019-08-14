e2133c9e045e0a28c76831503d2aaad23da320de
Michael Veksler <Mihails.Vekslers@zabbix.com>
2019-08-14 11:42:39 +0300

    .......... [DEV-1254] c call 'system.sw.packages'

--- go/src/zabbix/pkg/zbxlib/checks.go
+++ go/src/zabbix/pkg/zbxlib/checks.go
@@ -53,6 +53,7 @@ int	SYSTEM_HW_CPU(AGENT_REQUEST *request, AGENT_RESULT *result);
 int	SYSTEM_HW_DEVICES(AGENT_REQUEST *request, AGENT_RESULT *result);
 int	SYSTEM_HW_MACADDR(AGENT_REQUEST *request, AGENT_RESULT *result);
 int	SYSTEM_SW_OS(AGENT_REQUEST *request, AGENT_RESULT *result);
+int	SYSTEM_SW_PACKAGES(AGENT_REQUEST *request, AGENT_RESULT *result);
 
 static int execute_check(const char *key, zbx_agent_check_t check_func, char **value, char **error)
 {
@@ -150,6 +151,8 @@ func ExecuteCheck(key string, params []string) (result *string, err error) {
 		cfunc = unsafe.Pointer(C.SYSTEM_HW_MACADDR)
 	case "system.sw.os":
 		cfunc = unsafe.Pointer(C.SYSTEM_SW_OS)
+	case "system.sw.packages":
+		cfunc = unsafe.Pointer(C.SYSTEM_SW_PACKAGES)
 
 	default:
 		return nil, fmt.Errorf("Unsupported metric %s", key)
