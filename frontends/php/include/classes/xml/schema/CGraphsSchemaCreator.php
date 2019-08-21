f4f4a954048b3c8fbe87997bb5df831d560d1dfd
Miks Kronkalns <miks.kronkalns@zabbix.com>
2019-08-21 15:43:33 +0300

    ..F....... [ZBXNEXT-5228] fixed validation in graph schema export handlers; fixed coding style

--- frontends/php/include/classes/xml/schema/CGraphsSchemaCreator.php
+++ frontends/php/include/classes/xml/schema/CGraphsSchemaCreator.php
@@ -97,17 +97,13 @@ class CGraphsSchemaCreator implements CSchemaCreator {
 						(new CStringXmlTag('ymax_item_1'))
 							->setDefaultValue('0')
 							->setExportHandler(function(array $data, CXmlTagInterface $class) {
-								if ($data['ymax_type_1'] == 2) {
-									if (array_key_exists('ymax_item_1', $data)) {
-										if (!array_key_exists('host', $data['ymax_item_1']) &&
-											!array_key_exists('key', $data['ymax_item_1'])) {
-											throw new Exception(
-												_s('Invalid tag "%1$s": %2$s.',
-													'/zabbix_export/graphs/graph/ymax_item_1',
-													_('an array is expected'))
-											);
-										}
-									}
+								if ($data['ymax_type_1'] == CXmlConstantValue::ITEM
+										&& array_key_exists('ymax_item_1', $data)
+										&& (!array_key_exists('host', $data['ymax_item_1'])
+											|| !array_key_exists('key', $data['ymax_item_1']))) {
+									throw new Exception(_s('Invalid tag "%1$s": %2$s.',
+										'/zabbix_export/graphs/graph/ymax_item_1', _('an array is expected')
+									));
 								}
 
 								return $data['ymax_item_1'];
@@ -120,17 +116,13 @@ class CGraphsSchemaCreator implements CSchemaCreator {
 						(new CStringXmlTag('ymin_item_1'))
 							->setDefaultValue('0')
 							->setExportHandler(function(array $data, CXmlTagInterface $class) {
-								if ($data['ymin_type_1'] == 2) {
-									if (array_key_exists('ymin_item_1', $data)) {
-										if (!array_key_exists('host', $data['ymin_item_1']) &&
-											!array_key_exists('key', $data['ymin_item_1'])) {
-											throw new Exception(
-												_s('Invalid tag "%1$s": %2$s.',
-													'/zabbix_export/graphs/graph/ymin_item_1',
-													_('an array is expected'))
-											);
-										}
-									}
+								if ($data['ymin_type_1'] == CXmlConstantValue::ITEM
+										&& array_key_exists('ymin_item_1', $data)
+										&& (!array_key_exists('host', $data['ymin_item_1'])
+											|| !array_key_exists('key', $data['ymin_item_1']))) {
+									throw new Exception(_s('Invalid tag "%1$s": %2$s.',
+										'/zabbix_export/graphs/graph/ymin_item_1', _('an array is expected')
+									));
 								}
 
 								return $data['ymax_item_1'];
@@ -140,7 +132,6 @@ class CGraphsSchemaCreator implements CSchemaCreator {
 							->addConstant(CXmlConstantName::CALCULATED, CXmlConstantValue::CALCULATED)
 							->addConstant(CXmlConstantName::FIXED, CXmlConstantValue::FIXED)
 							->addConstant(CXmlConstantName::ITEM, CXmlConstantValue::ITEM)
-
 					)
 			);
 	}
