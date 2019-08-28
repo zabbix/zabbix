04b3e55cd71c8df62f618bd5c2247c61925baae5
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2019-08-28 12:40:28 +0300

    .......... [DEV-1119] overriden constructor for an sql query due to syntax-check error

--- frontends/php/tests/selenium/dashboard/testHostAvailabilityWidget.php
+++ frontends/php/tests/selenium/dashboard/testHostAvailabilityWidget.php
@@ -30,13 +30,21 @@ class testHostAvailabilityWidget extends CWebTest {
 	 * SQL query to get widget and widget_field tables to compare hash values, but without widget_fieldid
 	 * because it can change.
 	 */
-	private $sql = 'SELECT wf.widgetid, wf.type, wf.name, wf.value_int, wf.value_str, wf.value_groupid, wf.value_hostid,'.
+	private $sql;
+
+	/**
+	 * Overriden constructor.
+	 */
+	public function __construct($name = null, array $data = [], $data_name = '') {
+		parent::__construct($name, $data, $data_name);
+
+		$this->sql = 'SELECT wf.widgetid, wf.type, wf.name, wf.value_int, wf.value_str, wf.value_groupid, wf.value_hostid,'.
 			' wf.value_itemid, wf.value_graphid, wf.value_sysmapid, w.widgetid, w.dashboardid, w.type, w.name, w.x, w.y,'.
 			' w.width, w.height'.
 			' FROM widget_field wf'.
 			' INNER JOIN widget w'.
 			' ON w.widgetid=wf.widgetid ORDER BY wf.widgetid, wf.name';
-
+	}
 
 	public static function getCreateWidgetData() {
 		return [
