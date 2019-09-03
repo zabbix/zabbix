7a5fd4c669289edecfc845f5db90b070eb266f54
Andrejs Kozlovs <andrejs.kozlovs@zabbix.com>
2019-09-03 15:49:28 +0300

    .......PS. [ZBX-16230] fixed coding style
    * commit 'af6ecf8e321ee624c0c55837a8dcd83928effe80':
      .......PS. [ZBX-16230] code style fixes
      .......PS. [ZBX-16230] fixed NULL pointer dereferencing
    
    (cherry picked from commit 6416c2653adecc5701d26a3a05202d9958903639)
    (cherry picked from commit 463485465cf29ea035e94e818d046b59676b5c5a)

--- src/libs/zbxcommon/misc.c
+++ src/libs/zbxcommon/misc.c
@@ -2241,18 +2241,21 @@ int	calculate_item_nextcheck_unreachable(int simple_interval, const zbx_custom_i
 	nextcheck = disable_until;
 	tmax = disable_until + SEC_PER_YEAR;
 
-	while (nextcheck < tmax)
+	if (NULL != custom_intervals)
 	{
-		if (0 != get_current_delay(simple_interval, custom_intervals->flexible, nextcheck))
-			break;
-
-		/* find the flexible interval change */
-		if (FAIL == get_next_delay_interval(custom_intervals->flexible, nextcheck, &next_interval))
+		while (nextcheck < tmax)
 		{
-			nextcheck = ZBX_JAN_2038;
-			break;
+			if (0 != get_current_delay(simple_interval, custom_intervals->flexible, nextcheck))
+				break;
+
+			/* find the flexible interval change */
+			if (FAIL == get_next_delay_interval(custom_intervals->flexible, nextcheck, &next_interval))
+			{
+				nextcheck = ZBX_JAN_2038;
+				break;
+			}
+			nextcheck = next_interval;
 		}
-		nextcheck = next_interval;
 	}
 
 	if (0 != scheduled_check && scheduled_check < nextcheck)
