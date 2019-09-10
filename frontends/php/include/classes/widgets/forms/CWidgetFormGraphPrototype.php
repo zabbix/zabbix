e90a0d0f22c406b4b29f55fc6516595ddc863082
Andrejs Verza <andrejs.verza@zabbix.com>
2019-09-10 11:02:03 +0300

    ..F....... [ZBXNEXT-5252] fixed translation of simple graph prototype

--- frontends/php/include/classes/widgets/forms/CWidgetFormGraphPrototype.php
+++ frontends/php/include/classes/widgets/forms/CWidgetFormGraphPrototype.php
@@ -45,7 +45,7 @@ class CWidgetFormGraphPrototype extends CWidgetForm {
 		if (array_key_exists('source_type', $this->data)
 				&& $this->data['source_type'] == ZBX_WIDGET_FIELD_RESOURCE_SIMPLE_GRAPH_PROTOTYPE) {
 			// Select simple graph prototype field.
-			$field_item_prototype = (new CWidgetFieldMultiselectItemPrototype('itemid', _('Simple graph prototype')))
+			$field_item_prototype = (new CWidgetFieldMultiselectItemPrototype('itemid', _('Item prototype')))
 				->setFlags(CWidgetField::FLAG_NOT_EMPTY | CWidgetField::FLAG_LABEL_ASTERISK)
 				->setMultiple(false)
 				->setFilterParameter('numeric', true) // For filtering item prototypes.
