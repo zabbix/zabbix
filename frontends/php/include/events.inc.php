033bf6f2839d7c8ef1c33f85031e75629a2548c2
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2019-09-20 17:04:15 +0300

    ..F....... [ZBXNEXT-5404] made URLs used in the description popup clickable

--- frontends/php/include/events.inc.php
+++ frontends/php/include/events.inc.php
@@ -241,7 +241,7 @@ function make_event_details($event, $backurl) {
 
 	$table
 		->addRow([_('Tags'), $tags[$event['eventid']]])
-		->addRow([_('Description'), zbx_nl2br($event['comments'])]);
+		->addRow([_('Description'), zbx_str2links($event['comments'])]);
 
 	return $table;
 }
