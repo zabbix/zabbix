765bb5ee85b79c721a1f5fdf9363a68020968060
Gregory Chalenko <gregory.chalenko@zabbix.com>
2019-09-23 19:25:01 +0300

    ..F....... [ZBXNEXT-5386,ZBXNEXT-5387] macro parser changes draft

--- frontends/php/include/classes/parsers/CMacroParser.php
+++ frontends/php/include/classes/parsers/CMacroParser.php
@@ -40,15 +40,21 @@ class CMacroParser extends CParser {
 	 */
 	private $set_parser;
 
+	private $unquoted_suffix;
+
 	/**
 	 * An options array.
 	 *
 	 * Supported options:
 	 *   'allow_reference' => true		support of reference {MACRO<1-9>}
+	 *   'allow_quoted_suffix' => true  support of quoted last part of macro {EVENT.TAGS."Jira id"}
 	 *
 	 * @var array
 	 */
-	private $options = ['allow_reference' => false];
+	private $options = [
+		'allow_reference' => false,
+		'allow_quoted_suffix' => false
+	];
 
 	/**
 	 * Array of strings to search for.
@@ -57,11 +63,17 @@ class CMacroParser extends CParser {
 	 * @param array $options
 	 */
 	public function __construct(array $macros, array $options = []) {
-		$this->set_parser = new CSetParser(array_map(function($macro) { return substr($macro, 1, -1); }, $macros));
+		$this->needles = [];
 
-		if (array_key_exists('allow_reference', $options)) {
-			$this->options['allow_reference'] = $options['allow_reference'];
+		foreach ($macros as $macro) {
+			$this->needles[] = substr($macro, 1, -1);
 		}
+
+		$this->max_match_len = max(array_map('strlen', $this->needles));
+		$this->min_match_len = min(array_map('strlen', $this->needles));
+		//$this->set_parser = new CSetParser(array_map(function($macro) { return substr($macro, 1, -1); }, $macros));
+
+		$this->options = $options + $this->options;
 	}
 
 	/**
@@ -74,18 +86,44 @@ class CMacroParser extends CParser {
 		$this->match = '';
 		$this->macro = '';
 		$this->n = 0;
-
+		$source_len = strlen($source);
 		$p = $pos;
 
-		if (!isset($source[$p]) || $source[$p] != '{') {
-			return self::PARSE_FAIL;
+		if ($p >= $source_len || $source[$p] != '{') {
+			return CParser::PARSE_FAIL;
 		}
+
 		$p++;
 
-		if ($this->set_parser->parse($source, $p) == self::PARSE_FAIL) {
-			return self::PARSE_FAIL;
+		if ($this->findMatch($source, $p) == CParser::PARSE_FAIL) {
+			return CParser::PARSE_FAIL;
 		}
-		$p += $this->set_parser->getLength();
+
+		$p += strlen($this->macro);
+
+		if ($this->options['allow_quoted_suffix'] && $p < $source_len && $source[$p] == '"') {
+			$p++;
+
+			if ($this->findSuffix($source, $p) == CParser::PARSE_FAIL) {
+				$this->macro = '';
+
+				return CParser::PARSE_FAIL;
+			}
+
+			$p += strlen($this->unquoted_suffix) + 2;
+
+			if ($p >= $source_len || $source[$p] !== '"') {
+				$this->macro = '';
+				$this->unquoted_suffix = '';
+
+				return CParser::PARSE_FAIL;
+			}
+		}
+
+		// if ($this->set_parser->parse($source, $p) == self::PARSE_FAIL) {
+		// 	return self::PARSE_FAIL;
+		// }
+		// $p += $this->set_parser->getLength();
 
 		if ($this->options['allow_reference']) {
 			if (isset($source[$p]) && $source[$p] >= '1' && $source[$p] <= '9') {
@@ -97,15 +135,54 @@ class CMacroParser extends CParser {
 		if (!isset($source[$p]) || $source[$p] != '}') {
 			$this->n = 0;
 
-			return self::PARSE_FAIL;
+			return CParser::PARSE_FAIL;
 		}
 		$p++;
 
 		$this->length = $p - $pos;
 		$this->match = substr($source, $pos, $this->length);
-		$this->macro = $this->set_parser->getMatch();
+		// $this->macro = $this->set_parser->getMatch();
 
-		return (isset($source[$pos + $this->length]) ? self::PARSE_SUCCESS_CONT : self::PARSE_SUCCESS);
+		return $p < $source_len ? CParser::PARSE_SUCCESS_CONT : CParser::PARSE_SUCCESS;
+	}
+
+	protected function findSuffix($source, $p) {
+		$escaped = false;
+		$source_len = strlen($source);
+		$pos = $p;
+
+		while ($p < $source_len && ($escaped || preg_match('/[0-9A-Z_ ]/', $source[$p]) == 1)) {
+			if ($escaped || $source[$p] == '\\') {
+				$escaped = !$escaped;
+			}
+
+			$p++;
+		}
+
+		if ($escaped) {
+			return CParser::PARSE_FAIL;
+		}
+
+		$this->unquoted_suffix = substr($source, $p, $p - $pos - 1);
+		return CParser::PARSE_SUCCESS;
+	}
+
+	protected function findMatch($source, $p) {
+		$len = $this->max_match_len;
+
+		while ($len >= $this->min_match_len) {
+			$needle = substr($source[$p], $len);
+
+			if (in_array($needle, $this->needles)) {
+				$this->macro = $needle;
+
+				return CParser::PARSE_SUCCESS;
+			}
+
+			$len--;
+		}
+
+		return CParser::PARSE_FAIL;
 	}
 
 	/**
