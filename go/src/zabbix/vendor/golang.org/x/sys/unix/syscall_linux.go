a2fa5f9e1b78d7d822394ae3513a923b47cf2440
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2019-09-25 14:29:29 +0300

    .......... [ZBX-16481] Spelling issues in the code for master v6

--- go/src/zabbix/vendor/golang.org/x/sys/unix/syscall_linux.go
+++ go/src/zabbix/vendor/golang.org/x/sys/unix/syscall_linux.go
@@ -55,7 +55,7 @@ func FanotifyMark(fd int, flags uint, mask uint64, dirFd int, pathname string) (
 //sys	fchmodat(dirfd int, path string, mode uint32) (err error)
 
 func Fchmodat(dirfd int, path string, mode uint32, flags int) (err error) {
-	// Linux fchmodat doesn't support the flags parameter. Mimic glibc's behavior
+	// Linux fchmodat doesn't support the flags parameter. Mimick glibc's behavior
 	// and check the flags. Otherwise the mode would be applied to the symlink
 	// destination which is not what the user expects.
 	if flags&^AT_SYMLINK_NOFOLLOW != 0 {
