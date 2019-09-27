fc0bc233f86fb4c10f781316068a8caf15f3464f
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2019-09-27 14:07:44 +0300

    ........ST [ZBX-16671] moved macro substitutes closer to strdup

--- src/zabbix_server/httppoller/httptest.c
+++ src/zabbix_server/httppoller/httptest.c
@@ -730,12 +730,17 @@ static void	process_httptest(DC_HOST *host, zbx_httptest_t *httptest)
 		db_httpstep.name = row[2];
 
 		db_httpstep.url = zbx_strdup(NULL, row[3]);
-		db_httpstep.required = zbx_strdup(NULL, row[6]);
-		db_httpstep.status_codes = zbx_strdup(NULL, row[7]);
-
 		substitute_simple_macros(NULL, NULL, NULL, NULL, NULL, host, NULL, NULL, NULL,
 				&db_httpstep.url, MACRO_TYPE_HTTPTEST_FIELD, NULL, 0);
 		http_substitute_variables(httptest, &db_httpstep.url);
+
+		db_httpstep.required = zbx_strdup(NULL, row[6]);
+		substitute_simple_macros(NULL, NULL, NULL, NULL, NULL, host, NULL, NULL, NULL,
+				&db_httpstep.required, MACRO_TYPE_HTTPTEST_FIELD, NULL, 0);
+
+		db_httpstep.status_codes = zbx_strdup(NULL, row[7]);
+		substitute_simple_macros(NULL, NULL, NULL, NULL, &host->hostid, NULL, NULL, NULL, NULL,
+				&db_httpstep.status_codes, MACRO_TYPE_COMMON, NULL, 0);
 
 		db_httpstep.post_type = atoi(row[8]);
 
@@ -769,12 +774,6 @@ static void	process_httptest(DC_HOST *host, zbx_httptest_t *httptest)
 			err_str = zbx_dsprintf(err_str, "timeout \"%s\" exceeds 1 hour limit", buffer);
 			goto httpstep_error;
 		}
-
-		substitute_simple_macros(NULL, NULL, NULL, NULL, NULL, host, NULL, NULL, NULL,
-				&db_httpstep.required, MACRO_TYPE_HTTPTEST_FIELD, NULL, 0);
-
-		substitute_simple_macros(NULL, NULL, NULL, NULL, &host->hostid, NULL, NULL, NULL, NULL,
-				&db_httpstep.status_codes, MACRO_TYPE_COMMON, NULL, 0);
 
 		db_httpstep.follow_redirects = atoi(row[9]);
 		db_httpstep.retrieve_mode = atoi(row[10]);
