42010e60fd7b7e4ff461d2ac595f12749d1e7585
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2019-09-29 18:41:58 +0300

    ..F....... [ZBXNEXT-5386,ZBXNEXT-5387] fixed "Undefined index: params in app/controllers/CControllerPopupMediatypeTestEdit.php:81"

--- frontends/php/app/controllers/CControllerPopupMediatypeTestEdit.php
+++ frontends/php/app/controllers/CControllerPopupMediatypeTestEdit.php
@@ -48,7 +48,7 @@ class CControllerPopupMediatypeTestEdit extends CController {
 
 	protected function doAction() {
 		$mediatype = API::MediaType()->get([
-			'output' => ['type', 'status', 'params'],
+			'output' => ['type', 'status', 'parameters'],
 			'mediatypeids' => $this->getInput('mediatypeid'),
 		]);
 
@@ -78,7 +78,7 @@ class CControllerPopupMediatypeTestEdit extends CController {
 			'sendto' => '',
 			'subject' => _('Test subject'),
 			'message' => _('This is the test message from Zabbix'),
-			'parameters' => $mediatype[0]['params'],
+			'parameters' => $mediatype[0]['parameters'],
 			'type' => $mediatype[0]['type'],
 			'enabled' => ($mediatype[0]['status'] == MEDIA_STATUS_ACTIVE),
 			'user' => ['debug_mode' => $this->getDebugMode()]
