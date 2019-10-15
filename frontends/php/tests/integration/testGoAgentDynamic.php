163c41f2276be0d68cb0427187aab9353264a43d
Andrejs Tumilovics <andrejs.tumilovics@zabbix.com>
2019-10-15 12:51:41 +0300

    .......... [DEV-1253,DEV-1317] fixed tresholds and done some code cleanup

--- frontends/php/tests/integration/testGoAgentDynamic.php
+++ frontends/php/tests/integration/testGoAgentDynamic.php
@@ -19,6 +19,9 @@
  */
 require_once dirname(__FILE__) . '/../include/CIntegrationTest.php';
 
+define('COMPARE_AVERAGE', 0);
+define('COMPARE_LAST', 1);
+
 /**
  * Test suite for angent2 (GO agent) dynamic metric collection.
  *
@@ -34,24 +37,135 @@ class testDataCollection extends CIntegrationTest
 	// List of items to match
 	// Item name, key, type, data type, treshold, compare average values (or last)
 	private static $items = [
-		['CPU utilization', 'system.cpu.util[,,avg1]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_FLOAT, 100.0, false], // Result is unstable (large difference)
-		['CPU load', 'system.cpu.load[,avg1]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_FLOAT, 100.0, false],
-		['VFS read', 'vfs.dev.read[,operations]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_UINT64, 100, false],
-		['VFS write', 'vfs.dev.write[,operations]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_UINT64, 100, false],
-		['Proc CPU util', 'proc.cpu.util[,,,,avg1]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_FLOAT, 5.0, true],
-		['Swap in', 'system.swap.in[,pages]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_UINT64, 100, false],
-		['Swap out', 'system.swap.out[,pages]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_UINT64, 100, false],
-		['Proc mem', 'proc.mem[,root]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_UINT64, 100000000, false],
-		['Web page', 'web.page.perf[https://www.zabbix.com]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_FLOAT, 10.0, false],
-		['Net TCP', 'net.tcp.service.perf[ssh]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_FLOAT, 0.05, false],
-		['Net UDP', 'net.udp.service.perf[ntp]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_FLOAT, 0.05, false],
-		['Swap', 'system.swap.size[,pfree]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_UINT64, 100, false],
-		['Vfs indoe', 'vfs.fs.inode[/,pfree]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_UINT64, 100, false],
-		['Vfs size', 'vfs.fs.size[/tmp,free]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_UINT64, 100, false],
-		['Memory', 'vm.memory.size[free]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_UINT64, 1000000, false]
-//		['System run', 'system.swap.size[,pfree]', ITEM_TYPE_ZABBIX, ITEM_VALUE_TYPE_TEXT] // Agent2 doesn't fill this value
-//		sensor
-//		zabbix.stats
+		[
+			'name' => 'CPU utilization',
+			'key' => 'system.cpu.util[,,avg1]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_FLOAT,
+			'treshold' => 100.0,
+			'compareType' => COMPARE_LAST
+		],
+		[
+			'name' => 'CPU load',
+			'key' => 'system.cpu.load[,avg1]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_FLOAT,
+			'treshold' => 100.0,
+			'compareType' => COMPARE_LAST
+		],
+		[
+			'name' => 'VFS read',
+			'key' => 'vfs.dev.read[,operations]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_UINT64,
+			'treshold' => 500,
+			'compareType' => COMPARE_LAST
+		],
+		[
+			'name' => 'VFS write',
+			'key' => 'vfs.dev.write[,operations]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_UINT64,
+			'treshold' => 500,
+			'compareType' => COMPARE_LAST
+		],
+		[
+			'name' => 'Proc CPU util',
+			'key' => 'proc.cpu.util[,,,,avg1]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_FLOAT,
+			'treshold' => 5.0,
+			'compareType' => COMPARE_AVERAGE
+		],
+		[
+			'name' => 'Swap in',
+			'key' => 'system.swap.in[,pages]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_UINT64,
+			'treshold' => 100,
+			'compareType' => COMPARE_LAST
+		],
+		[
+			'name' => 'Swap out',
+			'key' => 'system.swap.out[,pages]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_UINT64,
+			'treshold' => 100,
+			'compareType' => COMPARE_LAST
+		],
+		[
+			'name' => 'Proc mem',
+			'key' => 'proc.mem[,root]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_UINT64,
+			'treshold' => 100000000,
+			'compareType' => COMPARE_LAST
+		],
+		[
+			'name' => 'Web page',
+			'key' => 'web.page.perf[http://localhost]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_FLOAT,
+			'treshold' => 10.0,
+			'compareType' => COMPARE_AVERAGE
+		],
+		[
+			'name' => 'Net TCP',
+			'key' => 'net.tcp.service.perf[ssh]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_FLOAT,
+			'treshold' => 0.05,
+			'compareType' => COMPARE_LAST
+		],
+		[
+			'name' => 'Net UDP',
+			'key' => 'net.udp.service.perf[ntp]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_FLOAT,
+			'treshold' => 0.05,
+			'compareType' => COMPARE_LAST
+		],
+		[
+			'name' => 'Swap',
+			'key' => 'system.swap.size[,pfree]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_UINT64,
+			'treshold' => 100,
+			'compareType' => COMPARE_LAST
+		],
+		[
+			'name' => 'Vfs inode',
+			'key' => 'vfs.fs.inode[/,pfree]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_FLOAT,
+			'treshold' => 0.1,
+			'compareType' => COMPARE_LAST
+		],
+		[
+			'name' => 'Vfs size',
+			'key' => 'vfs.fs.size[/tmp,free]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_UINT64,
+			'treshold' => 16384,
+			'compareType' => COMPARE_LAST
+		],
+		[
+			'name' => 'Memory',
+			'key' => 'vm.memory.size[free]',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_UINT64,
+			'treshold' => 1000000,
+			'compareType' => COMPARE_LAST
+		],
+		[	// Should be treated as a special case, since this metric returns JSON object.
+			// Maybe, it should e pulled to separate test suite. At this point we just compare it as string.
+			'name' => 'Stats',
+			'key' => 'zabbix.stats',
+			'type' => ITEM_TYPE_ZABBIX,
+			'valueType' => ITEM_VALUE_TYPE_TEXT,
+			'treshold' => 100,
+			'compareType' => COMPARE_LAST
+		]
 	];
 
 	/**
@@ -125,27 +239,25 @@ class testDataCollection extends CIntegrationTest
 		foreach (self::$items as $i) {
 			array_push($new_items, [
 				'hostid' => self::$hostids[self::$agent_name],
-				'name' => $i[0],
-				'key_' => $i[1],
-				'type' => $i[2],
-				'value_type' => $i[3],
+				'name' => $i['name'],
+				'key_' => $i['key'],
+				'type' => $i['type'],
+				'value_type' => $i['valueType'],
 				'delay' => '1s',
 				'interfaceid' => $interfaceids[self::$agent_name]
 			],
 			[
 				'hostid' => self::$hostids[self::$go_agent_name],
-				'name' => $i[0],
-				'key_' => $i[1],
-				'type' => $i[2],
-				'value_type' => $i[3],
+				'name' => $i['name'],
+				'key_' => $i['key'],
+				'type' => $i['type'],
+				'value_type' => $i['valueType'],
 				'delay' => '1s',
 				'interfaceid' => $interfaceids[self::$go_agent_name]
 			]);
 		}
 
-// 		echo "\nitem.create request:".json_encode($new_items)."\n";
 		$response = $this->call('item.create', $new_items);
-// 		echo "\nitem.create response:".json_encode($response)."\n";
 
 		$this->assertArrayHasKey('itemids', $response['result']);
 		$this->assertEquals(count($new_items), count($response['result']['itemids']));
@@ -153,11 +265,10 @@ class testDataCollection extends CIntegrationTest
 		// Get item IDs
 		self::$itemids = [];
 		foreach (self::$items as $index => $value) {
-			$name = $value[1];
+			$name = $value['key'];
 			self::$itemids[self::$agent_name.":".$name] = $response['result']['itemids'][$index*2];
 			self::$itemids[self::$go_agent_name.":".$name] = $response['result']['itemids'][($index*2)+1];
 		}
-// 		echo "\n===>:".json_encode(self::$itemids)."\n";
 
 		return true;
 	}
@@ -178,12 +289,12 @@ class testDataCollection extends CIntegrationTest
 			self::COMPONENT_AGENT => [
 				'Hostname' => self::$agent_name,
 				'ServerActive' => '127.0.0.1',
-				'EnableRemoteCommands' => '1'
+				'DebugLevel' => '5'
 			],
 			self::COMPONENT_AGENT2 => [
 				'Hostname' => self::$go_agent_name,
 				'ServerActive' => '127.0.0.1',
-				'EnableRemoteCommands' => '1'
+				'Plugins.Uptime.Capacity'	=> '10'
 			]
 		];
 	}
@@ -196,7 +307,6 @@ class testDataCollection extends CIntegrationTest
 	 * @hosts agent, go_agent
 	 */
 	public function testDynamicMetricCollection() {
-		echo "\ntestDynamicMetricCollection\n";
 		$this->waitForLogLineToBePresent(self::COMPONENT_SERVER, [
 			'enabling Zabbix agent checks on host "'.self::$agent_name.'": host became available',
 			'enabling Zabbix agent checks on host "'.self::$go_agent_name.'": host became available'
@@ -204,30 +314,33 @@ class testDataCollection extends CIntegrationTest
 
 		// Go over all values and compare them
 		foreach (self::$items as $i) {
-			echo "= ".$i[0]." =\n";
-			$name = $i[1];
-			$type = $i[3];
 			$agent_data = $this->call('history.get', [
-				'itemids'	=> self::$itemids[self::$agent_name.":".$name],
-				'history'	=> $type
+				'itemids'	=> self::$itemids[self::$agent_name.":".$i['key']],
+				'history'	=> $i['valueType']
 			]);
 			$agent2_data = $this->call('history.get', [
-				'itemids'	=> self::$itemids[self::$go_agent_name.":".$name],
-				'history'	=> $type
+				'itemids'	=> self::$itemids[self::$go_agent_name.":".$i['key']],
+				'history'	=> $i['valueType']
 			]);
-			echo "\nAgent data:".json_encode($agent_data)."\n";
-			echo "\nGO Agent data:".json_encode($agent2_data)."\n";
+			$this->assertArrayHasKey('result', $agent_data);
+			$this->assertTrue(0 < sizeof($agent_data['result']), "No metrics");
+			$this->assertArrayHasKey('result', $agent2_data);
+			$this->assertTrue(0 < sizeof($agent2_data['result']), "No metrics");
 			$a = end($agent_data['result'])['value'];
 			$b = end($agent2_data['result'])['value'];
-			switch ($type)
+			switch ($i['valueType'])
 			{
 				case ITEM_VALUE_TYPE_TEXT:
-					echo "\n> A:".$a." B:".$b."\n";
-					$this->assertEquals($value[$field], $item[$field]);
+					if (0 != $i['treshold'])
+					{
+						$a = substr($a, 0, $i['treshold']);
+						$b = substr($b, 0, $i['treshold']);
+					}
+					$this->assertEquals($a, $b, "Strings do not match for ".$i['name']);
 					break;
 				case ITEM_VALUE_TYPE_FLOAT:
 				case ITEM_VALUE_TYPE_UINT64:
-					if ($i[5])
+					if ($i['compareType'] == COMPARE_AVERAGE)
 					{
 						// Calculate average of all samples
 						$a = 0.0;
@@ -236,19 +349,25 @@ class testDataCollection extends CIntegrationTest
 							$records++;
 							$a += $e['value'];
 						}
-						$a /= $records;
+						if ($records > 1)
+						{
+							$a /= $records;
+						}
 						$b = 0.0;
 						$records = 0;
 						foreach ($agent2_data['result'] as $e) {
 							$records++;
 							$b += $e['value'];
 						}
-						$b /= $records;
+						if ($records > 1)
+						{
+							$b /= $records;
+						}
 					}
 					$diff = abs(abs($a) - abs($b));
-					$treshold = $i[4];
-					echo "\n> A:".$a." B:".$b." Diff:".$diff."\n";
-					$this->assertTrue($diff < $treshold);
+					$this->assertTrue($diff < $i['treshold'],
+						"Difference for ".$i['name']." is more than defined treshold "
+						.$diff." > ".$i['treshold']);
 					break;
 			}
 		}
