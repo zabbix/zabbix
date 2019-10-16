7f65c1f6cab822b7a65945e80c9e84111ba8c290
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2019-10-16 11:04:44 +0300

    ..F....... [ZBXNEXT-5486] moved trigger display options to MVC

--- frontends/php/app/controllers/CControllerTrigDisplayEdit.php
+++ frontends/php/app/controllers/CControllerTrigDisplayEdit.php
@@ -27,7 +27,17 @@ class CControllerTrigDisplayEdit extends CController {
 
 	protected function checkInput() {
 		$fields = [
-			'demo' => ''
+			'custom_color'        => 'int32',
+			'problem_unack_color' => 'string',
+			'problem_ack_color'   => 'string',
+			'ok_unack_color'      => 'string',
+			'ok_ack_color'        => 'string',
+			'problem_unack_style' => 'int32',
+			'problem_ack_style'   => 'int32',
+			'ok_unack_style'      => 'int32',
+			'ok_ack_style'        => 'int32',
+			'ok_period'           => 'string',
+			'blink_period'        => 'string'
 		];
 
 		$ret = $this->validateInput($fields);
@@ -47,21 +57,22 @@ class CControllerTrigDisplayEdit extends CController {
 		$config = select_config();
 
 		$data = [
-			'custom_color' => $config['custom_color'],
-			'problem_unack_color' => $config['problem_unack_color'],
-			'problem_ack_color' => $config['problem_ack_color'],
-			'ok_unack_color' => $config['ok_unack_color'],
-			'ok_ack_color' => $config['ok_ack_color'],
-			'problem_unack_style' => $config['problem_unack_style'],
-			'problem_ack_style' => $config['problem_ack_style'],
-			'ok_unack_style' => $config['ok_unack_style'],
-			'ok_ack_style' => $config['ok_ack_style'],
-			'ok_period' => $config['ok_period'],
-			'blink_period' => $config['blink_period']
+			'custom_color'        => $this->getInput('custom_color',        $config['custom_color']),
+			'problem_unack_color' => $this->getInput('problem_unack_color', $config['problem_unack_color']),
+			'problem_ack_color'   => $this->getInput('problem_ack_color',   $config['problem_ack_color']),
+			'ok_unack_color'      => $this->getInput('ok_unack_color',      $config['ok_unack_color']),
+			'ok_ack_color'        => $this->getInput('ok_ack_color',        $config['ok_ack_color']),
+			'problem_unack_style' => $this->getInput('problem_unack_style', $config['problem_unack_style']),
+			'problem_ack_style'   => $this->getInput('problem_ack_style',   $config['problem_ack_style']),
+			'ok_unack_style'      => $this->getInput('ok_unack_style',      $config['ok_unack_style']),
+			'ok_ack_style'        => $this->getInput('ok_ack_style',        $config['ok_ack_style']),
+			'ok_period'           => $this->getInput('ok_period',           $config['ok_period']),
+			'blink_period'        => $this->getInput('blink_period',        $config['blink_period'])
 		];
 
 		$response = new CControllerResponseData($data);
 		$response->setTitle(_('Configuration of trigger displaying options'));
+
 		$this->setResponse($response);
 	}
 }
--- frontends/php/app/controllers/CControllerTrigDisplayUpdate.php
+++ frontends/php/app/controllers/CControllerTrigDisplayUpdate.php
@@ -21,19 +21,40 @@
 
 class CControllerTrigDisplayUpdate extends CController {
 
-	protected function init() {
-		$this->disableSIDValidation();
-	}
-
 	protected function checkInput() {
 		$fields = [
-			'demo' => ''
+			'custom_color'        => 'int32 | in '.EVENT_CUSTOM_COLOR_DISABLED.','.EVENT_CUSTOM_COLOR_ENABLED,
+			'problem_unack_color' => 'string',
+			'problem_ack_color'   => 'string',
+			'ok_unack_color'      => 'string',
+			'ok_ack_color'        => 'string',
+			'problem_unack_style' => 'int32 | in 0,1',
+			'problem_ack_style'   => 'int32 | in 0,1',
+			'ok_unack_style'      => 'int32 | in 0,1',
+			'ok_ack_style'        => 'int32 | in 0,1',
+			'ok_period'           => 'required | string | not_empty',
+			'blink_period'        => 'required | string | not_empty'
 		];
 
 		$ret = $this->validateInput($fields);
 
 		if (!$ret) {
-			$this->setResponse(new CControllerResponseFatal());
+			switch ($this->getValidationError()) {
+				case self::VALIDATION_ERROR:
+					$response = new CControllerResponseRedirect((new CUrl('zabbix.php'))
+						->setArgument('action', 'trigdisplay.edit')
+					);
+
+					$response->setFormData($this->getInputAll());
+					$response->setMessageError(_('Cannot update configuration'));
+
+					$this->setResponse($response);
+					break;
+
+				case self::VALIDATION_FATAL_ERROR:
+					$this->setResponse(new CControllerResponseFatal());
+					break;
+			}
 		}
 
 		return $ret;
@@ -44,12 +65,58 @@ class CControllerTrigDisplayUpdate extends CController {
 	}
 
 	protected function doAction() {
-		$data = [
-			'demo' => __FILE__
+		$update_values = [
+			'custom_color'        => $this->getInput('custom_color', EVENT_CUSTOM_COLOR_DISABLED),
+			'problem_unack_style' => $this->getInput('problem_unack_style'),
+			'problem_ack_style'   => $this->getInput('problem_ack_style'),
+			'ok_unack_style'      => $this->getInput('ok_unack_style'),
+			'ok_ack_style'        => $this->getInput('ok_ack_style'),
+			'ok_period'           => $this->getInput('ok_period'),
+			'blink_period'        => $this->getInput('blink_period')
 		];
 
-		$response = new CControllerResponseData($data);
-		$response->setTitle(_('CControllerTrigDisplayUpdate'));
+		$response = new CControllerResponseRedirect((new CUrl('zabbix.php'))
+			->setArgument('action', 'trigdisplay.edit')
+		);
+
+		if ($update_values['custom_color'] == EVENT_CUSTOM_COLOR_ENABLED) {
+			$update_values['problem_unack_color'] = $this->getInput('problem_unack_color');
+			$update_values['problem_ack_color']   = $this->getInput('problem_ack_color');
+			$update_values['ok_unack_color']      = $this->getInput('ok_unack_color');
+			$update_values['ok_ack_color']        = $this->getInput('ok_ack_color');
+
+			$color_validator = new CColorValidator();
+
+			$color_caption = [
+				'problem_unack_color' => _('Unacknowledged PROBLEM events'),
+				'problem_ack_color'   => _('Acknowledged PROBLEM events'),
+				'ok_unack_color'      => _('Unacknowledged RESOLVED events'),
+				'ok_ack_color'        => _('Acknowledged RESOLVED events')
+			];
+
+			foreach ($color_caption as $field => $caption) {
+				if (!$color_validator->validate($this->getInput($field))) {
+					error(_s('Colour "%1$s" is not correct: expecting hexadecimal colour code (6 symbols).', $caption));
+					$response->setMessageError(_('Cannot update configuration'));
+					$response->setFormData($this->getInputAll());
+
+					return $this->setResponse($response);
+				}
+			}
+		}
+
+		DBstart();
+		$result = update_config($update_values);
+		$result = DBend($result);
+
+		if ($result) {
+			$response->setMessageOk(_('Configuration updated'));
+		}
+		else {
+			$response->setMessageError(_('Cannot update configuration'));
+			$response->setFormData($this->getInputAll());
+		}
+
 		$this->setResponse($response);
 	}
 }
