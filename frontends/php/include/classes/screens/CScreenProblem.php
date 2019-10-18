1da8ed3df6ec42f78ca88c860f5016ea86164b9d
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2019-10-18 12:17:45 +0300

    ..F....... [ZBXNEXT-4825] implemented in Monitoring -> Problems the button "Export to CSV" from now on exports all pages

--- frontends/php/include/classes/screens/CScreenProblem.php
+++ frontends/php/include/classes/screens/CScreenProblem.php
@@ -779,7 +779,9 @@ class CScreenProblem extends CScreenBase {
 		$data = self::getData($this->data['filter'], $this->config);
 		$data = self::sortData($data, $this->config, $this->data['sort'], $this->data['sortorder']);
 
-		$paging = getPagingLine($data['problems'], ZBX_SORT_UP, clone $url);
+		if ($this->data['action'] === 'problem.view') {
+			$paging = getPagingLine($data['problems'], ZBX_SORT_UP, clone $url);
+		}
 
 		$data = self::makeData($data, $this->data['filter']);
 
