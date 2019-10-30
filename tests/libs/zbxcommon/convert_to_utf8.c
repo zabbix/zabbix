9378f628d45fa0e166c4c54add8262f5b96c4041
Andris Mednis <Andris.Mednis@zabbix.com>
2019-10-30 18:21:49 +0200

    ...G...... [ZBX-16596] fixed zbx_mock_test_entry() for testing convert_to_utf8()

--- tests/libs/zbxcommon/convert_to_utf8.c
+++ tests/libs/zbxcommon/convert_to_utf8.c
@@ -26,30 +26,41 @@
 
 void	zbx_mock_test_entry(void **state)
 {
+	int		expected_result;
+	size_t		in_buffer_length, expected_result_buffer_length;
+	char		*in_buffer, *result_buffer, *expected_result_buffer;
+	const char	*enc;
+
 	ZBX_UNUSED(state);
 
-	int expected_result = zbx_mock_str_to_return_code(zbx_mock_get_parameter_string("out.return"));
-	ssize_t in_buffer_length = zbx_mock_get_parameter_uint64("in.buffer_length");
-	ssize_t	expected_result_buffer_length = zbx_mock_get_parameter_uint64("out.expected_result_buffer_length");
-	char* in_buffer = zbx_yaml_assemble_binary_sequence("in.buffer", in_buffer_length);
-	char* expected_result_buffer =
-		zbx_yaml_assemble_binary_sequence("out.expected_result_buffer", expected_result_buffer_length);
-	const char *enc  = zbx_mock_get_parameter_string("in.enc");
+	expected_result = zbx_mock_str_to_return_code(zbx_mock_get_parameter_string("out.return"));
+	in_buffer_length = zbx_mock_get_parameter_uint64("in.buffer_length");
+	expected_result_buffer_length = zbx_mock_get_parameter_uint64("out.expected_result_buffer_length");
+	in_buffer = zbx_yaml_assemble_binary_sequence("in.buffer", in_buffer_length);
 
-	char* result_buffer = convert_to_utf8(in_buffer, in_buffer_length, enc);
+	expected_result_buffer = zbx_yaml_assemble_binary_sequence("out.expected_result_buffer",
+			expected_result_buffer_length);
+
+	enc  = zbx_mock_get_parameter_string("in.enc");		/* encoding */
+
+	result_buffer = convert_to_utf8(in_buffer, in_buffer_length, enc);
+
+	zbx_free(in_buffer);
 
 	if (0 != memcmp(result_buffer, expected_result_buffer, expected_result_buffer_length))
 	{
+		zbx_free(expected_result_buffer);
+		zbx_free(result_buffer);
+
 		if (SUCCEED == expected_result)
-		{
 			fail_msg("Expected the same result but there are differences");
-		}
 	}
 	else
 	{
+		zbx_free(expected_result_buffer);
+		zbx_free(result_buffer);
+
 		if ( SUCCEED != expected_result)
-		{
-			fail_msg("Expected different result but there are differences");
-		}
+			fail_msg("Expected differences but result is the same");
 	}
 }
