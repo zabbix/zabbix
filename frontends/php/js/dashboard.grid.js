4788e00e7577235dff479d18d372fd411be791d5
Andrejs Verza <andrejs.verza@zabbix.com>
2019-11-12 13:43:02 +0200

    ..F....... [DEV-1340] removed reference to the exact widget types from the dashboard.grid.js
    * commit '8ae399fdd7317c7497c54488550eebd7941ad2e7':
      ..F....... [DEV-1340] fixed too large clock widget in screens
      ..F....... [DEV-1340] fixed clock svg aligning in the middle of the widget across all browsers #2
      ..F....... [DEV-1340] fixed clock svg aligning in the middle of the widget across all browsers
      ..F....... [DEV-1340] fixed coding style
      ..F....... [DEV-1340] removed reference to the exact widget type (clock, sysmap) from the dashboard.grid.js
      ..F....... [DEV-1340] removed reference to the exact widget type (svggraph) from the dashboard.grid.js
    
    (cherry picked from commit 280bc80cf57cbf6ae0572a22f4e29d822542bef3)

--- frontends/php/js/dashboard.grid.js
+++ frontends/php/js/dashboard.grid.js
@@ -1480,15 +1480,6 @@
 					.toggleClass('resizing-top', data['resizing_top'])
 					.toggleClass('resizing-left', data['resizing_left']);
 
-				// Hack for Safari to manually accept parent container height in pixels on widget resize.
-				if (SF) {
-					$.each(data['widgets'], function() {
-						if (this.type === 'clock' || this.type === 'sysmap') {
-							this.content_body.find(':first').height(this.content_body.height());
-						}
-					});
-				}
-
 				/*
 				 * 1. Prevent physically resizing widgets beyond the allowed limits.
 				 * 2. Prevent browser's vertical scrollbar from appearing when resizing right size of the widgets.
@@ -1538,15 +1529,6 @@
 				stopWidgetPositioning($obj, data, widget);
 
 				widget['container'].removeAttr('style');
-
-				// Hack for Safari to manually accept parent container height in pixels when done widget snapping to grid.
-				if (SF) {
-					$.each(data['widgets'], function() {
-						if (this.type === 'clock' || this.type === 'sysmap') {
-							this.content_body.find(':first').height(this.content_body.height());
-						}
-					});
-				}
 
 				if (widget['iterator']) {
 					alignIteratorContents($obj, data, widget, widget['pos']);
@@ -3597,7 +3579,7 @@
 						 * Set the 'sticked-to-top' class before updating the body for it's mutation handler
 						 * to have actual data for the popup positioning.
 						 */
-						if (data.dialogue['widget_type'] === 'svggraph') {
+						if (response.options.stick_to_top) {
 							jQuery('[data-dialogueid="widgetConfg"]').addClass('sticked-to-top');
 						}
 
