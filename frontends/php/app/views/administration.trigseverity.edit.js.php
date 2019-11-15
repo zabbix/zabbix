fbb954abd54c30bb6c620c5e97f2d4ec064c8591
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2019-11-15 08:30:52 +0200

    ..F....... [ZBXNEXT-5486] minor coding style fix

--- frontends/php/app/views/administration.trigseverity.edit.js.php
+++ frontends/php/app/views/administration.trigseverity.edit.js.php
@@ -23,11 +23,11 @@ $schema = DB::getSchema('config');
 ?>
 
 <script type="text/javascript">
-	jQuery(document).ready(function() {
-		jQuery("#resetDefaults").click(function() {
+	jQuery(function($) {
+		$("#resetDefaults").click(function() {
 			overlayDialogue({
 				'title': <?= CJs::encodeJson(_('Reset confirmation')) ?>,
-				'content': jQuery('<span>').text(<?= CJs::encodeJson(_('Reset all fields to default values?')) ?>),
+				'content': $('<span>').text(<?= CJs::encodeJson(_('Reset all fields to default values?')) ?>),
 				'buttons': [
 					{
 						'title': <?= CJs::encodeJson(_('Cancel')) ?>,
@@ -39,28 +39,28 @@ $schema = DB::getSchema('config');
 						'title': <?= CJs::encodeJson(_('Reset defaults')) ?>,
 						'focused': true,
 						'action': function() {
-							jQuery('#severity_name_0').val("<?= $schema['fields']['severity_name_0']['default'] ?>");
-							jQuery('#severity_name_1').val("<?= $schema['fields']['severity_name_1']['default'] ?>");
-							jQuery('#severity_name_2').val("<?= $schema['fields']['severity_name_2']['default'] ?>");
-							jQuery('#severity_name_3').val("<?= $schema['fields']['severity_name_3']['default'] ?>");
-							jQuery('#severity_name_4').val("<?= $schema['fields']['severity_name_4']['default'] ?>");
-							jQuery('#severity_name_5').val("<?= $schema['fields']['severity_name_5']['default'] ?>");
-							jQuery('#severity_color_0')
+							$('#severity_name_0').val("<?= $schema['fields']['severity_name_0']['default'] ?>");
+							$('#severity_name_1').val("<?= $schema['fields']['severity_name_1']['default'] ?>");
+							$('#severity_name_2').val("<?= $schema['fields']['severity_name_2']['default'] ?>");
+							$('#severity_name_3').val("<?= $schema['fields']['severity_name_3']['default'] ?>");
+							$('#severity_name_4').val("<?= $schema['fields']['severity_name_4']['default'] ?>");
+							$('#severity_name_5').val("<?= $schema['fields']['severity_name_5']['default'] ?>");
+							$('#severity_color_0')
 								.val("<?= $schema['fields']['severity_color_0']['default'] ?>")
 								.change();
-							jQuery('#severity_color_1')
+							$('#severity_color_1')
 								.val("<?= $schema['fields']['severity_color_1']['default'] ?>")
 								.change();
-							jQuery('#severity_color_2')
+							$('#severity_color_2')
 								.val("<?= $schema['fields']['severity_color_2']['default'] ?>")
 								.change();
-							jQuery('#severity_color_3')
+							$('#severity_color_3')
 								.val("<?= $schema['fields']['severity_color_3']['default'] ?>")
 								.change();
-							jQuery('#severity_color_4')
+							$('#severity_color_4')
 								.val("<?= $schema['fields']['severity_color_4']['default'] ?>")
 								.change();
-							jQuery('#severity_color_5')
+							$('#severity_color_5')
 								.val("<?= $schema['fields']['severity_color_5']['default'] ?>")
 								.change();
 						}
@@ -69,7 +69,7 @@ $schema = DB::getSchema('config');
 			}, this);
 		});
 
-		var $form = jQuery('form');
+		var $form = $('form');
 		$form.on('submit', function() {
 			$form.trimValues(['#severity_name_0', '#severity_name_1', '#severity_name_2', '#severity_name_3',
 				'#severity_name_4', '#severity_name_5'
