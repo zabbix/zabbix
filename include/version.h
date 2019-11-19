67ce9f47f900e0497b5c63e7f374a34d2d467510
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2019-11-19 11:07:56 +0200

    .......... [ZBXNEXT-826] updated release date [4.0.15rc1]

--- include/version.h
+++ include/version.h
@@ -24,7 +24,7 @@
 #define ZBX_STR(str)	ZBX_STR2(str)
 
 #define APPLICATION_NAME	"Zabbix Agent"
-#define ZABBIX_REVDATE		"28 October 2019"
+#define ZABBIX_REVDATE		"19 November 2019"
 #define ZABBIX_VERSION_MAJOR	4
 #define ZABBIX_VERSION_MINOR	0
 #define ZABBIX_VERSION_PATCH	15
