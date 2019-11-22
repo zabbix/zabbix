d85b72bad1291abd24e36354acb5d463a02685d9
Sergey Petrov <nikor@hill.ripn.net>
2019-11-22 16:26:17 +0300

    Master can not have replication lag

--- templates/db/postgresql/postgresql/pgsql.replication.lag.sql
+++ templates/db/postgresql/postgresql/pgsql.replication.lag.sql
@@ -5,24 +5,29 @@ DECLARE
 BEGIN
 	SELECT current_setting('server_version_num') INTO ver;
 
-	IF (ver >= 100000) THEN
-		SELECT * INTO res from (
-			SELECT
-				CASE WHEN pg_last_wal_receive_lsn() = pg_last_wal_replay_lsn()
-					THEN 0
-					ELSE COALESCE(EXTRACT(EPOCH FROM now() - pg_last_xact_replay_timestamp())::integer, 0)
-				END
-			) T;
-
+	IF NOT(pg_is_in_recovery()) THEN
+		select 0 into res;
 	ELSE
-		SELECT * INTO res from (
-			SELECT
-				CASE WHEN pg_last_xlog_receive_location() = pg_last_xlog_replay_location()
-					THEN 0
-					ELSE COALESCE(EXTRACT(EPOCH FROM now() - pg_last_xact_replay_timestamp())::integer, 0)
-				END
-			) T;
-	END IF;
+		IF (ver >= 100000) THEN
+			SELECT * INTO res from (
+				SELECT
+					CASE WHEN pg_last_wal_receive_lsn() = pg_last_wal_replay_lsn()
+						THEN 0
+						ELSE COALESCE(EXTRACT(EPOCH FROM now() - pg_last_xact_replay_timestamp())::integer, 0)
+					END
+				) T;
+
+		ELSE
+			SELECT * INTO res from (
+				SELECT
+					CASE WHEN pg_last_xlog_receive_location() = pg_last_xlog_replay_location()
+						THEN 0
+						ELSE COALESCE(EXTRACT(EPOCH FROM now() - pg_last_xact_replay_timestamp())::integer, 0)
+					END
+				) T;
+		END IF;
+
+	END IF;	
 
 	perform set_config('zbx_tmp.repl_lag_res', res, false);
 END $$;
