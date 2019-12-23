321d6189b3036c9c853f36308e0bab9f4ee7ed78
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2019-12-23 10:37:50 +0200

    ..F....... [ZBX-12899] fixed formatting issue

--- frontends/php/app/views/monitoring.widget.navtree.view.php
+++ frontends/php/app/views/monitoring.widget.navtree.view.php
@@ -18,6 +18,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 **/
 
+
 $item = new CNavigationTree([
 	'problems' => $data['problems'],
 	'severity_config' => $data['severity_config'],
