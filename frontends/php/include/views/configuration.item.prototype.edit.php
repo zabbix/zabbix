268b21e415b4f094c024a0061cb4313af9515294
Ivo Kurzemnieks <ivo.kurzemnieks@zabbix.com>
2020-01-09 12:23:16 +0200

    ..F....... [ZBXNEXT-5614,ZBXNEXT-1423] added ipmi.get key and description to item helper and made IPMI sensor field optional for items, item prototypes and LLD rules

--- frontends/php/include/views/configuration.item.prototype.edit.php
+++ frontends/php/include/views/configuration.item.prototype.edit.php
@@ -512,11 +512,8 @@ $form_list
 	->addRow(_('Port'),
 		(new CTextBox('port', $data['port'], false, 64))->setWidth(ZBX_TEXTAREA_SMALL_WIDTH), 'row_port'
 	)
-	->addRow(
-		(new CLabel(_('IPMI sensor'), 'ipmi_sensor'))->setAsteriskMark(),
-		(new CTextBox('ipmi_sensor', $data['ipmi_sensor'], $readonly, 128))
-			->setWidth(ZBX_TEXTAREA_STANDARD_WIDTH)
-			->setAriaRequired(),
+	->addRow(_('IPMI sensor'),
+		(new CTextBox('ipmi_sensor', $data['ipmi_sensor'], $readonly, 128))->setWidth(ZBX_TEXTAREA_STANDARD_WIDTH),
 		'row_ipmi_sensor'
 	)
 	// Append authentication method to form list.
