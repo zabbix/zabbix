af6e033441ad02013a3101b55729f8f7f0f8bce6
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2020-01-13 13:31:02 +0200

    .......... [DEV-1385] fixed PHPDoc

--- frontends/php/include/js.inc.php
+++ frontends/php/include/js.inc.php
@@ -22,8 +22,7 @@
 /**
  * Convert PHP variable to string version of JavaScript style
  *
- * @deprecated use CJs::encodeJson() instead
- * @see CJs::encodeJson()
+ * @deprecated use json_encode instead
  *
  * @param mixed $value
  * @param bool  $as_object return string containing javascript object
