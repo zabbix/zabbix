04cc5c8cf884805eb736fb5cfba04bf38347a4ad
Miks Kronkalns <miks.kronkalns@zabbix.com>
2020-01-28 09:48:11 +0200

    ..F....... [ZBXNEXT-5287,ZBXNEXT-5288] fixed macros support of calculated item formula field

--- frontends/php/app/controllers/CControllerPopupItemTest.php
+++ frontends/php/app/controllers/CControllerPopupItemTest.php
@@ -94,10 +94,6 @@ abstract class CControllerPopupItemTest extends CController {
 			'host' => ['{HOSTNAME}', '{HOST.HOST}', '{HOST.NAME}'],
 			'interface' => ['{HOST.IP}', '{IPADDRESS}', '{HOST.DNS}', '{HOST.CONN}', '{HOST.PORT}']
 		],
-		'params_f' => [
-			'host' => ['{HOSTNAME}', '{HOST.HOST}', '{HOST.NAME}'],
-			'interface' => ['{HOST.IP}', '{IPADDRESS}', '{HOST.DNS}', '{HOST.CONN}', '{HOST.PORT}']
-		],
 		'jmx_endpoint' => [
 			'host' => ['{HOSTNAME}', '{HOST.HOST}', '{HOST.NAME}'],
 			'interface' => ['{HOST.IP}', '{IPADDRESS}', '{HOST.DNS}', '{HOST.CONN}', '{HOST.PORT}']
@@ -137,6 +133,7 @@ abstract class CControllerPopupItemTest extends CController {
 			'interface' => ['{HOST.IP}', '{IPADDRESS}', '{HOST.DNS}', '{HOST.CONN}', '{HOST.PORT}'],
 			'item' => ['{ITEM.ID}', '{ITEM.KEY}']
 		],
+		'params_f' => [],
 		'snmp_community' => [],
 		'snmp_oid' => []
 	];
