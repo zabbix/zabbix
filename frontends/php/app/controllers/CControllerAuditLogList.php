59de75512436d938f30a7cf3b2cf0b8c8a26ae2b
Gregory Chalenko <gregory.chalenko@zabbix.com>
2020-02-13 10:18:25 +0200

    ..F....... [ZBXNEXT-4584] fixed lineoverflow

--- frontends/php/app/controllers/CControllerAuditLogList.php
+++ frontends/php/app/controllers/CControllerAuditLogList.php
@@ -50,7 +50,9 @@ class CControllerAuditLogList extends CController {
 		if ($this->getInput('filter_set', 0)) {
 			CProfile::update('web.auditlogs.filter.alias', $this->getInput('alias', ''), PROFILE_TYPE_STR);
 			CProfile::update('web.auditlogs.filter.action', $this->getInput('auditlog_action', -1), PROFILE_TYPE_INT);
-			CProfile::update('web.auditlogs.filter.resourcetype', $this->getInput('resourcetype', -1), PROFILE_TYPE_INT);
+			CProfile::update('web.auditlogs.filter.resourcetype', $this->getInput('resourcetype', -1),
+				PROFILE_TYPE_INT
+			);
 		}
 		elseif ($this->getInput('filter_rst', 0)) {
 			CProfile::delete('web.auditlogs.filter.alias');
