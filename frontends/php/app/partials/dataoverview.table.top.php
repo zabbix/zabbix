62e576cd92ca9c3f868176705b4aaf68df3917f3
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2020-02-24 13:33:33 +0200

    ..F....... [ZBXNEXT-5702] fixed transtation string

--- frontends/php/app/partials/dataoverview.table.top.php
+++ frontends/php/app/partials/dataoverview.table.top.php
@@ -54,7 +54,7 @@ foreach ($data['items_by_name'] as $name => $hostid_to_itemid) {
 
 if ($data['has_hidden_data']) {
 	$table->setFooter([
-		(new CCol(_('Not all results are displayed. Please specify a more specific search criteria.')))
+		(new CCol(_('Not all results are displayed. Please provide more specific search criteria.')))
 			->setColSpan($table->getNumCols())
 			->addClass(ZBX_STYLE_LIST_TABLE_FOOTER)
 	]);
