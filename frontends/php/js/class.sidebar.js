98755311f9b7a8118e2b073d4853855afb4e4015
Alexander Shubin <aleksandrs.subins@zabbix.com>
2020-02-28 18:28:10 +0200

    ..F....... [ZBXNEXT-5569] minor changes

--- frontends/php/js/class.sidebar.js
+++ frontends/php/js/class.sidebar.js
@@ -163,7 +163,7 @@ class CSidebar extends CBaseComponent {
 			},
 
 			focusin: (e) => {
-				console.log('focusin', e);
+				// console.log('focusin', e);
 				if (e.relatedTarget === null) {
 					this._is_focused = true;
 					this.open();
@@ -171,7 +171,7 @@ class CSidebar extends CBaseComponent {
 			},
 
 			focusout: (e) => {
-				console.log('focusout', e);
+				// console.log('focusout', e);
 				if (e.relatedTarget === null) {
 					this._is_focused = false;
 					this.close();
