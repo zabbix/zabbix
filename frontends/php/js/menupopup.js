ffd0e5c1ccf8462530cf82e7e66a3bd3ff68bd8a
Vasily Goncharenko <vasilijs.goncarenko@zabbix.com>
2020-03-03 09:28:56 +0200

    ..F....... [ZBXNEXT-5725] fixed map popup

--- frontends/php/js/menupopup.js
+++ frontends/php/js/menupopup.js
@@ -320,7 +320,7 @@ function getMenuPopupMapElementTrigger(options) {
 	problems_url.setArgument('action', 'problem.view');
 	problems_url.setArgument('filter_triggerids[]', options.triggerids);
 	if (typeof options.severity_min !== 'undefined') {
-		problems_url.setArgument('filter_severities', options.severity_min);
+		problems_url.setArgument('filter_severities[]', options.severity_min);
 	}
 	if (typeof options.show_suppressed !== 'undefined' && options.show_suppressed) {
 		problems_url.setArgument('filter_show_suppressed', '1');
