8e8ac3a7a101b1fa13b6f3e2f3ad13cf1e5c99ec
Vasily Goncharenko <vasilijs.goncarenko@zabbix.com>
2020-03-05 15:10:29 +0200

    ..F....... [ZBXNEXT-5725] added minor changes

--- frontends/php/js/menupopup.js
+++ frontends/php/js/menupopup.js
@@ -82,7 +82,7 @@ function getMenuPopupHistory(options) {
  * @param {bool}   options['showScreens']        Link to host screen page.
  * @param {bool}   options['showTriggers']       Link to Monitoring->Problems page.
  * @param {bool}   options['hasGoTo']            "Go to" block in popup.
- * @param {int}    options['severity_min']       (optional)
+ * @param {int}    options['severities']         (optional)
  * @param {bool}   options['show_suppressed']    (optional)
  * @param {array}  options['urls']               (optional)
  * @param {string} options['url'][]['label']
@@ -148,7 +148,7 @@ function getMenuPopupHost(options, trigger_elmnt) {
 			var url = new Curl('zabbix.php', false);
 			url.setArgument('action', 'problem.view');
 			url.setArgument('filter_hostids[]', options.hostid);
-			if (typeof options.severity_min !== 'undefined') {
+			if (typeof options.severities !== 'undefined') {
 				url.setArgument('filter_severities[]', options.severities);
 			}
 			if (typeof options.show_suppressed !== 'undefined' && options.show_suppressed) {
@@ -256,7 +256,7 @@ function getMenuPopupMapElementSubmap(options) {
  * Get menu popup host group map element section data.
  *
  * @param {string} options['groupid']
- * @param {int}    options['severities']       (optional)
+ * @param {int}    options['severities']         (optional)
  * @param {bool}   options['show_suppressed']    (optional)
  * @param {array}  options['urls']               (optional)
  * @param {string} options['url'][]['label']
@@ -305,7 +305,7 @@ function getMenuPopupMapElementGroup(options) {
  * Get menu popup trigger map element section data.
  *
  * @param {array}  options['triggerids']
- * @param {int}    options['severities']     (optional)
+ * @param {int}    options['severities']       (optional)
  * @param {bool}   options['show_suppressed']  (optional)
  * @param {array}  options['urls']             (optional)
  * @param {string} options['url'][]['label']
