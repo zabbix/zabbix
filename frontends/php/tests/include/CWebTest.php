103fca4e4a604ccd8a996923597ecad23dcc371d
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2020-03-30 17:35:42 +0300

    .......... [DEV-1471] deleted unnecessary workaround

--- frontends/php/tests/include/CWebTest.php
+++ frontends/php/tests/include/CWebTest.php
@@ -105,12 +105,6 @@ class CWebTest extends CTest {
 		$errors = [];
 		if (self::$shared_page !== null) {
 				foreach (self::$shared_page->getBrowserLog() as $log) {
-					// Workaround for ID duplicates.
-					// TODO: remove workaround after fix ZBX-17220
-					if (strpos($log['message'], 'elements with non-unique id') !== false) {
-						continue;
-					}
-
 					$errors[] = $log['message'];
 				}
 		}
