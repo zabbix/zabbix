595fccb8bd30a82427654e53750337297b866d40
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2020-03-30 14:12:03 +0300

    ...G...... [ZBX-17442] fixed web.page.get to time out when there is no response
    * commit 'de5ac6d7105e445b1ba264d1d7c0d0df98eded60':
      .D........ [ZBX-17442] added ChangeLog entry
      ...G...... [ZBX-17442] fixed web.page.get to time out when there is no response
    
    (cherry picked from commit 6da8b6f10653e4b6a6d3db1765dda5acf76980f7)
    (cherry picked from commit 6efe0530b1c46441d89d4b446d03425bd3ee1dc5)

--- src/libs/zbxsysinfo/common/http.c
+++ src/libs/zbxsysinfo/common/http.c
@@ -169,7 +169,8 @@ static int	curl_page_get(char *url, char **buffer, char **error)
 			CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &page)) ||
 			CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_HEADER, 1L)) ||
 			(NULL != CONFIG_SOURCE_IP &&
-			CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_INTERFACE, CONFIG_SOURCE_IP))))
+			CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_INTERFACE, CONFIG_SOURCE_IP))) ||
+			CURLE_OK != (err = curl_easy_setopt(easyhandle, CURLOPT_TIMEOUT, (long)CONFIG_TIMEOUT)))
 	{
 		*error = zbx_dsprintf(*error, "Cannot set cURL option: %s.", curl_easy_strerror(err));
 		goto out;
