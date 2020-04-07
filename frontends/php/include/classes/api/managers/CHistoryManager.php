2d482e2f8f1134b0b9e6a4068df82a994d634fc6
Valdis Murzins <valdis.murzins@zabbix.com>
2020-04-07 18:21:47 +0300

    A......... [ZBXNEXT-5825,ZBXNEXT-5826] fixed value retrival for line graphs

--- frontends/php/include/classes/api/managers/CHistoryManager.php
+++ frontends/php/include/classes/api/managers/CHistoryManager.php
@@ -763,7 +763,7 @@ class CHistoryManager {
 			];
 
 			// Clock value is divided by 1000 as it is stored as milliseconds.
-			$formula = 'Math.floor((params.width*((doc[\'clock\'].date.getMillis()/1000+params.delta)%params.size))'.
+			$formula = 'Math.floor((params.width*((doc[\'clock\'].value.getMillis()/1000+params.delta)%params.size))'.
 					'/params.size)';
 
 			$script = [
