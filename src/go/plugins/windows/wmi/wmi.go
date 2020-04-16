47424e960802dfb62db5262a337e0c4c5a9d85ac
Andris Zeila <andris.zeila@zabbix.com>
2020-04-16 10:15:54 +0300

    ...G...... [DEV-1464] added wmi.get, wmi.getall, system.sw.arch, system.uname, system.hostname metric support to agent2 on windows
    * commit '02660c79392a747084caf3eee9e044d898d551b3':
      .......... [DEV-1464] changed clean target to remove all object files in output directory
      .......... [DEV-1367] added system.uname parameter validation
      .......... [DEV-1464] added system architechture string translation from GOARCH to format used in zabbix
      .......... [DEV-1464] fixed linux build
      .......... [DEV-1464] added support of system.sw.arch, system.uname, system.hostname to windows agent2
      .......... [DEV-1464] added wmi.get, wmi.getall metric support
    
    (cherry picked from commit 3f2c34fafa8c0aea5aa0159578ed4a798a5e4275)

--- src/go/plugins/windows/wmi/wmi.go
+++ src/go/plugins/windows/wmi/wmi.go
@@ -17,10 +17,14 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-package uname
+package wmi
 
 import (
+	"encoding/json"
+	"errors"
+
 	"zabbix.com/pkg/plugin"
+	"zabbix.com/pkg/wmi"
 )
 
 // Plugin -
@@ -32,22 +36,30 @@ var impl Plugin
 
 // Export -
 func (p *Plugin) Export(key string, params []string, ctx plugin.ContextProvider) (result interface{}, err error) {
+	if len(params) != 2 {
+		return nil, errors.New("Invalid number of parameters.")
+	}
 	switch key {
-	case "system.uname":
-		return getUname(params)
-	case "system.hostname":
-		return getHostname(params)
-	case "system.sw.arch":
-		return getSwArch(params)
+	case "wmi.get":
+		return wmi.QueryValue(params[0], params[1])
+	case "wmi.getall":
+		m, err := wmi.QueryTable(params[0], params[1])
+		if err != nil {
+			return nil, err
+		}
+		b, err := json.Marshal(&m)
+		if err != nil {
+			return nil, err
+		}
+		return string(b), nil
 	default:
 		return nil, plugin.UnsupportedMetricError
 	}
-
 }
 
 func init() {
-	plugin.RegisterMetrics(&impl, "Uname",
-		"system.uname", "Returns system uname.",
-		"system.hostname", "Returns system host name.",
-		"system.sw.arch", "Software architecture information.")
+	plugin.RegisterMetrics(&impl, "Wmi",
+		"wmi.get", "Execute WMI query and return the first selected object.",
+		"wmi.getall", "Execute WMI query and return the whole response converted in JSON format.",
+	)
 }
