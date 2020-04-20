aa2a78d1c29a64fcf00029f4d4e41d23b394463d
Roberts Lataria <roberts.lataria@zabbix.com>
2020-04-20 16:20:37 +0300

    ..F....... [ZBX-17577] fixed remove of selected items in multiselect
    (cherry picked from commit 0bc7020d209edaa19b19ec0318f29272ef87fa39)

--- ui/js/multiselect.js
+++ ui/js/multiselect.js
@@ -387,10 +387,15 @@ jQuery(function($) {
 			}
 
 			$obj
-				.on('mousedown', function() {
+				.on('mousedown', function(event) {
 					if (isSearchFieldVisible($obj) && ms.options.selectedLimit != 1) {
 						$obj.addClass('active');
 						$('.selected li.selected', $obj).removeClass('selected');
+
+						// Focus input only when not clicked on selected item.
+						if (!$(event.target).parents('.multiselect-list').length) {
+							$('input[type="text"]', $obj).focus();
+						}
 					}
 				})
 				.on('remove', function() {
