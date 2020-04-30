bd56db556a3c3547233a822e5c9c225889d41470
Vadim Ipatov <vadim.ipatov@zabbix.com>
2020-04-30 16:58:42 +0300

    ...G...... [DEV-1546] Memcached enabled for windows, updated zabbix_agent2.win.conf

--- src/go/plugins/plugins_windows.go
+++ src/go/plugins/plugins_windows.go
@@ -21,6 +21,7 @@ package plugins
 
 import (
 	_ "zabbix.com/plugins/log"
+	_ "zabbix.com/plugins/memcached"
 	_ "zabbix.com/plugins/net/netif"
 	_ "zabbix.com/plugins/net/tcp"
 	_ "zabbix.com/plugins/net/udp"
