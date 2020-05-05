c717eb0d6793ea60ab5f3a6cba1fb3ac85cf629d
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2020-05-05 14:35:17 +0300

    .......... [ZBX-17667] fixed code formatting

--- ui/include/classes/api/APIException.php
+++ ui/include/classes/api/APIException.php
@@ -26,6 +26,6 @@ class APIException extends Exception {
 	 * @param string $message
 	 */
 	public function __construct($code = ZBX_API_ERROR_INTERNAL, $message = '') {
-        parent::__construct($message, $code);
-    }
+		parent::__construct($message, $code);
+	}
 }
