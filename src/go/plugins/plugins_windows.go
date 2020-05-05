15ac431e99ed13fec3876fdd2a1ae0b21f8326ed
Maxim Chudinov <maxim.chudinov@zabbix.com>
2020-05-05 21:50:38 +0300

    ...G...... [DEV-1546] Mysql enabled for windows, updated zabbix_agent2.conf and zabbix_agent2.win.conf

--- src/go/plugins/plugins_windows.go
+++ src/go/plugins/plugins_windows.go
@@ -22,6 +22,7 @@ package plugins
 import (
 	_ "zabbix.com/plugins/log"
 	_ "zabbix.com/plugins/memcached"
+	_ "zabbix.com/plugins/mysql"
 	_ "zabbix.com/plugins/net/netif"
 	_ "zabbix.com/plugins/net/tcp"
 	_ "zabbix.com/plugins/net/udp"
