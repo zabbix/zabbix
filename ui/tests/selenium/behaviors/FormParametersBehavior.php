c71ea4b4bf3767463e48f4ec2ee5c09555d4a182
Vjaceslavs Bogdanovs <vjaceslavs.bogdanovs@zabbix.com>
2020-05-12 15:46:35 +0300

    .......... [DEV-1543] added exception message for missing table selector in FormParametersBehavior

--- ui/tests/selenium/behaviors/FormParametersBehavior.php
+++ ui/tests/selenium/behaviors/FormParametersBehavior.php
@@ -55,7 +55,7 @@ class CFormParametersBehavior extends CBehavior {
 	 */
 	protected function getTable() {
 		if ($this->table_selector === null) {
-			throw new Exception('');
+			throw new Exception('Table selector is not specified.');
 		}
 
 		$selector = (is_array($this->table_selector) && is_callable($this->table_selector))
