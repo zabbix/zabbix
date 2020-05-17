904a05ac1bdda7ba861dcdea06f826db3d51a3e6
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2020-05-17 20:29:49 +0300

    ..F....... [ZBX-17642] reverted rev 779f66a027 because the fix that caused this problem was also reverted

--- ui/include/classes/graphdraw/CLineGraphDraw.php
+++ ui/include/classes/graphdraw/CLineGraphDraw.php
@@ -85,30 +85,27 @@ class CLineGraphDraw extends CGraphDraw {
 	 * Add single item object to graph. If invalid 'delay' interval passed method will interrupt current request with
 	 * error message.
 	 *
-	 * @param array  $graph_item                              Array of graph item properties.
-	 * @param string $graph_item['itemid']                    Item id.
-	 * @param string $graph_item['type']                      Item type.
-	 * @param string $graph_item['name']                      Item host display name.
-	 * @param string $graph_item['hostname']                  Item hostname.
-	 * @param string $graph_item['key_']                      Item key_ field value.
-	 * @param string $graph_item['value_type']                Item value type.
-	 * @param string $graph_item['history']                   Item history field value.
-	 * @param string $graph_item['trends']                    Item trends field value.
-	 * @param string $graph_item['delay']                     Item delay.
-	 * @param string $graph_item['master_itemid']             Master item id for item of type ITEM_TYPE_DEPENDENT.
-	 * @param string $graph_item['units']                     Item units value.
-	 * @param string $graph_item['hostid']                    Item host id.
-	 * @param string $graph_item['hostname']                  Item host name.
-	 * @param string $graph_item['color']                     Item presentation color.
-	 * @param int    $graph_item['drawtype']                  Item presentation draw type, could be one of
-	 *                                                        GRAPH_ITEM_DRAWTYPE_* constants.
-	 * @param int    $graph_item['yaxisside']                 Item axis side, could be one of GRAPH_YAXIS_SIDE_* constants.
-	 * @param int    $graph_item['calc_fnc']                  Item calculation function, could be one of CALC_FNC_* constants.
-	 * @param int    $graph_item['calc_type']                 Item graph presentation calculation type, GRAPH_ITEM_SIMPLE or
-	 *                                                        GRAPH_ITEM_SUM.
-	 * @param array  $graph_item['preprocessing']             Item preprocessing steps.
-	 * @param string $graph_item['preprocessing'][]['type']   Item preprocessing step type.
-	 * @param string $graph_item['preprocessing'][]['params'] Item preprocessing step parameters.
+	 * @param array  $graph_item                   Array of graph item properties.
+	 * @param string $graph_item['itemid']         Item id.
+	 * @param string $graph_item['type']           Item type.
+	 * @param string $graph_item['name']           Item host display name.
+	 * @param string $graph_item['hostname']       Item hostname.
+	 * @param string $graph_item['key_']           Item key_ field value.
+	 * @param string $graph_item['value_type']     Item value type.
+	 * @param string $graph_item['history']        Item history field value.
+	 * @param string $graph_item['trends']         Item trends field value.
+	 * @param string $graph_item['delay']          Item delay.
+	 * @param string $graph_item['master_itemid']  Master item id for item of type ITEM_TYPE_DEPENDENT.
+	 * @param string $graph_item['units']          Item units value.
+	 * @param string $graph_item['hostid']         Item host id.
+	 * @param string $graph_item['hostname']       Item host name.
+	 * @param string $graph_item['color']          Item presentation color.
+	 * @param int    $graph_item['drawtype']       Item presentation draw type, could be one of
+	 *                                             GRAPH_ITEM_DRAWTYPE_* constants.
+	 * @param int    $graph_item['yaxisside']      Item axis side, could be one of GRAPH_YAXIS_SIDE_* constants.
+	 * @param int    $graph_item['calc_fnc']       Item calculation function, could be one of CALC_FNC_* constants.
+	 * @param int    $graph_item['calc_type']      Item graph presentation calculation type, GRAPH_ITEM_SIMPLE or
+	 *                                             GRAPH_ITEM_SUM.
 	 */
 	public function addItem(array $graph_item) {
 		if ($this->type == GRAPH_TYPE_STACKED) {
