8f773dfdd68daa822d032324e0901970da9efc5f
Viktors Tjarve <viktors.tjarve@zabbix.com>
2020-05-18 16:29:52 +0300

    ...G...... [ZBXNEXT-5939] added native support of item 'vfs.file.exists' to Agent2

--- src/go/plugins/vfs/file/exists.go
+++ src/go/plugins/vfs/file/exists.go
@@ -21,23 +21,115 @@ package file
 
 import (
 	"errors"
+	"fmt"
+	"os"
+	"strings"
 )
+
+// File Types
+const (
+	zbxFtFile = 1 << iota
+	zbxFtDir
+	zbxFtSym
+	zbxFtSock
+	zbxFtBdev
+	zbxFtCdev
+	zbxFtFifo
+	zbxFtAll
+	zbxFtDev
+	zbxFtAllMask = (zbxFtFile | zbxFtDir | zbxFtSym | zbxFtSock | zbxFtBdev | zbxFtCdev | zbxFtFifo)
+	zbxFtDev2    = (zbxFtBdev | zbxFtCdev)
+)
+
+type fileType uint16
+
+func (f fileType) HasType(t fileType) bool { return f&t != 0 }
+func (f *fileType) AddType(t fileType)     { *f |= t }
+func (f *fileType) ClearType(t fileType)   { *f &= ^t }
+
+func typesToMask(types []string) (fileType, error) {
+	var mask fileType = 0
+	var fType fileType
+	template := [9]string{"file", "dir", "sym", "sock", "bdev", "cdev", "fifo", "all", "dev"}
+
+	if len(types) == 0 || types[0] == "" {
+		return 0, nil
+	}
+
+	for i := 0; i < len(types); i++ {
+		fType = 1
+
+		for j := 0; j <= len(template); j++ {
+
+			if j == len(template) {
+				return 0, fmt.Errorf("Invalid type \"%s\".", types[i])
+			}
+
+			if template[j] == types[i] {
+				break
+			}
+
+			fType <<= 1
+		}
+
+		mask.AddType(fType)
+	}
+
+	if mask.HasType(zbxFtAll) {
+		mask.AddType(zbxFtAllMask)
+	}
+
+	if mask.HasType(zbxFtDev) {
+		mask.AddType(zbxFtDev2)
+	}
+
+	return mask, nil
+}
 
 // Export -
 func (p *Plugin) exportExists(params []string) (result interface{}, err error) {
-	if len(params) > 1 {
+	var typesIncl fileType
+	var typesExcl fileType
+
+	if len(params) > 3 {
 		return nil, errors.New("Too many parameters.")
 	}
 	if len(params) == 0 || params[0] == "" {
 		return nil, errors.New("Invalid first parameter.")
 	}
 
-	ret := 0
-
-	if f, err := stdOs.Stat(params[0]); err == nil {
-		if mode := f.Mode(); mode.IsRegular() {
-			ret = 1
+	if len(params) > 1 && params[1] != "" {
+		if typesIncl, err = typesToMask(strings.Split(params[1], ",")); err != nil {
+			return nil, err
 		}
 	}
-	return ret, nil
+
+	if len(params) > 2 && params[2] != "" {
+		if typesExcl, err = typesToMask(strings.Split(params[2], ",")); err != nil {
+			return nil, err
+		}
+	}
+
+	if typesIncl == 0 {
+		if typesExcl == 0 {
+			typesIncl.AddType(zbxFtFile)
+		} else {
+			typesIncl.AddType(zbxFtAllMask)
+		}
+	}
+
+	typesIncl.ClearType(typesExcl)
+
+	if f, err := os.Lstat(params[0]); err == nil {
+		if (f.Mode().IsRegular() && typesIncl.HasType(zbxFtFile)) ||
+			(f.Mode().IsDir() && typesIncl.HasType(zbxFtDir)) ||
+			(f.Mode()&os.ModeSymlink != 0 && typesIncl.HasType(zbxFtSym)) ||
+			(f.Mode()&os.ModeSocket != 0 && typesIncl.HasType(zbxFtSock)) ||
+			(f.Mode()&os.ModeDevice != 0 && typesIncl.HasType(zbxFtBdev)) ||
+			(f.Mode()&os.ModeCharDevice != 0 && typesIncl.HasType(zbxFtCdev)) ||
+			(f.Mode()&os.ModeNamedPipe != 0 && typesIncl.HasType(zbxFtFifo)) {
+			return 1, nil
+		}
+	}
+	return 0, nil
 }
