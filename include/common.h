883bd0188e6f1d43b61adc35586b835febc69d09
Andris Zeila <andris.zeila@zabbix.com>
2020-05-21 11:32:47 +0300

    ........S. [ZBXNEXT-5847] added audit logging of global script execution results

--- include/common.h
+++ include/common.h
@@ -1642,4 +1642,8 @@ int	zbx_variant_to_value_type(zbx_variant_t *value, unsigned char value_type, in
 
 int	zbx_str_extract(const char *text, size_t len, char **value);
 
+#define AUDIT_ACTION_EXECUTE	7
+#define AUDIT_RESOURCE_SCRIPT	25
+
+
 #endif
