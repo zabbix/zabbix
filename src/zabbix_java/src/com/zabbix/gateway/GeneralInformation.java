f6a64e0f17ea4b6f8b81f47ab28e59cf9c5ffe67
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2020-05-25 10:38:32 +0300

    .......... [ZBXNEXT-826] updated release date and version [5.0.1]

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "18 May 2020";
+	static final String REVISION_DATE = "25 May 2020";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "5.0.1rc1";
+	static final String VERSION = "5.0.1";
 
 	static void printVersion()
 	{
