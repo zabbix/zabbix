734f10613d81edbd5fa44ba387544fc5244c4709
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2020-05-28 18:32:40 +0300

    ...G...... [DEV-1508] fixed file permissions

--- build/win32/project/zabbix_agent2_desc.h
+++ build/win32/project/zabbix_agent2_desc.h
@@ -1,5 +1,5 @@
-#ifndef _ZABBIX_AGENT_DESC_H_
-#define _ZABBIX_AGENT_DESC_H_
+#ifndef _ZABBIX_AGENT2_DESC_H_
+#define _ZABBIX_AGENT2_DESC_H_
 
 #define VER_FILEDESCRIPTION_STR		"zabbix_agent2.exe"
 
