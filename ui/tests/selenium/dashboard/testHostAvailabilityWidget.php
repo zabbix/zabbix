aeb87c2ed4ac87bf192ae38201cd247b230625ba
Natalja Karpinska <natalja.karpinska@zabbix.com>
2020-06-04 15:40:17 +0300

    .......... [DEV-1369] fixed Selenium tests, added additional sorting parameters in sql query for widget fields

--- ui/tests/selenium/dashboard/testHostAvailabilityWidget.php
+++ ui/tests/selenium/dashboard/testHostAvailabilityWidget.php
@@ -35,7 +35,8 @@ class testHostAvailabilityWidget extends CWebTest {
 			' w.width, w.height'.
 			' FROM widget_field wf'.
 			' INNER JOIN widget w'.
-			' ON w.widgetid=wf.widgetid ORDER BY wf.widgetid, wf.name, wf.value_int, wf.value_groupid';
+			' ON w.widgetid=wf.widgetid ORDER BY wf.widgetid, wf.name, wf.value_int, wf.value_str, wf.value_groupid,'.
+			' wf.value_itemid, wf.value_graphid';
 
 	public static function getCreateWidgetData() {
 		return [
