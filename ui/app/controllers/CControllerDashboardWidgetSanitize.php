161a8dc0e509a74ff7bef93ebb997cd5669c06e8
Andrejs Verza <andrejs.verza@zabbix.com>
2020-07-10 18:20:21 +0300

    ..F....... [ZBX-17898] removed debug info

--- ui/app/controllers/CControllerDashboardWidgetSanitize.php
+++ ui/app/controllers/CControllerDashboardWidgetSanitize.php
@@ -18,7 +18,6 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-sleep(3);
 
 /**
  * Controller to sanitize widget fields before copied widget is created.
