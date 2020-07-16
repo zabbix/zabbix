7e40b7291acf578ba3a2e3eede18feb9c7a4d6b3
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2020-07-16 14:55:21 +0300

    A......... [ZBX-17468] removed auditing readonly fields

--- ui/include/classes/api/services/CHost.php
+++ ui/include/classes/api/services/CHost.php
@@ -968,13 +968,10 @@ class CHost extends CHostGeneral {
 		sort($hostids);
 
 		$db_hosts = $this->get([
-			'output' => ['hostid', 'proxy_hostid', 'host', 'status', 'disable_until', 'error', 'available',
-				'errors_from', 'lastaccess', 'ipmi_authtype', 'ipmi_privilege', 'ipmi_username', 'ipmi_password',
-				'ipmi_disable_until', 'ipmi_available', 'snmp_disable_until', 'snmp_available', 'maintenanceid',
-				'maintenance_status', 'maintenance_type', 'maintenance_from', 'ipmi_errors_from', 'snmp_errors_from',
-				'ipmi_error', 'snmp_error', 'jmx_disable_until', 'jmx_available', 'jmx_errors_from', 'jmx_error',
-				'name', 'flags', 'templateid', 'description', 'tls_connect', 'tls_accept', 'tls_issuer', 'tls_subject',
-				'tls_psk_identity', 'tls_psk', 'proxy_address', 'auto_compress', 'discover', 'inventory_mode'
+			'output' => ['hostid', 'proxy_hostid', 'host', 'status', 'lastaccess', 'ipmi_authtype', 'ipmi_privilege',
+				'ipmi_username', 'ipmi_password', 'name', 'templateid', 'description', 'tls_connect', 'tls_accept',
+				'tls_issuer', 'tls_subject', 'tls_psk_identity', 'tls_psk', 'proxy_address', 'auto_compress',
+				'discover', 'inventory_mode'
 			],
 			'hostids' => $hostids,
 			'editable' => true,
