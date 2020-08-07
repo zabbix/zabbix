f22cdee74496499cbec6a11570d8a934afad3b0f
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2020-08-07 16:53:22 +0300

    .......... [ZBXNEXT-6085] updated API tests

--- ui/tests/api_json/testDiscoveryRule.php
+++ ui/tests/api_json/testDiscoveryRule.php
@@ -4121,26 +4121,6 @@ class testDiscoveryRule extends CAPITest {
 				],
 				'expected_error' => 'Invalid parameter "/1/overrides/1/operations/1": unexpected parameter "optag".'
 			],
-			'Test /1/overrides/1/operations/1/optag is is not supported for host prototype object.' => [
-				'discoveryrules' => [
-					$new_lld_overrides([
-						[
-							'name' => 'override',
-							'step' => 1,
-							'operations' => [
-								[
-									'operationobject' => OPERATION_OBJECT_HOST_PROTOTYPE,
-									'operator' => CONDITION_OPERATOR_NOT_REGEXP,
-									'optag' => [
-										['tag' => 'www']
-									]
-								]
-							]
-						]
-					])
-				],
-				'expected_error' => 'Invalid parameter "/1/overrides/1/operations/1": unexpected parameter "optag".'
-			],
 			// LLD rule override operation template
 			'Test /1/overrides/1/operations/1/optemplate cannot be empty.' => [
 				'discoveryrules' => [
@@ -4852,6 +4832,16 @@ class testDiscoveryRule extends CAPITest {
 											'templateid' => '50010'
 										]
 									],
+									'optag' => [
+										[
+											'tag' => 'tag1',
+											'value' => 'value1'
+										],
+										[
+											'tag' => 'tag2',
+											'value' => 'value2'
+										]
+									],
 									'opinventory' => [
 										'inventory_mode' => HOST_INVENTORY_AUTOMATIC
 									]
@@ -4925,6 +4915,16 @@ class testDiscoveryRule extends CAPITest {
 									'optemplate' => [
 										[
 											'templateid' => '50010'
+										]
+									],
+									'optag' => [
+										[
+											'tag' => 'tag1',
+											'value' => 'value1'
+										],
+										[
+											'tag' => 'tag2',
+											'value' => 'value2'
 										]
 									],
 									'opinventory' => [
@@ -5452,6 +5452,10 @@ if ($debug) {
 							['templateid' => '10265'],
 							['templateid' => '50010']
 						],
+						'optag' => [
+							['tag' => 'tag1', 'value' => 'value1'],
+							['tag' => 'tag2', 'value' => 'value2']
+						],
 						'opinventory' => ['inventory_mode' => HOST_INVENTORY_AUTOMATIC],
 						'opdiscover' => ['discover' => ZBX_PROTOTYPE_NO_DISCOVER]
 					]
@@ -5616,6 +5620,16 @@ file_put_contents('/tmp/testDiscoveryRuleOverrides.txt', var_export(['time' => (
 											'templateid' => '50010'
 										]
 									],
+									'optag' => [
+										[
+											'tag' => 'tag1',
+											'value' => 'value1'
+										],
+										[
+											'tag' => 'tag2',
+											'value' => 'value2'
+										]
+									],
 									'opinventory' => [
 										'inventory_mode' => HOST_INVENTORY_AUTOMATIC
 									]
@@ -5738,6 +5752,16 @@ file_put_contents('/tmp/testDiscoveryRuleOverrides.txt', var_export(['time' => (
 										],
 										[
 											'templateid' => '50010'
+										]
+									],
+									'optag' => [
+										[
+											'tag' => 'tag1',
+											'value' => 'value1'
+										],
+										[
+											'tag' => 'tag2',
+											'value' => 'value2'
 										]
 									],
 									'opinventory' => [
@@ -5898,6 +5922,16 @@ file_put_contents('/tmp/testDiscoveryRuleOverrides.txt', var_export(['time' => (
 							],
 							[
 								'templateid' => '50010'
+							]
+						],
+						'optag' => [
+							[
+								'tag' => 'tag1',
+								'value' => 'value1'
+							],
+							[
+								'tag' => 'tag2',
+								'value' => 'value2'
 							]
 						],
 						'opinventory' => [
