c76acc8c07d657cdd8bf361515264b7708e2a889
Kirils Ustalovs <kirils.ustalovs@zabbix.com>
2020-08-07 13:26:43 +0300

    ..F....... [ZBXNEXT-5898] added unit test for resolveItemDescriptions function

--- ui/include/classes/macros/CMacrosResolver.php
+++ ui/include/classes/macros/CMacrosResolver.php
@@ -1640,7 +1640,7 @@ class CMacrosResolver extends CMacrosResolverGeneral {
 	 *
 	 * @return array  $items
 	 */
-	public function resolveItemDescriptions(array $items): array {
+	public function resolveItemDescriptions(array $items, $test = null): array {
 		$types = ['usermacros' => true];
 		$macro_values = [];
 		$usermacros = [];
@@ -1653,7 +1653,11 @@ class CMacrosResolver extends CMacrosResolverGeneral {
 			}
 		}
 
-		foreach ($this->getUserMacros($usermacros) as $key => $usermacros_data) {
+		if ($test === null) {
+			$test = $this->getUserMacros($usermacros);
+		}
+
+		foreach ($test as $key => $usermacros_data) {
 			$macro_values[$key] = array_key_exists($key, $macro_values)
 				? array_merge($macro_values[$key], $usermacros_data['macros'])
 				: $usermacros_data['macros'];
---
+++ ui/tests/unit/include/classes/resolvers/CMacrosResolverTest.php
@@ -0,0 +1,108 @@
+<?php
+
+class CMacrosResolverTest extends PHPUnit_Framework_TestCase {
+	public function testResolveItemDescriptions() {
+		$a = new CMacrosResolver();
+
+		$items = [
+			30896 => [
+				'itemid' => 30896,
+				'type' => 0,
+				'hostid' => 10084,
+				'name' => 'TEST',
+				'key_' => 'test_test_test',
+				'delay' => '1m',
+				'history' => '90d',
+				'trends' => '365d',
+				'status' => 0,
+				'value_type' => 3,
+				'units' => '',
+				'valuemapid' => 0,
+				'description' => 'aaaaaaaaaaa {$TEST} bbbbbbbbbbbb {$TEST}',
+				'state' => 0,
+				'error' => '',
+				'key_expanded' => 'test_test_test',
+				'name_expanded' => 'TEST'
+			],
+			29164 => [
+				'itemid' => 29164,
+				'type' => 0,
+				'hostid' => 10084,
+				'name' => 'TEST2',
+				'key_' => 'test_test_test2',
+				'delay' => '1m',
+				'history' => '90d',
+				'trends' => '365d',
+				'status' => 0,
+				'value_type' => 3,
+				'units' => '',
+				'valuemapid' => 0,
+				'description' => 'aaaaaaaaaaa',
+				'state' => 0,
+				'error' => '',
+				'key_expanded' => 'test_test_test2',
+				'name_expanded' => 'TEST2'
+			]
+		];
+
+		$_items = [
+			30896 => [
+				'itemid' => 30896,
+				'type' => 0,
+				'hostid' => 10084,
+				'name' => 'TEST',
+				'key_' => 'test_test_test',
+				'delay' => '1m',
+				'history' => '90d',
+				'trends' => '365d',
+				'status' => 0,
+				'value_type' => 3,
+				'units' => '',
+				'valuemapid' => 0,
+				'description' => 'aaaaaaaaaaa test123 bbbbbbbbbbbb test123',
+				'state' => 0,
+				'error' => '',
+				'key_expanded' => 'test_test_test',
+				'name_expanded' => 'TEST'
+			],
+			29164 => [
+				'itemid' => 29164,
+				'type' => 0,
+				'hostid' => 10084,
+				'name' => 'TEST2',
+				'key_' => 'test_test_test2',
+				'delay' => '1m',
+				'history' => '90d',
+				'trends' => '365d',
+				'status' => 0,
+				'value_type' => 3,
+				'units' => '',
+				'valuemapid' => 0,
+				'description' => 'aaaaaaaaaaa',
+				'state' => 0,
+				'error' => '',
+				'key_expanded' => 'test_test_test2',
+				'name_expanded' => 'TEST2'
+			]
+		];
+
+		$test = [
+			30896 => [
+				'hostids' => [
+					0 => 10084
+				],
+				'macros' => [
+					'{$TEST}' => 'test123'
+				]
+			],
+			29164 => [
+				'hostids' => [],
+				'macros' => []
+			]
+		];
+
+		$resolved = $a->resolveItemDescriptions($items, $test);
+
+		$this->assertEquals($resolved, $_items);
+	}
+}
