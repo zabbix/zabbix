f95571efd0af8667e1ed0706e6db64cb31175e13
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2020-08-09 12:02:39 +0300

    A......... [ZBX-13383] improved performance of the trigger and trigger prototype inheritance

--- ui/include/classes/api/services/CTriggerPrototype.php
+++ ui/include/classes/api/services/CTriggerPrototype.php
@@ -431,10 +431,7 @@ class CTriggerPrototype extends CTriggerGeneral {
 
 		$this->validateCreate($trigger_prototypes);
 		$this->createReal($trigger_prototypes);
-
-		foreach ($trigger_prototypes as $trigger_prototype) {
-			$this->inherit($trigger_prototype);
-		}
+		$this->inherit($trigger_prototypes);
 
 		$addDependencies = false;
 
@@ -462,14 +459,10 @@ class CTriggerPrototype extends CTriggerGeneral {
 	 */
 	public function update(array $trigger_prototypes) {
 		$trigger_prototypes = zbx_toArray($trigger_prototypes);
-		$db_triggers = [];
 
 		$this->validateUpdate($trigger_prototypes, $db_triggers);
 		$this->updateReal($trigger_prototypes, $db_triggers);
-
-		foreach ($trigger_prototypes as $trigger_prototype) {
-			$this->inherit($trigger_prototype);
-		}
+		$this->inherit($trigger_prototypes);
 
 		$updateDependencies = false;
 
