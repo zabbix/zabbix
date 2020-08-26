3961db0d9f00a1f75827bc9605902028d0887bc9
Ivo Kurzemnieks <ivo.kurzemnieks@zabbix.com>
2020-08-26 19:30:13 +0300

    ..F....... [DEV-1648] reverted changes made to SAML libraries and removed them from composer and autoload

--- ui/vendor/composer/autoload_psr4.php
+++ ui/vendor/composer/autoload_psr4.php
@@ -8,6 +8,4 @@ $baseDir = dirname($vendorDir);
 return array(
     'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
     'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
-    'RobRichards\\XMLSecLibs\\' => array($vendorDir . '/robrichards/xmlseclibs/src'),
-    'OneLogin\\' => array($vendorDir . '/onelogin/php-saml/src'),
 );
--- ui/vendor/composer/autoload_static.php
+++ ui/vendor/composer/autoload_static.php
@@ -17,14 +17,6 @@ class ComposerStaticInit3e832710df7ec8c92736d5724fad7cab
             'Symfony\\Polyfill\\Ctype\\' => 23,
             'Symfony\\Component\\Yaml\\' => 23,
         ),
-        'R' => 
-        array (
-            'RobRichards\\XMLSecLibs\\' => 23,
-        ),
-        'O' => 
-        array (
-            'OneLogin\\' => 9,
-        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -35,14 +27,6 @@ class ComposerStaticInit3e832710df7ec8c92736d5724fad7cab
         'Symfony\\Component\\Yaml\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/yaml',
-        ),
-        'RobRichards\\XMLSecLibs\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/robrichards/xmlseclibs/src',
-        ),
-        'OneLogin\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/onelogin/php-saml/src',
         ),
     );
 
