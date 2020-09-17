623efd5fcc535cb2cb6956a2e071fd8dcf70b02e
Miks Kronkalns <miks.kronkalns@zabbix.com>
2020-09-17 15:27:54 +0300

    ..F....... [ZBXNEXT-1660,ZBXNEXT-6107] removed redundant check

--- ui/include/classes/helpers/CVaultHelper.php
+++ ui/include/classes/helpers/CVaultHelper.php
@@ -84,7 +84,7 @@ class CVaultHelper {
 
 		$secret = json_decode($secret, true);
 
-		if ($secret !== null && is_array($secret) && array_key_exists('data', $secret) && is_array($secret['data'])
+		if (is_array($secret) && array_key_exists('data', $secret) && is_array($secret['data'])
 				&& array_key_exists('data', $secret['data']) && is_array($secret['data']['data'])) {
 			return $secret['data']['data'];
 		}
