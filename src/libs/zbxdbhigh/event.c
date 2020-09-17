86f53c769387096f9faa6c477ac32685671404aa
Andris Zeila <andris.zeila@zabbix.com>
2020-09-17 11:51:38 +0300

    ........S. [ZBXNEXT-6140] fixed memory leak

--- src/libs/zbxdbhigh/event.c
+++ src/libs/zbxdbhigh/event.c
@@ -227,6 +227,7 @@ void	zbx_db_trigger_clean(DB_TRIGGER *trigger)
 	zbx_free(trigger->comments);
 	zbx_free(trigger->url);
 	zbx_free(trigger->opdata);
+	zbx_free(trigger->event_name);
 }
 
 /******************************************************************************
