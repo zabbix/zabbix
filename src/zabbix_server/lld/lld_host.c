10cf82f1ca18fe66d37789fe7f977315fc3afaf6
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2020-09-23 10:43:45 +0300

    ........S. [ZBX-18386] removed empty line

--- src/zabbix_server/lld/lld_host.c
+++ src/zabbix_server/lld/lld_host.c
@@ -3127,7 +3127,6 @@ static void	lld_interface_make(zbx_vector_ptr_t *interfaces, zbx_uint64_t parent
 		interface->dns = NULL;
 		interface->port = NULL;
 		interface->data.snmp = NULL;
-
 		interface->flags = ZBX_FLAG_LLD_INTERFACE_REMOVE;
 
 		zbx_vector_ptr_append(interfaces, interface);
