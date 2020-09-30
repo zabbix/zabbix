6570d3b8e72074f1b7f3ded90e17af07db0d7fdb
Andrejs Verza <andrejs.verza@zabbix.com>
2020-09-30 09:11:54 +0300

    A......... [ZBX-18417] removed redundant code

--- ui/include/classes/import/converters/C44ImportConverter.php
+++ ui/include/classes/import/converters/C44ImportConverter.php
@@ -174,25 +174,6 @@ class C44ImportConverter extends CConverter {
 	}
 
 	/**
-	 * Remapping interface array keys.
-	 *
-	 * @param array $interfaces
-	 *
-	 * @return array
-	 */
-	protected function remapInterfaceKeys(array $interfaces): array {
-		$data = [];
-		$number = 0;
-
-		foreach ($interfaces as $interface) {
-			$data['interface'. (($number > 0) ? $number : '')] = $interface;
-			$number++;
-		}
-
-		return $data;
-	}
-
-	/**
 	 * Extract SNMP fields from items, discovery rules and item prototypes.
 	 *
 	 * @param array $host
@@ -505,8 +486,6 @@ class C44ImportConverter extends CConverter {
 					$host['interfaces'][] = $value;
 				}
 			}
-
-			$host['interfaces'] = $this->remapInterfaceKeys($host['interfaces']);
 
 			// Set proper default field for interfaces.
 			if (array_key_exists('interfaces', $host)) {
