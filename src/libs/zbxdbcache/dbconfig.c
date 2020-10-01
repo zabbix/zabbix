bfd7852f6ce9fedf198ceec8970fbff75a44fd2b
Andris Zeila <andris.zeila@zabbix.com>
2020-10-01 12:14:40 +0300

    ........S. [ZBX-17493] added discarding of incoming values outside history/trends storage periods on server
    Merge in ZBX/zabbix from feature/ZBX-17493-4.0 to release/4.0
    
    * commit '061a06a18c0361592b730870981d36d4752bf580':
      .D........ [ZBX-17493] added changelog entry
      ........S. [ZBX-17493] fixed trend lookup
      ........S. [ZBX-17493] changed trends checking queries to try smaller periods before checking for existing trends after the value timestamp
      ........S. [ZBX-17493] fixed check for numeric item type when processing history data
      ........S. [ZBX-17493] added discarding of incoming values outside history/trends storage periods on server
    
    (cherry picked from commit 3c45561f42d6ca3b58ca0746eafe44f58beb0e47)

--- src/libs/zbxdbcache/dbconfig.c
+++ src/libs/zbxdbcache/dbconfig.c
@@ -2702,6 +2702,7 @@ static void	DCsync_items(zbx_dbsync_t *sync, int flags)
 				trends_sec = config->config->hk.trends;
 
 			numitem->trends = (0 != trends_sec);
+			numitem->trends_sec = trends_sec;
 
 			DCstrpool_replace(found, &numitem->units, row[26]);
 		}
@@ -6713,6 +6714,7 @@ static void	DCget_item(DC_ITEM *dst_item, const ZBX_DC_ITEM *src_item)
 			numitem = (ZBX_DC_NUMITEM *)zbx_hashset_search(&config->numitems, &src_item->itemid);
 
 			dst_item->trends = numitem->trends;
+			dst_item->trends_sec = numitem->trends_sec;
 			dst_item->units = zbx_strdup(NULL, numitem->units);
 			break;
 		case ITEM_VALUE_TYPE_LOG:
