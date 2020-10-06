861a17f5894f95e2b494eadd18236f04923a1ddb
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2020-10-06 18:20:15 +0300

    .......PS. [ZBX-18407] fixed dependent items not working if it's master item is calculated and host is monitored by Zabbix proxy
    .......PS. [ZBX-18407] fixed dependent items not working if it's master item is calculated and host is monitored by Zabbix proxy
    
    * commit 'cfc7cad5c1037dce4e3384f67420123517bbcd18':
      .D........ [ZBX-18407] added ChangeLog entry
      .......PS. [ZBX-18365] fixed dependent items not working if master item is calculated and host is monitored by Zabbix proxy
    
    (cherry picked from commit cd02b2466c322163f0a3a7c84e56773acac25ef7)

--- src/libs/zbxdbcache/dbsync.c
+++ src/libs/zbxdbcache/dbsync.c
@@ -3595,11 +3595,11 @@ int	zbx_dbsync_compare_item_preprocs(zbx_dbsync_t *sync)
 			" where pp.itemid=i.itemid"
 				" and i.hostid=h.hostid"
 				" and (h.proxy_hostid is null"
-					" or i.type in (%d,%d,%d))"
+					" or i.type in (%d,%d,%d,%d))"
 				" and h.status in (%d,%d)"
 				" and i.flags<>%d"
 			" order by pp.itemid",
-			ITEM_TYPE_INTERNAL, ITEM_TYPE_AGGREGATE, ITEM_TYPE_CALCULATED,
+			ITEM_TYPE_INTERNAL, ITEM_TYPE_AGGREGATE, ITEM_TYPE_CALCULATED, ITEM_TYPE_DEPENDENT,
 			HOST_STATUS_MONITORED, HOST_STATUS_NOT_MONITORED,
 			ZBX_FLAG_DISCOVERY_PROTOTYPE)))
 	{
@@ -3623,8 +3623,14 @@ int	zbx_dbsync_compare_item_preprocs(zbx_dbsync_t *sync)
 		unsigned char	type;
 
 		ZBX_STR2UCHAR(type, dbrow[8]);
-		if (SUCCEED != DBis_null(dbrow[10]) && SUCCEED != is_item_processed_by_server(type, dbrow[9]))
+
+		/* Sync preproccessing for all dependent items as they can depend on item processed by Zabbx server, */
+		/* avoid syncing preprocessing for internal items monitored by Zabbix proxy                          */
+		if (type != ITEM_TYPE_DEPENDENT && SUCCEED != DBis_null(dbrow[10]) &&
+				SUCCEED != is_item_processed_by_server(type, dbrow[9]))
+		{
 			continue;
+		}
 
 		ZBX_STR2UINT64(rowid, dbrow[0]);
 		zbx_hashset_insert(&ids, &rowid, sizeof(rowid));
