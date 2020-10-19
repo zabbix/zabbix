08e594cc7e83f5126452086b0fac253a646f9cd7
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2020-10-19 09:22:01 +0300

    .......... [ZBXNEXT-686] fixed makeSql test case

--- ui/tests/general/makeSqlTest.php
+++ ui/tests/general/makeSqlTest.php
@@ -79,11 +79,11 @@ class makeSqlTest extends CTest {
 					'output' => ['userid'],
 					'filter' => [
 						'userid' => [2],
-						'type' => [0, 1]
+						'roleid' => [3, 4]
 					]
 				],
-				'SELECT userid FROM users WHERE userid=2 AND type IN (0,1)',
-				'SELECT userid FROM users WHERE userid=2 AND type IN (0,1)'
+				'SELECT userid FROM users WHERE userid=2 AND roleid IN (3,4)',
+				'SELECT userid FROM users WHERE userid=2 AND roleid IN (3,4)'
 			]
 		];
 	}
