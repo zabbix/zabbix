8f12267210826189e978824e3eaec865bdf69b66
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2020-10-19 10:54:22 +0300

    .......... [ZBXNEXT-826] updated release date [5.0.5rc1]

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -29,7 +29,7 @@ import (
 
 const (
 	APPLICATION_NAME        = "Zabbix Agent"
-	ZABBIX_REVDATE          = "28 September 2020"
+	ZABBIX_REVDATE          = "19 October 2020"
 	ZABBIX_VERSION_MAJOR    = 5
 	ZABBIX_VERSION_MINOR    = 0
 	ZABBIX_VERSION_PATCH    = 5
