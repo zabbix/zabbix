726e20b81180ff0f7cf25748fbcbb165d2b97151
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2020-10-21 10:58:09 +0300

    ..F....... [DEV-1695] replaced select in Administration -> General -> Icon mapping
     Conflicts:
    	ui/app/views/administration.iconmap.edit.php
    	ui/app/views/js/administration.iconmap.edit.js.php

--- ui/app/views/js/administration.iconmap.edit.js.php
+++ ui/app/views/js/administration.iconmap.edit.js.php
@@ -31,13 +31,15 @@
 			(new CDiv())->addClass(ZBX_STYLE_DRAG_ICON)
 		))->addClass(ZBX_STYLE_TD_DRAG_ICON),
 		(new CSpan('#0:'))->addClass('rowNum'),
-		(new CComboBox('iconmap[mappings][#{iconmappingid}][inventory_link]', null, null, $data['inventory_list']))
+		(new CSelect('iconmap[mappings][#{iconmappingid}][inventory_link]'))
+			->addOptions(CSelect::createOptionsFromArray($data['inventory_list']))
 			->setId('iconmap_mappings_#{iconmappingid}_inventory_link'),
 		(new CTextBox('iconmap[mappings][#{iconmappingid}][expression]', '', false, 64))
 			->setId('iconmap_mappings_#{iconmappingid}_expression')
 			->setAriaRequired()
 			->setWidth(ZBX_TEXTAREA_SMALL_WIDTH),
-		(new CComboBox('iconmap[mappings][#{iconmappingid}][iconid]', null, null, $data['icon_list']))
+		(new CSelect('iconmap[mappings][#{iconmappingid}][iconid]'))
+			->addOptions(CSelect::createOptionsFromArray($data['icon_list']))
 			->setId('iconmap_mappings_#{iconmappingid}_iconid')
 			->addClass('js-mapping-icon'),
 		(new CCol(
