eaa427cf19cf4511283bd0becfbd977567deec75
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2020-10-26 10:37:46 +0200

    .......... [ZBXNEXT-826] updated release date and version [5.0.5]

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -29,11 +29,11 @@ import (
 
 const (
 	APPLICATION_NAME        = "Zabbix Agent"
-	ZABBIX_REVDATE          = "19 October 2020"
+	ZABBIX_REVDATE          = "26 October 2020"
 	ZABBIX_VERSION_MAJOR    = 5
 	ZABBIX_VERSION_MINOR    = 0
 	ZABBIX_VERSION_PATCH    = 5
-	ZABBIX_VERSION_RC       = "rc1"
+	ZABBIX_VERSION_RC       = ""
 	ZABBIX_VERSION_RC_NUM   = "{ZABBIX_RC_NUM}"
 	ZABBIX_VERSION_REVISION = "{ZABBIX_REVISION}"
 	copyrightMessage        = "Copyright (C) 2020 Zabbix SIA\n" +
--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "19 October 2020";
+	static final String REVISION_DATE = "26 October 2020";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "5.0.5rc1";
+	static final String VERSION = "5.0.5";
 
 	static void printVersion()
 	{
