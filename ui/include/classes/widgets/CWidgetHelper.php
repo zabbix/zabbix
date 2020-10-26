4f881b4777d26792cfdbc4fdd12d6163b0e3f5c3
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2020-10-26 16:46:22 +0200

    ..F....... [DEV-1695] fixed broken z-select in dashboard widget type field
    The scale(1) fix 429ec6e1ef56ab66abc38117367bb36b56fa21e7 had to be
    removed, as it breaks z-select.
    
    Instead, wrapped 2nd column in additional div.
    
    The DOM ordering - 2nd column comes first, to avoid issue described in ZBX-18393
    
     Conflicts:
    	ui/include/classes/widgets/CWidgetHelper.php

--- ui/include/classes/widgets/CWidgetHelper.php
+++ ui/include/classes/widgets/CWidgetHelper.php
@@ -47,16 +47,23 @@ class CWidgetHelper {
 	public static function createFormList($name, $type, $view_mode, $known_widget_types, $field_rf_rate) {
 		$form_list = (new CFormList())
 			->addItem((new CListItem([
-					(new CDiv(new CLabel(_('Type'), 'type')))->addClass(ZBX_STYLE_TABLE_FORMS_TD_LEFT),
-					(new CDiv(new CComboBox('type', $type, 'updateWidgetConfigDialogue()', $known_widget_types)))
-						->addClass(ZBX_STYLE_TABLE_FORMS_TD_RIGHT),
-					(new CDiv((new CCheckBox('show_header'))
+					(new CDiv(new CLabel(_('Type'), 'label-type')))->addClass(ZBX_STYLE_TABLE_FORMS_TD_LEFT),
+					(new CDiv([
+						(new CDiv((new CCheckBox('show_header'))
 							->setLabel(_('Show header'))
 							->setLabelPosition(CCheckBox::LABEL_POSITION_LEFT)
 							->setId('show_header')
-							->setChecked($view_mode == ZBX_WIDGET_VIEW_MODE_NORMAL)))
-						->addClass(ZBX_STYLE_TABLE_FORMS_SECOND_COLUMN)
-				]))->addClass('table-forms-row-with-second-field')
+							->setChecked($view_mode == ZBX_WIDGET_VIEW_MODE_NORMAL)
+						))->addClass(ZBX_STYLE_TABLE_FORMS_SECOND_COLUMN),
+						(new CSelect('type'))
+							->setFocusableElementId('label-type')
+							->setId('type')
+							->setValue($type)
+							->setAttribute('autofocus', 'autofocus')
+							->addOptions(CSelect::createOptionsFromArray($known_widget_types))
+					]))->addClass(ZBX_STYLE_TABLE_FORMS_TD_RIGHT)
+				]))
+				->addClass('table-forms-row-with-second-field')
 			)
 			->addRow(_('Name'),
 				(new CTextBox('name', $name))
