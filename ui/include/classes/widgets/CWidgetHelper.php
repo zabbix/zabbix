d0f26c04e5c93b4f931c86e6a72786d395fe2c6e
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2020-10-29 13:31:27 +0200

    ..F....... [DEV-1695] deleted CComboBox php classes
    Since CListBox inherited from CComboBox, most of CComboBox got copied
    over to CListBox.
    
    Renamed CComboItem into CListBoxItem.

--- ui/include/classes/widgets/CWidgetHelper.php
+++ ui/include/classes/widgets/CWidgetHelper.php
@@ -359,7 +359,7 @@ class CWidgetHelper {
 	}
 
 	/**
-	 * Creates CComboBox field without values, to later fill it by JS script.
+	 * Creates select field without values, to later fill it by JS script.
 	 *
 	 * @param CWidgetFieldWidgetListComboBox $field
 	 *
