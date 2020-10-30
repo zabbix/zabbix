350049b9324fe96e38140c06d5d653f973fc2fa2
Andrejs Kozlovs <andrejs.kozlovs@zabbix.com>
2020-10-30 08:01:10 +0200

    ...G...... [ZBX-18185] fixed compilation error on Solaris 10
    Merge in ZBX/zabbix from feature/ZBX-18185-4.0 to release/4.0
    
    * commit '10510da5533cd4d759f292c258deb150d9939792':
      ...G...... [ZBX-18185] fixed compilation error on Solaris 10
    
    (cherry picked from commit e0d23f9f89ad93ba84d3c50995e7ab5a17276cc3)
    (cherry picked from commit 79c018811c87396edc393d81a2332d4d32346f92)
    (cherry picked from commit bef39f26630b751c886f65ef3b1c685989bb7bc9)

--- include/mutexs.h
+++ include/mutexs.h
@@ -44,7 +44,9 @@ typedef enum
 	ZBX_MUTEX_SQLITE3,
 	ZBX_MUTEX_PROCSTAT,
 	ZBX_MUTEX_PROXY_HISTORY,
+#ifdef HAVE_VMINFO_T_UPDATES
 	ZBX_MUTEX_KSTAT,
+#endif
 	ZBX_MUTEX_MODBUS,
 	/* NOTE: Do not forget to sync changes here with mutex names in diag_add_locks_info()! */
 	ZBX_MUTEX_COUNT
