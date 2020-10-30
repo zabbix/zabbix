492c8b814b33364cfd1ce9c3de0f3f0fd3fa1873
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2020-10-30 09:51:46 +0200

    ..F....... [DEV-1695] renamed class CWidgetFieldWidgetListComboBox -> CWidgetFieldWidgetSelect

--- ui/include/classes/widgets/CWidgetHelper.php
+++ ui/include/classes/widgets/CWidgetHelper.php
@@ -348,7 +348,7 @@ class CWidgetHelper {
 	/**
 	 * Creates select field without values, to later fill it by JS script.
 	 *
-	 * @param CWidgetFieldWidgetListComboBox $field
+	 * @param CWidgetFieldWidgetSelect $field
 	 *
 	 * @return CComboBox
 	 */
