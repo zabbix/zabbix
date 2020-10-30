266ed27ad357f0c8b8745872ca39c34c3f25d2c2
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2020-10-30 09:51:46 +0200

    ..F....... [DEV-1695] renamed class CWidgetFieldWidgetListComboBox -> CWidgetFieldWidgetSelect

--- ui/include/classes/widgets/forms/CWidgetFormMap.php
+++ ui/include/classes/widgets/forms/CWidgetFormMap.php
@@ -53,7 +53,7 @@ class CWidgetFormMap extends CWidgetForm {
 
 		if ($field_source_type->getValue() === WIDGET_SYSMAP_SOURCETYPE_FILTER) {
 			// Select filter widget field.
-			$field_filter_widget = (new CWidgetFieldWidgetListComboBox('filter_widget_reference', _('Filter'),
+			$field_filter_widget = (new CWidgetFieldWidgetSelect('filter_widget_reference', _('Filter'),
 				'type', 'navtree'
 			))
 				->setDefault('')
