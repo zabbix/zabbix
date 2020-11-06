92bc719d849a95c681e6b282888c17bf7772bbdd
Gregory Chalenko <gregory.chalenko@zabbix.com>
2020-11-06 12:07:14 +0200

    ..F....... [ZBX-17865] fix for modal form right column text wrapping

--- ui/app/views/popup.acknowledge.edit.php
+++ ui/app/views/popup.acknowledge.edit.php
@@ -30,7 +30,7 @@ $form = (new CForm())
 	->addVar('eventids', $data['eventids']);
 
 $form_list = (new CFormList())
-	->addRow(new CLabel(_('Problem')), (new CSpan($data['problem_name']))->addClass(ZBX_STYLE_WORDWRAP))
+	->addRow(new CLabel(_('Problem')), $data['problem_name'])
 	->addRow(
 		new CLabel(_('Message'), 'message'),
 		(new CTextArea('message', $data['message']))
