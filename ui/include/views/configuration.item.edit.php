e33df7ec4270516ec4f827994e93c7c808efbe26
Gregory Chalenko <gregory.chalenko@zabbix.com>
2020-11-06 08:36:45 +0200

    ..F....... [ZBXNEXT-2480] added autocomplete to http proxy field for item, item prototype and discovery rule form

--- ui/include/views/configuration.item.edit.php
+++ ui/include/views/configuration.item.edit.php
@@ -316,7 +316,8 @@ $form_list
 		new CLabel(_('HTTP proxy'), 'http_proxy'),
 		(new CTextBox('http_proxy', $data['http_proxy'], $readonly, DB::getFieldLength('items', 'http_proxy')))
 			->setWidth(ZBX_TEXTAREA_STANDARD_WIDTH)
-			->setAttribute('placeholder', '[protocol://][user[:password]@]proxy.example.com[:port]'),
+			->setAttribute('placeholder', '[protocol://][user[:password]@]proxy.example.com[:port]')
+			->disableAutocomplete(),
 		'http_proxy_row'
 	)
 	// Append ITEM_TYPE_HTTPAGENT HTTP authentication to form list.
