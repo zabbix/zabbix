415c88026255b569a155d3fcd016d1807b7ebd74
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2020-11-10 16:15:30 +0200

    .......PS. [ZBXNEXT-782] fixed ipmi host availability update

--- src/zabbix_server/ipmi/ipmi_manager.c
+++ src/zabbix_server/ipmi/ipmi_manager.c
@@ -713,6 +713,9 @@ static void	ipmi_manager_deactivate_host(zbx_ipmi_manager_t *manager, zbx_uint64
 	ipmi_manager_update_host(manager, &item.host);
 
 	DCconfig_clean_items(&item, &errcode, 1);
+	if (NULL != data)
+		zbx_availability_flush(data, data_offset);
+	zbx_free(data);
 }
 
 /******************************************************************************
