bea83f623daeff53ba06a591e549bdcd766d9931
Gregory Chalenko <gregory.chalenko@zabbix.com>
2020-11-16 10:40:37 +0200

    ....I..... [ZBXNEXT-6311] database upgrade; schema changes

--- src/libs/zbxdbupgrade/dbupgrade_5030.c
+++ src/libs/zbxdbupgrade/dbupgrade_5030.c
@@ -29,10 +29,163 @@
 
 extern unsigned char	program_type;
 
-/*static int	DBpatch_5030000(void)
+static int	DBpatch_5030000(void)
 {
-	*** put the first upgrade patch here ***
-}*/
+	const ZBX_FIELD	field = {"available", 0, NULL, NULL, 5, ZBX_TYPE_INT, ZBX_NOTNULL, 0};
+
+	return DBadd_field("interface", &field);
+}
+
+static int	DBpatch_5030001(void)
+{
+	const ZBX_FIELD	field = {"error", "", NULL, NULL, 2048, ZBX_TYPE_CHAR, ZBX_NOTNULL, 0};
+
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBadd_field("interface", &field);
+}
+
+static int	DBpatch_5030002(void)
+{
+	const ZBX_FIELD	field = {"errors_from", 0, NULL, NULL, 5, ZBX_TYPE_INT, ZBX_NOTNULL, 0};
+
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBadd_field("interface", &field);
+}
+
+static int	DBpatch_5030003(void)
+{
+	const ZBX_FIELD	field = {"disable_until", 0, NULL, NULL, 5, ZBX_TYPE_INT, ZBX_NOTNULL, 0};
+
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBadd_field("interface", &field);
+}
+
+static int	DBpatch_5030004(void)
+{
+	return DBdrop_field("hosts", "available");
+}
+
+static int	DBpatch_5030005(void)
+{
+	return DBdrop_field("hosts", "ipmi_available");
+}
+
+static int	DBpatch_5030006(void)
+{
+	return DBdrop_field("hosts", "snmp_available");
+}
+
+static int	DBpatch_5030007(void)
+{
+	return DBdrop_field("hosts", "jmx_available");
+}
+
+static int	DBpatch_5030008(void)
+{
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBdrop_field("hosts", "disable_until");
+}
+
+static int	DBpatch_5030009(void)
+{
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBdrop_field("hosts", "ipmi_disable_until");
+}
+
+static int	DBpatch_5030010(void)
+{
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBdrop_field("hosts", "snmp_disable_until");
+}
+
+static int	DBpatch_5030011(void)
+{
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBdrop_field("hosts", "jmx_disable_until");
+}
+
+static int	DBpatch_5030012(void)
+{
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBdrop_field("hosts", "errors_from");
+}
+
+static int	DBpatch_5030013(void)
+{
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBdrop_field("hosts", "ipmi_errors_from");
+}
+
+static int	DBpatch_5030014(void)
+{
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBdrop_field("hosts", "snmp_errors_from");
+}
+
+static int	DBpatch_5030015(void)
+{
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBdrop_field("hosts", "jmx_errors_from");
+}
+
+static int	DBpatch_5030016(void)
+{
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBdrop_field("hosts", "error");
+}
+
+static int	DBpatch_5030017(void)
+{
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBdrop_field("hosts", "ipmi_error");
+}
+
+static int	DBpatch_5030018(void)
+{
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBdrop_field("hosts", "snmp_error");
+}
+
+static int	DBpatch_5030019(void)
+{
+	if (ZBX_PROGRAM_TYPE_PROXY == program_type)
+		return SUCCEED;
+
+	return DBdrop_field("hosts", "jmx_error");
+}
+
+static int	DBpatch_5030020(void)
+{
+	return DBcreate_index("interface", "interface_3", "available", 0);
+}
 
 #endif
 
@@ -40,6 +193,26 @@ DBPATCH_START(5030)
 
 /* version, duplicates flag, mandatory flag */
 
-/*DBPATCH_ADD(5030000, 0, 1)*/
+DBPATCH_ADD(5030000, 0, 1)
+DBPATCH_ADD(5030001, 0, 1)
+DBPATCH_ADD(5030002, 0, 1)
+DBPATCH_ADD(5030003, 0, 1)
+DBPATCH_ADD(5030004, 0, 1)
+DBPATCH_ADD(5030005, 0, 1)
+DBPATCH_ADD(5030006, 0, 1)
+DBPATCH_ADD(5030007, 0, 1)
+DBPATCH_ADD(5030008, 0, 1)
+DBPATCH_ADD(5030009, 0, 1)
+DBPATCH_ADD(5030010, 0, 1)
+DBPATCH_ADD(5030011, 0, 1)
+DBPATCH_ADD(5030012, 0, 1)
+DBPATCH_ADD(5030013, 0, 1)
+DBPATCH_ADD(5030014, 0, 1)
+DBPATCH_ADD(5030015, 0, 1)
+DBPATCH_ADD(5030016, 0, 1)
+DBPATCH_ADD(5030017, 0, 1)
+DBPATCH_ADD(5030018, 0, 1)
+DBPATCH_ADD(5030019, 0, 1)
+DBPATCH_ADD(5030020, 0, 1)
 
 DBPATCH_END()
