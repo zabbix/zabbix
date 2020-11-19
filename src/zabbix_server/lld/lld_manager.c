d0df814601096df026571409880a20b222924149
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2020-11-19 12:56:37 +0200

    ........S. [ZBX-18650] fixed handling of discovery rules with metadata; improved handling of discovery rules without metadata

--- src/zabbix_server/lld/lld_manager.c
+++ src/zabbix_server/lld/lld_manager.c
@@ -338,13 +338,8 @@ static void	lld_queue_request(zbx_lld_manager_t *manager, const zbx_ipc_message_
 	}
 	else
 	{
-		if ((((NULL != data->value && NULL != rule->tail->value) &&
-				(NULL == data->error && NULL == rule->tail->error) &&
-						0 == strcmp(data->value, rule->tail->value)) ||
-				((NULL == data->value && NULL == rule->tail->value) &&
-						(NULL != data->error && NULL != rule->tail->error &&
-								0 == strcmp(data->error, rule->tail->error)))) &&
-				data->meta == rule->tail->meta)
+		if (0 == data->meta && 0 == zbx_strcmp_null(data->error, rule->tail->error) &&
+				0 == zbx_strcmp_null(data->value, rule->tail->value))
 		{
 			zabbix_log(LOG_LEVEL_DEBUG, "skip repeating discovery rule values: " ZBX_FS_UI64, itemid);
 
