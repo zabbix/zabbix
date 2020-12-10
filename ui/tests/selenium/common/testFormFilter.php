e87049d9fec66b9217880eba703c44e2cf3ccade
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2020-12-10 12:08:42 +0200

    .......... [DEV-1720] adding waitingpge, for jenkins checks

--- ui/tests/selenium/common/testFormFilter.php
+++ ui/tests/selenium/common/testFormFilter.php
@@ -225,6 +225,7 @@ class testFormFilter extends CWebTest {
 		$this->page->waitUntilReady();
 		$home_form = $this->query('xpath://div[@id="tabfilter_0"]/form')->asForm()->one();
 		if ($this->page->getTitle() === 'Problems') {
+			$this->page->waitUntilReady();
 			$home_form->query('id:show_timeline_0')->one()->asCheckbox()->fill(false);
 		}
 
