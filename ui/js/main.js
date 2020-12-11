709aa73b5bb75c17fbab4a2cbc27d6c0e95418b0
Ivo Kurzemnieks <ivo.kurzemnieks@zabbix.com>
2020-12-11 10:35:47 +0200

    ..F....... [ZBXNEXT-6376] reverted code regarding the old dropdowns

--- ui/js/main.js
+++ ui/js/main.js
@@ -793,6 +793,10 @@ function getConditionFormula(conditions, evalType) {
 			$row = $(template.evaluate($.extend(data, options.dataCallback(data))));
 
 			for (const name in data) {
+				// Set 'select' value.
+				$row.find('[name$="[' + counter + '][' + name + ']"] option[value="' + data[name] + '"]')
+					.attr('selected', 'selected');
+
 				// Set 'z-select' value.
 				$row.find('z-select[name$="[' + counter + '][' + name + ']"]').val(data[name])
 					.attr('selected', 'selected');
