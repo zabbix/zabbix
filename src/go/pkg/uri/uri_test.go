4c9fccbec4b5ea5a50cff4a5e2481b65443c1833
Vadim Ipatov <vadim.ipatov@zabbix.com>
2020-12-16 17:01:22 +0200

    ...G...... [ZBXNEXT-6331] updated tests for pkg/uri

--- src/go/pkg/uri/uri_test.go
+++ src/go/pkg/uri/uri_test.go
@@ -68,10 +68,20 @@ func TestURI_String(t *testing.T) {
 		want   string
 	}{
 		{
-			"Should return URI with creds",
+			"Should return URI with creds. Test 1",
 			fields{scheme: "https", host: "127.0.0.1", port: "8003", user: "zabbix",
 				password: "a35c2787-6ab4-4f6b-b538-0fcf91e678ed"},
 			"https://zabbix:a35c2787-6ab4-4f6b-b538-0fcf91e678ed@127.0.0.1:8003",
+		},
+		{
+			"Should return URI with creds. Test 2",
+			fields{scheme: "unix", socket: "/tmp/redis.sock", user: "zabbix", password: "secret"},
+			"unix://zabbix:secret@/tmp/redis.sock",
+		},
+		{
+			"Should return URI with user only",
+			fields{scheme: "unix", socket: "/tmp/redis.sock", user: "zabbix"},
+			"unix://zabbix@/tmp/redis.sock",
 		},
 		{
 			"Should return URI with creds containing special characters",
@@ -100,7 +110,7 @@ func TestURI_String(t *testing.T) {
 			"https://127.0.0.1",
 		},
 		{
-			"Should return URI socket",
+			"Should return URI with socket",
 			fields{scheme: "unix", socket: "/var/lib/mysql/mysql.sock"},
 			"unix:///var/lib/mysql/mysql.sock",
 		},
@@ -223,7 +233,13 @@ func TestNew(t *testing.T) {
 			false,
 		},
 		{
-			"Parse URI with unix scheme",
+			"Parse URI with unix scheme. Test 1",
+			args{"unix:/var/run/memcached.sock", nil},
+			&URI{scheme: "unix", socket: "/var/run/memcached.sock", rawUri: "unix:/var/run/memcached.sock"},
+			false,
+		},
+		{
+			"Parse URI with unix scheme. Test 2",
 			args{"unix:///var/run/memcached.sock", nil},
 			&URI{scheme: "unix", socket: "/var/run/memcached.sock", rawUri: "unix:///var/run/memcached.sock"},
 			false,
