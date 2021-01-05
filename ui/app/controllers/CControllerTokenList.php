aff31e519a03bb4543ff825b06236e10b35d11c5
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2021-01-05 14:31:06 +0200

    ..F....... [ZBXNEXT-6207] fixed coding style

--- ui/app/controllers/CControllerTokenList.php
+++ ui/app/controllers/CControllerTokenList.php
@@ -51,8 +51,8 @@ class CControllerTokenList extends CController {
 	}
 
 	protected function checkPermissions() {
-		return $this->checkAccess(CRoleHelper::ACTIONS_MANAGE_API_TOKENS)
-			&& $this->checkAccess(CRoleHelper::UI_ADMINISTRATION_GENERAL);
+		return ($this->checkAccess(CRoleHelper::ACTIONS_MANAGE_API_TOKENS)
+			&& $this->checkAccess(CRoleHelper::UI_ADMINISTRATION_GENERAL));
 	}
 
 	protected function doAction() {
--- ui/app/controllers/CControllerTokenView.php
+++ ui/app/controllers/CControllerTokenView.php
@@ -41,8 +41,8 @@ class CControllerTokenView extends CController {
 	}
 
 	protected function checkPermissions() {
-		return $this->checkAccess(CRoleHelper::ACTIONS_MANAGE_API_TOKENS)
-				&& $this->checkAccess(CRoleHelper::UI_ADMINISTRATION_GENERAL);
+		return ($this->checkAccess(CRoleHelper::ACTIONS_MANAGE_API_TOKENS)
+			&& $this->checkAccess(CRoleHelper::UI_ADMINISTRATION_GENERAL));
 	}
 
 	protected function doAction() {
