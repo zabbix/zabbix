56c8486eff6972f9807e356e917451ad4ebc123d
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2021-01-05 14:15:39 +0200

    ..F....... [ZBXNEXT-6207] fixed coding style
    removed extra tab and added parentheses
    
    "Returning boolean values MUST be in parenthesis."

--- ui/app/controllers/CControllerTokenEdit.php
+++ ui/app/controllers/CControllerTokenEdit.php
@@ -46,8 +46,8 @@ class CControllerTokenEdit extends CController {
 	}
 
 	protected function checkPermissions() {
-		return $this->checkAccess(CRoleHelper::ACTIONS_MANAGE_API_TOKENS)
-				&& $this->checkAccess(CRoleHelper::UI_ADMINISTRATION_GENERAL);
+		return ($this->checkAccess(CRoleHelper::ACTIONS_MANAGE_API_TOKENS)
+			&& $this->checkAccess(CRoleHelper::UI_ADMINISTRATION_GENERAL));
 	}
 
 	protected function doAction() {
