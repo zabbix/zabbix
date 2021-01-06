8c317613014d995a65af8eeb88f596218d10df53
Miks Kronkalns <miks.kronkalns@zabbix.com>
2021-01-06 15:32:35 +0200

    A......... [ZBXNEXT-6376] fixed selection by inherited tags

--- ui/include/classes/api/helpers/CApiTagHelper.php
+++ ui/include/classes/api/helpers/CApiTagHelper.php
@@ -58,7 +58,8 @@ class CApiTagHelper {
 				$value = str_replace(['!', '%', '_'], ['!!', '!%', '!_'], $value);
 				$value = '%'.mb_strtoupper($value).'%';
 
-				$values_by_tag['NOT EXISTS'][$tag['tag']][] = 'UPPER('.$table.'.value) LIKE '.zbx_dbstr($value)." ESCAPE '!'";
+				$values_by_tag['NOT EXISTS'][$tag['tag']][]
+						= 'UPPER('.$table.'.value) LIKE '.zbx_dbstr($value)." ESCAPE '!'";
 			}
 			elseif ($operator == TAG_OPERATOR_NOT_EQUAL) {
 				$values_by_tag['NOT EXISTS'][$tag['tag']][] = $table.'.value='.zbx_dbstr($value);
@@ -71,7 +72,8 @@ class CApiTagHelper {
 				$value = str_replace(['!', '%', '_'], ['!!', '!%', '!_'], $value);
 				$value = '%'.mb_strtoupper($value).'%';
 
-				$values_by_tag['EXISTS'][$tag['tag']][] = 'UPPER('.$table.'.value) LIKE '.zbx_dbstr($value)." ESCAPE '!'";
+				$values_by_tag['EXISTS'][$tag['tag']][]
+						= 'UPPER('.$table.'.value) LIKE '.zbx_dbstr($value)." ESCAPE '!'";
 			}
 			elseif ($operator == TAG_OPERATOR_EQUAL) {
 				$values_by_tag['EXISTS'][$tag['tag']][] = $table.'.value='.zbx_dbstr($value);
@@ -86,7 +88,9 @@ class CApiTagHelper {
 
 				$statement = $table.'.tag='.zbx_dbstr($tag);
 				if ($values) {
-					$statement .= ' AND ('.implode(' OR ', $values).')';
+					$statement .= (count($values) == 1)
+						? ' AND '.implode(' OR ', $values)
+						: ' AND ('.implode(' OR ', $values).')';
 				}
 
 				$sql_where[] = $prefix.' ('.
@@ -108,61 +112,91 @@ class CApiTagHelper {
 	}
 
 	/**
-	 * Function returns template tags matching filter tags based on given operator filter.
+	 * Function prepares two lists:
+	 * - templateids matching $filter_tag;
+	 * - as well as list of templateids which MUST be excluded from results.
 	 *
 	 * @param array  $filter_tag
-	 * @param string $filter_tag['tag']
 	 * @param int    $filter_tag['operator']
+	 * @param string $filter_tag['tag']
 	 * @param string $filter_tag['value']
-	 * @param array  $host_tag
-	 * @param string $host_tag['tag']
-	 * @param string $host_tag['value']
+	 * @param array  $template_tags
+	 * @param string $template_tags[]['tag']
+	 * @param string $template_tags[]['value']
+	 * @param ?int   $template_tags[]['hostid']
 	 *
 	 * @return array
 	 */
-	public static function getMatchingTemplateTag(array $filter_tag, array $template_tags): array {
-		if ($filter_tag['operator'] == TAG_OPERATOR_NOT_EXISTS
-				|| $filter_tag['operator'] == TAG_OPERATOR_NOT_LIKE
-				|| ($filter_tag['operator'] == TAG_OPERATOR_NOT_EQUAL && $filter_tag['value'] !== '')) {
-			return [];
-		}
+	public static function checkMatchingTemplates(array $filter_tag, array $template_tags): array {
+		$include = [];
+		$exclude = [];
 
-		$_template_tags = [];
-		foreach ($template_tags as $tag) {
-			$_template_tags[$tag['tag']][] = $tag;
-		}
-
-		$return = [];
 		switch ($filter_tag['operator']) {
-			case TAG_OPERATOR_EXISTS:
-			case TAG_OPERATOR_NOT_EXISTS:
-				if (array_key_exists($filter_tag['tag'], $_template_tags)) {
-					$return = $_template_tags[$filter_tag['tag']];
-				}
-				break;
-
 			case TAG_OPERATOR_LIKE:
-			case TAG_OPERATOR_NOT_LIKE:
-				if (array_key_exists($filter_tag['tag'], $_template_tags)) {
-					$return = array_filter($_template_tags[$filter_tag['tag']], function($template_tag) use($filter_tag) {
-						$template_tag_value = mb_strtolower($template_tag['value']);
-						$filter_tag_value = mb_strtolower($filter_tag['value']);
-
-						return ($filter_tag_value === '' || mb_strpos($template_tag_value, $filter_tag_value) !== false);
-					});
+				foreach ($template_tags as $tag) {
+					if ($tag['hosttagid'] != 0
+							&& $filter_tag['tag'] === $tag['tag']
+							&& ($filter_tag['value'] === ''
+								|| mb_stripos($tag['value'], $filter_tag['value']) !== false)) {
+						$include[$tag['hostid']] = true;
+					}
 				}
 				break;
 
 			case TAG_OPERATOR_EQUAL:
+				foreach ($template_tags as $tag) {
+					if ($tag['hosttagid'] != 0
+							&& $filter_tag['tag'] === $tag['tag']
+							&& $filter_tag['value'] === $tag['value']) {
+						$include[$tag['hostid']] = true;
+					}
+				}
+				break;
+
+			case TAG_OPERATOR_NOT_LIKE:
+				$include = array_flip(array_column($template_tags, 'hostid'));
+				foreach ($template_tags as $tag) {
+					if ($tag['hosttagid'] != 0
+							&& $filter_tag['tag'] === $tag['tag']
+							&& ($filter_tag['value'] === ''
+								|| mb_stripos($tag['value'], $filter_tag['value']) !== false)) {
+						$exclude[$tag['hostid']] = true;
+						unset($include[$tag['hostid']]);
+					}
+				}
+				break;
+
 			case TAG_OPERATOR_NOT_EQUAL:
-				if (array_key_exists($filter_tag['tag'], $_template_tags)) {
-					$return = array_filter($_template_tags[$filter_tag['tag']], function($template_tag) use($filter_tag) {
-						return ($filter_tag['value'] === $template_tag['value']);
-					});
+				$include = array_flip(array_column($template_tags, 'hostid'));
+				foreach ($template_tags as $tag) {
+					if ($tag['hosttagid'] != 0
+							&& $filter_tag['tag'] === $tag['tag']
+							&& $filter_tag['value'] === $tag['value']) {
+						$exclude[$tag['hostid']] = true;
+						unset($include[$tag['hostid']]);
+					}
+				}
+				break;
+
+			case TAG_OPERATOR_EXISTS:
+				foreach ($template_tags as $tag) {
+					if ($tag['hosttagid'] != 0 && $filter_tag['tag'] === $tag['tag']) {
+						$include[$tag['hostid']] = true;
+					}
+				}
+				break;
+
+			case TAG_OPERATOR_NOT_EXISTS:
+				$include = array_flip(array_column($template_tags, 'hostid'));
+				foreach ($template_tags as $tag) {
+					if ($tag['hosttagid'] != 0 && $filter_tag['tag'] === $tag['tag']) {
+						$exclude[$tag['hostid']] = true;
+						unset($include[$tag['hostid']]);
+					}
 				}
 				break;
 		}
 
-		return zbx_toHash($return, 'hostid');
+		return [$include, $exclude];
 	}
 }
