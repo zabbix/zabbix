a8a2136c20b7f21dd5fc41880663831a89632a4d
Roberts Lataria <roberts.lataria@zabbix.com>
2021-01-11 17:38:21 +0200

    A......... [ZBXNEXT-5868] fixed code style

--- ui/include/classes/api/services/CHost.php
+++ ui/include/classes/api/services/CHost.php
@@ -1796,7 +1796,7 @@ class CHost extends CHostGeneral {
 				'type' => API_INTS32, 'flags' => API_ALLOW_NULL | API_NORMALIZE | API_NOT_EMPTY, 'in' => implode(',', range(TRIGGER_SEVERITY_NOT_CLASSIFIED, TRIGGER_SEVERITY_COUNT - 1)), 'uniq' => true
 			],
 			'withProblemsSuppressed' =>		['type' => API_BOOLEAN, 'flags' => API_ALLOW_NULL],
-			'selectValueMaps' =>			['type' => API_OUTPUT, 'flags' => API_ALLOW_NULL, 'in' => 'valuemapid,name,mappings', 'default' => null]
+			'selectValueMaps' =>			['type' => API_OUTPUT, 'flags' => API_ALLOW_NULL, 'in' => 'valuemapid,name,mappings']
 		]];
 		$options_filter = array_intersect_key($options, $api_input_rules['fields']);
 		if (!CApiInputValidator::validate($api_input_rules, $options_filter, '/', $error)) {
