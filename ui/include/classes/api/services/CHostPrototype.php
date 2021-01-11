87e24c6b0a353823dca31b1277de5bbf95edc547
Roberts Lataria <roberts.lataria@zabbix.com>
2021-01-11 17:25:26 +0200

    A......... [ZBXNEXT-5868] reverted changes from host prototype

--- ui/include/classes/api/services/CHostPrototype.php
+++ ui/include/classes/api/services/CHostPrototype.php
@@ -78,7 +78,6 @@ class CHostPrototype extends CHostBase {
 			'selectTemplates' =>		['type' => API_OUTPUT, 'flags' => API_ALLOW_NULL | API_ALLOW_COUNT, 'in' => implode(',', $hosts_fields), 'default' => null],
 			'selectMacros' =>			['type' => API_OUTPUT, 'flags' => API_ALLOW_NULL, 'in' => implode(',', $hostmacro_fields), 'default' => null],
 			'selectTags' =>				['type' => API_OUTPUT, 'flags' => API_ALLOW_NULL, 'in' => implode(',', ['tag', 'value']), 'default' => null],
-			'selectValueMaps' =>		['type' => API_OUTPUT, 'flags' => API_ALLOW_NULL, 'in' => 'valuemapid,hostid,name,mappings', 'default' => null],
 			// sort and limit
 			'sortfield' =>				['type' => API_STRINGS_UTF8, 'flags' => API_NORMALIZE, 'in' => implode(',', $this->sortColumns), 'uniq' => true, 'default' => []],
 			'sortorder' =>				['type' => API_SORTORDER, 'default' => []],
@@ -319,7 +318,6 @@ class CHostPrototype extends CHostBase {
 		$host_prototypes = $this->createReal($host_prototypes);
 		$this->createInterfaces($host_prototypes);
 		$this->createMacros(array_column($host_prototypes, 'macros', 'hostid'));
-
 		$this->inherit($host_prototypes);
 
 		$this->addAuditBulk(AUDIT_ACTION_ADD, AUDIT_RESOURCE_HOST_PROTOTYPE, $host_prototypes);
