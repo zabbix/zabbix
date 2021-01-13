bb66c78ac6fb43a7f50ccd699fda980ce2e2678a
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2021-01-13 13:00:30 +0200

    ........S. [ZBXNEXT-6360,ZBXNEXT-6366] post review fix

--- src/zabbix_server/escalator/escalator.c
+++ src/zabbix_server/escalator/escalator.c
@@ -1236,7 +1236,7 @@ static void	execute_commands(const DB_EVENT *event, const DB_EVENT *r_event, con
 				if (0 == host.proxy_hostid || ZBX_SCRIPT_EXECUTE_ON_SERVER == script.execute_on)
 				{
 					rc = zbx_script_execute(&script, &host, NULL, event, NULL, error, sizeof(error),
-							 NULL);
+							NULL);
 					status = ALERT_STATUS_SENT;
 				}
 				else
