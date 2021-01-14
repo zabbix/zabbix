f3131fd38b2a3a84a8abeb3a0d1478f453f79c67
Roberts Lataria <roberts.lataria@zabbix.com>
2021-01-14 13:58:35 +0200

    A......... [ZBXNEXT-5868] return addRelatedObjects call to host prototype API

--- ui/include/classes/api/services/CHostPrototype.php
+++ ui/include/classes/api/services/CHostPrototype.php
@@ -1315,6 +1315,8 @@ class CHostPrototype extends CHostBase {
 	 * @return array
 	 */
 	protected function addRelatedObjects(array $options, array $result) {
+		$result = parent::addRelatedObjects($options, $result);
+
 		$hostPrototypeIds = array_keys($result);
 
 		// adding discovery rule
