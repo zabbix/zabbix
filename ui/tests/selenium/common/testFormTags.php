8950e665cbd7dde1c3bdc776c87c4911e6879cb5
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2021-01-20 11:22:21 +0200

    .......... [DEV-1769] fixed removing focus

--- ui/tests/selenium/common/testFormTags.php
+++ ui/tests/selenium/common/testFormTags.php
@@ -216,7 +216,6 @@ class testFormTags extends CWebTest {
 		// Check screenshots of text area right after filling.
 		if ($data['name'] === 'With tags' || $data['name'] === 'Long tag name and value') {
 			$this->page->removeFocus();
-			sleep(1);
 			$screenshot_area = $this->query('id:tags-table')->one();
 			$this->assertScreenshot($screenshot_area, $data['name']);
 		}
@@ -473,7 +472,6 @@ class testFormTags extends CWebTest {
 		// Check screenshot of text area after saving.
 		if ($data['name'] === 'With tags' || $data['name'] === 'Long tag name and value') {
 			$this->page->removeFocus();
-			sleep(1);
 			$screenshot_area = $this->query('id:tags-table')->one();
 			$this->assertScreenshot($screenshot_area, $data['name']);
 		}
