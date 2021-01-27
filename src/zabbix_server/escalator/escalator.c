73e77ed9603b5e1e0245760404cfff4d899aeaf2
Andris Mednis <Andris.Mednis@zabbix.com>
2021-01-28 00:51:54 +0200

    ........S. [ZBXNEXT-6368] moved get_dynamic_hostid() into separate file and renamed to get_host_from_event()

--- src/zabbix_server/escalator/escalator.c
+++ src/zabbix_server/escalator/escalator.c
@@ -32,6 +32,7 @@
 #include "../scripts/scripts.h"
 #include "zbxcrypto.h"
 #include "comms.h"
+#include "../../libs/zbxserver/get_host_from_event.h"
 #include "../../libs/zbxserver/zabbix_users.h"
 
 extern int	CONFIG_ESCALATOR_FORKS;
@@ -832,140 +833,6 @@ static void	add_command_alert(zbx_db_insert_t *db_insert, int alerts_num, zbx_ui
 	zabbix_log(LOG_LEVEL_DEBUG, "End of %s()", __func__);
 }
 
-#ifdef HAVE_OPENIPMI
-#	define ZBX_IPMI_FIELDS_NUM	4	/* number of selected IPMI-related fields in functions */
-						/* get_dynamic_hostid() and execute_commands() */
-#else
-#	define ZBX_IPMI_FIELDS_NUM	0
-#endif
-
-static int	get_dynamic_hostid(const DB_EVENT *event, DC_HOST *host, char *error, size_t max_error_len)
-{
-	DB_RESULT	result;
-	DB_ROW		row;
-	char		sql[512];	/* do not forget to adjust size if SQLs change */
-	size_t		offset;
-	int		ret = SUCCEED;
-
-	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __func__);
-
-	offset = zbx_snprintf(sql, sizeof(sql), "select distinct h.hostid,h.proxy_hostid,h.host,h.tls_connect");
-#ifdef HAVE_OPENIPMI
-	offset += zbx_snprintf(sql + offset, sizeof(sql) - offset,
-			/* do not forget to update ZBX_IPMI_FIELDS_NUM if number of selected IPMI fields changes */
-			",h.ipmi_authtype,h.ipmi_privilege,h.ipmi_username,h.ipmi_password");
-#endif
-#if defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
-	offset += zbx_snprintf(sql + offset, sizeof(sql) - offset,
-			",h.tls_issuer,h.tls_subject,h.tls_psk_identity,h.tls_psk");
-#endif
-	switch (event->source)
-	{
-		case EVENT_SOURCE_TRIGGERS:
-			zbx_snprintf(sql + offset, sizeof(sql) - offset,
-					" from functions f,items i,hosts h"
-					" where f.itemid=i.itemid"
-						" and i.hostid=h.hostid"
-						" and h.status=%d"
-						" and f.triggerid=" ZBX_FS_UI64,
-					HOST_STATUS_MONITORED, event->objectid);
-
-			break;
-		case EVENT_SOURCE_DISCOVERY:
-			offset += zbx_snprintf(sql + offset, sizeof(sql) - offset,
-					" from hosts h,interface i,dservices ds"
-					" where h.hostid=i.hostid"
-						" and i.ip=ds.ip"
-						" and i.useip=1"
-						" and h.status=%d",
-						HOST_STATUS_MONITORED);
-
-			switch (event->object)
-			{
-				case EVENT_OBJECT_DHOST:
-					zbx_snprintf(sql + offset, sizeof(sql) - offset,
-							" and ds.dhostid=" ZBX_FS_UI64, event->objectid);
-					break;
-				case EVENT_OBJECT_DSERVICE:
-					zbx_snprintf(sql + offset, sizeof(sql) - offset,
-							" and ds.dserviceid=" ZBX_FS_UI64, event->objectid);
-					break;
-			}
-			break;
-		case EVENT_SOURCE_AUTOREGISTRATION:
-			zbx_snprintf(sql + offset, sizeof(sql) - offset,
-					" from autoreg_host a,hosts h"
-					" where " ZBX_SQL_NULLCMP("a.proxy_hostid", "h.proxy_hostid")
-						" and a.host=h.host"
-						" and h.status=%d"
-						" and h.flags<>%d"
-						" and a.autoreg_hostid=" ZBX_FS_UI64,
-					HOST_STATUS_MONITORED, ZBX_FLAG_DISCOVERY_PROTOTYPE, event->objectid);
-			break;
-		default:
-			zbx_snprintf(error, max_error_len, "Unsupported event source [%d]", event->source);
-			return FAIL;
-	}
-
-	host->hostid = 0;
-
-	result = DBselect("%s", sql);
-
-	while (NULL != (row = DBfetch(result)))
-	{
-		if (0 != host->hostid)
-		{
-			switch (event->source)
-			{
-				case EVENT_SOURCE_TRIGGERS:
-					zbx_strlcpy(error, "Too many hosts in a trigger expression", max_error_len);
-					break;
-				case EVENT_SOURCE_DISCOVERY:
-					zbx_strlcpy(error, "Too many hosts with same IP addresses", max_error_len);
-					break;
-			}
-			ret = FAIL;
-			break;
-		}
-
-		ZBX_STR2UINT64(host->hostid, row[0]);
-		ZBX_DBROW2UINT64(host->proxy_hostid, row[1]);
-		strscpy(host->host, row[2]);
-		ZBX_STR2UCHAR(host->tls_connect, row[3]);
-
-#ifdef HAVE_OPENIPMI
-		host->ipmi_authtype = (signed char)atoi(row[4]);
-		host->ipmi_privilege = (unsigned char)atoi(row[5]);
-		strscpy(host->ipmi_username, row[6]);
-		strscpy(host->ipmi_password, row[7]);
-#endif
-#if defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
-		strscpy(host->tls_issuer, row[4 + ZBX_IPMI_FIELDS_NUM]);
-		strscpy(host->tls_subject, row[5 + ZBX_IPMI_FIELDS_NUM]);
-		strscpy(host->tls_psk_identity, row[6 + ZBX_IPMI_FIELDS_NUM]);
-		strscpy(host->tls_psk, row[7 + ZBX_IPMI_FIELDS_NUM]);
-#endif
-	}
-	DBfree_result(result);
-
-	if (FAIL == ret)
-	{
-		host->hostid = 0;
-		*host->host = '\0';
-	}
-	else if (0 == host->hostid)
-	{
-		*host->host = '\0';
-
-		zbx_strlcpy(error, "Cannot find a corresponding host", max_error_len);
-		ret = FAIL;
-	}
-
-	zabbix_log(LOG_LEVEL_DEBUG, "End of %s():%s", __func__, zbx_result_string(ret));
-
-	return ret;
-}
-
 /******************************************************************************
  *                                                                            *
  * Function: get_operation_groupids                                           *
@@ -994,6 +861,13 @@ static void	get_operation_groupids(zbx_uint64_t operationid, zbx_vector_uint64_t
 	zbx_free(sql);
 	zbx_vector_uint64_destroy(&parent_groupids);
 }
+
+#ifdef HAVE_OPENIPMI
+#	define ZBX_IPMI_FIELDS_NUM	4	/* number of selected IPMI-related fields in function */
+						/* execute_commands() */
+#else
+#	define ZBX_IPMI_FIELDS_NUM	0
+#endif
 
 static void	execute_commands(const DB_EVENT *event, const DB_EVENT *r_event, const DB_ACKNOWLEDGE *ack,
 		zbx_uint64_t actionid, zbx_uint64_t operationid, int esc_step, int macro_type,
@@ -1146,7 +1020,7 @@ static void	execute_commands(const DB_EVENT *event, const DB_EVENT *r_event, con
 				strscpy(host.tls_psk, row[17 + ZBX_IPMI_FIELDS_NUM]);
 #endif
 			}
-			else if (SUCCEED == (rc = get_dynamic_hostid((NULL != r_event ? r_event : event), &host, error,
+			else if (SUCCEED == (rc = get_host_from_event((NULL != r_event ? r_event : event), &host, error,
 						sizeof(error))))
 			{
 				if (FAIL != zbx_vector_uint64_search(&executed_on_hosts, host.hostid,
