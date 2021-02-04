bc15f0edae9f2dd0983e4c5fcf826f001fc9411c
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2021-02-04 16:39:56 +0200

    .......... [DEV-1828] refactored assertTitle and assertHeader functions

--- ui/tests/selenium/testPageLowLevelDiscovery.php
+++ ui/tests/selenium/testPageLowLevelDiscovery.php
@@ -92,8 +92,8 @@ class testPageLowLevelDiscovery extends CWebTest {
 		// Checking Title, Header and Column names.
 		$headers = ['', 'Host', 'Name', 'Items', 'Triggers', 'Graphs', 'Hosts',
 				'Key', 'Interval', 'Type', 'Status', 'Info'];
-		$this->assertPageTitle('Configuration of discovery rules');
-		$this->assertPageHeader('Discovery rules');
+		$this->page->assertPageTitle('Configuration of discovery rules');
+		$this->page->assertPageHeader('Discovery rules');
 		$table = $this->query('class:list-table')->asTable()->one();
 		$this->assertSame($headers, $table->getHeadersText());
 
