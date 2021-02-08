758a957d721a9082d20282effea2cd853b282f25
Eriks Sneiders <eriks.sneiders@zabbix.com>
2021-02-08 15:01:52 +0200

    ...G...... [ZBXNEXT-6306] added native linux support for key vm.memory.size in Zabbix agent 2
    * commit 'ea9d64080729f8dcd21015e14612de9093bb4ff2':
      .......... [ZBXNEXT-6306] added change log
      ...G...... [ZBXNEXT-6306] improved EINTR handling
      .......... [ZBXNEXT-6306] changed return and internal parsing type to uint64 and added EINTR err check
      ...G...... [ZBXNEXT-6306] added a missing error check and code clean up
      ...G...... [ZBXNEXT-6306] code clean up
      ...G...... [ZBXNEXT-6306] code clean up
      ...G...... [ZBXNEXT-6306] implemented native linux support for key vm.memory.size in Zabbix agent 2
    
    (cherry picked from commit c86a6edf7ad7d3404b672114c49963f131680024)

--- src/go/pkg/procfs/procfs_test.go
+++ src/go/pkg/procfs/procfs_test.go
@@ -2,7 +2,7 @@
 
 /*
 ** Zabbix
-** Copyright (C) 2001-2020 Zabbix SIA
+** Copyright (C) 2001-2021 Zabbix SIA
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -19,9 +19,17 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-package proc
+package procfs
 
-import "testing"
+import (
+	"testing"
+)
+
+func BenchmarkReadAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = ReadAll("/proc/self/stat")
+	}
+}
 
 var testData = []byte(`
 Name:	foo-bar
@@ -42,7 +50,7 @@ func Test_byteFromProcFileData(t *testing.T) {
 	tests := []struct {
 		name      string
 		args      args
-		wantValue float64
+		wantValue uint64
 		wantFound bool
 		wantErr   bool
 	}{
@@ -58,16 +66,16 @@ func Test_byteFromProcFileData(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotValue, gotFound, err := byteFromProcFileData(tt.args.data, tt.args.valueName)
+			gotValue, gotFound, err := ByteFromProcFileData(tt.args.data, tt.args.valueName)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("byteFromProcFileData() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("ByteFromProcFileData() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if gotValue != tt.wantValue {
-				t.Errorf("byteFromProcFileData() gotValue = %v, want %v", gotValue, tt.wantValue)
+				t.Errorf("ByteFromProcFileData() gotValue = %v, want %v", gotValue, tt.wantValue)
 			}
 			if gotFound != tt.wantFound {
-				t.Errorf("byteFromProcFileData() gotFound = %v, want %v", gotFound, tt.wantFound)
+				t.Errorf("ByteFromProcFileData() gotFound = %v, want %v", gotFound, tt.wantFound)
 			}
 		})
 	}
