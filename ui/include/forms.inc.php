3bd6ebd8dcd32c3b3988d242221d767fa9618a83
Miks Kronkalns <miks.kronkalns@zabbix.com>
2021-02-08 10:45:33 +0200

    ..F....... [ZBXNEXT-2976] fixed undefined index in template item prototype inherited tags form

--- ui/include/forms.inc.php
+++ ui/include/forms.inc.php
@@ -1088,7 +1088,7 @@ function getItemFormData(array $item = [], array $options = []) {
 
 	if (!$data['is_discovery_rule']) {
 		// Select inherited tags.
-		if ($data['show_inherited_tags']) {
+		if ($data['show_inherited_tags'] && array_key_exists('item', $data)) {
 			if ($data['item']['discoveryRule']) {
 				$items = [$data['item']['discoveryRule']];
 				$parent_templates = getItemParentTemplates($items, ZBX_FLAG_DISCOVERY_RULE)['templates'];
