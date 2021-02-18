a916837f0450a779f5f5f325601f19dd0854c091
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2021-02-18 12:56:14 +0200

    .......... [DEV-1833] refactored test item tests

--- ui/tests/selenium/common/testItemTest.php
+++ ui/tests/selenium/common/testItemTest.php
@@ -24,14 +24,24 @@ require_once dirname(__FILE__).'/../../include/CWebTest.php';
 require_once dirname(__FILE__).'/../../../include/items.inc.php';
 require_once dirname(__FILE__).'/../traits/MacrosTrait.php';
 require_once dirname(__FILE__).'/../traits/PreprocessingTrait.php';
+require_once dirname(__FILE__).'/../behaviors/CMessageBehavior.php';
 
 /**
  * Base class for "Test item" function tests.
  */
-class testFormItemTest extends CWebTest {
+class testItemTest extends CWebTest {
 
 	use MacrosTrait;
 	use PreprocessingTrait;
+
+	/**
+	 * Attach MessageBehavior to the test.
+	 *
+	 * @return array
+	 */
+	public function getBehaviors() {
+		return [CMessageBehavior::class];
+	}
 
 	/**
 	 * Test item button state data for item, item prototype and LLD.
@@ -70,15 +80,22 @@ class testFormItemTest extends CWebTest {
 	 *
 	 * @param arary		$data			data provider
 	 * @param string	$item_name		item given name
-	 * @param string	$create_link	url for creating new item
-	 * @param string	$saved_link		url for opening saved item
 	 * @param string	$item_type		type of an item: item, prototype or lld rule
 	 * @param string	$success_text	text part of a success message
 	 * @param boolean	$check_now		possibility of executing item instantly
 	 * @param boolean	$is_host		true if host, false if template
+	 * @param string	$id				id of a host, template or LLD rule
+	 * @param string	$items			pointer to form in URL
 	 */
-	public function checkTestButtonState($data, $item_name, $create_link, $saved_link,
-			$item_type, $success_text, $check_now, $is_host) {
+	public function checkTestButtonState($data, $item_name, $item_type, $success_text, $check_now, $is_host, $id, $items = null) {
+		$create_link = ($items === null)
+			? 'disc_prototypes.php?form=create&context=host&parent_discoveryid='.$id
+			: $items.'.php?form=create&context=host&hostid='.$id;
+
+		$saved_link = ($items === null)
+			? 'disc_prototypes.php?form=update&context=host&parent_discoveryid='.$id.'&itemid='
+			: $items.'.php?form=update&context=host&hostid='.$id.'&itemid=';
+
 		$this->page->login()->open($create_link);
 		$item_form = $this->query('name:itemForm')->waitUntilPresent()->asForm()->one();
 
@@ -100,14 +117,11 @@ class testFormItemTest extends CWebTest {
 			$type = $item_form->getField('Type')->getValue();
 
 			for ($i = 0; $i < 2; $i++) {
-
 				if ($type === 'IPMI agent' && $is_host === false) {
 					$enabled = false;
 				}
 				else {
-					$enabled = (!in_array($type, ['Zabbix agent (active)',
-							'SNMP trap', 'Zabbix trapper','Dependent item'
-					]));
+					$enabled = (!in_array($type, ['Zabbix agent (active)', 'SNMP trap',  'Zabbix trapper','Dependent item']));
 				}
 
 				$this->checkTestButtonInPreprocessing($item_type, $enabled, $i);
@@ -117,8 +131,7 @@ class testFormItemTest extends CWebTest {
 				 */
 				if ($i === 0) {
 					if ($check_now) {
-						$execute_button = $this->query('id:check_now')
-								->waitUntilVisible()->one();
+						$execute_button = $this->query('id:check_now')->waitUntilVisible()->one();
 						$this->assertTrue($execute_button->isEnabled($enabled));
 					}
 
@@ -127,8 +140,7 @@ class testFormItemTest extends CWebTest {
 					if ($item_type === 'Item prototype'
 						&& array_key_exists('Master item', $update)) {
 							sleep(2);
-							$item_form->getFieldContainer('Master item')
-									->asMultiselect()->select($update['Master item']);
+							$item_form->getFieldContainer('Master item')->asMultiselect()->select($update['Master item']);
 					}
 
 					$type = $update['Type'];
@@ -244,7 +256,7 @@ class testFormItemTest extends CWebTest {
 					'host_interface' => '127.0.0.2:161',
 					'snmp_fields' => [
 						'version' => 'SNMPv2',
-						'comunity' => 'public'
+						'community' => 'public'
 					]
 				]
 			],
@@ -258,7 +270,7 @@ class testFormItemTest extends CWebTest {
 					'host_interface' => '127.0.0.5:161',
 					'snmp_fields' => [
 						'version' => 'SNMPv1',
-						'comunity' => 'public'
+						'community' => 'public'
 					]
 				]
 			],
@@ -648,19 +660,23 @@ class testFormItemTest extends CWebTest {
 	/**
 	 * Check test item form.
 	 *
-	 * @param string	$create_link	url for creating new item
 	 * @param arary		$data			data provider
 	 * @param boolean	$is_host		true if host, false if template
+	 * @param string	$id				id of a host, template or LLD rule
+	 * @param string	$items			pointer to form in URL
 	 * @param boolean   $lld            true if lld, false if item or item prototype
 	 */
-	public function checkTestItem($create_link, $data, $is_host, $lld = false) {
+	public function checkTestItem($data, $is_host, $id, $items = null, $lld = false) {
+		$create_link = ($items === null)
+			? 'disc_prototypes.php?form=create&context=host&parent_discoveryid='.$id
+			: $items.'.php?form=create&context=host&hostid='.$id;
+
 		if (!$is_host && $data['fields']['Type'] === 'IPMI agent') {
 			return;
 		}
 
 		$this->page->login()->open($create_link);
 		$item_form = $this->query('name:itemForm')->waitUntilPresent()->asForm()->one();
-
 		$item_form->fill($data['fields']);
 
 		if ($is_host) {
@@ -669,8 +685,7 @@ class testFormItemTest extends CWebTest {
 				$item_form->getField('Host interface')->fill($data['host_interface']);
 			}
 			// Get ip and port separately.
-			$host_interface = explode(':', $item_form->getField('Host interface')
-				->getText(), 2);
+			$host_interface = explode(':', $item_form->getField('Host interface')->getText(), 2);
 		}
 
 		if (CTestArrayHelper::get($data, 'preprocessing')){
@@ -685,7 +700,7 @@ class testFormItemTest extends CWebTest {
 		switch ($data['expected']) {
 			case TEST_GOOD:
 				$this->assertEquals('Test item', $overlay->getTitle());
-				$test_form = $this->query('id:preprocessing-test-form')->waitUntilPresent()->one()->waitUntilReady();
+				$test_form = $this->query('id:preprocessing-test-form')->asForm()->waitUntilPresent()->one()->waitUntilReady();
 				// Check "Get value from host" checkbox.
 				$get_host_value = $test_form->query('id:get_value')->asCheckbox()->one();
 				$this->assertTrue($get_host_value->isEnabled());
@@ -720,7 +735,7 @@ class testFormItemTest extends CWebTest {
 						'port' => 'id:interface_port',
 						'proxy' => 'id:proxy_hostid',
 						'version' => 'id:interface_details_version',
-						'comunity' => 'id:interface_details_community'
+						'community' => 'id:interface_details_community'
 					];
 				}
 				else {
@@ -811,7 +826,7 @@ class testFormItemTest extends CWebTest {
 									'port' => $host_interface[1],
 									'proxy' => $proxy,
 									'version' => CTestArrayHelper::get($data, 'snmp_fields.version', 'SNMPv2'),
-									'comunity' => CTestArrayHelper::get($data, 'snmp_fields.comunity', 'public')
+									'community' => CTestArrayHelper::get($data, 'snmp_fields.community', 'public')
 								];
 							}
 							else {
@@ -820,7 +835,7 @@ class testFormItemTest extends CWebTest {
 									'port' => '',
 									'proxy' => '(no proxy)',
 									'version' => 'SNMPv2',
-									'comunity' => ''
+									'community' => ''
 								];
 							}
 
@@ -829,7 +844,7 @@ class testFormItemTest extends CWebTest {
 								'port' => true,
 								'proxy' => true,
 								'version' => true,
-								'comunity' => true
+								'community' => true
 							];
 						}
 						break;
@@ -881,27 +896,60 @@ class testFormItemTest extends CWebTest {
 					$elements['port']->fill($data['interface']['port']);
 				}
 
+				if ($is_host || array_key_exists('interface', $data) || in_array($data['fields']['Type'],
+						['Zabbix internal', 'External check', 'Database monitor', 'HTTP agent', 'JMX agent',
+						'Zabbix aggregate', 'Calculated'])) {
+					$details = 'Connection to Zabbix server "localhost" refused. Possible reasons:';
+				}
+				else {
+					$details = ($data['fields']['Type'] === 'SNMP agent')
+						? 'Incorrect value for field "SNMP community": cannot be empty.'
+						: ['Incorrect value for field "Host address": cannot be empty.'];
+				}
+
 				// Click Get value button.
 				$button = $test_form->query('button:Get value')->one();
 				$button->click();
-				$this->checkServerMessage(['Connection to Zabbix server "localhost" refused. Possible reasons:']);
+				$this->checkServerMessage($details);
 
 				// Click Test button in test form.
 				$overlay->query('button:Get value and test')->one()->waitUntilVisible()->click();
-				$this->checkServerMessage(['Connection to Zabbix server "localhost" refused. Possible reasons:']);
+				$this->checkServerMessage($details);
 
 				// Check empty interface fields.
-				if (in_array($data['fields']['Type'], ['Zabbix agent', 'SNMP agent', 'IPMI agent'])) {
+				if (in_array($data['fields']['Type'], ['Zabbix agent', 'SNMP agent', 'IPMI agent', 'Simple check'])) {
+					if ($data['fields']['Type'] !== 'Simple check') {
+						$elements['port']->clear();
+						$button->click();
+
+						if (!$is_host && !array_key_exists('interface', $data)) {
+							$details = ($data['fields']['Type'] === 'SNMP agent')
+								? 'Incorrect value for field "SNMP community": cannot be empty.'
+								: 'Incorrect value for field "Host address": cannot be empty.';
+						}
+						else {
+							$details = 'Incorrect value for field "Port": cannot be empty.';
+						}
+
+						$this->checkServerMessage($details);
+					}
+
 					$elements['address']->clear();
-					$elements['port']->clear();
 					$button->click();
-					$this->checkServerMessage(['Incorrect value for field "Host address": cannot be empty.',
-							'Incorrect value for field "Port": cannot be empty.']);
-				}
-				if ($data['fields']['Type'] === 'Simple check') {
-					$elements['address']->clear();
-					$button->click();
-					$this->checkServerMessage(['Incorrect value for field "Host address": cannot be empty.']);
+					$details = (!$is_host && $data['fields']['Type'] === 'SNMP agent')
+							? 'Incorrect value for field "SNMP community": cannot be empty.'
+							: 'Incorrect value for field "Host address": cannot be empty.';
+					$this->checkServerMessage($details);
+
+					// Check SNMP empty fields for Template.
+					if (!$is_host && (CTestArrayHelper::get($data, 'snmp_fields.community'))) {
+						$test_form->fill(['id:interface_details_community' => $data['snmp_fields']['community']]);
+						$button->click();
+						$this->checkServerMessage('Incorrect value for field "Host address": cannot be empty.');
+						$elements['address']->fill('127.0.0.1');
+						$button->click();
+						$this->checkServerMessage('Incorrect value for field "Port": cannot be empty.');
+					}
 				}
 
 				// Uncheck "Get value from host" checkbox.
@@ -977,9 +1025,7 @@ class testFormItemTest extends CWebTest {
 				}
 				break;
 			case TEST_BAD:
-				$message = $overlay->query('tag:output')->waitUntilPresent()->asMessage()->one();
-				$this->assertTrue($message->isBad());
-				$this->assertTrue($message->hasLine($data['error']));
+				$this->assertMessage(TEST_BAD, null, $data['error']);
 				$overlay->close();
 				break;
 		}
@@ -992,13 +1038,10 @@ class testFormItemTest extends CWebTest {
 	 * @param array $data data provider
 	 */
 	private function checkValueFields($data, $not_supported, $lld = false) {
-		$test_form = $this->query('id:preprocessing-test-form')
-				->waitUntilPresent()->one()->waitUntilReady();
+		$test_form = $this->query('id:preprocessing-test-form')->waitUntilPresent()->one()->waitUntilReady();
 		$get_host_value = $test_form->query('id:get_value')->asCheckbox()->one();
-
 		$checked = $get_host_value->isChecked();
 		$prev_enabled = false;
-
 		$value = $test_form->query('id:value')->asMultiline()->one();
 
 		if (!$checked) {
@@ -1026,21 +1069,16 @@ class testFormItemTest extends CWebTest {
 			$this->assertTrue($value->isEnabled(!$checked));
 		}
 
-		$this->assertTrue($test_form->query('id:prev_value')->asMultiline()
-				->one()->isEnabled($checked && $prev_enabled));
-		$this->assertTrue($test_form->query('id:prev_time')
-				->one()->isEnabled($prev_enabled));
+		$this->assertTrue($test_form->query('id:prev_value')->asMultiline()->one()->isEnabled($checked && $prev_enabled));
+		$this->assertTrue($test_form->query('id:prev_time')->one()->isEnabled($prev_enabled));
 		$this->assertFalse($test_form->query('id:time')->one()->isEnabled());
 		$this->assertTrue($test_form->query('id:eol')->one()->isEnabled());
 	}
 
-	private function checkServerMessage($message) {
+	private function checkServerMessage($details) {
 		$test_form = $this->query('id:preprocessing-test-form')->asForm()->one();
 		$message = $test_form->getOverlayMessage();
-		$this->assertTrue($message->isBad());
-		foreach ($message as $line) {
-			$this->assertTrue($message->hasLine($line));
-		}
+		$this->assertMessage(TEST_BAD, null, $details);
 		$message->close();
 	}
 
@@ -1057,12 +1095,10 @@ class testFormItemTest extends CWebTest {
 		$item_form->selectTab($item_type);
 	}
 
-	private function saveFormAndCheckMessage($message_text) {
+	private function saveFormAndCheckMessage($message) {
 		$item_form = $this->query('name:itemForm')->waitUntilPresent()->asForm()->one();
 		$item_form->submit();
 		$this->page->waitUntilReady();
-		$message = CMessageElement::find()->one();
-		$this->assertTrue($message->isGood());
-		$this->assertEquals($message_text, $message->getTitle());
+		$this->assertMessage(TEST_GOOD, $message);
 	}
 }
