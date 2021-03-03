b279cff5f23e89a0f56fa5cce360752602b3aef0
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2021-03-03 11:57:17 +0200

    .......... [ZBXNEXT-686] fixed leap year case

--- ui/tests/selenium/testFormAdministrationGeneralGUI.php
+++ ui/tests/selenium/testFormAdministrationGeneralGUI.php
@@ -1012,8 +1012,10 @@ class testFormAdministrationGeneralGUI extends testFormAdministrationGeneral {
 			case 'Max period for time selector':
 				$this->query('id:from')->one()->fill('now-5y');
 				$this->query('button:Apply')->one()->click();
-				$this->assertEquals('Maximum time period to display is 366 days.',
-				$this->query('class:time-input-error')->waitUntilPresent()->one()->getText());
+				// Days count for the case when current or past year is leap year.
+				$days_count = (new DateTime())->diff((new DateTime())->sub(new DateInterval('P1Y')))->days;
+				$this->assertEquals('Maximum time period to display is '.$days_count.' days.',
+						$this->query('class:time-input-error')->waitUntilPresent()->one()->getText());
 				break;
 		}
 	}
