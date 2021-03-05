a0b82820738957835c544c337da3b916bb9b27e8
Alexander Shubin <aleksandrs.subins@zabbix.com>
2021-03-05 16:55:48 +0200

    ..F....... [ZBXNEXT-6309] refactored clock widget

--- ui/js/widgets/class.widget.clock.js
+++ ui/js/widgets/class.widget.clock.js
@@ -20,4 +20,85 @@
 
 class CWidgetClock extends CWidget {
 
+	constructor({
+		...config,
+		time,
+		time_zone_string,
+		time_zone_offset
+	}) {
+		super(config);
+
+		this._time = time;
+		this._time_zone_string = time_zone_string;
+		this._time_zone_offset = time_zone_offset;
+	}
+
+	start() {
+		super.start();
+
+	// 	'$("#'.$this->getId().'").zbx_clock('.
+	// 	json_encode([
+	// 		'time' => $this->time,
+	// 		'time_zone_string' => $this->time_zone_string,
+	// 		'time_zone_offset' => $this->time_zone_offset,
+	// 		'clock_id' => $this->getId()
+	// ]).
+	// 	');'.
+	}
+
+	activate() {
+		super.activate();
+
+		this._clock_hands_rotate();
+	}
+
+	_clock_hands_rotate() {
+		const now = new Date();
+
+		let time_offset = 0;
+
+		if (this._time !== null) {
+			time_offset = now.getTime() - this._time * 1000;
+		}
+
+		if (this._time_zone_offset !== null) {
+			time_offset += (- now.getTimezoneOffset() * 60 - this._time_zone_offset) * 1000;
+		}
+
+		if (time_offset != 0) {
+			now.setTime(now.getTime() - time_offset);
+		}
+
+		let h = now.getHours() % 12;
+		let m = now.getMinutes();
+		let s = now.getSeconds();
+
+		this._clock_hand_rotate(this._target.querySelector('.clock-hand-h'), 30 * (h + m / 60 + s / 3600));
+		this._clock_hand_rotate(this._target.querySelector('.clock-hand-m'), 6 * (m + s / 60));
+		this._clock_hand_rotate(this._target.querySelector('.clock-hand-s'), 6 * s);
+	}
+
+	_clock_hand_rotate(clock_hand, degree) {
+		clock_hand.setAttribute('transform', `rotate(${degree} 50 50)`);
+	}
+
+	_registerEvents() {
+		super._registerEvents();
+
+		this._events = {
+			...this._events,
+
+			tick: () => {
+				this._clock_hands_rotate();
+			}
+		}
+
+		this._interval_id = setInterval(this._events.tick, 1000);
+	}
+
+	_unregisterEvents() {
+		super._unregisterEvents();
+
+		clearInterval(this._interval_id);
+	}
 }
