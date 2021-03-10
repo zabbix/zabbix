2e8a7a74f415353055ba72a06c2764b3cf0cdd6e
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2021-03-10 19:17:29 +0200

    .......... [DEV-1854] created autotests for Value mapping tab in host and template configuration forms

--- ui/tests/selenium/SeleniumTests.php
+++ ui/tests/selenium/SeleniumTests.php
@@ -137,6 +137,8 @@ require_once dirname(__FILE__).'/testFormTriggerPrototype.php';
 require_once dirname(__FILE__).'/testFormUser.php';
 require_once dirname(__FILE__).'/testFormUserMedia.php';
 require_once dirname(__FILE__).'/testFormUserProfile.php';
+require_once dirname(__FILE__).'/testFormValueMappingsHost.php';
+require_once dirname(__FILE__).'/testFormValueMappingsTemplate.php';
 require_once dirname(__FILE__).'/testFormWeb.php';
 require_once dirname(__FILE__).'/testFormWebStep.php';
 require_once dirname(__FILE__).'/testFormApplication.php';
@@ -295,6 +297,8 @@ class SeleniumTests {
 		$suite->addTestSuite('testFormUser');
 		$suite->addTestSuite('testFormUserMedia');
 		$suite->addTestSuite('testFormUserProfile');
+		$suite->addTestSuite('testFormValueMappingsHost');
+		$suite->addTestSuite('testFormValueMappingsTemplate');
 		$suite->addTestSuite('testFormWeb');
 		$suite->addTestSuite('testFormWebStep');
 		$suite->addTestSuite('testFormApplication');
