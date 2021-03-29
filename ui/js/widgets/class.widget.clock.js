85354fbc6491430c6b8d2afb2696d47944c26ee1
Alexander Shubin <aleksandrs.subins@zabbix.com>
2021-03-29 13:34:20 +0300

    ..F....... [ZBXNEXT-6309] fixed code style

--- ui/js/widgets/class.widget.clock.js
+++ ui/js/widgets/class.widget.clock.js
@@ -47,13 +47,10 @@ class CWidgetClock extends CWidget {
 		this._stopClock();
 
 		if (response.clock_data !== undefined) {
-
 			this._has_contents = true;
 			this._time_offset = 0;
 
 			const now = new Date();
-
-			// console.log(now, now.getTimezoneOffset() * 60, response.clock_data);
 
 			if (response.clock_data.time !== null) {
 				this._time_offset = now.getTime() - parseInt(response.clock_data.time) * 1000;
