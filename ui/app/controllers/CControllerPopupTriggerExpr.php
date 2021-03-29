6223d168881906110aad35639f792caa8255dc36
Miks Kronkalns <miks.kronkalns@zabbix.com>
2021-03-29 13:06:45 +0300

    ..F....... [ZBXNEXT-6451] fixed trigger expression condition popup

--- ui/app/controllers/CControllerPopupTriggerExpr.php
+++ ui/app/controllers/CControllerPopupTriggerExpr.php
@@ -569,7 +569,8 @@ class CControllerPopupTriggerExpr extends CController {
 							if (array_key_exists($fn_name, $this->functions)
 									&& in_array($operator_token->match, $this->functions[$fn_name]['operators'])) {
 								$operator = $operator_token->match;
-								$value = array_key_exists('string', $value_token->data)
+								$value = (($value_token instanceof CTriggerExprParserResult)
+										&& array_key_exists('string', $value_token->data))
 									? $value_token->data['string']
 									: $value_token->match;
 							}
