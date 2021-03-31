cfa4fbd0f793263285dc9608bf27d84ad720cb6b
Andrejs Verza <andrejs.verza@zabbix.com>
2021-03-31 10:40:20 +0300

    ..F....... [ZBXNEXT-6309] replaced dashbrds with dashboards in profiles #3

--- src/libs/zbxdbupgrade/dbupgrade_5030.c
+++ src/libs/zbxdbupgrade/dbupgrade_5030.c
@@ -3638,7 +3638,7 @@ static int	DBpatch_5030109(void)
 		return SUCCEED;
 
 	if (ZBX_DB_OK > DBexecute("update profiles set idx=CONCAT('web.dashboard.widget.navtree.item-', SUBSTR(idx, 21))"
-			" where idx like 'web.dashbrd.navtree-%.toggle'"))
+			" where idx like 'web.dashbrd.navtree-%%.toggle'"))
 		return FAIL;
 
 	return SUCCEED;
