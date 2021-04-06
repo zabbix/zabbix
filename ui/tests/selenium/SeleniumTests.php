34de44ec921523e7745b7f658e89f45284c74bd9
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2021-04-06 15:42:43 +0300

    .......... [DEV-1856] added new test for sid check 5.2

--- ui/tests/selenium/SeleniumTests.php
+++ ui/tests/selenium/SeleniumTests.php
@@ -168,6 +168,7 @@ require_once dirname(__FILE__).'/dashboard/testDashboardDynamicItemWidgets.php';
 require_once dirname(__FILE__).'/dashboard/testDashboardGraphWidget.php';
 require_once dirname(__FILE__).'/dashboard/testDashboardHostAvailabilityWidget.php';
 require_once dirname(__FILE__).'/dashboard/testDashboardProblemsBySeverityWidget.php';
+require_once dirname(__FILE__).'/testSID.php';
 
 class SeleniumTests {
 	public static function suite() {
@@ -323,6 +324,7 @@ class SeleniumTests {
 		$suite->addTestSuite('testDashboardGraphWidget');
 		$suite->addTestSuite('testDashboardHostAvailabilityWidget');
 		$suite->addTestSuite('testDashboardProblemsBySeverityWidget');
+		$suite->addTestSuite('testSID');
 
 		return $suite;
 	}
