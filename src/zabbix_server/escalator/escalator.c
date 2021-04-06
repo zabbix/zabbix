2a57a58f45c5e4d839be2c736dd9a562e26a5e81
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2021-04-06 11:52:46 +0300

    ........S. [ZBX-18912] fixed coding style

--- src/zabbix_server/escalator/escalator.c
+++ src/zabbix_server/escalator/escalator.c
@@ -508,7 +508,7 @@ static void	add_user_msgs(zbx_uint64_t userid, zbx_uint64_t operationid, zbx_uin
 				ZBX_DBROW2UINT64(mediatypeid, row[0]);
 			}
 
-			if (atoi(row[1]) != 1)
+			if (1 != atoi(row[1]))
 			{
 				add_user_msg(userid, mediatypeid, user_msg, row[2], row[3], actionid, event, r_event,
 						ack, macro_t, cancel_err, ZBX_ALERT_MESSAGE_ERR_NONE, tz);
