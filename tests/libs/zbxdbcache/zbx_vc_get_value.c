7c8ce7723d74deb8a246e01529803190c8adf4bd
Andris Zeila <andris.zeila@zabbix.com>
2021-04-14 11:27:52 +0300

    ........S. [ZBXNEXT-6497] added read/write locking to value cache
    Merge in ZBX/zabbix from feature/DEV-1639-5.0 to release/5.0
    
    * commit '50df03c3cfe89dcb755b734159cf364532f5d00d':
      .......... [DEV-1639] added component to changelog entry
      ........S. [DEV-1639] fixed warning
      ........S. [DEV-1639] fixed item update during valuecache read lock,                       fixed item removal with disabled value cache
      ........S. [DEV-1639] moved time() call outside loop/lock when updating statistics
      ........S. [DEV-1639] fixed possibility of item being removed by housekeeper when item is added and data has not been yet retrieved
      ........S. [DEV-1639] changed write lock to read lock when retrieving value cache statistics
      ........S. [DEV-1639] fixed item removal from value cache on data retrieval failure
      ........S. [DEV-1639] fixed value cache item cleanup on data retrieval failure
      ........S. [DEV-1639] added value cache rwlock to diaginfo stats
      ........S. [DEV-1639] fixed indentation
      ........S. [DEV-1639] added const
      .......... [ZBXNEXT-6497] added changelog entry
      ........S. [DEV-1639] removed valuecache item state
      ........S. [DEV-1639] added read/write locks to value cache
    
    (cherry picked from commit 36ad10a84cba20853fd9e2d82ca14298e688b07a)

--- tests/libs/zbxdbcache/zbx_vc_get_value.c
+++ tests/libs/zbxdbcache/zbx_vc_get_value.c
@@ -52,6 +52,9 @@ void	zbx_mock_test_entry(void **state)
 	/* set small cache size to force smaller cache free request size (5% of cache size) */
 	CONFIG_VALUE_CACHE_SIZE = ZBX_KIBIBYTE;
 
+	err = zbx_locks_create(&error);
+	zbx_mock_assert_result_eq("Lock initialization failed", SUCCEED, err);
+
 	err = zbx_vc_init(&error);
 	zbx_mock_assert_result_eq("Value cache initialization failed", SUCCEED, err);
 
@@ -89,6 +92,7 @@ void	zbx_mock_test_entry(void **state)
 
 	zbx_vector_history_record_reserve(&returned, 1);
 	err = zbx_vc_get_value(itemid, value_type, &ts, &returned.values[0]);
+	zbx_vc_flush_stats();
 	returned.values_num = 1;
 	zbx_mock_assert_result_eq("zbx_vc_get_values() return value", SUCCEED, err);
 
