5001881044ab18faa8241b3ba2e5f4718f43b97d
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2021-05-17 13:18:30 +0300

    .......... [ZBXNEXT-826]  updated release version [6.0.0alpha1]; updated API version [6.0.0]

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -27,10 +27,10 @@ import (
 
 const (
 	ZABBIX_REVDATE          = "14 May 2021"
-	ZABBIX_VERSION_MAJOR    = 5
-	ZABBIX_VERSION_MINOR    = 4
+	ZABBIX_VERSION_MAJOR    = 6
+	ZABBIX_VERSION_MINOR    = 0
 	ZABBIX_VERSION_PATCH    = 0
-	ZABBIX_VERSION_RC       = ""
+	ZABBIX_VERSION_RC       = "alpha1"
 	ZABBIX_VERSION_RC_NUM   = "{ZABBIX_RC_NUM}"
 	ZABBIX_VERSION_REVISION = "{ZABBIX_REVISION}"
 	copyrightMessage        = "Copyright (C) 2021 Zabbix SIA\n" +
