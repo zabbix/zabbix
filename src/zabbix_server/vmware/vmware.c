b9c1d76fd0709a81d098bbbe382677af5e6972c9
Michael Veksler <Mihails.Vekslers@zabbix.com>
2021-05-20 13:30:33 +0300

    .......PS. [ZBXNEXT-6517] removed the last folder name from the chain

--- src/zabbix_server/vmware/vmware.c
+++ src/zabbix_server/vmware/vmware.c
@@ -2809,23 +2809,26 @@ static int	vmware_service_get_vm_folder(xmlDoc *xdoc, char **vm_folder)
 
 		if (NULL == (fl = zbx_xml_read_doc_value(xdoc , tmp)))
 		{
+			zbx_free(folder);
 			zbx_free(id_esc);
 			return FAIL;
 		}
 
-		if (NULL == folder)
+		zbx_snprintf(tmp, sizeof(tmp), ZBX_XPATH_GET_FOLDER_PARENTID("%s"), id_esc);
+		zbx_free(id_esc);
+		id = zbx_xml_read_doc_value(xdoc , tmp);
+
+		if (NULL == folder)	/* We always resolve the first 'Folder' name */
 		{
 			folder = fl;
 			fl = NULL;
 		}
-		else
+		else if (NULL != id)	/* We do not include the last default 'Folder' */
 			folder = zbx_dsprintf(folder, "%s/%s", fl, folder);
 
 		zbx_free(fl);
-		zbx_snprintf(tmp, sizeof(tmp), ZBX_XPATH_GET_FOLDER_PARENTID("%s"), id_esc);
-		zbx_free(id_esc);
 	}
-	while (NULL != (id = zbx_xml_read_doc_value(xdoc , tmp)));
+	while (NULL != id);
 
 	zbx_free(*vm_folder);
 	*vm_folder = folder;
