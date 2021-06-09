b89f8c6e8f1f3bbff149ef5a6753912ce84e986f
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2021-06-09 23:54:15 +0300

    ....I..PS. [ZBXNEXT-3022,ZBXNEXT-6670] fixed role rule conversion, added index creating with a db patch

--- src/libs/zbxdbupgrade/dbupgrade_5050.c
+++ src/libs/zbxdbupgrade/dbupgrade_5050.c
@@ -60,12 +60,93 @@ static int	DBpatch_5050002(void)
 
 static int	DBpatch_5050003(void)
 {
-	if (ZBX_DB_OK > DBexecute("update role_rule set name='actions.manage_services' where name='ui.default_access'"))
+	return DBcreate_index("service_tag", "service_tag_1", "serviceid", 0);
+}
+
+static void	DBpatch_get_role_rules(zbx_uint64_t roleid, int *ui_default_access, int *actions_default_access,
+		int *ui_conf_services)
+{
+	DB_ROW		row;
+	DB_RESULT	result;
+
+	result = DBselect("select value_int,name from role_rule where roleid=" ZBX_FS_UI64 " and name in "
+			"('ui.default_access', 'actions.default_access', 'ui.configuration.services')", roleid);
+
+	while (NULL != (row = DBfetch(result)))
 	{
-		return FAIL;
+		int	value;
+		char	*name;
+
+		value = atoi(row[0]);
+		name = row[1];
+
+		if (0 == strcmp(name, "ui.default_access"))
+		{
+			*ui_default_access = value;
+		}
+		else if (0 == strcmp(name, "actions.default_access"))
+		{
+			*actions_default_access = value;
+		}
+		else if (0 == strcmp(name, "ui.configuration.services"))
+		{
+			*ui_conf_services = value;
+		}
 	}
 
-	return SUCCEED;
+	DBfree_result(result);
+}
+
+static int	DBpatch_5050004(void)
+{
+	DB_ROW		row;
+	DB_RESULT	result;
+	int	i, ret = SUCCEED;
+
+	result = DBselect("select distinct roleid from role_rule");
+
+	while (NULL != (row = DBfetch(result)))
+	{
+		zbx_int64_t	roleid;
+		int ui_def_access = -1, act_def_access = -1, ui_conf_services = -1;
+
+		ZBX_STR2UINT64(roleid, row[0]);
+
+		DBpatch_get_role_rules(roleid, &ui_def_access, &act_def_access, &ui_conf_services);
+
+		if (ui_def_access == act_def_access && -1 != ui_conf_services)
+		{
+			if (ZBX_DB_OK > DBexecute("update role_rule set name='actions.manage_services' where"
+					" roleid=" ZBX_FS_UI64 " and name='ui.configuration.services'", roleid))
+			{
+				ret = FAIL;
+				goto out;
+			}
+		}
+		else if (1 == ui_def_access && -1 == ui_conf_services && 0 == act_def_access)
+		{
+			if (ZBX_DB_OK > DBexecute("insert into role_rule (role_ruleid, roleid, type, name, value_int, "
+					"value_str, value_moduleid) values (" ZBX_FS_UI64 "," ZBX_FS_UI64 ",0,"
+					"'actions.manage_services',1,'',NULL)", DBget_maxid("role_rule"), roleid))
+			{
+				ret = FAIL;
+				goto out;
+			}
+		}
+		else if (0 == ui_def_access && 1 == ui_conf_services && 1 == act_def_access)
+		{
+			if (ZBX_DB_OK > DBexecute("delete from role_rule where roleid=2 and "
+					"name='ui.configuration.services'"))
+			{
+				ret = FAIL;
+				goto out;
+			}
+		}
+
+	}
+out:
+	DBfree_result(result);
+	return ret;
 }
 
 #endif
@@ -78,5 +159,6 @@ DBPATCH_ADD(5050000, 0, 1)
 DBPATCH_ADD(5050001, 0, 1)
 DBPATCH_ADD(5050002, 0, 1)
 DBPATCH_ADD(5050003, 0, 1)
+DBPATCH_ADD(5050004, 0, 1)
 
 DBPATCH_END()
