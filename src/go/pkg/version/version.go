983c9cc68ace59de06164b8caaa75b11fcc89fec
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2021-06-21 11:05:42 +0300

    .......... [ZBXNEXT-826] updated release date [5.2.7rc1]

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -29,7 +29,7 @@ import (
 
 const (
 	APPLICATION_NAME        = "Zabbix Agent"
-	ZABBIX_REVDATE          = "29 March 2021"
+	ZABBIX_REVDATE          = "21 June 2021"
 	ZABBIX_VERSION_MAJOR    = 5
 	ZABBIX_VERSION_MINOR    = 2
 	ZABBIX_VERSION_PATCH    = 7
--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,7 +22,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "29 March 2021";
+	static final String REVISION_DATE = "21 June 2021";
 	static final String REVISION = "{ZABBIX_REVISION}";
 	static final String VERSION = "5.2.7rc1";
 
