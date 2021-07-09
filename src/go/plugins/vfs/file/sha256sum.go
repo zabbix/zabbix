e6064df5161d4fdcac1be78902ed11b4087105d5
Michael Veksler <Mihails.Vekslers@zabbix.com>
2021-07-09 17:43:55 +0300

    ...G...... [ZBXNEXT-6688] added sha256 file for vfs.file.cksum for Agent 2

---
+++ src/go/plugins/vfs/file/sha256sum.go
@@ -0,0 +1,47 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2021 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+package file
+
+import (
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
+	"time"
+
+	"zabbix.com/pkg/std"
+)
+
+func sha256sum(file std.File, start time.Time, timeout int) (result interface{}, err error) {
+	var bnum int64
+	bnum = 16 * 1024
+	buf := make([]byte, bnum)
+
+	hash := sha256.New()
+
+	for bnum > 0 {
+		bnum, _ = io.CopyBuffer(hash, file, buf)
+		if time.Since(start) > time.Duration(timeout)*time.Second {
+			return nil, errors.New("Timeout while processing item")
+		}
+	}
+
+	return fmt.Sprintf("%x", hash.Sum(nil)), nil
+}
