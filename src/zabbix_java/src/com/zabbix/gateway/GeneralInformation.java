ba940a28f3c2cd2bf19434d5ec1373ab2405afcc
Jurijs Klopovskis <jurijs.klopovskis@zabbix.com>
2021-07-19 10:01:37 +0300

    .......... [ZBXNEXT-826] changed release version to 5.4.3rc2

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "16 July 2021";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "5.4.3";
+	static final String VERSION = "5.4.3rc2";
 
 	static void printVersion()
 	{
