d4608fee66c3e06e64e09f30f6d9184e773ef9ea
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2021-07-21 14:18:23 +0300

    .......... [ZBXNEXT-826] updated release version [5.4.4rc1]; updated API version [5.4.4]

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "21 July 2021";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "5.4.3";
+	static final String VERSION = "5.4.4rc1";
 
 	static void printVersion()
 	{
