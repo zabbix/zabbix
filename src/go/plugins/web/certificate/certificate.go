fcc118bc414f11ffbc7c06bfe096c2d85f142923
Eriks Sneiders <eriks.sneiders@zabbix.com>
2021-08-02 13:54:27 +0300

    ...G...... [ZBXNEXT-6708] fixing sub issues
    (cherry picked from commit 83ebeb205e2bdfdb2a174eb3efb22f0fff8219ec)

--- src/go/plugins/web/certificate/certificate.go
+++ src/go/plugins/web/certificate/certificate.go
@@ -73,6 +73,14 @@ type Plugin struct {
 	options Options
 }
 
+const (
+	dateFormat         = "Jan 02 15:04:05 2006 GMT"
+	allParameters      = 3
+	noThirdParameter   = 2
+	onlyFirstParameter = 1
+	emptyParameters    = 0
+)
+
 var impl Plugin
 
 func (p *Plugin) Configure(global *plugin.GlobalOptions, options interface{}) {
@@ -81,6 +89,7 @@ func (p *Plugin) Configure(global *plugin.GlobalOptions, options interface{}) {
 
 func (p *Plugin) Validate(options interface{}) error {
 	var o Options
+
 	return conf.Unmarshal(options, &o)
 }
 
@@ -89,40 +98,47 @@ func (p *Plugin) Export(key string, params []string, ctx plugin.ContextProvider)
 		return nil, plugin.UnsupportedMetricError
 	}
 
-	hostname, port, dnsName, err := getParameters(params)
+	return p.webCertificateGet(params)
+}
+
+func (p *Plugin) webCertificateGet(params []string) (interface{}, error) {
+	hostname, port, domain, err := getParameters(params)
 	if err != nil {
 		return nil, zbxerr.ErrorInvalidParams.Wrap(err)
 	}
 
-	cert, err := getCertificatesPEM(fmt.Sprintf("%s:%s", hostname, port), p.options.Timeout)
+	certs, err := getCertificatesPEM(fmt.Sprintf("%s:%s", hostname, port), domain, p.options.Timeout)
 	if err != nil {
 		return nil, zbxerr.ErrorCannotFetchData.Wrap(err)
 	}
 
+	if len(certs) < 1 {
+		return nil, zbxerr.ErrorCannotFetchData
+	}
+
+	leaf := certs[0]
+	leaf.KeyUsage = x509.KeyUsageCertSign
+
 	var o Output
-
-	cert.KeyUsage = x509.KeyUsageCertSign
-
 	o.X509 = Cert{
-		cert.Version, fmt.Sprintf("%x", cert.SerialNumber.Bytes()), cert.SignatureAlgorithm.String(), cert.Issuer.ToRDNSequence().String(),
-		CertTime{cert.NotBefore.Format(time.RFC822), cert.NotBefore.Unix()},
-		CertTime{cert.NotAfter.Format(time.RFC822), cert.NotAfter.Unix()}, cert.Subject.ToRDNSequence().String(),
-		cert.PublicKeyAlgorithm.String(), cert.DNSNames,
+		leaf.Version,
+		fmt.Sprintf("%x", leaf.SerialNumber.Bytes()),
+		leaf.SignatureAlgorithm.String(),
+		leaf.Issuer.ToRDNSequence().String(),
+		CertTime{leaf.NotBefore.UTC().Format(dateFormat), leaf.NotBefore.Unix()},
+		CertTime{leaf.NotAfter.UTC().Format(dateFormat), leaf.NotAfter.Unix()},
+		leaf.Subject.ToRDNSequence().String(),
+		leaf.PublicKeyAlgorithm.String(),
+		leaf.DNSNames,
 	}
 
-	if _, err := cert.Verify(x509.VerifyOptions{DNSName: dnsName}); err != nil {
-		if errors.As(err, &x509.UnknownAuthorityError{}) && o.X509.Subject == o.X509.Issuer {
-			o.Result = ValidationResult{"valid-but-self-signed",
-				"certificate verified successfully, but determined to be self signed"}
-		} else {
-			o.Result = ValidationResult{"invalid", fmt.Sprintf("failed to verify certificate: %s", err.Error())}
-		}
-	} else {
-		o.Result = ValidationResult{"valid", "certificate verified successfully"}
-	}
+	o.Result = getValidationResult(
+		leaf, createVerificationOptions(domain, certs),
+		leaf.Subject.ToRDNSequence().String(), leaf.Issuer.ToRDNSequence().String(),
+	)
 
-	o.Sha1Fingerprint = fmt.Sprintf("%x", sha1.Sum(cert.Raw))
-	o.Sha256Fingerprint = fmt.Sprintf("%x", sha256.Sum256(cert.Raw))
+	o.Sha1Fingerprint = fmt.Sprintf("%x", sha1.Sum(leaf.Raw))
+	o.Sha256Fingerprint = fmt.Sprintf("%x", sha256.Sum256(leaf.Raw))
 
 	b, err := json.Marshal(o)
 	if err != nil {
@@ -132,23 +148,54 @@ func (p *Plugin) Export(key string, params []string, ctx plugin.ContextProvider)
 	return string(b), nil
 }
 
-func getParameters(params []string) (hostname, port, dnsName string, err error) {
-	switch len(params) {
-	case 3:
-		if params[2] != "" {
-			hostname, port, err = validateURL(params[2], params[1])
-			dnsName = params[0]
+func createVerificationOptions(domain string, certs []*x509.Certificate) x509.VerifyOptions {
+	opts := x509.VerifyOptions{
+		DNSName:       domain,
+		Intermediates: x509.NewCertPool(),
+	}
+
+	for _, cert := range certs[1:] {
+		opts.Intermediates.AddCert(cert)
+	}
+
+	return opts
+}
+
+func getValidationResult(leaf *x509.Certificate, opts x509.VerifyOptions, subject, issuer string) ValidationResult {
+	var out ValidationResult
+
+	if _, err := leaf.Verify(opts); err != nil {
+		if errors.As(err, &x509.UnknownAuthorityError{}) && subject == issuer {
+			out = ValidationResult{
+				"valid-but-self-signed",
+				"certificate verified successfully, but determined to be self signed",
+			}
 		} else {
-			hostname, port, err = validateURL(params[0], params[1])
-			dnsName = hostname
+			out = ValidationResult{"invalid", fmt.Sprintf("failed to verify certificate: %s", err.Error())}
 		}
-	case 2:
-		hostname, port, err = validateURL(params[0], params[1])
-		dnsName = hostname
-	case 1:
-		hostname, port, err = validateURL(params[0], "")
-		dnsName = hostname
-	case 0:
+	} else {
+		out = ValidationResult{"valid", "certificate verified successfully"}
+	}
+
+	return out
+}
+
+func getParameters(params []string) (hostname, port, domain string, err error) {
+	switch len(params) {
+	case allParameters:
+		hostname, port, err = parseURL(params[0], params[1])
+		domain = hostname
+
+		if params[2] != "" {
+			hostname, port, err = parseURL(params[2], params[1])
+		}
+	case noThirdParameter:
+		hostname, port, err = parseURL(params[0], params[1])
+		domain = hostname
+	case onlyFirstParameter:
+		hostname, port, err = parseURL(params[0], "")
+		domain = hostname
+	case emptyParameters:
 		err = zbxerr.ErrorTooFewParameters
 	default:
 		err = zbxerr.ErrorTooManyParameters
@@ -157,43 +204,42 @@ func getParameters(params []string) (hostname, port, dnsName string, err error) 
 	return
 }
 
-func validateURL(url, port string) (string, string, error) {
-	out, err := uri.New(url, &uri.Defaults{Port: port, Scheme: "https"})
+func parseURL(url, port string) (string, string, error) {
+	uri, err := uri.New(url, &uri.Defaults{Port: port, Scheme: "https"})
 	if err != nil {
 		return "", "", err
 	}
 
-	if out.Scheme() != "" && out.Scheme() != "https" {
+	if uri.Scheme() != "" && uri.Scheme() != "https" {
 		return "", "", errors.New("scheme must be https")
 	}
 
-	if out.Port() != "" && port != "" && out.Port() != port {
+	return getHostAndPort(uri, port)
+}
+
+func getHostAndPort(uri *uri.URI, port string) (string, string, error) {
+	if uri.Port() != "" && port != "" && uri.Port() != port {
 		return "", "", errors.New("port set incorrectly")
 	}
 
-	if out.Port() == "" && port == "" {
-		return out.Host(), "443", nil
+	if uri.Port() == "" && port == "" {
+		return uri.Host(), "443", nil
 	}
 
-	return out.Host(), out.Port(), nil
+	return uri.Host(), uri.Port(), nil
 }
 
-func getCertificatesPEM(address string, timeout int) (*x509.Certificate, error) {
+func getCertificatesPEM(address, domain string, timeout int) ([]*x509.Certificate, error) {
 	var dialer net.Dialer
 	dialer.Timeout = time.Duration(timeout) * time.Second
 
-	conn, err := tls.DialWithDialer(&dialer, "tcp", address, &tls.Config{InsecureSkipVerify: true})
+	conn, err := tls.DialWithDialer(&dialer, "tcp", address, &tls.Config{InsecureSkipVerify: true, ServerName: domain})
 	if err != nil {
 		return nil, err
 	}
 	defer conn.Close()
 
-	certs := conn.ConnectionState().PeerCertificates
-	if len(certs) < 1 {
-		return nil, zbxerr.ErrorCannotFetchData
-	}
-
-	return certs[0], nil
+	return conn.ConnectionState().PeerCertificates, nil
 }
 
 func init() {
--- src/go/plugins/web/certificate/certificate_test.go
+++ src/go/plugins/web/certificate/certificate_test.go
@@ -18,7 +18,52 @@
 **/
 package webcertificate
 
-import "testing"
+import (
+	"testing"
+
+	"zabbix.com/pkg/uri"
+)
+
+func Test_getParameters(t *testing.T) {
+	type args struct {
+		params []string
+	}
+	tests := []struct {
+		name         string
+		args         args
+		wantHostname string
+		wantPort     string
+		wantDomain   string
+		wantErr      bool
+	}{
+		{"+only_hostname", args{[]string{"example.com"}}, "example.com", "443", "example.com", false},
+		{"+only_ip", args{[]string{"127.0.0.1"}}, "127.0.0.1", "443", "127.0.0.1", false},
+		{"+full", args{[]string{"example.com", "443", "127.0.0.1"}}, "127.0.0.1", "443", "example.com", false},
+		{"+empty_port", args{[]string{"example.com", "", "127.0.0.1"}}, "127.0.0.1", "443", "example.com", false},
+		{"+empty_port_and_ip", args{[]string{"example.com", "", ""}}, "example.com", "443", "example.com", false},
+		{"-too_many_params", args{[]string{"example.com", "443", "127.0.0.1", "foobar"}}, "", "", "", true},
+		{"-too_few_params", args{[]string{}}, "", "", "", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotHostname, gotPort, gotDomain, err := getParameters(tt.args.params)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getParameters() error = %v, wantErr %v", err, tt.wantErr)
+
+				return
+			}
+			if gotHostname != tt.wantHostname {
+				t.Errorf("getParameters() gotHostname = %v, want %v", gotHostname, tt.wantHostname)
+			}
+			if gotPort != tt.wantPort {
+				t.Errorf("getParameters() gotPort = %v, want %v", gotPort, tt.wantPort)
+			}
+			if gotDomain != tt.wantDomain {
+				t.Errorf("getParameters() gotDomain = %v, want %v", gotDomain, tt.wantDomain)
+			}
+		})
+	}
+}
 
 func Test_validateURL(t *testing.T) {
 	type args struct {
@@ -56,16 +101,53 @@ func Test_validateURL(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1, err := validateURL(tt.args.url, tt.args.port)
+			got, got1, err := parseURL(tt.args.url, tt.args.port)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("validateURL() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("parseURL() error = %v, wantErr %v", err, tt.wantErr)
+
 				return
 			}
 			if got != tt.host {
-				t.Errorf("validateURL() got = %v, want %v", got, tt.host)
+				t.Errorf("parseURL() got = %v, want %v", got, tt.host)
 			}
 			if got1 != tt.port {
-				t.Errorf("validateURL() got1 = %v, want %v", got1, tt.port)
+				t.Errorf("parseURL() got1 = %v, want %v", got1, tt.port)
+			}
+		})
+	}
+}
+
+func Test_getHostAndPort(t *testing.T) {
+	type args struct {
+		rawUri string
+		port   string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantHost string
+		wantPort string
+		wantErr  bool
+	}{
+		{"+full", args{"https://example.com:443", "443"}, "example.com", "443", false},
+		{"+empty_port", args{"example.com:443", ""}, "example.com", "443", false},
+		{"+empty_port", args{"example.com", ""}, "example.com", "443", false},
+		{"-port_mismatch", args{"example.com:443", "332"}, "", "", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u, _ := uri.New(tt.args.rawUri, nil)
+			got, got1, err := getHostAndPort(u, tt.args.port)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getHostAndPort() error = %v, wantErr %v", err, tt.wantErr)
+
+				return
+			}
+			if got != tt.wantHost {
+				t.Errorf("getHostAndPort() host = %v, want %v", got, tt.wantHost)
+			}
+			if got1 != tt.wantPort {
+				t.Errorf("getHostAndPort() port = %v, want %v", got1, tt.wantPort)
 			}
 		})
 	}
