fff9ee021e5e6eb87ce4b8394f91a6210bacf975
Janis Freibergs <janis.freibergs@zabbix.com>
2021-08-12 14:47:55 +0300

    ..F....... [ZBXNEXT-6757] de-duplicated repeating parts of host create & update by extending from common CControllerHostUpdateGeneral; refactoring/code style

--- ui/app/controllers/CControllerHostCreate.php
+++ ui/app/controllers/CControllerHostCreate.php
@@ -22,63 +22,12 @@
 /**
  * Controller for host creation.
  */
-class CControllerHostCreate extends CController {
+class CControllerHostCreate extends CControllerHostUpdateGeneral {
 
 	protected function checkInput(): bool {
-		$fields = [
-			'host'				=> 'required|db hosts.host|not_empty',
-			'visiblename'		=> 'db hosts.name',
-			'description'		=> 'db hosts.description',
-			'status'			=> 'required|db hosts.status|in '.implode(',', [HOST_STATUS_MONITORED,
-										HOST_STATUS_NOT_MONITORED
-									]),
-			'proxy_hostid'		=> 'db hosts.proxy_hostid',
-			'interfaces'		=> 'array',
-			'mainInterfaces'	=> 'array',
-			'groups'			=> 'required|array',
-			'tags'				=> 'array',
-			'add_templates'		=> 'array_db hosts.hostid',
-			'ipmi_authtype'		=> 'in '.implode(',', [IPMI_AUTHTYPE_DEFAULT, IPMI_AUTHTYPE_NONE, IPMI_AUTHTYPE_MD2,
-										IPMI_AUTHTYPE_MD5, IPMI_AUTHTYPE_STRAIGHT, IPMI_AUTHTYPE_OEM,
-										IPMI_AUTHTYPE_RMCP_PLUS
-									]),
-			'ipmi_privilege'	=> 'in '.implode(',', [IPMI_PRIVILEGE_CALLBACK, IPMI_PRIVILEGE_USER,
-										IPMI_PRIVILEGE_OPERATOR, IPMI_PRIVILEGE_ADMIN, IPMI_PRIVILEGE_OEM
-									]),
-			'ipmi_username'		=> 'db hosts.ipmi_username',
-			'ipmi_password'		=> 'db hosts.ipmi_password',
-			'tls_connect'		=> 'db hosts.tls_connect|in '.implode(',', [HOST_ENCRYPTION_NONE, HOST_ENCRYPTION_PSK,
-										HOST_ENCRYPTION_CERTIFICATE
-									]),
-			'tls_accept'		=> 'db hosts.tls_accept|ge 0|le '.
-										(0 | HOST_ENCRYPTION_NONE | HOST_ENCRYPTION_PSK | HOST_ENCRYPTION_CERTIFICATE),
-			'tls_subject'		=> 'db hosts.tls_subject',
-			'tls_issuer'		=> 'db hosts.tls_issuer',
-			'tls_psk_identity'	=> 'db hosts.tls_psk_identity',
-			'tls_psk'			=> 'db hosts.tls_psk',
-			'inventory_mode'	=> 'db host_inventory.inventory_mode|in '.implode(',', [HOST_INVENTORY_DISABLED,
-										HOST_INVENTORY_MANUAL, HOST_INVENTORY_AUTOMATIC
-									]),
-			'host_inventory'	=> 'array',
-			'macros'			=> 'array',
-			'valuemaps'			=> 'array',
-			'full_clone'		=> 'in 1',
-			'clone_hostid'		=> 'db hosts.hostid'
-		];
+		$fields = ['host' => 'required|db hosts.host|not_empty'] + self::getValidationFields();
 
-		$ret = $this->validateInput($fields);
-
-		if (!$ret) {
-			$output = [];
-
-			if (($messages = getMessages()) !== null) {
-				$output['errors'] = $messages->toString();
-			}
-
-			$this->setResponse((new CControllerResponseData(['main_block' => json_encode($output)]))->disableView());
-		}
-
-		return $ret;
+		return parent::checkInputFields($fields);
 	}
 
 	protected function checkPermissions(): bool {
@@ -98,21 +47,16 @@ class CControllerHostCreate extends CController {
 		return $ret;
 	}
 
-	/**
-	 * Data collection, validation and creation of new host. May produce various validation and integrity exceptions,
-	 * which are stored as error messages by the underlying mechanisms.
-	 *
-	 * @return array Response data, empty on errors.
-	 */
-	private function createHost(): array {
+	protected function doAction(): void {
+		$output = [];
 		$host = array_filter([
 			'status' => $this->getInput('status', HOST_STATUS_NOT_MONITORED),
 			'proxy_hostid' => $this->getInput('proxy_hostid', 0),
-			'groups' => $this->processHostGroups(),
-			'interfaces' => $this->processHostInterfaces(),
-			'tags' => $this->processTags(),
-			'templates' => zbx_toObject($this->getInput('add_templates', []), 'templateid'),
-			'macros' => $this->processUserMacros(),
+			'groups' => $this->processHostGroups($this->getInput('groups', [])),
+			'interfaces' => $this->processHostInterfaces($this->getInput('interfaces', [])),
+			'tags' => $this->processTags($this->getInput('tags', [])),
+			'templates' => $this->processTemplates([$this->getInput('add_templates', [])]),
+			'macros' => $this->processUserMacros($this->getInput('macros', [])),
 			'inventory' => ($this->getInput('inventory_mode', HOST_INVENTORY_DISABLED) != HOST_INVENTORY_DISABLED)
 				? $this->getInput('host_inventory', [])
 				: [],
@@ -155,28 +99,14 @@ class CControllerHostCreate extends CController {
 				$details[] = $message['message'];
 			}
 
-			return [
-				'hostid' => $hostids['hostids'][0],
-				'message' => makeMessageBox(true, [], _('Host added'), true, false)->toString(),
+			$output = [
+				'message' => makeMessageBox(true, $messages, _('Host added'), true, false)->toString(),
 				'message_raw' => _('Host added'),
 				'details' => $details
 			];
 		}
 
-		return [];
-	}
-
-	protected function doAction(): void {
-		$failed = false;
-
-		try {
-			$output = $this->createHost();
-		}
-		catch (Exception $exception) {
-			$failed = true;
-		}
-
-		if ($failed && ($messages = getMessages()) !== null) {
+		if (!$output && ($messages = getMessages()) !== null) {
 			$output = ['errors' => $messages->toString()];
 		}
 
@@ -209,100 +139,6 @@ class CControllerHostCreate extends CController {
 		}
 
 		return $host;
-	}
-
-	/**
-	 * Prepare host interfaces for host.create method.
-	 *
-	 * @return array
-	 */
-	private function processHostInterfaces(): array {
-		$interfaces = $this->getInput('interfaces', []);
-
-		foreach ($interfaces as $key => $interface) {
-			if ($interface['type'] == INTERFACE_TYPE_SNMP) {
-				if (!array_key_exists('details', $interface)) {
-					$interface['details'] = [];
-				}
-
-				$interfaces[$key]['details']['bulk'] = array_key_exists('bulk', $interface['details'])
-					? SNMP_BULK_ENABLED
-					: SNMP_BULK_DISABLED;
-			}
-
-			if ($interface['isNew']) {
-				unset($interfaces[$key]['interfaceid']);
-			}
-
-			unset($interfaces[$key]['isNew']);
-			$interfaces[$key]['main'] = 0;
-		}
-
-		$main_interfaces = $this->getInput('mainInterfaces', []);
-		foreach ([INTERFACE_TYPE_AGENT, INTERFACE_TYPE_SNMP, INTERFACE_TYPE_JMX, INTERFACE_TYPE_IPMI] as $type) {
-			if (array_key_exists($type, $main_interfaces) && array_key_exists($main_interfaces[$type], $interfaces)) {
-				$interfaces[$main_interfaces[$type]]['main'] = INTERFACE_PRIMARY;
-			}
-		}
-
-		return $interfaces;
-	}
-
-	/**
-	 * Prepare host level user macros for host.create method.
-	 *
-	 * @return array
-	 */
-	private function processUserMacros(): array {
-		return array_filter(cleanInheritedMacros($this->getInput('macros', [])),
-			function (array $macro): bool {
-				return (bool) array_filter(
-					array_intersect_key($macro, array_flip(['hostmacroid', 'macro', 'value', 'description']))
-				);
-			}
-		);
-	}
-
-	/**
-	 * Prepare host tags for host.create method.
-	 *
-	 * @return array
-	 */
-	private function processTags(): array {
-		return array_filter($this->getInput('tags', []),
-			function (array $tag): bool {
-				return ($tag['tag'] !== '' || $tag['value'] !== '');
-			}
-		);
-	}
-
-	/**
-	 * Prepare host groups for host.create method.
-	 *
-	 * @return array
-	 */
-	private function processHostGroups(): array {
-		$new_groups = [];
-		$groups = $this->getInput('groups', []);
-
-		foreach ($groups as $idx => $group) {
-			if (is_array($group) && array_key_exists('new', $group)) {
-				$new_groups[] = ['name' => $group['new']];
-				unset($groups[$idx]);
-			}
-		}
-
-		if ($new_groups) {
-			$new_groupid = API::HostGroup()->create($new_groups);
-
-			if (!$new_groupid) {
-				throw new Exception();
-			}
-
-			$groups = array_merge($groups, $new_groupid['groupids']);
-		}
-
-		return zbx_toObject($groups, 'groupid');
 	}
 
 	/**
@@ -374,7 +210,7 @@ class CControllerHostCreate extends CController {
 
 		// Copy graphs.
 		$db_graphs = API::Graph()->get([
-			'output' => [],
+			'output' => ['graphid'],
 			'selectHosts' => ['hostid'],
 			'selectItems' => ['type'],
 			'hostids' => $src_hostid,
--- ui/app/controllers/CControllerHostUpdate.php
+++ ui/app/controllers/CControllerHostUpdate.php
@@ -22,62 +22,12 @@
 /**
  * Controller for host update.
  */
-class CControllerHostUpdate extends CController {
+class CControllerHostUpdate extends CControllerHostUpdateGeneral {
 
 	protected function checkInput(): bool {
-		$fields = [
-			'hostid'			=> 'required|db hosts.hostid',
-			'host'				=> 'required|db hosts.host|not_empty',
-			'visiblename'		=> 'db hosts.name',
-			'description'		=> 'db hosts.description',
-			'status'			=> 'required|db hosts.status|in '.implode(',', [HOST_STATUS_MONITORED, HOST_STATUS_NOT_MONITORED]),
-			'proxy_hostid'		=> 'db hosts.proxy_hostid',
-			'interfaces'		=> 'array',
-			'mainInterfaces'	=> 'array',
-			'groups'			=> 'required|array',
-			'tags'				=> 'array',
-			'templates'			=> 'array_db hosts.hostid',
-			'add_templates'		=> 'array_db hosts.hostid',
-			'clear_templates'	=> 'array_db hosts.hostid',
-			'ipmi_authtype'		=> 'in '.implode(',', [IPMI_AUTHTYPE_DEFAULT, IPMI_AUTHTYPE_NONE, IPMI_AUTHTYPE_MD2,
-									IPMI_AUTHTYPE_MD5, IPMI_AUTHTYPE_STRAIGHT, IPMI_AUTHTYPE_OEM,
-									IPMI_AUTHTYPE_RMCP_PLUS
-								]),
-			'ipmi_privilege'	=> 'in '.implode(',', [IPMI_PRIVILEGE_CALLBACK, IPMI_PRIVILEGE_USER,
-									IPMI_PRIVILEGE_OPERATOR, IPMI_PRIVILEGE_ADMIN, IPMI_PRIVILEGE_OEM
-								]),
-			'ipmi_username'		=> 'db hosts.ipmi_username',
-			'ipmi_password'		=> 'db hosts.ipmi_password',
-			'tls_connect'		=> 'db hosts.tls_connect|in '.implode(',', [HOST_ENCRYPTION_NONE, HOST_ENCRYPTION_PSK,
-									HOST_ENCRYPTION_CERTIFICATE
-								]),
-			'tls_accept'		=> 'db hosts.tls_accept|ge 0|le '.
-										(0 | HOST_ENCRYPTION_NONE | HOST_ENCRYPTION_PSK | HOST_ENCRYPTION_CERTIFICATE),
-			'tls_subject'		=> 'db hosts.tls_subject',
-			'tls_issuer'		=> 'db hosts.tls_issuer',
-			'tls_psk_identity'	=> 'db hosts.tls_psk_identity',
-			'tls_psk'			=> 'db hosts.tls_psk',
-			'inventory_mode'	=> 'db host_inventory.inventory_mode|in '.implode(',', [HOST_INVENTORY_DISABLED,
-										HOST_INVENTORY_MANUAL, HOST_INVENTORY_AUTOMATIC
-								]),
-			'host_inventory'	=> 'array',
-			'macros'			=> 'array',
-			'valuemaps'			=> 'array'
-		];
+		$fields = ['hostid' => 'required|db hosts.hostid'] + self::getValidationFields();
 
-		$ret = $this->validateInput($fields);
-
-		if (!$ret) {
-			$output = [];
-
-			if (($messages = getMessages()) !== null) {
-				$output['errors'] = $messages->toString();
-			}
-
-			$this->setResponse((new CControllerResponseData(['main_block' => json_encode($output)]))->disableView());
-		}
-
-		return $ret;
+		return parent::checkInputFields($fields);
 	}
 
 	protected function checkPermissions(): bool {
@@ -103,25 +53,22 @@ class CControllerHostUpdate extends CController {
 		return true;
 	}
 
-	/**
-	 * Data collection, validation and update of host. May produce various validation and integrity exceptions,
-	 * which are stored as error messages by the underlying mechanisms.
-	 *
-	 * @return array Response data, empty on errors.
-	 */
-	private function updateHost(): array {
+	protected function doAction(): void {
+		$output = [];
 		$host = [
 			'hostid' => $this->host['hostid'],
 			'host' => $this->getInput('host', $this->host['host']),
 			'name' => $this->getInput('visiblename', $this->host['name']),
 			'status' => $this->getInput('status', $this->host['status']),
 			'proxy_hostid' => $this->getInput('proxy_hostid', $this->host['proxy_hostid']),
-			'groups' => $this->processHostGroups(),
-			'interfaces' => $this->processHostInterfaces(),
-			'tags' => $this->processTags(),
-			'templates' => $this->processTemplates(),
+			'groups' => $this->processHostGroups($this->getInput('groups', [])),
+			'interfaces' => $this->processHostInterfaces($this->getInput('interfaces', [])),
+			'tags' => $this->processTags($this->getInput('tags', [])),
+			'templates' => $this->processTemplates([$this->getInput('add_templates', []),
+				$this->getInput('templates', [])
+			]),
 			'clear_templates' => zbx_toObject($this->getInput('clear_templates', []), 'templateid'),
-			'macros' => $this->processUserMacros(),
+			'macros' => $this->processUserMacros($this->getInput('macros', [])),
 			'inventory' => ($this->getInput('inventory_mode', $this->host['inventory_mode']) != HOST_INVENTORY_DISABLED)
 				? $this->getInput('host_inventory', [])
 				: [],
@@ -165,138 +112,18 @@ class CControllerHostUpdate extends CController {
 				$details[] = $message['message'];
 			}
 
-			return [
-				'hostid' => $hostids['hostids'][0],
+			$output = [
 				'message' => makeMessageBox(true, $messages, _('Host updated'), true, false)->toString(),
 				'message_raw' => _('Host updated'),
 				'details' => $details
 			];
 		}
 
-		return [];
-	}
-
-	protected function doAction(): void {
-		$failed = false;
-
-		try {
-			$output = $this->updateHost();
-		}
-		catch (Exception $exception) {
-			$failed = true;
-		}
-
-		if ($failed && ($messages = getMessages()) !== null) {
+		if (!$output && ($messages = getMessages()) !== null) {
 			$output = ['errors' => $messages->toString()];
 		}
 
-		// Set response.
 		$this->setResponse((new CControllerResponseData(['main_block' => json_encode($output)]))->disableView());
-	}
-
-	/**
-	 * Prepare host interfaces for host.update method.
-	 *
-	 * @return array
-	 */
-	private function processHostInterfaces(): array {
-		$interfaces = $this->getInput('interfaces', []);
-
-		foreach ($interfaces as $key => $interface) {
-			if ($interface['type'] == INTERFACE_TYPE_SNMP) {
-				if (!array_key_exists('details', $interface)) {
-					$interface['details'] = [];
-				}
-
-				$interfaces[$key]['details']['bulk'] = array_key_exists('bulk', $interface['details'])
-					? SNMP_BULK_ENABLED
-					: SNMP_BULK_DISABLED;
-			}
-
-			if ($interface['isNew']) {
-				unset($interfaces[$key]['interfaceid']);
-			}
-
-			unset($interfaces[$key]['isNew']);
-			$interfaces[$key]['main'] = 0;
-		}
-
-		$main_interfaces = $this->getInput('mainInterfaces', []);
-		foreach ([INTERFACE_TYPE_AGENT, INTERFACE_TYPE_SNMP, INTERFACE_TYPE_JMX, INTERFACE_TYPE_IPMI] as $type) {
-			if (array_key_exists($type, $main_interfaces) && array_key_exists($main_interfaces[$type], $interfaces)) {
-				$interfaces[$main_interfaces[$type]]['main'] = INTERFACE_PRIMARY;
-			}
-		}
-
-		return $interfaces;
-	}
-
-	/**
-	 * Prepare host level user macros for host.update method.
-	 *
-	 * @return array
-	 */
-	private function processUserMacros(): array {
-		return array_filter(cleanInheritedMacros($this->getInput('macros', [])),
-			function (array $macro): bool {
-				return (bool) array_filter(
-					array_intersect_key($macro, array_flip(['hostmacroid', 'macro', 'value', 'description']))
-				);
-			}
-		);
-	}
-
-	/**
-	 * Prepare host tags for host.update method.
-	 *
-	 * @return array
-	 */
-	private function processTags(): array {
-		return array_filter($this->getInput('tags', []),
-			function (array $tag): bool {
-				return ($tag['tag'] !== '' || $tag['value'] !== '');
-			}
-		);
-	}
-
-	/**
-	 * Prepare host groups for host.update method.
-	 *
-	 * @return array
-	 */
-	private function processHostGroups(): array {
-		$new_groups = [];
-		$groups = $this->getInput('groups', []);
-
-		foreach ($groups as $idx => $group) {
-			if (is_array($group) && array_key_exists('new', $group)) {
-				$new_groups[] = ['name' => $group['new']];
-				unset($groups[$idx]);
-			}
-		}
-
-		if ($new_groups) {
-			$new_groupid = API::HostGroup()->create($new_groups);
-
-			if (!$new_groupid) {
-				throw new Exception();
-			}
-
-			$groups = array_merge($groups, $new_groupid['groupids']);
-		}
-
-		return zbx_toObject($groups, 'groupid');
-	}
-
-	/**
-	 * Prepare templates for host.update method.
-	 *
-	 * @return array
-	 */
-	private function processTemplates(): array {
-		return zbx_toObject(array_merge($this->getInput('add_templates', []), $this->getInput('templates', [])),
-			'templateid'
-		);
 	}
 
 	/**
