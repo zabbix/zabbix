cf2451a30884c6a8678c50e7c23717b64ad40e81
Andrejs Kozlovs <andrejs.kozlovs@zabbix.com>
2021-08-12 11:31:11 +0300

    ........S. [ZBXNEXT-6810] removed unused prototype

--- include/db.h
+++ include/db.h
@@ -627,7 +627,6 @@ int	DBadd_graph_item_to_linked_hosts(int gitemid, int hostid);
 int	DBcopy_template_elements(zbx_uint64_t hostid, zbx_vector_uint64_t *lnk_templateids, char **error);
 int	DBdelete_template_elements(zbx_uint64_t hostid, const char *hostname, zbx_vector_uint64_t *del_templateids,
 		char **error);
-int	DBdelete_template_elements_for_lld(zbx_uint64_t hostid, zbx_vector_uint64_t *del_templateids, char **error);
 
 void	DBdelete_items(zbx_vector_uint64_t *itemids);
 void	DBdelete_graphs(zbx_vector_uint64_t *graphids);
