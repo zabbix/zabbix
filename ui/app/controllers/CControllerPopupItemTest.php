4a844b5358af4bb1aa7e773d960c88b7e36a4654
Ivo Kurzemnieks <ivo.kurzemnieks@zabbix.com>
2021-08-16 12:22:37 +0300

    ..F....... [ZBX-19120] implemented a different encoding for XML content macros for http items in item test popup form

--- ui/app/controllers/CControllerPopupItemTest.php
+++ ui/app/controllers/CControllerPopupItemTest.php
@@ -1015,9 +1015,11 @@ abstract class CControllerPopupItemTest extends CController {
 						? $macros_posted[$macro]
 						: '';
 
-					if (($inputs['post_type'] == ZBX_POSTTYPE_JSON || $inputs['post_type'] == ZBX_POSTTYPE_XML)
-							&& $field === 'posts' && !is_numeric($macro_value)) {
+					if ($inputs['post_type'] == ZBX_POSTTYPE_JSON && $field === 'posts' && !is_numeric($macro_value)) {
 						$macro_value = json_encode($macro_value);
+					}
+					elseif ($inputs['post_type'] == ZBX_POSTTYPE_XML && $field === 'posts') {
+						$macro_value = htmlentities($macro_value);
 					}
 
 					$inputs[$field] = substr_replace($inputs[$field], $macro_value, $pos, strlen($macro));
