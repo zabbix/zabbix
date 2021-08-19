3c0d2d2a1617b4e6394ba5415ec9076daa6e77e0
Andrejs Kozlovs <andrejs.kozlovs@zabbix.com>
2021-08-19 10:07:07 +0300

    ........S. [ZBXNEXT-6810] fixed another type2 parameter

--- src/libs/zbxaudit/audit_host.h
+++ src/libs/zbxaudit/audit_host.h
@@ -81,24 +81,24 @@ PREPARE_AUDIT_HOST_INTERFACE_H(funcname, contextname, const char*)						\
 PREPARE_AUDIT_HOST_H(host, AUDIT_RESOURCE_HOST)
 PREPARE_AUDIT_HOST_H(host_prototype, AUDIT_RESOURCE_HOST_PROTOTYPE)
 
-#define PREPARE_AUDIT_HOST_UPDATE_H(resource, type1, type2)						\
+#define PREPARE_AUDIT_HOST_UPDATE_H(resource, type1)						\
 void	zbx_audit_host_update_json_update_##resource(zbx_uint64_t hostid, type1 old_##resource,		\
 		type1 new_##resource);									\
 
-PREPARE_AUDIT_HOST_UPDATE_H(host, const char*, string)
-PREPARE_AUDIT_HOST_UPDATE_H(name, const char*, string)
-PREPARE_AUDIT_HOST_UPDATE_H(proxy_hostid, zbx_uint64_t, uint64)
-PREPARE_AUDIT_HOST_UPDATE_H(ipmi_authtype, int, int)
-PREPARE_AUDIT_HOST_UPDATE_H(ipmi_privilege, int, int)
-PREPARE_AUDIT_HOST_UPDATE_H(ipmi_username, const char*, string)
-PREPARE_AUDIT_HOST_UPDATE_H(ipmi_password, const char*, string)
-PREPARE_AUDIT_HOST_UPDATE_H(tls_connect, int, int)
-PREPARE_AUDIT_HOST_UPDATE_H(tls_accept, int, int)
-PREPARE_AUDIT_HOST_UPDATE_H(tls_issuer, const char*, string)
-PREPARE_AUDIT_HOST_UPDATE_H(tls_subject, const char*, string)
-PREPARE_AUDIT_HOST_UPDATE_H(tls_psk_identity, const char*, string)
-PREPARE_AUDIT_HOST_UPDATE_H(tls_psk, const char*, string)
-PREPARE_AUDIT_HOST_UPDATE_H(custom_interfaces, int, int)
+PREPARE_AUDIT_HOST_UPDATE_H(host, const char*)
+PREPARE_AUDIT_HOST_UPDATE_H(name, const char*)
+PREPARE_AUDIT_HOST_UPDATE_H(proxy_hostid, zbx_uint64_t)
+PREPARE_AUDIT_HOST_UPDATE_H(ipmi_authtype, int)
+PREPARE_AUDIT_HOST_UPDATE_H(ipmi_privilege, int)
+PREPARE_AUDIT_HOST_UPDATE_H(ipmi_username, const char*)
+PREPARE_AUDIT_HOST_UPDATE_H(ipmi_password, const char*)
+PREPARE_AUDIT_HOST_UPDATE_H(tls_connect, int)
+PREPARE_AUDIT_HOST_UPDATE_H(tls_accept, int)
+PREPARE_AUDIT_HOST_UPDATE_H(tls_issuer, const char*)
+PREPARE_AUDIT_HOST_UPDATE_H(tls_subject, const char*)
+PREPARE_AUDIT_HOST_UPDATE_H(tls_psk_identity, const char*)
+PREPARE_AUDIT_HOST_UPDATE_H(tls_psk, const char*)
+PREPARE_AUDIT_HOST_UPDATE_H(custom_interfaces, int)
 #undef PREPARE_AUDIT_HOST_UPDATE_H
 
 void	zbx_audit_host_update_json_delete_interface(zbx_uint64_t hostid, zbx_uint64_t interfaceid);
