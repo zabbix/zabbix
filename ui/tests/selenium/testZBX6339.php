6624dc6cf118e25d65d027d5ec2f9f33cdc26d31
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2021-08-26 18:55:04 +0300

    .......... [ZBXNEXT-686] fixed autotest after last template build

--- ui/tests/selenium/testZBX6339.php
+++ ui/tests/selenium/testZBX6339.php
@@ -51,10 +51,14 @@ class testZBX6339 extends CLegacyWebTest {
 		$host = $screen['host_name'];
 
 		$this->zbxTestLogin('templates.php');
-		// Check if template name present on page, if not, check on second page.
-		if ($this->query('link', $host)->one(false)->isValid() === false) {
+
+		// Check if template name present on page, if not, check on next page.
+		for ($i = 0; $i < 2; $i++) {
+			if ($this->query('link', $host)->one(false)->isValid() === true) {
+				break;
+			}
 			$this->query('xpath://div[@class="table-paging"]//span[@class="arrow-right"]/..')->one()->click();
-			$this->zbxTestWaitForPageToLoad();
+			$this->page->waitUntilReady();
 		}
 		$this->zbxTestClickLinkText($host);
 
