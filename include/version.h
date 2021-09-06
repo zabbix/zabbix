d6ba98635521ff14c93919ceffb233ce57817e69
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2021-09-06 16:22:26 +0300

    .......... [ZBXNEXT-826] updated release date [6.0.0alpha2]

--- include/version.h
+++ include/version.h
@@ -24,7 +24,7 @@
 #define ZBX_STR(str)	ZBX_STR2(str)
 
 #define APPLICATION_NAME	"Zabbix Agent"
-#define ZABBIX_REVDATE		"25 August 2021"
+#define ZABBIX_REVDATE		"6 September 2021"
 #define ZABBIX_VERSION_MAJOR	6
 #define ZABBIX_VERSION_MINOR	0
 #define ZABBIX_VERSION_PATCH	0
--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -26,7 +26,7 @@ import (
 )
 
 const (
-	ZABBIX_REVDATE          = "25 August 2021"
+	ZABBIX_REVDATE          = "6 September 2021"
 	ZABBIX_VERSION_MAJOR    = 6
 	ZABBIX_VERSION_MINOR    = 0
 	ZABBIX_VERSION_PATCH    = 0
