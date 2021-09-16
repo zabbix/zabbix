01bf8add9934712c4b9bcb880aa359625c514e0c
Roberts Lataria <roberts.lataria@zabbix.com>
2021-09-16 12:11:18 +0300

    ..F....... [ZBXNEXT-6470,ZBXNEXT-6889] fixed proxy cloning when tls_connect is psk; fixed input validation style

--- ui/app/controllers/CControllerProxyUpdate.php
+++ ui/app/controllers/CControllerProxyUpdate.php
@@ -23,29 +23,29 @@ class CControllerProxyUpdate extends CController {
 
 	protected function checkInput() {
 		$fields = [
-			'proxyid' =>		'fatal|required|db       hosts.hostid',
-			'host' =>			'db       hosts.host',
-			'status' =>			'db       hosts.status     |in '.HOST_STATUS_PROXY_ACTIVE.','.HOST_STATUS_PROXY_PASSIVE,
-			'dns' =>			'db       interface.dns',
-			'ip' =>				'db       interface.ip',
-			'useip' =>			'db       interface.useip  |in 0,1',
-			'port' =>			'db       interface.port',
-			'proxy_address' =>	'db       hosts.proxy_address',
-			'description' =>	'db       hosts.description',
-			'tls_connect' => 	'db       hosts.tls_connect    |in '.HOST_ENCRYPTION_NONE.','.HOST_ENCRYPTION_PSK.','.
+			'proxyid' =>			'fatal|required|db hosts.hostid',
+			'host' =>				'db hosts.host',
+			'status' =>				'db hosts.status|in '.HOST_STATUS_PROXY_ACTIVE.','.HOST_STATUS_PROXY_PASSIVE,
+			'dns' =>				'db interface.dns',
+			'ip' =>					'db interface.ip',
+			'useip' =>				'db interface.useip|in 0,1',
+			'port' =>				'db interface.port',
+			'proxy_address' =>		'db hosts.proxy_address',
+			'description' =>		'db hosts.description',
+			'tls_connect' =>		'db hosts.tls_connect|in '.HOST_ENCRYPTION_NONE.','.HOST_ENCRYPTION_PSK.','.
 				HOST_ENCRYPTION_CERTIFICATE,
-			'tls_accept' => 	'db       hosts.tls_accept     |in 0,'.HOST_ENCRYPTION_NONE.','.HOST_ENCRYPTION_PSK.','.
+			'tls_accept' =>			'db hosts.tls_accept|in 0,'.HOST_ENCRYPTION_NONE.','.HOST_ENCRYPTION_PSK.','.
 				(HOST_ENCRYPTION_NONE | HOST_ENCRYPTION_PSK).','.
 				HOST_ENCRYPTION_CERTIFICATE.','.
 				(HOST_ENCRYPTION_NONE | HOST_ENCRYPTION_CERTIFICATE).','.
 				(HOST_ENCRYPTION_PSK | HOST_ENCRYPTION_CERTIFICATE).','.
 				(HOST_ENCRYPTION_NONE | HOST_ENCRYPTION_PSK | HOST_ENCRYPTION_CERTIFICATE),
-			'tls_psk_identity'=>'db       hosts.tls_psk_identity',
-			'tls_psk' =>		'db       hosts.tls_psk',
-			'psk_edit_mode' =>	'in 0,1',
-			'tls_issuer' => 	'db       hosts.tls_issuer',
-			'tls_subject' => 	'db       hosts.tls_subject',
-			'form_refresh' =>	'int32'
+			'tls_psk_identity' =>	'db hosts.tls_psk_identity',
+			'tls_psk' =>			'db hosts.tls_psk',
+			'psk_edit_mode' =>		'in 0,1',
+			'tls_issuer' =>			'db hosts.tls_issuer',
+			'tls_subject' =>		'db hosts.tls_subject',
+			'form_refresh' =>		'int32'
 		];
 
 		$ret = $this->validateInput($fields);
