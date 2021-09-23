4e4336b0d11fd487289f790cc0cd1ea5d7c544fe
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2021-09-23 16:44:14 +0300

    ...G...... [ZBXNEXT-6923] added active check address cleanup on winndows agent

--- src/zabbix_agent/active.c
+++ src/zabbix_agent/active.c
@@ -1301,7 +1301,7 @@ ZBX_THREAD_ENTRY(active_checks_thread, args)
 	zbx_free(session_token);
 
 #ifdef _WINDOWS
-	zbx_addr_free(activechk_args.addrs);
+	zbx_addr_free(&activechk_args.addrs);
 	free_active_metrics();
 
 	ZBX_DO_EXIT();
