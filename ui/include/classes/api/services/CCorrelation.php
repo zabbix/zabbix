dd91d8b2778b5a546bf0f90945cbb40fb5e41273
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2021-10-04 10:17:13 +0300

    A......... [ZBXNEXT-6470,ZBXNEXT-6929] added possibility to pass operator for all condition types

--- ui/include/classes/api/services/CCorrelation.php
+++ ui/include/classes/api/services/CCorrelation.php
@@ -518,7 +518,7 @@ class CCorrelation extends CApiService {
 			'operator' =>	['type' => API_MULTIPLE, 'rules' => [
 								['if' => ['field' => 'type', 'in' => ZBX_CORR_CONDITION_NEW_EVENT_HOSTGROUP], 'type' => API_INT32, 'in' => implode(',', [CONDITION_OPERATOR_EQUAL, CONDITION_OPERATOR_NOT_EQUAL])],
 								['if' => ['field' => 'type', 'in' => implode(',', [ZBX_CORR_CONDITION_OLD_EVENT_TAG_VALUE, ZBX_CORR_CONDITION_NEW_EVENT_TAG_VALUE])], 'type' => API_INT32, 'in' => implode(',', [CONDITION_OPERATOR_EQUAL, CONDITION_OPERATOR_NOT_EQUAL, CONDITION_OPERATOR_LIKE, CONDITION_OPERATOR_NOT_LIKE])],
-								['else' => true, 'type' => API_UNEXPECTED]
+								['else' => true, 'type' => API_INT32, 'in' => CONDITION_OPERATOR_EQUAL]
 			]],
 			'tag' =>		['type' => API_MULTIPLE, 'rules' => [
 								['if' => ['field' => 'type', 'in' => implode(',', [ZBX_CORR_CONDITION_OLD_EVENT_TAG, ZBX_CORR_CONDITION_NEW_EVENT_TAG, ZBX_CORR_CONDITION_OLD_EVENT_TAG_VALUE, ZBX_CORR_CONDITION_NEW_EVENT_TAG_VALUE])], 'type' => API_STRING_UTF8, 'flags' => API_REQUIRED | API_NOT_EMPTY, 'length' => DB::getFieldLength('corr_condition_tag', 'tag')],
