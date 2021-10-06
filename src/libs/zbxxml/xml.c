e35760ffcaf4344f23b9e4176148e65e4bd020a2
Michael Veksler <Mihails.Vekslers@zabbix.com>
2021-10-06 20:33:24 +0300

    .......PS. [ZBXNEXT-6608] refactoring of zbxxml
    (cherry picked from commit 24084653ebf521172bd8c93f499b18f1d773ed15)

--- src/libs/zbxxml/xml.c
+++ src/libs/zbxxml/xml.c
@@ -704,12 +704,9 @@ int	zbx_xml_to_json(char *xml_data, char **jstr, char **errmsg)
 	*errmsg = zbx_dsprintf(*errmsg, "Zabbix was compiled without libxml2 support");
 	return FAIL;
 #else
-	struct zbx_json			json;
 	xmlDoc				*doc = NULL;
 	xmlNode				*node;
 	int				ret = FAIL;
-	zbx_vector_xml_node_ptr_t	nodes;
-	char				*out;
 
 	if (FAIL == zbx_open_xml(xml_data, XML_PARSE_NOBLANKS, -1, (void **)&doc, (void **)&node, errmsg))
 	{
@@ -720,6 +717,41 @@ int	zbx_xml_to_json(char *xml_data, char **jstr, char **errmsg)
 			goto clean;
 	}
 
+	ret = zbx_xmlnode_to_json((void *)node, jstr);
+clean:
+	xmlFreeDoc(doc);
+exit:
+	return ret;
+#endif /* HAVE_LIBXML2 */
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Function: zbx_xmlnode_to_json                                              *
+ *                                                                            *
+ * Purpose: convert XML format value to JSON format                           *
+ *                                                                            *
+ * Parameters: xml_node - [IN] the XML data to process                        *
+ *             jstr     - [OUT] the JSON output                               *
+ *             errmsg   - [OUT] error message                                 *
+ *                                                                            *
+ * Return value: SUCCEED - the value was processed successfully               *
+ *               FAIL - otherwise                                             *
+ *                                                                            *
+ ******************************************************************************/
+int	zbx_xmlnode_to_json(void *xml_node, char **jstr)
+{
+#ifndef HAVE_LIBXML2
+	ZBX_UNUSED(xml_data);
+	ZBX_UNUSED(jstr);
+	*errmsg = zbx_dsprintf(*errmsg, "Zabbix was compiled without libxml2 support");
+	return FAIL;
+#else
+	struct zbx_json			json;
+	zbx_vector_xml_node_ptr_t	nodes;
+	char				*out;
+	xmlNode				*node = (xmlNode*)xml_node;
+
 	zbx_json_init(&json, ZBX_JSON_STAT_BUF_LEN);
 
 	zbx_vector_xml_node_ptr_create(&nodes);
@@ -727,15 +759,11 @@ int	zbx_xml_to_json(char *xml_data, char **jstr, char **errmsg)
 	xml_to_vector(node, &nodes);
 	vector_to_json(&nodes, &json, &out);
 	*jstr = zbx_strdup(*jstr, json.buffer);
-	ret = SUCCEED;
 
 	zbx_vector_xml_node_ptr_clear_ext(&nodes, zbx_xml_node_free);
 	zbx_vector_xml_node_ptr_destroy(&nodes);
 	zbx_json_free(&json);
-clean:
-	xmlFreeDoc(doc);
-exit:
-	return ret;
+	return SUCCEED;
 #endif /* HAVE_LIBXML2 */
 }
 
