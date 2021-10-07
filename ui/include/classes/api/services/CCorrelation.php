5b63475cf7e7b1a45788b9a296004c6d16f7fb46
Roberts Lataria <roberts.lataria@zabbix.com>
2021-10-07 15:09:03 +0300

    A......... [ZBXNEXT-6470,ZBXNEXT-6929] added return type hint to getFilterValidationRules function

--- ui/include/classes/api/services/CCorrelation.php
+++ ui/include/classes/api/services/CCorrelation.php
@@ -512,7 +512,7 @@ class CCorrelation extends CApiService {
 	 *
 	 * @return array
 	 */
-	private static function getFilterValidationRules() {
+	private static function getFilterValidationRules(): array {
 		$condition_fields = [
 			'type' =>		['type' => API_INT32, 'flags' => API_REQUIRED, 'in' => implode(',', [ZBX_CORR_CONDITION_OLD_EVENT_TAG, ZBX_CORR_CONDITION_NEW_EVENT_TAG, ZBX_CORR_CONDITION_NEW_EVENT_HOSTGROUP, ZBX_CORR_CONDITION_EVENT_TAG_PAIR, ZBX_CORR_CONDITION_OLD_EVENT_TAG_VALUE, ZBX_CORR_CONDITION_NEW_EVENT_TAG_VALUE])],
 			'operator' =>	['type' => API_MULTIPLE, 'rules' => [
