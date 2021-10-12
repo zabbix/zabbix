7b49710537951d7dcac5da4caeb374ae53c97c24
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2021-10-12 11:27:53 +0300

    .......... [ZBXNEXT-686] micro style fix

--- ui/tests/include/web/elements/CWidgetElement.php
+++ ui/tests/include/web/elements/CWidgetElement.php
@@ -76,19 +76,19 @@ class CWidgetElement extends CElement {
 	 */
 	public function edit() {
 	// Edit can sometimes fail so we have to retry this operation.
-	for ($i = 0; $i < 2; $i++) {
-		$this->query('xpath:.//button[@class="btn-widget-edit"]')->waitUntilPresent()->one()->click(true);
+		for ($i = 0; $i < 2; $i++) {
+			$this->query('xpath:.//button[@class="btn-widget-edit"]')->waitUntilPresent()->one()->click(true);
 
-		try {
-			return $this->query('xpath://div[@data-dialogueid="widget_properties"]//form')->waitUntilVisible()->asForm()->one();
-		}
-		catch (\Exception $e) {
-			if ($i === 1) {
-				throw $e;
+			try {
+				return $this->query('xpath://div[@data-dialogueid="widget_properties"]//form')->waitUntilVisible()->asForm()->one();
+			}
+			catch (\Exception $e) {
+				if ($i === 1) {
+					throw $e;
+				}
 			}
 		}
 	}
-}
 
 	/**
 	 * @inheritdoc
