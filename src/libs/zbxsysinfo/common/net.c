3b9ef7a9257bcdcfb0e49350b51ef0ac9d928458
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2021-10-15 10:20:35 +0300

    ...G...PS. [ZBXNEXT-6923] improved setting of send and receive timeout

--- src/libs/zbxsysinfo/common/net.c
+++ src/libs/zbxsysinfo/common/net.c
@@ -36,8 +36,8 @@ int	tcp_expect(const char *host, unsigned short port, int timeout, const char *r
 
 	*value_int = 0;
 
-	if (SUCCEED != (net = zbx_tcp_connect(&s, CONFIG_SOURCE_IP, host, port, timeout, ZBX_TCP_SEC_UNENCRYPTED,
-			NULL, NULL)))
+	if (SUCCEED != (net = zbx_tcp_connect(&s, CONFIG_SOURCE_IP, host, port, timeout, ZBX_TCP_SEC_UNENCRYPTED, NULL,
+			NULL)))
 	{
 		goto out;
 	}
