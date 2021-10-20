ce83a4e7f46887b4b9e4ed4fb1c76d6e176cf2a9
Eriks Sneiders <eriks.sneiders@zabbix.com>
2021-10-20 13:19:46 +0300

    ...G...... [ZBXNEXT-6708] fixed 3rd parameter for Zabbix agent 2 web.certificate.get plugin
    * commit 'b8d591eabaa3aba395f9c278d87aac06eab12169':
      .......... [ZBXNEXT-6708] added ChangeLog entry
      ...G...... [ZBXNEXT-6708] updated 3rd  error message on fail
      ...G...... [ZBXNEXT-6708] updated 3rd parameter parsing and error message on fail
      ...G...... [ZBXNEXT-6708] updated 3rd parameter to handle ipv6 error better
      ...G...... [ZBXNEXT-6708] fixed 3rd parameter for Zabbix agent 2 web.certificate.get plugin
    
    (cherry picked from commit 698ec73b4ae26d401e128025044efd1949c333e5)

--- src/go/plugins/web/certificate/certificate_test.go
+++ src/go/plugins/web/certificate/certificate_test.go
@@ -41,6 +41,11 @@ func Test_getParameters(t *testing.T) {
 		{"+full", args{[]string{"example.com", "443", "127.0.0.1"}}, "127.0.0.1", "443", "example.com", false},
 		{"+empty_port", args{[]string{"example.com", "", "127.0.0.1"}}, "127.0.0.1", "443", "example.com", false},
 		{"+empty_port_and_ip", args{[]string{"example.com", "", ""}}, "example.com", "443", "example.com", false},
+		{"+two_params", args{[]string{"example.com", "443"}}, "example.com", "443", "example.com", false},
+		{"+two_params_port_with_host", args{[]string{"example.com:443", ""}}, "example.com", "443", "example.com", false},
+		{"+two_params_full", args{[]string{"example.com:443", "443"}}, "example.com", "443", "example.com", false},
+		{"-full_ipv6", args{[]string{"example.com", "443", "::1"}}, "", "", "", true},
+		{"-full_ipv6_first", args{[]string{"::1", "443", ""}}, "", "", "", true},
 		{"-too_many_params", args{[]string{"example.com", "443", "127.0.0.1", "foobar"}}, "", "", "", true},
 		{"-too_few_params", args{[]string{}}, "", "", "", true},
 	}
@@ -65,7 +70,78 @@ func Test_getParameters(t *testing.T) {
 	}
 }
 
-func Test_validateURL(t *testing.T) {
+func Test_parseParameters(t *testing.T) {
+	type args struct {
+		params []string
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantAddress string
+		wantPort    string
+		wantDomain  string
+		wantErr     bool
+	}{
+		{"+1st_param_hostname", args{[]string{"example.com", "443", "1.2.3.4"}}, "1.2.3.4", "443", "example.com", false},
+		{"+1st_param_hostname_full",
+			args{[]string{"https://www.example.com", "443", "1.2.3.4"}}, "1.2.3.4", "443", "www.example.com", false,
+		},
+		{"+3rd_param_hostname", args{[]string{"1.2.3.4", "443", "example.com"}}, "1.2.3.4", "443", "example.com", false},
+		{"+1st_param_full",
+			args{[]string{"https://1.2.3.4:443", "443", "example.com"}}, "1.2.3.4", "443", "example.com", false,
+		},
+		{"+empty_3rdParam", args{[]string{"example.com", "443", ""}}, "example.com", "443", "example.com", false},
+		{"-ipv6", args{[]string{"example.com", "443", "::1"}}, "", "", "", true},
+		{"-ipv6_brackets", args{[]string{"example.com", "443", "[::1]"}}, "", "", "", true},
+		{"-not_ip", args{[]string{"example.com", "443", "www.example.com"}}, "example.com", "443", "www.example.com", false},
+		{"-3rd_param_not_hostname", args{[]string{"1.2.3.4", "443", "https://www.example.com"}}, "", "", "", true},
+		{"-1st_param_http", args{[]string{"http://1.2.3.4:443", "443", "example.com"}}, "", "", "", true},
+		{"-not_http", args{[]string{"http://1.2.3.4:443", "443", "example.com"}}, "", "", "", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotHost, gotPort, gotDomain, err := getParsedParameters(tt.args.params)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseParameters() error = %v, wantErr %v", err, tt.wantErr)
+
+				return
+			}
+			if gotHost != tt.wantAddress {
+				t.Errorf("parseParameters() gotAddress = %v, want %v", gotHost, tt.wantAddress)
+			}
+			if gotPort != tt.wantPort {
+				t.Errorf("parseParameters() gotPort = %v, want %v", gotPort, tt.wantPort)
+			}
+			if gotDomain != tt.wantDomain {
+				t.Errorf("parseParameters() gotDomain = %v, want %v", gotDomain, tt.wantDomain)
+			}
+		})
+	}
+}
+
+func Test_cutBrackets(t *testing.T) {
+	type args struct {
+		in string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"+ipv6", args{"[::1]"}, "::1"},
+		{"+ipv4", args{"127.0.0.1"}, "127.0.0.1"},
+		{"+empty", args{""}, ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cutBrackets(tt.args.in); got != tt.want {
+				t.Errorf("cutBrackets() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseURL(t *testing.T) {
 	type args struct {
 		url  string
 		port string
@@ -90,6 +166,11 @@ func Test_validateURL(t *testing.T) {
 		{"+path", args{"www.google.com/foo/bar", "443"}, "www.google.com", "443", false},
 		{"+path_port", args{"www.google.com:443/foo/bar", "443"}, "www.google.com", "443", false},
 		{"+full_path", args{"https://www.google.com:443/foo/bar", "443"}, "www.google.com", "443", false},
+		{"+ip", args{"https://127.0.0.1:443", "443"}, "127.0.0.1", "443", false},
+		{"+ipv6_full", args{"https://::1:443", "443"}, "", "", true},
+		{"+ipv6_full_brackets", args{"https://[::1]:443", "443"}, "", "", true},
+		{"+ipv6_brackets", args{"[::1]", "443"}, "", "", true},
+		{"+ipv6_port_both_brackets", args{"[::1]:443", "443"}, "", "", true},
 		{"+ip", args{"https://127.0.0.1:443", "443"}, "127.0.0.1", "443", false},
 		{"-mismatch_port", args{"www.google.com:123", "443"}, "", "", true},
 		{"-invalid_url", args{"www.google.com:999999", ""}, "", "", true},
