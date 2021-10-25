56531813ce4c29801d09679716d94d82ee8f28f4
Janis Freibergs <janis.freibergs@zabbix.com>
2021-10-25 11:39:54 +0300

    ..F....... [ZBXNEXT-6921] modified $dbversion_status to array for re-use

--- ui/app/partials/administration.system.info.php
+++ ui/app/partials/administration.system.info.php
@@ -135,43 +135,43 @@ if (!$data['system_info']['history_pk']) {
 // Check DB version.
 if ($data['user_type'] == USER_TYPE_SUPER_ADMIN) {
 	foreach ($data['system_info']['dbversion_status'] as $dbversion) {
-		if ($dbversion->flag == DB_VERSION_SUPPORTED) {
+		if ($dbversion['flag'] == DB_VERSION_SUPPORTED) {
 			continue;
 		}
 
 		switch ($dbversion->flag) {
 			case DB_VERSION_LOWER_THAN_MINIMUM:
-				$error = _s('Minimum required %1$s database version is %2$s.', $dbversion->database,
-					$dbversion->min_version
+				$error = _s('Minimum required %1$s database version is %2$s.', $dbversion['database'],
+					$dbversion['min_version']
 				);
 				break;
 
 			case DB_VERSION_HIGHER_THAN_MAXIMUM:
-				$error = _s('Maximum required %1$s database version is %2$s.', $dbversion->database,
-					$dbversion->max_version
+				$error = _s('Maximum required %1$s database version is %2$s.', $dbversion['database'],
+					$dbversion['max_version']
 				);
 				break;
 
 			case DB_VERSION_FAILED_TO_RETRIEVE:
 				$error = _('Unable to retrieve database version.');
-				$dbversion->current_version = '';
+				$dbversion['current_version'] = '';
 				break;
 
 			case DB_VERSION_NOT_SUPPORTED_ERROR:
 				$error = _s('Error! Unable to start Zabbix server due to unsupported %1$s database server version. Must be at least (%2$s)',
-					$dbversion->database, $dbversion->min_supported_version
+					$dbversion['database'], $dbversion['min_supported_version']
 				);
 				break;
 
 			case DB_VERSION_NOT_SUPPORTED_WARNING:
 				$error = _s('Warning! Unsupported %1$s database server version. Should be at least (%2$s)',
-					$dbversion->database, $dbversion->min_supported_version
+					$dbversion['database'], $dbversion['min_supported_version']
 				);
 				break;
 		}
 
 		$info_table->addRow(
-			(new CRow([$dbversion->database, $dbversion->current_version, $error]))->addClass(ZBX_STYLE_RED)
+			(new CRow([$dbversion['database'], $dbversion['current_version'], $error]))->addClass(ZBX_STYLE_RED)
 		);
 	}
 
