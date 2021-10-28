a746376e108f7122ca8f64281746eed0b766daad
Miks Kronkalns <miks.kronkalns@zabbix.com>
2021-10-28 11:47:53 +0300

    ..F....... [ZBXNEXT-6945] fixed geomap configuration validation

--- ui/app/controllers/CControllerGeomapsUpdate.php
+++ ui/app/controllers/CControllerGeomapsUpdate.php
@@ -27,8 +27,8 @@ class CControllerGeomapsUpdate extends CController {
 	protected function checkInput(): bool {
 		$fields = [
 			'geomaps_tile_provider'		=> 'required|db config.geomaps_tile_provider',
-			'geomaps_tile_url'			=> 'required|db config.geomaps_tile_url|not_empty',
-			'geomaps_max_zoom'			=> 'required|db config.geomaps_max_zoom|ge 0|le 99999',
+			'geomaps_tile_url'			=> 'required|not_empty|db config.geomaps_tile_url',
+			'geomaps_max_zoom'			=> 'required|not_empty|db config.geomaps_max_zoom|ge 0|le 99999',
 			'geomaps_attribution'		=> 'db config.geomaps_attribution'
 		];
 
