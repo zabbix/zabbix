8c7e9905252c789cef5fec357a542230a49de019
Janis Freibergs <janis.freibergs@zabbix.com>
2021-11-04 16:30:45 +0200

    ..F....... [ZBXNEXT-6921] fixed test dependency

--- ui/tests/general/testHistoryManager.php
+++ ui/tests/general/testHistoryManager.php
@@ -19,11 +19,12 @@
 **/
 
 
-require_once dirname(__FILE__).'/../include/CTest.php';
-require_once dirname(__FILE__).'/../../include/classes/api/managers/CHistoryManager.php';
-require_once dirname(__FILE__).'/../../include/classes/core/CRegistryFactory.php';
-require_once dirname(__FILE__).'/../../include/classes/core/Manager.php';
-require_once dirname(__FILE__).'/../../include/classes/helpers/CArrayHelper.php';
+require_once __DIR__.'/../include/CTest.php';
+require_once __DIR__.'/../../include/classes/api/managers/CHistoryManager.php';
+require_once __DIR__.'/../../include/classes/core/CRegistryFactory.php';
+require_once __DIR__.'/../../include/classes/core/Manager.php';
+require_once __DIR__.'/../../include/classes/helpers/CArrayHelper.php';
+require_once __DIR__.'/../../include/classes/helpers/CSettingsHelper.php';
 
 /**
  * @backup history_uint
