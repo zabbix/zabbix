4a09aa3c0ec5f4a8dcf9acea6532b50cea70aa69
Miks Kronkalns <miks.kronkalns@zabbix.com>
2021-11-04 13:57:44 +0200

    ..F....... [ZBXNEXT-6945] fixed reset to initial view

--- ui/app/controllers/CControllerProfileUpdate.php
+++ ui/app/controllers/CControllerProfileUpdate.php
@@ -131,8 +131,13 @@ class CControllerProfileUpdate extends CController {
 			// PROFILE_TYPE_STR
 			case 'web.dashboard.widget.geomap.default_view':
 				$value_str = $this->getInput('value_str');
-				foreach ($this->getInput('idx2') as $idx2) {
-					CProfile::update($idx, $value_str, PROFILE_TYPE_STR, $idx2);
+				if ($value_str === '') { // default value
+					CProfile::delete($idx, $this->getInput('idx2'));
+				}
+				else {
+					foreach ($this->getInput('idx2') as $idx2) {
+						CProfile::update($idx, $value_str, PROFILE_TYPE_STR, $idx2);
+					}
 				}
 				break;
 
