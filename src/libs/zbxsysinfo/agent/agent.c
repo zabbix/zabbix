ae5efbabfa07778d0f471dc2fde18cbd1ab6571f
Andrejs Kozlovs <andrejs.kozlovs@zabbix.com>
2021-11-05 16:24:50 +0200

    ...G...... [ZBXNEXT-6651] added new item agent.variant for Agent and Agent2
    Merge in ZBX/zabbix from feature/ZBXNEXT-6651-5.0 to release/5.0
    
    * commit '1259d980dcb19d634beaa2d6ecc3e4e1237f564e':
      ...G...... [ZBXNEXT-6651] fixed PR warning
      ...G...... [ZBXNEXT-6651] changed items order
      .D........ [ZBXNEXT-6651] added ChangeLog
      ...G...... [ZBXNEXT-6651] added new item agent.variant for Agent
      ..F....... [ZBXNEXT-6651,ZBXNEXT-7012] added new item key "agent.variant" to the helper for selecting item keys
    
    (cherry picked from commit 0a4ded934ebd495f2fef85fc12a896dde459bb4e)

--- src/libs/zbxsysinfo/agent/agent.c
+++ src/libs/zbxsysinfo/agent/agent.c
@@ -27,12 +27,14 @@ extern ZBX_THREAD_LOCAL char	*CONFIG_HOSTNAME;
 static int	AGENT_HOSTNAME(AGENT_REQUEST *request, AGENT_RESULT *result);
 static int	AGENT_PING(AGENT_REQUEST *request, AGENT_RESULT *result);
 static int	AGENT_VERSION(AGENT_REQUEST *request, AGENT_RESULT *result);
+static int	AGENT_VARIANT(AGENT_REQUEST *request, AGENT_RESULT *result);
 
 ZBX_METRIC	parameters_agent[] =
 /*	KEY			FLAG		FUNCTION	TEST PARAMETERS */
 {
 	{"agent.hostname",	0,		AGENT_HOSTNAME,	NULL},
 	{"agent.ping",		0,		AGENT_PING,	NULL},
+	{"agent.variant",	0,		AGENT_VARIANT,	NULL},
 	{"agent.version",	0,		AGENT_VERSION,	NULL},
 	{"modbus.get",		CF_HAVEPARAMS,	MODBUS_GET,	"tcp://127.0.0.1"},
 	{NULL}
@@ -70,6 +72,15 @@ static int	AGENT_VERSION(AGENT_REQUEST *request, AGENT_RESULT *result)
 	ZBX_UNUSED(request);
 
 	SET_STR_RESULT(result, zbx_strdup(NULL, ZABBIX_VERSION));
+
+	return SYSINFO_RET_OK;
+}
+
+static int	AGENT_VARIANT(AGENT_REQUEST *request, AGENT_RESULT *result)
+{
+	ZBX_UNUSED(request);
+
+	SET_UI64_RESULT(result, 1);
 
 	return SYSINFO_RET_OK;
 }
