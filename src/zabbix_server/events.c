181a44d596927681e347af6bf28c2a012a04c1a0
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2021-11-05 21:36:25 +0200

    ........S. [ZBX-19651] fixed notification sending when problem is not in maintenance mode

--- src/zabbix_server/events.c
+++ src/zabbix_server/events.c
@@ -1871,6 +1871,40 @@ exit:
 
 /******************************************************************************
  *                                                                            *
+ * Function: trigger_has_non_maintained_host                                  *
+ *                                                                            *
+ * Purpose: check if event is trigger on the non-maintained host              *
+ *                                                                            *
+ ******************************************************************************/
+static int	trigger_has_non_maintained_host(DB_EVENT *event)
+{
+	zbx_hashset_t		hosts;
+	zbx_hashset_iter_t	iter;
+	DC_HOST			*host;
+	int			ret = FAIL;
+
+	zbx_hashset_create(&hosts, 10, ZBX_DEFAULT_UINT64_HASH_FUNC, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
+
+	db_trigger_get_hosts(&hosts, &event->trigger);
+	zbx_hashset_iter_reset(&hosts, &iter);
+
+	while (NULL != (host = (DC_HOST *)zbx_hashset_iter_next(&iter)))
+	{
+		if (HOST_MAINTENANCE_STATUS_OFF == host->maintenance_status)
+		{
+			ret = SUCCEED;
+			break;
+		}
+	}
+
+	zbx_hashset_clear(&hosts);
+	zbx_hashset_destroy(&hosts);
+
+	return ret;
+}
+
+/******************************************************************************
+ *                                                                            *
  * Function: add_event_suppress_data                                          *
  *                                                                            *
  * Purpose: adds event suppress data for problem events matching active       *
@@ -1924,6 +1958,8 @@ static void	add_event_suppress_data(zbx_vector_ptr_t *event_refs, zbx_vector_uin
 
 				for (i = 0; i < query->maintenances.values_num; i++)
 				{
+					DB_EVENT *event = (DB_EVENT *)event_refs->values[j];
+
 					/* when locking maintenances not-locked (deleted) maintenance ids */
 					/* are removed from the maintenanceids vector                   */
 					if (FAIL == zbx_vector_uint64_bsearch(maintenanceids,
@@ -1933,11 +1969,18 @@ static void	add_event_suppress_data(zbx_vector_ptr_t *event_refs, zbx_vector_uin
 						continue;
 					}
 
-					zbx_db_insert_add_values(&db_insert, __UINT64_C(0), query->eventid,
-							query->maintenances.values[i].first,
-							(int)query->maintenances.values[i].second);
+					if (SUCCEED == trigger_has_non_maintained_host(event))
+					{
+						event->suppressed = ZBX_PROBLEM_SUPPRESSED_FALSE;
+					}
+					else
+					{
+						zbx_db_insert_add_values(&db_insert, __UINT64_C(0), query->eventid,
+								query->maintenances.values[i].first,
+								(int)query->maintenances.values[i].second);
 
-					((DB_EVENT *)event_refs->values[j])->suppressed = ZBX_PROBLEM_SUPPRESSED_TRUE;
+						event->suppressed = ZBX_PROBLEM_SUPPRESSED_TRUE;
+					}
 				}
 			}
 
