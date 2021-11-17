0f3378e2bc5cb09afd9dd41e451b10f508834a36
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2021-11-17 15:12:21 +0200

    ........S. [ZBXNEXT-6955] fixed error comment message 2

--- src/libs/zbxserver/evalfunc.c
+++ src/libs/zbxserver/evalfunc.c
@@ -2947,7 +2947,7 @@ static int	evaluate_TREND(zbx_variant_t *value, DC_ITEM *item, const char *func,
 
 		if (start_detect_period < start)
 		{
-			*error = zbx_strdup(*error, "evaluation period must not exceed detection period");
+			*error = zbx_strdup(*error, "detection period must not exceed evaluation period");
 			goto out;
 		}
 
