38ce2dcc6c099f13143ac56adcb9c70a28549157
Alexander Shubin <aleksandrs.subins@zabbix.com>
2021-11-24 12:57:52 +0200

    ..F....... [ZBXNEXT-6966] fixed scrollbar for FF

--- ui/app/views/monitoring.widget.item.view.php
+++ ui/app/views/monitoring.widget.item.view.php
@@ -141,7 +141,9 @@ else {
 		}
 	}
 
-	$body = (new CLink($items, $data['data']['url']))->addClass('dashboard-grid-widget-item');
+	$body = (new CDiv(
+		new CLink($items, $data['data']['url'])
+	))->addClass('dashboard-grid-widget-item');
 
 	if ($data['data']['bg_color'] !== '') {
 		$body->addStyle('background-color: #'.$data['data']['bg_color']);
