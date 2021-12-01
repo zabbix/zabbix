e7c0b574c1ecd9851f997cb7bc5427bb6d387ddc
Vyacheslav Khaliev <vyacheslav.khaliev@zabbix.com>
2021-12-01 21:01:59 +0500

    .........T [ZBX-18389] fixed sql query for 32 and 64 bit xids postgresql versions in postgresql by user parameters template

--- templates/db/postgresql/postgresql/pgsql.frozenxid.sql
+++ templates/db/postgresql/postgresql/pgsql.frozenxid.sql
@@ -1,8 +1,8 @@
 SELECT row_to_json(T)
 FROM (
-	SELECT 
+	SELECT
 		extract(epoch FROM now())::integer AS ts,
-		((1 - max(age(d.datfrozenxid))::double precision / 
+		((1 - max(age(d.datfrozenxid))::double precision /
 		current_setting('autovacuum_freeze_max_age')::bigint) * 100)::numeric(9,6)
 		AS prc_before_av,
 		((1 - abs(max(age(d.datfrozenxid))::double precision /
