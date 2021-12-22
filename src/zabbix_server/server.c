6413c64a00065d69552bbcb20ba7cfd85f2445d9
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2021-12-22 16:16:57 +0200

    .......... [ZBXNEXT-7122] resolved conflicts

--- src/zabbix_server/server.c
+++ src/zabbix_server/server.c
@@ -174,6 +174,7 @@ static int	ha_status = ZBX_NODE_STATUS_UNKNOWN;
 static int	ha_failover_delay = ZBX_HA_DEFAULT_FAILOVER_DELAY;
 zbx_cuid_t	ha_sessionid;
 
+
 unsigned char			program_type	= ZBX_PROGRAM_TYPE_SERVER;
 ZBX_THREAD_LOCAL unsigned char	process_type	= ZBX_PROCESS_TYPE_UNKNOWN;
 ZBX_THREAD_LOCAL int		process_num	= 0;
@@ -346,6 +347,14 @@ int	CONFIG_DOUBLE_PRECISION		= ZBX_DB_DBL_PRECISION_ENABLED;
 char	*CONFIG_WEBSERVICE_URL	= NULL;
 
 int	CONFIG_SERVICEMAN_SYNC_FREQUENCY	= 60;
+
+#if defined(HAVE_POSTGRESQL)
+extern int	ZBX_TSDB_VERSION;
+#endif
+
+struct zbx_db_version_info_t	db_version_info;
+
+static volatile sig_atomic_t	zbx_rtc_command;
 
 int	get_process_info_by_thread(int local_server_num, unsigned char *local_process_type, int *local_process_num);
 
@@ -1090,9 +1099,8 @@ int	main(int argc, char **argv)
 
 static void	zbx_check_db(void)
 {
-	struct zbx_db_version_info_t	db_version_info;
-	struct zbx_json			db_version_json;
-	int				result = SUCCEED;
+	struct zbx_json	db_version_json;
+	int		result = SUCCEED;
 
 	memset(&db_version_info, 0, sizeof(db_version_info));
 	result = zbx_db_check_version_info(&db_version_info, CONFIG_ALLOW_UNSUPPORTED_DB_VERSIONS);
@@ -1127,6 +1135,11 @@ static void	zbx_check_db(void)
 			zabbix_log(LOG_LEVEL_WARNING, "database could be upgraded to use primary keys in history tables");
 		}
 
+#if defined(HAVE_POSTGRESQL)
+		if (ZBX_TSDB_VERSION > 0)
+			zbx_tsdb_update_dbversion_info(&db_version_info);
+#endif
+
 		zbx_db_version_json_create(&db_version_json, &db_version_info);
 
 		if (SUCCEED == result)
@@ -1137,6 +1150,7 @@ static void	zbx_check_db(void)
 	}
 
 	DBclose();
+
 	zbx_free(db_version_info.friendly_current_version);
 	zbx_free(db_version_info.extension);
 	zbx_free(db_version_info.ext_friendly_current_version);
@@ -1144,6 +1158,7 @@ static void	zbx_check_db(void)
 
 	if(SUCCEED != result)
 	{
+		zbx_free(db_version_info.friendly_current_version);
 		exit(EXIT_FAILURE);
 	}
 }
@@ -1494,6 +1509,7 @@ static void	server_teardown(zbx_rtc_t *rtc, zbx_socket_t *listen_sock)
 		exit(EXIT_FAILURE);
 	}
 }
+
 
 int	MAIN_ZABBIX_ENTRY(int flags)
 {
@@ -1912,6 +1928,8 @@ void	zbx_on_exit(int ret)
 #if defined(PS_OVERWRITE_ARGV)
 	setproctitle_free_env();
 #endif
+
+	zbx_free(db_version_info.friendly_current_version);
 
 	exit(EXIT_SUCCESS);
 }
