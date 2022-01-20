591b28ec52e83d0fc238f6e788f4fd8285e1d110
Gregory Chalenko <gregory.chalenko@zabbix.com>
2022-01-20 23:43:24 +0200

    ..F....... [ZBXNEXT-7125] added widget view with test data

--- ui/app/views/monitoring.widget.tophosts.view.php
+++ ui/app/views/monitoring.widget.tophosts.view.php
@@ -19,16 +19,152 @@
 **/
 
 
+// TEST DATA
+$data['configuration'] = [
+	0 => [
+		'name' => 'Host',
+		'type' => CWidgetFieldColumnsList::DATA_HOST_NAME
+	],
+	1 => [
+		'name' => '',
+		'type' => CWidgetFieldColumnsList::DATA_TEXT,
+		'base_color' => '#E0E0E0'
+	],
+	2 => [
+		'name' => 'CPU',
+		'type' => CWidgetFieldColumnsList::DATA_ITEM_VALUE,
+		'display' => CWidgetFieldColumnsList::DISPLAY_AS_IS,
+		'base_color' => '',
+		'thresholds' => [
+			['color' => '#FFE082', 'threshold' => 3],
+			['color' => '#FF5722', 'threshold' => 15]
+		]
+	],
+	3 => [
+		'name' => 'CPU',
+		'type' => CWidgetFieldColumnsList::DATA_ITEM_VALUE,
+		'display' => CWidgetFieldColumnsList::DISPLAY_BAR,
+		'base_color' => '#81C784'
+	],
+	4 => [
+		'name' => 'CPU',
+		'type' => CWidgetFieldColumnsList::DATA_ITEM_VALUE,
+		'display' => CWidgetFieldColumnsList::DISPLAY_INDICATORS,
+		'base_color' => '#81C784',
+		'thresholds' => [
+			['color' => '#FFE082', 'threshold' => 3],
+			['color' => '#FF5722', 'threshold' => 15]
+		]
+	],
+	5 => [
+		'name' => 'CPU',
+		'type' => CWidgetFieldColumnsList::DATA_ITEM_VALUE,
+		'display' => CWidgetFieldColumnsList::DISPLAY_INDICATORS,
+		'min' => 0,
+		'max' => 20,
+		'base_color' => '#81C784',
+		'thresholds' => [
+			['color' => '#FFE082', 'threshold' => 3],
+			['color' => '#FF5722', 'threshold' => 15]
+		]
+	]
+];
+
+// Generate random data for testing.
+$data['columns'] = [];
+for ($i = 0; $i < 5; $i++) {
+	$rnd = rand(0,20);
+	$data['columns'][] = [
+		['value' => 'Zabbix server', 'hostid' => 10084],
+		['value' => 'Static text with replaced macro. Too long to fit in column'],
+		['value' => $rnd],
+		['value' => $rnd],
+		['value' => $rnd],
+		['value' => $rnd]
+	];
+}
 /**
  * @var CView $this
  */
-$table = new CTable();
+$table = (new CTableInfo())->setHeader(array_column($data['configuration'], 'name'));
 
+foreach ($data['columns'] as $columns) {
+	$row = [];
 
+	foreach ($columns as $i => $column) {
+		$value = $column['value'];
+		$column_config = $data['configuration'][$i];
+
+		switch ($column_config['type']) {
+			case CWidgetFieldColumnsList::DATA_HOST_NAME:
+				$cell = (new CLinkAction($value))->setMenuPopup(CMenuPopupHelper::getHost($column['hostid']));
+				break;
+
+			case CWidgetFieldColumnsList::DATA_TEXT:
+				$cell = new CDiv($value);
+				break;
+
+			case CWidgetFieldColumnsList::DATA_ITEM_VALUE:
+				if ($column_config['display'] == CWidgetFieldColumnsList::DISPLAY_AS_IS) {
+					$cell = (new CDiv($value))->addClass('item-value');
+					break;
+				}
+
+				$cell = (new CBarGauge())
+					->setValue($value)
+					->addClass('item-value');
+
+				if (array_key_exists('thresholds', $column_config)) {
+					foreach ($column_config['thresholds'] as $threshold) {
+						$cell->addThreshold($threshold['threshold'], $threshold['color']);
+					}
+				}
+
+				if ($column_config['display'] == CWidgetFieldColumnsList::DISPLAY_BAR) {
+					$cell->setAttribute('solid', 1);
+				}
+
+				if (array_key_exists('base_color', $column_config)) {
+					$cell->setAttribute('fill', $column_config['base_color']);
+				}
+
+				if (array_key_exists('min', $column_config)) {
+					$cell->setAttribute('min', $column_config['min']);
+				}
+
+				if (array_key_exists('max', $column_config)) {
+					$cell->setAttribute('max', $column_config['max']);
+				}
+				break;
+		}
+
+		$color = array_key_exists('base_color', $column_config) ? $column_config['base_color'] : '';
+
+		if (array_key_exists('thresholds', $column_config)
+				&& array_key_exists('display', $column_config)
+				&& $column_config['display'] == CWidgetFieldColumnsList::DISPLAY_AS_IS) {
+			foreach ($column_config['thresholds'] as $threshold) {
+				if ($value >= $threshold['threshold']) {
+					$color = $threshold['color'];
+				}
+			}
+		}
+
+		if (!is_a($cell, CBarGauge::class) && $color !== '') {
+			$cell = (new CCol($cell))->addStyle('background-color: '.$color);
+		}
+
+		$row[] = $cell;
+	}
+
+	$table->addRow($row);
+}
 
 $output = [
 	'name' => $data['name'],
-	'body' => $table->toString()
+	'body' => (new CDiv($table))
+					->addClass('dashboard-grid-widget-tophosts')
+					->toString()
 ];
 
 if (($messages = getMessages()) !== null) {
