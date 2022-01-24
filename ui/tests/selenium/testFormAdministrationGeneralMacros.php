6e34a8a848a9f95975a3627e28c65ce1442aefca
Andrejs Zazuks <andrejs.zazuks@zabbix.com>
2022-01-24 13:37:35 +0200

    .......... [DEV-2056] added macro to name and key

--- ui/tests/selenium/testFormAdministrationGeneralMacros.php
+++ ui/tests/selenium/testFormAdministrationGeneralMacros.php
@@ -832,6 +832,7 @@ class testFormAdministrationGeneralMacros extends CLegacyWebTest {
 		$item_url = 'zabbix.php?action=latest.view&filter_hostids%5B%5D=99134&filter_select=&filter_evaltype=0'.
 				'&filter_tags%5B0%5D%5Btag%5D=&filter_tags%5B0%5D%5Boperator%5D=0&filter_tags%5B0%5D%5Bvalue%5D'.
 				'=&filter_show_details=1&filter_show_without_data=1&filter_set=1';
+		$item_url_host = 'items.php?filter_set=1&context=host&filter_hostids%5B0%5D=99134';
 		$macro = [
 			'macro' => '{$Z_GLOBAL_MACRO_2_RESOLVE}',
 			'value' => 'Value 2 B resolved'
@@ -842,6 +843,8 @@ class testFormAdministrationGeneralMacros extends CLegacyWebTest {
 //		Caused by https://support.zabbix.com/browse/ZBXNEXT-7115
 //		Support of user macros in item names has been dropped.
 		$this->assertTrue($this->query('link', 'trap['.$macro['value'].']')->exists());
+		$this->page->open($item_url_host)->waitUntilReady();
+		$this->assertTrue($this->query('link', 'Macro value: '.$macro['macro'])->exists());
 
 		// Change macro type.
 		$this->page->open('zabbix.php?action=macros.edit')->waitUntilReady();
