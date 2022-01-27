236a40494f0c701edcec2d04b4df86eb7bf00211
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2022-01-27 18:05:20 +0200

    .......... [DEV-2064] fixed windows build

--- src/libs/zbxcrypto/tls.c
+++ src/libs/zbxcrypto/tls.c
@@ -17,7 +17,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-#include "config.h"
+#include "sysinc.h"
 
 #if defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
 
