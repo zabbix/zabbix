858a9e55002197aa3d7c6ef8c91db5b7d686394e
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2022-01-28 00:41:08 +0200

    .......... [DEV-2062] removed duplicates from tests
    (cherry picked from commit f25da1fc9c156a7b362dd34be6fcdac14b41270f)
    (cherry picked from commit ddb1267290ccdc1b0ae9775bea0d2b188d336145)

--- tests/libs/zbxsysinfo/linux/NET_IF_COMMON.h
+++ tests/libs/zbxsysinfo/linux/NET_IF_COMMON.h
@@ -17,10 +17,12 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-#include "KERNEL_COMMON.h"
-#include "zbxmocktest.h"
+#ifndef NET_IF_COMMON_H
+#define NET_IF_COMMON_H
 
-void	zbx_mock_test_entry(void **state)
-{
-	zbx_mock_test_entry_KERNEL_COMMON(state, ZABBIX_MOCK_KERNEL_MAXFILES);
-}
+#define ZABBIX_MOCK_NET_IF_IN		0
+#define ZABBIX_MOCK_NET_IF_OUT		1
+#define ZABBIX_MOCK_NET_IF_TOTAL	2
+
+void	zbx_mock_test_entry_NET_IF_COMMON(void **state, int net_if_func);
+#endif
