9ad16dd301700235c32c2a73790a938971cdd355
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2022-01-27 15:44:27 +0200

    .......... [DEV-2064] fixed modbus build 2

--- src/libs/zbxsysinfo/agent/modbus.c
+++ src/libs/zbxsysinfo/agent/modbus.c
@@ -19,10 +19,10 @@
 
 #include "common.h"
 
-#ifdef HAVE_LIBMODBUS
-
 #include "modbtype.h"
 
+#ifdef HAVE_LIBMODBUS
+#include "modbus.h"
 #include "mutexs.h"
 
 #ifdef _WINDOWS
@@ -31,8 +31,6 @@
 #		define DLLBUILD
 #	endif
 #endif
-
-#include "modbus.h"
 
 zbx_mutex_t	modbus_lock = ZBX_MUTEX_NULL;
 
