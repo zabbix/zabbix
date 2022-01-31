c475f187522deccc37cdb953af24095afdfd0730
Andrejs Kozlovs <andrejs.kozlovs@zabbix.com>
2022-01-31 09:15:42 +0200

    ...G...... [ZBX-20472] fixed database lock crash in Zabbix Agent 2 using SQLite

--- src/go/internal/agent/resultcache/diskcache.go
+++ src/go/internal/agent/resultcache/diskcache.go
@@ -22,6 +22,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"sync"
 	"sync/atomic"
 	"time"
 
@@ -39,6 +40,10 @@ const (
 	DbVariableNotSet = -1
 	StorageTolerance = 600
 	DataLimit        = 10000
+)
+
+var (
+	cacheLock sync.Mutex
 )
 
 type DiskCache struct {
@@ -91,7 +96,9 @@ func (c *DiskCache) resultFetch(rows *sql.Rows) (d *AgentData, err error) {
 }
 
 func (c *DiskCache) getOldestWriteClock(table string) (clock int64, err error) {
+	cacheLock.Lock()
 	rows, err := c.database.Query(fmt.Sprintf("SELECT MIN(write_clock) FROM %s", table))
+	cacheLock.Unlock()
 	if err != nil {
 		return
 	}
@@ -112,7 +119,9 @@ func (c *DiskCache) getOldestWriteClock(table string) (clock int64, err error) {
 }
 
 func (c *DiskCache) getLastID(table string) (id uint64, err error) {
+	cacheLock.Lock()
 	rows, err := c.database.Query(fmt.Sprintf("SELECT MAX(id) FROM %s", table))
+	cacheLock.Unlock()
 	if err != nil {
 		return
 	}
@@ -159,9 +168,13 @@ func (c *DiskCache) upload(u Uploader) (err error) {
 	var rows *sql.Rows
 	var maxDataId, maxLogId uint64
 
-	if rows, err = c.database.Query(fmt.Sprintf("SELECT "+
+	cacheLock.Lock()
+	rows, err = c.database.Query(fmt.Sprintf("SELECT "+
 		"id,itemid,lastlogsize,mtime,state,value,eventsource,eventid,eventseverity,eventtimestamp,clock,ns"+
-		" FROM data_%d ORDER BY id LIMIT ?", c.serverID), DataLimit); err != nil {
+		" FROM data_%d ORDER BY id LIMIT ?", c.serverID), DataLimit)
+	cacheLock.Unlock()
+
+	if err != nil {
 		c.Errf("cannot select from data table: %s", err.Error())
 		return
 	}
@@ -173,14 +186,17 @@ func (c *DiskCache) upload(u Uploader) (err error) {
 		}
 	}()
 
+	cacheLock.Lock()
 	for rows.Next() {
 		if result, err = c.resultFetch(rows); err != nil {
 			rows.Close()
+			cacheLock.Unlock()
 			return
 		}
 		result.persistent = false
 		results = append(results, result)
 	}
+	cacheLock.Unlock()
 	if err = rows.Err(); err != nil {
 		return
 	}
@@ -190,23 +206,27 @@ func (c *DiskCache) upload(u Uploader) (err error) {
 	}
 
 	if dataLen != DataLimit {
-		if rows, err = c.database.Query(fmt.Sprintf("SELECT "+
+		cacheLock.Lock()
+		rows, err = c.database.Query(fmt.Sprintf("SELECT "+
 			"id,itemid,lastlogsize,mtime,state,value,eventsource,eventid,eventseverity,eventtimestamp,clock,ns"+
-			" FROM log_%d ORDER BY id LIMIT ?", c.serverID),
-			DataLimit-len(results)); err != nil {
-
+			" FROM log_%d ORDER BY id LIMIT ?", c.serverID), DataLimit-len(results))
+		cacheLock.Unlock()
+		if err != nil {
 			c.Errf("cannot select from log table: %s", err.Error())
 			return
 		}
 
+		cacheLock.Lock()
 		for rows.Next() {
 			if result, err = c.resultFetch(rows); err != nil {
 				rows.Close()
+				cacheLock.Unlock()
 				return
 			}
 			result.persistent = true
 			results = append(results, result)
 		}
+		cacheLock.Unlock()
 		if err = rows.Err(); err != nil {
 			return
 		}
@@ -251,7 +271,10 @@ func (c *DiskCache) upload(u Uploader) (err error) {
 		c.lastError = nil
 	}
 	if maxDataId != 0 {
-		if _, err = c.database.Exec(fmt.Sprintf("DELETE FROM data_%d WHERE id<=?", c.serverID), maxDataId); err != nil {
+		cacheLock.Lock()
+		_, err = c.database.Exec(fmt.Sprintf("DELETE FROM data_%d WHERE id<=?", c.serverID), maxDataId)
+		cacheLock.Unlock()
+		if err != nil {
 			return fmt.Errorf("cannot delete from data_%d: %s", c.serverID, err)
 		}
 		if err = c.updateDataRange(); err != nil {
@@ -259,7 +282,10 @@ func (c *DiskCache) upload(u Uploader) (err error) {
 		}
 	}
 	if maxLogId != 0 {
-		if _, err = c.database.Exec(fmt.Sprintf("DELETE FROM log_%d WHERE id<=?", c.serverID), maxLogId); err != nil {
+		cacheLock.Lock()
+		_, err = c.database.Exec(fmt.Sprintf("DELETE FROM log_%d WHERE id<=?", c.serverID), maxLogId)
+		cacheLock.Unlock()
+		if err != nil {
 			return fmt.Errorf("cannot delete from log_%d: %s", c.serverID, err)
 		}
 		if err = c.updateLogRange(); err != nil {
@@ -341,7 +367,9 @@ func (c *DiskCache) write(r *plugin.Result) {
 		if (now - c.oldestLog) > c.storagePeriod {
 			atomic.StoreUint32(&c.persistFlag, 1)
 		}
+		cacheLock.Lock()
 		stmt, err = c.database.Prepare(c.insertResultTable(fmt.Sprintf("log_%d", c.serverID)))
+		cacheLock.Unlock()
 
 		if err != nil {
 			c.Errf("cannot prepare SQL query to insert history in log_%d : %s", c.serverID, err)
@@ -356,7 +384,10 @@ func (c *DiskCache) write(r *plugin.Result) {
 
 		if (now - c.oldestData) > c.storagePeriod+StorageTolerance {
 			query := fmt.Sprintf("DELETE FROM data_%d WHERE clock<?", c.serverID)
-			if _, err = c.database.Exec(query, now-c.storagePeriod); err != nil {
+			cacheLock.Lock()
+			_, err = c.database.Exec(query, now-c.storagePeriod)
+			cacheLock.Unlock()
+			if err != nil {
 				c.Errf("cannot delete old data from data_%d : %s", c.serverID, err)
 			}
 			c.oldestData, err = c.getOldestWriteClock(tableName("data", c.serverID))
@@ -364,7 +395,9 @@ func (c *DiskCache) write(r *plugin.Result) {
 				c.Errf("cannot query minimum write clock from data_%d : %s", c.serverID, err)
 			}
 		}
+		cacheLock.Lock()
 		stmt, err = c.database.Prepare(c.insertResultTable(fmt.Sprintf("data_%d", c.serverID)))
+		cacheLock.Unlock()
 		if err != nil {
 			c.Errf("cannot prepare SQL query to insert history in data_%d : %s", c.serverID, err)
 		} else {
@@ -372,8 +405,10 @@ func (c *DiskCache) write(r *plugin.Result) {
 		}
 	}
 	if stmt != nil {
+		cacheLock.Lock()
 		_, err = stmt.Exec(c.lastDataID, now, r.Itemid, LastLogsize, Mtime, State, Value,
 			EventSource, EventID, EventSeverity, EventTimestamp, clock, ns)
+		cacheLock.Unlock()
 		if err != nil {
 			c.Errf("cannot execute SQL statement : %s", err)
 		}
@@ -426,22 +461,28 @@ func (c *DiskCache) init(options *agent.AgentOptions) {
 	c.updateOptions(options)
 
 	var err error
+	cacheLock.Lock()
 	c.database, err = sql.Open("sqlite3", agent.Options.PersistentBufferFile)
+	cacheLock.Unlock()
 	if err != nil {
 		return
 	}
 
+	cacheLock.Lock()
 	rows, err := c.database.Query(fmt.Sprintf("SELECT id FROM registry WHERE address = '%s'", c.uploader.Addr()))
+	cacheLock.Unlock()
 
 	if err == nil {
 		defer rows.Close()
 
+		cacheLock.Lock()
 		for rows.Next() {
 			if err = rows.Scan(&c.serverID); err != nil {
 				c.Errf("cannot retrieve diskcache server ID ")
 				c.serverID = 0
 			}
 		}
+		cacheLock.Unlock()
 	}
 
 	/* log history is removed before disk cache is initialized, so only data history needs to be checked */
