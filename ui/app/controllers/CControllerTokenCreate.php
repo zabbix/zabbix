2e5e19b79182d411a4198ffdefd2ff46c681ff5a
Elina Pulke <elina.pulke@zabbix.com>
2022-02-02 15:32:20 +0200

    ..F....... [ZBXNEXT-7138] fixed eror message title

--- ui/app/controllers/CControllerTokenCreate.php
+++ ui/app/controllers/CControllerTokenCreate.php
@@ -40,7 +40,7 @@ class CControllerTokenCreate extends CController
 			$this->setResponse(
 				new CControllerResponseData(['main_block' => json_encode([
 					'error' => [
-						'title' => _('Cannot add host'),
+						'title' => _('Cannot add API token'),
 						'messages' => array_column(get_and_clear_messages(), 'message')
 					]
 				])])
