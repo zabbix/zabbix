1fe464b8e8f551f6c373fecc5522975fab7b9ef8
Reinis Detlavs <reinis.detlavs@zabbix.com>
2022-02-07 17:30:45 +0200

    ..F....... [ZBX-20253] fixed incorrect usage of function "length" in zabbix server trigger creation

--- ui/app/views/popup.triggerexpr.php
+++ ui/app/views/popup.triggerexpr.php
@@ -138,7 +138,7 @@ if (array_key_exists('params', $data['functions'][$data['selectedFunction']])) {
 	$count_functions = [
 		'acos', 'ascii', 'asin', 'atan', 'atan2', 'between', 'bitand', 'bitlength', 'bitlshift', 'bitnot', 'bitor',
 		'bitrshift', 'bitxor', 'bytelength', 'cbrt', 'ceil', 'char', 'concat', 'cos', 'cosh', 'cot', 'degrees', 'exp',
-		'expm1', 'floor', 'in', 'insert', 'last', 'left', 'log', 'log10', 'ltrim', 'mid', 'mod', 'power', 'radians',
+		'expm1', 'floor', 'in', 'insert', 'last', 'left', 'length', 'log', 'log10', 'ltrim', 'mid', 'mod', 'power', 'radians',
 		'repeat', 'replace', 'right', 'round', 'rtrim', 'signum', 'sin', 'sinh', 'sqrt', 'tan', 'trim', 'truncate'
 	];
 
