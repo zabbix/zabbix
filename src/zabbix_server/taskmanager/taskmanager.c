9a9b381ec4a396abe29410597cc1e7bac7ba0a6d
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2022-02-09 07:35:06 +0200

    .......PS. [ZBXNEXT-1580] small refactoring

--- src/zabbix_server/taskmanager/taskmanager.c
+++ src/zabbix_server/taskmanager/taskmanager.c
@@ -760,7 +760,7 @@ static int	tm_process_data(zbx_ipc_async_socket_t *rtc, zbx_vector_uint64_t *tas
 
 		switch (data_type)
 		{
-			case ZBX_TM_DATA_TYPE_PROXY_CONFIG_RELOAD:
+			case ZBX_TM_DATA_TYPE_PROXY_HOSTIDS:
 				tm_process_proxy_config_reload_task(rtc, row[2]);
 				zbx_vector_uint64_append(&done_taskids, taskid);
 				break;
