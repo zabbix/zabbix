22252d5438897317ab0405cc6478f86c11b2fc98
Alexander Shubin <aleksandrs.subins@zabbix.com>
2022-02-10 14:21:47 +0200

    ..F....... [ZBX-20426] fixed code style

--- ui/include/defines.inc.php
+++ ui/include/defines.inc.php
@@ -1851,7 +1851,7 @@ define('ZBX_STYLE_ICON_INFO', 'icon-info');
 define('ZBX_STYLE_ICON_INVISIBLE', 'icon-invisible');
 define('ZBX_STYLE_ICON_USER', 'icon-user');
 define('ZBX_STYLE_ICON_USER_GROUP', 'icon-user-group');
-define('ZBX_STYLE_ICON_MAINT', 'icon-maint');
+define('ZBX_STYLE_ICON_MAINTENANCE', 'icon-maintenance');
 define('ZBX_STYLE_ICON_WZRD_ACTION', 'icon-wzrd-action');
 define('ZBX_STYLE_ACTION_COMMAND', 'icon-action-command');
 define('ZBX_STYLE_ACTION_ICON_CLOSE', 'icon-action-close');
