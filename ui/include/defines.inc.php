34579aa0509be44b91eb0196eb6ac48224280736
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2022-02-23 15:01:47 +0200

    .......... [ZBXNEXT-826] updated release version and date [6.0.1rc2]

--- ui/include/defines.inc.php
+++ ui/include/defines.inc.php
@@ -18,7 +18,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-define('ZABBIX_VERSION',		'6.0.1rc1');
+define('ZABBIX_VERSION',		'6.0.1rc2');
 define('ZABBIX_API_VERSION',	'6.0.1');
 define('ZABBIX_EXPORT_VERSION',	'6.0');
 
