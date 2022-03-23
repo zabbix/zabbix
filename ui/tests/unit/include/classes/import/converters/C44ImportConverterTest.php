85a16d731517acba58f544825580cfc6620c6774
Aleksejs Cikuns <aleksejs.cikuns@zabbix.com>
2022-03-23 13:53:22 +0200

    .......... [ZBX-20698] fixed unit tests

--- ui/tests/unit/include/classes/import/converters/C44ImportConverterTest.php
+++ ui/tests/unit/include/classes/import/converters/C44ImportConverterTest.php
@@ -316,7 +316,7 @@ class C44ImportConverterTest extends CImportConverterTest {
 									'interface_ref' => 'if1',
 								],
 								[
-									'interface_ref' => 'if2',
+									'interface_ref' => 'if3',
 									'type' => 'SNMP',
 									'port' => '161',
 									'details' => [
@@ -327,14 +327,14 @@ class C44ImportConverterTest extends CImportConverterTest {
 							],
 							'items' => [
 								[
-									'interface_ref' => 'if2',
+									'interface_ref' => 'if3',
 									'key' => 'snmptrap.fallback',
 									'type' => 'SNMP_TRAP',
 									'name' => 'SNMp trap',
 									'delay' => 0
 								],
 								[
-									'interface_ref' => 'if2',
+									'interface_ref' => 'if3',
 									'key' => 'snmptrap[asd]',
 									'type' => 'SNMP_TRAP',
 									'name' => 'Snmp trap item',
