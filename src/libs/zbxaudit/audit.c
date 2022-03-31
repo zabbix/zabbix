d77b86a3d9112d8381719b9ed56f9c3b5a7dff8f
Andrejs Kozlovs <andrejs.kozlovs@zabbix.com>
2022-03-31 11:42:30 +0300

    ........S. [ZBX-20750] fixed problem with audit log details displaying in some cases

--- src/libs/zbxaudit/audit.c
+++ src/libs/zbxaudit/audit.c
@@ -311,15 +311,11 @@ void	zbx_audit_flush(void)
 		if (AUDIT_ACTION_DELETE == (*audit_entry)->audit_action ||
 				0 != strcmp((*audit_entry)->details_json.buffer, "{}"))
 		{
-			char	*details_esc;
-
-			details_esc = DBdyn_escape_string((*audit_entry)->details_json.buffer);
-
 			zbx_db_insert_add_values(&db_insert_audit, (*audit_entry)->audit_cuid, AUDIT_USERID,
 					AUDIT_USERNAME, (int)time(NULL), (*audit_entry)->audit_action, AUDIT_IP,
 					(*audit_entry)->id, (*audit_entry)->name, (*audit_entry)->resource_type,
-					recsetid_cuid, 0 == strcmp(details_esc, "{}") ? "" : details_esc);
-			zbx_free(details_esc);
+					recsetid_cuid, 0 == strcmp((*audit_entry)->details_json.buffer, "{}") ? "" :
+					(*audit_entry)->details_json.buffer);
 		}
 	}
 
@@ -344,7 +340,7 @@ int	zbx_audit_flush_once(void)
 
 	while (NULL != (audit_entry = (zbx_audit_entry_t **)zbx_hashset_iter_next(&iter)))
 	{
-		char	id[ZBX_MAX_UINT64_LEN + 1], *pvalue, *name_esc, *details_esc;
+		char	id[ZBX_MAX_UINT64_LEN + 1], *pvalue, *name_esc;
 		const char	*pfield;
 
 		if (AUDIT_ACTION_DELETE != (*audit_entry)->audit_action &&
@@ -366,16 +362,15 @@ int	zbx_audit_flush_once(void)
 		}
 
 		name_esc = DBdyn_escape_string((*audit_entry)->name);
-		details_esc = DBdyn_escape_string((*audit_entry)->details_json.buffer);
 
 		ret = DBexecute_once("insert into auditlog (auditid,userid,username,"
 				"clock,action,ip,%s,resourcename,resourcetype,recordsetid,details) values"
 				" ('%s'," AUDIT_USERID_SQL ",'%s','%d','%d','%s','%s','%s',%d,'%s','%s')",
 				pfield, (*audit_entry)->audit_cuid, AUDIT_USERNAME, (int)time(NULL),
 				(*audit_entry)->audit_action, AUDIT_IP, pvalue, name_esc, (*audit_entry)->resource_type,
-				recsetid_cuid, 0 == strcmp(details_esc, "{}") ? "" : details_esc);
+				recsetid_cuid, 0 == strcmp((*audit_entry)->details_json.buffer, "{}") ? "" :
+				(*audit_entry)->details_json.buffer);
 
-		zbx_free(details_esc);
 		zbx_free(name_esc);
 
 		if (ZBX_DB_OK > ret)
