f4fe1908d235b84e0a32357a8494e04ff4793944
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2022-04-01 14:58:57 +0300

    ..F....... [ZBX-20638] made error messages more accurate so that it is clear that Zabbix server has not been started

--- ui/app/partials/administration.system.info.php
+++ ui/app/partials/administration.system.info.php
@@ -141,42 +141,42 @@ if ($data['user_type'] == USER_TYPE_SUPER_ADMIN) {
 
 		switch ($dbversion['flag']) {
 			case DB_VERSION_LOWER_THAN_MINIMUM:
-				$error = _s('Minimum required %1$s database version is %2$s.', $dbversion['database'],
-					$dbversion['min_version']
+				$error = _s('Error! Unable to start Zabbix server. Minimum required %1$s database version is %2$s.',
+					$dbversion['database'], $dbversion['min_version']
 				);
 				break;
 
 			case DB_VERSION_HIGHER_THAN_MAXIMUM:
-				$error = _s('Maximum required %1$s database version is %2$s.', $dbversion['database'],
-					$dbversion['max_version']
+				$error = _s('Error! Unable to start Zabbix server. Maximum required %1$s database version is %2$s.',
+					$dbversion['database'], $dbversion['max_version']
 				);
 				break;
 
 			case DB_VERSION_FAILED_TO_RETRIEVE:
-				$error = _('Unable to retrieve database version.');
+				$error = _('Warning! Unable to retrieve database version.');
 				$dbversion['current_version'] = '';
 				break;
 
 			case DB_VERSION_NOT_SUPPORTED_ERROR:
-				$error = _s('Error! Unable to start Zabbix server due to unsupported %1$s database server version. Must be at least (%2$s)',
+				$error = _s('Error! Unable to start Zabbix server due to unsupported %1$s database server version. Must be at least (%2$s).',
 					$dbversion['database'], $dbversion['min_supported_version']
 				);
 				break;
 
 			case DB_VERSION_NOT_SUPPORTED_WARNING:
-				$error = _s('Warning! Unsupported %1$s database server version. Should be at least (%2$s)',
+				$error = _s('Warning! Unsupported %1$s database server version. Should be at least (%2$s).',
 					$dbversion['database'], $dbversion['min_supported_version']
 				);
 				break;
 
 			case DB_VERSION_HIGHER_THAN_MAXIMUM_ERROR:
-				$error = _s('Error! Unable to start Zabbix server due to unsupported %1$s database server version. Must not be higher than (%2$s)',
+				$error = _s('Error! Unable to start Zabbix server due to unsupported %1$s database server version. Must not be higher than (%2$s).',
 					$dbversion['database'], $dbversion['max_version']
 				);
 				break;
 
 			case DB_VERSION_HIGHER_THAN_MAXIMUM_WARNING:
-				$error = _s('Warning! Unsupported %1$s database server version. Should not be higher than (%2$s)',
+				$error = _s('Warning! Unsupported %1$s database server version. Should not be higher than (%2$s).',
 					$dbversion['database'], $dbversion['max_version']
 				);
 				break;
