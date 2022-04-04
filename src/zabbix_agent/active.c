6c55f131a3bed39b715298fb0d67e8cb8d70889f
Mihails Prihodko <mihails.prihodko@zabbix.com>
2022-04-04 16:24:12 +0300

    .......... [DEV-2087] type conversion was made more uniform

--- src/zabbix_agent/active.c
+++ src/zabbix_agent/active.c
@@ -1458,7 +1458,7 @@ ZBX_THREAD_ENTRY(active_checks_thread, args)
 	zbx_free(session_token);
 
 #ifdef _WINDOWS
-	zbx_vector_ptr_clear_ext(&activechk_args.addrs, (zbx_ptr_free_func_t)zbx_addr_free);
+	zbx_vector_ptr_clear_ext(&activechk_args.addrs, (zbx_clean_func_t)zbx_addr_free);
 	zbx_vector_ptr_destroy(&activechk_args.addrs);
 	free_active_metrics();
 
