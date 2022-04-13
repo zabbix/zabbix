faf2f8efbe5f6d3c6cc175911a88715921cd3af9
Andrejs Verza <andrejs.verza@zabbix.com>
2022-04-13 16:34:44 +0300

    ..F....... [ZBX-20782] fixed host enable/disable mass-update actions resulting in error response

--- ui/app/controllers/CControllerPopupMassupdateHost.php
+++ ui/app/controllers/CControllerPopupMassupdateHost.php
@@ -25,7 +25,7 @@ class CControllerPopupMassupdateHost extends CControllerPopupMassupdateAbstract 
 
 	protected function checkInput(): bool {
 		$fields = [
-			'ids' => 'required|array',
+			'hostids' => 'required|array',
 			'update' => 'in 1',
 			'visible' => 'array',
 			'tags' => 'array',
@@ -86,7 +86,7 @@ class CControllerPopupMassupdateHost extends CControllerPopupMassupdateAbstract 
 	protected function checkPermissions(): bool {
 		$hosts = API::Host()->get([
 			'output' => [],
-			'hostids' => $this->getInput('ids'),
+			'hostids' => $this->getInput('hostids'),
 			'editable' => true
 		]);
 
@@ -96,7 +96,7 @@ class CControllerPopupMassupdateHost extends CControllerPopupMassupdateAbstract 
 	protected function doAction(): void {
 		if ($this->hasInput('update')) {
 			$output = [];
-			$hostids = $this->getInput('ids');
+			$hostids = $this->getInput('hostids');
 			$visible = $this->getInput('visible', []);
 			$macros = array_filter(cleanInheritedMacros($this->getInput('macros', [])),
 				function (array $macro): bool {
@@ -480,7 +480,7 @@ class CControllerPopupMassupdateHost extends CControllerPopupMassupdateAbstract 
 				'user' => [
 					'debug_mode' => $this->getDebugMode()
 				],
-				'ids' => $this->getInput('ids'),
+				'ids' => $this->getInput('hostids'),
 				'inventories' => zbx_toHash(getHostInventories(), 'db_field'),
 				'location_url' => (new CUrl('zabbix.php'))
 					->setArgument('action', 'host.list')
