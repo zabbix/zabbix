690d13de8d2b671abd58cedb688f17da4763f2d3
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2022-04-19 11:49:23 +0300

    .......... [ZBXNEXT-826] updated release date [6.2.0alpha1]

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -26,7 +26,7 @@ import (
 )
 
 const (
-	ZABBIX_REVDATE          = "14 February 2022"
+	ZABBIX_REVDATE          = "19 April 2022"
 	ZABBIX_VERSION_MAJOR    = 6
 	ZABBIX_VERSION_MINOR    = 2
 	ZABBIX_VERSION_PATCH    = 0
