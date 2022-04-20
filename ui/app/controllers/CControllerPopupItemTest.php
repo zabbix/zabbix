d535ad9b9238b2ecc92df1608bdee1e9a5f9635d
Reinis Detlavs <reinis.detlavs@zabbix.com>
2022-04-20 13:04:29 +0300

    ..F....... [ZBX-20299] fixed not detecting user macros by "Test item" dialog if they are used in the calculated item formula
    * commit 'c001dcc78886b822898a2a2cf5bd23768e421fc6':
      ..F....... [ZBX-20299] fixed code styles
      ..F....... [ZBX-20299] fixed not detecting user macros by "Test item" dialog if they are used in the calculated item formula
    
    (cherry picked from commit 6da7abd84c7d80ddb4e9ac8a8197549308cf55e5)

--- ui/app/controllers/CControllerPopupItemTest.php
+++ ui/app/controllers/CControllerPopupItemTest.php
@@ -1057,13 +1057,12 @@ abstract class CControllerPopupItemTest extends CController {
 	/**
 	 * Resolve macros used in the calculates item formula.
 	 *
-	 * @param string $formula  Calculated item formula.
+	 * @param string $formula        Calculated item formula.
+	 * @param array  $macros_posted  Macros.
 	 *
-	 * @return array
+	 * @return string
 	 */
-	private function resolveCalcFormulaMacros(string $formula) {
-		$macros_posted = $this->getInput('macros', []);
-
+	private function resolveCalcFormulaMacros(string $formula, array $macros_posted): string {
 		if (!$macros_posted) {
 			return $formula;
 		}
@@ -1090,51 +1089,74 @@ abstract class CControllerPopupItemTest extends CController {
 			CExpressionParserResult::TOKEN_TYPE_STRING,
 			CExpressionParserResult::TOKEN_TYPE_HIST_FUNCTION
 		]);
+
 		foreach ($tokens as $token) {
+			if ($pos_left != $token['pos']) {
+				$expression[] = substr($formula, $pos_left, $token['pos'] - $pos_left);
+			}
+			$pos_left = $token['pos'] + $token['length'];
+
 			switch ($token['type']) {
 				case CExpressionParserResult::TOKEN_TYPE_USER_MACRO:
 				case CExpressionParserResult::TOKEN_TYPE_LLD_MACRO:
-					if ($pos_left != $token['pos']) {
-						$expression[] = substr($formula, $pos_left, $token['pos'] - $pos_left);
-					}
-					$pos_left = $token['pos'] + $token['length'];
-
 					$expression[] = array_key_exists($token['match'], $macros_posted)
 						? CExpressionParser::quoteString($macros_posted[$token['match']], false)
 						: $token['match'];
 					break;
 
 				case CExpressionParserResult::TOKEN_TYPE_STRING:
-					if ($pos_left != $token['pos']) {
-						$expression[] = substr($formula, $pos_left, $token['pos'] - $pos_left);
-					}
-					$pos_left = $token['pos'] + $token['length'];
-
 					$string = strtr(CExpressionParser::unquoteString($token['match']), $macros_posted);
 					$expression[] = CExpressionParser::quoteString($string, false, true);
 					break;
 
 				case CExpressionParserResult::TOKEN_TYPE_HIST_FUNCTION:
-					foreach ($token['data']['parameters'][0]['data']['filter']['tokens'] as $filter_token) {
-						switch ($filter_token['type']) {
-							case CFilterParser::TOKEN_TYPE_USER_MACRO:
-							case CFilterParser::TOKEN_TYPE_LLD_MACRO:
-								if ($pos_left != $filter_token['pos']) {
-									$expression[] = substr($formula, $pos_left, $filter_token['pos'] - $pos_left);
-								}
-								$pos_left = $filter_token['pos'] + $filter_token['length'];
+					$pos_left = $token['pos'];
 
-								$string = strtr($filter_token['match'], $macros_posted);
+					foreach ($token['data']['parameters'] as $parameter) {
+						if ($pos_left != $parameter['pos']) {
+							$expression[] = substr($formula, $pos_left, $parameter['pos'] - $pos_left);
+						}
+						$pos_left = $parameter['pos'] + $parameter['length'];
+
+						switch ($parameter['type']) {
+							case CHistFunctionParser::PARAM_TYPE_QUERY:
+								$pos_left = $parameter['pos'];
+
+								foreach ($parameter['data']['filter']['tokens'] as $filter_token) {
+									if ($pos_left != $filter_token['pos']) {
+										$expression[] = substr($formula, $pos_left, $filter_token['pos'] - $pos_left);
+									}
+									$pos_left = $filter_token['pos'] + $filter_token['length'];
+
+									switch ($filter_token['type']) {
+										case CFilterParser::TOKEN_TYPE_USER_MACRO:
+										case CFilterParser::TOKEN_TYPE_LLD_MACRO:
+											$string = strtr($filter_token['match'], $macros_posted);
+											$expression[] = CFilterParser::quoteString($string);
+											break;
+
+										case CFilterParser::TOKEN_TYPE_STRING:
+											$string = strtr(CFilterParser::unquoteString($filter_token['match']),
+												$macros_posted
+											);
+											$expression[] = CFilterParser::quoteString($string);
+											break;
+									}
+								}
+								break;
+
+							case CHistFunctionParser::PARAM_TYPE_PERIOD:
+								$string = strtr($parameter['match'], $macros_posted);
+								$expression[] = $string;
+								break;
+
+							case CHistFunctionParser::PARAM_TYPE_QUOTED:
+								$string = strtr(CFilterParser::unquoteString($parameter['match']), $macros_posted);
 								$expression[] = CFilterParser::quoteString($string);
 								break;
 
-							case CFilterParser::TOKEN_TYPE_STRING:
-								if ($pos_left != $filter_token['pos']) {
-									$expression[] = substr($formula, $pos_left, $filter_token['pos'] - $pos_left);
-								}
-								$pos_left = $filter_token['pos'] + $filter_token['length'];
-
-								$string = strtr(CFilterParser::unquoteString($filter_token['match']), $macros_posted);
+							case CHistFunctionParser::PARAM_TYPE_UNQUOTED:
+								$string = strtr($parameter['match'], $macros_posted);
 								$expression[] = CFilterParser::quoteString($string);
 								break;
 						}
