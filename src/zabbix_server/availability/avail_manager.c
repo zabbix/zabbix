9eb6078cfc1a433b66d2016defa95bd1b7bbc085
Andris Zeila <andris.zeila@zabbix.com>
2022-04-28 20:25:26 +0300

    ........S. [ZBXNEXT-5088] coding style fix, decreased unavailable proxy check to 10 minutes

--- src/zabbix_server/availability/avail_manager.c
+++ src/zabbix_server/availability/avail_manager.c
@@ -40,7 +40,7 @@ zbx_active_avail_proxy_t;
 #define ZBX_AVAILABILITY_MANAGER_DELAY				1
 #define ZBX_AVAILABILITY_MANAGER_FLUSH_DELAY_SEC		5
 #define ZBX_AVAILABILITY_MANAGER_ACTIVE_HEARTBEAT_DELAY_SEC	10
-#define ZBX_AVAILABILITY_MANAGER_PROXY_ACTIVE_AVAIL_DELAY_SEC	3600
+#define ZBX_AVAILABILITY_MANAGER_PROXY_ACTIVE_AVAIL_DELAY_SEC	(SEC_PER_MIN * 10)
 
 static int	interface_availability_compare(const void *d1, const void *d2)
 {
@@ -387,19 +387,21 @@ static void	active_checks_calculate_proxy_availability(zbx_avail_active_hb_cache
 {
 	zbx_hashset_iter_t		iter;
 	zbx_active_avail_proxy_t	*proxy_avail;
-
+	int				now;
 
 	if (0 == cache->proxy_avail.num_data)
 		return;
 
+	now = time(NULL);
 	zbx_hashset_iter_reset(&cache->proxy_avail, &iter);
 
 	while (NULL != (proxy_avail = (zbx_active_avail_proxy_t *)zbx_hashset_iter_next(&iter)))
 	{
-		if (proxy_avail->lastaccess + ZBX_AVAILABILITY_MANAGER_PROXY_ACTIVE_AVAIL_DELAY_SEC <= time(NULL))
+		if (proxy_avail->lastaccess + ZBX_AVAILABILITY_MANAGER_PROXY_ACTIVE_AVAIL_DELAY_SEC <= now)
 		{
-			if (ZBX_DB_OK > DBexecute("update host_rtdata set active_available=%i where hostid in (select hostid from hosts where "
-					"proxy_hostid=" ZBX_FS_UI64 ")", INTERFACE_AVAILABLE_UNKNOWN, proxy_avail->hostid))
+			if (ZBX_DB_OK > DBexecute("update host_rtdata set active_available=%i"
+					" where hostid in (select hostid from hosts where proxy_hostid=" ZBX_FS_UI64 ")",
+					INTERFACE_AVAILABLE_UNKNOWN, proxy_avail->hostid))
 			{
 				continue;
 			}
