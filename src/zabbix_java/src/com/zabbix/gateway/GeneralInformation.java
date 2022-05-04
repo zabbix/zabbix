e99ca6c1ec4052cd207873494b345115b8252b51
Natalja Romancaka <natalja.romancaka@zabbix.com>
2022-05-04 16:16:08 +0300

    .......... [ZBXNEXT-826] updated release version [6.0.5rc1]; updated API version [6.0.5]

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "3 May 2022";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "6.0.4";
+	static final String VERSION = "6.0.5rc1";
 
 	static void printVersion()
 	{
