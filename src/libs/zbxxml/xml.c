5d1e9592168c233b0deb534684a96f809cd68198
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2022-05-06 16:36:52 +0300

    .......PS. [ZBXNEXT-7029] fixed xml node removal

--- src/libs/zbxxml/xml.c
+++ src/libs/zbxxml/xml.c
@@ -1440,6 +1440,7 @@ int	zbx_xml_node_remove(xmlDoc *xdoc, xmlNode *node, const char *xpath)
 	{
 		xmlUnlinkNode(nodeset->nodeTab[i]);
 		xmlFreeNode(nodeset->nodeTab[i]);
+		nodeset->nodeTab[i] = NULL;
 	}
 
 	ret = SUCCEED;
