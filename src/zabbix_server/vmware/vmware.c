85067fca558c4f12dabc119c59e1abd334dfba31
Michael Veksler <Mihails.Vekslers@zabbix.com>
2022-05-09 17:09:26 +0300

    .......PS. [ZBXNEXT-7029] fixed error of removing xml node from empty result

--- src/zabbix_server/vmware/vmware.c
+++ src/zabbix_server/vmware/vmware.c
@@ -6636,13 +6636,8 @@ static void	vmware_service_dvswitch_load(CURL *easyhandle, zbx_vector_cq_value_t
 		if (NULL == (node = zbx_xml_doc_get(doc, "/*/*" ZBX_XPATH_LN("FetchDVPortsResponse"))))
 			continue;
 
-		if (0 == strcmp(cqv->instance->mode, "state") &&
-				SUCCEED != zbx_xml_node_remove(doc ,node, ZBX_XNN("returnval") ZBX_XPATH_LN("config")))
-		{
-			cqv->response = zbx_strdup(NULL, "Cannot remove \"config\" node from FetchDVPortsResponse.");
-			cqv->status = ZBX_VMWARE_CQV_ERROR;
-			continue;
-		}
+		if (0 == strcmp(cqv->instance->mode, "state"))	/* Ignore node remove error for empty result */
+				zbx_xml_node_remove(doc ,node, ZBX_XNN("returnval") ZBX_XPATH_LN("config"));
 
 		if (SUCCEED != zbx_xmlnode_to_json(node, &cqv->response))
 		{
