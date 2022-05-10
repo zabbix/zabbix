05e242a5fb3f80f17ae0559fe443692e719c6468
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2022-05-10 11:34:14 +0000

    .......... [DEV-2113] fixed makefiles

--- src/libs/zbxserver/evaluate_simple.c
+++ src/libs/zbxserver/evaluate_simple.c
@@ -17,7 +17,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-#include "expression_evaluate.h"
+#include "zbxserver.h"
 
 #include "common.h"
 #include "zbxvariant.h"
