32929452e00673a08693911c81ce84a514f9b583
Michael Veksler <Mihails.Vekslers@zabbix.com>
2022-05-12 16:55:04 +0300

    .......PS. [ZBX-20600] refactoring dsname for check_vcenter_datastore_hv_list

--- src/zabbix_server/poller/checks_simple_vmware.c
+++ src/zabbix_server/poller/checks_simple_vmware.c
@@ -2359,19 +2359,19 @@ int	check_vcenter_datastore_hv_list(AGENT_REQUEST *request, const char *username
 	if (NULL == (service = get_vmware_service(url, username, password, result, &ret)))
 		goto unlock;
 
-	for (i = 0; i < service->data->datastores.values_num; i++)
+
+	if (NULL == (datastore = ds_get(&service->data->datastores, ds_name)))
 	{
-		if (0 != strcmp(ds_name, service->data->datastores.values[i]->name))
-			continue;
+		zbx_vmware_datastore_t	ds_cmp =  {.name = (char *)ds_name};
+
+		if (FAIL == (i = zbx_vector_vmware_datastore_search(&service->data->datastores, &ds_cmp,
+				vmware_ds_name_compare)))
+		{
+			SET_MSG_RESULT(result, zbx_strdup(NULL, "Unknown datastore name."));
+			goto unlock;
+		}
 
 		datastore = service->data->datastores.values[i];
-		break;
-	}
-
-	if (NULL == datastore)
-	{
-		SET_MSG_RESULT(result, zbx_strdup(NULL, "Unknown datastore name."));
-		goto unlock;
 	}
 
 	for (i=0; i < datastore->hv_uuids_access.values_num; i++)
