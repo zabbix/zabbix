57c2b0dfa0207376c42630f92da6712d1a8cbba1
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2022-05-13 17:04:17 +0300

    ..F....... [ZBX-20242] fixed inability to change LDAP bind password

--- ui/app/controllers/CControllerAuthenticationEdit.php
+++ ui/app/controllers/CControllerAuthenticationEdit.php
@@ -92,13 +92,11 @@ class CControllerAuthenticationEdit extends CController {
 		$openssl_status = (new CFrontendSetup())->checkPhpOpenSsl();
 
 		$data = [
-			'action_submit' => 'authentication.update',
-			'action_passw_change' => 'authentication.edit',
 			'ldap_error' => ($ldap_status['result'] == CFrontendSetup::CHECK_OK) ? '' : $ldap_status['error'],
 			'ldap_test_password' => '',
 			'ldap_test_user' => CWebUser::$data['username'],
 			'saml_error' => ($openssl_status['result'] == CFrontendSetup::CHECK_OK) ? '' : $openssl_status['error'],
-			'change_bind_password' => 0,
+			'change_bind_password' => 1,
 			'form_refresh' => 0
 		];
 
