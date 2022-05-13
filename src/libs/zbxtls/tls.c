33152c42b621b7acaeb6ff5d7a26b41e94d1ccf6
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2022-05-13 12:28:55 +0000

    .......... [DEV-2137] added fix

--- src/libs/zbxtls/tls.c
+++ src/libs/zbxtls/tls.c
@@ -17,11 +17,10 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-#include "zbxtls.h"
-
 #include "zbxsysinc.h"
 
 #if defined(HAVE_GNUTLS) || defined(HAVE_OPENSSL)
+#include "zbxtls.h"
 
 #include "tls.h"
 #include "zbxcomms.h"
