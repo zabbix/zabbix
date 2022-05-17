d3e31d69b6bdf69df5a5d0891932cff430dfb870
Andris Zeila <andris.zeila@zabbix.com>
2022-05-17 12:48:21 +0300

    .......PS. [ZBXNEXT-7553] removed duplicate typedef

--- src/libs/zbxdbcache/dbconfig.h
+++ src/libs/zbxdbcache/dbconfig.h
@@ -905,4 +905,8 @@ char	*dc_expand_user_macros(const char *text, const zbx_uint64_t *hostids, int h
 #define ZBX_TRIGGER_TIMER_FUNCTION_TREND	0x0004
 #define ZBX_TRIGGER_TIMER_FUNCTION		(ZBX_TRIGGER_TIMER_FUNCTION_TIME | ZBX_TRIGGER_TIMER_FUNCTION_TREND)
 
+
+zbx_um_cache_t	*um_cache_sync(zbx_um_cache_t *cache, zbx_dbsync_t *gmacros, zbx_dbsync_t *hmacros,
+		zbx_dbsync_t *htmpls);
+
 #endif
