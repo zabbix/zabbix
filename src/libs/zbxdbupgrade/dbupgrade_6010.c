677404e08f20213feda3e14004f59df32f12e354
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2022-05-19 18:26:19 +0300

    ........S. [ZBXNEXT-5517] fixed db patch

--- src/libs/zbxdbupgrade/dbupgrade_6010.c
+++ src/libs/zbxdbupgrade/dbupgrade_6010.c
@@ -477,7 +477,7 @@ static int	DBpatch_6010025(void)
 
 static int	DBpatch_6010026(void)
 {
-	const	ZBX_FIELD field = {"state", "0", NULL, NULL, 0, ZBX_TYPE_INT, ZBX_NOTNULL, 0};
+	const	ZBX_FIELD field = {"automatic", "0", NULL, NULL, 0, ZBX_TYPE_INT, ZBX_NOTNULL, 0};
 
 	return DBadd_field("hostmacro", &field);
 }
