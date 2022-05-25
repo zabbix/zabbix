9ae441498bad95d5d7950bf43c835f3a4a2febb6
Gregory Chalenko <gregory.chalenko@zabbix.com>
2022-05-25 11:02:04 +0300

    A......... [ZBXNEXT-7554] fixed host and template tags removal

--- ui/include/classes/api/services/CHost.php
+++ ui/include/classes/api/services/CHost.php
@@ -1573,6 +1573,7 @@ class CHost extends CHostGeneral {
 		DB::delete('host_inventory', ['hostid' => $hostids]);
 
 		// delete host
+		DB::delete('host_tag', ['hostid' => $hostids]);
 		DB::update('hosts', [
 			'values' => ['templateid' => 0],
 			'where' => ['hostid' => $hostids, 'flags' => ZBX_FLAG_DISCOVERY_PROTOTYPE]
