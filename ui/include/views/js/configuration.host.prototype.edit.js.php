a91bb6eb00d78bf584e30e3f7b18a3f6430ee9b7
Elina Pulke <elina.pulke@zabbix.com>
2022-06-01 09:11:35 +0300

    ..F....... [ZBX-20565] minor fix - moved code to a more appropriate place

--- ui/include/views/js/configuration.host.prototype.edit.js.php
+++ ui/include/views/js/configuration.host.prototype.edit.js.php
@@ -48,14 +48,6 @@
 			return;
 		}
 	}
-
-	jQuery(function($) {
-		$('#host')
-			.on('input keydown paste', function () {
-				$('#name').attr('placeholder', $(this).val());
-			})
-			.trigger('input');
-	});
 </script>
 
 <script type="text/javascript">
@@ -261,5 +253,11 @@
 		}
 
 		jQuery('input[name=tls_connect]').trigger('change');
+
+		jQuery('#host')
+			.on('input keydown paste', function () {
+				$('#name').attr('placeholder', $(this).val());
+			})
+			.trigger('input');
 	});
 </script>
