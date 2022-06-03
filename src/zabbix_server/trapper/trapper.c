a6dfef4eb1e24a4a18d1e24dde957ca07adbe43e
Jurijs Klopovskis <jurijs.klopovskis@zabbix.com>
2022-06-03 11:03:18 +0300

    ........S. [ZBX-20105] renamed zbx_comms_parse_response to comms_parse_response

--- src/zabbix_server/trapper/trapper.c
+++ src/zabbix_server/trapper/trapper.c
@@ -985,7 +985,7 @@ static int	process_active_check_heartbeat(struct zbx_json_parse *jp)
 	return SUCCEED;
 }
 
-static int	zbx_comms_parse_response(char *xml, char *host, size_t host_len, char *key, size_t key_len,
+static int	comms_parse_response(char *xml, char *host, size_t host_len, char *key, size_t key_len,
 		char *data, size_t data_len, char *lastlogsize, size_t lastlogsize_len,
 		char *timestamp, size_t timestamp_len, char *source, size_t source_len,
 		char *severity, size_t severity_len)
@@ -1234,7 +1234,7 @@ static int	process_trap(zbx_socket_t *sock, char *s, ssize_t bytes_received, zbx
 
 		if ('<' == *s)	/* XML protocol */
 		{
-			zbx_comms_parse_response(s, host, sizeof(host), key, sizeof(key), value_dec,
+			comms_parse_response(s, host, sizeof(host), key, sizeof(key), value_dec,
 					sizeof(value_dec), lastlogsize, sizeof(lastlogsize), timestamp,
 					sizeof(timestamp), source, sizeof(source), severity, sizeof(severity));
 
