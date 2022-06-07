847dbf804ce6a3782a369431cbb985d65f68a19f
Natalja Romancaka <natalja.romancaka@zabbix.com>
2022-06-07 09:09:21 +0300

    .......... [ZBXNEXT-686] fixed Selenium tests, removed script tag from xpath

--- ui/tests/selenium/testTagBasedPermissions.php
+++ ui/tests/selenium/testTagBasedPermissions.php
@@ -200,6 +200,7 @@ class testTagBasedPermissions extends CLegacyWebTest {
 			$this->zbxTestLaunchOverlayDialog('Triggers');
 			COverlayDialogElement::find()->one()->waitUntilReady()->setDataContext($this->trigger_host);
 			$this->zbxTestClickLinkTextWait($name);
+			COverlayDialogElement::ensureNotPresent();
 			// Apply filter
 			$this->query('name:filter_apply')->one()->click();
 			$table->waitUntilReloaded();
