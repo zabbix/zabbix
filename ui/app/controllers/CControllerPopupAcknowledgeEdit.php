f63e7852011ff7793af3f48c7fdc08ef8bff8bdf
Miks Kronkalns <miks.kronkalns@zabbix.com>
2022-06-08 12:29:09 +0300

    ..F....... [ZBXNEXT-721] reverted commit 4342c82bf6853bc76b626a0dad97f3cbbf0fbece

--- ui/app/controllers/CControllerPopupAcknowledgeEdit.php
+++ ui/app/controllers/CControllerPopupAcknowledgeEdit.php
@@ -144,10 +144,10 @@ class CControllerPopupAcknowledgeEdit extends CController {
 		foreach ($events as $event) {
 			$can_be_closed = true;
 			$can_be_suppressed = true;
-			$can_be_unsuppressed = isEventRecentlySuppressed($event['acknowledges']);
+			$can_be_unsuppressed = false;
 
 			// Only manually suppressed problems can be unsuppressed.
-			if ($data['allowed_suppress'] && $can_be_unsuppressed) {
+			if ($this->checkAccess(CRoleHelper::ACTIONS_SUPPRESS_PROBLEMS)) {
 				foreach ($event['suppression_data'] as $suppression) {
 					if ($suppression['maintenanceid'] == 0) {
 						$can_be_unsuppressed = true;
