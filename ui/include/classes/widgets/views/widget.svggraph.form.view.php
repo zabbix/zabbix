481b0eec46b78703ee38ba4a78b55c9d6ea81466
Roberts Lataria <roberts.lataria@zabbix.com>
2022-06-09 17:30:47 +0300

    ..F....... [ZBXNEXT-6940] fixed code style

--- ui/include/classes/widgets/views/widget.svggraph.form.view.php
+++ ui/include/classes/widgets/views/widget.svggraph.form.view.php
@@ -42,7 +42,6 @@ $jq_templates = [];
 $graph_preview = (new CDiv())
 	->addClass(ZBX_STYLE_SVG_GRAPH_PREVIEW)
 	->addItem((new CDiv())->setId('svg-graph-preview'));
-	// ->addStyle('height: 10px !important; overflow: hidden;'); // FIXME: debug
 
 $form_tabs = (new CTabView())
 	->addTab('data_set',  _('Data set'), getDatasetTab($fields, $jq_templates, $form->getName()),
