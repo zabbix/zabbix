ccee2598bd450eecbc0cfa4f13f90734cedca48b
Andris Zeila <andris.zeila@zabbix.com>
2022-06-11 08:56:41 +0300

    ........S. [ZBX-21145] reworked host prototype template linking to use common tag merging logic

--- include/db.h
+++ include/db.h
@@ -894,12 +894,14 @@ typedef struct
 }
 zbx_db_tag_t;
 
+ZBX_PTR_VECTOR_DECL(db_tag_ptr, zbx_db_tag_t *)
+
 zbx_db_tag_t	*zbx_db_tag_create(const char *tag_tag, const char *tag_value);
 void		zbx_db_tag_free(zbx_db_tag_t *tag);
 int		zbx_db_tag_compare_func(const void *d1, const void *d2);
 int		zbx_db_tag_compare_func_template(const void *d1, const void *d2);
 
-ZBX_PTR_VECTOR_DECL(db_tag_ptr, zbx_db_tag_t *)
+void	zbx_db_tag_merge(zbx_vector_db_tag_ptr_t *dst, zbx_vector_db_tag_ptr_t *src);
 
 typedef enum
 {
