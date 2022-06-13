c021240e91a82f05e958c721ff3c7df7bfc75da6
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2022-06-13 22:10:21 +0100

    .......... [DEV-2019] increased click attempts

--- ui/tests/include/web/elements/CDashboardElement.php
+++ ui/tests/include/web/elements/CDashboardElement.php
@@ -105,11 +105,22 @@ class CDashboardElement extends CElement {
 		$controls = $this->getControls();
 
 		if (!$controls->query('xpath:.//nav[@class="dashboard-edit"]')->one()->isDisplayed()) {
-			$controls->query('id:dashboard-edit')->one()->click();
-			$controls->query('xpath:.//nav[@class="dashboard-edit"]')->waitUntilVisible();
-		}
+			// Click can sometimes fail so we have to retry this operation.
+			for ($i = 0; $i < 2; $i++) {
+				$controls->query('id:dashboard-edit')->one()->click();
 
-		return $this;
+				try {
+					$controls->query('xpath:.//nav[@class="dashboard-edit"]')->waitUntilVisible();
+
+					return $this;
+				}
+				catch (\Exception $e) {
+					if ($i === 1) {
+						throw $e;
+					}
+				}
+			}
+		}
 	}
 
 	/**
