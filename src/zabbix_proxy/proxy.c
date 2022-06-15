e439ffba7c84534254f1e4cc8f0c84dbdf0ad290
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2022-06-15 03:29:08 +0300

    .......P.. [ZBX-20638] fixed proxy startup with less than minimal db version
    (cherry picked from commit d3c8176f2be1fd78c752767fc9cc1ccb3bbe7591)

--- src/zabbix_proxy/proxy.c
+++ src/zabbix_proxy/proxy.c
@@ -1114,7 +1114,15 @@ static void	zbx_check_db(void)
 
 	DBextract_version_info(&db_version_info);
 
-	if (DB_VERSION_NOT_SUPPORTED_ERROR == db_version_info.flag ||
+	if (db_version_info.current_version < db_version_info.min_version)
+	{
+		zabbix_log(LOG_LEVEL_ERR, "Error! Current %s database server version is too old (%s)",
+				db_version_info.database, db_version_info.friendly_current_version);
+		zabbix_log(LOG_LEVEL_ERR, "Must be a least %s", db_version_info.friendly_min_version);
+
+		exit(EXIT_FAILURE);
+	}
+	else if (DB_VERSION_NOT_SUPPORTED_ERROR == db_version_info.flag ||
 			DB_VERSION_HIGHER_THAN_MAXIMUM == db_version_info.flag)
 	{
 		if (0 == CONFIG_ALLOW_UNSUPPORTED_DB_VERSIONS)
