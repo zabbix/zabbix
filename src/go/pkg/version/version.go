5c44273651f42feeee38f2efd245de42dcb1b88a
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2022-06-20 11:07:15 +0300

    .......... [ZBXNEXT-826] updated Changelog

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -29,7 +29,7 @@ import (
 
 const (
 	APPLICATION_NAME        = "Zabbix Agent"
-	ZABBIX_REVDATE          = "30 May 2022"
+	ZABBIX_REVDATE          = "20 June 2022"
 	ZABBIX_VERSION_MAJOR    = 5
 	ZABBIX_VERSION_MINOR    = 0
 	ZABBIX_VERSION_PATCH    = 25
--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,7 +22,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "30 May 2022";
+	static final String REVISION_DATE = "20 June 2022";
 	static final String REVISION = "{ZABBIX_REVISION}";
 	static final String VERSION = "5.0.25rc1";
 
