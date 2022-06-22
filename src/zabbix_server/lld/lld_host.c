b8cbb820ecd8b1a73e915eec7389ef696e4adb80
Andris Zeila <andris.zeila@zabbix.com>
2022-06-22 10:02:07 +0300

    ........S. [ZBX-21145] removed redundant loop

--- src/zabbix_server/lld/lld_host.c
+++ src/zabbix_server/lld/lld_host.c
@@ -385,9 +385,6 @@ static void	lld_hosts_get_tags(zbx_vector_ptr_t *hosts)
 		zbx_vector_db_tag_ptr_append(&host->tags, tag);
 	}
 out:
-	for (i = 0; i < hosts->values_num; i++)
-		host = (zbx_lld_host_t *)hosts->values[i];
-
 	DBfree_result(result);
 	zbx_free(sql);
 	zbx_vector_uint64_destroy(&hostids);
