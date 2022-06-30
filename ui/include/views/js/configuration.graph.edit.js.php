0f57ad95e43226df176a6f6b6e932836ae1378c2
Roberts Lataria <roberts.lataria@zabbix.com>
2022-06-30 16:25:42 +0300

    ..F....... [ZBX-7706] fixed graph preview min and max height

--- ui/include/views/js/configuration.graph.edit.js.php
+++ ui/include/views/js/configuration.graph.edit.js.php
@@ -567,6 +567,20 @@
 					src.setArgument('yaxismin', $('#yaxismin').val());
 					src.setArgument('yaxismax', $('#yaxismax').val());
 
+					if ($('#ymin_type').val() == <?= GRAPH_YAXIS_TYPE_ITEM_VALUE ?>) {
+						const ymin_item_data = $('#ymin_itemid').multiSelect('getData');
+						if (ymin_item_data.length) {
+							src.setArgument('ymin_itemid', ymin_item_data[0]['id']);
+						}
+					}
+
+					if ($('#ymax_type').val() == <?= GRAPH_YAXIS_TYPE_ITEM_VALUE ?>) {
+						const ymax_item_data = $('#ymax_itemid').multiSelect('getData');
+						if (ymax_item_data.length) {
+							src.setArgument('ymax_itemid', ymax_item_data[0]['id']);
+						}
+					}
+
 					src.setArgument('showworkperiod', $('#show_work_period').is(':checked') ? 1 : 0);
 					src.setArgument('showtriggers', $('#show_triggers').is(':checked') ? 1 : 0);
 				}
