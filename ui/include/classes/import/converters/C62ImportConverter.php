3574974cda1bee3cce447516abee592469687218
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2022-07-06 14:01:43 +0000

    .......... [ZBXNEXT-826] updated export file version [6.4]

--- ui/include/classes/import/converters/C62ImportConverter.php
+++ ui/include/classes/import/converters/C62ImportConverter.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types = 0);
 /*
 ** Zabbix
 ** Copyright (C) 2001-2022 Zabbix SIA
@@ -20,25 +20,20 @@
 
 
 /**
- * Factory for creating import conversions.
+ * Converter for converting import data from 6.2 to 6.4.
  */
-class CImportConverterFactory extends CRegistryFactory {
+class C62ImportConverter extends CConverter {
 
-	public function __construct() {
-		parent::__construct([
-			'1.0' => 'C10ImportConverter',
-			'2.0' => 'C20ImportConverter',
-			'3.0' => 'C30ImportConverter',
-			'3.2' => 'C32ImportConverter',
-			'3.4' => 'C34ImportConverter',
-			'4.0' => 'C40ImportConverter',
-			'4.2' => 'C42ImportConverter',
-			'4.4' => 'C44ImportConverter',
-			'5.0' => 'C50ImportConverter',
-			'5.2' => 'C52ImportConverter',
-			'5.4' => 'C54ImportConverter',
-			'6.0' => 'C60ImportConverter',
-			'6.2' => 'C62ImportConverter'
-		]);
+	/**
+	 * Convert import data from 6.2 to 6.4 version.
+	 *
+	 * @param array $data
+	 *
+	 * @return array
+	 */
+	public function convert(array $data): array {
+		$data['zabbix_export']['version'] = '6.4';
+
+		return $data;
 	}
 }
--- ui/include/defines.inc.php
+++ ui/include/defines.inc.php
@@ -20,7 +20,7 @@
 
 define('ZABBIX_VERSION',		'6.4.0alpha1');
 define('ZABBIX_API_VERSION',	'6.4.0');
-define('ZABBIX_EXPORT_VERSION',	'6.2');
+define('ZABBIX_EXPORT_VERSION',	'6.4');
 
 define('ZABBIX_DB_VERSION',		6020000);
 
