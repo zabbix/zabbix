548a188de503e201f59c868348565d0763bedff7
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2022-07-10 18:13:33 +0000

    ....I..... [ZBXNEXT-826] fixed dbupgrade files content
    (cherry picked from commit cfb9deaf612f22303e5a23bf36ae37eda1bb1ecd)

--- src/libs/zbxdbupgrade/dbupgrade_6020.c
+++ src/libs/zbxdbupgrade/dbupgrade_6020.c
@@ -22,22 +22,22 @@
 extern unsigned char	program_type;
 
 /*
- * 6.0 maintenance database patches
+ * 6.2 maintenance database patches
  */
 
 #ifndef HAVE_SQLITE3
 
-static int	DBpatch_6000000(void)
+static int	DBpatch_6020000(void)
 {
 	return SUCCEED;
 }
 
 #endif
 
-DBPATCH_START(6000)
+DBPATCH_START(6020)
 
 /* version, duplicates flag, mandatory flag */
 
-DBPATCH_ADD(6000000, 0, 1)
+DBPATCH_ADD(6020000, 0, 1)
 
 DBPATCH_END()
