8e928d85b3b982e27652256e800a30b8849dfd84
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2022-08-02 18:48:39 +0300

    ........S. [ZBXNEXT-3290] added revision retrieval from configuration cache and from user macro cache

--- src/libs/zbxdbcache/dbconfig.c
+++ src/libs/zbxdbcache/dbconfig.c
@@ -7122,6 +7122,24 @@ int	DCcheck_proxy_permissions(const char *host, const zbx_socket_t *sock, zbx_ui
 	return SUCCEED;
 }
 
+static int	dc_get_max_host_revision(const zbx_um_cache_t *cache, zbx_uint64_t hostid, zbx_uint32_t *revision)
+{
+	const zbx_um_host_t	* const *phost;
+	int			i;
+	zbx_uint64_t		*phostid = &hostid;
+
+	if (NULL == (phost = (const zbx_um_host_t * const *)zbx_hashset_search(&cache->hosts, &phostid)))
+		return FAIL;
+
+	if ((*phost)->revision > *revision)
+		*revision = (*phost)->revision;
+
+	for (i = 0; i < (*phost)->templateids.values_num; i++)
+		dc_get_max_host_revision(cache, (*phost)->templateids.values[i], revision);
+
+	return SUCCEED;
+}
+
 int	DCcheck_host_permissions(const char *host, const zbx_socket_t *sock, zbx_uint64_t *hostid,
 		zbx_uint32_t *revision, char **error)
 {
@@ -7133,7 +7151,7 @@ int	DCcheck_host_permissions(const char *host, const zbx_socket_t *sock, zbx_uin
 		return FAIL;
 
 	RDLOCK_CACHE;
-// check revision and attempt to retrieve host with items here
+
 	if (NULL == (dc_host = DCfind_host(host)))
 	{
 		UNLOCK_CACHE;
@@ -7165,7 +7183,9 @@ int	DCcheck_host_permissions(const char *host, const zbx_socket_t *sock, zbx_uin
 	}
 
 	*hostid = dc_host->hostid;
-	*revision = dc_host->revision;
+	*revision = MAX(dc_host->revision, config->expression_revision);
+
+	dc_get_max_host_revision(config->um_cache, *hostid, revision);
 
 	UNLOCK_CACHE;
 
