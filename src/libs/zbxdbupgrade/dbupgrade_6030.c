cde1256d19a10977d3c0c867e6c15facb680d40a
Andris Zeila <andris.zeila@zabbix.com>
2022-08-04 12:14:39 +0300

    ....I..... [ZBXNEXT-7830] updated database patch to insert additional api_token rule when converting ui.administration.general

--- src/libs/zbxdbupgrade/dbupgrade_6030.c
+++ src/libs/zbxdbupgrade/dbupgrade_6030.c
@@ -76,7 +76,10 @@ static int	DBpatch_6030000(void)
 
 			zbx_db_insert_add_values(&db_insert, __UINT64_C(0), roleid, type,
 					"ui.administration.macros", value_int);
-		}
+
+			zbx_db_insert_add_values(&db_insert, __UINT64_C(0), roleid, type,
+					"ui.administration.api_tokens", value_int);
+}
 		else
 		{
 			zbx_db_insert_add_values(&db_insert, __UINT64_C(0), roleid, type,
