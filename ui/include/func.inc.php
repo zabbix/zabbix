2f6bee084bbea917deda4f16d9762b0c687ecaef
Reinis Detlavs <reinis.detlavs@zabbix.com>
2022-08-12 15:02:35 +0300

    ..F....... [ZBX-21342] fixed sessionStorage being updated when creating/deleting/updating actions

--- ui/include/func.inc.php
+++ ui/include/func.inc.php
@@ -2163,7 +2163,8 @@ function hasErrorMessages() {
  * @param array  $keepids   checked rows ids
  */
 function uncheckTableRows($parentid = null, $keepids = []) {
-	$key = implode('_', array_filter(['cb', basename($_SERVER['SCRIPT_NAME'], '.php'), $parentid]));
+	$basename = basename($_SERVER['SCRIPT_NAME'], '.php');
+	$key = implode('_', ['cb', $basename, $parentid]);
 
 	if ($keepids) {
 		// If $keepids will not have same key as value, it will create mess, when new checkbox will be checked.
