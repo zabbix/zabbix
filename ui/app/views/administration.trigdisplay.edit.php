0279346638c0314293cf5393c40f7878d535763c
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2022-08-15 08:00:33 +0000

    ..F....... [ZBX-21449] fixed accessibility attributes

--- ui/app/views/administration.trigdisplay.edit.php
+++ ui/app/views/administration.trigdisplay.edit.php
@@ -155,7 +155,7 @@ $form_list = (new CFormList())
 	->addInfo(_('Custom severity names affect all locales and require manual translation!'));
 
 $form = (new CForm())
-	->setAttribute('aria-labeledby', ZBX_STYLE_PAGE_TITLE)
+	->setAttribute('aria-labelledby', ZBX_STYLE_PAGE_TITLE)
 	->setAction((new CUrl('zabbix.php'))
 		->setArgument('action', 'trigdisplay.update')
 		->getUrl()
