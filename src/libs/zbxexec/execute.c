12f5eae744ae9bcfaad33c287c3730c43a66dd9d
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2022-08-22 17:33:52 +0300

    .......PS. [ZBX-21454] added sigemptyset to block signal during script execution

--- src/libs/zbxexec/execute.c
+++ src/libs/zbxexec/execute.c
@@ -464,6 +464,8 @@ close:
 	zbx_free(wdir);
 
 #else	/* not _WINDOWS */
+	/* block signals to prevent interruption of statements when runtime control command is issued */
+	sigemptyset(&mask);
 	sigaddset(&mask, SIGUSR1);
 	sigaddset(&mask, SIGUSR2);
 
