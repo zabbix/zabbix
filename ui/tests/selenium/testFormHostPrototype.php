5c823325b7644350c75f7a4020afbad2bb5e8820
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2022-08-25 11:20:56 +0000

    ..F....... [ZBX-21495] updated Selenuim tests

--- ui/tests/selenium/testFormHostPrototype.php
+++ ui/tests/selenium/testFormHostPrototype.php
@@ -70,7 +70,10 @@ class testFormHostPrototype extends CLegacyWebTest {
 
 		// Check layout at IPMI tab.
 		$this->zbxTestTabSwitch('IPMI');
-		foreach (['ipmi_authtype', 'ipmi_privilege', 'ipmi_username', 'ipmi_password'] as $id) {
+		foreach (['ipmi_authtype', 'ipmi_privilege'] as $id) {
+			$this->zbxTestAssertElementPresentXpath('//z-select[@id="'.$id.'"][@readonly]');
+		}
+		foreach (['ipmi_username', 'ipmi_password'] as $id) {
 			$this->zbxTestAssertElementPresentXpath('//input[@id="'.$id.'"][@readonly]');
 		}
 
