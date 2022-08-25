9e6417b9d2aaff7b0972d84fea8d1c3d9bd57b6b
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2022-08-25 10:33:27 +0300

    .......... [DEV-2190] removed not needed backup

--- ui/tests/api_json/testAuditlogAutoregistration.php
+++ ui/tests/api_json/testAuditlogAutoregistration.php
@@ -22,7 +22,7 @@
 require_once dirname(__FILE__).'/testAuditlogCommon.php';
 
 /**
- * @backup config_autoreg_tls, config, ids
+ * @backup config_autoreg_tls, config
  */
 class testAuditlogAutoregistration extends testAuditlogCommon {
 
