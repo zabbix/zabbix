854e8a9661e84d734146113a36ef5a8dbff7192e
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2022-08-26 13:53:26 +0300

    .......... [DEV-2190] updated audit scheduled report API autotest

--- ui/tests/api_json/testAuditlogScheduledReport.php
+++ ui/tests/api_json/testAuditlogScheduledReport.php
@@ -19,134 +19,136 @@
 **/
 
 
-require_once dirname(__FILE__).'/../include/CAPITest.php';
+require_once dirname(__FILE__).'/testAuditlogCommon.php';
 
 /**
  * @backup report, ids
  */
-class testAuditlogScheduledReport extends CAPITest {
+class testAuditlogScheduledReport extends testAuditlogCommon {
 
 	protected static $resourceid;
 
 	public function testAuditlogScheduledReport_Create() {
-		$created = "{\"report.userid\":[\"add\",\"1\"],\"report.name\":[\"add\",\"Report for audit\"],".
-				"\"report.dashboardid\":[\"add\",\"1\"],\"report.period\":[\"add\",\"1\"],\"report.cycle\":[".
-				"\"add\",\"1\"],\"report.start_time\":[\"add\",\"43200\"],\"report.weekdays\":[\"add\",\"31\"],".
-				"\"report.active_since\":[\"add\",\"1617235200\"],\"report.active_till\":[\"add\",\"1630454399\"],".
-				"\"report.subject\":[\"add\",\"Weekly report\"],\"report.message\":[\"add\",\"Report accompanying text".
-				"\"],\"report.status\":[\"add\",\"1\"],\"report.description\":[\"add\",\"Report description\"],".
-				"\"report.users[1]\":[\"add\"],\"report.users[1].userid\":[\"add\",\"1\"],\"report.users[1].access_userid".
-				"\":[\"add\",\"1\"],\"report.users[1].reportuserid\":[\"add\",\"1\"],\"report.users[2]\":[\"add\"],".
-				"\"report.users[2].userid\":[\"add\",\"2\"],\"report.users[2].access_userid\":[\"add\",\"0\"],".
-				"\"report.users[2].exclude\":[\"add\",\"1\"],\"report.users[2].reportuserid\":[\"add\",\"2\"],".
-				"\"report.user_groups[1]\":[\"add\"],\"report.user_groups[1].usrgrpid\":[\"add\",\"7\"],".
-				"\"report.user_groups[1].access_userid\":[\"add\",\"0\"],\"report.user_groups[1].reportusrgrpid\":[".
-				"\"add\",\"1\"],\"report.reportid\":[\"add\",\"1\"]}";
-
 		$create = $this->call('report.create', [
 			[
-				'userid' => '1',
+				'userid' => 1,
 				'name' => 'Report for audit',
-				'dashboardid' => '1',
-				'period' => '1',
-				'cycle' => '1',
-				'start_time' => '43200',
-				'weekdays' => '31',
+				'dashboardid' => 1,
+				'period' => 1,
+				'cycle' => 1,
+				'start_time' => 43200,
+				'weekdays' => 31,
 				'active_since' => '2021-04-01',
 				'active_till' => '2021-08-31',
 				'subject' => 'Weekly report',
 				'message' => 'Report accompanying text',
-				'status' => '1',
+				'status' => 1,
 				'description' => 'Report description',
 				'users' => [
 					[
-						'userid' => '1',
-						'access_userid' => '1',
-						'exclude' => '0'
-					],
-					[
-						'userid' => '2',
-						'access_userid' => '0',
-						'exclude' => '1'
+						'userid' => 1,
+						'access_userid' => 1,
+						'exclude' => 0
 					]
 				],
 				'user_groups' => [
 					[
-						'usrgrpid' => '7',
-						'access_userid' => '0'
+						'usrgrpid' => 7,
+						'access_userid' => 0
 					]
 				]
 			]
 		]);
-
 		self::$resourceid = $create['result']['reportids'][0];
-		$this->sendGetRequest('details', 0, $created);
+
+		$created = "{\"report.userid\":[\"add\",\"1\"],".
+				"\"report.name\":[\"add\",\"Report for audit\"],".
+				"\"report.dashboardid\":[\"add\",\"1\"],".
+				"\"report.period\":[\"add\",\"1\"],".
+				"\"report.cycle\":[\"add\",\"1\"],".
+				"\"report.start_time\":[\"add\",\"43200\"],".
+				"\"report.weekdays\":[\"add\",\"31\"],".
+				"\"report.active_since\":[\"add\",\"1617235200\"],".
+				"\"report.active_till\":[\"add\",\"1630454399\"],".
+				"\"report.subject\":[\"add\",\"Weekly report\"],".
+				"\"report.message\":[\"add\",\"Report accompanying text\"],".
+				"\"report.status\":[\"add\",\"1\"],".
+				"\"report.description\":[\"add\",\"Report description\"],".
+				"\"report.users[1]\":[\"add\"],".
+				"\"report.users[1].userid\":[\"add\",\"1\"],".
+				"\"report.users[1].access_userid\":[\"add\",\"1\"],".
+				"\"report.users[1].reportuserid\":[\"add\",\"1\"],".
+				"\"report.user_groups[1]\":[\"add\"],".
+				"\"report.user_groups[1].usrgrpid\":[\"add\",\"7\"],".
+				"\"report.user_groups[1].access_userid\":[\"add\",\"0\"],".
+				"\"report.user_groups[1].reportusrgrpid\":[\"add\",\"1\"],".
+				"\"report.reportid\":[\"add\",\"". self::$resourceid."\"]}";
+
+		$this->sendGetRequest('details', 0, $created, self::$resourceid);
 	}
 
 	public function testAuditlogScheduledReport_Update() {
-		$updated = "{\"report.users[1]\":[\"delete\"],\"report.user_groups[1]\":[\"delete\"],\"report.user_groups[2]".
-				"\":[\"add\"],\"report.name\":[\"update\",\"Updated report for audit\",\"Report for audit\"],".
-				"\"report.dashboardid\":[\"update\",\"2\",\"1\"],\"report.period\":[\"update\",\"3\",\"1\"],".
-				"\"report.cycle\":[\"update\",\"2\",\"1\"],\"report.start_time\":[\"update\",\"44200\",\"43200\"],".
-				"\"report.weekdays\":[\"update\",\"0\",\"31\"],\"report.active_since\":[\"update\",\"1640995200\",".
-				"\"1617235200\"],\"report.active_till\":[\"update\",\"1648771199\",\"1630454399\"],\"report.subject".
-				"\":[\"update\",\"Updated subject\",\"Weekly report\"],\"report.message\":[\"update\",\"Updated message".
-				"\",\"Report accompanying text\"],\"report.status\":[\"update\",\"0\",\"1\"],\"report.description".
-				"\":[\"update\",\"Updated description\",\"Report description\"],\"report.user_groups[2].usrgrpid".
-				"\":[\"add\",\"8\"],\"report.user_groups[2].access_userid\":[\"add\",\"1\"],".
-				"\"report.user_groups[2].reportusrgrpid\":[\"add\",\"2\"]}";
-
 		$this->call('report.update', [
 			[
 				'reportid' => self::$resourceid,
-				'userid' => '1',
+				'userid' => 1,
 				'name' => 'Updated report for audit',
-				'dashboardid' => '2',
-				'period' => '3',
-				'cycle' => '2',
-				'start_time' => '44200',
-				'weekdays' => '0',
+				'dashboardid' => 2,
+				'period' => 3,
+				'cycle' => 2,
+				'start_time' => 44200,
+				'weekdays' => 0,
 				'active_since' => '2022-01-01',
 				'active_till' => '2022-03-31',
 				'subject' => 'Updated subject',
 				'message' => 'Updated message',
-				'status' => '0',
+				'status' => 0,
 				'description' => 'Updated description',
 				'users' => [
 					[
-						'userid' => '2',
-						'access_userid' => '0',
-						'exclude' => '1'
+						'userid' => 2,
+						'access_userid' => 0,
+						'exclude' => 1
 					]
 				],
 				'user_groups' => [
 					[
-						'usrgrpid' => '8',
-						'access_userid' => '1'
+						'usrgrpid' => 8,
+						'access_userid' => 1
 					]
 				]
 			]
 		]);
 
-		$this->sendGetRequest('details', 1, $updated);
+		$updated = "{\"report.users[1]\":[\"delete\"],".
+				"\"report.user_groups[1]\":[\"delete\"],".
+				"\"report.users[2]\":[\"add\"],".
+				"\"report.user_groups[2]\":[\"add\"],".
+				"\"report.name\":[\"update\",\"Updated report for audit\",\"Report for audit\"],".
+				"\"report.dashboardid\":[\"update\",\"2\",\"1\"],".
+				"\"report.period\":[\"update\",\"3\",\"1\"],".
+				"\"report.cycle\":[\"update\",\"2\",\"1\"],".
+				"\"report.start_time\":[\"update\",\"44200\",\"43200\"],".
+				"\"report.weekdays\":[\"update\",\"0\",\"31\"],".
+				"\"report.active_since\":[\"update\",\"1640995200\",\"1617235200\"],".
+				"\"report.active_till\":[\"update\",\"1648771199\",\"1630454399\"],".
+				"\"report.subject\":[\"update\",\"Updated subject\",\"Weekly report\"],".
+				"\"report.message\":[\"update\",\"Updated message\",\"Report accompanying text\"],".
+				"\"report.status\":[\"update\",\"0\",\"1\"],".
+				"\"report.description\":[\"update\",\"Updated description\",\"Report description\"],".
+				"\"report.users[2].userid\":[\"add\",\"2\"],".
+				"\"report.users[2].access_userid\":[\"add\",\"0\"],".
+				"\"report.users[2].exclude\":[\"add\",\"1\"],".
+				"\"report.users[2].reportuserid\":[\"add\",\"2\"],".
+				"\"report.user_groups[2].usrgrpid\":[\"add\",\"8\"],".
+				"\"report.user_groups[2].access_userid\":[\"add\",\"1\"],".
+				"\"report.user_groups[2].reportusrgrpid\":[\"add\",\"2\"]}";
+
+		$this->sendGetRequest('details', 1, $updated, self::$resourceid);
 	}
 
 	public function testAuditlogScheduledReport_Delete() {
 		$this->call('report.delete', [self::$resourceid]);
-		$this->sendGetRequest('resourcename', 2, 'Updated report for audit');
-	}
-
-	private function sendGetRequest($output, $action, $result) {
-		$get = $this->call('auditlog.get', [
-			'output' => [$output],
-			'sortfield' => 'clock',
-			'sortorder' => 'DESC',
-			'filter' => [
-				'resourceid' => self::$resourceid,
-				'action' => $action
-			]
-		]);
-
-		$this->assertEquals($result, $get['result'][0][$output]);
+		$this->sendGetRequest('resourcename', 2, 'Updated report for audit', self::$resourceid);
 	}
 }
