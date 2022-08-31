a9d0a995cd212e07bfd82263ce7c79a52a5de29b
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2022-08-31 15:31:00 +0100

    .......... [DEV-2237] added empty lines

--- ui/tests/selenium/SeleniumTests.php
+++ ui/tests/selenium/SeleniumTests.php
@@ -18,6 +18,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
+
 require_once dirname(__FILE__).'/testDocumentationLinks.php';
 require_once dirname(__FILE__).'/testGeneric.php';
 require_once dirname(__FILE__).'/testExecuteNow.php';
