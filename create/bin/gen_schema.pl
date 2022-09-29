79c416387964adc4a398e5091e55b379b0469b7a
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2022-09-29 16:15:54 +0300

    ........S. [ZBX-17419] added more various fixes

--- create/bin/gen_schema.pl
+++ create/bin/gen_schema.pl
@@ -873,32 +873,34 @@ sub process_update_trigger_function($)
 {
 
 	my $line = shift;
-	my $out;
+	my $out = "";
 
 	my ($original_column_name, $indexed_column_name, $idname, $func_name) = split(/\|/, $line, 4);
 
 
 	if ($output{"database"} eq "mysql")
 	{
-		$out = "create trigger ${table_name}_${indexed_column_name}_insert${eol}\n";
+		$out .= "create trigger ${table_name}_${indexed_column_name}_insert${eol}\n";
 		$out .= "after insert on ${table_name} for each row${eol}\n";
 		$out .= "begin${eol}\n";
 		$out .= "if new.${original_column_name} <> old.${original_column_name}${eol}\n";
 		$out .= "then${eol}\n";
-		$out .= "update ${table_name} set ${indexed_column_name}=${func_name}($original_column_name)${eol}\n";
-		$out .= "end${eol}\n"
+		$out .= "update ${table_name} set ${indexed_column_name}=${func_name}($original_column_name);${eol}\n";
+		$out .= "end if;${eol}\n";
+		$out .= "end;${eol}\n";
 
 		$out = "create trigger ${table_name}_${indexed_column_name}_update${eol}\n";
 		$out .= "after update on ${table_name} for each row${eol}\n";
 		$out .= "begin${eol}\n";
 		$out .= "if new.${original_column_name} <> old.${original_column_name}${eol}\n";
 		$out .= "then${eol}\n";
-		$out .= "update ${table_name} set ${indexed_column_name}=${func_name}($original_column_name)${eol}\n";
-		$out .= "end${eol}\n";
+		$out .= "update ${table_name} set ${indexed_column_name}=${func_name}($original_column_name);${eol}\n";
+		$out .= "end if;${eol}\n";
+		$out .= "end;${eol}\n";
 	}
 	elsif ($output{"database"} eq "postgresql")
 	{
-		$out = "";
+		$out .= "";
 		$out .= "create or replace function ${table_name}_${indexed_column_name}_${func_name}()${eol}\n";
 		$out .= "returns trigger language plpgsql as \$func\$${eol}\n";
 		$out .= "begin${eol}\n";
@@ -912,7 +914,7 @@ sub process_update_trigger_function($)
 		$out .= "for each row execute procedure ${table_name}_${indexed_column_name}_${func_name}();${eol}\n";
 		$out .= "create trigger ${table_name}_${indexed_column_name}_update after update {$eol}\n";
 		$out .= "of ${original_column_name} on ${table_name} ${eol}\n";
-		$out .= "for each row execute procedure ${table_name}_${indexed_column_name}_${function}();${eol}\n";
+		$out .= "for each row execute procedure ${table_name}_${indexed_column_name}_${func_name}();${eol}\n";
 	}
 	$triggers .= $out;
 }
