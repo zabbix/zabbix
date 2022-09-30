4a711b7cdaa83c7a818a6eca176e8a9adef4d051
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2022-09-30 11:24:15 +0300

    ........S. [ZBX-17419] fixed upgrade patch

--- src/libs/zbxdbupgrade/dbupgrade_6030.c
+++ src/libs/zbxdbupgrade/dbupgrade_6030.c
@@ -464,7 +464,7 @@ static int	DBpatch_6030059(void)
 
 static int	DBpatch_6030060(void)
 {
-	const ZBX_FIELD field = {"name_up", "", NULL, NULL, 0, ZBX_TYPE_CHAR, ZBX_NOTNULL, 0};
+	const ZBX_FIELD field = {"name_up", "", NULL, NULL, 128, ZBX_TYPE_CHAR, ZBX_NOTNULL, 0};
 
 	return DBadd_field("hosts", &field);
 }
