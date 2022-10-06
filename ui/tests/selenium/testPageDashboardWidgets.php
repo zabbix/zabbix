62df235b09f5f0573e484125ad65e81c66b76960
Natalja Romancaka <natalja.romancaka@zabbix.com>
2022-10-06 08:18:31 +0300

    .......... [DEV-2184] removed profiles table backup

--- ui/tests/selenium/testPageDashboardWidgets.php
+++ ui/tests/selenium/testPageDashboardWidgets.php
@@ -22,7 +22,7 @@
 require_once dirname(__FILE__) . '/../include/CWebTest.php';
 
 /**
- * @backup dashboard, profiles
+ * @backup dashboard
  * @dataSource LoginUsers
  */
 class testPageDashboardWidgets extends CWebTest {
