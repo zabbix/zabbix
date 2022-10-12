0c4e5bcbdd357af48272192c6c4937c9b44b37db
Eliza Sekace <eliza.sekace@zabbix.com>
2022-10-12 16:40:57 +0300

    ..F....... [ZBXNEXT-7835] fixed state of the filter is not saved

--- ui/app/controllers/CControllerProfileUpdate.php
+++ ui/app/controllers/CControllerProfileUpdate.php
@@ -33,7 +33,7 @@ class CControllerProfileUpdate extends CController {
 
 		if ($ret) {
 			switch ($this->getInput('idx')) {
-				case 'web.actionconf.filter.active':
+				case 'web.action.list.filter.active':
 				case 'web.auditacts.filter.active':
 				case 'web.auditlog.filter.active':
 				case 'web.avail_report.filter.active':
