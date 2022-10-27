ce40433ea1957fc646cd97190b212d502105a2be
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2022-10-27 11:52:35 +0300

    .......... [ZBXNEXT-826] updated release date and version [5.0.29rc2]

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -29,11 +29,11 @@ import (
 
 const (
 	APPLICATION_NAME        = "Zabbix Agent"
-	ZABBIX_REVDATE          = "25 October 2022"
+	ZABBIX_REVDATE          = "27 October 2022"
 	ZABBIX_VERSION_MAJOR    = 5
 	ZABBIX_VERSION_MINOR    = 0
 	ZABBIX_VERSION_PATCH    = 29
-	ZABBIX_VERSION_RC       = "rc1"
+	ZABBIX_VERSION_RC       = "rc2"
 	ZABBIX_VERSION_RC_NUM   = "{ZABBIX_RC_NUM}"
 	ZABBIX_VERSION_REVISION = "{ZABBIX_REVISION}"
 	copyrightMessage        = "Copyright (C) 2022 Zabbix SIA\n" +
