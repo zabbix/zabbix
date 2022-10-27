ea4a8926619294a5fa878b822b1d547fae0b644e
Elina Pulke <elina.pulke@zabbix.com>
2022-10-27 11:50:05 +0300

    A......... [ZBXNEXT-276] minor coding style fix for SCIM API filter match value

--- ui/include/classes/scim/API.php
+++ ui/include/classes/scim/API.php
@@ -75,7 +75,7 @@ class API {
 			preg_match('/^userName eq "(?<value>(?:[^"]|\\\\")*)"$/', $_GET['filter'], $filter_value);
 
 			if (array_key_exists('value', $filter_value)) {
-				$input['userName'] = $filter_value[1];
+				$input['userName'] = $filter_value['value'];
 			}
 			else {
 				throw new Exception(_('This filter is not supported'), 400);
