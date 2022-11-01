09932389a1f5a5abb6f54dcbc7b6f72a30b0a257
Michael Veksler <Mihails.Vekslers@zabbix.com>
2022-11-01 11:30:22 +0200

    .......PS. [ZBX-19466] fixed DB index uniqueness for global script names

--- src/libs/zbxdbupgrade/dbupgrade_6030.c
+++ src/libs/zbxdbupgrade/dbupgrade_6030.c
@@ -462,18 +462,28 @@ static int	DBpatch_6030059(void)
 	return DBset_default("media_type", &field);
 }
 
-static int DBpatch_6030060(void)
+static int	DBpatch_6030060(void)
 {
 	const ZBX_FIELD	field = {"url_name", "", NULL, NULL, 64, ZBX_TYPE_CHAR, ZBX_NOTNULL, 0};
 
 	return DBadd_field("triggers", &field);
 }
 
-static int DBpatch_6030061(void)
+static int	DBpatch_6030061(void)
 {
 	const ZBX_FIELD	field = {"url", "", NULL, NULL, 2048, ZBX_TYPE_CHAR, ZBX_NOTNULL, 0};
 
 	return DBmodify_field_type("triggers", &field, NULL);
+}
+
+static int	DBpatch_6030062(void)
+{
+	return DBdrop_index("scripts", "scripts_3");
+}
+
+static int	DBpatch_6030063(void)
+{
+	return DBcreate_index("scripts", "scripts_3", "name,menu_path", 1);
 }
 
 #endif
@@ -544,5 +554,7 @@ DBPATCH_ADD(6030058, 0, 1)
 DBPATCH_ADD(6030059, 0, 1)
 DBPATCH_ADD(6030060, 0, 1)
 DBPATCH_ADD(6030061, 0, 1)
+DBPATCH_ADD(6030062, 0, 1)
+DBPATCH_ADD(6030063, 0, 1)
 
 DBPATCH_END()
