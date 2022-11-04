d94743a44e1307fa66f06ab574c58e042566c09c
Andris Zeila <andris.zeila@zabbix.com>
2022-11-04 11:19:57 +0200

    .......PS. [ZBXNEXT-8040] added object based json parsing and jsonpath indexing for simple expressions
    Merge in ZBX/zabbix from feature/ZBXNEXT-8040-6.3 to master
    
    * commit '6835ef59b1bef2bbe783f90b8399bee0f1f93ab8':
      .......PS. [ZBXNEXT-8040] fixed warnings
      .......PS. [ZBXNEXT-8040] remove duplicate indexes/names when compiling jsonpath
      .......PS. [ZBXNEXT-8040] fixed jsonpath indexing when relative path is on comparison right side
      .......PS. [ZBXNEXT-8040] fixed invalid nested jsonpath in expression
      .......PS. [ZBXNEXT-8040] ported jsonpath 'first found' optimization from ZBXNEXT-8009
      .......PS. [ZBXNEXT-8040] fixed memory leak in json object index
      .D........ [ZBXNEXT-8040] added changelog entry
      .......... [ZBXNEXT-8040] added missing file
      .......PS. [ZBXNEXT-8040] addded json object caching in jsonpath preprocessing step
      .......... [ZBXNEXT-8040] updated jsonpath cmocka tests to use directly zbx_jsonobj_query and test it twice to check index reuse
      .......PS. [ZBXNEXT-8040] added support for more complex expression indexing
      .......PS. [ZBXNEXT-8040] added object based json parser and json path processing with indexing
    
    (cherry picked from commit e13326c4e6c1dfa1e5bd7906b72d636053140128)

--- src/libs/zbxjson/jsonpath.h
+++ src/libs/zbxjson/jsonpath.h
@@ -20,8 +20,9 @@
 #ifndef ZABBIX_JSONPATH_H
 #define ZABBIX_JSONPATH_H
 
-#include "zbxalgo.h"
 #include "zbxjson.h"
+
+typedef struct zbx_jsonpath_token zbx_jsonpath_token_t;
 
 typedef enum
 {
@@ -79,10 +80,19 @@ typedef struct
 }
 zbx_jsonpath_range_t;
 
+typedef enum
+{
+	ZBX_JSONPATH_EXPRESSION_INDEX_TRUE,
+	ZBX_JSONPATH_EXPRESSION_INDEX_FALSE,
+}
+zbx_json_path_expression_index_t;
+
 /* expression tokens in postfix notation */
 typedef struct
 {
 	zbx_vector_ptr_t	tokens;
+	zbx_jsonpath_token_t	*index_token;	/* relative path token that is used to index parent object */
+	zbx_jsonpath_token_t	*value_token;	/* the index value token */
 }
 zbx_jsonpath_expression_t;
 
@@ -151,12 +161,11 @@ typedef enum
 }
 zbx_jsonpath_token_type_t;
 
-typedef struct
+struct zbx_jsonpath_token
 {
 	unsigned char	type;
 	char		*text;
 	zbx_jsonpath_t	*path;
-}
-zbx_jsonpath_token_t;
+};
 
 #endif
