75c6f7f66fc829fc3e164c731dd561b504d53b9d
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2022-11-09 13:46:53 +0000

    .......... [ZBX-17419] fixed mysql upgrade patch

--- src/libs/zbxdbupgrade/dbupgrade.c
+++ src/libs/zbxdbupgrade/dbupgrade.c
@@ -1068,7 +1068,7 @@ int	zbx_dbupgrade_attach_trigger_with_function_on_insert(const char *table_name,
 			"before insert on %s for each row\n"
 			"begin\n"
 				":new.%s:=%s(:new.%s);\n"
-			"end;\n",
+			"end;",
 			table_name, indexed_column_name, table_name, indexed_column_name, func_name,
 			original_column_name);
 #elif HAVE_MYSQL
@@ -1077,7 +1077,7 @@ int	zbx_dbupgrade_attach_trigger_with_function_on_insert(const char *table_name,
 	zbx_snprintf_alloc(&sql, &sql_alloc, &sql_offset,
 			"create trigger %s_%s_insert\n"
 			"before insert on %s for each row\n"
-				"set new.%s=%s(new.%s);\n"
+				"set new.%s=%s(new.%s)"
 			table_name, indexed_column_name, table_name, indexed_column_name, func_name,
 			original_column_name);
 #elif defined(HAVE_POSTGRESQL)
