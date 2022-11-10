febee718128b145548494ca7e729f1fc29721f64
Elina Pulke <elina.pulke@zabbix.com>
2022-11-10 08:56:26 +0200

    ..F....... [ZBXNEXT-276] fixed SAML field trimming before submit

--- ui/app/views/js/administration.authentication.edit.js.php
+++ ui/app/views/js/administration.authentication.edit.js.php
@@ -186,8 +186,9 @@
 		}
 
 		_authFormSubmit() {
-			const fields_to_trim = ['#http_strip_domains', '#saml_idp_entityid', '#saml_sso_url', '#saml_slo_url',
-				'#saml_username_attribute', '#saml_sp_entityid', '#saml_nameid_format'
+			const fields_to_trim = ['#http_strip_domains', '#idp_entityid', '#sso_url', '#slo_url',
+				'#username_attribute', '#sp_entityid', '#nameid_format', '#saml_group_name', '#saml_user_username',
+				'#saml_user_lastname'
 			];
 			document.querySelectorAll(fields_to_trim.join(', ')).forEach((elem) => {
 				elem.value = elem.value.trim();
