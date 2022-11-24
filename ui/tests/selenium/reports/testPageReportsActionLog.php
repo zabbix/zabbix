ccd97ed0ea68d9bbc63f4173b7a1dbc6163bc20b
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2022-11-24 16:27:27 +0200

    .......... [DEV-2352] updated layout scenario for testPageReportsActionLog

--- ui/tests/selenium/reports/testPageReportsActionLog.php
+++ ui/tests/selenium/reports/testPageReportsActionLog.php
@@ -20,94 +20,112 @@
 
 
 require_once dirname(__FILE__).'/../../include/CLegacyWebTest.php';
+require_once dirname(__FILE__).'/../traits/TableTrait.php';
 
+/**
+ * @backup alerts
+ *
+ *
+ */
 class testPageReportsActionLog extends CLegacyWebTest {
 
-	public function testPageReportsActionLog_CheckLayout() {
-		// dates can be in relative format, example: now-1y/y, now-1w, now
-		$this->zbxTestLogin('zabbix.php?action=actionlog.list&from=now-2y&to=now');
-		$this->zbxTestCheckTitle('Action log');
-		$this->zbxTestAssertElementPresentId('config');
-		$this->zbxTestCheckHeader('Action log');
+	use TableTrait;
 
-		$this->zbxTestTextPresent('Recipient');
-		$this->zbxTestAssertElementPresentId('filter_userids__ms');
-		$this->zbxTestAssertElementPresentXpath("//button[@name='filter_set']");
-		$this->zbxTestAssertElementPresentXpath("//button[contains(text(),'Reset')]");
-		$this->zbxTestTextPresent(['Time', 'Action','Media type', 'Status', 'Recipient', 'Message', 'Status', 'Info']);
+	public static function prepareInsertActionsData() {
+		DBexecute("INSERT INTO alerts (alertid, actionid, eventid, userid, clock, mediatypeid, sendto, subject, ".
+				"message, status, retries, error, esc_step, alerttype, parameters) VALUES (8, 13, 1, 1, ".
+				"1329724870, 10, 'test.test@zabbix.com', 'subject here', 'message here', 1, 0, '', 1, 0, '');"
+		);
 
+		DBexecute("INSERT INTO alerts (alertid, actionid, eventid, userid, clock, mediatypeid, sendto, subject, ".
+				"message, status, retries, error, esc_step, alerttype, parameters) VALUES (9, 13, 1, 9, ".
+				"1329724880, 3, '77777777', 'subject here', 'message here', 1, 0, '', 1, 0, '');"
+		);
 	}
 
-	public static function allAuditActions() {
-		return CDBHelper::getDataProvider('SELECT * FROM alerts ORDER BY alertid LIMIT 7');
+	public function testPageReportsActionLog_CheckLayout() {
+		$this->page->login()->open('zabbix.php?action=actionlog.list&from=now-2y&to=now');
+
+		// If the filter is not visible - enable it.
+		if ($this->query('xpath://li[@aria-labelledby="ui-id-2" and @aria-selected="false"]')->exists()) {
+			$this->query('id:ui-id-2')->one()->click();
+		}
+
+		$form = $this->query('name:zbx_filter')->asForm()->one();
+		$table = $this->query('class:list-table')->asTable()->one();
+
+		// Check filter buttons.
+		foreach (['Apply', 'Reset'] as $button) {
+			$this->assertTrue($form->query('xpath:.//div[@class="filter-forms"]/button[text()="'.$button.'"]')
+				->one()->isClickable()
+			);
+		}
+
+		// Check that Export to CSV button is clickable.
+		$this->assertTrue($this->query('button:Export to CSV')->one()->isClickable());
+
+		// Check form labels.
+		$this->assertEquals(['Recipients', 'Actions', 'Media types', 'Status', 'Search string'], $form->getLabels()->asText());
+
+		// Check Search string field max length.
+		$this->assertEquals(255, $form->getField('Search string')->waitUntilVisible()->getAttribute('maxlength'));
+
+		// Check table headers.
+		$this->assertEquals(['Time', 'Action', 'Media type', 'Recipient', 'Message', 'Status', 'Info'], $table->getHeadersText());
+
+		// Check status available values.
+		$this->assertEquals(['In progress', 'Sent/Executed', 'Failed'], $this->query('id:filter_status')
+				->asCheckboxList()->one()->getLabels()->asText()
+		);
+	}
+
+	public static function getCheckFilterData() {
+		return [
+			// #0
+			[
+				[
+					'fields' => [
+						'Recipients' => 'test-timezone'
+					],
+					'result_amount' => 1
+				]
+			]
+		];
 	}
 
 	/**
-	* @dataProvider allAuditActions
-	*/
-	public function testPageReportsActionLog_CheckValues($auditactions) {
-		$time = $auditactions['clock'];
-		$today = date("Y-m-d H:i:s", $time);
+	 * @dataProvider getCheckFilterData
+	 */
+	public function testPageReportsActionLog_CheckFilter($data) {
+		$this->page->login()->open('zabbix.php?action=actionlog.list&from=2012-02-20+09:01:00&to=2012-02-20+11:01:00&'.
+				'filter_messages=&filter_set=1');
 
-		$this->zbxTestLogin('zabbix.php?action=actionlog.list&'.http_build_query([
-			'from' => date('Y-m-d H:i:s', $time - 3600),
-			'to' => date('Y-m-d H:i:s', $time + 3600)
-		]));
-		$this->zbxTestCheckTitle('Action log');
-		$this->zbxTestAssertElementPresentId('config');
-		$this->zbxTestCheckHeader('Action log');
-
-		$status = '';
-		$type = '';
-		$retries = '';
-
-		if ($auditactions['status'] == 1 && $auditactions['alerttype'] == 0) {
-			$status = 'Sent';
-		}
-		if ($auditactions['status'] == 0 && $auditactions['alerttype'] == 0 && $auditactions['retries'] == 0) {
-			$status = 'Failed';
-		}
-		if ($auditactions['status'] == 0 && $auditactions['alerttype'] == 0 && $auditactions['retries'] <> 0) {
-			$status = 'In progress';
-		}
-		if ($auditactions['status'] == 1 && $auditactions['alerttype'] == 1) {
-			$status = 'Executed';
+		// If the filter is not visible - enable it.
+		if ($this->query('xpath://li[@aria-labelledby="ui-id-2" and @aria-selected="false"]')->exists()) {
+			$this->query('id:ui-id-2')->one()->click();
 		}
 
-		$sql = 'SELECT mt.name FROM media_type mt, alerts a WHERE a.mediatypeid = mt.mediatypeid AND a.alertid='.zbx_dbstr($auditactions['alertid']);
-		$type = DBfetch(DBselect($sql));
+		$table = $this->query('class:list-table')->asTable()->one();
+		$form = $this->query('name:zbx_filter')->asForm()->one();
+		$form->fill($data['fields'])->submit();
+		$this->page->waitUntilReady();
 
-		if ($auditactions['status'] == 1) {
-			$retries = '';
+		if (array_key_exists('result_amount', $data)) {
+			$this->assertEquals($data['result_amount'], $table->getRows()->count());
+			$this->assertTableStats($data['result_amount']);
+
+			if (array_key_exists('fields', $data)) {
+				foreach ($data['fields'] as $column => $values) {
+					$column_values = $this->getTableColumnData(substr($column, 0, -1));
+					foreach ($column_values as $column_value) {
+						foreach ($values as $value) {
+							if (str_contains($column_value, $value)) {
+								$column_values = array_values(array_diff($column_values, [$column_value]));
+							}
+						}
+					}
+				}
+			}
 		}
-		if ($status == 'In progress' || $status == 'not sent') {
-			$retries = $auditactions['retries'];
-		}
-
-		$message = str_replace('>', '&gt;', $auditactions['message']);
-		$subject = str_replace('>', '&gt;', $auditactions['subject']);
-		$info = str_replace('"', '&amp;quot;', $auditactions['error']);
-
-		$this->zbxTestTextPresent(
-				[
-					$today,
-					CTestArrayHelper::get($type, 'name'),
-					$status,
-					$retries,
-					$auditactions['sendto'],
-					$subject,
-					$message,
-					$info
-				]
-		);
-
-		$this->zbxTestExpandFilterTab();
-		$this->zbxTestClickButtonMultiselect('filter_userids_');
-		$this->zbxTestLaunchOverlayDialog('Users');
-		$this->zbxTestClickLinkText('guest');
-		$this->zbxTestClickXpathWait("//button[@name='filter_set']");
-		$this->zbxTestTextPresent('No data found.');
-
-		$this->zbxTestClickButtonText('Reset');
 	}
 }
