8b3069ac2a3cf32356772e5b7cb7316b4c7a3902
Aleksejs Cikuns <aleksejs.cikuns@zabbix.com>
2022-11-30 13:33:16 +0200

    ..F....... [DEV-2299] fixed code style

--- ui/tests/selenium/dashboard/testDashboardPages.php
+++ ui/tests/selenium/dashboard/testDashboardPages.php
@@ -37,14 +37,14 @@ class testDashboardPages extends CWebTest {
 	 *
 	 * @var string
 	 */
-	const NEXT_BUTTON = 'xpath://button[@class="dashboard-next-page btn-iterator-page-next"]';
+	const NEXT_BUTTON = 'xpath://button[@class="dashboard-next-page btn-iterator-page-next zi-chevron-right"]';
 
 	/**
 	 * Previous page button in dashboard.
 	 *
 	 * @var string
 	 */
-	const PREVIOUS_BUTTON = 'xpath://button[@class="dashboard-previous-page btn-iterator-page-previous"]';
+	const PREVIOUS_BUTTON = 'xpath://button[@class="dashboard-previous-page btn-iterator-page-previous zi-chevron-left"]';
 
 	/**
 	 * Attach MessageBehavior to the test.
