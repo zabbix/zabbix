f4f4de07b359cdf8f6513b9ad0aa32f3d3d71613
Eliza Sekace <eliza.sekace@zabbix.com>
2022-12-12 14:30:53 +0200

    ..F....... [ZBX-21961] fixed message/command label in Actions block in Event details

--- ui/include/actions.inc.php
+++ ui/include/actions.inc.php
@@ -1937,8 +1937,10 @@ function makeEventDetailsActionsTable(array $data, array $users, array $mediatyp
 		if ($action['action_type'] == ZBX_EVENT_HISTORY_ALERT && $action['alerttype'] == ALERT_TYPE_MESSAGE) {
 			$message = [bold($action['subject']), BR(), BR(), zbx_nl2br($action['message'])];
 		}
-		elseif (($action['action_type'] == ZBX_EVENT_HISTORY_ALERT && $action['alerttype'] == ALERT_TYPE_COMMAND)
-				|| $action['action_type'] == ZBX_EVENT_HISTORY_MANUAL_UPDATE) {
+		elseif ($action['action_type'] == ZBX_EVENT_HISTORY_MANUAL_UPDATE) {
+			$message = zbx_nl2br($action['message']);
+		}
+		elseif ($action['action_type'] == ZBX_EVENT_HISTORY_ALERT && $action['alerttype'] == ALERT_TYPE_COMMAND) {
 			$message = [
 				bold(_('Command').':'),
 				BR(),
