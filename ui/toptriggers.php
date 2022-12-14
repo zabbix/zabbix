2b436944f8f20b4874bfa2fe73c5c040bc775181
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2022-12-14 12:27:30 +0000

    ..F....... [DEV-2085] removed useless code in toptriggers.php

--- ui/toptriggers.php
+++ ui/toptriggers.php
@@ -47,10 +47,7 @@ $data['config'] = select_config();
  * Filter
  */
 if (hasRequest('filter_set')) {
-	// prepare severity array
-	$severities = hasRequest('severities') ? array_keys(getRequest('severities')) : [];
-
-	CProfile::updateArray('web.toptriggers.filter.severities', $severities, PROFILE_TYPE_STR);
+	CProfile::updateArray('web.toptriggers.filter.severities', getRequest('severities', []), PROFILE_TYPE_STR);
 	CProfile::updateArray('web.toptriggers.filter.groupids', getRequest('groupids', []), PROFILE_TYPE_STR);
 	CProfile::updateArray('web.toptriggers.filter.hostids', getRequest('hostids', []), PROFILE_TYPE_STR);
 }
