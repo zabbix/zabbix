b7d8478fd4ab6e93ce666eecd930a6d91df5d17f
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2022-12-19 14:16:55 +0000

    .......... [DEV-2085] fixed testUrlParameters Selenium tests

--- ui/tests/selenium/testUrlParameters.php
+++ ui/tests/selenium/testUrlParameters.php
@@ -783,14 +783,14 @@ class testUrlParameters extends CLegacyWebTest {
 					[
 						'url' => 'hostinventoriesoverview.php?filter_groups%5B%5D=abc&filter_groupby=&filter_set=1',
 						'text_present' => [
-							'Zabbix has received an incorrect request.',
+							'Page received incorrect data',
 							'Field "filter_groups" is not integer.'
 						]
 					],
 					[
 						'url' => 'hostinventoriesoverview.php?filter_groups%5B%5D=&filter_groupby=&filter_set=1',
 						'text_present' => [
-							'Zabbix has received an incorrect request.',
+							'Page received incorrect data',
 							'Field "filter_groups" is not integer.'
 						]
 					],
@@ -829,14 +829,14 @@ class testUrlParameters extends CLegacyWebTest {
 					[
 						'url' => 'hostinventories.php?filter_groups%5B%5D=abc&filter_set=1',
 						'text_present' => [
-							'Zabbix has received an incorrect request.',
+							'Page received incorrect data',
 							'Field "filter_groups" is not integer.'
 						]
 					],
 					[
 						'url' => 'hostinventories.php?filter_groups%5B%5D=&filter_set=1',
 						'text_present' => [
-							'Zabbix has received an incorrect request.',
+							'Page received incorrect data',
 							'Field "filter_groups" is not integer.'
 						]
 					],
