7421bbf74301d9a1ab2856f2887270cf0f8ff955
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2022-12-21 19:06:23 +0200

    ........S. [ZBXNEXT-8105] added history transfer from history syncer to connector manager

---
+++ src/libs/zbxconnector/connector.c
@@ -0,0 +1,62 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2022 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "zbxconnector.h"
+
+#include "log.h"
+#include "zbxipcservice.h"
+
+void	zbx_connector_send(zbx_uint32_t code, unsigned char *data, zbx_uint32_t size)
+{
+	static zbx_ipc_socket_t	socket;
+
+	/* each process has a permanent connection to availability manager */
+	if (0 == socket.fd)
+	{
+		char	*error = NULL;
+
+		if (FAIL == zbx_ipc_socket_open(&socket, ZBX_IPC_SERVICE_CONNECTOR, SEC_PER_MIN, &error))
+		{
+			zabbix_log(LOG_LEVEL_CRIT, "cannot connect to availability manager service: %s", error);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (FAIL == zbx_ipc_socket_write(&socket, code, data, size))
+	{
+		zabbix_log(LOG_LEVEL_CRIT, "cannot send data to availability manager service");
+		exit(EXIT_FAILURE);
+	}
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Purpose: frees interface availability data                                 *
+ *                                                                            *
+ * Parameters: availability - [IN] interface availability data                *
+ *                                                                            *
+ ******************************************************************************/
+void	zbx_connector_object_free(zbx_connector_object_t *connector_object)
+{
+	zbx_free(connector_object->str);
+	zbx_free(connector_object);
+}
+
+ZBX_PTR_VECTOR_IMPL(connector_object_ptr, zbx_connector_object_t *)
+
---
+++ src/libs/zbxconnector/connector_protocol.c
@@ -0,0 +1,73 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2022 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "zbxserialize.h"
+#include "zbxconnector.h"
+#include "log.h"
+#include "zbxjson.h"
+
+void	zbx_connector_serialize_object(unsigned char **data, size_t *data_alloc, size_t *data_offset,
+		const zbx_connector_object_t *connector_object)
+{
+	zbx_uint32_t	data_len = 0, error_len;
+	unsigned char	*ptr;
+
+	zbx_serialize_prepare_value(data_len, connector_object->objectid);
+	zbx_serialize_prepare_value(data_len, connector_object->ts.sec);
+	zbx_serialize_prepare_value(data_len, connector_object->ts.ns);
+	zbx_serialize_prepare_str_len(data_len, connector_object->str, error_len);
+
+	if (NULL == *data)
+		*data = (unsigned char *)zbx_calloc(NULL, (*data_alloc = MAX(1024, data_len)), 1);
+
+	while (data_len > *data_alloc - *data_offset)
+	{
+		*data_alloc *= 2;
+		*data = (unsigned char *)zbx_realloc(*data, *data_alloc);
+	}
+	ptr = *data + *data_offset;
+	*data_offset += data_len;
+
+	ptr += zbx_serialize_value(ptr, connector_object->objectid);
+	ptr += zbx_serialize_value(ptr, connector_object->ts.sec);
+	ptr += zbx_serialize_value(ptr, connector_object->ts.ns);
+	zbx_serialize_str(ptr, connector_object->str, error_len);
+}
+
+void	zbx_connector_deserialize_object(const unsigned char *data, zbx_uint32_t size,
+		zbx_vector_connector_object_ptr_t *connector_objects)
+{
+	const unsigned char	*end = data + size;
+
+	while (data < end)
+	{
+		zbx_connector_object_t	*connector_object;
+		zbx_uint32_t		deserialize_str_len;
+
+		connector_object = (zbx_connector_object_t *)zbx_malloc(NULL,
+				sizeof(zbx_connector_object_t));
+
+		zbx_vector_connector_object_ptr_append(connector_objects, connector_object);
+
+		data += zbx_deserialize_value(data, &connector_object->objectid);
+		data += zbx_deserialize_value(data, &connector_object->ts.sec);
+		data += zbx_deserialize_value(data, &connector_object->ts.ns);
+		data += zbx_deserialize_str(data, &connector_object->str, deserialize_str_len);
+	}
+}
