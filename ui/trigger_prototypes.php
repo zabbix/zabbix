ec6f63a00c233cc04251eff42917f911651c5631
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2023-01-06 08:41:11 +0000

    ..F....... [DEV-2386] fixed "form_refresh" input parameter validation in the "Trigger prototype" form

--- ui/trigger_prototypes.php
+++ ui/trigger_prototypes.php
@@ -102,7 +102,7 @@ $fields = [
 	'delete' =>									[T_ZBX_STR, O_OPT, P_SYS|P_ACT, null,	null],
 	'cancel' =>									[T_ZBX_STR, O_OPT, P_SYS,	null,		null],
 	'form' =>									[T_ZBX_STR, O_OPT, P_SYS,	null,		null],
-	'form_refresh' =>							[T_ZBX_INT, O_OPT, null,	null,		null],
+	'form_refresh' =>							[T_ZBX_INT, O_OPT, P_SYS,	null,		null],
 	// sort and sortorder
 	'sort' =>									[T_ZBX_STR, O_OPT, P_SYS, IN('"description","priority","status","discover"'),		null],
 	'sortorder' =>								[T_ZBX_STR, O_OPT, P_SYS, IN('"'.ZBX_SORT_DOWN.'","'.ZBX_SORT_UP.'"'),	null]
@@ -548,7 +548,7 @@ elseif (isset($_REQUEST['form'])) {
 	$data = getTriggerFormData([
 		'config' => $config,
 		'form' => getRequest('form'),
-		'form_refresh' => getRequest('form_refresh'),
+		'form_refresh' => getRequest('form_refresh', 0),
 		'parent_discoveryid' => getRequest('parent_discoveryid'),
 		'dependencies' => getRequest('dependencies', []),
 		'db_dependencies' => [],
