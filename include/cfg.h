b63d7739a25029c2420d42e314b7d79191e4e3bf
Armands Arseniuss Skolmeisters <armands.skolmeisters@zabbix.com>
2023-01-12 11:42:35 +0200

    ...G.J.PS. [DEV-2161] changed function order

--- include/cfg.h
+++ include/cfg.h
@@ -67,10 +67,10 @@ typedef struct
 	int	(*cfg_custom_parameter_parser_func)(const char *value, const struct cfg_line *cfg);
 }	cfg_custom_parameter_parser_t;
 
-void	zbx_init_library_cfg(unsigned char program_type);
-
 void	zbx_addr_copy(zbx_vector_ptr_t *addr_to, const zbx_vector_ptr_t *addr_from);
 void	zbx_addr_free(zbx_addr_t *addr);
+
+void	zbx_init_library_cfg(unsigned char program_type);
 
 int	parse_cfg_file(const char *cfg_file, struct cfg_line *cfg, int optional, int strict, int noexit);
 
