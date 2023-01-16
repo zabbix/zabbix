abafbebc7e8c6499af2e14a8a3d17654e98027eb
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2023-01-16 15:50:12 +0200

    ..F....... [ZBXNEXT-8105,ZBXNEXT-8154] fixed connectors position in the main menu

--- ui/include/classes/helpers/CMenuHelper.php
+++ ui/include/classes/helpers/CMenuHelper.php
@@ -354,12 +354,12 @@ class CMenuHelper {
 							->setAction('trigdisplay.edit'),
 						(new CMenuItem(_('Geographical maps')))
 							->setAction('geomaps.edit'),
-						(new CMenuItem(_('Connectors')))
-							->setAction('connector.list')
-							->setAliases(['connector.edit']),
 						(new CMenuItem(_('Modules')))
 							->setAction('module.list')
 							->setAliases(['module.edit', 'module.scan']),
+						(new CMenuItem(_('Connectors')))
+							->setAction('connector.list')
+							->setAliases(['connector.edit']),
 						(new CMenuItem(_('Other')))
 							->setAction('miscconfig.edit')
 					])))
