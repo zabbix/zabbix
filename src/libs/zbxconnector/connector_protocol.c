cbf8b2983b176d466d6bb42cd527d15951499b1c
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2023-01-20 11:08:23 +0200

    ........S. [ZBXNEXT-8105] style fix

--- src/libs/zbxconnector/connector_protocol.c
+++ src/libs/zbxconnector/connector_protocol.c
@@ -17,8 +17,8 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-#include "zbxserialize.h"
 #include "zbxconnector.h"
+#include "zbxserialize.h"
 #include "log.h"
 #include "zbxjson.h"
 
