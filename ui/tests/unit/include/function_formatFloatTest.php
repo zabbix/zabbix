ac66198f5052666b5f1655bb0c18a1a005f68d67
Andrejs Verza <andrejs.verza@zabbix.com>
2023-01-23 10:51:34 +0200

    ..F....... [ZBX-22222] fixed coding style

--- ui/tests/unit/include/function_formatFloatTest.php
+++ ui/tests/unit/include/function_formatFloatTest.php
@@ -25,100 +25,108 @@ class function_formatFloatTest extends TestCase {
 
 	public static function dataProvider() {
 		return [
-			[0,						15,	null,	false,	true,	'0'],
-			[1,						15,	null,	false,	true,	'1'],
-			[9.4,					15,	null,	false,	true,	'9'],
-			[9.5,					15,	null,	false,	true,	'10'],
-			[9.99999999999999E+14,	15,	null,	false,	true,	'999999999999999'],
-			[9.99999999999999E+15,	15,	null,	false,	true,	'1E+16'],
-			[1.1,					15,	4,		false,	true,	'1.1'],
-			[1.0001,				15,	4,		false,	true,	'1.0001'],
-			[1.00004,				15,	4,		false,	true,	'1'],
-			[1.00005,				15,	4,		false,	true,	'1.0001'],
-			[0.000012344,			15,	4,		false,	true,	'0.00001234'],
-			[0.000012345,			15,	4,		false,	true,	'0.00001235'],
-			[100.00004,				15,	4,		false,	true,	'100'],
-			[100.00005,				15,	4,		false,	true,	'100.0001'],
-			[1E-14,					15,	4,		false,	true,	'0.00000000000001'],
-			[1E-15,					15,	4,		false,	true,	'1E-15'],
-			[1.0004E-14,			15,	4,		false,	true,	'1.0004E-14'],
-			[1.0005E-14,			15,	4,		false,	true,	'1.0005E-14'],
-			[1E+6,					4,	null,	false,	true,	'1000000'],
-			[1E+7,					4,	null,	false,	true,	'1E+7'],
-			[1.4E+100,				4,	null,	false,	true,	'1E+100'],
-			[1.5E+100,				4,	null,	false,	true,	'2E+100'],
-			[1.004E+100,			4,	2,		false,	true,	'1E+100'],
-			[1.005E+100,			4,	2,		false,	true,	'1.01E+100'],
-			[0.129,					4,	2,		false,	true,	'0.13'],
-			[0.0129,				4,	2,		false,	true,	'1.29E-2'],
-			[0.00129,				4,	2,		false,	true,	'1.29E-3'],
-			[0.000129,				4,	2,		false,	true,	'1.29E-4'],
-			[0.129,					4,	2,		true,	true,	'0.13'],
-			[0.0129,				4,	2,		true,	true,	'1.29E-2'],
-			[0.00129,				4,	2,		true,	true,	'1.29E-3'],
-			[0.000129,				4,	2,		true,	true,	'1.29E-4'],
-			[0.0004,				15,	4,		true,	true,	'0.0004'],
-			[0.0005,				15,	4,		true,	true,	'0.0005'],
-			[0.00004,				15,	4,		true,	true,	'4.0000E-5'],
-			[0.00005,				15,	4,		true,	true,	'5.0000E-5'],
-			[100.0004,				15,	4,		true,	true,	'100.0004'],
-			[100.0005,				15,	4,		true,	true,	'100.0005'],
-			[100.00004,				15,	4,		true,	true,	'100.0000'],
-			[100.00005,				15,	4,		true,	true,	'100.0001'],
-			[0,						15,	null,	false,	false,	'0'],
-			[1,						15,	null,	false,	false,	'1'],
-			[9.4,					15,	null,	false,	false,	'9'],
-			[9.5,					15,	null,	false,	false,	'10'],
-			[9.99999999999999E+14,	15,	null,	false,	false,	'999999999999999'],
-			[9.99999999999999E+15,	15,	null,	false,	false,	'1E+16'],
-			[1.1,					15,	4,		false,	false,	'1.1'],
-			[1.0001,				15,	4,		false,	false,	'1.0001'],
-			[1.00004,				15,	4,		false,	false,	'1'],
-			[1.00005,				15,	4,		false,	false,	'1.0001'],
-			[0.000012344,			15,	4,		false,	false,	'0.00001234'],
-			[0.000012345,			15,	4,		false,	false,	'0.00001235'],
-			[100.00004,				15,	4,		false,	false,	'100'],
-			[100.00005,				15,	4,		false,	false,	'100.0001'],
-			[1E-14,					15,	4,		false,	false,	'0.00000000000001'],
-			[1E-15,					15,	4,		false,	false,	'0.000000000000001'],
-			[1.0004E-14,			15,	4,		false,	false,	'0.00000000000001'],
-			[1.0005E-14,			15,	4,		false,	false,	'0.00000000000001001'],
-			[1E+6,					4,	null,	false,	false,	'1000000'],
-			[1E+7,					4,	null,	false,	false,	'1E+7'],
-			[1.4E+100,				4,	null,	false,	false,	'1E+100'],
-			[1.5E+100,				4,	null,	false,	false,	'2E+100'],
-			[1.004E+100,			4,	2,		false,	false,	'1E+100'],
-			[1.005E+100,			4,	2,		false,	false,	'1.01E+100'],
-			[0.129,					4,	2,		false,	false,	'0.13'],
-			[0.0129,				4,	2,		false,	false,	'0.013'],
-			[0.00129,				4,	2,		false,	false,	'0.0013'],
-			[0.000129,				4,	2,		false,	false,	'0.00013'],
-			[0.129,					4,	2,		true,	false,	'0.13'],
-			[0.0129,				4,	2,		true,	false,	'0.01'],
-			[0.00129,				4,	2,		true,	false,	'0.00'],
-			[0.000129,				4,	2,		true,	false,	'0.00'],
-			[0.0004,				15,	4,		true,	false,	'0.0004'],
-			[0.0005,				15,	4,		true,	false,	'0.0005'],
-			[0.00004,				15,	4,		true,	false,	'0.0000'],
-			[0.00005,				15,	4,		true,	false,	'0.0001'],
-			[100.0004,				15,	4,		true,	false,	'100.0004'],
-			[100.0005,				15,	4,		true,	false,	'100.0005'],
-			[100.00004,				15,	4,		true,	false,	'100.0000'],
-			[100.00005,				15,	4,		true,	false,	'100.0001']
+			[0,						[],	'0'],
+			[1,						[],	'1'],
+			[9.4,					[],	'9'],
+			[9.5,					[],	'10'],
+			[9.99999999999999E+14,	[],	'999999999999999'],
+			[9.99999999999999E+15,	[],	'1E+16'],
+
+			[1.1,					['decimals' => 4],	'1.1'],
+			[1.0001,				['decimals' => 4],	'1.0001'],
+			[1.00004,				['decimals' => 4],	'1'],
+			[1.00005,				['decimals' => 4],	'1.0001'],
+			[0.000012344,			['decimals' => 4],	'0.00001234'],
+			[0.000012345,			['decimals' => 4],	'0.00001235'],
+			[100.00004,				['decimals' => 4],	'100'],
+			[100.00005,				['decimals' => 4],	'100.0001'],
+			[1E-14,					['decimals' => 4],	'0.00000000000001'],
+			[1E-15,					['decimals' => 4],	'1E-15'],
+			[1.0004E-14,			['decimals' => 4],	'1.0004E-14'],
+			[1.0005E-14,			['decimals' => 4],	'1.0005E-14'],
+
+			[1E+6,					['precision' => 4],	'1000000'],
+			[1E+7,					['precision' => 4],	'1E+7'],
+			[1.4E+100,				['precision' => 4],	'1E+100'],
+			[1.5E+100,				['precision' => 4],	'2E+100'],
+
+			[1.004E+100,			['precision' => 4, 'decimals' => 2],	'1E+100'],
+			[1.005E+100,			['precision' => 4, 'decimals' => 2],	'1.01E+100'],
+			[0.129,					['precision' => 4, 'decimals' => 2],	'0.13'],
+			[0.0129,				['precision' => 4, 'decimals' => 2],	'1.29E-2'],
+			[0.00129,				['precision' => 4, 'decimals' => 2],	'1.29E-3'],
+			[0.000129,				['precision' => 4, 'decimals' => 2],	'1.29E-4'],
+
+			[0.129,					['precision' => 4, 'decimals' => 2, 'decimals_exact' => true],	'0.13'],
+			[0.0129,				['precision' => 4, 'decimals' => 2, 'decimals_exact' => true],	'1.29E-2'],
+			[0.00129,				['precision' => 4, 'decimals' => 2, 'decimals_exact' => true],	'1.29E-3'],
+			[0.000129,				['precision' => 4, 'decimals' => 2, 'decimals_exact' => true],	'1.29E-4'],
+
+			[0.0004,				['decimals' => 4, 'decimals_exact' => true],	'0.0004'],
+			[0.0005,				['decimals' => 4, 'decimals_exact' => true],	'0.0005'],
+			[0.00004,				['decimals' => 4, 'decimals_exact' => true],	'4.0000E-5'],
+			[0.00005,				['decimals' => 4, 'decimals_exact' => true],	'5.0000E-5'],
+			[100.0004,				['decimals' => 4, 'decimals_exact' => true],	'100.0004'],
+			[100.0005,				['decimals' => 4, 'decimals_exact' => true],	'100.0005'],
+			[100.00004,				['decimals' => 4, 'decimals_exact' => true],	'100.0000'],
+			[100.00005,				['decimals' => 4, 'decimals_exact' => true],	'100.0001'],
+
+			[0,						['small_scientific' => false],	'0'],
+			[1,						['small_scientific' => false],	'1'],
+			[9.4,					['small_scientific' => false],	'9'],
+			[9.5,					['small_scientific' => false],	'10'],
+			[9.99999999999999E+14,	['small_scientific' => false],	'999999999999999'],
+			[9.99999999999999E+15,	['small_scientific' => false],	'1E+16'],
+
+			[1.1,					['decimals' => 4, 'small_scientific' => false],	'1.1'],
+			[1.0001,				['decimals' => 4, 'small_scientific' => false],	'1.0001'],
+			[1.00004,				['decimals' => 4, 'small_scientific' => false],	'1'],
+			[1.00005,				['decimals' => 4, 'small_scientific' => false],	'1.0001'],
+			[0.000012344,			['decimals' => 4, 'small_scientific' => false],	'0.00001234'],
+			[0.000012345,			['decimals' => 4, 'small_scientific' => false],	'0.00001235'],
+			[100.00004,				['decimals' => 4, 'small_scientific' => false],	'100'],
+			[100.00005,				['decimals' => 4, 'small_scientific' => false],	'100.0001'],
+			[1E-14,					['decimals' => 4, 'small_scientific' => false],	'0.00000000000001'],
+			[1E-15,					['decimals' => 4, 'small_scientific' => false],	'0.000000000000001'],
+			[1.0004E-14,			['decimals' => 4, 'small_scientific' => false],	'0.00000000000001'],
+			[1.0005E-14,			['decimals' => 4, 'small_scientific' => false],	'0.00000000000001001'],
+
+			[1E+6,					['precision' => 4, 'small_scientific' => false],	'1000000'],
+			[1E+7,					['precision' => 4, 'small_scientific' => false],	'1E+7'],
+			[1.4E+100,				['precision' => 4, 'small_scientific' => false],	'1E+100'],
+			[1.5E+100,				['precision' => 4, 'small_scientific' => false],	'2E+100'],
+
+			[1.004E+100,			['precision' => 4, 'decimals' => 2, 'small_scientific' => false],	'1E+100'],
+			[1.005E+100,			['precision' => 4, 'decimals' => 2, 'small_scientific' => false],	'1.01E+100'],
+			[0.129,					['precision' => 4, 'decimals' => 2, 'small_scientific' => false],	'0.13'],
+			[0.0129,				['precision' => 4, 'decimals' => 2, 'small_scientific' => false],	'0.013'],
+			[0.00129,				['precision' => 4, 'decimals' => 2, 'small_scientific' => false],	'0.0013'],
+			[0.000129,				['precision' => 4, 'decimals' => 2, 'small_scientific' => false],	'0.00013'],
+
+			[0.129,					['precision' => 4, 'decimals' => 2, 'decimals_exact' => true, 'small_scientific' => false],	'0.13'],
+			[0.0129,				['precision' => 4, 'decimals' => 2, 'decimals_exact' => true, 'small_scientific' => false],	'0.01'],
+			[0.00129,				['precision' => 4, 'decimals' => 2, 'decimals_exact' => true, 'small_scientific' => false],	'0.00'],
+			[0.000129,				['precision' => 4, 'decimals' => 2, 'decimals_exact' => true, 'small_scientific' => false],	'0.00'],
+
+			[0.0004,				['decimals' => 4, 'decimals_exact' => true, 'small_scientific' => false],	'0.0004'],
+			[0.0005,				['decimals' => 4, 'decimals_exact' => true, 'small_scientific' => false],	'0.0005'],
+			[0.00004,				['decimals' => 4, 'decimals_exact' => true, 'small_scientific' => false],	'0.0000'],
+			[0.00005,				['decimals' => 4, 'decimals_exact' => true, 'small_scientific' => false],	'0.0001'],
+			[100.0004,				['decimals' => 4, 'decimals_exact' => true, 'small_scientific' => false],	'100.0004'],
+			[100.0005,				['decimals' => 4, 'decimals_exact' => true, 'small_scientific' => false],	'100.0005'],
+			[100.00004,				['decimals' => 4, 'decimals_exact' => true, 'small_scientific' => false],	'100.0000'],
+			[100.00005,				['decimals' => 4, 'decimals_exact' => true, 'small_scientific' => false],	'100.0001']
 		];
 	}
 
 	/**
 	 * @dataProvider dataProvider
 	 *
-	 * @param float    $source
-	 * @param int|null $precision
-	 * @param int|null $decimals
-	 * @param bool     $exact
-	 * @param bool     $small_scientific
-	 * @param string   $expected
+	 * @param float  $source
+	 * @param array  $options
+	 * @param string $expected
 	*/
-	public function test($source, $precision, $decimals, $exact, $small_scientific, $expected) {
-		$this->assertSame($expected, formatFloat($source, $precision, $decimals, $exact, $small_scientific));
+	public function test(float $source, array $options, string $expected) {
+		$this->assertSame($expected, formatFloat($source, $options));
 	}
 }
