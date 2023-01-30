04c55e948e65c1c0400a2636fd6ea7bf1c3b016a
Natalja Romancaka <natalja.romancaka@zabbix.com>
2023-01-30 11:20:18 +0200

    .......... [ZBXNEXT-8204] fixed Selenium tests: changed findElement to query(), fixed mysql message depending on php version

--- ui/tests/selenium/testFormSetup.php
+++ ui/tests/selenium/testFormSetup.php
@@ -348,7 +348,7 @@ class testFormSetup extends CWebTest {
 	}
 
 	public function getDbConnectionDetails() {
-		return [
+		$provider = [
 			// Incorrect DB host.
 			[
 				[
@@ -545,6 +545,31 @@ class testFormSetup extends CWebTest {
 				]
 			]
 		];
+
+		// MySQL database error depends on php version.
+		$mapping = [
+			'Error connecting to database. Empty cipher.' => [
+				'8.1.0' => '(trying to connect via (null))'
+			],
+			'php_network_getaddresses: getaddrinfo failed: Name or service not known' => [
+				'8.1.0' => 'php_network_getaddresses: getaddrinfo for incorrect_DB_host failed: Name or service not known'
+			]
+		];
+
+		foreach ($provider as &$data) {
+			if (array_key_exists('mysql_error', $data[0]) && array_key_exists($data[0]['mysql_error'], $mapping)) {
+				foreach ($mapping[$data[0]['mysql_error']] as $version => $map) {
+					if (version_compare(phpversion(), $version, '<')) {
+						continue;
+					}
+
+					$data[0]['mysql_error'] = $map;
+				}
+			}
+		}
+		unset($data);
+
+		return $provider;
 	}
 
 	/**
