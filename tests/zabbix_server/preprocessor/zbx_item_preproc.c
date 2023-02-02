ad54d8450495a9eac2a06f312b04b8ec77ff3f30
Andris Zeila <andris.zeila@zabbix.com>
2023-02-02 12:35:21 +0200

    .......... [ZBXNEXT-8166] fixed test case

--- tests/zabbix_server/preprocessor/zbx_item_preproc.c
+++ tests/zabbix_server/preprocessor/zbx_item_preproc.c
@@ -251,6 +251,7 @@ void	zbx_mock_test_entry(void **state)
 	int				mib_translation_case = 0;
 
 	preproc_init_snmp();
+	pp_context_init(&ctx);
 
 	if (ZBX_MOCK_SUCCESS == zbx_mock_parameter_exists("in.netsnmp_required"))
 		mib_translation_case = 1;
