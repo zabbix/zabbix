75c9105e1e871aa7f660de20d07e493fdf91183a
Ivo Kurzemnieks <ivo.kurzemnieks@zabbix.com>
2023-02-09 09:30:21 +0200

    A......... [ZBX-19466] reverted sorting options in script.getScriptsByHosts method

--- ui/include/classes/api/services/CScript.php
+++ ui/include/classes/api/services/CScript.php
@@ -979,7 +979,7 @@ class CScript extends CApiService {
 		$scripts = $this->get([
 			'output' => API_OUTPUT_EXTEND,
 			'hostids' => $hostids,
-			'sortfield' => ['name', 'menu_path'],
+			'sortfield' => 'name',
 			'preservekeys' => true
 		]);
 
