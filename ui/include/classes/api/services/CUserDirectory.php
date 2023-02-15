05cde5ee61f9f927e3b7435e761ab38d07a432d2
Miks Kronkalns <miks.kronkalns@zabbix.com>
2023-02-15 12:47:11 +0200

    A......... [ZBX-22236] removed excess flag API_NOT_EMPTY from validation rules for userdirectory.test validation method

--- ui/include/classes/api/services/CUserDirectory.php
+++ ui/include/classes/api/services/CUserDirectory.php
@@ -1091,7 +1091,7 @@ class CUserDirectory extends CApiService {
 			'provision_media' =>	['type' => API_MULTIPLE, 'rules' => [
 										['if' => ['field' => 'provision_status', 'in' => implode(',', [JIT_PROVISIONING_ENABLED])], 'type' => API_OBJECTS, 'flags' => API_NORMALIZE, 'uniq' => [['mediatypeid', 'attribute']], 'fields' => [
 											'name' =>			['type' => API_STRING_UTF8, 'flags' => API_REQUIRED | API_NOT_EMPTY, 'length' => DB::getFieldLength('userdirectory_media', 'name')],
-											'mediatypeid' =>	['type' => API_ID, 'flags' => API_REQUIRED | API_NOT_EMPTY],
+											'mediatypeid' =>	['type' => API_ID, 'flags' => API_REQUIRED],
 											'attribute' =>		['type' => API_STRING_UTF8, 'flags' => API_REQUIRED | API_NOT_EMPTY, 'length' => DB::getFieldLength('userdirectory_media', 'attribute')]
 										]],
 										['else' => true, 'type' => API_OBJECTS, 'length' => 0]
