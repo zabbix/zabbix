d7048fafec7fbbb036988efb56c1fed980723942
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2023-02-22 18:48:32 +0200

    .......... [DEV-2371] fixed typo

--- ui/tests/selenium/testFormAdministrationAuthenticationLdap.php
+++ ui/tests/selenium/testFormAdministrationAuthenticationLdap.php
@@ -1,25 +1,24 @@
 <?php
-
 /*
- * * Zabbix
- * * Copyright (C) 2001-2023 Zabbix SIA
- * *
- * * This program is free software; you can redistribute it and/or modify
- * * it under the terms of the GNU General Public License as published by
- * * the Free Software Foundation; either version 2 of the License, or
- * * (at your option) any later version.
- * *
- * * This program is distributed in the hope that it will be useful,
- * * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * * GNU General Public License for more details.
- * *
- * * You should have received a copy of the GNU General Public License
- * * along with this program; if not, write to the Free Software
- * * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * */
+** Zabbix
+** Copyright (C) 2001-2023 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
 
-require_once dirname(__FILE__) . '/../include/CWebTest.php';
+require_once dirname(__FILE__).'/../include/CWebTest.php';
 
 /**
  * @backup config
@@ -157,17 +156,17 @@ class testFormAdministrationAuthenticationLdap extends CWebTest {
 		if (array_key_exists('error', $data)) {
 			$this->assertTrue($message->isBad());
 			$this->assertEquals($data['error'], $message->getTitle());
-		} else {
+		}
+		else {
 			$this->assertTrue($message->isGood());
 			$this->assertEquals('Authentication settings updated', $message->getTitle());
 			// Check DB configuration.
-			$sql = 'SELECT authentication_type,ldap_host,ldap_port,ldap_base_dn,ldap_bind_dn,ldap_bind_password,' .
-				'ldap_search_attribute,http_auth_enabled,http_login_form,http_strip_domains,http_case_sensitive,' .
-				'ldap_configured,ldap_case_sensitive' .
-				' FROM config';
+			$sql = 'SELECT authentication_type,ldap_host,ldap_port,ldap_base_dn,ldap_bind_dn,ldap_bind_password,'.
+					'ldap_search_attribute,http_auth_enabled,http_login_form,http_strip_domains,http_case_sensitive,'.
+					'ldap_configured,ldap_case_sensitive'.
+					' FROM config';
 			$result = CDBHelper::getRow($sql);
 			$this->assertEquals($data['db_check'], $result);
 		}
 	}
-
 }
