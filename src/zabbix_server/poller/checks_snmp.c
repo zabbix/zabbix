88f7a1db49da05f80b1725648470141fa7b50952
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2023-02-24 09:03:13 -0500

    .......PS. [ZBX-22419] fixed code style

--- src/zabbix_server/poller/checks_snmp.c
+++ src/zabbix_server/poller/checks_snmp.c
@@ -1938,7 +1938,7 @@ static void	snmp_bulkwalk_remove_matching_oids(zbx_vector_snmp_oid_t *oids)
 	{
 		while (0 == memcmp(oids->values[i - 1]->str_oid, oids->values[i]->str_oid, len))
 		{
-			if (NULL == strchr(oids->values[i]->str_oid + len - 1, '.'))
+			if ('.' != oids->values[i]->str_oid[len])
 				break;
 
 			vector_snmp_oid_free(oids->values[i]);
