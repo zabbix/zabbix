8722e030fdf89688f7d93beefc4721dab13c6746
Tomass Olups <tomass.olups@zabbix.com>
2023-03-02 09:13:39 +0200

    .......... [ZBXNEXT-826] updated release date and version [5.0.32rc2]

--- include/version.h
+++ include/version.h
@@ -24,7 +24,7 @@
 #define ZBX_STR(str)	ZBX_STR2(str)
 
 #define APPLICATION_NAME	"Zabbix Agent"
-#define ZABBIX_REVDATE		"27 February 2023"
+#define ZABBIX_REVDATE		"2 March 2023"
 #define ZABBIX_VERSION_MAJOR	5
 #define ZABBIX_VERSION_MINOR	0
 #define ZABBIX_VERSION_PATCH	32
