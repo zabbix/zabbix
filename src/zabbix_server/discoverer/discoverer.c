a5553492d2734fd9f47ff889a3486e87d44f3a7b
Mihails Prihodko <mihails.prihodko@zabbix.com>
2023-03-08 13:37:44 +0200

    .......PS. [ZBXNEXT-8079] fixed style

--- src/zabbix_server/discoverer/discoverer.c
+++ src/zabbix_server/discoverer/discoverer.c
@@ -329,7 +329,6 @@ static int	discover_service(const DB_DCHECK *dcheck, char *ip, int port, int con
 				memset(&host, 0, sizeof(host));
 				host.addr = strdup(ip);
 
-
 				if (SUCCEED != zbx_ping(&host, 1, 3, 0, 0, 0, dcheck->allow_redirect, error,
 						sizeof(error)) || 0 == host.rcv)
 				{
