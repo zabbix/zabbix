ddc75f578b544ab699f80a79dc01b4d755174f87
Tomass Olups <tomass.olups@zabbix.com>
2023-03-13 21:43:37 +0900

    .......... [ZBXNEXT-8226] updated failing autotests

--- ui/tests/selenium/geomaps/testFormAdministrationGeneralGeomaps.php
+++ ui/tests/selenium/geomaps/testFormAdministrationGeneralGeomaps.php
@@ -147,7 +147,7 @@ class testFormAdministrationGeneralGeomaps extends CWebTest {
 
 			// Check Service tab fields' maxlength.
 			$limits = [
-				'Tile URL' => 1024,
+				'Tile URL' => 2048,
 				'Attribution' => 1024,
 				'Max zoom level' => 10
 			];
