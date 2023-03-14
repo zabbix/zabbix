e76eb5878d98f87a326d5f26e4b579419e4f0c25
Andrejs Verza <andrejs.verza@zabbix.com>
2023-03-14 18:01:41 +0200

    ..F....... [ZBX-22507] fixed positioning of the debug information on dashboards in editing mode

--- ui/js/class.dashboard.page.js
+++ ui/js/class.dashboard.page.js
@@ -724,6 +724,7 @@ class CDashboardPage extends CBaseComponent {
 			while (!element.classList.contains('wrapper'));
 
 			height = Math.max(height, min_height);
+			height = Math.min(height, this._cell_height * this._max_rows);
 		}
 
 		this._dashboard_grid.style.height = `${height}px`;
