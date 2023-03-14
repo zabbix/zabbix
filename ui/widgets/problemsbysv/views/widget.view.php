33f0262ccbbcd65743e9c17fe78dfda39f060160
Reinis Detlavs <reinis.detlavs@zabbix.com>
2023-03-14 11:14:30 +0200

    ..F....... [ZBX-21642] fixed not clearing filter when clicking on specific host
    * commit '39054d9e3f5fa39b40e93acce316aa93050ad562':
      .......... [ZBX-21642] updated autotest testFormUpdateProblem on master
      .......... [ZBX-21642] reverted changes that included new element in URL on master
      .......... [ZBX-21642] updated failing testPageTriggerUrl autotest on master
      ..F....... [ZBX-21642] fixed filters not being cleared when navigating from various places to Monitoring->Problems and Monitoring->Latest data
      ..F....... [ZBX-21642] fixed filter tab usability when parameter filter_set given
      ..F....... [ZBX-21642] changed error type
      ..F....... [ZBX-21642] added validation for case when page request has parameter filter_set
      .......... [ZBX-21642] updated failing Jenkins autotests on master
      ..F....... [ZBX-21642] fixed coding style
      ..F....... [ZBX-21642] fixed not clearing filter when clicking on specific host
    
    (cherry picked from commit 2602811fc6bd7f4ee9074d58b2da5c71d45fb170)

--- ui/widgets/problemsbysv/views/widget.view.php
+++ ui/widgets/problemsbysv/views/widget.view.php
@@ -56,7 +56,7 @@ else {
 
 	$group_url = (new CUrl('zabbix.php'))
 		->setArgument('action', 'problem.view')
-		->setArgument('filter_name')
+		->setArgument('filter_set', '1')
 		->setArgument('show', TRIGGERS_OPTION_RECENT_PROBLEM)
 		->setArgument('hostids', array_key_exists('hostids', $data['filter']) ? $data['filter']['hostids'] : null)
 		->setArgument('name', array_key_exists('problem', $data['filter']) ? $data['filter']['problem'] : null)
