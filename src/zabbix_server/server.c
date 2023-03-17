48ba5678184932469d1f6880c73fedbfd21635e0
Armands Arseniuss Skolmeisters <armands.skolmeisters@zabbix.com>
2023-03-17 14:05:32 +0200

    .......PS. [ZBX-22097] reverted cache synchronization order upon exit

--- src/zabbix_server/server.c
+++ src/zabbix_server/server.c
@@ -1104,13 +1104,6 @@ static void	zbx_on_exit(int ret)
 		zbx_locks_disable();
 #endif
 
-	if (ZBX_NODE_STATUS_ACTIVE == ha_status)
-	{
-		zbx_db_connect(ZBX_DB_CONNECT_EXIT);
-		free_database_cache(ZBX_SYNC_ALL);
-		zbx_db_close();
-	}
-
 	if (SUCCEED != zbx_ha_stop(&error))
 	{
 		zabbix_log(LOG_LEVEL_CRIT, "cannot stop HA manager: %s", error);
@@ -1122,6 +1115,11 @@ static void	zbx_on_exit(int ret)
 	{
 		zbx_free_metrics();
 		zbx_ipc_service_free_env();
+
+		zbx_db_connect(ZBX_DB_CONNECT_EXIT);
+		free_database_cache(ZBX_SYNC_ALL);
+		zbx_db_close();
+
 		free_configuration_cache();
 
 		/* free history value cache */
