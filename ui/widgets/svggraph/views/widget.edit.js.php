4c14cfbf19886c6c096b8e1b225dc2743c350af6
Valdis Murzins <valdis.murzins@zabbix.com>
2023-03-20 11:40:56 +0200

    ..F....... [ZBX-22004] fixed CSortable opacity design
    * commit 'd2c8ba88f2e3202ca001e2d0eebbc92315b1c742':
      ..F....... [ZBX-22004] generated CSS files
      ..F....... [ZBX-22004] fixed radiobutton border
      ..F....... [ZBX-22004] fixed CSortable opacity design; removed unnecessary z-indexes in modern radiobutton
    
    (cherry picked from commit 00e0fcda5bf0163948d91e3c18affe5049e6c8e2)

--- ui/widgets/svggraph/views/widget.edit.js.php
+++ ui/widgets/svggraph/views/widget.edit.js.php
@@ -455,10 +455,7 @@ window.widget_svggraph_form = new class {
 		if (this._sortable_data_set === undefined) {
 			this._sortable_data_set = new CSortable(
 				document.querySelector('#data_set .<?= ZBX_STYLE_LIST_VERTICAL_ACCORDION ?>'),
-				{
-					is_vertical: true,
-					with_opacity: true
-				}
+				{is_vertical: true}
 			);
 
 			this._sortable_data_set.on(SORTABLE_EVENT_DRAG_END, () => {
