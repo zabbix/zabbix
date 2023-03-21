1e0e4146ca40fec028dbdbde56aea54dc78f08f8
Armands Arseniuss Skolmeisters <armands.skolmeisters@zabbix.com>
2023-03-21 13:43:28 +0200

    ...G...PS. [ZBX-21795] removed empty line

--- src/libs/zbxcomms/telnet.c
+++ src/libs/zbxcomms/telnet.c
@@ -471,7 +471,6 @@ int	zbx_telnet_execute(ZBX_SOCKET socket_fd, const char *command, AGENT_RESULT *
 			SET_MSG_RESULT(result, zbx_strdup(NULL, "Cannot find prompt after command execution: "
 					"execution timed out"));
 			goto fail;
-
 		}
 		SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot find prompt after command execution: %s",
 				strerror_from_system(zbx_socket_last_error())));
