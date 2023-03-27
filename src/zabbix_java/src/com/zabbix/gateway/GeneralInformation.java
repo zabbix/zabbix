ce4ea363c0ead9c4ede428b167412e6079f9ec27
Tomass Olups <tomass.olups@zabbix.com>
2023-03-27 18:17:52 +0300

    .......... [ZBXNEXT-826] updated release changelog

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "27 March 2023";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "5.0.33";
+	static final String VERSION = "5.0.34rc1";
 
 	static void printVersion()
 	{
