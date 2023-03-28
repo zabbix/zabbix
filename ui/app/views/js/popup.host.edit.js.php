64fb0f69e311515f78622178764bec144e83ab46
Miks Kronkalns <miks.kronkalns@zabbix.com>
2023-03-28 14:56:33 +0300

    ..F....... [ZBX-22238] fixed non-writable hostgroup appearance in host group field when host is cloned; added warning message

--- ui/app/views/js/popup.host.edit.js.php
+++ ui/app/views/js/popup.host.edit.js.php
@@ -29,7 +29,7 @@ window.host_edit_popup = {
 	dialogue: null,
 	form: null,
 
-	init({popup_url, form_name, host_interfaces, host_is_discovered, warning}) {
+	init({popup_url, form_name, host_interfaces, host_is_discovered, warnings}) {
 		this.overlay = overlays_stack.getById('host_edit');
 		this.dialogue = this.overlay.$dialogue[0];
 		this.form = this.overlay.$dialogue.$body[0].querySelector('form');
@@ -40,8 +40,10 @@ window.host_edit_popup = {
 
 		host_edit.init({form_name, host_interfaces, host_is_discovered});
 
-		if (warning !== null) {
-			const message_box = makeMessageBox('warning', warning, null, true, false)[0];
+		if (warnings.length) {
+			const message_box = warnings.length == 1
+				? makeMessageBox('warning', warnings, null, true, false)[0]
+				: makeMessageBox('warning', warnings, <?= json_encode(_('Details from cloned host was changed.')) ?>, true, false)[0];
 
 			this.form.parentNode.insertBefore(message_box, this.form);
 		}
