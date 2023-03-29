e8004b71f3f2d87b4880fd960e6e279155ac3c6a
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2023-03-29 10:53:08 +0300

    .......... [ZBXNEXT-686] fixed the expected number of templates in autotest after a new template was added

--- ui/tests/selenium/lld/testPageLowLevelDiscovery.php
+++ ui/tests/selenium/lld/testPageLowLevelDiscovery.php
@@ -282,7 +282,7 @@ class testPageLowLevelDiscovery extends CWebTest {
 						'Host groups' => 'Templates/Databases'
 					],
 					'context' => 'template',
-					'rows' => 85
+					'rows' => 86
 				]
 			],
 			[
