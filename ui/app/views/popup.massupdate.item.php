988cefabd89001dc8c91e0808bfdc66a53b69f3c
Miks Kronkalns <miks.kronkalns@zabbix.com>
2023-04-07 17:18:17 +0300

    ..F....... [DEV-1071] fixed unneeded DIV container

--- ui/app/views/popup.massupdate.item.php
+++ ui/app/views/popup.massupdate.item.php
@@ -84,13 +84,12 @@ $item_form_list
 	->addRow(
 		(new CVisibilityBox('visible[post_type]', 'post_type_container', _('Original')))
 			->setLabel(_('Request body type')),
-		(new CDiv(
-			(new CRadioButtonList('post_type', (int) DB::getDefault('items', 'post_type')))
-				->addValue(_('Raw data'), ZBX_POSTTYPE_RAW)
-				->addValue(_('JSON data'), ZBX_POSTTYPE_JSON)
-				->addValue(_('XML data'), ZBX_POSTTYPE_XML)
-				->setModern(true)
-		))->setId('post_type_container')
+		(new CRadioButtonList('post_type', (int) DB::getDefault('items', 'post_type')))
+			->addValue(_('Raw data'), ZBX_POSTTYPE_RAW)
+			->addValue(_('JSON data'), ZBX_POSTTYPE_JSON)
+			->addValue(_('XML data'), ZBX_POSTTYPE_XML)
+			->setId('post_type_container')
+			->setModern(true)
 	)
 	->addRow(
 		(new CVisibilityBox('visible[timeout]', 'timeout', _('Original')))->setLabel(_('Timeout')),
