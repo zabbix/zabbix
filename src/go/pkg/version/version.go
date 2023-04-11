f06f9cffa4bc41a7cf18453f34c9ff66534d43f8
Tomass Olups <tomass.olups@zabbix.com>
2023-04-11 09:24:08 +0300

    .......... [ZBXNEXT-826] updated release date and version [6.0.16]

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -26,11 +26,11 @@ import (
 )
 
 const (
-	ZABBIX_REVDATE          = "5 April 2023"
+	ZABBIX_REVDATE          = "11 April 2023"
 	ZABBIX_VERSION_MAJOR    = 6
 	ZABBIX_VERSION_MINOR    = 0
 	ZABBIX_VERSION_PATCH    = 16
-	ZABBIX_VERSION_RC       = "rc1"
+	ZABBIX_VERSION_RC       = ""
 	ZABBIX_VERSION_RC_NUM   = "{ZABBIX_RC_NUM}"
 	ZABBIX_VERSION_REVISION = "{ZABBIX_REVISION}"
 	copyrightMessage        = "Copyright (C) 2023 Zabbix SIA\n" +
