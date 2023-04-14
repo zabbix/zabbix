85ea2e4729092f15c565fa72acf2a5b7f8a059a1
Natalja Romancaka <natalja.romancaka@zabbix.com>
2023-04-14 08:41:38 +0300

    .......... [DEV-2424] fixed typo

--- ui/tests/selenium/common/testFormGroups.php
+++ ui/tests/selenium/common/testFormGroups.php
@@ -52,7 +52,7 @@ class testFormGroups extends CWebTest {
 	const DELETE_GROUP = 'Group for Delete test';
 
 	/**
-	 * Host and template sub group name for clone test scenario.
+	 * Host and template subgroup name for clone test scenario.
 	 */
 	const SUBGROUP = 'Group1/Subgroup1/Subgroup2';
 
