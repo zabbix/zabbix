4f32916669b91c4e87013600385c61e5946d65c2
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2023-04-19 11:54:43 +0000

    ..F....... [ZBX-22386] fixed formatting

--- ui/widgets/geomap/actions/WidgetView.php
+++ ui/widgets/geomap/actions/WidgetView.php
@@ -234,7 +234,9 @@ class WidgetView extends CControllerDashboardWidgetView {
 			return [
 				'tile_url' => CSettingsHelper::get(CSettingsHelper::GEOMAPS_TILE_URL),
 				'max_zoom' => CSettingsHelper::get(CSettingsHelper::GEOMAPS_MAX_ZOOM),
-				'attribution' => htmlspecialchars(CSettingsHelper::get(CSettingsHelper::GEOMAPS_ATTRIBUTION), ENT_NOQUOTES, 'UTF-8')
+				'attribution' => htmlspecialchars(CSettingsHelper::get(CSettingsHelper::GEOMAPS_ATTRIBUTION),
+					ENT_NOQUOTES, 'UTF-8'
+				)
 			];
 		}
 
