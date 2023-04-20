62efb66e94e377ffa837dd4873b78557123fec6f
Konstantins Prutkovs <konstantins.prutkovs@zabbix.com>
2023-04-20 09:44:44 -0400

    ........S. [ZBX-22713] fixed Zabbix server compilation problem for NetBSD

--- src/zabbix_server/lld/lld_host.c
+++ src/zabbix_server/lld/lld_host.c
@@ -3905,7 +3905,7 @@ static int	lld_if_update_compare(const void *d1, const void *d2)
 	return u1->diff_num - u2->diff_num;
 }
 
-static zbx_uint64_t	popcount64(zbx_uint64_t mask)
+static zbx_uint64_t	zbx_popcount64(zbx_uint64_t mask)
 {
 	mask -= (mask >> 1) & __UINT64_C(0x5555555555555555);
 	mask = (mask & __UINT64_C(0x3333333333333333)) + (mask >> 2 & __UINT64_C(0x3333333333333333));
@@ -4008,7 +4008,7 @@ static void	lld_host_interfaces_make(zbx_uint64_t hostid, zbx_vector_ptr_t *host
 			update->ifnew = ifnew;
 			update->ifold = interfaces->values[j];
 			update->flags = lld_interface_compare(update->ifold, update->ifnew);
-			update->diff_num = popcount64(update->flags);
+			update->diff_num = zbx_popcount64(update->flags);
 
 			zbx_vector_if_update_append(&updates, update);
 		}
