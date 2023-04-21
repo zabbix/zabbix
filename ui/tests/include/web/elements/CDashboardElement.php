e552c187808fdc6911ddf6d8c5af8cefc2b351e7
Natalja Romancaka <natalja.romancaka@zabbix.com>
2023-04-21 16:07:07 +0300

    .......... [DEV-2341] added mouse hover function

--- ui/tests/include/web/elements/CDashboardElement.php
+++ ui/tests/include/web/elements/CDashboardElement.php
@@ -181,7 +181,12 @@ class CDashboardElement extends CElement {
 				$controls->query('xpath:.//nav[@class="dashboard-edit"]')->waitUntilNotVisible(2);
 			}
 			catch (TimeoutException $ex) {
-				$button->click(true);
+				try {
+					$button->click(true);
+				}
+				catch (\Exception $ex) {
+					// Code is not missing here.
+				}
 			}
 
 			$controls->query('xpath:.//nav[@class="dashboard-edit"]')->waitUntilNotVisible();
