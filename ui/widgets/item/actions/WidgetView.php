e2664fc12ca20da6e638d751c2975e1ef5398aba
Janis Freibergs <janis.freibergs@zabbix.com>
2023-04-24 18:13:51 +0300

    ..F....... [ZBXNEXT-8211] fixed item value widget color override

--- ui/widgets/item/actions/WidgetView.php
+++ ui/widgets/item/actions/WidgetView.php
@@ -208,7 +208,7 @@ class WidgetView extends CControllerDashboardWidgetView {
 					case ITEM_VALUE_TYPE_LOG:
 					case ITEM_VALUE_TYPE_BINARY:
 						$value = $value_type == ITEM_VALUE_TYPE_BINARY
-							? italic(_('binary value'))->addClass(ZBX_STYLE_GREY)
+							? italic(_('binary value'))
 							: formatHistoryValue($last_value, $items[$itemid], false);
 
 						if (array_key_exists(Widget::SHOW_CHANGE_INDICATOR, $show) && $prev_value !== null
