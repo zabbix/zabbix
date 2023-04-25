d154a257da9ba5d70ac1ba4586a15c62f849ce8e
Eliza Sekace <eliza.sekace@zabbix.com>
2023-04-25 11:56:28 +0300

    ..F....... [ZBXNEXT-8386] added comment, added strict type declaration

--- ui/app/views/js/configuration.discovery.edit.js.php
+++ ui/app/views/js/configuration.discovery.edit.js.php
@@ -17,8 +17,12 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
-?>
 
+
+/**
+ * @var CView $this
+ */
+?>
 
 window.drule_edit_popup = new class {
 
