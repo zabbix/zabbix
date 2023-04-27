ac9daa8bc44fd054725fa0acea264d89900ab42d
Andrejs Verza <andrejs.verza@zabbix.com>
2023-04-27 09:49:50 +0300

    ..F....... [ZBXNEXT-8145] fixed coding style related to ZBX-22245

--- ui/js/class.dashboard.page.js
+++ ui/js/class.dashboard.page.js
@@ -450,6 +450,7 @@ class CDashboardPage {
 	_createInaccessibleWidget({widgetid, pos, unique_id}) {
 		return this._createWidget(CWidgetInaccessible, {
 			type: 'inaccessible',
+			name: '',
 			view_mode: ZBX_WIDGET_VIEW_MODE_HIDDEN_HEADER,
 			fields: {},
 			defaults: {
