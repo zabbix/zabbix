398b05ead9f03c6e1536ae9db599199c3754e502
Miks Kronkalns <miks.kronkalns@zabbix.com>
2023-04-28 09:10:00 +0300

    ..F....... [DEV-2472] fixed xss vulnerability in graph item properties

--- ui/graphs.php
+++ ui/graphs.php
@@ -95,13 +95,19 @@ if (isset($_REQUEST['yaxismax']) && zbx_empty($_REQUEST['yaxismax'])) {
 }
 check_fields($fields);
 
+$color_validator = new CColorValidator();
 $gitems = [];
+
 foreach (getRequest('items', []) as $item) {
 	$gitem = json_decode($item, true);
 
-	if ((array_key_exists('itemid', $gitem) && ctype_digit($gitem['itemid']))
-			&& (array_key_exists('type', $gitem) && ctype_digit($gitem['type']))
-			&& (array_key_exists('drawtype', $gitem) && ctype_digit($gitem['drawtype']))) {
+	if ($gitem
+			&& array_key_exists('itemid', $gitem) && ctype_digit($gitem['itemid'])
+			&& array_key_exists('type', $gitem) && ctype_digit($gitem['type'])
+			&& array_key_exists('drawtype', $gitem) && ctype_digit($gitem['drawtype'])
+			&& array_key_exists('calc_fnc', $gitem) && ctype_digit($gitem['calc_fnc'])
+			&& array_key_exists('yaxisside', $gitem) && ctype_digit($gitem['yaxisside'])
+			&& array_key_exists('color', $gitem) && $color_validator->validate($gitem['color'])) {
 		$gitems[] = $gitem;
 	}
 }
