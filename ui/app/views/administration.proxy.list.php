0ccdca6d76ce8ed9ecaf975ceec101c73b744385
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2023-04-28 12:23:45 +0000

    ..F....... [ZBX-22386] removed extra semicolon
    (cherry picked from commit 9d297f2c708bee7b9d5f9cb97b11864fb9a156a3)

--- ui/app/views/administration.proxy.list.php
+++ ui/app/views/administration.proxy.list.php
@@ -86,7 +86,7 @@ foreach ($data['proxies'] as $proxy) {
 
 	foreach ($proxy['hosts'] as $host) {
 		if (++$i > $data['config']['max_in_table']) {
-			$hosts[] = [' ', HELLIP()];;
+			$hosts[] = [' ', HELLIP()];
 
 			break;
 		}
