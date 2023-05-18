56148c29df5a8517c2ecfedac8a9749dd68a220d
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2023-05-18 17:06:54 +0300

    ....I..... [ZBXNEXT-8245] fixed included file order

--- src/libs/zbxdbupgrade/dbupgrade_6050.c
+++ src/libs/zbxdbupgrade/dbupgrade_6050.c
@@ -17,9 +17,10 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-#include "zbxdbhigh.h"
 #include "dbupgrade.h"
+
 #include "zbxdbschema.h"
+#include "zbxdbhigh.h"
 #include "log.h"
 
 /*
