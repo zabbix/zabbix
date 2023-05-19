f14cfd209a747096ab4fa566d77bc383c927de0a
Miks Kronkalns <miks.kronkalns@zabbix.com>
2023-05-19 16:02:12 +0300

    ..F....... [ZBXNEXT-2299] fixed acknowledgement status field becoming non-operational after switching between tabs

--- ui/app/partials/monitoring.problem.filter.php
+++ ui/app/partials/monitoring.problem.filter.php
@@ -142,9 +142,9 @@ $left_column
 		_('Acknowledgement status'),
 		(new CHorList())
 			->addItem((new CRadioButtonList('acknowledgement_status', (int) $data['acknowledgement_status']))
-				->addValue(_('All'), ZBX_ACK_STATUS_ALL)
-				->addValue(_('Unacknowledged'), ZBX_ACK_STATUS_UNACK)
-				->addValue(_('Acknowledged'), ZBX_ACK_STATUS_ACK)
+				->addValue(_('All'), ZBX_ACK_STATUS_ALL, 'acknowledgement_status_0_#{uniqid}')
+				->addValue(_('Unacknowledged'), ZBX_ACK_STATUS_UNACK, 'acknowledgement_status_1_#{uniqid}')
+				->addValue(_('Acknowledged'), ZBX_ACK_STATUS_ACK, 'acknowledgement_status_2_#{uniqid}')
 				->setModern(true)
 			)
 			->addItem((new CCheckBox('acknowledged_by_me', 1))
@@ -152,6 +152,7 @@ $left_column
 				->setChecked($data['acknowledged_by_me'] == 1)
 				->setUncheckedValue(0)
 				->setLabel(_('By me'))
+				->setId('acknowledged_by_me_#{uniqid}')
 			)
 	);
 
