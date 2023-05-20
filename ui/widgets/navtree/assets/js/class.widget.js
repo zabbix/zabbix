247fe2706315a6c4abe1096f94884ab71b0d91ae
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2023-05-20 12:42:58 +0000

    ..F....... [ZBX-21883] fixed JS error when trying to add Map tree element without map

--- ui/widgets/navtree/assets/js/class.widget.js
+++ ui/widgets/navtree/assets/js/class.widget.js
@@ -882,7 +882,9 @@ class CWidgetNavTree extends CWidget {
 									method: 'POST',
 									data: {
 										name: form_inputs.name.value.trim(),
-										sysmapid: form_inputs.sysmapid.value,
+										sysmapid: typeof form_inputs.sysmapid !== 'undefined'
+											? form_inputs.sysmapid.value
+											: '0',
 										add_submaps: form_inputs.add_submaps.checked ? 1 : 0,
 										depth: depth
 									},
