1e5494208aea1551628f38fab8a692b30a029691
Janis Nulle <janis.nulle@zabbix.com>
2023-05-23 11:35:37 +0300

    .......... [DEV-2489] testPageSearch_SearchHost initial version

--- ui/tests/selenium/testPageSearch.php
+++ ui/tests/selenium/testPageSearch.php
@@ -19,28 +19,30 @@
 **/
 
 require_once dirname(__FILE__).'/../include/CLegacyWebTest.php';
+require_once dirname(__FILE__).'/../include/CWebTest.php';
+require_once dirname(__FILE__).'/traits/TableTrait.php';
+
+define('HOST_WIDGET', ['id' => 'search_hosts_widget', 'title' => 'Hosts']);
+define('HOST_GROUP_WIDGET', ['id' => 'search_hostgroup_widget', 'title' => 'Host groups']);
+define('TEMPLATE_WIDGET', ['id' => 'search_templates_widget', 'title' => 'Templates']);
 
 class testPageSearch extends CWebTest {
-	public function testPageSearch_FindZabbixServer_Old() {
-		$this->zbxTestLogin('zabbix.php?action=dashboard.view');
-		$this->zbxTestInputTypeWait('search', 'ЗАББИКС Сервер');
-		$this->zbxTestClickXpath('//button[@class="search-icon"]');
-		$this->zbxTestCheckTitle('Search');
-		$this->zbxTestCheckHeader('Search: ЗАББИКС Сервер');
-		$this->zbxTestTextPresent(['Hosts', 'Host groups', 'Templates']);
-		$this->zbxTestTextPresent('Displaying 1 of 1 found');
-		$this->zbxTestTextPresent('No data found.');
-		$this->zbxTestTextPresent('ЗАББИКС Сервер');
-		$this->zbxTestTextNotPresent('Zabbix server');
-		$this->zbxTestTextPresent('127.0.0.1');
-		$this->zbxTestTextPresent(['Latest data', 'Triggers', 'Applications', 'Items', 'Triggers', 'Graphs', 'Problems']);
-	}
+
+	use TableTrait;
 
 	/**
 	 * Search for an existing Host and check the results page.
 	 */
-	public function testPageSearch_FindHost() {
+	public function testPageSearch_SearchHost() {
 		$this->openSearchResults('ЗАББИКС Сервер');
+		$title = $this->query('id:page-title-general')->waitUntilVisible()->one()->getText();
+		$this->assertEquals('Search: ЗАББИКС Сервер', $title);
+
+		$expectedTableData = [['Host' => 'ЗАББИКС Сервер'], ['IP' => '127.0.0.1'], ['DNS' => '']];
+
+		$this->verifySearchResultWidget(HOST_WIDGET, $expectedTableData, 1, 1);
+		$this->verifySearchResultWidget(HOST_GROUP_WIDGET, "No data found.", 0, 0);
+		$this->verifySearchResultWidget(TEMPLATE_WIDGET, "No data found.", 0, 0);
 	}
 
 	/**
@@ -51,6 +53,31 @@ class testPageSearch extends CWebTest {
 		$form = $this->query('class:form-search')->asForm()->one()->waitUntilVisible();
 		$form->query('id:search')->one()->fill($searchString);
 		$form->submit();
+	}
+
+	/**
+	 * Asserts that a Search result widget contains the expected values.
+	 *
+	 * @param $widgetParams			array of witget parameters
+	 * @param $expectedTableData	expected table data as an array or a string
+	 * @param $countShown			expected shown count in widget footer
+	 * @param $countTotal			expected total count in widget footer
+	 */
+	private function verifySearchResultWidget($widgetParams, $expectedTableData = null, $countShown = null, $countTotal = null){
+		$this->assertEquals($widgetParams['title'],
+			$this->query('xpath://*[@id="'.$widgetParams['id'].'"]//h4')->one()->getText());
+		if ($expectedTableData) {
+			if(is_array($expectedTableData)){
+				$this->assertTableHasData($expectedTableData,'xpath://div[@id="'.$widgetParams['id'].'"]//table');
+			}else{
+				$tableText = $this->query('xpath://*[@id="'.$widgetParams['id'].'"]//td')->one()->getText();
+				$this->assertEquals($expectedTableData, $tableText);
+			}
+		}
+		if ($countShown !== null && $countTotal !== null) {
+			$footerText = $this->query('xpath://*[@id="'.$widgetParams['id'].'"]//ul[@class="dashbrd-widget-foot"]//li')->one()->getText();
+			$this->assertEquals('Displaying '.$countShown.' of '.$countTotal.' found', $footerText);
+		}
 	}
 
 	public function testPageSearch_FindNotExistingHost() {
