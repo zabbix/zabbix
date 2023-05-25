b1a5d3e032d80cd528fa9c624601d060a7743066
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2023-05-25 10:01:56 +0300

    .......PS. [ZBXNEXT-2732] fixed to use strcmp() when rdns flag is not set

--- src/libs/zbxicmpping/icmpping.c
+++ src/libs/zbxicmpping/icmpping.c
@@ -489,7 +489,8 @@ static int	check_hostip_response(char *resp, ZBX_FPING_HOST *hosts, const int ho
 
 	for (i = 0; i < hosts_count; i++)
 	{
-		if (SUCCEED == zbx_ip_in_list(tmp, hosts[i].addr))
+		if ((0 != rdns && SUCCEED == zbx_ip_in_list(tmp, hosts[i].addr)) ||
+				(0 == rdns && 0 == strcmp(tmp, hosts[i].addr)))
 		{
 			*host = &hosts[i];
 			ret = SUCCEED;
