f543bbf55218f46d47ff7cb9eb7f4a6c26979b1b
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2023-05-25 13:24:16 +0300

    ...G...... [ZBX-22762] applied suggested changes

--- src/go/plugins/vfs/fs/fscaller.go
+++ src/go/plugins/vfs/fs/fscaller.go
@@ -31,22 +31,16 @@ var stuckMounts map[string]int
 var stuckMux sync.Mutex
 
 type fsCaller struct {
-	fsFunc  func(path string) (stats *FsStats, err error)
-	paths   []string
-	errChan chan error
+	fsFunc                func(path string) (stats *FsStats, err error)
+	paths                 []string
+	errChan               chan error
 	outChanStuckUnchecked chan *FsStats
-	outChanStuckChecked chan interface{}
-	p       *Plugin
+	outChanStuckChecked   chan interface{}
+	p                     *Plugin
 }
 
 func (f *fsCaller) executeFunc(path string) {
 	stats, err := f.fsFunc(path)
-
-	defer func() {
-		stuckMux.Lock()
-		stuckMounts[path] = 0
-		stuckMux.Unlock()
-	}()
 
 	if err != nil {
 		f.errChan <- err
@@ -63,46 +57,39 @@ func (f *fsCaller) checkNotStuckAndExecute(path string) {
 		return
 	}
 
+	defer func() {
+		resetStuck(path)
+	}()
+
 	go f.executeFunc(path)
+
+	select {
+	case stat := <-f.outChanStuckUnchecked:
+		f.outChanStuckChecked <- stat
+		return
+	case err := <-f.errChan:
+		f.outChanStuckChecked <- err
+		return
+	case <-time.After(timeout * time.Second):
+		f.outChanStuckChecked <- fmt.Errorf("operation on mount '%s' timed out", path)
+	}
+
+	incStuck(path)
 
 	for {
 		select {
-		case stat := <-f.outChanStuckUnchecked:
-
-			defer func() {
-				stuckMux.Lock()
-				stuckMounts[path] = 0
-				stuckMux.Unlock()
-			}()
-
-			f.outChanStuckChecked <- stat
-
+		case <-f.outChanStuckUnchecked:
 			return
-		case err := <-f.errChan:
-
-			defer func() {
-				stuckMux.Lock()
-				stuckMounts[path] = 0
-				stuckMux.Unlock()
-			}()
-
-			f.outChanStuckChecked <- err
-
+		case <-f.errChan:
 			return
-		case <-time.After(timeout * time.Second):
-			stuckMux.Lock()
-			stuckMounts[path]++
-			stuckMux.Unlock()
-			f.outChanStuckChecked <- fmt.Errorf("operation on mount '%s' timed out", path)
 		}
 	}
 }
 
 func (f *fsCaller) run(path string) (stat *FsStats, err error) {
-
 	go f.checkNotStuckAndExecute(path)
 
-	v := <- f.outChanStuckChecked
+	v := <-f.outChanStuckChecked
 
 	switch d := v.(type) {
 	case *FsStats:
@@ -120,13 +107,24 @@ func isStuck(path string) bool {
 	return stuckMounts[path] > 0
 }
 
+func resetStuck(path string) {
+	stuckMux.Lock()
+	defer stuckMux.Unlock()
+	stuckMounts[path] = 0
+}
+
+func incStuck(path string) {
+	stuckMux.Lock()
+	defer stuckMux.Unlock()
+	stuckMounts[path]++
+}
+
 func (p *Plugin) newFSCaller(fsFunc func(path string) (stats *FsStats, err error), fsLen int) *fsCaller {
 	fc := fsCaller{}
 	fc.fsFunc = fsFunc
 	fc.errChan = make(chan error, fsLen)
 	fc.outChanStuckUnchecked = make(chan *FsStats, fsLen)
-	const stuckCheckedLen = 2
-	fc.outChanStuckChecked = make(chan interface{}, stuckCheckedLen)
+	fc.outChanStuckChecked = make(chan interface{})
 	fc.p = p
 
 	return &fc
