8fda0c411158ef7dd6205a166ac765fa181a40fb
Alexander Shubin <aleksandrs.subins@zabbix.com>
2023-05-29 10:17:12 +0300

    ..F....... [DEV-2299] fixed vertical position of visibility box captions in massupdate forms

--- ui/js/common.js
+++ ui/js/common.js
@@ -1115,6 +1115,7 @@ function visibilityStatusChanges(value, objectid, replace_to) {
 		}
 		else if (!value) {
 			const new_obj = document.createElement('span');
+			new_obj.classList.add('visibility-box-caption');
 			new_obj.setAttribute('name', obj.name);
 			new_obj.setAttribute('id', obj.id);
 			new_obj.innerHTML = replace_to;
