a507c210ed179d2ba655680a7158aab6c88df5f9
Eliza Sekace <eliza.sekace@zabbix.com>
2023-06-02 14:54:05 +0300

    ..F....... [ZBXNEXT-8413] fixed response for media type edit popup when validation failed

--- ui/app/controllers/CControllerMediatypeEdit.php
+++ ui/app/controllers/CControllerMediatypeEdit.php
@@ -35,7 +35,13 @@ class CControllerMediatypeEdit extends CController {
 		$ret = $this->validateInput($fields);
 
 		if (!$ret) {
-			$this->setResponse(new CControllerResponseFatal());
+			$this->setResponse(
+				(new CControllerResponseData(['main_block' => json_encode([
+					'error' => [
+						'messages' => array_column(get_and_clear_messages(), 'message')
+					]
+				])]))->disableView()
+			);
 		}
 
 		return $ret;
