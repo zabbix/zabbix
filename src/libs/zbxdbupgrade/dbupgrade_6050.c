b30d15c6df030361a332932ac8ba89a8427745fd
Diana Dauksa <diana.dauksa@zabbix.com>
2023-06-05 14:57:26 +0300

    ..F.I..... [ZBXNEXT-8331] added updated DB patch and widget module details

--- src/libs/zbxdbupgrade/dbupgrade_6050.c
+++ src/libs/zbxdbupgrade/dbupgrade_6050.c
@@ -246,6 +246,22 @@ static int	DBpatch_6050022(void)
 	return SUCCEED;
 }
 
+static int	DBpatch_6050023(void)
+{
+	zbx_db_insert_t	db_insert;
+
+	if (0 == (DBget_program_type() & ZBX_PROGRAM_TYPE_SERVER))
+		return SUCCEED;
+
+	zbx_db_insert_prepare(&db_insert, "module", "moduleid", "id", "relative_path", "status", "config", NULL);
+	zbx_db_insert_add_values(&db_insert, __UINT64_C(0), "piechart", "widgets/piechart", 1, "[]");
+	zbx_db_insert_autoincrement(&db_insert, "moduleid");
+	int ret = zbx_db_insert_execute(&db_insert);
+	zbx_db_insert_clean(&db_insert);
+
+	return ret;
+}
+
 #endif
 
 DBPATCH_START(6050)
@@ -275,5 +291,6 @@ DBPATCH_ADD(6050019, 0, 1)
 DBPATCH_ADD(6050020, 0, 1)
 DBPATCH_ADD(6050021, 0, 1)
 DBPATCH_ADD(6050022, 0, 1)
+DBPATCH_ADD(6050023, 0, 1)
 
 DBPATCH_END()
