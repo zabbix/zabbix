e9476717336f3a605b2b79a608de4fae5d0f053d
Diana Dauksa <diana.dauksa@zabbix.com>
2023-06-07 15:28:31 +0300

    ..F....... [ZBXNEXT-8331] added widget configuration form, initial JS, widget field value saving and changing in the DB

---
+++ ui/widgets/piechart/includes/WidgetForm.php
@@ -0,0 +1,245 @@
+<?php declare(strict_types = 0);
+/*
+** Zabbix
+** Copyright (C) 2001-2023 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+namespace Widgets\PieChart\Includes;
+
+use CRangeTimeParser,
+	CSettingsHelper;
+
+use Zabbix\Widgets\{
+	CWidgetField,
+	CWidgetForm
+};
+
+use Zabbix\Widgets\Fields\{CWidgetFieldCheckBox,
+	CWidgetFieldColor,
+	CWidgetFieldDatePicker,
+	CWidgetFieldPieChartDataSet,
+	CWidgetFieldIntegerBox,
+	CWidgetFieldRadioButtonList,
+	CWidgetFieldRangeControl,
+	CWidgetFieldTextBox};
+
+/**
+ * Pie chart widget form view.
+ */
+class WidgetForm extends CWidgetForm {
+
+	private bool $chart_time_on = false;
+	private bool $legend_on = true;
+	private bool $show_total_on = false;
+	public function validate(bool $strict = false): array {
+		$errors = parent::validate($strict);
+
+		// Test chart custom time period.
+		if ($this->getFieldValue('chart_time') == PIE_CHART_CUSTOM_TIME_ON) {
+			$errors = array_merge($errors, self::validateTimeSelectorPeriod($this->getFieldValue('time_from'),
+				$this->getFieldValue('time_to')
+			));
+		}
+
+		return $errors;
+	}
+
+	protected function normalizeValues(array $values): array {
+		$values = parent::normalizeValues($values);
+
+		if (array_key_exists('chart_time', $values)) {
+			$this->chart_time_on = $values['chart_time'] == PIE_CHART_CUSTOM_TIME_ON;
+		}
+
+		if (!$this->chart_time_on) {
+			unset($values['time_from'], $values['time_to']);
+		}
+
+		if (array_key_exists('legend', $values)) {
+			$this->legend_on = $values['legend'] == PIE_CHART_LEGEND_ON;
+		}
+
+		return $values;
+	}
+
+	public function addFields(): self {
+		return $this
+			->initDataSetFields()
+			->initDisplayingOptionsFields()
+			->initTimePeriodFields()
+			->initLegendFields();
+	}
+
+	private function initDataSetFields(): self {
+		return $this->addField(
+			(new CWidgetFieldPieChartDataSet('ds', _('Data set')))->setFlags(CWidgetField::FLAG_NOT_EMPTY)
+		);
+	}
+
+	private function initDisplayingOptionsFields(): self {
+		return $this
+			->addField(
+				(new CWidgetFieldRadioButtonList('source', _('History data selection'), [
+					PIE_CHART_DATA_SOURCE_AUTO => _x('Auto', 'history source selection method'),
+					PIE_CHART_DATA_SOURCE_HISTORY => _('History'),
+					PIE_CHART_DATA_SOURCE_TRENDS => _('Trends')
+				]))->setDefault(PIE_CHART_DATA_SOURCE_AUTO)
+			)
+			->addField(
+				(new CWidgetFieldRadioButtonList('draw', _('Draw'), [
+					PIE_CHART_DRAW_PIE => _('Pie'),
+					PIE_CHART_DRAW_DOUGHNUT => _('Doughnut')
+				]))->setDefault(PIE_CHART_DRAW_PIE)
+			)
+			->addField(
+				(new CWidgetFieldRangeControl('width', _('Width'),
+					PIE_CHART_WIDTH_MIN, PIE_CHART_WIDTH_DEFAULT, PIE_CHART_WIDTH_STEP
+				))
+					->setDefault(PIE_CHART_WIDTH_DEFAULT)
+			)
+			->addField(
+				(new CWidgetFieldRangeControl('stroke', _('Stroke width'),
+					PIE_CHART_STROKE_MIN, PIE_CHART_STROKE_MAX
+				))
+					->setDefault(PIE_CHART_STROKE_DEFAULT)
+			)
+			->addField(
+				(new CWidgetFieldRangeControl('sector_space', _('Space between sectors'),
+					PIE_CHART_SECTORS_SPACE_MIN, PIE_CHART_SECTORS_SPACE_MAX
+				))
+					->setDefault(PIE_CHART_SECTORS_SPACE_DEFAULT)
+			)
+			->addField(
+				new CWidgetFieldCheckBox('merge_sectors', null, _('Merge sectors smaller than '))
+			)
+			->addField(
+				(new CWidgetFieldIntegerBox('merge_percentage', null, 1, 100))
+					->setDefault(PIE_CHART_MERGE_PERCENT_DEFAULT)
+			)
+			->addField(
+				(new CWidgetFieldColor('merge_color'))
+			)
+			->addField(
+				(new CWidgetFieldCheckBox('show_total', _('Show total value')))
+			)
+			->addField(
+				(new CWidgetFieldIntegerBox('value_size', _('Size'), 1, 100))
+					->setDefault(PIE_CHART_VALUE_SIZE_DEFAULT)
+			)
+			->addField(
+				(new CWidgetFieldIntegerBox('decimal_places', _('Decimal places'), 1, 6))
+					->setDefault(PIE_CHART_VALUE_DECIMALS_DEFAULT)
+					->setFlags(CWidgetField::FLAG_NOT_EMPTY)
+			)
+			->addField(
+				(new CWidgetFieldCheckBox('units_show', null, _('Units')))
+			)
+			->addField(
+				(new CWidgetFieldTextBox('units_value'))
+					->setFlags(!$this->show_total_on ? CWidgetField::FLAG_DISABLED : 0x00)
+			)
+			->addField(
+				(new CWidgetFieldCheckBox('value_bold', _('Bold')))
+			)
+			->addField(
+				new CWidgetFieldColor('value_color', _('Color'))
+			);
+	}
+
+	private function initTimePeriodFields(): self {
+		return $this
+			->addField(
+				new CWidgetFieldCheckBox('chart_time', _('Set custom time period'))
+			)
+			->addField(
+				(new CWidgetFieldDatePicker('time_from', _('From')))
+					->setDefault('now-1h')
+					->setFlags($this->chart_time_on
+						? CWidgetField::FLAG_NOT_EMPTY
+						: CWidgetField::FLAG_NOT_EMPTY | CWidgetField::FLAG_DISABLED
+					)
+			)
+			->addField(
+				(new CWidgetFieldDatePicker('time_to', _('To')))
+					->setDefault('now')
+					->setFlags($this->chart_time_on
+						? CWidgetField::FLAG_NOT_EMPTY
+						: CWidgetField::FLAG_NOT_EMPTY | CWidgetField::FLAG_DISABLED
+					)
+			);
+	}
+
+	private function initLegendFields(): self {
+		return $this
+			->addField(
+				(new CWidgetFieldCheckBox('legend', _('Show legend')))->setDefault(PIE_CHART_LEGEND_ON)
+			)
+			->addField(
+				(new CWidgetFieldRangeControl('legend_lines', _('Number of rows'),
+					PIE_CHART_LEGEND_LINES_MIN, PIE_CHART_LEGEND_LINES_MAX
+				))
+					->setDefault(PIE_CHART_LEGEND_LINES_MIN)
+					->setFlags(!$this->legend_on ? CWidgetField::FLAG_DISABLED : 0x00)
+			)
+			->addField(
+				(new CWidgetFieldRangeControl('legend_columns', _('Number of columns'),
+					PIE_CHART_LEGEND_COLUMNS_MIN, PIE_CHART_LEGEND_COLUMNS_MAX
+				))
+					->setDefault(PIE_CHART_LEGEND_COLUMNS_MAX)
+					->setFlags(!$this->legend_on ? CWidgetField::FLAG_DISABLED : 0x00)
+			);
+	}
+
+
+	/**
+	 * Check if widget configuration is set to use custom time.
+	 */
+	public static function hasOverrideTime(array $fields_values): bool {
+		return array_key_exists('chart_time', $fields_values)
+			&& $fields_values['chart_time'] == PIE_CHART_CUSTOM_TIME_ON;
+	}
+
+	private static function validateTimeSelectorPeriod(string $from, string $to): array {
+		$errors = [];
+		$ts = [];
+		$ts['now'] = time();
+		$range_time_parser = new CRangeTimeParser();
+
+		foreach (['from' => $from, 'to' => $to] as $field => $value) {
+			$range_time_parser->parse($value);
+			$ts[$field] = $range_time_parser->getDateTime($field === 'from')->getTimestamp();
+		}
+
+		$period = $ts['to'] - $ts['from'] + 1;
+		$range_time_parser->parse('now-'.CSettingsHelper::get(CSettingsHelper::MAX_PERIOD));
+		$max_period = 1 + $ts['now'] - $range_time_parser->getDateTime(true)->getTimestamp();
+
+		if ($period < ZBX_MIN_PERIOD) {
+			$errors[] = _n('Minimum time period to display is %1$s minute.',
+				'Minimum time period to display is %1$s minutes.', (int) (ZBX_MIN_PERIOD / SEC_PER_MIN)
+			);
+		}
+		elseif ($period > $max_period) {
+			$errors[] = _n('Maximum time period to display is %1$s day.',
+				'Maximum time period to display is %1$s days.', (int) round($max_period / SEC_PER_DAY)
+			);
+		}
+
+		return $errors;
+	}
+}
---
+++ ui/widgets/piechart/views/widget.edit.php
@@ -0,0 +1,194 @@
+<?php declare(strict_types = 0);
+/*
+** Zabbix
+** Copyright (C) 2001-2023 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+**/
+
+
+/**
+ * Pie chart widget form view.
+ *
+ * @var CView $this
+ * @var array $data
+ */
+
+use Zabbix\Widgets\Fields\CWidgetFieldPieChartDataSet;
+
+$form = (new CWidgetFormView($data));
+
+$form_tabs = (new CTabView())
+	->addTab('data_set', _('Data set'), getDatasetTab($form, $data['fields']),
+		TAB_INDICATOR_PIE_CHART_DATASET
+	)
+	->addTab('displaying_options', _('Displaying options'), getDisplayOptionsTab($form, $data['fields']),
+		TAB_INDICATOR_PIE_CHART_DISPLAY_OPTIONS
+	)
+	->addTab('time_period', _('Time period'), getTimePeriodTab($form, $data['fields']),
+		TAB_INDICATOR_PIE_CHART_TIME
+	)
+	->addTab('legend_tab', _('Legend'), getLegendTab($form, $data['fields']),
+		TAB_INDICATOR_PIE_CHART_LEGEND
+	)
+	->setSelected(0)
+	->addClass('pie-chart-widget-config-tabs');
+
+$form
+	->addItem($form_tabs)
+	->addJavaScript($form_tabs->makeJavascript())
+	->includeJsFile('widget.edit.js.php')
+	->addJavaScript('widget_piechart_form.init('.json_encode([
+			'form_tabs_id' => $form_tabs->getId(),
+			'color_palette' => CWidgetFieldPieChartDataSet::DEFAULT_COLOR_PALETTE,
+			'templateid' => $data['templateid']
+		], JSON_THROW_ON_ERROR).');')
+	->show();
+
+function getDatasetTab(CWidgetFormView $form, array $fields): array {
+	$dataset = $form->registerField(new CWidgetFieldPieChartDataSetView($fields['ds']));
+
+	return [
+		(new CDiv($dataset->getView()))->addClass(ZBX_STYLE_LIST_VERTICAL_ACCORDION),
+		(new CDiv($dataset->getFooterView()))->addClass(ZBX_STYLE_LIST_ACCORDION_FOOT)
+	];
+}
+
+function getDisplayOptionsTab(CWidgetFormView $form, array $fields): CDiv {
+	$source = $form->registerField(new CWidgetFieldRadioButtonListView($fields['source']));
+	$draw = $form->registerField(new CWidgetFieldRadioButtonListView($fields['draw']));
+	$width = $form->registerField(new CWidgetFieldRangeControlView($fields['width']));
+	$stroke = $form->registerField(new CWidgetFieldRangeControlView($fields['stroke']));
+	$sector_space = $form->registerField(new CWidgetFieldRangeControlView($fields['sector_space']));
+	$merge_sectors = $form->registerField(new CWidgetFieldCheckBoxView($fields['merge_sectors']));
+	$merge_percentage = $form->registerField(new CWidgetFieldIntegerBoxView($fields['merge_percentage']));
+	$merge_color = $form->registerField(new CWidgetFieldColorView($fields['merge_color']));
+	$show_total = $form->registerField(new CWidgetFieldCheckBoxView($fields['show_total']));
+	$value_size = $form->registerField(new CWidgetFieldIntegerBoxView($fields['value_size']));
+	$decimal_places = $form->registerField(new CWidgetFieldIntegerBoxView($fields['decimal_places']));
+	$value_bold = $form->registerField(new CWidgetFieldCheckBoxView($fields['value_bold']));
+	$value_color = $form->registerField(new CWidgetFieldColorView($fields['value_color']));
+	$units_show = $form->registerField(new CWidgetFieldCheckBoxView($fields['units_show']));
+	$units_value = $form->registerField(new CWidgetFieldTextBoxView($fields['units_value']));
+
+	return (new CDiv())
+		->addClass(ZBX_STYLE_GRID_COLUMNS)
+		->addClass(ZBX_STYLE_GRID_COLUMNS_2)
+		->addItem(
+			(new CFormGrid())
+				->addItem([
+					$source->getLabel(),
+					new CFormField($source->getView())
+				])
+				->addItem([
+					$draw->getLabel(),
+					new CFormField($draw->getView())
+				])
+				->addItem([
+					$width->getLabel(),
+					(new CFormField([$width->getView(), ' %']))->addClass('field-size')
+				])
+				->addItem([
+					$stroke->getLabel(),
+					new CFormField($stroke->getView())
+				])
+				->addItem([
+					$sector_space->getLabel(),
+					new CFormField($sector_space->getView())
+				])
+				->addItem([
+					$merge_sectors->getLabel(),
+					(new CFormField([
+						$merge_sectors->getView(), ($merge_percentage->getView())->setWidth(55), ' % ', $merge_color->getView()
+					]))->addClass('field-size')
+				])
+
+		)
+		->addItem(
+			(new CFormGrid())
+				->addItem([
+					$show_total->getLabel(),
+					new CFormField($show_total->getView())
+				])
+				->addItem([
+					$value_size->getLabel(),
+					(new CFormField([$value_size->getView(), ' %']))->addClass('field-size')
+				])
+				->addItem([
+					$decimal_places->getLabel(),
+					new CFormField($decimal_places->getView())
+				])
+				->addItem([
+					$units_show->getView(),
+					(new CFormField(($units_value->getView())->setWidth(ZBX_TEXTAREA_MEDIUM_WIDTH)))
+				])
+				->addItem([
+					$value_bold->getLabel(),
+					new CFormField($value_bold->getView())
+				])
+				->addItem([
+					$value_color->getLabel(),
+					new CFormField($value_color->getView())
+				])
+		);
+}
+
+function getTimePeriodTab(CWidgetFormView $form, array $fields): CFormGrid {
+	$chart_time = $form->registerField(new CWidgetFieldCheckBoxView($fields['chart_time']));
+	$time_from = $form->registerField(
+		(new CWidgetFieldDatePickerView($fields['time_from']))
+			->setDateFormat(ZBX_FULL_DATE_TIME)
+			->setPlaceholder(_('YYYY-MM-DD hh:mm:ss'))
+	);
+	$time_to = $form->registerField(
+		(new CWidgetFieldDatePickerView($fields['time_to']))
+			->setDateFormat(ZBX_FULL_DATE_TIME)
+			->setPlaceholder(_('YYYY-MM-DD hh:mm:ss'))
+	);
+
+	return (new CFormGrid())
+		->addItem([
+			$chart_time->getLabel(),
+			new CFormField($chart_time->getView())
+		])
+		->addItem([
+			$time_from->getLabel(),
+			new CFormField($time_from->getView())
+		])
+		->addItem([
+			$time_to->getLabel(),
+			new CFormField($time_to->getView())
+		]);
+}
+
+function getLegendTab(CWidgetFormView $form, array $fields): CFormGrid {
+	$legend = $form->registerField(new CWidgetFieldCheckBoxView($fields['legend']));
+	$legend_lines = $form->registerField(new CWidgetFieldRangeControlView($fields['legend_lines']));
+	$legend_columns = $form->registerField(new CWidgetFieldRangeControlView($fields['legend_columns']));
+
+	return (new CFormGrid())
+		->addItem([
+			$legend->getLabel(),
+			new CFormField($legend->getView())
+		])
+		->addItem([
+			$legend_lines->getLabel(),
+			new CFormField($legend_lines->getView())
+		])
+		->addItem([
+			$legend_columns->getLabel(),
+			new CFormField($legend_columns->getView())
+		]);
+}
