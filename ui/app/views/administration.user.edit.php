236b709c339c2956b3edb03de7a52bbc1d23a0a0
Alexander Shubin <aleksandrs.subins@zabbix.com>
2023-06-09 11:33:31 +0300

    ..F....... [DEV-2299] fixed media list for user

--- ui/app/views/administration.user.edit.php
+++ ui/app/views/administration.user.edit.php
@@ -354,10 +354,11 @@ if ($data['action'] === 'user.edit' || CWebUser::$data['type'] > USER_TYPE_ZABBI
 				->setEnabled(!$data['readonly']);
 		}
 		else {
-			$media_name = new CDiv([new CSpan($media['name']),
+			$media_name = [
+				new CSpan($media['name']),
 				makeWarningIcon(_('Media type disabled by Administration.'))
-			]);
-			$status = (new CDiv(_('Disabled')))->addClass(ZBX_STYLE_RED);
+			];
+			$status = (new CSpan(_('Disabled')))->addClass(ZBX_STYLE_RED);
 		}
 
 		$parameters = [
