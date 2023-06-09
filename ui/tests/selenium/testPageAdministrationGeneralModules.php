f1a8d788188c162bd60c6ae20318d096ef8257a7
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2023-06-09 18:26:22 +0300

    .......... [ZBX-22243] fixed modules test vol.2

--- ui/tests/selenium/testPageAdministrationGeneralModules.php
+++ ui/tests/selenium/testPageAdministrationGeneralModules.php
@@ -982,7 +982,6 @@ class testPageAdministrationGeneralModules extends CWebTest {
 
 		// Cancel editing dashboard not to interfere with following cases from data provider.
 		$this->query('link:Cancel')->one()->click();
-		$this->page->acceptAlert();
 	}
 
 	/**
