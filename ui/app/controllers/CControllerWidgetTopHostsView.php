b4e23de1b12caa6b413e790b968474a28fadeb96
Andrejs Verza <andrejs.verza@zabbix.com>
2023-06-09 18:01:26 +0300

    ..F....... [ZBX-22369] fixed min/max and threshold calculation with respect to item units

--- ui/app/controllers/CControllerWidgetTopHostsView.php
+++ ui/app/controllers/CControllerWidgetTopHostsView.php
@@ -127,6 +127,18 @@ class CControllerWidgetTopHostsView extends CControllerWidget {
 			$master_hostids[$master_items[$itemid]['hostid']] = true;
 		}
 
+		$number_parser = new CNumberParser([
+			'with_size_suffix' => true,
+			'with_time_suffix' => true,
+			'is_binary_size' => false
+		]);
+
+		$number_parser_binary = new CNumberParser([
+			'with_size_suffix' => true,
+			'with_time_suffix' => true,
+			'is_binary_size' => true
+		]);
+
 		$item_values = [];
 
 		foreach ($configuration as $column_index => &$column) {
@@ -150,36 +162,31 @@ class CControllerWidgetTopHostsView extends CControllerWidget {
 				$column_item_values = self::getItemValues($column_items, $column, $time_now);
 			}
 
-			$is_binary = false;
+			if ($calc_extremes && ($column['min'] !== '' || $column['max'] !== '')) {
+				if ($column['min'] !== '') {
+					$number_parser_binary->parse($column['min']);
+					$column['min_binary'] = $number_parser_binary->calcValue();
 
-			foreach ($column_items as $item) {
-				if (isBinaryUnits($item['units'])) {
-					$is_binary = true;
-					break;
-				}
-			}
-
-			$number_parser = new CNumberParser([
-				'with_size_suffix' => true,
-				'with_time_suffix' => true,
-				'is_binary_size' => $is_binary
-			]);
-
-			if ($calc_extremes) {
-				if ($column['min'] !== '' && $number_parser->parse($column['min']) == CParser::PARSE_SUCCESS) {
+					$number_parser->parse($column['min']);
 					$column['min'] = $number_parser->calcValue();
 				}
 
-				if ($column['max'] !== '' && $number_parser->parse($column['max']) == CParser::PARSE_SUCCESS) {
+				if ($column['max'] !== '') {
+					$number_parser_binary->parse($column['max']);
+					$column['max_binary'] = $number_parser_binary->calcValue();
+
+					$number_parser->parse($column['max']);
 					$column['max'] = $number_parser->calcValue();
 				}
 			}
 
 			if (array_key_exists('thresholds', $column)) {
 				foreach ($column['thresholds'] as &$threshold) {
-					if ($number_parser->parse($threshold['threshold']) == CParser::PARSE_SUCCESS) {
-						$threshold['threshold'] = $number_parser->calcValue();
-					}
+					$number_parser_binary->parse($threshold['threshold']);
+					$threshold['threshold_binary'] = $number_parser_binary->calcValue();
+
+					$number_parser->parse($threshold['threshold']);
+					$threshold['threshold'] = $number_parser->calcValue();
 				}
 				unset($threshold);
 			}
@@ -188,10 +195,12 @@ class CControllerWidgetTopHostsView extends CControllerWidget {
 				if ($calc_extremes) {
 					if ($column['min'] === '') {
 						$column['min'] = $master_items_min;
+						$column['min_binary'] = $column['min'];
 					}
 
 					if ($column['max'] === '') {
 						$column['max'] = $master_items_max;
+						$column['max_binary'] = $column['max'];
 					}
 				}
 			}
@@ -199,10 +208,12 @@ class CControllerWidgetTopHostsView extends CControllerWidget {
 				if ($calc_extremes && $column_item_values) {
 					if ($column['min'] === '') {
 						$column['min'] = min($column_item_values);
+						$column['min_binary'] = $column['min'];
 					}
 
 					if ($column['max'] === '') {
 						$column['max'] = max($column_item_values);
+						$column['max_binary'] = $column['max'];
 					}
 				}
 			}
@@ -213,7 +224,8 @@ class CControllerWidgetTopHostsView extends CControllerWidget {
 				if (array_key_exists($column_items[$itemid]['hostid'], $master_hostids)) {
 					$item_values[$column_index][$column_items[$itemid]['hostid']] = [
 						'value' => $column_item_value,
-						'item' => $column_items[$itemid]
+						'item' => $column_items[$itemid],
+						'is_binary_units' => isBinaryUnits($column_items[$itemid]['units'])
 					];
 				}
 			}
@@ -268,7 +280,8 @@ class CControllerWidgetTopHostsView extends CControllerWidget {
 						$row[] = array_key_exists($hostid, $item_values[$column_index])
 							? [
 								'value' => $item_values[$column_index][$hostid]['value'],
-								'item' => $item_values[$column_index][$hostid]['item']
+								'item' => $item_values[$column_index][$hostid]['item'],
+								'is_binary_units' => $item_values[$column_index][$hostid]['is_binary_units']
 							]
 							: null;
 
