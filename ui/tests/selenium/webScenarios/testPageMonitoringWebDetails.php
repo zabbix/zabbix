032947875d63be992754c088ea626ea2917a8541
Janis Nulle <janis.nulle@zabbix.com>
2023-06-13 16:29:00 +0300

    .......... [DEV-2508] added data insertion for testPageMonitoringWebDetails_DataDisplay - WIP

--- ui/tests/selenium/webScenarios/testPageMonitoringWebDetails.php
+++ ui/tests/selenium/webScenarios/testPageMonitoringWebDetails.php
@@ -22,6 +22,12 @@
 require_once dirname(__FILE__).'/../../include/CWebTest.php';
 require_once dirname(__FILE__).'/../traits/TableTrait.php';
 
+define('HTTPSTEP_ITEM_TYPE_RSPCODE', 0); // Response code.
+define('HTTPSTEP_ITEM_TYPE_TIME', 1); // Response time, in seconds.
+define('HTTPSTEP_ITEM_TYPE_IN', 2); // Download speed, in bytes per second.
+define('HTTPSTEP_ITEM_TYPE_LASTSTEP', 3); // Download speed, in bytes per second.
+define('HTTPSTEP_ITEM_TYPE_LASTERROR', 4); // Download speed, in bytes per second.
+
 /**
  * @backup hosts
  *
@@ -34,6 +40,7 @@ class testPageMonitoringWebDetails extends CWebTest {
 	protected const HOST_NAME = 'Host with web scenarios';
 
 	protected static $host_id;
+	protected static $httptest_id;
 
 	public function prepareData() {
 		$response = CDataHelper::createHosts([
@@ -45,6 +52,34 @@ class testPageMonitoringWebDetails extends CWebTest {
 			]
 		]);
 		self::$host_id = $response['hostids'][self::HOST_NAME];
+
+		$response = CDataHelper::call('httptest.create', [
+			[
+				'name' => 'Web scenario',
+				'hostid' => self::$host_id,
+				'steps' => [
+					[
+						'name' => 'Step 1',
+						'url' => 'http://example.com',
+						'no' => 1
+					]
+				]
+			]
+		]);
+		self::$httptest_id = $response['httptestids'][0];
+	}
+
+	/**
+	 * Test the general layout.
+	 */
+	public function testPageMonitoringWebDetails_Layout() {
+		$this->page->login()->open('httpdetails.php?httptestid='.self::$web_scenario_id)->waitUntilReady();
+
+		// Assert filter.
+		/*$form = $this->query('name:zbx_filter')->asForm()->one();
+		$this->assertEquals(['Host groups', 'Hosts', 'Name', 'Tags', 'Show tags', 'Tag display priority', 'State', 'Show details'],
+			$form->getLabels()->asText()
+		);*/
 	}
 
 	public function getWebScenarioData()
@@ -53,26 +88,27 @@ class testPageMonitoringWebDetails extends CWebTest {
 			[
 				[
 					'name' => 'Many steps',
-					'step_names' => $this->getGeneratedStepNames(50)
+					'steps' => $this->getGeneratedStepNames(50),
+					'item_data' => [HTTPSTEP_ITEM_TYPE_LASTSTEP => 0]
 				]
 			],
 			[
 				[
 					'name' => 'TEST ЗАББИКС !@#$%-() 🙂🙃 <br/> &nbsp;',
-					'step_names' => [
-						'test ēõšŗ тест 测试 テスト δοκιμή',
-						'!@#$%^&*_+\\/()[]{}<>🙂🙃',
-						'<script>window.onload=function(){alert("hi!");}</script>'
+					'steps' => [
+						['name' => 'test ēõšŗ тест 测试 テスト δοκιμή'],
+						['name' => '!@#$%^&*_+\\/()[]{}<>🙂🙃'],
+						['name' => '<script>window.onload=function(){alert("hi!");}</script>']
 					]
 				]
 			],
 			[
 				[
 					'name' => ' 	Test whitespace 	',
-					'step_names' => [
-						'	Tabs	',
-						' Spaces ',
-						"\nNewline\n"
+					'steps' => [
+						['name' => '	Tabs	'],
+						['name' => ' Spaces '],
+						['name' => "\nNewline\n"]
 					]
 				]
 			]
@@ -80,21 +116,19 @@ class testPageMonitoringWebDetails extends CWebTest {
 	}
 
 	/**
-	 * Test the general layout.
+	 * Test the display of data in the table.
 	 *
 	 * @dataProvider getWebScenarioData
 	 */
 	public function testPageMonitoringWebDetails_DataDisplay($data) {
-		// Used for step creation with API.
-		$steps = [];
-
-		// Fill in step data that's not displayed in this page.
-		foreach ($data['step_names'] as $i => $step_name){
-			$step = [];
-			$step['name'] = $step_name;
-			$step['url'] = 'http://example.com';
-			$step['no'] = $i;
-			$steps[] = $step;
+		// Fill in step data so that they can be created with API.
+		$api_steps = [];
+		foreach ($data['steps'] as $i => $step){
+			$api_step = [];
+			$api_step['name'] = $step['name'];
+			$api_step['url'] = 'http://example.com';
+			$api_step['no'] = $i;
+			$api_steps[] = $api_step;
 		}
 
 		// Create a new web scenario.
@@ -102,31 +136,73 @@ class testPageMonitoringWebDetails extends CWebTest {
 			[
 				'name' => $data['name'],
 				'hostid' => self::$host_id,
-				'steps' => $steps
+				'steps' => $api_steps
 			]
 		]);
-		$this->page->login()->open('httpdetails.php?httptestid='.$response['httptestids'][0])->waitUntilReady();
+		$httptest_id = $response['httptestids'][0];
 
-		// Assert page.
-		$this->assertEquals('Details of web scenario: '.trim($data['name']), $this->query('id:page-title-general')->one()->getText());
-		$expected_step_names = [];
+		CTestArrayHelper::get();
 
-		foreach ($steps as $step){
-			$expected_step_names[] = [trim($step['name'])];
+		// Generate item data for the table. For the entire web scenario.
+		foreach ($data['item_data'] as $data_type => $data_value){
+			$sql = 'SELECT ti.itemid FROM httptestitem ti '.
+				'JOIN items i ON ti.itemid=i.itemid '.
+				'WHERE ti.httptestid = '.$httptest_id.' '.
+				'AND ti.type = '.$data_type;
+			$item_id = CDBHelper::getValue($sql);
+			CDataHelper::addItemData($item_id, $data_value);
 		}
 
+		// Generate item data for the table. For each step.
+		foreach ($data['steps'] as $i => $step){
+			// Each step can have different types of data.
+			foreach ($step['item_data'] as $data_type => $data_value){
+				$sql = 'SELECT si.itemid FROM httpstepitem si '.
+						'JOIN httpstep s ON si.httpstepid=s.httpstepid '.
+						'JOIN httptest t ON s.httptestid=t.httptestid '.
+						'WHERE t.httptestid = '.$httptest_id.' '.
+						'AND s.no = '.$i.' '.
+						'AND si.type = '.$data_type;
+				$item_id = CDBHelper::getValue($sql);
+				CDataHelper::addItemData($item_id, $data_value);
+			}
+		}
+
+		$this->page->login()->open('httpdetails.php?httptestid='.$httptest_id)->waitUntilReady();
+
+		// Assert title.
+		$this->assertEquals('Details of web scenario: '.trim($data['name']), $this->query('id:page-title-general')->one()->getText());
+
+		// Assert data table.
+		$expected_steps = [];
+		foreach ($data['steps'] as $step){
+			$expected_steps[] = ['Step' => trim($step['name'])];
+		}
 		// The table contains an additional TOTAL row.
-		$expected_step_names[] = ['TOTAL'];
-		$this->assertTableData($expected_step_names);
+		$expected_steps[] = ['Step' => 'TOTAL'];
+		$this->assertTableData($expected_steps);
+
 	}
 
 	/**
-	 * Generates an array of strings like 'Step-1' with a length of $count.
+	 * Generates an array of steps with a length of $count.
 	 */
 	protected  function getGeneratedStepNames($count) {
 		$result = [];
 		for ($i = 1; $i <= $count; $i++) {
-			$result[] = 'Step-'.$i;
+			$result[] = [
+				'name' => 'Step-'.$i,
+				'item_data' => [
+					HTTPSTEP_ITEM_TYPE_RSPCODE => 200,
+					HTTPSTEP_ITEM_TYPE_TIME => 0.123456,
+					HTTPSTEP_ITEM_TYPE_IN => 3000
+				],
+				'expected_data' => [
+					HTTPSTEP_ITEM_TYPE_RSPCODE => '200',
+					HTTPSTEP_ITEM_TYPE_TIME => '123.46ms',
+					HTTPSTEP_ITEM_TYPE_IN => '3000'
+				]
+			];
 		}
 		return $result;
 	}
