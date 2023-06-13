4d43ac91d92b79c35c667c9f11e135dfb108b55c
Eliza Sekace <eliza.sekace@zabbix.com>
2023-06-13 22:45:52 +0300

    ..F....... [ZBXNEXT-8413] fixed glitch when opening media type configuration form  popup in ios safari

--- ui/app/views/mediatype.edit.php
+++ ui/app/views/mediatype.edit.php
@@ -499,7 +499,8 @@ $form
 			'smtp_server_default' => $email_defaults['smtp_server'],
 			'smtp_email_default' =>  $email_defaults['smtp_email']
 		]).');'))->setOnDocumentReady()
-	);
+	)
+	->addStyle('display: none;');
 
 if ($data['mediatypeid']) {
 	$buttons = [
