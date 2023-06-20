c3aba802378434b41174eb41eaaaedcc52012b5b
Jurijs Klopovskis <jurijs.klopovskis@zabbix.com>
2023-06-20 10:22:00 +0300

    .......... [ZBXNEXT-826] updated release date for 6.4.4rc1

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -26,7 +26,7 @@ import (
 )
 
 const (
-	ZABBIX_REVDATE          = "30 May 2023"
+	ZABBIX_REVDATE          = "20 June 2023"
 	ZABBIX_VERSION_MAJOR    = 6
 	ZABBIX_VERSION_MINOR    = 4
 	ZABBIX_VERSION_PATCH    = 4
