4f893b47cf3d08cf0a95196fa14d500c5395e8ed
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2023-06-21 09:36:12 +0300

    .......... [DEV-2176] fixed Jenkins build

--- src/zabbix_server/server.c
+++ src/zabbix_server/server.c
@@ -268,8 +268,12 @@ static int	get_config_forks(unsigned char process_type)
 ZBX_PROPERTY_DECL2(char *, const char *, zbx_config_source_ip, NULL)
 ZBX_PROPERTY_DECL2(char *, const char *, zbx_config_tmpdir, NULL)
 ZBX_PROPERTY_DECL2(char *, const char *, zbx_config_fping_location, NULL)
+char	*zbx_config_fping6_location = NULL;
 #ifdef HAVE_IPV6
-ZBX_PROPERTY_DECL2(char *, const char *, zbx_config_fping6_location, NULL)
+static const char	*get_zbx_config_fping6_location(void)
+{
+	return zbx_config_fping6_location;
+}
 #endif
 ZBX_PROPERTY_DECL2(char *, const char *, zbx_config_alert_scripts_path, NULL)
 ZBX_PROPERTY_DECL(int, zbx_config_timeout, 3)
