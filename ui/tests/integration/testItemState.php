4191b9952d2da705b3b06daed7ca5282b895cf23
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2023-06-22 16:27:12 +0300

    .......PS. [DEV-2517] added async checks

--- ui/tests/integration/testItemState.php
+++ ui/tests/integration/testItemState.php
@@ -275,7 +275,7 @@ class testItemState extends CIntegrationTest {
 		]);
 
 		$this->assertEquals($state, $response['result'][0]['state'], 'Unexpected item state='.
-				$response['result'][0]['state'].' (expected='.$state.').'.$response['result'][0]
+				$response['result'][0]['state'].' (expected='.$state.').'.$response['result'][0]['error']
 		);
 
 		// Verify item checks intervals
