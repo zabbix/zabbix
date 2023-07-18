f030837da4527301488224c39a86083e504aada8
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2023-07-18 15:19:09 +0300

    .......... [ZBX-23002] updated cmocka tests

--- tests/libs/zbxcommon/convert_to_utf8.c
+++ tests/libs/zbxcommon/convert_to_utf8.c
@@ -19,6 +19,7 @@
 
 #include "zbxmocktest.h"
 #include "zbxmockdata.h"
+#include "zbxmockassert.h"
 #include "zbxmockutil.h"
 #include "zbxmockhelper.h"
 
@@ -26,9 +27,9 @@
 
 void	zbx_mock_test_entry(void **state)
 {
-	int		expected_result;
+	int		expected_result, actual_result;
 	size_t		in_buffer_length, expected_result_buffer_length;
-	char		*in_buffer, *result_buffer, *expected_result_buffer, *err_msg;
+	char		*in_buffer, *result_buffer, *expected_result_buffer, *err_msg = NULL;
 	const char	*encoding;
 
 	ZBX_UNUSED(state);
@@ -43,24 +44,48 @@ void	zbx_mock_test_entry(void **state)
 
 	encoding  = zbx_mock_get_parameter_string("in.encoding");
 
-	if (FAIL == zbx_convert_to_utf8(in_buffer, in_buffer_length, encoding, &result_buffer, &err_msg))
-		fail_msg("%s", err_msg);
+	actual_result = zbx_convert_to_utf8(in_buffer, in_buffer_length, encoding, &result_buffer, &err_msg);
 
-	zbx_free(in_buffer);
+	if (expected_result != actual_result)
+	{
+		if (expected_result_buffer_length != strlen(result_buffer) ||
+				0 != memcmp(result_buffer, expected_result_buffer, expected_result_buffer_length))
+		{
+			zbx_free(expected_result_buffer);
+			zbx_free(result_buffer);
+			zbx_free(in_buffer);
+			zbx_free(err_msg);
 
-	if (expected_result_buffer_length != strlen(result_buffer) ||
-			0 != memcmp(result_buffer, expected_result_buffer, expected_result_buffer_length))
+			if (SUCCEED == expected_result)
+				fail_msg("Expected the same result but there are differences");
+		}
+		else
+		{
+			zbx_free(expected_result_buffer);
+			zbx_free(result_buffer);
+			zbx_free(in_buffer);
+			zbx_free(err_msg);
+
+			fail_msg("Expected result: %d, but got: %d", expected_result, actual_result);
+		}
+	}
+	else if (FAIL == expected_result)
 	{
 		zbx_free(expected_result_buffer);
 		zbx_free(result_buffer);
+		zbx_free(in_buffer);
 
-		if (SUCCEED == expected_result)
-			fail_msg("Expected the same result but there are differences");
+		zbx_mock_assert_ptr_ne("Unexpected error", NULL, err_msg);
+		zbx_mock_assert_str_eq("Unexpected error message",
+				zbx_mock_get_parameter_string("out.error"), err_msg);
+		zbx_free(err_msg);
 	}
 	else
 	{
 		zbx_free(expected_result_buffer);
 		zbx_free(result_buffer);
+		zbx_free(in_buffer);
+		zbx_free(err_msg);
 
 		if (SUCCEED != expected_result)
 			fail_msg("Expected differences but result is the same");
