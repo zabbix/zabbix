43474b50fb22f6c7c57a90c655b805079fa637be
Nikita Gogolevs <nikita.gogolevs@zabbix.com>
2023-07-19 10:10:18 +0300

    .......... [DEV-2490] Changed the filter's label, minor data adjustment

--- ui/tests/selenium/testPageMaintenance.php
+++ ui/tests/selenium/testPageMaintenance.php
@@ -47,7 +47,7 @@ class testPageMaintenance extends CWebTest {
 	protected static $approaching_maintenance = 'Approaching maintenance';
 	protected static $host_maintenance = 'Maintenance with assigned host';
 	protected static $multiple_group_maintenance = 'Maintenance with 2 host groups';
-	protected static $filter_name_maintenance = 'Maintenance для фильтра - $¢Řĩ₱₮';
+	protected static $filter_name_maintenance = 'Maintenance для фильтра - ʍąɨɲţ€ɲąɲȼ€';
 	protected static $active_maintenance = 'Active maintenance';
 
 	public function prepareMaintenanceData() {
@@ -220,7 +220,7 @@ class testPageMaintenance extends CWebTest {
 						'Host group' => ['Zabbix servers', 'Discovered hosts']
 					],
 					[
-						'Name' => 'Maintenance для фильтра - $¢Řĩ₱₮',
+						'Name' => 'Maintenance для фильтра - ʍąɨɲţ€ɲąɲȼ€',
 						'Maintenance type' => 'With data collection',
 						'Active since' => '2023-06-06 00:00',
 						'Active till' => '2023-07-06 00:00',
@@ -278,8 +278,8 @@ class testPageMaintenance extends CWebTest {
 		$form->getLabels()->asText()
 		);
 
-	// Name validation
-		$this->assertEquals(255, $form->getField('id:filter_name')->getAttribute('maxlength'));
+	// Name validation - TODO: DEV-2568
+		$this->assertEquals(255, $form->getField('Name')->getAttribute('maxlength'));
 
 	// State check
 		$this->assertEquals(['Any', 'Active', 'Approaching', 'Expired'], $form->getField('State')->asSegmentedRadio()
@@ -343,7 +343,7 @@ class testPageMaintenance extends CWebTest {
 			[
 				[
 					'filter' => [
-						'id:filter_name' => '  '
+						'Name' => '  '
 					]
 				]
 			],
@@ -351,7 +351,7 @@ class testPageMaintenance extends CWebTest {
 			[
 				[
 					'filter' => [
-						'id:filter_name' => '$¢Řĩ₱₮'
+						'Name' => 'ʍąɨɲţ€ɲąɲȼ€'
 					],
 					'expected' => [
 						self::$filter_name_maintenance
@@ -422,7 +422,7 @@ class testPageMaintenance extends CWebTest {
 			[
 				[
 					'filter' => [
-						'id:filter_name' => 'Host',
+						'Name' => 'Host',
 						'State' => 'Expired',
 						'Host groups' => 'Zabbix servers'
 					],
@@ -450,7 +450,7 @@ class testPageMaintenance extends CWebTest {
 		// Check that expected maintenances are returned in the list.
 		$this->assertTableDataColumn(CTestArrayHelper::get($data, 'expected', []));
 
-		// Check the displaying amount - Tabletrait
+		// Check the displaying amount
 		$maintenance_count = count((CTestArrayHelper::get($data, 'expected', [])));
 		$displayed_amount_text = $this-> query('class:table-stats')->one()->getText();
 		$this->assertEquals('Displaying '.$maintenance_count.' of '.$maintenance_count.' found', $displayed_amount_text);
