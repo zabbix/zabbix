9af3ec577eb6fd27848661a3897eb2a60375215e
Mihails Prihodko <mihails.prihodko@zabbix.com>
2023-07-20 12:02:02 +0300

    .......... [ZBXNEXT-6524] normalized and brushed up cmocka tests

--- tests/libs/zbxdbhigh/zbx_merge_tags.c
+++ tests/libs/zbxdbhigh/zbx_merge_tags.c
@@ -17,180 +17,14 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
+#include "dbhigh_test.h"
+
 #include "zbxmocktest.h"
 #include "zbxmockutil.h"
 #include "zbxmockassert.h"
 
 #include "zbxcommon.h"
 #include "zbxdbhigh.h"
-
-static int	compare_db_tags_and_values(const void *d1, const void *d2)
-{
-	int ret;
-
-	const zbx_db_tag_t *tag1 = *(const zbx_db_tag_t * const *)d1;
-	const zbx_db_tag_t *tag2 = *(const zbx_db_tag_t * const *)d2;
-
-	if (0 == (ret = strcmp(tag1->tag, tag2->tag)))
-		ret = strcmp(tag1->value, tag2->value);
-
-	return ret;
-}
-
-static zbx_uint64_t	get_flags(zbx_mock_handle_t	htag)
-{
-	zbx_mock_handle_t	hflags, hflag;
-	int			flags_num = 1;
-	zbx_uint64_t		flags = ZBX_FLAG_DB_TAG_UNSET;
-	zbx_mock_error_t	err;
-
-	err = zbx_mock_object_member(htag, "flags", &hflags);
-
-	if (ZBX_MOCK_NO_SUCH_MEMBER == err)
-		goto out;
-
-	if (ZBX_MOCK_SUCCESS != err)
-		fail_msg("Cannot read flags '%s'", zbx_mock_error_string(err));
-
-	while (ZBX_MOCK_END_OF_VECTOR != (err = (zbx_mock_vector_element(hflags, &hflag))))
-	{
-		const char	*flag;
-
-		if (ZBX_MOCK_SUCCESS != err || ZBX_MOCK_SUCCESS != (err = zbx_mock_string(hflag, &flag)))
-			fail_msg("Cannot read flag #%d: %s", flags_num, zbx_mock_error_string(err));
-		else if (0 == strcmp(flag, "ZBX_FLAG_DB_TAG_UNSET"))
-			flags |= ZBX_FLAG_DB_TAG_UNSET;
-		else if (0 == strcmp(flag, "ZBX_FLAG_DB_TAG_UPDATE_AUTOMATIC"))
-			flags |= ZBX_FLAG_DB_TAG_UPDATE_AUTOMATIC;
-		else if (0 == strcmp(flag, "ZBX_FLAG_DB_TAG_UPDATE_VALUE"))
-			flags |= ZBX_FLAG_DB_TAG_UPDATE_VALUE;
-		else if (0 == strcmp(flag, "ZBX_FLAG_DB_TAG_UPDATE_TAG"))
-			flags |= ZBX_FLAG_DB_TAG_UPDATE_TAG;
-		else if (0 == strcmp(flag, "ZBX_FLAG_DB_TAG_REMOVE"))
-			flags |= ZBX_FLAG_DB_TAG_REMOVE;
-		else if (0 == strcmp(flag, "ZBX_FLAG_DB_TAG_UPDATE"))
-			flags |= ZBX_FLAG_DB_TAG_UPDATE;
-		else
-			fail_msg("Unknown flag #%d: %s", flags_num, zbx_mock_error_string(err));
-
-		flags_num++;
-	}
-out:
-	return flags;
-}
-
-static int	automatic_str_to_int(const char *str)
-{
-	int	out = -1;
-
-	if (0 == strcmp(str, "ZBX_DB_TAG_NORMAL"))
-		out = ZBX_DB_TAG_NORMAL;
-	else if (0 == strcmp(str, "ZBX_DB_TAG_AUTOMATIC"))
-		out = ZBX_DB_TAG_AUTOMATIC;
-	else
-		fail_msg("Unknown value of 'automatic' \"%s\"", str);
-
-	return out;
-}
-
-static void	read_tags(const char *path, zbx_vector_db_tag_ptr_t *tags)
-{
-	int			i = 0;
-	zbx_mock_handle_t	htag_vector, htag;
-	zbx_mock_error_t	mock_err;
-
-	htag_vector = zbx_mock_get_parameter_handle(path);
-
-	while (ZBX_MOCK_END_OF_VECTOR != (mock_err = (zbx_mock_vector_element(htag_vector, &htag))))
-	{
-		zbx_mock_handle_t	hid, hname, hname_orig, hvalue, hvalue_orig, hauto;
-		zbx_db_tag_t		*db_tag;
-		const char		*tag_str, *tag_orig_str, *value_str, *value_orig_str, *auto_str;
-
-		if (ZBX_MOCK_SUCCESS != mock_err)
-			fail_msg("Cannot read '%s' %s", path, zbx_mock_error_string(mock_err));
-
-		/* tag */
-		if (ZBX_MOCK_SUCCESS != (mock_err = zbx_mock_object_member(htag, "tag", &hname)))
-			fail_msg("Cannot read '%s[%d].tag': %s", path, i, zbx_mock_error_string(mock_err));
-		if (ZBX_MOCK_SUCCESS != (mock_err = zbx_mock_string(hname, &tag_str)))
-			fail_msg("Cannot read '%s[%d].tag': %s", path, i, zbx_mock_error_string(mock_err));
-
-		/* value */
-		if (ZBX_MOCK_SUCCESS != (mock_err = zbx_mock_object_member(htag, "value", &hvalue)))
-			fail_msg("Cannot read '%s[%d].value': %s", path, i, zbx_mock_error_string(mock_err));
-		if (ZBX_MOCK_SUCCESS != (mock_err = zbx_mock_string(hvalue, &value_str)))
-			fail_msg("Cannot read '%s[%d].value': %s", path, i, zbx_mock_error_string(mock_err));
-
-		db_tag = zbx_db_tag_create(tag_str, value_str);
-
-		/* tagid */
-		mock_err = zbx_mock_object_member(htag, "tagid", &hid);
-
-		if (ZBX_MOCK_SUCCESS == mock_err)
-		{
-			if (ZBX_MOCK_SUCCESS != (mock_err = zbx_mock_uint64(hid, &db_tag->tagid)))
-				fail_msg("Cannot read '%s[%d].tagid': %s", path, i, zbx_mock_error_string(mock_err));
-		}
-		else if (ZBX_MOCK_NO_SUCH_MEMBER != mock_err)
-		{
-			fail_msg("Cannot read '%s[%d].tagid': %s", path, i, zbx_mock_error_string(mock_err));
-		}
-
-		/* tag_orig */
-		mock_err = zbx_mock_object_member(htag, "tag_orig", &hname_orig);
-
-		if (ZBX_MOCK_SUCCESS == mock_err)
-		{
-			if (ZBX_MOCK_SUCCESS != (mock_err = zbx_mock_string(hname_orig, &tag_orig_str)))
-				fail_msg("Cannot read '%s[%d].tag_orig': %s", path, i, zbx_mock_error_string(mock_err));
-			/* "~" is the canonical representation of NULL in YAML */
-			else if (0 != strcmp("~", tag_orig_str))
-				db_tag->tag_orig = zbx_strdup(NULL, tag_orig_str);
-		}
-		else if (ZBX_MOCK_NO_SUCH_MEMBER != mock_err)
-			fail_msg("Cannot read '%s[%d].tag_orig': %s", path, i, zbx_mock_error_string(mock_err));
-
-		/* value_orig */
-		mock_err = zbx_mock_object_member(htag, "value_orig", &hvalue_orig);
-
-		if (ZBX_MOCK_SUCCESS == mock_err)
-		{
-			if (ZBX_MOCK_SUCCESS != (mock_err = zbx_mock_string(hvalue_orig, &value_orig_str)))
-			{
-				fail_msg("Cannot read '%s[%d].value_orig': %s", path, i,
-						zbx_mock_error_string(mock_err));
-			}
-			/* "~" is the canonical representation of NULL in YAML */
-			else if (0 != strcmp("~", value_orig_str))
-				db_tag->value_orig = zbx_strdup(NULL, value_orig_str);
-		}
-		else if (ZBX_MOCK_NO_SUCH_MEMBER != mock_err)
-			fail_msg("Cannot read '%s[%d].value_orig': %s", path, i, zbx_mock_error_string(mock_err));
-
-		/* automatic */
-		mock_err = zbx_mock_object_member(htag, "automatic", &hauto);
-
-		if (ZBX_MOCK_SUCCESS == mock_err)
-		{
-			if (ZBX_MOCK_SUCCESS != (mock_err = zbx_mock_string(hauto, &auto_str)))
-			{
-				fail_msg("Cannot read '%s[%d].automatic': %s", path, i,
-						zbx_mock_error_string(mock_err));
-			}
-			else
-				db_tag->automatic = automatic_str_to_int(auto_str);
-		}
-		else if (ZBX_MOCK_NO_SUCH_MEMBER != mock_err)
-			fail_msg("Cannot read '%s[%d].automatic': %s", path, i, zbx_mock_error_string(mock_err));
-
-		/* flags */
-		db_tag->flags = get_flags(htag);
-
-		zbx_vector_db_tag_ptr_append(tags, db_tag);
-		i++;
-	}
-}
 
 void	zbx_mock_test_entry(void **state)
 {
@@ -203,14 +37,14 @@ void	zbx_mock_test_entry(void **state)
 	zbx_vector_db_tag_ptr_create(&src_tags);
 	zbx_vector_db_tag_ptr_create(&expected_dst_tags);
 
-	read_tags("in.dst_tags", &dst_tags);
-	read_tags("in.src_tags", &src_tags);
-	read_tags("out.dst_tags", &expected_dst_tags);
+	tags_read("in.dst_tags", &dst_tags);
+	tags_read("in.src_tags", &src_tags);
+	tags_read("out.dst_tags", &expected_dst_tags);
 
 	(void)zbx_merge_tags(&dst_tags, &src_tags, NULL, NULL);
 
 	zbx_mock_assert_int_eq("Unexpected host tag count", expected_dst_tags.values_num, dst_tags.values_num);
-	zbx_vector_db_tag_ptr_sort(&dst_tags, compare_db_tags_and_values);
+	zbx_vector_db_tag_ptr_sort(&dst_tags, db_tags_and_values_compare);
 
 	for (i = 0; i < dst_tags.values_num; i++)
 	{
@@ -218,19 +52,8 @@ void	zbx_mock_test_entry(void **state)
 				dst_tags.values[i]->tag);
 		zbx_mock_assert_str_eq("Unexpected tag value", expected_dst_tags.values[i]->value,
 				dst_tags.values[i]->value);
-		/*
-		Disabled because order of tags is not guaranteed
-		zbx_mock_assert_str_eq("Unexpected tag name orig", ZBX_NULL2STR(expected_dst_tags.values[i]->tag_orig),
-				ZBX_NULL2STR(dst_tags.values[i]->tag_orig));
-		zbx_mock_assert_str_eq("Unexpected tag value orig", ZBX_NULL2STR(expected_dst_tags.values[i]->value_orig),
-				ZBX_NULL2STR(dst_tags.values[i]->value_orig));
-		*/
 		zbx_mock_assert_int_eq("Unexpected automatic", expected_dst_tags.values[i]->automatic,
 				dst_tags.values[i]->automatic);
-		/*
-		Disabled because order of tags is not guaranteed
-		zbx_mock_assert_uint64_eq("Unexpected tagid", expected_dst_tags.values[i]->tagid,
-				dst_tags.values[i]->tagid); */
 		zbx_mock_assert_uint64_eq("Unexpected flags", expected_dst_tags.values[i]->flags,
 				dst_tags.values[i]->flags);
 
