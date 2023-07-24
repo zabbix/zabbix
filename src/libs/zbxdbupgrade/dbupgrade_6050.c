c72c6ab4b06cf98f57d9a5e344c54f95d5fa2b31
Andris Zeila <andris.zeila@zabbix.com>
2023-07-24 10:20:10 +0300

    .......... [ZBXNEXT-6144] fixed warnings

--- src/libs/zbxdbupgrade/dbupgrade_6050.c
+++ src/libs/zbxdbupgrade/dbupgrade_6050.c
@@ -320,8 +320,6 @@ static int	DBpatch_6050028(void)
 	const zbx_db_field_t	field = {"id", NULL, NULL, NULL, 0, ZBX_TYPE_ID, ZBX_NOTNULL, 0};
 
 	return DBdrop_field_autoincrement("proxy_autoreg_host", &field);
-
-	return SUCCEED;
 }
 
 static int	DBpatch_6050029(void)
