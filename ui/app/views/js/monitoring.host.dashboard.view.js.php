3a4a929a94bfec3beeca16775687de19ed18b1a2
Gregory Chalenko <gregory.chalenko@zabbix.com>
2023-08-02 08:40:35 -0500

    ..F....... [ZBXNEXT-8486] added popup menu support for item form and host form on templated dashboards

--- ui/app/views/js/monitoring.host.dashboard.view.js.php
+++ ui/app/views/js/monitoring.host.dashboard.view.js.php
@@ -100,7 +100,63 @@
 
 			dashboardChange(e) {
 				e.target.closest('form').submit();
+			},
+
+			responseHandler(e) {
+				const data = e.detail;
+
+				if ('success' in data) {
+					postMessageOk(data.success.title);
+
+					if ('messages' in data.success) {
+						postMessageDetails('success', data.success.messages);
+					}
+				}
+				else {
+					postMessageError(data.error.title);
+
+					if ('messages' in data.error) {
+						postMessageDetails('error', data.error.messages);
+					}
+				}
+
+				location.href = location.href;
 			}
+		},
+
+		openItemForm(target, data) {
+			const overlay = PopUp('item.edit', data, {
+				dialogueid: 'item-edit',
+				dialogue_class: 'modal-popup-large',
+				trigger_element: target
+			});
+
+			overlay.$dialogue[0].addEventListener('dialogue.submit',
+				e => this.events.responseHandler({detail: {success: e.detail}})
+			);
+		},
+
+		editHost(hostid) {
+			const host_data = {hostid};
+
+			this.openHostPopup(host_data);
+		},
+
+		openHostPopup(host_data) {
+			const original_url = location.href;
+			const overlay = PopUp('popup.host.edit', host_data, {
+				dialogueid: 'host_edit',
+				dialogue_class: 'modal-popup-large',
+				prevent_navigation: true
+			});
+
+			overlay.$dialogue[0].addEventListener('dialogue.submit',
+				e => this.events.responseHandler({detail: {success: e.detail}})
+			);
+			overlay.$dialogue[0].addEventListener('dialogue.create', this.events.responseHandler);
+			overlay.$dialogue[0].addEventListener('dialogue.update', this.events.responseHandler);
+			overlay.$dialogue[0].addEventListener('dialogue.delete', this.events.responseHandler);
+			overlay.$dialogue[0].addEventListener('overlay.close', () => history.replaceState({}, '', original_url));
 		}
 	}
 </script>
