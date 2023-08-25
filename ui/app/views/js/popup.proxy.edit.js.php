013b067547f4c24bbe56c182a1be790bb50478b1
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2023-08-25 17:28:59 +0300

    ..F....... [ZBXNEXT-1096,ZBXNEXT-8468] fixed proxy per-item-type timeouts not being trimmed on form submit

--- ui/app/views/js/popup.proxy.edit.js.php
+++ ui/app/views/js/popup.proxy.edit.js.php
@@ -197,8 +197,10 @@ window.proxy_edit_popup = new class {
 			fields.clone_psk = false;
 		}
 
-		for (const name of ['name', 'allowed_addresses', 'address', 'port', 'description', 'tls_psk_identity', 'tls_psk',
-				'tls_issuer', 'tls_subject']) {
+		for (const name of ['name', 'allowed_addresses', 'address', 'port', 'description', 'tls_psk_identity',
+				'tls_psk', 'tls_issuer', 'tls_subject', 'timeout_zabbix_agent', 'timeout_simple_check',
+				'timeout_snmp_agent', 'timeout_external_check', 'timeout_db_monitor', 'timeout_http_agent',
+				'timeout_ssh_agent', 'timeout_telnet_agent', 'timeout_script']) {
 			if (name in fields) {
 				fields[name] = fields[name].trim();
 			}
