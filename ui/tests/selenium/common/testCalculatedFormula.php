6ac589d5db864de72cbe315f608ee834c2fdde37
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2023-08-30 10:24:44 +0300

    ..F....... [ZBX-22818] fixed some aggregation functions not working with last_foreach()

--- ui/tests/selenium/common/testCalculatedFormula.php
+++ ui/tests/selenium/common/testCalculatedFormula.php
@@ -2719,6 +2719,11 @@ class testCalculatedFormula extends CWebTest {
 			// foreach() aggregated functions.
 			[
 				[
+					'formula' => 'mad(last_foreach(/*/trap))'
+				]
+			],
+			[
+				[
 					'formula' => 'sum(last_foreach(/*/trap))'
 				]
 			],
@@ -2919,7 +2924,7 @@ class testCalculatedFormula extends CWebTest {
 				[
 					'expected' => TEST_BAD,
 					'formula' => 'mad()',
-					'error' => 'Invalid parameter "/1/params": incorrect usage of function "mad".'
+					'error' => 'Invalid parameter "/1/params": invalid number of parameters in function "mad".'
 				]
 			],
 			[
