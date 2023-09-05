2e5f81ca19acb0b909d626d963ec19eb03c663cb
Diana Dauksa <diana.dauksa@zabbix.com>
2023-09-05 17:02:01 +0300

    ..F....... [ZBX-22433] updated 'Variables', 'Headers', 'Posts' value field max length to respective DB field's length

--- ui/include/views/configuration.httpconf.edit.php
+++ ui/include/views/configuration.httpconf.edit.php
@@ -151,7 +151,7 @@ $scenario_tab
 						(new CTextAreaFlexible('variables[#{rowNum}][value]', '#{value}', ['add_post_js' => false]))
 							->removeId()
 							->setWidth(ZBX_TEXTAREA_HTTP_PAIR_VALUE_WIDTH)
-							->setMaxlength(2000)
+							->setMaxlength(DB::getFieldLength('httpstep_field', 'value'))
 							->setAttribute('placeholder', _('value'))
 							->disableSpellcheck(),
 						(new CCol(
@@ -192,7 +192,7 @@ $scenario_tab
 						(new CTextAreaFlexible('headers[#{rowNum}][value]', '#{value}', ['add_post_js' => false]))
 							->removeId()
 							->setWidth(ZBX_TEXTAREA_HTTP_PAIR_VALUE_WIDTH)
-							->setMaxlength(2000)
+							->setMaxlength(DB::getFieldLength('httpstep_field', 'value'))
 							->setAttribute('placeholder', _('value'))
 							->disableSpellcheck(),
 						(new CCol(
