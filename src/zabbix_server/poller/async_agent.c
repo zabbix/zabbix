63f78091760db7940a09a75cb52c542150a2a1af
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2023-09-07 07:06:12 -0400

    .......PS. [ZBXNEXT-1096] fixed timeout for zbx_tcp_send_context_init

--- src/zabbix_server/poller/async_agent.c
+++ src/zabbix_server/poller/async_agent.c
@@ -285,7 +285,7 @@ int	zbx_async_check_agent(zbx_dc_item_t *item, AGENT_RESULT *result,  zbx_async_
 		agent_context->server_name = NULL;
 #endif
 	zbx_socket_clean(&agent_context->s);
-	zbx_tcp_send_context_init(agent_context->item.key, strlen(agent_context->item.key), timeout_sec + 1, ZBX_TCP_PROTOCOL,
+	zbx_tcp_send_context_init(agent_context->item.key, strlen(agent_context->item.key), timeout_sec, ZBX_TCP_PROTOCOL,
 		&agent_context->tcp_send_context);
 	agent_context->step = ZABBIX_AGENT_STEP_CONNECT_WAIT;
 
