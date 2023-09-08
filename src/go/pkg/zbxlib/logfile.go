033de2ef59de2f2d6b4f46891ce7a6cf03751a03
Andris Zeila <andris.zeila@zabbix.com>
2023-09-08 16:03:38 +0300

    ...G...... [DEV-2522] added fatal signal handler with backtrace logging for process_log_check() call from Go

--- src/go/pkg/zbxlib/logfile.go
+++ src/go/pkg/zbxlib/logfile.go
@@ -27,6 +27,7 @@ package zbxlib
 #include "../src/zabbix_agent/metrics.h"
 #include "../src/zabbix_agent/logfiles/logfiles.h"
 #include "zbx_item_constants.h"
+#include "../src/libs/zbxnix/fatal.h"
 
 typedef ZBX_ACTIVE_METRIC* ZBX_ACTIVE_METRIC_LP;
 typedef zbx_vector_ptr_t * zbx_vector_ptr_lp_t;
@@ -196,22 +197,48 @@ int	process_value_cb(zbx_vector_addr_ptr_t *addrs, zbx_vector_ptr_t *agent2_resu
 	return SUCCEED;
 }
 
-static zbx_vector_pre_persistent_lp_t new_prep_vec(void)
+#if !defined(__MINGW32__)
+
+static ZBX_THREAD_LOCAL struct sigaction sa_old;
+
+static void	fatal_signal_handler(int sig, siginfo_t *siginfo, void *context)
 {
-	zbx_vector_pre_persistent_lp_t vect;
+	zbx_log_fatal_info(context, ZBX_FATAL_LOG_FULL_INFO);
 
-	vect = (zbx_vector_pre_persistent_lp_t)zbx_malloc(NULL, sizeof(zbx_vector_pre_persistent_t));
-	zbx_vector_pre_persistent_create(vect);
-
-	return vect;
+	sigaction(SIGSEGV, &sa_old, NULL);
+	raise(sig);
 }
+#endif
 
-static void free_prep_vec(zbx_vector_pre_persistent_lp_t vect)
+static int	invoke_process_log_check(zbx_vector_ptr_t *agent2_result, zbx_vector_expression_t *regexps,
+		ZBX_ACTIVE_METRIC *metric, zbx_process_value_func_t process_value_cb, zbx_uint64_t *lastlogsize_sent,
+		int *mtime_sent, char **error, const zbx_config_tls_t *config_tls, int config_timeout,
+		const char *config_source_ip, const char *config_hostname, zbx_uint64_t itemid,
+		int config_buffer_send, int config_buffer_size, int config_max_lines_per_second)
 {
-	// In Agent2 this vector is expected to be empty because 'persistent directory' parameter is not allowed.
-	// Therefore a simplified cleanup is used.
-	zbx_vector_pre_persistent_destroy(vect);
-	zbx_free(vect);
+	int	ret;
+	zbx_vector_pre_persistent_t	vect;
+
+#if !defined(__MINGW32__)
+	struct sigaction	sa_new;
+	sigemptyset(&sa_new.sa_mask);
+	sa_new.sa_flags = SA_SIGINFO;
+	sa_new.sa_sigaction = fatal_signal_handler;
+	sigaction(SIGSEGV, &sa_new, &sa_old);
+#endif
+
+	zbx_vector_pre_persistent_create(&vect);
+
+	ret = process_log_check(NULL, agent2_result, regexps, metric, process_value_cb, lastlogsize_sent, mtime_sent, error,
+		&vect, config_tls, config_timeout, config_source_ip, config_hostname, itemid, config_buffer_send,
+		config_buffer_size, config_max_lines_per_second);
+
+#if !defined(__MINGW32__)
+	sigaction(SIGSEGV, &sa_old, NULL);
+#endif
+
+	zbx_vector_pre_persistent_destroy(&vect);
+	return ret;
 }
 
 void	zbx_config_tls_init_for_agent2(zbx_config_tls_t *config_tls, unsigned int accept, unsigned int connect,
@@ -398,13 +425,6 @@ func ProcessLogCheck(data unsafe.Pointer, item *LogItem, refresh int, cblob unsa
 	}
 
 	var cerrmsg *C.char
-	log.Tracef("Calling C function \"new_prep_vec()\"")
-	cprepVec := C.new_prep_vec() // In Agent2 it is always empty vector. Not used but required for linking.
-
-	defer func() {
-		log.Tracef("Calling C function \"free_prep_vec()\"")
-		C.free_prep_vec(cprepVec)
-	}()
 
 	cSourceIP := (C.CString)(agent.Options.SourceIP)
 	cHostname := (C.CString)(agent.Options.Hostname)
@@ -416,11 +436,11 @@ func ProcessLogCheck(data unsafe.Pointer, item *LogItem, refresh int, cblob unsa
 		C.free(unsafe.Pointer(cHostname))
 	}()
 
-	log.Tracef("Calling C function \"process_log_check()\"")
-	ret := C.process_log_check(nil, C.zbx_vector_ptr_lp_t(unsafe.Pointer(result)),
+	log.Tracef("Calling C function \"invoke_process_log_check()\"")
+	ret := C.invoke_process_log_check(C.zbx_vector_ptr_lp_t(unsafe.Pointer(result)),
 		C.zbx_vector_expression_lp_t(cblob), C.ZBX_ACTIVE_METRIC_LP(data),
 		C.zbx_process_value_func_t(C.process_value_cb), &clastLogsizeSent,
-		&cmtimeSent, &cerrmsg, cprepVec, ctlsConfig_p, (C.int)(agent.Options.Timeout),
+		&cmtimeSent, &cerrmsg, ctlsConfig_p, (C.int)(agent.Options.Timeout),
 		cSourceIP, cHostname, C.zbx_uint64_t(itemid), (C.int)(agent.Options.BufferSend),
 		(C.int)(agent.Options.BufferSize), (C.zbx_config_max_lines_per_second))
 
