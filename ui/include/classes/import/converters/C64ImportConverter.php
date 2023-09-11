2112ffe6307f1d809e047d2c45a3b1ce6929156e
Andrejs Verza <andrejs.verza@zabbix.com>
2023-09-11 16:02:38 +0300

    ..F....... [ZBXNEXT-8145] updated import converter

--- ui/include/classes/import/converters/C64ImportConverter.php
+++ ui/include/classes/import/converters/C64ImportConverter.php
@@ -34,6 +34,101 @@ class C64ImportConverter extends CConverter {
 	public function convert(array $data): array {
 		$data['zabbix_export']['version'] = '7.0';
 
+		if (array_key_exists('templates', $data['zabbix_export'])) {
+			$data['zabbix_export']['templates'] = self::convertTemplates($data['zabbix_export']['templates']);
+		}
+
 		return $data;
+	}
+
+	/**
+	 * Convert templates.
+	 *
+	 * @param array $templates
+	 *
+	 * @return array
+	 */
+	private static function convertTemplates(array $templates): array {
+		foreach ($templates as &$template) {
+			if (array_key_exists('dashboards', $template)) {
+				$template['dashboards'] = self::convertDashboards($template['dashboards']);
+			}
+		}
+		unset($template);
+
+		return $templates;
+	}
+
+	/**
+	 * Convert dashboards.
+	 *
+	 * @param array $dashboards
+	 *
+	 * @return array
+	 */
+	private static function convertDashboards(array $dashboards): array {
+		foreach ($dashboards as &$dashboard) {
+			if (!array_key_exists('pages', $dashboard)) {
+				continue;
+			}
+
+			$reference_index = 0;
+
+			foreach ($dashboard['pages'] as &$dashboard_page) {
+				if (!array_key_exists('widgets', $dashboard_page)) {
+					continue;
+				}
+
+				foreach ($dashboard_page['widgets'] as &$widget) {
+					if (in_array($widget['type'], ['graph', 'svggraph', 'graphprototype'])) {
+						$reference = self::createWidgetReference($reference_index++);
+
+						if (!array_key_exists('fields', $widget)) {
+							$widget['fields'] = [];
+						}
+
+						$widget['fields'][] = [
+							'type' => 'STRING',
+							'name' => 'reference',
+							'value' => $reference
+						];
+
+						usort($widget['fields'],
+							static function(array $widget_field_a, array $widget_field_b): int {
+								return strnatcasecmp($widget_field_a['name'], $widget_field_b['name']);
+							}
+						);
+					}
+				}
+				unset($widget);
+			}
+			unset($dashboard_page);
+		}
+		unset($dashboard);
+
+		return $dashboards;
+	}
+
+	/**
+	 * Create a unique widget reference (required for broadcasting widgets).
+	 *
+	 * @param int $index  Unique reference index
+	 *
+	 * @return string
+	 */
+	private static function createWidgetReference(int $index): string {
+		$pow_base = 26;
+
+		$num_reference_ABCDE = ($pow_base ** 3) + ($pow_base ** 2) * 2 + ($pow_base ** 1) * 3 + 4;
+		$num_reference = $num_reference_ABCDE + $index;
+
+		$reference = '';
+
+		for ($i = 0; $i < 5; $i++) {
+			$reference = chr(65 + $num_reference % $pow_base).$reference;
+			$num_reference = floor($num_reference / $pow_base);
+		}
+
+		return $reference;
 	}
 }
