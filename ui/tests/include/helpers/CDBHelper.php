31d20d314b6507fe8f7dc95e70551d1ee10c0dbc
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2023-09-18 13:40:50 +0000

    .......... [ZBXNEXT-686] made PostgreSQL database restorind consecutively
    (cherry picked from commit b9251d380c865a249ff7e973cfe897eb6c711dba)
    (cherry picked from commit 144290888fb422ad5e43bfcb2d2c9b64d5c0cb69)
    (cherry picked from commit a05c4845d85fd5eb3d70f0bb17b18f5b43b5090f)
    (cherry picked from commit 25c27c72ea92826068a03764f8863e96d5565de6)

--- ui/tests/include/helpers/CDBHelper.php
+++ ui/tests/include/helpers/CDBHelper.php
@@ -360,7 +360,7 @@ class CDBHelper {
 			$cmd .= $port;
 
 			$file = PHPUNIT_COMPONENT_DIR.$DB['DATABASE'].$suffix.'.dump';
-			$cmd .= ' --username='.$DB['USER'].' --format=d --single-transaction --clean --dbname='.$DB['DATABASE'];
+			$cmd .= ' --username='.$DB['USER'].' --format=d --jobs=1 --clean --dbname='.$DB['DATABASE'];
 			$cmd .= ' '.$file;
 
 			if (self::$db_extension  == ZBX_DB_EXTENSION_TIMESCALEDB) {
