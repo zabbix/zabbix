d326f57356e52932c8273d63bc5eb08bf0c5cf25
Dace Petra <dace.petra@zabbix.com>
2023-09-22 13:37:10 +0300

    ..F....... [ZBX-22666] fixed Geomap widget disabled filter in dashboard edit mode

--- ui/js/class.geomaps.js
+++ ui/js/class.geomaps.js
@@ -66,9 +66,9 @@ L.Control.severityFilterFilterControl = L.Control.extend({
 	},
 
 	onAdd: function(map) {
-		this.div = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
-		const btn = L.DomUtil.create('a', 'geomap-filter-button', this.div);
-		this.bar = L.DomUtil.create('ul', 'checkbox-list geomap-filter', this.div);
+		this._geomap_filter_div = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
+
+		const btn = L.DomUtil.create('a', 'geomap-filter-button', this._geomap_filter_div);
 
 		btn.ariaLabel = t('Severity filter');
 		btn.title = t('Severity filter');
@@ -76,6 +76,8 @@ L.Control.severityFilterFilterControl = L.Control.extend({
 		btn.href = '#';
 
 		if (!this._disabled) {
+			this.bar = L.DomUtil.create('ul', 'checkbox-list geomap-filter', this._geomap_filter_div);
+
 			for (const [severity, prop] of this._severity_levels) {
 				const li = L.DomUtil.create('li', '', this.bar);
 				const chbox = L.DomUtil.create('input', '', li);
@@ -93,22 +95,26 @@ L.Control.severityFilterFilterControl = L.Control.extend({
 			}
 
 			L.DomEvent.on(btn, 'click', () => {
-				if (!this.div.classList.contains('disabled')) {
+				if (!this._geomap_filter_div.classList.contains('disabled')) {
 					this.bar.classList.toggle('collapsed')
 				}
 			});
 			L.DomEvent.on(this.bar, 'dblclick', (e) => {L.DomEvent.stopPropagation(e)});
-			L.DomEvent.on(this.div, 'change', () => {
+			L.DomEvent.on(this._geomap_filter_div, 'change', () => {
 				map.updateFilter([...this.bar.querySelectorAll('input[type="checkbox"]:checked')].map(n => n.value));
 			});
 		}
 		else {
-			this.div.classList.add('disabled');
+			this._geomap_filter_div.classList.add('disabled');
 		}
 
 		L.DomEvent.on(btn, 'dblclick', (e) => {L.DomEvent.stopPropagation(e)});
 
-		return this.div;
+		return this._geomap_filter_div;
+	},
+
+	disable: function() {
+		this._geomap_filter_div.classList.add('disabled');
 	},
 
 	close: function() {
