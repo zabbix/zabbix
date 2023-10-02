8cedefa8bc64307396da4c02803b4fe92190168c
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2023-10-02 17:30:18 +0300

    ..F....... [ZBX-22055] fixed no permissions message not being displayed if there is no map navigation tree widget refenced by map widget

--- ui/js/widgets/class.widget.map.js
+++ ui/js/widgets/class.widget.map.js
@@ -124,7 +124,9 @@ class CWidgetMap extends CWidget {
 
 	_promiseUpdate() {
 		if (!this._has_contents || this._map_svg === null) {
-			if (this._sysmapid !== null || this._source_type == WIDGET_SYSMAP_SOURCETYPE_MAP) {
+			if (this._sysmapid !== null
+					|| this._source_type == WIDGET_SYSMAP_SOURCETYPE_MAP
+					|| this._filter_widget === null) {
 				return super._promiseUpdate();
 			}
 
