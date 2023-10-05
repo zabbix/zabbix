f6213ea0326fa2fbed8b07c7a165ff0d6583a728
Janis Nulle <janis.nulle@zabbix.com>
2023-10-05 13:26:55 +0000

    .......... [DEV-2640] updated testDashboardPieChartWidget_Create - work in progress

--- ui/tests/selenium/dashboard/testDashboardPieChartWidget.php
+++ ui/tests/selenium/dashboard/testDashboardPieChartWidget.php
@@ -19,15 +19,25 @@
 **/
 
 require_once dirname(__FILE__).'/../../include/CWebTest.php';
+require_once dirname(__FILE__).'/../behaviors/CMessageBehavior.php';
 
 /**
- * @backup widget, profiles
+ * @backup hosts, widget, profiles
  *
  * @onBefore prepareData
  */
 class testDashboardPieChartWidget extends CWebTest
 {
 	protected static $dashboardid;
+
+	/**
+	 * Attach MessageBehavior to the test.
+	 *
+	 * @return array
+	 */
+	public function getBehaviors() {
+		return [CMessageBehavior::class];
+	}
 
 	/**
 	 * Create the needed initial data in database and set static variables.
@@ -49,16 +59,46 @@ class testDashboardPieChartWidget extends CWebTest
 
 	public function getCreateData() {
 		return [
+			// Missing host pattern.
 			[
 				[
-					'expected' => TEST_BAD,
+					'fields' => [
+						'Data set' => ['item' => '*']
+					],
+					'result' => TEST_BAD,
+					'error' => 'Invalid parameter "Data set/1/hosts": cannot be empty.'
+				]
+			],
+			// Missing item pattern.
+			[
+				[
+					'fields' => [
+						'Data set' => ['host' => '*']
+					],
+					'result' => TEST_BAD,
+					'error' => 'Invalid parameter "Data set/1/items": cannot be empty.'
+				]
+			],
+			// Minimum required fields.
+			[
+				[
 					'fields' => [
 						'Data set' => [
-							'host' => '*',
-							'item' => '*'
+							'host' => 'test host',
+							'item' => 'test item'
 						]
-					],
-					'error' => 'Invalid parameter "Item": cannot be empty.'
+					]
+				]
+			],
+			// Minimum required.
+			[
+				[
+					'fields' => [
+						'Data set' => [
+							'host' => 'test host',
+							'item' => 'test item'
+						]
+					]
 				]
 			],
 		];
@@ -74,24 +114,90 @@ class testDashboardPieChartWidget extends CWebTest
 
 		$dashboard = CDashboardElement::find()->one();
 		$old_widget_count = $dashboard->getWidgets()->count();
-
 		$form = $dashboard->edit()->addWidget()->asForm();
 
+		// Fill data and submit.
 		$this->fillForm($data['fields'], $form);
 		$form->submit();
 
+		$test_good_expected = (CTestArrayHelper::get($data, 'result', TEST_GOOD) === TEST_GOOD);
+
+		if ($test_good_expected) {
+			COverlayDialogElement::ensureNotPresent();
+
+			// Save Dashboard.
+			$widget = $dashboard->getWidget($this->calculateWidgetName($data['fields']));
+			$this->waitForWidgetToLoad($widget);
+			$dashboard->save();
+
+			// Assert successful save.
+			$message = CMessageElement::find()->waitUntilPresent()->one();
+			$this->assertTrue($message->isGood());
+			$this->assertEquals('Dashboard updated', $message->getTitle());
+
+			// Assert data in edit form.
+			$form = $widget->edit();
+			$this->checkForm($data['fields'], $form);
+		}
+		else {
+			// Assert error message.
+			$message = CMessageElement::find()->waitUntilPresent()->one();
+			$this->assertTrue($message->isBad());
+			$this->assertEquals($data['error'], $message->getLines()->get(0)->getText());
+		}
+
+		// Check total Widget count.
+		$this->assertEquals($old_widget_count + ($test_good_expected ? 1 : 0), $dashboard->getWidgets()->count());
+	}
+
+	/**
+	 * Waits for a widget to stop loading and to show the pie chart.
+	 *
+	 * @param CWidgetElement $widget    widget element to wait
+	 */
+	protected function waitForWidgetToLoad($widget) {
+		$widget->query('xpath://div[contains(@class, "is-loading")]')->waitUntilNotPresent();
+		$widget->getContent()->query('class:svg-pie-chart')->waitUntilVisible();
+	}
+
+	/**
+	 * Calculates widget name from field data in data provider.
+	 * If no name is provided, then use an MD5 as the name so that it is unique.
+	 *
+	 * @param array $fields    field data for calculating the widget name
+	 */
+	protected function calculateWidgetName($fields) {
+		return (array_key_exists('main_fields', $fields) && array_key_exists('Name', $fields['main_fields']))
+				? $fields['main_fields']['Name']
+				: md5(serialize($fields));
+	}
+
+	/**
+	 * Check Pie chart widget form contains the provided data.
+	 *
+	 * @param array $fields         field data to check
+	 * @param CFormElement $form    form to be checked
+	 */
+	protected function checkForm($fields, $form) {
+		// ToDo: write the check function.
+		$form->checkValue(['Name' => 'abd9ac388db7de8a01a1f38d8db257f8']);
 	}
 
 	/**
 	 * Fill Pie chart widget form with provided data.
 	 *
 	 * @param array $fields         field data to fill
-	 * @param CFormElement $form    CFormElement to be filled
+	 * @param CFormElement $form    form to be filled
 	 */
 	protected function fillForm($fields, $form) {
-		$form->fill(CTestArrayHelper::get($fields, 'main_fields', []));
+		// Fill main fields.
+		$main_fields['Name'] = $this->calculateWidgetName($fields);
+		$form->fill($main_fields);
+
+		// Fill datasets.
 		$this->fillDatasets(CTestArrayHelper::get($fields, 'Data set', []), $form);
 
+		// Fill the other tabs.
 		$tabs = ['Displaying options', 'Time period', 'Legend'];
 
 		foreach ($tabs as $tab) {
@@ -121,13 +227,13 @@ class testDashboardPieChartWidget extends CWebTest
 		$count_sets = $form->query('xpath://li[contains(@class, "list-accordion-item")]')->all()->count();
 
 		foreach ($data_sets as $i => $data_set) {
-			$mapping = [
+			$selectors = [
 				'host' => 'xpath://div[@id="ds_'.$i.'_hosts_"]/..',
 				'item' => 'xpath://div[@id="ds_'.$i.'_items_"]/..'
 			];
 
-			// If host or item of data set exist in data provider, add the xpath selector and value from data provider to them.
-			foreach ($mapping as $field => $selector) {
+			// Exchange 'host' and 'item' keys for the actual selector the fields have.
+			foreach ($selectors as $field => $selector) {
 				if (array_key_exists($field, $data_set)) {
 					$data_set = [$selector => $data_set[$field]] + $data_set;
 					unset($data_set[$field]);
@@ -136,7 +242,7 @@ class testDashboardPieChartWidget extends CWebTest
 
 			$form->fill($data_set);
 
-			// Open next dataset if needed. Either create new or open existing.
+			// Open next dataset if needed. Either create a new one or open an existing one.
 			if ($i !== $last) {
 				if ($i + 1 < $count_sets) {
 					$i += 2;
