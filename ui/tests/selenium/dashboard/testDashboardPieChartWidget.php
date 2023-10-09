c30aba070b87067c3d57da90a8b163726bbdf797
Janis Nulle <janis.nulle@zabbix.com>
2023-10-09 12:29:25 +0000

    .......... [DEV-2640] updated testDashboardPieChartWidget_Create - work in progress

--- ui/tests/selenium/dashboard/testDashboardPieChartWidget.php
+++ ui/tests/selenium/dashboard/testDashboardPieChartWidget.php
@@ -59,7 +59,7 @@ class testDashboardPieChartWidget extends CWebTest
 
 	public function getCreateData() {
 		return [
-			// Missing host pattern.
+			// Missing Host pattern.
 			[
 				[
 					'fields' => [
@@ -69,7 +69,7 @@ class testDashboardPieChartWidget extends CWebTest
 					'error' => 'Invalid parameter "Data set/1/hosts": cannot be empty.'
 				]
 			],
-			// Missing item pattern.
+			// Missing Item pattern.
 			[
 				[
 					'fields' => [
@@ -79,41 +79,186 @@ class testDashboardPieChartWidget extends CWebTest
 					'error' => 'Invalid parameter "Data set/1/items": cannot be empty.'
 				]
 			],
-			// Minimum required fields.
+			// Mandatory fields only.
 			[
 				[
-					'fields' => [
-						'Data set' => [
-							'host' => 'test host',
-							'item' => 'test item'
-						]
-					]
+					'fields' => []
 				]
 			],
-			// All possible fields.
+			// Largest number of fields possible.
 			[
 				[
 					'fields' => [
+						'main_fields' => [
+							'Name' => 'Test all possible fields',
+							'Show header' => false,
+							'Refresh interval' => '30 seconds'
+						],
 						'Data set' => [
 							[
-								'host' => 'test host',
-								'item' => 'test item',
+								'host' => ['Host*', 'one', 'two'],
+								'item' => ['Item*', 'one', 'two'],
 								'Aggregation function' => 'min',
 								'Data set aggregation' => 'max',
-								'Data set label' => 'Label 123'
+								'Data set label' => 'Label 1'
 							],
 							[
-								'host' => 'test host',
-								'item' => 'test item'
+								'host' => 'test host 2',
+								'item' => 'test item 2',
+								'Aggregation function' => 'first',
+								'Data set aggregation' => 'count',
+								'Data set label' => 'Label 2'
 							]
 						],
 						'Displaying options' => [
 							'History data selection' => 'History',
-							'Draw' => 'Doughnut'
+							'Draw' => 'Doughnut',
+							'Width' => '40',
+							'Space between sectors' => '2',
+							'id:merge' => true,
+							'id:merge_percent' => '10',
+							'Show total value' => true,
+							'id:value_size_type' => 'Custom',
+							'id:value_size_custom_input' => '25',
+							'Decimal places' => '1',
+							'id:units_show' => true,
+							'id:units' => 'GG',
+							'Bold' => true,
+							'Color' => '4FC3F7'
+						],
+						'Time period' => [
+							'Time period' => 'Custom',
+							'From' => 'now-3h',
+							'To' => 'now-2h'
+						],
+						'Legend' => [
+							'Show legend' => true,
+							'Show aggregation function' => true,
+							'Number of rows' => 2,
+							'Number of columns' => 3
 						]
 					]
 				]
 			],
+			// Unicode values.
+			[
+				[
+					'fields' => [
+						'main_fields' => [
+							'Name' => '🙂🙃 &nbsp; <script>alert("hi!");</script>'
+						],
+						'Data set' => [
+							'host' => '&nbsp; <script>alert("host");</script>',
+							'item' => '&nbsp; <script>alert("item");</script>',
+							'Data set label' => '🙂🙃 &nbsp; <script>alert("hi!");</script>'
+						],
+						'Displaying options' => [
+							'Draw' => 'Doughnut',
+							'Show total value' => true,
+							'id:units_show' => true,
+							'id:units' => '🙂🙃 &nbsp; <script>alert("hi!");</script>'
+						]
+					]
+				]
+			],
+			// Merge sector % value too small.
+			[
+				[
+					'fields' => [
+						'Displaying options' => [
+							'id:merge' => true,
+							'id:merge_percent' => '0'
+						]
+					],
+					'result' => TEST_BAD,
+					'error' => 'Invalid parameter "merge_percent": value must be one of 1-10.'
+				]
+			],
+			// Merge sector % value too big.
+			[
+				[
+					'fields' => [
+						'Displaying options' => [
+							'id:merge' => true,
+							'id:merge_percent' => '11'
+						]
+					],
+					'result' => TEST_BAD,
+					'error' => 'Invalid parameter "merge_percent": value must be one of 1-10.'
+				]
+			],
+			// Decimal places value too big.
+			[
+				[
+					'fields' => [
+						'Displaying options' => [
+							'Draw' => 'Doughnut',
+							'Show total value' => true,
+							'Decimal places' => '7'
+						]
+					],
+					'result' => TEST_BAD,
+					'error' => 'Invalid parameter "Decimal places": value must be one of 0-6.'
+				]
+			],
+			// Empty Time period (Widget).
+			[
+				[
+					'fields' => [
+						'Time period' => [
+							'Time period' => 'Widget'
+						]
+					],
+					'result' => TEST_BAD,
+					'error' => 'Invalid parameter "Time period/Widget": cannot be empty.'
+				]
+			],
+			// Empty Time period (Custom).
+			[
+				[
+					'fields' => [
+						'Time period' => [
+							'Time period' => 'Custom',
+							'From' => '',
+							'To' => ''
+						]
+					],
+					'result' => TEST_BAD,
+					'error' => [
+						'Invalid parameter "Time period/From": cannot be empty.',
+						'Invalid parameter "Time period/To": cannot be empty.'
+					]
+				]
+			],
+			// Invalid Time period (Custom).
+			[
+				[
+					'fields' => [
+						'Time period' => [
+							'Time period' => 'Custom',
+							'From' => '0',
+							'To' => '2020-13-32'
+						]
+					],
+					'result' => TEST_BAD,
+					'error' => [
+						'Invalid parameter "Time period/From": a time is expected.',
+						'Invalid parameter "Time period/To": a time is expected.'
+					]
+				]
+			],
+			// Different Time period formats.
+			[
+				[
+					'fields' => [
+						'Time period' => [
+							'Time period' => 'Custom',
+							'From' => '2020',
+							'To' => '2020-10-10 00:00:00'
+						]
+					]
+				]
+			]
 		];
 	}
 
@@ -156,7 +301,12 @@ class testDashboardPieChartWidget extends CWebTest
 			// Assert error message.
 			$message = CMessageElement::find()->waitUntilPresent()->one();
 			$this->assertTrue($message->isBad());
-			$this->assertEquals($data['error'], $message->getLines()->get(0)->getText());
+
+			$errors = is_array($data['error']) ? $data['error'] : [$data['error']];
+
+			foreach ($errors as $i => $error) {
+				$this->assertEquals($error, $message->getLines()->get($i)->getText());
+			}
 		}
 
 		// Check total Widget count.
@@ -198,18 +348,22 @@ class testDashboardPieChartWidget extends CWebTest
 		$form->checkValue($main_fields);
 
 		// Check Data set tab.
-		if (CTestArrayHelper::isAssociative($fields['Data set'])) {
-			$fields['Data set'] = [$fields['Data set']];
+		$data_sets = CTestArrayHelper::get($fields, 'Data set', ['host' => 'Test Host', 'item' => 'Test Item']);
+
+		if (CTestArrayHelper::isAssociative($data_sets)) {
+			$data_sets = [$data_sets];
 		}
 
-		$last = count($fields['Data set']) - 1;
+		$last = count($data_sets) - 1;
 
-		foreach ($fields['Data set'] as $i => $data_set) {
+		foreach ($data_sets as $i => $data_set) {
 			// Prepare host and item fields.
 			$mapping = [
 				'host' => 'xpath://div[@id="ds_'.$i.'_hosts_"]/..',
 				'item' => 'xpath://div[@id="ds_'.$i.'_items_"]/..'
 			];
+
+			// Change 'host' and 'item' keys to the actual selectors these fields have.
 			foreach ($mapping as $field => $selector) {
 				$data_set = [$selector => $data_set[$field]] + $data_set;
 				unset($data_set[$field]);
@@ -249,7 +403,8 @@ class testDashboardPieChartWidget extends CWebTest
 		$form->fill($main_fields);
 
 		// Fill datasets.
-		$this->fillDatasets(CTestArrayHelper::get($fields, 'Data set', []), $form);
+		$data_sets = CTestArrayHelper::get($fields, 'Data set', ['host' => 'Test Host', 'item' => 'Test Item']);
+		$this->fillDatasets($data_sets, $form);
 
 		// Fill the other tabs.
 		$tabs = ['Displaying options', 'Time period', 'Legend'];
@@ -281,13 +436,13 @@ class testDashboardPieChartWidget extends CWebTest
 		$count_sets = $form->query('xpath://li[contains(@class, "list-accordion-item")]')->all()->count();
 
 		foreach ($data_sets as $i => $data_set) {
-			$selectors = [
+			$mapping = [
 				'host' => 'xpath://div[@id="ds_'.$i.'_hosts_"]/..',
 				'item' => 'xpath://div[@id="ds_'.$i.'_items_"]/..'
 			];
 
 			// Exchange 'host' and 'item' keys for the actual selector the fields have.
-			foreach ($selectors as $field => $selector) {
+			foreach ($mapping as $field => $selector) {
 				if (array_key_exists($field, $data_set)) {
 					$data_set = [$selector => $data_set[$field]] + $data_set;
 					unset($data_set[$field]);
