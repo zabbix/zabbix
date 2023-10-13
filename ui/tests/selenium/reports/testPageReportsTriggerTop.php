a42480419d730e1183153d72e696177e3930edbe
Kirils Seve <kirils.seve@zabbix.com>
2023-10-13 15:44:54 +0300

    .......... [DEV-2585] added comment for testPageReportsTriggerTop_Filter test data provider

--- ui/tests/selenium/reports/testPageReportsTriggerTop.php
+++ ui/tests/selenium/reports/testPageReportsTriggerTop.php
@@ -603,7 +603,11 @@ class testPageReportsTriggerTop extends CWebTest {
 					]
 				]
 			],
-			// Search by date label.
+			/*
+			 * Search by date label.
+			 * Note: This test case depends on time when executed. E.g. if execution time is around 00:00 - 00:30 expected
+			 * result will be different, because some of prepareData generated problems will appear in yesterday's filter.
+			 */
 			[
 				[
 					'date' => [
