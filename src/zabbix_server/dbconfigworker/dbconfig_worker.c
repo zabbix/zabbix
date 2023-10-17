206b586b0447c634717dcc792b49e15eba6ade50
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2023-10-17 10:25:46 +0300

    .......PS. [ZBXNEXT-7460] added new process for macro sync

--- src/zabbix_server/dbconfigworker/dbconfig_worker.c
+++ src/zabbix_server/dbconfigworker/dbconfig_worker.c
@@ -32,6 +32,79 @@
 #define ZBX_CONNECTOR_MANAGER_DELAY	1
 #define ZBX_CONNECTOR_FLUSH_INTERVAL	1
 
+
+static int	dbsync_macros(void)
+{
+	zbx_db_result_t		result;
+	zbx_db_row_t		row;
+	int			deleted = 0;
+	zbx_dc_um_handle_t	*um_handle;
+	char			*sql = NULL;
+	size_t			sql_alloc = 0, sql_offset = 0;
+	double			sec = zbx_time();
+
+	zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __func__);
+
+	um_handle = zbx_dc_open_user_macros();
+	zbx_db_begin();
+
+	result = zbx_db_select("select i.itemid,i.hostid,i.name,i.name_resolved"
+			" from items i"
+			" join hosts h on i.hostid=h.hostid"
+			" where i.name_upper like '%%{$%%' and (h.status=0 or h.status=1)"
+					" and (i.flags=0 or i.flags=1 or i.flags=4)"
+			" order by itemid");
+
+	zbx_db_begin_multiple_update(&sql, &sql_alloc, &sql_offset);
+
+	zabbix_log(LOG_LEVEL_DEBUG, "fetch started");
+
+	while (NULL != (row = zbx_db_fetch(result)))
+	{
+		zbx_uint64_t	itemid, hostid;
+		const char	*name_resolved_current;
+		char		*name_resolved_new;
+
+		ZBX_STR2UINT64(itemid, row[0]);
+		ZBX_STR2UINT64(hostid, row[1]);
+		name_resolved_new = zbx_strdup(NULL, row[2]);
+		name_resolved_current = row[3];
+
+		(void)zbx_dc_expand_user_macros(um_handle, &name_resolved_new, &hostid, 1, NULL);
+
+		if (0 != strcmp(name_resolved_current, name_resolved_new))
+		{
+			char	*name_resolved_esc;
+		
+			name_resolved_esc = zbx_db_dyn_escape_string(name_resolved_new);
+
+			zbx_snprintf_alloc(&sql, &sql_alloc, &sql_offset, "update items set"
+					" name_resolved='%s',name_resolved_upper=upper(name_resolved)"
+					" where itemid=" ZBX_FS_UI64 ";\n",
+					name_resolved_esc, itemid);
+			zbx_db_execute_overflowed_sql(&sql, &sql_alloc, &sql_offset);
+
+			zbx_free(name_resolved_esc);
+		}
+
+		zbx_free(name_resolved_new);
+	}
+	zbx_db_free_result(result);
+
+	zbx_db_end_multiple_update(&sql, &sql_alloc, &sql_offset);
+
+	if (sql_offset > 16)	/* In ORACLE always present begin..end; */
+		zbx_db_execute("%s", sql);
+
+	zbx_free(sql);
+	zbx_dc_close_user_macros(um_handle);
+	zbx_db_commit();
+
+	zabbix_log(LOG_LEVEL_INFORMATION, "End of %s() in:" ZBX_FS_DBL, __func__, zbx_time() - sec);
+
+	return deleted;
+}
+
 ZBX_THREAD_ENTRY(dbconfig_worker_thread, args)
 {
 	zbx_ipc_service_t			service;
@@ -53,6 +126,9 @@ ZBX_THREAD_ENTRY(dbconfig_worker_thread, args)
 	zabbix_log(LOG_LEVEL_INFORMATION, "%s #%d started [%s #%d]", get_program_type_string(info->program_type),
 				server_num, get_process_type_string(process_type), process_num);
 
+	zbx_setproctitle("%s [connecting to the database]", get_process_type_string(process_type));
+	zbx_db_connect(ZBX_DB_CONNECT_NORMAL);
+
 	if (FAIL == zbx_ipc_service_start(&service, ZBX_IPC_SERVICE_DBCONFIG_WORKER, &error))
 	{
 		zabbix_log(LOG_LEVEL_CRIT, "cannot start connector manager service: %s", error);
@@ -66,6 +142,8 @@ ZBX_THREAD_ENTRY(dbconfig_worker_thread, args)
 	zbx_setproctitle("%s #%d started", get_process_type_string(process_type), process_num);
 
 	zbx_vector_uint64_create(&hostids);
+	zabbix_increase_log_level();
+	dbsync_macros();
 
 	while (ZBX_IS_RUNNING())
 	{
