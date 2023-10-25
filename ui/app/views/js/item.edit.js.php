zabbix/zabbix

commit a652deb03d4b93199634d237fd92ded323559b39

..F....... [ZBXNEXT-8486] added changes to clone item without post validation

ui/app/views/js/item.edit.js.php
@@ -99,6 +99,12 @@ window.item_edit_form = new class {
 	}
 
 	initForm(field_switches) {
+		const updateSortOrder = function() {
+			let index = 0;
+
+			this.querySelectorAll('[name$="[sortorder]"]').forEach(node => node.value = index++);
+		}
+
 		new CViewSwitcher('allow_traps', 'change', field_switches.for_traps);
 		new CViewSwitcher('authtype', 'change', field_switches.for_authtype);
 		new CViewSwitcher('http_authtype', 'change', field_switches.for_http_auth_type);
@@ -141,12 +147,14 @@ window.item_edit_form = new class {
 		});
 		jQuery('#query-fields-table').dynamicRows({
 			sortable: true,
+			sortableOptions: {update: updateSortOrder},
 			template: '#query-field-row-tmpl',
 			rows: this.form_data.query_fields,
 			allow_empty: true
 		}).sortable({disabled: this.form_readonly});
 		jQuery('#headers-table').dynamicRows({
 			sortable: true,
+			sortableOptions: {update: updateSortOrder},
 			template: '#item-header-row-tmpl',
 			rows: this.form_data.headers,
 			allow_empty: true
@@ -284,13 +292,12 @@ window.item_edit_form = new class {
 	}
 
 	clone() {
-		const el = document.createElement('input');
+		this.overlay.setLoading();
 
-		el.type = 'hidden';
-		el.name = 'clone';
-		el.value = 1;
-		this.form.appendChild(el);
-		reloadPopup(this.form, this.actions.form);
+		PopUp('item.edit', {clone: 1, ...this.#getFormFields()}, {
+			dialogueid: 'item-edit',
+			dialogue_class: 'modal-popup-large'
+		});
 	}
 
 	create() {
@@ -409,12 +416,12 @@ window.item_edit_form = new class {
 
 				case 'query_fields':
 				case 'headers':
-					for (const [i, value] of Object.entries(fields[key].name)) {
-						fields[key].name[i] = value.trim();
-					}
-
-					for (const [i, value] of Object.entries(fields[key].value)) {
-						fields[key].value[i] = value.trim();
+					for (const [i, param] of Object.entries(fields.parameters)) {
+						fields.parameters[i] = {
+							name: param.name.trim(),
+							value: param.value.trim(),
+							sortorder: param.sortorder
+						}
 					}
 
 					break;
@@ -444,6 +451,10 @@ window.item_edit_form = new class {
 
 					break;
 			}
+		}
+
+		if (fields.preprocessing) {
+			fields.preprocessing = Object.entries(fields.preprocessing).map(p => p[1]);
 		}
 
 		return fields;
@@ -511,6 +522,10 @@ window.item_edit_form = new class {
 	#isTestableItem() {
 		const key = this.field.key.value;
 		const type = parseInt(this.field.type.value, 10);
+
+		if (key.trim() === '') {
+			return false;
+		}
 
 		return type == ITEM_TYPE_SIMPLE
 			? key.substr(0, 7) !== 'vmware.' && key.substr(0, 8) !== 'icmpping'

ui/app/controllers/CControllerItem.php
@@ -194,11 +194,11 @@ abstract class CControllerItem extends CController {
 	}
 
 	/**
-	 * Get form input data to be sent to API.
+	 * Get form fields value normalized.
 	 *
-	 * @return array
+	 * @return array form fields data.
 	 */
-	protected function getInputForApi(): array {
+	protected function getFormValues(): array {
 		$unchecked_values = [
 			'allow_traps' => HTTPCHECK_ALLOW_TRAPS_OFF,
 			'follow_redirects' => HTTPTEST_STEP_FOLLOW_REDIRECTS_OFF,
@@ -209,7 +209,8 @@ abstract class CControllerItem extends CController {
 		];
 		$input = $unchecked_values + CItemHelper::getDefaults();
 		$this->getInputs($input, array_keys($input));
+		$input = CItemHelper::normalizeFormData($input);
 
-		return CItemHelper::convertFormInputForApi($input);
+		return $input;
 	}
 }

ui/app/controllers/CControllerItemCreate.php
@@ -70,8 +70,8 @@ class CControllerItemCreate extends CControllerItem {
 	}
 
 	protected function getInputForApi(): array {
-		$input = parent::getInputForApi();
-
+		$input = $this->getFormValues();
+		$input = CItemHelper::convertFormInputForApi($input);
 		$input['hosts'] = API::Host()->get([
 			'output' => ['hostid', 'status'],
 			'hostids' => [$this->getInput('hostid')],

ui/app/controllers/CControllerItemEdit.php
@@ -60,7 +60,8 @@ class CControllerItemEdit extends CControllerItem {
 
 	public function doAction() {
 		$host = $this->getInput('context') === 'host' ? $this->getHost() : $this->getTemplate();
-		$item = $this->getItem($host);
+		$item = $this->hasInput('clone') ? $this->getClone($host) : $this->getItem($host);
+		$item['context'] = $this->getInput('context');
 		$inherited_timeouts = getInheritedTimeouts($host['proxyid'])['timeouts'];
 		$item['inherited_timeout'] = $inherited_timeouts[$item['type']] ?? '';
 
@@ -171,43 +172,63 @@ class CControllerItemEdit extends CControllerItem {
 	}
 
 	/**
+	 * Get item form input for clone action.
+	 *
+	 * @param array $host  Item host data.
+	 *
+	 * @return array item clone form data.
+	 */
+	protected function getClone(array $host): array {
+		$item = [
+			'itemid' => 0,
+			'templateid' => 0,
+			'hostid' => $host['hostid'],
+			'flags' => ZBX_FLAG_DISCOVERY_NORMAL,
+			'parent_items' => [],
+			'discovered' => false,
+			'templated' => false
+		] + $this->getFormValues();
+
+		if ($item['valuemapid']) {
+			$valuemap = API::ValueMap()->get([
+				'output' => ['valuemapid', 'name', 'hostid'],
+				'valuemapids' => [$item['valuemapid']]
+			]);
+			$item['valuemap'] = $valuemap ? reset($valuemap) : [];
+
+			if ($item['valuemap'] && $this->getInput('templateid', 0)) {
+				$host_valuemap = API::ValueMap()->get([
+					'output' => ['valuemapid'],
+					'search' => ['name' => $item['valuemap']['name']],
+					'filter' => ['hostid' => $host['hostid']]
+				]);
+				$item['valuemap']['valuemapid'] = $host_valuemap ? $host_valuemap[0]['valuemapid'] : 0;
+			}
+		}
+
+		if ($item['master_itemid']) {
+			$master_item = API::Item()->get([
+				'output' => ['itemid', 'name'],
+				'itemids' => $item['master_itemid'],
+				'webitems' => true
+			]);
+			$item['master_item'] = $master_item ? reset($master_item) : [];
+		}
+
+		return $item;
+	}
+
+	/**
 	 * Get form data for item from database.
 	 *
-	 * @param array $host
+	 * @param array $host  Item host data.
 	 *
-	 * @return array
+	 * @return array item form data.
 	 */
 	protected function getItem(array $host): array {
 		$item = [];
 
-		if ($this->hasInput('clone')) {
-			$item = [
-				'itemid' => 0,
-				'templateid' => 0,
-				'flags' => ZBX_FLAG_DISCOVERY_NORMAL,
-				'parent_items' => [],
-				'discovered' => false,
-				'templated' => false,
-				'inventory_link' => 0
-			] + $this->getInputForApi();
-
-			if ($item['valuemapid'] && $this->getInput('templateid', 0)) {
-				$item_valuemap = API::ValueMap()->get([
-					'output' => ['name'],
-					'valuemapids' => [$item['valuemapid']]
-				]);
-
-				if ($item_valuemap) {
-					$host_valuemap = API::ValueMap()->get([
-						'output' => ['valuemapid'],
-						'search' => ['name' => $item_valuemap[0]['name']],
-						'filter' => ['hostid' => $host['hostid']]
-					]);
-					$item['valuemapid'] = $host_valuemap ? $host_valuemap[0]['valuemapid'] : 0;
-				}
-			}
-		}
-		elseif ($this->hasInput('itemid')) {
+		if ($this->hasInput('itemid')) {
 			[$item] = API::Item()->get([
 				'output' => [
 					'itemid', 'type', 'snmp_oid', 'hostid', 'name', 'key_', 'delay', 'history', 'trends', 'status',
@@ -225,12 +246,10 @@ class CControllerItemEdit extends CControllerItem {
 				'selectTags' => ['tag', 'value'],
 				'itemids' => [$this->getInput('itemid')]
 			]);
-		}
-
-		if ($item) {
 			$item = CItemHelper::convertApiInputForForm($item);
 		}
-		else {
+
+		if (!$item) {
 			$item = CItemHelper::getDefaults();
 			$item['hostid'] = $host['hostid'];
 
@@ -248,8 +267,6 @@ class CControllerItemEdit extends CControllerItem {
 				}
 			}
 		}
-
-		$item['context'] = $this->getInput('context');
 
 		return $item;
 	}

ui/app/controllers/CControllerItemPrototype.php
@@ -211,11 +211,11 @@ abstract class CControllerItemPrototype extends CController {
 	}
 
 	/**
-	 * Get form input data to be sent to API.
+	 * Get form fields value normalized.
 	 *
-	 * @return array
+	 * @return array form fields data.
 	 */
-	protected function getInputForApi(): array {
+	protected function getFormValues(): array {
 		$unchecked_values = [
 			'allow_traps' => HTTPCHECK_ALLOW_TRAPS_OFF,
 			'discover' => ZBX_PROTOTYPE_NO_DISCOVER,
@@ -227,7 +227,8 @@ abstract class CControllerItemPrototype extends CController {
 		];
 		$input = $unchecked_values + CItemPrototypeHelper::getDefaults();
 		$this->getInputs($input, array_keys($input));
+		$input = CItemPrototypeHelper::normalizeFormData($input);
 
-		return CItemPrototypeHelper::convertFormInputForApi($input);
+		return $input;
 	}
 }

ui/app/controllers/CControllerItemPrototypeCreate.php
@@ -70,8 +70,8 @@ class CControllerItemPrototypeCreate extends CControllerItemPrototype {
 	}
 
 	protected function getInputForApi(): array {
-		$input = parent::getInputForApi();
-
+		$input = $this->getFormValues();
+		$input = CItemPrototypeHelper::convertFormInputForApi($input);
 		[$lld_rule] = API::DiscoveryRule()->get([
 			'output' => ['itemid', 'hostid'],
 			'selectHosts' => ['status'],

ui/app/controllers/CControllerItemPrototypeEdit.php
@@ -60,7 +60,8 @@ class CControllerItemPrototypeEdit extends CControllerItemPrototype {
 
 	public function doAction() {
 		$host = $this->getInput('context') === 'host' ? $this->getHost() : $this->getTemplate();
-		$item = $this->getItemPrototype($host);
+		$item = $this->hasInput('clone') ? $this->getClone($host) : $this->getItemPrototype($host);
+		$item['context'] = $this->getInput('context');
 		$inherited_timeouts = getInheritedTimeouts($host['proxyid'])['timeouts'];
 		$item['inherited_timeout'] = $inherited_timeouts[$item['type']] ?? '';
 
@@ -151,6 +152,61 @@ class CControllerItemPrototypeEdit extends CControllerItemPrototype {
 	}
 
 	/**
+	 * Get item form input for clone action.
+	 *
+	 * @param array $host  Item host data.
+	 *
+	 * @return array item clone form data.
+	 */
+	protected function getClone(array $host): array {
+		$item = [
+			'itemid' => 0,
+			'templateid' => 0,
+			'flags' => ZBX_FLAG_DISCOVERY_PROTOTYPE,
+			'parent_items' => [],
+			'parent_discoveryid' => $this->getInput('parent_discoveryid'),
+			'discovered' => false,
+			'templated' => false
+		] + $this->getFormValues();
+
+		if ($item['valuemapid']) {
+			$valuemap = API::ValueMap()->get([
+				'output' => ['valuemapid', 'name', 'hostid'],
+				'valuemapids' => [$item['valuemapid']]
+			]);
+			$item['valuemap'] = $valuemap ? reset($valuemap) : [];
+
+			if ($item['valuemap'] && $this->getInput('templateid', 0)) {
+				$host_valuemap = API::ValueMap()->get([
+					'output' => ['valuemapid'],
+					'search' => ['name' => $item['valuemap']['name']],
+					'filter' => ['hostid' => $host['hostid']]
+				]);
+				$item['valuemap']['valuemapid'] = $host_valuemap ? $host_valuemap[0]['valuemapid'] : 0;
+			}
+		}
+
+		if ($item['master_itemid']) {
+			$master_item = API::ItemPrototype()->get([
+				'output' => ['itemid', 'name'],
+				'itemids' => $item['master_itemid']
+			]);
+
+			if (!$master_item) {
+				$master_item = API::Item()->get([
+					'output' => ['itemid', 'name'],
+					'itemids' => $item['master_itemid'],
+					'webitems' => true
+				]);
+			}
+
+			$item['master_item'] = $master_item ? reset($master_item) : [];
+		}
+
+		return $item;
+	}
+
+	/**
 	 * Get item prototype data.
 	 *
 	 * @param array $host
@@ -160,34 +216,7 @@ class CControllerItemPrototypeEdit extends CControllerItemPrototype {
 	protected function getItemPrototype(array $host): array {
 		$item = [];
 
-		if ($this->hasInput('clone')) {
-			$item = [
-				'itemid' => 0,
-				'templateid' => 0,
-				'flags' => ZBX_FLAG_DISCOVERY_PROTOTYPE,
-				'parent_items' => [],
-				'parent_discoveryid' => $this->getInput('parent_discoveryid'),
-				'discovered' => false,
-				'templated' => false
-			] + $this->getInputForApi();
-
-			if ($item['valuemapid'] && $this->getInput('templateid', 0)) {
-				$item_valuemap = API::ValueMap()->get([
-					'output' => ['name'],
-					'valuemapids' => [$item['valuemapid']]
-				]);
-
-				if ($item_valuemap) {
-					$host_valuemap = API::ValueMap()->get([
-						'output' => ['valuemapid'],
-						'search' => ['name' => $item_valuemap[0]['name']],
-						'filter' => ['hostid' => $item['hostid']]
-					]);
-					$item['valuemapid'] = $host_valuemap ? $host_valuemap[0]['valuemapid'] : 0;
-				}
-			}
-		}
-		elseif ($this->hasInput('itemid')) {
+		if ($this->hasInput('itemid')) {
 			[$item] = API::ItemPrototype()->get([
 				'output' => [
 					'itemid', 'type', 'snmp_oid', 'hostid', 'name', 'key_', 'delay', 'history', 'trends', 'status',
@@ -203,12 +232,10 @@ class CControllerItemPrototypeEdit extends CControllerItemPrototype {
 				'selectTags' => ['tag', 'value'],
 				'itemids' => $this->getInput('itemid')
 			]);
-		}
-
-		if ($item) {
 			$item = CItemPrototypeHelper::convertApiInputForForm($item);
 		}
-		else {
+
+		if (!$item) {
 			$item = CItemPrototypeHelper::getDefaults();
 			$item['hostid'] = $host['hostid'];
 			$item['parent_discoveryid'] = $this->getInput('parent_discoveryid');
@@ -235,8 +262,6 @@ class CControllerItemPrototypeEdit extends CControllerItemPrototype {
 				}
 			}
 		}
-
-		$item['context'] = $this->getInput('context');
 
 		return $item;
 	}

ui/app/controllers/CControllerItemPrototypeUpdate.php
@@ -70,8 +70,8 @@ class CControllerItemPrototypeUpdate extends CControllerItemPrototype {
 	}
 
 	protected function getInputForApi(): array {
-		$input = parent::getInputForApi();
-
+		$input = $this->getFormValues();
+		$input = CItemPrototypeHelper::convertFormInputForApi($input);
 		[$db_item] = API::ItemPrototype()->get([
 			'output' => ['templateid', 'flags'],
 			'selectHosts' => ['status'],

commit 3a75f39ccf908eafaad7eead88595e1610e29bef

..F....... [ZBXNEXT-8486] fixed item clone broken in a652deb03d4

ui/app/views/js/item.edit.js.php
@@ -294,7 +294,7 @@ window.item_edit_form = new class {
 	clone() {
 		this.overlay.setLoading();
 
-		PopUp('item.edit', {clone: 1, ...this.#getFormFields()}, {
+		PopUp(this.actions.form, {clone: 1, ...this.#getFormFields()}, {
 			dialogueid: 'item-edit',
 			dialogue_class: 'modal-popup-large'
 		});