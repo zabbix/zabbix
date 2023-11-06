e0f95c7d54a9faa0735207f31d99ca359725fe69
Dmitrijs Fofanovs <dmitrijs.fofanovs@zabbix.com>
2023-11-06 14:15:09 +0200

    A......... [ZBXNEXT-8735,ZBXNEXT-8783] fixed ordering in validation on create and update for connectors API

--- ui/include/classes/api/services/CConnector.php
+++ ui/include/classes/api/services/CConnector.php
@@ -200,11 +200,11 @@ class CConnector extends CApiService {
 			'name' =>				['type' => API_STRING_UTF8, 'flags' => API_REQUIRED | API_NOT_EMPTY, 'length' => DB::getFieldLength('connector', 'name')],
 			'protocol' =>			['type' => API_INT32, 'in' => ZBX_STREAMING_PROTOCOL_V1],
 			'data_type' =>			['type' => API_INT32, 'in' => implode(',', [ZBX_CONNECTOR_DATA_TYPE_ITEM_VALUES, ZBX_CONNECTOR_DATA_TYPE_EVENTS]), 'default' => DB::getDefault('connector', 'data_type')],
+			'url' =>				['type' => API_URL, 'flags' => API_REQUIRED | API_NOT_EMPTY | API_ALLOW_USER_MACRO, 'length' => DB::getFieldLength('connector', 'url')],
 			'item_value_type' => 	['type' => API_MULTIPLE, 'rules' => [
 										['if' => ['field' => 'data_type', 'in' => ZBX_CONNECTOR_DATA_TYPE_ITEM_VALUES], 'type' => API_INT32, 'in' => ZBX_CONNECTOR_ITEM_VALUE_TYPE_FLOAT.':'.DB::getDefault('connector', 'item_value_type'), 'default' => DB::getDefault('connector', 'item_value_type')],
 										['else' => true, 'type' => API_INT32, 'in' => ZBX_CONNECTOR_ITEM_VALUE_TYPE_FLOAT.':'.DB::getDefault('connector', 'item_value_type'), 'default' => DB::getDefault('connector', 'item_value_type')]
 			]],
-			'url' =>				['type' => API_URL, 'flags' => API_REQUIRED | API_NOT_EMPTY | API_ALLOW_USER_MACRO, 'length' => DB::getFieldLength('connector', 'url')],
 			'max_records' =>		['type' => API_INT32, 'in' => '0:'.ZBX_MAX_INT32],
 			'max_senders' =>		['type' => API_INT32, 'in' => '1:100'],
 			'max_attempts' =>		['type' => API_INT32, 'in' => '1:5', 'default' => DB::getDefault('connector', 'max_attempts')],
@@ -365,11 +365,11 @@ class CConnector extends CApiService {
 			'name' =>				['type' => API_STRING_UTF8, 'flags' => API_NOT_EMPTY, 'length' => DB::getFieldLength('connector', 'name')],
 			'protocol' =>			['type' => API_INT32, 'in' => ZBX_STREAMING_PROTOCOL_V1],
 			'data_type' =>			['type' => API_INT32, 'in' => implode(',', [ZBX_CONNECTOR_DATA_TYPE_ITEM_VALUES, ZBX_CONNECTOR_DATA_TYPE_EVENTS])],
+			'url' =>				['type' => API_URL, 'flags' => API_NOT_EMPTY | API_ALLOW_USER_MACRO, 'length' => DB::getFieldLength('connector', 'url')],
 			'item_value_type' => 	['type' => API_MULTIPLE, 'rules' => [
 										['if' => ['field' => 'data_type', 'in' => ZBX_CONNECTOR_DATA_TYPE_ITEM_VALUES], 'type' => API_INT32, 'in' => ZBX_CONNECTOR_ITEM_VALUE_TYPE_FLOAT.':'.DB::getDefault('connector', 'item_value_type')],
 										['else' => true, 'type' => API_INT32, 'in' => ZBX_CONNECTOR_ITEM_VALUE_TYPE_FLOAT.':'.DB::getDefault('connector', 'item_value_type')]
 			]],
-			'url' =>				['type' => API_URL, 'flags' => API_NOT_EMPTY | API_ALLOW_USER_MACRO, 'length' => DB::getFieldLength('connector', 'url')],
 			'max_records' =>		['type' => API_INT32, 'in' => '0:'.ZBX_MAX_INT32],
 			'max_senders' =>		['type' => API_INT32, 'in' => '1:100'],
 			'max_attempts' =>		['type' => API_INT32, 'in' => '1:5'],
