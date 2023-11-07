c4d88b9bcf2cafae7db4eec1b6135cad4a967937
Anna Poga <anna.poga@zabbix.com>
2023-11-07 10:37:45 +0200

    ..F....... [ZBX-23269] fixed sizes of loading modals
    * commit '1277adf5a2c2808685673201cf0a25b3987f465e':
      ..F....... [ZBX-23269] fixed size of loading modal in scheduled report subscriptions
      ..F....... [ZBX-23269] fixed sizes of loading modals
    
    (cherry picked from commit f8153a6a25cbfab712744b724e9b0c06bb43f0bf)

--- ui/app/views/administration.user.edit.php
+++ ui/app/views/administration.user.edit.php
@@ -407,7 +407,9 @@ if ($data['action'] === 'user.edit' || CWebUser::$data['type'] > USER_TYPE_ZABBI
 						(new CButtonLink(_('Edit')))
 							->setAttribute('data-parameters', json_encode($parameters))
 							->setEnabled(!$data['readonly'])
-							->onClick('PopUp("popup.media", JSON.parse(this.dataset.parameters));'),
+							->onClick('PopUp("popup.media", JSON.parse(this.dataset.parameters),
+								{dialogue_class: "modal-popup-generic"});'
+							),
 						(new CButtonLink(_('Remove')))
 							->setEnabled(!$data['readonly'])
 							->onClick('removeMedia('.$index.');')
@@ -422,7 +424,10 @@ if ($data['action'] === 'user.edit' || CWebUser::$data['type'] > USER_TYPE_ZABBI
 			$media_table_info,
 			(new CButtonLink(_('Add')))
 				->setEnabled(!$data['readonly'])
-				->onClick('PopUp("popup.media", '.json_encode(['dstfrm' => $user_form->getName()]).');')
+				->onClick('PopUp("popup.media", '.
+					json_encode(['dstfrm' => $user_form->getName()]).
+					', {dialogue_class: "modal-popup-generic"});'
+				)
 		]))
 			->addClass(ZBX_STYLE_TABLE_FORMS_SEPARATOR)
 			->setAttribute('style', 'min-width: '.ZBX_TEXTAREA_BIG_WIDTH.'px;')
@@ -652,7 +657,7 @@ if ($data['action'] === 'user.edit') {
 			foreach ($data['modules'] as $moduleid => $module_name) {
 				$elements[] = (new CSpan($module_name))->addClass(
 					array_key_exists($moduleid, $data['disabled_moduleids'])
-							|| $data['modules_rules'][$moduleid] == MODULE_STATUS_DISABLED
+					|| $data['modules_rules'][$moduleid] == MODULE_STATUS_DISABLED
 						? ZBX_STYLE_STATUS_GREY
 						: ZBX_STYLE_STATUS_GREEN
 				);
@@ -672,8 +677,8 @@ if ($data['action'] === 'user.edit') {
 		$permissions_form_list
 			->addRow((new CTag('h4', true, _('Access to API')))->addClass('input-section-header'))
 			->addRow((new CDiv((new CSpan($api_access_enabled ? _('Enabled') : _('Disabled')))->addClass(
-					$api_access_enabled ? ZBX_STYLE_STATUS_GREEN : ZBX_STYLE_STATUS_GREY
-				)))
+				$api_access_enabled ? ZBX_STYLE_STATUS_GREEN : ZBX_STYLE_STATUS_GREY
+			)))
 				->setWidth(ZBX_TEXTAREA_BIG_WIDTH)
 				->addClass('rules-status-container')
 			);
@@ -867,8 +872,8 @@ $html_page
 
 if ($data['action'] === 'user.edit') {
 	(new CScriptTag('view.init('.json_encode([
-		'userid' => $data['userid'] ?: null
-	]).');'))
+			'userid' => $data['userid'] ?: null
+		]).');'))
 		->setOnDocumentReady()
 		->show();
 }
