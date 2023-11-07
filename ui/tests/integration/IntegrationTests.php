f549cf7b2241ba096d68f2a7b2035c1fe0d0f24c
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2023-11-07 11:49:23 +0200

    .......... [ZBXNEXT-7460] added integration test

--- ui/tests/integration/IntegrationTests.php
+++ ui/tests/integration/IntegrationTests.php
@@ -47,7 +47,7 @@ require_once dirname(__FILE__).'/testEventsCauseAndSymptoms.php';
 require_once dirname(__FILE__).'/testDiscoveryRules.php';
 require_once dirname(__FILE__).'/testAutoregistration.php';
 require_once dirname(__FILE__).'/testHistoryPush.php';
-require_once dirname(__FILE__).'/testItemTimeouts.php';
+require_once dirname(__FILE__).'/testUserMacrosInItemNames.php';
 
 use PHPUnit\Framework\TestSuite;
 
@@ -87,6 +87,7 @@ class IntegrationTests {
 		$suite->addTestSuite('testEventsCauseAndSymptoms');
 		$suite->addTestSuite('testHistoryPush');
 		$suite->addTestSuite('testItemTimeouts');
+		$suite->addTestSuite('testUserMacrosInItemNames');
 
 		return $suite;
 	}
