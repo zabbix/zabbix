6c75b31b5cc1c788bda842eea40806d0e8f830b2
Dmitrijs Fofanovs <dmitrijs.fofanovs@zabbix.com>
2023-11-15 17:02:14 +0200

    ..F....... [ZBX-21973] fixed translation string hintbox code style

--- ui/app/views/administration.miscconfig.edit.php
+++ ui/app/views/administration.miscconfig.edit.php
@@ -113,20 +113,20 @@ $from_list = (new CFormList())
 		(new CLabel([_('Use X-Frame-Options HTTP header'),
 			makeHelpIcon([
 				_('X-Frame-Options HTTP header supported values:'),
-				(new CList(
-					[
-						new CListItem([
-							_s('%1$s (default) or %2$s (must be single-quoted) - the page can only be displayed in a frame on the same origin as the page itself%3$s', 'SAMEORIGIN', '\'self\'', ';')
-						]),
-						new CListItem([
-							_s('%1$s (default) or %2$s (must be single-quoted) - the page cannot be displayed in a frame, regardless of the site attempting to do so%3$s', 'DENY', '\'none\'', ';')
-						]),
-						new CListItem([
-							_s('a string of space-separated hostnames; adding %1$s (must be single-quoted) to the list will allow the page to be displayed in a frame on the same origin as the page itself%2$s', '\'self\'', '.'),
-						])
-					]
-				))->addClass(ZBX_STYLE_LIST_DASHED),
-				_s('Note, that using %1$s or %2$s without single quotes will result in them being regarded as hostnames%3$s', '\'self\'', '\'none\'', '.')
+				(new CList([
+					new CListItem(_s('%1$s (default) or %2$s (must be single-quoted) - the page can only be displayed in a frame on the same origin as the page itself%3$s',
+						'SAMEORIGIN', '\'self\'', ';'
+					)),
+					new CListItem(_s('%1$s (default) or %2$s (must be single-quoted) - the page cannot be displayed in a frame, regardless of the site attempting to do so%3$s',
+						'DENY', '\'none\'', ';'
+					)),
+					new CListItem(_s('a string of space-separated hostnames; adding %1$s (must be single-quoted) to the list will allow the page to be displayed in a frame on the same origin as the page itself%2$s',
+						'\'self\'', '.'
+					))
+				]))->addClass(ZBX_STYLE_LIST_DASHED),
+				_s('Note, that using %1$s or %2$s without single quotes will result in them being regarded as hostnames%3$s',
+					'\'self\'', '\'none\'', '.'
+				)
 			])
 		], 'x_frame_header_enabled'))->setAsteriskMark(),
 		[
