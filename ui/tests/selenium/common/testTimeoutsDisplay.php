e4a8965f8a994971f698fafcaca907bd258cd2b9
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2023-11-15 13:02:31 +0200

    .......... [DEV-2722] rewrited host and proxy timeouts check

--- ui/tests/selenium/common/testTimeoutsDisplay.php
+++ ui/tests/selenium/common/testTimeoutsDisplay.php
@@ -35,7 +35,7 @@ class testTimeoutsDisplay extends CWebTest {
 		];
 	}
 
-	const DEFAULT_VALUES = [
+	const GLOBAL_DEFAULT = [
 		'Zabbix agent' => '3s',
 		'Simple check' => '3s',
 		'SNMP agent' => '3s',
@@ -47,7 +47,7 @@ class testTimeoutsDisplay extends CWebTest {
 		'Script' => '3s'
 	];
 
-	const SECONDS_VALUES = [
+	const GLOBAL_CUSTOM = [
 		'Zabbix agent' => '300s',
 		'Simple check' => '300s',
 		'SNMP agent' => '300s',
@@ -59,7 +59,7 @@ class testTimeoutsDisplay extends CWebTest {
 		'Script' => '300s'
 	];
 
-	const MACROS_VALUES = [
+	const GLOBAL_MACROS = [
 		'Zabbix agent' => '{$MACROS}',
 		'Simple check' => '{$MACROS}',
 		'SNMP agent' => '{$MACROS}',
@@ -71,39 +71,87 @@ class testTimeoutsDisplay extends CWebTest {
 		'Script' => '{$MACROS}'
 	];
 
+	const PROXY_MACROS = [
+		'Zabbix agent' => '{$PROXY}',
+		'Simple check' => '{$PROXY}',
+		'SNMP agent' => '{$PROXY}',
+		'External check' => '{$PROXY}',
+		'Database monitor' => '{$PROXY}',
+		'HTTP agent' => '{$PROXY}',
+		'SSH agent' => '{$PROXY}',
+		'TELNET agent' => '{$PROXY}',
+		'Script' => '{$PROXY}'
+	];
+
+	const PROXY_CUSTOM = [
+		'Zabbix agent' => '255s',
+		'Simple check' => '255s',
+		'SNMP agent' => '255s',
+		'External check' => '255s',
+		'Database monitor' => '255s',
+		'HTTP agent' => '255s',
+		'SSH agent' => '255s',
+		'TELNET agent' => '255s',
+		'Script' => '255s'
+	];
+
 	/**
+	 * Change global timeouts values and check them.
+	 *
 	 * @param string $timeout_values  	Reset default, macros values or seconds.
 	 * @param string $link    			Link to table with items, items prototype, discovery rules.
-	 * @param string $table_selector	Table selector for items, discovery rules, items prototype.
+	 * @param string $button_name		Table selector for items, discovery rules, items prototype.
+	 * @param string $proxy				Fill proxy timeouts.
 	 */
-	public function checkGlobal($timeout_values, $link, $table_selector) {
-		// Value for active agent timeout. Global Zabbix agent timeout used in active and passive agents.
-		if ($timeout_values === 'macros') {
-			$values = self::MACROS_VALUES;
-			$new_value = ['Zabbix agent (active)' => '{$MACROS}'];
+	public function checkGlobal($timeout_values, $link, $button_name, $proxy = false) {
+		// Global Zabbix agent timeout used in active and passive agents. Add this value to array.
+		switch ($timeout_values) {
+			case 'proxy_macros':
+				$values = self::PROXY_MACROS;
+				$new_value = ['Zabbix agent (active)' => '{$PROXY}'];
+				break;
+
+			case 'proxy_custom':
+				$values = self::PROXY_CUSTOM;
+				$new_value = ['Zabbix agent (active)' => '255s'];
+				break;
+
+			case 'global_default':
+				$values = self::GLOBAL_DEFAULT;
+				$new_value = ['Zabbix agent (active)' => '3s'];
+				break;
+
+			case 'global_macros':
+				$values = self::GLOBAL_MACROS;
+				$new_value = ['Zabbix agent (active)' => '{$MACROS}'];
+				break;
+
+			case 'global_custom':
+				$values = self::GLOBAL_CUSTOM;
+				$new_value = ['Zabbix agent (active)' => '300s'];
+				break;
 		}
-		elseif ($timeout_values === 'seconds') {
-			$values = self::SECONDS_VALUES;
-			$new_value = ['Zabbix agent (active)' => '300s'];
+
+		// Change timeout values for proxy or global timeouts.
+		if ($proxy) {
+			$this->fillProxyTimeouts($values);
 		}
 		else {
-			$values = self::DEFAULT_VALUES;
-			$new_value = ['Zabbix agent (active)' => '3s'];
+			$this->fillGlobalTimeouts($values);
 		}
 
-		$this->fillValues($values);
-
-		// Add created Zabbix active timeout to array.
+		// Add Zabbix agent active timeout to array.
 		$values = array_merge($values, $new_value);
 
+		// Navigate to page for timeouts checking.
 		$this->page->open($link)->waitUntilReady();
 
-		// Check timeout value in items one by one, after changes in Administration->Timeouts.
-		foreach ($values as $item_name => $timeout) {
-			$this->query($table_selector)->asTable()->one()->query('link', $item_name)->one()->click();
+		// Check timeout value in items one by one, after changes in Administration->Timeouts or Proxy->timeouts.
+		foreach ($values as $item_type => $timeout) {
+			$this->query('button', $button_name)->one()->click();
 
 			// Discovery rule doesn't have overlay.
-			if ($table_selector === 'name:discovery') {
+			if ($button_name === 'Create discovery rule') {
 				$this->page->waitUntilReady();
 			}
 			else {
@@ -111,12 +159,19 @@ class testTimeoutsDisplay extends CWebTest {
 			}
 
 			$form = $this->query('name:itemForm')->asForm()->one();
+
+			// Timeout field for SNMP agent appears after adding walk[1] to SNMP OID field.
+			$fill_form_values = ($item_type === 'SNMP agent')
+					? ['Type' => $item_type, 'SNMP OID' => 'walk[1]']
+					: ['Type' => $item_type];
+
+			$form->fill($fill_form_values);
 			$radio = $form->query('id:custom_timeout')->asSegmentedRadio()->one();
 			$this->assertEquals('Global', $radio->getText());
 			$this->assertTrue($radio->isEnabled());
 			$form->checkValue(['Timeout' => $timeout]);
 
-			if ($table_selector === 'name:discovery') {
+			if ($button_name === 'Create discovery rule') {
 				$this->page->open($link)->waitUntilReady();
 			}
 			else {
@@ -125,7 +180,12 @@ class testTimeoutsDisplay extends CWebTest {
 		}
 	}
 
-	protected function fillValues($values) {
+	/**
+	 * Fill timeouts.
+	 *
+	 * @param array $values		Timeouts values to fill.
+	 */
+	protected function fillGlobalTimeouts($values) {
 		$this->page->login()->open('zabbix.php?action=timeouts.edit')->waitUntilReady();
 		$form = $this->query('id:timeouts')->waitUntilVisible()->asForm()->one();
 
@@ -135,7 +195,7 @@ class testTimeoutsDisplay extends CWebTest {
 		}
 
 		// Reset if we need to check default values.
-		if ($values === self::DEFAULT_VALUES) {
+		if ($values === self::GLOBAL_DEFAULT) {
 			$form->query('button:Reset defaults')->one()->click();
 			COverlayDialogElement::find()->waitUntilVisible()->one()->query('button:Reset defaults')->one()->click();
 		}
@@ -152,5 +212,35 @@ class testTimeoutsDisplay extends CWebTest {
 
 		// Check form.
 		$form->checkValue($values);
+	}
+
+	protected function fillProxyTimeouts($values) {
+		$this->page->login()->open('zabbix.php?action=proxy.list')->waitUntilReady();
+		$this->query('name:proxy_list')->asTable()->one()->query('link:Proxy assigned to host')->one()->click();
+		$form = COverlayDialogElement::find()->waitUntilReady()->asForm()->one();
+		$form->selectTab('Timeouts');
+		$form->invalidate();
+
+		// Check values in form. If they are the same, we can skip and don't update them.
+		if ($form->checkValue($values, false) === true) {
+			COverlayDialogElement::find()->one()->close();
+			return;
+		}
+
+		// If proxy use Global timeouts values - change parameter to global and Reset global values ot timeouts page.
+		if ($values === self::GLOBAL_DEFAULT) {
+			$form->fill(['Timeouts for item types' => 'Global']);
+			$form->submit();
+			COverlayDialogElement::ensureNotPresent();
+			$this->page->waitUntilReady();
+			$this->fillGlobalTimeouts($values);
+		}
+		else {
+			$form->fill(['Timeouts for item types' => 'Override']);
+			$form->fill($values);
+			$form->submit();
+			COverlayDialogElement::ensureNotPresent();
+			$this->page->waitUntilReady();
+		}
 	}
 }
--- ui/tests/selenium/timeouts/testHostsTimeouts.php
+++ ui/tests/selenium/timeouts/testHostsTimeouts.php
@@ -28,10 +28,8 @@ require_once dirname(__FILE__).'/../common/testTimeoutsDisplay.php';
  */
 class testHostsTimeouts extends testTimeoutsDisplay {
 
-	protected static $proxyid;
 	protected static $hostid;
 	protected static $hostid_druleids;
-	protected static $interfaceid;
 
 	public static function prepareTimeoutsData() {
 		CDataHelper::call('proxy.create',
@@ -42,7 +40,7 @@ class testHostsTimeouts extends testTimeoutsDisplay {
 				]
 			]
 		);
-		self::$proxyid = CDataHelper::getIds('name');
+		$proxyid = CDataHelper::getIds('name');
 
 		$host_result = CDataHelper::createHosts([
 			[
@@ -74,84 +72,42 @@ class testHostsTimeouts extends testTimeoutsDisplay {
 						]
 					]
 				],
-				'items' => [
+				'discoveryrules' => [
 					[
 						'name' => 'Zabbix agent',
-						'key_' => 'zabbix_agent',
+						'key_' => 'zabbix_agent_drule',
 						'type' => ITEM_TYPE_ZABBIX,
-						'value_type' => ITEM_VALUE_TYPE_UINT64,
 						'delay' => 5
+					]
+				]
+			],
+			[
+				'host' => 'Host for timeouts check with proxy',
+				'groups' => [
+					[
+						'groupid' => 4 // Zabbix servers
+					]
+				],
+				'interfaces' => [
+					[
+						'type'=> INTERFACE_TYPE_AGENT,
+						'main' => INTERFACE_PRIMARY,
+						'useip' => INTERFACE_USE_DNS,
+						'ip' => '',
+						'dns' => 'zabbix_agent',
+						'port' => '1'
 					],
 					[
-						'name' => 'Zabbix agent (active)',
-						'key_' => 'zabbix_agent_active',
-						'type' => ITEM_TYPE_ZABBIX_ACTIVE,
-						'value_type' => ITEM_VALUE_TYPE_UINT64,
-						'delay' => 5
-					],
-					[
-						'name' => 'Simple check',
-						'key_' => 'simple_check',
-						'type' => ITEM_TYPE_SIMPLE,
-						'value_type' => ITEM_VALUE_TYPE_UINT64,
-						'delay' => 5
-					],
-					[
-						'name' => 'SNMP agent',
-						'key_' => 'snmp',
-						'type' => ITEM_TYPE_SNMP,
-						'value_type' => ITEM_VALUE_TYPE_UINT64,
-						'snmp_oid' => 'walk[222]',
-						'delay' => 5
-					],
-					[
-						'name' => 'External check',
-						'key_' => 'external',
-						'type' => ITEM_TYPE_EXTERNAL,
-						'value_type' => ITEM_VALUE_TYPE_UINT64,
-						'delay' => 5
-					],
-					[
-						'name' => 'Database monitor',
-						'key_' => 'database',
-						'type' => ITEM_TYPE_DB_MONITOR,
-						'value_type' => ITEM_VALUE_TYPE_UINT64,
-						'params' => 'test',
-						'delay' => 5
-					],
-					[
-						'name' => 'HTTP agent',
-						'key_' => 'http',
-						'type' => ITEM_TYPE_HTTPAGENT,
-						'value_type' => ITEM_VALUE_TYPE_UINT64,
-						'url' => 'test',
-						'delay' => 5
-					],
-					[
-						'name' => 'SSH agent',
-						'key_' => 'ssh',
-						'type' => ITEM_TYPE_SSH,
-						'value_type' => ITEM_VALUE_TYPE_UINT64,
-						'params' => 'test',
-						'username' => 'test_username',
-						'delay' => 5
-					],
-					[
-						'name' => 'TELNET agent',
-						'key_' => 'telnet',
-						'type' => ITEM_TYPE_TELNET,
-						'value_type' => ITEM_VALUE_TYPE_UINT64,
-						'params' => 'test',
-						'username' => 'test_username',
-						'delay' => 5
-					],
-					[
-						'name' => 'Script',
-						'key_' => 'script',
-						'type' => ITEM_TYPE_SCRIPT,
-						'value_type' => ITEM_VALUE_TYPE_UINT64,
-						'params' => 'test',
-						'delay' => 5
+						'type'=> INTERFACE_TYPE_SNMP,
+						'main' => INTERFACE_PRIMARY,
+						'useip' => INTERFACE_USE_DNS,
+						'ip' => '',
+						'dns' => 'snmp',
+						'port' => '2',
+						'details' => [
+							'version' => 1,
+							'community' => '{$SNMP_COMMUNITY}'
+						]
 					]
 				],
 				'discoveryrules' => [
@@ -160,235 +116,128 @@ class testHostsTimeouts extends testTimeoutsDisplay {
 						'key_' => 'zabbix_agent_drule',
 						'type' => ITEM_TYPE_ZABBIX,
 						'delay' => 5
-					],
-					[
-						'name' => 'Zabbix agent (active)',
-						'key_' => 'zabbix_agent_drule_active',
-						'type' => ITEM_TYPE_ZABBIX_ACTIVE,
-						'delay' => 5
-					],
-					[
-						'name' => 'Simple check',
-						'key_' => 'simple_check_drule',
-						'type' => ITEM_TYPE_SIMPLE,
-						'delay' => 5
-					],
-					[
-						'name' => 'SNMP agent',
-						'key_' => 'snmp_drule',
-						'type' => ITEM_TYPE_SNMP,
-						'snmp_oid' => 'walk[222]',
-						'delay' => 5
-					],
-					[
-						'name' => 'External check',
-						'key_' => 'external_drule',
-						'type' => ITEM_TYPE_EXTERNAL,
-						'delay' => 5
-					],
-					[
-						'name' => 'Database monitor',
-						'key_' => 'database_drule',
-						'type' => ITEM_TYPE_DB_MONITOR,
-						'params' => 'test',
-						'delay' => 5
-					],
-					[
-						'name' => 'HTTP agent',
-						'key_' => 'http_drule',
-						'type' => ITEM_TYPE_HTTPAGENT,
-						'url' => 'test',
-						'delay' => 5
-					],
-					[
-						'name' => 'SSH agent',
-						'key_' => 'ssh_drule',
-						'type' => ITEM_TYPE_SSH,
-						'params' => 'test',
-						'username' => 'test_username',
-						'delay' => 5
-					],
-					[
-						'name' => 'TELNET agent',
-						'key_' => 'telnet_drule',
-						'type' => ITEM_TYPE_TELNET,
-						'params' => 'test',
-						'username' => 'test_username',
-						'delay' => 5
-					],
-					[
-						'name' => 'Script',
-						'key_' => 'script_drule',
-						'type' => ITEM_TYPE_SCRIPT,
-						'params' => 'test',
-						'delay' => 5
 					]
 				]
 			]
 		]);
 		self::$hostid = $host_result['hostids'];
 		self::$hostid_druleids = $host_result['discoveryruleids'];
-		self::$interfaceid = CDataHelper::getInterfaces([self::$hostid['Host for timeouts check']]);
 
-		CDataHelper::call('itemprototype.create', [
+		CDataHelper::call('host.update',
 			[
-				'name' => 'Zabbix agent',
-				'key_' => 'zabbix_agent_[{#KEY}]',
-				'hostid' => self::$hostid['Host for timeouts check'],
-				'ruleid' => self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'],
-				'type' => ITEM_TYPE_ZABBIX,
-				'value_type' => ITEM_VALUE_TYPE_UINT64,
-				'interfaceid' => self::$interfaceid['ids']['Host for timeouts check']['zabbix_agent:1'],
-				'delay' => 5
-			],
-			[
-				'name' => 'Zabbix agent (active)',
-				'key_' => 'zabbix_agent_active_[{#KEY}]',
-				'hostid' => self::$hostid['Host for timeouts check'],
-				'ruleid' => self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'],
-				'type' => ITEM_TYPE_ZABBIX_ACTIVE,
-				'value_type' => ITEM_VALUE_TYPE_UINT64,
-				'delay' => 5
-			],
-			[
-				'name' => 'Simple check',
-				'key_' => 'simple_check_[{#KEY}]',
-				'hostid' => self::$hostid['Host for timeouts check'],
-				'ruleid' => self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'],
-				'type' => ITEM_TYPE_SIMPLE,
-				'value_type' => ITEM_VALUE_TYPE_UINT64,
-				'interfaceid' => self::$interfaceid['ids']['Host for timeouts check']['zabbix_agent:1'],
-				'delay' => 5
-			],
-			[
-				'name' => 'SNMP agent',
-				'key_' => 'snmp_[{#KEY}]',
-				'hostid' => self::$hostid['Host for timeouts check'],
-				'ruleid' => self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'],
-				'type' => ITEM_TYPE_SNMP,
-				'value_type' => ITEM_VALUE_TYPE_UINT64,
-				'interfaceid' => self::$interfaceid['ids']['Host for timeouts check']['snmp:2'],
-				'snmp_oid' => 'walk[222]',
-				'delay' => 5
-			],
-			[
-				'name' => 'External check',
-				'key_' => 'external_[{#KEY}]',
-				'hostid' => self::$hostid['Host for timeouts check'],
-				'ruleid' => self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'],
-				'type' => ITEM_TYPE_EXTERNAL,
-				'value_type' => ITEM_VALUE_TYPE_UINT64,
-				'interfaceid' => self::$interfaceid['ids']['Host for timeouts check']['zabbix_agent:1'],
-				'delay' => 5
-			],
-			[
-				'name' => 'Database monitor',
-				'key_' => 'database_[{#KEY}]',
-				'hostid' => self::$hostid['Host for timeouts check'],
-				'ruleid' => self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'],
-				'type' => ITEM_TYPE_DB_MONITOR,
-				'value_type' => ITEM_VALUE_TYPE_UINT64,
-				'params' => 'test',
-				'delay' => 5
-			],
-			[
-				'name' => 'HTTP agent',
-				'key_' => 'http_[{#KEY}]',
-				'hostid' => self::$hostid['Host for timeouts check'],
-				'ruleid' => self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'],
-				'type' => ITEM_TYPE_HTTPAGENT,
-				'value_type' => ITEM_VALUE_TYPE_UINT64,
-				'url' => 'test',
-				'delay' => 5
-			],
-			[
-				'name' => 'SSH agent',
-				'key_' => 'ssh_[{#KEY}]',
-				'hostid' => self::$hostid['Host for timeouts check'],
-				'ruleid' => self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'],
-				'type' => ITEM_TYPE_SSH,
-				'value_type' => ITEM_VALUE_TYPE_UINT64,
-				'interfaceid' => self::$interfaceid['ids']['Host for timeouts check']['zabbix_agent:1'],
-				'params' => 'test',
-				'username' => 'test_username',
-				'delay' => 5
-			],
-			[
-				'name' => 'TELNET agent',
-				'key_' => 'telnet_[{#KEY}]',
-				'hostid' => self::$hostid['Host for timeouts check'],
-				'ruleid' => self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'],
-				'type' => ITEM_TYPE_TELNET,
-				'value_type' => ITEM_VALUE_TYPE_UINT64,
-				'interfaceid' => self::$interfaceid['ids']['Host for timeouts check']['zabbix_agent:1'],
-				'params' => 'test',
-				'username' => 'test_username',
-				'delay' => 5
-			],
-			[
-				'name' => 'Script',
-				'key_' => 'script_[{#KEY}]',
-				'hostid' => self::$hostid['Host for timeouts check'],
-				'ruleid' => self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'],
-				'type' => ITEM_TYPE_SCRIPT,
-				'value_type' => ITEM_VALUE_TYPE_UINT64,
-				'params' => 'test',
-				'delay' => 5
+				[
+					'hostid' => self::$hostid['Host for timeouts check with proxy'],
+					'proxyid' => $proxyid['Proxy assigned to host']
+				]
 			]
-		]);
+		);
 	}
 
 	public function testHostsTimeouts_checkItemsMacros() {
 		$link = 'zabbix.php?action=item.list&context=host&filter_set=1&filter_hostids%5B0%5D='.
 				self::$hostid['Host for timeouts check'];
-		$this->checkGlobal('macros', $link, 'name:item_list');
+		$this->checkGlobal('global_macros', $link, 'Create item');
 	}
 
 	public function testHostsTimeouts_checkDiscoveryMacros() {
 		$link = 'host_discovery.php?filter_set=1&context=host&filter_hostids%5B0%5D='.
-			self::$hostid['Host for timeouts check'];
-		$this->checkGlobal('macros', $link, 'name:discovery');
+				self::$hostid['Host for timeouts check'];
+		$this->checkGlobal('global_macros', $link, 'Create discovery rule');
 	}
 
 	public function testHostsTimeouts_checkPrototypeMacros() {
 		$link = 'zabbix.php?action=item.prototype.list&context=host&parent_discoveryid='.
-			self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'];
-		$this->checkGlobal('macros', $link, 'name:itemprototype');
+				self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'];
+		$this->checkGlobal('global_macros', $link, 'Create item prototype');
 	}
 
-	public function testHostsTimeouts_checkItemsSeconds() {
+	public function testHostsTimeouts_checkItemsCustom() {
 		$link = 'zabbix.php?action=item.list&context=host&filter_set=1&filter_hostids%5B0%5D='.
 				self::$hostid['Host for timeouts check'];
-		$this->checkGlobal('seconds', $link, 'name:item_list');
+		$this->checkGlobal('global_custom', $link, 'Create item');
 	}
 
-	public function testHostsTimeouts_checkDiscoverySeconds() {
+	public function testHostsTimeouts_checkDiscoveryCustom() {
 		$link = 'host_discovery.php?filter_set=1&context=host&filter_hostids%5B0%5D='.
-			self::$hostid['Host for timeouts check'];
-		$this->checkGlobal('seconds', $link, 'name:discovery');
+				self::$hostid['Host for timeouts check'];
+		$this->checkGlobal('global_custom', $link, 'Create discovery rule');
 	}
 
-	public function testHostsTimeouts_checkPrototypeSeconds() {
+	public function testHostsTimeouts_checkPrototypeCustom() {
 		$link = 'zabbix.php?action=item.prototype.list&context=host&parent_discoveryid='.
-			self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'];
-		$this->checkGlobal('seconds', $link, 'name:itemprototype');
+				self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'];
+		$this->checkGlobal('global_custom', $link, 'Create item prototype');
 	}
 
 	public function testHostsTimeouts_checkItemsDefault() {
 		$link = 'zabbix.php?action=item.list&context=host&filter_set=1&filter_hostids%5B0%5D='.
 				self::$hostid['Host for timeouts check'];
-		$this->checkGlobal('reset', $link, 'name:item_list');
+		$this->checkGlobal('global_default', $link, 'Create item');
 	}
 
 	public function testHostsTimeouts_checkDiscoveryDefault() {
 		$link = 'host_discovery.php?filter_set=1&context=host&filter_hostids%5B0%5D='.
-			self::$hostid['Host for timeouts check'];
-		$this->checkGlobal('reset', $link, 'name:discovery');
+				self::$hostid['Host for timeouts check'];
+		$this->checkGlobal('global_default', $link, 'Create discovery rule');
 	}
 
 	public function testHostsTimeouts_checkPrototypeDefault() {
 		$link = 'zabbix.php?action=item.prototype.list&context=host&parent_discoveryid='.
-			self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'];
-		$this->checkGlobal('reset', $link, 'name:itemprototype');
+				self::$hostid_druleids['Host for timeouts check:zabbix_agent_drule'];
+		$this->checkGlobal('global_default', $link, 'Create item prototype');
+	}
+
+	public function testHostsTimeouts_checkItemsProxyDefault() {
+		$link = 'zabbix.php?action=item.list&context=host&filter_set=1&filter_hostids%5B0%5D='.
+				self::$hostid['Host for timeouts check with proxy'];
+		$this->checkGlobal('global_default', $link, 'Create item', true);
+	}
+
+	public function testHostsTimeouts_checkDiscoveryProxyDefault() {
+		$link = 'host_discovery.php?filter_set=1&context=host&filter_hostids%5B0%5D='.
+				self::$hostid['Host for timeouts check with proxy'];
+		$this->checkGlobal('global_default', $link, 'Create discovery rule', true);
+	}
+
+	public function testHostsTimeouts_checkPrototypeProxyDefault() {
+		$link = 'zabbix.php?action=item.prototype.list&context=host&parent_discoveryid='.
+				self::$hostid_druleids['Host for timeouts check with proxy:zabbix_agent_drule'];
+		$this->checkGlobal('global_default', $link, 'Create item prototype', true);
+	}
+
+	public function testHostsTimeouts_checkItemsProxyMacros() {
+		$link = 'zabbix.php?action=item.list&context=host&filter_set=1&filter_hostids%5B0%5D='.
+				self::$hostid['Host for timeouts check with proxy'];
+		$this->checkGlobal('proxy_macros', $link, 'Create item', true);
+	}
+
+	public function testHostsTimeouts_checkDiscoveryProxyMacros() {
+		$link = 'host_discovery.php?filter_set=1&context=host&filter_hostids%5B0%5D='.
+				self::$hostid['Host for timeouts check with proxy'];
+		$this->checkGlobal('proxy_macros', $link, 'Create discovery rule', true);
+	}
+
+	public function testHostsTimeouts_checkPrototypeProxyMacros() {
+		$link = 'zabbix.php?action=item.prototype.list&context=host&parent_discoveryid='.
+				self::$hostid_druleids['Host for timeouts check with proxy:zabbix_agent_drule'];
+		$this->checkGlobal('proxy_macros', $link, 'Create item prototype', true);
+	}
+
+	public function testHostsTimeouts_checkItemsProxyCustom() {
+		$link = 'zabbix.php?action=item.list&context=host&filter_set=1&filter_hostids%5B0%5D='.
+				self::$hostid['Host for timeouts check with proxy'];
+		$this->checkGlobal('proxy_custom', $link, 'Create item', true);
+	}
+
+	public function testHostsTimeouts_checkDiscoveryProxyCustom() {
+		$link = 'host_discovery.php?filter_set=1&context=host&filter_hostids%5B0%5D='.
+				self::$hostid['Host for timeouts check with proxy'];
+		$this->checkGlobal('proxy_custom', $link, 'Create discovery rule', true);
+	}
+
+	public function testHostsTimeouts_checkPrototypeProxyCustom() {
+		$link = 'zabbix.php?action=item.prototype.list&context=host&parent_discoveryid='.
+				self::$hostid_druleids['Host for timeouts check with proxy:zabbix_agent_drule'];
+		$this->checkGlobal('proxy_custom', $link, 'Create item prototype', true);
 	}
 }
