c100436687a313f4e7367ef0c032ea5b7ed4ad4b
Mihails Prihodko <mihails.prihodko@zabbix.com>
2023-11-16 11:04:35 +0200

    ....I..... [ZBX-23580] fixed memory leak

--- src/libs/zbxdbupgrade/dbupgrade_6050.c
+++ src/libs/zbxdbupgrade/dbupgrade_6050.c
@@ -1719,8 +1719,10 @@ static int	DBpatch_6050144(void)
 	int			ret = SUCCEED;
 	size_t			sql_alloc = 0, sql_offset = 0;
 	char			*sql = NULL, *params = NULL;
+	zbx_eval_context_t	ctx;
 	zbx_vector_uint32_t	del_tokens;
 
+	zbx_eval_init(&ctx);
 	zbx_vector_uint32_create(&del_tokens);
 
 	zbx_db_begin_multiple_update(&sql, &sql_alloc, &sql_offset);
@@ -1732,7 +1734,8 @@ static int	DBpatch_6050144(void)
 	{
 		int			i;
 		char			*error = NULL;
-		zbx_eval_context_t	ctx;
+
+		zbx_eval_clear(&ctx);
 
 		if (FAIL == zbx_eval_parse_expression(&ctx, row[1], ZBX_EVAL_PARSE_CALC_EXPRESSION, &error))
 		{
@@ -1790,6 +1793,7 @@ static int	DBpatch_6050144(void)
 			ret = FAIL;
 	}
 
+	zbx_eval_clear(&ctx);
 	zbx_vector_uint32_destroy(&del_tokens);
 
 	zbx_free(sql);
