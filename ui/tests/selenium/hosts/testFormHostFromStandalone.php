f6218de36512c8ef24bdfa2e0b51220be032e12f
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2023-12-14 15:05:35 +0200

    .......... [ZBXNEXT-5878] removed todo

--- ui/tests/selenium/hosts/testFormHostFromStandalone.php
+++ ui/tests/selenium/hosts/testFormHostFromStandalone.php
@@ -224,14 +224,7 @@ class testFormHostFromStandalone extends testFormHost {
 
 		$form->submit();
 		$this->page->waitUntilReady();
-
-		// TODO: Update the message details after ZBX-21366 is merged.
-		$message_details = [
-			'Templates "Test of discovered host 2 template for clear" unlinked from hosts "Discovered host from prototype 1".',
-			'Templates "Test of discovered host 1 template for unlink" unlinked from hosts "Discovered host from prototype 1".'
-		];
-
-		$this->assertMessage(TEST_GOOD, 'Host updated', $message_details);
+		$this->assertMessage(TEST_GOOD, 'Host updated');
 
 		foreach ($filtered_results as $result) {
 			// Open hosts objects and check objects on Discovered host after unlinking some templates.
