c6c427261eecd0fbbbe9f9cdd41fc7b4dfa28834
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2023-12-15 16:18:31 +0200

    ........S. [ZBXNEXT-5878] fixed cmocka tests

--- tests/libs/zbxpreproc/zbx_item_preproc.c
+++ tests/libs/zbxpreproc/zbx_item_preproc.c
@@ -31,6 +31,7 @@
 #include "libs/zbxpreproc/pp_execute.h"
 #include "libs/zbxpreproc/preproc_snmp.h"
 #include "libs/zbxpreproc/pp_cache.h"
+#include "libs/zbxpreproc/pp_error.h"
 
 #ifdef HAVE_NETSNMP
 #define SNMP_NO_DEBUGGING
@@ -317,6 +318,7 @@ void	zbx_mock_test_entry(void **state)
 	if (1 == mib_translation_case && FAIL == check_mib_existence(&step))
 	{
 		preproc_shutdown_snmp();
+		release_step(&step);
 		skip();
 	}
 #endif
