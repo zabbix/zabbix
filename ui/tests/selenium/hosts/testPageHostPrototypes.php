c281e6983ac99ede87367424ee9c5204d03c3de2
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2023-12-27 13:24:51 +0200

    .......... [DEV-2735] added delete scenario

--- ui/tests/selenium/hosts/testPageHostPrototypes.php
+++ ui/tests/selenium/hosts/testPageHostPrototypes.php
@@ -343,218 +343,71 @@ class testPageHostPrototypes extends CWebTest {
 		}
 	}
 
-	public function testPageHostPrototypes_SimpleDelete() {
-		$this->page->login()->open('host_prototypes.php?context=host&sort=name&sortorder=ASC&parent_discoveryid='.
-				self::$host_druleids['Host for host prototype check:drule'])->waitUntilReady();
-		$sql = 'SELECT null FROM hosts WHERE hostid='.self::$prototype_hostids['1 Host prototype monitored discovered {#H}'];
-
-		// Check that host prototype exists in DB and displayed in Host prototype table.
-		$this->assertEquals(1, CDBHelper::getCount($sql));
-		$this->assertTrue(in_array('1 Host prototype monitored discovered {#H}', $this->getTableColumnData('Name')));
-
-		// Select host prototype and delete it.
-		$this->selectTableRows('1 Host prototype monitored discovered {#H}');
-		$this->query('button:Delete')->one()->click();
-		$this->page->acceptAlert();
-		$this->page->waitUntilReady();
-
-		// Check that host prototype doesn't exist in DB and not displayed in Host prototype table.
-		$this->assertFalse(in_array('1 Host prototype monitored discovered {#H}', $this->getTableColumnData('Name')));
-		$this->assertEquals(0, CDBHelper::getCount($sql));
+	public static function getDeleteData() {
+		return [
+			// #0 Cancel delete.
+			[
+				[
+					'name' => ['1 Host prototype monitored discovered {#H}'],
+					'cancel' => true
+				]
+			],
+			// #1 Delete one.
+			[
+				[
+					'name' => ['2 Host prototype not monitored discovered {#H}'],
+					'message' => 'Host prototype deleted'
+				]
+			],
+			// #2 Delete more than 1.
+			[
+				[
+					'name' => [
+						'3 Host prototype not monitored not discovered {#H}',
+						'4 Host prototype monitored not discovered {#H}'
+					],
+					'message' => 'Host prototypes deleted'
+				]
+			]
+		];
 	}
 
-//	const DICROVERY_RULE_ID = 90001;
-//	const HOST_PROTOTYPES_COUNT = 8;
-//
-//	public function testPageHostPrototypes_CheckLayout() {
-//		$this->zbxTestLogin('host_prototypes.php?parent_discoveryid='.self::DICROVERY_RULE_ID.'&context=host');
-//		$this->zbxTestCheckTitle('Configuration of host prototypes');
-//		$this->zbxTestCheckHeader('Host prototypes');
-//
-//		$table = $this->query('xpath://form[@name="hosts"]/table[@class="list-table"]')->asTable()->one();
-//		$headers = ['', 'Name', 'Templates', 'Create enabled', 'Discover', 'Tags'];
-//		$this->assertSame($headers, $table->getHeadersText());
-//
-//		foreach (['Create enabled', 'Create disabled', 'Delete'] as $button) {
-//			$element = $this->query('button', $button)->one();
-//			$this->assertTrue($element->isPresent());
-//			$this->assertFalse($element->isEnabled());
-//		}
-//
-//		$this->assertTableStats(self::HOST_PROTOTYPES_COUNT);
-//
-//		// Check tags on the specific host prototype.
-//		$tags = $table->findRow('Name', 'Host prototype {#1}')->getColumn('Tags')->query('class:tag')->all();
-//		$this->assertEquals(['host_proto_tag_1: value1', 'host_proto_tag_2: value2'], $tags->asText());
-//
-//		foreach ($tags as $tag) {
-//			$tag->click();
-//			$hint = $this->query('xpath://div[@data-hintboxid]')
-//					->asOverlayDialog()->waitUntilPresent()->all()->last();
-//			$this->assertEquals($tag->getText(), $hint->getText());
-//			$hint->close();
-//		}
-//	}
-//
-//	public static function getSelectedData() {
-//		return [
-//			[
-//				[
-//					'item' => 'Discovery rule 1',
-//					'hosts' => [
-//						'Host prototype {#1}'
-//					]
-//				]
-//			],
-//			[
-//				[
-//					'item' => 'Discovery rule 2',
-//					'hosts' => 'all'
-//				]
-//			],
-//			[
-//				[
-//					'item' => 'Discovery rule 3',
-//					'hosts' => [
-//						'Host prototype {#7}',
-//						'Host prototype {#9}',
-//						'Host prototype {#10}'
-//					]
-//				]
-//			]
-//		];
-//	}
-//
-//	/**
-//	 * Select specified hosts from host prototype page.
-//	 *
-//	 * @param array $data	test case data from data provider
-//	 */
-//	private function selectHostPrototype($data) {
-//		$discoveryid = DBfetch(DBselect("SELECT itemid FROM items WHERE name=".zbx_dbstr($data['item'])));
-//		$this->zbxTestLogin("host_prototypes.php?parent_discoveryid=".$discoveryid['itemid'].'&context=host');
-//
-//		if ($data['hosts'] === 'all') {
-//			$this->zbxTestCheckboxSelect('all_hosts');
-//			return;
-//		}
-//
-//		foreach ($data['hosts'] as $host) {
-//			$result = DBselect('SELECT hostid FROM hosts WHERE host='.zbx_dbstr($host));
-//			while ($row = DBfetch($result)) {
-//				$this->zbxTestCheckboxSelect('group_hostid_'.$row['hostid']);
-//			}
-//		}
-//	}
-//
-//	/**
-//	 * Check specific page action.
-//	 * Actions are defined by buttons pressed on page.
-//	 *
-//	 * @param array  $data		test case data from data provider
-//	 * @param string $action	button text (action to be executed)
-//	 * @param int    $status	host status to be checked in DB
-//	 */
-//	protected function checkPageAction($data, $action, $status = null) {
-//		// Click on button with required action.
-//		if ($action === 'Click on state') {
-//			foreach ($data['hosts'] as $host) {
-//				$id = DBfetch(DBselect('SELECT hostid FROM hosts WHERE name='.zbx_dbstr($host)));
-//				$this->zbxTestClickXpathWait("//a[contains(@onclick,'group_hostid%5B%5D=".$id['hostid']."')]");
-//			}
-//		}
-//		else {
-//			$this->selectHostPrototype($data);
-//			$this->zbxTestClickButtonText($action);
-//			$this->zbxTestAcceptAlert();
-//		}
-//
-//		$this->zbxTestIsElementPresent('//*[@class="msg-good"]');
-//		$this->zbxTestCheckTitle('Configuration of host prototypes');
-//		$this->zbxTestCheckHeader('Host prototypes');
-//
-//		// Create query part for status (if any).
-//		$status_criteria = ($status !== null) ? (' AND status='.$status) : '';
-//
-//		// Check the results in DB.
-//		if ($data['hosts'] === 'all') {
-//			$sql = 'SELECT NULL'.
-//						' FROM hosts'.
-//						' WHERE hostid IN ('.
-//							'SELECT hostid'.
-//							' FROM host_discovery'.
-//							' WHERE parent_itemid IN ('.
-//								'SELECT itemid'.
-//								' FROM items'.
-//								' WHERE name='.zbx_dbstr($data['item']).
-//							')'.
-//						')';
-//		}
-//		else {
-//			$names = [];
-//			foreach ($data['hosts'] as $host) {
-//				$names[] = zbx_dbstr($host);
-//			}
-//
-//			$sql = 'SELECT NULL'.
-//					' FROM hosts'.
-//					' WHERE host IN ('.implode(',', $names).')';
-//		}
-//
-//		$this->assertEquals(0, CDBHelper::getCount($sql.$status_criteria));
-//	}
-//
-//	/**
-//	 * @dataProvider getSelectedData
-//	 */
-//	public function testPageHostPrototypes_DisableSelected($data) {
-//		$this->checkPageAction($data, 'Create disabled', HOST_STATUS_MONITORED);
-//	}
-//
-//	/**
-//	 * @dataProvider getSelectedData
-//	 */
-//	public function testPageHostPrototypes_EnableSelected($data) {
-//		$this->checkPageAction($data, 'Create enabled', HOST_STATUS_NOT_MONITORED);
-//	}
-//
-//	/**
-//	 * @dataProvider getSelectedData
-//	 */
-//	public function testPageHostPrototypes_DeleteSelected($data) {
-//		$this->checkPageAction($data, 'Delete');
-//	}
-//
-//	public static function getHostPrototypeData() {
-//		return [
-//			[
-//				[
-//					'item' => 'Discovery rule 1',
-//					'hosts' => [
-//						'Host prototype {#2}'
-//					],
-//					'status' => HOST_STATUS_NOT_MONITORED
-//				]
-//			],
-//			[
-//				[
-//					'item' => 'Discovery rule 1',
-//					'hosts' => [
-//						'Host prototype {#3}'
-//					],
-//					'status' => HOST_STATUS_MONITORED
-//				]
-//			]
-//		];
-//	}
-//
-//	/**
-//	 * @dataProvider getHostPrototypeData
-//	 */
-//	public function testPageHostPrototypes_SingleEnableDisable($data) {
-//		$discoveryid = DBfetch(DBselect("SELECT itemid FROM items WHERE name=".zbx_dbstr($data['item'])));
-//		$this->zbxTestLogin("host_prototypes.php?parent_discoveryid=".$discoveryid['itemid'].'&context=host');
-//
-//		$this->checkPageAction($data, 'Click on state', $data['status']);
-//	}
+	/**
+	 * @dataProvider getDeleteData
+	 */
+	public function testPageHostPrototypes_Delete($data) {
+		$this->page->login()->open('host_prototypes.php?context=host&sort=name&sortorder=ASC&parent_discoveryid='.
+				self::$host_druleids['Host for host prototype check:drule'])->waitUntilReady();
+		$sql = 'SELECT null FROM hosts WHERE hostid=';
+
+		// Check that host prototype exists in DB and displayed in Host prototype table.
+		foreach ($data['name'] as $name) {
+			$this->assertTrue(in_array($name, $this->getTableColumnData('Name')));
+			$this->assertEquals(1, CDBHelper::getCount($sql.self::$prototype_hostids[$name]));
+		}
+
+		// Select host prototype and click on Delete button.
+		$this->selectTableRows($data['name']);
+		$this->query('button:Delete')->one()->click();
+
+		// Check that after canceling Delete, host prototype still exists in DB nad displayed in table.
+		if (array_key_exists('cancel', $data)) {
+			$this->page->dismissAlert();
+			foreach ($data['name'] as $name) {
+				$this->assertTrue(in_array($name, $this->getTableColumnData('Name')));
+				$this->assertEquals(1, CDBHelper::getCount($sql.self::$prototype_hostids[$name]));
+			}
+		}
+		else {
+			$this->page->acceptAlert();
+			$this->page->waitUntilReady();
+			$this->assertMessage(TEST_GOOD, $data['message']);
+
+			// Check that host prototype doesn't exist in DB and not displayed in Host prototype table.
+			foreach ($data['name'] as $name) {
+				$this->assertFalse(in_array($name, $this->getTableColumnData('Name')));
+				$this->assertEquals(0, CDBHelper::getCount($sql.self::$prototype_hostids[$name]));
+			}
+		}
+	}
 }
