0b89e0bae0f03a9412df56178b81bdda7fa09467
Diana Dauksa <diana.dauksa@zabbix.com>
2024-01-08 13:36:37 +0200

    ..F....... [ZBX-22961] fixed ResizeObserver observation error appearing in Safari browser

--- ui/js/class.form.fieldset.collapsible.js
+++ ui/js/class.form.fieldset.collapsible.js
@@ -42,7 +42,12 @@ class CFormFieldsetCollapsible {
 		});
 
 		for (const element of this._observed_fields) {
-			new ResizeObserver(() => this._update()).observe(element);
+			new ResizeObserver(() => {
+				// Use of setTimeout() to prevent ResizeObserver observation error in Safari.
+				setTimeout(() => {
+					this._update();
+				});
+			}).observe(element);
 		}
 	}
 
