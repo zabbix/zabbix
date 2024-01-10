6cc66ed6559ab21a878eddfd521b122492e0029b
Mihails Prihodko <mihails.prihodko@zabbix.com>
2024-01-10 17:00:51 +0200

    ........S. [ZBXNEXT-8586] added support of Jira media type for internal events

--- src/libs/zbxevent/event.c
+++ src/libs/zbxevent/event.c
@@ -266,6 +266,11 @@ void	zbx_event_get_macro_value(const char *macro, const zbx_db_event *event, cha
 			zbx_event_get_tag(macro + ZBX_CONST_STRLEN(MVAR_EVENT_TAGS_PREFIX), event, replace_to);
 		}
 	}
+	else if (EVENT_SOURCE_INTERNAL == event->source &&
+			0 == strncmp(macro, MVAR_EVENT_TAGS_PREFIX, ZBX_CONST_STRLEN(MVAR_EVENT_TAGS_PREFIX)))
+	{
+		zbx_event_get_tag(macro + ZBX_CONST_STRLEN(MVAR_EVENT_TAGS_PREFIX), event, replace_to);
+	}
 	else if (EVENT_SOURCE_SERVICE == event->source)
 	{
 		if (0 == strcmp(macro, MVAR_EVENT_NSEVERITY))
