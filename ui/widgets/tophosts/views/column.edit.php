95df4e1748ee5f4e9c10bb49b34cba597e121bdf
Dace Petra <dace.petra@zabbix.com>
2024-01-11 17:22:53 +0200

    ..F....... [ZBX-23587] fixed widget input field selecting when clicking on label

--- ui/widgets/tophosts/views/column.edit.php
+++ ui/widgets/tophosts/views/column.edit.php
@@ -165,7 +165,7 @@ $form_grid->addItem([
 
 // Base color.
 $form_grid->addItem([
-	new CLabel(_('Base color'), 'base_color'),
+	new CLabel(_('Base color'), 'lbl_base_color'),
 	new CFormField(new CColor('base_color', $data['base_color']))
 ]);
 
@@ -227,10 +227,11 @@ $form_grid->addItem([
 			(makeWarningIcon(_('With this setting only numeric items will be displayed.')))
 				->setId('tophosts-column-aggregate-function-warning')
 		],
-		'aggregate_function'
+		'column_aggregate_function'
 	),
 	new CFormField(
-		(new CSelect('aggregate_function'))->setId('aggregate_function')
+		(new CSelect('aggregate_function'))
+			->setId('aggregate_function')
 			->setValue($data['aggregate_function'])
 			->addOptions(CSelect::createOptionsFromArray([
 				AGGREGATE_NONE => CItemHelper::getAggregateFunctionName(AGGREGATE_NONE),
@@ -242,6 +243,7 @@ $form_grid->addItem([
 				AGGREGATE_FIRST => CItemHelper::getAggregateFunctionName(AGGREGATE_FIRST),
 				AGGREGATE_LAST => CItemHelper::getAggregateFunctionName(AGGREGATE_LAST)
 			]))
+			->setFocusableElementId('column_aggregate_function')
 	)
 ]);
 
