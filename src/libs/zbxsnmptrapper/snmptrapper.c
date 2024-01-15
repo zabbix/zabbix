10a47f590be51fdb836163ee8fceb0016bad0fd7
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2024-01-15 09:42:20 +0200

    .......PS. [ZBX-21192] fixed SNMP traps continuing from wrong location when HA node is switched

--- src/libs/zbxsnmptrapper/snmptrapper.c
+++ src/libs/zbxsnmptrapper/snmptrapper.c
@@ -17,8 +17,6 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-#define _GNU_SOURCE
-#include <time.h>
 #include "zbxsnmptrapper.h"
 
 #include "zbxtimekeeper.h"
