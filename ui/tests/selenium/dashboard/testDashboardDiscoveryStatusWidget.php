d2305e24ae92561c604d868a260f1eeba48ac0d1
Nikita Gogolevs <nikita.gogolevs@zabbix.com>
2024-01-17 16:33:36 +0200

    .......... [DEV-2717] removed profiles from backup

--- ui/tests/selenium/dashboard/testDashboardDiscoveryStatusWidget.php
+++ ui/tests/selenium/dashboard/testDashboardDiscoveryStatusWidget.php
@@ -25,7 +25,7 @@ require_once dirname(__FILE__).'/../behaviors/CMessageBehavior.php';
 require_once dirname(__FILE__).'/../behaviors/CTableBehavior.php';
 
 /**
- * @backup widget, profiles
+ * @backup widget
  *
  * @onBefore prepareDiscoveryStatusWidgetData
  *
