187486d507f86446ef9875d4b570c02589b18863
Continuous Integration <ci@zabbix.com>
2024-01-22 12:44:00 +0200

    .......... [ZBXNEXT-826] updated release date for 5.0.41rc1

--- include/version.h
+++ include/version.h
@@ -24,7 +24,7 @@
 #define ZBX_STR(str)	ZBX_STR2(str)
 
 #define APPLICATION_NAME	"Zabbix Agent"
-#define ZABBIX_REVDATE		"30 November 2023"
+#define ZABBIX_REVDATE		"22 January 2024"
 #define ZABBIX_VERSION_MAJOR	5
 #define ZABBIX_VERSION_MINOR	0
 #define ZABBIX_VERSION_PATCH	41
--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -29,7 +29,7 @@ import (
 
 const (
 	APPLICATION_NAME        = "Zabbix Agent"
-	ZABBIX_REVDATE          = "30 November 2023"
+	ZABBIX_REVDATE          = "22 January 2024"
 	ZABBIX_VERSION_MAJOR    = 5
 	ZABBIX_VERSION_MINOR    = 0
 	ZABBIX_VERSION_PATCH    = 41
