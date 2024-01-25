2e545cb53b3403b84889a37f47d92ee1854dada7
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2024-01-25 16:56:31 +0200

    .......... [ZBXNEXT-686] removed trailing comma

--- ui/tests/selenium/users/testFormUserProfile.php
+++ ui/tests/selenium/users/testFormUserProfile.php
@@ -456,7 +456,7 @@ class testFormUserProfile extends CLegacyWebTest {
 				'expected' => TEST_GOOD,
 				'timeout' => '1d',
 				'suppressed' => false
-			]],
+			]]
 			// TODO: uncomment after ZBX-23993 is fixed.
 //			[[
 //				'expected' => TEST_GOOD
