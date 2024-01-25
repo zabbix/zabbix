90b14369f7aea7497b888fb2cf364f245c7056c7
Andrejs Kozlovs <andrejs.kozlovs@zabbix.com>
2024-01-25 09:05:04 +0200

    ........S. [ZBX-23814] fixed memory leak

--- src/libs/zbxexpression/expression.c
+++ src/libs/zbxexpression/expression.c
@@ -2774,7 +2774,7 @@ int	substitute_simple_macros_impl(const zbx_uint64_t *actionid, const zbx_db_eve
 			zbx_free(replace_to);
 		}
 
-		if (ZBX_TOKEN_FUNC_MACRO == token.type)
+		if (ZBX_TOKEN_FUNC_MACRO == token.type || ZBX_TOKEN_USER_FUNC_MACRO == token.type)
 			zbx_free(m_ptr);
 
 		pos++;
