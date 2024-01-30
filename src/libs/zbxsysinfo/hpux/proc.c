f5322bc3632e3cd1dca6500dd0f8e74e8085f26f
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2024-01-30 16:27:55 +0200

    ...G...... [ZBX-23888] added reporting of regex errors in proc metrics for hpux

--- src/libs/zbxsysinfo/hpux/proc.c
+++ src/libs/zbxsysinfo/hpux/proc.c
@@ -53,10 +53,11 @@ static int	check_procstate(struct pst_status pst, int zbx_proc_stat)
 int	proc_num(AGENT_REQUEST *request, AGENT_RESULT *result)
 {
 #define ZBX_BURST	((size_t)10)
-	char			*procname, *proccomm, *param;
+	char			*procname, *proccomm, *param, *rxp_error = NULL;
 	struct passwd		*usrinfo;
 	int			proccount = 0, invalid_user = 0, zbx_proc_stat, count, idx = 0;
 	struct pst_status	pst[ZBX_BURST];
+	zbx_regexp_t		*proccomm_rxp = NULL;
 
 	if (4 < request->nparam)
 	{
@@ -104,8 +105,22 @@ int	proc_num(AGENT_REQUEST *request, AGENT_RESULT *result)
 	}
 
 	proccomm = get_rparam(request, 3);
-	if (NULL != proccomm && '\0' == *proccomm)
-		proccomm = NULL;
+
+	if (NULL != proccomm)
+	{
+		if ('\0' == *proccomm)
+		{
+			proccomm = NULL;
+		}
+		else if (SUCCEED != zbx_regexp_compile(proccomm, &proccomm_rxp, &rxp_error))
+		{
+			SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Invalid regular expression in fourth parameter: "
+					"%s", rxp_error));
+
+			zbx_free(rxp_error);
+			return SYSINFO_RET_FAIL;
+		}
+	}
 
 	if (1 == invalid_user)	/* handle 0 for non-existent user after all parameters have been parsed and validated */
 		goto out;
@@ -133,7 +148,7 @@ int	proc_num(AGENT_REQUEST *request, AGENT_RESULT *result)
 				if (-1 == pstat(PSTAT_GETCOMMANDLINE, un, sizeof(cmdline), 1, pst[i].pst_pid))
 					continue;
 
-				if (NULL == zbx_regexp_match(cmdline, proccomm, NULL))
+				if (0 != zbx_regexp_match_precompiled(cmdline, proccomm_rxp))
 					continue;
 			}
 
@@ -154,6 +169,9 @@ int	proc_num(AGENT_REQUEST *request, AGENT_RESULT *result)
 	}
 out:
 	SET_UI64_RESULT(result, proccount);
+
+	if (NULL != proccomm_rxp)
+		zbx_regexp_free(proccomm_rxp);
 
 	return SYSINFO_RET_OK;
 #undef ZBX_BURST
