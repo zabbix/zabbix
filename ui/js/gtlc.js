6fc1fd52118839e40dabd962aac85a87e727e7ac
Andrejs Verza <andrejs.verza@zabbix.com>
2024-01-30 09:31:13 +0200

    ..F....... [ZBX-23817] fixed coding style

--- ui/js/gtlc.js
+++ ui/js/gtlc.js
@@ -303,11 +303,11 @@ jQuery(function($) {
 		.on('mousedown', 'img', selectionHandlerDragStart)
 		.on('dblclick', 'img', function(event) {
 			if (typeof $(event.target).data('zbx_sbox') !== 'undefined') {
-				const obj = event.target.hasAttribute('id')
+				const obj = event.target.id in timeControl.objectList
 					? timeControl.objectList[event.target.id]
-					: timeControl.objectList.charts_view;
+					: null;
 
-				if (obj.useCustomEvents !== 1) {
+				if (obj === null || obj.useCustomEvents !== 1) {
 					$.publish('timeselector.zoomout', {
 						from: element.from.val(),
 						to: element.to.val()
@@ -458,11 +458,11 @@ jQuery(function($) {
 			return cancelEvent(event);
 		}
 
-		const obj = event.data.target[0].hasAttribute('id')
+		const obj = event.data.target[0].id in timeControl.objectList
 			? timeControl.objectList[event.data.target[0].id]
-			: timeControl.objectList.charts_view;
+			: null;
 
-		if (obj.useCustomEvents !== 1) {
+		if (obj === null || obj.useCustomEvents !== 1) {
 			$.publish('timeselector.rangeoffset', {
 				from_offset: Math.ceil(from_offset),
 				to_offset: Math.ceil(to_offset)
