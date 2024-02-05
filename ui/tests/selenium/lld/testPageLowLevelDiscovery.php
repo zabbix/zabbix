3591a0757ce442ad747f3830a35f39d800bb9535
Natalja Romancaka <natalja.romancaka@zabbix.com>
2024-02-05 10:46:36 +0200

    .......... [ZBXNEXT-686] fixed Selenium test, changed templates data

--- ui/tests/selenium/lld/testPageLowLevelDiscovery.php
+++ ui/tests/selenium/lld/testPageLowLevelDiscovery.php
@@ -297,7 +297,7 @@ class testPageLowLevelDiscovery extends CWebTest {
 						'Template groups' => 'Templates/Databases'
 					],
 					'context' => 'template',
-					'rows' => 88
+					'rows' => 90
 				]
 			],
 			// #1
@@ -502,11 +502,11 @@ class testPageLowLevelDiscovery extends CWebTest {
 					'filter' => [
 						'Type' => 'Database monitor',
 						'Update interval' => '1h',
-						'Name'=> 'Non-local database'
+						'Name'=> 'PDB'
 					],
 					'context' => 'template',
 					'expected' => [
-						'Non-local database discovery'
+						'PDB discovery'
 					]
 				]
 			],
