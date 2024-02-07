7a510bcbe48d7d04f8eaa0a2d003b2d973cbd28c
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2024-02-07 10:39:01 +0200

    ...G...... [ZBX-23888] fixed return on solaris

--- src/libs/zbxsysinfo/solaris/proc.c
+++ src/libs/zbxsysinfo/solaris/proc.c
@@ -664,7 +664,8 @@ int	PROC_NUM(AGENT_REQUEST *request, AGENT_RESULT *result)
 					" with agent running on a Solaris version with zone support, but compiled on"
 					" a Solaris version without zone support. Consider using \"all\" or install"
 					" agent with Solaris zone support."));
-			return SYSINFO_RET_FAIL;
+			ret = SYSINFO_RET_FAIL;
+			goto clean;
 		}
 #endif
 	}
