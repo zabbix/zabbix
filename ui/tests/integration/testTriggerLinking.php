3dba80b9f99c023527841131eb7089f890502742
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2024-02-16 15:19:38 +0200

    ........S. [ZBX-24049] fixed test 8

--- ui/tests/integration/testTriggerLinking.php
+++ ui/tests/integration/testTriggerLinking.php
@@ -58,7 +58,8 @@ class testTriggerLinking extends CIntegrationTest {
 
 	private static $templateids = array();
 	private static $stringids = array();
-	private static $triggerids = array();
+	private static $triggerids_deps = array();
+	private static $triggers_same_descr_mapping_to_id = array();
 
 	public function createTemplates() {
 
@@ -177,7 +178,7 @@ class testTriggerLinking extends CIntegrationTest {
 
 			$this->assertArrayHasKey('triggerids', $response['result']);
 			$this->assertArrayHasKey(0, $response['result']['triggerids']);
-			array_push(self::$triggerids, $response['result']['triggerids']);
+			//array_push(self::$triggerids, $response['result']['triggerids']);
 
 			//$this->assertEquals("badger", $response['result']['triggerids'][0], $response['result']['triggerids'][0]);
 
@@ -217,7 +218,9 @@ class testTriggerLinking extends CIntegrationTest {
 
 			$this->assertArrayHasKey('triggerids', $response_2['result']);
 			$this->assertArrayHasKey(0, $response_2['result']['triggerids']);
-
+			//array_push(self::$triggerids, $response['result']['triggerids']);
+			self::$triggerids_deps[$response_2['result']['triggerids'][0]] = $response['result']['triggerids'];
+			self::$triggers_same_descr_mapping_to_id[$response_2['result']['triggerids'][0]] = $i
 		}
 
 		$this->setupActions();
@@ -315,9 +318,9 @@ class testTriggerLinking extends CIntegrationTest {
 
 			if ($entry['description'] == self::TRIGGER_DESCRIPTION_SAME_ALL)
 			{
-				$this->assertEquals($entry['dependencies'][0], self::$triggerids[$i], $ep);
-				$i++;
-				continue;
+				$this->assertEquals($entry['dependencies'][0], self::$triggerids_deps[$entry['triggerid']], $ep);
+				$i = $triggers_same_descr_mapping_to_id
+					//continue;
 			}
 
 			$this->assertEquals(self::TAG_NAME_PRE . "_" . self::$stringids[$i], $entry['tags'][0]['tag'], $ep);
