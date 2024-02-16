734e7299a0ffe93c0476d3b3da26172d3aa4c6d1
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2024-02-16 16:01:47 +0200

    ...G...PS. [ZBX-23941] fixed coding style comment

--- src/libs/zbxcomms/tls.c
+++ src/libs/zbxcomms/tls.c
@@ -17,8 +17,9 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-#include "zbxcommon.h"
+
 #include "zbxcomms.h"
+#include "zbxcommon.h"
 
 #include "comms.h"
 #include "tls.h"
