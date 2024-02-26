c5fe8f4d2d2306c87ffac842031fa95c6b40a9a9
Continuous Integration <ci@zabbix.com>
2024-02-26 11:43:31 +0200

    .......... [ZBXNEXT-826] updated release date and version for 6.0.27

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -26,11 +26,11 @@ import (
 )
 
 const (
-	ZABBIX_REVDATE          = "19 February 2024"
+	ZABBIX_REVDATE          = "26 February 2024"
 	ZABBIX_VERSION_MAJOR    = 6
 	ZABBIX_VERSION_MINOR    = 0
 	ZABBIX_VERSION_PATCH    = 27
-	ZABBIX_VERSION_RC       = "rc1"
+	ZABBIX_VERSION_RC       = ""
 	ZABBIX_VERSION_RC_NUM   = "{ZABBIX_RC_NUM}"
 	ZABBIX_VERSION_REVISION = "{ZABBIX_REVISION}"
 	copyrightMessage        = "Copyright (C) 2024 Zabbix SIA\n" +
--- ui/include/defines.inc.php
+++ ui/include/defines.inc.php
@@ -18,7 +18,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-define('ZABBIX_VERSION',		'6.0.27rc1');
+define('ZABBIX_VERSION',		'6.0.27');
 define('ZABBIX_API_VERSION',	'6.0.27');
 define('ZABBIX_EXPORT_VERSION',	'6.0');
 
