c72984cf1bee53336bc5cbeb6822e30bc8384492
Diana Dauksa <diana.dauksa@zabbix.com>
2024-02-29 14:36:23 +0200

    ..F....... [ZBXNEXT-8685] fixed retrieved data ordering and passing to widget view 1

--- ui/widgets/hostnavigator/actions/WidgetView.php
+++ ui/widgets/hostnavigator/actions/WidgetView.php
@@ -27,6 +27,11 @@ use API,
 
 class WidgetView extends CControllerDashboardWidgetView {
 
+	private const SHOW_IN_MAINTENANCE_ON = 1;
+	private const HOST_STATUS_ANY = 0;
+	private const SHOW_PROBLEMS_UNSUPPRESSED = 1;
+	private const SHOW_PROBLEMS_OFF = 2;
+
 	protected function init(): void {
 		parent::init();
 
@@ -52,17 +57,23 @@ class WidgetView extends CControllerDashboardWidgetView {
 	}
 
 	private function getHosts(): array {
+		$no_data = [
+			'hosts' => [],
+			'is_limit_exceeded' => false,
+			'maintenances' => []
+		];
+
 		$override_hostid = $this->fields_values['override_hostid'] ? $this->fields_values['override_hostid'][0] : '';
 
 		if ($this->isTemplateDashboard() && $override_hostid === '') {
-			return [];
+			return $no_data;
 		}
 
 		$groupids = !$this->isTemplateDashboard() && $this->fields_values['groupids']
 			? getSubGroups($this->fields_values['groupids'])
 			: null;
 
-		$output = $this->fields_values['maintenance'] == 1
+		$output = $this->fields_values['maintenance'] == self::SHOW_IN_MAINTENANCE_ON
 			? ['hostid', 'name', 'maintenanceid', 'maintenance_status']
 			: ['hostid', 'name'];
 
@@ -82,8 +93,10 @@ class WidgetView extends CControllerDashboardWidgetView {
 				'searchByAny' => true,
 				'severities' => $this->fields_values['severities'] ?: null,
 				'filter' => [
-					'status' => ($this->fields_values['status'] == 0) ? null : $this->fields_values['status'],
-					'maintenance_status' => $this->fields_values['maintenance'] != 1
+					'status' => $this->fields_values['status'] == self::HOST_STATUS_ANY
+						? null
+						: $this->fields_values['status'],
+					'maintenance_status' => $this->fields_values['maintenance'] != self::SHOW_IN_MAINTENANCE_ON
 						? HOST_MAINTENANCE_STATUS_OFF
 						: null
 				],
@@ -102,8 +115,10 @@ class WidgetView extends CControllerDashboardWidgetView {
 				'hostids' => [$hostid],
 				'severities' => $this->fields_values['severities'] ?: null,
 				'filter' => [
-					'status' => ($this->fields_values['status'] == 0) ? null : $this->fields_values['status'],
-					'maintenance_status' => $this->fields_values['maintenance'] != 1
+					'status' => $this->fields_values['status'] == self::HOST_STATUS_ANY
+						? null
+						: $this->fields_values['status'],
+					'maintenance_status' => $this->fields_values['maintenance'] != self::SHOW_IN_MAINTENANCE_ON
 						? HOST_MAINTENANCE_STATUS_OFF
 						: null
 				],
@@ -113,23 +128,28 @@ class WidgetView extends CControllerDashboardWidgetView {
 		}
 
 		if (!$hosts) {
-			return [];
+			return $no_data;
 		}
+
+		$is_limit_exceeded = false;
 
 		if (count($hosts) > $this->fields_values['limit']) {
-			$exceeds_limit = true;
+			$is_limit_exceeded = true;
 			array_pop($hosts);
 		}
-		else {
-			$exceeds_limit = false;
-		}
 
-		if ($this->fields_values['problems'] != 2) {
+		if ($this->fields_values['problems'] != self::SHOW_PROBLEMS_OFF) {
+			$hostids = [];
+
+			foreach ($hosts as $host) {
+				$hostids[] = $host['hostid'];
+			}
+
 			// Select triggers and problems to calculate number of problems for each host.
 			$triggers = API::Trigger()->get([
 				'output' => [],
 				'selectHosts' => ['hostid'],
-				'hostids' => array_keys($hosts),
+				'hostids' => $hostids,
 				'skipDependent' => true,
 				'monitored' => true,
 				'preservekeys' => true
@@ -140,69 +160,70 @@ class WidgetView extends CControllerDashboardWidgetView {
 				'source' => EVENT_SOURCE_TRIGGERS,
 				'object' => EVENT_OBJECT_TRIGGER,
 				'objectids' => array_keys($triggers),
-				'suppressed' => ($this->fields_values['problems'] == 1) ? false : null,
+				'suppressed' => ($this->fields_values['problems'] == self::SHOW_PROBLEMS_UNSUPPRESSED) ? false : null,
+				'severities' => $this->fields_values['severities'] ?: null,
 				'symptom' => false
 			]);
 
 			// Group all problems per host per severity.
 			$host_problems = [];
+
 			foreach ($problems as $problem) {
 				foreach ($triggers[$problem['objectid']]['hosts'] as $trigger_host) {
 					$host_problems[$trigger_host['hostid']][$problem['severity']][$problem['eventid']] = true;
 				}
 			}
-		}
 
-		$maintenanceids = [];
-
-		foreach ($hosts as &$host) {
-			if ($this->fields_values['maintenance'] == 1 && $host['maintenance_status'] == HOST_MAINTENANCE_STATUS_ON) {
-				$maintenanceids[$host['maintenanceid']] = true;
-			}
-
-			if ($this->fields_values['problems'] != 2) {
+			foreach ($hosts as &$host) {
 				// Fill empty arrays for hosts without problems.
 				if (!array_key_exists($host['hostid'], $host_problems)) {
 					$host_problems[$host['hostid']] = [];
 				}
 
 				// Count the number of problems (as value) per severity (as key).
-				for ($severity = TRIGGER_SEVERITY_COUNT - 1; $severity >= TRIGGER_SEVERITY_NOT_CLASSIFIED; $severity--) {
+				for ($severity = TRIGGER_SEVERITY_INFORMATION; $severity <= TRIGGER_SEVERITY_DISASTER; $severity++) {
 					$host['problem_count'][$severity] = array_key_exists($severity, $host_problems[$host['hostid']])
 						? count($host_problems[$host['hostid']][$severity])
 						: 0;
 				}
 			}
+			unset($host);
 		}
-		unset($host);
 
-		if ($this->fields_values['maintenance'] == 1 && $maintenanceids) {
-			$maintenances = API::Maintenance()->get([
-				'output' => ['name', 'maintenance_type', 'description'],
-				'maintenanceids' => array_keys($maintenanceids),
-				'preservekeys' => true
-			]);
+		$maintenances = [];
+
+		if ($this->fields_values['maintenance'] == self::SHOW_IN_MAINTENANCE_ON) {
+			$maintenanceids = [];
 
 			foreach ($hosts as &$host) {
-				$maintenanceid = $host['maintenanceid'];
-
-				if (array_key_exists($maintenanceid, $maintenances)) {
-					// Merge maintenance info into the hosts data
-					$host['maintenance_info'] = $maintenances[$maintenanceid];
-					$host['in_maintenance'] = true;
+				if ($host['maintenance_status'] == HOST_MAINTENANCE_STATUS_ON) {
+					$maintenanceids[$host['maintenanceid']] = true;
 				}
 				else {
-					$host['in_maintenance'] = false;
+					unset($host['maintenanceid']);
 				}
-
-				unset($host['maintenance_status'], $host['maintenanceid']);
+				unset($host['maintenance_status']);
 			}
 			unset($host);
+
+			if ($maintenanceids) {
+				$maintenances = API::Maintenance()->get([
+					'output' => ['name', 'maintenance_type', 'description'],
+					'maintenanceids' => array_keys($maintenanceids),
+					'preservekeys' => true
+				]);
+
+				foreach ($maintenances as &$maintenance) {
+					unset($maintenance['maintenanceid']);
+				}
+				unset($maintenance);
+			}
 		}
 
 		return [
 			'hosts' => $hosts,
-			'exceeds_limit' => $exceeds_limit
+			'is_limit_exceeded' => $is_limit_exceeded,
+			'maintenances' => $maintenances
 		];
 	}
 
@@ -216,7 +237,8 @@ class WidgetView extends CControllerDashboardWidgetView {
 				TRIGGER_SEVERITY_HIGH => ZBX_STYLE_HIGH_BG,
 				TRIGGER_SEVERITY_DISASTER => ZBX_STYLE_DISASTER_BG
 			],
-			//'group_by' => $this->fields_values['group_by'] ?: []
+			'show_problems' => $this->fields_values['problems'] != self::SHOW_PROBLEMS_OFF,
+			'group_by' => []
 		];
 
 		return $config;
