3e7557e9cc49a6ed0024d8e8fe3c71a020cb7ca8
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2024-03-06 14:02:40 +0200

    ..F....... [ZBXNEXT-8758,ZBXNEXT-8938] replaced proxy dropdown list with radio button list and multiselect

--- ui/app/views/js/configuration.discovery.list.js.php
+++ ui/app/views/js/configuration.discovery.list.js.php
@@ -58,7 +58,7 @@
 		_edit(parameters = {}) {
 			const overlay = PopUp('discovery.edit', parameters, {
 				dialogueid: 'discoveryForm',
-				dialogue_class: 'modal-popup-medium',
+				dialogue_class: 'modal-popup-static',
 				prevent_navigation: true
 			});
 
