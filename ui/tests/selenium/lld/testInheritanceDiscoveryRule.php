4a240faaaedb6714efeb253976a4a803e4f06675
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2024-03-08 14:04:24 +0200

    .......... [ZBXNEXT-2020,ZBXNEXT-8918] added debug info

--- ui/tests/selenium/lld/testInheritanceDiscoveryRule.php
+++ ui/tests/selenium/lld/testInheritanceDiscoveryRule.php
@@ -48,6 +48,8 @@ class testInheritanceDiscoveryRule extends CLegacyWebTest {
 	public function testInheritanceDiscoveryRule_SimpleUpdate($data) {
 		$sqlDiscovery = 'SELECT * FROM items ORDER BY itemid';
 		$oldHashDiscovery = CDBHelper::getHash($sqlDiscovery);
+		var_dump(CDBHelper::getAll($sqlDiscovery));
+		var_dump('-------------');
 
 		$this->zbxTestLogin('host_discovery.php?form=update&context=host&itemid='.$data['itemid']);
 		$this->zbxTestClickWait('update');
@@ -55,7 +57,7 @@ class testInheritanceDiscoveryRule extends CLegacyWebTest {
 		$this->zbxTestTextPresent('Discovery rule updated');
 
 		$this->assertEquals($oldHashDiscovery, CDBHelper::getHash($sqlDiscovery));
-
+		var_dump(CDBHelper::getAll($sqlDiscovery));
 	}
 
 	// Returns create data.
