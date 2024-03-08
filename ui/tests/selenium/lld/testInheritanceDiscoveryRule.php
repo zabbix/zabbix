a19624b8b9fbe70cec1189583103cd2a1a634b37
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2024-03-08 14:29:05 +0200

    .......... [ZBXNEXT-2020,ZBXNEXT-8918] fixed debug info

--- ui/tests/selenium/lld/testInheritanceDiscoveryRule.php
+++ ui/tests/selenium/lld/testInheritanceDiscoveryRule.php
@@ -48,7 +48,7 @@ class testInheritanceDiscoveryRule extends CLegacyWebTest {
 	public function testInheritanceDiscoveryRule_SimpleUpdate($data) {
 		$sqlDiscovery = 'SELECT * FROM items ORDER BY itemid';
 		$oldHashDiscovery = CDBHelper::getHash($sqlDiscovery);
-		var_dump(CDBHelper::getAll('SELECT * FROM items ORDER BY itemid WHERE hostid=15000'));
+		echo (json_encode(CDBHelper::getAll('SELECT * FROM items WHERE hostid=15000 ORDER BY itemid'), JSON_PRETTY_PRINT));
 		var_dump('-------------');
 
 		$this->zbxTestLogin('host_discovery.php?form=update&context=host&itemid='.$data['itemid']);
@@ -56,8 +56,8 @@ class testInheritanceDiscoveryRule extends CLegacyWebTest {
 		$this->zbxTestCheckTitle('Configuration of discovery rules');
 		$this->zbxTestTextPresent('Discovery rule updated');
 
+		echo (json_encode(CDBHelper::getAll('SELECT * FROM items WHERE hostid=15000 ORDER BY itemid'), JSON_PRETTY_PRINT));
 		$this->assertEquals($oldHashDiscovery, CDBHelper::getHash($sqlDiscovery));
-		var_dump(CDBHelper::getAll('SELECT * FROM items ORDER BY itemid WHERE hostid=15000'));
 	}
 
 	// Returns create data.
