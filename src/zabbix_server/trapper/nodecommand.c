f158a5191dff4e9f7c154bfefe6f2b661ecd69a2
dimir <vladimir.levijev@zabbix.com>
2024-03-11 18:53:47 +0200

    ........S. [DEV-3121] fixed possible injection when adding audit log entry of a global script exection

--- src/zabbix_server/trapper/nodecommand.c
+++ src/zabbix_server/trapper/nodecommand.c
@@ -581,7 +581,15 @@ int	node_process_command(zbx_socket_t *sock, const char *data, const struct zbx_
 		}
 	}
 
-	if (SUCCEED != zbx_json_value_by_name(jp, ZBX_PROTO_TAG_CLIENTIP, clientip, sizeof(clientip), NULL))
+	if (SUCCEED == zbx_json_value_by_name(jp, ZBX_PROTO_TAG_CLIENTIP, clientip, sizeof(clientip), NULL))
+	{
+		if (SUCCEED != is_ip(clientip))
+		{
+			result = zbx_dsprintf(result, "Invalid IP in the %s tag.", ZBX_PROTO_TAG_CLIENTIP);
+			goto finish;
+		}
+	}
+	else
 		*clientip = '\0';
 
 	if (SUCCEED == (ret = execute_script(scriptid, hostid, eventid, &user, clientip, &result, &debug)))
