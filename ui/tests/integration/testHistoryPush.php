338eddf872a650e86ae5c3aaf69168528fa693bf
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2024-03-13 16:24:22 +0200

    .......... [ZBXNEXT-8541] made integration test for history.push more stable
    (cherry picked from commit 4b437a18f81a8af8a9ae97870bc77346386ba8e6)

--- ui/tests/integration/testHistoryPush.php
+++ ui/tests/integration/testHistoryPush.php
@@ -479,6 +479,8 @@ class testHistoryPush extends CIntegrationTest {
 		]);
 
 		$this->reloadConfigurationCache();
+		$this->waitForLogLineToBePresent(self::COMPONENT_SERVER,
+			'End of zbx_dc_sync_configuration()', true);
 
 		$response = $this->call('history.push', [
 			'itemid' => self::$itemids['trapper_uint_non_monitored_host'],
@@ -514,6 +516,8 @@ class testHistoryPush extends CIntegrationTest {
 		$maintenance_id = $response['result']['maintenanceids'][0];
 
 		$this->reloadConfigurationCache();
+		$this->waitForLogLineToBePresent(self::COMPONENT_SERVER,
+			'End of zbx_dc_sync_configuration()', true);
 		$this->waitForLogLineToBePresent(self::COMPONENT_SERVER,
 			'End of zbx_dc_update_maintenances() started:1 stopped:0 running:1', true);
 
