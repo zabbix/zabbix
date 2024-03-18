6839b23842a9d26cabe3aff480832771f361d128
Eliza Sekace <eliza.sekace@zabbix.com>
2024-03-18 16:11:32 +0200

    ..F....... [ZBXNEXT-2020,ZBXNEXT-8918] updated import converter

--- ui/include/classes/import/converters/C64ImportConverter.php
+++ ui/include/classes/import/converters/C64ImportConverter.php
@@ -256,9 +256,14 @@ class C64ImportConverter extends CConverter {
 				$discovery_rule['trigger_prototypes'] = self::convertTriggers($discovery_rule['trigger_prototypes']);
 			}
 
+			$discovery_rule['enabled_lifetime_type'] = CXmlConstantName::LLD_DISABLE_NEVER;
+			$discovery_rule['enabled_lifetime'] = '0';
+
 			if (array_key_exists('lifetime', $discovery_rule)) {
 				if ($discovery_rule['lifetime'] === '0') {
 					$discovery_rule['lifetime_type'] = CXmlConstantName::LLD_DELETE_IMMEDIATELY;
+					$discovery_rule['enabled_lifetime_type'] = CXmlConstantName::LLD_DISABLE_IMMEDIATELY;
+					$discovery_rule['enabled_lifetime'] = DB::getDefault('items', 'enabled_lifetime');
 				}
 				else {
 					$discovery_rule['lifetime_type'] = CXmlConstantName::LLD_DELETE_AFTER;
@@ -267,9 +272,6 @@ class C64ImportConverter extends CConverter {
 			else {
 				$discovery_rule['lifetime'] = '30d';
 			}
-
-			$discovery_rule['enabled_lifetime_type'] = CXmlConstantName::LLD_DISABLE_NEVER;
-			$discovery_rule['enabled_lifetime'] = '0';
 		}
 		unset($discovery_rule);
 
