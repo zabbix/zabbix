4c0851fd42672d9dfc351922c473c759b3b57db8
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2024-03-25 15:46:22 +0200

    .......... [DEV-762] added more Makefiles

---
+++ tests/libs/zbxcommon/init_library_common.c
@@ -0,0 +1,45 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2024 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+#include "zbxmocktest.h"
+#include "zbxmockdata.h"
+#include "zbxmockutil.h"
+
+#include "zbxnum.h"
+
+static void    zbx_log_impl(int level, const char *fmt, va_list args)
+{
+	ZBX_UNUSED(level);
+	ZBX_UNUSED(fmt);
+	ZBX_UNUSED(args);
+}
+
+
+void	zbx_mock_test_entry(void **state)
+{
+	int		expected_result, actual_result;
+	const char	*is_number;
+
+	ZBX_UNUSED(state);
+
+	is_number = zbx_mock_get_parameter_string("in.num");
+	expected_result = zbx_mock_str_to_return_code(zbx_mock_get_parameter_string("out.return"));
+
+	zbx_init_library_common(zbx_log_impl, get_zbx_progname);
+}
