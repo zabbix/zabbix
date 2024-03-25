a2688d6f3287ae98b833b2ff0081b90de13998a4
Continuous Integration <ci@zabbix.com>
2024-03-25 13:36:45 +0200

    .......... [ZBXNEXT-826] updated release date and version for 6.0.28

--- ui/include/defines.inc.php
+++ ui/include/defines.inc.php
@@ -18,7 +18,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-define('ZABBIX_VERSION',		'6.0.28rc1');
+define('ZABBIX_VERSION',		'6.0.28');
 define('ZABBIX_API_VERSION',	'6.0.28');
 define('ZABBIX_EXPORT_VERSION',	'6.0');
 
