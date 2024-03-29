f800c4ff048606858fd51439e2611adca59d94bd
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2024-03-29 20:40:50 +0200

    .......... [DEV-3233] refactored host prototype macros test

--- ui/tests/selenium/macros/testFormMacrosHostPrototype.php
+++ ui/tests/selenium/macros/testFormMacrosHostPrototype.php
@@ -28,13 +28,33 @@ require_once dirname(__FILE__).'/../common/testFormMacros.php';
  */
 class testFormMacrosHostPrototype extends testFormMacros {
 
-	const IS_PROTOTYPE	= true;
-	protected static $main_lldid;
+	const HOSTMACROS_UPDATE = 'Host prototype for macros {#UPDATE}';
+	const HOSTMACROS_REMOVE = 'Host prototype for macros {#DELETE}';
+	protected static $lldid;
+	protected static $inherited_macros_prototypeid;
+	protected static $update_macros_prototypeid;
+	protected static $remove_macros_prototypeid;
+	protected static $create_secret_macros_prototypeid;
+	protected static $update_secret_macros_prototypeid;
+	protected static $vault_macros_validation_prototypeid;
+	protected static $vault_macros_create_prototypeid;
+	protected static $hashi_macros_create_prototypeid;
+	protected static $vault_macros_update_prototypeid;
+
+	protected $vault_object = 'host prototype';
+	protected $hashi_error_field = '/1/macros/3/value';
+	protected $cyber_error_field = '/1/macros/4/value';
+	protected $update_vault_macro = '{$VAULT_HOST_MACRO3_CHANGED}';
+	protected $vault_macro_index = 0;
+
+	protected $revert_macro_1 = '{$Z_HOST_PROTOTYPE_MACRO_REVERT}';
+	protected $revert_macro_2 = '{$Z_HOST_PROTOTYPE_MACRO_2_TEXT_REVERT}';
+	protected $revert_macro_object = 'host';
 
 	public function prepareHostPrototypeMacrosData() {
 		CDataHelper::createHosts([
 			[
-				'host' => 'Host for host prortotypes macros testing',
+				'host' => 'Host for host prototypes macros testing',
 				'groups' => [['groupid' => self::ZABBIX_SERVERS_GROUPID]],
 				'discoveryrules' => [
 					[
@@ -45,97 +65,176 @@ class testFormMacrosHostPrototype extends testFormMacros {
 				]
 			]
 		]);
+		self::$lldid = CDBHelper::getValue('SELECT itemid FROM items WHERE name='.zbx_dbstr('Main LLD'));
 
-		self::$main_lldid = CDBHelper::getValue('SELECT itemid FROM items WHERE name='.zbx_dbstr('Main LLD'));
-
-		$response = CDataHelper::call('hostprototype.create', [
-			'host' => 'Host prototype for Inherited {#MACROS} removing',
-			'ruleid' => self::$main_lldid,
-			'groupLinks' =>  [['groupid'=> self::ZABBIX_SERVERS_GROUPID]],
-			'macros' => [
-				[
-					'macro' => '{$TEST_MACRO123}',
-					'value' => 'test123',
-					'description' => 'description 123'
-				],
-				[
-					'macro' => '{$MACRO_FOR_DELETE_HOST1}',
-					'value' => 'test1',
-					'description' => 'description 1'
-				],
-				[
-					'macro' => '{$MACRO_FOR_DELETE_HOST2}',
-					'value' => 'test2',
-					'description' => 'description 2'
-				],
-				[
-					'macro' => '{$MACRO_FOR_DELETE_GLOBAL1}',
-					'value' => 'test global 1',
-					'description' => 'global description 1'
-				],
-				[
-					'macro' => '{$MACRO_FOR_DELETE_GLOBAL2}',
-					'value' => 'test global 2',
-					'description' => 'global description 2'
-				],
-				[
-					'macro' => '{$SNMP_COMMUNITY}',
-					'value' => 'redefined value',
-					'description' => 'redefined description'
-				]
-			]
-		]);
-
-		self::$host_prototypeid_remove_inherited = $response['hostids'][0];
-
-		CDataHelper::call('hostprototype.update', [
+		CDataHelper::call('hostprototype.create', [
 			[
-				'hostid' => 90010,
+				'host' => 'Host prototype for Inherited {#MACROS} removing',
+				'ruleid' => self::$lldid,
+				'groupLinks' =>  [['groupid'=> self::ZABBIX_SERVERS_GROUPID]],
 				'macros' => [
-					'macro' => '{$NEWMACROS}',
-					'value' => 'something/value:key',
-					'type' => 2
+					[
+						'macro' => '{$TEST_MACRO123}',
+						'value' => 'test123',
+						'description' => 'description 123'
+					],
+					[
+						'macro' => '{$MACRO_FOR_DELETE_HOST1}',
+						'value' => 'test1',
+						'description' => 'description 1'
+					],
+					[
+						'macro' => '{$MACRO_FOR_DELETE_HOST2}',
+						'value' => 'test2',
+						'description' => 'description 2'
+					],
+					[
+						'macro' => '{$MACRO_FOR_DELETE_GLOBAL1}',
+						'value' => 'test global 1',
+						'description' => 'global description 1'
+					],
+					[
+						'macro' => '{$MACRO_FOR_DELETE_GLOBAL2}',
+						'value' => 'test global 2',
+						'description' => 'global description 2'
+					],
+					[
+						'macro' => '{$SNMP_COMMUNITY}',
+						'value' => 'redefined value',
+						'description' => 'redefined description'
+					]
 				]
-			]
+			],
+			[
+				'host' => self::HOSTMACROS_UPDATE,
+				'ruleid' => self::$lldid,
+				'groupLinks' =>  [['groupid'=> self::ZABBIX_SERVERS_GROUPID]],
+				'macros' => [
+					[
+						'macro' => '{$UPDATE_MACRO_1}',
+						'value' => 'Update macro value 1',
+						'description' => 'Update macro description 1'
+					],
+					[
+						'macro' => '{$UPDATE_MACRO_2}',
+						'value' => 'Update macro value 2',
+						'description' => 'Update macro description 2'
+					]
+				]
+			],
+			[
+				'host' => self::HOSTMACROS_REMOVE,
+				'ruleid' => self::$lldid,
+				'groupLinks' =>  [['groupid'=> self::ZABBIX_SERVERS_GROUPID]],
+				'macros' => [
+					[
+						'macro' => '{$DELETE_MACRO_1}',
+						'value' => 'Delete macro value 1',
+						'description' => 'Delete macro description 1'
+					],
+					[
+						'macro' => '{$DELETE_MACRO_2}',
+						'value' => 'Delete macro value 2',
+						'description' => 'Delete macro description 2'
+					]
+				]
+			],
+			[
+				'host' => 'Host prototype for {#SECRET_MACROS} create',
+				'ruleid' => self::$lldid,
+				'groupLinks' =>  [['groupid'=> self::ZABBIX_SERVERS_GROUPID]]
+			],
+			[
+				'host' => 'Host prototype for {#SECRET_MACROS} update',
+				'ruleid' => self::$lldid,
+				'groupLinks' =>  [['groupid'=> self::ZABBIX_SERVERS_GROUPID]],
+				'macros' => [
+					[
+						'macro' => '{$PROTOTYPE_SECRET_2_SECRET}',
+						'value' => 'This text should stay secret',
+						'description' => 'Secret macro to me updated',
+						'type' => ZBX_MACRO_TYPE_SECRET
+					],
+					[
+						'macro' => '{$PROTOTYPE_SECRET_2_TEXT}',
+						'value' => 'This text should become visible',
+						'description' => 'Secret macro to become visible',
+						'type' => ZBX_MACRO_TYPE_SECRET
+					],
+					[
+						'macro' => '{$PROTOTYPE_TEXT_2_SECRET}',
+						'value' => 'This text should become secret',
+						'description' => 'Text macro to become secret',
+						'type' => ZBX_MACRO_TYPE_TEXT
+					],
+					[
+						'macro' => '{$Z_HOST_PROTOTYPE_MACRO_REVERT}',
+						'value' => 'Secret host value',
+						'description' => 'Value change Revert',
+						'type' => ZBX_MACRO_TYPE_SECRET
+					],
+					[
+						'macro' => '{$Z_HOST_PROTOTYPE_MACRO_2_TEXT_REVERT}',
+						'value' => 'Secret host value 2',
+						'description' => 'Value and type change revert',
+						'type' => ZBX_MACRO_TYPE_SECRET
+					]
+				]
+			],
+			[
+				'host' => 'Host prototype for {#VAULT_MACROS} validation',
+				'ruleid' => self::$lldid,
+				'groupLinks' =>  [['groupid'=> self::ZABBIX_SERVERS_GROUPID]],
+				'macros' => [
+					[
+						'macro' => '{$NEWMACROS}',
+						'value' => 'something/value:key',
+						'type' => ZBX_MACRO_TYPE_VAULT
+					]
+				]
+			],
+			[
+				'host' => 'Empty prototype for {#VAULT_MACROS} create',
+				'ruleid' => self::$lldid,
+				'groupLinks' =>  [['groupid'=> self::ZABBIX_SERVERS_GROUPID]]
+			],
+			[
+				'host' => 'Empty prototype for {#VAULT_MACROS} create Hashicorp',
+				'ruleid' => self::$lldid,
+				'groupLinks' =>  [['groupid'=> self::ZABBIX_SERVERS_GROUPID]]
+			],
+			[
+				'host' => 'Host prototype for {#VAULT_MACROS} update',
+				'ruleid' => self::$lldid,
+				'groupLinks' =>  [['groupid'=> self::ZABBIX_SERVERS_GROUPID]],
+				'macros' => [
+					[
+						'macro' => '{$VAULT_HOST_MACRO}',
+						'value' => 'secret/path:key',
+						'description' => 'Change name, value, description',
+						'type' => ZBX_MACRO_TYPE_VAULT
+					]
+				]
+			],
 		]);
+		$host_prototypes = CDataHelper::getIds('host');
+
+		self::$inherited_macros_prototypeid = $host_prototypes['Host prototype for Inherited {#MACROS} removing'];
+		self::$update_macros_prototypeid = $host_prototypes[self::HOSTMACROS_UPDATE];
+		self::$remove_macros_prototypeid = $host_prototypes[self::HOSTMACROS_REMOVE];
+		self::$create_secret_macros_prototypeid = $host_prototypes['Host prototype for {#SECRET_MACROS} create'];
+		self::$update_secret_macros_prototypeid = $host_prototypes['Host prototype for {#SECRET_MACROS} update'];
+		self::$vault_macros_validation_prototypeid = $host_prototypes['Host prototype for {#VAULT_MACROS} validation'];
+		self::$vault_macros_create_prototypeid = $host_prototypes['Empty prototype for {#VAULT_MACROS} create'];
+		self::$hashi_macros_create_prototypeid = $host_prototypes['Empty prototype for {#VAULT_MACROS} create Hashicorp'];
+		self::$vault_macros_update_prototypeid = $host_prototypes['Host prototype for {#VAULT_MACROS} update'];
 	}
-
-	/**
-	 * The name of the host for updating macros, id=99200.
-	 *
-	 * @var string
-	 */
-	protected $host_name_update = 'Host prototype for macros {#UPDATE}';
-
-	/**
-	 * The name of the host for removing macros, id=99201.
-	 *
-	 * @var string
-	 */
-	protected $host_name_remove = 'Host prototype for macros {#DELETE}';
-
-	/**
-	 * The id of the host prototype for removing inherited macros.
-	 *
-	 * @var integer
-	 */
-	protected static $host_prototypeid_remove_inherited;
-
-	public $vault_object = 'host prototype';
-	public $hashi_error_field = '/1/macros/6/value';
-	public $cyber_error_field = '/1/macros/4/value';
-	public $update_vault_macro = '{$VAULT_HOST_MACRO3_CHANGED}';
-	public $vault_macro_index = 0;
-
-	public $revert_macro_1 = '{$Z_HOST_PROTOTYPE_MACRO_REVERT}';
-	public $revert_macro_2 = '{$Z_HOST_PROTOTYPE_MACRO_2_TEXT_REVERT}';
-	public $revert_macro_object = 'host';
 
 	/**
 	 * @dataProvider getCreateMacrosData
 	 */
 	public function testFormMacrosHostPrototype_Create($data) {
-		$this->checkMacros($data, 'host prototype', null, false, self::IS_PROTOTYPE, self::$main_lldid);
+		$this->checkMacros($data, 'host prototype', null, false, true, self::$lldid);
 	}
 
 	/**
@@ -143,32 +242,28 @@ class testFormMacrosHostPrototype extends testFormMacros {
 	 * @dataProvider getUpdateMacrosCommonData
 	 */
 	public function testFormMacrosHostPrototype_Update($data) {
-		$this->checkMacros($data, 'host prototype', $this->host_name_update, true, self::IS_PROTOTYPE, self::$main_lldid);
+		$this->checkMacros($data, 'host prototype', self::HOSTMACROS_UPDATE, true, true,
+				self::$lldid
+		);
 	}
 
 	public function testFormMacrosHostPrototype_RemoveAll() {
-		$this->checkRemoveAll($this->host_name_remove, 'host prototype', self::IS_PROTOTYPE, self::$main_lldid);
+		$this->checkRemoveAll(self::HOSTMACROS_REMOVE, 'host prototype', true, self::$lldid);
 	}
 
 	/**
 	 * @dataProvider getCheckInheritedMacrosData
 	 */
 	public function testFormMacrosHostPrototype_ChangeInheritedMacro($data) {
-		$this->checkChangeInheritedMacros($data, 'host prototype', self::IS_PROTOTYPE, self::$main_lldid);
-	}
-
-	public function prepareHostPrototypeRemoveMacrosData() {
-
+		$this->checkChangeInheritedMacros($data, 'host prototype', true, self::$lldid);
 	}
 
 	/**
 	 * @dataProvider getRemoveInheritedMacrosData
-	 *
-	 * @onBeforeOnce prepareHostPrototypeRemoveMacrosData
 	 */
 	public function testFormMacrosHostPrototype_RemoveInheritedMacro($data) {
-		$this->checkRemoveInheritedMacros($data, 'host prototype', self::$host_prototypeid_remove_inherited,
-				self::IS_PROTOTYPE, self::$main_lldid);
+		$this->checkRemoveInheritedMacros($data, 'host prototype', self::$inherited_macros_prototypeid,
+				true, self::$lldid);
 	}
 
 	public function getCreateSecretMacrosData() {
@@ -218,8 +313,8 @@ class testFormMacrosHostPrototype extends testFormMacros {
 	 * @dataProvider getCreateSecretMacrosData
 	 */
 	public function testFormMacrosHostPrototype_CreateSecretMacros($data) {
-		$this->createSecretMacros($data, 'host_prototypes.php?form=update&context=host&parent_discoveryid=90001&hostid=99205',
-				'host-prototype'
+		$this->createSecretMacros($data, 'host_prototypes.php?form=update&context=host&parent_discoveryid='.
+				self::$lldid.'&hostid='.self::$create_secret_macros_prototypeid, 'host-prototype'
 		);
 	}
 
@@ -264,33 +359,42 @@ class testFormMacrosHostPrototype extends testFormMacros {
 	 * @dataProvider getUpdateSecretMacrosData
 	 */
 	public function testFormMacrosHostPrototype_UpdateSecretMacros($data) {
-		$this->updateSecretMacros($data, 'host_prototypes.php?form=update&context=host&parent_discoveryid=90001&hostid=99206',
-				'host-prototype');
+		$this->updateSecretMacros($data, 'host_prototypes.php?form=update&context=host&parent_discoveryid='.
+				self::$lldid.'&hostid='.self::$update_secret_macros_prototypeid, 'host-prototype'
+		);
 	}
 
 	/**
 	 * @dataProvider getRevertSecretMacrosData
 	 */
 	public function testFormMacrosHostPrototype_RevertSecretMacroChanges($data) {
-		$this->revertSecretMacroChanges($data, 'host_prototypes.php?form=update&context=host&parent_discoveryid=90001&hostid=99206',
-				'host-prototype');
+		$this->revertSecretMacroChanges($data, 'host_prototypes.php?form=update&context=host&parent_discoveryid='.
+				self::$lldid.'&hostid='.self::$update_secret_macros_prototypeid, 'host-prototype'
+		);
 	}
 
 	/**
 	 * Check Vault macros validation.
 	 */
-	public function testFormMacrosHostPrototype_checkVaultValidation() {
-		$this->checkVaultValidation('host_prototypes.php?form=update&context=host&parent_discoveryid=90003&hostid=90010',
-				'host-prototype');
+	public function testFormMacrosHostPrototype_CheckVaultValidation() {
+		$this->checkVaultValidation('host_prototypes.php?form=update&context=host&parent_discoveryid='.
+				self::$lldid.'&hostid='.self::$vault_macros_validation_prototypeid, 'host-prototype'
+		);
 	}
 
 	/**
 	 * @dataProvider getCreateVaultMacrosData
+	 * ! This scenario should be only ran with all cases, hashi_error_field and cyber_error_field
+	 * may not work correctly when run single case. !
 	 */
 	public function testFormMacrosHostPrototype_CreateVaultMacros($data) {
-		$hostid = ($data['vault'] === 'Hashicorp') ? '99205' : '90002';
-		$this->createVaultMacros($data, 'host_prototypes.php?form=update&context=host&parent_discoveryid=90001&hostid='.$hostid,
-				'host-prototype');
+		$hostid = ($data['vault'] === 'Hashicorp')
+			? self::$hashi_macros_create_prototypeid
+			: self::$vault_macros_create_prototypeid;
+
+		$this->createVaultMacros($data, 'host_prototypes.php?form=update&context=host&parent_discoveryid='.
+				self::$lldid.'&hostid='.$hostid, 'host-prototype'
+		);
 	}
 
 	/**
@@ -298,7 +402,8 @@ class testFormMacrosHostPrototype extends testFormMacros {
 	 * @dataProvider getUpdateVaultMacrosCommonData
 	 */
 	public function testFormMacrosHostPrototype_UpdateVaultMacros($data) {
-		$this->updateVaultMacros($data, 'host_prototypes.php?form=update&context=host&parent_discoveryid=90003&hostid=90008',
-				'host-prototype');
+		$this->updateVaultMacros($data, 'host_prototypes.php?form=update&context=host&parent_discoveryid=
+				'.self::$lldid.'&hostid='.self::$vault_macros_update_prototypeid, 'host-prototype'
+		);
 	}
 }
