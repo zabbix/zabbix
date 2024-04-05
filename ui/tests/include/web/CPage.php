1bb0c35c89072be679bbe9d25fe69959d68f7032
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2024-04-05 11:16:19 +0300

    .......... [ZBXNEXT-686] enables --remote-debugging-pipe instead of the default --remote-debugging-port

--- ui/tests/include/web/CPage.php
+++ ui/tests/include/web/CPage.php
@@ -100,7 +100,8 @@ class CPage {
 				'--no-sandbox',
 				'--enable-font-antialiasing=false',
 				'--window-size='.self::DEFAULT_PAGE_WIDTH.','.self::DEFAULT_PAGE_HEIGHT,
-				'--disable-dev-shm-usage'
+				'--disable-dev-shm-usage',
+				'--remote-debugging-pipe'
 			]);
 
 			if (defined('PHPUNIT_BROWSER_LOG_DIR')) {
