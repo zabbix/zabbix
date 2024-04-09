974d32c6c5b84d37537f6109f2c52272fd1147e0
Dmitrijs Fofanovs <dmitrijs.fofanovs@zabbix.com>
2024-04-09 14:44:36 +0300

    ..F....... [ZBXNEXT-9077,ZBXNEXT-9104] fixed typo

--- ui/include/classes/vaults/CVaultCyberArk.php
+++ ui/include/classes/vaults/CVaultCyberArk.php
@@ -38,7 +38,7 @@ class CVaultCyberArk extends CVault {
 
 	public function __construct(string $api_endpoint, string $db_prefix, string $db_path, ?string $cert_file, ?string $key_file) {
 		$this->api_endpoint = $api_endpoint;
-		$this->db_prefix = $db_prefix === '' ? self::API_ENDPOINT_DEFAULT : $db_prefix;
+		$this->db_prefix = $db_prefix === '' ? self::DB_PREFIX_DEFAULT : $db_prefix;
 		$this->db_path = $db_path;
 		$this->cert_file = $cert_file !== null ? trim($cert_file) : null;
 		$this->key_file = $key_file !== null ? trim($key_file) : null;
