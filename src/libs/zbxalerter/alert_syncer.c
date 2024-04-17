yusufarslanturk/zabbix-bgmot-2fa

........S. [ZBXNEXT-7930] added user macros support for username and password fields in email media type

src/zabbix_server/alerter/alert_syncer.c
@@ -35,6 +35,7 @@
 #include "zbxthreads.h"
 #include "zbxtime.h"
 #include "zbxtypes.h"
+#include "zbxmedia.h"
 
 #define ZBX_POLL_INTERVAL		1
 
@@ -79,6 +80,9 @@ static zbx_am_db_alert_t	*am_db_create_alert(zbx_uint64_t alertid, zbx_uint64_t 
 
 	alert->status = status;
 	alert->retries = retries;
+
+	alert->expression = NULL;
+	alert->recovery_expression = NULL;
 
 	return alert;
 }
@@ -221,6 +225,77 @@ static int	am_db_get_alerts(zbx_vector_ptr_t *alerts)
 	return ret;
 }
 
+static void	am_db_get_trigger_expressions(zbx_vector_uint64_t *auth_email_mediatypeids, zbx_vector_ptr_t *alerts)
+{
+	int			i;
+	char			*sql = NULL;
+	size_t			sql_alloc = 0, sql_offset = 0;
+	DB_RESULT		result;
+	DB_ROW			row;
+	zbx_am_db_alert_t	*alert;
+	zbx_vector_uint64_t	triggerids;
+
+	if (0 == auth_email_mediatypeids->values_num)
+		return;
+
+	zbx_vector_uint64_create(&triggerids);
+	zbx_vector_uint64_sort(auth_email_mediatypeids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
+
+	for (i = 0; i < alerts->values_num; i++)
+	{
+		alert = (zbx_am_db_alert_t *)alerts->values[i];
+
+		if (((EVENT_SOURCE_INTERNAL == alert->source && EVENT_OBJECT_TRIGGER == alert->object) ||
+				EVENT_SOURCE_TRIGGERS == alert->source) &&
+				FAIL != zbx_vector_uint64_bsearch(auth_email_mediatypeids, alert->mediatypeid,
+				ZBX_DEFAULT_UINT64_COMPARE_FUNC))
+		{
+			zbx_vector_uint64_append(&triggerids, alert->objectid);
+		}
+	}
+
+	if (0 == triggerids.values_num)
+	{
+		zbx_vector_uint64_destroy(&triggerids);
+		return;
+	}
+
+	zbx_vector_uint64_sort(&triggerids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
+	zbx_vector_uint64_uniq(&triggerids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
+
+	zbx_db_add_condition_alloc(&sql, &sql_alloc, &sql_offset, "triggerid", triggerids.values,
+			triggerids.values_num);
+
+	result = zbx_db_select(
+			"select triggerid,expression,recovery_expression"
+			" from triggers"
+			" where%s",
+			sql);
+
+	while (NULL != (row = zbx_db_fetch(result)))
+	{
+		zbx_uint64_t	triggerid;
+
+		ZBX_STR2UINT64(triggerid, row[0]);
+
+		for (i = 0; i < alerts->values_num; i++)
+		{
+			alert = (zbx_am_db_alert_t *)alerts->values[i];
+
+			if (((EVENT_SOURCE_INTERNAL == alert->source && EVENT_OBJECT_TRIGGER == alert->object) ||
+					EVENT_SOURCE_TRIGGERS == alert->source) && triggerid == alert->objectid)
+			{
+				alert->expression = zbx_strdup(alert->expression, row[1]);
+				alert->recovery_expression = zbx_strdup(alert->recovery_expression, row[2]);
+			}
+		}
+	}
+	zbx_db_free_result(result);
+
+	zbx_free(sql);
+	zbx_vector_uint64_destroy(&triggerids);
+}
+
 #define ZBX_UPDATE_STR(dst, src, ret)			\
 	if (NULL == dst || 0 != strcmp(dst, src))	\
 	{						\
@@ -300,14 +375,17 @@ static zbx_am_db_mediatype_t	*am_db_update_mediatype(zbx_am_db_t *amdb, time_t n
  *                                                                            *
  * Purpose: updates alert manager media types                                 *
  *                                                                            *
- * Parameters: amdb            - [IN] alert manager cache                     *
- *             mediatypeids    - [IN] media type identifiers                  *
- *             medatypeids_num - [IN] number of media type identifiers        *
- *             mediatypes      - [OUT] updated mediatypes                     *
+ * Parameters: amdb                     - [IN] the alert manager cache        *
+ *             mediatypeids             - [IN] the media type identifiers     *
+ *             medatypeids_num          - [IN] the number of media type       *
+ *                                             identifiers                    *
+ *             mediatypes               - [OUT] the updated mediatypes        *
+ *             auth_email_mediatypeids  - [OUT] email media types ids with    *
+ *                                              authentication enabled        *
  *                                                                            *
  ******************************************************************************/
 static void	am_db_update_mediatypes(zbx_am_db_t *amdb, const zbx_uint64_t *mediatypeids, int mediatypeids_num,
-		zbx_vector_ptr_t *mediatypes)
+		zbx_vector_ptr_t *mediatypes, zbx_vector_uint64_t *auth_email_mediatypeids)
 {
 	DB_RESULT		result;
 	DB_ROW			row;
@@ -360,6 +438,12 @@ static void	am_db_update_mediatypes(zbx_am_db_t *amdb, const zbx_uint64_t *media
 
 		if (NULL != mediatype)
 			zbx_vector_ptr_append(mediatypes, mediatype);
+
+		if (NULL != auth_email_mediatypeids && MEDIA_TYPE_EMAIL == type &&
+				SMTP_AUTHENTICATION_NORMAL_PASSWORD == smtp_authentication)
+		{
+			zbx_vector_uint64_append(auth_email_mediatypeids, mediatypeid);
+		}
 	}
 	zbx_db_free_result(result);
 
@@ -381,10 +465,11 @@ static int	am_db_queue_alerts(zbx_am_db_t *amdb)
 	zbx_vector_ptr_t	alerts, mediatypes;
 	int			i, alerts_num;
 	zbx_am_db_alert_t	*alert;
-	zbx_vector_uint64_t	mediatypeids;
+	zbx_vector_uint64_t	mediatypeids, auth_email_mediatypeids;
 
 	zbx_vector_ptr_create(&alerts);
 	zbx_vector_uint64_create(&mediatypeids);
+	zbx_vector_uint64_create(&auth_email_mediatypeids);
 	zbx_vector_ptr_create(&mediatypes);
 
 	if (FAIL == am_db_get_alerts(&alerts) || 0 == alerts.values_num)
@@ -398,7 +483,10 @@ static int	am_db_queue_alerts(zbx_am_db_t *amdb)
 
 	zbx_vector_uint64_sort(&mediatypeids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
 	zbx_vector_uint64_uniq(&mediatypeids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
-	am_db_update_mediatypes(amdb, mediatypeids.values, mediatypeids.values_num, &mediatypes);
+	am_db_update_mediatypes(amdb, mediatypeids.values, mediatypeids.values_num, &mediatypes,
+			&auth_email_mediatypeids);
+
+	am_db_get_trigger_expressions(&auth_email_mediatypeids, &alerts);
 
 	if (0 != mediatypes.values_num)
 	{
@@ -424,10 +512,10 @@ static int	am_db_queue_alerts(zbx_am_db_t *amdb)
 		zbx_ipc_socket_write(&amdb->am, ZBX_IPC_ALERTER_ALERTS, data, data_len);
 		zbx_free(data);
 	}
-
 out:
 	zbx_vector_ptr_destroy(&mediatypes);
 	zbx_vector_uint64_destroy(&mediatypeids);
+	zbx_vector_uint64_destroy(&auth_email_mediatypeids);
 	alerts_num = alerts.values_num;
 	zbx_vector_ptr_clear_ext(&alerts, (zbx_clean_func_t)zbx_am_db_alert_free);
 	zbx_vector_ptr_destroy(&alerts);
@@ -874,7 +962,7 @@ static void	am_db_update_watchdog(zbx_am_db_t *amdb)
 	{
 		zbx_vector_uint64_sort(&mediatypeids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
 		zbx_vector_uint64_uniq(&mediatypeids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
-		am_db_update_mediatypes(amdb, mediatypeids.values, mediatypeids.values_num, &mediatypes);
+		am_db_update_mediatypes(amdb, mediatypeids.values, mediatypeids.values_num, &mediatypes, NULL);
 
 		if (0 != mediatypes.values_num)
 		{

src/zabbix_server/alerter/alerter.c
@@ -32,6 +32,8 @@
 #include "zbxstr.h"
 #include "zbxtime.h"
 #include "zbxtypes.h"
+#include "zbxserver.h"
+#include "zbxdbwrap.h"
 
 #define	ALARM_ACTION_TIMEOUT	40
 
@@ -152,19 +154,47 @@ static char	*create_email_inreplyto(zbx_uint64_t mediatypeid, const char *sendto
  ******************************************************************************/
 static void	alerter_process_email(zbx_ipc_socket_t *socket, zbx_ipc_message_t *ipc_message)
 {
-	zbx_uint64_t	alertid, mediatypeid, eventid;
+	zbx_uint64_t	alertid, mediatypeid, eventid, objectid;
 	char		*sendto, *subject, *message, *smtp_server, *smtp_helo, *smtp_email, *username, *password,
-			*inreplyto;
+			*inreplyto, *expression, *recovery_expression;
 	unsigned short	smtp_port;
 	unsigned char	smtp_security, smtp_verify_peer, smtp_verify_host, smtp_authentication, content_type;
-	int		ret;
+	int		object, source, ret;
 	char		error[MAX_STRING_LEN];
 
-	zbx_alerter_deserialize_email(ipc_message->data, &alertid, &mediatypeid, &eventid, &sendto, &subject, &message,
-			&smtp_server, &smtp_port, &smtp_helo, &smtp_email, &smtp_security, &smtp_verify_peer,
-			&smtp_verify_host, &smtp_authentication, &username, &password, &content_type);
+	zbx_alerter_deserialize_email(ipc_message->data, &alertid, &mediatypeid, &eventid, &source, &object, &objectid,
+			&sendto, &subject, &message, &smtp_server, &smtp_port, &smtp_helo, &smtp_email, &smtp_security,
+			&smtp_verify_peer, &smtp_verify_host, &smtp_authentication, &username, &password,
+			&content_type, &expression, &recovery_expression);
 
 	inreplyto = create_email_inreplyto(mediatypeid, sendto, eventid);
+
+	if (SMTP_AUTHENTICATION_NORMAL_PASSWORD == smtp_authentication)
+	{
+		/* fill data required by substitute_simple_macros_unmasked() for MACRO_TYPE_ALERT_EMAIL */
+
+		zbx_db_event	event = {.eventid = eventid, .source = source, .object = object, .objectid = objectid};
+
+		memset(&event.trigger, 0, sizeof(zbx_db_trigger));
+		event.trigger.expression = expression;
+		event.trigger.recovery_expression = recovery_expression;
+
+		zbx_dc_um_handle_t	*um_handle = zbx_dc_open_user_macros();
+
+		zbx_substitute_simple_macros_unmasked(NULL, &event, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+				NULL, NULL, NULL, &username, MACRO_TYPE_ALERT_EMAIL, NULL, 0);
+		zbx_substitute_simple_macros_unmasked(NULL, &event, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+				NULL, NULL, NULL, &password, MACRO_TYPE_ALERT_EMAIL, NULL, 0);
+
+		zbx_dc_close_user_macros(um_handle);
+		zbx_db_trigger_clean(&event.trigger);
+	}
+	else
+	{
+		zbx_free(expression);
+		zbx_free(recovery_expression);
+	}
+
 	ret = send_email(smtp_server, smtp_port, smtp_helo, smtp_email, sendto, inreplyto, subject, message,
 			smtp_security, smtp_verify_peer, smtp_verify_host, smtp_authentication, username, password,
 			content_type, ALARM_ACTION_TIMEOUT, error, sizeof(error));

src/libs/zbxserver/expression.c
@@ -4922,6 +4922,29 @@ static int	substitute_simple_macros_impl(const zbx_uint64_t *actionid, const zbx
 				replace_to = zbx_strdup(replace_to, dc_item->key_orig);
 			}
 		}
+		else if (0 == indexed_macro && 0 != (macro_type & MACRO_TYPE_ALERT_EMAIL) &&
+				ZBX_TOKEN_USER_MACRO == token.type)
+		{
+			if ((EVENT_SOURCE_INTERNAL == event->source && EVENT_OBJECT_TRIGGER == event->object) ||
+					EVENT_SOURCE_TRIGGERS == event->source)
+			{
+				if (SUCCEED == zbx_db_trigger_get_all_hostids(&event->trigger, &phostids))
+				{
+					zbx_dc_get_user_macro(um_handle, m, phostids->values, phostids->values_num,
+							&replace_to);
+				}
+			}
+			else if (EVENT_SOURCE_INTERNAL == event->source && (EVENT_OBJECT_ITEM == event->object ||
+					EVENT_OBJECT_LLDRULE == event->object))
+			{
+				cache_item_hostid(&hostids, event->objectid);
+				zbx_dc_get_user_macro(um_handle, m, hostids.values, hostids.values_num, &replace_to);
+			}
+			else
+				zbx_dc_get_user_macro(um_handle, m, NULL, 0, &replace_to);
+
+			pos = token.loc.r;
+		}
 		else if (0 == indexed_macro && 0 != (macro_type & MACRO_TYPE_JMX_ENDPOINT))
 		{
 			if (ZBX_TOKEN_USER_MACRO == token.type)

zabbix/zabbix

(cherry picked from commit 96788be21b8109993a4a091ccef84bab64e1073c)

src/libs/zbxalerter/alert_syncer.c
@@ -35,6 +35,7 @@
 #include "zbxthreads.h"
 #include "zbxtime.h"
 #include "zbxtypes.h"
+#include "zbxmedia.h"
 
 typedef struct
 {
@@ -74,6 +75,9 @@ static zbx_am_db_alert_t	*am_db_create_alert(zbx_uint64_t alertid, zbx_uint64_t 
 
 	alert->status = status;
 	alert->retries = retries;
+
+	alert->expression = NULL;
+	alert->recovery_expression = NULL;
 
 	return alert;
 }
@@ -218,6 +222,78 @@ static int	am_db_get_alerts(zbx_vector_am_db_alert_ptr_t *alerts)
 	return ret;
 }
 
+static void	am_db_get_trigger_expressions(zbx_vector_uint64_t *auth_email_mediatypeids,
+		zbx_vector_am_db_alert_ptr_t *alerts)
+{
+	int			i;
+	char			*sql = NULL;
+	size_t			sql_alloc = 0, sql_offset = 0;
+	zbx_db_result_t		result;
+	zbx_db_row_t		row;
+	zbx_am_db_alert_t	*alert;
+	zbx_vector_uint64_t	triggerids;
+
+	if (0 == auth_email_mediatypeids->values_num)
+		return;
+
+	zbx_vector_uint64_create(&triggerids);
+	zbx_vector_uint64_sort(auth_email_mediatypeids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
+
+	for (i = 0; i < alerts->values_num; i++)
+	{
+		alert = (zbx_am_db_alert_t *)alerts->values[i];
+
+		if (((EVENT_SOURCE_INTERNAL == alert->source && EVENT_OBJECT_TRIGGER == alert->object) ||
+				EVENT_SOURCE_TRIGGERS == alert->source) &&
+				FAIL != zbx_vector_uint64_bsearch(auth_email_mediatypeids, alert->mediatypeid,
+				ZBX_DEFAULT_UINT64_COMPARE_FUNC))
+		{
+			zbx_vector_uint64_append(&triggerids, alert->objectid);
+		}
+	}
+
+	if (0 == triggerids.values_num)
+	{
+		zbx_vector_uint64_destroy(&triggerids);
+		return;
+	}
+
+	zbx_vector_uint64_sort(&triggerids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
+	zbx_vector_uint64_uniq(&triggerids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
+
+	zbx_db_add_condition_alloc(&sql, &sql_alloc, &sql_offset, "triggerid", triggerids.values,
+			triggerids.values_num);
+
+	result = zbx_db_select(
+			"select triggerid,expression,recovery_expression"
+			" from triggers"
+			" where%s",
+			sql);
+
+	while (NULL != (row = zbx_db_fetch(result)))
+	{
+		zbx_uint64_t	triggerid;
+
+		ZBX_STR2UINT64(triggerid, row[0]);
+
+		for (i = 0; i < alerts->values_num; i++)
+		{
+			alert = (zbx_am_db_alert_t *)alerts->values[i];
+
+			if (((EVENT_SOURCE_INTERNAL == alert->source && EVENT_OBJECT_TRIGGER == alert->object) ||
+					EVENT_SOURCE_TRIGGERS == alert->source) && triggerid == alert->objectid)
+			{
+				alert->expression = zbx_strdup(alert->expression, row[1]);
+				alert->recovery_expression = zbx_strdup(alert->recovery_expression, row[2]);
+			}
+		}
+	}
+	zbx_db_free_result(result);
+
+	zbx_free(sql);
+	zbx_vector_uint64_destroy(&triggerids);
+}
+
 #define ZBX_UPDATE_STR(dst, src, ret)				\
 	do							\
 	{							\
@@ -305,14 +381,16 @@ static zbx_am_db_mediatype_t	*am_db_update_mediatype(zbx_am_db_t *amdb, time_t n
  *                                                                            *
  * Purpose: updates alert manager media types                                 *
  *                                                                            *
- * Parameters: amdb             - [IN] alert manager cache                    *
- *             mediatypeids     - [IN]                                        *
- *             mediatypeids_num - [IN]                                        *
- *             mediatypes       - [OUT]                                       *
+ * Parameters: amdb                     - [IN] the alert manager cache        *
+ *             mediatypeids             - [IN]                                *
+ *             medatypeids_num          - [IN]                                *
+ *             mediatypes               - [OUT]                               *
+ *             auth_email_mediatypeids  - [OUT] email media types ids with    *
+ *                                              authentication enabled        *
  *                                                                            *
  ******************************************************************************/
 static void	am_db_update_mediatypes(zbx_am_db_t *amdb, const zbx_uint64_t *mediatypeids, int mediatypeids_num,
-		zbx_vector_am_db_mediatype_ptr_t *mediatypes)
+		zbx_vector_am_db_mediatype_ptr_t *mediatypes, zbx_vector_uint64_t *auth_email_mediatypeids)
 {
 	zbx_db_result_t	result;
 	zbx_db_row_t	row;
@@ -367,6 +445,12 @@ static void	am_db_update_mediatypes(zbx_am_db_t *amdb, const zbx_uint64_t *media
 
 		if (NULL != mediatype)
 			zbx_vector_am_db_mediatype_ptr_append(mediatypes, mediatype);
+
+		if (NULL != auth_email_mediatypeids && MEDIA_TYPE_EMAIL == type &&
+				SMTP_AUTHENTICATION_NORMAL_PASSWORD == smtp_authentication)
+		{
+			zbx_vector_uint64_append(auth_email_mediatypeids, mediatypeid);
+		}
 	}
 	zbx_db_free_result(result);
 
@@ -389,10 +473,11 @@ static int	am_db_queue_alerts(zbx_am_db_t *amdb)
 	zbx_vector_am_db_alert_ptr_t		alerts;
 	int					alerts_num;
 	zbx_am_db_alert_t			*alert;
-	zbx_vector_uint64_t			mediatypeids;
+	zbx_vector_uint64_t			mediatypeids, auth_email_mediatypeids;
 
 	zbx_vector_am_db_alert_ptr_create(&alerts);
 	zbx_vector_uint64_create(&mediatypeids);
+	zbx_vector_uint64_create(&auth_email_mediatypeids);
 	zbx_vector_am_db_mediatype_ptr_create(&mediatypes);
 
 	if (FAIL == am_db_get_alerts(&alerts) || 0 == alerts.values_num)
@@ -406,7 +491,10 @@ static int	am_db_queue_alerts(zbx_am_db_t *amdb)
 
 	zbx_vector_uint64_sort(&mediatypeids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
 	zbx_vector_uint64_uniq(&mediatypeids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
-	am_db_update_mediatypes(amdb, mediatypeids.values, mediatypeids.values_num, &mediatypes);
+	am_db_update_mediatypes(amdb, mediatypeids.values, mediatypeids.values_num, &mediatypes,
+			&auth_email_mediatypeids);
+
+	am_db_get_trigger_expressions(&auth_email_mediatypeids, &alerts);
 
 	if (0 != mediatypes.values_num)
 	{
@@ -440,6 +528,7 @@ static int	am_db_queue_alerts(zbx_am_db_t *amdb)
 out:
 	zbx_vector_am_db_mediatype_ptr_destroy(&mediatypes);
 	zbx_vector_uint64_destroy(&mediatypeids);
+	zbx_vector_uint64_destroy(&auth_email_mediatypeids);
 	alerts_num = alerts.values_num;
 	zbx_vector_am_db_alert_ptr_clear_ext(&alerts, zbx_am_db_alert_free);
 	zbx_vector_am_db_alert_ptr_destroy(&alerts);
@@ -900,7 +989,7 @@ static void	am_db_update_watchdog(zbx_am_db_t *amdb)
 	{
 		zbx_vector_uint64_sort(&mediatypeids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
 		zbx_vector_uint64_uniq(&mediatypeids, ZBX_DEFAULT_UINT64_COMPARE_FUNC);
-		am_db_update_mediatypes(amdb, mediatypeids.values, mediatypeids.values_num, &mediatypes);
+		am_db_update_mediatypes(amdb, mediatypeids.values, mediatypeids.values_num, &mediatypes, NULL);
 
 		if (0 != mediatypes.values_num)
 		{

src/libs/zbxalerter/alerter.c
@@ -33,6 +33,8 @@
 #include "zbxstr.h"
 #include "zbxtime.h"
 #include "zbxtypes.h"
+#include "zbxexpression.h"
+#include "zbxdbwrap.h"
 
 #define	ALARM_ACTION_TIMEOUT	40
 
@@ -158,18 +160,46 @@ static char	*create_email_inreplyto(zbx_uint64_t mediatypeid, const char *sendto
 static void	alerter_process_email(zbx_ipc_socket_t *socket, zbx_ipc_message_t *ipc_message,
 		const char *config_source_ip, const char *config_ssl_ca_location)
 {
-	zbx_uint64_t	alertid, mediatypeid, eventid;
+	zbx_uint64_t	alertid, mediatypeid, eventid, objectid;
 	char		*sendto, *subject, *message, *smtp_server, *smtp_helo, *smtp_email, *username, *password,
-			*inreplyto, *error = NULL;
+			*inreplyto, *expression, *recovery_expression, *error = NULL;
 	unsigned short	smtp_port;
 	unsigned char	smtp_security, smtp_verify_peer, smtp_verify_host, smtp_authentication, content_type;
-	int		ret;
+	int		object, source, ret;
 
-	zbx_alerter_deserialize_email(ipc_message->data, &alertid, &mediatypeid, &eventid, &sendto, &subject, &message,
-			&smtp_server, &smtp_port, &smtp_helo, &smtp_email, &smtp_security, &smtp_verify_peer,
-			&smtp_verify_host, &smtp_authentication, &username, &password, &content_type);
+	zbx_alerter_deserialize_email(ipc_message->data, &alertid, &mediatypeid, &eventid, &source, &object, &objectid,
+			&sendto, &subject, &message, &smtp_server, &smtp_port, &smtp_helo, &smtp_email, &smtp_security,
+			&smtp_verify_peer, &smtp_verify_host, &smtp_authentication, &username, &password,
+			&content_type, &expression, &recovery_expression);
 
 	inreplyto = create_email_inreplyto(mediatypeid, sendto, eventid);
+
+	if (SMTP_AUTHENTICATION_NORMAL_PASSWORD == smtp_authentication)
+	{
+		/* fill data required by substitute_simple_macros_unmasked() for ZBX_MACRO_TYPE_ALERT_EMAIL */
+
+		zbx_db_event	event = {.eventid = eventid, .source = source, .object = object, .objectid = objectid};
+
+		memset(&event.trigger, 0, sizeof(zbx_db_trigger));
+		event.trigger.expression = expression;
+		event.trigger.recovery_expression = recovery_expression;
+
+		zbx_dc_um_handle_t	*um_handle = zbx_dc_open_user_macros();
+
+		zbx_substitute_simple_macros_unmasked(NULL, &event, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+				NULL, NULL, NULL, &username, ZBX_MACRO_TYPE_ALERT_EMAIL, NULL, 0);
+		zbx_substitute_simple_macros_unmasked(NULL, &event, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+				NULL, NULL, NULL, &password, ZBX_MACRO_TYPE_ALERT_EMAIL, NULL, 0);
+
+		zbx_dc_close_user_macros(um_handle);
+		zbx_db_trigger_clean(&event.trigger);
+	}
+	else
+	{
+		zbx_free(expression);
+		zbx_free(recovery_expression);
+	}
+
 	ret = send_email(smtp_server, smtp_port, smtp_helo, smtp_email, sendto, inreplyto, subject, message,
 			smtp_security, smtp_verify_peer, smtp_verify_host, smtp_authentication, username, password,
 			content_type, ALARM_ACTION_TIMEOUT, config_source_ip, config_ssl_ca_location, &error);

src/libs/zbxexpression/expression.c
@@ -2521,6 +2521,29 @@ int	substitute_simple_macros_impl(const zbx_uint64_t *actionid, const zbx_db_eve
 				replace_to = zbx_strdup(replace_to, dc_item->key_orig);
 			}
 		}
+		else if (0 == indexed_macro && 0 != (macro_type & ZBX_MACRO_TYPE_ALERT_EMAIL) &&
+				ZBX_TOKEN_USER_MACRO == token.type)
+		{
+			if ((EVENT_SOURCE_INTERNAL == event->source && EVENT_OBJECT_TRIGGER == event->object) ||
+					EVENT_SOURCE_TRIGGERS == event->source)
+			{
+				if (SUCCEED == zbx_db_trigger_get_all_hostids(&event->trigger, &phostids))
+				{
+					zbx_dc_get_user_macro(um_handle, m, phostids->values, phostids->values_num,
+							&replace_to);
+				}
+			}
+			else if (EVENT_SOURCE_INTERNAL == event->source && (EVENT_OBJECT_ITEM == event->object ||
+					EVENT_OBJECT_LLDRULE == event->object))
+			{
+				cache_item_hostid(&hostids, event->objectid);
+				zbx_dc_get_user_macro(um_handle, m, hostids.values, hostids.values_num, &replace_to);
+			}
+			else
+				zbx_dc_get_user_macro(um_handle, m, NULL, 0, &replace_to);
+
+			pos = token.loc.r;
+		}
 		else if (0 == indexed_macro && 0 != (macro_type & ZBX_MACRO_TYPE_JMX_ENDPOINT))
 		{
 			if (ZBX_TOKEN_USER_MACRO == token.type || (ZBX_TOKEN_USER_FUNC_MACRO == token.type &&