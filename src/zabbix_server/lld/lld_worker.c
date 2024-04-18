bf95ff41925bf54050539edb9c966c1d3f99bfc2
Armands Arseniuss Skolmeisters <armands.skolmeisters@zabbix.com>
2024-04-18 18:00:54 +0300

    ........S. [ZBXNEXT-8970] removed unnecessary warnings

--- src/zabbix_server/lld/lld_worker.c
+++ src/zabbix_server/lld/lld_worker.c
@@ -226,10 +226,7 @@ ZBX_THREAD_ENTRY(lld_worker_thread, args)
 #undef STAT_INTERVAL
 		zbx_update_selfmon_counter(info, ZBX_PROCESS_STATE_IDLE);
 		if (SUCCEED != zbx_ipc_socket_read(&lld_socket, &message))
-		{
-			zabbix_log(LOG_LEVEL_CRIT, "cannot read LLD manager service request");
 			exit(EXIT_FAILURE);
-		}
 		zbx_update_selfmon_counter(info, ZBX_PROCESS_STATE_BUSY);
 
 		time_read = zbx_time();
