fd081ace204daf49eb507bc140c1d76ca8f97446
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2024-04-23 09:39:06 +0300

    .......... [ZBXNEXT-826] updated release version to 6.0.30rc1

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "22 April 2024";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "6.0.29";
+	static final String VERSION = "6.0.30rc1";
 
 	static void printVersion()
 	{
--- ui/include/defines.inc.php
+++ ui/include/defines.inc.php
@@ -18,8 +18,8 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-define('ZABBIX_VERSION',		'6.0.29');
-define('ZABBIX_API_VERSION',	'6.0.29');
+define('ZABBIX_VERSION',		'6.0.30rc1');
+define('ZABBIX_API_VERSION',	'6.0.30');
 define('ZABBIX_EXPORT_VERSION',	'6.0');
 
 define('ZABBIX_DB_VERSION',		6000000);
