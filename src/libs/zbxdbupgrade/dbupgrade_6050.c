d8b4e563bbcaa97b1a6fc21fe4bb3ea413ce235a
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2024-05-16 12:06:52 -0100

    ........S. [ZBXNEXT-8686] fixed code formatting according to coding guidelines

--- src/libs/zbxdbupgrade/dbupgrade_6050.c
+++ src/libs/zbxdbupgrade/dbupgrade_6050.c
@@ -399,11 +399,15 @@ static int	DBpatch_6050032(void)
 
 static int	DBpatch_6050033(void)
 {
+	zbx_uint64_t	moduleid;
+
 	if (0 == (DBget_program_type() & ZBX_PROGRAM_TYPE_SERVER))
 		return SUCCEED;
 
+	moduleid = zbx_db_get_maxid("module");
+
 	if (ZBX_DB_OK > zbx_db_execute("insert into module (moduleid,id,relative_path,status,config) values"
-			" (" ZBX_FS_UI64 ",'toptriggers','widgets/toptriggers',%d,'[]')", zbx_db_get_maxid("module"), 1))
+			" (" ZBX_FS_UI64 ",'toptriggers','widgets/toptriggers',%d,'[]')", moduleid, 1))
 	{
 		return FAIL;
 	}
@@ -478,9 +482,9 @@ static int	DBpatch_6050039(void)
 			" where h.status in (%i,%i)",
 			DEPRECATED_STATUS_PROXY_PASSIVE, DEPRECATED_STATUS_PROXY_ACTIVE);
 
-	zbx_db_insert_prepare(&db_insert_proxies, "proxy", "proxyid", "name", "operating_mode", "description", "tls_connect",
-			"tls_accept", "tls_issuer", "tls_subject", "tls_psk_identity", "tls_psk", "allowed_addresses",
-			"address", "port", (char *)NULL);
+	zbx_db_insert_prepare(&db_insert_proxies, "proxy", "proxyid", "name", "operating_mode", "description",
+			"tls_connect", "tls_accept", "tls_issuer", "tls_subject", "tls_psk_identity", "tls_psk",
+			"allowed_addresses", "address", "port", (char *)NULL);
 
 	while (NULL != (row = zbx_db_fetch(result)))
 	{
@@ -494,8 +498,8 @@ static int	DBpatch_6050039(void)
 
 		if (DEPRECATED_STATUS_PROXY_ACTIVE == status)
 		{
-			zbx_db_insert_add_values(&db_insert_proxies, proxyid, row[1], PROXY_OPERATING_MODE_ACTIVE, row[3],
-					tls_connect, tls_accept, row[6], row[7], row[8], row[9], row[10],
+			zbx_db_insert_add_values(&db_insert_proxies, proxyid, row[1], PROXY_OPERATING_MODE_ACTIVE,
+					row[3], tls_connect, tls_accept, row[6], row[7], row[8], row[9], row[10],
 					"127.0.0.1", "10051");
 		}
 		else if (DEPRECATED_STATUS_PROXY_PASSIVE == status)
@@ -515,8 +519,9 @@ static int	DBpatch_6050039(void)
 				zabbix_log(LOG_LEVEL_WARNING, "cannot select interface for proxy '%s'",  row[1]);
 			}
 
-			zbx_db_insert_add_values(&db_insert_proxies, proxyid, row[1], PROXY_OPERATING_MODE_PASSIVE, row[3],
-					tls_connect, tls_accept, row[6], row[7], row[8], row[9], "", address, port);
+			zbx_db_insert_add_values(&db_insert_proxies, proxyid, row[1], PROXY_OPERATING_MODE_PASSIVE,
+					row[3], tls_connect, tls_accept, row[6], row[7], row[8], row[9], "", address,
+					port);
 		}
 	}
 	zbx_db_free_result(result);
@@ -4013,11 +4018,15 @@ out:
 
 static int	DBpatch_6050295(void)
 {
+	zbx_uint64_t	moduleid;
+
 	if (0 == (DBget_program_type() & ZBX_PROGRAM_TYPE_SERVER))
 		return SUCCEED;
 
+	moduleid = zbx_db_get_maxid("module");
+
 	if (ZBX_DB_OK > zbx_db_execute("insert into module (moduleid,id,relative_path,status,config) values"
-			" (" ZBX_FS_UI64 ",'itemnavigator','widgets/itemnavigator',%d,'[]')", zbx_db_get_maxid("module"), 1))
+			" (" ZBX_FS_UI64 ",'itemnavigator','widgets/itemnavigator',%d,'[]')", moduleid, 1))
 	{
 		return FAIL;
 	}
