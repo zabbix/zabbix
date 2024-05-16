47ffa9cf2cb0fc2513e8936a977041989c36add0
Andrejs Verza <andrejs.verza@zabbix.com>
2024-05-16 12:03:00 +0300

    ..F....... [ZBX-24488] fixed template importer to correctly recognize plaintext widgets

--- ui/include/classes/import/converters/C64ImportConverter.php
+++ ui/include/classes/import/converters/C64ImportConverter.php
@@ -351,6 +351,43 @@ class C64ImportConverter extends CConverter {
 				}
 
 				foreach ($dashboard_page['widgets'] as &$widget) {
+					if (array_key_exists('x', $widget) && is_numeric($widget['x'])) {
+						$widget['x'] = (string) ((int) $widget['x'] * 3);
+					}
+
+					if (array_key_exists('width', $widget)) {
+						if (is_numeric($widget['width'])) {
+							$widget['width'] = (string) ((int) $widget['width'] * 3);
+						}
+					}
+					else {
+						$widget['width'] = '3';
+					}
+
+					if (array_key_exists('fields', $widget)) {
+						foreach ($widget['fields'] as &$field) {
+							$field['name'] = preg_replace('/^([a-z]+)\.([a-z_]+)\.(\d+)\.(\d+)$/',
+								'$1.$3.$2.$4', $field['name']
+							);
+							$field['name'] = preg_replace('/^([a-z]+)\.([a-z_]+)\.(\d+)$/',
+								'$1.$3.$2', $field['name']
+							);
+						}
+						unset($field);
+					}
+
+					if (in_array($widget['type'], ['graph', 'graphprototype'])) {
+						if (!array_key_exists('fields', $widget)) {
+							$widget['fields'] = [];
+						}
+
+						$widget['fields'][] = [
+							'type' => CXmlConstantName::DASHBOARD_WIDGET_FIELD_TYPE_STRING,
+							'name' => 'reference',
+							'value' => self::createWidgetReference($reference_index++)
+						];
+					}
+
 					if ($widget['type'] === 'plaintext') {
 						$widget['type'] = 'itemhistory';
 
@@ -362,6 +399,11 @@ class C64ImportConverter extends CConverter {
 								'type' => CXmlConstantName::DASHBOARD_WIDGET_FIELD_TYPE_INTEGER,
 								'name' => 'show_timestamp',
 								'value' => '1'
+							],
+							[
+								'type' => CXmlConstantName::DASHBOARD_WIDGET_FIELD_TYPE_STRING,
+								'name' => 'reference',
+								'value' => self::createWidgetReference($reference_index++)
 							]
 						];
 
@@ -422,49 +464,13 @@ class C64ImportConverter extends CConverter {
 						$widget['fields'] = $new_fields;
 					}
 
-					if (in_array($widget['type'], ['graph', 'graphprototype', 'itemhistory'])) {
-						$reference = self::createWidgetReference($reference_index++);
-
-						if (!array_key_exists('fields', $widget)) {
-							$widget['fields'] = [];
-						}
-
-						$widget['fields'][] = [
-							'type' => 'STRING',
-							'name' => 'reference',
-							'value' => $reference
-						];
-
+					if (array_key_exists('fields', $widget)) {
+						// Fields must be sorted not to trigger diff view.
 						usort($widget['fields'],
-							static function(array $widget_field_a, array $widget_field_b): int {
+							static function (array $widget_field_a, array $widget_field_b): int {
 								return strnatcasecmp($widget_field_a['name'], $widget_field_b['name']);
 							}
 						);
-					}
-
-					if (array_key_exists('fields', $widget)) {
-						foreach ($widget['fields'] as &$field) {
-							$field['name'] = preg_replace('/^([a-z]+)\.([a-z_]+)\.(\d+)\.(\d+)$/',
-								'$1.$3.$2.$4', $field['name']
-							);
-							$field['name'] = preg_replace('/^([a-z]+)\.([a-z_]+)\.(\d+)$/',
-								'$1.$3.$2', $field['name']
-							);
-						}
-						unset($field);
-					}
-
-					if (array_key_exists('x', $widget) && is_numeric($widget['x'])) {
-						$widget['x'] = (string) ((int) $widget['x'] * 3);
-					}
-
-					if (array_key_exists('width', $widget)) {
-						if (is_numeric($widget['width'])) {
-							$widget['width'] = (string) ((int) $widget['width'] * 3);
-						}
-					}
-					else {
-						$widget['width'] = '3';
 					}
 				}
 				unset($widget);
