2e56b21ceb4e6dce41b414b68002865bde434833
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2024-05-20 20:28:23 +0300

    .......... [ZBXNEXT-686] stabilized remove item tag test

--- ui/tests/selenium/common/testFormTags.php
+++ ui/tests/selenium/common/testFormTags.php
@@ -1353,7 +1353,7 @@ class testFormTags extends CWebTest {
 				$this->query('button:Apply')->one()->waitUntilClickable()->click();
 			}
 
-			$this->query('link', $this->remove_name)->waitUntilPresent()->one()->click();
+			$this->query('link', $this->remove_name)->waitUntilPresent()->one()->forceClick();
 		}
 
 		$locators = [
