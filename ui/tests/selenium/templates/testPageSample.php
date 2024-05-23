dd1c409fdcf6ec081b3be1b3bdf373da37d1266e
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2024-05-23 15:54:29 -0100

    .......... [DEV-3736] fixed formatting of the file headers

--- ui/tests/selenium/templates/testPageSample.php
+++ ui/tests/selenium/templates/testPageSample.php
@@ -1,18 +1,19 @@
+<?php
 /*
-** zabbix
-** copyright (c) 2001-2024 zabbix sia
+** Zabbix
+** Copyright (C) 2001-2024 Zabbix SIA
 **
-** this program is free software; you can redistribute it and/or modify
-** it under the terms of the gnu general public license as published by
-** the free software foundation; either version 2 of the license, or
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
 **
-** this program is distributed in the hope that it will be useful,
-** but without any warranty; without even the implied warranty of
-** merchantability or fitness for a particular purpose. see the
-** gnu general public license for more details.
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
 **
-** you should have received a copy of the gnu general public license
-** along with this program; if not, write to the free software
-** foundation, inc., 51 franklin street, fifth floor, boston, ma  02110-1301, usa.
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
