ce99b61aba64512e9591726753c8cade3eb15870
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2024-05-29 15:13:53 +0300

    .......... [DEV-2560] fixed copyright note

--- tests/libs/zbxicmpping/get_interval_option.c
+++ tests/libs/zbxicmpping/get_interval_option.c
@@ -1,20 +1,15 @@
 /*
-** Zabbix
-** Copyright (C) 2001-2023 Zabbix SIA
+** Copyright (C) 2001-2024 Zabbix SIA
 **
-** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 2 of the License, or
-** (at your option) any later version.
+** This program is free software: you can redistribute it and/or modify it under the terms of
+** the GNU Affero General Public License as published by the Free Software Foundation, version 3.
 **
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-** GNU General Public License for more details.
+** This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+** without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU Affero General Public License for more details.
 **
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+** You should have received a copy of the GNU Affero General Public License along with this program.
+** If not, see <https://www.gnu.org/licenses/>.
 **/
 
 #include "zbxmocktest.h"
@@ -104,7 +99,7 @@ void	zbx_mock_test_entry(void **state)
 	hosts[0].addr = (char *)zbx_mock_get_parameter_string("in.target_host_addr");
 	hosts[0].status = status;
 
-	ret = get_interval_option("/usr/bin/fping", hosts, 1, &value, error, ZBX_ITEM_ERROR_LEN_MAX);
+	ret = get_interval_option("/usr/bin/fping", hosts, hosts_cnt, &value, error, ZBX_ITEM_ERROR_LEN_MAX);
 
 	zbx_mock_assert_int_eq("get_interval_option() return value", zbx_mock_str_to_return_code("SUCCEED"), ret);
 	zbx_mock_assert_str_eq("error message returned by get_interval_option()", "", error);
