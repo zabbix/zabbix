d9eee54ca6943d421515a795e81fff6151c13e69
Andris Zeila <andris.zeila@zabbix.com>
2024-05-31 15:40:21 +0300

    .......PS. [DEV-3755] reworked native object attachment to js objects

--- src/libs/zbxembed/browser_element.c
+++ src/libs/zbxembed/browser_element.c
@@ -33,34 +33,43 @@ zbx_wd_element_t;
 
 /******************************************************************************
  *                                                                            *
- * Purpose: return backing C structure embedded in element object         *
+ * Purpose: return backing C structure embedded in element object             *
  *                                                                            *
  ******************************************************************************/
 static zbx_wd_element_t *wd_element(duk_context *ctx)
 {
 	zbx_wd_element_t	*el;
+	zbx_es_env_t		*env;
 
-	duk_push_this(ctx);
-	duk_get_prop_string(ctx, -1, "\xff""\xff""d");
-	el = (zbx_wd_element_t *)duk_to_pointer(ctx, -1);
+	if (NULL == (env = zbx_es_get_env(ctx)))
+	{
+		(void)duk_push_error_object(ctx, DUK_RET_EVAL_ERROR, "cannot access internal environment");
+
+		return NULL;
+	}
+
+	if (NULL == (el = (zbx_wd_element_t *)es_obj_get_data(env)))
+		(void)duk_push_error_object(ctx, DUK_RET_EVAL_ERROR, "cannot find native data attached to object");
 
 	return el;
 }
 
 /******************************************************************************
  *                                                                            *
- * Purpose: element destructor                                            *
+ * Purpose: element destructor                                                *
  *                                                                            *
  ******************************************************************************/
 static duk_ret_t	wd_element_dtor(duk_context *ctx)
 {
 	zbx_wd_element_t	*el;
+	zbx_es_env_t		*env;
 
 	zabbix_log(LOG_LEVEL_TRACE, "Element::~Element()");
 
-	duk_get_prop_string(ctx, 0, "\xff""\xff""d");
+	if (NULL == (env = zbx_es_get_env(ctx)))
+		return duk_error(ctx, DUK_RET_EVAL_ERROR, "cannot access internal environment");
 
-	if (NULL != (el = (zbx_wd_element_t *)duk_to_pointer(ctx, -1)))
+	if (NULL != (el = (zbx_wd_element_t *)es_obj_detach_data(env)))
 	{
 		webdriver_release(el->wd);
 		zbx_free(el->id);
@@ -78,18 +87,19 @@ static duk_ret_t	wd_element_dtor(duk_context *ctx)
 static duk_ret_t	wd_element_ctor(duk_context *ctx, zbx_webdriver_t *wd, const char *elementid)
 {
 	zbx_wd_element_t	*el;
+	zbx_es_env_t		*env;
 
 	zabbix_log(LOG_LEVEL_TRACE, "Element::Element()");
+
+	if (NULL == (env = zbx_es_get_env(ctx)))
+		return duk_error(ctx, DUK_RET_TYPE_ERROR, "cannot access internal environment");
 
 	el = (zbx_wd_element_t *)zbx_malloc(NULL, sizeof(zbx_wd_element_t));
 	el->wd = webdriver_addref(wd);
 	el->id = zbx_strdup(NULL, elementid);
 
 	duk_push_object(ctx);
-	duk_push_string(ctx, "\xff""\xff""d");
-	duk_push_pointer(ctx, el);
-	duk_def_prop(ctx, -3, DUK_DEFPROP_HAVE_VALUE | DUK_DEFPROP_CLEAR_WRITABLE | DUK_DEFPROP_HAVE_ENUMERABLE |
-			DUK_DEFPROP_HAVE_CONFIGURABLE);
+	es_obj_attach_data(env, el);
 
 	duk_push_c_function(ctx, wd_element_dtor, 1);
 	duk_set_finalizer(ctx, -2);
