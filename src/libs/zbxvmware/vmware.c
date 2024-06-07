9555659d5c81557292baa67738b3a1240996043b
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2024-06-07 12:05:29 +0300

    .......PS. [ZBX-7998] added VMware service username, password and URL check for empty values
    * commit '411e08dd148c3d0457bd6d0566e09025e7ac3009':
      .......PS. [ZBX-7998] added VMware service username, password and URL check for empty values
      .......PS. [ZBX-7998] improved VMware service URL validation
    
    (cherry picked from commit 7b3488765c01c78b383e3a7ef5910a085a60fd6c)
    (cherry picked from commit 6f1bb3874eabe1d3b245245ce69a7d7f826ec6dc)

--- src/libs/zbxvmware/vmware.c
+++ src/libs/zbxvmware/vmware.c
@@ -877,6 +877,39 @@ static void	vmware_data_free(zbx_vmware_data_t *data)
 	zbx_free(data);
 }
 
+/******************************************************************************
+ *                                                                            *
+ * Purpose: validation of url suffix                                          *
+ *                                                                            *
+ * Parameters: url   - [IN]                                                   *
+ * Parameters: error - [OUT]                                                  *
+ *                                                                            *
+ ******************************************************************************/
+static void	vmware_validate_url_suffix(const char *url, char **error)
+{
+#define VMWARE_URL_SUFFIX_SDK	"/sdk"
+	size_t	len = strlen(url);
+
+	if (ZBX_CONST_STRLEN(VMWARE_URL_SUFFIX_SDK) > len ||
+			0 != strcmp(url + (len - ZBX_CONST_STRLEN(VMWARE_URL_SUFFIX_SDK)), VMWARE_URL_SUFFIX_SDK))
+	{
+		*error = zbx_strdup(*error, "Invalid URL: missing '" VMWARE_URL_SUFFIX_SDK "'.");
+	}
+#undef VMWARE_URL_SUFFIX_SDK
+}
+
+#define VMWARE_VALIDATE_EMPTY(field, field_str)						\
+do											\
+{											\
+	if ('\0' == *field)								\
+	{										\
+		if (NULL == *error)							\
+			*error = zbx_dsprintf(*error, "Empty %s", field_str);		\
+		else									\
+			*error = zbx_dsprintf(*error, "%s, %s", *error, field_str);	\
+	}										\
+} while(0)
+
 /*******************************************************************************
  *                                                                             *
  * Parameters: service               - [IN] vmware service                     *
@@ -913,6 +946,16 @@ int	vmware_service_authenticate(zbx_vmware_service_t *service, CURL *easyhandle,
 	int		ret = FAIL;
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s() '%s'@'%s'", __func__, service->username, service->url);
+
+	VMWARE_VALIDATE_EMPTY(service->url, "URL");
+	VMWARE_VALIDATE_EMPTY(service->username, "username");
+	VMWARE_VALIDATE_EMPTY(service->password, "password");
+
+	if (NULL != *error)
+	{
+		*error = zbx_strdcat(*error, ".");
+		goto out;
+	}
 
 	if (CURLE_OK != (err = curl_easy_setopt(easyhandle, opt = CURLOPT_COOKIEFILE, "")) ||
 			CURLE_OK != (err = curl_easy_setopt(easyhandle, opt = CURLOPT_FOLLOWLOCATION, 1L)) ||
@@ -957,7 +1000,10 @@ int	vmware_service_authenticate(zbx_vmware_service_t *service, CURL *easyhandle,
 				username_esc, password_esc);
 
 		if (SUCCEED != zbx_soap_post(__func__, easyhandle, xml, &doc, NULL, error) && NULL == doc)
+		{
+			vmware_validate_url_suffix(service->url, error);
 			goto out;
+		}
 
 		if (NULL == *error)
 		{
@@ -987,7 +1033,10 @@ int	vmware_service_authenticate(zbx_vmware_service_t *service, CURL *easyhandle,
 			get_vmware_service_objects()[service->type].session_manager, username_esc, password_esc);
 
 	if (SUCCEED != zbx_soap_post(__func__, easyhandle, xml, NULL, NULL, error))
+	{
+		vmware_validate_url_suffix(service->url, error);
 		goto out;
+	}
 
 	ret = SUCCEED;
 out:
@@ -1001,6 +1050,8 @@ out:
 	return ret;
 #	undef ZBX_POST_VMWARE_AUTH
 }
+
+#undef VMWARE_VALIDATE_EMPTY
 
 /******************************************************************************
  *                                                                            *
