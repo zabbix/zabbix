yusufarslanturk/zabbix-bgmot-2fa

........S. [ZBX-24119] fixed possible blocking of alert manager when it periodically pings database

include/zbxdbhigh.h
@@ -486,6 +486,7 @@ void	zbx_db_table_prepare(const char *tablename, struct zbx_json *json);
 #endif
 int		zbx_db_execute(const char *fmt, ...) __zbx_attr_format_printf(1, 2);
 int		zbx_db_execute_once(const char *fmt, ...) __zbx_attr_format_printf(1, 2);
+DB_RESULT	zbx_db_select_once(const char *fmt, ...)__zbx_attr_format_printf(1, 2);
 DB_RESULT	zbx_db_select(const char *fmt, ...) __zbx_attr_format_printf(1, 2);
 DB_RESULT	zbx_db_select_n(const char *query, int n);
 DB_ROW		zbx_db_fetch(DB_RESULT result);

src/zabbix_server/alerter/alert_manager.c
@@ -2256,6 +2256,28 @@ static void	am_process_diag_top_sources(zbx_am_t *manager, zbx_ipc_client_t *cli
 	zabbix_log(LOG_LEVEL_DEBUG, "End of %s()", __func__);
 }
 
+static int	am_check_dbstatus()
+{
+	int		ret = ZBX_DB_OK;
+	DB_RESULT	res;
+
+#ifdef HAVE_ORACLE
+	res = zbx_db_select_once("select null from dual");
+#else
+	res = zbx_db_select_once("select null");
+#endif
+
+	if ((DB_RESULT)ZBX_DB_DOWN == res || NULL == res)
+	{
+		zbx_db_close();
+		ret = zbx_db_connect(ZBX_DB_CONNECT_ONCE);
+	}
+	else
+		zbx_db_free_result(res);
+
+	return ret;
+}
+
 ZBX_THREAD_ENTRY(zbx_alert_manager_thread, args)
 {
 	zbx_thread_alert_manager_args	*alert_manager_args_in = (zbx_thread_alert_manager_args *)
@@ -2289,7 +2311,7 @@ ZBX_THREAD_ENTRY(zbx_alert_manager_thread, args)
 		exit(EXIT_FAILURE);
 	}
 
-	manager.dbstatus = ZBX_DB_OK;
+	manager.dbstatus = zbx_db_connect(ZBX_DB_CONNECT_ONCE);
 
 	/* initialize statistics */
 	time_stat = zbx_time();
@@ -2303,8 +2325,7 @@ ZBX_THREAD_ENTRY(zbx_alert_manager_thread, args)
 
 		if ((time_ping + ZBX_DB_PING_FREQUENCY) < now)
 		{
-			manager.dbstatus = zbx_db_connect(ZBX_DB_CONNECT_ONCE);
-			zbx_db_close();
+			manager.dbstatus = am_check_dbstatus();
 			time_ping = now;
 		}
 		if (ZBX_DB_DOWN == manager.dbstatus)

src/libs/zbxdbhigh/db.c
@@ -481,6 +481,27 @@ DB_ROW	zbx_db_fetch(DB_RESULT result)
  *                                                                            *
  * Purpose: execute a select statement                                        *
  *                                                                            *
+ * Comments: don't retry until DB is up                                       *
+ *                                                                            *
+ ******************************************************************************/
+DB_RESULT	zbx_db_select_once(const char *fmt, ...)
+{
+	va_list		args;
+	DB_RESULT	rc;
+
+	va_start(args, fmt);
+
+	rc = zbx_db_vselect(fmt, args);
+
+	va_end(args);
+
+	return rc;
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Purpose: execute a select statement                                        *
+ *                                                                            *
  * Comments: retry until DB is up                                             *
  *                                                                            *
  ******************************************************************************/

zabbix/zabbix

(cherry picked from commit 1f21d801f2f944d96268ebfebc25f8df194ac993)

include/zbxdbhigh.h
@@ -501,6 +501,7 @@ int	zbx_db_check_oracle_colum_type(const char *table_name, const char *column_na
 #endif
 int		zbx_db_execute(const char *fmt, ...) __zbx_attr_format_printf(1, 2);
 int		zbx_db_execute_once(const char *fmt, ...) __zbx_attr_format_printf(1, 2);
+zbx_db_result_t	zbx_db_select_once(const char *fmt, ...)__zbx_attr_format_printf(1, 2);
 zbx_db_result_t	zbx_db_select(const char *fmt, ...) __zbx_attr_format_printf(1, 2);
 zbx_db_result_t	zbx_db_select_n(const char *query, int n);
 zbx_db_row_t	zbx_db_fetch(zbx_db_result_t result);

src/libs/zbxalerter/alert_manager.c
@@ -2275,6 +2275,28 @@ static void	am_process_diag_top_sources(zbx_am_t *manager, zbx_ipc_client_t *cli
 	zabbix_log(LOG_LEVEL_DEBUG, "End of %s()", __func__);
 }
 
+static int	am_check_dbstatus()
+{
+	int		ret = ZBX_DB_OK;
+	zbx_db_result_t	res;
+
+#ifdef HAVE_ORACLE
+	res = zbx_db_select_once("select null from dual");
+#else
+	res = zbx_db_select_once("select null");
+#endif
+
+	if ((zbx_db_result_t)ZBX_DB_DOWN == res || NULL == res)
+	{
+		zbx_db_close();
+		ret = zbx_db_connect(ZBX_DB_CONNECT_ONCE);
+	}
+	else
+		zbx_db_free_result(res);
+
+	return ret;
+}
+
 ZBX_THREAD_ENTRY(zbx_alert_manager_thread, args)
 {
 #define ZBX_DB_PING_FREQUENCY			SEC_PER_MIN
@@ -2304,7 +2326,7 @@ ZBX_THREAD_ENTRY(zbx_alert_manager_thread, args)
 		exit(EXIT_FAILURE);
 	}
 
-	manager.dbstatus = ZBX_DB_OK;
+	manager.dbstatus = zbx_db_connect(ZBX_DB_CONNECT_ONCE);
 
 	/* initialize statistics */
 	time_stat = zbx_time();
@@ -2327,8 +2349,7 @@ ZBX_THREAD_ENTRY(zbx_alert_manager_thread, args)
 
 		if ((time_ping + ZBX_DB_PING_FREQUENCY) < now)
 		{
-			manager.dbstatus = zbx_db_connect(ZBX_DB_CONNECT_ONCE);
-			zbx_db_close();
+			manager.dbstatus = am_check_dbstatus();
 			time_ping = now;
 		}
 		if (ZBX_DB_DOWN == manager.dbstatus)

src/libs/zbxdbhigh/db.c
@@ -502,6 +502,27 @@ zbx_db_row_t	zbx_db_fetch(zbx_db_result_t result)
  *                                                                            *
  * Purpose: execute a select statement                                        *
  *                                                                            *
+ * Comments: don't retry until DB is up                                       *
+ *                                                                            *
+ ******************************************************************************/
+zbx_db_result_t	zbx_db_select_once(const char *fmt, ...)
+{
+	va_list		args;
+	zbx_db_result_t	rc;
+
+	va_start(args, fmt);
+
+	rc = zbx_db_vselect(fmt, args);
+
+	va_end(args);
+
+	return rc;
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Purpose: execute a select statement                                        *
+ *                                                                            *
  * Comments: retry until DB is up                                             *
  *                                                                            *
  ******************************************************************************/