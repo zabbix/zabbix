8f5c5d228e304313832a59ce42e1655cf539cd3b
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2024-06-14 12:30:31 +0300

    .......... [ZBXNEXT-686] added trigger to test data

--- ui/tests/selenium/dashboards/testDashboardsTemplatedDashboardForm.php
+++ ui/tests/selenium/dashboards/testDashboardsTemplatedDashboardForm.php
@@ -95,6 +95,13 @@ class testDashboardsTemplatedDashboardForm extends CWebTest {
 			]
 		]);
 
+		CDataHelper::call('trigger.create', [
+			[
+				'description' => 'item disabled for enabled trigger',
+				'expression' => 'last(/'.self::TEMPLATE.'/templ_key[1])=0'
+			]
+		]);
+
 		$item_protototypes = CDataHelper::call('itemprototype.create', [
 			[
 				'hostid' => self::$update_templateid,
