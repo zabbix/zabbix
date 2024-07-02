a74ad8bacfdc6f60311db3abdd476fa1c761b617
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2024-07-02 11:14:24 +0300

    ...G....S. [ZBX-24719] fixed debug

--- src/go/plugins/systemrun/systemrun.go
+++ src/go/plugins/systemrun/systemrun.go
@@ -68,8 +68,8 @@ func (p *Plugin) Export(key string, params []string, ctx plugin.ContextProvider)
 		return nil, fmt.Errorf("Invalid first parameter.")
 	}
 
-	fmt.Printf("CTX IS NIL 2: %d", ctx==nil)
-	fmt.Printf("CTX TIMEOUT 2: %s", ctx.Timeout())
+	fmt.Printf("CTX IS NIL 2: %t", ctx==nil)
+	fmt.Printf("CTX TIMEOUT 2: %d", ctx.Timeout())
 
 	if p.options.LogRemoteCommands == 1 && (ctx == nil || ctx.ClientID() != agent.LocalChecksClientID) {
 		p.Warningf("Executing command:'%s'", params[0])
