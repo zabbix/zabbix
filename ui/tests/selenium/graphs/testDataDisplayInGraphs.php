fe432a6f13499b47e8fa83e2350e939ac0617ef1
Natalja Romancaka <natalja.romancaka@zabbix.com>
2024-07-09 13:22:54 +0300

    .......... [ZBXNEXT-686] fixed Selenium tests, added missing function to framework and additional waits

--- ui/tests/selenium/graphs/testDataDisplayInGraphs.php
+++ ui/tests/selenium/graphs/testDataDisplayInGraphs.php
@@ -2190,6 +2190,7 @@ class testDataDisplayInGraphs extends CWebTest {
 			}
 
 			$filter_form->submit();
+			$this->page->waitUntilReady();
 
 			// Switch to kiosk mode if screenshot needs to be checked in Kiosk mode.
 			if (CTestArrayHelper::get($data, 'kiosk_mode')) {
