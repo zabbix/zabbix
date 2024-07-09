dd93a4bbf75de97078460d1f10a2b0ba7a6e42d6
Continuous Integration <ci@zabbix.com>
2024-07-09 14:36:21 +0300

    .......... [ZBXNEXT-826] updated release date for 6.0.32rc1

--- include/version.h
+++ include/version.h
@@ -24,7 +24,7 @@
 #define ZBX_STR(str)	ZBX_STR2(str)
 
 #define APPLICATION_NAME	"Zabbix Agent"
-#define ZABBIX_REVDATE		"17 June 2024"
+#define ZABBIX_REVDATE		"9 July 2024"
 #define ZABBIX_VERSION_MAJOR	6
 #define ZABBIX_VERSION_MINOR	0
 #define ZABBIX_VERSION_PATCH	32
