b6fca7c293f897d9a0279833f8e8c7521fb61bf4
Romans Trizna <romans.trizna@zabbix.com>
2024-08-13 13:58:21 +0300

    .......... [DEV-3899] Increased cmocka coverage for libzbxstr

--- tests/libs/zbxstr/zbx_remove_chars.c
+++ tests/libs/zbxstr/zbx_remove_chars.c
@@ -27,11 +27,11 @@ void	zbx_mock_test_entry(void **state)
 
 	src = zbx_mock_get_parameter_string("in.string");
 
-    list =  zbx_mock_get_parameter_string("in.list");
+	list =  zbx_mock_get_parameter_string("in.list");
 
 	exp_result = zbx_mock_get_parameter_string("out.return");
 
 	zbx_remove_chars(src, list);
 
-    zbx_mock_assert_str_eq("return value",  exp_result, src);
+	zbx_mock_assert_str_eq("return value",  exp_result, src);
 }
