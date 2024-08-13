eb48688f93b6137cd8a7e4538e4c9ee48f807a4a
Romans Trizna <romans.trizna@zabbix.com>
2024-08-13 13:56:21 +0300

    .......... [DEV-3899] Increased cmocka coverage for libzbxstr

---
+++ tests/libs/zbxstr/zbx_remove_chars.c
@@ -0,0 +1,37 @@
+/*
+** Copyright (C) 2001-2024 Zabbix SIA
+**
+** This program is free software: you can redistribute it and/or modify it under the terms of
+** the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+**
+** This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+** without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU Affero General Public License for more details.
+**
+** You should have received a copy of the GNU Affero General Public License along with this program.
+** If not, see <https://www.gnu.org/licenses/>.
+**/
+#include "zbxmocktest.h"
+#include "zbxmockdata.h"
+#include "zbxmockutil.h"
+#include "zbxmockassert.h"
+
+#include "zbxstr.h"
+
+void	zbx_mock_test_entry(void **state)
+{
+
+	const char  *src, *list, *exp_result, *act_result;
+
+	ZBX_UNUSED(state);
+
+	src = zbx_mock_get_parameter_string("in.string");
+
+    list =  zbx_mock_get_parameter_string("in.list");
+
+	exp_result = zbx_mock_get_parameter_string("out.return");
+
+	zbx_remove_chars(src, list);
+
+    zbx_mock_assert_str_eq("return value",  exp_result, src);
+}
