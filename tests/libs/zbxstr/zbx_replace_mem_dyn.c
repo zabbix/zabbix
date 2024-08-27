ccdb319aca77328a8fc806a55e2a55feff0536b2
Romans Trizna <romans.trizna@zabbix.com>
2024-08-27 17:35:17 +0300

    .......... [DEV-3899] format fix

--- tests/libs/zbxstr/zbx_replace_mem_dyn.c
+++ tests/libs/zbxstr/zbx_replace_mem_dyn.c
@@ -22,25 +22,22 @@
 void	zbx_mock_test_entry(void **state)
 {
 	ZBX_UNUSED(state);
-//zbx_replace_mem_dyn(char **data, size_t *data_alloc, size_t *data_len, size_t offset, size_t sz_to, const char *from, size_t sz_from)
+
 	char		*data = zbx_mock_get_parameter_string("in.data");
-    printf("1------------\n");
 	size_t		*data_alloc = zbx_mock_get_parameter_uint64("in.data_alloc");
-    printf("2------------\n");
 	size_t		*data_len = zbx_mock_get_parameter_uint64("in.data_len");
-    printf("3------------\n");
-    printf("%ld\n", data_len);
+	printf("%ld\n", data_len);
 	size_t		offset = zbx_mock_get_parameter_uint64("in.offset");
-    printf("4------------\n");
+	printf("4------------\n");
 	size_t		sz_to = zbx_mock_get_parameter_uint64("in.sz_to");
-    printf("5------------\n");
+	printf("5------------\n");
 	const char		*from = zbx_mock_get_parameter_string("in.from");
-    printf("6------------\n");
+	printf("6------------\n");
 	size_t		sz_from = zbx_mock_get_parameter_uint64("in.sz_from");
-    printf("7------------\n");
+	printf("7------------\n");
 	int		exp_result = zbx_mock_get_parameter_uint64("out.exp_result");
-    printf("8------------\n");
+	printf("8------------\n");
 	int		act_result = zbx_replace_mem_dyn(&data, data_alloc, data_len, offset, sz_to, from, sz_from);
-    printf("9------------\n");
+	printf("9------------\n");
 	zbx_mock_assert_int_eq("Unexpected error message X", exp_result, act_result);
 }
