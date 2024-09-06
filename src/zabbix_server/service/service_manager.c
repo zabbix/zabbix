9c4afb12463fd6af58e45e97e25ffb1f634e8d20
Andris Zeila <andris.zeila@zabbix.com>
2024-09-06 13:11:45 +0300

    ........S. [ZBX-25154] changed event suppression caching in service manager from a counter to vector of maintenanceids

--- src/zabbix_server/service/service_manager.c
+++ src/zabbix_server/service/service_manager.c
@@ -110,10 +110,17 @@ typedef struct
 }
 zbx_service_manager_t;
 
+static void	event_add_maintenanceid(zbx_event_t *event, zbx_uint64_t maintenanceid);
+
 /*#define ZBX_AVAILABILITY_MANAGER_DELAY		1*/
 
 static void	event_free(zbx_event_t *event)
 {
+	if (NULL != event->maintenanceids)
+	{
+		zbx_vector_uint64_destroy(event->maintenanceids);
+		zbx_free(event->maintenanceids);
+	}
 	zbx_vector_ptr_clear_ext(&event->tags, (zbx_clean_func_t)zbx_free_tag);
 	zbx_vector_ptr_destroy(&event->tags);
 	zbx_free(event);
@@ -259,7 +266,7 @@ static void	db_get_events(zbx_hashset_t *problem_events)
 			event->value = TRIGGER_VALUE_PROBLEM;
 			event->severity = atoi(row[2]);
 			event->mtime = 0;
-			event->suppressed = 0;
+			event->maintenanceids = NULL;
 			zbx_vector_ptr_create(&event->tags);
 			zbx_hashset_insert(problem_events, &event, sizeof(zbx_event_t *));
 		}
@@ -276,7 +283,7 @@ static void	db_get_events(zbx_hashset_t *problem_events)
 	}
 	DBfree_result(result);
 
-	result = DBselect("select eventid,count(eventid) from event_suppress group by eventid");
+	result = DBselect("select eventid,maintenanceid from event_suppress");
 	while (NULL != (row = DBfetch(result)))
 	{
 		zbx_event_t	event_local, *event_p, **ptr;
@@ -286,8 +293,10 @@ static void	db_get_events(zbx_hashset_t *problem_events)
 
 		if (NULL != (ptr = zbx_hashset_search(problem_events, &event_p)))
 		{
-			event_p = *ptr;
-			event_p->suppressed = atoi(row[1]);
+			zbx_uint64_t	maintenanceid;
+
+			ZBX_STR2UINT64(maintenanceid, row[1]);
+			event_add_maintenanceid(*ptr, maintenanceid);
 		}
 	}
 	DBfree_result(result);
@@ -2595,7 +2604,7 @@ static void	db_update_services(zbx_service_manager_t *manager)
 			{
 				event = *ptr;
 
-				if (0 < event->suppressed)
+				if (NULL != event->maintenanceids)
 					continue;
 			}
 
@@ -2742,33 +2751,66 @@ static void	process_deleted_problems(zbx_vector_uint64_t *eventids, zbx_service_
 	zabbix_log(LOG_LEVEL_DEBUG, "End of %s()", __func__);
 }
 
-static void	process_problem_suppression(zbx_vector_uint64_t *eventids, zbx_service_manager_t *service_manager,
-		int is_suppressed)
+static void	event_add_maintenanceid(zbx_event_t *event, zbx_uint64_t maintenanceid)
+{
+	if (NULL == event->maintenanceids)
+	{
+		event->maintenanceids = (zbx_vector_uint64_t *)zbx_malloc(NULL, sizeof(zbx_vector_uint64_t));
+		zbx_vector_uint64_create(event->maintenanceids);
+	}
+
+	if (FAIL == zbx_vector_uint64_search(event->maintenanceids, maintenanceid, ZBX_DEFAULT_UINT64_COMPARE_FUNC))
+		zbx_vector_uint64_append(event->maintenanceids, maintenanceid);
+}
+
+static void	event_remove_maintenanceid(zbx_event_t *event, zbx_uint64_t maintenanceid)
+{
+	if (NULL == event->maintenanceids)
+		return;
+
+	int	i;
+
+	if (FAIL != (i = zbx_vector_uint64_search(event->maintenanceids, maintenanceid, ZBX_DEFAULT_UINT64_COMPARE_FUNC)))
+		zbx_vector_uint64_remove_noorder(event->maintenanceids, i);
+
+	if (0 == event->maintenanceids->values_num)
+	{
+		zbx_vector_uint64_destroy(event->maintenanceids);
+		zbx_free(event->maintenanceids);
+	}
+}
+
+static void	process_problem_suppression(zbx_vector_uint64_pair_t *event_maintenances,
+		zbx_service_manager_t *service_manager, int is_suppressed)
 {
 	int		i;
 
-	for (i = 0; i < eventids->values_num; i++)
+	for (i = 0; i < event_maintenances->values_num; i++)
 	{
 		zbx_event_t			event_local, *event = &event_local, **pevent;
 		zbx_service_problem_index_t	*pi, pi_local;
 		zbx_services_diff_t		*services_diff, services_diff_local;
-		int				suppressed_old;
+		int				maintenance_num = 0;
 
-		event_local.eventid = eventids->values[i];
+		event_local.eventid = event_maintenances->values[i].first;
 
 		if (NULL == (pevent = (zbx_event_t **)zbx_hashset_search(&service_manager->problem_events, &event)))
 			continue;
 
-		suppressed_old = (*pevent)->suppressed;
+		if (NULL != (*pevent)->maintenanceids)
+			maintenance_num = (*pevent)->maintenanceids->values_num;
 
-		(*pevent)->suppressed += is_suppressed;
+		if (-1 == is_suppressed)
+			event_remove_maintenanceid(*pevent, event_maintenances->values[i].second);
+		else
+			event_add_maintenanceid(*pevent, event_maintenances->values[i].second);
 
-		if (0 != suppressed_old && 0 != (*pevent)->suppressed)
+		if (0 != maintenance_num && NULL != (*pevent)->maintenanceids)
 			continue;
 
 		(*pevent)->mtime = (int)time(NULL);
 
-		pi_local.eventid = eventids->values[i];
+		pi_local.eventid = (*pevent)->eventid;
 
 		if (NULL == (pi = zbx_hashset_search(&service_manager->service_problems_index, &pi_local)))
 			continue;
@@ -3223,14 +3265,24 @@ static void	dump_events(zbx_hashset_t *events)
 	{
 		event = *ptr;
 
-		zabbix_log(LOG_LEVEL_TRACE, "eventid:" ZBX_FS_UI64 " value:%d severity:%d clock:%d", event->eventid,
-				event->value, event->severity, event->clock);
+		zabbix_log(LOG_LEVEL_TRACE, "eventid:" ZBX_FS_UI64 " value:%d severity:%d clock:%d",
+				event->eventid, event->value, event->severity, event->clock);
 
 		for (i = 0; i < event->tags.values_num; i++)
 		{
 			const zbx_tag_t	*tag = (const zbx_tag_t *)event->tags.values[i];
 
 			zabbix_log(LOG_LEVEL_TRACE, "  tag:'%s' value:'%s'", tag->tag, tag->value);
+		}
+
+		if (NULL != event->maintenanceids)
+		{
+
+			for (i = 0; i < event->maintenanceids->values_num; i++)
+			{
+				zabbix_log(LOG_LEVEL_TRACE, "  maintenanceid:" ZBX_FS_UI64,
+						event->maintenanceids->values[i]);
+			}
 		}
 	}
 }
@@ -3497,16 +3549,18 @@ ZBX_THREAD_ENTRY(service_manager_thread, args)
 
 		if (NULL != message)
 		{
-			zbx_vector_ptr_t	events;
-			zbx_vector_uint64_t	eventids;
+			zbx_vector_ptr_t		events;
+			zbx_vector_uint64_t		eventids;
+			zbx_vector_uint64_pair_t	id_pairs;
 
 			zbx_vector_ptr_create(&events);
 			zbx_vector_uint64_create(&eventids);
+			zbx_vector_uint64_pair_create(&id_pairs);
 
 			switch (message->code)
 			{
 				case ZBX_IPC_SERVICE_SERVICE_PROBLEMS:
-					zbx_service_deserialize(message->data, message->size, &events);
+					zbx_service_deserialize_event(message->data, message->size, &events);
 					process_events(&events, &service_manager);
 					events_num += events.values_num;
 					break;
@@ -3521,12 +3575,12 @@ ZBX_THREAD_ENTRY(service_manager_thread, args)
 					problems_delete_num += events.values_num;
 					break;
 				case ZBX_IPC_SERVICE_SERVICE_EVENTS_UNSUPPRESS:
-					zbx_service_deserialize_ids(message->data, message->size, &eventids);
-					process_problem_suppression(&eventids, &service_manager, -1);
+					zbx_service_deserialize_id_pairs(message->data, &id_pairs);
+					process_problem_suppression(&id_pairs, &service_manager, -1);
 					break;
 				case ZBX_IPC_SERVICE_SERVICE_EVENTS_SUPPRESS:
-					zbx_service_deserialize_ids(message->data, message->size, &eventids);
-					process_problem_suppression(&eventids, &service_manager, 1);
+					zbx_service_deserialize_id_pairs(message->data, &id_pairs);
+					process_problem_suppression(&id_pairs, &service_manager, 1);
 					break;
 				case ZBX_IPC_SERVICE_SERVICE_ROOTCAUSE:
 					process_rootcause(message, &service_manager, client);
@@ -3551,6 +3605,7 @@ ZBX_THREAD_ENTRY(service_manager_thread, args)
 			}
 
 			zbx_ipc_message_free(message);
+			zbx_vector_uint64_pair_destroy(&id_pairs);
 			zbx_vector_uint64_destroy(&eventids);
 			zbx_vector_ptr_destroy(&events);
 		}
