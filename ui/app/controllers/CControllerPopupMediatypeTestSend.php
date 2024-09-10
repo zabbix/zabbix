343725662bc69a44bc0602fb1a6cae7c70275b71
Robert Gontarski <robert.gontarski@zabbix.com>
2024-09-10 08:47:26 +0200

    ..F....... [DEV-3952] changed user permission verification to media type ui only

--- ui/app/controllers/CControllerPopupMediatypeTestSend.php
+++ ui/app/controllers/CControllerPopupMediatypeTestSend.php
@@ -55,7 +55,7 @@ class CControllerPopupMediatypeTestSend extends CController {
 	}
 
 	protected function checkPermissions() {
-		return ($this->getUserType() == USER_TYPE_SUPER_ADMIN);
+		return $this->checkAccess(CRoleHelper::UI_ADMINISTRATION_MEDIA_TYPES);
 	}
 
 	/**
