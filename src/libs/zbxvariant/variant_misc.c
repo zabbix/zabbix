ef0bec417ec732fb4a62bdc4796355a8773399f8
Romans Trizna <romans.trizna@zabbix.com>
2024-09-10 14:59:18 +0300

    ........S. [ZBX-24582] Format fixes

--- src/libs/zbxvariant/variant_misc.c
+++ src/libs/zbxvariant/variant_misc.c
@@ -31,7 +31,7 @@
  ******************************************************************************/
 int	zbx_variant_to_value_type(zbx_variant_t *value, unsigned char value_type, char **errmsg)
 {
-	#define	ZBX_MAX_ERROR_DESC_BUFF	240
+#define ZBX_MAX_ERROR_DESC_BUFF	240
 	int	ret;
 	char	*value_desc;
 	char	error_buffer[ZBX_MAX_ERROR_DESC_BUFF];
@@ -76,10 +76,10 @@ int	zbx_variant_to_value_type(zbx_variant_t *value, unsigned char value_type, ch
 		zbx_strlcat(error_buffer, value_desc, char_max);
 		*errmsg = zbx_dsprintf(NULL, "Value of type \"%s\" is not suitable for value type \"%s\". Value"
 				"\"%s...\"", zbx_variant_type_desc(value), zbx_item_value_type_string(value_type),
-						error_buffer);
+				error_buffer);
 		zbx_free(value_desc);
 	}
 
 	return ret;
-	#undef	ZBX_MAX_ERROR_DESC_BUFF
+#undef ZBX_MAX_ERROR_DESC_BUFF
 }
