ff8a69a13a5b46b2e0fac2176c142e655fdb65df
Wiktor Czerneha <wiktor.czerneha@zabbix.com>
2024-09-11 15:42:22 +0200

    A......... [ZBX-25034] fixed export of host prototype discovery and template override is using visible name

--- ui/include/classes/export/CConfigurationExport.php
+++ ui/include/classes/export/CConfigurationExport.php
@@ -847,7 +847,7 @@ class CConfigurationExport {
 
 		if ($templateids) {
 			$templates = API::Template()->get([
-				'output' => ['name'],
+				'output' => ['host'],
 				'templateids' => array_keys($templateids),
 				'preservekeys' => true
 			]);
@@ -858,7 +858,7 @@ class CConfigurationExport {
 							if (array_key_exists('optemplate', $operation)) {
 								$operation['templates'] = [];
 								foreach ($operation['optemplate'] as $template) {
-									$operation['templates'][] = ['name' => $templates[$template['templateid']]['name']];
+									$operation['templates'][] = ['name' => $templates[$template['templateid']]['host']];
 								}
 								unset($operation['optemplate']);
 							}
