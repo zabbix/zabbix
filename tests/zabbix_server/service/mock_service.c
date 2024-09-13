9d14fdf9d92769744776e884d7372ae1c3beb82d
Andris Zeila <andris.zeila@zabbix.com>
2024-09-13 14:48:37 +0300

    .......... [ZBX-25154] removed unused function wrappers

--- tests/zabbix_server/service/mock_service.c
+++ tests/zabbix_server/service/mock_service.c
@@ -38,18 +38,6 @@ void	__wrap_zbx_rtc_notify_finished_sync(int config_timeout, zbx_uint32_t code, 
 		void *rtc);
 void	__wrap_zbx_dc_set_itservices_num(int num);
 
-void	__wrap_zbx_recalc_time_period(int *tm_start, int table_group)
-{
-	ZBX_UNUSED(tm_start);
-	ZBX_UNUSED(table_group);
-}
-
-void	__wrap_zbx_config_get(void *cfg, int flags)
-{
-	ZBX_UNUSED(cfg);
-	ZBX_UNUSED(flags);
-}
-
 void	__wrap_zbx_dc_set_itservices_num(int num)
 {
 	ZBX_UNUSED(num);
