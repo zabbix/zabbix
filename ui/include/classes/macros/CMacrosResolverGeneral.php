11715c451f98ea1f11b2c538984b5806663f92a9
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2024-09-13 11:05:00 +0000

    ..F....... [ZBX-23171] reverted the incomplete fix
    This reverts commit 5bc8ce30432b5b41950b90ff04ba0efff5489ff0.

--- ui/include/classes/macros/CMacrosResolverGeneral.php
+++ ui/include/classes/macros/CMacrosResolverGeneral.php
@@ -2715,7 +2715,7 @@ class CMacrosResolverGeneral {
 				// Searching context coincidence, if regex array not empty.
 				elseif ($context !== null && count($host_macros[$hostid][$macro]['regex'])) {
 					foreach ($host_macros[$hostid][$macro]['regex'] as $regex => $val) {
-						if (preg_match('/'.preg_quote(trim($regex, '/'), '/').'/', $context) === 1) {
+						if (preg_match('/'.strtr(trim($regex, '/'), ['/' => '\\/']).'/', $context) === 1) {
 							return [
 								'value' => $val,
 								'value_default' => $value_default
