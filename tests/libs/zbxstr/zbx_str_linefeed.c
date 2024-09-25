5c2cc75d39454702075694eafb25c75b5d413db2
Romans Trizna <romans.trizna@zabbix.com>
2024-09-25 09:25:37 +0300

    .......... [DEV-3899] new test cases added

--- tests/libs/zbxstr/zbx_str_linefeed.c
+++ tests/libs/zbxstr/zbx_str_linefeed.c
@@ -21,9 +21,9 @@
 
 void	zbx_mock_test_entry(void **state)
 {
-	const char		*scr = zbx_mock_get_parameter_string("in.scr");
-	const char		*exp_result = zbx_mock_get_parameter_string("out.string");
-	const char		*delim = zbx_mock_get_parameter_string("in.delim");
+	const char	*scr = zbx_mock_get_parameter_string("in.scr");
+	const char	*exp_result = zbx_mock_get_parameter_string("out.string");
+	const char	*delim = zbx_mock_get_parameter_string("in.delim");
 	size_t		maxline = zbx_mock_get_parameter_uint64("in.maxline");
 	char		*act_result = zbx_str_linefeed(scr, maxline, delim);
 
