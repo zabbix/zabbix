0fb21610a58c24d4a9cdadb2b98787f3d42e75a8
Janis Nulle <janis.nulle@zabbix.com>
2024-09-30 11:36:20 +0300

    .......... [DEV-2672] added additional preprocessing steps to the cone scenarios

--- ui/tests/selenium/common/testFormPreprocessing.php
+++ ui/tests/selenium/common/testFormPreprocessing.php
@@ -177,6 +177,36 @@ abstract class testFormPreprocessing extends CWebTest {
 			'params' => "1\n2",
 			'error_handler' => 0,
 			'error_handler_params' => ''
+		],
+		[
+			'type' => '26',
+			'params' => "1\ntest",
+			'error_handler' => 1,
+			'error_handler_params' => ''
+		],
+		[
+			'type' => '27',
+			'params' => '',
+			'error_handler' => 0,
+			'error_handler_params' => ''
+		],
+		[
+			'type' => '28',
+			'params' => "OID\n1",
+			'error_handler' => 0,
+			'error_handler_params' => ''
+		],
+		[
+			'type' => '29',
+			'params' => "test\nOID\n1",
+			'error_handler' => 0,
+			'error_handler_params' => ''
+		],
+		[
+			'type' => '30',
+			'params' => '1',
+			'error_handler' => 0,
+			'error_handler_params' => ''
 		]
 	];
 
