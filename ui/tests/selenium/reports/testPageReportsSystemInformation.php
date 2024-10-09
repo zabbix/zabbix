690a8320c77d04ff8c98825213506df5a4e93cde
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2024-10-09 18:53:06 +0300

    .......... [ZBXNEXT-686] commented screenshot tests with mouse pointer

--- ui/tests/selenium/reports/testPageReportsSystemInformation.php
+++ ui/tests/selenium/reports/testPageReportsSystemInformation.php
@@ -22,13 +22,14 @@ require_once dirname(__FILE__).'/../common/testSystemInformation.php';
  */
 class testPageReportsSystemInformation extends testSystemInformation {
 
-	public function testPageReportsSystemInformation_checkDisabledHA() {
-		$this->page->login()->open('zabbix.php?action=report.status')->waitUntilReady();
-		$this->assertScreenshotExcept(null,
-				[$this->query('xpath://footer')->one(),
-				$this->query('xpath://table[@class="list-table sticky-header"]/tbody/tr[3]/td[1]')->one()],
-				'report_without_ha');
-	}
+// Commented until Jenkins issue investigated.
+//	public function testPageReportsSystemInformation_checkDisabledHA() {
+//		$this->page->login()->open('zabbix.php?action=report.status')->waitUntilReady();
+//		$this->assertScreenshotExcept(null,
+//				[$this->query('xpath://footer')->one(),
+//				$this->query('xpath://table[@class="list-table sticky-header"]/tbody/tr[3]/td[1]')->one()],
+//				'report_without_ha');
+//	}
 
 	/**
 	 * @onBefore prepareHANodeData
