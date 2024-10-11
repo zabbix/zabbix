6087c19a282a5b585d5a7f1e173efd9a7bfc9267
Sergejs Boidenko <sergejs.boidenko@zabbix.com>
2024-10-11 16:14:41 +0300

    .......PS. [ZBXNEXT-6726] added auto renew Hashicorp vault periodic service tokens
    * commit '1929a674ad02487ca159156ac6750834b3c41511':
      .......PS. [ZBXNEXT-6726] fixed warnings
      .......PS. [ZBXNEXT-6726] improved warning messages
      .......PS. [ZBXNEXT-6726] style
      .......PS. [ZBXNEXT-6726] fixed subtask 10, 11; Frequency of renew decreased to 10 seconds in case of errors, write only one time if vault token renew is started to fail and once when vault token renew is working again
      .......P.. [ZBXNEXT-6726] fixed subtask 8, if active Zabbix proxy cannot connect to Zabbix server then token now renewed
      .......PS. [ZBXNEXT-6726] removed redundant initialization
      .......P.. [ZBXNEXT-6726] fixed subtask 8, if active Zabbix proxy cannot connect to Zabbix server then token now renewed
      .......PS. [ZBXNEXT-6726] fixed subtask 9, main process now renew token instead of configuration syncer
      .......PS. [ZBXNEXT-6726] imroved renew timing
      .......PS. [ZBXNEXT-6726] fixed renewable check
      .......PS. [ZBXNEXT-6726] fixed renewable check
      .......PS. [ZBXNEXT-6726] added small style fixes
      .......PS. [ZBXNEXT-6726] fixed style
      .......PS. [ZBXNEXT-6726] fixed subtasks 2,3,4
      .......PS. [ZBXNEXT-6726] added auto renew Hashicorp vault periodic service tokens
    
    (cherry picked from commit 3e3310022947ca647cc6e5238118a43dd1ee0799)

--- src/libs/zbxvault/hashicorp.h
+++ src/libs/zbxvault/hashicorp.h
@@ -21,4 +21,8 @@ int	zbx_hashicorp_kvs_get(const char *vault_url, const char *prefix, const char 
 		const char *ssl_key_file, const char *config_source_ip, const char *config_ssl_ca_location,
 		const char *config_ssl_cert_location, const char *config_ssl_key_location, const char *path,
 		long timeout, zbx_hashset_t *kvs, char **error);
+
+void	zbx_hashicorp_renew_token(const char *vault_url, const char *token, const char *ssl_cert_file,
+		const char *ssl_key_file, const char *config_source_ip, const char *config_ssl_ca_location,
+		const char *config_ssl_cert_location, const char *config_ssl_key_location, long timeout);
 #endif
