141e6bed93283d68e56700e5a03360caf6439f84
Michael Veksler <Mihails.Vekslers@zabbix.com>
2024-10-15 11:41:09 +0300

    .......PS. [ZBX-25391] fixed error in the log file when retrieving VMware tags from an ESXi host
    (cherry picked from commit d7d6550e474e4c04021c6f62e8c4bcbbabf82324)
    (cherry picked from commit ca2802c22eefa90bb1b5a93be0f8cc5511f328d8)

--- src/libs/zbxvmware/vmware_rest.c
+++ src/libs/zbxvmware/vmware_rest.c
@@ -944,10 +944,10 @@ int	zbx_vmware_service_update_tags(zbx_vmware_service_t *service, const char *co
 	zbx_vmware_lock();
 	version = service->major_version * 100 + service->minor_version * 10 + service->update_version;
 
-	if (650 > version)
+	if (650 > version || ZBX_VMWARE_TYPE_VCENTER != service->type)
 	{
 		zbx_vmware_unlock();
-		error = zbx_strdup(error, "Tags are supported since vmware version 6.5.");
+		error = zbx_strdup(error, "Tags are supported since VMware virtual center version 6.5.");
 		goto out;
 	}
 
