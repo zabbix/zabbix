e1c7d0da37a1647789d5fbfd9c22b72845c7f029
Kirils Seve <kirils.seve@zabbix.com>
2024-10-22 11:39:14 +0300

    .......... [ZBXNEXT-826] updated release version to 6.0.36rc1

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "21 October 2024";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "6.0.35";
+	static final String VERSION = "6.0.36rc1";
 
 	static void printVersion()
 	{
--- ui/tests/api_json/testAPIInfo.php
+++ ui/tests/api_json/testAPIInfo.php
@@ -36,6 +36,6 @@ class testAPIInfo extends CAPITest {
 		$this->disableAuthorization();
 		$result = $this->call('apiinfo.version', []);
 
-		$this->assertSame('6.0.35', $result['result']);
+		$this->assertSame('6.0.36', $result['result']);
 	}
 }
