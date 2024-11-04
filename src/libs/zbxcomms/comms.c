7e0037d140f9c3996c835eac174c60b378e9a9b4
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2024-11-04 14:12:49 +0200

    ...G...PS. [ZBX-25301] updated ZBX_MAX_RECV_LARGE_DATA_SIZE definition

--- src/libs/zbxcomms/comms.c
+++ src/libs/zbxcomms/comms.c
@@ -819,7 +819,6 @@ int	zbx_tcp_send_context_init(const char *data, size_t len, size_t reserved, uns
 	if (0 == (flags & ZBX_TCP_PROTOCOL))
 		return SUCCEED;
 
-#if !defined(_WINDOWS) && (8 == SIZEOF_SIZE_T)
 	if (ZBX_MAX_RECV_LARGE_DATA_SIZE < len)
 	{
 		zbx_set_socket_strerror("cannot send data: message size " ZBX_FS_UI64 " exceeds the maximum"
@@ -834,7 +833,7 @@ int	zbx_tcp_send_context_init(const char *data, size_t len, size_t reserved, uns
 				ZBX_MAX_RECV_LARGE_DATA_SIZE);
 		return FAIL;
 	}
-#endif
+
 	if (0 != (flags & ZBX_TCP_COMPRESS))
 	{
 		/* compress if not compressed yet */
