466e6a3dd53d8339ab2cdee480e5bf3fa34dce61
Alexander Shubin <aleksandrs.subins@zabbix.com>
2024-11-04 15:08:22 +0200

    ....I..... [ZBXNEXT-9199] fixed db patch

--- src/libs/zbxdbupgrade/dbupgrade_7010.c
+++ src/libs/zbxdbupgrade/dbupgrade_7010.c
@@ -186,7 +186,7 @@ static int	DBpatch_7010015(void)
 	if (ZBX_DB_OK > zbx_db_execute(
 			"update widget_field as wf join widget as w on (w.widgetid=wf.widgetid)"
 			" set wf.value_int=case when wf.value_int=1 then 0 when wf.value_int=2 then 1 when wf.value_int=3 then 2 end"
-			" where w.type='tophosts' and wf.name like 'columns.%.history'"))
+			" where w.type='tophosts' and wf.name like 'columns.%%.history'"))
 	{
 		return FAIL;
 	}
@@ -202,11 +202,11 @@ static int	DBpatch_7010016(void)
 	if (ZBX_DB_OK > zbx_db_execute(
 			"delete wf from widget_field as wf join widget as w on (w.widgetid=wf.widgetid)"
 			" where w.type='tophosts' and ("
-			" (wf.name like 'columns.%.base_color' and wf.value_str='')"
-			" or (wf.name like 'columns.%.display' and wf.value_int=1)"
-			" or (wf.name like 'columns.%.decimal_places' and wf.value_int=2)"
-			" or (wf.name like 'columns.%.aggregate_function' and wf.value_int=0)"
-			" or (wf.name like 'columns.%.history' and wf.value_int=0))"))
+			" (wf.name like 'columns.%%.base_color' and wf.value_str='')"
+			" or (wf.name like 'columns.%%.display' and wf.value_int=1)"
+			" or (wf.name like 'columns.%%.decimal_places' and wf.value_int=2)"
+			" or (wf.name like 'columns.%%.aggregate_function' and wf.value_int=0)"
+			" or (wf.name like 'columns.%%.history' and wf.value_int=0))"))
 	{
 		return FAIL;
 	}
