e303ca1a39abf7ac82616c87c2a09a0d24a0d530
Continuous Integration <ci@zabbix.com>
2024-11-11 09:19:57 +0200

    .......... [ZBXNEXT-826] updated release date for 5.0.45rc1

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,7 +22,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "23 September 2024";
+	static final String REVISION_DATE = "11 November 2024";
 	static final String REVISION = "{ZABBIX_REVISION}";
 	static final String VERSION = "5.0.45rc1";
 
