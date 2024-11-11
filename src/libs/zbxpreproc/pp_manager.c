0fb8a0beca740fde0d537ba0913ccc1a5fc42848
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2024-11-11 13:59:10 +0200

    .......PS. [ZBX-25050] improved preprocessing manager memory consumption by starting 16 preprocessing workers by default and releasing reserved memory dail
    * commit 'f674005f9552e86d733cee41a4b5fee419f0ffa5':
      .......PS. [ZBX-25050] improved preprocessing manager memory consumption by starting 16 preprocessing workers by default and releasing reserved memory daily
    
    (cherry picked from commit 9940e6410479f2501fd2455d1afd0df450ac60fc)

--- src/libs/zbxpreproc/pp_manager.c
+++ src/libs/zbxpreproc/pp_manager.c
@@ -1183,7 +1183,7 @@ ZBX_THREAD_ENTRY(zbx_pp_manager_thread, args)
 	char					*error = NULL;
 	zbx_ipc_client_t			*client;
 	zbx_ipc_message_t			*message;
-	double					time_stat, time_idle = 0, time_flush, time_vps_update;
+	double					time_stat, time_idle = 0, time_flush, time_vps_update, time_trim;
 	zbx_timespec_t				timeout = {PP_MANAGER_DELAY_SEC, PP_MANAGER_DELAY_NS};
 	const zbx_thread_info_t			*info = &((zbx_thread_args_t *)args)->info;
 	int					server_num = ((zbx_thread_args_t *)args)->info.server_num,
@@ -1235,6 +1235,7 @@ ZBX_THREAD_ENTRY(zbx_pp_manager_thread, args)
 	time_stat = zbx_time();
 	time_flush = time_stat;
 	time_vps_update = time_stat;
+	time_trim = time_stat;
 
 	zbx_setproctitle("%s #%d started", get_process_type_string(process_type), process_num);
 
@@ -1344,6 +1345,15 @@ ZBX_THREAD_ENTRY(zbx_pp_manager_thread, args)
 		{
 			zbx_vps_monitor_add_collected(0);
 			time_vps_update = sec;
+		}
+
+		/* release memory in case of peak periods */
+		if (SEC_PER_DAY <= sec - time_trim)
+		{
+#ifdef	HAVE_MALLOC_TRIM
+			malloc_trim(128 * ZBX_MEBIBYTE);
+#endif
+			time_trim = sec;
 		}
 	}
 out:
