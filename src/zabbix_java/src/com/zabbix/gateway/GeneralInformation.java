8f1f6037562f5db1f69b828d382d0b32fe8b1fcb
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2024-12-17 18:14:55 +0200

    .......... [ZBXNEXT-826] updated release version to 6.0.38rc1

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "17 December 2024";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "6.0.37";
+	static final String VERSION = "6.0.38rc1";
 
 	static void printVersion()
 	{
