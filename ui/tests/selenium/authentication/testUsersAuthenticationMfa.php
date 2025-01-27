a63b4f91006ed70f3708f3fce75d83b716f038e2
Janis Nulle <janis.nulle@zabbix.com>
2025-01-27 15:35:52 +0200

    .......... [DEV-3126] added create test

--- ui/tests/selenium/authentication/testUsersAuthenticationMfa.php
+++ ui/tests/selenium/authentication/testUsersAuthenticationMfa.php
@@ -18,8 +18,13 @@ require_once dirname(__FILE__).'/../common/testFormAuthentication.php';
 
 /**
  * @backup config, usrgrp
+ * @onBefore prepareData
  */
 class testUsersAuthenticationMfa extends testFormAuthentication {
+
+	const DUO_DEFAULT_API_HOSTNAME = 'api-3edf651c.test.test';
+	const DUO_DEFAULT_CLIENT_ID = 'DI6GX0DNF2J21PXVLXBB';
+	const DUO_DEFAULT_CLIENT_SECRET = 'SNkg6BvonVsNn2EYzAUC';
 
 	/**
 	 * Attach MessageBehavior to the test.
@@ -28,6 +33,24 @@ class testUsersAuthenticationMfa extends testFormAuthentication {
 	 */
 	public function getBehaviors() {
 		return [CMessageBehavior::class];
+	}
+
+	public function prepareData() {
+		CDataHelper::call('mfa.create', [
+			[
+				'type' => MFA_TYPE_TOTP,
+				'name' => 'Pre-existing TOTP',
+				'hash_function' => TOTP_HASH_SHA1,
+				'code_length' => TOTP_CODE_LENGTH_6
+			],
+			[
+				'type' => MFA_TYPE_DUO,
+				'name' => 'Pre-existing Duo',
+				'api_hostname' => self::DUO_DEFAULT_API_HOSTNAME,
+				'clientid' => self::DUO_DEFAULT_CLIENT_ID,
+				'client_secret' => self::DUO_DEFAULT_CLIENT_SECRET
+			]
+		]);
 	}
 
 	public function testUsersAuthenticationMfa_Layout() {
@@ -61,13 +84,15 @@ class testUsersAuthenticationMfa extends testFormAuthentication {
 		$dialog_form = $dialog->asForm();
 
 		$method_fields = [
-			'Type' => ['visible' => true, 'value' => 'TOTP' ],
+			'Type' => ['visible' => true, 'value' => 'TOTP'],
 			'Name' => ['visible' => true, 'maxlength' => 128, 'value' => ''],
-			'Hash function' => ['visible' => true, 'value' => 'SHA-1' ],
-			'Code length' => ['visible' => true, 'value' => '6' ],
-			'API hostname' => ['visible' => false, 'maxlength' => 1024, 'value' => '' ],
+			'Hash function' => ['visible' => true, 'value' => 'SHA-1'],
+			'Code length' => ['visible' => true, 'value' => '6'],
+			'API hostname' => ['visible' => false, 'maxlength' => 1024, 'value' => ''],
 			'Client ID' => ['visible' => false, 'maxlength' => 32, 'value' => '' ],
-			'Client secret' => ['visible' => false, 'maxlength' => 64, 'value' => '' ]
+			'Client secret' => ['visible' => false, 'maxlength' => 64, 'value' => '',
+				'type' => 'password', 'autocomplete' => 'new-password'
+			]
 		];
 
 		foreach ($method_fields as $label => $attributes) {
@@ -79,8 +104,10 @@ class testUsersAuthenticationMfa extends testFormAuthentication {
 				$this->assertEquals($attributes['value'], $field->getValue());
 			}
 
-			if (array_key_exists('maxlength', $attributes)) {
-				$this->assertEquals($attributes['maxlength'], $field->getAttribute('maxlength'));
+			foreach (['maxlength', 'type', 'autocomplete'] as $attribute) {
+				if (array_key_exists($attribute, $attributes)) {
+					$this->assertEquals($attributes[$attribute], $field->getAttribute($attribute));
+				}
 			}
 		}
 
@@ -132,8 +159,24 @@ class testUsersAuthenticationMfa extends testFormAuthentication {
 			],
 			[
 				[
+					// TOTP name contains leading and trailing spaces.
+					'fields' => [
+						'Name' => '  TOTP whitespace  '
+					]
+				]
+			],
+			[
+				[
+					// TOTP name with special characters.
+					'fields' => [
+						'Name' => '👍©æ<script>alert("hi!")</script>&nbsp;'
+					]
+				]
+			],
+			[
+				[
 					// TOTP with Name field missing.
-					'expected' => TEST_BAD,
+					'expected_method_form' => TEST_BAD,
 					'error' => 'Incorrect value for field "name": cannot be empty.'
 				]
 			],
@@ -155,6 +198,116 @@ class testUsersAuthenticationMfa extends testFormAuthentication {
 						'Code length' => '8'
 					]
 				]
+			],
+			[
+				[
+					// TOTP name already used.
+					'expected_authentication_form' => TEST_BAD,
+					'fields' => [
+						'Name' => 'Pre-existing TOTP'
+					],
+					'error' => 'MFA method "Pre-existing TOTP" already exists.'
+				]
+			],
+			[
+				[
+					// TOTP name already used - leading and trailing spaces.
+					'expected_authentication_form' => TEST_BAD,
+					'fields' => [
+						'Name' => '  Pre-existing TOTP  '
+					],
+					'error' => 'MFA method "Pre-existing TOTP" already exists.'
+				]
+			],
+			[
+				[
+					// Duo standart case.
+					'fields' => [
+						'Type' => 'Duo Universal Prompt',
+						'Name' => 'Duo standart'
+					]
+				]
+			],
+			[
+				[
+					// Duo whitespaces.
+					'fields' => [
+						'Type' => 'Duo Universal Prompt',
+						'Name' => '  Duo whitespace  ',
+						'API hostname' => '  some.api.hostname  ',
+						'Client ID' => '  test  '
+					]
+				]
+			],
+			[
+				[
+					// Duo special characters.
+					'fields' => [
+						'Type' => 'Duo Universal Prompt',
+						'Name' => 'Name: 👍©æ<script>alert("hi!")</script>&nbsp;',
+						'API hostname' => 'API: 👍©æ<script>alert("hi!")</script>&nbsp;',
+						'Client ID' => '👍©æ<script>alert("hi!")</script>',
+						'Client secret' => 'Pass: 👍©æ<script>alert("hi!")</script>&nbsp;'
+					]
+				]
+			],
+			[
+				[
+					// Duo with Name field missing.
+					'expected_method_form' => TEST_BAD,
+					'error' => 'Incorrect value for field "name": cannot be empty.',
+					'fields' => [
+						'Type' => 'Duo Universal Prompt',
+						'Name' => ''
+					]
+				]
+			],
+			[
+				[
+					// Duo with API hostname field missing.
+					'expected_method_form' => TEST_BAD,
+					'error' => 'Incorrect value for field "api_hostname": cannot be empty.',
+					'fields' => [
+						'Type' => 'Duo Universal Prompt',
+						'Name' => 'Duo missing API',
+						'API hostname' => ''
+					]
+				]
+			],
+			[
+				[
+					// Duo with Client ID field missing.
+					'expected_method_form' => TEST_BAD,
+					'error' => 'Incorrect value for field "clientid": cannot be empty.',
+					'fields' => [
+						'Type' => 'Duo Universal Prompt',
+						'Name' => 'Duo missing API',
+						'Client ID' => '',
+					]
+				]
+			],
+			[
+				[
+					// Duo with Client secret field missing.
+					'expected_method_form' => TEST_BAD,
+					'error' => 'Incorrect value for field "client_secret": cannot be empty.',
+					'fields' => [
+						'Type' => 'Duo Universal Prompt',
+						'Name' => 'Duo missing API',
+						'Client secret' => ''
+					]
+				]
+			],
+			[
+				[
+					// Duo name already used.
+					'expected_authentication_form' => TEST_BAD,
+					'fields' => [
+						'Type' => 'Duo Universal Prompt',
+						'Name' => 'Pre-existing Duo'
+					],
+					'error' => 'MFA method "Pre-existing Duo" already exists.'
+				]
 			]
 		];
 	}
@@ -172,71 +325,96 @@ class testUsersAuthenticationMfa extends testFormAuthentication {
 		// Fill in data.
 		$dialog = COverlayDialogElement::find()->waitUntilReady()->one();
 		$dialog_form = $dialog->asForm();
-		$fields = CTestArrayHelper::get($data, 'fields', []);
+		$fields = $this->setDefaultFieldsData(CTestArrayHelper::get($data, 'fields', []));
 		$dialog_form->fill($fields);
 
-		// Fields array with any missing default values.
-		$fields_full = $this->setDefaultFields($fields);
 		$dialog->query('button:Add')->one()->click();
 
-		// If a validation error expected.
-		if (CTestArrayHelper::get($data, 'expected', TEST_GOOD) === TEST_BAD) {
+		// If a validation error expected in the Mehod edit form.
+		if (CTestArrayHelper::get($data, 'expected_method_form', TEST_GOOD) === TEST_BAD) {
 			$this->assertMessage(TEST_BAD, 'Invalid MFA configuration', $data['error']);
 			$dialog->close();
 		} else {
-			// Verify data before saving.
-			$this->checkMethodsTableAndMethodForm($mfa_form, $dialog, $fields_full);
+			// Open the Method edit for to verify data is still there.
+			$this->checkMethodsTableAndMethodForm($mfa_form, $dialog, $fields);
 
 			// Save changes to authentication configuration.
 			$mfa_form->query('button:Update')->waitUntilClickable()->one()->click();
 
-			// Verify data after saving.
-			$this->page->waitUntilReady();
-			$mfa_form->invalidate();
-			$mfa_form->selectTab('MFA settings');
-			$this->checkMethodsTableAndMethodForm($mfa_form, $dialog, $fields_full);
+			// Check if an error is expected when saving authentication configuration as a whole.
+			if (CTestArrayHelper::get($data, 'expected_authentication_form', TEST_GOOD) === TEST_BAD) {
+				$this->assertMessage(TEST_BAD, 'Cannot update authentication', $data['error']);
+			} else {
+				// Verify data after saving.
+				$this->page->waitUntilReady();
+				$this->assertMessage(TEST_GOOD, 'Authentication settings updated');
+				$mfa_form->invalidate();
+				$mfa_form->selectTab('MFA settings');
+				$this->checkMethodsTableAndMethodForm($mfa_form, $dialog, $fields);
+			}
 		}
 	}
 
 	/**
 	 * Checks that data is displayed as expected in the MFA methods table.
 	 *
-	 * @param CFormElement          $mfa_form       The form element that contains the method table.
-	 * @param COverlayDialogElement $dialog         The dialog that contains the edit form.
-	 * @param array                 $fields_full    Expected fields data for the record.
+	 * @param CFormElement          $mfa_form    The form element that contains the method table.
+	 * @param COverlayDialogElement $dialog      The dialog that contains the edit form.
+	 * @param array                 $fields      Expected fields data for the record.
 	 */
-	protected function checkMethodsTableAndMethodForm($mfa_form, $dialog, $fields_full) {
+	protected function checkMethodsTableAndMethodForm($mfa_form, $dialog, $fields) {
+		// Transform the field data so what is expected in UI.
+		$fields['Type'] = CTestArrayHelper::get($fields, 'Type', 'TOTP');
+		if ($fields['Type'] === 'TOTP') {
+			$fields['Hash function'] = CTestArrayHelper::get($fields, 'Hash function', 'SHA-1');
+			$fields['Code length'] = CTestArrayHelper::get($fields, 'Code length', '6');
+		}
+
+		// Trim text fields.
+		foreach (['Name', 'API hostname', 'Client ID'] as $field) {
+			if (array_key_exists($field, $fields)) {
+				$fields[$field] = trim($fields[$field]);
+			}
+		}
+
 		// Check data in the table.
 		$table = $mfa_form->getFieldContainer('Methods')->query('xpath:.//table')->one()->asTable();
-		$row = $table->findRow('Name', $fields_full['Name']);
+		$row = $table->findRow('Name', $fields['Name']);
 		$expected_table_data = [
-			'Name' => $fields_full['Name'],
-			'Type' => $fields_full['Type'],
-			'User groups' => "0",
+			'Name' => $fields['Name'],
+			'Type' => $fields['Type'],
+			'User groups' => '0',
 			'Action' => 'Remove'
 		];
 		$row->assertValues($expected_table_data);
 
-		// Open the edit form and verify fields.
+		// Open the edit form.
 		$row->getColumn('Name')->query('xpath:.//a')->one()->click();
 		$dialog_form = $dialog->asForm();
 		$dialog_form->invalidate();
-		$dialog_form->checkValue($fields_full);
+
+		// Special check in the case of DUO client secret.
+		if (array_key_exists('Client secret', $fields)) {
+			$this->assertTrue($dialog_form->getFieldContainer('Client secret')
+					->query('button:Change client secret')->one()->isClickable());
+			unset($fields['Client secret']);
+		}
+
+		// Verify field data.
+		$dialog_form->checkValue($fields);
 		$dialog->close();
 	}
 
 	/**
-	 * Returns a fields array populated with default values, used for verifying data.
+	 * Populates field data array with the repetitive data for Duo.
 	 *
-	 * @param array $fields    Fields data from data provider.
+	 * @fields array $fields    Fields data array to populate.
 	 */
-	protected function setDefaultFields($fields) {
-		$fields['Type'] = CTestArrayHelper::get($fields, 'Type', 'TOTP');
-
-		// Set the defaults if MFA type is TOTP.
-		if ($fields['Type'] === 'TOTP') {
-			$fields['Hash function'] = CTestArrayHelper::get($fields, 'Hash function', 'SHA-1');
-			$fields['Code length'] = CTestArrayHelper::get($fields, 'Code length', '6');
+	protected function setDefaultFieldsData($fields) {
+		if (CTestArrayHelper::get($fields, 'Type', '') === 'Duo Universal Prompt') {
+			$fields['API hostname'] = CTestArrayHelper::get($fields, 'API hostname', self::DUO_DEFAULT_API_HOSTNAME);
+			$fields['Client ID'] = CTestArrayHelper::get($fields, 'Client ID', self::DUO_DEFAULT_CLIENT_ID);
+			$fields['Client secret'] = CTestArrayHelper::get($fields, 'Client secret', self::DUO_DEFAULT_CLIENT_SECRET);
 		}
 
 		return $fields;
