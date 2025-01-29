6502df28ebd6cbd1ec56380e9a87245856e6d626
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2025-01-29 11:51:02 +0200

    ....I..... [ZBX-25868] fixed missing permissions for built-in templates

--- src/libs/zbxdbupgrade/dbupgrade_7030.c
+++ src/libs/zbxdbupgrade/dbupgrade_7030.c
@@ -14,16 +14,88 @@
 
 #include "dbupgrade.h"
 
+#include "zbxdb.h"
+#include "zbxalgo.h"
+#include "zbxnum.h"
+#include "dbupgrade_common.h"
+
 /*
  * 7.4 development database patches
  */
 
 #ifndef HAVE_SQLITE3
 
-/*static int	DBpatch_7030000(void)
+static int	DBpatch_7030000(void)
 {
-	*** first upgrade patch ***
-}*/
+	int			ret = SUCCEED;
+	char			*sql = NULL;
+	size_t			sql_alloc = 0, sql_offset = 0;
+	zbx_vector_uint64_t	ids, hgsetids;
+	zbx_db_result_t		result;
+	zbx_db_row_t		row;
+	zbx_db_insert_t		db_insert;
+
+	if (0 == (DBget_program_type() & ZBX_PROGRAM_TYPE_SERVER))
+		return SUCCEED;
+
+	zbx_vector_uint64_create(&ids);
+	zbx_vector_uint64_create(&hgsetids);
+
+	/* 3 - HOST_STATUS_TEMPLATE */
+	/* 0 - ZBX_FLAG_DISCOVERY_NORMAL */
+	/* 4 - ZBX_FLAG_DISCOVERY_CREATED */
+	zbx_db_select_uint64("select hostid from hosts"
+			" where status=3"
+				" and flags in (0,4)"
+				" and hostid not in (select hostid from host_hgset)", &ids);
+
+	if (0 == ids.values_num)
+		goto out;
+
+	ret = permission_hgsets_add(&ids, &hgsetids);
+
+	if (FAIL == ret || 0 == hgsetids.values_num)
+		goto out;
+
+	zbx_db_insert_prepare(&db_insert, "permission", "ugsetid", "hgsetid", "permission", (char*)NULL);
+	zbx_db_add_condition_alloc(&sql, &sql_alloc, &sql_offset, "h.hgsetid", hgsetids.values, hgsetids.values_num);
+
+	result = zbx_db_select("select u.ugsetid,h.hgsetid,max(r.permission)"
+			" from hgset h"
+			" join hgset_group hg"
+				" on h.hgsetid=hg.hgsetid"
+			" join rights r on hg.groupid=r.id"
+			" join ugset_group ug"
+				" on r.groupid=ug.usrgrpid"
+			" join ugset u"
+				" on ug.ugsetid=u.ugsetid"
+			" where%s"
+			" group by u.ugsetid,h.hgsetid"
+			" having min(r.permission)>0"
+			" order by u.ugsetid,h.hgsetid", sql);
+	zbx_free(sql);
+
+	while (NULL != (row = zbx_db_fetch(result)))
+	{
+		zbx_uint64_t	hgsetid, ugsetid;
+		int		permission;
+
+		ZBX_STR2UINT64(ugsetid, row[0]);
+		ZBX_STR2UINT64(hgsetid, row[1]);
+		permission = atoi(row[2]);
+
+		zbx_db_insert_add_values(&db_insert, ugsetid, hgsetid, permission);
+	}
+	zbx_db_free_result(result);
+
+	ret = zbx_db_insert_execute(&db_insert);
+	zbx_db_insert_clean(&db_insert);
+out:
+	zbx_vector_uint64_destroy(&hgsetids);
+	zbx_vector_uint64_destroy(&ids);
+
+	return ret;
+}
 
 #endif
 
@@ -31,6 +103,6 @@ DBPATCH_START(7030)
 
 /* version, duplicates flag, mandatory flag */
 
-/*DBPATCH_ADD(7030000, 0, 1)*/
+DBPATCH_ADD(7030000, 0, 1)
 
 DBPATCH_END()
--- src/libs/zbxdbupgrade/dbupgrade_common.c
+++ src/libs/zbxdbupgrade/dbupgrade_common.c
@@ -18,6 +18,15 @@
 #include "zbxdb.h"
 #include "zbxalgo.h"
 #include "zbxnum.h"
+#include "zbxhash.h"
+#include "zbxcrypto.h"
+
+typedef struct
+{
+	char			hash_str[ZBX_SHA256_DIGEST_SIZE * 2 + 1];
+	zbx_vector_uint64_t	groupids;
+	zbx_vector_uint64_t	ids;
+} zbx_dbu_group_set_t;
 
 int	delete_problems_with_nonexistent_object(void)
 {
@@ -97,3 +106,256 @@ int	drop_c_problem_2_index(void)
 	return SUCCEED;
 }
 #endif
+
+static zbx_hash_t	permission_group_set_hash(const void *data)
+{
+	const zbx_dbu_group_set_t	*group_set = (const zbx_dbu_group_set_t *)data;
+
+	return ZBX_DEFAULT_STRING_HASH_FUNC(group_set->hash_str);
+}
+
+static int	permission_group_set_compare(const void *d1, const void *d2)
+{
+	const zbx_dbu_group_set_t	*group_set1 = (const zbx_dbu_group_set_t *)d1;
+	const zbx_dbu_group_set_t	*group_set2 = (const zbx_dbu_group_set_t *)d2;
+
+	return strcmp(group_set1->hash_str, group_set2->hash_str);
+}
+
+static int	permission_groupsets_make(zbx_vector_uint64_t *ids, const char *fld_name_id,
+		const char *fld_name_groupid, const char *tbl_name_groups, zbx_hashset_t *group_sets,
+		int allow_empty_groups)
+{
+	int			ret = SUCCEED;
+	char			id_str[MAX_ID_LEN + 2];
+	zbx_db_result_t		result;
+	zbx_db_row_t		row;
+	zbx_vector_uint64_t	groupids;
+	zbx_dbu_group_set_t	*gset_ptr;
+
+	id_str[0] = '|';
+	zbx_vector_uint64_create(&groupids);
+
+	for (int i = 0; i < ids->values_num; i++)
+	{
+		unsigned char		hash[ZBX_SHA256_DIGEST_SIZE];
+		char			*id_str_p = id_str + 1;
+		sha256_ctx		ctx;
+		zbx_dbu_group_set_t	gset;
+
+		zbx_sha256_init(&ctx);
+
+		result = zbx_db_select("select %s from %s where %s=" ZBX_FS_UI64 " order by %s",
+				fld_name_groupid, tbl_name_groups, fld_name_id, ids->values[i], fld_name_groupid);
+
+		while (NULL != (row = zbx_db_fetch(result)))
+		{
+			zbx_uint64_t	groupid;
+
+			ZBX_STR2UINT64(groupid, row[0]);
+
+			if (1 == groupids.values_num)
+				id_str_p = id_str;
+
+			zbx_snprintf(id_str + 1, MAX_ID_LEN + 1, "%s", row[0]);
+			zbx_sha256_process_bytes(id_str_p, strlen(id_str_p), &ctx);
+			zbx_vector_uint64_append(&groupids, groupid);
+		}
+		zbx_db_free_result(result);
+
+		if (0 == groupids.values_num)
+		{
+			if (0 == allow_empty_groups)
+			{
+				zabbix_log(LOG_LEVEL_WARNING, "host or template [hostid=" ZBX_FS_UI64 "] is not"
+						" assigned to any group, permissions not granted", ids->values[i]);
+			}
+
+			continue;
+		}
+
+		zbx_sha256_finish(&ctx, hash);
+		(void)zbx_bin2hex(hash, ZBX_SHA256_DIGEST_SIZE, gset.hash_str,
+				ZBX_SHA256_DIGEST_SIZE * 2 + 1);
+
+		if (NULL == (gset_ptr = zbx_hashset_search(group_sets, &gset)))
+		{
+			zbx_vector_uint64_create(&gset.ids);
+			zbx_vector_uint64_create(&gset.groupids);
+			zbx_vector_uint64_append_array(&gset.groupids, groupids.values, groupids.values_num);
+
+			if (NULL == (gset_ptr = zbx_hashset_insert(group_sets, &gset, sizeof(zbx_dbu_group_set_t))))
+			{
+				ret = FAIL;
+				break;
+			}
+		}
+
+		zbx_vector_uint64_append(&gset_ptr->ids, ids->values[i]);
+		zbx_vector_uint64_clear(&groupids);
+	}
+
+	zbx_vector_uint64_destroy(&groupids);
+
+	return ret;
+}
+
+static int	permission_groupsets_insert(const char *tbl_name, zbx_hashset_t *group_sets, zbx_db_insert_t *db_gset,
+		zbx_db_insert_t *db_gset_groups, zbx_db_insert_t *db_gset_parents, zbx_vector_uint64_t *hgsetids)
+{
+	zbx_uint64_t		gsetid;
+	zbx_hashset_iter_t	iter;
+	zbx_dbu_group_set_t	*gset_ptr;
+
+	if (0 == group_sets->num_data)
+		return SUCCEED;
+
+	gsetid = zbx_db_get_maxid_num(tbl_name, group_sets->num_data);
+
+	zbx_hashset_iter_reset(group_sets, &iter);
+
+	while (NULL != (gset_ptr = (zbx_dbu_group_set_t *)zbx_hashset_iter_next(&iter)))
+	{
+		int	i;
+
+		if (NULL != hgsetids)
+		{
+			char		*sql;
+			zbx_db_result_t	result;
+			zbx_db_row_t	row;
+
+			sql = zbx_dsprintf(NULL, "select hgsetid from hgset where hash='%s'", gset_ptr->hash_str);
+			result = zbx_db_select_n(sql, 1);
+			zbx_free(sql);
+
+			if (NULL != (row = zbx_db_fetch(result)))
+			{
+				zbx_uint64_t	hgsetid;
+
+				ZBX_STR2UINT64(hgsetid, row[0]);
+				zbx_db_free_result(result);
+
+				for (i = 0; i < gset_ptr->ids.values_num; i++)
+					zbx_db_insert_add_values(db_gset_parents, gset_ptr->ids.values[i], hgsetid);
+
+				continue;
+			}
+
+			zbx_db_free_result(result);
+			zbx_vector_uint64_append(hgsetids, gsetid);
+		}
+
+		zbx_db_insert_add_values(db_gset, gsetid, gset_ptr->hash_str);
+
+		for (i = 0; i < gset_ptr->groupids.values_num; i++)
+			zbx_db_insert_add_values(db_gset_groups, gsetid, gset_ptr->groupids.values[i]);
+
+		for (i = 0; i < gset_ptr->ids.values_num; i++)
+			zbx_db_insert_add_values(db_gset_parents, gset_ptr->ids.values[i], gsetid);
+
+		gsetid++;
+	}
+
+	if (FAIL == zbx_db_insert_execute(db_gset) ||
+			FAIL == zbx_db_insert_execute(db_gset_groups) ||
+			FAIL == zbx_db_insert_execute(db_gset_parents))
+	{
+		return FAIL;
+	}
+
+	return SUCCEED;
+}
+
+static void	permission_groupsets_destroy(zbx_hashset_t *group_sets)
+{
+	zbx_hashset_iter_t	iter;
+	zbx_dbu_group_set_t	*gset_ptr;
+
+	zbx_hashset_iter_reset(group_sets, &iter);
+
+	while (NULL != (gset_ptr = (zbx_dbu_group_set_t *)zbx_hashset_iter_next(&iter)))
+	{
+		zbx_vector_uint64_destroy(&gset_ptr->groupids);
+		zbx_vector_uint64_destroy(&gset_ptr->ids);
+	}
+
+	zbx_hashset_destroy(group_sets);
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Purpose: calculates hashes and adds new group sets for hosts               *
+ *                                                                            *
+ * Parameters: ids      - [IN] host IDs                                       *
+ *             hgsetids - [IN/OUT] added host group sets IDs, optional        *
+ *                                                                            *
+ * Return value: SUCCEED - new host group sets added successfully,            *
+ *                           or not needed                                    *
+ *               FAIL    - otherwise                                          *
+ *                                                                            *
+ * Comments: If hgsetids parameter was specified, then function checks if     *
+ *           hgset already exists before adding new one.                      *
+ *                                                                            *
+******************************************************************************/
+int	permission_hgsets_add(zbx_vector_uint64_t *ids, zbx_vector_uint64_t *hgsetids)
+{
+	int		ret;
+	zbx_hashset_t	hgsets;
+	zbx_db_insert_t	db_insert, db_insert_groups, db_insert_hosts;
+
+	zbx_hashset_create(&hgsets, 1, permission_group_set_hash, permission_group_set_compare);
+	zbx_db_insert_prepare(&db_insert, "hgset", "hgsetid", "hash", (char*)NULL);
+	zbx_db_insert_prepare(&db_insert_groups, "hgset_group", "hgsetid", "groupid", (char*)NULL);
+	zbx_db_insert_prepare(&db_insert_hosts, "host_hgset", "hostid", "hgsetid", (char*)NULL);
+
+	if (SUCCEED == (ret = permission_groupsets_make(ids, "hostid", "groupid", "hosts_groups", &hgsets, 0)))
+	{
+		ret = permission_groupsets_insert("hgset", &hgsets, &db_insert, &db_insert_groups, &db_insert_hosts,
+				hgsetids);
+	}
+
+	zbx_db_insert_clean(&db_insert);
+	zbx_db_insert_clean(&db_insert_groups);
+	zbx_db_insert_clean(&db_insert_hosts);
+
+	permission_groupsets_destroy(&hgsets);
+
+	return ret;
+}
+
+/******************************************************************************
+ *                                                                            *
+ * Purpose: calculates hashes and adds new group sets for users               *
+ *                                                                            *
+ * Parameters: ids  - [IN] user IDs                                           *
+ *                                                                            *
+ * Return value: SUCCEED - new user group sets added successfully,            *
+ *                           or not needed                                    *
+ *               FAIL    - otherwise                                          *
+ *                                                                            *
+******************************************************************************/
+int	permission_ugsets_add(zbx_vector_uint64_t *ids)
+{
+	int			ret;
+	zbx_hashset_t		ugsets;
+	zbx_db_insert_t		db_insert, db_insert_groups, db_insert_users;
+
+	zbx_hashset_create(&ugsets, 1, permission_group_set_hash, permission_group_set_compare);
+	zbx_db_insert_prepare(&db_insert, "ugset", "ugsetid", "hash", (char*)NULL);
+	zbx_db_insert_prepare(&db_insert_groups, "ugset_group", "ugsetid", "usrgrpid", (char*)NULL);
+	zbx_db_insert_prepare(&db_insert_users, "user_ugset", "userid", "ugsetid", (char*)NULL);
+
+	if (SUCCEED == (ret = permission_groupsets_make(ids, "userid", "usrgrpid", "users_groups", &ugsets, 1)))
+	{
+		ret = permission_groupsets_insert("ugset", &ugsets, &db_insert, &db_insert_groups, &db_insert_users,
+				NULL);
+	}
+
+	zbx_db_insert_clean(&db_insert);
+	zbx_db_insert_clean(&db_insert_groups);
+	zbx_db_insert_clean(&db_insert_users);
+
+	permission_groupsets_destroy(&ugsets);
+
+	return ret;
+}
