b598bd19a9505b2d47671b5c5d48899af6c0da42
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2025-01-30 13:59:55 +0200

    .......... [ZBX-25442] updated cmocka tests to correspond 7.0

--- tests/libs/zbxsysinfo/agent/MODBUS_GET.c
+++ tests/libs/zbxsysinfo/agent/MODBUS_GET.c
@@ -20,7 +20,7 @@
 #include "zbxmocktest.h"
 #include "zbxmockassert.h"
 #include "zbxmockutil.h"
-#include "modbtype.h"
+#include "../../../../src/libs/zbxsysinfo/agent/modbtype.h"
 #include <modbus.h>
 
 modbus_t	*__wrap_modbus_new_rtu(char *port, int baudrate,
@@ -140,7 +140,7 @@ static int	read_registers_common(int func, int addr, int nb, uint16_t *dest)
 
 	for (int i = 0; i < nb * chr_num; i += chr_num)
 	{
-		if (SUCCEED != is_hex_n_range(data + i, (size_t)chr_num, dest + k++, sizeof(uint16_t), 0, 0xFFFF))
+		if (SUCCEED != zbx_is_hex_n_range(data + i, (size_t)chr_num, dest + k++, sizeof(uint16_t), 0, 0xFFFF))
 			fail_msg("Invalid data value: %s", data);
 	}
 
@@ -187,13 +187,13 @@ void	zbx_mock_test_entry(void **state)
 	ZBX_UNUSED(state);
 
 	item_key = zbx_mock_get_parameter_string("in.key");
-	init_request(&request);
+	zbx_init_agent_request(&request);
 
-	if (SUCCEED != parse_item_key(item_key, &request))
+	if (SUCCEED != zbx_parse_item_key(item_key, &request))
 		fail_msg("Cannot parse item key: '%s'", item_key);
 
-	init_result(&result);
-	ret = MODBUS_GET(&request, &result);
+	zbx_init_agent_result(&result);
+	ret = modbus_get(&request, &result);
 
 	zbx_mock_assert_sysinfo_ret_eq("Return value",
 			zbx_mock_str_to_return_code(zbx_mock_get_parameter_string("out.return")), ret);
@@ -204,10 +204,10 @@ void	zbx_mock_test_entry(void **state)
 
 		if (ZBX_MOCK_SUCCESS == zbx_mock_out_parameter("msg", &handle))
 		{
-			if (0 != ISSET_MSG(&result))
+			if (0 != ZBX_ISSET_MSG(&result))
 			{
 				zbx_mock_assert_str_eq("Error message", zbx_mock_get_parameter_string("out.msg"),
-						*GET_MSG_RESULT(&result));
+						*ZBX_GET_MSG_RESULT(&result));
 			}
 			else
 				fail_msg("No error message");
@@ -216,24 +216,24 @@ void	zbx_mock_test_entry(void **state)
 		goto out;
 	}
 
-	if (0 != ISSET_STR(&result))
+	if (0 != ZBX_ISSET_STR(&result))
 	{
 		zbx_mock_assert_str_eq("result (str)", zbx_mock_get_parameter_string("out.result"),
-				*GET_STR_RESULT(&result));
+				*ZBX_GET_STR_RESULT(&result));
 	}
-	else if (0 != ISSET_UI64(&result))
+	else if (0 != ZBX_ISSET_UI64(&result))
 	{
 		zbx_mock_assert_uint64_eq("result (ui64)", zbx_mock_get_parameter_uint64("out.result"),
-				*GET_UI64_RESULT(&result));
+				*ZBX_GET_UI64_RESULT(&result));
 	}
-	else if (0 != ISSET_DBL(&result))
+	else if (0 != ZBX_ISSET_DBL(&result))
 	{
 		zbx_mock_assert_double_eq("result (dbl)", zbx_mock_get_parameter_float("out.result"),
-				*GET_DBL_RESULT(&result));
+				*ZBX_GET_DBL_RESULT(&result));
 	}
 	else
 		fail_msg("Invalid result type");
 out:
-	free_request(&request);
-	free_result(&result);
+	zbx_free_agent_request(&request);
+	zbx_free_agent_result(&result);
 }
