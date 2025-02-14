1f9d25c7e326f716c95f2bca0b704cc8468313b7
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2025-02-14 16:39:16 +0200

    A.F.....S. [ZBX-23760] added prevention of deleting host groups, templates, hosts, triggers, services, discovery rules and discovery checks if they are used in action conditions or operations
    * commit '34ec260b78163262490c5a9b1f09cd7fa5f80a8a': (22 commits)
      .......... [ZBX-23760] fixed coding style
      .......... [ZBX-23760] fixed error message in autotests
      A......... [ZBX-23760] removed extra dots from error messages
      ..F....... [ZBX-23760] fixed undefined array key "opgroup" errors
      A......... [ZBX-23760] fixed error messages
      A......... [ZBX-23760] fixed permission check sequence
      .......... [ZBX-23760] fixed unit tests
      A......... [ZBX-23760] fixed code style of the implemented checks; fixed error message for the case when deleting network discovery rule has checks which are used in actions; changed the error message for the case when during the update of discovery rule the discovery check which is used in actions was deleted
      A......... [ZBX-23760] fixed code style
      A......... [ZBX-23760] removed no more needed code for disabling actions on template delete
      A......... [ZBX-23760] updated condition order
      ........S. [ZBX-23760] removed redundant query
      A......... [ZBX-23760] fixed code style in permissions check of action.get API method
      A......... [ZBX-23760] removed redundant check when deleting the triggers
      .......... [ZBX-23760] fixed int tests
      ........S. [ZBX-23760] merged hostgroup changes, fixed cmocka build failure
      ........S. [ZBX-23760] merged server changes
      .......... [ZBX-23760] fixed error message in Selenium test
      .......... [ZBX-23760] updated error messages in Selenium tests
      .D........ [ZBX-23760] added changelog file
      ...
    
    (cherry picked from commit ba2efa8abea8f9c92221d7c235cb78d3271f6990)

--- ui/include/classes/api/services/CScript.php
+++ ui/include/classes/api/services/CScript.php
@@ -787,6 +787,7 @@ class CScript extends CApiService {
 		$db_groups = API::HostGroup()->get([
 			'output' => [],
 			'groupids' => $groupids,
+			'filter' => ['flags' => ZBX_FLAG_DISCOVERY_NORMAL],
 			'preservekeys' => true
 		]);
 
