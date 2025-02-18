63486bfab3d5755cd3832e251ebb4d20fd209a58
Continuous Integration <ci@zabbix.com>
2025-02-18 09:41:05 +0200

    .......... [ZBXNEXT-826] updated release date for 7.4.0alpha1

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -22,7 +22,7 @@ import (
 )
 
 const (
-	ZABBIX_REVDATE          = "9 December 2024"
+	ZABBIX_REVDATE          = "18 February 2025"
 	ZABBIX_VERSION_MAJOR    = 7
 	ZABBIX_VERSION_MINOR    = 4
 	ZABBIX_VERSION_PATCH    = 0
--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -17,7 +17,7 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "9 December 2024";
+	static final String REVISION_DATE = "18 February 2025";
 	static final String REVISION = "{ZABBIX_REVISION}";
 	static final String VERSION = "7.4.0alpha1";
 
