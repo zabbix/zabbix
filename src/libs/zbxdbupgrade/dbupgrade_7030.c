541af271a5b4b6a1ceb300399e1002c3c78b541c
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2025-02-26 10:50:01 +0200

    ........S. [ZBX-25365] added db upgrade patch

--- src/libs/zbxdbupgrade/dbupgrade_7030.c
+++ src/libs/zbxdbupgrade/dbupgrade_7030.c
@@ -152,6 +152,24 @@ static int	DBpatch_7030011(void)
 	return DBdrop_table("config");
 }
 
+static int	DBpatch_7030012(void)
+{
+	if (0 == (DBget_program_type() & ZBX_PROGRAM_TYPE_SERVER))
+		return SUCCEED;
+
+	/* 2 - SYSMAP_ELEMENT_TYPE_TRIGGER */
+	if (ZBX_DB_OK > zbx_db_execute("delete from sysmaps_elements"
+			" where elementtype=2"
+				" and selementid not in ("
+					"select distinct selementid from sysmap_element_trigger"
+				")"))
+	{
+		return FAIL;
+	}
+
+	return SUCCEED;
+}
+
 #endif
 
 DBPATCH_START(7030)
@@ -170,5 +188,6 @@ DBPATCH_ADD(7030008, 0, 1)
 DBPATCH_ADD(7030009, 0, 1)
 DBPATCH_ADD(7030010, 0, 1)
 DBPATCH_ADD(7030011, 0, 1)
+DBPATCH_ADD(7030012, 0, 1)
 
 DBPATCH_END()
