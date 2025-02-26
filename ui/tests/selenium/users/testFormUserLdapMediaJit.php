3d7649bc59fde3d87e0c1a744ee36a3f7275ac78
Nikita Gogolevs <nikita.gogolevs@zabbix.com>
2025-02-26 16:08:37 +0200

    .......... [DEV-3698] Selenium: adjusted syntax of SQL query

--- ui/tests/selenium/users/testFormUserLdapMediaJit.php
+++ ui/tests/selenium/users/testFormUserLdapMediaJit.php
@@ -53,10 +53,10 @@ class testFormUserLdapMediaJit extends CWebTest {
 	 * Enable media types before test.
 	 */
 	public function prepareJitMedia() {
-		$mediatypeids = CDBHelper::getAll('SELECT mediatypeid FROM media_type WHERE name IN (\'iTop\', \'SMS\', '
-				.'\'MS Teams Workflow\', \'Slack\', \'OTRS\', \'Opsgenie\', \'Brevis.one\', \'Github\', \'Discord\', '
-				.'\'iLert\', \'SIGNL4\', \'SysAid\', \'Jira\', \'Line\', \'Email\', \'PagerDuty\', \'Pushover\', '
-				.'\'Telegram\', \'Redmine\', \'Zammad\', \'VictorOps\', \'ServiceNow\')'
+		$mediatypeids = CDBHelper::getAll('SELECT mediatypeid FROM media_type WHERE name IN (\'iTop\', \'SMS\','.
+				' \'MS Teams Workflow\', \'Slack\', \'OTRS\', \'Opsgenie\', \'Brevis.one\', \'Github\', \'Discord\','.
+				' \'iLert\', \'SIGNL4\', \'SysAid\', \'Jira\', \'Line\', \'Email\', \'PagerDuty\', \'Pushover\','.
+				' \'Telegram\', \'Redmine\', \'Zammad\', \'VictorOps\', \'ServiceNow\')'
 		);
 
 		$update_api = [];
