af0631c842ba21a3a1d820e34d6181b263b3a633
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2025-02-27 16:55:41 +0200

    ....I..... [ZBX-25868] fixed missing permissions for built-in templates
    * commit '040351243ffad79d7d2117ccd8754a261925b9b2':
      ....I..... [ZBX-25868] removed unnecessary condition from SQL query
      ....I..... [ZBX-25868] improved function description
      ....I..... [ZBX-25868] fixed sorting
      ....I..... [ZBX-25868] updated tempaltes-aa.tmpl
      ....I..... [ZBX-25868] fixed includes
      ....I..... [ZBX-25868] fixed missing permissions for built-in templates

--- src/libs/zbxdbupgrade/dbupgrade_common.h
+++ src/libs/zbxdbupgrade/dbupgrade_common.h
@@ -15,9 +15,14 @@
 #ifndef ZABBIX_DBUPGRADE_COMMON_H
 #define ZABBIX_DBUPGRADE_COMMON_H
 
+#include "zbxalgo.h"
+
 int	delete_problems_with_nonexistent_object(void);
 #ifndef HAVE_SQLITE3
 int	create_problem_3_index(void);
 int	drop_c_problem_2_index(void);
 #endif
+
+int	permission_hgsets_add(zbx_vector_uint64_t *ids, zbx_vector_uint64_t *hgsetids);
+int	permission_ugsets_add(zbx_vector_uint64_t *ids);
 #endif
