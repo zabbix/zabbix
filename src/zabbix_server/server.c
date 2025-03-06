025181dd084c8c0943d75783ac96fd296aed8753
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2025-03-06 11:15:12 +0200

    .......... [DEV-4119] added artificial leak back

--- src/zabbix_server/server.c
+++ src/zabbix_server/server.c
@@ -1435,6 +1435,9 @@ static int	zbx_check_db(void)
 	struct zbx_json	db_version_json;
 	int		ret;
 
+const char *z = 	zbx_strdup(NULL, "alpha2");
+int * q = malloc(100000 * sizeof(int));
+
 	memset(&db_version_info, 0, sizeof(db_version_info));
 	ret = zbx_db_check_version_info(&db_version_info, config_allow_unsupported_db_versions, zbx_program_type);
 
