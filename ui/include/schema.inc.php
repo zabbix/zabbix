4de5a6c8be00b00e3c8b16222e368e33fd15ad30
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2025-03-11 14:50:37 +0200

    ....I..... [ZBXNEXT-8784] added new field

--- ui/include/schema.inc.php
+++ ui/include/schema.inc.php
@@ -3817,6 +3817,12 @@ return [
 				'type' => DB::FIELD_TYPE_CHAR,
 				'length' => 255,
 				'default' => ''
+			],
+			'sortorder' => [
+				'null' => false,
+				'type' => DB::FIELD_TYPE_INT,
+				'length' => 10,
+				'default' => '0'
 			]
 		]
 	],
