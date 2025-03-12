4b7ae415d94356a7657e3759a02ba98459c7bb87
Robert Gontarski <robert.gontarski@zabbix.com>
2025-03-12 02:49:44 -0700

    ..F....... [DEV-4226] added debug info into response

--- ui/app/views/monitoring.latest.view.refresh.php
+++ ui/app/views/monitoring.latest.view.refresh.php
@@ -31,4 +31,9 @@ if (($messages = getMessages()) !== null) {
 	$output['messages'] = $messages->toString();
 }
 
+if (CWebUser::$data['debug_mode'] == GROUP_DEBUG_MODE_ENABLED) {
+	CProfiler::getInstance()->stop();
+	$output['debug'] = CProfiler::getInstance()->make()->toString();
+}
+
 echo json_encode($output);
