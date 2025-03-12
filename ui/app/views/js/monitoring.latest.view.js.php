4ae57b43fe1621dd7ea029f96f9132094d301da0
Robert Gontarski <robert.gontarski@zabbix.com>
2025-03-12 02:52:20 -0700

    ..F....... [DEV-4226] added refresh debug after refresh filters

--- ui/app/views/js/monitoring.latest.view.js.php
+++ ui/app/views/js/monitoring.latest.view.js.php
@@ -144,6 +144,7 @@
 						postMessageDetails('success', e.detail.messages);
 					}
 
+					this._refreshResults();
 					this.refresh();
 				});
 			});
@@ -175,8 +176,7 @@
 		},
 
 		reloadPartialAndTabCounters() {
-			this.refresh_url = new Curl('');
-
+			this._refreshResults();
 			this.unscheduleRefresh();
 			this.refresh();
 
@@ -243,6 +243,46 @@
 				this._popup_message_box.remove();
 				this._popup_message_box = null;
 			}
+		},
+
+		_refreshDebug(debug) {
+			document.querySelector('.wrapper > .debug-output').replaceWith(
+				new DOMParser().parseFromString(debug, 'text/html').body.firstElementChild
+			);
+		},
+
+		_refreshResults() {
+			const url = new Curl();
+			const refresh_url = new Curl('zabbix.php');
+			const data = Object.assign({}, this.filter_defaults, this.global_timerange, url.getArgumentsObject());
+
+			// Modify filter data.
+			data.inventory = data.inventory
+				? data.inventory.filter(inventory => 'value' in inventory && inventory.value !== '')
+				: data.inventory;
+			data.tags = data.tags
+				? data.tags.filter(tag => !(tag.tag === '' && tag.value === ''))
+				: data.tags;
+			data.severities = data.severities
+				? data.severities.filter((value, key) => value == key)
+				: data.severities;
+			data.page = this.refresh_url.getArgument('page') ?? 1;
+
+			if (!data.filter_custom_time) {
+				data.from = this.global_timerange.from;
+				data.to = this.global_timerange.to;
+			}
+
+			Object.entries(data).forEach(([key, value]) => {
+				if (['filter_show_counter', 'filter_custom_time', 'action'].indexOf(key) !== -1) {
+					return;
+				}
+
+				refresh_url.setArgument(key, value);
+			});
+
+			refresh_url.setArgument('action', 'host.view.refresh');
+			this.refresh_url = refresh_url;
 		},
 
 		refresh() {
@@ -335,6 +375,10 @@
 				this._addRefreshMessage(response.messages);
 			}
 
+			if ('debug' in response) {
+				this._refreshDebug(response.debug);
+			}
+
 			this.initExpandableSubfilter();
 		},
 
@@ -368,6 +412,7 @@
 			if (this.refresh_interval > 0) {
 				this.timeout = setTimeout((function () {
 					this.timeout = null;
+					this._refreshResults();
 					this.refresh();
 				}).bind(this), this.refresh_interval);
 			}
