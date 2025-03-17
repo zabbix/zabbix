d76cb728ce8509936c2275b32234a6d8d00df03a
Romans Trizna <romans.trizna@zabbix.com>
2025-03-17 15:42:01 +0200

    ........S. [ZBX-26056] fixed agent stop

--- ui/tests/integration/testTriggerLinking.php
+++ ui/tests/integration/testTriggerLinking.php
@@ -561,9 +561,9 @@ class testTriggerLinking extends CIntegrationTest {
 		/* We need agent 2 only because it will have the different host metadata from the agent 1.
 			This would retrigger the autoregistration with linking. Stop this for now.
 			If I knew how to change host metadata of agent 1 in integration test - I would not need agent2. */
-		$this->killComponent(self::COMPONENT_AGENT2);
+		/*$this->killComponent(self::COMPONENT_AGENT2);
 		$this->killComponent(self::COMPONENT_AGENT);
-		$this->killComponent(self::COMPONENT_SERVER);
+		$this->killComponent(self::COMPONENT_SERVER);*/
 		$this->prepareTemplatesWithConflictsAndSetupActionsToLinkFirstSetOfTemplates();
 		$this->startComponent(self::COMPONENT_SERVER);
 		sleep(1);
