5df8b97ae775b9c6d4235b73b3591c05eda9e4e1
Eliza Sekace <eliza.sekace@zabbix.com>
2025-03-25 09:38:00 +0200

    ..F....... [ZBXNEXT-9850] fixed return URL after deleting a host in graph prototype list

--- ui/app/views/js/graph.prototype.list.js.php
+++ ui/app/views/js/graph.prototype.list.js.php
@@ -87,11 +87,9 @@
 					uncheckTableRows('graphs_' + this.checkbox_hash, [], false);
 
 					if (data.submit.success.action === 'delete') {
-						const url = new URL('zabbix.php', location.href);
+						const url = new URL('host_discovery.php', location.href);
 
-						url.searchParams.set('action', 'graph.prototype.list');
 						url.searchParams.set('context', this.context);
-						url.searchParams.set('parent_discoveryid', this.parent_discoveryid);
 
 						event.setRedirectUrl(url.href);
 					}
