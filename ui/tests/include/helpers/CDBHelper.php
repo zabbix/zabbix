aa49cd261072553af7755de28d92c36cce117cc9
Natalja Romancaka <natalja.romancaka@zabbix.com>
2025-04-04 16:13:51 +0300

    .......... [DEV-4265] added item tags to setTriggerProblem method in Selenium framework
    (cherry picked from commit 38092affb1a9c4796d98cfbcec97bae2fe15efba)

--- ui/tests/include/helpers/CDBHelper.php
+++ ui/tests/include/helpers/CDBHelper.php
@@ -555,11 +555,15 @@ class CDBHelper {
 			if ($trigger) {
 				$trigger = $trigger[0];
 
-				$tags = DB::select('trigger_tag', [
+				$trigger_tags = DB::select('trigger_tag', [
 					'output' => ['tag', 'value'],
-					'filter' => ['triggerid' => $trigger['triggerid']],
-					'preservekeys' => true
+					'filter' => ['triggerid' => $trigger['triggerid']]
 				]);
+				$item_tags = CDBHelper::getAll(
+					'SELECT tag, value FROM item_tag WHERE itemid IN'.
+						' (SELECT DISTINCT itemid FROM functions WHERE triggerid='.zbx_dbstr($trigger['triggerid']).')'
+				);
+				$all_tags = array_merge($trigger_tags, $item_tags);
 
 				$time = time();
 
@@ -625,11 +629,18 @@ class CDBHelper {
 						}
 					}
 
-					if ($tags) {
-						foreach ($tags as &$tag) {
-							$tag['eventid'] = $fields['eventid'];
+					if ($all_tags) {
+						// Keep unique 'tag' => 'value' pair from merged trigger and item tags.
+						$unique = [];
+						foreach ($all_tags as &$tag) {
+							$key = $tag['tag'].'|'.$tag['value'];
+							if (!array_key_exists($key, $unique)) {
+								$unique[$key] = $tag;
+								$unique[$key]['eventid'] = $fields['eventid'];
+							}
 						}
 						unset($tag);
+						$tags = array_values($unique);
 
 						DB::insertBatch('event_tag', $tags);
 
