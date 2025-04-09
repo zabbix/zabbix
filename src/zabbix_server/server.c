f40bae6bb04d6e1371a76bac308ed2d2aaeb915b
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2025-04-09 15:11:07 +0300

    .......... [DEV-4119] removed artificial issue

--- src/zabbix_server/server.c
+++ src/zabbix_server/server.c
@@ -1435,14 +1435,6 @@ static int	zbx_check_db(void)
 	struct zbx_json	db_version_json;
 	int		ret;
 
-	int k = 0x7fffffff;
-	k += 9;
-	zabbix_log(LOG_LEVEL_INFORMATION, "BADER: %d", k);
-
-	int x = INT_MAX;
-	x += 1;
-	zabbix_log(LOG_LEVEL_INFORMATION, "BADER2: %d", x);
-
 	memset(&db_version_info, 0, sizeof(db_version_info));
 	ret = zbx_db_check_version_info(&db_version_info, config_allow_unsupported_db_versions, zbx_program_type);
 
