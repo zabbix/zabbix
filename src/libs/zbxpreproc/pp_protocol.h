a6af6c7872841956221d3cd6df51f0ec2bc9946d
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2025-04-10 08:50:10 +0300

    .......PS. [ZBXNEXT-9877] added zabbix[preprocessing] internal item
    * commit 'ae523aaa8674160efd4667fef897d4cabffa99d9':
      .......PS. [ZBXNEXT-9877] fixed variable initialization
      .......PS. [ZBXNEXT-9877] simplified preprocessor_add_size_json()
      .......PS. [ZBXNEXT-9877] fixed '-R diaginfo=preprocessing'
      .......PS. [ZBXNEXT-9877] fixed parameter check
      .......PS. [ZBXNEXT-9877] added zabbix[preprocessing] internal item
    
    (cherry picked from commit 983046624d0734f71d24c1995f7268fedd3ae46c)
    (cherry picked from commit 5aaf42da1864a134a6753aeccf1223f6bde0b8e7)

--- src/libs/zbxpreproc/pp_protocol.h
+++ src/libs/zbxpreproc/pp_protocol.h
@@ -34,6 +34,7 @@
 #define ZBX_IPC_PREPROCESSOR_TOP_STATS_RESULT		10008
 #define ZBX_IPC_PREPROCESSOR_USAGE_STATS		10009
 #define ZBX_IPC_PREPROCESSOR_TOP_PEAK			10010
+#define ZBX_IPC_PREPROCESSOR_SIZE			10011
 
 /* item value data used in preprocessing manager */
 typedef struct
@@ -72,10 +73,20 @@ void	zbx_preprocessor_unpack_test_result(zbx_vector_pp_result_ptr_t *results, zb
 		const unsigned char *data);
 
 zbx_uint32_t	zbx_preprocessor_pack_diag_stats(unsigned char **data, zbx_uint64_t preproc_num,
-		zbx_uint64_t pending_num, zbx_uint64_t finished_num, zbx_uint64_t sequences_num);
+		zbx_uint64_t pending_num, zbx_uint64_t finished_num, zbx_uint64_t sequences_num,
+		zbx_uint64_t queued_num, zbx_uint64_t queued_sz, zbx_uint64_t direct_num, zbx_uint64_t direct_sz);
+
+zbx_uint32_t	zbx_preprocessor_pack_values_stats(unsigned char **data, zbx_uint64_t queued_num,
+		zbx_uint64_t queued_sz, zbx_uint64_t direct_num, zbx_uint64_t direct_sz, zbx_uint64_t enqueued_num);
 
 void	zbx_preprocessor_unpack_diag_stats(zbx_uint64_t *preproc_num, zbx_uint64_t *pending_num,
-		zbx_uint64_t *finished_num, zbx_uint64_t *sequences_num, const unsigned char *data);
+		zbx_uint64_t *finished_num, zbx_uint64_t *sequences_num, zbx_uint64_t *queued_num,
+		zbx_uint64_t *queued_sz, zbx_uint64_t *direct_num, zbx_uint64_t *direct_sz,
+		const unsigned char *data);
+
+void	zbx_preprocessor_unpack_values_stats(zbx_uint64_t *queued_num, zbx_uint64_t *queued_sz,
+		zbx_uint64_t *direct_num, zbx_uint64_t *direct_sz, zbx_uint64_t *enqueued_num,
+		const unsigned char *data);
 
 zbx_uint32_t	zbx_preprocessor_pack_top_stats_request(unsigned char **data, int limit);
 
