6f9b8c73ed48cc21a7eaaa7e9b72f30ef49d8c28
Andrejs Verza <andrejs.verza@zabbix.com>
2025-04-16 14:19:57 +0300

    ..F....... [ZBXNEXT-9319] fixed console error when misconfigured Graph prototype widget has listeners

--- ui/widgets/graph/assets/js/class.widget.js
+++ ui/widgets/graph/assets/js/class.widget.js
@@ -145,7 +145,9 @@ class CWidgetGraph extends CWidget {
 					.getDashboardPage(this._dashboard_page.unique_id)
 					.getWidget(descriptor.sender_unique_id);
 
-				has_custom_time_period = graph_prototype_widget.hasCustomTimePeriod();
+				if (!(graph_prototype_widget instanceof CWidgetMisconfigured)) {
+					has_custom_time_period = graph_prototype_widget.hasCustomTimePeriod();
+				}
 			}
 			else {
 				has_custom_time_period = false;
