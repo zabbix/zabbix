c27b9fd5c1fe97f772beca351b01543fd22722cf
Anna Poga <anna.poga@zabbix.com>
2025-04-17 15:52:35 +0300

    ..F....... [ZBXNEXT-8919] changed default color from red to first palette in Graph widget

--- ui/widgets/svggraph/includes/CWidgetFieldDataSet.php
+++ ui/widgets/svggraph/includes/CWidgetFieldDataSet.php
@@ -39,8 +39,8 @@ class CWidgetFieldDataSet extends CWidgetField {
 		'6AC8FF', 'EE2B29', '3CA20D', '6F4BBC', '00A1FF', 'F3601B', '1CAE59', '45CFDB', '894BBC', '6D6D6D'
 	];
 
-	// First color from the default color palette.
-	private const DEFAULT_COLOR = 'FF465C';
+	// First palette from predefined palettes.
+	private const DEFAULT_PALETTE = PALETTE_PREFIX.'0';
 
 	public function __construct(string $name, ?string $label = null) {
 		parent::__construct($name, $label);
@@ -53,8 +53,8 @@ class CWidgetFieldDataSet extends CWidgetField {
 				'items'					=> ['type' => API_STRINGS_UTF8],
 				'itemids'				=> ['type' => API_IDS],
 				'references'			=> ['type' => API_STRINGS_UTF8],
-				'color'					=> ['type' => API_COLOR],
-				'color_palette'			=> ['type' => API_STRING_UTF8],
+				'color'					=> ['type' => API_COLOR, 'flags' => API_NOT_EMPTY],
+				'color_palette'			=> ['type' => API_STRING_UTF8, 'flags' => API_NOT_EMPTY],
 				'type'					=> ['type' => API_INT32, 'flags' => API_REQUIRED, 'in' => implode(',', [SVG_GRAPH_TYPE_LINE, SVG_GRAPH_TYPE_POINTS, SVG_GRAPH_TYPE_STAIRCASE, SVG_GRAPH_TYPE_BAR])],
 				'stacked'				=> ['type' => API_INT32, 'flags' => API_REQUIRED, 'in' => implode(',', [SVG_GRAPH_STACKED_OFF, SVG_GRAPH_STACKED_ON])],
 				'width'					=> ['type' => API_INT32, 'in' => '0:10'],
@@ -94,7 +94,7 @@ class CWidgetFieldDataSet extends CWidgetField {
 			'items' => [],
 			'itemids' => [],
 			'references' => [],
-			'color' => self::DEFAULT_COLOR,
+			'color_palette' => self::DEFAULT_PALETTE,
 			'type' => SVG_GRAPH_TYPE_LINE,
 			'stacked' => SVG_GRAPH_STACKED_OFF,
 			'width' => SVG_GRAPH_DEFAULT_WIDTH,
@@ -212,16 +212,6 @@ class CWidgetFieldDataSet extends CWidgetField {
 		}
 		unset($data);
 
-		foreach ($value as $data) {
-			if ($data['dataset_type'] == self::DATASET_TYPE_PATTERN_ITEM
-					&& (!array_key_exists('color', $data) || $data['color'] === '')
-					&& (!array_key_exists('color_palette', $data) || $data['color_palette'] === '')) {
-				$errors[] = _s('Invalid parameter "%1$s": %2$s.', _('Data set color'),
-					_('a hexadecimal color code (6 symbols) or color palette code is expected'));
-				break;
-			}
-		}
-
 		if (!$errors) {
 			$this->setValue($value);
 		}
@@ -297,18 +287,18 @@ class CWidgetFieldDataSet extends CWidgetField {
 				}
 			}
 			else {
-				if (array_key_exists('color_palette', $value)) {
-					$widget_fields[] = [
-						'type' => ZBX_WIDGET_FIELD_TYPE_STR,
-						'name' => $this->name.'.'.$index.'.color_palette',
-						'value' => $value['color_palette']
-					];
-				}
-				else if (array_key_exists('color', $value)) {
+				if (array_key_exists('color', $value)) {
 					$widget_fields[] = [
 						'type' => ZBX_WIDGET_FIELD_TYPE_STR,
 						'name' => $this->name.'.'.$index.'.color',
 						'value' => $value['color']
+					];
+				}
+				else if (array_key_exists('color_palette', $value)) {
+					$widget_fields[] = [
+						'type' => ZBX_WIDGET_FIELD_TYPE_STR,
+						'name' => $this->name.'.'.$index.'.color_palette',
+						'value' => $value['color_palette']
 					];
 				}
 			}
