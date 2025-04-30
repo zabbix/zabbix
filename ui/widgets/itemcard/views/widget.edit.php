4c21a37dbb6e82dda0e4b1ab45835b2cf4434be4
Andrejs Verza <andrejs.verza@zabbix.com>
2025-04-30 15:04:37 +0300

    ..F....... [ZBXNEXT-9319] fixed missed update to Item card widget after update to master

--- ui/widgets/itemcard/views/widget.edit.php
+++ ui/widgets/itemcard/views/widget.edit.php
@@ -38,5 +38,5 @@ use Widgets\ItemCard\Includes\CWidgetFieldItemSectionsView;
 		: null
 	)
 	->includeJsFile('widget.edit.js.php')
-	->addJavaScript('widget_form.init();')
+	->initFormJs('widget_form.init();')
 	->show();
