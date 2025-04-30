31c27daa5182951b2adbd4b2e65ef5c44145dcad
Martins Talbergs <martins.talbergs@zabbix.com>
2025-04-30 16:43:34 +0300

    ..F....... [ZBXNEXT-9827] added macro validation to wizard

--- ui/app/controllers/CControllerHostWizardCreate.php
+++ ui/app/controllers/CControllerHostWizardCreate.php
@@ -59,6 +59,84 @@ class CControllerHostWizardCreate extends CControllerHostUpdateGeneral {
 		return $this->checkAccess(CRoleHelper::UI_CONFIGURATION_HOSTS);
 	}
 
+	protected static function getValidateMacroRules(): array {
+		return ['type' => API_OBJECT, 'fields' => [
+				'type' =>				['type' => API_INT32, 'flags' => API_REQUIRED, 'in' => implode(',', [ZBX_WIZARD_FIELD_NOCONF, ZBX_WIZARD_FIELD_TEXT, ZBX_WIZARD_FIELD_LIST, ZBX_WIZARD_FIELD_CHECKBOX])],
+				'label' =>				['type' => API_MULTIPLE, 'rules' => [
+					['if' => ['field' => 'type', 'in' => implode(',', [ZBX_WIZARD_FIELD_TEXT, ZBX_WIZARD_FIELD_LIST, ZBX_WIZARD_FIELD_CHECKBOX])], 'type' => API_STRING_UTF8, 'flags' => API_REQUIRED | API_NOT_EMPTY, 'length' => DB::getFieldLength('hostmacro_config', 'label')],
+					['else' => true, 'type' => API_STRING_UTF8, 'in' => DB::getDefault('hostmacro_config', 'label')]
+				]],
+				'description' =>		['type' => API_MULTIPLE, 'rules' => [
+					['if' => ['field' => 'type', 'in' => implode(',', [ZBX_WIZARD_FIELD_TEXT, ZBX_WIZARD_FIELD_LIST, ZBX_WIZARD_FIELD_CHECKBOX])], 'type' => API_STRING_UTF8, 'length' => DB::getFieldLength('hostmacro_config', 'description')],
+					['else' => true, 'type' => API_STRING_UTF8, 'in' => DB::getDefault('hostmacro_config', 'description')]
+				]],
+				'required' =>			['type' => API_MULTIPLE, 'rules' => [
+					['if' => ['field' => 'type', 'in' => implode(',', [ZBX_WIZARD_FIELD_TEXT, ZBX_WIZARD_FIELD_LIST])], 'type' => API_INT32, 'in' => implode(',', [ZBX_WIZARD_FIELD_NOT_REQUIRED, ZBX_WIZARD_FIELD_REQUIRED])],
+					['else' => true, 'type' => API_INT32, 'in' => DB::getDefault('hostmacro_config', 'required')]
+				]],
+				'regex' =>				['type' => API_MULTIPLE, 'rules' => [
+					['if' => ['field' => 'type', 'in' => ZBX_WIZARD_FIELD_TEXT], 'type' => API_REGEX, 'length' => DB::getFieldLength('hostmacro_config', 'regex')],
+					['else' => true, 'type' => API_STRING_UTF8, 'in' => DB::getDefault('hostmacro_config', 'regex')]
+				]],
+				'options' =>			['type' => API_MULTIPLE, 'rules' => [
+					['if' => ['field' => 'type', 'in' => ZBX_WIZARD_FIELD_LIST], 'type' => API_OBJECTS, 'flags' => API_REQUIRED | API_NOT_EMPTY, 'uniq' => [['value', 'text']], 'fields' => [
+						'value' => ['type' => API_STRING_UTF8, 'flags' => API_REQUIRED],
+						'text' => ['type' => API_STRING_UTF8, 'flags' => API_REQUIRED | API_NOT_EMPTY]
+					]],
+					['if' => ['field' => 'type', 'in' => ZBX_WIZARD_FIELD_CHECKBOX], 'type' => API_OBJECTS, 'flags' => API_REQUIRED | API_NOT_EMPTY, 'length' => 1, 'fields' => [
+						'checked' => ['type' => API_STRING_UTF8, 'flags' => API_REQUIRED],
+						'unchecked' => ['type' => API_STRING_UTF8, 'flags' => API_REQUIRED]
+					]],
+					['else' => true, 'type' => API_OBJECTS, 'length' => 0]
+				]]
+			]
+		];
+	}
+
+	protected static function validateMacros(array $macros): array {
+		dump(compact('macros') + ['source' => __FUNCTION__]);
+		$errors = [];
+
+		foreach ($macros as $path => $macro) {
+			if (!CApiInputValidator::validate(self::getValidateMacroRules(), $macro['config'], $path, $error)) {
+				$errors[] = $error;
+				continue;
+			}
+
+			if ($macro['config']['regex'] && !preg_match('/'.$macro['config']['regex'].'/', $macro['value'])) {
+				$errors[] = _s('Host configuration macro "%1$s" has incorrect value.', $macro['macro']);
+			}
+		}
+
+		return $errors ? [
+			'title' => _('Cannot add host'),
+			'messages' => $errors
+		] : [];
+	}
+
+	protected static function addMacroConfig(array $macros): array {
+		$configs = [];
+		$user_macros = API::UserMacro()->get([
+			'output' => 'extend',
+			'filter' => ['macro' => array_column($macros, 'macro')]
+		]);
+
+		foreach ($user_macros as $user_macro) {
+			if (!array_key_exists('config', $user_macro)) {
+				continue;
+			}
+
+			$configs[$user_macro['macro']] = $user_macro['config'];
+		}
+
+		foreach ($macros as &$macro) {
+			$macro['config'] = $configs[$macro['macro']];
+		}
+		unset($macro);
+
+		return $macros;
+	}
+
 	protected function doAction(): void {
 		// Validate and update interfaces.
 		$address_parser = new CAddressParser(['usermacros' => true, 'lldmacros' => true, 'macros' => true]);
@@ -106,6 +184,12 @@ class CControllerHostWizardCreate extends CControllerHostUpdateGeneral {
 			}
 			else {
 				$host['tls_connect'] = HOST_ENCRYPTION_NONE;
+			}
+
+			if ($error = self::validateMacros(self::addMacroConfig($host['macros']))) {
+				$this->setResponse(new CControllerResponseData(['main_block' => json_encode(['error' => $error])]));
+
+				return;
 			}
 
 			$result = API::Host()->create($host);
