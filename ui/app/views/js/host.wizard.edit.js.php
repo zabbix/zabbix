a05a471b8ed2c1b10a1e9ab5b70e4d3ed86e3b82
Alexander Shubin <aleksandrs.subins@zabbix.com>
2025-05-08 15:54:26 +0300

    ..F....... [ZBXNEXT-9827] fixed interface port check

--- ui/app/views/js/host.wizard.edit.js.php
+++ ui/app/views/js/host.wizard.edit.js.php
@@ -924,7 +924,7 @@ window.host_wizard_edit = new class {
 				}
 
 				this.#next_button.toggleAttribute('disabled', Object.values(this.#data.interfaces)
-					.some(({address, port}) => address.trim() === '' || port.trim() === '')
+					.some(({address, port}) => address.trim() === '' || String(port).trim() === '')
 				);
 				break;
 		}
