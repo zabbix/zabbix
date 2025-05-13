da539ce466b7271a13681727046ffcc32fe2bc58
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2025-05-13 09:49:29 +0300

    ........S. [ZBX-26259] fixed formatting of the SQL query

--- src/libs/zbxdbupgrade/dbupgrade_7030.c
+++ src/libs/zbxdbupgrade/dbupgrade_7030.c
@@ -458,9 +458,8 @@ static int	DBpatch_7030033(void)
 	/* 2 - ZBX_FLAG_DISCOVERY_PROTOTYPE */
 	if (ZBX_DB_OK > zbx_db_execute("delete from item_rtdata"
 			" where exists ("
-				" select 1 from items i where"
-					" item_rtdata.itemid=i.itemid and i.flags=2"
-				")"))
+				"select null from items i where item_rtdata.itemid=i.itemid and i.flags=2"
+			")"))
 	{
 		return FAIL;
 	}
