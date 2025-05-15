6653ac9ba22ae2ad5bd7a476b700d0519939b2ab
Dmitrijs Fofanovs <dmitrijs.fofanovs@zabbix.com>
2025-05-15 19:00:08 +0300

    ..F....... [ZBX-26398] fixed code style

--- ui/js/class.widget-field.multiselect.js
+++ ui/js/class.widget-field.multiselect.js
@@ -158,7 +158,7 @@ class CWidgetFieldMultiselect extends CWidgetField {
 		this.#multiselect
 			.on('before-add', () => {
 				// Do not use this.getName(), since name can be dynamic.
-				let name = this.#multiselect.multiSelect('getOption', 'name')
+				const name = this.#multiselect.multiSelect('getOption', 'name')
 					.replace(new RegExp(`(\\[]|\\[${CWidgetBase.FOREIGN_REFERENCE_KEY}\\])$`), '');
 
 				if (this.#is_selecting_typed_reference !== this.#is_selected_typed_reference) {
