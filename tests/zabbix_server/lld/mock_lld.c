33e773161c698c68bec16fe0ebccd332dba4c007
Andris Zeila <andris.zeila@zabbix.com>
2025-05-20 15:10:19 +0300

    .......... [ZBXNEXT-1527] fixed header

--- tests/zabbix_server/lld/mock_lld.c
+++ tests/zabbix_server/lld/mock_lld.c
@@ -7,7 +7,7 @@
 ** This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 ** without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 ** See the GNU Affero General Public License for more details.
-**lld_rule_process_nested_rule
+**
 ** You should have received a copy of the GNU Affero General Public License along with this program.
 ** If not, see <https://www.gnu.org/licenses/>.
 **/
