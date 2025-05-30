9063d030839928e1d97337d6a453ea60e793c456
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2025-05-30 11:33:01 +0300

    .......P.. [ZBX-26469] increased timeouts

--- ui/tests/integration/testDiscoveryRules.php
+++ ui/tests/integration/testDiscoveryRules.php
@@ -539,7 +539,7 @@ class testDiscoveryRules extends CIntegrationTest {
 	public function testDiscoveryRules_snmpErrorViaServer(): void  {
 		$this->stopComponent(self::COMPONENT_SERVER);
 
-		sleep(300);
+		sleep(500);
 
 		self::deleteAllActions();
 		self::deleteAllDrules();
@@ -578,6 +578,11 @@ class testDiscoveryRules extends CIntegrationTest {
 
 		$this->waitForDiscoveryErr(self::SNMPAGENT_EXPECTED_INVALID_OID_ERR_MSG);
 		$this->waitForDiscovery(self::SNMPSIM_HOST_IP);
+
+		$this->stopComponent(self::COMPONENT_SERVER);
+
+		sleep(300);
+
 	}
 
 	/**
