0724007917f979787c1b3f099e24d0d726300002
Wiktor Czerneha <wiktor.czerneha@zabbix.com>
2025-06-02 09:10:12 +0200

    ..F....... [ZBX-24027] fixed broken checks and radio buttons in Network discovery configuration form

--- ui/app/views/js/configuration.discovery.edit.js.php
+++ ui/app/views/js/configuration.discovery.edit.js.php
@@ -221,7 +221,7 @@ window.drule_edit_popup = new class {
 			this.#addInputFields(input);
 		}
 
-		this.#updateRadioButtonRows(input, update, row);
+		this.#updateRadioButtonRows(input, update);
 	}
 
 	#updateCheckWarningIcon(input) {
@@ -253,44 +253,43 @@ window.drule_edit_popup = new class {
 		}
 	}
 
-	#updateRadioButtonRows(input, update, row = null) {
+	#updateRadioButtonRows(input, update) {
 		const templates = {
-			unique_template: ['#unique-row-tmpl', '#device-uniqueness-list', 'uniqueness_criteria_', 'ip'],
-			host_template: ['#host-source-row-tmpl', '#host_source', 'host_source_', 'chk_dns'],
-			name_template: ['#name-source-row-tmpl', '#name_source', 'name_source_', 'chk_host']
+			unique_template: ['unique-row-tmpl', 'device-uniqueness-list', 'uniqueness_criteria_', 'ip'],
+			host_template: ['host-source-row-tmpl', 'host_source', 'host_source_', 'chk_dns'],
+			name_template: ['name-source-row-tmpl', 'name_source', 'name_source_', 'chk_host']
 		};
 
 		const need_to_add_row = this.available_device_types.includes(parseInt(input.type));
 
-		for (const [template, list, key, def] of Object.values(templates)) {
+		for (const [template_id, list_id, key, def] of Object.values(templates)) {
 			if (need_to_add_row) {
 				if (update === false) {
-					const template_html = document.querySelector(template).innerHTML;
+					const template_html = document.getElementById(template_id).innerHTML;
 
-					document.querySelector(list)
+					document.getElementById(list_id)
 						.insertAdjacentHTML('beforeend', new Template(template_html).evaluate(input));
 				}
 				else {
-					const template_html = document.querySelector(template).innerHTML;
-					const list_item = document.querySelector(`${list} input[value$="${input.dcheckid}"]`)
-						?.closest('li');
+					const template_html = document.getElementById(template_id).innerHTML;
+					const list_item = document.getElementById(`${key + input.dcheckid}`)?.closest('li');
 
 					if (list_item) {
 						list_item.outerHTML = new Template(template_html).evaluate(input);
 					}
 					else {
-						document.querySelector(list).insertAdjacentHTML('beforeend',
+						document.getElementById(list_id).insertAdjacentHTML('beforeend',
 							new Template(template_html).evaluate(input)
 						);
 					}
 				}
 			}
 			else {
-				const dcheck_checkbox = document.querySelector(`${list} input[value$="${input.dcheckid}"]`);
+				const dcheck_checkbox = document.querySelector(`#${list_id} input[value$="${input.dcheckid}"]`);
 
 				if (dcheck_checkbox !== null) {
 					if (dcheck_checkbox.checked) {
-						document.querySelector(`#${key}${def}`).checked = true;
+						document.getElementById(`${key + def}`).checked = true;
 					}
 
 					dcheck_checkbox.closest('li')?.remove();
