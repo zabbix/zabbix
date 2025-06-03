6ec8a2fa66861b80b8a28ebf3cbd4a3cd4c534ef
Romans Trizna <romans.trizna@zabbix.com>
2025-06-03 14:58:13 +0300

    .......... [DEV-4078] added test zbx_eval_var_vector_to_dbl

--- tests/libs/zbxeval/zbx_eval_var_vector_to_dbl.c
+++ tests/libs/zbxeval/zbx_eval_var_vector_to_dbl.c
@@ -22,57 +22,100 @@
 #include "mock_eval.h"
 #include "zbxdbwrap.h"
 
-static void	extract_yaml_values(const char *path, zbx_vector_str_t *values)
+static int	compare_dbl_vectors(zbx_vector_dbl_t *v1, zbx_vector_dbl_t *v2)
 {
-	zbx_mock_handle_t	hvalues, hvalue;
-	zbx_mock_error_t	err;
-	int			value_num = 0;
+	if (v1->values_num != v2->values_num)
+		return FAIL;
 
-	hvalues = zbx_mock_get_parameter_handle(path);
-
-	while (ZBX_MOCK_END_OF_VECTOR != (err = (zbx_mock_vector_element(hvalues, &hvalue))))
+	for (int i = 0; i < v1->values_num; i++)
 	{
-		const char	*value;
+		if (fabs(v1->values[i] - v2->values[i]) > 1e-9) // Compare two doubles with a small tolerance (epsilon)
+			return FAIL;
+	}
 
-		if (ZBX_MOCK_SUCCESS != err || ZBX_MOCK_SUCCESS != (err = zbx_mock_string(hvalue, &value)))
-		{
-			value = NULL;
-			fail_msg("Cannot read value #%d: %s", value_num, zbx_mock_error_string(err));
-		}
+	return SUCCEED;
+}
 
-		char	*dup_value = zbx_strdup(NULL, value);
-		zbx_vector_str_append(values, dup_value);
+static void	extract_yaml_values_dbl(zbx_mock_handle_t hdata, zbx_vector_dbl_t *values)
+{
+	zbx_mock_error_t	err;
+	zbx_mock_handle_t	hvalue;
 
-		value_num++;
+	while (ZBX_MOCK_END_OF_VECTOR != zbx_mock_vector_element(hdata, &hvalue))
+	{
+		double	value;
+
+		if (ZBX_MOCK_SUCCESS != (err = zbx_mock_float(hvalue, &value)))
+			fail_msg("Cannot read vector member: %s", zbx_mock_error_string(err));
+
+		zbx_vector_dbl_append(values, value);
 	}
 }
 
-static int	upend_var_vector(zbx_vector_var_t *vector, zbx_vector_str_t *str_vector)
+static int	upend_var_vector(zbx_vector_str_t *str_vector, zbx_vector_var_t *input_vector)
 {
-	zbx_variant_t	variant;
+	const char		*type = zbx_mock_get_parameter_string("in.variant_type");
+	zbx_variant_t		variant;
 
-	extract_yaml_values("in.data", str_vector);
-
-	for(int i = 0; i < str_vector->values_num; i++)
+	if (SUCCEED == strcmp(type, "STR"))
 	{
+		zbx_mock_extract_yaml_values_str("in.data", str_vector);
 
+		for(int i = 0; i < str_vector->values_num; i++)
+		{
+			zbx_variant_set_str(&variant, str_vector->values[i]);
+			zbx_vector_var_append(input_vector, variant);
+		}
 	}
 
+	if (SUCCEED == strcmp(type, "UI64"))
+	{
+		zbx_vector_uint64_t	ui64_vector;
+
+		zbx_vector_uint64_create(&ui64_vector);
+		zbx_mock_extract_yaml_values_uint64(zbx_mock_get_parameter_handle("in.data"), &ui64_vector);
+
+		for(int i = 0; i < ui64_vector.values_num; i++)
+		{
+			zbx_variant_set_ui64(&variant, ui64_vector.values[i]);
+			zbx_vector_var_append(input_vector, variant);
+		}
+	}
 }
 
 void	zbx_mock_test_entry(void **state)
 {
 	zbx_vector_var_t	input_vector;
-	zbx_vector_dbl_t	output_vector;
+	zbx_vector_dbl_t	output_vector, exp_vector;
 	zbx_vector_str_t	str_vector;
 	char			*error = NULL;
+	zbx_variant_t		variant;
+	int			result,
+				exp_result = zbx_mock_str_to_return_code(zbx_mock_get_parameter_string("out.return"));
 
 	ZBX_UNUSED(state);
 
 	zbx_vector_var_create(&input_vector);
 	zbx_vector_str_create(&str_vector);
+	zbx_vector_dbl_create(&output_vector);
 
+	upend_var_vector(&str_vector, &input_vector);
+printf("input count %d\n",input_vector.values_num);
+	result = zbx_eval_var_vector_to_dbl(&input_vector, &output_vector, &error);
+printf("output count %d\n",output_vector.values_num);
+	for (int i = 0; i < output_vector.values_num; i ++)
+	{
+		printf("output %f\n", output_vector.values[i]);
+	}
+	zbx_mock_assert_int_eq("return value:", exp_result, result);
 
-	zbx_eval_var_vector_to_dbl(&input_vector, &output_vector, &error);
+	if (SUCCEED == result)
+	{
+		zbx_vector_dbl_create(&exp_vector);
+		extract_yaml_values_dbl(zbx_mock_get_parameter_handle("out.result"), &exp_vector);
+		zbx_mock_assert_int_eq("return vector compare return value:", SUCCEED,
+				compare_dbl_vectors(&output_vector,&exp_vector));
+	}
 
+	zbx_vector_str_clear_ext(&str_vector, zbx_str_free);
 }
