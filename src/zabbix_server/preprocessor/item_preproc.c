fa313dbc16170508535764a86b3f248945903ef3
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2025-06-05 10:30:59 +0300

    .......PS. [ZBX-26529] fixed memory leak when global variables are used in javascript preprocessing
    * commit 'fb4d812163583f1835a25a110b08a9857ee455af':
      .D........ [ZBX-26529] added ChangeLog entry
      .......PS. [DEV-4306] fixed memory leak when global variables are used in javascript preprocessing
    
    (cherry picked from commit e1488ac1f0a95a68fb686b3d0d48afcfb21b0374)

--- src/zabbix_server/preprocessor/item_preproc.c
+++ src/zabbix_server/preprocessor/item_preproc.c
@@ -1493,6 +1493,16 @@ static int	item_preproc_script(zbx_variant_t *value, const char *params, zbx_var
 		if (NULL != output)
 			zbx_variant_set_str(value, output);
 
+		if (ZBX_MEBIBYTE < zbx_es_total_alloc(es))
+		{
+			if (SUCCEED != zbx_es_destroy_env(es, &error))
+			{
+				zabbix_log(LOG_LEVEL_WARNING,
+						"Cannot destroy embedded scripting engine environment: %s", error);
+				zbx_free(error);
+			}
+		}
+
 		return SUCCEED;
 	}
 fail:
