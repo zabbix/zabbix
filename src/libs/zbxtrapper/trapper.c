b3230af7f32df6571cffa722c8e9ab872e9f1003
Armands Arseniuss Skolmeisters <armands.skolmeisters@zabbix.com>
2025-06-18 17:14:36 +0300

    .......PS. [DEV-4315] fixed missing macro encasing

--- src/libs/zbxtrapper/trapper.c
+++ src/libs/zbxtrapper/trapper.c
@@ -1540,8 +1540,8 @@ ZBX_THREAD_ENTRY(zbx_trapper_thread, args)
 	}
 #ifdef HAVE_NETSNMP
 out:
-#endif
 	zbx_ipc_async_socket_close(&rtc);
+#endif
 
 	zbx_setproctitle("%s #%d [terminated]", get_process_type_string(process_type), process_num);
 
