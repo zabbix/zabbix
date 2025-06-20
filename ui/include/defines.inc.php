bbfa75c770d867d35b06554d880aff27ba710937
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2025-06-20 13:40:30 +0300

    .......... [ZBXNEXT-826] updated release version to 7.0.15

--- ui/include/defines.inc.php
+++ ui/include/defines.inc.php
@@ -13,7 +13,7 @@
 ** If not, see <https://www.gnu.org/licenses/>.
 **/
 
-define('ZABBIX_VERSION',		'7.0.15rc1');
+define('ZABBIX_VERSION',		'7.0.15');
 define('ZABBIX_API_VERSION',	'7.0.15');
 define('ZABBIX_EXPORT_VERSION',	'7.0');
 
