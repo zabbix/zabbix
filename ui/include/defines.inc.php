f001fa8065b4ac76f0ed90afab0a7f6aed76164e
Natalja Romancaka <natalja.romancaka@zabbix.com>
2025-06-27 08:41:37 +0300

    .......... [ZBXNEXT-826] updated release version to 7.0.16

--- ui/include/defines.inc.php
+++ ui/include/defines.inc.php
@@ -13,8 +13,8 @@
 ** If not, see <https://www.gnu.org/licenses/>.
 **/
 
-define('ZABBIX_VERSION',		'7.0.15');
-define('ZABBIX_API_VERSION',	'7.0.15');
+define('ZABBIX_VERSION',		'7.0.16');
+define('ZABBIX_API_VERSION',	'7.0.16');
 define('ZABBIX_EXPORT_VERSION',	'7.0');
 
 define('ZABBIX_DB_VERSION',		7000000);
