e47fee3fbc552f651648d383b658b08674188144
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2025-07-10 14:57:07 +0300

    .......PS. [ZBX-26528] reimplemented
    .......PS. [ZBX-26528] reimplemented

--- src/libs/zbxodbc/odbc.c
+++ src/libs/zbxodbc/odbc.c
@@ -99,19 +99,27 @@ static int	zbx_odbc_diag(SQLSMALLINT h_type, SQLHANDLE h, SQLRETURN rc, char **d
 
 	if (SQL_ERROR == rc || SQL_SUCCESS_WITH_INFO == rc)
 	{
-		SQLCHAR		sql_state[SQL_SQLSTATE_SIZE + 1], err_msg[512];
+		SQLCHAR		sql_state[SQL_SQLSTATE_SIZE + 1];
 		SQLINTEGER	err_code = 0;
-		SQLSMALLINT	rec_nr = 1;
+		SQLSMALLINT	rec_nr = 1, msg_len;
 
-		memset(err_msg, 0, sizeof(err_msg));
-
-		while (0 != SQL_SUCCEEDED(SQLGetDiagRec(h_type, h, rec_nr++, sql_state, &err_code, err_msg,
-				sizeof(err_msg) - 1, NULL)))
+		while (0 != SQL_SUCCEEDED(SQLGetDiagRec(h_type, h, rec_nr, sql_state, &err_code, NULL, 0, &msg_len)))
 		{
-			zbx_chrcpy_alloc(&buffer, &alloc, &offset, (NULL == buffer ? ':' : '|'));
-			zbx_replace_invalid_utf8((char *)err_msg);
-			zbx_snprintf_alloc(&buffer, &alloc, &offset, "[%s][%ld][%s]", sql_state, (long)err_code,
-					err_msg);
+			char	*err_msg;
+
+			err_msg = zbx_malloc(NULL, msg_len + 1);
+
+			if (0 != SQL_SUCCEEDED(SQLGetDiagRec(h_type, h, rec_nr, sql_state, &err_code,
+					(SQLCHAR *)err_msg, msg_len + 1, NULL)))
+			{
+				zbx_chrcpy_alloc(&buffer, &alloc, &offset, (NULL == buffer ? ':' : '|'));
+				zbx_replace_invalid_utf8((char *)err_msg);
+				zbx_snprintf_alloc(&buffer, &alloc, &offset, "[%s][%ld][%s]", sql_state,
+					(long)err_code, err_msg);
+			}
+
+			zbx_free(err_msg);
+			rec_nr++;
 		}
 	}
 
