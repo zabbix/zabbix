0fc2aedcc1f0b547e9e5b122ec0b048e28f13d2b
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2025-07-24 13:11:05 +0300

    .......PS. [DEV-4405] reduced preprocessing allocations when syncing configuration

--- src/libs/zbxcacheconfig/dbconfig.c
+++ src/libs/zbxcacheconfig/dbconfig.c
@@ -10043,7 +10043,7 @@ void	zbx_dc_config_get_preprocessable_items(zbx_hashset_t *items, zbx_dc_um_shar
 		return;
 
 	zbx_vector_dc_item_ptr_create(&items_sync);
-	zbx_vector_dc_item_ptr_reserve(&items_sync, 100);
+	zbx_vector_dc_item_ptr_reserve(&items_sync, MAX(items->num_data, 100));
 
 	RDLOCK_CACHE;
 
