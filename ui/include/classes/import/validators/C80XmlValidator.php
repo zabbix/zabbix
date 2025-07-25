36fa0bc853101062ec9f0de75ffb1f360577bda1
Gregory Chalenko <gregory.chalenko@zabbix.com>
2025-07-25 15:28:46 +0300

    A.F....... [ZBXNEXT-10069] added import and export constants and support

--- ui/include/classes/import/validators/C80XmlValidator.php
+++ ui/include/classes/import/validators/C80XmlValidator.php
@@ -384,7 +384,8 @@ class C80XmlValidator extends CXmlValidatorGeneral {
 		CXmlConstantValue::LOG => CXmlConstantName::LOG,
 		CXmlConstantValue::UNSIGNED => CXmlConstantName::UNSIGNED,
 		CXmlConstantValue::TEXT => CXmlConstantName::TEXT,
-		CXmlConstantValue::BINARY => CXmlConstantName::BINARY
+		CXmlConstantValue::BINARY => CXmlConstantName::BINARY,
+		CXmlConstantValue::VALUE_TYPE_JSON => CXmlConstantName::JSON
 	];
 
 	private $TRIGGER_PRIORITY = [
