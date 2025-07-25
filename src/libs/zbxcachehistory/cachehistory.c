85d6e744e6900939abb94f272be0630d93f8d04a
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2025-07-25 10:52:13 +0300

    ........S. [ZBX-24311] fixed slow trend sync when receiving trends from present and then from past again

--- src/libs/zbxcachehistory/cachehistory.c
+++ src/libs/zbxcachehistory/cachehistory.c
@@ -744,19 +744,12 @@ void	zbx_db_flush_trends(ZBX_DC_TREND *trends, int *trends_num, zbx_vector_uint6
 		{
 			if (clock >= trend->disable_from)
 				continue;
-
-			selects_num++;
-			if (ZBX_HC_SYNC_MAX == selects_num)
-			{
-				trends_to = i + 1;
-				break;
-			}
-
-			continue;
 		}
-
-		if (NULL != trends_diff)
-			uint64_array_add(&itemids, &itemids_alloc, &itemids_num, trend->itemid, 64);
+		else
+		{
+			if (NULL != trends_diff)
+				uint64_array_add(&itemids, &itemids_alloc, &itemids_num, trend->itemid, 64);
+		}
 
 		selects_num++;
 		if (ZBX_HC_SYNC_MAX == selects_num)
