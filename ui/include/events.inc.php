52019cfddd9f7d7f1d28ff129c90be49704d6ae5
Maksym Snihach <maksym.snihach@zabbix.com>
2025-07-30 15:44:30 +0200

    ..F....... [ZBX-24885] added the tags wrapper for problems, events, added max-width for .tags-wrapper

--- ui/include/events.inc.php
+++ ui/include/events.inc.php
@@ -168,7 +168,7 @@ function make_event_details(array $event, array $allowed) {
 	$tags = makeTags([$event]);
 
 	$table
-		->addRow([_('Tags'), $tags[$event['eventid']]])
+		->addRow([_('Tags'), (new CDiv($tags[$event['eventid']]))->addClass(ZBX_STYLE_TAGS_WRAPPER)])
 		->addRow([_('Description'), (new CDiv(zbx_str2links($event['comments'])))->addClass(ZBX_STYLE_WORDBREAK)]);
 
 	if ($event['cause_eventid'] == 0) {
