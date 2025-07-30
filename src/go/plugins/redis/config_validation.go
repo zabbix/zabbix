315949982f3e52f3b2fbb7c05d41ee2908aa1713
Oskars Pucka <oskars.pucka@zabbix.com>
2025-07-30 14:42:16 +0300

    ...G...... [ZBXNEXT-7744] tls config validation and configuration improvements

--- src/go/plugins/redis/config_validation.go
+++ src/go/plugins/redis/config_validation.go
@@ -32,40 +32,23 @@ type fieldRequirement int
 
 // gives a map which describes what field combinations are allowed depending on TLSConnect value.
 func getValidationRules(tlsConnect comms.TLSConnectionType) map[comms.ConfigSetting]fieldRequirement {
+	common := map[comms.ConfigSetting]fieldRequirement{
+		comms.URI:      optional,
+		comms.User:     optional,
+		comms.Password: optional,
+
+		comms.TLSCertFile: optional,
+		comms.TLSKeyFile:  optional,
+	}
+
 	switch tlsConnect {
 	case comms.Disabled, comms.Required:
-		return map[comms.ConfigSetting]fieldRequirement{
-			comms.URI:      required,
-			comms.User:     optional,
-			comms.Password: required,
-
-			comms.TLSCAFile:   forbidden,
-			comms.TLSCertFile: forbidden,
-			comms.TLSKeyFile:  forbidden,
-		}
-	case comms.VerifyCA:
-		return map[comms.ConfigSetting]fieldRequirement{
-			comms.URI:      required,
-			comms.User:     optional,
-			comms.Password: required,
-
-			comms.TLSCAFile:   required,
-			comms.TLSCertFile: forbidden,
-			comms.TLSKeyFile:  forbidden,
-		}
-	case comms.VerifyFull:
-		return map[comms.ConfigSetting]fieldRequirement{
-			comms.URI:      required,
-			comms.User:     optional,
-			comms.Password: required,
-
-			comms.TLSCAFile:   required,
-			comms.TLSCertFile: required,
-			comms.TLSKeyFile:  required,
-		}
-	default:
-		return map[comms.ConfigSetting]fieldRequirement{}
+		common[comms.TLSCAFile] = forbidden
+	case comms.VerifyCA, comms.VerifyFull:
+		common[comms.TLSCAFile] = required
 	}
+
+	return map[comms.ConfigSetting]fieldRequirement{}
 }
 
 // Field groups that must come from the same source (session or defaults).
