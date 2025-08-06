cc8a78fc80d3c6950b7302157c697da511b1fc17
Ginta Berzina <ginta.berzina@zabbix.com>
2025-08-06 12:06:39 +0300

    ..F....... [ZBX-26463] fixed issues with maximum length when creating group in host/template edit form

--- ui/app/views/template.massupdate.php
+++ ui/app/views/template.massupdate.php
@@ -83,6 +83,7 @@ $template_tab
 				'name' => 'groups[]',
 				'object_name' => 'templateGroup',
 				'add_new' => (CWebUser::getType() == USER_TYPE_SUPER_ADMIN),
+				'maxlength' => DB::getFieldLength('hstgrp', 'name'),
 				'data' => [],
 				'popup' => [
 					'parameters' => [
