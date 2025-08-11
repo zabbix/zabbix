7c2eb6a0417f69c456e885a8bee5002c118b1eab
Maksym Snihach <maksym.snihach@zabbix.com>
2025-08-11 08:40:07 +0200

    ..F....... [ZBXNEXT-9745] moved the user property to the form array in the Script edit view

--- ui/app/views/administration.script.edit.php
+++ ui/app/views/administration.script.edit.php
@@ -473,7 +473,7 @@ $output = [
 	'dialogue_class' => 'modal-popup-large'
 ];
 
-if ($data['user']['debug_mode'] == GROUP_DEBUG_MODE_ENABLED) {
+if ($data['form']['user']['debug_mode'] == GROUP_DEBUG_MODE_ENABLED) {
 	CProfiler::getInstance()->stop();
 	$output['debug'] = CProfiler::getInstance()->make()->toString();
 }
