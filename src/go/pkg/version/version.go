40573c111594a4a96aee6c4670e4df252d278bb7
Continuous Integration <ci@zabbix.com>
2025-08-19 09:15:37 +0300

    .......... [ZBXNEXT-826] updated release date and version for 6.0.41

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -27,11 +27,11 @@ import (
 )
 
 const (
-	ZABBIX_REVDATE          = "12 August 2025"
+	ZABBIX_REVDATE          = "19 August 2025"
 	ZABBIX_VERSION_MAJOR    = 6
 	ZABBIX_VERSION_MINOR    = 0
 	ZABBIX_VERSION_PATCH    = 41
-	ZABBIX_VERSION_RC       = "rc1"
+	ZABBIX_VERSION_RC       = ""
 	ZABBIX_VERSION_RC_NUM   = "{ZABBIX_RC_NUM}"
 	ZABBIX_VERSION_REVISION = "{ZABBIX_REVISION}"
 	copyrightMessage        = "Copyright (C) 2025 Zabbix SIA\n" +
--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "12 August 2025";
+	static final String REVISION_DATE = "19 August 2025";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "6.0.41rc1";
+	static final String VERSION = "6.0.41";
 
 	static void printVersion()
 	{
