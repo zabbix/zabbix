66b8794b531f0bf6934ba192a8fe82681297a7d9
Andris Mednis <Andris.Mednis@zabbix.com>
2025-08-21 00:03:26 +0300

    ........S. [ZBX-25574] replaced negative selection (where type not in) with positive one (where type in) in validate_host()

--- src/libs/zbxdbwrap/host.c
+++ src/libs/zbxdbwrap/host.c
@@ -1115,15 +1115,15 @@ static int	validate_host(zbx_uint64_t hostid, zbx_vector_uint64_t *templateids, 
 
 		sql_offset = 0;
 
+		/* check if items from template require interfaces and */
+		/* are interfaces of required types configured for host */
 		zbx_snprintf_alloc(&sql, &sql_alloc, &sql_offset,
 				"select distinct type"
 				" from items"
-				" where type not in (%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d)"
+				" where type in (%d,%d,%d,%d,%d)"
 					" and",
-				/* item types with interface types INTERFACE_TYPE_OPT or INTERFACE_TYPE_UNKNOWN */
-				ITEM_TYPE_TRAPPER, ITEM_TYPE_INTERNAL, ITEM_TYPE_ZABBIX_ACTIVE,
-				ITEM_TYPE_HTTPTEST, ITEM_TYPE_DB_MONITOR, ITEM_TYPE_CALCULATED, ITEM_TYPE_DEPENDENT,
-				ITEM_TYPE_HTTPAGENT, ITEM_TYPE_SCRIPT, ITEM_TYPE_BROWSER, ITEM_TYPE_NESTED_LLD);
+				ITEM_TYPE_ZABBIX, ITEM_TYPE_IPMI, ITEM_TYPE_JMX, ITEM_TYPE_SNMPTRAP, ITEM_TYPE_SNMP);
+
 		zbx_db_add_condition_alloc(&sql, &sql_alloc, &sql_offset, "hostid",
 				templateids->values, templateids->values_num);
 
