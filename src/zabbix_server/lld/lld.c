a63e199cf5b444088cd91b3a904ec9cd7a6e27b5
Andris Zeila <andris.zeila@zabbix.com>
2025-08-26 15:56:17 +0300

    ........S. [ZBX-26552] removed unused variables

--- src/zabbix_server/lld/lld.c
+++ src/zabbix_server/lld/lld.c
@@ -1258,10 +1258,9 @@ out:
 int	lld_text_has_lld_macro(const char *text)
 {
 	zbx_token_t	token;
-	size_t		pos = 0, macro_len = 0;
-	int		lld_macro_num = 0;
+	int		pos = 0;
 
-	while (SUCCEED == zbx_token_find(text, (int)pos, &token, ZBX_TOKEN_SEARCH_BASIC))
+	while (SUCCEED == zbx_token_find(text, pos, &token, ZBX_TOKEN_SEARCH_BASIC))
 	{
 		if (ZBX_TOKEN_LLD_MACRO == token.type || ZBX_TOKEN_LLD_FUNC_MACRO == token.type)
 			return SUCCEED;
