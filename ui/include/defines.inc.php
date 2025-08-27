b2d0d741e85164e12aefd526d91f945b20b66911
Nikita Gogolevs <nikita.gogolevs@zabbix.com>
2025-08-27 14:32:13 +0300

    .......... [ZBXNEXT-826] updated release version to 7.0.19rc1

--- ui/include/defines.inc.php
+++ ui/include/defines.inc.php
@@ -13,8 +13,8 @@
 ** If not, see <https://www.gnu.org/licenses/>.
 **/
 
-define('ZABBIX_VERSION',		'7.0.18');
-define('ZABBIX_API_VERSION',	'7.0.18');
+define('ZABBIX_VERSION',		'7.0.19rc1');
+define('ZABBIX_API_VERSION',	'7.0.19');
 define('ZABBIX_EXPORT_VERSION',	'7.0');
 
 define('ZABBIX_DB_VERSION',		7000000);
