a1f83a2d5bbd231692df3b096fbc9a4f1dc8c623
Armands Arseniuss Skolmeisters <armands.skolmeisters@zabbix.com>
2025-09-02 11:34:41 +0300

    .......... [DEV-3695] fixed double free

--- src/libs/zbxalerter/alerter.c
+++ src/libs/zbxalerter/alerter.c
@@ -255,8 +255,6 @@ static void	alerter_process_email(zbx_ipc_socket_t *socket, zbx_ipc_message_t *i
 
 	alerter_send_result(socket, NULL, ret, (SUCCEED == ret ? NULL : error), NULL);
 
-	zbx_free(expression);
-	zbx_free(recovery_expression);
 	zbx_free(error);
 	zbx_free(inreplyto);
 	zbx_free(sendto);
