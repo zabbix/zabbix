7018b7826255407f1cefe37d40d4ea621ba074cb
Maksym Snihach <maksym.snihach@zabbix.com>
2025-09-05 11:11:44 +0200

    ..F....... [ZBX-24885] added the tags wrapper for the Item card

--- ui/widgets/hostcard/views/widget.view.php
+++ ui/widgets/hostcard/views/widget.view.php
@@ -407,7 +407,7 @@ function makeSectionTags(array $host_tags): CDiv {
 	}
 
 	if ($tags) {
-		$tags[] = (new CButtonIcon(ZBX_ICON_MORE))->setHint($tags, ZBX_STYLE_HINTBOX_WRAP . ' ' . ZBX_STYLE_TAGS_WRAPPER);
+		$tags[] = (new CButtonIcon(ZBX_ICON_MORE))->setHint($tags, ZBX_STYLE_HINTBOX_WRAP.' '.ZBX_STYLE_TAGS_WRAPPER);
 	}
 
 	return (new CDiv(
