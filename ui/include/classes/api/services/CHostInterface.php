89caaf85f76cb346599008e72d922f507dd49fb7
Artem Luchko <artem.luchko@zabbix.com>
2025-09-08 08:56:14 +0000

    A......... [ZBX-26462] updated and minimized the ability to achieve full table lock when deleting different api objects

--- ui/include/classes/api/services/CHostInterface.php
+++ ui/include/classes/api/services/CHostInterface.php
@@ -590,7 +590,6 @@ class CHostInterface extends CApiService {
 		$this->checkMainInterfacesOnDelete($interfaceids);
 
 		DB::delete('interface', ['interfaceid' => $interfaceids]);
-		DB::delete('interface_snmp', ['interfaceid' => $interfaceids]);
 
 		return ['interfaceids' => $interfaceids];
 	}
