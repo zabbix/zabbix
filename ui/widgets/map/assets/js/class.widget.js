16cbd2bb6a60ff465e9bc0924ea0d0de1d313169
Wiktor Czerneha <wiktor.czerneha@zabbix.com>
2025-09-16 13:20:53 +0200

    ..F....... [ZBX-26069] fixed onReferredUpdate method for widget Map

--- ui/widgets/map/assets/js/class.widget.js
+++ ui/widgets/map/assets/js/class.widget.js
@@ -180,7 +180,8 @@ class CWidgetMap extends CWidget {
 	}
 
 	onReferredUpdate() {
-		if (Object.keys(this.#map_svg.elements).length === 0 || this.#selected_elements_data.has(this.#sysmapid)) {
+		if (this.#map_svg === null || Object.keys(this.#map_svg.elements).length === 0
+				|| this.#selected_elements_data.has(this.#sysmapid)) {
 			return;
 		}
 
