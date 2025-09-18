a5271529c6091008f5861426e677ea9549e53fb0
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2025-09-18 15:35:56 +0300

    A......... [ZBX-26419] improved performance of problem and event views by optimizing SQL queries to join only the necessary triggers, eliminating redundant post-join filtering
    Merge in ZBX/zabbix from feature/ZBX-26419-7.0 to release/7.0
    
    * commit '1495e34213384b1d30deed172ff1f793da9aebb3':
      A......... [ZBX-26419] improved performance of problem and event views by optimizing SQL queries to join only the necessary triggers, eliminating redundant post-join filtering
      A......... [ZBX-26419] improved performance of problem and event views by optimizing SQL queries to join only the necessary triggers, eliminating redundant post-join filtering
      .D........ [ZBX-26419] fixed ChangeLog entry
      ..F....... [ZBX-26419] improved performance of problem and event views by optimizing SQL queries to join only the necessary triggers, eliminating redundant post-join filtering
      ..F....... [ZBX-26419] added style fix
      ..F....... [ZBX-26419] improved performance of problem and event views by optimizing SQL queries to join only the necessary triggers, eliminating redundant post-join filtering
      ..F....... [ZBX-26419] improved performance of problem and event views by optimizing SQL queries to join only the necessary triggers, eliminating redundant post-join filtering
      ..F....... [ZBX-26419] improved performance of problem and event views by optimizing SQL queries to join only the necessary triggers, eliminating redundant post-join filtering
      ..F....... [ZBX-26419] improved performance of problem and event views by optimizing SQL queries to join only the necessary triggers, eliminating redundant post-join filtering
      ..F....... [ZBX-26419] improved performance of problem and event views by optimizing SQL queries to join only the necessary triggers, eliminating redundant post-join filtering
      ..F....... [ZBX-26419] improved performance of problem and event views by optimizing SQL queries to join only the necessary triggers, eliminating redundant post-join filtering
      ..F....... [ZBX-26419] improved performance of problem and event views by optimizing SQL queries to join only the necessary triggers, eliminating redundant post-join filtering
      ..F....... [ZBX-26419] improved performance when viewing,counting problems or events
      ..F....... [ZBX-26419] improved performance when viewing or counting problems
    
    (cherry picked from commit 0ae47f71b85504ba6227f33532be0ee48d291494)
    (cherry picked from commit f61630a239a04e80436db4b2781d2bb19b5b7b1d)
    (cherry picked from commit 1b44437efc648478b3283c4ee130544e6e52e906)

--- ui/include/classes/api/services/CGraphItem.php
+++ ui/include/classes/api/services/CGraphItem.php
@@ -87,7 +87,7 @@ class CGraphItem extends CApiService {
 				' LEFT JOIN permission p1 ON hh1.hgsetid=p1.hgsetid'.
 					' AND p1.ugsetid=p.ugsetid'.
 				' WHERE gi.graphid=gi1.graphid'.
-					' AND p1.permission IS NULL'.
+					' AND p1.hgsetid IS NULL'.
 			')';
 		}
 
