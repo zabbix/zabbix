c0ad19b322c97c5a229c30c95e5381aeac3d7997
Georgijs Klavins <georgijs.klavins@zabbix.com>
2025-09-24 17:05:10 +0300

    ..F....... [ZBX-26548] added a comment for added timeout

--- ui/include/views/js/common.init.js.php
+++ ui/include/views/js/common.init.js.php
@@ -27,6 +27,8 @@
 
 		// the chkbxRange.init() method must be called after the inserted post scripts and initializing cookies
 		cookie.init();
+		// Timeout is added to reliably restore checkbox states, when using Back and Forward navigation
+		// in different browsers.
 		setTimeout(() => chkbxRange.init());
 	});
 
