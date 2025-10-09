3c65c7e6da498f3cf3c2d5ebabe19eb6ad3097d4
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2025-10-09 10:00:17 +0300

    .......... [DEV-4538] added debug for integration test

--- ui/tests/integration/testDiscoveryRules.php
+++ ui/tests/integration/testDiscoveryRules.php
@@ -532,8 +532,25 @@ class testDiscoveryRules extends CIntegrationTest {
 		]);
 	}
 
+
+	/**
+	* Component configuration provider for agent related tests.
+	*
+	* @return array
+	*/
+	public function configurationProvider_checkDataCollection() {
+		return [
+			self::COMPONENT_SERVER => [
+				'UnavailableDelay' => 5,
+				'UnreachableDelay' => 1,
+				'DebugLevel' => 5,
+				'LogFileSize' => 0
+			],
+		]
+
 	/**
 	 * @depends testDiscoveryRules_opDelHostTags
+	 * @configurationDataProvider configurationProvider_checkDataCollection
 	 * @required-components server
 	 */
 	public function testDiscoveryRules_snmpErrorViaServer(): void  {
