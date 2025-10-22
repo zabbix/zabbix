0933319ec88c5f828305551d351d3f827b006fbf
Ginta Berzina <ginta.berzina@zabbix.com>
2025-10-22 13:08:23 +0300

    ..F....... [ZBXNEXT-9746] fixed error when tag filter host is deleted

--- ui/app/controllers/CControllerUsergroupUpdateGeneral.php
+++ ui/app/controllers/CControllerUsergroupUpdateGeneral.php
@@ -34,23 +34,18 @@ abstract class CControllerUsergroupUpdateGeneral extends CController {
 		]);
 	}
 
-	protected function processTagFilters(array $input_tag_filters): ?array
-	{
+	protected function processTagFilters(array $input_tag_filters): array {
 		$tag_filters = [];
+
 		foreach ($input_tag_filters as $hostgroup) {
-			if (in_array($hostgroup['groupid'], array_column($this->db_hostgroups, 'groupid'))) {
-				foreach ($hostgroup['tags'] as $tag_filter) {
-					if ($hostgroup['groupid'] != 0) {
-						$tag_filters[] = [
-							'groupid' => $hostgroup['groupid'],
-							'tag' => $tag_filter['tag'],
-							'value' => $tag_filter['value']
-						];
-					}
+			foreach ($hostgroup['tags'] as $tag_filter) {
+				if ($hostgroup['groupid'] != 0) {
+					$tag_filters[] = [
+						'groupid' => $hostgroup['groupid'],
+						'tag' => $tag_filter['tag'],
+						'value' => $tag_filter['value']
+					];
 				}
-			}
-			else {
-				return null;
 			}
 		}
 
@@ -60,19 +55,19 @@ abstract class CControllerUsergroupUpdateGeneral extends CController {
 	protected function processUserGroupInputData(array &$user_group): bool {
 		$user_group['users'] = zbx_toObject($this->getInput('userids', []), 'userid');
 		$this->getInputs($user_group, ['users_status', 'gui_access', 'debug_mode', 'userdirectoryid', 'mfaid',
-			'name', 'hostgroup_rights', 'templategroup_rights'
+			'name', 'hostgroup_rights', 'templategroup_rights', 'tag_filters'
 		]);
 
-		if (!$this->checkGroupsExist($user_group['hostgroup_rights'],$this->db_hostgroups)
-				|| !$this->checkGroupsExist($user_group['templategroup_rights'], $this->db_templategroups)) {
+		if (!$this->checkGroupsExist($user_group['hostgroup_rights'], $this->db_hostgroups, true)
+				|| !$this->checkGroupsExist($user_group['templategroup_rights'], $this->db_templategroups, true)
+				|| !$this->checkGroupsExist($user_group['tag_filters'], $this->db_hostgroups, false)) {
 			CMessageHelper::addError(_('No permissions to referred object or it does not exist!'));
 
 			return false;
 		}
 		$user_group['hostgroup_rights'] = $this->processRights($user_group['hostgroup_rights']);
 		$user_group['templategroup_rights'] = $this->processRights($user_group['templategroup_rights']);
-
-		$user_group['tag_filters'] = $this->processTagFilters($this->getInput('tag_filters', []));
+		$user_group['tag_filters'] = $this->processTagFilters($user_group['tag_filters']);
 
 		if (array_key_exists('mfaid', $user_group) && $user_group['mfaid'] == -1) {
 			$user_group['mfa_status'] = GROUP_MFA_DISABLED;
@@ -85,8 +80,14 @@ abstract class CControllerUsergroupUpdateGeneral extends CController {
 		return true;
 	}
 
-	private function checkGroupsExist(array $groups, $db_groups): bool {
-		$groupids = array_merge(...array_values(array_column($groups, 'groupids')));
+	private function checkGroupsExist(array $groups, array $db_groups, bool $multi): bool {
+		if ($multi) {
+			$groupids = array_merge(...array_values(array_column($groups, 'groupids')));
+		}
+		else {
+			$groupids = array_values(array_column($groups, 'groupid'));
+		}
+
 		$existing_groupids = array_column($db_groups, 'groupid');
 
 		if (array_diff($groupids, $existing_groupids)) {
