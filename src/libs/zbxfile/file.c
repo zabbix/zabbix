4f4a2f6523f0f30a93478f437b8883421f6db30f
Vjaceslavs Smelovs <vjaceslavs.smelovs@zabbix.com>
2025-10-24 16:59:18 +0300

    .......PS. [ZBX-26018] removed zbx_alarm functions

--- src/libs/zbxfile/file.c
+++ src/libs/zbxfile/file.c
@@ -169,9 +169,6 @@ char	*zbx_fgets(char *buffer, int size, FILE *fp)
 	{
 		errno = 0;
 		s = fgets(buffer, size, fp);
-
-		if (SUCCEED == zbx_alarm_timed_out())
-			return NULL;
 	}
 	while (EINTR == errno && NULL == s);
 
