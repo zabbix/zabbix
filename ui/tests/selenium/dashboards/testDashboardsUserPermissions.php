0126640b14a519450ac2a9461ded9dbca60dbb38
Deniss Ponomarenko <deniss.ponomarenko@zabbix.com>
2025-10-30 11:08:40 -0300

    .......... [DEV-4268] added dashboard sharing checks, URL access

--- ui/tests/selenium/dashboards/testDashboardsUserPermissions.php
+++ ui/tests/selenium/dashboards/testDashboardsUserPermissions.php
@@ -16,6 +16,7 @@
 require_once __DIR__.'/../../include/CWebTest.php';
 require_once __DIR__.'/../behaviors/CMessageBehavior.php';
 require_once __DIR__.'/../../include/helpers/CDataHelper.php';
+require_once __DIR__.'/../common/testWidgets.php';
 
 /**
  * @backup hosts, hosts_groups, hosts_templates, users, dashboard_user, users_groups, dashboard
@@ -85,6 +86,13 @@ class testDashboardsUserPermissions extends CWebTest {
 	protected static $userid;
 
 	/**
+	 * ID of created dashboard.
+	 *
+	 * @var integer
+	 */
+	protected static $dashboardid;
+
+	/**
 	 * Function used to create users, template, dashboards, hosts and user groups.
 	 */
 	public static function prepareTestData() {
@@ -142,44 +150,44 @@ class testDashboardsUserPermissions extends CWebTest {
 				'name' => 'Read/Write access to template and host',
 				'templategroup_rights' => [
 					'id' => self::$template_groupids['Template group for dashboard access testing'],
-					'permission' => 3 // Read-write access.
+					'permission' => PERM_READ_WRITE
 				],
 				'hostgroup_rights' => [
 					'id' => self::$host_groupids['Host group for dashboard access testing'],
-					'permission' => 3 // Read-write access.
+					'permission' => PERM_READ_WRITE
 				]
 			],
 			[
 				'name' => 'Read access to template and host',
 				'templategroup_rights' => [
 					'id' => self::$template_groupids['Template group for dashboard access testing'],
-					'permission' => 2 // Read-only access.
+					'permission' => PERM_READ
 				],
 				'hostgroup_rights' => [
 					'id' => self::$host_groupids['Host group for dashboard access testing'],
-					'permission' => 2 // Read-only access.
+					'permission' => PERM_READ
 				]
 			],
 			[
 				'name' => 'Deny access to template and host',
 				'templategroup_rights' => [
 					'id' => self::$template_groupids['Template group for dashboard access testing'],
-					'permission' => 0 // Denied access.
+					'permission' => PERM_DENY
 				],
 				'hostgroup_rights' => [
 					'id' => self::$host_groupids['Host group for dashboard access testing'],
-					'permission' => 0 // Access denied.
+					'permission' => PERM_DENY
 				]
 			],
 			[
 				'name' => 'Read access to host, but deny from template',
 				'templategroup_rights' => [
 					'id' => self::$template_groupids['Template group for dashboard access testing'],
-					'permission' => 0 // Denied access.
+					'permission' => PERM_DENY // Denied access.
 				],
 				'hostgroup_rights' => [
 					'id' => self::$host_groupids['Host group for dashboard access testing'],
-					'permission' => 2 // Read-only access.
+					'permission' => PERM_READ
 				]
 			]
 		]);
@@ -192,9 +200,22 @@ class testDashboardsUserPermissions extends CWebTest {
 				'roleid' => USER_TYPE_SUPER_ADMIN
 			]
 		])['userids'][0];
+
+		self::$dashboardid = CDataHelper::call('dashboard.create', [
+			[
+				'name' => 'Test sharing functionality',
+				'pages' => [[]],
+				'userGroups' => [
+					[
+						'usrgrpid' => self::$user_groups['Read access to host, but deny from template'],
+						'permission' => PERM_READ
+					]
+				]
+			]
+		])['dashboardids'][0];
 	}
 
-	public static function dashboardChecks() {
+	public static function dashboardPermissions() {
 		return [
 			'Super admin with full access' => [
 				[
@@ -289,18 +310,18 @@ class testDashboardsUserPermissions extends CWebTest {
 					'user_role' => USER_TYPE_ZABBIX_USER,
 					'user_group' => 'Read access to host, but deny from template',
 					'view' => true,
-					'host' => false
+					'edit' => false
 				]
 			]
 		];
 	}
 
 	/**
-	 * @dataProvider dashboardChecks
+	 * @dataProvider dashboardPermissions
 	 */
-	public function testDashboardsUserPermissions_Dashboard($data) {
+	public function testDashboardsUserPermissions_HostDashboard($data) {
 		// Reuse and update the existing user to prevent creating 10+ separate test users during test preparation.
-		$this->updateUser($data['user_group'], $data['user_role']);
+		$this->updateUser($data['user_role'], $data['user_group']);
 
 		// Login under updated user and open host list in monitorting section.
 		$this->page->userLogin(self::USERNAME, self::PASSWORD);
@@ -309,53 +330,239 @@ class testDashboardsUserPermissions extends CWebTest {
 		)->waitUntilReady();
 
 		$table = $this->query('class:list-table')->waitUntilPresent()->asTable()->one();
-		if($data['view']){
-			$this->assertTrue($table->findRow('Name', self::HOSTNAME)->getColumn('Dashboards')->query('link',
-					'Dashboards')->one()->isClickable()
+
+		if ($data['view']) {
+			$column = $table->findRow('Name', self::HOSTNAME)->getColumn('Dashboards');
+
+			// Check dashboard counter.
+			$this->assertEquals('1', $column->query('xpath:./sup')->one()->getText());
+
+			// Open inherited dashboard.
+			$this->assertTrue($column->query('link:Dashboards')->one()->isClickable());
+			$column->query('link:Dashboards')->one()->click();
+			$this->page->waitUntilReady();
+
+			// Check dashboard name.
+			$this->assertEquals('Check user group access', $this->query('class:host-dashboard-navigation')
+					->query('xpath:.//div[@class="selected-tab"]')->one()->getText()
 			);
-			$table->findRow('Name', self::HOSTNAME)->getColumn('Dashboards')->query('link','Dashboards')->one()
-					->waitUntilClickable()->click();
-		}
-		else if (!$data['view'] && $data['user_group'] === 'Read access to host, but deny from template') {
-			$this->assertFalse($table->findRow('Name', self::HOSTNAME)->getColumn('Dashboards')->query('link',
-					'Dashboards')->one()->isClickable()
-			);
+
+			// Check that there is no editing panel.
+			$this->assertFalse($this->query('xpath:.//nav[@class="dashboard-edit"]')->exists());
 		}
 		else {
+			// No access, check that there is no host in table.
 			$this->assertFalse($table->findRow('Name', self::HOSTNAME)->isPresent());
 		}
-
-		// Check dashboard counter in table.
-			// if user group has access: click on it, check Dashboard name
-			// if denied access -> should be inactive 'Dashboards' link
 	}
 
 	/**
-	 * @dataProvider dashboardChecks
+	 * @dataProvider dashboardPermissions
 	 */
-	public function testDashboardsUserPermissions_DashboardURL($data) {
-		// Open dashboard URL
-		// If user has access, check Dashboard name
-		// If denied access, compare Error message
+	public function testDashboardsUserPermissions_HostDashboardURL($data) {
+		// Reuse and update the existing user to prevent creating 10+ separate test users during test preparation.
+		$this->updateUser($data['user_role'], $data['user_group']);
+
+		// Login under updated user and open inherited dashboard.
+		$this->page->userLogin(self::USERNAME, self::PASSWORD);
+		$this->page->open('zabbix.php?action=host.dashboard.view&hostid='.self::$hostid[self::HOSTNAME])->waitUntilReady();
+
+		if ($data['view']) {
+			// Check dashboard name.
+			$this->assertEquals('Check user group access', $this->query('class:host-dashboard-navigation')
+					->query('xpath:.//div[@class="selected-tab"]')->one()->getText()
+			);
+
+			// Check that there is no editing panel.
+			$this->assertFalse($this->query('xpath:.//nav[@class="dashboard-edit"]')->exists());
+		}
+		else {
+			$this->assertMessage(TEST_BAD, 'Access denied', 'You are logged in as "'.self::USERNAME.'". '.
+					'You have no permissions to access this page.');
+			$this->query('button:Go to "Dashboards"')->one()->waitUntilClickable()->click();
+		}
 	}
 
 	/**
-	 * @dataProvider dashboardChecks
+	 * @dataProvider dashboardPermissions
 	 */
-	public function testDashboardsUserPermissions_Edit($data) {
-		// Open page with selected template via URL
-		// If user has access, check 'Dashboards' column
-		//					   click on Dashboard, check Dashboard name, click on Edit button, check configuration form
-		// If denied access, compare Error message
+	public function testDashboardsUserPermissions_TemplateDashboardURL($data) {
+		// Reuse and update the existing user to prevent creating 10+ separate test users during test preparation.
+		$this->updateUser($data['user_role'], $data['user_group']);
+
+		// Login under updated user and open inherited dashboard.
+		$this->page->userLogin(self::USERNAME, self::PASSWORD);
+		$this->page->open('zabbix.php?action=template.dashboard.edit&dashboardid='.
+				self::$template_dashboardids['Check user group access'])->waitUntilReady();
+
+		if ($data['edit']) {
+			$this->addClockWidget();
+			// Check successfull message after dashboard update.
+			$this->page->waitUntilReady();
+			$this->assertMessage(TEST_GOOD, 'Dashboard updated');
+		}
+		else {
+			$this->assertMessage(TEST_BAD, 'Access denied', 'You are logged in as "'.self::USERNAME.'". '.
+					'You have no permissions to access this page.');
+			$this->query('button:Go to "Dashboards"')->one()->waitUntilClickable()->click();
+		}
+	}
+
+	public static function sharingPermissions() {
+		return [
+			'Super admin with write access' => [
+				[
+					'user_role' => USER_TYPE_SUPER_ADMIN,
+					'permissions' => PERM_READ_WRITE,
+					'dashboard_group' => 'Read/Write access to template and host',
+					'user_group' => 'Read/Write access to template and host',
+					'edit' => true,
+					'view' => true
+				]
+			],
+			'Super admin with read access' => [
+				[
+					'user_role' => USER_TYPE_SUPER_ADMIN,
+					'permissions' => PERM_READ,
+					'dashboard_group' => 'Read access to template and host',
+					'user_group' => 'Read access to template and host',
+					'edit' => true,
+					'view' => true
+				]
+			],
+			'Super admin with deny access' => [
+				[
+					'user_role' => USER_TYPE_SUPER_ADMIN,
+					'permissions' => PERM_READ,
+					'dashboard_group' => 'Read/Write access to template and host',
+					'user_group' => 'Read access to template and host',
+					'edit' => true,
+					'view' => true
+				]
+			],
+			'Admin with write access' => [
+				[
+					'user_role' => USER_TYPE_ZABBIX_ADMIN,
+					'permissions' => PERM_READ_WRITE,
+					'dashboard_group' => 'Read/Write access to template and host',
+					'user_group' => 'Read/Write access to template and host',
+					'edit' => true,
+					'view' => true
+				]
+			],
+			'Admin with read access' => [
+				[
+					'user_role' => USER_TYPE_ZABBIX_ADMIN,
+					'permissions' => PERM_READ,
+					'dashboard_group' => 'Read access to template and host',
+					'user_group' => 'Read access to template and host',
+					'edit' => false,
+					'view' => true
+				]
+			],
+			'Admin with deny access' => [
+				[
+					'user_role' => USER_TYPE_ZABBIX_ADMIN,
+					'permissions' => PERM_READ,
+					'dashboard_group' => 'Read/Write access to template and host',
+					'user_group' => 'Read access to template and host',
+					'view' => false
+				]
+			],
+			'User with write access' => [
+				[
+					'user_role' => USER_TYPE_ZABBIX_USER,
+					'permissions' => PERM_READ_WRITE,
+					'dashboard_group' => 'Read/Write access to template and host',
+					'user_group' => 'Read/Write access to template and host',
+					'edit' => true,
+					'view' => true
+				]
+			],
+			'User with read access' => [
+				[
+					'user_role' => USER_TYPE_ZABBIX_USER,
+					'permissions' => PERM_READ,
+					'dashboard_group' => 'Read access to template and host',
+					'user_group' => 'Read access to template and host',
+					'edit' => false,
+					'view' => true
+				]
+			],
+			'User with deny access' => [
+				[
+					'user_role' => USER_TYPE_ZABBIX_USER,
+					'permissions' => PERM_READ,
+					'dashboard_group' => 'Read/Write access to template and host',
+					'user_group' => 'Read access to template and host',
+					'view' => false
+				]
+			]
+		];
 	}
 
 	/**
-	 * Update user role and group via API request.
+	 * Verify user access to the dashboard through Sharing.
+	 *
+	 * @dataProvider sharingPermissions
+	 */
+	public function testDashboardsUserPermissions_DashboardShare($data) {
+		// Reuse and update the existing user to prevent creating 10+ separate test users during test preparation.
+		$this->updateUser($data['user_role'], $data['user_group']);
+
+		// Update dashboard permissions.
+		$this->updateDashboardAccess($data['permissions'], $data['dashboard_group']);
+
+		// Login under updated user and open inherited dashboard.
+		$this->page->userLogin(self::USERNAME, self::PASSWORD);
+		$this->page->open('zabbix.php?action=dashboard.view&dashboardid='.self::$dashboardid)->waitUntilReady();
+
+		if ($data['view']) {
+			// Check dashboard name.
+			$this->assertEquals('Test sharing functionality', $this->query('id:page-title-general')->one()->getText());
+
+			if ($data['edit']) {
+				$this->addClockWidget('global');
+				// Check successfull message after dashboard update.
+				$this->page->waitUntilReady();
+				$this->assertMessage(TEST_GOOD, 'Dashboard updated');
+			}
+			else {
+				$this->assertFalse($this->query('xpath:.//nav[@class="dashboard-edit"]')->one()->isClickable());
+			}
+		}
+		else {
+			$this->assertMessage(TEST_BAD, 'No permissions to referred object or it does not exist!');
+		}
+	}
+
+	/**
+	 * Change dashboard permissions using API request.
+	 *
+	 * @param integer $permissions      read or write
+	 * @param integer $group            user group ID
+	 */
+	public static function updateDashboardAccess($permissions, $group) {
+		CDataHelper::call('dashboard.update', [
+			[
+				'dashboardid' => self::$dashboardid,
+				'userGroups' => [
+					[
+						'usrgrpid' => self::$user_groups[$group],
+						'permission' => $permissions
+					]
+				]
+			]
+		]);
+	}
+
+	/**
+	 * Change user role and group via API request for existing user.
 	 *
 	 * @param string  $group             user group name
 	 * @param integer $password          user role ID
 	 */
-	public static function updateUser($group, $role) {
+	public static function updateUser($role, $group = 'Read/Write access to template and host') {
 		CDataHelper::call('user.update', [
 			[
 				'userid' => self::$userid,
@@ -365,5 +572,27 @@ class testDashboardsUserPermissions extends CWebTest {
 				'roleid' => $role
 			]
 		]);
+	}
+
+	/**
+	 * Add 'Clock' widget to test edit functionality.
+	 *
+	 * @param string  $source		flag for additional edit() click
+	 */
+	public static function addClockWidget($source = 'template') {
+		$dashboard = CDashboardElement::find()->one()->waitUntilVisible();
+
+		if ($source === 'global'){
+			$dashboard->edit()->addWidget()->asForm();
+		}
+		else {
+			$dashboard->addWidget()->asForm();
+		}
+
+		$dialog = COverlayDialogElement::find()->one()->asForm();
+		$dialog->fill(['Type' => CFormElement::RELOADABLE_FILL('Clock')]);
+		$dialog->submit();
+
+		$dashboard->getControls()->query('id:dashboard-save')->one()->click();
 	}
 }
