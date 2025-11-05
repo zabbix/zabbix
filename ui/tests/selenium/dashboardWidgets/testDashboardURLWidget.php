2d639f84ce316a670a8c9d9d0f2e8c9728fae811
Natalja Romancaka <natalja.romancaka@zabbix.com>
2025-11-05 17:05:33 +0200

    .......... [ZBXNEXT-686] updated Selenium test, added workaround for unstable hoverMouse in testDashboardURLWidget

--- ui/tests/selenium/dashboardWidgets/testDashboardURLWidget.php
+++ ui/tests/selenium/dashboardWidgets/testDashboardURLWidget.php
@@ -356,7 +356,6 @@ class testDashboardURLWidget extends CWebTest {
 				[
 					'expected' => TEST_GOOD,
 					'fields' => [
-						'Show header' => false,
 						'Refresh interval' => '15 minutes',
 						'URL' => 'tel://zabbix.com'
 					]
@@ -366,7 +365,7 @@ class testDashboardURLWidget extends CWebTest {
 				[
 					'expected' => TEST_GOOD,
 					'fields' => [
-						'Show header' => true,
+						'Show header' => false,
 						'Refresh interval' => 'No refresh',
 						'URL' => 'ssh://zabbix.com'
 					]
