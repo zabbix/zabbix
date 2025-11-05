55b409a93bcb03305cd4434386735f798b931217
Vjaceslavs Smelovs <vjaceslavs.smelovs@zabbix.com>
2025-11-05 17:00:56 +0200

    ........S. [ZBX-27032] added CURLOPT_NOBODY reset

--- src/libs/zbxembed/httprequest.c
+++ src/libs/zbxembed/httprequest.c
@@ -389,10 +389,15 @@ static duk_ret_t	es_httprequest_query(duk_context *ctx, const char *http_request
 	{
 		ZBX_CURL_SETOPT(ctx, request->handle, CURLOPT_NOBODY, 1L, err);
 	}
-	else if (0 != contents_len)
+	else
 	{
-		ZBX_CURL_SETOPT(ctx, request->handle, CURLOPT_POSTFIELDS, contents, err);
-		ZBX_CURL_SETOPT(ctx, request->handle, CURLOPT_POSTFIELDSIZE, (long)contents_len, err);
+		ZBX_CURL_SETOPT(ctx, request->handle, CURLOPT_NOBODY, 0L, err);
+
+		if (0 != contents_len)
+		{
+			ZBX_CURL_SETOPT(ctx, request->handle, CURLOPT_POSTFIELDS, contents, err);
+			ZBX_CURL_SETOPT(ctx, request->handle, CURLOPT_POSTFIELDSIZE, (long)contents_len, err);
+		}
 	}
 
 	ZBX_CURL_SETOPT(ctx, request->handle, CURLOPT_ACCEPT_ENCODING, "", err);
