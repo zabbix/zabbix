2d2ead27b5ac414f3212fb3e4a4709dcf7081d13
Kirils Seve <kirils.seve@zabbix.com>
2025-11-11 16:54:15 +0200

    .......... [DEV-4614] added data source with host groups, hosts and items

---
+++ ui/tests/selenium/data/sources/MonitoringOverview.php
@@ -0,0 +1,227 @@
+<?php
+/*
+** Copyright (C) 2001-2025 Zabbix SIA
+**
+** This program is free software: you can redistribute it and/or modify it under the terms of
+** the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+**
+** This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+** without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU Affero General Public License for more details.
+**
+** You should have received a copy of the GNU Affero General Public License along with this program.
+** If not, see <https://www.gnu.org/licenses/>.
+**/
+
+
+class MonitoringOverview {
+	public static function load() {
+		CDataHelper::call('hostgroup.create', [
+			['name' => 'Group to check Overview'],
+			['name' => 'Another group to check Overview']
+		]);
+		$groupids = CDataHelper::getIds('name');
+
+		// Create hosts with interfaces.
+		$host = CDataHelper::createHosts([
+			[
+				'host' => '1_Host_to_check_Monitoring_Overview',
+				'name' => '1_Host_to_check_Monitoring_Overview',
+				'status' => HOST_STATUS_MONITORED,
+				'groups' => [
+					'groupid' => $groupids['Group to check Overview']
+				],
+				'items' => [
+					[
+						'name' => '1_item',
+						'key_' => 'trap[1]',
+						'type' => ITEM_TYPE_TRAPPER,
+						'value_type' => ITEM_VALUE_TYPE_UINT64,
+						'history' => '90d',
+						'status' => ITEM_STATUS_ACTIVE,
+						'tags' => [
+							[
+								'tag' => 'DataBase',
+								'value' => 'mysql'
+							]
+						]
+					],
+					[
+						'name' => '2_item',
+						'key_' => 'trap[2]',
+						'type' => ITEM_TYPE_TRAPPER,
+						'value_type' => ITEM_VALUE_TYPE_UINT64,
+						'history' => '90d',
+						'status' => ITEM_STATUS_ACTIVE,
+						'tags' => [
+							[
+								'tag' => 'DataBase',
+								'value' => 'PostgreSQL'
+							]
+						]
+					]
+				],
+				'interfaces' => [
+					[
+						'type' => INTERFACE_TYPE_AGENT,
+						'useip' => INTERFACE_USE_IP,
+						'main' => INTERFACE_PRIMARY,
+						'ip' => '127.0.0.1',
+						'dns' => '',
+						'port' => '10050'
+					]
+				]
+			],
+			[
+				'host' => '3_Host_to_check_Monitoring_Overview',
+				'name' => '3_Host_to_check_Monitoring_Overview',
+				'status' => HOST_STATUS_MONITORED,
+				'groups' => [
+					'groupid' => $groupids['Group to check Overview']
+				],
+				'interfaces' => [
+					[
+						'type' => INTERFACE_TYPE_AGENT,
+						'useip' => INTERFACE_USE_IP,
+						'main' => INTERFACE_PRIMARY,
+						'ip' => '127.0.0.1',
+						'dns' => '',
+						'port' => '10050'
+					]
+				],
+				'items' => [
+					[
+						'name' => '3_item',
+						'key_' => 'trap[3]',
+						'type' => ITEM_TYPE_TRAPPER,
+						'value_type' => ITEM_VALUE_TYPE_UINT64,
+						'history' => '90d',
+						'status' => ITEM_STATUS_ACTIVE,
+						'tags' => [
+							[
+								'tag' => 'DataBase',
+								'value' => 'Oracle'
+							]
+						]
+					]
+				],
+				'inventory_mode' => HOST_INVENTORY_MANUAL,
+				'inventory' => [
+					'type' => 'Type',
+					'type_full' => 'Type (Full details)',
+					'name' => 'Name',
+					'alias' => 'Alias',
+					'os' => 'OS',
+					'os_full' => 'OS (Full details)',
+					'os_short' => 'OS (Short)',
+					'serialno_a' => 'Serial number A',
+					'serialno_b' => 'Serial number B',
+					'tag' => 'Tag',
+					'asset_tag' => 'Asset tag',
+					'macaddress_a' => 'MAC address A',
+					'macaddress_b' => 'MAC address B',
+					'hardware' => 'Hardware',
+					'hardware_full' => 'Hardware (Full details)',
+					'software' => 'Software',
+					'software_full' => 'Software (Full details)',
+					'software_app_a' => 'Software application A',
+					'software_app_b' => 'Software application B',
+					'software_app_c' => 'Software application C',
+					'software_app_d' => 'Software application D',
+					'software_app_e' => 'Software application E',
+					'contact' => 'Contact',
+					'location' => 'Location',
+					'location_lat' => 'Location latitud',
+					'location_lon' => 'Location longitu',
+					'notes' => 'Notes',
+					'chassis' => 'Chassis',
+					'model' => 'Model',
+					'hw_arch' => 'HW architecture',
+					'vendor' => 'Vendor',
+					'contract_number' => 'Contract number',
+					'installer_name' => 'Installer name',
+					'deployment_status' => 'Deployment status',
+					'url_a' => 'URL A',
+					'url_b' => 'URL B',
+					'url_c' => 'URL C',
+					'host_networks' => 'Host networks',
+					'host_netmask' => 'Host subnet mask',
+					'host_router' => 'Host router',
+					'oob_ip' => 'OOB IP address',
+					'oob_netmask' => 'OOB subnet mask',
+					'oob_router' => 'OOB router',
+					'date_hw_purchase' => 'Date HW purchased',
+					'date_hw_install' => 'Date HW installed',
+					'date_hw_expiry' => 'Date HW maintenance expires',
+					'date_hw_decomm' => 'Date hw decommissioned',
+					'site_address_a' => 'Site address A',
+					'site_address_b' => 'Site address B',
+					'site_address_c' => 'Site address C',
+					'site_city' => 'Site city',
+					'site_state' => 'Site state / province',
+					'site_country' => 'Site country',
+					'site_zip' => 'Site ZIP / postal',
+					'site_rack' => 'Site rack location',
+					'site_notes' => 'Site notes',
+					'poc_1_name' => 'Primary POC name',
+					'poc_1_email' => 'Primary POC email',
+					'poc_1_phone_a' => 'Primary POC phone A',
+					'poc_1_phone_b' => 'Primary POC phone B',
+					'poc_1_cell' => 'Primary POC cell',
+					'poc_1_screen' => 'Primary POC screen name',
+					'poc_1_notes' => 'Primary POC notes',
+					'poc_2_name' => 'Secondary POC name',
+					'poc_2_email' => 'Secondary POC email',
+					'poc_2_phone_a' => 'Secondary POC phone A',
+					'poc_2_phone_b' => 'Secondary POC phone B',
+					'poc_2_cell' => 'Secondary POC cell',
+					'poc_2_screen' => 'Secondary POC screen name',
+					'poc_2_notes' => 'Secondary POC notes'
+				]
+			],
+			[
+				'host' => '4_Host_to_check_Monitoring_Overview',
+				'name' => '4_Host_to_check_Monitoring_Overview',
+				'status' => HOST_STATUS_MONITORED,
+				'groups' => [
+					'groupid' => $groupids['Another group to check Overview']
+				],
+				'items' => [
+					[
+						'name' => '4_item',
+						'key_' => 'trap[4]',
+						'type' => ITEM_TYPE_TRAPPER,
+						'value_type' => ITEM_VALUE_TYPE_UINT64,
+						'history' => '90d',
+						'status' => ITEM_STATUS_ACTIVE,
+						'units' => 'UNIT',
+						'tags' => [
+							[
+								'tag' => 'DataBase',
+								'value' => 'Oracle DB'
+							]
+						]
+					]
+				],
+				'interfaces' => [
+					[
+						'type' => INTERFACE_TYPE_AGENT,
+						'useip' => INTERFACE_USE_IP,
+						'main' => INTERFACE_PRIMARY,
+						'ip' => '127.0.0.1',
+						'dns' => '',
+						'port' => '10050'
+					]
+				]
+			]
+		]);
+		$itemids = CDataHelper::getIds('name');
+
+
+		return [
+			'groupids' => $groupids,
+			'hostids' => $host['hostids'],
+			'itemids' => $itemids
+		];
+	}
+}
