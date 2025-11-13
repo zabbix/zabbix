8adcb4b2bd304c4565a47317a0edc5e742b73312
Martins Talbergs <martins.talbergs@zabbix.com>
2025-11-13 12:10:07 +0200

    ..F....... [ZBXNEXT-9754] fixed static method call

--- ui/app/controllers/CControllerProxyGroupCreate.php
+++ ui/app/controllers/CControllerProxyGroupCreate.php
@@ -35,7 +35,7 @@ class CControllerProxyGroupCreate extends CController {
 	}
 
 	protected function checkInput(): bool {
-		$ret = $this->validateInput($this->getValidationRules());
+		$ret = $this->validateInput(self::getValidationRules());
 
 		if (!$ret) {
 			$form_errors = $this->getValidationError();
--- ui/app/controllers/CControllerProxyGroupUpdate.php
+++ ui/app/controllers/CControllerProxyGroupUpdate.php
@@ -36,7 +36,7 @@ class CControllerProxyGroupUpdate extends CController {
 	}
 
 	protected function checkInput(): bool {
-		$ret = $this->validateInput($this->getValidationRules());
+		$ret = $this->validateInput(self::getValidationRules());
 
 		if (!$ret) {
 			$form_errors = $this->getValidationError();
