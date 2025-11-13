366a5264382b0b922a0e3d0c5b1e1da862e5a9c8
Martins Talbergs <martins.talbergs@zabbix.com>
2025-11-13 12:07:48 +0200

    ..F....... [ZBXNEXT-9754] using native integer validation rule attributes

--- ui/app/controllers/CControllerProxyGroupUpdate.php
+++ ui/app/controllers/CControllerProxyGroupUpdate.php
@@ -30,10 +30,7 @@ class CControllerProxyGroupUpdate extends CController {
 			'failover_delay' => ['db proxy_group.failover_delay', 'required', 'not_empty',
 				'use' => [CTimeUnitValidator::class, ['min' => 10, 'max' => 15 * SEC_PER_MIN, 'usermacros' => true]]
 			],
-			'min_online' => ['integer', 'required',
-				// TODO: Uncomment once DEV-4248 is merged.
-				/* 'use' => [CNumberValidator::class, ['min' => 0, 'max' => 1000]] */
-			],
+			'min_online' => ['integer', 'required', 'min' => 0, 'max' => 1000],
 			'description' => ['db proxy_group.description']
 		]];
 	}
