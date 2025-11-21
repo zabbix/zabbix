849ef2676e8e57f5c155eb29a48763560094a176
Janis Freibergs <janis.freibergs@zabbix.com>
2025-11-21 10:01:58 +0200

    A...I..... [ZBX-25284] transferred changes and adjusted optional upgrade patch from minor branch

--- src/libs/zbxdbupgrade/dbupgrade_7040.c
+++ src/libs/zbxdbupgrade/dbupgrade_7040.c
@@ -14,6 +14,8 @@
 
 #include "dbupgrade.h"
 
+#include "zbxdb.h"
+
 /*
  * 7.4 maintenance database patches
  */
@@ -25,6 +27,146 @@ static int	DBpatch_7040000(void)
 	return SUCCEED;
 }
 
+static int	DBpatch_7040001(void)
+{
+	/* 2 - ZBX_FLAG_DISCOVERY_PROTOTYPE */
+	/* 6 - ZBX_FLAG_DISCOVERY_PROTOTYPE_CREATED */
+	if (ZBX_DB_OK > zbx_db_execute(
+			"delete from httptestitem"
+				" where httptestid in ("
+					" select ht.httptestid from hosts h,httptest ht"
+					" where h.hostid=ht.hostid and h.flags in (2,6)"
+				")"))
+	{
+		return FAIL;
+	}
+
+	return SUCCEED;
+}
+
+static int	DBpatch_7040002(void)
+{
+	/* 2 - ZBX_FLAG_DISCOVERY_PROTOTYPE */
+	/* 6 - ZBX_FLAG_DISCOVERY_PROTOTYPE_CREATED */
+	if (ZBX_DB_OK > zbx_db_execute(
+			"delete from httpstepitem"
+				" where httpstepid in ("
+					" select hts.httpstepid"
+					" from hosts h,httptest ht,httpstep hts"
+					" where h.hostid=ht.hostid"
+						" and ht.httptestid=hts.httptestid and h.flags in (2,6)"
+				")"))
+	{
+		return FAIL;
+	}
+
+	return SUCCEED;
+}
+
+static int	DBpatch_7040003(void)
+{
+	/* 2 - ZBX_FLAG_DISCOVERY_PROTOTYPE */
+	/* 6 - ZBX_FLAG_DISCOVERY_PROTOTYPE_CREATED */
+	if (ZBX_DB_OK > zbx_db_execute(
+			"delete from item_tag"
+				" where itemid in ("
+					" select i.itemid from hosts h,items i"
+					" where h.hostid=i.hostid and h.flags in (2,6)"
+				")"))
+	{
+		return FAIL;
+	}
+
+	return SUCCEED;
+}
+
+static int	DBpatch_7040004(void)
+{
+	/* 2 - ZBX_FLAG_DISCOVERY_PROTOTYPE */
+	/* 6 - ZBX_FLAG_DISCOVERY_PROTOTYPE_CREATED */
+	if (ZBX_DB_OK > zbx_db_execute(
+			"delete from items"
+				" where itemid in ("
+					" select i.itemid from hosts h,items i"
+					" where h.hostid=i.hostid and h.flags in (2,6)"
+				")"))
+	{
+		return FAIL;
+	}
+
+	return SUCCEED;
+}
+
+static int	DBpatch_7040005(void)
+{
+	/* 2 - ZBX_FLAG_DISCOVERY_PROTOTYPE */
+	/* 6 - ZBX_FLAG_DISCOVERY_PROTOTYPE_CREATED */
+	if (ZBX_DB_OK > zbx_db_execute(
+			"delete from httpstep_field"
+				" where httpstepid in ("
+					" select hts.httpstepid"
+					" from hosts h,httptest ht,httpstep hts"
+					" where h.hostid=ht.hostid"
+						" and ht.httptestid=hts.httptestid and h.flags in (2,6)"
+				")"))
+	{
+		return FAIL;
+	}
+
+	return SUCCEED;
+}
+
+static int	DBpatch_7040006(void)
+{
+	/* 2 - ZBX_FLAG_DISCOVERY_PROTOTYPE */
+	/* 6 - ZBX_FLAG_DISCOVERY_PROTOTYPE_CREATED */
+	if (ZBX_DB_OK > zbx_db_execute(
+			"delete from httpstep"
+				" where httptestid in ("
+					" select ht.httptestid from hosts h,httptest ht"
+					" where h.hostid=ht.hostid and h.flags in (2,6)"
+				")"))
+	{
+		return FAIL;
+	}
+
+	return SUCCEED;
+}
+
+static int	DBpatch_7040007(void)
+{
+	/* 2 - ZBX_FLAG_DISCOVERY_PROTOTYPE */
+	/* 6 - ZBX_FLAG_DISCOVERY_PROTOTYPE_CREATED */
+	if (ZBX_DB_OK > zbx_db_execute(
+			"delete from httptest_field"
+				" where httptestid in ("
+					" select ht.httptestid from hosts h,httptest ht"
+					" where h.hostid=ht.hostid and h.flags in (2,6)"
+				")"))
+	{
+		return FAIL;
+	}
+
+	return SUCCEED;
+}
+
+static int	DBpatch_7040008(void)
+{
+	/* 2 - ZBX_FLAG_DISCOVERY_PROTOTYPE */
+	/* 6 - ZBX_FLAG_DISCOVERY_PROTOTYPE_CREATED */
+	if (ZBX_DB_OK > zbx_db_execute(
+			"delete from httptest"
+				" where httptestid in ("
+					" select ht.httptestid from httptest ht,hosts h"
+					" where ht.hostid=h.hostid and h.flags in (2,6)"
+				")"))
+	{
+		return FAIL;
+	}
+
+	return SUCCEED;
+}
+
 #endif
 
 DBPATCH_START(7040)
@@ -32,5 +174,13 @@ DBPATCH_START(7040)
 /* version, duplicates flag, mandatory flag */
 
 DBPATCH_ADD(7040000, 0, 1)
+DBPATCH_ADD(7040001, 0, 0)
+DBPATCH_ADD(7040002, 0, 0)
+DBPATCH_ADD(7040003, 0, 0)
+DBPATCH_ADD(7040004, 0, 0)
+DBPATCH_ADD(7040005, 0, 0)
+DBPATCH_ADD(7040006, 0, 0)
+DBPATCH_ADD(7040007, 0, 0)
+DBPATCH_ADD(7040008, 0, 0)
 
 DBPATCH_END()
