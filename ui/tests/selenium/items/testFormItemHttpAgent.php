2af2d8b5da2c1efb94292ca1ebd6e9ef0f4b5122
Deniss Ponomarenko <deniss.ponomarenko@zabbix.com>
2025-11-27 08:07:31 -0300

    .......... [ZBXNEXT-9595] rewrote the query for the broken screenshot in the item form for the HTTP agent

--- ui/tests/selenium/items/testFormItemHttpAgent.php
+++ ui/tests/selenium/items/testFormItemHttpAgent.php
@@ -944,8 +944,12 @@ class testFormItemHttpAgent extends CLegacyWebTest {
 		// Take a screenshot to test draggable object position of query and headers fields.
 		if (array_key_exists('screenshot', $data)) {
 			$this->page->removeFocus();
-			$this->assertScreenshot($form->query('id:query-fields-table')->one(), 'Query fields');
-			$this->assertScreenshot($form->query('id:headers-table')->one(), 'Headers fields');
+
+			foreach (['js-item-query-fields-field' => 'Query fields', 'js-item-headers-field' => 'Headers fields'] as $id => $name) {
+				$this->assertScreenshot($this->query('xpath://div[@id='. CXPathHelper::escapeQuotes($id).']/div')->one(),
+						$name
+				);
+			}
 		}
 
 		if (array_key_exists('request_type', $data)) {
