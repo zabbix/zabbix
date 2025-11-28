d6d78d36a879aab0655bd542966a8e82cb436613
Ginta Berzina <ginta.berzina@zabbix.com>
2025-11-28 14:47:00 +0200

    ..F....... [ZBXNEXT-9744] added menu enrty url validation

--- ui/app/controllers/CControllerMediatypeCreate.php
+++ ui/app/controllers/CControllerMediatypeCreate.php
@@ -203,6 +203,7 @@ class CControllerMediatypeCreate extends CControllerMediatypeUpdateGeneral {
 				'when' => ['type', 'in' => [MEDIA_TYPE_WEBHOOK]]
 			],
 			'event_menu_url' =>	['db media_type.event_menu_url', 'required', 'not_empty',
+				'use' => [CUrlValidator::class],
 				'when' => [
 					['type', 'in' => [MEDIA_TYPE_WEBHOOK]],
 					['show_event_menu', 'in' => [ZBX_EVENT_MENU_SHOW]]
