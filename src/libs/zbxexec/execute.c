09d4201d3dda44d06037d04fcd79bbdc7cdbc3a7
Romans Trizna <romans.trizna@zabbix.com>
2025-12-09 15:12:28 +0200

    ...G....S. [ZBX-27227] fixed style for frontend message

--- src/libs/zbxexec/execute.c
+++ src/libs/zbxexec/execute.c
@@ -515,10 +515,11 @@ close:
 		}
 		else if (MAX_EXECUTE_OUTPUT_LEN <= offset + rc)
 		{
-			zbx_snprintf(error, max_error_len, "command output exceeded limit of %d KB",
+			zbx_snprintf(error, max_error_len, "Command output exceeded limit of %d KB",
 					MAX_EXECUTE_OUTPUT_LEN / ZBX_KIBIBYTE);
 
-			zabbix_log(LOG_LEVEL_ERR, "%s", error);
+			zabbix_log(LOG_LEVEL_ERR, "command output exceeded limit of %d KB",
+					MAX_EXECUTE_OUTPUT_LEN / ZBX_KIBIBYTE);
 		}
 		else if (0 == WIFEXITED(status) || (ZBX_EXIT_CODE_CHECKS_ENABLED == flag && 0 != WEXITSTATUS(status)))
 		{
