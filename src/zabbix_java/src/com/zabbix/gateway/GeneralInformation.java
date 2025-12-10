d6945823c94c24658d2fe2042baa91d623a2a6cf
Kirils Seve <kirils.seve@zabbix.com>
2025-12-10 16:46:42 +0200

    .......... [ZBXNEXT-826] updated release version to 7.0.22rc2

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -19,7 +19,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "9 December 2025";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "7.0.22rc1";
+	static final String VERSION = "7.0.22rc2";
 
 	static void printVersion()
 	{
