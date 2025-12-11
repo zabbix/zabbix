3bb0a2baae4763aef8bf88b014841629966d519d
Armands Arseniuss Skolmeisters <armands.skolmeisters@zabbix.com>
2025-12-11 18:44:29 +0200

    .......PS. [DEV-4676] fixed regex match

--- src/libs/zbxregexp/zbxregexp.c
+++ src/libs/zbxregexp/zbxregexp.c
@@ -748,6 +748,11 @@ char	*zbx_regexp_match(const char *string, const char *pattern, int *len)
 	return zbx_regexp(string, pattern, ZBX_REGEXP_MULTILINE, len);
 }
 
+char	*zbx_regexp_match_full(const char *string, const char *pattern, int *len)
+{
+	return zbx_regexp(string, pattern, 0, len);
+}
+
 /******************************************************************************
  *                                                                            *
  * Purpose: zbx_strncpy_alloc with maximum allocated memory limit.            *
