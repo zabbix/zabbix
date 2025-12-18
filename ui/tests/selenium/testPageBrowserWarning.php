3e8454ff35deda6496460590959ab73f3673f317
Glebs Mirosnikovs <glebs.mirosnikovs@zabbix.com>
2025-12-18 14:13:23 +0200

    .......... [DEV-4680] testPageBrowserWarning format and assertScreenshot added

--- ui/tests/selenium/testPageBrowserWarning.php
+++ ui/tests/selenium/testPageBrowserWarning.php
@@ -15,23 +15,23 @@
 
 require_once __DIR__.'/../include/CWebTest.php';
 
-//Test checkes  /browserwarning.php  web page.
+//Test checks  /browserwarning.php  web page.
 
 class testPageBrowserWarning extends CWebTest {
 
 	public function testPageBrowserWarning_CheckLayout() {
 		$this->page->login()->open('browserwarning.php')->waitUntilReady();
-		$this->page->assertTitle('You are using an outdated browser.');
+		$this->assertEquals('You are using an outdated browser.', $this->page->getTitle());
 
-		//Assert text - 1st paragraph
-		$this->assertEquals("Zabbix frontend is built on advanced, modern technologies and does not support old browsers. It is highly recommended that you choose and install a modern browser. It is free of charge and only takes a couple of minutes.",
+		// Assert text - 1st paragraph.
+		$this->assertEquals('Zabbix frontend is built on advanced, modern technologies and does not support old browsers. It is highly recommended that you choose and install a modern browser. It is free of charge and only takes a couple of minutes.',
 		$this->query('css:.browser-warning-container > p:nth-of-type(1)')->one()->getText());
 
-		//Assert text -  2nd paragraph
-		$this->assertEquals("New browsers usually come with support for new technologies, increasing web page speed, better privacy settings and so on. They also resolve security and functional issues.",
+		// Assert text -  2nd paragraph.
+		$this->assertEquals('New browsers usually come with support for new technologies, increasing web page speed, better privacy settings and so on. They also resolve security and functional issues.',
 		$this->query('css:.browser-warning-container > p:nth-of-type(2)')->one()->getText());
 
-		//Check, that links are clickable
+		// Check, that links are clickable.
 		$links = [
 			'Google Chrome' => 'http://www.google.com/chrome',
 			'Mozilla Firefox' => 'http://www.mozilla.org/firefox',
@@ -39,11 +39,14 @@ class testPageBrowserWarning extends CWebTest {
 			'Opera browser' => 'http://www.opera.com/download',
 			'Apple Safari' => 'http://www.apple.com/safari/download'
 		];
-		foreach($links as $key => $item){
-		$this->assertEquals($this->query("link:$key")->one()->getAttribute('href'), $item);
+		foreach ($links as $key => $item) {
+			$this->assertEquals($this->query('link:' .$key)->one()->getAttribute('href'), $item);
 		}
 
-		//Check, that user can go to Zabbix page
+		// Screenshot comparison
+		$this->assertScreenshot($this->query('css:.browser-warning-container')->one());
+
+		// Check, that user can go to Zabbix page.
 		$this->query('link:Continue despite this warning')->one()->click();
 		$this->query('css:#dashboard')->one()->waitUntilPresent();
 		$this->page->assertTitle('Dashboard');
