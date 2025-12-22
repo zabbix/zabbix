12180498c2bd3b04e840f25a24193319d9b3d485
Glebs Mirosnikovs <glebs.mirosnikovs@zabbix.com>
2025-12-22 11:05:21 +0200

    .......... [DEV-4680] fixed the code after review

--- ui/tests/selenium/testPageBrowserWarning.php
+++ ui/tests/selenium/testPageBrowserWarning.php
@@ -13,25 +13,28 @@
 ** If not, see <https://www.gnu.org/licenses/>.
 **/
 
-require_once __DIR__.'/../include/CWebTest.php';
 
-//Test checks  /browserwarning.php  web page.
+require_once __DIR__.'/../include/CWebTest.php';
 
 class testPageBrowserWarning extends CWebTest {
 
-	public function testPageBrowserWarning_CheckLayout() {
+	public function testPageBrowserWarning_WhenNotLoggedIn() {
+		$this->page->open('browserwarning.php');
+		$this->query('link:Continue despite this warning')->one()->click();
+		$this->assertTrue($this->query('button:Sign in')->one()->isClickable());
+	}
+
+	public function testPageBrowserWarning_Layout() {
 		$this->page->login()->open('browserwarning.php')->waitUntilReady();
-		$this->assertEquals('You are using an outdated browser.', $this->page->getTitle());
+		$this->assertEquals('You are using an outdated browser.', $this->query('tag:h2')->one()->getText());
 
-		// Assert text - 1st paragraph.
-		$this->assertEquals('Zabbix frontend is built on advanced, modern technologies and does not support old browsers. It is highly recommended that you choose and install a modern browser. It is free of charge and only takes a couple of minutes.',
-		$this->query('css:.browser-warning-container > p:nth-of-type(1)')->one()->getText());
+		$text = ['Zabbix frontend is built on advanced, modern technologies and does not support old browsers.'.
+		' It is highly recommended that you choose and install a modern browser. It is free of charge'.
+		' and only takes a couple of minutes.', 'New browsers usually come with support for new technologies,'.
+		' increasing web page speed, better privacy'.
+		' settings and so on. They also resolve security and functional issues.'];
+		$this->assertEquals($text, $this->query('tag:p')->all()->asText());
 
-		// Assert text -  2nd paragraph.
-		$this->assertEquals('New browsers usually come with support for new technologies, increasing web page speed, better privacy settings and so on. They also resolve security and functional issues.',
-		$this->query('css:.browser-warning-container > p:nth-of-type(2)')->one()->getText());
-
-		// Check, that links are clickable.
 		$links = [
 			'Google Chrome' => 'http://www.google.com/chrome',
 			'Mozilla Firefox' => 'http://www.mozilla.org/firefox',
@@ -39,16 +42,16 @@ class testPageBrowserWarning extends CWebTest {
 			'Opera browser' => 'http://www.opera.com/download',
 			'Apple Safari' => 'http://www.apple.com/safari/download'
 		];
-		foreach ($links as $key => $item) {
-			$this->assertEquals($this->query('link:' .$key)->one()->getAttribute('href'), $item);
+		foreach ($links as $link => $url) {
+			$this->assertEquals($this->query('link', $link)->one()->getAttribute('href'), $url);
+			$this->assertTrue($this->query('link', $link)->one()->isClickable());
 		}
 
-		// Screenshot comparison
-		$this->assertScreenshot($this->query('css:.browser-warning-container')->one());
+		$this->assertScreenshot($this->query('class:browser-warning-container')->one());
 
-		// Check, that user can go to Zabbix page.
+		// Navigate to dashboard page for logged in user.
 		$this->query('link:Continue despite this warning')->one()->click();
-		$this->query('css:#dashboard')->one()->waitUntilPresent();
-		$this->page->assertTitle('Dashboard');
+		$this->page->waitUntilReady();
+		$this->page->assertHeader('Dashboard for Copying widgets _1');
 	}
 }
