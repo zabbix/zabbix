29cc480e85f9e810abb66cee5cfae779a6d52cc6
Natalja Romancaka <natalja.romancaka@zabbix.com>
2025-12-29 16:33:54 +0200

    .......... [ZBXNEXT-686] changed workaround for item card widget Selenium test due to unsorted trigger list on postgresql

--- ui/tests/selenium/dashboardWidgets/testDashboardItemCardWidget.php
+++ ui/tests/selenium/dashboardWidgets/testDashboardItemCardWidget.php
@@ -1959,8 +1959,10 @@ class testDashboardItemCardWidget extends testWidgets {
 		if (array_key_exists('Triggers', $data)) {
 			// Check list of triggers.
 			$triggers = $widget->query('class:section-triggers')->query('class:triggers')->query('class:trigger')->all();
-			$actualNames = array_map('trim', str_replace(',', '', $triggers->asText()));
-			$this->assertEquals(array_column($data['Triggers'], 'Name'), $actualNames);
+			$actual_names = array_map('trim', str_replace(',', '', $triggers->asText()));
+
+			// Workaround: PostgreSQL returns unsorted trigger list.
+			$this->assertEqualsCanonicalizing(array_column($data['Triggers'], 'Name'), $actual_names);
 
 			// Check trigger counter.
 			$this->assertEquals(count($data['Triggers']), $widget->query('class:section-triggers')
@@ -2152,22 +2154,31 @@ class testDashboardItemCardWidget extends testWidgets {
 				break;
 
 			case 'severity':
-				$link = ($data['widget_name'] === 'Master item from host')
-					? 'zabbix.php?action=problem.view&hostids%5B0%5D='.self::$host_ids['hostids']
-						[self::HOST_NAME].'&triggerids%5B0%5D='.
-						self::$trigger_ids['Not classified trigger'].'&triggerids%5B1%5D='.
-						self::$trigger_ids['Information trigger'].'&triggerids%5B2%5D='.
-						self::$trigger_ids['Warning trigger'].'&triggerids%5B3%5D='.
-						self::$trigger_ids['Average trigger'].'&triggerids%5B4%5D='.
-						self::$trigger_ids['High trigger'].'&triggerids%5B5%5D='.
-						self::$trigger_ids['Disaster trigger'].'&filter_set=1'
-					: 'zabbix.php?action=problem.view&hostids%5B0%5D='.
-						self::$host_ids['hostids'][self::HOST_NAME].'&triggerids%5B0%5D='.
-						self::$trigger_ids['Trigger 1'].'&filter_set=1';
+				// Workaround: PostgreSQL returns unsorted trigger list.
+				$href = $widget->query('class:sections-header')->query('class:section-item')
+						->query('class:problem-icon-link')->one()->getAttribute('href');
+				if ($data['widget_name'] === 'Master item from host') {
+					parse_str(parse_url($href, PHP_URL_QUERY), $query);
+					$this->assertEquals('problem.view', $query['action']);
+					$this->assertEquals(self::$host_ids['hostids'][self::HOST_NAME], $query['hostids'][0]);
+					$this->assertEquals(1, $query['filter_set']);
 
-				$this->assertEquals($link, $widget->query('class:sections-header')->query('class:section-item')
-						->query('class:problem-icon-link')->one()->getAttribute('href')
-				);
+					$expected_triggers = [self::$trigger_ids['Not classified trigger'],
+						self::$trigger_ids['Information trigger'], self::$trigger_ids['Warning trigger'],
+						self::$trigger_ids['Average trigger'], self::$trigger_ids['High trigger'],
+						self::$trigger_ids['Disaster trigger']
+					];
+					$this->assertEqualsCanonicalizing($expected_triggers, $query['triggerids'],
+							'Trigger IDs order is not important'
+					);
+				}
+				else {
+					$link = 'zabbix.php?action=problem.view&hostids%5B0%5D='.
+							self::$host_ids['hostids'][self::HOST_NAME].'&triggerids%5B0%5D='.
+							self::$trigger_ids['Trigger 1'].'&filter_set=1';
+					$this->assertEquals($link, $href);
+				}
+
 				$widget->query('class:sections-header')->query('class:section-item')->query('class:problem-icon-link')
 						->one()->click();
 				$this->page->assertTitle('Problems');
@@ -2347,7 +2358,17 @@ class testDashboardItemCardWidget extends testWidgets {
 				self::$dashboard_ids['Dashboard for Item Card widget display check']
 		)->waitUntilReady();
 
-		$this->assertScreenshot(CDashboardElement::find()->one()->getWidget($data['Name']), 'itemcard_'.$data['Name']);
+		$widget = CDashboardElement::find()->waitUntilReady()->one()->getWidget($data['Name']);
+
+		// Workaround: PostgreSQL returns unsorted trigger list.
+		if ($data['Name'] === 'Master item from host'){
+			$this->assertScreenshotExcept(CDashboardElement::find()->waitUntilReady()->one()->getWidget($data['Name']),
+					[$widget->query('class:section-triggers')->query('class:triggers')->one()], 'itemcard_'.$data['Name']
+			);
+		}
+		else {
+			$this->assertScreenshot($widget, 'itemcard_'.$data['Name']);
+		}
 	}
 
 	/**
