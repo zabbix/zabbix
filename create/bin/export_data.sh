72788296a0317646e4e55682f86ca3f3de4f0c12
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2026-01-05 09:06:05 +0200

    .......... [ZBXNEXT-826] updated copyright year [2026]

--- create/bin/export_data.sh
+++ create/bin/export_data.sh
@@ -14,7 +14,7 @@ schema=$basedir/../src/schema.tmpl
 
 echo "--
 -- Zabbix
--- Copyright (C) 2001-2025 Zabbix SIA
+-- Copyright (C) 2001-2026 Zabbix SIA
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
