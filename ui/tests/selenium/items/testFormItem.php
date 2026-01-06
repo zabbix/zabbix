70b615183f4762c7f154c9d509f0230c739405a1
Natalja Romancaka <natalja.romancaka@zabbix.com>
2026-01-06 13:49:49 +0200

    .......... [ZBXNEXT-686] updated Selenium tests, added message check and reload after filling the field

--- ui/tests/selenium/items/testFormItem.php
+++ ui/tests/selenium/items/testFormItem.php
@@ -2189,6 +2189,7 @@ class testFormItem extends CLegacyWebTest {
 		$this->zbxTestCheckboxSelect('hk_trends_global', false);
 
 		$this->zbxTestClickWait('update');
+		$this->assertMessage(TEST_GOOD, 'Configuration updated');
 
 		$this->zbxTestOpen(self::HOST_LIST_PAGE);
 		$this->filterEntriesAndOpenItems();
@@ -2227,6 +2228,7 @@ class testFormItem extends CLegacyWebTest {
 		$this->zbxTestCheckboxSelect('hk_trends_global', false);
 
 		$this->zbxTestClickWait('update');
+		$this->assertMessage(TEST_GOOD, 'Configuration updated');
 
 		$this->zbxTestOpen(self::HOST_LIST_PAGE);
 		$this->filterEntriesAndOpenItems();
