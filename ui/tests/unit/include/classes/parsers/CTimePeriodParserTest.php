95d10f604f6998085854e5e1a90601f4c66ef3cb
Maksym Snihach <maksym.snihach@zabbix.com>
2026-01-12 11:29:24 +0100

    ..F....... [ZBX-22722] added test cases for parse time period for CTimePeriodParser

--- ui/tests/unit/include/classes/parsers/CTimePeriodParserTest.php
+++ ui/tests/unit/include/classes/parsers/CTimePeriodParserTest.php
@@ -458,6 +458,76 @@ class CTimePeriodParserTest extends TestCase {
 		];
 	}
 
+	public static function parseTimePeriodProvider(): array
+	{
+		return [
+			// success
+			[
+				'1-7,00:00-23:00',
+				0,
+				[
+					'rc' => CParser::PARSE_SUCCESS
+				],
+				[
+					'wd_from' => 1,
+					'wd_till' => 7,
+					'h_from' => '00',
+					'm_from' => '00',
+					'h_till' => '23',
+					'm_till' => '00',
+				],
+			],
+			[
+				'7,00:00-23:00',
+				0,
+				[
+					'rc' => CParser::PARSE_SUCCESS
+				],
+				[
+					'wd_from' => 7,
+					'wd_till' => 7,
+					'h_from' => '00',
+					'm_from' => '00',
+					'h_till' => '23',
+					'm_till' => '00',
+				],
+			],
+			// fail
+			[
+				'7-1,00:00-23:00',
+				0,
+				[
+					'rc' => CParser::PARSE_FAIL,
+				],
+				[],
+			],
+			[
+				'1-5,10:60-12:00',
+				0,
+				[
+					'rc' => CParser::PARSE_FAIL,
+				],
+				[],
+			],
+			[
+				'1-5,12:00-10:00',
+				0,
+				[
+					'rc' => CParser::PARSE_FAIL,
+				],
+				[],
+			],
+			[
+				'1-5,25:00-26:00',
+				0,
+				[
+					'rc' => CParser::PARSE_FAIL,
+				],
+				[],
+			],
+		];
+	}
+
 	/**
 	 * @dataProvider dataProvider
 	 *
@@ -474,5 +544,22 @@ class CTimePeriodParserTest extends TestCase {
 			'match' => $parser->getMatch()
 		]);
 		$this->assertSame(strlen($expected['match']), $parser->getLength());
+	}
+
+	/**
+	 * @dataProvider parseTimePeriodProvider
+	 *
+	 * @param string $source
+	 * @param int    $pos
+	 * @param array  $options
+	 * @param array  $expected
+	 */
+	public function testParseTimePeriod($source, $pos, $options, $expected) {
+		$parser = new CTimePeriodParser([]);
+
+		$rc = $parser->parse($source, $pos);
+
+		$this->assertSame($options['rc'], $rc);
+		$this->assertSame($expected, $parser->getPeriodParts());
 	}
 }
