da077f82ed84568f94a6cb071e8273886dd572d0
Ginta Berzina <ginta.berzina@zabbix.com>
2026-01-13 10:04:27 +0200

    ..F....... [ZBXNEXT-9851] fix error message

--- ui/app/controllers/CControllerLldRuleUpdateGeneral.php
+++ ui/app/controllers/CControllerLldRuleUpdateGeneral.php
@@ -129,7 +129,7 @@ abstract class CControllerLldRuleUpdateGeneral extends CController {
 			],
 			'status_codes' => ['db items.status_codes',
 				'use' => [CRangesParser::class, ['usermacros' => true, 'lldmacros' => false, 'with_minus' => true]],
-				'messages' => ['use' => _('Invalid range expression.')],
+				'messages' => ['use' => _('Invalid HTTP status code or range.')],
 				'when' => ['type', 'in' => [ITEM_TYPE_HTTPAGENT]]
 			],
 			'follow_redirects' => ['db items.follow_redirects',
