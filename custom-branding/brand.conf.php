42b25e87f4d4897b3ca23bb5acdd0e678dedcafa
Salman-TCM <salman.connects@gmail.com>
2026-01-15 17:01:15 +0600

    add docker and custom branding

---
+++ custom-branding/brand.conf.php
@@ -0,0 +1,10 @@
+<?php
+// BTRC Fixed Broadband QoS Monitoring System - Custom Branding
+
+return [
+    'BRAND_LOGO' => 'https://btrc.gov.bd/site-assets/images/logo.png',
+    'BRAND_LOGO_SIDEBAR' => null,
+    'BRAND_LOGO_SIDEBAR_COMPACT' => null,
+    'BRAND_FOOTER' => 'BTRC Fixed Broadband QoS Monitoring System',
+    'BRAND_HELP_URL' => ''
+];
