eb2981a1f2a17b29410c77e0e2438e5594dafbb6
Oskars Pucka <oskars.pucka@zabbix.com>
2026-01-16 14:40:18 +0200

    ...G...... [DEV-4674] added percentile to UnsafeUserParameters list

--- src/go/internal/agent/plugin_userparameter.go
+++ src/go/internal/agent/plugin_userparameter.go
@@ -71,7 +71,7 @@ func (p *UserParameterPlugin) cmd(key string, params []string) (string, error) {
 				if int(s[i]-'0') <= len(params) {
 					param := params[s[i]-'0'-1]
 					if p.unsafeUserParameters == 0 {
-						if j := strings.IndexAny(param, "\\'\"`*?[]{}~$!&;()<>|#@\n"); j != -1 {
+						if j := strings.IndexAny(param, "\\'\"`*?[]{}~$!&;()<>|#@%\n"); j != -1 {
 							if unicode.IsPrint(rune(param[j])) {
 								return "", fmt.Errorf("Character \"%c\" is not allowed", param[j])
 							}
