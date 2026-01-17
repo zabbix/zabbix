474e2fb76bbe8f6f5d50c8e09008d60015f3f457
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2026-01-17 14:31:04 +0200

    .......PS. [ZBX-27391] fixed tests

--- tests/libs/zbxcacheconfig/um_cache_sync.c
+++ tests/libs/zbxcacheconfig/um_cache_sync.c
@@ -138,7 +138,7 @@ void	zbx_mock_test_entry(void **state)
 		zbx_dbsync_init(&htmpls, NULL, ZBX_DBSYNC_UPDATE);
 
 		um_mock_cache_diff(mock_cache, &steps.values[i]->mock_cache, &gmacros, &hmacros, &htmpls);
-		umc = steps.values[i]->cache = um_cache_sync(umc, 0, &gmacros, &hmacros, &htmpls, &config_vault
+		umc = steps.values[i]->cache = um_cache_sync(umc, 0, &gmacros, &hmacros, &htmpls, &config_vault,
 				&um_cache_dup_sec, &um_cache_dup_size);
 		umc->refcount += steps.values[i]->refs;
 
