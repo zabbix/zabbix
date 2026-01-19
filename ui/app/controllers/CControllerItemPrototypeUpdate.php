1968dbb7debeec16239e73979011690abee0162f
Ginta Berzina <ginta.berzina@zabbix.com>
2026-01-19 10:09:38 +0200

    ..F....... [ZBX-26503] fixed validation rules for item query fields and headers

--- ui/app/controllers/CControllerItemPrototypeUpdate.php
+++ ui/app/controllers/CControllerItemPrototypeUpdate.php
@@ -112,7 +112,10 @@ class CControllerItemPrototypeUpdate extends CControllerItemPrototype {
 			'query_fields' => ['objects',
 				'fields' => [
 					'value' => ['string', 'length' => 255],
-					'name' => ['string', 'required', 'not_empty', 'length' => 255, 'when' => ['value', 'not_empty']],
+					'name' => [
+						['string', 'required', 'length' => 255],
+						['string', 'required', 'length' => 255, 'not_empty', 'when' => ['value', 'not_empty']]
+					],
 					'sortorder' => ['integer']
 				],
 				'when' => ['type', 'in' => [ITEM_TYPE_HTTPAGENT]]
@@ -150,7 +153,10 @@ class CControllerItemPrototypeUpdate extends CControllerItemPrototype {
 			'headers' => ['objects',
 				'fields' => [
 					'value' => ['string', 'length' => 2000],
-					'name' => ['string', 'required', 'not_empty', 'length' => 255, 'when' => ['value', 'not_empty']]
+					'name' => [
+						['string', 'required', 'length' => 255],
+						['string', 'required', 'length' => 255, 'not_empty', 'when' => ['value', 'not_empty']]
+					]
 				],
 				'when' => ['type', 'in' => [ITEM_TYPE_HTTPAGENT]]
 			],
