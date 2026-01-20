f29a23daad6b4a60793f7d22b4cd224f70ca6315
Andrejs Verza <andrejs.verza@zabbix.com>
2026-01-20 11:50:19 +0200

    A......... [ZBXNEXT-5419] removed unrelated code from the abstract importer class

--- ui/include/classes/import/importers/CImporter.php
+++ ui/include/classes/import/importers/CImporter.php
@@ -1,6 +1,6 @@
 <?php
 /*
-** Copyright (C) 2001-2025 Zabbix SIA
+** Copyright (C) 2001-2026 Zabbix SIA
 **
 ** This program is free software: you can redistribute it and/or modify it under the terms of
 ** the GNU Affero General Public License as published by the Free Software Foundation, version 3.
@@ -15,8 +15,6 @@
 
 
 abstract class CImporter {
-
-	protected const ALLOW_TESTMODE = false;
 
 	/**
 	 * @var CImportReferencer
@@ -33,38 +31,16 @@ abstract class CImporter {
 	 */
 	protected $options = [];
 
-	protected bool $testmode = false;
-
-	protected array $missing_objects = [
-		'items' => [],
-		'actions' => [],
-		'mediatypes' => [],
-		'host' => [],
-		'hostgroups' => [],
-		'graphs' => [],
-		'sysmaps' => [],
-		'sla' => [],
-		'services' => [],
-		'users' => []
-	];
-
 	/**
 	 * @param array						$options					import options "createMissing", "updateExisting" and "deleteMissing"
 	 * @param CImportReferencer			$referencer					class containing all importable objects
 	 * @param CImportedObjectContainer	$importedObjectContainer	class containing processed host and template IDs
-	 * @param bool						$testmode					skip calls to modify records in the database.
 	 */
 	public function __construct(array $options, CImportReferencer $referencer,
-			CImportedObjectContainer $importedObjectContainer, bool $testmode = false) {
+			CImportedObjectContainer $importedObjectContainer) {
 		$this->options = $options;
 		$this->referencer = $referencer;
 		$this->importedObjectContainer = $importedObjectContainer;
-
-		if ($testmode && !static::ALLOW_TESTMODE) {
-			throw new \Exception('Test mode for ' . static::class . ' import is not allowed.');
-		}
-
-		$this->testmode = $testmode;
 	}
 
 	/**
@@ -75,31 +51,4 @@ abstract class CImporter {
 	 * @return mixed
 	 */
 	abstract public function import(array $elements);
-
-	/**
-	 * Register missing referred object.
-	 *
-	 * @param string $object_type
-	 * @param array  $uniq_config
-	 *
-	 * @throws Exception
-	 * @return void
-	 */
-	protected function addMissingObject(string $object_type, array $uniq_config): void {
-		if (!array_key_exists($object_type, $this->missing_objects)) {
-			throw new Exception('Unknown object type: '.$object_type);
-		}
-
-		$key = str_replace(' ', '_', implode('_', array_values($uniq_config)));
-		$this->missing_objects[$object_type][$key] = $uniq_config;
-	}
-
-	/**
-	 * Return array of missing objects that were skipped during import
-	 *
-	 * @return array|array[]
-	 */
-	public function getMissingObjects(): array {
-		return $this->missing_objects;
-	}
 }
