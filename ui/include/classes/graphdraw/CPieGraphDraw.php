627a00ec34b67b19b373776e0d02f121ec2b281c
Katerina Kuzmina <katerina.kuzmina@zabbix.com>
2026-01-21 09:27:07 +0200

    ..F....... [ZBXNEXT-10069] fixed value display in graphs with type Pie/Exploded for items with type JSON/binary

--- ui/include/classes/graphdraw/CPieGraphDraw.php
+++ ui/include/classes/graphdraw/CPieGraphDraw.php
@@ -158,13 +158,11 @@ class CPieGraphDraw extends CGraphDraw {
 		}
 
 		$items = [];
+		$from_time = $this->from_time;
+		$to_time = $this->to_time;
 
 		for ($i = 0; $i < $this->num; $i++) {
 			$item = $this->items[$i];
-
-			$from_time = $this->from_time;
-			$to_time = $this->to_time;
-
 			$to_resolve = [];
 
 			// Override item history setting with housekeeping settings, if they are enabled in config.
