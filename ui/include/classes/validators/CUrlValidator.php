7f62c4779c00c524840f289f54d2dcd44045cccd
Ginta Berzina <ginta.berzina@zabbix.com>
2026-01-23 16:10:39 +0200

    ..F....... [ZBXNEXT-9744] revert CUrlValidator changes. Leave commented out valid url validation rules.

--- ui/include/classes/validators/CUrlValidator.php
+++ ui/include/classes/validators/CUrlValidator.php
@@ -16,37 +16,8 @@
 
 class CUrlValidator extends CValidator {
 
-	protected bool $allow_user_macro = true;
-	protected bool $allow_event_tags_macro = false;
-
-	public function __construct(array $options = []) {
-		if (array_key_exists('allow_user_macro', $options)) {
-			$this->allow_user_macro = $options['allow_user_macro'];
-		}
-
-		if (array_key_exists('allow_event_tags_macro', $options)) {
-			$this->allow_event_tags_macro = $options['allow_event_tags_macro'];
-		}
-	}
-
-	/**
-	 * Checks if the given string is:
-	 * - either macro or valid url with CHtmlUrlValidator
-	 *
-	 * @param string $value
-	 *
-	 * @return bool
-	 */
 	public function validate($value) {
-		$options = [
-			'allow_user_macro' => $this->allow_user_macro,
-			'allow_event_tags_macro' => $this->allow_event_tags_macro,
-			'allow_inventory_macro' => INVENTORY_URL_MACRO_NONE,
-			'allow_manualinput_macro' => false,
-			'validate_uri_schemes' => (bool) CSettingsHelper::get(CSettingsHelper::VALIDATE_URI_SCHEMES)
-		];
-
-		if (!CHtmlUrlValidator::validate((string) $value, $options)) {
+		if (!CHtmlUrlValidator::validate((string) $value)) {
 			$this->setError(_('unacceptable URL'));
 
 			return false;
