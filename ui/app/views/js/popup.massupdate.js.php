55c9b82d69fada7eabe33fd5a70cbac432db431a
Artem Luchko <artem.luchko@zabbix.com>
2026-02-03 10:56:22 +0000

    ..F....... [DEV-4554] fixed massupdate tags serrialization

--- ui/app/views/js/popup.massupdate.js.php
+++ ui/app/views/js/popup.massupdate.js.php
@@ -455,7 +455,7 @@ function submitPopup(overlay) {
 	fetch(url.getUrl(), {
 		method: 'post',
 		headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
-		body: $(form).serialize()
+		body: $.param(getFormFields($(form)[0]))
 	})
 	.then((response) => response.json())
 	.then((response) => {
