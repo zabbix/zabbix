1bb8031fafc084cbdd7883dee234f06c7ab8510a
Deniss Ponomarenko <deniss.ponomarenko@zabbix.com>
2026-02-09 09:56:11 -0300

    .......... [DEV-4633] refactor screenshot data check to use CTestArrayHelper

--- ui/tests/selenium/dashboardWidgets/testDashboardItemHistoryWidget.php
+++ ui/tests/selenium/dashboardWidgets/testDashboardItemHistoryWidget.php
@@ -2738,7 +2738,7 @@ class testDashboardItemHistoryWidget extends testWidgets {
 
 			$this->assertEquals($data['result'], $this->getWidgetTableData($widget));
 
-			if (array_key_exists('screenshot', $data)) {
+			if (CTestArrayHelper::get($data, 'screenshot')) {
 				// Find all the values ​​of the Timestamp column to hide them in the screenshot, since the values ​​are dynamic.
 				$timestamp_cells = $widget->query('xpath:.//tbody/tr/*[1]')->all()->asArray();
 				$this->assertScreenshotExcept($widget, $timestamp_cells, 'HTML encode check');
