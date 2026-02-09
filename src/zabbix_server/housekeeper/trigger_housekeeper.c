13db825032e6353cac7a12735fa708133da68e50
Armands Arseniuss Skolmeisters <armands.skolmeisters@zabbix.com>
2026-02-09 14:44:47 +0200

    ........S. [ZBXNEXT-10076] fixed statistics in housekeeper

--- src/zabbix_server/housekeeper/trigger_housekeeper.c
+++ src/zabbix_server/housekeeper/trigger_housekeeper.c
@@ -69,6 +69,8 @@ static int	housekeep_problems_events(const zbx_vector_uint64_t *eventids, int ev
 		sql_offset = 0;
 		zbx_db_add_condition_alloc(&sql, &sql_alloc, &sql_offset, "eventid", eventids_offset, count);
 
+		int	commited_problems, commited_events;
+
 		do
 		{
 			int	res;
@@ -76,19 +78,22 @@ static int	housekeep_problems_events(const zbx_vector_uint64_t *eventids, int ev
 			zbx_db_begin();
 
 			if (ZBX_DB_OK < (res = zbx_db_execute("delete from problem where%s", sql)))
-				*deleted_problems += res;
+				commited_problems = res;
 
 			if (ZBX_HK_OPTION_DISABLED != events_mode)
 			{
 				zbx_db_execute("delete from event_recovery where%s", sql);
 				if (ZBX_DB_OK < (res = zbx_db_execute("delete from events where%s", sql)))
-					*deleted_events += res;
+					commited_events = res;
 			}
 		}
 		while (ZBX_DB_DOWN == (txn_rc = zbx_db_commit()));
 
 		if (ZBX_DB_OK != txn_rc)
 			break;
+
+		*deleted_problems += commited_problems;
+		*deleted_events += commited_events;
 
 		offset += count;
 	}
