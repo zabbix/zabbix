06fc1dc2406b184de37f7c6d7222c6cee0952daa
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2026-02-27 11:30:08 +0200

    ..F....... [DEV-4718] updated onelogin/php-saml to version 4.3.1

--- ui/vendor/composer/autoload_static.php
+++ ui/vendor/composer/autoload_static.php
@@ -46,11 +46,16 @@ class ComposerStaticInit3e832710df7ec8c92736d5724fad7cab
         ),
     );
 
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit3e832710df7ec8c92736d5724fad7cab::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit3e832710df7ec8c92736d5724fad7cab::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit3e832710df7ec8c92736d5724fad7cab::$classMap;
 
         }, null, ClassLoader::class);
     }
