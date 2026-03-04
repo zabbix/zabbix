90e67edff447a17005c4ba1dfd82fee795cdfb91
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2026-03-04 14:25:32 +0200

    ...G...... [ZBX-27312] fixed issue of Zabbix agent 2 sometimes returning wrong result for proc.num
    (cherry picked from commit 250a1e2a24bf27eae75607f59ee366b8ff8fbf83)

--- ui/tests/integration/IntegrationTests.php
+++ ui/tests/integration/IntegrationTests.php
@@ -91,6 +91,7 @@ class IntegrationTests {
 		$suite->addTestSuite('testServiceRoles');
 		$suite->addTestSuite('testMacros');
 		$suite->addTestSuite('testExpressionTriggerMacros');
+		$suite->addTestSuite('testAgentItems');
 		$suite->addTestSuite('testScriptItems');
 		$suite->addTestSuite('testItemRate');
 		$suite->addTestSuite('testHistoryValueDuplicates');
