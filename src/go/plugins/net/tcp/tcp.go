a7bfbfd8dd7c72dfcaddfb7f46790292f79823d0
Armands Arseniuss Skolmeisters <armands.skolmeisters@zabbix.com>
2026-03-06 15:54:10 +0200

    ...G...... [ZBX-27336] added unittest

--- src/go/plugins/net/tcp/tcp.go
+++ src/go/plugins/net/tcp/tcp.go
@@ -489,6 +489,8 @@ func isAlnum(c byte) bool {
 		(c >= 'A' && c <= 'Z')
 }
 
+// isDNS checks if host is valid DNS name (should work the same as zbx_is_dns in C)
+//
 //nolint:cyclop,gocyclo // high complexity due to DNS validation, splitting not practical
 func isDNS(host string) bool {
 	n := len(host)
--- src/go/plugins/net/tcp/tcp_test.go
+++ src/go/plugins/net/tcp/tcp_test.go
@@ -16,6 +16,8 @@ package tcpudp
 
 import (
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func Test_removeScheme(t *testing.T) {
@@ -98,6 +100,51 @@ func Test_loginReceived(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := loginReceived(tt.args.buf); got != tt.want {
 				t.Errorf("loginReceived() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isDNS(t *testing.T) {
+	t.Parallel()
+
+	// Synchronize with tests/libs/zbxip/zbx_is_dns.yaml
+
+	type args struct {
+		host string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"-empty", args{""}, false},
+		{"-starts_with_dash", args{"-"}, false},
+		{"-dash_before_sep", args{"a-.a"}, false},
+		{"-starts_after_sep", args{"a.-a"}, false},
+		{"-too_long_label", args{"0123456789012345678901234567890123456789012345678901234567890123456789"}, false},
+		{"+label63", args{"123456789-123456789-123456789-123456789-123456789-123456789-123"}, true},
+		{"-label64", args{"123456789-123456789-123456789-123456789-123456789-123456789-1234"}, false},
+		{"+hostname253", args{"123456789-123456789-123456789-123456789-123456789.123456789-123456789-123456789-123456789-123456789.123456789-123456789-123456789-123456789-123456789.123456789-123456789-123456789-123456789-123456789.123456789-123456789-123456789-123456789-123456789.123"}, true},
+		{"-label254", args{"123456789-123456789-123456789-123456789-123456789.123456789-123456789-123456789-123456789-123456789.123456789-123456789-123456789-123456789-123456789.123456789-123456789-123456789-123456789-123456789.123456789-123456789-123456789-123456789-123456789.1234"}, false},
+		{"-unicode", args{"tēst.zabbix.com"}, false},
+		{"-leading_dot", args{".example.com"}, false},
+		{"-underscore", args{"example_com.com"}, false},
+		{"-space", args{"ex ample.com"}, false},
+		{"-end_with_dash", args{"example.com-"}, false},
+		{"-empty_label", args{"example.."}, false},
+		{"+min_label", args{"a.com"}, true},
+		{"+punny_code", args{"xn--bcher-kva.com"}, true},
+		{"+localhost", args{"localhost"}, true},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := isDNS(tt.args.host)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Fatalf("isDNS() = %s", diff)
 			}
 		})
 	}
