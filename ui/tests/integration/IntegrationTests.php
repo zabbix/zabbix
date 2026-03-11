6e488466d7dac8394681661cda0a0c0730d44886
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2026-03-11 15:48:05 +0200

    .......PS. [ZBX-27406] revert

--- ui/tests/integration/IntegrationTests.php
+++ ui/tests/integration/IntegrationTests.php
@@ -59,7 +59,6 @@ require_once dirname(__FILE__).'/testUserMacrosWithContext.php';
 require_once dirname(__FILE__).'/testUserMacrosWithContextRegex.php';
 require_once dirname(__FILE__).'/testLLDLinking.php';
 require_once dirname(__FILE__).'/testCalculatedExpression.php';
-require_once dirname(__FILE__).'/testItemDependent.php';
 
 use PHPUnit\Framework\TestSuite;
 
@@ -114,7 +113,6 @@ class IntegrationTests {
 		$suite->addTestSuite('testUserMacrosWithContextRegex');
 		$suite->addTestSuite('testLLDLinking');
 		$suite->addTestSuite('testCalculatedExpression');
-		$suite->addTestSuite('testItemDependent');
 		return $suite;
 	}
 }
