b913998b6d5325e2cc924a7a6d0c5e70ae04bfa1
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2026-03-11 15:05:53 +0200

    ........S. [ZBX-27453] added forecast overflow use-case 2

--- ui/tests/integration/testCalculatedExpression.php
+++ ui/tests/integration/testCalculatedExpression.php
@@ -444,13 +444,37 @@ class testCalculatedExpression extends CIntegrationTest {
 
 	}
 
+	private function createForecastCalculatedItemWithFormula($formula, $keySuffix)
+	{
+		$params = [
+			'formula'   => $formula,
+			'data_type' => ITEM_VALUE_TYPE_FLOAT,
+			'delay'     => '1s'
+		];
+
+		$response = $this->call('item.create', [
+			'name'       => self::CALCULATED_ITEM_KEY . '.' . $keySuffix,
+			'key_'       => self::CALCULATED_ITEM_KEY . '.' . $keySuffix,
+			'type'       => ITEM_TYPE_CALCULATED,
+			'hostid'     => self::$hostid,
+			'value_type' => ITEM_VALUE_TYPE_FLOAT,
+			'params'     => $params
+		]);
+
+		$this->assertArrayHasKey('itemids', $response['result']);
+		$this->assertEquals(1, count($response['result']['itemids']));
+
+		return $response['result']['itemids'][0];
+	}
+
+
 
 	public function testCalculatedExpression_Forecast_Overflow()
 	{
 		$trapId = $this->createTrap();
 
 		$formula = 'forecast(/' . self::HOST_NAME . '/' . self::TRAPPER_ITEM_KEY . self::$iterator . ',#3)';
-		$itemid = $this->createCalculatedItemWithFormula($formula, 'forecast_overflow');
+		$itemid = $this->createForecastCalculatedItemWithFormula($formula, 'forecast_overflow');
 		self::$itemIds = array_merge(self::$itemIds, [$itemid]);
 
 		$this->reloadConfigurationCache(self::COMPONENT_SERVER);
