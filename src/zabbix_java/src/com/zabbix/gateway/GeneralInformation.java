67e74595c84b17403354661266b932c28fe8bf63
Deniss Ponomarenko <deniss.ponomarenko@zabbix.com>
2026-03-17 05:53:48 -0300

    .......... [ZBXNEXT-826] updated release version to 7.0.25rc1

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -19,7 +19,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "12 March 2026";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "7.0.24";
+	static final String VERSION = "7.0.25rc1";
 
 	static void printVersion()
 	{
