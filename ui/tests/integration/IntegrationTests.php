325267a00ab15c05401b69dc178356c3f991a29b
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2026-03-20 10:20:48 +0200

    .......... [DEV-4516] fixed conditional jump warnings caused by compiler adding padding to structs
    (cherry picked from commit 05bf597ab48a08634096b0dea70b78638b0fdc57)

--- ui/tests/integration/IntegrationTests.php
+++ ui/tests/integration/IntegrationTests.php
@@ -62,6 +62,7 @@ require_once dirname(__FILE__).'/testUserMacrosWithContext.php';
 require_once dirname(__FILE__).'/testUserMacrosWithContextRegex.php';
 require_once dirname(__FILE__).'/testNestedLLD.php';
 require_once dirname(__FILE__).'/testCalculatedExpression.php';
+require_once dirname(__FILE__).'/testDiagInfo.php';
 
 use PHPUnit\Framework\TestSuite;
 
@@ -120,6 +121,7 @@ class IntegrationTests {
 		$suite->addTestSuite('testUserMacrosWithContextRegex');
 		$suite->addTestSuite('testNestedLLD');
 		$suite->addTestSuite('testCalculatedExpression');
+		$suite->addTestSuite('testDiagInfo');
 		return $suite;
 	}
 }
