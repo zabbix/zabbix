f8d8b3c3e76610c0ea84d77fab1661b9e07b1a93
Martins Talbergs <martins.talbergs@zabbix.com>
2026-03-27 17:49:49 +0200

    ..F....... [ZBXNEXT-8791] organized set of condition templates

--- ui/app/views/js/correlation.edit.js.php
+++ ui/app/views/js/correlation.edit.js.php
@@ -21,7 +21,7 @@
 
 window.correlation_edit_popup = new class {
 
-	init({rules, clone_rules, templates_data, templates_types}) {
+	init({rules, clone_rules, templates_data}) {
 		this.overlay = overlays_stack.getById('correlation.edit');
 		this.dialogue = this.overlay.$dialogue[0];
 		this.footer = this.overlay.$dialogue.$footer[0];
@@ -34,8 +34,9 @@ window.correlation_edit_popup = new class {
 		ZABBIX.PopupManager.setReturnUrl(return_url.href);
 
 		this.row_templates = {}
-		for (const type of templates_types) {
-			this.row_templates[type] = new Template(document.getElementById(`condition-row-tmpl-${type}`).innerHTML);
+		for (const template of document.getElementById('condition-templates').querySelectorAll('template')) {
+			const type = parseInt(template.content.querySelector('[data-conditiontype]').dataset.conditiontype, 10);
+			this.row_templates[type] = new Template(template.innerHTML);
 		}
 
 		templates_data.forEach((condition, index) => this.#addConditionRow(condition, index));
