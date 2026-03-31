2303a2307b9df962d6c7d6f4db82d5ab784d1538
Kristofers Astafjevs <kristofers.astafjevs@zabbix.com>
2026-03-31 14:50:32 +0300

    ..F....... [ZBX-27661] fixed input not being focused when changing psk in proxy edit form

--- ui/app/views/js/proxy.edit.js.php
+++ ui/app/views/js/proxy.edit.js.php
@@ -80,6 +80,8 @@ window.proxy_edit_popup = new class {
 			document.getElementById(id).disabled = false;
 		}
 
+		document.getElementById('tls_psk_identity').focus();
+
 		this.display_change_psk = false;
 	}
 
