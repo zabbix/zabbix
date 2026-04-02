f35c3c3156d3b8f7c328472d813cf387ac7e84fb
Artem Luchko <artem.luchko@zabbix.com>
2026-04-02 07:07:16 +0000

    ..F....... [ZBX-27617] improved code style

--- ui/app/views/js/host.wizard.edit.js.php
+++ ui/app/views/js/host.wizard.edit.js.php
@@ -2240,13 +2240,15 @@ window.host_wizard_edit = new class {
 				message: `${type === 'error' && messages.length > 1 ? '- ' : ''}${message}`
 			});
 
-			if (field.parentElement.classList.contains(ZBX_STYLE_FORM_FIELD)) {
+			const parent = field.parentElement;
+
+			if (parent.classList.contains(ZBX_STYLE_FORM_FIELD)) {
 				field.parentNode.insertBefore(message_element, field.nextSibling);
 			}
-			else if (field.parentElement.parentElement.classList.contains('macro-input-group')) {
-				field.parentElement.parentElement.parentNode.insertBefore(message_element, field.nextSibling);
+			else if (parent.parentElement.classList.contains('<?= CMacroValue::ZBX_STYLE_MACRO_INPUT_GROUP ?>')) {
+				parent.parentElement.parentNode.insertBefore(message_element, field.nextSibling);
 			} else {
-				field.parentElement.parentNode.insertBefore(message_element, field.parentElement.nextSibling);
+				parent.parentNode.insertBefore(message_element, parent.nextSibling);
 			}
 		});
 	}
