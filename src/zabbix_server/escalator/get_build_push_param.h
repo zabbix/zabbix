b88f86869749902872b381226a6c721c9a57c00f
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2026-04-06 22:12:00 +0300

    ........S. [ZBXNEXT-10357] moved push param notification logic to separate file

---
+++ src/zabbix_server/escalator/get_build_push_param.h
@@ -0,0 +1,20 @@
+/*
+** Copyright (C) 2001-2026 Zabbix SIA
+**
+** This program is free software: you can redistribute it and/or modify it under the terms of
+** the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+**
+** This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+** without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU Affero General Public License for more details.
+**
+** You should have received a copy of the GNU Affero General Public License along with this program.
+** If not, see <https://www.gnu.org/licenses/>.
+**/
+
+#include "zbxdbhigh.h"
+
+void	get_build_push_params(const zbx_db_event *event, const zbx_db_event *r_event,
+		zbx_uint64_t actionid, zbx_uint64_t userid, zbx_uint64_t mediatypeid, const char *sendto,
+		const char *subject, const char *message, const zbx_db_acknowledge *ack,
+		const zbx_service_alarm_t *service_alarm, const zbx_db_service *service, char **params, const char *tz);
