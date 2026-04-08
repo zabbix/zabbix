4e5ef12f9265b46bfe95510b9a34a0aaf12a2bb4
Ginta Berzina <ginta.berzina@zabbix.com>
2026-04-08 08:10:50 +0300

    ..F....... [ZBX-27612] fixed code style

--- ui/app/views/js/oauth.edit.js.php
+++ ui/app/views/js/oauth.edit.js.php
@@ -84,11 +84,9 @@ window.oauth_edit_popup = new class {
 								this.form_element.querySelector('[name="authorization_mode"][value="manual"]').click();
 								this.form_element.querySelector('[name="code"]').focus();
 
-								return Promise.reject({title: null, messages: [this.messages.authorization_error]});
+								throw {error: {title: null, messages: [this.messages.authorization_error]}};
 							}
 
-							return Promise.reject(error);
-						}).catch(error => {
 							throw {error};
 						})
 					)
