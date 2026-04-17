210cbdd37e0571341614a5c9c7c463478fc89fbf
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2026-04-17 14:10:40 +0300

    A......... [DEV-4427] added integration test

---
+++ ui/tests/integration/testHistoryValue.php
@@ -0,0 +1,127 @@
+<?php
+/*
+** Copyright (C) 2001-2026 Zabbix SIA
+**
+** This program is free software: you can redistribute it and/or modify it under the terms of
+** the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+**
+** This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+** without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU Affero General Public License for more details.
+**
+** You should have received a copy of the GNU Affero General Public License along with this program.
+** If not, see <https://www.gnu.org/licenses/>.
+**/
+
+require_once dirname(__FILE__).'/../include/CIntegrationTest.php';
+
+/**
+ * Test suite for history value storage.
+ *
+ * @required-components server
+ * @configurationDataProvider serverConfigurationProvider
+ * @hosts test_history_value
+ * @backup history,history_uint,history_str,history_text,history_log,items
+ */
+class testHistoryValue extends CIntegrationTest {
+	const HOSTNAME = 'test_history_value';
+
+	private static $hostid;
+	private static $items = [];
+
+	public function prepareData() {
+		$response = $this->call('host.create', [
+			[
+				'host' => self::HOSTNAME,
+				'interfaces' => [],
+				'groups' => [['groupid' => 4]],
+				'status' => HOST_STATUS_MONITORED
+			]
+		]);
+		$this->assertArrayHasKey('hostids', $response['result']);
+		self::$hostid = $response['result']['hostids'][0];
+
+		$item_defs = [
+			['key_' => 'trapper_float', 'value_type' => ITEM_VALUE_TYPE_FLOAT],
+			['key_' => 'trapper_uint', 'value_type' => ITEM_VALUE_TYPE_UINT64],
+			['key_' => 'trapper_str', 'value_type' => ITEM_VALUE_TYPE_STR],
+			['key_' => 'trapper_text', 'value_type' => ITEM_VALUE_TYPE_TEXT],
+			['key_' => 'trapper_log', 'value_type' => ITEM_VALUE_TYPE_LOG]
+		];
+
+		foreach ($item_defs as $def) {
+			$response = $this->call('item.create', [
+				'hostid' => self::$hostid,
+				'name' => $def['key_'],
+				'key_' => $def['key_'],
+				'type' => ITEM_TYPE_TRAPPER,
+				'value_type' => $def['value_type']
+			]);
+			$this->assertArrayHasKey('itemids', $response['result']);
+			self::$items[$def['key_']] = [
+				'itemid' => $response['result']['itemids'][0],
+				'value_type' => $def['value_type']
+			];
+		}
+
+		return true;
+	}
+
+	public function serverConfigurationProvider() {
+		return [
+			self::COMPONENT_SERVER => [
+				'LogFileSize' => 20,
+				'StartTrappers' => 1,
+				'StartDBSyncers' => 1
+			]
+		];
+	}
+
+	public function testHistoryValue_sendAndRetrieve() {
+		$tm = time();
+
+		$cases = [
+			'trapper_float' => [
+				['host' => self::HOSTNAME, 'key' => 'trapper_float', 'value' => 1.5, 'clock' => $tm, 'ns' => 1],
+				['host' => self::HOSTNAME, 'key' => 'trapper_float', 'value' => 2.5, 'clock' => $tm + 1, 'ns' => 1],
+				['host' => self::HOSTNAME, 'key' => 'trapper_float', 'value' => 3.5, 'clock' => $tm + 2, 'ns' => 1]
+			],
+			'trapper_uint' => [
+				['host' => self::HOSTNAME, 'key' => 'trapper_uint', 'value' => 10, 'clock' => $tm, 'ns' => 1],
+				['host' => self::HOSTNAME, 'key' => 'trapper_uint', 'value' => 20, 'clock' => $tm + 1, 'ns' => 1],
+				['host' => self::HOSTNAME, 'key' => 'trapper_uint', 'value' => 30, 'clock' => $tm + 2, 'ns' => 1]
+			],
+			'trapper_str' => [
+				['host' => self::HOSTNAME, 'key' => 'trapper_str', 'value' => 'alpha', 'clock' => $tm, 'ns' => 1],
+				['host' => self::HOSTNAME, 'key' => 'trapper_str', 'value' => 'beta', 'clock' => $tm + 1, 'ns' => 1],
+				['host' => self::HOSTNAME, 'key' => 'trapper_str', 'value' => 'gamma', 'clock' => $tm + 2, 'ns' => 1]
+			],
+			'trapper_text' => [
+				['host' => self::HOSTNAME, 'key' => 'trapper_text', 'value' => 'text_a', 'clock' => $tm, 'ns' => 1],
+				['host' => self::HOSTNAME, 'key' => 'trapper_text', 'value' => 'text_b', 'clock' => $tm + 1, 'ns' => 1],
+				['host' => self::HOSTNAME, 'key' => 'trapper_text', 'value' => 'text_c', 'clock' => $tm + 2, 'ns' => 1]
+			],
+			'trapper_log' => [
+				['host' => self::HOSTNAME, 'key' => 'trapper_log', 'value' => 'log_1', 'clock' => $tm, 'ns' => 1],
+				['host' => self::HOSTNAME, 'key' => 'trapper_log', 'value' => 'log_2', 'clock' => $tm + 1, 'ns' => 1],
+				['host' => self::HOSTNAME, 'key' => 'trapper_log', 'value' => 'log_3', 'clock' => $tm + 2, 'ns' => 1]
+			]
+		];
+
+		foreach ($cases as $key => $values) {
+			$this->sendDataValues('sender', $values, self::COMPONENT_SERVER);
+
+			$item = self::$items[$key];
+			$response = $this->callUntilDataIsPresent('history.get', [
+				'history' => $item['value_type'],
+				'itemids' => [$item['itemid']],
+				'sortfield' => 'clock',
+				'sortorder' => 'ASC'
+			], 5, 5);
+
+			$this->assertEquals(count($values), count($response['result']));
+		}
+
+		return true;
+	}
+}
