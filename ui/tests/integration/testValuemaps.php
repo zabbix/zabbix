c41e77c243b56aad1214ae586dc058d7f98eba0a
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2026-04-18 15:24:43 +0300

    .......... [DEV-4888] added another test sleeps

--- ui/tests/integration/testValuemaps.php
+++ ui/tests/integration/testValuemaps.php
@@ -262,6 +262,7 @@ class testValuemaps extends CIntegrationTest {
 	 * @dataProvider getValuemaps
 	 */
 	public function testValuemaps_checkProblemName($inputData, $inputType, $valuemap, $outputData) {
+		sleep(300);
 		$valuemap['hostid'] = self::$hostid;
 		$response = $this->call('valuemap.create', $valuemap);
 		$this->assertArrayHasKey('valuemapids', $response['result']);
