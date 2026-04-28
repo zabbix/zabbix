be0108716772eb280e266c1349d01496c0473375
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2026-04-28 14:40:16 +0300

    .......... [DEV-4427] fixed history import

--- database/clickhouse/history_import.sh
+++ database/clickhouse/history_import.sh
@@ -2,4 +2,4 @@
 
 . clickhouse.sh
 
-cat - | echo curl "$CH_URL?query=INSERT%20INTO%20$CH_DB.history%20FORMAT%20CSV" --data-binary @-
+cat - | curl "$CH_URL?query=INSERT%20INTO%20$CH_DB.history%20FORMAT%20CSV" --data-binary @-
