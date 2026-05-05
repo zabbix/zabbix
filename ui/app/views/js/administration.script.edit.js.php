e728ccefb97ee3cb18a98b5b1fedf9a061758ab9
Ginta Berzina <ginta.berzina@zabbix.com>
2026-05-05 14:20:57 +0300

    ..F....... [ZBXNEXT-9745] fix query selector

--- ui/app/views/js/administration.script.edit.js.php
+++ ui/app/views/js/administration.script.edit.js.php
@@ -282,7 +282,7 @@ window.script_edit_popup = new class {
 		const parameters_table_element = this.form_element.querySelector('#parameters-table tbody');
 		let row_index = 0;
 
-		while (parameters_table_element.querySelector(`[name="parameters[${row_index}][name]"`) !== null) {
+		while (parameters_table_element.querySelector(`[name="parameters[${row_index}][name]"]`) !== null) {
 			row_index++;
 		}
 
