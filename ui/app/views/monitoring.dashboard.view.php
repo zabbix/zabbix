63f51613968992072580405521ea2f57ccc8ad71
Artem Luchko <artem.luchko@zabbix.com>
2026-05-08 09:21:15 +0000

    ..F....... [ZBX-26465] added aria-label for Dashboard properties icon

--- ui/app/views/monitoring.dashboard.view.php
+++ ui/app/views/monitoring.dashboard.view.php
@@ -132,7 +132,8 @@ $html_page = (new CHtmlPage())
 				(new CTag('nav', true, new CList([
 					(new CButton('dashboard-config'))
 						->addClass(ZBX_STYLE_BTN_ICON)
-						->addClass(ZBX_ICON_COG_FILLED),
+						->addClass(ZBX_ICON_COG_FILLED)
+						->setAttribute('aria-label', _('Dashboard properties')),
 					(new CList())
 						->addClass(ZBX_STYLE_BTN_SPLIT)
 						->addItem(
