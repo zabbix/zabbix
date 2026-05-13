b0c31e462338da08c554c809172bec72cff1b446
Georgijs Klavins <georgijs.klavins@zabbix.com>
2026-05-13 10:39:50 +0300

    ..F....... [ZBXNEXT-5158] fixed testBrowserMonitoring integration test

--- ui/tests/integration/data/browser.js
+++ ui/tests/integration/data/browser.js
@@ -178,7 +178,7 @@ try
 
 	clickElement(browser, "link text", "Hosts");
 
-	Zabbix.sleep(5000); // Hosts is clicked and datatable is loaded
+	Zabbix.sleep(1000); // Hosts is clicked and datatable is loaded
 
 	clickElement(browser, "xpath", "//input[@id='all_hosts']");
 	clickElement(browser, "xpath", "//button[text()='Disable']");
