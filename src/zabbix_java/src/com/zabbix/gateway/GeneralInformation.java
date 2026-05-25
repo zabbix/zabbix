f4317b4db92554b2ac78120de9c9140489459417
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2026-05-25 09:08:32 +0300

    .......... [ZBXNEXT-826] updated release version to 8.0.0beta2

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -19,7 +19,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "21 May 2026";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "8.0.0beta1";
+	static final String VERSION = "8.0.0beta2";
 
 	static void printVersion()
 	{
