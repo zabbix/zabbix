80e1feb946cee5e51c17399412610c5d0eab9f54
Wiktor Czerneha <wiktor.czerneha@zabbix.com>
2026-05-26 10:26:17 +0200

    A......... [ZBXNEXT-10357] implemented custom DER/PEM converter to check EC public keys and ES256 algorithm signature

--- ui/include/classes/api/helpers/CApiDpopHelper.php
+++ ui/include/classes/api/helpers/CApiDpopHelper.php
@@ -27,18 +27,28 @@ class CApiDpopHelper {
 
 	private const SIGNATURE_ALGORITHM = 'ES256';
 
+	private const USED_FIREBASE = false;
+
+	private const ASN1_INTEGER 			 = 0x02;
+	private const ASN1_BIT_STRING        = 0x03;
+	private const ASN1_OBJECT_IDENTIFIER = 0x06;
+	private const ASN1_SEQUENCE          = 0x30;
+
+	private const OID_EC_PUBLIC_KEY = '1.2.840.10045.2.1';
+	private const OID_PRIME256V1    = '1.2.840.10045.3.1.7';
+
 	/**
 	 * @throws APIException
 	 */
-	public static function verifyDpopSignature(string $signature, array $kid_keys, string $access_token,
+	public static function verifyDpopSignature(string $dpop_token, array $kid_keys, string $access_token,
 			string $requested_api_method, int $check_time): void {
-		$segments = explode('.', $signature);
+		$segments = explode('.', $dpop_token);
 
 		if (count($segments) != 3) {
 			throw new APIException(ZBX_API_ERROR_NO_AUTH, _('Not authorized.'), 'Wrong number of JWT segments.');
 		}
 
-		[$encoded_header, $encoded_payload] = $segments;
+		[$encoded_header, $encoded_payload, $encoded_signature] = $segments;
 
 		$header = json_decode(JWT::urlsafeB64Decode($encoded_header), true);
 
@@ -62,7 +72,37 @@ class CApiDpopHelper {
 
 		self::checkJti($payload);
 
-		self::checkSignature($signature, $header, $kid_keys, $check_time);
+		$jwk = json_decode($kid_keys[$header['kid']], true);
+
+		try {
+			if (self::USED_FIREBASE) {
+				JWT::$timestamp = $check_time;
+				JWT::$leeway = self::TIME_SKEW;
+
+				$key = JWK::parseKey($jwk, self::SIGNATURE_ALGORITHM);
+
+				JWT::decode($dpop_token, $key);
+			}
+			else {
+				$public_key_pem = self::createPemFromJwk($jwk);
+
+				$der_signature = self::jwtSignatureToDerEncode(self::base64UrlDecode($encoded_signature));
+
+				$rezult = openssl_verify($encoded_header.'.'.$encoded_payload, $der_signature, $public_key_pem,
+					OPENSSL_ALGO_SHA256
+				);
+
+				if ($rezult == 0) {
+					throw new Exception('Signature verification failed.');
+				}
+				elseif ($rezult == -1) {
+					throw new Exception('OpenSSL error: '.openssl_error_string());
+				}
+			}
+		}
+		catch (Exception $e) {
+			throw new APIException(ZBX_API_ERROR_NO_AUTH, _('Not authorized.'), rtrim($e->getMessage(), '.').'.');
+		}
 	}
 
 	/**
@@ -104,7 +144,7 @@ class CApiDpopHelper {
 			throw new APIException(ZBX_API_ERROR_NO_AUTH, _('Not authorized.'), 'Missing ath claim.');
 		}
 
-		$expected_ath = JWT::urlsafeB64Encode(hash('sha256', $access_token, true));
+		$expected_ath = self::base64UrlEncode(hash('sha256', $access_token, true));
 
 		if (!hash_equals($expected_ath, $payload['ath'])) {
 			throw new APIException(ZBX_API_ERROR_NO_AUTH, _('Not authorized.'), 'Invalid ath value.');
@@ -167,31 +207,22 @@ class CApiDpopHelper {
 		DB::insertBatch('dpop_jti_cache', [$dpop_jti_cache_ins], false);
 	}
 
-	private static function checkSignature(string $signature, array $header, array $kid_keys, int $check_time): void {
-		JWT::$timestamp = $check_time;
-		JWT::$leeway = self::TIME_SKEW;
-
-		$jwk = json_decode($kid_keys[$header['kid']], true);
-
-		try {
-			$key = JWK::parseKey($jwk, self::SIGNATURE_ALGORITHM);
-
-			JWT::decode($signature, $key);
-		}
-		catch (Exception $e) {
-			throw new APIException(ZBX_API_ERROR_NO_AUTH, _('Not authorized.'), $e->getMessage().'.');
-		}
-	}
-
 	public static function checkJwkIntegrity(array $jwk): bool {
 		try {
-			$key = JWK::parseKey($jwk, self::SIGNATURE_ALGORITHM);
+			if (self::USED_FIREBASE) {
+				$key = JWK::parseKey($jwk, self::SIGNATURE_ALGORITHM);
+
+				$public_key_pem = $key->getKeyMaterial();
+			}
+			else {
+				$public_key_pem = self::createPemFromJwk($jwk);
+			}
+
+			if (openssl_pkey_get_public($public_key_pem) === false) {
+				return false;
+			}
 		}
 		catch (Exception $e) {
-			return false;
-		}
-
-		if (openssl_pkey_get_public($key->getKeyMaterial()) === false) {
 			return false;
 		}
 
@@ -202,5 +233,187 @@ class CApiDpopHelper {
 
 	public static function getServerId(): string {
 		return 'server_id';
+	}
+
+	/**
+	 * Convert JWT ES256 signature (R || S) into ASN.1 DER format. The encoding rules are defined in RFC 3279.
+	 *
+	 * @param string $signature
+	 *
+	 * @return string
+	 *
+	 * @throws Exception
+	 */
+	private static function jwtSignatureToDerEncode(string $signature): string {
+		if (strlen($signature) != 64) {
+			throw new Exception('Invalid signature length.');
+		}
+
+		$r = substr($signature, 0, 32);
+		$s = substr($signature, 32, 32);
+
+		return self::asn1SequenceEncode(self::asn1IntegerEncode($r).self::asn1IntegerEncode($s));
+	}
+
+	/**
+	 * Builds a PEM encoded EC public key from JWK (P-256). The encoding rules are defined in RFC 5480, 7468.
+	 *
+	 * @param array $jwk
+	 *
+	 * @return string
+	 */
+	private static function createPemFromJwk(array $jwk): string {
+		$algorithm_identifier = self::asn1SequenceEncode(self::asn1ObjectIdentifierEncode(self::OID_EC_PUBLIC_KEY).
+			self::asn1ObjectIdentifierEncode(self::OID_PRIME256V1)
+		);
+
+		// Uncompressed point (0x04) and 32-byte coordinates (P-256).
+		$ec_point = chr(0x04).self::base64UrlDecode($jwk['x']).self::base64UrlDecode($jwk['y']);
+
+		$subject_public_key = self::asn1BitStringEncode($ec_point);
+
+		$subject_public_key_info = self::asn1SequenceEncode($algorithm_identifier.$subject_public_key);
+
+		return "-----BEGIN PUBLIC KEY-----\n".
+			chunk_split(base64_encode($subject_public_key_info), 64, "\n").
+			"-----END PUBLIC KEY-----\n";
+	}
+
+	public static function base64UrlEncode(string $data): string {
+		if (self::USED_FIREBASE) {
+			return JWT::urlsafeB64Encode($data);
+		}
+
+		return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+	}
+
+	public static function base64UrlDecode(string $data): string {
+		if (self::USED_FIREBASE) {
+			return JWT::urlsafeB64Decode($data);
+		}
+
+		$padding_length = 4 - strlen($data) % 4;
+
+		if ($padding_length < 4) {
+			$data .= str_repeat('=', $padding_length);
+		}
+
+		return base64_decode(strtr($data, '-_', '+/'));
+	}
+
+	/**
+	 * Encode ASN.1 DER SEQUENCE. The encoding rules are defined in ITU-T X.690 section 8.9.
+	 *
+	 * @param string $data
+	 *
+	 * @return string
+	 */
+	private static function asn1SequenceEncode(string $data): string {
+		return chr(self::ASN1_SEQUENCE).self::asn1Length($data).$data;
+	}
+
+	/**
+	 * Encode ASN.1 DER OBJECT IDENTIFIER (OID). The encoding rules are defined in ITU-T X.690 section 8.19.
+	 *
+	 * @param string $data
+	 *
+	 * @return string
+	 */
+	private static function asn1ObjectIdentifierEncode(string $data): string {
+		$parts = array_map('intval', explode('.', $data));
+
+		// ASN.1 encodes first two OID components as first * 40 + second.
+		$first_byte = 40 * array_shift($parts) + array_shift($parts);
+
+		$encoded = chr($first_byte);
+
+		foreach ($parts as $part) {
+			$bin = '';
+
+			do {
+				// Extract lowest 7 bits.
+				$byte = $part & 0x7f;
+
+				$part = $part >> 7;
+
+				// Set continuation bit.
+				if ($bin !== '') {
+					$byte |= 0x80;
+				}
+
+				// Prepend byte to build big-endian representation.
+				$bin = chr($byte).$bin;
+			}
+			while ($part > 0);
+
+			$encoded .= $bin;
+		}
+
+		return chr(self::ASN1_OBJECT_IDENTIFIER).self::asn1Length($encoded).$encoded;
+	}
+
+	/**
+	 * Encode ASN.1 DER BIT STRING. The encoding rules are defined in ITU-T X.690 section 8.6.
+	 *
+	 * @param string $data
+	 *
+	 * @return string
+	 */
+	private static function asn1BitStringEncode(string $data): string {
+		// EC public key are byte-aligned, therefore unused bits = 0.
+		$data = chr(0).$data;
+
+		return chr(self::ASN1_BIT_STRING).self::asn1Length($data).$data;
+	}
+
+	/**
+	 * Encode ASN.1 DER INTEGER. The encoding rules are defined in ITU-T X.690 sections 8.3.
+	 *
+	 * @param string $data
+	 *
+	 * @return string
+	 */
+	private static function asn1IntegerEncode(string $data): string {
+		// Remove unnecessary leading zero bytes.
+		$data = ltrim($data, chr(0));
+
+		if ($data === '') {
+			$data = chr(0);
+		}
+
+		// If highest bit is set, prepend 00 byte to keep integer positive.
+		if ((ord($data[0]) & 0x80) != 0) {
+			$data = chr(0).$data;
+		}
+
+		return chr(self::ASN1_INTEGER).self::asn1Length($data).$data;
+	}
+
+	/**
+	 * Encode ASN.1 DER length field. Length encoding rules are defined in ITU-T X.690 sections 8.1.3 and 10.1.
+	 *
+	 * @param string $data
+	 *
+	 * @return string
+	 */
+	private static function asn1Length(string $data): string {
+		$length = strlen($data);
+
+		// DER short form lengths: 0..127 encoded as single byte.
+		if ($length < 0x80) {
+			return chr($length);
+		}
+
+		// DER long form lengths.
+		$bytes = '';
+
+		while ($length > 0) {
+			// Extract lowest 8 bits and prepend byte to build big-endian representation.
+			$bytes = chr($length & 0xff).$bytes;
+
+			$length >>= 8;
+		}
+
+		return chr(0x80 | strlen($bytes)).$bytes;
 	}
 }
