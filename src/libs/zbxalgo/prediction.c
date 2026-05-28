f3dba6987dc2b55602d08ef98c9cdecaf867ed32
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2026-05-28 17:29:13 +0300

    ........S. [ZBX-27453] updated timeleft() calculation to avoid NaNs

--- src/libs/zbxalgo/prediction.c
+++ src/libs/zbxalgo/prediction.c
@@ -1157,9 +1157,27 @@ double	zbx_timeleft(double *t, double *x, int n, double now, double threshold, z
 		goto out;
 	}
 
+	double a, b, exponent;
+
+	a = ZBX_MATRIX_EL(coefficients, 0, 0);
+	b = ZBX_MATRIX_EL(coefficients, 1, 0);
+
+	if (0 != isnan(a) || 0 != isnan(b) || 0.0 == b)
+	{
+		res = FAIL;
+		goto out;
+	}
+
+	if (0 != isinf(a) || 0 != isinf(b))
+	{
+		result = DBL_MAX;
+		goto out;
+	}
+
 	if (FIT_LINEAR == fit)
 	{
-		result = (threshold - ZBX_MATRIX_EL(coefficients, 0, 0)) / ZBX_MATRIX_EL(coefficients, 1, 0) - now;
+		result = (threshold - ZBX_MATRIX_EL(coefficients, 0, 0)) /
+				ZBX_MATRIX_EL(coefficients, 1, 0) - now;
 	}
 	else if (FIT_POLYNOMIAL == fit)
 	{
@@ -1167,28 +1185,82 @@ double	zbx_timeleft(double *t, double *x, int n, double now, double threshold, z
 	}
 	else if (FIT_EXPONENTIAL == fit)
 	{
-		result = (log(threshold) - ZBX_MATRIX_EL(coefficients, 0, 0)) / ZBX_MATRIX_EL(coefficients, 1, 0) - now;
+		if (0 != isnan(threshold) || 0.0 >= threshold)
+		{
+			res = FAIL;
+			goto out;
+		}
+
+		if (0 != isinf(threshold))
+		{
+			result = DBL_MAX;
+			goto out;
+		}
+
+		result = (log(threshold) - ZBX_MATRIX_EL(coefficients, 0, 0)) /
+				ZBX_MATRIX_EL(coefficients, 1, 0) - now;
 	}
 	else if (FIT_LOGARITHMIC == fit)
 	{
-		result = exp((threshold - ZBX_MATRIX_EL(coefficients, 0, 0)) / ZBX_MATRIX_EL(coefficients, 1, 0)) - now;
+		exponent = (threshold - ZBX_MATRIX_EL(coefficients, 0, 0)) /
+				ZBX_MATRIX_EL(coefficients, 1, 0);
+
+		if (0 != isnan(exponent))
+		{
+			res = FAIL;
+			goto out;
+		}
+
+		if (0 != isinf(exponent) || exponent > log(DBL_MAX))
+		{
+			result = DBL_MAX;
+			goto out;
+		}
+
+		result = exp(exponent) - now;
 	}
 	else if (FIT_POWER == fit)
 	{
-		result = exp((log(threshold) - ZBX_MATRIX_EL(coefficients, 0, 0)) / ZBX_MATRIX_EL(coefficients, 1, 0))
-				- now;
-	}
-	else
-	{
-		THIS_SHOULD_NEVER_HAPPEN;
-		res = FAIL;
+		if (0 != isnan(threshold) || 0.0 >= threshold)
+		{
+			res = FAIL;
+			goto out;
+		}
+
+		if (0 != isinf(threshold))
+		{
+			result = DBL_MAX;
+			goto out;
+		}
+
+		exponent = (log(threshold) - ZBX_MATRIX_EL(coefficients, 0, 0)) /
+			ZBX_MATRIX_EL(coefficients, 1, 0);
+
+		if (0 != isnan(exponent))
+		{
+			res = FAIL;
+			goto out;
+		}
+
+		if (0 != isinf(exponent) || exponent > log(DBL_MAX))
+		{
+			result = DBL_MAX;
+			goto out;
+		}
+
+		result = exp(exponent) - now;
 	}
 out:
 	if (SUCCEED != res)
 	{
 		result = ZBX_MATH_ERROR;
 	}
-	else if (0 == isfinite(result) || 0.0 > result || DBL_MAX < result)
+	else if (0 != isnan(result))
+	{
+		zabbix_log(LOG_LEVEL_DEBUG, "numerical error");
+		result = ZBX_MATH_ERROR;
+	}
+	else if (0 != isinf(result) || 0.0 > result || DBL_MAX < result)
 	{
 		result = DBL_MAX;
 	}
