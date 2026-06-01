98c1c2afa452f48f0a75eb339ad6c641c58186be
Ginta Berzina <ginta.berzina@zabbix.com>
2026-06-01 10:06:32 +0300

    ..F....... [DEV-4644] fixed multiselect field error collecting (matches master)

--- ui/js/class.form.js
+++ ui/js/class.form.js
@@ -453,7 +453,7 @@ class CForm {
 
 			if (field instanceof CFieldMultiselect) {
 				const affixed_path = '/' + field_name + '_new';
-				const affixed_subfield = new RegExp('^/' + field_name + '_new/');
+				const affixed_subfield = new RegExp(`^/(${field_name}|${field_name}_new)/`);
 
 				for (const error_path in raw_errors) {
 					const affixed = error_path === affixed_path || affixed_subfield.test(error_path);
@@ -465,7 +465,7 @@ class CForm {
 					if (raw_errors[error_path].length) {
 						raw_errors[field_path] = [...raw_errors[field_path], ...raw_errors[error_path]]
 							.filter(({message}) => message.length);
-						raw_errors[error_path] = [];
+						delete raw_errors[error_path];
 					}
 				}
 			}
