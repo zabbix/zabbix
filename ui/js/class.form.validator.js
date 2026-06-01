f8d3fa1ae9fa0aaf1af0c681234f9c0f0872ef7e
Ginta Berzina <ginta.berzina@zabbix.com>
2026-06-01 09:44:08 +0300

    ..F....... [DEV-4644] fixed getting related field data in case of different structures results in missing path

--- ui/js/class.form.validator.js
+++ ui/js/class.form.validator.js
@@ -256,7 +256,7 @@ class CFormValidator {
 			while (path_for_data_lookup.length) {
 				const part = path_for_data_lookup.shift();
 
-				if (!(part in field_data)) {
+				if (typeof(field_data) !== 'object' || !(part in field_data)) {
 					return null;
 				}
 
