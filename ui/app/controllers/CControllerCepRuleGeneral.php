37f1ccbbdda99de24c47dc3978ac2ac6c54ef15f
Martins Talbergs <martins.talbergs@zabbix.com>
2026-06-03 09:31:46 +0300

    ..F....... [ZBXNEXT-10353] corrected update/create rules branching

--- ui/app/controllers/CControllerCepRuleGeneral.php
+++ ui/app/controllers/CControllerCepRuleGeneral.php
@@ -27,7 +27,7 @@ abstract class CControllerCepRuleGeneral extends CController {
 	}
 
 	protected function checkInput(): bool {
-		$ret = $this->validateInput(self::getValidationRules($this->hasInput('cepruleid')));
+		$ret = $this->validateInput(self::getValidationRules(existing: $this->getAction() === 'ceprule.update'));
 
 		if (!$ret) {
 			$form_errors = $this->getValidationError();
