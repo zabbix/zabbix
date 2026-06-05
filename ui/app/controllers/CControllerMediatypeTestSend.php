5ff0640f1f161a87b0e56d40945c28e36934e2fd
Ginta Berzina <ginta.berzina@zabbix.com>
2026-06-05 13:11:07 +0300

    ..F....... [ZBXNEXT-10393] fixed push media type test to allow subject and message to be empty

--- ui/app/controllers/CControllerMediatypeTestSend.php
+++ ui/app/controllers/CControllerMediatypeTestSend.php
@@ -83,8 +83,7 @@ class CControllerMediatypeTestSend extends CController {
 
 		$ret = true;
 
-		if (($this->mediatype['type'] != MEDIA_TYPE_EXEC && $this->mediatype['type'] != MEDIA_TYPE_WEBHOOK)
-				|| ($this->mediatype['type'] == MEDIA_TYPE_PUSH && $this->getInput('subject', '') === '')) {
+		if (!in_array($this->mediatype['type'], [MEDIA_TYPE_EXEC, MEDIA_TYPE_WEBHOOK, MEDIA_TYPE_PUSH])) {
 			$validator = new CNewValidator(array_map('trim', $this->getInputAll()), [
 				'message' =>	'string|not_empty'
 			]);
