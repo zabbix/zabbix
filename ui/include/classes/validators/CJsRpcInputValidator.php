d66dcd8acab92cd7668b302d5409202c8f0961b4
Ivo Kurzemnieks <ivo.kurzemnieks@zabbix.com>
2026-06-08 09:36:27 +0300

    ..F....... [DEV-4920] fixed coding style

--- ui/include/classes/validators/CJsRpcInputValidator.php
+++ ui/include/classes/validators/CJsRpcInputValidator.php
@@ -460,7 +460,7 @@ class CJsRpcInputValidator {
 					'filter' => ['type' => API_OBJECT, 'fields' => [
 						'value_type' => ['type' => API_INTS32, 'in' => implode(',', [ITEM_VALUE_TYPE_FLOAT,
 							ITEM_VALUE_TYPE_STR, ITEM_VALUE_TYPE_LOG, ITEM_VALUE_TYPE_UINT64, ITEM_VALUE_TYPE_TEXT
-						])],
+						])]
 					]],
 					'host_pattern' => ['type' => API_STRINGS_UTF8],
 					'host_pattern_multiple' => ['type' => API_INT32],
