abe48c86829720d6fc3e1b8447297eaa1bf57cc2
Vjaceslavs Smelovs <vjaceslavs.smelovs@zabbix.com>
2026-06-08 15:11:14 +0300

    .....J.... [DEV-4424] updated SERVER configuration

--- src/zabbix_java/settings.sh
+++ src/zabbix_java/settings.sh
@@ -4,6 +4,7 @@
 ### Option: zabbix.server
 #	List of comma delimited IP addresses, optionally in CIDR notation, or DNS names of Zabbix servers and Zabbix proxies.
 #	Incoming connections will be accepted only from the hosts listed here.
+#	If not set or empty, all incoming connections are accepted.
 #	If IPv6 support is enabled then '127.0.0.1', '::127.0.0.1', '::ffff:127.0.0.1' are treated equally
 #	and '::/0' will allow any IPv4 or IPv6 address.
 #	'0.0.0.0/0' can be used to allow any IPv4 address.
@@ -11,7 +12,7 @@
 #
 # Mandatory: no
 # Default:
-# SERVER="127.0.0.1,::1"
+# SERVER=
 
 ### Option: zabbix.listenIP
 #	IP address to listen on.
