cd2edd8d9342d40b8a91b7f04feaba252a3f3754
Georgijs Klavins <georgijs.klavins@zabbix.com>
2026-06-09 10:21:05 +0300

    ..F....... [ZBX-24054] fixed template cloning with extended amount of mappings
    * commit '18e419cf5215a88c56716e71b4753729daf5b146':
      ..F....... [ZBX-24054] fixed template cloning with extended amount of mappings

--- ui/js/common.js
+++ ui/js/common.js
@@ -318,6 +318,10 @@ function PopUp(action, parameters, {
 		overlay.$dialogue[0].dispatchEvent(new CustomEvent('dialogue.reload'));
 	}
 
+	if (action === 'template.edit') {
+		parameters = JSON.stringify(parameters);
+	}
+
 	overlay
 		.load(action, parameters)
 		.then(function(resp) {
