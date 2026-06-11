6900a8761015bdcd1c91c22c3b44183b0f9be0d0
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2026-06-11 13:43:38 +0300

    .......... [ZBXNEXT-10353] updated integration test

--- ui/tests/templates/zabbix.conf.php
+++ ui/tests/templates/zabbix.conf.php
@@ -26,7 +26,6 @@ $ZBX_SERVER_NAME		= 'TEST_SERVER_NAME';
 $IMAGE_FORMAT_DEFAULT	= IMAGE_FORMAT_PNG;
 
 $ZBX_FEATURE_FLAGS['banners_enabled'] = false;
-$HISTORY_PROVIDERS[] = ['types' => ['uint','dbl','str','log','text','json'],'provider' => 'clickhouse','url' => 'http://localhost:8123','db' => 'zabbix','username' => 'zabbix', 'password' => 'zabbix'];
 
 // PHP runtime error log file for unit tests.
 define('PHPUNIT_ERROR_LOG', '{PHPUNIT_ERROR_LOG}');
