c33dea170e4a2dad9e8c20c87626af3856d0f1bc
Martins Talbergs <martins.talbergs@zabbix.com>
2026-06-12 10:13:33 +0300

    .......... [DEV-4974] updated libraries onelogin/php-saml:4.3.2 robrichards/xmlseclibs:3.1.5

--- ui/vendor/composer/autoload_real.php
+++ ui/vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit3e832710df7ec8c92736d5724fad7cab
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit3e832710df7ec8c92736d5724fad7cab', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit3e832710df7ec8c92736d5724fad7cab', 'loadClassLoader'));
@@ -33,25 +31,18 @@ class ComposerAutoloaderInit3e832710df7ec8c92736d5724fad7cab
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit3e832710df7ec8c92736d5724fad7cab::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire3e832710df7ec8c92736d5724fad7cab($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit3e832710df7ec8c92736d5724fad7cab::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire3e832710df7ec8c92736d5724fad7cab($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
