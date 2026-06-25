3777e46d2bef20d518b672f6bb9914fd8935437a
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2026-06-25 17:34:12 +0300

    ........S. [ZBXNEXT-10357] added ipv6 guard to the tests

--- tests/libs/zbxcfg/zbx_cfg_prepare_bridge_adapter_connect_to.c
+++ tests/libs/zbxcfg/zbx_cfg_prepare_bridge_adapter_connect_to.c
@@ -19,24 +19,33 @@
 
 #include "zbxcommon.h"
 #include "zbxcfg.h"
+#include "zbxip.h"
 
 void	zbx_mock_test_entry(void **state)
 {
 	const char	*url = zbx_mock_get_parameter_string("in.url");
 	const char	*connect_to = zbx_mock_get_parameter_string("in.connect_to");
+	int		ipv = zbx_mock_get_parameter_int("in.ipv");
 	int		exp_result = zbx_mock_str_to_return_code(zbx_mock_get_parameter_string("out.return"));
 	char		*curl_connect_to = NULL, *error = NULL;
 	int		result;
 
 	ZBX_UNUSED(state);
 
-	result = zbx_cfg_prepare_bridge_adapter_connect_to(url, connect_to, &curl_connect_to, &error);
-	zbx_mock_assert_int_eq("return value", exp_result, result);
-
-	if (SUCCEED == exp_result)
+#ifndef HAVE_IPV6
+	if (ZBX_IPRANGE_V4 == ipv)
+#else
+	if (ZBX_IPRANGE_V4 == ipv || ZBX_IPRANGE_V6 == ipv)
+#endif
 	{
-		zbx_mock_assert_str_eq("curl_connect_to",
-				zbx_mock_get_parameter_string("out.curl_connect_to"), curl_connect_to);
+		result = zbx_cfg_prepare_bridge_adapter_connect_to(url, connect_to, &curl_connect_to, &error);
+		zbx_mock_assert_int_eq("return value", exp_result, result);
+
+		if (SUCCEED == exp_result)
+		{
+			zbx_mock_assert_str_eq("curl_connect_to",
+					zbx_mock_get_parameter_string("out.curl_connect_to"), curl_connect_to);
+		}
 	}
 
 	zbx_free(curl_connect_to);
