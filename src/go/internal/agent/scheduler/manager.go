4248cb1803f5d69ca165bfb21c91dd883be5043b
Oskars Pucka <oskars.pucka@zabbix.com>
2026-07-03 14:41:38 +0300

    ...G...... [DEV-5010] lint fixes

--- src/go/internal/agent/scheduler/manager.go
+++ src/go/internal/agent/scheduler/manager.go
@@ -560,6 +560,7 @@ func (m *Manager) run() {
 	lastTick := time.Now()
 	cleaned := lastTick
 	time.Sleep(time.Duration(1e9 - lastTick.Nanosecond()))
+
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
