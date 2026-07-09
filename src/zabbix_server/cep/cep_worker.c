49423d9b3e71e99722539f14c959df4b61eab255
Andris Zeila <andris.zeila@zabbix.com>
2026-07-09 13:25:46 +0300

    ........S. [ZBXNEXT-10353] added logging of event task result

--- src/zabbix_server/cep/cep_worker.c
+++ src/zabbix_server/cep/cep_worker.c
@@ -757,7 +757,7 @@ static void	cep_worker_process_task_event(zbx_cep_worker_t *worker, zbx_cep_task
 				task->db_event->object);
 	}
 
-	zabbix_log(LOG_LEVEL_DEBUG, "End of %s()", __func__);
+	zabbix_log(LOG_LEVEL_DEBUG, "End of %s() event_op:%d", __func__, task->event_op);
 }
 
 /******************************************************************************
