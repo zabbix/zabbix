dea8b9ac838cf2e614f70e730695990c1b1b4a39
Rostislavs Belovs <rostislavs.belovs@zabbix.com>
2026-07-13 09:14:06 +0300

    .......... [DEV-5028] Selenium tests: delete redundant empty line

--- ui/tests/include/web/elements/CWidgetElement.php
+++ ui/tests/include/web/elements/CWidgetElement.php
@@ -46,7 +46,6 @@ class CWidgetElement extends CElement {
 		$this->query('xpath:.//li[@class="widget-info-button"]/button')->waitUntilPresent()->one()->click(true);
 		$hintbox = $this->query('xpath://div[contains(@class, "overlay-dialogue hintbox wordbreak")]')->waitUntilVisible()->one();
 
-
 		$hint_text = $hintbox->getText();
 
 		return $hint_text;
