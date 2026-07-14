0ef86be43e544b3daac676b991238f81b80e9932
Sergejs Cernisenko <sergejs.cernisenko@zabbix.com>
2026-07-14 14:33:57 +0300

    ..F....... [ZBX-27942] code review changes: - added 'web_layout_mode' class property to use for the mode check in methods - renamed 'time_selector_toggle_timeout' property to 'time_period_update_timeout' to better represent it's meaning

--- ui/app/views/js/monitoring.dashboard.view.js.php
+++ ui/app/views/js/monitoring.dashboard.view.js.php
@@ -54,12 +54,17 @@
 		/**
 		 * @type {number|null}
 		 */
-		#time_selector_toggle_timeout = null;
+		#time_period_update_timeout = null;
 
 		/**
 		 * @type {number|null}
 		 */
 		#host_override_toggle_timeout = null;
+
+		/**
+		 * @type {number}
+		 */
+		#web_layout_mode;
 
 		init({
 			dashboard,
@@ -74,6 +79,7 @@
 			this.#dashboard = dashboard;
 			this.#dashboard_time_period = dashboard_time_period;
 			this.#clone = clone;
+			this.#web_layout_mode = web_layout_mode;
 
 			timeControl.refreshPage = false;
 
@@ -189,7 +195,7 @@
 				}
 			}
 
-			ZABBIX.Dashboard.on(CDashboard.EVENT_REFERRED_UPDATE, e => this.#onReferredUpdate(e, web_layout_mode));
+			ZABBIX.Dashboard.on(CDashboard.EVENT_REFERRED_UPDATE, e => this.#onReferredUpdate(e));
 
 			ZABBIX.Dashboard.on(CDashboard.EVENT_FEEDBACK, e => this.#onFeedback(e));
 
@@ -567,21 +573,21 @@
 			});
 		}
 
-		#onReferredUpdate(e, web_layout_mode) {
+		#onReferredUpdate(e) {
 			switch (e.detail.type) {
 				case CWidgetsData.DATA_TYPE_TIME_PERIOD:
-					if (this.#time_selector_toggle_timeout !== null) {
-						clearTimeout(this.#time_selector_toggle_timeout);
+					if (this.#time_period_update_timeout !== null) {
+						clearTimeout(this.#time_period_update_timeout);
 					}
 
-					this.#time_selector_toggle_timeout = setTimeout(() => {
-						this.#time_selector_toggle_timeout = null;
+					this.#time_period_update_timeout = setTimeout(() => {
+						this.#time_period_update_timeout = null;
 
 						if (this.#dashboard.dashboardid !== null && !this.#clone) {
 							this.#updateHistory({add_new: false});
 						}
 
-						if (web_layout_mode === undefined || web_layout_mode != <?= ZBX_LAYOUT_KIOSKMODE ?>) {
+						if (this.#web_layout_mode != <?= ZBX_LAYOUT_KIOSKMODE ?>) {
 							this.#toggleTimeSelector(e.detail.is_referred);
 						}
 					});
