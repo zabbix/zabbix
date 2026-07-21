86da6c3b0eed7329342c0594111f6520de8bcc0c
Natalja Romancaka <natalja.romancaka@zabbix.com>
2026-07-21 07:47:24 +0300

    .......... [ZBXNEXT-686] fixed invalid lld_override_operationid references in api test data

--- ui/tests/api_json/data/data_test.sql
+++ ui/tests/api_json/data/data_test.sql
@@ -1093,9 +1093,9 @@ INSERT INTO lld_override_opdiscover (lld_override_operationid,discover) VALUES (
 INSERT INTO lld_override_opdiscover (lld_override_operationid,discover) VALUES (11010,1);
 INSERT INTO lld_override_opdiscover (lld_override_operationid,discover) VALUES (11011,1);
 INSERT INTO lld_override_ophistory (lld_override_operationid,history) VALUES (11008,'92d');
-INSERT INTO lld_override_opinventory (lld_override_operationid,inventory_mode) VALUES (10011,1);
+INSERT INTO lld_override_opinventory (lld_override_operationid,inventory_mode) VALUES (11011,1);
 INSERT INTO lld_override_opperiod (lld_override_operationid,delay) VALUES (11008,'1m;wd1-3h4-16;10s/1-5,00:00-20:00;5s/5-7,00:00-24:00');
-INSERT INTO lld_override_opseverity (lld_override_operationid,severity) VALUES (10009,3);
+INSERT INTO lld_override_opseverity (lld_override_operationid,severity) VALUES (11009,3);
 INSERT INTO lld_override_opstatus (lld_override_operationid,status) VALUES (11007,1);
 INSERT INTO lld_override_opstatus (lld_override_operationid,status) VALUES (11008,0);
 INSERT INTO lld_override_opstatus (lld_override_operationid,status) VALUES (11009,1);
