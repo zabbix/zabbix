16aaeba12e03686d4a49205826f00a14ff0be248
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2026-07-27 14:38:12 +0300

    .......... [DEV-4972] temp commented the fix

--- src/libs/zbxcachehistory/cachehistory.c
+++ src/libs/zbxcachehistory/cachehistory.c
@@ -3609,8 +3609,8 @@ int	zbx_hc_is_itemid_cached_and_normal(zbx_uint64_t itemid, int period_start)
 	LOCK_CACHE;
 
 	if (NULL != (item = (zbx_hc_item_t *)zbx_hashset_search(&cache->history_items, &itemid)) &&
-			NULL != item->tail && ITEM_STATE_NORMAL == item->tail->state &&
-			0 == (item->tail->flags & ZBX_DC_FLAG_NOVALUE) && item->tail->ts.sec >= period_start)
+			NULL != item->tail && ITEM_STATE_NORMAL == item->tail->state) /*&&
+			/ 0 == (item->tail->flags & ZBX_DC_FLAG_NOVALUE) && item->tail->ts.sec >= period_start)*/
 	{
 			ret = SUCCEED;
 	}
