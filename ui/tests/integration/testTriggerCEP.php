ef7646ad91e82a740bfdbd95410688ad14bb30e2
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2026-07-27 13:42:15 +0300

    .......... [ZBXNEXT-10353] updated integration tests

--- ui/tests/integration/testTriggerCEP.php
+++ ui/tests/integration/testTriggerCEP.php
@@ -6067,6 +6067,7 @@ HEREDOC;
 		// problem is open in the system at this point, so cached_events must drain back to zero.
 		$this->assertCepStatEquals('tasks', 'cached_events', 0);
 		$this->assertCepStatEquals('tasks', 'cached_objects', 0);
+		$this->executeRuntimeControlCommand(self::COMPONENT_SERVER, 'diaginfo=cep');
 	}
 
 	/**
