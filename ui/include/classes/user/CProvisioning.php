9884559164e3bd8dd7c9ed7617fd8811c0f49706
Uldis Olekss <uldis.olekss@zabbix.com>
2026-07-27 14:20:27 +0300

    ..F....... [DEV-5026] fixed group wildcard handling in user and provision group matching

--- ui/include/classes/user/CProvisioning.php
+++ ui/include/classes/user/CProvisioning.php
@@ -320,7 +320,10 @@ class CProvisioning {
 			}
 			elseif (!$match) {
 				$regex = preg_quote($provision_group['name'], '/');
-				$regex = '/'.str_replace('\\*', '.*', $regex).'/';
+				$regex = str_replace('\\*', '.*', $regex);
+				$prefix = (str_starts_with($provision_group['name'], '*')) ? '' : '^';
+				$suffix = (str_ends_with($provision_group['name'], '*')) ? '' : '$';
+				$regex = '/'.$prefix.$regex.$suffix.'/';
 				$match = false;
 
 				foreach ($group_names as $group_name) {
