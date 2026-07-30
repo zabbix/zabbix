32b3c19fc7bc65bb90bbc6706c151bbaec23bcb2
Vjaceslavs Smelovs <vjaceslavs.smelovs@zabbix.com>
2026-07-30 10:49:54 +0300

    ...G...... [ZBXNEXT-10438] implemented vfs.fs.get mode and mountpoint for agent on freebsd

--- src/libs/zbxsysinfo/freebsd/diskspace.c
+++ src/libs/zbxsysinfo/freebsd/diskspace.c
@@ -279,16 +279,82 @@ int	vfs_fs_discovery(AGENT_REQUEST *request, AGENT_RESULT *result)
 
 static int	vfs_fs_get_local(AGENT_REQUEST *request, AGENT_RESULT *result)
 {
-	int			rc, ret = SYSINFO_RET_FAIL;
+	int			rc, ret = SYSINFO_RET_FAIL, mode_short = 0;
 	struct statfs		*mntbuf;
 	struct zbx_json		j;
 	zbx_uint64_t		total, not_used, used, itotal, inot_used, iused;
 	double			pfree, pused, ipfree, ipused;
-	char			*error;
+	char			*mode, *mountpoint, *error;
 	zbx_vector_ptr_t	mntpoints;
 	zbx_mpoint_t		*mntpoint;
 	zbx_fsname_t		fsname;
 
+	/* validate parameter count */
+	if (2 < request->nparam)
+	{
+		SET_MSG_RESULT(result, zbx_strdup(NULL, "Too many parameters."));
+		return SYSINFO_RET_FAIL;
+	}
+
+	/* parse parameters */
+	mode = get_rparam(request, 0);
+	mountpoint = get_rparam(request, 1);
+
+	/* validate mode */
+	if (NULL != mode && '\0' != *mode)
+	{
+		if (0 == strcmp(mode, "short"))
+			mode_short = 1;
+		else if (0 != strcmp(mode, "full"))
+		{
+			SET_MSG_RESULT(result, zbx_strdup(NULL, "Invalid first parameter."));
+			return SYSINFO_RET_FAIL;
+		}
+	}
+
+	/* NULL or empty mode defaults to "full" */
+	/* empty mountpoint means no filter */
+	if (NULL != mountpoint && '\0' == *mountpoint)
+		mountpoint = NULL;
+
+	/* process short mode */
+	if (1 == mode_short)
+	{
+		if (0 == (rc = getmntinfo(&mntbuf, MNT_NOWAIT)))
+		{
+			SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot obtain system information: %s",
+					zbx_strerror(errno)));
+			return SYSINFO_RET_FAIL;
+		}
+
+		zbx_json_initarray(&j, ZBX_JSON_STAT_BUF_LEN);
+
+		for (int i = 0; i < rc; i++)
+		{
+			fsname.mpoint = mntbuf[i].f_mntonname;
+
+			/* apply mountpoint filter */
+			if (NULL != mountpoint && 0 != strcmp(fsname.mpoint, mountpoint))
+				continue;
+
+			zbx_json_addobject(&j, NULL);
+			zbx_json_addstring(&j, ZBX_SYSINFO_TAG_FSNAME, fsname.mpoint, ZBX_JSON_TYPE_STRING);
+			zbx_json_addstring(&j, ZBX_SYSINFO_TAG_FSTYPE, mntbuf[i].f_fstypename, ZBX_JSON_TYPE_STRING);
+			zbx_json_addstring(&j, ZBX_SYSINFO_TAG_FSOPTIONS, zbx_format_mntopt_string(mntopts,
+					mntbuf[i].f_flags & MNT_VISFLAGMASK), ZBX_JSON_TYPE_STRING);
+			zbx_json_close(&j);
+		}
+
+		zbx_json_close(&j);
+
+		SET_STR_RESULT(result, zbx_strdup(NULL, j.buffer));
+
+		zbx_json_free(&j);
+
+		return SYSINFO_RET_OK;
+	}
+
+	/* process full mode */
 	if (0 == (rc = getmntinfo(&mntbuf, MNT_NOWAIT)))
 	{
 		SET_MSG_RESULT(result, zbx_dsprintf(NULL, "Cannot obtain system information: %s", zbx_strerror(errno)));
@@ -301,11 +367,16 @@ static int	vfs_fs_get_local(AGENT_REQUEST *request, AGENT_RESULT *result)
 	{
 		fsname.mpoint = mntbuf[i].f_mntonname;
 
+		/* apply mountpoint filter */
+		if (NULL != mountpoint && 0 != strcmp(fsname.mpoint, mountpoint))
+			continue;
+
 		if (SYSINFO_RET_OK != get_fs_size_stat(fsname.mpoint, &total, &not_used, &used, &pfree, &pused, &error))
 		{
 			zbx_free(error);
 			continue;
 		}
+
 		if (SYSINFO_RET_OK != get_fs_inode_stat(fsname.mpoint, &itotal, &inot_used, &iused, &ipfree, &ipused,
 				"pused", &error))
 		{
@@ -345,6 +416,10 @@ static int	vfs_fs_get_local(AGENT_REQUEST *request, AGENT_RESULT *result)
 
 		fsname.mpoint = mntbuf[i].f_mntonname;
 		fsname.type = mntbuf[i].f_fstypename;
+
+		/* apply mountpoint filter */
+		if (NULL != mountpoint && 0 != strcmp(fsname.mpoint, mountpoint))
+			continue;
 
 		if (FAIL != (idx = zbx_vector_ptr_search(&mntpoints, &fsname, zbx_fsname_compare)))
 		{
