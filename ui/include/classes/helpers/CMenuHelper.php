1c3c5af2d7e644da68cb2a33c86014e69b1488d8
Artem Luchko <artem.luchko@zabbix.com>
2026-07-31 11:45:52 +0300

    ..F....... [ZBX-26465] improved aria-label text for all functional buttons and icons and aria-expanded of side menu elements for Screen Reader users
    * commit '2e1c83ba253f9f21e6570ec53a8b8e7d25e40589':
      ..F....... [ZBX-26465] fixed translation
      ..F....... [ZBX-26465] fixed aria-label on help icons
      ..F....... [ZBX-26465] reverted redunant cherry-picking code
      ..F....... [ZBX-26465] fixed translation
      ..F....... [ZBX-26465] improved aria-label text for all functional buttons and icons for Screen Reader users
    
    (cherry picked from commit 7725439e12d6f712da3b02b6bba3868c826ce75d)
    conflicts resolved:
    - ui/include/classes/helpers/CMenuHelper.php
    - ui/include/events.inc.php
    - ui/js/class.overlay.js
    - ui/tests/selenium/common/testFormFilter.php
    - ui/tests/selenium/dashboards/testDashboardsPages.php
    - ui/tests/selenium/latestData/testPageMonitoringLatestData.php

--- ui/include/classes/helpers/CMenuHelper.php
+++ ui/include/classes/helpers/CMenuHelper.php
@@ -223,6 +223,7 @@ class CMenuHelper {
 			CWebUser::checkAccess(CRoleHelper::UI_CONFIGURATION_AUTOREGISTRATION_ACTIONS) ||
 			CWebUser::checkAccess(CRoleHelper::UI_CONFIGURATION_INTERNAL_ACTIONS))
 				? (new CMenuItem(_('Actions')))
+					->setId('main-menu-actions')
 					->setSubMenu(new CMenu(array_filter([
 						CWebUser::checkAccess(CRoleHelper::UI_CONFIGURATION_TRIGGER_ACTIONS)
 							? (new CMenuItem(_('Trigger actions')))
@@ -338,6 +339,7 @@ class CMenuHelper {
 		$submenu_administration = [
 			CWebUser::checkAccess(CRoleHelper::UI_ADMINISTRATION_GENERAL)
 				? (new CMenuItem(_('General')))
+					->setId('main-menu-general')
 					->setSubMenu(new CMenu(array_filter([
 						(new CMenuItem(_('GUI')))
 							->setAction('gui.edit'),
@@ -394,6 +396,7 @@ class CMenuHelper {
 				: null,
 			CWebUser::checkAccess(CRoleHelper::UI_ADMINISTRATION_QUEUE)
 				? (new CMenuItem(_('Queue')))
+					->setId('main-menu-queue')
 					->setSubMenu(new CMenu([
 						(new CMenuItem(_('Queue overview')))
 							->setAction('queue.overview'),
