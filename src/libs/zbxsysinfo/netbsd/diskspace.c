05ef597ed1378484ae730cdf844c333a79824162
Vjaceslavs Smelovs <vjaceslavs.smelovs@zabbix.com>
2026-08-03 14:06:56 +0300

    ...G...... [ZBXNEXT-10438] refactored vfs.fs.get for agent on netbsd

--- src/libs/zbxsysinfo/netbsd/diskspace.c
+++ src/libs/zbxsysinfo/netbsd/diskspace.c
@@ -267,6 +267,11 @@ int	vfs_fs_discovery(AGENT_REQUEST *request, AGENT_RESULT *result)
 	return SYSINFO_RET_OK;
 }
 
+static int	match_mountpoint(const char *current, const char *requested)
+{
+	return (NULL == requested || 0 == strcmp(current, requested)) ? SUCCEED : FAIL;
+}
+
 static int	vfs_fs_get_local(AGENT_REQUEST *request, AGENT_RESULT *result)
 {
 	int			rc, ret = SYSINFO_RET_FAIL, mode_short = 0;
@@ -302,7 +307,6 @@ static int	vfs_fs_get_local(AGENT_REQUEST *request, AGENT_RESULT *result)
 		}
 	}
 
-	/* NULL or empty mode defaults to "full" */
 	/* empty mountpoint means no filter */
 	if (NULL != mountpoint && '\0' == *mountpoint)
 		mountpoint = NULL;
@@ -322,17 +326,20 @@ static int	vfs_fs_get_local(AGENT_REQUEST *request, AGENT_RESULT *result)
 
 		for (int i = 0; i < rc; i++)
 		{
+			char *options;
+
 			fsname.mpoint = mntbuf[i].f_mntonname;
 
 			/* apply mountpoint filter */
-			if (NULL != mountpoint && 0 != strcmp(fsname.mpoint, mountpoint))
+			if (FAIL == match_mountpoint(fsname.mpoint, mountpoint))
 				continue;
 
 			zbx_json_addobject(&j, NULL);
 			zbx_json_addstring(&j, ZBX_SYSINFO_TAG_FSNAME, fsname.mpoint, ZBX_JSON_TYPE_STRING);
 			zbx_json_addstring(&j, ZBX_SYSINFO_TAG_FSTYPE, mntbuf[i].f_fstypename, ZBX_JSON_TYPE_STRING);
-			zbx_json_addstring(&j, ZBX_SYSINFO_TAG_FSOPTIONS, zbx_format_mntopt_string(mntopts,
-					mntbuf[i].f_flag & MNT_VISFLAGMASK), ZBX_JSON_TYPE_STRING);
+			options = zbx_format_mntopt_string(mntopts, mntbuf[i].f_flag & MNT_VISFLAGMASK);
+			zbx_json_addstring(&j, ZBX_SYSINFO_TAG_FSOPTIONS, options, ZBX_JSON_TYPE_STRING);
+			zbx_free(options);
 			zbx_json_close(&j);
 		}
 
@@ -360,7 +367,7 @@ static int	vfs_fs_get_local(AGENT_REQUEST *request, AGENT_RESULT *result)
 		fsname.mpoint = mntbuf[i].f_mntonname;
 
 		/* apply mountpoint filter */
-		if (NULL != mountpoint && 0 != strcmp(fsname.mpoint, mountpoint))
+		if (FAIL == match_mountpoint(fsname.mpoint, mountpoint))
 			continue;
 
 		if (SYSINFO_RET_OK != get_fs_size_stat(fsname.mpoint, &total, &not_used, &used, &pfree, &pused,&error))
@@ -410,7 +417,7 @@ static int	vfs_fs_get_local(AGENT_REQUEST *request, AGENT_RESULT *result)
 		fsname.type = mntbuf[i].f_fstypename;
 
 		/* apply mountpoint filter */
-		if (NULL != mountpoint && 0 != strcmp(fsname.mpoint, mountpoint))
+		if (FAIL == match_mountpoint(fsname.mpoint, mountpoint))
 			continue;
 
 		if (FAIL != (idx = zbx_vector_ptr_search(&mntpoints, &fsname, zbx_fsname_compare)))
