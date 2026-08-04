e4b474535ffc5e6c5d221d19b1411c8614d6492d
Natalja Romancaka <natalja.romancaka@zabbix.com>
2026-08-04 10:20:25 +0300

    .......... [DEV-5056] added waits for table reload

--- ui/tests/selenium/hosts/testFormHostPrototype.php
+++ ui/tests/selenium/hosts/testFormHostPrototype.php
@@ -990,9 +990,10 @@ class testFormHostPrototype extends CLegacyWebTest {
 
 		$form = $this->query('name:zbx_filter')->asForm()->waitUntilReady()->one();
 		$form->fill(['Name' => $host]);
+		$table = $this->query('class:list-table')->asTable()->one();
 		$this->query('button:Apply')->one()->waitUntilClickable()->click();
-		$this->query('xpath://table[@class="list-table"]')->asTable()->one()->findRow('Name', $host)
-				->getColumn('Discovery')->query('link:Discovery')->one()->click();
+		$table->waitUntilReloaded();
+		$table->findRow('Name', $host)->getColumn('Discovery')->query('link:Discovery')->one()->click();
 
 		$this->zbxTestClickLinkTextWait($discovery_rule);
 		$this->zbxTestClickLinkTextWait('Host prototypes');
