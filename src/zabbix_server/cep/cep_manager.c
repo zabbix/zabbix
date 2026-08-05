53dad427d5a4afde196a1ae32418afa576a9016f
Andris Zeila <andris.zeila@zabbix.com>
2026-08-05 14:31:56 +0300

    ........S. [ZBXNEXT-10353] restored debug logging in cep manager

--- src/zabbix_server/cep/cep_manager.c
+++ src/zabbix_server/cep/cep_manager.c
@@ -867,6 +867,22 @@ void	*zbx_cep_manager_thread(void *args)
 			time_stat = time_start;
 			time_idle = 0;
 			stats = stats_tmp;
+
+			/* WDN remove debug logging */
+			zbx_cep_t	*cep;
+			int	loglevel = zbx_set_log_level(LOG_LEVEL_TRACE);
+
+			cep_cache_acquire(&cep);
+			cep_dump(cep, "========");
+			cep_cache_release(&cep);
+
+			zbx_cep_window_pool_t	*pool;
+
+			cep_window_pool_acquire(&pool);
+			cep_window_pool_dump(pool);
+			cep_window_pool_release(&pool);
+
+			zbx_set_log_level(loglevel);
 		}
 
 		zbx_update_selfmon_counter(info, ZBX_PROCESS_STATE_IDLE);
