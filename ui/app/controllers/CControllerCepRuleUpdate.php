6bf6b4ccc0c6ad7290c380c1e6e21d49bfe26daf
Martins Talbergs <martins.talbergs@zabbix.com>
2026-08-06 10:07:02 +0300

    ..F....... [ZBXNEXT-10353] removed debug code

--- ui/app/controllers/CControllerCepRuleUpdate.php
+++ ui/app/controllers/CControllerCepRuleUpdate.php
@@ -203,11 +203,6 @@ class CControllerCepRuleUpdate extends CControllerCepRuleGeneral {
 			'stop' => ['db cep_rule.stop', 'required', 'in' => [CCepRuleHelper::EXECUTION_CONTINUE,
 				CCepRuleHelper::EXECUTION_STOP
 			]],
-			'sortorder' => ['db cep_rule.sortorder', 'required',
-				'use' => [CNumberValidator::class, ['min' => 1, 'max' => ZBX_MAX_INT64, 'with_float' => false,
-					'usermacros' => false, 'lldmacros' => false
-				]]
-			],
 			'sortorder' => ['db cep_rule.sortorder', 'required', 'min' => 1, 'max' => ZBX_MAX_INT32],
 			'description' => ['db cep_rule.description'],
 			'status' => ['db cep_rule.status', 'required',
