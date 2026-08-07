bce404d651eb56cb2452459c2cdecdce214a67a9
Andris Zeila <andris.zeila@zabbix.com>
2026-08-07 15:22:24 +0300

    ........S. [ZBXNEXT-10353] changed execute_when constant values

--- include/zbxcacheconfig.h
+++ include/zbxcacheconfig.h
@@ -1740,10 +1740,10 @@ zbx_vector_correlation_ptr_t	*zbx_correlation_config_get_correlations(zbx_correl
 #define ZBX_CEP_OP_UNSUPPRESS		19
 
 #define ZBX_CEP_WHEN_EVENT_OCCURRED	0
-#define ZBX_CEP_WHEN_EVENT_EVICTED	1
-#define ZBX_CEP_WHEN_WINDOW_CLOSED	2
-#define ZBX_CEP_WHEN_PATTERN_MATCH	3
-#define ZBX_CEP_WHEN_EVENT_ADDED	4
+#define ZBX_CEP_WHEN_EVENT_ADDED	1
+#define ZBX_CEP_WHEN_EVENT_EVICTED	2
+#define ZBX_CEP_WHEN_WINDOW_CLOSED	3
+#define ZBX_CEP_WHEN_PATTERN_MATCH	4
 
 typedef struct
 {
