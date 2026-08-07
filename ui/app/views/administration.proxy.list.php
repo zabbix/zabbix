7640acbe8e306ab95f23a1ceefec49ecc04b1522
Sergejs Cernisenko <sergejs.cernisenko@zabbix.com>
2026-08-07 15:22:39 +0300

    ..F....... [ZBXNEXT-10730] added global constant for Zabbix Cloud URL

--- ui/app/views/administration.proxy.list.php
+++ ui/app/views/administration.proxy.list.php
@@ -269,7 +269,7 @@ $form->addItem([
 	->setControls(
 		(new CTag('nav', true,
 			(new CList())
-				->addItem((new CLink(_('Create proxy in Cloud'), 'https://cloud.zabbix.com/create-proxy'))
+				->addItem((new CLink(_('Create proxy in Cloud'), ZBX_CREATE_PROXY_IN_CLOUD_URL))
 					->addClass(ZBX_STYLE_LINK_EXTERNAL)
 					->addClass(ZBX_STYLE_BTN)
 					->addClass(ZBX_STYLE_BTN_ALT)
