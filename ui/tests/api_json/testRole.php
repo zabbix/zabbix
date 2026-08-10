5cc6638af79c27c25c4a388f0590a20bbd256836
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2026-08-10 13:02:12 +0300

    .......... [DEV-5089] fixed API test failures

--- ui/tests/api_json/testRole.php
+++ ui/tests/api_json/testRole.php
@@ -216,7 +216,7 @@ class testRole extends CAPITest {
 								'status' => '1'
 							],
 							[
-								'name' => 'configuration.event_correlation',
+								'name' => 'configuration.ceprules',
 								'status' => '1'
 							],
 							[
@@ -934,7 +934,7 @@ class testRole extends CAPITest {
 									'status' => '1'
 								],
 								[
-									'name' => 'configuration.event_correlation',
+									'name' => 'configuration.ceprules',
 									'status' => '1'
 								],
 								[
