f56c57134fd922ab8dda96a9fc1d7f30f90c2e9a
Martins Talbergs <martins.talbergs@zabbix.com>
2026-08-11 16:26:13 +0300

    ..F....... [ZBXNEXT-10353] Revert "moved deprecation warning into view.php"
    This reverts commit 74e57a7fb976dad567aaa39ea245a08276c33f9d.

--- ui/app/views/correlation.edit.php
+++ ui/app/views/correlation.edit.php
@@ -440,17 +440,10 @@ else {
 	];
 }
 
-$deprecation_warning = makeMessageBox(
-	class: ZBX_STYLE_MSG_WARNING,
-	messages: [],
-	title: _('Global event correlation is deprecated and may be removed in next releases.'),
-	show_close_box: false
-);
-
 $output = [
 	'header' => $data['correlation']['correlationid'] === null ? _('New event correlation') : _('Event correlation'),
 	'doc_url' => CDocHelper::getUrl(CDocHelper::DATA_COLLECTION_CORRELATION_EDIT),
-	'body' => $deprecation_warning->toString().$form->toString(),
+	'body' => $form->toString(),
 	'buttons' => $buttons,
 	'script_inline' => getPagePostJs().
 		$this->readJsFile('correlation.edit.js.php').
--- ui/app/views/js/correlation.edit.js.php
+++ ui/app/views/js/correlation.edit.js.php
@@ -23,6 +23,10 @@ window.correlation_edit_popup = new class {
 
 	init({rules, clone_rules, conditions}) {
 		this.overlay = overlays_stack.getById('correlation.edit');
+		this.#showWarning(<?= json_encode(
+			_('Global event correlation is deprecated and may be removed in next releases.')
+		) ?>);
+
 		this.dialogue = this.overlay.$dialogue[0];
 		this.footer = this.overlay.$dialogue.$footer[0];
 		this.form_element = this.overlay.$dialogue.$body[0].querySelector('form');
@@ -327,5 +331,12 @@ window.correlation_edit_popup = new class {
 		const message_box = makeMessageBox('bad', messages, title)[0];
 
 		this.form_element.parentNode.insertBefore(message_box, this.form_element);
+	}
+
+	#showWarning(message) {
+		const message_node = makeMessageBox('warning', null, message, false)[0];
+
+		this.overlay.$dialogue.$body[0]
+			.insertAdjacentElement('beforebegin', message_node)
 	}
 };
