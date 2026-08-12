0f992167d48c344961f6d3408c527a9b4f2c3c5f
Romans Trizna <romans.trizna@zabbix.com>
2026-08-12 14:03:24 +0300

    ...G...PS. [ZBX-27227] fixed style

--- src/libs/zbxexec/execute.c
+++ src/libs/zbxexec/execute.c
@@ -19,7 +19,7 @@
 #include "zbxlog.h"
 
 /* the size of temporary buffer used to read from output stream */
-#define PIPE_BUFFER_SIZE	65536
+#define PIPE_BUFFER_SIZE	4096
 
 #ifdef _WINDOWS
 
@@ -65,10 +65,11 @@ static int	zbx_get_timediff_ms(struct _timeb *time1, struct _timeb *time2)
 static int	zbx_read_from_pipe(HANDLE hRead, HANDLE hProcess, char **buf, size_t *buf_size, size_t *offset,
 			int timeout_ms, char *error, size_t max_error_len)
 {
+#define ZBX_PIPE_READ_BUFFER_SIZE	65536
 	DWORD		in_buf_size = 0, read_bytes = 0;
 	DWORD		process_code = STILL_ACTIVE;
 	struct _timeb	start_time, current_time;
-	char 		tmp_buf[PIPE_BUFFER_SIZE];
+	char 		tmp_buf[ZBX_PIPE_READ_BUFFER_SIZE];
 
 	_ftime(&start_time);
 
@@ -128,6 +129,7 @@ static int	zbx_read_from_pipe(HANDLE hRead, HANDLE hProcess, char **buf, size_t 
 			(unsigned long)GetLastError(), *offset);
 
 	return SUCCEED;
+#undef ZBX_PIPE_READ_BUFFER_SIZE
 }
 
 #else	/* not _WINDOWS */
@@ -366,7 +368,7 @@ int	zbx_execute(const char *command, char **output, char *error, size_t max_erro
 	sa.lpSecurityDescriptor = NULL;
 
 	/* create a pipe for the child process's STDOUT */
-	if (0 == CreatePipe(&hRead, &hWrite, &sa, PIPE_BUFFER_SIZE))
+	if (0 == CreatePipe(&hRead, &hWrite, &sa, 0))
 	{
 		zbx_snprintf(error, max_error_len, "unable to create a pipe: %s",
 				zbx_strerror_from_system(GetLastError()));
