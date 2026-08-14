2fe40dff7862533636e5a9052f2f193b306a519a
Martins Talbergs <martins.talbergs@zabbix.com>
2026-08-14 15:34:35 +0300

    ..F....... [ZBXNEXT-10353] Revert "fixed default trigger type"
    This reverts commit 4d2a7f9537a407b37800f99968fa328846bb5726.

--- ui/app/views/popup.generic.php
+++ ui/app/views/popup.generic.php
@@ -378,10 +378,6 @@ switch ($data['popup_type']) {
 				array_pop($description);
 			}
 
-			if (!array_key_exists('state', $trigger)) {
-				$trigger['state'] = null;
-			}
-
 			$table->addRow([
 				$check_box,
 				$description,
