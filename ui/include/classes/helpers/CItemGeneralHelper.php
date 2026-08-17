97b1d11c1c6a003ce0ad89b16b0cc33d63345d97
Ginta Berzina <ginta.berzina@zabbix.com>
2026-08-17 11:18:59 +0300

    ..F....... [ZBXNEXT-9851] fixed to default delay check for only lld rule flag

--- ui/include/classes/helpers/CItemGeneralHelper.php
+++ ui/include/classes/helpers/CItemGeneralHelper.php
@@ -326,8 +326,7 @@ JAVASCRIPT;
 			if ($delay == 0 && ($item['type'] == ITEM_TYPE_TRAPPER || $item['type'] == ITEM_TYPE_SNMPTRAP
 					|| $item['type'] == ITEM_TYPE_DEPENDENT || $item['type'] == ITEM_TYPE_NESTED
 					|| ($item['type'] == ITEM_TYPE_ZABBIX_ACTIVE && strncmp($item['key'], 'mqtt.get', 8) == 0))) {
-
-				if ($item['flags'] & ZBX_FLAG_DISCOVERY_RULE || $item['flags'] & ZBX_FLAG_DISCOVERY_CREATED) {
+				if ($item['flags'] & ZBX_FLAG_DISCOVERY_RULE) {
 					$item['delay'] = ZBX_LLD_RULE_DELAY_DEFAULT;
 				}
 				else {
