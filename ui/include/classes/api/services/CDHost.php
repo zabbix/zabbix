cf03459af5f437385bdb748262f1d448763632b7
Maksym Snihach <maksym.snihach@zabbix.com>
2026-08-18 16:03:42 +0200

    A......... [ZBXNEXT-9185] added checking permissions for dhost.get() and dservice.get() for the corresponding proxy

--- ui/include/classes/api/services/CDHost.php
+++ ui/include/classes/api/services/CDHost.php
@@ -140,6 +140,15 @@ class CDHost extends CApiService {
 		if (zbx_ctype_digit($options['limit']) && $options['limit']) {
 			$sqlParts['limit'] = $options['limit'];
 		}
+
+		if (self::$userData['type'] != USER_TYPE_SUPER_ADMIN) {
+			$sqlParts['join']['dr'] = ['table' => 'drules', 'using' => 'druleid'];
+			$sqlParts['join']['p'] = ['type' => 'left', 'left_table' => 'dr', 'table' => 'proxy', 'using' => 'proxyid'];
+			$sqlParts['where'][] = '('.
+				'dr.proxyid IS NULL'.
+				' OR '.CApiUserGroupHelper::getProxyPermissionsCondition('p').
+				')';
+		}
 //-------
 
 		$sqlParts = $this->applyQueryOutputOptions($this->tableName(), $this->tableAlias(), $options, $sqlParts);
