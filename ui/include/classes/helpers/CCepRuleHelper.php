cffe6d05aa996c9a5fb1e647bd6093a3770d7a9f
Martins Talbergs <martins.talbergs@zabbix.com>
2026-08-18 14:55:37 +0300

    ..F....... [ZBXNEXT-10353] added missing execute_when Event added to window

--- ui/include/classes/helpers/CCepRuleHelper.php
+++ ui/include/classes/helpers/CCepRuleHelper.php
@@ -346,7 +346,8 @@ class CCepRuleHelper {
 			self::WHEN_EVENT_OCCURRED => _('Event occured'),
 			self::WHEN_EVENT_EVICTED => _('Event evicted'),
 			self::WHEN_WINDOW_CLOSED => _('Window closed'),
-			self::WHEN_PATTERN_MATCHED => _('Event pattern matched')
+			self::WHEN_PATTERN_MATCHED => _('Event pattern matched'),
+			self::WHEN_EVENT_ADDED => _('Event added to window')
 		];
 	}
 
