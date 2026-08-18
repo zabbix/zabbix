1e0ca631daf8041465d54b25482ddd497c2b592c
Maksym Snihach <maksym.snihach@zabbix.com>
2026-08-18 16:25:51 +0200

    A......... [ZBXNEXT-9185] added checking nopermissions option for dcheck.get

--- ui/include/classes/api/services/CDCheck.php
+++ ui/include/classes/api/services/CDCheck.php
@@ -114,7 +114,7 @@ class CDCheck extends CApiService {
 			$sqlParts['limit'] = $options['limit'];
 		}
 
-		if (self::$userData['type'] != USER_TYPE_SUPER_ADMIN) {
+		if (self::$userData['type'] != USER_TYPE_SUPER_ADMIN && !$options['nopermissions']) {
 			$sqlParts['join']['dr'] = ['table' => 'drules', 'using' => 'druleid'];
 			$sqlParts['join']['p'] = ['type' => 'left', 'left_table' => 'dr', 'table' => 'proxy', 'using' => 'proxyid'];
 			$sqlParts['where'][] = '('.
