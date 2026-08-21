ab2bb02c27976bc974fb3a2b6d291982337f01a8
Martins Talbergs <martins.talbergs@zabbix.com>
2026-08-21 13:19:12 +0300

    ..F....... [ZBXNEXT-10353] fixed document element focus

--- ui/app/partials/ceprule.window.php
+++ ui/app/partials/ceprule.window.php
@@ -79,7 +79,7 @@ zbx_add_post_js($tags->getPostJS());
 		)
 	)
 
-	->addItem((new CLabel(_('Group by'), 'ceprule-window-groupby-opt-group'))->setAsteriskMark()->setId('ceprule-groupby-label'))
+	->addItem((new CLabel(_('Group by'), 'ceprule-window-groupby'))->setAsteriskMark()->setId('ceprule-groupby-label'))
 	->addItem(new CFormField((new CList([
 		(new CListItem())
 			->addItem((new CCheckBox('window[group_by_host_group]', CCepRuleHelper::GROUP_BY_YES))
