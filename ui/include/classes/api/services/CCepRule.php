68a92efe5b846e06c842c5a3e93b4fc57b6ce459
Vladimirs Maksimovs <vladimirs.maksimovs@zabbix.com>
2026-08-21 15:44:33 +0300

    A......... [ZBXNEXT-10353] fixed inability to add suppress operation for CEP rule

--- ui/include/classes/api/services/CCepRule.php
+++ ui/include/classes/api/services/CCepRule.php
@@ -692,7 +692,7 @@ class CCepRule extends CApiService {
 										['else' => true, 'type' => API_INT32, 'in' => DB::getDefault('cep_operation', 'severity')]
 			]],
 			'suppress_duration' =>	['type' => API_MULTIPLE, 'rules' => [
-										['if' => ['field' => 'type', 'in' => implode(',', [CCepRuleHelper::OP_SUPPRESS])], 'type' => API_TIME_UNIT | API_TIME_UNIT_WITH_YEAR, 'flags' => API_REQUIRED, 'length' => DB::getFieldLength('cep_operation', 'suppress_duration')],
+										['if' => ['field' => 'type', 'in' => implode(',', [CCepRuleHelper::OP_SUPPRESS])], 'type' => API_TIME_UNIT, 'flags' => API_REQUIRED | API_TIME_UNIT_WITH_YEAR, 'length' => DB::getFieldLength('cep_operation', 'suppress_duration')],
 										['else' => true, 'type' => API_STRING_UTF8, 'in' => DB::getDefault('cep_operation', 'suppress_duration')]
 			]],
 			'tag' =>				['type' => API_MULTIPLE, 'rules' => [
