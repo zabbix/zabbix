9e85f210d0fabd801fa85748c284b7bac617822f
Continuous Integration <ci@zabbix.com>
2026-08-25 11:42:26 +0300

    .......... [ZBXNEXT-826] updated release date and version for 7.4.14

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -17,9 +17,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "18 August 2026";
+	static final String REVISION_DATE = "25 August 2026";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "7.4.14rc1";
+	static final String VERSION = "7.4.14";
 
 	static void printVersion()
 	{
