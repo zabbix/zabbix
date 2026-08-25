66e37052bd6d04216876b2c75a8d61393f1d6f58
Martins Talbergs <martins.talbergs@zabbix.com>
2026-08-25 10:28:10 +0300

    ..F....... [ZBXNEXT-10353] corrected execute when options order

--- ui/include/classes/helpers/CCepRuleHelper.php
+++ ui/include/classes/helpers/CCepRuleHelper.php
@@ -373,8 +373,8 @@ class CCepRuleHelper {
 			self::WHEN_EVENT_OCCURRED => _('Event occurred'),
 			self::WHEN_EVENT_ADDED => _('Event added to window'),
 			self::WHEN_EVENT_EVICTED => _('Event evicted'),
-			self::WHEN_WINDOW_CLOSED => _('Window closed'),
-			self::WHEN_PATTERN_MATCHED => _('Pattern matched')
+			self::WHEN_PATTERN_MATCHED => _('Pattern matched'),
+			self::WHEN_WINDOW_CLOSED => _('Window closed')
 		];
 	}
 
