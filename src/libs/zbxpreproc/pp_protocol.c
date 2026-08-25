405652e2a9b3009445861703672ba920f7eaad05
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2026-08-25 12:09:02 +0300

    .......... [DEV-5129] correct BIN usage in preprocessing

--- src/libs/zbxpreproc/pp_protocol.c
+++ src/libs/zbxpreproc/pp_protocol.c
@@ -1159,7 +1159,7 @@ void	zbx_preprocess_item_value(zbx_uint64_t itemid, unsigned char item_value_typ
 
 			/* Meta (lastlogsize/mtime) comes from a log-reading item KEY like: log or eventlog. */
 			/* It is not dependent on the item's configured VALUE TYPE.                          */
-			if (0 == ZBX_ISSET_VALUE(result))
+			if (0 == ZBX_ISSET_VALUE(result) && 0 == ZBX_ISSET_BIN(result))
 			{
 				zbx_dc_add_history(itemid, item_value_type, item_flags, result, ts, state, error);
 				goto out;
