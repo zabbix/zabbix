51d5db78254c46a1c2f66b492a911dca950e448c
Andris Zeila <andris.zeila@zabbix.com>
2026-08-26 10:20:53 +0300

    ........S. [ZBXNEXT-10353] fixed includes

--- src/zabbix_server/diag/diag_server.c
+++ src/zabbix_server/diag/diag_server.c
@@ -11,7 +11,6 @@
 ** You should have received a copy of the GNU Affero General Public License along with this program.
 ** If not, see <https://www.gnu.org/licenses/>.
 **/
-#include "zbxjson.h"
 #include "diag_server.h"
 #include "zbxdiag.h"
 
