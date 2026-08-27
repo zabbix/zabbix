a06391094b3250cfff3c2e8c5290870382533753
Sergejs Cernisenko <sergejs.cernisenko@zabbix.com>
2026-08-27 16:36:22 +0300

    ..F....... [ZBX-28060] removed unnecessary spaces

--- ui/include/classes/helpers/CItemHelper.php
+++ ui/include/classes/helpers/CItemHelper.php
@@ -88,7 +88,7 @@ class CItemHelper extends CItemGeneralHelper {
 				&& !array_key_exists('inaccessible', $item['valuemap'])
 				&& bccomp($item['valuemap']['hostid'], $item['hostid']) != 0) {
 			$templates = API::Template()->get(['output' => ['name'], 'templateids' => $item['valuemap']['hostid']]);
-			$item['valuemap']['prefix'] = $templates[0]['name'] . NAME_DELIMITER;
+			$item['valuemap']['prefix'] = $templates[0]['name'].NAME_DELIMITER;
 		}
 
 		if ($update_interval_parser->parse($item['delay']) == CParser::PARSE_SUCCESS) {
