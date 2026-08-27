4094166ad3cedb84413745636b3f1c230f1c70bd
Vladislavs Sokurenko <vladislavs.sokurenko@zabbix.com>
2026-08-27 15:38:26 +0300

    .D......S. [ZBX-28016] added ChangeLog entry

--- src/libs/zbxeval/misc.c
+++ src/libs/zbxeval/misc.c
@@ -518,7 +518,7 @@ static int	eval_has_usermacro(const char *str, size_t len)
  *                                                                            *
  * Parameters: itemquery    - [IN]                                            *
  *             len          - [IN] item query length                          *
- *             out          - [OUT] item query with expanded macros           *
+ *             out          - [IN/OUT] item query with expanded macros        *
  *             error        - [OUT] Error message, optional. If specified,    *
  *                                  the function will return failure at the   *
  *                                  first failed macro expansion.             *
