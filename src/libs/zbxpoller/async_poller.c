53f86a6dfa8055f64b09a7edaa1cfceac60695ad
Mikhail Korotkov <mikhail.korotkov@zabbix.com>
2026-08-27 17:22:06 +0300

    .......PS. [ZBXNEXT-10249] added item key and host in logs about exceeding telemetry query result row limit

--- src/libs/zbxpoller/async_poller.c
+++ src/libs/zbxpoller/async_poller.c
@@ -287,7 +287,9 @@ static void	process_telemetry_query_result(CURL *easy_handle, CURLcode err, void
 			if (SUCCEED_PARTIAL == parse_ret)
 			{
 				zabbix_log(LOG_LEVEL_WARNING,
-						"telemetry query result row limit exceeded, result was truncated");
+						"telemetry query result row limit exceeded for item \"%s:%s\", "
+						"result was truncated", item_context->host_host,
+						item_context->key_orig);
 			}
 
 			status = SUCCEED;
