587160869b2080423e172f994ab4d8ebc7582d5d
Jevgenijs Burlakovs <jevgenijs.burlakovs@zabbix.com>
2026-08-28 12:01:45 +0300

    .......PS. [ZBXNEXT-10745] Made diaginfo=preprocessing output more correctly distinguish for cumulative counters

--- src/libs/zbxpreproc/pp_diag.c
+++ src/libs/zbxpreproc/pp_diag.c
@@ -103,10 +103,10 @@ int	zbx_diag_add_preproc_info(const struct zbx_json_parse *jp, struct zbx_json *
 				zbx_json_adduint64(json, "finished tasks", finished_num);
 				zbx_json_adduint64(json, "task sequences", sequences_num);
 				zbx_json_adduint64(json, "finished count", processed_num);
-				zbx_json_adduint64(json, "queued count", queued_num);
-				zbx_json_adduint64(json, "queued size", queued_sz);
-				zbx_json_adduint64(json, "direct count", direct_num);
-				zbx_json_adduint64(json, "direct size", direct_sz);
+				zbx_json_adduint64(json, "queued cumulative count", queued_num);
+				zbx_json_adduint64(json, "queued cumulative size", queued_sz);
+				zbx_json_adduint64(json, "direct cumulative count", direct_num);
+				zbx_json_adduint64(json, "direct cumulative size", direct_sz);
 				zbx_json_adduint64(json, "history size", history_sz);
 				zbx_json_adduint64(json, "pending tasks peak", pending_peak_num);
 				zbx_json_adduint64(json, "finished tasks peak", finished_peak_num);
