00bce4d4e27bb647ddcc82e6c14ea4e0419a6380
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2026-08-31 16:33:10 +0300

    ..F....... [ZBXNEXT-9962] removed unused code

--- ui/app/controllers/CControllerUserProfileEdit.php
+++ ui/app/controllers/CControllerUserProfileEdit.php
@@ -30,7 +30,7 @@ class CControllerUserProfileEdit extends CControllerUserEditGeneral {
 
 		$users = API::User()->get([
 			'output' => ['username', 'name', 'surname', 'lang', 'theme', 'autologin', 'autologout', 'refresh',
-				'rows_per_page', 'url', 'timezone', 'provisioned', 'roleid'
+				'rows_per_page', 'url', 'timezone', 'provisioned'
 			],
 			'userids' => CWebUser::$data['userid'],
 			'editable' => true
