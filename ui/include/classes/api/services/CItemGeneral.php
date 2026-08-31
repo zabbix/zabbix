407e22e9a3b8e5ae4c89189db604dd278c926e8b
Gregory Chalenko <gregory.chalenko@zabbix.com>
2026-08-31 08:35:12 +0300

    A......... [DEV-4930] fixed inherited item and itemprototype validation for fields granularity and lookback_limit

--- ui/include/classes/api/services/CItemGeneral.php
+++ ui/include/classes/api/services/CItemGeneral.php
@@ -211,6 +211,10 @@ abstract class CItemGeneral extends CApiService {
 					$item += array_intersect_key($db_item, array_flip(['publickey', 'privatekey']));
 				}
 
+				if ($item['type'] == ITEM_TYPE_TELEMETRY_QUERY) {
+					$item += array_intersect_key($db_item, array_flip(['granularity', 'lookback_limit']));
+				}
+
 				$api_input_rules['fields'] += $item_type::getUpdateValidationRulesInherited($db_item);
 			}
 			elseif (in_array($db_item['flags'], [ZBX_FLAG_DISCOVERY_CREATED, ZBX_FLAG_DISCOVERY_RULE_CREATED])) {
