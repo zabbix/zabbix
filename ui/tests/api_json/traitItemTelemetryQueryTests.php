2dc598b72976eb3ab8638919b4d1a6590d6ee683
Gregory Chalenko <gregory.chalenko@zabbix.com>
2026-09-01 13:47:04 +0300

    .......... [DEV-4930] fixed api test error message for discovered item test; added test for query update for discovered item

--- ui/tests/api_json/traitItemTelemetryQueryTests.php
+++ ui/tests/api_json/traitItemTelemetryQueryTests.php
@@ -196,21 +196,28 @@ trait traitItemTelemetryQueryTests {
 			[
 				'time_shift' => '{$M}'
 			],
-			'Invalid parameter "/1": unexpected parameter "time_shift".'
+			'Invalid parameter "/1": cannot update readonly parameter "time_shift" of discovered object.'
 		];
 
 		yield '"lookback_limit" fail' => [
 			[
 				'lookback_limit' => '{$M}'
 			],
-			'Invalid parameter "/1": unexpected parameter "lookback_limit".'
+			'Invalid parameter "/1": cannot update readonly parameter "lookback_limit" of discovered object.'
 		];
 
 		yield '"granularity" fail' => [
 			[
 				'granularity' => '{$M}'
 			],
-			'Invalid parameter "/1": unexpected parameter "granularity".'
+			'Invalid parameter "/1": cannot update readonly parameter "granularity" of discovered object.'
+		];
+
+		yield '"query" fail' => [
+			[
+				'query' => []
+			],
+			'Invalid parameter "/1": cannot update readonly parameter "query" of discovered object.'
 		];
 	}
 
