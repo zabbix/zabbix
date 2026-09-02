b96a7e3a4b8188352fc0765b976cd00eef1fda47
Oskars Pucka <oskars.pucka@zabbix.com>
2026-09-02 14:52:04 +0300

    ...G...... [ZBXNEXT-10451] added nonempty tag to profiler config params

--- src/go/internal/agent/options_nix.go
+++ src/go/internal/agent/options_nix.go
@@ -46,10 +46,10 @@ type AgentOptions struct {
 	UserParameterDir           string   `conf:"optional"`
 	ControlSocket              string   `conf:"optional"`
 	Alias                      []string `conf:"optional"`
-	EnableProfiler             int      `conf:"optional,range=0:1,default=0"`
+	EnableProfiler             int      `conf:"optional,nonempty,range=0:1,default=0"`
 	ProfilerDir                string   `conf:"optional,nonempty,default=/tmp/zabbix_agent2_profiles"`
-	ProfilerMaxFilesPerProfile int      `conf:"optional,range=1:100,default=10"`
-	ProfilerInterval           int      `conf:"optional,range=1:86400,default=3600"`
+	ProfilerMaxFilesPerProfile int      `conf:"optional,nonempty,range=1:100,default=10"`
+	ProfilerInterval           int      `conf:"optional,nonempty,range=1:86400,default=3600"`
 	TLSConnect                 string   `conf:"optional,nonempty"`
 	TLSAccept                  string   `conf:"optional,nonempty"`
 	TLSPSKIdentity             string   `conf:"optional,nonempty"`
