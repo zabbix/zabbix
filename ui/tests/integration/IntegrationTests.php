237795bd67a0b906a58da2b9ab306a9a3d0e9484
Romans Trizna <romans.trizna@zabbix.com>
2026-09-10 11:13:49 +0300

    ........S. [ZBX-26411] fixed report manager failing to send scheduled reports when media types had been renamed
    * commit 'cf059bca78c1ad86a23e0c04fc4e0a88eba6772a':
      ........S. [ZBX-26411] fixed style
      ........S. [ZBX-26411] updated scheduled report integration test baseline timings
      ........S. [ZBX-26411] fixed report manager failing to send scheduled reports when media types had been renamed
    
    (cherry picked from commit e8d63a3ecf58db59b0eaa4feea9bca981e9cf632)

--- ui/tests/integration/IntegrationTests.php
+++ ui/tests/integration/IntegrationTests.php
@@ -67,6 +67,7 @@ require_once dirname(__FILE__).'/testLLDHistorySyncAtScale.php';
 /* require_once dirname(__FILE__).'/testLLDHistorySyncAtScaleSingleSyncer.php'; can be enabled to test with single history syncer */
 require_once dirname(__FILE__).'/testHousekeepingConfSync.php';
 require_once dirname(__FILE__).'/testTelnetChecks.php';
+require_once dirname(__FILE__).'/testScheduledReports.php';
 
 use PHPUnit\Framework\TestSuite;
 
@@ -130,6 +131,7 @@ class IntegrationTests {
 		/* $suite->addTestSuite('testLLDHistorySyncAtScaleSingleSyncer'); */
 		$suite->addTestSuite('testHousekeepingConfSync');
 		$suite->addTestSuite('testTelnetChecks');
+		$suite->addTestSuite('testScheduledReports');
 		return $suite;
 	}
 }
