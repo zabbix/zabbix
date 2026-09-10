44fa4e7a99a318c8b004b705427a2c83b7f2a3e7
Artem Luchko <artem.luchko@zabbix.com>
2026-09-10 15:43:42 +0200

    ..F....... [ZBXNEXT-8339] improved code logic and flow

--- ui/app/controllers/CControllerMaintenanceEdit.php
+++ ui/app/controllers/CControllerMaintenanceEdit.php
@@ -243,9 +243,7 @@ class CControllerMaintenanceEdit extends CController {
 		$hostids = [];
 		foreach ($db_triggers as $trigger) {
 			foreach ($trigger['hosts'] as $host) {
-				if (!array_key_exists($host['hostid'], $hostids)) {
-					$hostids[$host['hostid']] = true;
-				}
+				$hostids[$host['hostid']] = true;
 			}
 		}
 
