e8127692be95dc4dd304b1d86acd5c477d2e23d0
Dmitrijs Fofanovs <dmitrijs.fofanovs@zabbix.com>
2026-09-11 10:22:04 +0300

    .......... [ZBXNEXT-10083] fixed typo
    (cherry picked from commit 2ac9e475231b9619ef51f609412c663a2162d17b)

--- ui/include/classes/validators/CUrlValidator.php
+++ ui/include/classes/validators/CUrlValidator.php
@@ -34,7 +34,7 @@ class CUrlValidator extends CValidator {
 	 *   'schemes'            If not null, the URL scheme will be validated against the provided list.
 	 *                        The list is expected to contain schemes in lower case.
 	 *                        Scheme validation won't take place if the URL does not contain a scheme component.
-	 *    'required_scheme'   If true, the URL must contain scheme.
+	 *   'required_scheme'    If true, the URL must contain scheme.
 	 *
 	 * @var array
 	 */
