3ba9bd39d5c512b447e530dbf4ea2f518e819659
Gregory Chalenko <gregory.chalenko@zabbix.com>
2026-09-16 16:16:28 +0300

    .......... [DEV-4930] updated prepareQueryFilterForDb description

--- ui/include/classes/api/item_types/CItemTypeTelemetryQuery.php
+++ ui/include/classes/api/item_types/CItemTypeTelemetryQuery.php
@@ -326,9 +326,8 @@ class CItemTypeTelemetryQuery extends CItemType {
 	}
 
 	/**
-	 * Serialize "item.query" to JSON string.
-	 * Convert "item.query.filter.formula" from "A or B" like notation to "{0} or {1}" notation
-	 * removing "formulaid" property for each condition.
+	 * Serialize "item.query" to JSON string. Additionally make following steps:
+	 * - "query.aggregated_columns[].parameters[]" for "function" AGGREGATE_PERCENTILE converted to string, server requires each parameter to be a string
 	 *
 	 * @param array $query  Array for "item.query" configuration
 	 */
