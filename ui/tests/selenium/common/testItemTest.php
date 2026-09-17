8131b2a38d4848f561a2702903ca93ea55bf1c50
Sanija Smildzina <sanija.smildzina@zabbix.com>
2026-09-17 13:42:57 +0300

    .......... [DEV-5177] made change after code review, readded the check of proxy multiselect but being in disabled state now

--- ui/tests/selenium/common/testItemTest.php
+++ ui/tests/selenium/common/testItemTest.php
@@ -1016,6 +1016,11 @@ class testItemTest extends CWebTest {
 							'test_with' => 'Server'
 						];
 
+						if ($is_host) {
+							$fields_value['proxy'] = '';
+							$fields_state['proxy'] = false;
+						}
+
 						break;
 				}
 
