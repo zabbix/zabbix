06f9d77810e5f7085c9b8c64c1577b0f18ec4088
Georgijs Klavins <georgijs.klavins@zabbix.com>
2026-09-17 12:17:01 +0300

    ..F....... [DEV-5016] fixed code style

--- ui/js/class.form.js
+++ ui/js/class.form.js
@@ -168,7 +168,7 @@ class CForm {
 		return Object.values(all_fields).filter(x => !Object.values(sub_fields).includes(x));
 	}
 
-	getAllValues(raw_fields = []) {
+	getAllValues(untrimmed_fields = []) {
 		let result = Object.create(null);
 		let simple_fields = Object.create(null);
 
@@ -185,7 +185,7 @@ class CForm {
 				}
 			}
 			else {
-				simple_fields[key] = raw_fields?.includes(field.getName()) ? field.getValue() : field.getValueTrimmed();
+				simple_fields[key] = untrimmed_fields.includes(field.getName()) ? field.getValue() : field.getValueTrimmed();
 			}
 		}
 
