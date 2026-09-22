e7da53eea214a08118288b78c3529a6d6bf45746
Continuous Integration <ci@zabbix.com>
2026-09-22 09:33:12 +0300

    .......... [ZBXNEXT-826] updated release date and version for 7.0.31

--- ui/include/defines.inc.php
+++ ui/include/defines.inc.php
@@ -13,7 +13,7 @@
 ** If not, see <https://www.gnu.org/licenses/>.
 **/
 
-define('ZABBIX_VERSION',		'7.0.31rc1');
+define('ZABBIX_VERSION',		'7.0.31');
 define('ZABBIX_API_VERSION',	'7.0.31');
 define('ZABBIX_EXPORT_VERSION',	'7.0');
 
