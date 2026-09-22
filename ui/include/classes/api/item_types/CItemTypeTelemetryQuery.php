62f145d4215d6859191092348d0dae54d58146e8
Gregory Chalenko <gregory.chalenko@zabbix.com>
2026-09-22 15:07:04 +0300

    A......... [DEV-4930] coding style fix of fields order

--- ui/include/classes/api/item_types/CItemTypeTelemetryQuery.php
+++ ui/include/classes/api/item_types/CItemTypeTelemetryQuery.php
@@ -24,7 +24,7 @@ class CItemTypeTelemetryQuery extends CItemType {
 	/**
 	 * @inheritDoc
 	 */
-	public const FIELD_NAMES = ['time_shift', 'lookback_limit', 'granularity', 'query', 'delay', 'timeout'];
+	public const FIELD_NAMES = ['time_shift', 'lookback_limit', 'granularity', 'query', 'timeout', 'delay'];
 
 	public const SIGNAL_TYPE_TRACES = 0;
 	public const SIGNAL_TYPE_METRICS = 1;
