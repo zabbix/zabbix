7c9942a84758e99eb84a52ee719c781557b07434
Natalja Romancaka <natalja.romancaka@zabbix.com>
2026-09-25 12:48:44 +0300

    .......... [ZBXNEXT-826] updated release version to 7.0.32rc1

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -19,7 +19,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "22 September 2026";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "7.0.31";
+	static final String VERSION = "7.0.32rc1";
 
 	static void printVersion()
 	{
